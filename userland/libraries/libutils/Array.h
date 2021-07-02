2f23c1c77a5cd76424233117969f399d3acbbb48
krishpranav <krisna.pranav@gmail.com>
2021-07-02 19:52:38 +0530

    Array headers

--- userland/libraries/libutils/Array.h
+++ userland/libraries/libutils/Array.h
@@ -1,5 +1,12 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
 #pragma once
 
+// includes
 #include <libmath/MinMax.h>
 #include <libutils/Assert.h>
 
