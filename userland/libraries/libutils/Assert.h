7a27b12b219c456d83d277851550ba1de844f8a5
krishpranav <krisna.pranav@gmail.com>
2021-07-02 19:57:02 +0530

    not equal

--- userland/libraries/libutils/Assert.h
+++ userland/libraries/libutils/Assert.h
@@ -60,4 +60,16 @@ inline void equal(const A a, const B b, SourceLocation location = SourceLocation
     }
 }
 
+template <typename A, typename B>
+inline void not_equal(const A a, const B b, SourceLocation location = SourceLocation::current())
+{
+    if constexpr (!(__CONFIG_IS_RELEASE__))
+    {
+        if (a == static_cast<A>(b))
+        {
+            assert_failed(IO::format("{} equals {}", a, b).cstring(), location.file(), location.function(), location.line());
+        }
+    }
+}
+
 }
