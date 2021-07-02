b61cab9198aa2f6e0ae8cba26d55ba377bf7c0c2
krishpranav <krisna.pranav@gmail.com>
2021-07-02 19:53:10 +0530

    includes

---
+++ userland/libraries/libutils/Assert.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <assert.h>
+#include <libio/Format.h>
+#include <libutils/SourceLocation.h>
