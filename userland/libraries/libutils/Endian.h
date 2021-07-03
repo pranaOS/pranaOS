20e54f2276cf6cf0c2a342b79ae457144b91282c
krishpranav <krisna.pranav@gmail.com>
2021-07-03 08:30:04 +0530

    endian headers

--- userland/libraries/libutils/Endian.h
+++ userland/libraries/libutils/Endian.h
@@ -6,6 +6,7 @@
 
 #pragma once
 
+// includes
 #include <libutils/Prelude.h>
 
 namespace Utils
@@ -28,7 +29,6 @@ constexpr TValue swap_little_endian(TValue value)
 #endif
 }
 
-
 template <typename TValue>
 constexpr TValue swap_big_endian(TValue value)
 {
@@ -47,6 +47,23 @@ constexpr TValue swap_big_endian(TValue value)
 }
 
 template <typename TValue>
+struct PACKED BigEndian
+{
+private:
+    TValue _value{0};
+
+public:
+    constexpr BigEndian() {}
+
+    constexpr BigEndian(TValue value) : _value(swap_big_endian(value)) {}
+
+    constexpr TValue operator()() const
+    {
+        return swap_big_endian(_value);
+    }
+};
+
+template <typename TValue>
 struct PACKED LittleEndian
 {
 private:
@@ -62,3 +79,25 @@ public:
         return swap_little_endian(_value);
     }
 };
+
+using be_uint8_t = BigEndian<uint8_t>;
+using be_uint16_t = BigEndian<uint16_t>;
+using be_uint32_t = BigEndian<uint32_t>;
+using be_uint64_t = BigEndian<uint64_t>;
+
+using le_uint8_t = LittleEndian<uint8_t>;
+using le_uint16_t = LittleEndian<uint16_t>;
+using le_uint32_t = LittleEndian<uint32_t>;
+using le_uint64_t = LittleEndian<uint64_t>;
+
+using be_int8_t = BigEndian<int8_t>;
+using be_int16_t = BigEndian<int16_t>;
+using be_int32_t = BigEndian<int32_t>;
+using be_int64_t = BigEndian<int64_t>;
+
+using le_int8_t = LittleEndian<int8_t>;
+using le_int16_t = LittleEndian<int16_t>;
+using le_int32_t = LittleEndian<int32_t>;
+using le_int64_t = LittleEndian<int64_t>;
+
+}
