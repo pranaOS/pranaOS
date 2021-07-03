53ee02572e46c29a228c0c8bab15146de54bd4b1
krishpranav <krisna.pranav@gmail.com>
2021-07-03 14:14:29 +0530

    font compiler

--- meta/utils/font-compiler.py
+++ meta/utils/font-compiler.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python3
-# font compiler for pranaOS
+#!/usr/bin/python3
+# font compiler built in python for pranaOS
 
 # imports
-import json 
-import sys 
-import struct 
+import json
+import sys
+import struct
 
 in_filename = sys.argv[1]
 out_filename = sys.argv[2]
@@ -14,3 +14,20 @@ data = json.load(infp)
 infp.close()
 
 outfp = open(out_filename, 'wb')
+
+for character in data["characters"]:
+    glyph = data["characters"][character]
+    codepoint = ord(character)
+
+    outfp.write(struct.pack("I", codepoint))
+    outfp.write(struct.pack("I", glyph["x"]))
+    outfp.write(struct.pack("I", glyph["y"]))
+    outfp.write(struct.pack("I", glyph["width"]))
+    outfp.write(struct.pack("I", glyph["height"]))
+    outfp.write(struct.pack("i", glyph["originX"]))
+    outfp.write(struct.pack("i", glyph["originY"]))
+    outfp.write(struct.pack("I", glyph["advance"]))
+
+outfp.write(struct.pack("IIIIIiiI", 0, 0, 0, 0, 0, 0, 0, 0))
+
+outfp.close()
