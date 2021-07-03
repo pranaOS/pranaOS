3013d3c06ed03d440937da7948429bc009fb28cb
krishpranav <krisna.pranav@gmail.com>
2021-07-03 15:24:01 +0530

    strlcpy

---
+++ meta/hosted/plugs/String.cpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <string.h>
+
+size_t strlcpy(char *dst, const char *src, size_t maxlen)
+{
+    const size_t srclen = strlen(src);
+
+    if (srclen + 1 < maxlen)
+    {
+        memcpy(dst, src, srclen + 1);
+    }
+    else if (maxlen != 0)
+    {
+        memcpy(dst, src, maxlen - 1);
+        dst[maxlen - 1] = '\0';
+    }
+
+    return srclen;
+}
+
