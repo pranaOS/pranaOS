272cce6a98156695746bbe63de6f557abdbe14b3
krishpranav <krisna.pranav@gmail.com>
2021-07-03 08:28:19 +0530

    big swap

--- userland/libraries/libutils/Endian.h
+++ userland/libraries/libutils/Endian.h
@@ -27,3 +27,21 @@ constexpr TValue swap_little_endian(TValue value)
         return value;
 #endif
 }
+
+
+template <typename TValue>
+constexpr TValue swap_big_endian(TValue value)
+{
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+    if constexpr (sizeof(TValue) == 8)
+        return __builtin_bswap64(value);
+    if constexpr (sizeof(TValue) == 4)
+        return __builtin_bswap32(value);
+    if constexpr (sizeof(TValue) == 2)
+        return __builtin_bswap16(value);
+    if constexpr (sizeof(TValue) == 1)
+        return value;
+#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+    return value;
+#endif
+}
