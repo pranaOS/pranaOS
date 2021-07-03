d3858f18b9fdb3b7ee7220fe63be6af048185a66
krishpranav <krisna.pranav@gmail.com>
2021-07-03 15:16:38 +0530

    includes

---
+++ meta/hosted/plugs/Syscall.cpp
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <libabi/Syscalls.h>
+#include <libio/Streams.h>
+#include <libmath/MinMax.h>
+
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
