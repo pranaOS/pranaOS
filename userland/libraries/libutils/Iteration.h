22055cdbf34167ecc25dd03f53ec4b41db605119
krishpranav <krisna.pranav@gmail.com>
2021-07-03 12:58:59 +0530

    Iteration

---
+++ userland/libraries/libutils/Iteration.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+
+#pragma once
+
+// includes
+#include <libutils/Func.h>
+
+namespace Utils
+{
+
+enum struct Iteration
+{
+    CONTINUE,
+    STOP,
+};
+
+
+}
