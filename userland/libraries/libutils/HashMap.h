5a51733248528db4baddc68a513dbf8322094c65
krishpranav <krisna.pranav@gmail.com>
2021-07-03 10:50:15 +0530

    hash ma[

---
+++ userland/libraries/libutils/HashMap.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <libutils/Hash.h>
+#include <libutils/Vector.h>
+
+namespace Utils
+{
+
+template <typename TKey, typename TValue>
+struct HashMap
+{
+private:
+    
+}
+}
