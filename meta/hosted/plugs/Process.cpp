068082e6d4e7c94f189c3a9c43c00baa202b49d7
krishpranav <krisna.pranav@gmail.com>
2021-07-03 15:15:36 +0530

    meta: Process

---
+++ meta/hosted/plugs/Process.cpp
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <libsystem/process/Process.h>
+
+
+String process_resolve(String path)
+{
+    return path;
+}
+
+int process_this()
+{
+    return 69
+}
+
+const char *process()
+{
+    return "hosted";
+}
