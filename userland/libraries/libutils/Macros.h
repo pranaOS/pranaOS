d43c49cff1ced40921135fd8c89001054751b64a
krishpranav <krisna.pranav@gmail.com>
2021-07-03 13:47:50 +0530

    macros class name define

--- userland/libraries/libutils/Macros.h
+++ userland/libraries/libutils/Macros.h
@@ -21,3 +21,17 @@
 #define FLATTEN __attribute__((flatten))
 
 #define ALWAYS_INLINE __attribute__((always_inline))
+
+#define ALIGN_DOWN(__addr, __align) ((__addr) & ~((__align)-1))
+
+#define ALIGN_UP(__addr, __align) (((__addr) + (__align)-1) & ~((__align)-1))
+
+#define ARRAY_LENGTH(__array) (sizeof(__array) / sizeof(__array[0]))
+
+#define NONCOPYABLE(__class_name)                \
+    __class_name(const __class_name &) = delete; \
+    __class_name &operator=(const __class_name &) = delete;
+
+#define NONMOVABLE(__class_name)                  \
+    __class_name(const __class_name &&) = delete; \
+    __class_name &operator=(const __class_name &&) = delete;
