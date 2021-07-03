93ed7db639eabfcb733e23622ad0286effcbb4e2
krishpranav <krisna.pranav@gmail.com>
2021-07-03 12:45:43 +0530

    includes

---
+++ userland/libraries/libutils/Prelude.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <libabi/Process.h>
+#include <libutils/Macros.h>
+
