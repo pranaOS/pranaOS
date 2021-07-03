6f8e63019ccd05f9ec5436cb31d1aea5d2f83c45
krishpranav <krisna.pranav@gmail.com>
2021-07-03 15:19:58 +0530

    meta: Syscall open flags to posix

--- meta/hosted/plugs/Syscall.cpp
+++ meta/hosted/plugs/Syscall.cpp
@@ -15,3 +15,64 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+
+int open_flags_to_posix(JOpenFlag flags)
+{
+    int result = 0;
+
+    if ((flags & J_OPEN_WRITE) && (flags & J_OPEN_READ))
+    {
+        result |= O_RDWR;
+    }
+    else
+    {
+        result |= O_RDONLY;
+    }
+
+    if (flags & J_OPEN_CREATE)
+    {
+        result |= O_CREAT;
+    }
+
+    if (flags & J_OPEN_APPEND)
+    {
+        result |= O_APPEND;
+    }
+
+    if (flags & J_OPEN_TRUNC)
+    {
+        result |= O_TRUNC;
+    }
+
+    if (flags & J_OPEN_BUFFERED)
+    {
+        // FIXME: TODO
+    }
+
+    if (flags & J_OPEN_STREAM)
+    {
+        // FIXME: TODO
+    }
+
+    if (flags & J_OPEN_DIRECTORY)
+    {
+        // FIXME: TODO
+    }
+
+    if (flags & J_OPEN_SOCKET)
+    {
+        // FIXME: TODO
+    }
+
+    if (flags & J_OPEN_CLIENT)
+    {
+        // FIXME: TODO
+    }
+
+    if (flags & J_OPEN_SERVER)
+    {
+        // FIXME: TODO
+    }
+
+    return result;
+}
