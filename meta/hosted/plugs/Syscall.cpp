707ab2ab946019b1bc42051f952439fcb3cb55c5
krishpranav <krisna.pranav@gmail.com>
2021-07-03 15:23:24 +0530

    meta: syscall functions

--- meta/hosted/plugs/Syscall.cpp
+++ meta/hosted/plugs/Syscall.cpp
@@ -5,7 +5,7 @@
 */
 
 // includes
-#include <libabi/Syscalls.h>
+#include <abi/Syscalls.h>
 #include <libio/Streams.h>
 #include <libmath/MinMax.h>
 
@@ -95,4 +95,100 @@ int whence_to_posix(JWhence whence)
     }
 
     return -1;
+}
+
+JFileType mode_to_skift_file_type(mode_t mode)
+{
+    if (S_ISDIR(mode))
+    {
+        return J_FILE_TYPE_DIRECTORY;
+    }
+
+    if (S_ISFIFO(mode))
+    {
+        return J_FILE_TYPE_PIPE;
+    }
+
+    return J_FILE_TYPE_REGULAR;
+}
+
+JStat stat_to_skift(struct stat sb)
+{
+    JStat result;
+
+    result.size = sb.st_size;
+    result.type = mode_to_skift_file_type(sb.st_mode);
+
+    return result;
+}
+
+JResult errno_to_skift_result()
+{
+    JResult result = SUCCESS;
+
+    if (errno != 0 && result == SUCCESS)
+    {
+        // Catch all
+        return ERR_UNKNOWN;
+    }
+
+    errno = 0;
+
+    return result;
+}
+
+JResult j_handle_open(int *handle, const char *raw_path, size_t size, JOpenFlag flags)
+{
+    char buffer[256];
+    strlcpy(buffer, raw_path, MIN(256, size + 1));
+    *handle = open(buffer, open_flags_to_posix(flags));
+
+    return errno_to_skift_result();
+}
+
+JResult j_handle_close(int handle)
+{
+    close(handle);
+
+    return errno_to_skift_result();
+}
+
+JResult j_handle_read(int handle, void *buffer, size_t size, size_t *amount_read)
+{
+    *amount_read = read(handle, buffer, size);
+
+    return errno_to_skift_result();
+}
+
+JResult j_handle_write(int handle, const void *buffer, size_t size, size_t *amount_written)
+{
+    *amount_written = write(handle, buffer, size);
+
+    return errno_to_skift_result();
+}
+
+JResult j_handle_seek(int handle, ssize64_t *offset, JWhence whence, ssize64_t *result)
+{
+    *result = lseek(handle, *offset, whence_to_posix(whence));
+
+    return errno_to_skift_result();
+}
+
+JResult j_handle_stat(int handle, JStat *state)
+{
+    struct stat sb;
+    fstat(handle, &sb);
+
+    *state = stat_to_skift(sb);
+
+    return errno_to_skift_result();
+}
+
+JResult j_handle_call(int handle, IOCall call, void *args)
+{
+    UNUSED(handle);
+    UNUSED(call);
+    UNUSED(args);
+
+    return ERR_NOT_IMPLEMENTED;
 }
