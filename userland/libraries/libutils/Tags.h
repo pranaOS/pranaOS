25203c57ef8c304810786d1f9ec14b757378118c
krishpranav <krisna.pranav@gmail.com>
2021-07-04 12:30:05 +0530

    tags headers

--- userland/libraries/libutils/Tags.h
+++ userland/libraries/libutils/Tags.h
@@ -12,7 +12,7 @@ namespace Utils
 enum struct AdoptTag
 {
     __value
-}
+};
 
 enum struct CopyTag
 {
@@ -23,12 +23,14 @@ enum struct WrapTag
 {
     __value
 };
-
 enum struct NoneTag
 {
     __value
 };
 
-
+constexpr auto ADOPT = AdoptTag::__value;
+constexpr auto COPY = CopyTag::__value;
+constexpr auto WRAP = WrapTag::__value;
+constexpr auto NONE = NoneTag::__value;
 
 }
