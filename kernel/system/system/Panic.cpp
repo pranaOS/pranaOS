81dcf00db89749cf57f7441e5749ecf5a477f82f
krishpranav <krisna.pranav@gmail.com>
2021-07-04 16:15:29 +0530

    includes

---
+++ kernel/system/system/Panic.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <stdio.h>
+#include "archs/Arch.h"
+#include "system/Streams.h"
+#include "system/graphics/EarlyConsole.h"
+#include "system/graphics/Font.h"
+#include "system/interrupts/Interupts.h"
+#include "system/scheduling/Scheduler.h"
+#include "system/system/System.h"
+#include "system/tasking/Task.h"
