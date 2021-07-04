a7e5c8e8458e72ab7fb8db9e9cf999e9d96fd5f2
krishpranav <krisna.pranav@gmail.com>
2021-07-04 11:22:36 +0530

    update :std.h

--- userland/libraries/libutils/Std.h
+++ userland/libraries/libutils/Std.h
@@ -23,7 +23,7 @@ inline void *operator new[](size_t, void *ptr)
 }
 
 #else
-#   include <new>
+#    include <new>
 #endif
 
 namespace std
@@ -38,7 +38,7 @@ constexpr typename RemoveReference<T>::Type &&move(T &&arg)
 }
 
 template <typename T>
-constexpr T &&forward(typename RemoveRefreence<T>::Type &param)
+constexpr T &&forward(typename RemoveReference<T>::Type &param)
 {
     return static_cast<T &&>(param);
 }
@@ -49,5 +49,55 @@ constexpr T &&forward(typename RemoveReference<T>::Type &&param)
     return static_cast<T &&>(param);
 }
 
+template <typename T>
+void swap(T &left, T &right)
+{
+    T tmp = move(left);
+    left = move(right);
+    right = move(tmp);
 }
 
+template <typename T, typename U = T>
+constexpr T exchange(T &slot, U &&value)
+{
+    T old = move(slot);
+    slot = forward<U>(value);
+    return old;
+}
+
+template <typename T>
+struct initializer_list
+{
+private:
+    T *_data;
+    size_t _size;
+
+    constexpr initializer_list(const T *data, size_t size)
+        : _data(data), _size(size)
+    {
+    }
+
+public:
+    size_t size() const { return _size; }
+
+    constexpr initializer_list()
+        : _data(0), _size(0)
+    {
+    }
+
+    const T &operator[](size_t index) const
+    {
+        return _data[index];
+    }
+
+    T &operator[](size_t index)
+    {
+        return _data[index];
+    }
+
+    constexpr const T *begin() const { return _data; }
+
+    constexpr const T *end() const { return _data + _size; }
+};
+
+}
