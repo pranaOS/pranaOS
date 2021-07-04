fc3ffe0a72cce90cfa6986980a49459a9e0c06ce
krishpranav <krisna.pranav@gmail.com>
2021-07-04 12:28:52 +0530

    enum values

---
+++ userland/libraries/libutils/Tags.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+namespace Utils
+{
+
+enum struct AdoptTag
+{
+    __value
+}
+
+enum struct CopyTag
+{
+    __value
+};
+
+}
