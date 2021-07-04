a0c8b9e2aae31576ca0a8356f4d03564b2486ecf
krishpranav <krisna.pranav@gmail.com>
2021-07-04 15:36:08 +0530

    cpuid functions

--- kernel/archs/x86/CPUID.cpp
+++ kernel/archs/x86/CPUID.cpp
@@ -16,3 +16,14 @@ static inline int cpuid_string(int code, int where[4])
                  : "a"(code));
     return (int)where[0];
 }
+
+CPUID cpuid()
+{
+    CPUID cid;
+    memset(&cid.vendorid, 0, 16);
+    cpuid_string(0, (int *)&cid.vendorid[0]);
+    cid.RAW_ECX = cpuid_get_feature_ECX();
+    cid.RAW_EDX = cpuid_get_feature_EDX();
+
+    return cid;
+}
