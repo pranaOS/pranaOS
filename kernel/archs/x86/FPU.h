302fcf5e25905181e0a0d60f2091e2cd9ef8b2a8
krishpranav <krisna.pranav@gmail.com>
2021-07-04 15:47:04 +0530

    FPU headers

---
+++ kernel/archs/x86/FPU.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include "system/tasking/Task.h"
+
+void fpu_initialize();
+void fpu_save_context(Task *task);
+void fpu_load_context(Task *task);
+void fpu_init_context(Task *task);
