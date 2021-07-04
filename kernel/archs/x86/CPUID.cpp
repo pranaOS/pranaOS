b16beaa596a39ae56f1d97c47c22b7bb0121ccaf
krishpranav <krisna.pranav@gmail.com>
2021-07-04 15:35:52 +0530

    cpuid string volaite

---
+++ kernel/archs/x86/CPUID.cpp
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <string.h>
+#include "archs/x86/CPUID.h"
+
+static inline int cpuid_string(int code, int where[4])
+{
+    asm volatile("cpuid"
+                 : "=a"(*where), "=b"(*(where + 0)),
+                   "=d"(*(where + 1)), "=c"(*(where + 2))
+                 : "a"(code));
+    return (int)where[0];
+}
