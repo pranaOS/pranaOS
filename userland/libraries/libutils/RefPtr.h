d3f6ce567e852a95b37bd3ee2d32e60dce61f9ae
krishpranav <krisna.pranav@gmail.com>
2021-07-04 11:36:02 +0530

    public: RefPtr functions

--- userland/libraries/libutils/RefPtr.h
+++ userland/libraries/libutils/RefPtr.h
@@ -25,6 +25,7 @@ private:
 
 public:
     RefPtr() {}
+
     RefPtr(nullptr_t) {}
 
     RefPtr(T &object) : _ptr(&object)
@@ -32,7 +33,91 @@ public:
         ref_if_not_null(_ptr);
     }
 
-    RefPtr(AdoptTage, T &object) : _ptr(const_cast<T *>())
+    RefPtr(AdoptTag, T &object) : _ptr(const_cast<T *>(&object)) {}
+
+    RefPtr(const RefPtr &other) : _ptr(other.naked())
+    {
+        ref_if_not_null(_ptr);
+    }
+
+    RefPtr(AdoptTag, RefPtr &other) : _ptr(other.give_ref()) {}
+
+    RefPtr(RefPtr &&other) : _ptr(other.give_ref()) {}
+
+    template <typename U>
+    RefPtr(const RefPtr<U> &other) : _ptr(static_cast<T *>(other.naked()))
+    {
+        ref_if_not_null(_ptr);
+    }
+
+    template <typename U>
+    RefPtr(AdoptTag, RefPtr<U> &other) : _ptr(static_cast<T *>(other.give_ref())) {}
+
+    template <typename U>
+    RefPtr(RefPtr<U> &&other) : _ptr(static_cast<T *>(other.give_ref())) {}
+
+    ~RefPtr()
+    {
+        deref_if_not_null(_ptr);
+        _ptr = nullptr;
+    }
+
+    RefPtr &operator=(nullptr_t)
+    {
+        deref_if_not_null(_ptr);
+        _ptr = nullptr;
+
+        return *this;
+    }
+
+    RefPtr &operator=(const RefPtr &other)
+    {
+        if (_ptr != other.naked())
+        {
+            deref_if_not_null(_ptr);
+            _ptr = other.naked();
+            ref_if_not_null(_ptr);
+        }
+
+        return *this;
+    }
+
+    RefPtr &operator=(RefPtr &&other)
+    {
+        if (this != &other)
+        {
+            deref_if_not_null(_ptr);
+            _ptr = other.give_ref();
+        }
+
+        return *this;
+    }
+
+    template <typename U>
+    RefPtr &operator=(RefPtr<U> &other)
+    {
+        if (_ptr != other.naked())
+        {
+            deref_if_not_null(_ptr);
+            _ptr = other.naked();
+            ref_if_not_null(_ptr);
+        }
+
+        return *this;
+    }
+
+    template <typename U>
+    RefPtr &operator=(RefPtr<U> &&other)
+    {
+        if (this != static_cast<void *>(&other))
+        {
+            deref_if_not_null(_ptr);
+            _ptr = other.give_ref();
+        }
+
+        return *this;
+    }
+
 
 }
 
