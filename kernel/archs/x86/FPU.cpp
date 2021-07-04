289f9fb058b56af9449c0017b4364468cad2ceec
krishpranav <krisna.pranav@gmail.com>
2021-07-04 15:48:13 +0530

    fpu initialize: asm volatile

--- kernel/archs/x86/FPU.cpp
+++ kernel/archs/x86/FPU.cpp
@@ -10,3 +10,23 @@
 
 char fpu_initial_context[512] ALIGNED(16);
 char fpu_registers[512] ALIGNED(16);
+
+void fpu_initialize()
+{
+    asm volatile("clts");
+    size_t t;
+    asm volatile("mov %%cr0, %0"
+                 : "=r"(t));
+    t &= ~(1 << 2);
+    t |= (1 << 1);
+    t |= (1 << 5);
+    asm volatile("mov %0, %%cr0" ::"r"(t));
+
+    asm volatile("mov %%cr4, %0"
+                 : "=r"(t));
+    t |= 3 << 9;
+    asm volatile("mov %0, %%cr4" ::"r"(t));
+ 
+    asm volatile("fninit");
+    asm volatile("fxsave (%0)" ::"r"(fpu_initial_context));
+}
