dca7a7aa40c30e1c326412ff0a3c55567e83975b
krishpranav <krisna.pranav@gmail.com>
2021-07-04 15:51:54 +0530

    IO APIC headers

--- kernel/archs/x86/IOAPIC.h
+++ kernel/archs/x86/IOAPIC.h
@@ -13,7 +13,10 @@
 namespace Arch::x86
 {
 
-void ioapic_found(unintptr_t address);
+void ioapic_found(uintptr_t address);
 
+uint32_t apic_read(void *ioapicaddr, uint32_t reg);
+
+void ioapic_write(void *ioapicaddr, uint32_t reg, uint32_t value);
 
 }
