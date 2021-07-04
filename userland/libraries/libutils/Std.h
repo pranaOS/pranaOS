2b29987b66076291131bf5aa8e9a4954fc5e0c5d
krishpranav <krisna.pranav@gmail.com>
2021-07-04 09:44:47 +0530

    inline std

--- userland/libraries/libutils/Std.h
+++ userland/libraries/libutils/Std.h
@@ -9,3 +9,19 @@
 // includes
 #include <libutils/Traits.h>
 #include <stddef.h>
+
+#if __CONFIG_IS_HOSTED__ == 0
+
+inline void *operator new(size_t, void *ptr)
+{
+    return ptr;
+}
+
+inline void *operator new[](size_t, void *ptr)
+{
+    return ptr;
+}
+
+#else
+#   include <new>
+#endif
