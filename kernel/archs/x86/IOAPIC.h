aaa6ed60d79cb50e6129d9cbc552db243c0c56d5
krishpranav <krisna.pranav@gmail.com>
2021-07-04 15:51:38 +0530

    values: void

---
+++ kernel/archs/x86/IOAPIC.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+
+#pragma once
+
+// includes
+#include <libutils/Prelude.h>
+
+namespace Arch::x86
+{
+
+void ioapic_found(unintptr_t address);
+
+
+}
