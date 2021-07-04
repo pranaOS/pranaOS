b62208eda152be372157eadc1891c3a00adf84e8
krishpranav <krisna.pranav@gmail.com>
2021-07-04 16:20:39 +0530

    kernel panic functiosn

--- kernel/system/system/Panic.cpp
+++ kernel/system/system/Panic.cpp
@@ -42,3 +42,91 @@ static const char *const witty_comments[] = {
 
 static bool has_panic = false;
 static bool nested_panic = false;
+
+
+void system_panic_internal(SourceLocation location, void *stackframe, const char *message, ...)
+{
+    interrupts_retain();
+    interrupts_disable_holding();
+
+    font_set_bg(0xff171717);
+    font_set_fg(0xffFAFAFA);
+
+    early_console_enable();
+
+    va_list va;
+    va_start(va, message);
+
+    if (nested_panic)
+    {
+        system_stop();
+    }
+
+    Kernel::logln("\n");
+
+    if (!has_panic)
+    {
+        has_panic = true;
+
+        Kernel::logln("--- !!! ------------------------------------------------------------------------");
+        Kernel::logln("");
+        Kernel::logln("\tKERNEL PANIC");
+        Kernel::logln("\t// {}", witty_comments[system_get_tick() % ARRAY_LENGTH(witty_comments)]);
+    }
+    else
+    {
+        nested_panic = true;
+        Kernel::logln("--ERROR - - - - - - - - - - -- - - - - -- - - - - - - - - - - - - - - - - - - - - -");
+        Kernel::logln("");
+        Kernel::logln("\tNESTED KERNEL PANIC", YO_DAWG);
+        Kernel::logln("\t// {}", YO_DAWG);
+    }
+
+    char buffer[256];
+    vsnprintf(buffer, 256, message, va);
+    Kernel::logln(buffer);
+
+    Kernel::logln("\tthrow by {} {}() ln{}", location.file(), location.function(), location.line());
+    Kernel::logln("\tThe system was running for {} tick.", system_get_tick());
+
+    if (scheduler_running_id() != -1)
+    {
+        Kernel::logln("\tThe running process is {}: {}", scheduler_running_id(), scheduler_running()->name);
+    }
+
+    if (scheduler_is_context_switch())
+    {
+        Kernel::logln("\tWe are context switching", system_get_tick());
+    }
+
+    Kernel::logln("");
+
+    Kernel::logln("\tStackframe:");
+    if (stackframe)
+    {
+        Arch::dump_stack_frame(stackframe);
+    }
+    else
+    {
+        Arch::backtrace();
+    }
+    Kernel::logln("");
+
+    memory_dump();
+
+    Kernel::logln("");
+
+    if (!nested_panic)
+    {
+        task_dump(scheduler_running());
+    }
+
+    Kernel::logln("");
+    Kernel::logln("\tSystem halted!");
+    Kernel::logln("");
+
+    Kernel::logln("--------------------------------------------------------------------------------");
+    Kernel::logln("");
+
+    system_stop();
+}
