05cf33d274194e098c6974433227204d602c283a
krishpranav <krisna.pranav@gmail.com>
2021-07-04 16:25:48 +0530

    includes

---
+++ kernel/system/system/System.cpp
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include "system/Streams.h"
+#include <libsystem/BuildInfo.h>
+#include "archs/Arch.h"
+#include "system/scheduling/Scheduler.h"
+#include "system/system/System.h"
+
