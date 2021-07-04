45254aef47a030e8c15037ac2847dff968859620
krishpranav <krisna.pranav@gmail.com>
2021-07-04 12:23:50 +0530

    string storege headers

--- userland/libraries/libutils/StringStorage.h
+++ userland/libraries/libutils/StringStorage.h
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
 */
 
+
 #pragma once
 
 // includes
@@ -16,7 +17,6 @@ namespace Utils
 
 struct StringStorage final :
     public Storage
-
 {
 private:
     char *_buffer;
@@ -32,6 +32,34 @@ public:
 
     void *end() override { return reinterpret_cast<char *>(start()) + _length; }
 
+    StringStorage(CopyTag, const char *cstring)
+        : StringStorage(COPY, cstring, strlen(cstring))
+    {
+    }
 
+    StringStorage(CopyTag, const char *cstring, size_t length)
+    {
+        _length = strnlen(cstring, length);
+        _buffer = new char[_length + 1];
+        memcpy(_buffer, cstring, _length);
+        _buffer[_length] = '\0';
+    }
+
+    StringStorage(AdoptTag, char *cstring)
+        : StringStorage(ADOPT, cstring, strlen(cstring))
+    {
+    }
+
+    StringStorage(AdoptTag, char *buffer, size_t length)
+    {
+        _length = length;
+        _buffer = buffer;
+    }
+
+    ~StringStorage()
+    {
+        delete[] _buffer;
+    }
 };
+
 }
