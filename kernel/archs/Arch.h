f30de2dcfa420bfa2e90713e778d2fa2353bf59c
krishpranav <krisna.pranav@gmail.com>
2021-07-04 14:03:42 +0530

    kernel

---
+++ kernel/archs/Arch.h
@@ -0,0 +1,5 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
---
+++ kernel/archs/x86/COM.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <libutils/Prelude.h>
+
+enum COMPort
+{
+    COM1 = 0x3F8,
+    COM2 = 0x2F8,
+    COM3 = 0x3E8,
+    COM4 = 0x2E8,
+};
