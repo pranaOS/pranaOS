deef0bdc6992520aaf4ddd49ef5188dfaea156ea
krishpranav <krisna.pranav@gmail.com>
2021-07-04 14:07:40 +0530

    x86 com functions

--- kernel/archs/x86/COM.cpp
+++ kernel/archs/x86/COM.cpp
@@ -7,6 +7,7 @@
 // includes
 #include "archs/x86/COM.h"
 #include "archs/x86_32/x86_32.h"
+
 #include "system/interrupts/Interupts.h"
 
 bool com_can_read(COMPort port)
@@ -22,14 +23,14 @@ bool com_can_write(COMPort port)
 void com_wait_write(COMPort port)
 {
     while (!com_can_write(port))
-    { 
+    {  
     }
 }
 
 void com_wait_read(COMPort port)
 {
     while (!com_can_read(port))
-    { 
+    {  
     }
 }
 
@@ -37,4 +38,33 @@ void com_putc(COMPort port, char c)
 {
     com_wait_write(port);
     out8(port, c);
+}
+
+char com_getc(COMPort port)
+{
+    com_wait_read(port);
+    return in8(port);
+}
+
+size_t com_write(COMPort port, const void *buffer, size_t size)
+{
+    InterruptsRetainer retainer;
+
+    for (size_t i = 0; i < size; i++)
+    {
+        com_putc(port, ((const char *)buffer)[i]);
+    }
+
+    return size;
+}
+
+void com_initialize(COMPort port)
+{
+    out8(port + 2, 0);
+    out8(port + 3, 0x80);
+    out8(port + 0, 115200 / 9600);
+    out8(port + 1, 0);
+    out8(port + 3, 0x03);
+    out8(port + 4, 0);
+    out8(port + 1, 0x01);
 }
