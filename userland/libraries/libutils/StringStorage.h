215db76c4efbb1ccd3bc013459a2082aa6a11a75
krishpranav <krisna.pranav@gmail.com>
2021-07-04 12:23:26 +0530

    values: void, const, using

--- userland/libraries/libutils/StringStorage.h
+++ userland/libraries/libutils/StringStorage.h
@@ -21,5 +21,17 @@ struct StringStorage final :
 private:
     char *_buffer;
     size_t _length;
-}
+
+public:
+    using Storage::end;
+    using Storage::start;
+
+    const char *cstring() { return _buffer; }
+
+    void *start() override { return _buffer; }
+
+    void *end() override { return reinterpret_cast<char *>(start()) + _length; }
+
+
+};
 }
