99e9b427f983d0549b3ce9e6a9c95252b6250f4c
krishpranav <krisna.pranav@gmail.com>
2021-07-04 11:33:18 +0530

    private funs

---
+++ userland/libraries/libutils/RefPtr.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <libutils/RefCounted.h>
+#include <libutils/Std.h>
+#include <libutils/Tags.h>
+#include <libutils/Traits.h>
+
+namespace Utisl
+
+{
+
+template <typename T>
+struct RefPtr
+{
+private:
+    T *ptr = nullptr;
+
+
+
+}
+
+}
