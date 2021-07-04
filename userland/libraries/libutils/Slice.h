1e791722c12fb00c7bd15902c89191d78d4fbec4
krishpranav <krisna.pranav@gmail.com>
2021-07-04 12:06:55 +0530

    Slice values

--- userland/libraries/libutils/Slice.h
+++ userland/libraries/libutils/Slice.h
@@ -36,5 +36,45 @@ public:
         return reinterpret_cast<const char *>(start()) + _size;
     }
 
+        Slice()
+    {
+    }
+
+    Slice(RefPtr<Storage> storage)
+    {
+        _storage = storage;
+        _start = _storage->start();
+        _size = _storage->size();
+    }
+
+    Slice(RefPtr<Storage> storage, size_t offset, size_t size)
+    {
+        _storage = storage;
+        _start = _storage->start();
+        _start = static_cast<const void *>(static_cast<const char *>(_start) + offset);
+        _size = size;
+    }
+
+    Slice(const char *start)
+    {
+        _storage = nullptr;
+        _start = start;
+        _size = strlen(start);
+    }
+
+    Slice(const void *start, size_t size)
+    {
+        _storage = nullptr;
+        _start = start;
+        _size = size;
+    }
+
+    Slice(RawStorage &obj)
+    {
+        _storage = obj.storage();
+        _start = _storage->start();
+        _size = _storage->size();
+    }
+
 
 }
