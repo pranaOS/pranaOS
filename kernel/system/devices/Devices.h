bc11077ff6e6a0d3dd61177f1c123d221de8d2bf
krishpranav <krisna.pranav@gmail.com>
2021-07-05 09:32:33 +0530

    includes

---
+++ kernel/system/devices/Devices.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <libutils/Func.h>
+#include <libutils/Prelude.h>
+#include "system/devices/DeviceAddress.h"
+#include "system/devices/DeviceClass.h"
+#include "system/devices/Driver.h"
+
+void device_scan(IterFunc<DeviceAddress> address);
+void device_iterate(IterFunc<RefPtr<Device>> callback);
+void device_initialize();
+void devices_acknowledge_interrupt(int interrupt);
+void devices_handle_interrupt(int interrupt);
+void device_mount(RefPtr<Device> device);
