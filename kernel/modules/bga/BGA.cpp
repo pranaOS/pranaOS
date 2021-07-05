5bc9408df752cab12e739115e2bbf9efad430fd9
krishpranav <krisna.pranav@gmail.com>
2021-07-05 11:27:18 +0530

    modules: bga j result set resolution bga devices address [set]

--- kernel/modules/bga/BGA.cpp
+++ kernel/modules/bga/BGA.cpp
@@ -25,3 +25,55 @@ uint16_t BGA::read_register(uint16_t address)
     out16(BGA_ADDRESS, address);
     return in16(BGA_DATA);
 }
+
+JResult BGA::set_resolution(int width, int height)
+{
+    if (width * height * sizeof(uint32_t) > _framebuffer->size())
+    {
+        Kernel::logln("Not enoughs VRAM for setting the resolution to {}x{}.", width, height);
+
+        return ERR_OUT_OF_MEMORY;
+    }
+    else
+    {
+        write_register(BGA_REG_ENABLE, BGA_DISABLED);
+        write_register(BGA_REG_XRES, width);
+        write_register(BGA_REG_YRES, height);
+        write_register(BGA_REG_BPP, 32);
+        write_register(BGA_REG_ENABLE, BGA_ENABLED | BGA_LINEAR_FRAMEBUFFER);
+
+        if (read_register(BGA_REG_XRES) != width ||
+            read_register(BGA_REG_YRES) != height)
+        {
+            write_register(BGA_REG_ENABLE, BGA_DISABLED);
+
+            write_register(BGA_REG_XRES, _width);
+            write_register(BGA_REG_YRES, _height);
+
+            write_register(BGA_REG_BPP, 32);
+            write_register(BGA_REG_ENABLE, BGA_ENABLED | BGA_LINEAR_FRAMEBUFFER);
+
+            return ERR_INVALID_ARGUMENT;
+        }
+
+        _width = width;
+        _height = height;
+
+        graphic_did_find_framebuffer(
+            _framebuffer->base(),
+            _width,
+            _height,
+            _width * 4,
+            4);
+
+        Kernel::logln("Resolution set to {}x{}.", width, height);
+
+        return SUCCESS;
+    }
+}
+
+BGA::BGA(DeviceAddress address) : PCIDevice(address, DeviceClass::FRAMEBUFFER)
+{
+    _framebuffer = make<MMIORange>(bar(0).range());
+    set_resolution(handover()->framebuffer_width, handover()->framebuffer_height);
+}
