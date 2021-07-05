76986bdd77262c76acfd88f7029699ae8a2168ba
krishpranav <krisna.pranav@gmail.com>
2021-07-05 12:11:49 +0530

    modules: devfs DeviceInfo headers

---
+++ kernel/modules/devfs/DeviceInfo.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include "system/node/Node.h"
+
+struct FsDeviceInfo : public FsNode
+{
+private:
+public:
+    FsDeviceInfo();
+
+    JResult open(FsHandle &handle) override;
+
+    void close(FsHandle &handle) override;
+
+    ResultOr<size_t> read(FsHandle &handle, void *buffer, size_t size) override;
+};
+
+void device_info_initialize();
