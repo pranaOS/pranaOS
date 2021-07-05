11838161dd62283b737e530168b3b2bc8ac4b60b
krishpranav <krisna.pranav@gmail.com>
2021-07-05 11:58:32 +0530

    modules: private funcs

--- kernel/modules/com/LegacySerial.h
+++ kernel/modules/com/LegacySerial.h
@@ -3,3 +3,24 @@
  *
  * SPDX-License-Identifier: BSD-2-Clause
 */
+
+#pragma once
+
+// includes
+#include <libutils/Lock.h>
+#include <libutils/RingBuffer.h>
+#include "archs/x86/COM.h"
+#include "ps2/LegacyDevice.h"
+
+
+struct LegacySerial : public LegacyDevice
+{
+
+private:
+    RingBuffer<char> _buffer{4096};
+    Lock _buffer_lock{"legacy-serial"};
+
+    COMPort port() { return (COMPort)legacy_address(); }
+
+
+}
