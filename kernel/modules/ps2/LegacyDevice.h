6f855c760f5d5efd0a2925ed9bfa2bd9692b103b
krishpranav <krisna.pranav@gmail.com>
2021-07-05 14:52:05 +0530

    modules: Legacy Device headers ps2

--- kernel/modules/ps2/LegacyDevice.h
+++ kernel/modules/ps2/LegacyDevice.h
@@ -6,8 +6,8 @@
 
 #pragma once
 
+// includes
 #include "system/devices/Driver.h"
-
 #include "ps2/Legacy.h"
 
 struct LegacyDevice : public Device
@@ -21,5 +21,37 @@ public:
     {
     }
 
-    
+    int interrupt() override
+    {
+        switch (legacy_address())
+        {
+
+        case LEGACY_KEYBOARD:
+            return 1;
+
+        case LEGACY_COM2:
+        case LEGACY_COM4:
+            return 3;
+
+        case LEGACY_COM1:
+        case LEGACY_COM3:
+            return 4;
+
+        case LEGACY_ATA0:
+        case LEGACY_ATA1:
+            return 5;
+
+        case LEGACY_ATA2:
+        case LEGACY_ATA3:
+            return 6;
+
+        case LEGACY_MOUSE:
+            return 12;
+
+        default:
+            break;
+        }
+
+        ASSERT_NOT_REACHED();
+    }
 };
