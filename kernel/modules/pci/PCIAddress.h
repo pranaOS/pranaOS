6944becd1da146c8edbaa1c93fe582efdf5b2704
krishpranav <krisna.pranav@gmail.com>
2021-07-05 13:06:29 +0530

    modules: pci address

---
+++ kernel/modules/pci/PCIAddress.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+// includes
+#include "archs/x86_32/x86_32.h"
+
+#define PCI_VENDOR_ID 0x00
+#define PCI_DEVICE_ID 0x02
+#define PCI_COMMAND 0x04
+#define PCI_STATUS 0x06
+#define PCI_REVISION_ID 0x08
+#define PCI_SUBSYSTEM_ID 0x2E
+
+#define PCI_PROG_IF 0x09
+#define PCI_SUBCLASS 0x0a
+#define PCI_CLASS 0x0b
+#define PCI_CACHE_LINE_SIZE 0x0c
+#define PCI_LATENCY_TIMER 0x0d
+#define PCI_HEADER_TYPE 0x0e
+#define PCI_BIST 0x0f
+
+#define PCI_BAR0 0x10
+#define PCI_BAR1 0x14
+#define PCI_BAR2 0x18
+#define PCI_BAR3 0x1C
+#define PCI_BAR4 0x20
+#define PCI_BAR5 0x24
+
+#define PCI_INTERRUPT_LINE 0x3C
+#define PCI_INTERRUPT_PIN 0x3D
+
+#define PCI_SECONDARY_BUS 0x19
+
+#define PCI_HEADER_TYPE_DEVICE 0
+#define PCI_HEADER_TYPE_BRIDGE 1
+#define PCI_HEADER_TYPE_CARDBUS 2
+
+#define PCI_TYPE_BRIDGE 0x0604
+#define PCI_TYPE_SATA 0x0106
+
+#define PCI_ADDRESS_PORT 0xCF8
+#define PCI_VALUE_PORT 0xCFC
+
+#define PCI_NONE 0xFFFF
