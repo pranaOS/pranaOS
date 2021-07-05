3db2df0e83430107de2de7a6337ead5f35c56fbd
krishpranav <krisna.pranav@gmail.com>
2021-07-05 15:50:49 +0530

    modules: legacy keyboard load keymap

--- kernel/modules/ps2/LegacyKeyboard.cpp
+++ kernel/modules/ps2/LegacyKeyboard.cpp
@@ -9,3 +9,41 @@
 #include "system/Configs.h"
 #include "system/Streams.h"
 #include "ps2/LegacyKeyboard.h"
+
+KeyMap *keyboard_load_keymap(const char *keymap_path)
+{
+    CLEANUP(stream_cleanup)
+    Stream *keymap_file = stream_open(keymap_path, J_OPEN_READ);
+
+    if (handle_has_error(keymap_file))
+    {
+        Kernel::logln("Failed to load keymap from {}: {}", keymap_path, handle_error_string(keymap_file));
+        return nullptr;
+    }
+
+    JStat stat;
+    stream_stat(keymap_file, &stat);
+
+    if (stat.type != J_FILE_TYPE_REGULAR)
+    {
+        Kernel::logln("Failed to load keymap from {}: This is not a regular file", keymap_path);
+
+        return nullptr;
+    }
+
+    Kernel::logln("Allocating keymap of size {}kio", stat.size / 1024);
+    KeyMap *keymap = (KeyMap *)malloc(stat.size);
+
+    size_t read = stream_read(keymap_file, keymap, stat.size);
+
+    if (read != stat.size)
+    {
+        Kernel::logln("Failed to load keymap from {}: {}", keymap_path, handle_error_string(keymap_file));
+
+        free(keymap);
+
+        return nullptr;
+    }
+
+    return keymap;
+}
