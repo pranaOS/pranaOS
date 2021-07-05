e2f6be1eed72f3fbe86e27d0a306bcdfad949931
krishpranav <krisna.pranav@gmail.com>
2021-07-05 18:08:01 +0530

    unix: unix functions

---
+++ kernel/modules/unix/UNIX.cpp
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include "unix/UNIX.h"
+
+Iteration unix_scan(IterFunc<UNIXAddress> callback)
+{
+    if (callback(UNIX_ZERO) == Iteration::STOP)
+    {
+        return Iteration::STOP;
+    }
+
+    if (callback(UNIX_NULL) == Iteration::STOP)
+    {
+        return Iteration::STOP;
+    }
+
+    if (callback(UNIX_RANDOM) == Iteration::STOP)
+    {
+        return Iteration::STOP;
+    }
+
+    return Iteration::CONTINUE;
+}
