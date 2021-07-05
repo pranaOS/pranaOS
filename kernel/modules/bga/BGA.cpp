1702e18c86b053945563e90ff84c9d8656d4cb70
krishpranav <krisna.pranav@gmail.com>
2021-07-05 11:27:48 +0530

    modules: bga functions

--- kernel/modules/bga/BGA.cpp
+++ kernel/modules/bga/BGA.cpp
@@ -7,12 +7,12 @@
 // includes
 #include "system/Streams.h"
 #include <libmath/MinMax.h>
+
 #include "bga/BGA.h"
 #include "system/graphics/EarlyConsole.h"
 #include "system/graphics/Graphics.h"
 #include "system/handover/Handover.h"
 #include "system/interrupts/Interupts.h"
-
 
 void BGA::write_register(uint16_t address, uint16_t data)
 {
@@ -76,4 +76,62 @@ BGA::BGA(DeviceAddress address) : PCIDevice(address, DeviceClass::FRAMEBUFFER)
 {
     _framebuffer = make<MMIORange>(bar(0).range());
     set_resolution(handover()->framebuffer_width, handover()->framebuffer_height);
+}
+
+size_t BGA::size()
+{
+    return _framebuffer->size();
+}
+
+ResultOr<size_t> BGA::read(size64_t offset, void *buffer, size_t size)
+{
+    return _framebuffer->read(offset, buffer, size);
+}
+
+ResultOr<size_t> BGA::write(size64_t offset, const void *buffer, size_t size)
+{
+    return _framebuffer->write(offset, buffer, size);
+}
+
+JResult BGA::call(IOCall request, void *args)
+{
+    if (request == IOCALL_DISPLAY_GET_MODE)
+    {
+        IOCallDisplayModeArgs *mode = (IOCallDisplayModeArgs *)args;
+
+        mode->width = _width;
+        mode->height = _height;
+
+        return SUCCESS;
+    }
+    else if (request == IOCALL_DISPLAY_SET_MODE)
+    {
+        IOCallDisplayModeArgs *mode = (IOCallDisplayModeArgs *)args;
+
+        return set_resolution(mode->width, mode->height);
+    }
+    else if (request == IOCALL_DISPLAY_BLIT)
+    {
+        IOCallDisplayBlitArgs *blit = (IOCallDisplayBlitArgs *)args;
+
+        for (int y = MAX(0, blit->blit_y); y < MIN(_height, blit->blit_y + blit->blit_height); y++)
+        {
+            for (int x = MAX(0, blit->blit_x); x < MIN(_width, blit->blit_x + blit->blit_width); x++)
+            {
+                uint32_t pixel = blit->buffer[y * blit->buffer_width + x];
+
+                uint32_t converted_pixel = ((pixel >> 16) & 0x000000ff) |
+                                           ((pixel)&0xff00ff00) |
+                                           ((pixel << 16) & 0x00ff0000);
+
+                _framebuffer->write32((y * _width + x) * sizeof(uint32_t), converted_pixel);
+            }
+        }
+
+        return SUCCESS;
+    }
+    else
+    {
+        return ERR_INAPPROPRIATE_CALL_FOR_DEVICE;
+    }
 }
