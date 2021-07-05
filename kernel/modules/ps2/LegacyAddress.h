e5be1734bd956bcb8c9d51ac2eb9f1d578362cad
krishpranav <krisna.pranav@gmail.com>
2021-07-05 15:13:29 +0530

    modules: ps2 legacy address headers

---
+++ kernel/modules/ps2/LegacyAddress.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+enum LegacyAddress
+{
+    LEGACY_MOUSE,
+    LEGACY_KEYBOARD,
+
+    LEGACY_ATA0,
+    LEGACY_ATA1,
+    LEGACY_ATA2,
+    LEGACY_ATA3,
+
+    LEGACY_COM1 = 0X3F8,
+    LEGACY_COM2 = 0X2F8,
+    LEGACY_COM3 = 0X3E8,
+    LEGACY_COM4 = 0X2E8,
+};
