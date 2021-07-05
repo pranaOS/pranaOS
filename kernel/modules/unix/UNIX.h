8207957cfb2b96a33271d67bcd85623f9870883c
krishpranav <krisna.pranav@gmail.com>
2021-07-05 18:06:36 +0530

    modules: unix headers

---
+++ kernel/modules/unix/UNIX.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include "system/devices/DeviceAddress.h"
+
+Iteration unix_scan(IterFunc<UNIXAddress> callback);
