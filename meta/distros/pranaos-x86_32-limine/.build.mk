84a709c4d0349fec4d14186565970eadc89f34aa
krishpranav <krisna.pranav@gmail.com>
2021-07-05 13:05:11 +0530

    update: build makefile

--- meta/distros/pranaos-x86_32-limine/.build.mk
+++ meta/distros/pranaos-x86_32-limine/.build.mk
@@ -19,7 +19,7 @@ $(BOOTDISK): $(RAMDISK).gz $(KERNEL_BINARY).gz $(DISTRO_DIRECTORY)/limine.cfg $(
 	$(ECHFS) -m -p0 $(BOOTDISK) import $(KERNEL_BINARY).gz kernel.bin.gz
 	$(ECHFS) -m -p0 $(BOOTDISK) import $(RAMDISK).gz ramdisk.tar.gz
 	$(ECHFS) -m -p0 $(BOOTDISK) import $(DISTRO_DIRECTORY)/limine.cfg limine.cfg
-	$(ECHFS) -m -p0 $(BOOTDISK) import thirdparty/limine/limine.sys limine.sys
+	$(ECHFS) -m -p0 $(BOOTDISK) import extramodule/limine/limine.sys limine.sys
 
 	$(LIMINE) $(BOOTDISK) 1
 
