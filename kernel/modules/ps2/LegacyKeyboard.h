92f85b9b17436500b99acd61ee1162ebfbf1cc96
krishpranav <krisna.pranav@gmail.com>
2021-07-05 15:17:29 +0530

    modules: legacy keyboard private headers

---
+++ kernel/modules/ps2/LegacyKeyboard.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <abi/Keyboard.h>
+#include <libutils/Lock.h>
+#include <libutils/RingBuffer.h>
+
+#include "ps2/LegacyDevice.h"
+
+#define PS2KBD_ESCAPE 0xE0
+
+struct LegacyKeyboard : public LegacyDevice
+{
+private:
+    Lock _events_lock{"legacy-keyboard-event"};
+    RingBuffer<char> _events{sizeof(KeyboardPacket) * 1024};
+    bool _escaped = false;
+    KeyMap *_keymap = nullptr;
+    KeyMotion _keystate[__KEY_COUNT] = {};
+
+    Key scancode_to_key(int scancode);
+
+    Text::Rune key_to_rune(Key key);
+
+    KeyModifier modifiers();
+
+    void handle_key(Key key, KeyMotion motion);
+
+}
