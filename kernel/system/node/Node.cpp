e45bd4f9dc658aea6f4f723d11d55d31ac6f06fb
krishpranav <krisna.pranav@gmail.com>
2021-07-06 13:08:46 +0530

    node: node headers

--- kernel/system/node/Node.cpp
+++ kernel/system/node/Node.cpp
@@ -9,16 +9,53 @@
 #include "system/node/Handle.h"
 #include "system/node/Node.h"
 
-
-FsNode::FsNode(JFileType type)
+FsNode::FsNode(HjFileType type)
 {
     _type = type;
-}   
+}
 
 void FsNode::ref_handle(FsHandle &handle)
 {
-    if (handle.flags() & J_OPEN_READ)
+    if (handle.flags() & HJ_OPEN_READ)
     {
         __atomic_add_fetch(&_readers, 1, __ATOMIC_SEQ_CST);
+    }
+
+    if (handle.flags() & HJ_OPEN_WRITE)
+    {
+        __atomic_add_fetch(&_writers, 1, __ATOMIC_SEQ_CST);
+    }
+
+    if (handle.flags() & HJ_OPEN_CLIENT)
+    {
+        __atomic_add_fetch(&_clients, 1, __ATOMIC_SEQ_CST);
+    }
+
+    if (handle.flags() & HJ_OPEN_SERVER)
+    {
+        __atomic_add_fetch(&_server, 1, __ATOMIC_SEQ_CST);
+    }
+}
+
+void FsNode::deref_handle(FsHandle &handle)
+{
+    if (handle.flags() & HJ_OPEN_READ)
+    {
+        __atomic_sub_fetch(&_readers, 1, __ATOMIC_SEQ_CST);
+    }
+
+    if (handle.flags() & HJ_OPEN_WRITE)
+    {
+        __atomic_sub_fetch(&_writers, 1, __ATOMIC_SEQ_CST);
+    }
+
+    if (handle.flags() & HJ_OPEN_CLIENT)
+    {
+        __atomic_sub_fetch(&_clients, 1, __ATOMIC_SEQ_CST);
+    }
+
+    if (handle.flags() & HJ_OPEN_SERVER)
+    {
+        __atomic_sub_fetch(&_server, 1, __ATOMIC_SEQ_CST);
     }
 }
