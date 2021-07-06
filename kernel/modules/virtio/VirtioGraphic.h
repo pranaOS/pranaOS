16a2f9e51f0663280327bfd0efa4ed83cc3a1b6a
krishpranav <krisna.pranav@gmail.com>
2021-07-06 10:51:22 +0530

    virtio: virito graphic headers

---
+++ kernel/modules/virtio/VirtioGraphic.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+#include "virtio/VirtioDevice.h"
+
+struct VirtioGraphic : public VirtioDevice
+{
+private:
+public:
+    VirtioGraphic(DeviceAddress address) : VirtioDevice(address, DeviceClass::FRAMEBUFFER)
+    {
+    }
+
+    ~VirtioGraphic()
+    {
+    }
+};
