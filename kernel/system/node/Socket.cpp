90b39d65ce52f5787e884752b18ca6b587f9827a
krishpranav <krisna.pranav@gmail.com>
2021-07-06 18:53:05 +0530

    Fs socket

---
+++ kernel/system/node/Socket.cpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <assert.h>
+#include "system/node/Connection.h"
+#include "system/node/Socket.h"
+
+FsSocket::FsSocket() : FsNode(J_FILE_TYPE_SOCKET)
+{
+}
+
+ResultOr<RefPtr<FsNode>> FsSocket::connect()
+{
+    auto connection = make<FsConnection>();
+    _pending.push_back(connection);
+    return {connection};
+}
+
+bool FsSocket::can_accept()
+{
+    return _pending.any();
+}
