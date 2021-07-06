717fdbb514ff6e2bbcc5b0cdb7e0a789bf77a7d3
krishpranav <krisna.pranav@gmail.com>
2021-07-06 11:28:38 +0530

    node: fs device

--- kernel/system/node/Device.h
+++ kernel/system/node/Device.h
@@ -16,5 +16,29 @@ struct FsDevice : public FsNode
 private:
     RefPtr<Device> _device;
 
+public:
+    FsDevice(RefPtr<Device> device)
+        : FsNode(J_FILE_TYPE_DEVICE),
+          _device(device)
+    {
+    }
 
+    ~FsDevice() 
+    {
+    }
+
+    size_t size() override
+    {
+        return _device->size();
+    }
+
+    bool can_read(FsHandle &) override
+    {
+        return _device->can_read();
+    }
+
+    bool can_write(FsHandle &) override
+    {
+        return _device->can_write();
+    }
 }
