609723e6a5675f7c502a15055e5584105dc2ae48
krishpranav <krisna.pranav@gmail.com>
2021-07-06 18:55:15 +0530

    node: terminal headers

--- kernel/system/node/Terminal.h
+++ kernel/system/node/Terminal.h
@@ -18,4 +18,19 @@ private:
     int _width = 80;
     int _height = 25;
 
+public:
+    RingBuffer<char> server_to_client_buffer{BUFFER_SIZE};
+    RingBuffer<char> client_to_server_buffer{BUFFER_SIZE};
+
+    FsTerminal();
+
+    bool can_read(FsHandle &handle) override;
+
+    bool can_write(FsHandle &handle) override;
+
+    ResultOr<size_t> read(FsHandle &handle, void *buffer, size_t size) override;
+
+    ResultOr<size_t> write(FsHandle &handle, const void *buffer, size_t size) override;
+
+    HjResult call(FsHandle &handle, IOCall request, void *args) override;
 };
