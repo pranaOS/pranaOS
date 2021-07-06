23d100a66e610ef3c8ec18fd1b550719107585d7
krishpranav <krisna.pranav@gmail.com>
2021-07-06 10:32:33 +0530

    update

---
+++ kernel/modules/virtio/VirtioConsole.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include "virtio/VirtioDevice.h"
+
+struct VirtioConsole : public VirtioDevice
+{
+
+private:
+public:
+    VirtioConsole(DeviceAddress address) : VirtioDevice(address, DeviceClass::CONSOLE)
+    
+}
