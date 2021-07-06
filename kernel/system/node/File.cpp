a58d17e4e4551dc2b0f89f4d676c011003c444ee
krishpranav <krisna.pranav@gmail.com>
2021-07-06 18:31:34 +0530

    node: file funcs

--- kernel/system/node/File.cpp
+++ kernel/system/node/File.cpp
@@ -6,14 +6,13 @@
 
 
 // includes
-#include <libabi/Result.h>
+#include <abi/Result.h>
 #include <libmath/MinMax.h>
 #include <string.h>
 #include "system/node/File.h"
 #include "system/node/Handle.h"
 
-
-FsFile::FsFile() : FsNode(J_FILE_TYPE_REGULAR)
+FsFile::FsFile() : FsNode(HJ_FILE_TYPE_REGULAR)
 {
     _buffer = (char *)malloc(512);
     _buffer_allocated = 512;
@@ -25,9 +24,9 @@ FsFile::~FsFile()
     free(_buffer);
 }
 
-JResult FsFile::open(FsHandle &handle)
+HjResult FsFile::open(FsHandle &handle)
 {
-    if (handle.has_flag(J_OPEN_TRUNC))
+    if (handle.has_flag(HJ_OPEN_TRUNC))
     {
         free(_buffer);
         _buffer = (char *)malloc(512);
@@ -43,7 +42,6 @@ size_t FsFile::size()
     return _buffer_size;
 }
 
-
 ResultOr<size_t> FsFile::read(FsHandle &handle, void *buffer, size_t size)
 {
     size_t read = 0;
@@ -55,4 +53,18 @@ ResultOr<size_t> FsFile::read(FsHandle &handle, void *buffer, size_t size)
     }
 
     return read;
+}
+
+ResultOr<size_t> FsFile::write(FsHandle &handle, const void *buffer, size_t size)
+{
+    if ((handle.offset() + size) > _buffer_allocated)
+    {
+        _buffer = (char *)realloc(_buffer, handle.offset() + size);
+        _buffer_allocated = handle.offset() + size;
+    }
+
+    _buffer_size = MAX(handle.offset() + size, _buffer_size);
+    memcpy((char *)(_buffer) + handle.offset(), buffer, size);
+
+    return size;
 }
