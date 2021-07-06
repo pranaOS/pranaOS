ad7b87d7cab3adec1f22d7312e3dd9ea4086f58c
krishpranav <krisna.pranav@gmail.com>
2021-07-06 12:59:48 +0530

    node: file headers

--- kernel/system/node/File.h
+++ kernel/system/node/File.h
@@ -16,5 +16,16 @@ private:
     size_t _buffer_allocated;
     size_t _buffer_size;
 
+public:
+    FsFile();
 
-}
+    ~FsFile() override;
+
+    HjResult open(FsHandle &handle) override;
+
+    size_t size() override;
+
+    ResultOr<size_t> read(FsHandle &handle, void *buffer, size_t size) override;
+
+    ResultOr<size_t> write(FsHandle &handle, const void *buffer, size_t size) override;
+};
