a564216b96e9e694c0dcfbcb7ed690b20aa206c3
krishpranav <krisna.pranav@gmail.com>
2021-07-06 08:41:00 +0530

    unix: unix zero funcs

---
+++ kernel/modules/unix/UnixZero.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <string.h>
+#include "unix/UNIXDevice.h"
+
+struct UnixZero : public UNIXDevice
+{
+private:
+public:
+    UnixZero(DeviceAddress address) : UNIXDevice(address, DeviceClass::ZERO)
+    {
+    }
+
+    ResultOr<size_t> read(size64_t offset, void *buffer, size_t size) override
+    {
+        UNUSED(offset);
+
+        memset(buffer, 0, size);
+
+        return size;
+    }
+
+    ResultOr<size_t> write(size64_t offset, const void *buffer, size_t size) override
+    {
+        UNUSED(offset);
+        UNUSED(buffer);
+
+        return size;
+    }
+};
