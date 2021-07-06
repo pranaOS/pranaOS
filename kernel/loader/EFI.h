329cc974e92d2ba3e2d6a40160134b49bde5d578
krishpranav <krisna.pranav@gmail.com>
2021-07-06 10:59:41 +0530

    loader: EFI headers

---
+++ kernel/loader/EFI.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+extern "C"
+{
+// extern includes
+#include <efi.h>
+#include <efilib.h>
+}
