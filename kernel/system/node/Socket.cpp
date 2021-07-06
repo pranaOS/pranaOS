628ccfe5532fe07e3f7d2384464888c515b44a67
krishpranav <krisna.pranav@gmail.com>
2021-07-06 18:53:21 +0530

    node: socket funcs

--- kernel/system/node/Socket.cpp
+++ kernel/system/node/Socket.cpp
@@ -9,7 +9,7 @@
 #include "system/node/Connection.h"
 #include "system/node/Socket.h"
 
-FsSocket::FsSocket() : FsNode(J_FILE_TYPE_SOCKET)
+FsSocket::FsSocket() : FsNode(HJ_FILE_TYPE_SOCKET)
 {
 }
 
@@ -23,4 +23,13 @@ ResultOr<RefPtr<FsNode>> FsSocket::connect()
 bool FsSocket::can_accept()
 {
     return _pending.any();
+}
+
+ResultOr<RefPtr<FsNode>> FsSocket::accept()
+{
+    assert(_pending.any());
+
+    auto connection = _pending.pop_back();
+    connection->accepted();
+    return connection;
 }
