97a6ce0c9de5f7e03ff2ae1b7dc49b8cb438d390
krishpranav <krisna.pranav@gmail.com>
2021-07-06 08:39:18 +0530

    unix: unix random device classes

---
+++ kernel/modules/unix/UnixRandom.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <libmath/Random.h>
+#include "unix/UNIXDevice.h"
+
+struct UnixRandom : public UNIXDevice
+{
+private:
+    Math::Random _random{};
+
+public:
+    UnixRandom(DeviceAddress address) : UNIXDevice(address, DeviceClass::RANDOM)
+    {
+    }
+
+    ResultOr<size_t> read(size64_t offset, void *buffer, size_t size) override
+    {
+        UNUSED(offset);
+
+        for (size_t i = 0; i < size; i++)
+        {
+            ((char *)buffer)[i] = _random.next_u8();
+        }
+
+        return size;
+    }
+
+    ResultOr<size_t> write(size64_t offset, const void *buffer, size_t size) override
+    {
+        UNUSED(offset);
+        UNUSED(buffer);
+
+        return size;
+    }
+};
