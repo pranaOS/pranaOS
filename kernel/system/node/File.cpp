ba218dc16882cb83e3f2fd229d2f362365873c7d
krishpranav <krisna.pranav@gmail.com>
2021-07-06 18:27:40 +0530

    node: fs file g

--- kernel/system/node/File.cpp
+++ kernel/system/node/File.cpp
@@ -3,3 +3,24 @@
  *
  * SPDX-License-Identifier: BSD-2-Clause
 */
+
+
+// includes
+#include <libabi/Result.h>
+#include <libmath/MinMax.h>
+#include <string.h>
+#include "system/node/File.h"
+#include "system/node/Handle.h"
+
+
+FsFile::FsFile() : FsNode(J_FILE_TYPE_REGULAR)
+{
+    _buffer = (char *)malloc(512);
+    _buffer_allocated = 512;
+    _buffer_size = 0;
+}
+
+FsFile::~FsFile()
+{
+    free(_buffer);
+}
