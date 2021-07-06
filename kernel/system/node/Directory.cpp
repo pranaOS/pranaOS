b030a832ef7b5b2201491d7f8892b6ff50a69008
krishpranav <krisna.pranav@gmail.com>
2021-07-06 11:41:12 +0530

    node: Directory functions

--- kernel/system/node/Directory.cpp
+++ kernel/system/node/Directory.cpp
@@ -6,12 +6,11 @@
 
 // includes
 #include <string.h>
-#include <libabi/Result.h>
+#include <abi/Result.h>
 #include "system/node/Directory.h"
 #include "system/node/Handle.h"
 
-
-FsDirectory::FsDirecotry() : FsNode(J_FILE_TYPE_DIRECTORY)
+FsDirectory::FsDirectory() : FsNode(J_FILE_TYPE_DIRECTORY)
 {
 }
 
@@ -40,4 +39,82 @@ JResult FsDirectory::open(FsHandle &handle)
     handle.attached = listing;
 
     return SUCCESS;
+}
+
+void FsDirectory::close(FsHandle &handle)
+{
+    free(handle.attached);
+}
+
+ResultOr<size_t> FsDirectory::read(FsHandle &handle, void *buffer, size_t size)
+{
+    if (size == sizeof(JDirEntry))
+    {
+        size_t index = handle.offset() / sizeof(JDirEntry);
+
+        FileListing *listing = (FileListing *)handle.attached;
+
+        if (index < listing->count)
+        {
+            *((JDirEntry *)buffer) = listing->entries[index];
+
+            return sizeof(JDirEntry);
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+RefPtr<FsNode> FsDirectory::find(String name)
+{
+    RefPtr<FsNode> result;
+
+    _children.foreach([&](auto &entry) {
+        if (entry.name == name)
+        {
+            result = entry.node;
+
+            return Iteration::STOP;
+        }
+
+        return Iteration::CONTINUE;
+    });
+
+    return result;
+}
+
+JResult FsDirectory::link(String name, RefPtr<FsNode> child)
+{
+    if (find(name))
+    {
+        return ERR_FILE_EXISTS;
+    }
+
+    _children.push_back({name, child});
+
+    return SUCCESS;
+}
+
+JResult FsDirectory::unlink(String name)
+{
+
+    bool has_removed_an_entry = _children.remove_all_match(
+        [&](auto &e) {
+            return e.name == name;
+        });
+
+    if (has_removed_an_entry)
+    {
+        return SUCCESS;
+    }
+    else
+    {
+        return ERR_NO_SUCH_FILE_OR_DIRECTORY;
+    }
 }
