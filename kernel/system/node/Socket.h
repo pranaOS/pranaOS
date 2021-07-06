785558cc1cb2341d21909a2070a87e07f21cd684
krishpranav <krisna.pranav@gmail.com>
2021-07-06 18:50:19 +0530

    node: socket headers

---
+++ kernel/system/node/Socket.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <libutils/Vector.h>
+#include "system/node/Node.h"
+
+struct FsSocket : public FsNode
+{
+private:
+    Vector<RefPtr<FsNode>> _pending{};
+
+public:
+    FsSocket();
+
+    ResultOr<RefPtr<FsNode>> connect() override;
+
+    bool can_accept() override;
+
+    ResultOr<RefPtr<FsNode>> accept() override;
+};
