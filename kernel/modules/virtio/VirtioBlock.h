b2424410220b6e329092b1681cc8f3bce84dff05
krishpranav <krisna.pranav@gmail.com>
2021-07-06 10:30:34 +0530

    virtio: virtio block headers

---
+++ kernel/modules/virtio/VirtioBlock.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include "virtio/VirtioDevice.h"
+
+struct VirtioBlock : public VirtioDevice
+{
+private:
+public:
+    VirtioBlock(DeviceAddress address) : VirtioDevice(address, DeviceClass::DISK)
+    {
+    }
+
+    ~VirtioBlock()
+    {
+    }
+};
