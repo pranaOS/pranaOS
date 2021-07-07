58c02a1653d6288dea1ee47d691793a38e1e1f4f
krishpranav <krisna.pranav@gmail.com>
2021-07-07 16:47:52 +0530

    libraries: libabi network with mac address function

---
+++ userland/libraries/libabi/Network.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+
+#pragma once
+
+// includes
+#include <libutils/Prelude.h>
+
+struct Mac
+{
+    uint8_t bytes[6];
+
+    uint8_t operator[](int index) const
+    {
+        return bytes[index];
+    }
+};
