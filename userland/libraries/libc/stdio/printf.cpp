25420182f5409d9b9d37eec73002ac90b8f50aa0
krishpranav <krisna.pranav@gmail.com>
2021-07-07 15:07:27 +0530

    libc: stdio printf funcs

---
+++ userland/libraries/libc/stdio/printf.cpp
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+
+// includes
+#include <pranaos/Printf.h>
+#include <stdio.h>
+#include <string.h>
+
+void file_append(printf_info_t *info, char c)
+{
+    fputc(c, (FILE *)info->output);
+}
+
+int vfprintf(FILE *file, const char *fmt, va_list va)
+{
+    printf_info_t info = {};
+
+    info.format = fmt;
+    info.append = file_append;
+    info.output = file;
+    info.allocated = -1;
+
+    return _-printf(&info, va);
+}
