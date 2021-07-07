35783916710ccc26fa7cff62a572b85c12530a56
krishpranav <krisna.pranav@gmail.com>
2021-07-07 14:38:11 +0530

    libc: bits abs headers

---
+++ userland/libraries/libc/bits/abs.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <__libc__.h>
+
+__BEGIN_HEADER
+
+int abs(int x);
+long labs(long j);
+long long llabs(long long x);
+
+double fabs(double x);
+float fabsf(float x);
+long double fabsl(long double x);
+
+__END_HEADER;
