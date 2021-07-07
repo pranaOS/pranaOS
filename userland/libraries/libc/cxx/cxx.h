f4e052f7a45d5a36488562463baebfac5487f906
krishpranav <krisna.pranav@gmail.com>
2021-07-07 14:46:25 +0530

    libc: cxx

---
+++ userland/libraries/libc/cxx/cxx.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <libutils/Prelude.h>
+
+extern "C" void __cxa_finalize(void *dso_handle);
