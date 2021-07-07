a9ddc8a53d3c77eb61f7dee58cf2e2d2cc480aea
krishpranav <krisna.pranav@gmail.com>
2021-07-07 14:51:59 +0530

    libc: cxx new delete funcs

---
+++ userland/libraries/libc/cxx/new-delete.cpp
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <assert.h>
+#include <libc/cxx/cxx.h>
+
+void *__attribute__((weak)) operator new(size_t size)
+{
+    return malloc(size);
+}
+
+void *__attribute__((weak)) operator new[](size_t size)
+{
+    return malloc(size);
+}
+
+void __attribute__((weak)) operator delete(void *ptr)
+{
+    free(ptr);
+}
+
+void __attribute__((weak)) operator delete[](void *ptr)
+{
+    free(ptr);
+}
+
+void __attribute__((weak)) operator delete(void *ptr, size_t size)
+{
+    UNUSED(size);
+
+    free(ptr);
+}
+
+void __attribute__((weak)) operator delete[](void *ptr, size_t size)
+{
+    UNUSED(size);
+
+    free(ptr);
+}
+
+extern "C" void __attribute__((weak)) __cxa_pure_virtual()
+{
+    ASSERT_NOT_REACHED();
+}
