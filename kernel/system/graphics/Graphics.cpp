6d51e7c430c1902145b51eb96439436704ef9f85
krishpranav <krisna.pranav@gmail.com>
2021-07-07 10:37:40 +0530

    graphics: framebuffer, handover

--- kernel/system/graphics/Graphics.cpp
+++ kernel/system/graphics/Graphics.cpp
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
 */
 
+// includes
 #include "system/Streams.h"
 #include "system/graphics/Graphics.h"
 #include "system/interrupts/Interupts.h"
@@ -13,3 +14,37 @@ static int _framebuffer_width = 0;
 static int _framebuffer_height = 0;
 static int _framebuffer_pitch = 0;
 static int _framebuffer_bpp = 0;
+
+void graphic_early_initialize(Handover *handover)
+{
+    graphic_did_find_framebuffer(
+        handover->framebuffer_addr,
+        handover->framebuffer_width,
+        handover->framebuffer_height,
+        handover->framebuffer_pitch,
+        handover->framebuffer_bpp);
+}
+
+void graphic_initialize(Handover *handover)
+{
+    if (_framebuffer_address != 0)
+    {
+        return;
+    }
+
+    framebuffer_initialize(handover);
+}
+
+
+void graphic_did_find_framebuffer(uintptr_t address, int width, int height, int pitch, int bpp)
+{
+    InterruptsRetainer retainer;
+
+    _framebuffer_address = address;
+    _framebuffer_width = width;
+    _framebuffer_height = height;
+    _framebuffer_pitch = pitch;
+    _framebuffer_bpp = bpp;
+
+    Kernel::logln("Framebuffer: w:{} h:{} p:{} bpp:{}", width, height, pitch, bpp);
+}
