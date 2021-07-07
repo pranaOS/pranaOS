4bf899e7ad2edcbd52eacf4edad534cd54f2c9d2
krishpranav <krisna.pranav@gmail.com>
2021-07-07 10:15:56 +0530

    graphics: font headers

---
+++ kernel/system/graphics/Font.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2021, nuke123-sudo
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <libtext/Rune.h>
+
+int font_height();
+int font_width();
+
+void font_draw(Text::Rune cp, int x, int y);
+
+void font_set_fg(int color);
+void font_set_bg(int color);
