c0367585ab25614e2972982b9b24278b8256d629
krishpranav <krisna.pranav@gmail.com>
2021-07-07 10:57:33 +0530

    interupts: Interupts headers

---
+++ kernel/system/interrupts/Interupts.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <libutils/Prelude.h>
+
+#define ASSERT_INTERRUPTS_RETAINED() assert(interrupts_retained())
+#define ASSERT_INTERRUPTS_NOT_RETAINED() assert(!interrupts_retained())
+
+void interrupts_initialize();
+
+bool interrupts_retained();
+
+void interrupts_enable_holding();
+
+void interrupts_disable_holding();
+
+void interrupts_retain();
+
+void interrupts_release();
+
+struct InterruptsRetainer
+{
+private:
+    NONCOPYABLE(InterruptsRetainer);
+    NONMOVABLE(InterruptsRetainer);
+
+public:
+    InterruptsRetainer()
+    {
+        interrupts_retain();
+    }
+
+    ~InterruptsRetainer()
+    {
+        interrupts_release();
+    }
+};
