6781037b46ea7a04b811d4701112c3fd76dfdedd
krishpranav <krisna.pranav@gmail.com>
2021-07-07 16:50:48 +0530

    libraries: libabi paths headers for the full proj

--- userland/libraries/libabi/Paths.h
+++ userland/libraries/libabi/Paths.h
@@ -14,5 +14,10 @@
 
 #define TEXTMODE_DEVICE_PATH DEVICE_PATH "/textmode"
 
+#define FRAMEBUFFER_DEVICE_PATH DEVICE_PATH "/framebuffer"
+
+#define NETWORK_DEVICE_PATH DEVICE_PATH "/network"
+
+#define SERIAL_DEVICE_PATH DEVICE_PATH "/serial"
 
 #define UNIX_DEVICE_PATH(__device) DEVICE_PATH "/" __device
