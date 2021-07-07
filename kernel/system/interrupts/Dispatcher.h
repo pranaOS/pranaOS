04e28d3e5b8b7e2ed09253879437cf1bf06b89f7
krishpranav <krisna.pranav@gmail.com>
2021-07-07 10:43:49 +0530

    interrupts: Dispatcher headers

---
+++ kernel/system/interrupts/Dispatcher.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include "system/node/Node.h"
+
+typedef void (*DispatcherInteruptHandler)();
+
+void dispatcher_initialize();
+void dispatcher_dispatch(int interrupt);
+void dispatcher_service();
