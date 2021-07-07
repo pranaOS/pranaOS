38f5397ec2c55a75e7f8556bb7c070121c543282
krishpranav <krisna.pranav@gmail.com>
2021-07-07 14:20:34 +0530

    libc: pranaos custom print functions

--- userland/libraries/libc/pranaos/Printf.h
+++ userland/libraries/libc/pranaos/Printf.h
@@ -27,7 +27,6 @@ typedef enum
     PFALIGN_RIGHT
 } printf_align_t;
 
-
 typedef struct printf_info
 {
     char c;
@@ -37,13 +36,49 @@ typedef struct printf_info
     const char *format;
     int format_offset;
 
+    // Formating
     char padding;
     printf_align_t align;
-    size_t lenght;
+    size_t length;
 
     void *output;
     int written;
     int allocated;
 } printf_info_t;
 
-typedef int (*printf_formatter_impl_t)(printf_info_t *info, va_list va);
+typedef int (*printf_formatter_impl_t)(printf_info_t *info, va_list *va);
+
+typedef struct
+{
+    char c;
+    printf_formatter_impl_t impl;
+} printf_formatter_t;
+
+#define PRINTF_PEEK()                                  \
+    {                                                  \
+        info->c = info->format[info->format_offset++]; \
+        if (info->c == '\0')                           \
+            return info->written;                      \
+    }
+
+#define PRINTF_APPEND(__c)                                             \
+    {                                                                  \
+        if (info->allocated != -1 && info->written >= info->allocated) \
+            return info->written;                                      \
+                                                                       \
+        info->append(info, __c);                                       \
+        info->written++;                                               \
+    }
+
+#define PRINTF_PADDING(__buffer, __a)                                      \
+    {                                                                      \
+        if (info->align == (__a) && strlen(__buffer) < info->length)       \
+        {                                                                  \
+            for (size_t i = 0; i < (info->length - strlen(__buffer)); i++) \
+            {                                                              \
+                PRINTF_APPEND(info->padding);                              \
+            }                                                              \
+        }                                                                  \
+    }
+
+int __printf(printf_info_t *info, va_list va);
