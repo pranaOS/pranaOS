4ad8e55263cd0467d2a18c25637969b450087df2
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2021-07-07 18:53:41 +0530

    process headers

---
+++ userland/libraries/libabi/Process.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+#define PROCESS_NAME_SIZE 128
+#define PROCESS_STACK_SIZE 65536
+#define PROCESS_ARG_COUNT 128
+#define PROCESS_HANDLE_COUNT 128
+#define PROCESS_SUCCESS (0)
+#define PROCESS_FAILURE (1)
