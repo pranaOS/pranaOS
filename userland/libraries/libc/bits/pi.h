c674b491d2088ba9a57a5c5e6c0c95986319259f
krishpranav <krisna.pranav@gmail.com>
2021-07-07 14:39:48 +0530

    libc: bits pi headers

---
+++ userland/libraries/libc/bits/pi.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+#define PI (3.14159265358979323846264338327f)
+#define M_PI PI
+#define M_PI_2 (M_PI / 2.0)
+#define M_PI_4 (M_PI / 4.0)
+
+#define M_1_PI 0.31830988618379067154 
+#define M_2_PI (M_PI * 2.0)
+#define M_2_SQRTPI 1.12837916709551257390
