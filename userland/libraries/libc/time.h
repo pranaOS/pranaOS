7e60257558f46c44b97cf5f65e25b5cd85374d6d
krishpranav <krisna.pranav@gmail.com>
2021-07-08 19:20:50 +0530

    libraries: libc time [tm]

--- userland/libraries/libc/time.h
+++ userland/libraries/libc/time.h
@@ -18,5 +18,18 @@ typedef long int time_t;
 
 typedef long int clock_t;
 
+struct tm
+{
+    int tm_sec;  
+    int tm_min;  
+    int tm_hour; 
+    int tm_mday; 
+    int tm_mon;  
+    int tm_year; 
+    int tm_wday; 
+    int tm_yday; 
+    int tm_isdst;
+};
+
 
 __END_HEADER 
