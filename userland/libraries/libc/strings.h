671b10096097957d68c8ccfe5f97eee8cff49a60
krishpranav <krisna.pranav@gmail.com>
2021-07-08 19:17:01 +0530

    libraries: libc strings headers

---
+++ userland/libraries/libc/strings.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <__libc__.h>
+#include <stdlib.h>
+
+__BEGIN_HEADER
+
+int strcasecmp(const char *s1, const char *s2);
+int strncasecmp(const char *s1, const char *s2, size_t n);
+
+__END_HEADER
