d0b7188a7fbbf282e508258c57d78f8df3c75ec7
krishpranav <krisna.pranav@gmail.com>
2021-07-08 16:01:01 +0530

    libraries: libc fcntl define values

---
+++ userland/libraries/libc/fcntl.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <__libc__.h>
+#include <sys/types.h>
+
+__BEGIN_HEADER
+
+#define O_RDONLY 0x0000
+#define O_WRONLY 0x0001
+#define O_RDWR 0x0002
+#define O_APPEND 0x0008
+#define O_CREAT 0x0200
+#define O_TRUNC 0x0400
+#define O_EXCL 0x0800
+#define O_NOFOLLOW 0x1000
+#define O_PATH 0x2000
+#define O_NONBLOCK 0x4000
+#define O_DIRECTORY 0x8000
+
+#define F_GETFD 1
+#define F_SETFD 2
+
+#define F_GETFL 3
+#define F_SETFL 4
+
+#define F_GETLK 5
+#define F_SETLK 6
+#define F_SETLKW 7
+
+#define F_RDLCK 0
+#define F_WRLCK 1
+#define F_UNLCK 2
+
+__END_HEADER
