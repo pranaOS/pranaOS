d90dac90f6b6740b5f70461884515040198ff24d
krishpranav <krisna.pranav@gmail.com>
2021-07-08 16:43:38 +0530

    libraries: libc locale defined values

---
+++ userland/libraries/libc/locale.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <__libc__.h>
+
+__BEGIN_HEADER
+
+#define LC_ALL 1
+#define LC_COLLATE 2
+#define LC_CTYPE 3
+#define LC_MONETARY 4
+#define LC_NUMERIC 5
+#define LC_TIME 6
+#define LC_MESSAGES 7
+
+__END_HEADERS
