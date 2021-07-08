0a6e2923daa720479fb736a486d9c64b43b282d2
krishpranav <krisna.pranav@gmail.com>
2021-07-08 18:25:18 +0530

    libraries: libc new match double funcs

---
+++ userland/libraries/libc/new_math.h
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+void sincos(double, double *, double *);
+void sincosf(float, float *, float *);
+void sincosl(long double, long double *, long double *);
+
+double exp10(double);
+float exp10f(float);
+long double exp10l(long double);
+
+double pow10(double);
+float pow10f(float);
+long double pow10l(long double);
+
+
+double acos(double x);
+float acosf(float x);
+long double acosl(long double x);
+double asin(double x);
+float asinf(float x);
+long double asinl(long double x);
+double atan(double x);
+float atanf(float x);
+long double atanl(long double x);
+double atan2(double x, double y);
+float atan2f(float x, float y);
+long double atan2l(long double x, long double y);
+double cos(double x);
+float cosf(float x);
+long double cosl(long double x);
+double sin(double x);
+float sinf(float x);
+long double sinl(long double x);
+double tan(double x);
+float tanf(float x);
+long double tanl(long double x);
+
+
+double acosh(double x);
+float acoshf(float x);
+long double acoshl(long double x);
+
+double asinh(double x);
+float asinhf(float x);
+long double asinhl(long double x);
+
+double atanh(double x);
+float atanhf(float x);
+long double atanhl(long double x);
+
+double cosh(double x);
+float coshf(float x);
+long double coshl(long double x);
+
+double sinh(double x);
+float sinhf(float x);
+long double sinhl(long double x);
+
+double tanh(double x);
+float tanhf(float x);
+long double tanhl(long double x);
