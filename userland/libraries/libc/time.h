52d3fd2c7268d782d6917bb4e3588d6aa5a37ce8
krishpranav <krisna.pranav@gmail.com>
2021-07-08 19:22:15 +0530

    libraries: libc time headers

--- userland/libraries/libc/time.h
+++ userland/libraries/libc/time.h
@@ -8,6 +8,7 @@
 
 // includes
 #include <__libc__.h>
+
 #include <stddef.h>
 
 __BEGIN_HEADER
@@ -20,16 +21,33 @@ typedef long int clock_t;
 
 struct tm
 {
-    int tm_sec;  
-    int tm_min;  
+    int tm_sec;   
+    int tm_min;   
     int tm_hour; 
-    int tm_mday; 
-    int tm_mon;  
-    int tm_year; 
-    int tm_wday; 
-    int tm_yday; 
-    int tm_isdst;
+    int tm_mday;  
+    int tm_mon;   
+    int tm_year;  
+    int tm_wday;  
+    int tm_yday;  
+    int tm_isdst; 
 };
 
+time_t time(time_t *timer);
 
-__END_HEADER 
+clock_t clock(void);
+
+struct tm *gmtime(const time_t *timer);
+
+struct tm *localtime(const time_t *timer);
+
+size_t strftime(char *s, size_t n, const char *format, const struct tm *tptr);
+
+time_t mktime(struct tm *ptm);
+
+double difftime(time_t timer2, time_t timer1);
+
+char *asctime(const struct tm *pTime);
+
+char *ctime(const time_t *pTime);
+
+__END_HEADER
