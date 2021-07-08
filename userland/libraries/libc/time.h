4f8e45ea1ad2a745d62ba7de86532f0f119d5593
krishpranav <krisna.pranav@gmail.com>
2021-07-08 19:20:19 +0530

    libraries: libc time[clock]

---
+++ userland/libraries/libc/time.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <__libc__.h>
+#include <stddef.h>
+
+__BEGIN_HEADER
+
+#define CLOCKS_PER_SEC 1000
+
+typedef long int time_t;
+
+typedef long int clock_t;
+
+
+__END_HEADER 
