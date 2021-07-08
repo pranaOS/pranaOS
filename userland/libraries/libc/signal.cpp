45f53205e6811dbda4151bb3226d16aafc8371ce
krishpranav <krisna.pranav@gmail.com>
2021-07-08 18:36:45 +0530

    libraries: lic signal functions

--- userland/libraries/libc/signal.cpp
+++ userland/libraries/libc/signal.cpp
@@ -11,11 +11,23 @@
 
 sighandler_t signal(int sig, sighandler_t handler)
 {
-
+    //TODO: connect this handler
     UNUSED(sig);
     UNUSED(handler);
 
     ASSERT_NOT_REACHED();
 
     return NULL;
+}
+
+int raise(int sig)
+{
+    switch (sig)
+    {
+    case SIGTERM:
+    case SIGKILL:
+        return j_process_exit(-1) == JResult::SUCCESS ? 0 : -1;
+    default:
+        return -1;
+    }
 }
