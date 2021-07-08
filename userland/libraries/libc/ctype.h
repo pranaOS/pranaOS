a1c4d9b90cb41c567962905cc758005e9a39f81c
krishpranav <krisna.pranav@gmail.com>
2021-07-08 14:13:04 +0530

    libraries: libc ctype int funcs

---
+++ userland/libraries/libc/ctype.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+#include <__libc__.h>
+
+__BEGIN_HEADER
+
+int isalnum(int c);
+int isalpha(int c);
+int isdigit(int c);
+int islower(int c);
+int isprint(int c);
+int isgraph(int c);
+int iscntrl(int c);
+int isgraph(int c);
+int ispunct(int c);
+int isspace(int c);
+int isupper(int c);
+int isxdigit(int c);
+
+
+
+__END_HEADER
