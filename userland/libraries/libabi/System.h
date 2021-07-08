8122be51dcd5307c7ad0d94bb1967cd13d1495e1
krishpranav <krisna.pranav@gmail.com>
2021-07-08 08:58:04 +0530

    libraries: libabi System headers

--- userland/libraries/libabi/System.h
+++ userland/libraries/libabi/System.h
@@ -14,9 +14,18 @@
 
 struct SystemInfo
 {
-    char kernel_name[SYSTEM_INFO_FIELD_SIZE];    
-    char kernel_release[SYSTEM_INFO_FIELD_SIZE]; 
+    char kernel_name[SYSTEM_INFO_FIELD_SIZE];   
+    char kernel_release[SYSTEM_INFO_FIELD_SIZE];
     char kernel_build[SYSTEM_INFO_FIELD_SIZE];
     char system_name[SYSTEM_INFO_FIELD_SIZE]; 
     char machine[SYSTEM_INFO_FIELD_SIZE];     
+};
+
+struct SystemStatus
+{
+    ElapsedTime uptime;
+    size_t total_ram;
+    size_t used_ram;
+    int running_tasks;
+    int cpu_usage;
 };
