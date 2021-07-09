1a5715b65bf3c2e8ebf5e4ef71ee58b399444a84
krishpranav <krisna.pranav@gmail.com>
2021-07-09 17:23:20 +0530

    libraries: libio handle operator, read handle

--- userland/libraries/libio/Handle.h
+++ userland/libraries/libio/Handle.h
@@ -38,6 +38,35 @@ public:
         _result = J_handle_open(&_handle, resolved_path.cstring(), resolved_path.length(), flags);
     }
 
+    Handle(Handle &&other)
+    {
+        _handle = std::exchange(other._handle, HANDLE_INVALID_ID);
+        _result = std::exchange(other._result, ERR_BAD_HANDLE);
+    }
+
+    Handle &operator=(Handle &&other)
+    {
+        std::swap(_handle, other._handle);
+        std::swap(_result, other._result);
+        
+        return *this;
+    }
+
+    ~Handle()
+    {
+        if (_handle != HANDLE_INVALID_ID)
+        {
+            J_handle_close(_handle);
+            _handle = -1;
+        }
+    }
+
+    ResultOr<size_t> read(void *buffer, size_t size)
+    {
+        size_t data_read = 0;
+        _result = TRY(J_handle_read(_handle, buffer, size, &data_read));
+        return data_read;
+    }
 
 
 };
