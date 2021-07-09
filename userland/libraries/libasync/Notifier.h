fddf295937db6022fe6dcfd544ed0082cdfba0fb
krishpranav <krisna.pranav@gmail.com>
2021-07-09 11:04:58 +0530

    libraries: libasync notifer headers

--- userland/libraries/libasync/Notifier.h
+++ userland/libraries/libasync/Notifier.h
@@ -6,7 +6,7 @@
 
 #pragma once
 
-// includes
+// incldues
 #include <libasync/Source.h>
 #include <libio/Handle.h>
 #include <libutils/Func.h>
@@ -14,15 +14,36 @@
 namespace Async
 {
 
-struct Notifer :
+struct Notifier :
     public IO::RawHandle,
     public Source
-
 {
 private:
     RefPtr<IO::Handle> _handle;
     PollEvent _events;
     Func<void()> _callback;
+
+public:
+    RefPtr<IO::Handle> handle() override { return _handle; }
+    PollEvent events() { return _events; }
+
+    Notifier(RawHandle &raw_handle, PollEvent events, Func<void()> callback)
+        : _handle{raw_handle.handle()},
+          _events{events},
+          _callback{callback}
+    {
+        loop().register_notifier(this);
+    }
+
+    ~Notifier()
+    {
+        loop().unregister_notifier(this);
+    }
+
+    void invoke()
+    {
+        _callback();
+    }
 };
 
 }
