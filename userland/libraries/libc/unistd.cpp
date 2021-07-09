015053e5cd76ad8796bbe1c2d6e9b4af7f3f7008
krishpranav <krisna.pranav@gmail.com>
2021-07-09 08:16:51 +0530

    libraries: libc unistd functions

--- userland/libraries/libc/unistd.cpp
+++ userland/libraries/libc/unistd.cpp
@@ -7,7 +7,8 @@
 // includes
 #include <string.h>
 #include <unistd.h>
-#include <libabi/Syscalls.h>
+
+#include <abi/Syscalls.h>
 
 ssize_t read(int fd, void *buf, size_t count)
 {
@@ -24,10 +25,10 @@ ssize_t read(int fd, void *buf, size_t count)
     }
 }
 
-ssize_t write(int fd, void *buf, size_t count)
+ssize_t write(int fd, const void *buf, size_t count)
 {
-    size_t read = 0;
-    JResult result = J_handle_read(fd, buf, count, &written);
+    size_t written = 0;
+    JResult result = J_handle_write(fd, buf, count, &written);
 
     if (result != JResult::SUCCESS)
     {
@@ -39,3 +40,32 @@ ssize_t write(int fd, void *buf, size_t count)
     }
 }
 
+int unlink(const char *pathname)
+{
+    return J_filesystem_unlink(pathname, strlen(pathname)) == JResult::SUCCESS ? 0 : -1;
+}
+
+int rmdir(const char *dirname)
+{
+    return unlink(dirname);
+}
+
+int close(int fd)
+{
+    return J_handle_close(fd) == JResult::SUCCESS ? 0 : -1;
+}
+
+off_t lseek(int fd, off_t offset, int whence)
+{
+    ssize64_t offset_64 = offset;
+    ssize64_t offset_beg;
+
+    JResult result = J_handle_seek(fd, &offset_64, (JWhence)whence, &offset_beg);
+    if (result != JResult::SUCCESS)
+    {
+        // TODO: set errno
+        return (off_t)-1;
+    }
+
+    return offset_beg;
+}
