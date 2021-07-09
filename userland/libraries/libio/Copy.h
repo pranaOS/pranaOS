6cc05db5bd1479c558ea4988fb3423eb77154076
krishpranav <krisna.pranav@gmail.com>
2021-07-09 18:55:10 +0530

    libraries: libio Copy headers

--- userland/libraries/libio/Copy.h
+++ userland/libraries/libio/Copy.h
@@ -18,7 +18,7 @@ namespace IO
 
 constexpr int COPY_CHUNK_SIZE = 4096;
 
-static inline JResult copy(Reader &from, Writer &to, size_t n)
+static inline HjResult copy(Reader &from, Writer &to, size_t n)
 {
     size_t remaining = n;
 
@@ -46,7 +46,7 @@ static inline JResult copy(Reader &from, Writer &to, size_t n)
     } while (1);
 }
 
-static inline JResult copy(Reader &from, Writer &to)
+static inline HjResult copy(Reader &from, Writer &to)
 {
     do
     {
@@ -91,5 +91,88 @@ static inline ResultOr<Slice> read_all(MemoryReader &reader)
     }
 }
 
-    
+static inline HjResult write_all(Writer &writer, Slice data)
+{
+    MemoryReader memory{data};
+    return copy(memory, writer);
+}
+
+static inline ResultOr<size_t> copy_line(Reader &from, Writer &to, char delimiter = '\n')
+{
+    size_t written = 0;
+
+    char c;
+    size_t read = TRY(from.read(&c, sizeof(c)));
+
+    while (read > 0 && c != delimiter)
+    {
+        written += TRY(to.write(&c, sizeof(c)));
+        TRY(from.read(&c, sizeof(c)));
+    }
+
+    if (c == delimiter)
+    {
+        written += TRY(to.write(&c, sizeof(c)));
+    }
+
+    return written;
+}
+
+static inline ResultOr<size_t> copy_line(Scanner &scan, Writer &to, String delimiter = "\n", bool write_delim = true)
+{
+    size_t written = 0;
+
+    while (!scan.peek_is_word(delimiter.cstring()) &&
+           !scan.ended())
+    {
+        written += TRY(IO::write(to, scan.next()));
+    }
+
+    if (scan.skip_word(delimiter.cstring()) && write_delim)
+    {
+        IO::write(to, delimiter);
+    }
+
+    return written;
+}
+
+static inline HjResult head(Reader &from, Writer &to, char delimiter = '\n', size_t n = 10)
+{
+    for (size_t i = 0; i < n; i++)
+    {
+        TRY(copy_line(from, to, delimiter));
+    }
+
+    return SUCCESS;
+}
+
+static inline HjResult tail(Reader &from, Writer &to, char delimiter = '\n', size_t n = 10)
+{
+    Vector<Slice> tail;
+
+    auto append_line = [&](Slice line) {
+        tail.push_back(line);
+        if (tail.count() > n)
+        {
+            tail.pop();
+        }
+    };
+
+    size_t copied = 0;
+    MemoryWriter memory;
+
+    do
+    {
+        copied = TRY(copy_line(from, memory, delimiter));
+        append_line(Slice{memory.slice()});
+    } while (copied > 0);
+
+    for (auto line : tail)
+    {
+        TRY(write_all(to, line));
+    }
+
+    return SUCCESS;
+}
+
 }
