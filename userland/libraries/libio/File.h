5e98aefeb7ff8389ab6c4a9a991c4b83e2bf4178
krishpranav <krisna.pranav@gmail.com>
2021-07-09 19:22:19 +0530

    libraries: libio includes

---
+++ userland/libraries/libio/File.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <libio/Path.h>
+#include <libio/Handle.h>
+#include <libio/Reader.h>
+#include <libio/Writer.h>
