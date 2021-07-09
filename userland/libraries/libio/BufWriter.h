05294bd2c0925caef2b3f882191281de84bee0df
krishpranav <krisna.pranav@gmail.com>
2021-07-09 18:25:18 +0530

    libraries: bit writer private funcs

--- userland/libraries/libio/BufWriter.h
+++ userland/libraries/libio/BufWriter.h
@@ -3,3 +3,25 @@
  *
  * SPDX-License-Identifier: BSD-2-Clause
 */
+
+#pragma once
+
+// includes
+#include <libio/Write.h>
+#include <libutils/Prelude.h>
+
+namespace IO
+{
+
+struct BitWriter
+{
+
+private:
+    uint_fast32_t _bit_buffer;
+    uint8_t _bit_count;
+    Writer &_writer;
+
+
+};
+
+}
