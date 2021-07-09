d751d3f8cf2061c3f444633d06b90921ef0b85ce
krishpranav <krisna.pranav@gmail.com>
2021-07-09 10:49:50 +0530

    loop private funcs

--- userland/libraries/libasync/Loop.h
+++ userland/libraries/libasync/Loop.h
@@ -22,4 +22,32 @@ struct Notifier;
 struct Timer;
 
 struct Invoker;
+
+struct Loop : public RefCounted<Loop>
+{
+private:
+    bool _is_running = false;
+    int _exit_value = PROCESS_SUCCESS;
+
+    bool _nested_is_running = false;
+    int _nested_exit_value = 0;
+
+    Vector<HandlePoll> _polls;
+
+    Vector<Notifier *> _notifiers;
+    Vector<Timer *> _timers;
+    Vector<Invoker *> _invoker;
+
+    void update_polling_list();
+    
+    void update_notifier(int id, PollEvent event);
+
+    void update_timers();
+
+    void update_invoker();
+
+    Timeout get_timeout();
+
+
+};
 }
