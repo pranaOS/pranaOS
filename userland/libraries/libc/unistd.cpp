3aed450514e0da62c5e63f73651f68b3ae5017a3
krishpranav <krisna.pranav@gmail.com>
2021-07-09 08:15:29 +0530

    libraries: libc unistd read funcs

---
+++ userland/libraries/libc/unistd.cpp
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <string.h>
+#include <unistd.h>
+#include <libabi/Syscalls.h>
+
+ssize_t read(int fd, void *buf, size_t count)
+{
+    size_t read = 0;
+    JResult result = J_handle_read(fd, buf, count, &read);
+
+    if (result != JResult::SUCCESS)
+    {
+        return -1;
+    }
+    else
+    {
+        return (ssize_t)read;
+    }
+}
