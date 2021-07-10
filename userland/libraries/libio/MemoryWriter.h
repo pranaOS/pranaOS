12a6c265ee8d1ef1aba28deffd95451a4c32645e
krishpranav <krisna.pranav@gmail.com>
2021-07-10 11:04:57 +0530

    includes

---
+++ userland/libraries/libio/MemoryWriter.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021, nuke123-sudo
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+
+#pragma once
+
+// includes
+#include <libio/Writer.h>
+#include <libmath/MinMax.h>
+#include <libutils/SliceStorage.h>
+#include <libutils/StringStorage.h>
