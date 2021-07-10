818b56fb1534570b150f721559a776c15638520a
krishpranav <krisna.pranav@gmail.com>
2021-07-10 16:53:59 +0530

    libraries: libsystem stream headers

--- userland/libraries/libsystem/io/Stream.h
+++ userland/libraries/libsystem/io/Stream.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2021, nuke123-sudo
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
 #pragma once
 
 // includes
@@ -16,12 +22,27 @@ struct Stream
     bool has_unget;
     int unget_char;
 
-    bool is_end_of_file
+    bool is_end_of_file;
 };
 
-Stream *stream_open(const char *path, JOpenFlag flags);
+Stream *stream_open(const char *path, HjOpenFlag flags);
+
 Stream *stream_open_handle(int handle_id, JOpenFlag flags);
 
 void stream_close(Stream *stream);
-void stream_cleanup(Stream *stream)
 
+void stream_cleanup(Stream **stream);
+
+size_t stream_read(Stream *stream, void *buffer, size_t size);
+
+size_t stream_write(Stream *stream, const void *buffer, size_t size);
+
+HjResult stream_call(Stream *stream, IOCall request, void *arg);
+
+int stream_seek(Stream *stream, IO::SeekFrom from);
+
+void stream_stat(Stream *stream, HjStat *stat);
+
+int stream_format(Stream *stream, const char *fmt, ...);
+
+int stream_vprintf(Stream *stream, const char *fmt, va_list va);
