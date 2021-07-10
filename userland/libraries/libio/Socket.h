222084efbb5da1728513283893b52bd1f0f8ba2e
krishpranav <krisna.pranav@gmail.com>
2021-07-10 15:40:48 +0530

    libraries: libio Socket headers

--- userland/libraries/libio/Socket.h
+++ userland/libraries/libio/Socket.h
@@ -10,19 +10,40 @@
 #include <libio/Connection.h>
 #include <libio/Handle.h>
 
-
 namespace IO
 {
 
 struct Socket :
     public RawHandle
 {
-
 private:
     RefPtr<Handle> _handle;
 
+public:
+    RefPtr<Handle> handle() { return _handle; }
 
+    Socket()
+    {
+    }
 
+    Socket(String path, HjOpenFlag flags)
+        : _handle{make<Handle>(path, flags | HJ_OPEN_SOCKET)}
+    {
+    }
+
+    static ResultOr<Connection> connect(String path)
+    {
+        int hnd;
+        TRY(hj_handle_connect(&hnd, path.cstring(), path.length()));
+        return Connection{make<Handle>(hnd)};
+    }
+
+    ResultOr<Connection> accept()
+    {
+        auto accept_result = _handle->accept();
+        TRY(accept_result);
+        return Connection{accept_result.unwrap()};
+    }
 };
 
 }
