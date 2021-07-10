36ac4bd535825c547f07a3955c16b4678b8670ac
krishpranav <krisna.pranav@gmail.com>
2021-07-10 14:54:04 +0530

    libraries: libio ScopedReader headers

--- userland/libraries/libio/ScopedReader.h
+++ userland/libraries/libio/ScopedReader.h
@@ -19,11 +19,25 @@ struct ScopedReader :
 private:
     Reader &_reader;
     size_t _count = 0;
-    size_t _lenght = 0;
+    size_t _length = 0;
 
 public:
-    
+    ScopedReader(Reader &reader, size_t length) : _reader{reader}, _length{length}
+    {
+    }
 
+    inline ResultOr<size_t> length()
+    {
+        return _length;
+    }
+
+    ResultOr<size_t> read(void *buffer, size_t size) override
+    {
+        size_t to_read = MIN(_length - _count, size);
+        auto result = TRY(_reader.read(buffer, to_read));
+        _count += result;
+        return result;
+    }
 };
 
 }
