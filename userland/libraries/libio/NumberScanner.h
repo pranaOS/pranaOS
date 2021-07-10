c22506e782913355611b1a1614a5038b65a81df2
krishpranav <krisna.pranav@gmail.com>
2021-07-10 11:14:20 +0530

    libraries: libio NumberScanner headers

--- userland/libraries/libio/NumberScanner.h
+++ userland/libraries/libio/NumberScanner.h
@@ -21,7 +21,7 @@ struct NumberScanner
 public:
     static NumberScanner binary() { return {2}; }
 
-    static NumberScanner octal() {return {8}; }
+    static NumberScanner octal() { return {8}; }
 
     static NumberScanner decimal() { return {10}; }
 
@@ -43,7 +43,7 @@ public:
         return false;
     }
 
-        Optional<uint8_t> scan_digit(Scanner &scan)
+    Optional<uint8_t> scan_digit(Scanner &scan)
     {
         char c = scan.peek();
 
@@ -60,6 +60,78 @@ public:
         return NONE;
     }
 
+    Optional<uint64_t> scan_uint(Scanner &scan)
+    {
+        if (!is_digit(scan))
+        {
+            return NONE;
+        }
+
+        uint64_t value = 0;
+
+        while (!scan.ended() && is_digit(scan))
+        {
+            value = value * _base;
+            value += scan_digit(scan).unwrap();
+        }
+
+        return value;
+    }
+
+    Optional<int64_t> scan_int(Scanner &scan)
+    {
+        if (!is_digit(scan) &&
+            scan.peek() != '-')
+        {
+            return NONE;
+        }
+
+        bool is_negative = scan.skip('-');
+
+        auto unsigned_value = scan_uint(scan);
+
+        if (!unsigned_value.present())
+        {
+            return NONE;
+        }
+
+        if (is_negative)
+        {
+            return -unsigned_value.unwrap();
+        }
+        else
+        {
+            return unsigned_value.unwrap();
+        }
+    }
+
+    Optional<double> scan_float(Scanner &scan)
+    {
+        int64_t ipart = scan_int(scan).unwrap_or(0);
+
+        double fpart = 0;
+
+        if (scan.skip('.'))
+        {
+            double multiplier = (1.0 / _base);
+
+            while (is_digit(scan))
+            {
+                fpart += multiplier * scan_digit(scan).unwrap();
+                multiplier *= (1.0 / _base);
+            }
+        }
+
+        int64_t exp = 0;
+
+        if (scan.peek_is_any("eE"))
+        {
+            scan.next();
+            exp = scan_int(scan).unwrap_or(0);
+        }
+
+        return (ipart + fpart) * pow(_base, exp);
+    }
 };
 
 }
