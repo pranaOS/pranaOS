7528d95949f1c8ca0aaf9bb038e7ffaa9e26c699
krishpranav <krisna.pranav@gmail.com>
2021-07-10 16:10:25 +0530

    writer struct

---
+++ userland/libraries/libio/Writer.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <libio/Seek.h>
+
+namespace IO
+{
+
+struct Writer
+{
+    virtual ~Writer() {}
+    virtual ResultOr<size_t> write(const void *buffer, size_t size) = 0;
+    virtual JResult flush() { return SUCCESS; }
+
+};
+
+}
