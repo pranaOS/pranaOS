8cb76506a14f1cf20f6270f01de8767063a32767
krishpranav <krisna.pranav@gmail.com>
2021-07-10 16:57:43 +0530

    libraries: libsystem stream functions

--- userland/libraries/libsystem/io/Stream.cpp
+++ userland/libraries/libsystem/io/Stream.cpp
@@ -39,3 +39,43 @@ void stream_close(Stream *stream)
 
     free(stream);
 }
+
+
+void stream_cleanup(Stream **stream)
+{
+    if (*stream)
+    {
+        stream_close(*stream);
+        *stream = nullptr;
+    }
+}
+
+
+size_t stream_read(Stream *stream, void *buffer, size_t size)
+{
+    if (!stream)
+    {
+        return 0;
+    }
+
+    size_t result = 0;
+
+    if (stream->has_unget && size >= 1)
+    {
+        ((char *)buffer)[0] = stream->unget_char;
+        stream->has_unget = false;
+        buffer = &((char *)buffer)[1];
+        size--;
+
+        result = 1;
+    }
+
+    result = __plug_handle_read(HANDLE(stream), buffer, size);
+
+    if (result == 0)
+    {
+        stream->is_end_of_file = true;
+    }
+
+    return result;
+}
