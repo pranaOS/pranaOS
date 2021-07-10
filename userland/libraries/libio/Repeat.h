f9dca871bb0c4177126871def7ec7d93b1be0868
krishpranav <krisna.pranav@gmail.com>
2021-07-10 14:27:32 +0530

    libraries: libio Repeat headers

--- userland/libraries/libio/Repeat.h
+++ userland/libraries/libio/Repeat.h
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
 */
 
-#pragma once 
+#pragma once
 
 // includes
 #include <libio/Reader.h>
@@ -16,8 +16,20 @@ struct Repeat :
     public Reader
 {
 private:
-    uint8_t c;
+    uint8_t _c;
 
+public:
+    Repeat(uint8_t c) : _c{c} {}
+
+    ResultOr<size_t> read(void *buffer, size_t size)
+    {
+        for (size_t i = 0; i < size; i++)
+        {
+            ((uint8_t *)buffer)[i] = _c;
+        }
+
+        return size;
+    }
 };
 
 }
