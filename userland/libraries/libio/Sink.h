6596142585eed049ca230b99ca815e9182814032
krishpranav <krisna.pranav@gmail.com>
2021-07-10 15:32:13 +0530

    libraries: libio sink headers

---
+++ userland/libraries/libio/Sink.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <libio/Write.h>
+
+namespace IO
+{
+
+struct Sink :
+    public Writer
+{
+    ResultOr<size_t> write(const void *buffer, size_t size) override
+    {
+        UNUSED(buffer);
+        return size
+    }
+};
+
+}
