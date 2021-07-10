3d19f3354f054b31ca86a3978a2877124358f6f0
krishpranav <krisna.pranav@gmail.com>
2021-07-10 11:08:40 +0530

    libraries: libio MemoryWriter headers

--- userland/libraries/libio/MemoryWriter.h
+++ userland/libraries/libio/MemoryWriter.h
@@ -18,7 +18,7 @@ namespace IO
 
 struct MemoryWriter :
     public Writer,
-    public Seek 
+    public Seek
 {
 private:
     size_t _used = 0;
@@ -52,12 +52,12 @@ public:
         }
     }
 
-    ResultOr<size_t> lenght()
+    ResultOr<size_t> length()
     {
         return _used;
     }
 
-    ResultOr<size_t> tall()
+    ResultOr<size_t> tell()
     {
         return _position;
     }
@@ -83,6 +83,82 @@ public:
         }
     }
 
+    inline uint8_t *buffer()
+    {
+        return _buffer;
+    }
+
+    // Create a string and flush the buffer!;
+    RefPtr<StringStorage> string()
+    {
+        write('\0'); // Null terminator
+
+        uint8_t *result = _buffer;
+        size_t size = _used;
+
+        _buffer = nullptr;
+        _used = 0;
+        _size = 0;
+        _position = 0;
+
+        return make<StringStorage>(ADOPT, (char *)result, size - 1);
+    }
+
+    RefPtr<SliceStorage> slice()
+    {
+        uint8_t *result = _buffer;
+        size_t size = _used;
+
+        _buffer = nullptr;
+        _used = 0;
+        _size = 0;
+        _position = 0;
+
+        return make<SliceStorage>(ADOPT, (void *)result, size);
+    }
+
+    ResultOr<size_t> write(uint8_t v)
+    {
+        if (_size == 0)
+        {
+            _buffer = new uint8_t[16];
+            _buffer[0] = '\0';
+            _size = 16;
+            _used = 0;
+            _position = 0;
+        }
+
+        if (_used == _size)
+        {
+            auto new_size = _size + _size / 4;
+            auto new_buffer = new uint8_t[new_size];
+            memcpy(new_buffer, _buffer, _size);
+            delete[] _buffer;
+
+            _size = new_size;
+            _buffer = new_buffer;
+        }
+
+        _buffer[_position] = v;
+        _position++;
+
+        if (_position > _used)
+        {
+            _used = _position;
+        }
+
+        return 1;
+    }
+
+    ResultOr<size_t> write(const void *buffer, size_t size) override
+    {
+        for (size_t i = 0; i < size; i++)
+        {
+            write(((uint8_t *)buffer)[i]);
+        }
+
+        return size;
+    }
 };
 
 }
