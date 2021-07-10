a4f674c8fdac956446bbb05ec165c36bc3aa2cb3
krishpranav <krisna.pranav@gmail.com>
2021-07-10 09:16:07 +0530

    libraries: libio File functions

--- userland/libraries/libio/File.cpp
+++ userland/libraries/libio/File.cpp
@@ -10,20 +10,20 @@
 namespace IO
 {
 
-File::File(const char *path, HjOpenFlag flags)
-    : _handle{make<Handle>(path, flags | HJ_OPEN_STREAM)},
+File::File(const char *path, JOpenFlag flags)
+    : _handle{make<Handle>(path, flags | J_OPEN_STREAM)},
       _path{IO::Path::parse(path)}
 {
 }
 
-File::File(String path, HjOpenFlag flags)
-    : _handle{make<Handle>(path, flags | HJ_OPEN_STREAM)},
+File::File(String path, JOpenFlag flags)
+    : _handle{make<Handle>(path, flags | J_OPEN_STREAM)},
       _path{IO::Path::parse(path)}
 {
 }
 
-File::File(IO::Path &path, HjOpenFlag flags)
-    : _handle{make<Handle>(path.string(), flags | HJ_OPEN_STREAM)},
+File::File(IO::Path &path, JOpenFlag flags)
+    : _handle{make<Handle>(path.string(), flags | J_OPEN_STREAM)},
       _path{path}
 {
 }
@@ -40,12 +40,48 @@ ResultOr<size_t> File::read(void *buffer, size_t size)
 
 ResultOr<size_t> File::write(const void *buffer, size_t size)
 {
-    return _handle->write(buffer ,size);
+    return _handle->write(buffer, size);
 }
 
 ResultOr<size_t> File::call(IOCall call, void *args)
 {
     return _handle->call(call, args);
+}
+
+ResultOr<size_t> File::seek(SeekFrom from)
+{
+    auto seek_result = _handle->seek(from);
+
+    if (seek_result.success())
+    {
+        return (size_t)seek_result.unwrap();
+    }
+    else
+    {
+        return seek_result.result();
+    }
+}
+
+ResultOr<size_t> File::tell()
+{
+    return (size_t)_handle->tell().unwrap();
+}
+
+ResultOr<size_t> File::length()
+{
+    auto stat = TRY(_handle->stat());
+    return stat.size;
+}
+
+ResultOr<JFileType> File::type()
+{
+    auto stat = TRY(_handle->stat());
+    return stat.type;
+}
+
+bool File::exist()
+{
+    return _handle->valid();
 }
 
 }
