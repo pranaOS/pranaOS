5a6cd7a5d22fc4bf08a030fbfee9c2a63744f1bc
krishpranav <krisna.pranav@gmail.com>
2021-07-10 16:10:48 +0530

    libraries: libio Writer headers

--- userland/libraries/libio/Writer.h
+++ userland/libraries/libio/Writer.h
@@ -15,9 +15,13 @@ namespace IO
 struct Writer
 {
     virtual ~Writer() {}
-    virtual ResultOr<size_t> write(const void *buffer, size_t size) = 0;
-    virtual JResult flush() { return SUCCESS; }
 
+    virtual ResultOr<size_t> write(const void *buffer, size_t size) = 0;
+
+    virtual HjResult flush() { return SUCCESS; }
 };
+
+template <typename T>
+concept SeekableWriter = IsBaseOf<Writer, T>::value &&IsBaseOf<Seek, T>::value;
 
 }
