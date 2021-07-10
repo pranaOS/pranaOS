793d4ed0e4a6240cc4ed569388e315219338f3f3
krishpranav <krisna.pranav@gmail.com>
2021-07-10 16:33:43 +0530

    libraries: libsystme process

--- userland/libraries/libsystem/core/Plugs.h
+++ userland/libraries/libsystem/core/Plugs.h
@@ -20,3 +20,22 @@
 TimeStamp __plug_system_get_time();
 Tick __plug_system_get_ticks();
 
+int __plug_process_this();
+
+const char *__plug_process_name();
+
+JResult __plug_process_launch(Launchpad *launchpad, int *pid);
+
+void NO_RETURN __plug_process_exit(int code);
+
+JResult __plug_process_cancel(int pid);
+
+String __plug_process_resolve(String raw_path);
+
+JResult __plug_process_get_directory(char *buffer, size_t size);
+
+JResult __plug_process_set_directory(const char *directory);
+
+JResult __plug_process_sleep(int time);
+
+JResult __plug_process_wait(int pid, int *exit_value);
