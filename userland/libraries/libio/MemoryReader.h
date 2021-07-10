fe64fc07260b14e93e8d246d7de11b5ab4d9b320
krishpranav <krisna.pranav@gmail.com>
2021-07-10 11:03:56 +0530

    libraries: libio MemoryReader headers

--- userland/libraries/libio/MemoryReader.h
+++ userland/libraries/libio/MemoryReader.h
@@ -18,7 +18,6 @@ struct MemoryReader final :
     public Reader,
     public Seek
 {
-
 private:
     Slice _memory;
     size_t _position = 0;
@@ -46,6 +45,48 @@ public:
     {
     }
 
+    ResultOr<size_t> read(void *buffer, size_t size) override
+    {
+        if (_position > _memory.size())
+        {
+            return 0;
+        }
+
+        size_t remaining = MIN(_memory.size() - _position, size);
+
+        memcpy(buffer, ((uint8_t *)_memory.start()) + _position, remaining);
+        _position += remaining;
+
+        return remaining;
+    }
+
+    ResultOr<size_t> seek(SeekFrom from) override
+    {
+        switch (from.whence)
+        {
+        case Whence::START:
+            return _position = from.position;
+
+        case Whence::CURRENT:
+            return _position += from.position;
+
+        case Whence::END:
+            return _position = _memory.size() + from.position;
+
+        default:
+            ASSERT_NOT_REACHED();
+        }
+    }
+
+    ResultOr<size_t> length() override
+    {
+        return _memory.size();
+    }
+
+    ResultOr<size_t> tell() override
+    {
+        return _position;
+    }
 };
 
 }
