a0666ba3a8e250575052b1d880387d0c59901b61
krishpranav <krisna.pranav@gmail.com>
2021-07-10 18:09:52 +0530

    libraries: libsystem process launchpad values

--- userland/libraries/libsystem/process/Launchpad.h
+++ userland/libraries/libsystem/process/Launchpad.h
@@ -16,3 +16,17 @@
 Launchpad *launchpad_create(const char *name, const char *executable);
 
 void launchpad_destroy(Launchpad *launchpad);
+
+void launchpad_flags(Launchpad *launchpad, TaskFlags flags);
+
+void launchpad_argument(Launchpad *launchpad, const char *argument);
+
+void launchpad_arguments(Launchpad *launchpad, const Vector<String> &arguments);
+
+void launchpad_environment(Launchpad *launchpad, const char *buffer);
+
+void launchpad_handle(Launchpad *launchpad, IO::RawHandle &handle_to_pass, int destination);
+
+void launchpad_handle(Launchpad *launchpad, IO::Handle &handle, int destination);
+
+void launchpad_handle(Launchpad *launchpad, Handle *handle_to_pass, int destination);
