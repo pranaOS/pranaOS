6900ad5c86e6e2b830c97c7688dd18844650c495
krishpranav <krisna.pranav@gmail.com>
2021-07-10 16:08:05 +0530

    libraries: libio write funcs

---
+++ userland/libraries/libio/Write.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <string.h>
+#include <libio/Writer.h>
+#include <libutils/String.h>
+
+namespace IO
+{
+
+static inline ResultOr<size_t> write(Writer &writer, char value)
+{
+    return writer.write(&value, sizeof(value));
+}
+
+static inline ResultOr<size_t> write(Writer &writer, const char *cstring)
+{
+    return writer.write(cstring, strlen(cstring));
+}
+
+static inline ResultOr<size_t> write(Writer &writer, String string)
+{
+    return writer.write(string.cstring(), string.length());
+}
+
+}
