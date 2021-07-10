3a3ddbbde9396e0b7914267814f486006e6e09a6
krishpranav <krisna.pranav@gmail.com>
2021-07-10 11:00:58 +0530

    libraries: libio Memory reader public values

---
+++ userland/libraries/libio/MemoryReader.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2021, nuke123-sudo
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <libio/Reader.h>
+#include <libmath/MinMax.h>
+#include <libutils/Slice.h>
+
+namespace IO
+{
+
+struct MemoryReader final :
+    public Reader,
+    public Seek
+{
+
+};
+
+}
