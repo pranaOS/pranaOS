d32fc71c05a892fbb08927507c11ccfc873a8c25
krishpranav <krisna.pranav@gmail.com>
2021-07-10 15:33:49 +0530

    libraries: libio Skip headers

---
+++ userland/libraries/libio/Skip.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <libio/Copy.h>
+#include <libio/Sink.h>
+
+namespace IO
+{
+
+static inline HjResult skip(Reader &from, size_t n)
+{
+    Sink sink;
+    return copy(from, sink, n);
+}
+
+}
