a8ef2d622a491d80e83ab9fb8a51c3910aa80f13
krishpranav <krisna.pranav@gmail.com>
2021-07-10 16:03:00 +0530

    libraries: libio Streams functions

---
+++ userland/libraries/libio/Streams.cpp
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <libio/Streams.h>
+
+namespace IO
+{
+
+static InStream _in{};
+static OutStream _out{};
+static ErrStream _err{};
+static LogStream _log{};
+
+InStream &in() { return _in; }
+OutStream &out() { return _out; }
+ErrStream &err() { return _err; }
+LogStream &log() { return _log; }
+
+}
