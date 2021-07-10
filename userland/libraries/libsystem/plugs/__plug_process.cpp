1c13f40f29663f0761b404d6317176162d61317a
krishpranav <krisna.pranav@gmail.com>
2021-07-10 17:40:44 +0530

    libraries: libsystem plug process functions

--- userland/libraries/libsystem/plugs/__plug_process.cpp
+++ userland/libraries/libsystem/plugs/__plug_process.cpp
@@ -10,7 +10,6 @@
 #include <libio/Path.h>
 #include <libsystem/core/Plugs.h>
 
-
 int __plug_process_this()
 {
     int pid = -1;
@@ -22,12 +21,39 @@ static bool _has_cached_name = false;
 
 static char _cached_name[PROCESS_NAME_SIZE];
 
-const char *__plug_memory_name()
+const char *__plug_process_name()
 {
     if (!_has_cached_name)
     {
-        assert(J_process_name(_cached_name, PROCESS_NAME) == SUCCESS);
+        assert(J_process_name(_cached_name, PROCESS_NAME_SIZE) == SUCCESS);
     }
 
     return _cached_name;
+}
+
+JResult __plug_process_launch(Launchpad *launchpad, int *pid)
+{
+    return J_process_launch(launchpad, pid);
+}
+
+void __plug_process_exit(int code)
+{
+    J_process_exit(code);
+
+    ASSERT_NOT_REACHED();
+}
+
+JResult __plug_process_cancel(int pid)
+{
+    return J_process_cancel(pid);
+}
+
+JResult __plug_process_sleep(int time)
+{
+    return J_process_sleep(time);
+}
+
+JResult __plug_process_wait(int pid, int *exit_value)
+{
+    return J_process_wait(pid, exit_value);
 }
