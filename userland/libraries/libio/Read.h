1f520bcf438355040aec59b9311774acb6c6892f
krishpranav <krisna.pranav@gmail.com>
2021-07-10 12:24:26 +0530

    libraries: libio read vector

---
+++ userland/libraries/libio/Read.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <libio/Copy.h>
+#include <libio/Reader.h>
+#include <libutils/Assert.h>
+#include <libutils/Vector.h>
+
+namespace IO
+{
+
+template <typename T>
+ResultOr<size_t> read_vector(Reader &reader, Vector<T> &vector)
+{
+    size_t read = 0;
+
+    T object;
+    size_t last_read = TRY(reader.read(&object, sizeof(T)));
+    while (last_read > 0)
+    {
+        vector.push_back(std::move(object));
+        read += last_read;
+        
+        last_read = TRY(reader.read(&object, sizeof(T)));
+    }
+
+    return read;
+
+}
+
+}
