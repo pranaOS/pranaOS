da17bfd3ecd1b2309db39b4696611993111c4a80
krishpranav <krisna.pranav@gmail.com>
2021-07-11 11:13:33 +0530

    gdt

--- kernel/archs/x86_32/GDT.h
+++ kernel/archs/x86_32/GDT.h
@@ -7,8 +7,8 @@
 
 #pragma once
 
-// includes
 #include <libutils/Prelude.h>
+
 #include "system/Streams.h"
 
 namespace Arch::x86_32
@@ -56,3 +56,44 @@ struct PACKED TSS
     uint16_t trap;
     uint16_t iomap_base;
 };
+
+struct PACKED GDTDescriptor
+{
+    uint16_t size;
+    uint32_t offset;
+};
+
+struct PACKED GDTEntry
+{
+    uint16_t limit0_15;
+    uint16_t base0_15;
+    uint8_t base16_23;
+    uint8_t access;
+    uint8_t limit16_19 : 4;
+    uint8_t flags : 4;
+    uint8_t base24_31;
+
+    constexpr GDTEntry()
+        : GDTEntry(0, 0, 0, 0)
+    {
+    }
+
+    constexpr GDTEntry(uint32_t base, uint32_t limit, uint8_t access, uint8_t flags)
+        : limit0_15((uint16_t)((limit)&0xffff)),
+          base0_15((uint16_t)((base)&0xffff)),
+          base16_23((uint8_t)(((base) >> 16) & 0xff)),
+          access((access)),
+          limit16_19(((limit) >> 16) & 0x0f),
+          flags((flags)),
+          base24_31((uint8_t)(((base) >> 24) & 0xff))
+    {
+    }
+
+    constexpr GDTEntry(TSS *tss, uint8_t access, uint8_t flags)
+        : GDTEntry((uintptr_t)tss, ((uintptr_t)tss) + sizeof(TSS), access, flags)
+    {
+    }
+};
+
+
+}
