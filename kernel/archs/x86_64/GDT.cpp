357a3c7caf804cf886406d0ca8ddd5ded096a91c
krishpranav <krisna.pranav@gmail.com>
2021-07-11 16:41:05 +0530

    kernel: archs x86_64 GDT functions

--- kernel/archs/x86_64/GDT.cpp
+++ kernel/archs/x86_64/GDT.cpp
@@ -21,5 +21,31 @@ static TSS64 tss = {
     .iopb_offset = 0,
 };
 
+static GDT64 gdt = {};
+
+static GDTDescriptor64 gdt_descriptor = {
+    .size = sizeof(GDT64) - 1,
+    .offset = (uint64_t)&gdt,
+};
+
+void gdt_initialize()
+{
+    gdt.entries[0] = {0, 0, 0, 0}; 
+    gdt.entries[1] = {GDT_PRESENT | GDT_SEGMENT | GDT_READWRITE | GDT_EXECUTABLE, GDT_LONG_MODE_GRANULARITY};
+    gdt.entries[2] = {GDT_PRESENT | GDT_SEGMENT | GDT_READWRITE, 0};
+
+    gdt.entries[3] = {GDT_PRESENT | GDT_SEGMENT | GDT_READWRITE | GDT_EXECUTABLE | GDT_USER, GDT_LONG_MODE_GRANULARITY};
+    gdt.entries[4] = {GDT_PRESENT | GDT_SEGMENT | GDT_READWRITE | GDT_USER, 0};
+
+    gdt.tss = {(uintptr_t)&tss};
+
+    gdt_flush((uint64_t)&gdt_descriptor);
+}
+
+void set_kernel_stack(uint64_t stack)
+{
+    tss.rsp[0] = stack;
+    tss.ist[0] = stack;
+}
 
 }
