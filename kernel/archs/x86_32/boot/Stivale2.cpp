8e809b7f2ded6a237669112093d667981f669e5f
krishpranav <krisna.pranav@gmail.com>
2021-07-11 16:15:26 +0530

    kernel: archs x86_32 stivale 2 functions

---
+++ kernel/archs/x86_32/boot/Stivale2.cpp
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <libutils/Prelude.h>
+
+extern "C" void arch_x86_32_main(void *info, uint32_t magic);
+
+extern "C" void _kstart_stivale2(void *info)
+{
+    arch_x86_32_main(info, 0x73747632);
+}
