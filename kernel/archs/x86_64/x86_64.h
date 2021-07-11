d1dd0b471483a7cae641ce36f027e32e137b29e3
krishpranav <krisna.pranav@gmail.com>
2021-07-11 17:31:32 +0530

    kernel: archs x86_64 headers

---
+++ kernel/archs/x86_64/x86_64.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+#include "archs/x86/x86.h"
+
+namespace Arch::x86_64
+{
+
+static inline uint64_t RBP()
+{
+    uint64_t r;
+    asm volatile("mov %%rbp, %0"
+                 : "=r"(r));
+    return r;
+}
+
+}
