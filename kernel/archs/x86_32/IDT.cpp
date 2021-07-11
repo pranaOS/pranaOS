c5ff6ce3cc8b657c36056efc6ffea16d49d730bd
krishpranav <krisna.pranav@gmail.com>
2021-07-11 11:20:15 +0530

    kernel: archs x86_32 IDT functions

--- kernel/archs/x86_32/IDT.cpp
+++ kernel/archs/x86_32/IDT.cpp
@@ -19,4 +19,25 @@ IDTDescriptor idt_descriptor = {
     .offset = (uint32_t)&idt[0],
 };
 
+void idt_initialize()
+{
+    for (int i = 0; i < 3; i++)
+    {
+        idt[i] = IDT_ENTRY(__interrupt_vector[i], 0x08, INTGATE);
+    }
+
+    idt[3] = IDT_ENTRY(__interrupt_vector[3], 0x08, TRAPGATE);
+    idt[4] = IDT_ENTRY(__interrupt_vector[4], 0x08, TRAPGATE);
+
+    for (int i = 5; i < 48; i++)
+    {
+        idt[i] = IDT_ENTRY(__interrupt_vector[i], 0x08, INTGATE);
+    }
+
+    idt[127] = IDT_ENTRY(__interrupt_vector[48], 0x08, INTGATE);
+    idt[128] = IDT_ENTRY(__interrupt_vector[49], 0x08, INTGATE | IDT_USER);
+
+    idt_flush((uint32_t)&idt_descriptor);
+}
+
 }
