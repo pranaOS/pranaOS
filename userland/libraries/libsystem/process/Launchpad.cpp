e775a084efd5ca16ae76346bd250b80f0d9844cb
krishpranav <krisna.pranav@gmail.com>
2021-07-11 08:24:51 +0530

    libraries: libsystem process Launchpad functions

--- userland/libraries/libsystem/process/Launchpad.cpp
+++ userland/libraries/libsystem/process/Launchpad.cpp
@@ -31,20 +31,21 @@ Launchpad *launchpad_create(const char *name, const char *executable)
     launchpad->handles[3] = 3;
 
     auto executable_path = IO::Path::parse(executable);
-    launchpad_argument(launchpad, executable_path.base().cstring());
+    launchpad_argument(launchpad, executable_path.basename().cstring());
 
 #ifndef __KERNEL__
     auto env_copy = environment_copy();
     launchpad_environment(launchpad, strdup(env_copy.cstring()));
 #endif
+
     return launchpad;
 }
 
-void launchpad_destroy(Launchpad &launchpad)
+void launchpad_destroy(Launchpad *launchpad)
 {
     for (int i = 0; i < launchpad->argc; i++)
     {
-        free(laucnhpad->argv[i].buffer);
+        free(launchpad->argv[i].buffer);
     }
 
     if (launchpad->env)
@@ -53,4 +54,87 @@ void launchpad_destroy(Launchpad &launchpad)
     }
 
     free(launchpad);
+}
+
+void launchpad_flags(Launchpad *launchpad, TaskFlags flags)
+{
+    launchpad->flags = flags;
+}
+
+void launchpad_argument(Launchpad *launchpad, const char *argument)
+{
+    assert(launchpad->argc < PROCESS_ARG_COUNT);
+
+    if (argument)
+    {
+        launchpad->argv[launchpad->argc].buffer = strdup(argument);
+        launchpad->argv[launchpad->argc].size = strlen(argument);
+    }
+    else
+    {
+        launchpad->argv[launchpad->argc].buffer = strdup("");
+        launchpad->argv[launchpad->argc].size = strlen("");
+    }
+
+    launchpad->argv[launchpad->argc + 1].buffer = nullptr;
+
+    launchpad->argc++;
+}
+
+void launchpad_arguments(Launchpad *launchpad, const Vector<String> &arguments)
+{
+    for (size_t i = 0; i < arguments.count(); i++)
+    {
+        launchpad_argument(launchpad, arguments[i].cstring());
+    }
+}
+
+void launchpad_handle(Launchpad *launchpad, IO::RawHandle &handle_to_pass, int destination)
+{
+    assert(destination >= 0 && destination < PROCESS_ARG_COUNT);
+
+    launchpad->handles[destination] = handle_to_pass.handle()->id();
+}
+
+void launchpad_handle(Launchpad *launchpad, IO::Handle &handle, int destination)
+{
+    assert(destination >= 0 && destination < PROCESS_ARG_COUNT);
+
+    launchpad->handles[destination] = handle.id();
+}
+
+void launchpad_handle(Launchpad *launchpad, Handle *handle_to_pass, int destination)
+{
+    assert(destination >= 0 && destination < PROCESS_ARG_COUNT);
+
+    launchpad->handles[destination] = handle_to_pass->id;
+}
+
+void launchpad_environment(Launchpad *launchpad, const char *buffer)
+{
+    if (launchpad->env)
+    {
+        free(launchpad->env);
+    }
+
+    launchpad->env = strdup(buffer);
+    launchpad->env_size = strlen(buffer);
+}
+
+JResult launchpad_launch(Launchpad *launchpad, int *pid)
+{
+    int discard;
+
+    if (pid == nullptr)
+    {
+        pid = &discard;
+    }
+
+    strcpy(launchpad->executable, process_resolve(launchpad->executable).cstring());
+
+    JResult result = __plug_process_launch(launchpad, pid);
+
+    launchpad_destroy(launchpad);
+
+    return result;
 }
