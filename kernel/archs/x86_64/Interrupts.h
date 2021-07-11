202b48b30b60a0cd425aee2e239e083ebb6a105a
krishpranav <krisna.pranav@gmail.com>
2021-07-11 17:23:31 +0530

    kernel: archs x86_64 Interrupts headers

---
+++ kernel/archs/x86_64/Interrupts.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <libutils/Prelude.h>
+
+namespace Arch::x86_64
+{
+
+struct PACKED InterruptStackFrame
+{
+    uint64_t r15;
+    uint64_t r14;
+    uint64_t r13;
+    uint64_t r12;
+    uint64_t r11;
+    uint64_t r10;
+    uint64_t r9;
+    uint64_t r8;
+    uint64_t rbp;
+    uint64_t rdi;
+    uint64_t rsi;
+    uint64_t rdx;
+    uint64_t rcx;
+    uint64_t rbx;
+    uint64_t rax;
+
+    uint64_t intno;
+    uint64_t err;
+
+    uint64_t rip;
+    uint64_t cs;
+    uint64_t rflags;
+    uint64_t rsp;
+    uint64_t ss;
+};
+
+}
