c99a7cdba84f6637ebf768beb0407329d4e6b6e5
krishpranav <krisna.pranav@gmail.com>
2021-07-11 15:55:30 +0530

    kernel: archs x86_32 Paging headers

--- kernel/archs/x86_32/Paging.h
+++ kernel/archs/x86_32/Paging.h
@@ -80,4 +80,20 @@ void virtual_initialize();
 
 void virtual_memory_enable();
 
+bool virtual_present(PageDirectory *page_directory, uintptr_t virtual_address);
+
+uintptr_t virtual_to_physical(PageDirectory *page_directory, uintptr_t virtual_address);
+
+JResult virtual_map(PageDirectory *page_directory, MemoryRange physical_range, uintptr_t virtual_address, MemoryFlags flags);
+
+MemoryRange virtual_alloc(PageDirectory *page_directory, MemoryRange physical_range, MemoryFlags flags);
+
+void virtual_free(PageDirectory *page_directory, MemoryRange virtual_range);
+
+PageDirectory *page_directory_create();
+
+void page_directory_destroy(PageDirectory *page_directory);
+
+void page_directory_switch(PageDirectory *page_directory);
+
 }
