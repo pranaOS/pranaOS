c294db9ce1cea9f88ecab2117b90bfb98611454d
krishpranav <krisna.pranav@gmail.com>
2021-07-11 08:27:58 +0530

    libraries: libsystem process Process headers

--- userland/libraries/libsystem/process/Process.h
+++ userland/libraries/libsystem/process/Process.h
@@ -21,3 +21,15 @@ JResult process_run(const char *command, int *pid, TaskFlags flags);
 void NO_RETURN process_abort();
 
 void NO_RETURN process_exit(int code);
+
+JResult process_cancel(int pid);
+
+JResult process_get_directory(char *buffer, size_t size);
+
+JResult process_set_directory(const char *directory);
+
+String process_resolve(String path);
+
+JResult process_sleep(int time);
+
+JResult process_wait(int pid, int *exit_value);
