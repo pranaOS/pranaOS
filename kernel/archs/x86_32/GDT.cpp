9c0848865f5065ef326b45d965a5c1dc080d4420
krishpranav <krisna.pranav@gmail.com>
2021-07-11 11:15:51 +0530

    kernel: arch x86_32 tss

---
+++ kernel/archs/x86_32/GDT.cpp
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include "archs/x86_32/GDT.h"
+
+namespace Arch::x86_32
+{
+
+static TSS tss = {
+    .prev_tss = 0,
+    .esp0 = 0,
+    .ss0 = 0x10,
+    .esp1 = 0,
+    .ss1 = 0,
+    .esp2 = 0,
+    .ss2 = 0,
+    .cr3 = 0,
+    .eip = 0,
+    .eflags = 0x0202,
+    .eax = 0,
+    .ecx = 0,
+    .edx = 0,
+    .ebx = 0,
+    .esp = 0,
+    .ebp = 0,
+    .esi = 0,
+    .edi = 0,
+    .es = 0,
+    .cs = 0,
+    .ss = 0,
+    .ds = 0,
+    .fs = 0,
+    .gs = 0,
+    .ldt = 0,
+    .trap = 0,
+    .iomap_base = 0,
+};
+
+
+}
