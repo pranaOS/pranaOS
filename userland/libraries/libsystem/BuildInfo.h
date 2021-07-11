b6c68b61deda5e69c44d7357f7936c900b1aac02
krishpranav <krisna.pranav@gmail.com>
2021-07-11 08:46:44 +0530

    bulid config

---
+++ userland/libraries/libsystem/BuildInfo.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+#ifndef __BUILD_ARCH__
+#   define __BUILD_ARCH__ "unkown"
+#endif
+
+#ifndef __BUILD_CONFIG__
+#   define __BUILD_CONFIG__ "unknow"
