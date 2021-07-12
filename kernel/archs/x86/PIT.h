6f9e4a357ecb09c549eefb55fefe8404e2084a28
krishpranav <krisna.pranav@gmail.com>
2021-07-12 08:59:25 +0530

    kernel: archs x86 PIT headers

---
+++ kernel/archs/x86/PIT.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <libutils/Prelude.h>
+
+void pit_initialize(int frequency);
