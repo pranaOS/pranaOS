2c62c50d629b62715c3c579589973293dd9787a9
krishpranav <krisna.pranav@gmail.com>
2021-07-12 17:05:30 +0530

    kernel: system memory Physical functions

--- kernel/system/memory/Physical.cpp
+++ kernel/system/memory/Physical.cpp
@@ -15,7 +15,7 @@ size_t TOTAL_MEMORY = 0;
 size_t USED_MEMORY = 0;
 
 size_t best_bet = 0;
-uint8_t USED_MEMORY =0 ;
+uint8_t MEMORY[1024 * 1024 / 8] = {};
 
 bool physical_page_is_used(uintptr_t address)
 {
@@ -33,7 +33,7 @@ void physical_page_set_used(uintptr_t address)
         best_bet++;
     }
 
-    MEMORY[page / 8] != 1 << (page % 8);
+    MEMORY[page / 8] |= 1 << (page % 8);
 }
 
 void physical_page_set_free(uintptr_t address)
@@ -68,12 +68,66 @@ MemoryRange physical_alloc(size_t size)
     system_panic("Out of physical memory!\tTrying to allocat %dkio but free memory is %dkio !", size / 1024, (TOTAL_MEMORY - USED_MEMORY) / 1024);
 }
 
-
 void physical_free(MemoryRange range)
 {
     ASSERT_INTERRUPTS_RETAINED();
 
     assert(range.is_page_aligned());
 
-    physical_set_free();
+    physical_set_free(range);
+}
+
+bool physical_is_used(MemoryRange range)
+{
+    ASSERT_INTERRUPTS_RETAINED();
+
+    assert(range.is_page_aligned());
+
+    for (size_t i = 0; i < range.page_count(); i++)
+    {
+        uintptr_t address = range.base() + i * ARCH_PAGE_SIZE;
+
+        if (physical_page_is_used(address))
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void physical_set_used(MemoryRange range)
+{
+    ASSERT_INTERRUPTS_RETAINED();
+
+    assert(range.is_page_aligned());
+
+    for (size_t i = 0; i < range.page_count(); i++)
+    {
+        uintptr_t address = range.base() + i * ARCH_PAGE_SIZE;
+
+        if (!physical_page_is_used(address))
+        {
+            USED_MEMORY += ARCH_PAGE_SIZE;
+            physical_page_set_used(address);
+        }
+    }
+}
+
+void physical_set_free(MemoryRange range)
+{
+    ASSERT_INTERRUPTS_RETAINED();
+
+    assert(range.is_page_aligned());
+
+    for (size_t i = 0; i < range.page_count(); i++)
+    {
+        uintptr_t address = range.base() + i * ARCH_PAGE_SIZE;
+
+        if (physical_page_is_used(address))
+        {
+            USED_MEMORY -= ARCH_PAGE_SIZE;
+            physical_page_set_free(address);
+        }
+    }
 }
