01e5f0ba7fc7b561a08ba60ea6b3890202ac97c7
krishpranav <krisna.pranav@gmail.com>
2021-07-12 18:12:26 +0530

    kernel: system scheduling Scheduler headers

--- kernel/system/scheduling/Scheduler.h
+++ kernel/system/scheduling/Scheduler.h
@@ -15,10 +15,18 @@ void scheduler_initialize();
 
 void scheduler_did_create_idle_task(Task *task);
 
+void scheduler_did_create_running_task(Task *task);
+
 void scheduler_did_change_task_state(Task *task, TaskState oldstate, TaskState newstate);
 
 bool scheduler_is_context_switch();
 
+int scheduler_get_usage(int task_id);
+
 Task *scheduler_running();
 
-uintptr_t schedule(uinptr_t current_stack_pointer);
+int scheduler_running_id();
+
+void scheduler_yield();
+
+uintptr_t schedule(uintptr_t current_stack_pointer);
