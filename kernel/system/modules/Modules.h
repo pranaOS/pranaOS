88df47cf748a6f163ad330a3216025e20d35aa7e
krishpranav <krisna.pranav@gmail.com>
2021-07-12 17:29:03 +0530

    kernel: system modules headers

---
+++ kernel/system/modules/Modules.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include "system/handover/Handover.h"
+
+void modules_initialize(Handover *handover);
+
+void ramdisk_load(Module *module);
