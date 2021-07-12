288cc8a7017e307d143d6c99195179de75c0c6dd
krishpranav <krisna.pranav@gmail.com>
2021-07-12 16:57:29 +0530

    kernel: system memory Physical headers

--- kernel/system/memory/Physical.h
+++ kernel/system/memory/Physical.h
@@ -16,3 +16,10 @@ extern uint8_t MEMORY[1024 * 1024 / 8];
 
 MemoryRange physical_alloc(size_t size);
 
+void physical_free(MemoryRange range);
+
+bool physical_is_used(MemoryRange range);
+
+void physical_set_used(MemoryRange range);
+
+void physical_set_free(MemoryRange range);
