4cce54eabb33f8b65324e1e0c3153db7fe198b4b
krishpranav <krisna.pranav@gmail.com>
2021-07-12 11:08:16 +0530

    kernel: system handover define

---
+++ kernel/system/handover/Handover.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// inlcudes
+#include "system/memory/MemoryRange.h"
+
+#define HANDOVER_BOOTLOADER_NAME_SIZE 64
+#define HANDOVER_COMMAND_LINE_SIZE 256
+#define HANDOVER_MEMORY_MAP_SIZE 64
+#define HANDOVER_MODULES_SIZE 16
