6145a7116be3950311df805cb95faa584e55cbec
krishpranav <krisna.pranav@gmail.com>
2021-07-12 16:45:36 +0530

    kernel: memory MemoryObject headers

--- kernel/system/memory/MemoryObject.h
+++ kernel/system/memory/MemoryObject.h
@@ -22,3 +22,11 @@ struct MemoryObject
 void memory_object_initialize();
 
 MemoryObject *memory_object_create(size_t size);
+
+void memory_object_destroy(MemoryObject *memory_object);
+
+MemoryObject *memory_object_ref(MemoryObject *memory_object);
+
+void memory_object_deref(MemoryObject *memory_object);
+
+MemoryObject *memory_object_by_id(int id);
