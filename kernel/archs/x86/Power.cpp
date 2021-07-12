d05e5ef1558343362fd1a3d997a4a0b73bdb9d92
krishpranav <krisna.pranav@gmail.com>
2021-07-12 09:07:44 +0530

    kernel: archs x86 Power functions

--- kernel/archs/x86/Power.cpp
+++ kernel/archs/x86/Power.cpp
@@ -6,7 +6,9 @@
 
 // includes
 #include "system/Streams.h"
+
 #include "archs/Arch.h"
+
 #include "archs/x86/IOPort.h"
 #include "archs/x86/Power.h"
 #include "archs/x86/x86.h"
@@ -16,7 +18,7 @@ namespace Arch::x86
 
 void reboot_8042()
 {
-    Kernel::logln("Trying to reboot using the 8042.....");
+    Kernel::logln("Trying to reboot using the 8042...");
 
     uint8_t good = 0x02;
 
@@ -31,11 +33,26 @@ void reboot_8042()
 
 void reboot_triple_fault()
 {
-    Kernel::logln("Trying to reboot by doing a triple fault.....");
+    Kernel::logln("Trying to reboot by doing a triple fault...");
     cli();
     Arch::address_space_switch(0x0);
     *(uint32_t *)0x0 = 0xDEADDEAD;
 }
 
+void shutdown_virtual_machines()
+{
+    Kernel::logln("Maybe your are running a VM, trying to shutdown using io ports...");
+
+    out16(0xB004, 0x2000);
+
+    out16(0x604, 0x2000);
+
+    out16(0x4004, 0x3400);
+}
+
+void shutdown_acpi()
+{
+    Kernel::logln("Trying to shutdown using acpi...");
+}
 
 }
