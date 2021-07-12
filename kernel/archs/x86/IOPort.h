aedf62273824f5eff74aca3a85aaf7e489ba1cb9
krishpranav <krisna.pranav@gmail.com>
2021-07-12 08:46:40 +0530

    kernel: archs x86 inline values

---
+++ kernel/archs/x86/IOPort.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <libutils/Prelude.h>
+
+static inline uint8_t in8(uint16_t port)
+{
+    uint8_t data;
+    asm volatile("inb %1, %0"
+                 : "=a"(data)
+                 : "d"(port));
+    return data;
+}
+
+static inline uint16_t in16(uint16_t port)
+{
+    uint16_t data;
+    asm volatile("inw %1, %0"
+                 : "=a"(data)
+                 : "d"(port));
+    return data;
+}
