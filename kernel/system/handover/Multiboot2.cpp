fa5e28f7952df940d1b321e64b2f38325f461d12
krishpranav <krisna.pranav@gmail.com>
2021-07-12 15:53:34 +0530

    multiboot2 parse

--- kernel/system/handover/Multiboot2.cpp
+++ kernel/system/handover/Multiboot2.cpp
@@ -40,3 +40,59 @@ const char *_multiboot2_tag_name[] = {
     "EFI64_IH",
     "LOAD_BASE_ADDR",
 };
+
+void multiboot2_parse_memory_map(Handover *handover, struct multiboot_tag_mmap *tag)
+{
+    for (multiboot_memory_map_t *mmap = tag->entries;
+         (uint8_t *)mmap < (uint8_t *)tag + tag->size;
+         mmap = (multiboot_memory_map_t *)((uintptr_t)mmap + tag->entry_size))
+    {
+        assert(handover->memory_map_size < HANDOVER_MEMORY_MAP_SIZE);
+
+        if ((mmap->addr > UINT32_MAX) ||
+            (mmap->addr + mmap->len > UINT32_MAX))
+        {
+            continue;
+        }
+
+        if (mmap->type == MULTIBOOT_MEMORY_AVAILABLE)
+        {
+            handover->memory_usable += mmap->len;
+        }
+
+        MemoryMapEntry *entry = &handover->memory_map[handover->memory_map_size];
+
+        entry->range = MemoryRange::from_non_aligned_address(mmap->addr, mmap->len);
+
+        switch (mmap->type)
+        {
+        case MULTIBOOT_MEMORY_AVAILABLE:
+            entry->type = MEMORY_MAP_ENTRY_AVAILABLE;
+            break;
+        case MULTIBOOT_MEMORY_ACPI_RECLAIMABLE:
+            entry->type = MEMORY_MAP_ENTRY_ACPI_RECLAIMABLE;
+            break;
+        case MULTIBOOT_MEMORY_NVS:
+            entry->type = MEMORY_MAP_ENTRY_NVS;
+            break;
+        case MULTIBOOT_MEMORY_BADRAM:
+            entry->type = MEMORY_MAP_ENTRY_BADRAM;
+            break;
+        default:
+            entry->type = MEMORY_MAP_ENTRY_RESERVED;
+            break;
+        }
+
+        handover->memory_map_size++;
+    }
+}
+
+void multiboot2_parse_module(Handover *handover, struct multiboot_tag_module *m)
+{
+    assert(handover->modules_size < HANDOVER_MODULES_SIZE);
+
+    Module *module = &handover->modules[handover->modules_size];
+    module->range = MemoryRange::around_non_aligned_address(m->mod_start, m->mod_end - m->mod_start);
+    strncpy(module->command_line, (const char *)m->cmdline, HANDOVER_COMMAND_LINE_SIZE);
+    handover->modules_size++;
+}
