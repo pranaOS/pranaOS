eb03bfa22ac9ab3b836e185248ea985c60198933
krishpranav <krisna.pranav@gmail.com>
2021-07-12 09:06:52 +0530

    kernel: archs x86 reboot triple fault

--- kernel/archs/x86/Power.cpp
+++ kernel/archs/x86/Power.cpp
@@ -16,7 +16,7 @@ namespace Arch::x86
 
 void reboot_8042()
 {
-    Kernel::logln("Trying to reboot using the 8042...");
+    Kernel::logln("Trying to reboot using the 8042.....");
 
     uint8_t good = 0x02;
 
@@ -28,5 +28,14 @@ void reboot_8042()
     out8(0x64, 0xFE);
     Arch::halt();
 }
+
+void reboot_triple_fault()
+{
+    Kernel::logln("Trying to reboot by doing a triple fault.....");
+    cli();
+    Arch::address_space_switch(0x0);
+    *(uint32_t *)0x0 = 0xDEADDEAD;
+}
+
 
 }
