16386e98320f6d76378e916c610aa45cea8cf270
krishpranav <krisna.pranav@gmail.com>
2021-07-12 09:02:02 +0530

    kernel: archs x86 Power headers

---
+++ kernel/archs/x86/Power.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+namespace Arch::x86
+{
+
+void reboot_8042();
+void reboot_triple_fault();
+void shutdown_virtual_machines();
+void shutdown_acpi();
+
+}
