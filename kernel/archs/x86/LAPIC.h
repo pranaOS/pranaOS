e66094b0e8fd2ccf38e7825e649cbc219002bab0
krishpranav <krisna.pranav@gmail.com>
2021-07-12 08:48:43 +0530

    kernel: archs x86 LAPIC headers

---
+++ kernel/archs/x86/LAPIC.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <libutils/Prelude.h>
+
+namespace Arch::x86
+{
+
+void lapic_found(uintptr_t address);
+
+void lapic_initialize();
+
+void lapic_ack();
+
+}
