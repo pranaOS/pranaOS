9322ca48af0c60670987cbae2efdbec849196c64
krishpranav <krisna.pranav@gmail.com>
2021-07-13 10:45:18 +0530

    kernel: system tasking domain funcs

---
+++ kernel/system/tasking/Domain.cpp
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include "system/Streams.h"
+#include "system/node/Directory.h"
+#include "system/node/File.h"
+#include "system/node/Pipe.h"
+#include "system/node/Socket.h"
+#include "system/scheduling/Scheduler.h"
+#include "system/tasking/Domain.h"
+
+Domain::Domain()
+{
+    _root = make<FsDirectory>();
+}
+
+Domain::Domain(const Domain &parent)
+{
+    _root = parent._root;
+}
+
+Domain::~Domain()
+{
+}
+
+Domain &Domain::operator=(const Domain &other)
+{
+    if (this != &other)
+    {
+        _root = other._root;
+    }
+
+    return *this;
+}
