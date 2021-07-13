150eeca1bd4e220af7d9e3b9587ae2660ea0c246
krishpranav <krisna.pranav@gmail.com>
2021-07-13 11:24:34 +0530

    kernel: system tasking handles release, open

--- kernel/system/tasking/Handles.cpp
+++ kernel/system/tasking/Handles.cpp
@@ -74,5 +74,24 @@ RefPtr<FsHandle> Handles::acquire(int handle_index)
 
     _handles[handle_index]->acquire(scheduler_running_id());
     return _handles[handle_index];
+}
 
+JResult Handles::release(int handle_index)
+{
+    LockHolder holder(_lock);
+
+    if (!is_valid_handle(handle_index))
+    {
+        Kernel::logln("Got a bad handle {} from task {}", handle_index, scheduler_running_id());
+        return ERR_BAD_HANDLE;
+    }
+
+    _handles[handle_index]->release(scheduler_running_id());
+    return SUCCESS;
+}
+
+ResultOr<int> Handles::open(Domain &domain, IO::Path &path, HjOpenFlag flags)
+{
+    auto handle = TRY(domain.open(path, flags));
+    return add(handle);
 }
