9b54a6ec60b80e29b2240848dfc7c1494dd88abc
krishpranav <krisna.pranav@gmail.com>
2021-07-13 08:40:32 +0530

    kernel: system scheduling Scheduler void values

--- kernel/system/scheduling/Scheduler.cpp
+++ kernel/system/scheduling/Scheduler.cpp
@@ -19,3 +19,20 @@ static Task *idle = nullptr;
 
 static Utils::List<Task *> *blocked_tasks;
 static Utils::List<Task *> *running_tasks;
+
+void scheduler_initialize()
+{
+    blocked_tasks = new Utils::List<Task *>();
+    running_tasks = new Utils::List<Task *>();
+}
+
+void scheduler_did_create_idle_task(Task *task)
+{
+    idle = task;
+}
+
+void scheduler_did_create_running_task(Task *task)
+{
+    running = task;
+}
+
