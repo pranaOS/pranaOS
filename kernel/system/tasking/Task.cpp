ed134e70865d8f170cfbb9e4f1787421a3f20c8d
krishpranav <krisna.pranav@gmail.com>
2021-07-13 19:19:05 +0530

    kernel: tasking task values

--- kernel/system/tasking/Task.cpp
+++ kernel/system/tasking/Task.cpp
@@ -106,3 +106,119 @@ void Task::kill_me_if_you_dare()
         interrupts_release();
     }
 }
+
+Task *task_create(Task *parent, const char *name, TaskFlags flags)
+{
+    UNUSED(parent);
+
+    ASSERT_INTERRUPTS_RETAINED();
+
+    if (_tasks == nullptr)
+    {
+        _tasks = new List<Task *>();
+    }
+
+    Task *task = new Task{};
+
+    task->id = _task_ids++;
+    strlcpy(task->name, name, PROCESS_NAME_SIZE);
+    task->_state = TASK_STATE_NONE;
+    task->_flags = flags;
+
+    if (task->_flags & TASK_USER)
+    {
+        task->address_space = Arch::address_space_create();
+    }
+    else
+    {
+        task->address_space = Arch::kernel_address_space();
+    }
+
+    if (parent)
+        task->_domain = parent->_domain;
+
+    task->memory_mapping = new List<MemoryMapping *>();
+
+    memory_alloc(task->address_space, PROCESS_STACK_SIZE, MEMORY_CLEAR, (uintptr_t *)&task->kernel_stack);
+    task->kernel_stack_pointer = ((uintptr_t)task->kernel_stack + PROCESS_STACK_SIZE);
+
+    if (task->_flags & TASK_USER)
+    {
+        auto *parent_address_space = task_switch_address_space(scheduler_running(), task->address_space);
+        task_memory_map(task, 0xff000000, PROCESS_STACK_SIZE, MEMORY_CLEAR | MEMORY_USER);
+        task->user_stack_pointer = 0xff000000 + PROCESS_STACK_SIZE;
+        task->user_stack = (void *)0xff000000;
+        task_switch_address_space(scheduler_running(), parent_address_space);
+    }
+
+    Arch::save_context(task);
+
+    _tasks->push_back(task);
+
+    return task;
+}
+
+
+Task *task_clone(Task *parent, uintptr_t sp, uintptr_t ip, TaskFlags flags)
+{
+    ASSERT_INTERRUPTS_RETAINED();
+
+    if (_tasks == nullptr)
+    {
+        _tasks = new List<Task *>();
+    }
+
+    Task *task = new Task{};
+
+    task->id = _task_ids++;
+    task->_flags = flags;
+    strlcpy(task->name, parent->name, PROCESS_NAME_SIZE);
+    task->_state = TASK_STATE_NONE;
+
+    task->address_space = Arch::address_space_create();
+
+    task->memory_mapping = new List<MemoryMapping *>();
+
+    if (parent)
+    {
+        task->_domain = parent->_domain;
+    }
+    
+    for (int i = 0; i < PROCESS_HANDLE_COUNT; i++)
+    {
+        parent->handles().pass(task->handles(), i, i);
+    }
+
+    memory_alloc(task->address_space, PROCESS_STACK_SIZE, MEMORY_CLEAR, (uintptr_t *)&task->kernel_stack);
+    task->kernel_stack_pointer = ((uintptr_t)task->kernel_stack + PROCESS_STACK_SIZE);
+
+    for (auto *mapping : *parent->memory_mapping)
+    {
+        auto virtual_range = mapping->range();
+
+        auto physical_range = mapping->object->range();
+
+        void *buffer = malloc(physical_range.size());
+        assert(buffer);
+        assert(virtual_range.base());
+        memcpy(buffer, (void *)virtual_range.base(), virtual_range.size());
+
+        auto *parent_address_space = task_switch_address_space(scheduler_running(), task->address_space);
+
+        task_memory_map(task, virtual_range.base(), virtual_range.size(), MEMORY_USER);
+        memcpy((void *)virtual_range.base(), buffer, virtual_range.size());
+
+        task_switch_address_space(scheduler_running(), parent_address_space);
+
+        free(buffer);
+    }
+
+    task->user_stack_pointer = sp;
+    task->entry_point = (TaskEntryPoint)ip;
+
+    _tasks->push_back(task);
+
+    task_go(task);
+
+    return task;
+}
