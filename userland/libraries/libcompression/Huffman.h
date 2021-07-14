f3b4081d657c35f8e2f1c266fc28305a7a7909a5
krishpranav <krisna.pranav@gmail.com>
2021-07-14 19:32:01 +0530

    userland: libraries libcompression public

--- userland/libraries/libcompression/Huffman.h
+++ userland/libraries/libcompression/Huffman.h
@@ -17,6 +17,27 @@ struct HuffmanDecoder
 private:
     const Vector<unsigned int> &_alphabet;
     const Vector<unsigned int> &_code_bit_lengths;
+
+public:
+    inline HuffmanDecoder(Vector<unsigned int> &alphabet, Vector<unsigned int> &code_bit_lengths) : _alphabet(alphabet), _code_bit_lengths(code_bit_lengths)
+    {
+    }
+
+    unsigned int decode(IO::BitReader &input)
+    {
+        for (unsigned int i = 0; i < (unsigned int)_code_bit_lengths.count(); i++)
+        {
+            const auto cbl = _code_bit_lengths[i];
+            if (cbl == 0)
+            {
+                continue;
+            }
+
+            unsigned int code = input.peek_bits_reverse(cbl);
+        }
+
+    }
+
 };
 
 }
