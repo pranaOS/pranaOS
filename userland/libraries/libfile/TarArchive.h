60496a4b56f8b31fc736ef8737ed46946cb8c0b5
krishpranav <krisna.pranav@gmail.com>
2021-07-14 16:33:21 +0530

    userland: libraries libfile TARBlock

---
+++ userland/libraries/libfile/TarArchive.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <libfile/Archive.h>
+
+struct TARBlock
+{
+    char name[100];
+    char typeflag;
+    char linkname[100];
+    size_t size;
+    char *data;
+};
+
+bool tar_read(void *tarfile, TARBlock *block, size_t index);
