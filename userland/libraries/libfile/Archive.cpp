5e333b6f55e671a700a1048a6895664ed29efbd4
krishpranav <krisna.pranav@gmail.com>
2021-07-14 16:13:38 +0530

    userland: libraries libfile includes

---
+++ userland/libraries/libfile/Archive.cpp
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <libfile/Archive.h>
+#include <libfile/TARArchive.h>
+#include <libfile/ZipArchive.h>
