c2d82eb9e1611d117ead63d9dc136c2206f873fc
krishpranav <krisna.pranav@gmail.com>
2021-07-14 16:20:05 +0530

    userland: libraries libfile uintptr

--- userland/libraries/libfile/ELF.h
+++ userland/libraries/libfile/ELF.h
@@ -76,3 +76,25 @@
 #define ELF_SECTION_TYPE_SHLIB 10
 #define ELF_SECTION_TYPE_DYNSYM 11
 #define ELF_SECTION_TYPE_COUNT 12
+
+struct PACKED ELF32Header
+{
+
+
+    uint8_t ident[ELF_IDENT_COUNT];
+    uint16_t type;
+    uint16_t machine;
+    uint32_t version;
+    uint32_t entry;
+    uint32_t phoff;
+    uint32_t shoff;
+    uint32_t flags;
+    uint16_t ehsize;
+    uint16_t phentsize;
+    uint16_t phnum;
+    uint16_t shentsize;
+    uint16_t shnum;
+    uint16_t shstrndx;
+
+
+};
