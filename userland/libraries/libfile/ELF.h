42c0c27b5a34ea5c36689608a7663263d1d44bea
krishpranav <krisna.pranav@gmail.com>
2021-07-14 16:16:41 +0530

    userland: libraries libfile define values

---
+++ userland/libraries/libfile/ELF.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+
+#pragma once
+
+// includes
+#include <libutils/Prelude.h>
+
+#define ELF_MAG0 0x7F
+#define ELF_MAG1 'E'
+#define ELF_MAG2 'L'
+#define ELF_MAG3 'F'
+
+#define ELF_IDENT_COUNT 16
+#define ELF_IDENT_MAG0 0
+#define ELF_IDENT_MAG1 1
+#define ELF_IDENT_MAG2 2
+#define ELF_IDENT_MAG3 3
