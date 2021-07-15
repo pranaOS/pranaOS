f6c45ba83728f255ebca2855e5f5a9c9aea405cc
krishpranav <krisna.pranav@gmail.com>
2021-07-15 16:10:32 +0530

    userland: libraries libfilepicker dialog render

--- userland/libraries/libfilepicker/dialogs/dialogs/FilePicker.cpp
+++ userland/libraries/libfilepicker/dialogs/dialogs/FilePicker.cpp
@@ -30,4 +30,39 @@ String Dialog::get_title()
         _flags & DialogFlags::DIALOG_FLAGS_FOLDER ? "folder" : "document");
 }
 
+void Dialog::render(Widget::Window *window)
+{
+    window->size(Math::Vec2i(600, 400));
+
+    window->root()->add(Widget::titlebar(Graphic::Icon::get("widgets"), get_title()));
+
+    window->root()->add<ToolBar>(_navigation, nullptr, ToolBar::NO_HJ_OPEN_TERMINAL);
+
+    if (_flags & DialogFlags::DIALOG_FLAGS_FOLDER)
+    {
+        _browser = window->root()->add<DirectoryBrowser>(_navigation);
+    }
+    else
+    {
+        auto file_browser = window->root()->add<FileBrowser>(_navigation);
+        file_browser->on_element_selected = [this](String &path) {
+            _selected_file = path;
+            close(Widget::DialogResult::OK);
+        };
+        _browser = file_browser;
+    }
+
+    auto action_container = window->root()->add(Widget::panel());
+
+    if (_flags & DialogFlags::DIALOG_FLAGS_SAVE)
+    {
+        _text_field = action_container->add<Widget::TextField>(Widget::TextModel::empty());
+        _text_field->flags(Widget::Element::FILL);
+        _text_field->focus();
+    }
+
+    action_container->add(Widget::spacer());
+
+    create_buttons(action_container);
+}
 }
