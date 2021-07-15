bb82f36e83fce5dbcdb5e9cb15767f466d8043e0
krishpranav <krisna.pranav@gmail.com>
2021-07-15 14:11:40 +0530

    ports: ffmpeg build it

---
+++ ports/ffmpeg/build-it.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cd sources
+sysroot="$PWD/../../build/sysroot"
+export CPPFLAGS="-I$sysroot/System/Includes"
+export LDFLAGS="-L$sysroot/System/Libraries -static -lc"
+./configure \
+ --enable-cross-compile --target-os=minix \
+ --cross_prefix=i686-pc-pranaos- --arch=i686 --prefix=./install && make -j $(nproc)
+cd ..
