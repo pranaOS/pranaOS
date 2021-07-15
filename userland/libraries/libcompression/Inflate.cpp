8be4c8918468528451887e523f0fbc8f44a793d5
krishpranav <krisna.pranav@gmail.com>
2021-07-15 11:41:11 +0530

    userland: libarries libcompression static BASE values

---
+++ userland/libraries/libcompression/Inflate.cpp
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <libcompression/Common.h>
+#include <libcompression/Huffman.h>
+#include <libcompression/Inflate.h>
+#include <libio/BitReader.h>
+#include <libio/BufReader.h>
+#include <libio/Copy.h>
+#include <libio/MemoryWriter.h>
+#include <libio/Skip.h>
+#include <libio/Streams.h>
+#include <libutils/InlineRingBuffer.h>
+
+namespace Compression
+{
+
+
+static constexpr uint8_t BASE_LENGTH_EXTRA_BITS[] = {
+    0, 0, 0, 0, 0, 0, 0, 0,
+    1, 1, 1, 1,            
+    2, 2, 2, 2,            
+    3, 3, 3, 3,             
+    4, 4, 4, 4,             
+    5, 5, 5, 5,             
+    0                       
+};
+
+static constexpr uint16_t BASE_LENGTHS[] = {
+    3, 4, 5, 6, 7, 8, 9, 10,
+    11, 13, 15, 17,         
+    19, 23, 27, 31,         
+    35, 43, 51, 59,         
+    67, 83, 99, 115,        
+    131, 163, 195, 227,     
+    258                     
+};
+
+}
