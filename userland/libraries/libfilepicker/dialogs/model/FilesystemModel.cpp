b6842aaf69627fd46f879c16c433bfe99a3cdd92
krishpranav <krisna.pranav@gmail.com>
2021-07-16 11:33:17 +0530

    userland: libraries libfilepicker dialogs model geticonfornode

--- userland/libraries/libfilepicker/dialogs/model/FilesystemModel.cpp
+++ userland/libraries/libfilepicker/dialogs/model/FilesystemModel.cpp
@@ -16,11 +16,42 @@ namespace FilePicker
 
 static auto get_icon_for_node(String current_directory, IO::Directory::Entry &entry)
 {
-    if (entry.stat.type == J_FILE_TYPE_DIRECTORY)
+    if (entry.stat.type == HJ_FILE_TYPE_DIRECTORY)
     {
         auto manifest_path = IO::format("{}/{}/manifest.json", current_directory, entry.name);
 
-        IO::File manifest_file{manifest_file, J_OPEN_READ};
+        IO::File manifest_file{manifest_path, HJ_OPEN_READ};
+
+        if (manifest_file.exist())
+        {
+            auto root = Json::parse(manifest_file);
+
+            if (root.is(Json::OBJECT))
+            {
+                auto icon_name = root.get("icon");
+
+                if (icon_name.is(Json::STRING))
+                {
+                    return Graphic::Icon::get(icon_name.as_string());
+                }
+            }
+        }
+
+        return Graphic::Icon::get("folder");
+    }
+    else if (entry.stat.type == HJ_FILE_TYPE_PIPE ||
+             entry.stat.type == HJ_FILE_TYPE_DEVICE ||
+             entry.stat.type == HJ_FILE_TYPE_SOCKET)
+    {
+        return Graphic::Icon::get("pipe");
+    }
+    else if (entry.stat.type == HJ_FILE_TYPE_TERMINAL)
+    {
+        return Graphic::Icon::get("console-network");
+    }
+    else
+    {
+        return Graphic::Icon::get("file");
     }
 }
 
