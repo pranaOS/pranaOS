e0d222b8e6ebdad4679e071b0a58b7b0d2eada4d
krishpranav <krisna.pranav@gmail.com>
2021-07-16 11:24:26 +0530

    userland: libraries libfilepicker dialogs bookmarks

--- userland/libraries/libfilepicker/dialogs/model/Bookmarks.cpp
+++ userland/libraries/libfilepicker/dialogs/model/Bookmarks.cpp
@@ -23,4 +23,31 @@ void Bookmarks::add(Bookmarks &&bookmark)
     did_update();
 }
 
+void Bookmarks::remove(const IO::Path &path)
+{
+    for (size_t i = 0; i < _bookmarks.count(); i++)
+    {
+        if (_bookmarks[i].path() == path)
+        {
+            _bookmarks.remove_index(i);
+            did_update();
+            return;
+        }
+    }
+}
+
+bool Bookmarks::has(const IO::Path &path) const
+{
+    for (size_t i = 0; i < _bookmarks.count(); i++)
+    {
+        if (_bookmarks[i].path() == path)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
 }
