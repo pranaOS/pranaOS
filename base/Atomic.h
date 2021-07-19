338d2f20aa4193c0d9818952d4e96f61fc46d3c5
krishpranav <krisna.pranav@gmail.com>
2021-07-19 16:29:09 +0530

    base: Atomic static, template

--- base/Atomic.h
+++ base/Atomic.h
@@ -3,3 +3,36 @@
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
+
+#pragma once
+
+#include <base/Concepts.h>
+#include <base/Platform.h>
+#include <base/Types.h>
+
+namespace Base
+{
+
+static inline void atomic_signal_fence(MemoryOrder order) noexcept
+{
+    return __atomic_signal_fence(order);
+}
+
+static inline void atomic_thread_fence(MemoryOrder order) noexcept
+{
+    return __atomic_thread_fence(order);
+}
+
+static inline void full_memory_barrier() noexcept
+{
+    atomic_signal_fence(Base::MemoryOrder::memory_order_acq_rel);
+    atomic_thread_fence(Base::MemoryOrder::memory_order_acq_rel);
+}
+
+template<typename T>
+static inline T atomic_exchange(volatile T* var, T desired, MemoryOrder order = memory_order_seq_cst) noexcept
+{
+return __atomic_exchange_n(var, desired, order);
+}
+
+}
