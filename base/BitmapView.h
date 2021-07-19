9fcc11441f879c43a3db0810c8e3827cc0458f0d
krishpranav <krisna.pranav@gmail.com>
2021-07-19 17:56:56 +0530

    base: BitmapView headers

--- base/BitmapView.h
+++ base/BitmapView.h
@@ -48,6 +48,57 @@ public:
     {
         return count_in_range(0, m_size, value);
     }
+
+        size_t count_in_range(size_t start, size_t len, bool value) const
+    {
+        VERIFY(start < m_size);
+        VERIFY(start + len <= m_size);
+        if (len == 0)
+            return 0;
+
+        size_t count;
+        const u8* first = &m_data[start / 8];
+        const u8* last = &m_data[(start + len) / 8];
+        u8 byte = *first;
+        byte &= bitmask_first_byte[start % 8];
+        if (first == last) {
+            byte &= bitmask_last_byte[(start + len) % 8];
+            count = __builtin_popcount(byte);
+        } else {
+            count = __builtin_popcount(byte);
+            if (last < &m_data[size_in_bytes()]) {
+                byte = *last;
+                byte &= bitmask_last_byte[(start + len) % 8];
+                count += __builtin_popcount(byte);
+            }
+            if (++first < last) {
+                const u32* ptr32 = (const u32*)(((FlatPtr)first + sizeof(u32) - 1) & ~(sizeof(u32) - 1));
+                if ((const u8*)ptr32 > last)
+                    ptr32 = (const u32*)last;
+                while (first < (const u8*)ptr32) {
+                    count += __builtin_popcount(*first);
+                    first++;
+                }
+                const u32* last32 = (const u32*)((FlatPtr)last & ~(sizeof(u32) - 1));
+                while (ptr32 < last32) {
+                    count += __builtin_popcountl(*ptr32);
+                    ptr32++;
+                }
+                for (first = (const u8*)ptr32; first < last; first++)
+                    count += __builtin_popcount(*first);
+            }
+        }
+
+        if (!value)
+            count = len - count;
+        return count;
+    }
+
+    bool is_null() const { return !m_data; }
+
+    const u8* data() const { return m_data; }
+
+    
     
 };
     
