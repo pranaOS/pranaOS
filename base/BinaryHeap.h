e51a83ba1e9f2028a678d17a28936262e3a946d8
krishpranav <krisna.pranav@gmail.com>
2021-07-19 17:27:56 +0530

    base: BinaryHeap

--- base/BinaryHeap.h
+++ base/BinaryHeap.h
@@ -3,3 +3,33 @@
  *
  * SPDX-License-Identifier: BSD-2-Clause
 */
+
+#pragma once
+
+namespace Base 
+{
+
+template <typename K, typename V, size_t Capacity> 
+class BinaryHeap
+{
+public:
+    BinaryHeap() = default;
+    ~BinaryHeap() = default;
+
+    BinaryHeap(K keys[], V values[], size_t size)
+    {
+        VERIFY(size <= Capacity);
+        m_size = size;
+        for (size_t i = 0; i < size; i++) {
+            m_elements[i].key = keys[i];
+            m_elements[i].value = values[i];
+        }
+
+        for (ssize_t i = size / 2; i >= 0; i--) {
+            heapify_down(i);
+        }
+    }
+    
+};
+
+}
