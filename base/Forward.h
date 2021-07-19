039711954f992bc860b4d3af11e6fc2318207313
krishpranav <krisna.pranav@gmail.com>
2021-07-19 15:38:05 +0530

    base: Forward headers

--- base/Forward.h
+++ base/Forward.h
@@ -8,7 +8,7 @@
 
 #include <base/Types.h>
 
-namespace AK {
+namespace Base {
 
 namespace Detail {
 template<size_t inline_capacity>
@@ -16,7 +16,7 @@ class ByteBuffer;
 }
 
 class Bitmap;
-using ByteBuffer = AK::Detail::ByteBuffer<32>;
+using ByteBuffer = Base::Detail::ByteBuffer<32>;
 class IPv4Address;
 class JsonArray;
 class JsonObject;
@@ -38,5 +38,89 @@ class OutputStream;
 class InputBitStream;
 class OutputBitStream;
 class OutputMemoryStream;
+
+template<size_t Capacity>
+class CircularDuplexStream;
+
+template<typename T>
+class Span;
+
+template<typename T, size_t Size>
+struct Array;
+
+template<typename Container, typename ValueType>
+class SimpleIterator;
+
+using ReadonlyBytes = Span<const u8>;
+using Bytes = Span<u8>;
+
+template<typename T, Base::MemoryOrder DefaultMemoryOrder>
+class Atomic;
+
+template<typename T>
+class SinglyLinkedList;
+
+template<typename T>
+class DoublyLinkedList;
+
+template<typename T, size_t capacity>
+class CircularQueue;
+
+template<typename T>
+struct Traits;
+
+template<typename T, typename TraitsForT = Traits<T>, bool IsOrdered = false>
+class HashTable;
+
+template<typename T, typename TraitsForT = Traits<T>>
+using OrderedHashTable = HashTable<T, TraitsForT, true>;
+
+template<typename K, typename V, typename KeyTraits = Traits<K>, bool IsOrdered = false>
+class HashMap;
+
+template<typename K, typename V, typename KeyTraits = Traits<K>>
+using OrderedHashMap = HashMap<K, V, KeyTraits, true>;
+
+template<typename T>
+class Badge;
+
+template<typename T>
+class FixedArray;
+
+template<typename>
+class Function;
+
+template<typename Out, typename... In>
+class Function<Out(In...)>;
+
+template<typename T>
+class NonnullRefPtr;
+
+template<typename T>
+class NonnullOwnPtr;
+
+template<typename T, size_t inline_capacity = 0>
+class NonnullRefPtrVector;
+
+template<typename T, size_t inline_capacity = 0>
+class NonnullOwnPtrVector;
+
+template<typename T>
+class Optional;
+
+template<typename T>
+struct RefPtrTraits;
+
+template<typename T, typename PtrTraits = RefPtrTraits<T>>
+class RefPtr;
+
+template<typename T>
+class OwnPtr;
+
+template<typename T>
+class WeakPtr;
+
+template<typename T, size_t inline_capacity = 0>
+requires(!IsRvalueReference<T>) class Vector;
 
 }
