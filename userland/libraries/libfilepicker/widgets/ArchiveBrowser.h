7fe506418a0a464ee39215cf0da8f0e6928b2f8b
krishpranav <krisna.pranav@gmail.com>
2021-07-19 11:01:22 +0530

    libraries: libfilepicker widgets ArchiveBrowser headers

--- userland/libraries/libfilepicker/widgets/ArchiveBrowser.h
+++ userland/libraries/libfilepicker/widgets/ArchiveBrowser.h
@@ -21,8 +21,37 @@ private:
     RefPtr<Navigation> _navigation;
     RefPtr<Archive> _archive;
     RefPtr<ArchiveListing> _listing;
-    OwnPtr<Async::Observer<Navigation>> _navigation;
+    OwnPtr<Async::Observer<Navigation>> _navigation_observer;
 
-}
+public:
+    ArchiveBrowser(RefPtr<Navigation> navigation, RefPtr<Archive> archive)
+        : Table(), _navigation(navigation), _archive(archive)
+    {
+        _listing = make<ArchiveListing>(navigation, archive);
+        model(_listing);
 
-}
+        flags(Element::FILL);
+
+        empty_message("This archive is empty.");
+
+        _navigation_observer = navigation->observe([this](auto &) {
+            select(-1);
+            scroll_to_top();
+        });
+
+        on(Widget::Event::ACTION, [this](auto) {
+            if (selected() >= 0)
+            {
+                if (_listing->info(selected()).type == HJ_FILE_TYPE_DIRECTORY)
+                {
+                    _navigation->navigate(_listing->info(selected()).name);
+                }
+                else
+                {
+                }
+            }
+        });
+    }
+};
+
+}  
