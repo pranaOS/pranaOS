ea1b105650185df9f32ca4ed34af1422841fbe94
krishpranav <krisna.pranav@gmail.com>
2021-07-19 16:35:31 +0530

    base: Atomic typename headers

--- base/Atomic.h
+++ base/Atomic.h
@@ -10,23 +10,22 @@
 #include <base/Platform.h>
 #include <base/Types.h>
 
-namespace Base
-{
+namespace Base {
 
-static inline void atomic_signal_fence(MemoryOrder order) noexcept
+    static inline void atomic_signal_fence(MemoryOrder order) noexcept
 {
     return __atomic_signal_fence(order);
 }
 
 static inline void atomic_thread_fence(MemoryOrder order) noexcept
 {
-    return __atomic_thread_fence(order);
+return __atomic_thread_fence(order);
 }
 
 static inline void full_memory_barrier() noexcept
 {
-    atomic_signal_fence(Base::MemoryOrder::memory_order_acq_rel);
-    atomic_thread_fence(Base::MemoryOrder::memory_order_acq_rel);
+atomic_signal_fence(Base::MemoryOrder::memory_order_acq_rel);
+atomic_thread_fence(Base::MemoryOrder::memory_order_acq_rel);
 }
 
 template<typename T>
@@ -34,5 +33,18 @@ static inline T atomic_exchange(volatile T* var, T desired, MemoryOrder order = 
 {
 return __atomic_exchange_n(var, desired, order);
 }
+
+template<typename T, typename V = RemoveVolatile<T>>
+static inline V* atomic_exchange(volatile T** var, V* desired, MemoryOrder order = memory_order_seq_cst) noexcept
+{
+return __atomic_exchange_n(var, desired, order);
+}
+
+template<typename T, typename V = RemoveVolatile<T>>
+static inline V* atomic_exchange(volatile T** var, std::nullptr_t, MemoryOrder order = memory_order_seq_cst) noexcept
+{
+return __atomic_exchange_n(const_cast<V**>(var), nullptr, order);
+}
+
 
 }
