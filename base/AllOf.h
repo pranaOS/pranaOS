9cdf4c29e4b48ac23f6238184ba9c495fb783c3a
krishpranav <krisna.pranav@gmail.com>
2021-07-19 14:52:52 +0530

    base files

---
+++ base/AllOf.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <Base/Iterator.h>
+
+namespace Base {
+
+template<typename Container, typename ValueType>
+constexpr bool all_of(
+    const SimpleIterator<Container, ValueType>& begin,
+    const SimpleIterator<Container, ValueType>& end,
+    const auto& predicate)
+{
+    for (auto iter = begin; iter != end; ++iter) {
+        if (!predicate(*iter)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+}
+
+using Base::all_of;
