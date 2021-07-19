570cd99de2cd9a3126b569d81d6da2645d0fac98
krishpranav <krisna.pranav@gmail.com>
2021-07-19 17:00:34 +0530

    base: Base64 heades

---
+++ base/Base64.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <base/ByteBuffer.h>
+#include <base/String.h>
+#include <base/StringView.h>
+
+namespace Base {
+
+size_t calculate_base64_decoded_length(const StringView&);
+
+size_t calculate_base64_encoded_length(ReadonlyBytes);
+
+ByteBuffer decode_base64(const StringView&);
+
+String encode_base64(ReadonlyBytes);
+
+}
+
+using Base::decode_base64;
+using Base::encode_base64;
