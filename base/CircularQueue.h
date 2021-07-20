91b873f9c156ab40e0c8a20b806a7a05bc5ac8ef
krishpranav <krisna.pranav@gmail.com>
2021-07-20 14:15:21 +0530

    base: CircularQueue[ConstIterator]

--- base/CircularQueue.h
+++ base/CircularQueue.h
@@ -55,6 +55,36 @@ public:
             ++m_size;
     }
 
+    T dequeue()
+    {
+        VERIFY(!is_empty());
+        auto& slot = elements()[m_head];
+        T value = move(slot);
+        slot.~T();
+        m_head = (m_head + 1) % Capacity;
+        --m_size;
+        return value;
+    }
+
+    const T& at(size_t index) const { return elements()[(m_head + index) % Capacity]; }
+
+    const T& first() const { return at(0); }
+    const T& last() const { return at(size() - 1); }
+
+    class ConstIterator {
+    public:
+        bool operator!=(const ConstIterator& other) { return m_index != other.m_index; }
+        ConstIterator& operator++()
+        {
+            m_index = (m_index + 1) % Capacity;
+            if (m_index == m_queue.m_head)
+                m_index = m_queue.m_size;
+            return *this;
+        }
+
+        const T& operator*() const { return m_queue.elements()[m_index]; }
+
+    };
 };
 
 }
