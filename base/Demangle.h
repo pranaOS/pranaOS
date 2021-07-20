994606126298bd65e102d8619641e3a187af51b5
krishpranav <krisna.pranav@gmail.com>
2021-07-20 18:43:05 +0530

    base: Demangle headers

--- base/Demangle.h
+++ base/Demangle.h
@@ -6,7 +6,6 @@
 
 #pragma once
 
-// includes
 #include <base/String.h>
 #include <base/StringView.h>
 
@@ -14,13 +13,22 @@
 #    include <cxxabi.h>
 #endif
 
-
 namespace Base {
 
-inline String demange(const StringView& name)
+inline String demangle(const StringView& name)
 {
 #ifdef BUILDING_PRANAOS_TOOLCHAIN
     return name;
+#else
+    int status = 0;
+    auto* demangled_name = abi::__cxa_demangle(name.to_string().characters(), nullptr, nullptr, &status);
+    auto string = String(status == 0 ? demangled_name : name);
+    if (status == 0)
+        kfree(demangled_name);
+    return string;
+#endif
 }
 
 }
+
+using Base::demangle;
