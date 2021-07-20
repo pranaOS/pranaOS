185e65c9aec3c464ead80cc6090652d098715a05
krishpranav <krisna.pranav@gmail.com>
2021-07-20 14:13:30 +0530

    base: CircularQueue[void, bool, size_t]

--- base/CircularQueue.h
+++ base/CircularQueue.h
@@ -3,3 +3,58 @@
  *
  * SPDX-License-Identifier: BSD-2-Clause
 */
+
+#pragma once
+
+// includes
+#include <base/Assertions.h>
+#include <base/Forward.h>
+#include <base/StdLibExtras.h>
+
+namespace Base {
+
+template<typename T, size_t Capacity>
+class CircularQueue {
+    friend CircularDuplexStream<Capacity>;
+
+public:
+    CircularQueue()
+    {
+    }
+
+    ~CircularQueue()
+    {
+        clear();
+    }
+
+    void clear()
+    {
+        for (size_t i = 0; i < m_size; ++i)
+            elements()[(m_head + i) % Capacity].~T();
+
+        m_head = 0;
+        m_size = 0;
+    }
+
+    bool is_empty() const { return !m_size; }
+    size_t size() const { return m_size; }
+
+    size_t capacity() const { return Capacity; }
+
+    template<typename U = T>
+    void enqueue(U&& value)
+    {
+        auto& slot = elements()[(m_head + m_size) % Capacity];
+        if (m_size == Capacity)
+            slot.~T();
+
+        new (&slot) T(forward<U>(value));
+        if (m_size == Capacity)
+            m_head = (m_head + 1) % Capacity;
+        else
+            ++m_size;
+    }
+
+};
+
+}
