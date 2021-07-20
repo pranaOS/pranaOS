e185127662ab0ba934ac2ec5d79e73d540d4662f
krishpranav <krisna.pranav@gmail.com>
2021-07-20 10:48:21 +0530

    base: Checked[template]

---
+++ base/Checked.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2021, krishpranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+// includes
+#include <base/Assertions.h>
+#include <base/Concepts.h>
+#include <base/NumericLimits.h>
+#include <base/StdLibExtras.h>
+
+namespace Base {
+
+template<typename Destination, typename Source, bool destination_is_wider = (sizeof(Destination) >= sizeof(Source)), bool destination_is_signed = NumericLimits<Destination>::is_signed(), bool source_is_signed = NumericLimits<Source>::is_signed()>
+struct TypeBoundsChecker;
+
+template<typename Destination, typename Source>
+struct TypeBoundsChecker<Destination, Source, false, false, false> {
+    static constexpr bool is_within_range(Source value)
+    {
+        return value <= NumericLimits<Destination>::max();
+    }
+};
+
+template<typename Destination, typename Source>
+struct TypeBoundsChecker<Destination, Source, false, true, true> {
+    static constexpr bool is_within_range(Source value)
+    {
+        return value <= NumericLimits<Destination>::max()
+               && NumericLimits<Destination>::min() <= value;
+    }
+};
+
+
+}
