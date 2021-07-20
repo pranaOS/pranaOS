d6a8dfab450f69544e57b50db610c5e0cbb10d6b
krishpranav <krisna.pranav@gmail.com>
2021-07-20 08:48:53 +0530

    base: Buffered headers

--- base/Buffered.h
+++ base/Buffered.h
@@ -16,7 +16,6 @@
 
 namespace Base {
 
-
 template<typename StreamType, size_t Size = 4096, typename = void>
 class Buffered;
 
@@ -116,4 +115,81 @@ private:
     mutable size_t m_buffered { 0 };
 };
 
+template<typename StreamType, size_t Size>
+class Buffered<StreamType, Size, typename EnableIf<IsBaseOf<OutputStream, StreamType>>::Type> final : public OutputStream {
+    Base_MAKE_NONCOPYABLE(Buffered);
+
+public:
+    template<typename... Parameters>
+    explicit Buffered(Parameters&&... parameters)
+        : m_stream(forward<Parameters>(parameters)...)
+    {
+    }
+
+    Buffered(Buffered&& other)
+        : m_stream(move(other.m_stream))
+    {
+        other.buffer().copy_to(buffer());
+        m_buffered = exchange(other.m_buffered, 0);
+    }
+
+    ~Buffered()
+    {
+        if (m_buffered > 0)
+            flush();
+    }
+
+    bool has_recoverable_error() const override { return m_stream.has_recoverable_error(); }
+    bool has_fatal_error() const override { return m_stream.has_fatal_error(); }
+    bool has_any_error() const override { return m_stream.has_any_error(); }
+
+    bool handle_recoverable_error() override { return m_stream.handle_recoverable_error(); }
+    bool handle_fatal_error() override { return m_stream.handle_fatal_error(); }
+    bool handle_any_error() override { return m_stream.handle_any_error(); }
+
+    void set_recoverable_error() const override { return m_stream.set_recoverable_error(); }
+    void set_fatal_error() const override { return m_stream.set_fatal_error(); }
+
+    size_t write(ReadonlyBytes bytes) override
+    {
+        if (has_any_error())
+            return 0;
+
+        auto nwritten = bytes.copy_trimmed_to(buffer().slice(m_buffered));
+        m_buffered += nwritten;
+
+        if (m_buffered == Size) {
+            flush();
+
+            if (bytes.size() - nwritten >= Size)
+                nwritten += m_stream.write(bytes.slice(nwritten));
+
+            nwritten += write(bytes.slice(nwritten));
+        }
+
+        return nwritten;
+    }
+
+    bool write_or_error(ReadonlyBytes bytes) override
+    {
+        write(bytes);
+        return true;
+    }
+
+    void flush()
+    {
+        m_stream.write_or_error({ m_buffer, m_buffered });
+        m_buffered = 0;
+    }
+
+private:
+    Bytes buffer() { return { m_buffer, Size }; }
+
+    StreamType m_stream;
+    u8 m_buffer[Size];
+    size_t m_buffered { 0 };
+};
+
 }
+
+using Base::Buffered;
