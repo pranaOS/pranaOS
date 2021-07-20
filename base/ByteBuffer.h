41b9a26cd62cca86ce6f00ae8e836d2d2e3f4ca6
krishpranav <krisna.pranav@gmail.com>
2021-07-20 09:02:11 +0530

    base: ByteBuffer

--- base/ByteBuffer.h
+++ base/ByteBuffer.h
@@ -37,6 +37,41 @@ public:
         move_from(move(other));
     }
 
+    ByteBuffer& operator=(ByteBuffer&& other)
+    {
+        if (this != &other) {
+            if (!m_inline)
+                kfree_sized(m_outline_buffer, m_outline_capacity);
+            move_from(move(other);)
+        }
+        return *this;
+    }
+
+    ByteBuffer& operator=(ByteBuffer const& other)
+    {
+        if (this != &other) {
+            if (m_size > other.size())
+                trim(other.size(), true);
+            else
+                resize(other.size());
+            __builtin_memcpy(data(), other.data(), other.size());
+        }
+        return *this;
+    }
+
+    [[nodiscard]] static ByteBuffer create_uninitialized(size_t size)
+    {
+        return ByteBuffer(size);
+    }
+
+    [[nodiscard]] static ByteBuffer create_zeroed(size_t size)
+    {
+        auto buffer = create_uninitialized(size);
+        buffer.zero_fill();
+        VERIFY(size == 0 || (buffer[0] == 0 && buffer[size - 1] == 0));
+        return buffer;
+    }
+
 };
 
 }
