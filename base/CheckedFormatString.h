453a4e647564954d8fe46b0aa8e18e62b9ed45c4
krishpranav <krisna.pranav@gmail.com>
2021-07-20 11:31:10 +0530

    base: CheckedFormatString[template]

--- base/CheckedFormatString.h
+++ base/CheckedFormatString.h
@@ -39,4 +39,30 @@ struct Array {
     T __data[Size];
 };
 
+template<typename... Args>
+void compiletime_fail(Args...);
+
+template<size_t N>
+consteval auto extract_used_argument_index(const char (&fmt)[N], size_t specifier_start_index, size_t specifier_end_index, size_t& next_implicit_argument_index)
+{
+    struct {
+        size_t index_value { 0 };
+        bool saw_explicit_index { false };
+    } state;
+    for (size_t i = specifier_start_index; i < specifier_end_index; ++i) {
+        auto c = fmt[i];
+        if (c > '9' || c < '0')
+            break;
+
+        state.index_value *= 10;
+        state.index_value += c - '0';
+        state.saw_explicit_index = true;
+    }
+
+    if (!state.saw_explicit_index)
+        return next_implicit_argument_index++;
+
+    return state.index_value;
+}
+
 }
