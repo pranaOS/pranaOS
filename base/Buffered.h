2d7e9df81b197e51f2f98c3c44399d594cf5820f
krishpranav <krisna.pranav@gmail.com>
2021-07-20 08:46:26 +0530

    base: Buffered headers

--- base/Buffered.h
+++ base/Buffered.h
@@ -49,6 +49,37 @@ public:
     void set_recoverable_error() const override { return m_stream.set_recoverable_error(); }
     void set_fatal_error() const override { return m_stream.set_fatal_error(); }
 
+    size_t read(Bytes bytes) override
+    {
+        if (has_any_error())
+            return 0;
+
+        auto nread = buffer().trim(m_buffered).copy_trimmed_to(bytes);
+
+        m_buffered -= nread;
+        buffer().slice(nread, m_buffered).copy_to(buffer());
+
+        if (nread < bytes.size()) {
+            m_buffered = m_stream.read(buffer());
+
+            if (m_buffered == 0)
+                return nread;
+
+            nread += read(bytes.slice(nread));
+        }
+
+        return nread;
+    }
+
+    bool read_or_error(Bytes bytes) override
+    {
+        if (read(bytes) < bytes.size()) {
+            set_fatal_error();
+            return false;
+        }
+
+        return true;
+    }
 
 };
 
