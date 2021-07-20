6bd6fd15600ea154eb3d8b23dba03f0236badc25
krishpranav <krisna.pranav@gmail.com>
2021-07-20 18:40:59 +0530

    base: IterationDecision headers

---
+++ base/IterationDecision.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+namespace AK {
+
+enum class IterationDecision {
+    Continue,
+    Break,
+};
+
+}
+
+using AK::IterationDecision;
