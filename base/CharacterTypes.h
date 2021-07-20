f22db665a0266909743e5462655f934fe27a9f66
krishpranav <krisna.pranav@gmail.com>
2021-07-20 10:28:28 +0530

    base: CharacterTypes[constexpr bool]

--- base/CharacterTypes.h
+++ base/CharacterTypes.h
@@ -32,4 +32,42 @@ constexpr bool is_ascii_alpha(u32 code_point)
     return (code_point >= 'a' && code_point <= 'z');
 }
 
+constexpr bool is_ascii_alphanumeric(u32 code_point)
+{
+    return is_ascii_alpha(code_point) || is_ascii_digit(code_point);
+}
+
+constexpr bool is_ascii_hex_digit(u32 code_point)
+{
+    return is_ascii_digit(code_point) || (code_point >= 'A' && code_point <= 'F') || (code_point >= 'a' && code_point <= 'f');
+}
+
+constexpr bool is_ascii_blank(u32 code_point)
+{
+    return code_point == '\t' || code_point == ' ';
+}
+
+constexpr bool is_ascii_space(u32 code_point)
+{
+    return code_point == ' ' || code_point == '\t' || code_point == '\n' || code_point == '\v' || code_point == '\f' || code_point == '\r';
+}
+
+constexpr bool is_ascii_punctuation(u32 code_point)
+{
+    return (code_point >= 0x21 && code_point <= 0x2F) || (code_point >= 0x3A && code_point <= 0x40) || (code_point >= 0x5B && code_point <= 0x60) || (code_point >= 0x7B && code_point <= 0x7E);
+}
+
+constexpr bool is_ascii_graphical(u32 code_point)
+{
+    return code_point >= 0x21 && code_point <= 0x7E;
+}
+
+constexpr bool is_ascii_printable(u32 code_point)
+{
+    return code_point >= 0x20 && code_point <= 0x7E;
+}
+
+
+
+
 }
