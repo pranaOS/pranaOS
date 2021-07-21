8a62527b37a1cadb81bc9dbc0e05d8bc03092880
krishpranav <krisna.pranav@gmail.com>
2021-07-21 15:36:25 +0530

    base: Generic

--- base/GenericLexer.cpp
+++ base/GenericLexer.cpp
@@ -40,4 +40,47 @@ StringView GenericLexer::consume_line()
     return m_input.substring_view(start, length);
 }
 
+StringView GenericLexer::consume_line()
+{
+    size_t start = m_index;
+    while (!is_eof() && peek() != '\r' && peek() != '\n')
+        m_index++;
+    size_t length = m_index - start;
+
+    consume_specific('\r');
+    consume_specific('\n');
+
+    if (length == 0)
+        return {};
+    return m_input.substring_view(start, length);
+}
+
+StringView GenericLexer::consume_until(char stop)
+{
+    size_t start = m_index;
+    while (!is_eof() && peek() != stop)
+        m_index++;
+    size_t length = m_index - start;
+
+    ignore();
+
+    if (length == 0)
+        return {};
+    return m_input.substring_view(start, length);
+}
+
+StringView GenericLexer::consume_until(const char* stop)
+{
+    size_t start = m_index;
+    while (!is_eof() && !next_is(stop))
+        m_index++;
+    size_t length = m_index - start;
+
+    ignore(__builtin_strlen(stop));
+
+    if (length == 0)
+        return {};
+    return m_input.substring_view(start, length);
+}
+
 }
