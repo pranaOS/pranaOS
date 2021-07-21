d7b43c64d1972469a6d8fef8def6235287808766
krishpranav <krisna.pranav@gmail.com>
2021-07-21 15:45:41 +0530

    base: Hashmap[private, public]

---
+++ base/HashMap.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+#include <base/HashTable.h>
+#include <base/Optional.h>
+#include <base/Vector.h>
+
+#ifndef PRANAOS_LIBC_BUILD
+#    include <initializer_list>
+#endif
+
+
+namespace Base {
+
+template<typename K, typename V, typename KeyTraits, bool IsOrdered>
+class HashMap {
+private:
+    struct Entry {
+        K key;
+        V value;
+    };
+
+    struct EntryTraits {
+        static unsigned hash(const Entry& entry) { return KeyTraits::hash(entry.key); }
+        static bool equals(const Entry& a, const Entry& b) { return KeyTraits::equals(a.key, b.key); }
+    };
+
+public:
+    using KeyType = K;
+    using ValueType = V;
+
+    HashMap() = default;
+
+};
+
+}
