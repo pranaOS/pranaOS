580c294378c4947243c4ce6ca5c23546d3a1e01a
krishpranav <krisna.pranav@gmail.com>
2021-07-21 10:58:53 +0530

    base: EnumBits[discard values]

--- base/EnumBits.h
+++ base/EnumBits.h
@@ -7,11 +7,50 @@
 #pragma once
 
 // includes
-#include "AK/StdLibExtras.h"
+#include "base/StdLibExtras.h"
 
 
 #define BASE_ENUM_BITWISE_OPERATORS(Enum) \
         _BASE_ENUM_BITWISE_OPERATOR_INTERNAL(Enum, )
 
-#define AK_ENUM_BITWISE_FRIEND_OPERATORS(Enum) \
-    _AK_ENUM_BITWISE_OPERATORS_INTERNAL(Enum, friend)
+#define BASE_ENUM_BITWISE_FRIEND_OPERATORS(Enum) \
+    _BASE_ENUM_BITWISE_OPERATORS_INTERNAL(Enum, friend)
+
+
+#define _BASE_ENUM_BITWISE_OPERATORS_INTERNAL(Enum, Prefix)                  \
+                                                                           \
+    [[nodiscard]] Prefix constexpr Enum operator|(Enum lhs, Enum rhs)      \
+    {                                                                      \
+        using Type = UnderlyingType<Enum>;                                 \
+        return static_cast<Enum>(                                          \
+            static_cast<Type>(lhs) | static_cast<Type>(rhs));              \
+    }                                                                      \
+                                                                           \
+    [[nodiscard]] Prefix constexpr Enum operator&(Enum lhs, Enum rhs)      \
+    {                                                                      \
+        using Type = UnderlyingType<Enum>;                                 \
+        return static_cast<Enum>(                                          \
+            static_cast<Type>(lhs) & static_cast<Type>(rhs));              \
+    }                                                                      \
+                                                                           \
+    [[nodiscard]] Prefix constexpr Enum operator^(Enum lhs, Enum rhs)      \
+    {                                                                      \
+        using Type = UnderlyingType<Enum>;                                 \
+        return static_cast<Enum>(                                          \
+            static_cast<Type>(lhs) ^ static_cast<Type>(rhs));              \
+    }                                                                      \
+                                                                           \
+    [[nodiscard]] Prefix constexpr Enum operator~(Enum rhs)                \
+    {                                                                      \
+        using Type = UnderlyingType<Enum>;                                 \
+        return static_cast<Enum>(                                          \
+            ~static_cast<Type>(rhs));                                      \
+    }                                                                      \
+                                                                           \
+    Prefix constexpr Enum& operator|=(Enum& lhs, Enum rhs)                 \
+    {                                                                      \
+        using Type = UnderlyingType<Enum>;                                 \
+        lhs = static_cast<Enum>(                                           \
+            static_cast<Type>(lhs) | static_cast<Type>(rhs));              \
+        return lhs;                                                        \
+    }                                                                      \
