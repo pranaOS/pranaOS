71c6d07c16a76bca06731588d132eaf899dfa426
krishpranav <krisna.pranav@gmail.com>
2021-07-21 11:06:30 +0530

    base: FileStream

---
+++ base/FileStream.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+#include <base/Stream.h>
+#ifndef KERNEL
+#    include <errno.h>
+#    include <stdio.h>
+
+namespace Base {
+
+class InputFileStream : public InputStream {
+public:
+    explicit InputFileStream(int fd)
+        : m_file(fdopen(fd, "r"))
+        , m_owned(true)
+    {
+        if (!m_file)
+            set_fatal_error();
+    }
+
+    ~InputFileStream()
+    {
+        if (m_file) {
+            fflush(m_file);
+            if (m_owned)
+                fclose(m_file);
+        }
+    }
+
+    
+};
+
+}
