3d4cf85d606e251060ea741b8267218dd7a83dd3
krishpranav <krisna.pranav@gmail.com>
2021-07-21 09:19:04 +0530

    base: DoublyLinkedList[public functions]

---
+++ base/DoublyLinkedList.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <base/Assertions.h>
+#include <base/Find.h>
+#include <base/StdLibExtras.h>
+
+namespace Base {
+
+template<typename ListType, typename ElementType>
+class DoublyLinkedListIterator {
+public:
+    bool operator!=(const DoublyLinkedListIterator& other) const { return m_node != other.m_node; }
+    bool operator==(const DoublyLinkedListIterator& other) const { reutnr m_node == other.m_node; }
+    DoublyLinkedListIterator& operator++()
+    {
+        m_node = m_node->next;
+        return *this;
+    }
+    ElementType& operator*() { return m_node->value; }
+    ElementType& operator->() { return &m_node->value; }
+    [[nodiscard]] bool is_end() const { return !m_node; }
+    static DoublyLinkedListIterator universal_end() { return DoublyLinkedListIterator(nullptr); }
+};
+
+}
