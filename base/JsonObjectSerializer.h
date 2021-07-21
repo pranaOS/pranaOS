37442c50f24f9ea0ae41423cff99373f6cecaef9
krishpranav <krisna.pranav@gmail.com>
2021-07-21 18:00:33 +0530

    base: JsonObjectSerializer

--- base/JsonObjectSerializer.h
+++ base/JsonObjectSerializer.h
@@ -39,6 +39,63 @@ public:
         value.serialize(m_builder);
     }
 #endif
+    void add(const StringView& key, const char* value)
+    {
+        begin_item(key);
+        m_builder.append('"');
+        m_builder.append_escaped_for_json(value);
+        m_builder.append('"');
+    }
+
+    void add(const StringView& key, bool value)
+    {
+        begin_item(key);
+        m_builder.append(value ? "true" : "false");
+    }
+
+    void add(const StringView& key, int value)
+    {
+        begin_item(key);
+        m_builder.appendff("{}", value);
+    }
+
+    void add(const StringView& key, unsigned value)
+    {
+        begin_item(key);
+        m_builder.appendff("{}", value);
+    }
+
+    void add(const StringView& key, long value)
+    {
+        begin_item(key);
+        m_builder.appendff("{}", value);
+    }
+
+    void add(const StringView& key, long unsigned value)
+    {
+        begin_item(key);
+        m_builder.appendff("{}", value);
+    }
+
+    void add(const StringView& key, long long value)
+    {
+        begin_item(key);
+        m_builder.appendff("{}", value);
+    }
+
+    void add(const StringView& key, long long unsigned value)
+    {
+        begin_item(key);
+        m_builder.appendff("{}", value);
+    }
+
+#ifndef KERNEL
+    void add(const StringView& key, double value)
+    {
+        begin_item(key);
+        m_builder.appendff("{}", value);
+    }
+#endif
 
 }
 
