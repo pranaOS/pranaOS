e54cdc68763224677f8c2ec8dc738ccb5cf710cc
krishpranav <krisna.pranav@gmail.com>
2021-07-21 17:17:49 +0530

    base: IntrusiveRedBlackTree funcs

--- base/IntrusiveRedBlackTree.h
+++ base/IntrusiveRedBlackTree.h
@@ -43,5 +43,59 @@ public:
         return node_to_value(*node);
     }
 
+    void insert(V& value)
+    {
+        auto& node = value.*member;
+        BaseTree::insert(&node);
+        node.m_in_tree = true;
+    }
+
+        template<typename ElementType>
+    class BaseIterator {
+    public:
+        BaseIterator() = default;
+        bool operator!=(const BaseIterator& other) const { return m_node != other.m_node; }
+        BaseIterator& operator++()
+        {
+            if (!m_node)
+                return *this;
+            m_prev = m_node;
+            // the complexity is O(logn) for each successor call, but the total complexity for all elements comes out to O(n), meaning the amortized cost for a single call is O(1)
+            m_node = static_cast<TreeNode*>(BaseTree::successor(m_node));
+            return *this;
+        }
+        BaseIterator& operator--()
+        {
+            if (!m_prev)
+                return *this;
+            m_node = m_prev;
+            m_prev = static_cast<TreeNode*>(BaseTree::predecessor(m_prev));
+            return *this;
+        }
+        ElementType& operator*()
+        {
+            VERIFY(m_node);
+            return *node_to_value(*m_node);
+        }
+        ElementType* operator->()
+        {
+            VERIFY(m_node);
+            return node_to_value(*m_node);
+        }
+        [[nodiscard]] bool is_end() const { return !m_node; }
+        [[nodiscard]] bool is_begin() const { return !m_prev; }
+        [[nodiscard]] auto key() const { return m_node->key; }
+
+    private:
+        friend class IntrusiveRedBlackTree;
+        explicit BaseIterator(TreeNode* node, TreeNode* prev = nullptr)
+            : m_node(node)
+            , m_prev(prev)
+        {
+        }
+        TreeNode* m_node { nullptr };
+        TreeNode* m_prev { nullptr };
+    };
+
 }
 }
