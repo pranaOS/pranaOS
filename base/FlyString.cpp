99ad8aa976b1ee12511d2c14c054a7075bcfff0f
krishpranav <krisna.pranav@gmail.com>
2021-07-21 12:36:37 +0530

    base: FlyString

--- base/FlyString.cpp
+++ base/FlyString.cpp
@@ -32,5 +32,48 @@ static HashTable<StringImpl*, FlyStringImplTraits>& fly_impls()
     return *s_table;
 }
 
+void FlyString::did_destroy_impl(Badge<StringImpl>, StringImpl& impl)
+{
+    fly_impls().remove(&impl);
+}
+
+
+FlyString::FlyString(const String& string)
+{
+    if (string.is_null())
+        return;
+    if (string.impl()->is_fly()) {
+        m_impl = string.impl();
+        return;
+    }
+    auto it = fly_impls().find(const_cast<StringImpl*>(string.impl()));
+    if (it == fly_impls().end()) {
+        fly_impls().set(const_cast<StringImpl*>(string.impl()));
+        string.impl()->set_fly({}, true);
+        m_impl = string.impl();
+    } else {
+        VERIFY((*it)->is_fly());
+        m_impl = *it;
+    }
+}
+
+FlyString::FlyString(StringView const& string)
+{
+    if (string.is_null())
+        return;
+    auto it = fly_impls().find(string.hash(), [&](auto& candidate) {
+        return string == candidate;
+    });
+    if (it == fly_impls().end()) {
+        auto new_string = string.to_string();
+        fly_impls().set(new_string.impl());
+        new_string.impl()->set_fly({}, true);
+        m_impl = new_string.impl();
+    } else {
+        VERIFY((*it)->is_fly());
+        m_impl = *it;
+    }
+}
+
 
 }
