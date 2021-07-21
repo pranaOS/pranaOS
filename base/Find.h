7f02b9c56d022988744c0faf4590adcab8112466
krishpranav <krisna.pranav@gmail.com>
2021-07-21 12:03:42 +0530

    base: Find[TIteartor]

--- base/Find.h
+++ base/Find.h
@@ -3,3 +3,24 @@
  *
  * SPDX-License-Identifier: BSD-2-Clause
 */
+
+#pragma once
+
+// includes
+#include <base/Traits.h>
+#include <base/Types.h>
+
+namespace Base {
+
+template<typename TIterator, typename TUnaryPredicate>
+constexpr TIterator find_if(TIterator first, TIterator last, TUnaryPredicate&& pred)
+{
+    for (; first != last; ++first) {
+        if (pred(*first)) {
+            return first;
+        }
+    }
+    return last;
+}
+
+}
