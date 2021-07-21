ea34e9cea7265623ec7fa4b6781b43893a4c5609
krishpranav <krisna.pranav@gmail.com>
2021-07-21 15:35:39 +0530

    base: GenericLexer[StringView]

---
+++ base/GenericLexer.cpp
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <base/Assertions.h>
+#include <base/GenericLexer.h>
+#include <base/String.h>
+#include <base/StringBuilder.h>
+
+namespace Base {
+
+StringView GenericLexer::consume(size_t count)
+{
+    if (count == 0)
+        return {};
+    
+    size_t start = m_index;
+    size_t length = min(count, m_input.length() - m_index);
+    m_index += length;
+
+    return m_input.substring_view(start, length);
+}
+
+StringView GenericLexer::consume_line()
+{
+    size_t start = m_index;
+    while (!is_eof() && peek() != '\r' && peek() != '\n')
+        m_index++;
+    size_t length = m_index - start;
+
+    consume_specific('\r');
+    consume_specific('\n');
+
+    if (length == 0)
+        return {};
+
+    return m_input.substring_view(start, length);
+}
+
+}
