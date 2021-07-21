436f8198bc452e4898042415b1ae8aa92a84eac0
krishpranav <krisna.pranav@gmail.com>
2021-07-21 16:27:38 +0530

    base: IPv4Address

---
+++ base/IPv4Address.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+#include <base/Endian.h>
+#include <base/Optional.h>
+#include <base/String.h>
+#include <base/StringView.h>
+#include <base/Vector.h>
+
+namespace Base {
+
+class [[gnu::packed]] IPv4Address {
+    enum class SubnetClass : int {
+        A = 0,
+        B,
+        C,
+        D
+    };
+
+public:
+    using in_addr_t = u32;
+
+    constexpr IPv4Address() = default;
+
+    constexpr IPv4Address(u32 a, u32 b, u32 c, u32 d)
+    {
+        m_data = (d << 24) | (c << 16) | (b << 8) | a;
+    }
+
+    constexpr IPv4Address(const u8 data[4])
+    {
+        m_data = (u32(data[3]) << 24) | (u32(data[2]) << 16) | (u32(data[1]) << 8) | u32(data[0]);
+    }
+
+    constexpr IPv4Address(NetworkOrdered<u32> address)
+        : m_data(address)
+    {
+    }
+
+};
+
+}
