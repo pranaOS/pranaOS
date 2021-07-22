a0effa813ae3536de6a1f44d67938ed831b1b751
krishpranav <krisna.pranav@gmail.com>
2021-07-22 12:49:51 +0530

    base: MacAddress

--- base/MacAddress.h
+++ base/MacAddress.h
@@ -29,4 +29,34 @@ public:
         m_data[5] = f;
     }
 
+    constexpr ~MACAddress() = default;
+
+    constexpr const u8& operator[](unsigned i) const
+    {
+        VERIFY(i < s_mac_address_length);
+        return m_data[i];
+    }
+
+    constexpr bool operator==(const MACAddress& other) const
+    {
+        for (auto i = 0u; i < m_data.size(); ++i) {
+            if (m_data[i] != other.m_data[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    String to_string() const
+    {
+        return String::formatted("{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}", m_data[0], m_data[1], m_data[2], m_data[3], m_data[4], m_data[5]);
+
+   }
+
+   constexpr bool is_zero() const
+   {
+        return all_of(m_data.begin(), m_data.end(), [](const auto octet) { return octet == 0; });
+
+   }
+
 }
