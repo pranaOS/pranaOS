89503160ddc53bbd09f4261f13bfd571e5f8d155
krishpranav <krisna.pranav@gmail.com>
2021-07-22 13:03:57 +0530

    base: MappedFile headers

--- base/MappedFile.h
+++ base/MappedFile.h
@@ -17,23 +17,26 @@ namespace Base {
 
 class MappedFile : public RefCounted<MappedFile> {
     BASE_MAKE_NONCOPYABLE(MappedFile);
-    BASE_MAKE_NONCOPYABLE(MappedFile);
+    BASE_MAKE_NONMOVABLE(MappedFile);
 
 public:
     static Result<NonnullRefPtr<MappedFile>, OSError> map(const String& path);
     ~MappedFile();
 
     void* data() { return m_data; }
+    const void* data() const { return m_data; }
+
+    size_t size() const { return m_size; }
+
+    ReadonlyBytes bytes() const { return { m_data, m_size }; }
 
 private:
-    explicit MappedFile(void*, size_t)
+    explicit MappedFile(void*, size_t);
 
-    void* m_data { nullptr; }
+    void* m_data { nullptr };
     size_t m_size { 0 };
-
 };
 
 }
-
 
 using Base::MappedFile;
