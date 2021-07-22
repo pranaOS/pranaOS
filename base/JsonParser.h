d7298d2e9c90411c0d0cdc83280b61d89daa6984
krishpranav <krisna.pranav@gmail.com>
2021-07-22 09:10:24 +0530

    base: JsonParser headers

--- base/JsonParser.h
+++ base/JsonParser.h
@@ -6,6 +6,7 @@
 
 #pragma once
 
+// includes
 #include <base/GenericLexer.h>
 #include <base/JsonValue.h>
 
@@ -21,8 +22,20 @@ public:
     Optional<JsonValue> parse();
 
 private:
-    Optional<JsonValue> parser_helper();
+    Optional<JsonValue> parse_helper();
 
+    String consume_and_unescape_string();
+    Optional<JsonValue> parse_array();
+    Optional<JsonValue> parse_object();
+    Optional<JsonValue> parse_number();
+    Optional<JsonValue> parse_string();
+    Optional<JsonValue> parse_false();
+    Optional<JsonValue> parse_true();
+    Optional<JsonValue> parse_null();
+
+    String m_last_string_starting_with_character[256];
 };
 
 }
+
+using Base::JsonParser;
