c9aef76274affac94f4a64f34aab66fc8cf4fc2c
krishpranav <krisna.pranav@gmail.com>
2021-07-22 10:18:14 +0530

    base: JsonPath

---
+++ base/JsonPath.cpp
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <base/JsonObject.h>
+#include <base/JsonPath.h>
+#include <base/JsonValue.h>
+
+namespace Base {
+
+JsonPathElement JsonPathElement::any_array_element { Kind::AnyIndex };
+JsonPathElement JsonPathElement::any_object_element { Kind::AnyKey };
+
+JsonValue JsonPath::resolve(const JsonValue& top_root) const
+{
+    auto root = top_root;
+    for (auto& element : *this) {
+        switch (element.kind()) {
+        case JsonPathElement::Kind::Key:
+            root = JsonValue { root.as_object().get(element.key()) };
+            break;
+        case JsonPathElement::Kind::Index:
+            root = JsonValue { root.as_array().at(element.index()) };
+            break;
+        default:
+            VERIFY_NOT_REACHED();
+        }
+    }
+    return root;
+}
+
+}
