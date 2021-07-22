70e41842dc69b85bad5d18a7fb18855eddfe26b9
krishpranav <krisna.pranav@gmail.com>
2021-07-22 10:25:11 +0530

    base: JsonValue[class]

--- base/JsonValue.h
+++ base/JsonValue.h
@@ -3,3 +3,38 @@
  *
  * SPDX-License-Identifier: BSD-2-Clause
 */
+
+#pragma once
+
+// includes
+#include <base/Forward.h>
+#include <base/Optional.h>
+#include <base/String.h>
+#include <base/StringBuilder.h>
+
+namespace Base {
+
+class JsonValue {
+public:
+    enum class Type {
+        Null,
+        Int32,
+        UnsignedInt32,
+        Int64,
+        UnsignedInt64,
+#if !defined(KERNEL)
+        Double,
+#endif
+        Bool,
+        String,
+        Array,
+        Object,
+    };
+
+    static Optional<JsonValue> from_string(const StringView&);
+
+    explicit JsonValue(Type = Type::Null);
+
+};
+
+}
