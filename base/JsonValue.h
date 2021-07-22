6e48e5af5a3ba89973d042662c747c3ba4c17ed0
krishpranav <krisna.pranav@gmail.com>
2021-07-22 10:35:02 +0530

    base: JsonValue headers

--- base/JsonValue.h
+++ base/JsonValue.h
@@ -69,18 +69,18 @@ public:
     template<typename Builder>
     void serialize(Builder&) const;
 
-    String as_string_or(String const& alternative) const 
+    String as_string_or(String const& alternative) const
     {
         if (is_string())
-            return as_string()
+            return as_string();
         return alternative;
     }
 
     String to_string() const
     {
-        if (is_string())    
+        if (is_string())
             return as_string();
-        return serialized<StringBuilder()>;
+        return serialized<StringBuilder>();
     }
 
     int to_int(int default_value = 0) const { return to_i32(default_value); }
@@ -140,7 +140,7 @@ public:
         return *m_value.as_object;
     }
 
-    const JsonArray& as_array()
+    const JsonArray& as_array() const
     {
         VERIFY(is_array());
         return *m_value.as_array;
@@ -153,12 +153,95 @@ public:
         return m_value.as_double;
     }
 #endif
-    
+
     Type type() const
     {
         return m_type;
     }
 
+    bool is_null() const { return m_type == Type::Null; }
+    bool is_bool() const { return m_type == Type::Bool; }
+    bool is_string() const { return m_type == Type::String; }
+    bool is_i32() const { return m_type == Type::Int32; }
+    bool is_u32() const { return m_type == Type::UnsignedInt32; }
+    bool is_i64() const { return m_type == Type::Int64; }
+    bool is_u64() const { return m_type == Type::UnsignedInt64; }
+#if !defined(KERNEL)
+    bool is_double() const
+    {
+        return m_type == Type::Double;
+    }
+#endif
+    bool is_array() const
+    {
+        return m_type == Type::Array;
+    }
+    bool is_object() const { return m_type == Type::Object; }
+    bool is_number() const
+    {
+        switch (m_type) {
+        case Type::Int32:
+        case Type::UnsignedInt32:
+        case Type::Int64:
+        case Type::UnsignedInt64:
+#if !defined(KERNEL)
+        case Type::Double:
+#endif
+            return true;
+        default:
+            return false;
+        }
+    }
+
+    template<typename T>
+    T to_number(T default_value = 0) const
+    {
+#if !defined(KERNEL)
+        if (is_double())
+            return (T)as_double();
+#endif
+        if (type() == Type::Int32)
+            return (T)as_i32();
+        if (type() == Type::UnsignedInt32)
+            return (T)as_u32();
+        if (type() == Type::Int64)
+            return (T)as_i64();
+        if (type() == Type::UnsignedInt64)
+            return (T)as_u64();
+        return default_value;
+    }
+
+    bool equals(const JsonValue& other) const;
+
+private:
+    void clear();
+    void copy_from(const JsonValue&);
+
+    Type m_type { Type::Null };
+
+    union {
+        StringImpl* as_string { nullptr };
+        JsonArray* as_array;
+        JsonObject* as_object;
+#if !defined(KERNEL)
+        double as_double;
+#endif
+        i32 as_i32;
+        u32 as_u32;
+        i64 as_i64;
+        u64 as_u64;
+        bool as_bool;
+    } m_value;
+};
+
+template<>
+struct Formatter<JsonValue> : Formatter<StringView> {
+    void format(FormatBuilder& builder, const JsonValue& value)
+    {
+        Formatter<StringView>::format(builder, value.to_string());
+    }
 };
 
 }
+
+using Base::JsonValue;
