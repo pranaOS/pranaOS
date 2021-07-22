d2d93025c9e81610997d414ca7af412ad1e38de2
krishpranav <krisna.pranav@gmail.com>
2021-07-22 10:12:55 +0530

    userland: utilities echo function updated

--- userland/utilities/echo.cpp
+++ userland/utilities/echo.cpp
@@ -1,7 +1,32 @@
+/*
+ * Copyright (c) 2021, Ashwin-Paudel, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
 
-#include <stdio>
+// includes
+#include <base/String.h>
+#include <libcore/ArgsParser.h> // this libraries will be added soon
+#include <stdio.h>
+#include <unistd.h>
 
-int main(int argc, char* argv[]) {
-  std::cout << argv[1] << std::endl;
-  return 0;
+int main(int argc, char** argv) 
+{
+    if (pledge("stdio", nullptr) < 0) {
+        perror("pledge");
+        return 1;
+    }
+
+    Vector<const char*> values;
+    bool no_trailing_newline = false;
+
+    Core::ArgsParser args_parser;
+    args_parser.add_option(no_trailing_newline, "Do not output a trailed newline", nullptr, 'n');
+    args_parser.add_positional_argument(values, "Values to print out", "string", Core::ArgsParser::Required::No);
+    args_parser.parse(argc, argv);
+
+    out("{}", String::join(' ', values));
+    if (!no_trailing_newline)
+        outln();
+    return 0;
 }
