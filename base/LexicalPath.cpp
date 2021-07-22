234fe6d475b607d2f2e452cabf438011ea3ebc38
krishpranav <krisna.pranav@gmail.com>
2021-07-22 11:40:28 +0530

    base: LexicalPath

--- base/LexicalPath.cpp
+++ base/LexicalPath.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Krisna Pranav
+ * Copyright (c) 2021, Krisna Pranav, Alex5xt
  *
  * SPDX-License-Identifier: BSD-2-Clause
 */
@@ -62,5 +62,81 @@ Vector<String> LexicalPath::parts() const
         vector.unchecked_append(part);
     return vector;
 }
+
+bool LexicalPath::has_extension(StringView const& extension) const
+{
+    return m_string.ends_with(extension, CaseSensitivity::CaseInsensitive);
+}
+
+String LexicalPath::canonicalized_path(String path)
+{
+    if (path.is_null())
+        return {};
+
+    if (path.is_empty())
+        return ".";
+
+    if (!path.contains("."sv) && !path.contains("//"sv) && !path.ends_with('/'))
+        return path;
+
+    auto is_absolute = path[0] == '/';
+    auto parts = path.split_view('/');
+    size_t approximate_canonical_length = 0;
+    Vector<String> canonical_parts;
+
+    for (auto& part : parts) {
+        if (part == ".")
+            continue;
+        if (part == "..") {
+            if (canonical_parts.is_empty()) {
+                if (is_absolute) {
+                    continue;
+                }
+            } else {
+                if (canonical_parts.last() != "..") {
+                    canonical_parts.take_last();
+                    continue;
+                }
+            }
+        }
+        approximate_canonical_length += part.length() + 1;
+        canonical_parts.append(part);
+    }
+
+    if (canonical_parts.is_empty() && !is_absolute)
+        canonical_parts.append(".");
+
+    StringBuilder builder(approximate_canonical_length);
+    if (is_absolute)
+        builder.append('/');
+    builder.join('/', canonical_parts);
+    return builder.to_string();
+}
+
+String LexicalPath::relative_path(StringView const& a_path, StringView const& a_prefix)
+{
+    if (!a_path.starts_with('/') || !a_prefix.starts_with('/')) {
+        return {};
+    }
+
+    if (a_path == a_prefix)
+        return ".";
+
+    auto path = canonicalized_path(a_path);
+    auto prefix = canonicalized_path(a_prefix);
+
+    if (path == prefix)
+        return ".";
+
+    if (prefix == "/"sv)
+        return path.substring_view(1);
+
+    if (path.starts_with(prefix) && path[prefix.length()] == '/') {
+        return path.substring_view(prefix.length() + 1);
+    }
+
+    return path;
+}
+
 
 }
