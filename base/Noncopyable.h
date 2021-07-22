ca2da4f89790029e98a6b2a9e9fa19623aa5627a
krishpranav <krisna.pranav@gmail.com>
2021-07-22 16:17:55 +0530

    base: Noncopyable

---
+++ base/Noncopyable.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+#define BASE_MAKE_NONCOPYABLE(c) \
+private:                       \
+    c(const c&) = delete;      \
+    c& operator=(const c&) = delete
+
+#define BASE_MAKE_NONMOVABLE(c) \
+private:                      \
+    c(c&&) = delete;          \
+    c& operator=(c&&) = delete
