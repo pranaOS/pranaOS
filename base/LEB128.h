89a7c860e3bffe7225ad25bb94e629c443d8aac2
krishpranav <krisna.pranav@gmail.com>
2021-07-22 11:07:39 +0530

    base: LEB128 headers

--- base/LEB128.h
+++ base/LEB128.h
@@ -51,6 +51,59 @@ struct LEB128 {
         return true;
     }
 
+    template<typename StreamT, typename ValueType = ssize_t>
+    static bool read_signed(StreamT& stream, ValueType& result)
+    {
+
+        static_assert(sizeof(ValueType) <= sizeof(u64), "Error checking logic assumes 64 bits or less!");
+        [[maybe_unused]] size_t backup_offset = 0;
+        if constexpr (requires { stream.offset(); })
+            backup_offset = stream.offset();
+        InputStream& input_stream { stream };
+
+        i64 temp = 0;
+        size_t num_bytes = 0;
+        u8 byte = 0;
+        result = 0;
+
+        do {
+            if (input_stream.unreliable_eof()) {
+                if constexpr (requires { stream.seek(backup_offset); })
+                    stream.seek(backup_offset);
+                input_stream.set_fatal_error();
+                return false;
+            }
+
+            input_stream >> byte;
+            if (input_stream.has_any_error())
+                return false;
+
+            u64 masked_byte = byte & ~(1 << 7);
+            const bool shift_too_large_for_result = (num_bytes * 7 >= 64) && (masked_byte != ((temp < 0) ? 0x7Fu : 0u));
+            const bool shift_too_large_for_byte = (num_bytes * 7) == 63 && masked_byte != 0x00 && masked_byte != 0x7Fu;
+
+            if (shift_too_large_for_result || shift_too_large_for_byte)
+                return false;
+
+            temp = (temp) | (masked_byte << (num_bytes * 7));
+            ++num_bytes;
+        } while (byte & (1 << 7));
+
+        if ((num_bytes * 7) < 64 && (byte & 0x40)) {
+            temp |= ((u64)(-1) << (num_bytes * 7));
+        }
+
+        if constexpr (sizeof(ValueType) < sizeof(u64)) {
+            if (temp > NumericLimits<ValueType>::max() || temp < NumericLimits<ValueType>::min())
+                return false;
+        }
+
+        result = static_cast<ValueType>(temp);
+
+        return true;
+    }
 };
 
 }
+
+using Base::LEB128;
