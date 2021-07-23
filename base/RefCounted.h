39ecb55607daec1303fe3891559ec0220db2b06f
krishpranav <krisna.pranav@gmail.com>
2021-07-23 16:48:13 +0530

    base: RefCounted headers

--- base/RefCounted.h
+++ base/RefCounted.h
@@ -41,6 +41,55 @@ constexpr auto call_one_ref_left_if_present(...) -> FalseType
     return {};
 }
 
+class RefCountedBase {
+    BASE_MAKE_NONCOPYABLE(RefCountedBase);
+    BASE_MAKE_NONMOVABLE(RefCountedBase);
+
+public:
+    using RefCountType = unsigned int;
+    using AllowOwnPtr = FalseType;
+
+    void ref() const
+    {
+        auto old_ref_count = m_ref_count.fetch_add(1, Base::MemoryOrder::memory_order_relaxed);
+        VERIFY(old_ref_count > 0);
+        VERIFY(!Checked<RefCountType>::addition_would_overflow(old_ref_count, 1));
+    }
+
+    [[nodiscard]] bool try_ref() const
+    {
+        RefCountType expected = m_ref_count.load(Base::MemoryOrder::memory_order_relaxed);
+        for (;;) {
+            if (expected == 0)
+                return false;
+            VERIFY(!Checked<RefCountType>::addition_would_overflow(expected, 1));
+            if (m_ref_count.compare_exchange_strong(expected, expected + 1, Base::MemoryOrder::memory_order_acquire))
+                return true;
+        }
+    }
+
+    [[nodiscard]] RefCountType ref_count() const
+    {
+        return m_ref_count.load(Base::MemoryOrder::memory_order_relaxed);
+    }
+
+protected:
+    RefCountedBase() = default;
+    ~RefCountedBase()
+    {
+        VERIFY(m_ref_count.load(Base::MemoryOrder::memory_order_relaxed) == 0);
+    }
+
+    RefCountType deref_base() const
+    {
+        auto old_ref_count = m_ref_count.fetch_sub(1, Base::MemoryOrder::memory_order_acq_rel);
+        VERIFY(old_ref_count > 0);
+        return old_ref_count - 1;
+    }
+
+    mutable Atomic<RefCountType> m_ref_count { 1 };
+};
+
 
 
 }
