4f0a83e18fb748cb2884ecf2dc2fea90e68bd058
krishpranav <krisna.pranav@gmail.com>
2021-07-23 11:28:28 +0530

    base: OwnPtr[class, functions]

--- base/OwnPtr.h
+++ base/OwnPtr.h
@@ -46,6 +46,63 @@ public
             m_ptr = (T*)(0xe1e1e1e1);
 #endif
     }
+
+    OwnPtr(const OwnPtr&) = delete;
+    template<typename U>
+    OwnPtr(const OwnPtr<U>&) = delete;
+    OwnPtr& operator=(const OwnPtr&) = delete;
+    template<typename U>
+    OwnPtr& operator=(const OwnPtr<U>&) = delete;
+
+    template<typename U>
+    OwnPtr(const NonnullOwnPtr<U>&) = delete;
+    template<typename U>
+    OwnPtr& operator=(const NonnullOwnPtr<U>&) = delete;
+    template<typename U>
+    OwnPtr(const RefPtr<U>&) = delete;
+    template<typename U>
+    OwnPtr(const NonnullRefPtr<U>&) = delete;
+    template<typename U>
+    OwnPtr(const WeakPtr<U>&) = delete;
+    template<typename U>
+    OwnPtr& operator=(const RefPtr<U>&) = delete;
+    template<typename U>
+    OwnPtr& operator=(const NonnullRefPtr<U>&) = delete;
+    template<typename U>
+    OwnPtr& operator=(const WeakPtr<U>&) = delete;
+
+    OwnPtr& operator=(OwnPtr&& other)
+    {
+        OwnPtr ptr(move(other));
+        swap(ptr);
+        return *this;
+    }
+
+    template<typename U>
+    OwnPtr& operator=(OwnPtr<U>&& other)
+    {
+        OwnPtr ptr(move(other));
+        swap(ptr);
+        return *this;
+    }
+
+    template<typename U>
+    OwnPtr& operator=(NonnullOwnPtr<U>&& other)
+    {
+        OwnPtr ptr(move(other));
+        swap(ptr);
+        VERIFY(m_ptr);
+        return *this;
+    }
+
+    OwnPtr& operator=(T* ptr) = delete;
+
+    OwnPtr& operator=(std::nullptr_t)
+    {
+        clear();
+        return *this;
+    }
+
 };
 
 }
