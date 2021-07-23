87ec7cf83c8ca5b6c4496afa759680f19ac2a5a1
krishpranav <krisna.pranav@gmail.com>
2021-07-23 18:43:08 +0530

    base: ScopedGuard[template]

---
+++ base/ScopeGuard.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <base/StdLibExtras.h>
+
+namespace Base {
+
+template<typename Callback>
+class ScopeGuard {
+public:
+    ScopeGuard(Callback Callback)
+        : m_callback(move(callback))
+    {
+    }
+
+    ~ScopeGuard()
+    {
+        m_callback();
+    }
+
+private:
+    Callback m_callback;
+
+};
+
+}
