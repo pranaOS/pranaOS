345e10d00ac7d60602f708940f15e50501c787dd
krishpranav <krisna.pranav@gmail.com>
2021-07-23 11:17:44 +0530

    base: Optional headers

--- base/Optional.h
+++ base/Optional.h
@@ -4,13 +4,6 @@
  * SPDX-License-Identifier: BSD-2-Clause
 */
 
-/*
- * Copyright (c) 2018-2021, Andreas Kling <kling@serenityos.org>
- * Copyright (c) 2021, Daniel Bertalan <dani@danielbertalan.dev>
- *
- * SPDX-License-Identifier: BSD-2-Clause
- */
-
 #pragma once
 
 #include <base/Assertions.h>
