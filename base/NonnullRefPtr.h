1985d9e7a9540211cd6189e0147ad018de6529f2
krishpranav <krisna.pranav@gmail.com>
2021-07-23 08:54:11 +0530

    base: NonnullRefPtr[template, ALWAYS_INLINE]

--- base/NonnullRefPtr.h
+++ base/NonnullRefPtr.h
@@ -51,6 +51,77 @@ public:
     using ElementType = T;
 
     enum AdoptTag { Adopt };
+
+    ALWAYS_INLINE NonnullRefPtr(const T& object)
+        : m_bits((FlatPtr)&object)
+    {
+        VERIFY(!(m_bits & 1));
+        const_cast<T&>(object).ref();
+    }
+    template<typename U>
+    ALWAYS_INLINE NonnullRefPtr(const U& object)
+        : m_bits((FlatPtr) static_cast<const T*>(&object))
+    {
+        VERIFY(!(m_bits & 1));
+        const_cast<T&>(static_cast<const T&>(object)).ref();
+    }
+    ALWAYS_INLINE NonnullRefPtr(AdoptTag, T& object)
+        : m_bits((FlatPtr)&object)
+    {
+        VERIFY(!(m_bits & 1));
+    }
+    ALWAYS_INLINE NonnullRefPtr(NonnullRefPtr&& other)
+        : m_bits((FlatPtr)&other.leak_ref())
+    {
+        VERIFY(!(m_bits & 1));
+    }
+    template<typename U>
+    ALWAYS_INLINE NonnullRefPtr(NonnullRefPtr<U>&& other)
+        : m_bits((FlatPtr)&other.leak_ref())
+    {
+        VERIFY(!(m_bits & 1));
+    }
+    ALWAYS_INLINE NonnullRefPtr(const NonnullRefPtr& other)
+        : m_bits((FlatPtr)other.add_ref())
+    {
+        VERIFY(!(m_bits & 1));
+    }
+    template<typename U>
+    ALWAYS_INLINE NonnullRefPtr(const NonnullRefPtr<U>& other)
+        : m_bits((FlatPtr)other.add_ref())
+    {
+        VERIFY(!(m_bits & 1));
+    }
+    ALWAYS_INLINE ~NonnullRefPtr()
+    {
+        assign(nullptr);
+#ifdef SANITIZE_PTRS
+        if constexpr (sizeof(T*) == 8)
+            m_bits.store(0xb0b0b0b0b0b0b0b0, AK::MemoryOrder::memory_order_relaxed);
+        else
+            m_bits.store(0xb0b0b0b0, AK::MemoryOrder::memory_order_relaxed);
+#endif
+    }
+
+    template<typename U>
+    NonnullRefPtr(const OwnPtr<U>&) = delete;
+    template<typename U>
+    NonnullRefPtr& operator=(const OwnPtr<U>&) = delete;
+
+    template<typename U>
+    NonnullRefPtr(const RefPtr<U>&) = delete;
+    template<typename U>
+    NonnullRefPtr& operator=(const RefPtr<U>&) = delete;
+    NonnullRefPtr(const RefPtr<T>&) = delete;
+    NonnullRefPtr& operator=(const RefPtr<T>&) = delete;
+
+    NonnullRefPtr& operator=(const NonnullRefPtr& other)
+    {
+        if (this != &other)
+            assign(other.add_ref());
+        return *this;
+    }
+
 };
 
 }
