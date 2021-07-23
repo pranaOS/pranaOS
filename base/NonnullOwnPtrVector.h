f5ea5be905ecc3d40c23087e4614bef19c951168
krishpranav <krisna.pranav@gmail.com>
2021-07-23 08:30:43 +0530

    base: NonnullOwnPtrVector headers

---
+++ base/NonnullOwnPtrVector.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <base/NonnullOwnPtr.h>
+#include <base/NonnullPtrVector.h>
+
+namespace Base {
+
+template<typename T, size_t inline_capacity>
+class NonnullOwnPtrVector : public NonnullPtrVector<NonnullOwnPtr<T>, inline_capacity> {
+};
+
+}
+
+using Base::NonnullOwnPtrVector;
