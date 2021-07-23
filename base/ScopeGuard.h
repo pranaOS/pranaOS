10d75274be35072af73a252ce552ab64b7641928
krishpranav <krisna.pranav@gmail.com>
2021-07-23 18:43:58 +0530

    base: ScopeGuard headers

--- base/ScopeGuard.h
+++ base/ScopeGuard.h
@@ -14,7 +14,7 @@ namespace Base {
 template<typename Callback>
 class ScopeGuard {
 public:
-    ScopeGuard(Callback Callback)
+    ScopeGuard(Callback callback)
         : m_callback(move(callback))
     {
     }
@@ -26,7 +26,30 @@ public:
 
 private:
     Callback m_callback;
+};
 
+template<typename Callback>
+class ArmedScopeGuard {
+public:
+    ArmedScopeGuard(Callback callback)
+        : m_callback(move(callback))
+    {
+    }
+
+    ~ArmedScopeGuard()
+    {
+        if (m_armed)
+            m_callback();
+    }
+
+    void disarm() { m_armed = false; }
+
+private:
+    Callback m_callback;
+    bool m_armed { true };
 };
 
 }
+
+using Base::ArmedScopeGuard;
+using Base::ScopeGuard;
