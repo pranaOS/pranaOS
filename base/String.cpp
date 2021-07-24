0d1a3c2c71191814bf7119b2ddd4447eaef95eab
krishpranav <krisna.pranav@gmail.com>
2021-07-24 13:03:26 +0530

    base: String[string]

--- base/String.cpp
+++ base/String.cpp
@@ -118,5 +118,198 @@ Vector<String> String::split(char separator, bool keep_empty) const
     return split_limit(separator, 0, keep_empty);
 }
 
+Vector<String> String::split_limit(char separator, size_t limit, bool keep_empty) const
+{
+    if (is_empty())
+        return {};
+
+    Vector<String> v;
+    size_t substart = 0;
+    for (size_t i = 0; i < length() && (v.size() + 1) != limit; ++i) {
+        char ch = characters()[i];
+        if (ch == separator) {
+            size_t sublen = i - substart;
+            if (sublen != 0 || keep_empty)
+                v.append(substring(substart, sublen));
+            substart = i + 1;
+        }
+    }
+    size_t taillen = length() - substart;
+    if (taillen != 0 || keep_empty)
+        v.append(substring(substart, taillen));
+    return v;
+}
+
+Vector<StringView> String::split_view(const char separator, bool keep_empty) const
+{
+    if (is_empty())
+        return {};
+
+    Vector<StringView> v;
+    size_t substart = 0;
+    for (size_t i = 0; i < length(); ++i) {
+        char ch = characters()[i];
+        if (ch == separator) {
+            size_t sublen = i - substart;
+            if (sublen != 0 || keep_empty)
+                v.append(substring_view(substart, sublen));
+            substart = i + 1;
+        }
+    }
+    size_t taillen = length() - substart;
+    if (taillen != 0 || keep_empty)
+        v.append(substring_view(substart, taillen));
+    return v;
+}
+
+ByteBuffer String::to_byte_buffer() const
+{
+    if (!m_impl)
+        return {};
+    return ByteBuffer::copy(reinterpret_cast<const u8*>(characters()), length());
+}
+
+template<typename T>
+Optional<T> String::to_int(TrimWhitespace trim_whitespace) const
+{
+    return StringUtils::convert_to_int<T>(view(), trim_whitespace);
+}
+
+template Optional<i8> String::to_int(TrimWhitespace) const;
+template Optional<i16> String::to_int(TrimWhitespace) const;
+template Optional<i32> String::to_int(TrimWhitespace) const;
+template Optional<i64> String::to_int(TrimWhitespace) const;
+
+template<typename T>
+Optional<T> String::to_uint(TrimWhitespace trim_whitespace) const
+{
+    return StringUtils::convert_to_uint<T>(view(), trim_whitespace);
+}
+
+template Optional<u8> String::to_uint(TrimWhitespace) const;
+template Optional<u16> String::to_uint(TrimWhitespace) const;
+template Optional<u32> String::to_uint(TrimWhitespace) const;
+template Optional<u64> String::to_uint(TrimWhitespace) const;
+
+bool String::starts_with(const StringView& str, CaseSensitivity case_sensitivity) const
+{
+    return StringUtils::starts_with(*this, str, case_sensitivity);
+}
+
+bool String::starts_with(char ch) const
+{
+    if (is_empty())
+        return false;
+    return characters()[0] == ch;
+}
+
+bool String::ends_with(const StringView& str, CaseSensitivity case_sensitivity) const
+{
+    return StringUtils::ends_with(*this, str, case_sensitivity);
+}
+
+bool String::ends_with(char ch) const
+{
+    if (is_empty())
+        return false;
+    return characters()[length() - 1] == ch;
+}
+
+String String::repeated(char ch, size_t count)
+{
+    if (!count)
+        return empty();
+    char* buffer;
+    auto impl = StringImpl::create_uninitialized(count, buffer);
+    memset(buffer, ch, count);
+    return *impl;
+}
+
+String String::repeated(const StringView& string, size_t count)
+{
+    if (!count || string.is_empty())
+        return empty();
+    char* buffer;
+    auto impl = StringImpl::create_uninitialized(count * string.length(), buffer);
+    for (size_t i = 0; i < count; i++)
+        __builtin_memcpy(buffer + i * string.length(), string.characters_without_null_termination(), string.length());
+    return *impl;
+}
+
+String String::bijective_base_from(size_t value, unsigned base, StringView map)
+{
+    if (map.is_null())
+        map = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"sv;
+
+    VERIFY(base >= 2 && base <= map.length());
+
+    Array<char, round_up_to_power_of_two(sizeof(size_t) * 8 + 1, 2)> buffer;
+    size_t i = 0;
+    do {
+        buffer[i++] = map[value % base];
+        value /= base;
+    } while (value > 0);
+
+    if (i > 1)
+        --buffer[i - 1];
+
+    for (size_t j = 0; j < i / 2; ++j)
+        swap(buffer[j], buffer[i - j - 1]);
+
+    return String { ReadonlyBytes(buffer.data(), i) };
+}
+
+String String::roman_number_from(size_t value)
+{
+    if (value > 3999)
+        return String::number(value);
+
+    StringBuilder builder;
+
+    while (value > 0) {
+        if (value >= 1000) {
+            builder.append('M');
+            value -= 1000;
+        } else if (value >= 900) {
+            builder.append("CM"sv);
+            value -= 900;
+        } else if (value >= 500) {
+            builder.append('D');
+            value -= 500;
+        } else if (value >= 400) {
+            builder.append("CD"sv);
+            value -= 400;
+        } else if (value >= 100) {
+            builder.append('C');
+            value -= 100;
+        } else if (value >= 90) {
+            builder.append("XC"sv);
+            value -= 90;
+        } else if (value >= 50) {
+            builder.append('L');
+            value -= 50;
+        } else if (value >= 40) {
+            builder.append("XL"sv);
+            value -= 40;
+        } else if (value >= 10) {
+            builder.append('X');
+            value -= 10;
+        } else if (value == 9) {
+            builder.append("IX"sv);
+            value -= 9;
+        } else if (value >= 5 && value <= 8) {
+            builder.append('V');
+            value -= 5;
+        } else if (value == 4) {
+            builder.append("IV"sv);
+            value -= 4;
+        } else if (value <= 3) {
+            builder.append('I');
+            value -= 1;
+        }
+    }
+
+    return builder.to_string();
+}
 
 }
