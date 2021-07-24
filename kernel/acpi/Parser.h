a103d9a9b9c1699b2ab33713968285cd6fbfd671
krishpranav <krisna.pranav@gmail.com>
2021-07-24 11:15:09 +0530

    kernel: acpi Parser headers

--- kernel/acpi/Parser.h
+++ kernel/acpi/Parser.h
@@ -6,10 +6,11 @@
 
 #pragma once
 
+// includes
 #include <base/Types.h>
 #include <kernel/acpi/Definitions.h>
 #include <kernel/acpi/Initialize.h>
-#include <kernel/FileSystem/SysFSComponent.h>
+#include <kernel/filesystem/SysFSComponent.h>
 #include <kernel/PhysicalAddress.h>
 #include <kernel/vm/Region.h>
 #include <kernel/VirtualAddress.h>
@@ -24,21 +25,19 @@ private:
     ACPISysFSDirectory();
 };
 
-class ACPISysFSComponent : public SysFScomponent {
+class ACPISysFSComponent : public SysFSComponent {
 public:
     static NonnullRefPtr<ACPISysFSComponent> create(String name, PhysicalAddress, size_t table_size);
 
     virtual KResultOr<size_t> read_bytes(off_t, size_t, UserOrKernelBuffer&, FileDescription*) const override;
 
 protected:
-    OwnPtr<KBuffer> try_to_generate() const;
+    OwnPtr<KBuffer> try_to_generate_buffer() const;
     ACPISysFSComponent(String name, PhysicalAddress, size_t table_size);
 
     PhysicalAddress m_paddr;
     size_t m_length;
-
 };
-
 
 class Parser {
 public:
@@ -48,9 +47,33 @@ public:
     static void initialize(PhysicalAddress rsdp)
     {
         set_the(*new ParserType(rsdp));
-    }   
+    }
 
     virtual PhysicalAddress find_table(const StringView& signature);
+
+    virtual void try_acpi_reboot();
+    virtual bool can_reboot();
+    virtual void try_acpi_shutdown();
+    virtual bool can_shutdown() { return false; }
+
+    PhysicalAddress rsdp() const { return m_rsdp; }
+    PhysicalAddress main_system_description_table() const { return m_main_system_description_table; }
+    bool is_xsdt_supported() const { return m_xsdt_supported; }
+
+    void enumerate_static_tables(Function<void(const StringView&, PhysicalAddress, size_t)>);
+
+    virtual bool have_8042() const
+    {
+        return m_x86_specific_flags.keyboard_8042;
+    }
+
+    const FADTFlags::HardwareFeatures& hardware_features() const { return m_hardware_flags; }
+    const FADTFlags::x86_Specific_Flags& x86_specific_flags() const { return m_x86_specific_flags; }
+
+    virtual void enable_aml_interpretation();
+    virtual void enable_aml_interpretation(File&);
+    virtual void enable_aml_interpretation(u8*, u32);
+    virtual void disable_aml_interpretation();
 
 protected:
     explicit Parser(PhysicalAddress rsdp);
@@ -59,11 +82,27 @@ protected:
 private:
     static void set_the(Parser&);
 
-    size_t get_table_size(PhysicalAddress)
+    void locate_static_data();
+    void locate_main_system_description_table();
+    void initialize_main_system_description_table();
+    size_t get_table_size(PhysicalAddress);
     u8 get_table_revision(PhysicalAddress);
     void init_fadt();
     void init_facs();
 
+    bool validate_reset_register();
+    void access_generic_address(const Structures::GenericAddressStructure&, u32 value);
+
+    PhysicalAddress m_rsdp;
+    PhysicalAddress m_main_system_description_table;
+
+    Vector<PhysicalAddress> m_sdt_pointers;
+    PhysicalAddress m_fadt;
+    PhysicalAddress m_facs;
+
+    bool m_xsdt_supported { false };
+    FADTFlags::HardwareFeatures m_hardware_flags;
+    FADTFlags::x86_Specific_Flags m_x86_specific_flags;
 };
 
 }
