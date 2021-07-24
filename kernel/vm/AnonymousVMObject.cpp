f299160711a23aa0ce87732cec0e44d70f9c4708
krishpranav <krisna.pranav@gmail.com>
2021-07-24 11:59:59 +0530

    kernel: vm AnonymousVMObject headers

--- kernel/vm/AnonymousVMObject.cpp
+++ kernel/vm/AnonymousVMObject.cpp
@@ -7,7 +7,7 @@
 #include <kernel/arch/x86/SmapDisabler.h>
 #include <kernel/Debug.h>
 #include <kernel/Process.h>
-#include <Kernel/vm/AnonymousVMObject.h>
+#include <kernel/vm/AnonymousVMObject.h>
 #include <kernel/vm/MemoryManage.h>
 #include <kernel/vm/PhysicalPage.h>
 
@@ -100,9 +100,9 @@ AnonymousVMObject::AnonymousVMObject(AnonymousVMObject const& other)
     : VMObject(other)
     , m_volatile_ranges_cache({ 0, page_count() }) 
     , m_volatile_ranges_cache_dirty(true)          
-    , m_purgeable_ranges()                         
+    , m_purgeable_ranges()                        
     , m_unused_committed_pages(other.m_unused_committed_pages)
-    , m_cow_map()                                                      
+    , m_cow_map()                                 
     , m_shared_committed_cow_pages(other.m_shared_committed_cow_pages) 
 {
     VERIFY(other.m_lock.is_locked());
@@ -111,6 +111,7 @@ AnonymousVMObject::AnonymousVMObject(AnonymousVMObject const& other)
     ensure_or_reset_cow_map();
 
     if (m_unused_committed_pages > 0) {
+
         for (size_t i = 0; i < page_count(); i++) {
             auto& phys_page = m_physical_pages[i];
             if (phys_page && phys_page->is_lazy_committed_page()) {
@@ -209,5 +210,226 @@ bool AnonymousVMObject::is_any_volatile() const
     return false;
 }
 
+size_t AnonymousVMObject::remove_lazy_commit_pages(VolatilePageRange const& range)
+{
+    VERIFY(m_lock.is_locked());
+
+    size_t removed_count = 0;
+    auto range_end = range.base + range.count;
+    for (size_t i = range.base; i < range_end; i++) {
+        auto& phys_page = m_physical_pages[i];
+        if (phys_page && phys_page->is_lazy_committed_page()) {
+            phys_page = MM.shared_zero_page();
+            removed_count++;
+            VERIFY(m_unused_committed_pages > 0);
+            if (--m_unused_committed_pages == 0)
+                break;
+        }
+    }
+    return removed_count;
+}
+
+void AnonymousVMObject::update_volatile_cache()
+{
+    VERIFY(m_lock.is_locked());
+    VERIFY(m_volatile_ranges_cache_dirty);
+
+    m_volatile_ranges_cache.clear();
+    for_each_nonvolatile_range([&](VolatilePageRange const& range) {
+        m_volatile_ranges_cache.add_unchecked(range);
+    });
+
+    m_volatile_ranges_cache_dirty = false;
+}
+
+void AnonymousVMObject::range_made_volatile(VolatilePageRange const& range)
+{
+    VERIFY(m_lock.is_locked());
+
+    if (m_unused_committed_pages == 0)
+        return;
+
+    size_t uncommit_page_count = 0;
+    for_each_volatile_range([&](auto const& r) {
+        auto intersected = range.intersected(r);
+        if (!intersected.is_empty()) {
+            uncommit_page_count += remove_lazy_commit_pages(intersected);
+            if (m_unused_committed_pages == 0)
+                return IterationDecision::Break;
+        }
+        return IterationDecision::Continue;
+    });
+
+    if (uncommit_page_count > 0) {
+        dbgln_if(COMMIT_DEBUG, "Uncommit {} lazy-commit pages from {:p}", uncommit_page_count, this);
+        MM.uncommit_user_physical_pages(uncommit_page_count);
+    }
+
+    m_volatile_ranges_cache_dirty = true;
+}
+
+void AnonymousVMObject::range_made_nonvolatile(VolatilePageRange const&)
+{
+    VERIFY(m_lock.is_locked());
+    m_volatile_ranges_cache_dirty = true;
+}
+
+size_t AnonymousVMObject::count_needed_commit_pages_for_nonvolatile_range(VolatilePageRange const& range)
+{
+    VERIFY(m_lock.is_locked());
+    VERIFY(!range.is_empty());
+
+    size_t need_commit_pages = 0;
+    auto range_end = range.base + range.count;
+    for (size_t page_index = range.base; page_index < range_end; page_index++) {
+        if (!m_cow_map.is_null() && m_cow_map.get(page_index))
+            continue;
+        auto& phys_page = m_physical_pages[page_index];
+        if (phys_page && phys_page->is_shared_zero_page())
+            need_commit_pages++;
+    }
+    return need_commit_pages;
+}
+
+size_t AnonymousVMObject::mark_committed_pages_for_nonvolatile_range(VolatilePageRange const& range, size_t mark_total)
+{
+    VERIFY(m_lock.is_locked());
+    VERIFY(!range.is_empty());
+    VERIFY(mark_total > 0);
+
+    size_t pages_updated = 0;
+    auto range_end = range.base + range.count;
+    for (size_t page_index = range.base; page_index < range_end; page_index++) {
+        if (!m_cow_map.is_null() && m_cow_map.get(page_index))
+            continue;
+        auto& phys_page = m_physical_pages[page_index];
+        if (phys_page && phys_page->is_shared_zero_page()) {
+            phys_page = MM.lazy_committed_page();
+            if (++pages_updated == mark_total)
+                break;
+        }
+    }
+
+    dbgln_if(COMMIT_DEBUG, "Added {} lazy-commit pages to {:p}", pages_updated, this);
+
+    m_unused_committed_pages += pages_updated;
+    return pages_updated;
+}
+
+NonnullRefPtr<PhysicalPage> AnonymousVMObject::allocate_committed_page(Badge<Region>, size_t page_index)
+{
+    {
+        ScopedSpinLock lock(m_lock);
+
+        VERIFY(m_unused_committed_pages > 0);
+
+
+        VERIFY([&]() {
+            for (auto* purgeable_ranges : m_purgeable_ranges) {
+                if (purgeable_ranges->is_volatile(page_index))
+                    return false;
+            }
+            return true;
+        }());
+
+        m_unused_committed_pages--;
+    }
+    return MM.allocate_committed_user_physical_page(MemoryManager::ShouldZeroFill::Yes);
+}
+
+Bitmap& AnonymousVMObject::ensure_cow_map()
+{
+    if (m_cow_map.is_null())
+        m_cow_map = Bitmap { page_count(), true };
+    return m_cow_map;
+}
+
+void AnonymousVMObject::ensure_or_reset_cow_map()
+{
+    if (m_cow_map.is_null())
+        ensure_cow_map();
+    else
+        m_cow_map.fill(true);
+}
+
+bool AnonymousVMObject::should_cow(size_t page_index, bool is_shared) const
+{
+    auto& page = physical_pages()[page_index];
+    if (page && (page->is_shared_zero_page() || page->is_lazy_committed_page()))
+        return true;
+    if (is_shared)
+        return false;
+    return !m_cow_map.is_null() && m_cow_map.get(page_index);
+}
+
+void AnonymousVMObject::set_should_cow(size_t page_index, bool cow)
+{
+    ensure_cow_map().set(page_index, cow);
+}
+
+size_t AnonymousVMObject::cow_pages() const
+{
+    if (m_cow_map.is_null())
+        return 0;
+    return m_cow_map.count_slow(true);
+}
+
+bool AnonymousVMObject::is_nonvolatile(size_t page_index)
+{
+    if (m_volatile_ranges_cache_dirty)
+        update_volatile_cache();
+    return !m_volatile_ranges_cache.contains(page_index);
+}
+
+PageFaultResponse AnonymousVMObject::handle_cow_fault(size_t page_index, VirtualAddress vaddr)
+{
+    VERIFY_INTERRUPTS_DISABLED();
+    ScopedSpinLock lock(m_lock);
+    auto& page_slot = physical_pages()[page_index];
+    bool have_committed = m_shared_committed_cow_pages && is_nonvolatile(page_index);
+    if (page_slot->ref_count() == 1) {
+        dbgln_if(PAGE_FAULT_DEBUG, "    >> It's a COW page but nobody is sharing it anymore. Remap r/w");
+        set_should_cow(page_index, false);
+        if (have_committed) {
+            if (m_shared_committed_cow_pages->return_one())
+                m_shared_committed_cow_pages = nullptr;
+        }
+        return PageFaultResponse::Continue;
+    }
+
+    RefPtr<PhysicalPage> page;
+    if (have_committed) {
+        dbgln_if(PAGE_FAULT_DEBUG, "    >> It's a committed COW page and it's time to COW!");
+        page = m_shared_committed_cow_pages->allocate_one();
+    } else {
+        dbgln_if(PAGE_FAULT_DEBUG, "    >> It's a COW page and it's time to COW!");
+        page = MM.allocate_user_physical_page(MemoryManager::ShouldZeroFill::No);
+        if (page.is_null()) {
+            dmesgln("MM: handle_cow_fault was unable to allocate a physical page");
+            return PageFaultResponse::OutOfMemory;
+        }
+    }
+
+    u8* dest_ptr = MM.quickmap_page(*page);
+    dbgln_if(PAGE_FAULT_DEBUG, "      >> COW {} <- {}", page->paddr(), page_slot->paddr());
+    {
+        SmapDisabler disabler;
+        void* fault_at;
+        if (!safe_memcpy(dest_ptr, vaddr.as_ptr(), PAGE_SIZE, fault_at)) {
+            if ((u8*)fault_at >= dest_ptr && (u8*)fault_at <= dest_ptr + PAGE_SIZE)
+                dbgln("      >> COW: error copying page {}/{} to {}/{}: failed to write to page at {}",
+                    page_slot->paddr(), vaddr, page->paddr(), VirtualAddress(dest_ptr), VirtualAddress(fault_at));
+            else if ((u8*)fault_at >= vaddr.as_ptr() && (u8*)fault_at <= vaddr.as_ptr() + PAGE_SIZE)
+                dbgln("      >> COW: error copying page {}/{} to {}/{}: failed to read from page at {}",
+                    page_slot->paddr(), vaddr, page->paddr(), VirtualAddress(dest_ptr), VirtualAddress(fault_at));
+            else
+                VERIFY_NOT_REACHED();
+        }
+    }
+    page_slot = move(page);
+    MM.unquickmap_page();
+    set_should_cow(page_index, false);
+    return PageFaultResponse::Continue;
+}
 
 }
