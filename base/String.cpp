699378ac7404e502c38c4e3126b914305ebba432
krishpranav <krisna.pranav@gmail.com>
2021-07-24 13:04:34 +0530

    base: String function

--- base/String.cpp
+++ base/String.cpp
@@ -18,14 +18,14 @@ namespace Base {
 
 bool String::operator==(const FlyString& fly_string) const
 {
-    return *this == String(fly_string.impl())
+    return *this == String(fly_string.impl());
 }
 
-bool String::operator==(const String& other) const 
+bool String::operator==(const String& other) const
 {
     if (!m_impl)
         return !other.m_impl;
-    
+
     if (!other.m_impl)
         return false;
 
@@ -79,16 +79,26 @@ bool String::copy_characters_to_buffer(char* buffer, size_t buffer_size) const
     return characters_to_copy == length();
 }
 
-String string::isolated_copy() const
+String String::isolated_copy() const
 {
     if (!m_impl)
         return {};
     if (!m_impl->length())
         return empty();
     char* buffer;
-    auto impl = StringImpl::create_uninitalize(length(), buffer)
+    auto impl = StringImpl::create_uninitialized(length(), buffer);
     memcpy(buffer, m_impl->characters(), m_impl->length());
     return String(move(*impl));
+}
+
+String String::substring(size_t start, size_t length) const
+{
+    if (!length)
+        return String::empty();
+    VERIFY(m_impl);
+    VERIFY(!Checked<size_t>::addition_would_overflow(start, length));
+    VERIFY(start + length <= m_impl->length());
+    return { characters() + start, length };
 }
 
 String String::substring(size_t start) const
@@ -406,6 +416,96 @@ String escape_html_entities(const StringView& html)
 String::String(const FlyString& string)
     : m_impl(string.impl())
 {
+}
+
+String String::to_lowercase() const
+{
+    if (!m_impl)
+        return {};
+    return m_impl->to_lowercase();
+}
+
+String String::to_uppercase() const
+{
+    if (!m_impl)
+        return {};
+    return m_impl->to_uppercase();
+}
+
+String String::to_snakecase() const
+{
+    return StringUtils::to_snakecase(*this);
+}
+
+bool operator<(const char* characters, const String& string)
+{
+    if (!characters)
+        return !string.is_null();
+
+    if (string.is_null())
+        return false;
+
+    return __builtin_strcmp(characters, string.characters()) < 0;
+}
+
+bool operator>=(const char* characters, const String& string)
+{
+    return !(characters < string);
+}
+
+bool operator>(const char* characters, const String& string)
+{
+    if (!characters)
+        return !string.is_null();
+
+    if (string.is_null())
+        return false;
+
+    return __builtin_strcmp(characters, string.characters()) > 0;
+}
+
+bool operator<=(const char* characters, const String& string)
+{
+    return !(characters > string);
+}
+
+bool String::operator==(const char* cstring) const
+{
+    if (is_null())
+        return !cstring;
+    if (!cstring)
+        return false;
+    return !__builtin_strcmp(characters(), cstring);
+}
+
+InputStream& operator>>(InputStream& stream, String& string)
+{
+    StringBuilder builder;
+
+    for (;;) {
+        char next_char;
+        stream >> next_char;
+
+        if (stream.has_any_error()) {
+            stream.set_fatal_error();
+            string = nullptr;
+            return stream;
+        }
+
+        if (next_char) {
+            builder.append(next_char);
+        } else {
+            string = builder.to_string();
+            return stream;
+        }
+    }
+}
+
+String String::vformatted(StringView fmtstr, TypeErasedFormatParams params)
+{
+    StringBuilder builder;
+    vformat(builder, fmtstr, params);
+    return builder.to_string();
 }
 
 }
