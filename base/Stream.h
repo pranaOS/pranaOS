d63b67831b7fe76f68a0994e7d7301bc6f9c0dce
krishpranav <krisna.pranav@gmail.com>
2021-07-24 12:37:41 +0530

    base: Stream headers

--- base/Stream.h
+++ base/Stream.h
@@ -7,7 +7,7 @@
 #pragma once
 
 // includes
-#include <bases/Concepts.h>
+#include <base/Concepts.h>
 #include <base/Endian.h>
 #include <base/Forward.h>
 #include <base/Optional.h>
@@ -48,8 +48,9 @@ public:
 private:
     mutable bool m_recoverable_error { false };
     mutable bool m_fatal_error { false };
-
 };
+
+}
 
 namespace Base {
 
@@ -63,6 +64,87 @@ public:
     virtual bool discard_or_error(size_t count) = 0;
 };
 
+class OutputStream : public virtual Base::Detail::Stream {
+public:
+    virtual size_t write(ReadonlyBytes) = 0;
+    virtual bool write_or_error(ReadonlyBytes) = 0;
+};
+
+class DuplexStream
+    : public InputStream
+    , public OutputStream {
+};
+
+inline InputStream& operator>>(InputStream& stream, Bytes bytes)
+{
+    stream.read_or_error(bytes);
+    return stream;
 }
+inline OutputStream& operator<<(OutputStream& stream, ReadonlyBytes bytes)
+{
+    stream.write_or_error(bytes);
+    return stream;
+}
+
+template<typename T>
+InputStream& operator>>(InputStream& stream, LittleEndian<T>& value)
+{
+    return stream >> Bytes { &value.m_value, sizeof(value.m_value) };
+}
+template<typename T>
+OutputStream& operator<<(OutputStream& stream, LittleEndian<T> value)
+{
+    return stream << ReadonlyBytes { &value.m_value, sizeof(value.m_value) };
+}
+
+template<typename T>
+InputStream& operator>>(InputStream& stream, BigEndian<T>& value)
+{
+    return stream >> Bytes { &value.m_value, sizeof(value.m_value) };
+}
+template<typename T>
+OutputStream& operator<<(OutputStream& stream, BigEndian<T> value)
+{
+    return stream << ReadonlyBytes { &value.m_value, sizeof(value.m_value) };
+}
+
+template<typename T>
+InputStream& operator>>(InputStream& stream, Optional<T>& value)
+{
+    T temporary;
+    stream >> temporary;
+    value = temporary;
+    return stream;
+}
+
+template<typename Integral>
+InputStream& operator>>(InputStream& stream, Integral& value) requires IsIntegral<Integral>
+{
+    stream.read_or_error({ &value, sizeof(value) });
+    return stream;
+}
+template<typename Integral>
+OutputStream& operator<<(OutputStream& stream, Integral value) requires IsIntegral<Integral>
+{
+    stream.write_or_error({ &value, sizeof(value) });
+    return stream;
+}
+
+#ifndef KERNEL
+
+template<typename FloatingPoint>
+InputStream& operator>>(InputStream& stream, FloatingPoint& value) requires IsFloatingPoint<FloatingPoint>
+{
+    stream.read_or_error({ &value, sizeof(value) });
+    return stream;
+}
+template<typename FloatingPoint>
+OutputStream& operator<<(OutputStream& stream, FloatingPoint value) requires IsFloatingPoint<FloatingPoint>
+{
+    stream.write_or_error({ &value, sizeof(value) });
+    return stream;
+}
+
+#endif
 
 }
