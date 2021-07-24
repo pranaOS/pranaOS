cc098900e5c0f2b2662d88e90a7b34e03b7f06eb
krishpranav <krisna.pranav@gmail.com>
2021-07-24 11:57:39 +0530

    kernel: vm AnonymousVMObject[bool, RefPtr](funcs)

--- kernel/vm/AnonymousVMObject.cpp
+++ kernel/vm/AnonymousVMObject.cpp
@@ -78,5 +78,136 @@ AnonymousVMObject::AnonymousVMObject(size_t size, AllocationStrategy strategy)
     }
 }
 
+AnonymousVMObject::AnonymousVMObject(PhysicalAddress paddr, size_t size)
+    : VMObject(size)
+    , m_volatile_ranges_cache({ 0, page_count() })
+{
+    VERIFY(paddr.page_base() == paddr);
+    for (size_t i = 0; i < page_count(); ++i)
+        physical_pages()[i] = PhysicalPage::create(paddr.offset(i * PAGE_SIZE), MayReturnToFreeList::No);
+}
+
+AnonymousVMObject::AnonymousVMObject(Span<NonnullRefPtr<PhysicalPage>> physical_pages)
+    : VMObject(physical_pages.size() * PAGE_SIZE)
+    , m_volatile_ranges_cache({ 0, page_count() })
+{
+    for (size_t i = 0; i < physical_pages.size(); ++i) {
+        m_physical_pages[i] = physical_pages[i];
+    }
+}
+
+AnonymousVMObject::AnonymousVMObject(AnonymousVMObject const& other)
+    : VMObject(other)
+    , m_volatile_ranges_cache({ 0, page_count() }) 
+    , m_volatile_ranges_cache_dirty(true)          
+    , m_purgeable_ranges()                         
+    , m_unused_committed_pages(other.m_unused_committed_pages)
+    , m_cow_map()                                                      
+    , m_shared_committed_cow_pages(other.m_shared_committed_cow_pages) 
+{
+    VERIFY(other.m_lock.is_locked());
+    m_lock.initialize();
+
+    ensure_or_reset_cow_map();
+
+    if (m_unused_committed_pages > 0) {
+        for (size_t i = 0; i < page_count(); i++) {
+            auto& phys_page = m_physical_pages[i];
+            if (phys_page && phys_page->is_lazy_committed_page()) {
+                phys_page = MM.shared_zero_page();
+                if (--m_unused_committed_pages == 0)
+                    break;
+            }
+        }
+        VERIFY(m_unused_committed_pages == 0);
+    }
+}
+
+AnonymousVMObject::~AnonymousVMObject()
+{
+    if (m_unused_committed_pages > 0)
+        MM.uncommit_user_physical_pages(m_unused_committed_pages);
+}
+
+int AnonymousVMObject::purge()
+{
+    int purged_page_count = 0;
+    ScopedSpinLock lock(m_lock);
+    for_each_volatile_range([&](auto const& range) {
+        int purged_in_range = 0;
+        auto range_end = range.base + range.count;
+        for (size_t i = range.base; i < range_end; i++) {
+            auto& phys_page = m_physical_pages[i];
+            if (phys_page && !phys_page->is_shared_zero_page()) {
+                VERIFY(!phys_page->is_lazy_committed_page());
+                ++purged_in_range;
+            }
+            phys_page = MM.shared_zero_page();
+        }
+
+        if (purged_in_range > 0) {
+            purged_page_count += purged_in_range;
+            set_was_purged(range);
+            for_each_region([&](auto& region) {
+                if (auto owner = region.get_owner()) {
+                    dmesgln("Purged {} pages from region {} owned by {} at {} - {}",
+                        purged_in_range,
+                        region.name(),
+                        *owner,
+                        region.vaddr_from_page_index(range.base),
+                        region.vaddr_from_page_index(range.base + range.count));
+                } else {
+                    dmesgln("Purged {} pages from region {} (no ownership) at {} - {}",
+                        purged_in_range,
+                        region.name(),
+                        region.vaddr_from_page_index(range.base),
+                        region.vaddr_from_page_index(range.base + range.count));
+                }
+                region.remap_vmobject_page_range(range.base, range.count);
+            });
+        }
+    });
+    return purged_page_count;
+}
+
+void AnonymousVMObject::set_was_purged(VolatilePageRange const& range)
+{
+    VERIFY(m_lock.is_locked());
+    for (auto* purgeable_ranges : m_purgeable_ranges)
+        purgeable_ranges->set_was_purged(range);
+}
+
+void AnonymousVMObject::register_purgeable_page_ranges(PurgeablePageRanges& purgeable_page_ranges)
+{
+    ScopedSpinLock lock(m_lock);
+    purgeable_page_ranges.set_vmobject(this);
+    VERIFY(!m_purgeable_ranges.contains_slow(&purgeable_page_ranges));
+    m_purgeable_ranges.append(&purgeable_page_ranges);
+}
+
+void AnonymousVMObject::unregister_purgeable_page_ranges(PurgeablePageRanges& purgeable_page_ranges)
+{
+    ScopedSpinLock lock(m_lock);
+    for (size_t i = 0; i < m_purgeable_ranges.size(); i++) {
+        if (m_purgeable_ranges[i] != &purgeable_page_ranges)
+            continue;
+        purgeable_page_ranges.set_vmobject(nullptr);
+        m_purgeable_ranges.remove(i);
+        return;
+    }
+    VERIFY_NOT_REACHED();
+}
+
+bool AnonymousVMObject::is_any_volatile() const
+{
+    ScopedSpinLock lock(m_lock);
+    for (auto& volatile_ranges : m_purgeable_ranges) {
+        ScopedSpinLock lock(volatile_ranges->m_volatile_ranges_lock);
+        if (!volatile_ranges->is_empty())
+            return true;
+    }
+    return false;
+}
+
 
 }
