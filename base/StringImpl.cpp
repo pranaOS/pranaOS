23086769fbe0246bf4f6e5be9d3869ac6ea9bbd3
krishpranav <krisna.pranav@gmail.com>
2021-07-24 16:08:07 +0530

    base: StringImpl functions

--- base/StringImpl.cpp
+++ base/StringImpl.cpp
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
 */
 
+// includes
 #include <base/CharacterTypes.h>
 #include <base/FlyString.h>
 #include <base/HashTable.h>
@@ -13,8 +14,9 @@
 #include <base/StringImpl.h>
 #include <base/kmalloc.h>
 
-
 namespace Base {
+
+static StringImpl* s_the_empty_stringimpl = nullptr;
 
 StringImpl& StringImpl::the_empty_stringimpl()
 {
@@ -78,6 +80,64 @@ RefPtr<StringImpl> StringImpl::create(const char* cstring, ShouldChomp shouldCho
         return nullptr;
 
     return create(cstring, strlen(cstring), shouldChomp);
+}
+
+RefPtr<StringImpl> StringImpl::create(ReadonlyBytes bytes, ShouldChomp shouldChomp)
+{
+    return StringImpl::create(reinterpret_cast<const char*>(bytes.data()), bytes.size(), shouldChomp);
+}
+
+RefPtr<StringImpl> StringImpl::create_lowercased(char const* cstring, size_t length)
+{
+    if (!cstring)
+        return nullptr;
+    if (!length)
+        return the_empty_stringimpl();
+    char* buffer;
+    auto impl = create_uninitialized(length, buffer);
+    for (size_t i = 0; i < length; ++i)
+        buffer[i] = (char)to_ascii_lowercase(cstring[i]);
+    return impl;
+}
+
+RefPtr<StringImpl> StringImpl::create_uppercased(char const* cstring, size_t length)
+{
+    if (!cstring)
+        return nullptr;
+    if (!length)
+        return the_empty_stringimpl();
+    char* buffer;
+    auto impl = create_uninitialized(length, buffer);
+    for (size_t i = 0; i < length; ++i)
+        buffer[i] = (char)to_ascii_uppercase(cstring[i]);
+    return impl;
+}
+
+NonnullRefPtr<StringImpl> StringImpl::to_lowercase() const
+{
+    for (size_t i = 0; i < m_length; ++i) {
+        if (is_ascii_upper_alpha(characters()[i]))
+            return create_lowercased(characters(), m_length).release_nonnull();
+    }
+    return const_cast<StringImpl&>(*this);
+}
+
+NonnullRefPtr<StringImpl> StringImpl::to_uppercase() const
+{
+    for (size_t i = 0; i < m_length; ++i) {
+        if (is_ascii_lower_alpha(characters()[i]))
+            return create_uppercased(characters(), m_length).release_nonnull();
+    }
+    return const_cast<StringImpl&>(*this);
+}
+
+void StringImpl::compute_hash() const
+{
+    if (!length())
+        m_hash = 0;
+    else
+        m_hash = string_hash(characters(), m_length);
+    m_has_hash = true;
 }
 
 }
