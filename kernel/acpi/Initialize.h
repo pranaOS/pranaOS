385f8ed935b149bad3cb119f518dfe8cc42a84ce
krishpranav <krisna.pranav@gmail.com>
2021-07-24 10:50:26 +0530

    kernel: acpi Initialize headers

---
+++ kernel/acpi/Initialize.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+namespace Kernel::ACPI {
+
+bool is_enabled();
+void initialize();
+
+}
