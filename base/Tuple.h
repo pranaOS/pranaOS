02bd69c130845be1db6b3a009cb6cfe24fb7f0c0
krishpranav <krisna.pranav@gmail.com>
2021-07-24 18:27:26 +0530

    base: Tuple headers

--- base/Tuple.h
+++ base/Tuple.h
@@ -10,7 +10,7 @@
 #include <base/StdLibExtras.h>
 #include <base/TypeList.h>
 
-namespace AK::Detail {
+namespace Base::Detail {
 
 template<typename... Ts>
 struct Tuple {
@@ -176,6 +176,47 @@ struct Tuple : Detail::Tuple<Ts...> {
 
     static constexpr auto size() { return sizeof...(Ts); }
 
+private:
+    template<unsigned... Is>
+    Tuple(Tuple&& other, IndexSequence<Is...>)
+        : Detail::Tuple<Ts...>(move(other.get<Is>())...)
+    {
+    }
+
+    template<unsigned... Is>
+    Tuple(const Tuple& other, IndexSequence<Is...>)
+        : Detail::Tuple<Ts...>(other.get<Is>()...)
+    {
+    }
+
+    template<unsigned... Is>
+    void set(Tuple&& other, IndexSequence<Is...>)
+    {
+        ((get<Is>() = move(other.get<Is>())), ...);
+    }
+
+    template<unsigned... Is>
+    void set(const Tuple& other, IndexSequence<Is...>)
+    {
+        ((get<Is>() = other.get<Is>()), ...);
+    }
+
+    template<typename F, unsigned... Is>
+    auto apply_as_args(F&& f, IndexSequence<Is...>)
+    {
+        return forward<F>(f)(get<Is>()...);
+    }
+
+    template<typename F, unsigned... Is>
+    auto apply_as_args(F&& f, IndexSequence<Is...>) const
+    {
+        return forward<F>(f)(get<Is>()...);
+    }
 };
 
+template<class... Args>
+Tuple(Args... args) -> Tuple<Args...>;
+
 }
+
+using Base::Tuple;
