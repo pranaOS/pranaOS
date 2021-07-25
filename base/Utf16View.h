29683ec418d690e029a0957607da3dc34cb637c3
krishpranav <krisna.pranav@gmail.com>
2021-07-25 12:07:33 +0530

    base: Utf16View headers

--- base/Utf16View.h
+++ base/Utf16View.h
@@ -4,7 +4,6 @@
  * SPDX-License-Identifier: BSD-2-Clause
 */
 
-
 #pragma once
 
 // includes
@@ -56,4 +55,68 @@ private:
     size_t m_remaining_code_units { 0 };
 };
 
+class Utf16View {
+public:
+    static bool is_high_surrogate(u16);
+    static bool is_low_surrogate(u16);
+    static u32 decode_surrogate_pair(u16 high_surrogate, u16 low_surrogate);
+
+    Utf16View() = default;
+    ~Utf16View() = default;
+
+    explicit Utf16View(Span<u16 const> code_units)
+        : m_code_units(code_units)
+    {
+    }
+
+    bool operator==(Utf16View const& other) const;
+
+    enum class AllowInvalidCodeUnits {
+        Yes,
+        No,
+    };
+
+    String to_utf8(AllowInvalidCodeUnits = AllowInvalidCodeUnits::No) const;
+
+    bool is_null() const { return m_code_units.is_null(); }
+    bool is_empty() const { return m_code_units.is_empty(); }
+    size_t length_in_code_units() const { return m_code_units.size(); }
+    size_t length_in_code_points() const;
+
+    Utf16CodePointIterator begin() const { return { begin_ptr(), m_code_units.size() }; }
+    Utf16CodePointIterator end() const { return { end_ptr(), 0 }; }
+
+    u16 const* data() const { return m_code_units.data(); }
+    u16 code_unit_at(size_t index) const;
+
+    size_t code_point_offset_of(size_t code_unit_offset) const;
+    size_t code_unit_offset_of(size_t code_point_offset) const;
+
+    Utf16View substring_view(size_t code_unit_offset, size_t code_unit_length) const;
+    Utf16View substring_view(size_t code_unit_offset) const { return substring_view(code_unit_offset, length_in_code_units() - code_unit_offset); }
+
+    Utf16View unicode_substring_view(size_t code_point_offset, size_t code_point_length) const;
+    Utf16View unicode_substring_view(size_t code_point_offset) const { return unicode_substring_view(code_point_offset, length_in_code_points() - code_point_offset); }
+
+    bool validate(size_t& valid_code_units) const;
+    bool validate() const
+    {
+        size_t valid_code_units;
+        return validate(valid_code_units);
+    }
+
+    bool equals_ignoring_case(Utf16View const&) const;
+
+private:
+    u16 const* begin_ptr() const { return m_code_units.data(); }
+    u16 const* end_ptr() const { return begin_ptr() + m_code_units.size(); }
+
+    size_t calculate_length_in_code_points() const;
+
+    Span<u16 const> m_code_units;
+    mutable Optional<size_t> m_length_in_code_points;
+};
+
 }
+
+using Base::Utf16View;
