6ff18c23298f796f5731fc553239bfe31bab7a10
krishpranav <krisna.pranav@gmail.com>
2021-07-25 14:02:20 +0530

    kernel: vm InodeVMObject functions

--- kernel/vm/InodeVMObject.cpp
+++ kernel/vm/InodeVMObject.cpp
@@ -37,7 +37,16 @@ size_t InodeVMObject::amount_clean() const
     for (size_t i = 0; i < page_count(); ++i) {
         if (!m_dirty_pages.get(i) && m_physical_pages[i])
             ++count;
-    
+    }
+    return count * PAGE_SIZE;
+}
+
+size_t InodeVMObject::amount_dirty() const
+{
+    size_t count = 0;
+    for (size_t i = 0; i < m_dirty_pages.size(); ++i) {
+        if (m_dirty_pages.get(i))
+            ++count;
     }
     return count * PAGE_SIZE;
 }
@@ -58,6 +67,26 @@ int InodeVMObject::release_all_clean_pages()
             region.remap();
         });
     }
+    return count;
+}
+
+u32 InodeVMObject::writable_mappings() const
+{
+    u32 count = 0;
+    const_cast<InodeVMObject&>(*this).for_each_region([&](auto& region) {
+        if (region.is_writable())
+            ++count;
+    });
+    return count;
+}
+
+u32 InodeVMObject::executable_mappings() const
+{
+    u32 count = 0;
+    const_cast<InodeVMObject&>(*this).for_each_region([&](auto& region) {
+        if (region.is_executable())
+            ++count;
+    });
     return count;
 }
 
