7fd9c9fabc6f371b9f7c65fb82a73b8585430c9d
krishpranav <krisna.pranav@gmail.com>
2021-07-25 13:58:07 +0530

    kernel: vm InodeVMObject headers

--- kernel/vm/InodeVMObject.h
+++ kernel/vm/InodeVMObject.h
@@ -6,9 +6,10 @@
 
 #pragma once
 
+// includes
 #include <base/Bitmap.h>
 #include <kernel/UnixTypes.h>
-#include <kerenl/vm/VMObject.h>
+#include <kernel/vm/VMObject.h>
 
 namespace Kernel {
 
@@ -17,14 +18,27 @@ public:
     virtual ~InodeVMObject() override;
 
     Inode& inode() { return *m_inode; }
-    Inode const& inode() { return *m_inode; }
+    Inode const& inode() const { return *m_inode; }
 
     size_t amount_dirty() const;
-    size_t amout_clear() const;
+    size_t amount_clean() const;
+
+    int release_all_clean_pages();
+
+    u32 writable_mappings() const;
+    u32 executable_mappings() const;
 
 protected:
-    explicit InodeVMObject(Inode& size_t)
+    explicit InodeVMObject(Inode&, size_t);
+    explicit InodeVMObject(InodeVMObject const&);
 
+    InodeVMObject& operator=(InodeVMObject const&) = delete;
+    InodeVMObject& operator=(InodeVMObject&&) = delete;
+    InodeVMObject(InodeVMObject&&) = delete;
+
+    virtual bool is_inode() const final { return true; }
+
+    NonnullRefPtr<Inode> m_inode;
     Bitmap m_dirty_pages;
 };
 
