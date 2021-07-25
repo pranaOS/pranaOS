4a745316441ea655081b314b0a9601cd45a1f721
krishpranav <krisna.pranav@gmail.com>
2021-07-25 11:46:32 +0530

    base: UUID headers

--- base/UUID.h
+++ base/UUID.h
@@ -21,13 +21,22 @@ public:
     UUID(const StringView&);
     ~UUID() = default;
 
+    bool operator==(const UUID&) const;
+    bool operator!=(const UUID& other) const { return !(*this == other); }
+    bool operator<=(const UUID&) const = delete;
+    bool operator>=(const UUID&) const = delete;
+    bool operator<(const UUID&) const = delete;
+    bool operator>(const UUID&) const = delete;
+
+    String to_string() const;
+    bool is_zero() const;
 
 private:
     void convert_string_view_to_uuid(const StringView&);
     void fill_buffer(ByteBuffer);
 
     Array<u8, 16> m_uuid_buffer {};
-};  
+};
 
 }
 
