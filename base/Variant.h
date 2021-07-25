6ef28533ca6fc1e1b72ae42f84aefcf54f18df0c
krishpranav <krisna.pranav@gmail.com>
2021-07-25 12:28:41 +0530

    base: Variant[template]

---
+++ base/Variant.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <base/Array.h>
+#include <base/BitCast.h>
+#include <base/StdLibExtras.h>
+#include <base/TypeList.h>
+
+namespace Base::Detail {
+
+template<typename T, typename IndexType, IndexType InitialIndex, typename... InTypes>
+struct VariantIndexOf {
+    static_assert(DependentFalse<T, IndexType, InTypes...>, "Invalid VariantIndex instantiated");
+};
+
+template<typename T, typename IndexType, IndexType InitialIndex, typename InType, typename... RestOfInTypes>
+struct VariantIndexOf<T, IndexType, InitialIndex, InType, RestOfInTypes...> {
+    consteval IndexType operator()()
+    {
+        if constexpr (IsSame<T, InType>)
+            return InitialIndex;
+        else
+            return VariantIndexOf<T, IndexType, InitialIndex + 1, RestOfInTypes...> {}();
+    }
+};
+
+template<typename T, typename IndexType, IndexType InitialIndex>
+struct VariantIndexOf<T, IndexType, InitialIndex> {
+    consteval IndexType operator()() { return InitialIndex; }
+};
+
+template<typename T, typename IndexType, typename... Ts>
+consteval IndexType index_of()
+{
+    return VariantIndexOf<T, IndexType, 0, Ts...> {}();
+}
+
+
+}
