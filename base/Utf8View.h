49c402fb95f1b97e196fdc8000c8728549aedd99
krishpranav <krisna.pranav@gmail.com>
2021-07-25 12:25:17 +0530

    base: Utf8View headers

--- base/Utf8View.h
+++ base/Utf8View.h
@@ -33,6 +33,7 @@ public:
         return m_ptr - other.m_ptr;
     }
 
+
     size_t underlying_code_point_length_in_bytes() const;
     ReadonlyBytes underlying_code_point_bytes() const;
     bool done() const { return m_length == 0; }
@@ -43,4 +44,70 @@ private:
     size_t m_length;
 };
 
+class Utf8View {
+public:
+    using Iterator = Utf8CodePointIterator;
+
+    Utf8View() = default;
+    explicit Utf8View(const String&);
+    explicit Utf8View(const StringView&);
+    explicit Utf8View(const char*);
+    ~Utf8View() = default;
+
+    const StringView& as_string() const { return m_string; }
+
+    Utf8CodePointIterator begin() const;
+    Utf8CodePointIterator end() const;
+    Utf8CodePointIterator iterator_at_byte_offset(size_t) const;
+
+    const unsigned char* bytes() const { return begin_ptr(); }
+    size_t byte_length() const { return m_string.length(); }
+    size_t byte_offset_of(const Utf8CodePointIterator&) const;
+
+    Utf8View substring_view(size_t byte_offset, size_t byte_length) const;
+    Utf8View substring_view(size_t byte_offset) const { return substring_view(byte_offset, byte_length() - byte_offset); }
+    Utf8View unicode_substring_view(size_t code_point_offset, size_t code_point_length) const;
+    Utf8View unicode_substring_view(size_t code_point_offset) const { return unicode_substring_view(code_point_offset, length() - code_point_offset); }
+
+    bool is_empty() const { return m_string.is_empty(); }
+    bool is_null() const { return m_string.is_null(); }
+    bool starts_with(const Utf8View&) const;
+    bool contains(u32) const;
+
+    Utf8View trim(const Utf8View& characters, TrimMode mode = TrimMode::Both) const;
+
+    size_t iterator_offset(const Utf8CodePointIterator& it) const
+    {
+        return byte_offset_of(it);
+    }
+
+    bool validate(size_t& valid_bytes) const;
+    bool validate() const
+    {
+        size_t valid_bytes;
+        return validate(valid_bytes);
+    }
+
+    size_t length() const
+    {
+        if (!m_have_length) {
+            m_length = calculate_length();
+            m_have_length = true;
+        }
+        return m_length;
+    }
+
+private:
+    const unsigned char* begin_ptr() const;
+    const unsigned char* end_ptr() const;
+    size_t calculate_length() const;
+
+    StringView m_string;
+    mutable size_t m_length { 0 };
+    mutable bool m_have_length { false };
+};
+
 }
+
+using Base::Utf8CodePointIterator;
+using Base::Utf8View;
