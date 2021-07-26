2579b2072a0963eb60dffb05c712fbf398c9435f
krishpranav <krisna.pranav@gmail.com>
2021-07-26 16:17:26 +0530

    kernel: vm ScatterGatherList headers

--- kernel/vm/ScatterGatherList.h
+++ kernel/vm/ScatterGatherList.h
@@ -4,15 +4,19 @@
  * SPDX-License-Identifier: BSD-2-Clause
 */
 
-#pragma once 
+/*
+ * Copyright (c) 2021, the SerenityOS developers.
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
 
-// includes
-#include <base/Vector.h>
+#pragma once
+
+#include <ak/Vector.h>
 #include <kernel/devices/BlockDevice.h>
 #include <kernel/PhysicalAddress.h>
 #include <kernel/vm/AnonymousVMObject.h>
 #include <kernel/vm/MemoryManager.h>
-
 
 namespace Kernel {
 
@@ -20,12 +24,13 @@ class ScatterGatherList : public RefCounted<ScatterGatherList> {
 public:
     static RefPtr<ScatterGatherList> try_create(AsyncBlockDeviceRequest&, Span<NonnullRefPtr<PhysicalPage>> allocated_pages, size_t device_block_size);
     const VMObject& vmobject() const { return m_vm_object; }
-    size_t scatters_count() const { return m_vm_object->phyiscal_pages().size(); }
+    VirtualAddress dma_region() const { return m_dma_region->vaddr(); }
+    size_t scatters_count() const { return m_vm_object->physical_pages().size(); }
 
 private:
-    NonnullRefPtr<AnonymousVMObject> m_vm_object
+    ScatterGatherList(NonnullRefPtr<AnonymousVMObject>, AsyncBlockDeviceRequest&, size_t device_block_size);
+    NonnullRefPtr<AnonymousVMObject> m_vm_object;
     OwnPtr<Region> m_dma_region;
-
 };
 
 }
