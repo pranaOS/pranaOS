420dc5d5bfa3d8c08fe159dedf70ee96886d08d9
krishpranav <krisna.pranav@gmail.com>
2021-07-26 18:13:26 +0530

    kernel: vm SharedInodeVMObject headers

--- kernel/vm/SharedInodeVMObject.h
+++ kernel/vm/SharedInodeVMObject.h
@@ -18,14 +18,17 @@ class SharedInodeVMObject final : public InodeVMObject {
 
 public:
     static RefPtr<SharedInodeVMObject> try_create_with_inode(Inode&);
+    virtual RefPtr<VMObject> try_clone() override;
 
 private:
     virtual bool is_shared_inode() const override { return true; }
 
     explicit SharedInodeVMObject(Inode&, size_t);
+    explicit SharedInodeVMObject(SharedInodeVMObject const&);
+
+    virtual StringView class_name() const override { return "SharedInodeVMObject"sv; }
 
     SharedInodeVMObject& operator=(SharedInodeVMObject const&) = delete;
-
 };
 
 }
