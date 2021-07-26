7dc32c7d50f612e407de53cd2e0df63c9d68082c
krishpranav <krisna.pranav@gmail.com>
2021-07-26 18:02:20 +0530

    kernel: AtomicEdgeAction headers

--- kernel/AtomicEdgeAction.h
+++ kernel/AtomicEdgeAction.h
@@ -22,19 +22,58 @@ public:
         AtomicRefCountType desired = (1 << 1) | 1;
 
         for (;;) {
-            if (m_atomic_ref_count.compare(Base::memory_order_relaxed))
+            if (m_atomic_ref_count.compare_exchange_strong(expected, desired, Base::memory_order_relaxed))
                 break;
-            
+
+            Processor::wait_check();
+
+            expected &= ~1;
+            desired = expected + (1 << 1);
             VERIFY(desired > expected);
+            if (expected == 0)
+                desired |= 1;
         }
 
-        atomic_thread_func(Base::memory_order_acquie);
+        atomic_thread_fence(Base::memory_order_acquire);
 
         if (expected == 0) {
             first_ref_action();
 
-            m_atomic_ref_count.store(desired & ~1)
-            return true
+            m_atomic_ref_count.store(desired & ~1, Base::memory_order_release);
+            return true;
+        }
+        return false;
+    }
+
+    template<typename LastRefAction>
+    bool unref(LastRefAction last_ref_action)
+    {
+        AtomicRefCountType expected = 1 << 1;
+        AtomicRefCountType desired = (1 << 1) | 1;
+        for (;;) {
+            if (m_atomic_ref_count.compare_exchange_strong(expected, desired, Base::memory_order_relaxed))
+                break;
+
+            Processor::wait_check();
+
+            expected &= ~1;
+            VERIFY(expected != 0); 
+
+            if (expected == 1 << 1) {
+                desired = (1 << 1) | 1;
+            } else {
+                desired = expected - (1 << 1);
+                VERIFY(desired < expected);
+            }
+        }
+
+        Base::atomic_thread_fence(Base::memory_order_release);
+
+        if (expected == 1 << 1) {
+            last_ref_action();
+
+            m_atomic_ref_count.store(0, Base::memory_order_release);
+            return true;
         }
         return false;
     }
