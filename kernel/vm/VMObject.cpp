f6876acee75e3e6c18a7f9305c57df1dddc66dd8
krishpranav <krisna.pranav@gmail.com>
2021-07-26 18:56:55 +0530

    kernel: vm VMObject functions

--- kernel/vm/VMObject.cpp
+++ kernel/vm/VMObject.cpp
@@ -5,29 +5,34 @@
 */
 
 // includes
-#include <kernel/VM/MemoryManager.h>
-#include <kernel/VM/VMObject.h>
-
+#include <kernel/vm/MemoryManager.h>
+#include <kernel/vm/VMObject.h>
 
 namespace Kernel {
 
-
 VMObject::VMObject(VMObject const& other)
-	: m_physical_pages(other.m_physical_pages)
+    : m_physical_pages(other.m_physical_pages)
 {
-	MM.register_vmobject(*this);
+    MM.register_vmobject(*this);
+}
+
+VMObject::VMObject(size_t size)
+    : m_physical_pages(ceil_div(size, static_cast<size_t>(PAGE_SIZE)))
+{
+    MM.register_vmobject(*this);
 }
 
 VMObject::~VMObject()
 {
-	{
-		ScopedSpinLock lock(m_on_deleted_lock);
-		for (auto& it : m_on_deleted)
-			it->vmobject_deleted(*this);
-		m_on_deleted.clear();
-	}
+    {
+        ScopedSpinLock lock(m_on_deleted_lock);
+        for (auto& it : m_on_deleted)
+            it->vmobject_deleted(*this);
+        m_on_deleted.clear();
+    }
 
-	VERIFY(m_regions.is_empty());
+    MM.unregister_vmobject(*this);
+    VERIFY(m_regions.is_empty());
 }
 
 }
