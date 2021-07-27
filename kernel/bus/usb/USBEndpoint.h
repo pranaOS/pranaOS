ff493d77c6e5197371d3ea9630b016d5f93a67c0
krishpranav <krisna.pranav@gmail.com>
2021-07-27 17:54:11 +0530

    kernel: bus usb USBEndpoint headers

--- kernel/bus/usb/USBEndpoint.h
+++ kernel/bus/usb/USBEndpoint.h
@@ -25,6 +25,22 @@ class USBEndpoint {
     static constexpr u8 ENDPOINT_ATTRIBUTES_ISO_MODE_SYNC_TYPE = 0x0c;
     static constexpr u8 ENDPOINT_ATTRIBUTES_ISO_MODE_USAGE_TYPE = 0x30;
 
+public:
+    const USBEndpointDescriptor& descriptor() const { return m_descriptor; }
+
+    bool is_control() const { return (m_descriptor.endpoint_attributes_bitmap & ENDPOINT_ATTRIBUTES_TRANSFER_TYPE_MASK) == ENDPOINT_ATTRIBUTES_TRANSFER_TYPE_CONTROL; }
+    bool is_isochronous() const { return (m_descriptor.endpoint_attributes_bitmap & ENDPOINT_ATTRIBUTES_TRANSFER_TYPE_MASK) == ENDPOINT_ATTRIBUTES_TRANSFER_TYPE_ISOCHRONOUS; }
+    bool is_bulk() const { return (m_descriptor.endpoint_attributes_bitmap & ENDPOINT_ATTRIBUTES_TRANSFER_TYPE_MASK) == ENDPOINT_ATTRIBUTES_TRANSFER_TYPE_BULK; }
+    bool is_interrupt() const { return (m_descriptor.endpoint_attributes_bitmap & ENDPOINT_ATTRIBUTES_TRANSFER_TYPE_MASK) == ENDPOINT_ATTRIBUTES_TRANSFER_TYPE_INTERRUPT; }
+
+    u16 max_packet_size() const { return m_descriptor.max_packet_size; }
+    u8 polling_interval() const { return m_descriptor.poll_interval_in_frames; }
+
+private:
+    USBEndpoint();
+    USBEndpointDescriptor m_descriptor;
+
+    USBPipe m_pipe;
 };
 
 }
