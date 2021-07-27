c2a827465baa342e61bce5f6e4ff94b1403d6aa6
krishpranav <krisna.pranav@gmail.com>
2021-07-27 14:17:38 +0530

    kernel: arch x86 PageDirectory[void, bool]

--- kernel/arch/x86/PageDirectory.h
+++ kernel/arch/x86/PageDirectory.h
@@ -56,6 +56,46 @@ public:
         else
             m_raw &= ~bit;
     }
+private:
+    u32 m_raw;
+};
+
+class PageTableEntry {
+public:
+        bool is_present() const { return raw() & Present; }
+    void set_present(bool b) { set_bit(Present, b); }
+
+    bool is_user_allowed() const { return raw() & UserSupervisor; }
+    void set_user_allowed(bool b) { set_bit(UserSupervisor, b); }
+
+    bool is_writable() const { return raw() & ReadWrite; }
+    void set_writable(bool b) { set_bit(ReadWrite, b); }
+
+    bool is_write_through() const { return raw() & WriteThrough; }
+    void set_write_through(bool b) { set_bit(WriteThrough, b); }
+
+    bool is_cache_disabled() const { return raw() & CacheDisabled; }
+    void set_cache_disabled(bool b) { set_bit(CacheDisabled, b); }
+
+    bool is_global() const { return raw() & Global; }
+    void set_global(bool b) { set_bit(Global, b); }
+
+    bool is_execute_disabled() const { return raw() & NoExecute; }
+    void set_execute_disabled(bool b) { set_bit(NoExecute, b); }
+
+    bool is_null() const { return m_raw == 0; }
+    void clear() { m_raw = 0; }
+
+    void set_bit(u64 bit, bool value)
+    {
+        if (value)
+            m_raw |= bit;
+        else
+            m_raw &= ~bit;
+    }
+
+private:
+    u64 m_raw;
 
 };
 
