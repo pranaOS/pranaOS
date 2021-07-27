be0212e00a0da742c66618eb63e38f8ec5909537
krishpranav <krisna.pranav@gmail.com>
2021-07-27 13:25:47 +0530

    kernel: arch x86 InterruptDisabler[cli]

--- kernel/arch/x86/InterruptDisabler.h
+++ kernel/arch/x86/InterruptDisabler.h
@@ -3,3 +3,33 @@
  *
  * SPDX-License-Identifier: BSD-2-Clause
 */
+
+#pragma once
+
+// includes
+#include <base/Types.h>
+#include <kernel/IO.h>
+#include <kernel/arch/x86/ASM_wrapper.h>
+
+namespace Kernel {
+
+class InterruptDisabler {
+public:
+InterruptDisabler()
+    {
+        m_flags = cpu_Flags();
+
+cli();
+    }
+
+~InterruptDisabler()
+    {
+if (m_flags & 0x200)
+sti();
+    }
+
+private:
+    u32 m_flags;
+};
+
+}
