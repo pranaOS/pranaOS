ba9eba269d67207f394d1f00bc9c42422e5dfb22
krishpranav <krisna.pranav@gmail.com>
2021-07-27 17:40:29 +0530

    kernel update

--- kernel/system/.build.mk
+++
@@ -1,8 +0,0 @@
-KERNEL_SOURCES += \
-	$(wildcard kernel/system/*.cpp) \
-	$(wildcard kernel/system/*/*.cpp) \
-	$(wildcard kernel/system/*/*/*.cpp)
-
-KERNEL_ASSEMBLY_SOURCES += \
-	$(wildcard kernel/system/*.s) \
-	$(wildcard kernel/system/*/*.s)
