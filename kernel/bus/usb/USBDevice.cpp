a7032a49321106aeebb61aa87018c19de749f581
krishpranav <krisna.pranav@gmail.com>
2021-07-27 17:50:52 +0530

    kernel: bus usb USBDevice functions

--- kernel/bus/usb/USBDevice.cpp
+++ kernel/bus/usb/USBDevice.cpp
@@ -13,7 +13,7 @@
 #include <kernel/bus/usb/USBDevice.h>
 #include <kernel/bus/usb/USBRequest.h>
 
-static u32 s_next_usb_address = 1;
+static u32 s_next_usb_address = 1; 
 
 namespace Kernel::USB {
 
@@ -32,6 +32,64 @@ KResultOr<NonnullRefPtr<Device>> Device::try_create(PortNumber port, DeviceSpeed
         return enumerate_result;
 
     return device.release_nonnull();
+}
+
+Device::Device(PortNumber port, DeviceSpeed speed, NonnullOwnPtr<Pipe> default_pipe)
+    : m_device_port(port)
+    , m_device_speed(speed)
+    , m_address(0)
+    , m_default_pipe(move(default_pipe))
+{
+}
+
+KResult Device::enumerate()
+{
+    USBDeviceDescriptor dev_descriptor {};
+
+    auto transfer_length_or_error = m_default_pipe->control_transfer(USB_DEVICE_REQUEST_DEVICE_TO_HOST, USB_REQUEST_GET_DESCRIPTOR, 0x100, 0, 8, &dev_descriptor);
+
+    if (transfer_length_or_error.is_error())
+        return transfer_length_or_error.error();
+
+    auto transfer_length = transfer_length_or_error.release_value();
+
+    VERIFY(transfer_length > 0);
+
+    VERIFY(dev_descriptor.descriptor_header.descriptor_type == DESCRIPTOR_TYPE_DEVICE);
+    m_default_pipe->set_max_packet_size(dev_descriptor.max_packet_size);
+
+    transfer_length_or_error = m_default_pipe->control_transfer(USB_DEVICE_REQUEST_DEVICE_TO_HOST, USB_REQUEST_GET_DESCRIPTOR, 0x100, 0, sizeof(USBDeviceDescriptor), &dev_descriptor);
+
+    if (transfer_length_or_error.is_error())
+        return transfer_length_or_error.error();
+
+    transfer_length = transfer_length_or_error.release_value();
+
+    VERIFY(transfer_length > 0);
+
+    VERIFY(dev_descriptor.descriptor_header.descriptor_type == DESCRIPTOR_TYPE_DEVICE);
+
+    if constexpr (USB_DEBUG) {
+        dbgln("USB Device Descriptor for {:04x}:{:04x}", dev_descriptor.vendor_id, dev_descriptor.product_id);
+        dbgln("Device Class: {:02x}", dev_descriptor.device_class);
+        dbgln("Device Sub-Class: {:02x}", dev_descriptor.device_sub_class);
+        dbgln("Device Protocol: {:02x}", dev_descriptor.device_protocol);
+        dbgln("Max Packet Size: {:02x} bytes", dev_descriptor.max_packet_size);
+        dbgln("Number of configurations: {:02x}", dev_descriptor.num_configurations);
+    }
+
+    transfer_length_or_error = m_default_pipe->control_transfer(USB_DEVICE_REQUEST_HOST_TO_DEVICE, USB_REQUEST_SET_ADDRESS, s_next_usb_address, 0, 0, nullptr);
+
+    if (transfer_length_or_error.is_error())
+        return transfer_length_or_error.error();
+
+    transfer_length = transfer_length_or_error.release_value();
+
+    VERIFY(transfer_length > 0);
+    m_address = s_next_usb_address++;
+
+    memcpy(&m_device_descriptor, &dev_descriptor, sizeof(USBDeviceDescriptor));
+    return KSuccess;
 }
 
 Device::~Device()
