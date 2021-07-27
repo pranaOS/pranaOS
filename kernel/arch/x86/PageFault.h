c21d4c45f55a056da11a6bdd41ef0807384f6ea6
krishpranav <krisna.pranav@gmail.com>
2021-07-27 14:32:18 +0530

    kernel: arch x86 PageFault headers

--- kernel/arch/x86/PageFault.h
+++ kernel/arch/x86/PageFault.h
@@ -13,8 +13,7 @@
 namespace Kernel {
 
 struct PageFaultFlags {
-    
-enum Flags {
+    enum Flags {
         NotPresent = 0x00,
         ProtectionViolation = 0x01,
         Read = 0x00,
@@ -28,18 +27,35 @@ enum Flags {
 
 class PageFault {
 public:
-PageFault(u16 code, VirtualAddress vaddr)
-    : m_code(code)
-    , m_vaddr(vaddr)
+    PageFault(u16 code, VirtualAddress vaddr)
+        : m_code(code)
+        , m_vaddr(vaddr)
     {
     }
 
-enum class Access {
-    Read = PageFaultFlags::Read,
-    }
+    enum class Type {
+        PageNotPresent = PageFaultFlags::NotPresent,
+        ProtectionViolation = PageFaultFlags::ProtectionViolation,
+    };
 
-bool is_not_present() const { return m_code; }
-bool is_supervisor() const { return m_code; }
+    enum class Access {
+        Read = PageFaultFlags::Read,
+        Write = PageFaultFlags::Write,
+    };
+
+    VirtualAddress vaddr() const { return m_vaddr; }
+    u16 code() const { return m_code; }
+
+    Type type() const { return (Type)(m_code & 1); }
+    Access access() const { return (Access)(m_code & 2); }
+
+    bool is_not_present() const { return (m_code & 1) == PageFaultFlags::NotPresent; }
+    bool is_protection_violation() const { return (m_code & 1) == PageFaultFlags::ProtectionViolation; }
+    bool is_read() const { return (m_code & 2) == PageFaultFlags::Read; }
+    bool is_write() const { return (m_code & 2) == PageFaultFlags::Write; }
+    bool is_user() const { return (m_code & 4) == PageFaultFlags::UserMode; }
+    bool is_supervisor() const { return (m_code & 4) == PageFaultFlags::SupervisorMode; }
+    bool is_instruction_fetch() const { return (m_code & 16) == PageFaultFlags::InstructionFetch; }
 
 private:
     u16 m_code;
