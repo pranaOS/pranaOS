7d5568ecd3faea2d2266110a591b30d3b24a3d2e
krishpranav <krisna.pranav@gmail.com>
2021-07-27 13:26:40 +0530

    kernel: arch x86 InterruptDisabler headers

--- kernel/arch/x86/InterruptDisabler.h
+++ kernel/arch/x86/InterruptDisabler.h
@@ -9,27 +9,39 @@
 // includes
 #include <base/Types.h>
 #include <kernel/IO.h>
-#include <kernel/arch/x86/ASM_wrapper.h>
+#include <kernel/Arch/x86/ASM_wrapper.h>
 
 namespace Kernel {
 
 class InterruptDisabler {
 public:
-InterruptDisabler()
+    InterruptDisabler()
     {
-        m_flags = cpu_Flags();
-
-cli();
+        m_flags = cpu_flags();
+        cli();
     }
 
-~InterruptDisabler()
+    ~InterruptDisabler()
     {
-if (m_flags & 0x200)
-sti();
+        if (m_flags & 0x200)
+            sti();
     }
 
 private:
     u32 m_flags;
+};
+
+class NonMaskableInterruptDisabler {
+public:
+    NonMaskableInterruptDisabler()
+    {
+        IO::out8(0x70, IO::in8(0x70) | 0x80);
+    }
+
+    ~NonMaskableInterruptDisabler()
+    {
+        IO::out8(0x70, IO::in8(0x70) & 0x7F);
+    }
 };
 
 }
