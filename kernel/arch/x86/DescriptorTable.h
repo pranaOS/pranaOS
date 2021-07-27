d598ddf4db8e0b569964840465d530fa0a5b48f4
krishpranav <krisna.pranav@gmail.com>
2021-07-27 13:04:38 +0530

    kernel: arch x86 DescriptorTable headers

--- kernel/arch/x86/DescriptorTable.h
+++ kernel/arch/x86/DescriptorTable.h
@@ -6,6 +6,7 @@
 
 #pragma once
 
+// includes
 #include <base/Types.h>
 #include <kernel/VirtualAddress.h>
 
@@ -96,3 +97,78 @@ union [[gnu::packed]] Descriptor {
         limit_hi = (length >> 16) & 0xf;
     }
 };
+
+static_assert(sizeof(Descriptor) == 8);
+
+enum class IDTEntryType {
+    TaskGate32 = 0b0101,
+    InterruptGate16 = 0b110,
+    TrapGate16 = 0b111,
+    InterruptGate32 = 0b1110,
+    TrapGate32 = 0b1111,
+};
+
+struct [[gnu::packed]] IDTEntry
+{
+    u16 offset_1; 
+    u16 selector; 
+
+#if ARCH(I386)
+    u8 zero; 
+#else
+    struct {
+        u8 interrupt_stack_table : 3;
+        u8 zero : 5;
+    };
+#endif
+    struct {
+        u8 gate_type : 4;
+        u8 storage_segment : 1;
+        u8 descriptor_privilege_level : 2;
+        u8 present : 1;
+    } type_attr;  
+    u16 offset_2; 
+#if !ARCH(I386)
+    u32 offset_3;
+    u32 zeros;
+#endif
+
+    IDTEntry() = default;
+    IDTEntry(FlatPtr callback, u16 selector_, IDTEntryType type, u8 storage_segment, u8 privilege_level)
+        : offset_1 { (u16)((FlatPtr)callback & 0xFFFF) }
+        , selector { selector_ }
+#if !ARCH(I386)
+        , interrupt_stack_table { 0 }
+#endif
+        , zero { 0 }
+        , type_attr {
+            .gate_type = (u8)type,
+            .storage_segment = storage_segment,
+            .descriptor_privilege_level = (u8)(privilege_level & 0b11),
+            .present = 1,
+        }
+        , offset_2 { (u16)((FlatPtr)callback >> 16) }
+#if !ARCH(I386)
+        , offset_3 { (u32)(((FlatPtr)callback) >> 32) }
+        , zeros { 0 }
+#endif
+    {
+    }
+
+    FlatPtr off()
+    {
+#if ARCH(I386)
+        return (u32)offset_2 << 16 & (u32)offset_1;
+#else
+        return (u64)offset_3 << 32 & (u64)offset_2 << 16 & (u64)offset_1;
+#endif
+    }
+    IDTEntryType type()
+    {
+        return IDTEntryType(type_attr.gate_type);
+    }
+};
+
+static_assert(sizeof(IDTEntry) == 2 * sizeof(void*));
+
+}
