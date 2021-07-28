4ceb91b4f896ce5f1c3707fe09849f6828425224
krishpranav <krisna.pranav@gmail.com>
2021-07-28 19:25:03 +0530

    kernel: devices PCISerialDevice headers

--- kernel/devices/PCISerialDevice.h
+++ kernel/devices/PCISerialDevice.h
@@ -32,4 +32,12 @@ private:
         SerialDevice::Baud baud_rate { SerialDevice::Baud::Baud38400 };
     };
 
+    static constexpr BoardDefinition board_definitions[4] = {
+        { { PCI::VendorID::WCH, 0x3253 }, "WCH CH382 2S", 2, 0, 0xC0, 8, SerialDevice::Baud::Baud115200 },
+        { { PCI::VendorID::RedHat, 0x0002 }, "QEMU PCI 16550A", 1, 0, 0, 8, SerialDevice::Baud::Baud115200 },
+        { { PCI::VendorID::RedHat, 0x0003 }, "QEMU PCI Dual-port 16550A", 2, 0, 0, 8, SerialDevice::Baud::Baud115200 },
+        { { PCI::VendorID::RedHat, 0x0004 }, "QEMU PCI Quad-port 16550A", 4, 0, 0, 8, SerialDevice::Baud::Baud115200 }
+    };
+};
+
 }
