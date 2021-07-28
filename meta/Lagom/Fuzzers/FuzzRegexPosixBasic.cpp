6004dcf310e6d25354591f763bcfd091a846eb16
krishpranav <krisna.pranav@gmail.com>
2021-07-28 12:00:48 +0530

    meta: Lagom Fuzzers

---
+++ meta/Lagom/Fuzzers/FuzzRegexPosixBasic.cpp
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <base/StringView.h>
+#include <libregex/Regex.h>
+#include <stddef.h>
+#include <stdint.h>
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    auto pattern = StringView(static_cast<const unsigned char*>(data), size);
+    [[maybe_unused]] auto re = Regex<PosixBasic>(pattern);
+    return 0;
+}
