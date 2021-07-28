3ef983aa34c6a36c7b3553b1eee051e1204cf97f
krishpranav <krisna.pranav@gmail.com>
2021-07-28 18:02:17 +0530

    kernel: devices CharacterDevice functions

---
+++ kernel/devices/CharacterDevice.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <kernel/devices/CharacterDevice.h>
+
+namespace Kernel {
+
+CharacterDevice::~CharacterDevice()
+{
+}
+
+}
