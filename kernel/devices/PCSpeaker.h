b5bb2bfffb453b2c7fec36dbd3d58d2bd6337d42
krishpranav <krisna.pranav@gmail.com>
2021-07-28 19:28:28 +0530

    kernel: devices PCSpeaker[private]

---
+++ kernel/devices/PCSpeaker.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+class PCSpeaker {
+private:
+    static void tone_on(int frequency);
+}
