587448cee197f444c66ee3bded4cc112ad0670ca
krishpranav <krisna.pranav@gmail.com>
2021-07-28 14:24:31 +0530

    kernel: bus pci Device headers

--- kernel/bus/pci/Device.h
+++ kernel/bus/pci/Device.h
@@ -14,14 +14,14 @@
 namespace Kernel {
 class PCI::Device : public IRQHandler {
 public:
-    Address pci_address() const { return m_pci_address; }
+    Address pci_address() const { return m_pci_address; };
 
 protected:
     Device(Address pci_address);
+    Device(Address pci_address, u8 interrupt_vector);
     ~Device();
 
 private:
     Address m_pci_address;
 };
-
 }
