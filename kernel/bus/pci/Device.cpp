ebb5beeaa3200b7f07e3bec45cc334ff7e46765d
krishpranav <krisna.pranav@gmail.com>
2021-07-28 14:25:39 +0530

    kernel: bus pci Device functions

---
+++ kernel/bus/pci/Device.cpp
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <kernel/bus/pci/Device.h>
+
+namespace Kernel {
+namespace PCI {
+
+Device::Device(Address address)
+    : IRQHandler(get_interrupt_line(address))
+    , m_pci_address(address)
+{
+}
+
+Device::Device(Address address, u8 interrupt_vector)
+    : IRQHandler(interrupt_vector)
+    , m_pci_address(address)
+{
+}
+
+Device::~Device()
+{
+}
+
+}
+}
