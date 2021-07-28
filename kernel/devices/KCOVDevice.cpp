1bb1efb8990cd47979e3fcf66d6d87931fc36515
krishpranav <krisna.pranav@gmail.com>
2021-07-28 18:53:49 +0530

    kerenl: devices KCOVDevice functions

--- kernel/devices/KCOVDevice.cpp
+++ kernel/devices/KCOVDevice.cpp
@@ -10,8 +10,8 @@
 #include <kernel/devices/KCOVDevice.h>
 #include <kernel/devices/KCOVInstance.h>
 #include <kernel/filesystem/FileDescription.h>
-#include <libc/sys/ioctl_numbers.h>
 #include <kernel/Panic.h>
+#include <libc/sys/ioctl_numbers.h>
 
 namespace Kernel {
 
@@ -60,4 +60,93 @@ void KCOVDevice::free_process()
     kcov_instance->state = KCOVInstance::UNUSED;
     proc_instance->remove(pid);
     delete kcov_instance;
+}
+
+KResultOr<NonnullRefPtr<FileDescription>> KCOVDevice::open(int options)
+{
+    auto process = Process::current();
+    auto pid = process->pid();
+    if (proc_instance->get(pid).has_value())
+        return EBUSY; 
+    auto kcov_instance = new KCOVInstance(pid);
+    kcov_instance->state = KCOVInstance::OPENED;
+    proc_instance->set(pid, kcov_instance);
+
+    return File::open(options);
+}
+
+KResult KCOVDevice::ioctl(FileDescription&, unsigned request, Userspace<void*> arg)
+{
+    KResult return_value = KSuccess;
+    auto thread = Thread::current();
+    auto tid = thread->tid();
+    auto pid = thread->pid();
+    auto maybe_kcov_instance = proc_instance->get(pid);
+    if (!maybe_kcov_instance.has_value())
+        return ENXIO; 
+    auto kcov_instance = maybe_kcov_instance.value();
+
+    ScopedSpinLock lock(kcov_instance->lock);
+    switch (request) {
+    case KCOV_SETBUFSIZE: {
+        if (kcov_instance->state >= KCOVInstance::TRACING) {
+            return_value = EBUSY;
+            break;
+        }
+        return_value = kcov_instance->buffer_allocate((FlatPtr)arg.unsafe_userspace_ptr());
+        break;
+    }
+    case KCOV_ENABLE: {
+        if (kcov_instance->state >= KCOVInstance::TRACING) {
+            return_value = EBUSY;
+            break;
+        }
+        if (!kcov_instance->has_buffer()) {
+            return_value = ENOBUFS;
+            break;
+        }
+        VERIFY(kcov_instance->state == KCOVInstance::OPENED);
+        kcov_instance->state = KCOVInstance::TRACING;
+        thread_instance->set(tid, kcov_instance);
+        break;
+    }
+    case KCOV_DISABLE: {
+        auto maybe_kcov_instance = thread_instance->get(tid);
+        if (!maybe_kcov_instance.has_value()) {
+            return_value = ENOENT;
+            break;
+        }
+        VERIFY(kcov_instance->state == KCOVInstance::TRACING);
+        kcov_instance->state = KCOVInstance::OPENED;
+        thread_instance->remove(tid);
+        break;
+    }
+    default: {
+        return_value = EINVAL;
+    }
+    };
+
+    return return_value;
+}
+
+KResultOr<Region*> KCOVDevice::mmap(Process& process, FileDescription&, const Range& range, u64 offset, int prot, bool shared)
+{
+    auto pid = process.pid();
+    auto maybe_kcov_instance = proc_instance->get(pid);
+    VERIFY(maybe_kcov_instance.has_value());
+    auto kcov_instance = maybe_kcov_instance.value();
+
+    if (!kcov_instance->vmobject) {
+        return ENOBUFS; 
+    }
+
+    return process.space().allocate_region_with_vmobject(
+        range, *kcov_instance->vmobject, offset, {}, prot, shared);
+}
+
+String KCOVDevice::device_name() const
+{
+    return "kcov"sv;
+}
+
 }
