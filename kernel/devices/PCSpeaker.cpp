1a555bd0d021e4bbb2f120e557e37f2af3819a9a
krishpranav <krisna.pranav@gmail.com>
2021-07-28 19:30:35 +0530

    kernel: devices PCSpeaker functions

--- kernel/devices/PCSpeaker.cpp
+++ kernel/devices/PCSpeaker.cpp
@@ -9,9 +9,18 @@
 #include <kernel/IO.h>
 #include <kernel/time/PIT.h>
 
+void PCSpeaker::tone_on(int frequency)
+{
+    IO::out8(PIT_CTL, TIMER2_SELECT | WRITE_WORD | MODE_SQUARE_WAVE);
+    u16 timer_reload = BASE_FREQUENCY / frequency;
 
-void PCSpeaker::tone_on()
+    IO::out8(TIMER2_CTL, LSB(timer_reload));
+    IO::out8(TIMER2_CTL, MSB(timer_reload));
+
+    IO::out8(0x61, IO::in8(0x61) | 3);
+}
+
+void PCSpeaker::tone_off()
 {
     IO::out8(0x61, IO::in8(0x61) & ~3);
-
 }
