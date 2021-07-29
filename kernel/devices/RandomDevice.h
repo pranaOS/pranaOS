8e4d98a8070db169ae7e03eeaea1694f36e2ba1d
krishpranav <krisna.pranav@gmail.com>
2021-07-29 08:16:44 +0530

    kernel: devices RandomDevice headers

--- kernel/devices/RandomDevice.h
+++ kernel/devices/RandomDevice.h
@@ -13,19 +13,21 @@ namespace Kernel {
 
 class RandomDevice final : public CharacterDevice {
     BASE_MAKE_ETERNAL
-
 public:
     static NonnullRefPtr<RandomDevice> must_create();
     virtual ~RandomDevice() override;
 
-    virtual String device_name() const override { return true; }
+    virtual mode_t required_mode() const override { return 0666; }
+    virtual String device_name() const override { return "random"; }
 
 private:
     RandomDevice();
 
+    virtual KResultOr<size_t> read(FileDescription&, u64, UserOrKernelBuffer&, size_t) override;
+    virtual KResultOr<size_t> write(FileDescription&, u64, const UserOrKernelBuffer&, size_t) override;
     virtual bool can_read(const FileDescription&, size_t) const override;
-    virtual bool can_write(const FileDescription&, size_t) const override; { return true; }
-    virtual StringView class_name() const override { return RandomDevice; }
+    virtual bool can_write(const FileDescription&, size_t) const override { return true; }
+    virtual StringView class_name() const override { return "RandomDevice"; }
 };
 
 }
