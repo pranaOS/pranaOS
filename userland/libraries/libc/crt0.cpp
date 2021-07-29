4077a267778dd37cfe2f102bccb85a9d4e3fa442
krishpranav <krisna.pranav@gmail.com>
2021-07-29 13:21:16 +0530

    userland: libraries libc crt0 functions

--- userland/libraries/libc/crt0.cpp
+++ userland/libraries/libc/crt0.cpp
@@ -8,6 +8,7 @@
 #include <base/Types.h>
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/internals.h>
 #include <unistd.h>
 
@@ -19,24 +20,35 @@ extern size_t __stack_chk_guard;
 int main(int, char**, char**);
 
 int _entry(int argc, char** argv, char** env);
-void _start(int, char**, char**)
+void _start(int, char**, char**);
+
+NAKED void _start(int, char**, char**)
+{
+    asm(
+        "push $0\n"
+        "jmp _entry@plt\n");
+}
 
 int _entry(int argc, char** argv, char** env)
 {
     size_t original_stack_chk = __stack_chk_guard;
+    arc4random_buf(&__stack_chk_guard, sizeof(__stack_chk_guard));
 
     if (__stack_chk_guard == 0)
         __stack_chk_guard = original_stack_chk;
-    
+
     environ = env;
+    __environ_is_malloced = false;
 
     _init();
-    int status = main(argc, argv, environ)
-    
-    exit(status);
-    
-    return original_stack_chk;
-}
 
+    int status = main(argc, argv, environ);
+
+    exit(status);
+
+    __stack_chk_guard = original_stack_chk;
+
+    return 20150614;
+}
 }
 #endif
