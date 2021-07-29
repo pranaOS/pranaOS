522587d9521b91e3388ce57bc6f65a21be1aca9f
krishpranav <krisna.pranav@gmail.com>
2021-07-29 15:42:12 +0530

    kernel: devices hid KeyboardDevice headers

--- kernel/devices/hid/KeyboardDevice.h
+++ kernel/devices/hid/KeyboardDevice.h
@@ -24,19 +24,15 @@ public:
 
     virtual ~KeyboardDevice() override;
 
-    // ^CharacterDevice
     virtual KResultOr<size_t> read(FileDescription&, u64, UserOrKernelBuffer&, size_t) override;
     virtual bool can_read(const FileDescription&, size_t) const override;
     virtual KResultOr<size_t> write(FileDescription&, u64, const UserOrKernelBuffer&, size_t) override;
     virtual bool can_write(const FileDescription&, size_t) const override { return true; }
 
-    // ^HIDDevice
     virtual Type instrument_type() const override { return Type::Keyboard; }
 
-    // ^Device
     virtual mode_t required_mode() const override { return 0440; }
 
-    // ^File
     virtual KResult ioctl(FileDescription&, unsigned request, Userspace<void*> arg) override;
 
     virtual String device_name() const override { return String::formatted("keyboard{}", minor()); }
@@ -53,7 +49,6 @@ protected:
     KeyboardDevice();
     mutable SpinLock<u8> m_queue_lock;
     CircularQueue<Event, 16> m_queue;
-    // ^CharacterDevice
     virtual StringView class_name() const override { return "KeyboardDevice"; }
 
     u8 m_modifiers { 0 };
