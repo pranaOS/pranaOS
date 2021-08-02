1ea431a011c2ca457f710e7573b2891ec2f1328e
krishpranav <krisna.pranav@gmail.com>
2021-08-02 14:06:56 +0530

    userland: some libraries deleted

--- userland/libraries/libsystem/.build.mk
+++
@@ -1,7 +0,0 @@
-LIBS += SYSTEM
-
-SYSTEM_NAME = system
-SYSTEM_CXXFLAGS = \
-	-fno-tree-loop-distribute-patterns \
-	-fno-rtti \
-	-fno-exceptions
--- userland/libraries/libutils/.build.mk
+++
@@ -1,4 +0,0 @@
-LIBS += UTILS
-
-UTILS_NAME = utils
-UTILS_CXXFLAGS = -fno-tree-loop-distribute-patterns
