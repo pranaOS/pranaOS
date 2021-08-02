70146d1a477c3b90e4c272d72fed2656589269df
krishpranav <krisna.pranav@gmail.com>
2021-08-02 12:02:29 +0530

    sysroot: Spreadsheet[runtime(fucntion randrange, r)]

--- sysroot/res/js/Spreadsheet/runtime.js
+++ sysroot/res/js/Spreadsheet/runtime.js
@@ -76,3 +76,61 @@ class Position {
         return `<Cell at ${this.name}>`;
     }
 }
+
+function range(start, end, columnStep, rowStep) {
+    columnStep = integer(columnStep ?? 1);
+    rowStep = integer(rowStep ?? 1);
+    if (!(start instanceof Position)) {
+        start = parse_cell_name(start) ?? { column: "A", row: 0 };
+    }
+    if (!(end instanceof Position)) {
+        end = parse_cell_name(end) ?? start;
+    }
+
+    const cells = [];
+
+    const start_column_index = column_index(start.column);
+    const end_column_index = column_index(end.column);
+    const start_column = start_column_index > end_column_index ? end.column : start.column;
+    const distance = Math.abs(start_column_index - end_column_index);
+
+    for (let col = 0; col <= distance; col += columnStep) {
+        const column = column_arithmetic(start_column, col);
+        for (
+            let row = Math.min(start.row, end.row);
+            row <= Math.max(start.row, end.row);
+            row += rowStep
+        ) {
+            cells.push(column + row);
+        }
+    }
+    return cells;
+
+}
+
+function split(str, sep) {
+    const parts = [];
+    let splitIndex = -1;
+    for (;;) {
+        splitIndex = str.indexOf(sep);
+        if (splitIndex == -1) {
+            if (str.length) parts.push(str);
+            break;
+        }
+
+        parts.push(str.substring(0, splitIndex));
+        str = str.slice(splitIndex + sep.length);
+    }
+    return parts;
+}
+
+function R(fmt, ...args) {
+    if (args.length !== 0) throw new TypeError("R`` format must literal");
+
+    fmt = fmt[0];
+    return range(...split(fmt, ":"));
+}
+
+function randRange(min, max) {
+    return Math.random() * (max - min) + min;
+}
