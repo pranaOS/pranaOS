5e0de330488813554b577ce7689a43c3e14d9477
krishpranav <krisna.pranav@gmail.com>
2021-08-02 13:44:22 +0530

    kernel: graphics FramebufferDevice headers

--- kernel/graphics/FramebufferDevice.h
+++ kernel/graphics/FramebufferDevice.h
@@ -16,7 +16,6 @@
 #include <kernel/SpinLock.h>
 #include <kernel/vm/AnonymousVMObject.h>
 
-
 namespace Kernel {
 
 class FramebufferDevice : public BlockDevice {
@@ -37,5 +36,37 @@ public:
     virtual ~FramebufferDevice() {};
     void initialize();
 
+private:
+    virtual StringView class_name() const override { return "FramebufferDevice"; }
+
+    virtual bool can_read(const FileDescription&, size_t) const override final { return true; }
+    virtual bool can_write(const FileDescription&, size_t) const override final { return true; }
+    virtual void start_request(AsyncBlockDeviceRequest& request) override final { request.complete(AsyncDeviceRequest::Failure); }
+    virtual KResultOr<size_t> read(FileDescription&, u64, UserOrKernelBuffer&, size_t) override { return -EINVAL; }
+    virtual KResultOr<size_t> write(FileDescription&, u64, const UserOrKernelBuffer&, size_t) override { return -EINVAL; }
+
+    FramebufferDevice(const GraphicsDevice&, size_t, PhysicalAddress, size_t, size_t, size_t);
+
+    PhysicalAddress m_framebuffer_address;
+    size_t m_framebuffer_pitch { 0 };
+    size_t m_framebuffer_width { 0 };
+    size_t m_framebuffer_height { 0 };
+
+    SpinLock<u8> m_activation_lock;
+
+    RefPtr<AnonymousVMObject> m_real_framebuffer_vmobject;
+    RefPtr<AnonymousVMObject> m_swapped_framebuffer_vmobject;
+    OwnPtr<Region> m_real_framebuffer_region;
+    OwnPtr<Region> m_swapped_framebuffer_region;
+
+    bool m_graphical_writes_enabled { true };
+
+    RefPtr<AnonymousVMObject> m_userspace_real_framebuffer_vmobject;
+    Region* m_userspace_framebuffer_region { nullptr };
+
+    size_t m_y_offset { 0 };
+    size_t m_output_port_index;
+    NonnullRefPtr<GraphicsDevice> m_graphics_adapter;
+};
 
 }
