66cbaac7965b2dbf83f89fceab92a4238ebb5fc7
krishpranav <krisna.pranav@gmail.com>
2021-08-03 13:32:33 +0530

    userland: libraries libc ctype headers

--- userland/libraries/libc/ctype.h
+++ userland/libraries/libc/ctype.h
@@ -11,7 +11,6 @@
 
 __BEGIN_DECLS
 
-
 #define _U 01
 #define _L 02
 #define _N 04
@@ -106,5 +105,46 @@ static inline int __inline_toupper(int c)
         return c & ~0x20;
     return c;
 }
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int isalnum(int c);
+int isalpha(int c);
+int isascii(int c);
+int iscntrl(int c);
+int isdigit(int c);
+int isxdigit(int c);
+int isspace(int c);
+int ispunct(int c);
+int isprint(int c);
+int isgraph(int c);
+int islower(int c);
+int isupper(int c);
+int isblank(int c);
+int toascii(int c);
+int tolower(int c);
+int toupper(int c);
+
+#ifdef __cplusplus
+}
+#endif
+#define isalnum __inline_isalnum
+#define isalpha __inline_isalpha
+#define isascii __inline_isascii
+#define iscntrl __inline_iscntrl
+#define isdigit __inline_isdigit
+#define isxdigit __inline_isxdigit
+#define isspace __inline_isspace
+#define ispunct __inline_ispunct
+#define isprint __inline_isprint
+#define isgraph __inline_isgraph
+#define islower __inline_islower
+#define isupper __inline_isupper
+#define isblank __inline_isblank
+#define toascii __inline_toascii
+#define tolower __inline_tolower
+#define toupper __inline_toupper
 
 __END_DECLS
