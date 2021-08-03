acd36d86619559e307795aba3d8ff5daa48ee027
krishpranav <krisna.pranav@gmail.com>
2021-08-03 18:09:54 +0530

    kernel: CMOS

---
+++ kernel/CMOS.cpp
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <kernel/CMOS.h>
+#include <kernel/IO.h>
+
+namespace CMOS {
+
+u8 read(u8 index)
+{
+    IO::out8(0x70, index);
+    return IO::in8(0x71);
+}
+
+void write(u8 index, u8 data)
+{
+    IO::out8(0x70, index);
+    IO::out8(0x71, data);
+}
+
+}
