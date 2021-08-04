14f370b711e461a1fcf0e08f7cf77cc0f0550d22
krishpranav <krisna.pranav@gmail.com>
2021-08-04 12:53:08 +0530

    kernel: storage BMIDEChannel headers

---
+++ kernel/storage/BMIDEChannel.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+// includes
+#include <base/OwnPtr.h>
+#include <kernel/storage/IDEChannel.h>
+
+namespace Kernel {
+
+class AsyncBlockDeviceRequest;
+
+struct [[gnu::packed]] PhysicalRegionDescriptor {
+    u32 offset;
+    u16 size { 0 };
+    u16 end_of_table { 0 };
+};
+
+class IDEController;
+class BMIDEChannel final : public IDEChannel {
+    friend class IDEController;
+    friend class PATADiskDevice;
+
+public:
+    static NonnullRefPtr<BMIDEChannel> create(const IDEController&, IDEChannel::IOAddressGroup, IDEChannel::ChannelType type);
+    static NonnullRefPtr<BMIDEChannel> create(const IDEController&, u8 irq, IDEChannel::IOAddressGroup, IDEChannel::ChannelType type);
+    virtual ~BMIDEChannel() override {};
+
+    virtual bool is_dma_enabled() const override { return true; };
+
+private:
+    BMIDEChannel(const IDEController&, IDEChannel::IOAddressGroup, IDEChannel::ChannelType type);
+    BMIDEChannel(const IDEController&, u8 irq, IDEChannel::IOAddressGroup, IDEChannel::ChannelType type);
+    void initialize();
+
+    void complete_current_request(AsyncDeviceRequest::RequestResult);
+
+    virtual bool handle_irq(const RegisterState&) override;
+
+    virtual void send_ata_io_command(LBAMode lba_mode, Direction direction) const override;
+    virtual void ata_read_sectors(bool, u16) override;
+    virtual void ata_write_sectors(bool, u16) override;
+
+    PhysicalRegionDescriptor& prdt() { return *reinterpret_cast<PhysicalRegionDescriptor*>(m_prdt_region->vaddr().as_ptr()); }
+    OwnPtr<Region> m_prdt_region;
+    OwnPtr<Region> m_dma_buffer_region;
+    RefPtr<PhysicalPage> m_prdt_page;
+    RefPtr<PhysicalPage> m_dma_buffer_page;
+};
+}
