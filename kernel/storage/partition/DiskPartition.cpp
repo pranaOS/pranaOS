17c4f2f9ec12288dea8a15f56910e25fec389ecd
krishpranav <krisna.pranav@gmail.com>
2021-08-04 19:03:41 +0530

    kernel: storage partition DiskPartition

---
+++ kernel/storage/partition/DiskPartition.cpp
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <kernel/Debug.h>
+#include <kernel/filesystem/FileDescription.h>
+#include <kernel/storage/Partition/DiskPartition.h>
+
+namespace Kernel {
+
+NonnullRefPtr<DiskPartition> DiskPartition::create(BlockDevice& device, unsigned minor_number, DiskPartitionMetadata metadata)
+{
+    return adopt_ref(*new DiskPartition(device, minor_number, metadata));
+}
+
+DiskPartition::DiskPartition(BlockDevice& device, unsigned minor_number, DiskPartitionMetadata metadata)
+    : BlockDevice(100, minor_number, device.block_size())
+    , m_device(device)
+    , m_metadata(metadata)
+{
+}
+
+DiskPartition::~DiskPartition()
+{
+}
+
+const DiskPartitionMetadata& DiskPartition::metadata() const
+{
+    return m_metadata;
+}
+
+void DiskPartition::start_request(AsyncBlockDeviceRequest& request)
+{
+    request.add_sub_request(m_device->make_request<AsyncBlockDeviceRequest>(request.request_type(),
+        request.block_index() + m_metadata.start_block(), request.block_count(), request.buffer(), request.buffer_size()));
+}
+
+KResultOr<size_t> DiskPartition::read(FileDescription& fd, u64 offset, UserOrKernelBuffer& outbuf, size_t len)
+{
+    unsigned adjust = m_metadata.start_block() * block_size();
+    dbgln_if(OFFD_DEBUG, "DiskPartition::read offset={}, adjust={}, len={}", fd.offset(), adjust, len);
+    return m_device->read(fd, offset + adjust, outbuf, len);
+}
+
+bool DiskPartition::can_read(const FileDescription& fd, size_t offset) const
+{
+    unsigned adjust = m_metadata.start_block() * block_size();
+    dbgln_if(OFFD_DEBUG, "DiskPartition::can_read offset={}, adjust={}", offset, adjust);
+    return m_device->can_read(fd, offset + adjust);
+}
+
+KResultOr<size_t> DiskPartition::write(FileDescription& fd, u64 offset, const UserOrKernelBuffer& inbuf, size_t len)
+{
+    unsigned adjust = m_metadata.start_block() * block_size();
+    dbgln_if(OFFD_DEBUG, "DiskPartition::write offset={}, adjust={}, len={}", offset, adjust, len);
+    return m_device->write(fd, offset + adjust, inbuf, len);
+}
+
+bool DiskPartition::can_write(const FileDescription& fd, size_t offset) const
+{
+    unsigned adjust = m_metadata.start_block() * block_size();
+    dbgln_if(OFFD_DEBUG, "DiskPartition::can_write offset={}, adjust={}", offset, adjust);
+    return m_device->can_write(fd, offset + adjust);
+}
+
+String DiskPartition::device_name() const
+{
+    size_t partition_index = minor() % 16;
+    return String::formatted("{}{}", m_device->device_name(), partition_index + 1);
+}
+
+StringView DiskPartition::class_name() const
+{
+    return "DiskPartition";
+}
+
+}
