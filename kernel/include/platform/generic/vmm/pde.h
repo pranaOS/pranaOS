c4158e99a579a9ea9a30083e9bdf7b4ad3eef917
OliviaCE <88483682+OliviaCE@users.noreply.github.com>
2021-08-05 17:52:41 +0530

    pde headers

---
+++ kernel/include/platform/generic/vmm/pde.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021, OliviaCE
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#ifdef __i386__
+#include <platform/x86/vmm/pde.h>
+#elif __arm__
+#include <platform/aarch32/vmm/pde.h>
+#endif
