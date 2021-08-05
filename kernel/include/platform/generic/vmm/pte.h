2513873f375b7ab07b48f4a07d93681a039966c7
OliviaCE <88483682+OliviaCE@users.noreply.github.com>
2021-08-05 17:53:43 +0530

    pte headers

---
+++ kernel/include/platform/generic/vmm/pte.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021, OliviaCE
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#ifdef __i386__
+#include <platform/x86/vmm/pte.h>
+#elif __arm__
+#include <platform/aarch32/vmm/pte.h>
+#endif
