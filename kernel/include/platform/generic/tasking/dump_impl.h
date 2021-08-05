8ba30b64522a3f9c787d93f004a39ce84d5546bf
krishpranav <krisna.pranav@gmail.com>
2021-08-05 17:45:45 +0530

    kernel: include platform generic tasking dump_impl headers

---
+++ kernel/include/platform/generic/tasking/dump_impl.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+
+#ifdef __i386__
+#include <platform/x86/tasking/dump_impl.h>
+#elif __arm__
+#include <platform/aarch32/tasking/dump_impl.h>
+#endif
