d4a2246052bfd8a401fe23531cf3e48345f3705f
krishpranav <krisna.pranav@gmail.com>
2021-08-05 17:45:08 +0530

    kernel: include platform generic tasking context headers

---
+++ kernel/include/platform/generic/tasking/context.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+
+#ifdef __i386__
+#include <platform/x86/tasking/context.h>
+#elif __arm__
+#include <platform/aarch32/tasking/context.h>
+#endif
