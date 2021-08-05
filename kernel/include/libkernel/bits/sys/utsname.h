63cde3ece9a1af778c4e2556690df1c93640ed5d
krishpranav <krisna.pranav@gmail.com>
2021-08-05 13:43:17 +0530

    kernel: include libkernel bits sys utsname headers

---
+++ kernel/include/libkernel/bits/sys/utsname.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#ifndef _KERNEL_LIBKERNEL_BITS_SYS_UTSNAME_H
+#define _KERNEL_LIBKERNEL_BITS_SYS_UTSNAME_H
+
+#define UTSNAME_ENTRY_LEN 65
+
+struct utsname {
+    char sysname[UTSNAME_ENTRY_LEN];
+    char nodename[UTSNAME_ENTRY_LEN];
+    char release[UTSNAME_ENTRY_LEN];
+    char version[UTSNAME_ENTRY_LEN];
+    char machine[UTSNAME_ENTRY_LEN];
+};
+typedef struct utsname utsname_t;
+
+#endif 
