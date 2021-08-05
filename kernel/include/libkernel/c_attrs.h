6c82119fb925d8de7cbb306f786345fef9628f72
krishpranav <krisna.pranav@gmail.com>
2021-08-05 11:25:23 +0530

    include: libkernel cattrs

---
+++ kernel/include/libkernel/c_attrs.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#ifndef _KERNEL_LIBKERNEL_C_ATTRS_H
+#define _KERNEL_LIBKERNEL_C_ATTRS_H
+
+#ifndef PACKED
+#define PACKED __attribute__((packed))
+#endif 
+
+#ifndef ALWAYS_INLINE
+#define ALWAYS_INLINE __attribute__((always_inline))
+#endif 
+
+#ifndef NORETURN
+#define NORETURN __attribute__((noreturn))
+#endif 
+
+#endif 
