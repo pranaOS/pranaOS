40bf49d0e7e9d47fbfec7d31f1a53e8d745ecf34
OliviaCE <88483682+OliviaCE@users.noreply.github.com>
2021-08-05 17:53:16 +0530

    pf types headers

---
+++ kernel/include/platform/generic/vmm/pf_types.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021, OliviaCE
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#ifdef __i386__
+#include <platform/x86/vmm/pf_types.h>
+#elif __arm__
+#include <platform/aarch32/vmm/pf_types.h>
+#endif
