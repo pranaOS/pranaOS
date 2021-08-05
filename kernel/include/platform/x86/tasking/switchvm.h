e0962970fd43b21bd8552e460679069b2a13d693
krishpranav <krisna.pranav@gmail.com>
2021-08-05 19:23:25 +0530

    kernel: include platforms x86 switchvm tasking headers

---
+++ kernel/include/platform/x86/tasking/switchvm.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#ifndef _KERNEL_PLATFORM_X86_TASKING_SWITCHVM_H
+#define _KERNEL_PLATFORM_X86_TASKING_SWITCHVM_H
+
+// includes
+#include <libkernel/types.h>
+#include <tasking/tasking.h>
+
+void switchuvm(thread_t* thread);
+
+#endif
