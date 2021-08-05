a66d9339b4695c620301d9c2264b30209055ba12
krishpranav <krisna.pranav@gmail.com>
2021-08-05 08:41:31 +0530

    kernel: syscalls access

---
+++ kernel/syscalls/access.cpp
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <base/StringView.h>
+#include <kernel/fileSystem/VirtualFileSystem.h>
+#include <kernel/Process.h>
+
+namespace Kernel {
+
+KResultOr<FlatPtr> Process::sys$access(Userspace<const char*> user_path, size_t path_length, int mode)
+{
+    VERIFY_PROCESS_BIG_LOCK_ACQUIRED(this);
+    REQUIRE_PROMISE(rpath);
+    auto path = get_syscall_path_argument(user_path, path_length);
+    if (path.is_error())
+        return path.error();
+    return VirtualFileSystem::the().access(path.value()->view(), mode, current_directory());
+}
+
+}
