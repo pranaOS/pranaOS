f41479d4bca86e08ffbb2de232d7a94b30d9bdb1
krishpranav <krisna.pranav@gmail.com>
2021-08-05 17:40:18 +0530

    kernel: include platform generic pm settings headers

---
+++ kernel/include/platform/generic/pmm/settings.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#ifdef __i386__
+#include <platform/x86/pmm/settings.h>
+#elif __arm__
+#include <platform/aarch32/pmm/settings.h>
+#endif
