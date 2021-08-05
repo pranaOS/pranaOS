43861cc1c75558e6dc75bcc44b7ab426aa8de25a
krishpranav <krisna.pranav@gmail.com>
2021-08-05 13:06:44 +0530

    kernel: include libkernel version headers

---
+++ kernel/include/libkernel/version.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#ifndef _KERNEL_LIBKERNEL_VERSION_H
+#define _KERNEL_LIBKERNEL_VERSION_H
+
+#define OSTYPE "pranaOS"
+#define OSRELEASE "1.0.0-dev"
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 0
+#define VERSION_REVISION 0
+#define VERSION_VARIANT "0"
+
+#ifdef __i386__
+#define MACHINE "x86"
+#elif __arm__
+#define MACHINE "arm"
+#endif
+
+#endif
