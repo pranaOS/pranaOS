2711ae0fac3dc40a729c974cee3d85e65083f324
krishpranav <krisna.pranav@gmail.com>
2021-08-05 18:11:41 +0530

    kernel: include platform x86 pmm settings headers

---
+++ kernel/include/platform/x86/pmm/settings.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#ifndef _KERNEL_PLATFORM_X86_PMM_SETTINGS_H
+#define _KERNEL_PLATFORM_X86_PMM_SETTINGS_H
+
+// includes
+#include <platform/x86/memmap.h>
+
+#define MEMORY_MAP_REGION 0xA00
+#define PMM_BLOCK_SIZE (4096)
+#define PMM_BLOCK_SIZE_KB (4)
+#define PMM_BLOCKS_PER_BYTE (8)
+
+#endif 
