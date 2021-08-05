117d8780a778f80e7938c7db2569987186737f47
krishpranav <krisna.pranav@gmail.com>
2021-08-05 13:11:12 +0530

    kernel: includes libkernel types va_list headers

---
+++ kernel/include/libkernel/_types/_va_list.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#ifndef _KERNEL_LIBKERNEL__TYPES__VA_LIST_H
+#define _KERNEL_LIBKERNEL__TYPES__VA_LIST_H
+
+typedef __builtin_va_list va_list;
+
+#endif 
