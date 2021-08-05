79f3144b3cffc18aa1d3d731f9a548bc3336a522
krishpranav <krisna.pranav@gmail.com>
2021-08-05 13:05:56 +0530

    kernel: include libkernel syscall_structs headers

---
+++ kernel/include/libkernel/syscall_structs.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#ifndef _KERNEL_LIBKERNEL_SYSCALL_STRUCTS_H
+#define _KERNEL_LIBKERNEL_SYSCALL_STRUCTS_H
+
+// includes
+#include <libkernel/bits/fcntl.h>
+#include <libkernel/bits/sys/ioctls.h>
+#include <libkernel/bits/sys/mman.h>
+#include <libkernel/bits/sys/select.h>
+#include <libkernel/bits/sys/socket.h>
+#include <libkernel/bits/sys/stat.h>
+#include <libkernel/bits/sys/utsname.h>
+#include <libkernel/bits/syscalls.h>
+#include <libkernel/bits/thread.h>
+
+#endif
