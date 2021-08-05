b4446a36a7f722f06b89205ef08c2d51a85e74a5
krishpranav <krisna.pranav@gmail.com>
2021-08-05 13:45:22 +0530

    kernel: include libkernel bits sys ioctls headers

---
+++ kernel/include/libkernel/bits/sys/ioctls.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#ifndef _KERNEL_LIBKERNEL_BITS_SYS_IOCTLS_H
+#define _KERNEL_LIBKERNEL_BITS_SYS_IOCTLS_H
+
+#define TIOCGPGRP 0x0101
+#define TIOCSPGRP 0x0102
+#define TCGETS 0x0103
+#define TCSETS 0x0104
+#define TCSETSW 0x0105
+#define TCSETSF 0x0106
+
+#define BGA_SWAP_BUFFERS 0x0101
+#define BGA_GET_HEIGHT 0x0102
+#define BGA_GET_WIDTH 0x0103
+
+#endif 
