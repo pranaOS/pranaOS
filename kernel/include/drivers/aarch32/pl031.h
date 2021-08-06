62b7c03e682ab2114b3f5a818e9f60012cb43697
krishpranav <krisna.pranav@gmail.com>
2021-08-06 11:50:24 +0530

    kernel: include drivers aarch32 pl031 headers

---
+++ kernel/include/drivers/aarch32/pl031.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#ifndef _KERNEL_DRIVERS_AARCH32_PL031_H
+#define _KERNEL_DRIVERS_AARCH32_PL031_H
+
+// includes
+#include <drivers/driver_manager.h>
+#include <platform/aarch32/interrupts.h>
+#include <platform/aarch32/target/cortex-a15/device_settings.h>
+#include <libkernel/types.h>
+#include <libkernel/mask.h>
+
+struct pl031_registers {
+    uint32_t data;
+    uint32_t match;
+    uint32_t load;
+    uint32_t control;
+};
+typedef struct pl031_registers pl031_registers_t;
+
+void pl031_install();
+uint32_t pl031_read_rtc();
+
+#endif 
