95e207c9add2ed9b00b3edbee6244fc2ef62b278
krishpranav <krisna.pranav@gmail.com>
2021-08-06 19:37:56 +0530

    kernel: include mem kmalloc headers

--- kernel/include/mem/kmalloc.h
+++ kernel/include/mem/kmalloc.h
@@ -14,9 +14,12 @@
 #define KMALLOC_SPACE_SIZE (4 * MB)
 #define KMALLOC_BLOCK_SIZE 32
 
-void kmalloc_init()
+void kmalloc_init();
 void* kmalloc(uint32_t size);
+void* kmalloc_aligned(uint32_t size, uint32_t alignment);
+void* kmalloc_page_aligned();
 void kfree(void* ptr);
-void* kfree_aligned(void* ptr);
+void kfree_aligned(void* ptr);
+void* krealloc(void* ptr, uint32_t size);
 
 #endif 
