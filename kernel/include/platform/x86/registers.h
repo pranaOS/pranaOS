d41b332c21a2e3506b33e6cde5fac07545edc0f1
krishpranav <krisna.pranav@gmail.com>
2021-08-06 10:54:17 +0530

    kernel: include platform x86 registers headers

---
+++ kernel/include/platform/x86/registers.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#ifndef _KERNEL_PLATFORM_X86_REGISTERS_H
+#define _KERNEL_PLATFORM_X86_REGISTERS_H
+
+// includes
+#include <libkernel/types.h>
+
+uint32_t read_cr2();
+uint32_t read_cr3();
+uint32_t read_esp();
+extern uint32_t read_eip();
+
+static inline uint32_t read_ebp()
+{
+    uint32_t val;
+    asm volatile("movl %%ebp,%0"
+                 : "=r"(val));
+    return val;
+}
+
+static inline uint32_t read_cr0()
+{
+    uint32_t val;
+    asm volatile("movl %%cr0, %0"
+                 : "=r"(val));
+    return val;
+}
+
+static inline void write_cr0(uint32_t val)
+{
+    asm volatile("movl %0, %%cr0"
+                 :
+                 : "r"(val));
+}
+
+#endif 
