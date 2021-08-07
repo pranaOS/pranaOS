f539870161ad1ef82c10ec86a91b6347079782b4
krishpranav <krisna.pranav@gmail.com>
2021-08-07 12:22:11 +0530

    kernel: algo dynamic array

--- kernel/kernel/algo/dynamic_array.c
+++ kernel/kernel/algo/dynamic_array.c
@@ -21,13 +21,59 @@ static inline int _dynamic_array_resize(dynamic_array_t* v, uint32_t new_capacit
     return 0;
 }
 
+static inline int _dynamic_array_grow(dynamic_array_t* v)
+{
+    if (v->capacity) {
+        if (_dynamic_array_resize(v, 2 * v->capacity, v->element_size) != 0) {
+            return -1;
+        }
+    } else {
+        v->data = kmalloc(v->element_size); 
+        if (!v->data) {
+            return -1;
+        }
+        v->capacity = 1;
+    }
+    return 0;
+}
+
+int dynamic_array_init(dynamic_array_t* v, uint32_t element_size)
+{
+    v->size = 0;
+    v->capacity = 0;
+    v->element_size = element_size;
+    return 0;
+}
+
+int dynamic_array_init_of_size(dynamic_array_t* v, uint32_t element_size, uint32_t capacity)
+{
+    v->data = kmalloc(capacity * element_size);
+    if (!v->data) {
+        return -1;
+    }
+
+    v->size = 0;
+    v->capacity = capacity;
+    v->element_size = element_size;
+
+    return 0;
+}
+
+int dynamic_array_free(dynamic_array_t* v)
+{
+    kfree(v->data);
+    v->size = 0;
+    v->capacity = 0;
+    return 0;
+}
+
 void* dynamic_array_get(dynamic_array_t* v, int index)
 {
     if (index >= v->size) {
         log_warn("!!! dynamic array: index out of range");
         return 0;
     }
-    return (void*)v->data + index * v_element_size;
+    return (void*)v->data + index * v->element_size;
 }
 
 int dynamic_array_push(dynamic_array_t* v, void* element)
@@ -37,8 +83,7 @@ int dynamic_array_push(dynamic_array_t* v, void* element)
             return -1;
         }
     }
-
-    v->size;
+    v->size++;
     void* place = dynamic_array_get(v, v->size - 1);
     memcpy(place, element, v->element_size);
     return 0;
