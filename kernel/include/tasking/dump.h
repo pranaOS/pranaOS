458d521c8f30cfa5a33e89687581a9cac9794b7d
krishpranav <krisna.pranav@gmail.com>
2021-08-07 10:51:01 +0530

    kernel: include tasking dump headers

---
+++ kernel/include/tasking/dump.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#ifndef _KERNEL_TASKING_DUMP_H
+#define _KERNEL_TASKING_DUMP_H
+
+// includes
+#include <platform/generic/tasking/trapframe.h>
+#include <tasking/proc.h>
+
+void dump_and_kill(proc_t* p);
+int dump_prepare_kernel_data();
+int dump_kernel(const char* err);
+int dump_kernel_from_tf(const char* err, trapframe_t* tf);
+
+#endif
