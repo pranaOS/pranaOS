2da21bd2c70492a058aef32f57f8ee725400837a
krishpranav <krisna.pranav@gmail.com>
2021-08-07 11:02:28 +0530

    kernel: include tasking proc headers

--- kernel/include/tasking/proc.h
+++ kernel/include/tasking/proc.h
@@ -51,6 +51,7 @@ struct proc_zone {
     dentry_t* file;
     uint32_t offset;
 };
+
 typedef struct proc_zone proc_zone_t;
 
 enum PROC_STATUS {
@@ -61,6 +62,7 @@ enum PROC_STATUS {
 };
 
 struct thread;
+
 struct proc {
     pdirectory_t* pdir;
     pid_t pid;
@@ -90,6 +92,46 @@ struct proc {
 
 typedef struct proc proc_t;
 
+uint32_t proc_alloc_pid();
+struct thread* thread_by_pid(uint32_t pid);
 
+int proc_init_storage();
+int proc_setup(proc_t* p);
+int proc_setup_with_uid(proc_t* p, uid_t uid, gid_t gid);
+int proc_setup_tty(proc_t* p, tty_entry_t* tty);
+int proc_fill_up_stack(proc_t* p, int argc, char** argv, char** env);
+int proc_free(proc_t* p);
+int proc_free_lockless(proc_t* p);
+
+struct thread* proc_alloc_thread();
+struct thread* proc_create_thread(proc_t* p);
+void proc_kill_all_threads(proc_t* p);
+void proc_kill_all_threads_except(proc_t* p, struct thread* gthread);
+
+int kthread_setup(proc_t* p);
+int kthread_setup_regs(proc_t* p, void* entry_point);
+void kthread_setup_segment_regs(proc_t* p);
+int kthread_fill_up_stack(struct thread* thread, void* data);
+int kthread_free(proc_t* p);
+
+int proc_load(proc_t* p, struct thread* main_thread, const char* path);
+int proc_copy_of(proc_t* new_proc, struct thread* from_thread);
+
+int proc_die(proc_t* p);
+int proc_block_all_threads(proc_t* p, struct blocker* blocker);
+
+int proc_chdir(proc_t* p, const char* path);
+file_descriptor_t* proc_get_free_fd(proc_t* p);
+file_descriptor_t* proc_get_fd(proc_t* p, uint32_t index);
+int proc_get_fd_id(proc_t* proc, file_descriptor_t* fd);
+
+proc_zone_t* proc_new_zone(proc_t* p, uint32_t start, uint32_t len);
+proc_zone_t* proc_extend_zone(proc_t* proc, uint32_t start, uint32_t len);
+proc_zone_t* proc_new_random_zone(proc_t* p, uint32_t len);
+proc_zone_t* proc_new_random_zone_backward(proc_t* p, uint32_t len);
+proc_zone_t* proc_find_zone(proc_t* p, uint32_t addr);
+proc_zone_t* proc_find_zone_no_proc(dynamic_array_t* zones, uint32_t addr);
+int proc_delete_zone_no_proc(dynamic_array_t*, proc_zone_t*);
+int proc_delete_zone(proc_t*, proc_zone_t*);
 
 #endif 
