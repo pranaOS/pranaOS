95c72d62ddfecb5892a4427a0d8122bf792dc0b8
krishpranav <krisna.pranav@gmail.com>
2021-08-08 11:34:51 +0530

    kernel: drivers aarch32 pl050_keyboard

--- kernel/kernel/drivers/aarch32/pl050_keyboard.c
+++ kernel/kernel/drivers/aarch32/pl050_keyboard.c
@@ -9,7 +9,7 @@
 #include <drivers/generic/keyboard.h>
 #include <libkernel/bits/errno.h>
 #include <libkernel/libkernel.h>
-#include <libkern/log.h>
+#include <libkernel/log.h>
 #include <mem/vmm/vmm.h>
 #include <mem/vmm/zoner.h>
 #include <platform/aarch32/interrupts.h>
@@ -25,7 +25,17 @@ static inline int _pl050_map_itself()
     vmm_map_page(mapped_zone.start, PL050_KEYBOARD_BASE, PAGE_READABLE | PAGE_WRITABLE | PAGE_EXECUTABLE);
     registers = (pl050_registers_t*)mapped_zone.ptr;
     return 0;
+}
 
+static void pl050_keyboard_recieve_notification(uint32_t msg, uint32_t param)
+{
+    if (msg == DM_NOTIFICATION_DEVFS_READY) {
+        if (msg == DM_NOTIFICATION_DEVFS_READY) {
+            if (generic_keyboard_create_devfs() < 0) {
+                kpanic("Can't init pl050_keyboard in /dev");
+            }
+        }
+    }
 }
 
 static void _pl050_keyboard_int_handler()
@@ -34,21 +44,55 @@ static void _pl050_keyboard_int_handler()
     generic_emit_key_set1(data);
 }
 
-static inline void _keyboard_send_cmd(uint8_t cmd)
+static inline void _keybaord_send_cmd(uint8_t cmd)
 {
     registers->data = cmd;
     while ((registers->stat) & (1 << 5)) { }
-    int tmp = regisers->data;
+    int tmp = registers->data;
     ASSERT(tmp == 0xfa);
+}
+
+void pl050_keyboard_init(device_t* dev)
+{
+#ifdef DEBUG_PL050
+    log("PL050 KBD: Turning on");
+#endif
+    registers->cr = 0x4 | 0x10;
+
+    _keybaord_send_cmd(0xF0);
+    _keybaord_send_cmd(0x01);
+
+    irq_register_handler(PL050_KEYBOARD_IRQ_LINE, 0, 0, _pl050_keyboard_int_handler, BOOT_CPU_MASK);
+    generic_keyboard_init();
+}
+
+static driver_desc_t _pl050_keyboard_driver_info()
+{
+    driver_desc_t desc = { 0 };
+    desc.type = DRIVER_INPUT_SYSTEMS_DEVICE;
+    desc.auto_start = true;
+    desc.is_device_driver = false;
+    desc.is_device_needed = false;
+    desc.is_driver_needed = false;
+    desc.functions[DRIVER_NOTIFICATION] = pl050_keyboard_recieve_notification;
+    desc.functions[DRIVER_INPUT_SYSTEMS_ADD_DEVICE] = pl050_keyboard_init;
+    desc.functions[DRIVER_INPUT_SYSTEMS_GET_LAST_KEY] = 0;
+    desc.functions[DRIVER_INPUT_SYSTEMS_DISCARD_LAST_KEY] = 0;
+    desc.pci_serve_class = 0xff;
+    desc.pci_serve_subclass = 0xff;
+    desc.pci_serve_vendor_id = 0x00;
+    desc.pci_serve_device_id = 0x00;
+    return desc;
 }
 
 void pl050_keyboard_install()
 {
     if (_pl050_map_itself()) {
-#ifndef DEBUG_PL050
-        log_error("PL050 KBD: Can't map itself!!!");
-#endif 
+#ifdef DEBUG_PL050
+        log_error("PL050 KBD: Can't map itself!");
+#endif
         return;
     }
-    driver_install(_pl050_keyboard_info(), "pl050kb");
+
+    driver_install(_pl050_keyboard_driver_info(), "pl050kb");
 }
