8e5da7ae1ab10e78eb51b381ec337210e5e3a426
krishpranav <krisna.pranav@gmail.com>
2021-08-09 17:43:25 +0530

    kernel: io tty pty_slave

--- kernel/kernel/io/tty/pty_slave.c
+++ kernel/kernel/io/tty/pty_slave.c
@@ -56,3 +56,40 @@ int pty_slave_write(dentry_t* dentry, uint8_t* buf, uint32_t start, uint32_t len
     sync_ringbuffer_write(&pts->ptm->buffer, buf, len);
     return len;
 }
+
+int pty_slave_ioctl(dentry_t* dentry, uint32_t cmd, uint32_t arg)
+{
+    return 0;
+}
+
+int pty_slave_create(int id, pty_master_entry_t* ptm)
+{
+    ASSERT(0 <= id && id < 10 && id <= PTYS_COUNT);
+
+    dentry_t* mp;
+    if (vfs_resolve_path("/dev", &mp) < 0) {
+        return 0;
+    }
+
+    if (!pty_slaves[id].inode_indx) {
+        char name[8];
+        snprintf(name, 8, "pts%d", id);
+        file_ops_t fops = { 0 };
+        fops.can_read = pty_slave_can_read;
+        fops.can_write = pty_slave_can_write;
+        fops.read = pty_slave_read;
+        fops.write = pty_slave_write;
+        fops.ioctl = pty_slave_ioctl;
+        devfs_inode_t* res = devfs_register(mp, MKDEV(136, id), name, 4, 0, &fops);
+        pty_slaves[id].inode_indx = res->index;
+        pty_slaves[id].ptm = ptm;
+        pty_slaves[id].buffer = sync_ringbuffer_create_std();
+        ASSERT(pty_slaves[id].buffer.ringbuffer.zone.start);
+        ptm->pts = &pty_slaves[id];
+    } else {
+        pty_slaves[id].buffer.ringbuffer.start = pty_slaves[id].buffer.ringbuffer.end = 0;
+    }
+
+    dentry_put(mp);
+    return 0;
+}
