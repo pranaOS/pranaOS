6198f88c0a1a23d59e6cf9f4db6adef31badda19
krishpranav <krisna.pranav@gmail.com>
2021-08-09 17:07:47 +0530

    kernel: fs procfs

--- kernel/kernel/fs/procfs/procfs.c
+++ kernel/kernel/fs/procfs/procfs.c
@@ -11,7 +11,6 @@
 #include <libkernel/libkernel.h>
 #include <libkernel/log.h>
 
-
 extern const file_ops_t procfs_root_ops;
 int procfs_read_inode(dentry_t* dentry)
 {
@@ -45,4 +44,98 @@ fsdata_t procfs_data(dentry_t* dentry)
     fsdata.sb = 0;
     fsdata.gt = 0;
     return fsdata;
+}
+
+int procfs_can_read(dentry_t* dentry, uint32_t start)
+{
+    procfs_inode_t* procfs_inode = (procfs_inode_t*)dentry->inode;
+    if (!procfs_inode->ops->can_read) {
+        return -ENOEXEC;
+    }
+    return procfs_inode->ops->can_read(dentry, start);
+}
+
+int procfs_read(dentry_t* dentry, uint8_t* buf, uint32_t start, uint32_t len)
+{
+    procfs_inode_t* procfs_inode = (procfs_inode_t*)dentry->inode;
+    if (!procfs_inode->ops->read) {
+        return -ENOEXEC;
+    }
+    return procfs_inode->ops->read(dentry, buf, start, len);
+}
+
+int procfs_getdents(dentry_t* dir, uint8_t* buf, uint32_t* offset, uint32_t len)
+{
+    procfs_inode_t* procfs_inode = (procfs_inode_t*)dir->inode;
+    if (!procfs_inode->ops->getdents) {
+        return -ENOEXEC;
+    }
+    return procfs_inode->ops->getdents(dir, buf, offset, len);
+}
+
+int procfs_lookup(dentry_t* dir, const char* name, uint32_t len, dentry_t** result)
+{
+    procfs_inode_t* procfs_inode = (procfs_inode_t*)dir->inode;
+    if (!procfs_inode->ops->lookup) {
+        return -ENOEXEC;
+    }
+    return procfs_inode->ops->lookup(dir, name, len, result);
+}
+
+driver_desc_t _procfs_driver_info()
+{
+    driver_desc_t fs_desc = { 0 };
+    fs_desc.type = DRIVER_FILE_SYSTEM;
+    fs_desc.auto_start = false;
+    fs_desc.is_device_driver = false;
+    fs_desc.is_device_needed = false;
+    fs_desc.is_driver_needed = false;
+    fs_desc.functions[DRIVER_NOTIFICATION] = NULL;
+    fs_desc.functions[DRIVER_FILE_SYSTEM_RECOGNIZE] = NULL;
+    fs_desc.functions[DRIVER_FILE_SYSTEM_PREPARE_FS] = NULL;
+    fs_desc.functions[DRIVER_FILE_SYSTEM_OPEN] = NULL; 
+    fs_desc.functions[DRIVER_FILE_SYSTEM_CAN_READ] = procfs_can_read;
+    fs_desc.functions[DRIVER_FILE_SYSTEM_READ] = procfs_read;
+    fs_desc.functions[DRIVER_FILE_SYSTEM_WRITE] = NULL;
+    fs_desc.functions[DRIVER_FILE_SYSTEM_TRUNCATE] = NULL;
+    fs_desc.functions[DRIVER_FILE_SYSTEM_MKDIR] = NULL;
+    fs_desc.functions[DRIVER_FILE_SYSTEM_EJECT_DEVICE] = NULL;
+
+    fs_desc.functions[DRIVER_FILE_SYSTEM_READ_INODE] = procfs_read_inode;
+    fs_desc.functions[DRIVER_FILE_SYSTEM_WRITE_INODE] = procfs_write_inode;
+    fs_desc.functions[DRIVER_FILE_SYSTEM_FREE_INODE] = procfs_free_inode;
+    fs_desc.functions[DRIVER_FILE_SYSTEM_GET_FSDATA] = procfs_data;
+    fs_desc.functions[DRIVER_FILE_SYSTEM_LOOKUP] = procfs_lookup;
+    fs_desc.functions[DRIVER_FILE_SYSTEM_GETDENTS] = procfs_getdents;
+
+    fs_desc.functions[DRIVER_FILE_SYSTEM_FSTAT] = NULL;
+    fs_desc.functions[DRIVER_FILE_SYSTEM_IOCTL] = NULL;
+    fs_desc.functions[DRIVER_FILE_SYSTEM_MMAP] = NULL;
+    return fs_desc;
+}
+
+void procfs_install()
+{
+    driver_install(_procfs_driver_info(), "procfs");
+}
+
+int procfs_mount()
+{
+    dentry_t* mp;
+    if (vfs_resolve_path("/proc", &mp) < 0) {
+        return -ENOENT;
+    }
+    int driver_id = vfs_get_fs_id("procfs");
+    if (driver_id < 0) {
+#ifdef PROCFS_DEBUG
+        log("Procfs: no driver is installed, exiting");
+#endif
+        return -ENOENT;
+    }
+#ifdef PROCFS_DEBUG
+    log("procfs: %x", driver_id);
+#endif
+    int err = vfs_mount(mp, new_virtual_device(DEVICE_STORAGE), driver_id);
+    dentry_put(mp);
+    return err;
 }
