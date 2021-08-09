c5efaad7c10b385f9bad60b9839eb8af34cb818d
krishpranav <krisna.pranav@gmail.com>
2021-08-09 13:10:09 +0530

    boot: x86 stage2 drivers fs_desc headers

---
+++ boot/x86/stage2/drivers/fs_desc.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#ifndef STAGE2_FS_DESC
+#define STAGE2_FS_DESC
+
+typedef struct {
+    void* read;
+} fs_desc_t;
+
+#endif 
