84dd701f1c55b02286da7575c8893bc37167730d
krishpranav <krisna.pranav@gmail.com>
2021-08-09 18:33:49 +0530

    kernel: platform aarch32 system

---
+++ kernel/kernel/platform/aarch32/system.c
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <libkernel/libkernel.h>
+#include <platform/aarch32/system.h>
+#include <tasking/tasking.h>
+
+void system_disable_interrupts()
+{
+    THIS_CPU->int_depth_counter++;
+    asm volatile("cpsid i");
+}
+
+void system_enable_interrupts()
+{
+    THIS_CPU->int_depth_counter--;
+    ASSERT(THIS_CPU->int_depth_counter >= 0);
+    if (THIS_CPU->int_depth_counter == 0) {
+        asm volatile("cpsie i");
+    }
+}
+
+void system_enable_interrupts_only_counter()
+{
+    THIS_CPU->int_depth_counter--;
+    ASSERT(THIS_CPU->int_depth_counter >= 0);
+}
