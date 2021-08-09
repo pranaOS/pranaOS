9bf6bccb122e77aed5e4c5c1b0fb266054ac388d
krishpranav <krisna.pranav@gmail.com>
2021-08-09 12:14:38 +0530

    boot: x86 stage1 utils16

---
+++ boot/x86/stage1/utils16/print.s
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+[bits 16]
+
+print_string:
+    pusha
+    mov ah, 0x0e
+print_string_cycle:
+    cmp [bx], BYTE 0
+    je print_string_end
+    mov al, [bx]
+    int 0x10
+    add bx, 1
+    jmp print_string_cycle
+print_string_end:
+    popa
+    ret
