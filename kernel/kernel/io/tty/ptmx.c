6b9399899cbb7648b414c1dde7c218f37df9b3be
krishpranav <krisna.pranav@gmail.com>
2021-08-09 17:41:54 +0530

    kernel: io tty ptmx

--- kernel/kernel/io/tty/ptmx.c
+++ kernel/kernel/io/tty/ptmx.c
@@ -13,6 +13,21 @@
 
 #define PTY_DEBUG
 
+bool ptmx_can_read(dentry_t* dentry, uint32_t start)
+{
+    return true;
+}
+
+bool ptmx_can_write(dentry_t* dentry, uint32_t start)
+{
+    return true;
+}
+
+int ptmx_read(dentry_t* dentry, uint8_t* buf, uint32_t start, uint32_t len)
+{
+    return 0;
+}
+
 int ptmx_write(dentry_t* dentry, uint8_t* buf, uint32_t start, uint32_t len)
 {
     return 0;
@@ -20,21 +35,26 @@ int ptmx_write(dentry_t* dentry, uint8_t* buf, uint32_t start, uint32_t len)
 
 int ptmx_open(dentry_t* dentry, struct file_descriptor* fd, uint32_t flags)
 {
-#ifndef PTY_DEBUG
-    log("OPENING PTMX");
-#endif  
+#ifdef PTY_DEBUG
+    log("Opening ptmx");
+#endif
     return pty_master_alloc(fd);
 }
 
 int ptmx_install()
 {
-    dentry_t* mp
+    dentry_t* mp;
     if (vfs_resolve_path("/dev", &mp) < 0) {
         return -1;
     }
 
-    files_opts_t fops = { 0 };
+    file_ops_t fops = { 0 };
     fops.open = ptmx_open;
+    fops.can_read = ptmx_can_read;
+    fops.can_write = ptmx_can_write;
+    fops.read = ptmx_read;
+    fops.write = ptmx_write;
+    devfs_inode_t* res = devfs_register(mp, MKDEV(5, 2), "ptmx", 4, 0, &fops);
     dentry_put(mp);
     return 0;
 }
