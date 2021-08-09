b6c004853402b0c30fa07d669e8ba311476f321b
krishpranav <krisna.pranav@gmail.com>
2021-08-09 17:50:35 +0530

    kernel: io tty

--- kernel/kernel/io/tty/tty.c
+++ kernel/kernel/io/tty/tty.c
@@ -70,3 +70,96 @@ int tty_read(dentry_t* dentry, uint8_t* buf, uint32_t start, uint32_t len)
     }
     return leno;
 }
+
+
+int _tty_process_esc_seq(uint8_t* buf)
+{
+    if (buf[0] != '\x1b') {
+        return 0;
+    }
+    int argv[4] = { 0, 0, 0, 0 };
+    int id = 1;
+    int argc = 0;
+
+    if (buf[id] == '[') {
+        id++;
+        for (;;) {
+            int len = 0;
+            while ('0' <= buf[id + len] && buf[id + len] <= '9') {
+                len++;
+            }
+            argv[argc++] = stoi(&buf[id], len);
+            id += len;
+            if (buf[id] != ';') {
+                break;
+            }
+            id++;
+        }
+    }
+
+    char cmd = buf[id];
+    id++;
+    switch (cmd) {
+    case 'J':
+        if (argv[0] == 2) {
+        }
+        return id;
+    case 'H':
+        if (argc == 0) {
+        }
+        if (argc == 2) {
+        }
+        return id;
+    }
+    return 0;
+}
+
+int tty_write(dentry_t* dentry, uint8_t* buf, uint32_t start, uint32_t len)
+{
+#ifdef TTY_DEBUG_TIME
+    time_t cur_time = timeman_now();
+    int secs = cur_time % 60;
+    cur_time /= 60;
+    int mins = cur_time % 60;
+    cur_time /= 60;
+    int hrs = cur_time % 24;
+    time_t ticks = timeman_get_ticks_from_last_second();
+    log_not_formatted("[%d:%d:%d.%d] ", hrs, mins, secs, ticks);
+#endif
+    for (int i = 0; i < len; i++) {
+        if (buf[i] == '\x1b') {
+            i += _tty_process_esc_seq(&buf[i]);
+        } else {
+            log_not_formatted("%c", buf[i]);
+
+        }
+    }
+
+    return len;
+}
+
+int tty_ioctl(dentry_t* dentry, uint32_t cmd, uint32_t arg)
+{
+    tty_entry_t* tty = _tty_get(dentry);
+
+    switch (cmd) {
+    case TIOCGPGRP:
+        return tty->pgid;
+    case TIOCSPGRP:
+        tty->pgid = arg;
+        return 0;
+    case TCGETS:
+        memcpy((void*)arg, (void*)&tty->termios, sizeof(termios_t));
+        return 0;
+    case TCSETS:
+    case TCSETSW:
+    case TCSETSF:
+        memcpy((void*)&tty->termios, (void*)arg, sizeof(termios_t));
+        if (cmd == TCSETSF) {
+            _tty_flush_input(tty);
+        }
+        return 0;
+    }
+
+    return -EINVAL;
+}
