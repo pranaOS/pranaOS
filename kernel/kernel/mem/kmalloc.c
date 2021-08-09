f2cce90430e32fd446fe6dbf22bff675a8a1ff30
krishpranav <krisna.pranav@gmail.com>
2021-08-09 18:10:21 +0530

    kernel: mem kmalloc

--- kernel/kernel/mem/kmalloc.c
+++ kernel/kernel/mem/kmalloc.c
@@ -49,6 +49,73 @@ static void _kmalloc_init_bitmap()
 void kmalloc_init()
 {
     lock_init(&_kmalloc_lock);
-    _kmalloc_zone = zoner_new_zone(KMALLOC_SIZE);
+    _kmalloc_zone = zoner_new_zone(KMALLOC_SPACE_SIZE);
     _kmalloc_init_bitmap();
+}
+
+void* kmalloc(uint32_t size)
+{
+    lock_acquire(&_kmalloc_lock);
+    int act_size = size + sizeof(kmalloc_header_t);
+
+    int blocks_needed = (act_size + KMALLOC_BLOCK_SIZE - 1) / KMALLOC_BLOCK_SIZE;
+
+    int start = bitmap_find_space(bitmap, blocks_needed);
+    if (start < 0) {
+        log_error("[Err] NO SPACE AT KMALLOC");
+        system_stop();
+    }
+
+    kmalloc_header_t* space = (kmalloc_header_t*)kmalloc_to_vaddr(start);
+    space->len = act_size;
+    bitmap_set_range(bitmap, start, blocks_needed);
+
+    lock_release(&_kmalloc_lock);
+    return (void*)&space[1];
+}
+
+void* kmalloc_aligned(uint32_t size, uint32_t alignment)
+{
+    void* ptr = kmalloc(size + alignment + sizeof(void*));
+    uint32_t max_addr = (uint32_t)ptr + alignment;
+    void* aligned_ptr = (void*)(max_addr - (max_addr % alignment));
+    ((void**)aligned_ptr)[-1] = ptr;
+    return aligned_ptr;
+}
+
+void* kmalloc_page_aligned()
+{
+    return kmalloc_aligned(VMM_PAGE_SIZE, VMM_PAGE_SIZE);
+}
+
+void kfree(void* ptr)
+{
+    kmalloc_header_t* sptr = (kmalloc_header_t*)ptr;
+    int blocks_to_delete = (sptr[-1].len + KMALLOC_BLOCK_SIZE - 1) / KMALLOC_BLOCK_SIZE;
+    lock_acquire(&_kmalloc_lock);
+    bitmap_unset_range(bitmap, kmalloc_to_index((uint32_t)&sptr[-1]), blocks_to_delete);
+    lock_release(&_kmalloc_lock);
+}
+
+void kfree_aligned(void* ptr)
+{
+    kfree(((void**)ptr)[-1]);
+}
+
+void* krealloc(void* ptr, uint32_t new_size)
+{
+    uint32_t old_size = ((kmalloc_header_t*)ptr)[-1].len;
+    if (old_size == new_size) {
+        return ptr;
+    }
+
+    uint8_t* new_area = kmalloc(new_size);
+    if (new_area == 0) {
+        return 0;
+    }
+
+    memcpy(new_area, ptr, new_size > old_size ? new_size : old_size);
+    kfree(ptr);
+
+    return new_area;
 }
