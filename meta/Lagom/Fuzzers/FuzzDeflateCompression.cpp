fae9fa48de86df68fa7cee9c53b2474cb1f152e0
krishpranav <krisna.pranav@gmail.com>
2021-08-14 18:30:33 +0530

    meta: Lagom

---
+++ meta/Lagom/Fuzzers/FuzzDeflateCompression.cpp
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <libcompress/Deflate.h>
+#include <stdio.h>
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    auto result = Compress::DeflateCompressor::compress_all(ReadonlyBytes { data, size });
+    return result.has_value();
+}
