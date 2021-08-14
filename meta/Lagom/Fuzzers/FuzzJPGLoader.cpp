58919746a35b0fd3ac8581fb9ee93c1f48092dcc
krishpranav <krisna.pranav@gmail.com>
2021-08-14 18:39:52 +0530

    meta: Lagom Fuzzers

---
+++ meta/Lagom/Fuzzers/FuzzJPGLoader.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <libgfx/JPGLoader.h>
+#include <stddef.h>
+#include <stdint.h>
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    Gfx::load_jpg_from_memory(data, size);
+    return 0;
+}
