80a4161ef5566503aa93c9f0ab8efc63ed636053
krishpranav <krisna.pranav@gmail.com>
2021-08-19 14:25:38 +0530

    EP: EntryPoint

---
+++ EP/IDAllocator.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+#pragma once
+
+#include <EP/Forward.h>
+#include <EP/HashTable.h>
+
+namespace EP {
+
+class IDAllocator {
+
+public:
+    IDAllocator() = default;
+    ~IDAllocator() = default;
+
+    int allocate()
+    {
+        int r = rand();
+        for (int i = 0; i < 100000; ++i) {
+            int allocated_id = r + i;
+            // Make sure we never vend ID 0, as some code may interpret that as "no ID"
+            if (allocated_id == 0)
+                ++allocated_id;
+            if (!m_allocated_ids.contains(allocated_id)) {
+                m_allocated_ids.set(allocated_id);
+                return allocated_id;
+            }
+        }
+        VERIFY_NOT_REACHED();
+    }
+
+    void deallocate(int id)
+    {
+        m_allocated_ids.remove(id);
+    }
+
+private:
+    HashTable<int> m_allocated_ids;
+};
+}
+
+using EP::IDAllocator;
