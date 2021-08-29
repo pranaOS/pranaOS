a3bc67ac9dcb9e5bff6f51277fe79faf7f044978
krishpranav <krisna.pranav@gmail.com>
2021-08-29 13:23:33 +0530

    userland: utilities cat
    remove unnecessary includes require for cat application

--- userland/utilities/cat/main.c
+++ userland/utilities/cat/main.c
@@ -1,16 +1,8 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-// #include <arpa/inet.h>
-#include <libc/arpa/inet.h>
-// #include <math.h>
-// #include <sockios.h>
-#include <libc/ether/if_ether.h>
-#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/ioctl.h>
-#include <sys/socket.h>
 #include <unistd.h>
 #include <unistd.h>
 
