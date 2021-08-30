aaf364530e4779884b4c5a4fb0159c58958fbeb5
krishpranav <krisna.pranav@gmail.com>
2021-08-30 11:08:21 +0530

    libs: libobjc include isa header

---
+++ libs/libobjc/include/libobjc/isa.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef _LIBOBJC_ISA_H
+#define _LIBOBJC_ISA_H
+
+#define ISA_MASK 0xfffffffc
+
+#endif 
