b8ef210197b5538036996422b20017ecc2ec80f1
krishpranav <krisna.pranav@gmail.com>
2021-08-30 10:52:27 +0530

    libs: libobjc include module header

---
+++ libs/libobjc/include/libobjc/module.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef _LIBOBJC_MODULE_H
+#define _LIBOBJC_MODULE_H
+
+#include <libobjc/v1/decls.h>
+
+#endif 
