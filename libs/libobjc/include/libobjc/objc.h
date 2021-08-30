820a8a7ac699f275ae8caa62990a218bbdb54016
krishpranav <krisna.pranav@gmail.com>
2021-08-30 11:20:07 +0530

    libs: libobjc include objc header

---
+++ libs/libobjc/include/libobjc/objc.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef _LIBOBJC_OBJC_H
+#define _LIBOBJC_OBJC_H
+
+// includes
+#include <libobjc/helpers.h>
+#include <libobjc/runtime.h>
+
+OBJC_EXPORT id _Nullable objc_alloc(Class _Nullable cls);
+OBJC_EXPORT id _Nullable objc_alloc_init(Class _Nullable cls);
+
+#endif 
