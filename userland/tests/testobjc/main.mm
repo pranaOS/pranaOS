90c007e4816f010ffe214d7173be1cb6ec6d9622
krishpranav <krisna.pranav@gmail.com>
2021-08-30 12:27:41 +0530

    userland: tests testobjc main

--- userland/tests/testobjc/main.mm
+++ userland/tests/testobjc/main.mm
@@ -1,1 +1,31 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
 
+// includes
+#include <libfoundation/NSObject.h>
+#include <libobjc/helpers.h>
+#include <stdio.h>
+
+
+@interface SampleClass : NSObject {
+@public
+    int last_val;
+}
+
++ (void)sampleMethod;
+- (void)sampleMethod: int(val);
+- (int)get_last;
+@end
+
+
+int main()
+{
+    id objectAlloc = [[SampleClass alloc] init];
+    [objectAlloc sampleMethod:22];
+    [SampleClass sampleMethod];
+    printf("Last called with %d", [objectAlloc get_last]);
+    return 0;
+}
