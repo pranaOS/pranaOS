2d2a2400c7d2c2feb298cf860d0c20473092c52e
krishpranav <krisna.pranav@gmail.com>
2021-08-30 10:32:50 +0530

    libs: libobjc include libfoundation NSObject

---
+++ libs/libobjc/include/libfoundation/NSObject.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef _LIBOBJC_NSOBJECT_H
+#define _LIBOBJC_NSOBJECT_H
+
+#include <libobjc/runtime.h>
+
+@interface NSObject {
+    Class isa;
+}
+
++ (id)init;
++ (id)new;
++ (id)alloc;
+
+- (id)init;
+- (Class)class;
+
+@end
+
+#endif 
