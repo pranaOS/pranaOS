b2533c25cebc99ba99e4ab3738383cb1e6449054
krishpranav <krisna.pranav@gmail.com>
2021-08-30 12:35:48 +0530

    libs: libcxxabi src aeabi runtime

--- libs/libcxxabi/src/aeabi_runtime.cpp
+++ libs/libcxxabi/src/aeabi_runtime.cpp
@@ -1,8 +1,19 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
 extern "C" {
 
 extern int __cxa_atexit(void (*)(void*), void*, void*);
 int __aeabi_atexit(void* arg, void (*func)(void*), void* dso_handle)
 {
     return __cxa_atexit(func, arg, dso_handle);
+}
+
+int __aeabi_unwind_cpp_pr0()
+{
+    return 0;
 }
 }
