c92c78c07c241f73fa68b2aef92f5be88c93223b
krishpranav <krisna.pranav@gmail.com>
2021-08-30 11:02:20 +0530

    libs: libobjc include helpers

---
+++ libs/libobjc/include/libobjc/helpers.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef _LIBOBJC_HELPERS_H
+#define _LIBOBJC_HELPERS_H
+
+#include <stdio.h>
+
+#define OBJC_EXPORT extern "C"
+
+#define OBJC_DEBUG
+
+#ifdef OBJC_DEBUG
+#define OBJC_DEBUGPRINT(...) \
+    printf(__VA_ARGS__);     \
+    fflush(stdout)
+#else
+#define OBJC_DEBUGPRINT(...) (sizeof(int))
+#endif
+
+#endif 
