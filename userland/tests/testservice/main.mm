faecd54f12ce173893c2c593425b69a3644e05ba
krishpranav <krisna.pranav@gmail.com>
2021-09-01 11:25:39 +0530

    userland: testservice

--- userland/tests/testservice/main.mm
+++ userland/tests/testservice/main.mm
@@ -2,5 +2,21 @@
 #include <libobjc/helpers.h>
 #include <stdio.h>
 
+int start_platform_service(int state);
+
 @interface Service: NSObject
+@end
+
+@implementation Service
+
+- (void) startService {
+    int state = 1;
+    int result = start_platform_service(state);
+    if (result == 1) {
+        printf("Success");
+    } else {
+        printf("Failure");
+    }
+}
+
 @end
