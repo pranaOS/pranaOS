237ad5bf8b11778d0a6609d83def9dc27703774d
krishpranav <krisna.pranav@gmail.com>
2021-09-02 12:05:01 +0530

    libs: libc arpa inet

--- libs/libc/arpa/inet.c
+++ libs/libc/arpa/inet.c
@@ -1,7 +1,20 @@
-#include <stdio.h>
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
 
-int main()
+#include "inet.h"
+#include <stdio.h>
+#include <string.h>
+
+char *inet_ntop(uint32_t src, char *dst, uint16_t len)
 {
-    printf("arpa connections");
-    return 0;
+    const char fmt[] = "%u.%u.%u.%u";
+    char tmp[sizeof "255.255.255.255"];
+    uint8_t *sp = (uint8_t *)&src;
+    if (sprintf(tmp, fmt, sp[3], sp[2], sp[1], sp[0]) >= len)
+        return NULL;
+
+    return strcpy(dst, tmp);
 }
