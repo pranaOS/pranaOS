9a107983f97ebc9d485403575434401368b38356
krishpranav <krisna.pranav@gmail.com>
2021-09-02 11:56:21 +0530

    libs: libc include inet header

--- libs/libc/include/inet.h
+++ libs/libc/include/inet.h
@@ -1,3 +1,15 @@
-#include <stdio.h>
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
 
-char *inet(uint32_t src, char *dst);
+#ifndef _LIBC_ARPA_INET_H
+#define _LIBC_ARPA_INET_H 1
+
+// includes
+#include <stdint.h>
+
+char *inet_ntop(uint32_t src, char *dst, uint16_t len);
+
+#endif
