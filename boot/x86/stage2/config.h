601ca29d6a0f01a7743698e6335d1b861ce8b863
krishpranav <krisna.pranav@gmail.com>
2021-09-04 19:33:40 +0530

    boot: x86 stage2 config header

--- boot/x86/stage2/config.h
+++ boot/x86/stage2/config.h
@@ -1,7 +1,9 @@
-#ifndef STAGE2_CONFIG
-#define STAGE2_CONFIG
+// #ifndef STAGE2_CONFIG
+// #define STAGE2_CONFIG
+
+#pragma once
 
 #define KERNEL_PATH "/boot/kernel.bin"
 #define KERNEL_BASE 0x100000
 
-#endif // STAGE2_CONFIG
+// #endif // STAGE2_CONFIG
