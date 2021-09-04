8e9047cb7969820b07c12051c5d9f05f06cebb1a
krishpranav <krisna.pranav@gmail.com>
2021-09-04 17:05:14 +0530

    Libs: getopt
    updated getopt header /libs/libc

--- libs/libc/include/getopt.h
+++ libs/libc/include/getopt.h
@@ -4,25 +4,29 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#ifndef _LIBC_GETOPT_H
-#define _LIBC_GETOPT_H 1
+#pragma once
 
-/**
- @ brief struct option
- */
+// includes
+#include <sys/cdefs.h>
 
-struct option
-{
-    const char *name;
-    int has_arg;
-    int *flag;
-    int val;
-};
-
-int getopt_long(int argc, char *const argv[], const char *optstring, const struct option *longopts, int *longindex);
+__BEGIN_DECLS
 
 #define no_argument 0
 #define required_argument 1
 #define optional_argument 2
 
-#endif
+struct option {
+    const char* name;
+    int has_arg;
+    int* flag;
+    int val;
+};
+
+extern int opterr;
+extern int optopt;
+extern int optind;
+extern int optreset;
+extern char* optarg;
+int getopt_long(int argc, char* const* argv, const char* short_options, const struct option* long_options, int* out_long_option_index);
+
+__END_DECLS
