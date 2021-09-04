fcfd10a3642b3cc115d2576d3e0b1bb498bf4644
krishpranav <krisna.pranav@gmail.com>
2021-09-04 19:34:52 +0530

    boot: x86 stage2 drivers driver_desc header

--- boot/x86/stage2/drivers/drive_desc.h
+++ boot/x86/stage2/drivers/drive_desc.h
@@ -1,5 +1,7 @@
-#ifndef STAGE2_DRIVE_DESC
-#define STAGE2_DRIVE_DESC
+// #ifndef STAGE2_DRIVE_DESC
+// #define STAGE2_DRIVE_DESC
+
+#pragma once
 
 typedef struct {
     void* init;
@@ -7,4 +9,4 @@ typedef struct {
     void* write;
 } drive_desc_t;
 
-#endif // STAGE2_DRIVE_DESC
+// #endif // STAGE2_DRIVE_DESC
