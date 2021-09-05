15828a456c979292003c419aafb4bb8558716665
krishpranav <krisna.pranav@gmail.com>
2021-09-05 18:22:34 +0530

    kernel: include platform generic vmm mapping_table header

--- kernel/include/platform/generic/vmm/mapping_table.h
+++ kernel/include/platform/generic/vmm/mapping_table.h
@@ -4,8 +4,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#ifndef _KERNEL_PLATFORM_GENERIC_VMM_MAPPING_TABLE_H
-#define _KERNEL_PLATFORM_GENERIC_VMM_MAPPING_TABLE_H
+#pragma once
 
 #include <libkern/types.h>
 
@@ -20,5 +19,3 @@ typedef struct mapping_entry mapping_entry_t;
 
 extern mapping_entry_t kernel_mapping_table[]; // Should be in kernelspace
 extern mapping_entry_t extern_mapping_table[]; // Maps after kernel tables are ready, so can be outside kernelspace
-
-#endif // _KERNEL_PLATFORM_GENERIC_VMM_MAPPING_TABLE_H
