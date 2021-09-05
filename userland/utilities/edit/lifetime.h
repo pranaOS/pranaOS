9374400d75a84d8e3a2f8b53550b708c65c47f6d
krishpranav <krisna.pranav@gmail.com>
2021-09-05 17:21:03 +0530

    userland: utilities edit lifetime header

--- userland/utilities/edit/lifetime.h
+++ userland/utilities/edit/lifetime.h
@@ -1,5 +1,4 @@
-#ifndef _USERLAND_EDIT_LIFETIME_H
-#define _USERLAND_EDIT_LIFETIME_H
+#pragma once 
 
 #include "viewer.h"
 #include <termios.h>
@@ -10,5 +9,3 @@ void enable_raw_mode();
 void restore_termios();
 void exit_app();
 void crash_app();
-
-#endif
