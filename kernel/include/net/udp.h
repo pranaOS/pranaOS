9b1b40ed32640f610a7f629818c22007bca70f63
krishpranav <krisna.pranav@gmail.com>
2021-09-05 12:51:43 +0530

    kernel: include net udp header

--- kernel/include/net/udp.h
+++ kernel/include/net/udp.h
@@ -1,6 +1,3 @@
-// #ifndef NET_UDP_H
-// #define NET_UDP_H
-
 #pragma once 
 
 #include <stdint.h>
@@ -16,5 +13,3 @@ struct __attribute__((packed)) udp_packet
 
 void udp_build_header(struct udp_packet *udp, uint16_t msg_len, uint32_t source_ip, uint16_t source_port, uint32_t dest_ip, uint16_t dest_port);
 int udp_validate_header(struct udp_packet *udp, uint32_t source_ip, uint32_t dest_ip);
-
-// #endif
