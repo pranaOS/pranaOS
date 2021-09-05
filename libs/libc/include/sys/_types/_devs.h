c8b09a8dbe1d667c6f40cd991c74bcb910950827
krishpranav <krisna.pranav@gmail.com>
2021-09-05 17:43:53 +0530

    libs: libc include sys _types _devs header

--- libs/libc/include/sys/_types/_devs.h
+++ libs/libc/include/sys/_types/_devs.h
@@ -1,10 +1,7 @@
-#ifndef _LIBC_SYS__TYPES__DEVS_H
-#define _LIBC_SYS__TYPES__DEVS_H
+#pragma once
 
 #define MINORBITS 20
 #define MINORMASK ((1U << MINORBITS) - 1)
 #define major(dev) ((unsigned int)((dev) >> MINORBITS))
 #define minor(dev) ((unsigned int)((dev)&MINORMASK))
 #define makedev(ma, mi) (((ma) << MINORBITS) | (mi))
-
-#endif // _LIBC_SYS__TYPES__DEVS_H
