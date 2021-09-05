84dce26ec9187e7197d66ffe50ab94797b9f0c5d
krishpranav <krisna.pranav@gmail.com>
2021-09-05 12:52:29 +0530

    kernel: include libkern c_attrs header

--- kernel/include/libkern/c_attrs.h
+++ kernel/include/libkern/c_attrs.h
@@ -4,8 +4,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#ifndef _KERNEL_LIBKERN_C_ATTRS_H
-#define _KERNEL_LIBKERN_C_ATTRS_H
+#pragma once 
 
 #ifndef PACKED
 #define PACKED __attribute__((packed))
@@ -18,5 +17,3 @@
 #ifndef NORETURN
 #define NORETURN __attribute__((noreturn))
 #endif // NORETURN
-
-#endif // _KERNEL_LIBKERN_C_ATTRS_H
