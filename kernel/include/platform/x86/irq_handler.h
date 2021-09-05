c4509a0bb503ae2520c9e7d9c98db4399fd6c409
krishpranav <krisna.pranav@gmail.com>
2021-09-05 18:22:41 +0530

    kernel: include platform x86 irq_handler header

--- kernel/include/platform/x86/irq_handler.h
+++ kernel/include/platform/x86/irq_handler.h
@@ -4,13 +4,10 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#ifndef _KERNEL_PLATFORM_X86_IRQ_HANDLER_H
-#define _KERNEL_PLATFORM_X86_IRQ_HANDLER_H
+#pragma once
 
 #include <drivers/x86/display.h>
 #include <platform/x86/idt.h>
 
 void irq_handler(trapframe_t* tf);
 void irq_empty_handler();
-
-#endif
