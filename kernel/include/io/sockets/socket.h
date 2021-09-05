05955c5ade6cd035d2bdc5bb7df5670d9519b3ad
krishpranav <krisna.pranav@gmail.com>
2021-09-05 12:37:45 +0530

    kernel: include io sockets socket header

--- kernel/include/io/sockets/socket.h
+++ kernel/include/io/sockets/socket.h
@@ -4,8 +4,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#ifndef _KERNEL_IO_SOCKETS_SOCKET_H
-#define _KERNEL_IO_SOCKETS_SOCKET_H
+#pragma once 
 
 #include <fs/vfs.h>
 #include <libkern/syscall_structs.h>
@@ -16,5 +15,3 @@
 int socket_create(int domain, int type, int protocol, file_descriptor_t* fd, file_ops_t* ops);
 socket_t* socket_duplicate(socket_t* sock);
 int socket_put(socket_t* sock);
-
-#endif /* _KERNEL_IO_SOCKETS_SOCKET_H */
