7b509c5ff0c56ec112b743ab286dc355fbe5c5db
krishpranav <krisna.pranav@gmail.com>
2021-09-05 12:54:00 +0530

    kernel: include libkern stdarg header

--- kernel/include/libkern/stdarg.h
+++ kernel/include/libkern/stdarg.h
@@ -1,8 +1,5 @@
-#ifndef _KERNEL_LIBKERN_STDARG_H
-#define _KERNEL_LIBKERN_STDARG_H
+#pragma once 
 
 #define va_start(v, l) __builtin_va_start(v, l)
 #define va_end(v) __builtin_va_end(v)
 #define va_arg(v, l) __builtin_va_arg(v, l)
-
-#endif // _KERNEL_LIBKERN_STDARG_H
