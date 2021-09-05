6bf2a12cdf24adbf2ee18c2a83a6c63add22e786
krishpranav <krisna.pranav@gmail.com>
2021-09-05 17:45:57 +0530

    libs: libc include sys select header

--- libs/libc/include/sys/select.h
+++ libs/libc/include/sys/select.h
@@ -1,5 +1,4 @@
-#ifndef _LIBC_SYS_SELECT_H
-#define _LIBC_SYS_SELECT_H
+#pragma once
 
 #include <bits/sys/select.h>
 #include <bits/time.h>
@@ -12,5 +11,3 @@ __BEGIN_DECLS
 int select(int nfds, fd_set_t* readfds, fd_set_t* writefds, fd_set_t* exceptfds, timeval_t* timeout);
 
 __END_DECLS
-
-#endif // _LIBC_SYS_SELECT_H
