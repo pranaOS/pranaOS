505d1395988fd13e5664de073e4b8726c2d95154
krishpranav <krisna.pranav@gmail.com>
2021-09-06 17:13:04 +0530

    libs: libabi memory
    // FIXME

---
+++ libs/libabi/memory.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+// FIXME: memory funcs 
+
+#define MEMORY_NONE (0)
+typedef unsigned int MemoryFlags 
