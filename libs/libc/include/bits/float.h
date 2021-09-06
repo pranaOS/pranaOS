5722f2fac9788b92238c54c83db5b8570dfade89
krishpranav <krisna.pranav@gmail.com>
2021-09-06 13:18:18 +0530

    libs: libc include bits float header
    fixme

---
+++ libs/libc/include/bits/float.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+// FIXME: add more float define values 
+
+#define NAN (0.0 / 0.0)
+#define INFINITY (1.0 / 0.0)
