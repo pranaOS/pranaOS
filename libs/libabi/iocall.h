56c7d0efa0ff2e8258c8fe4db0b79793fa65f66e
krishpranav <krisna.pranav@gmail.com>
2021-09-06 17:02:48 +0530

    libs: libabi iocall header

--- libs/libabi/iocall.h
+++ libs/libabi/iocall.h
@@ -7,31 +7,67 @@
 #pragma once
 
 // includes
-#include <libabi/network.h>
+#include <libabi/Network.h>
 
-// FIXME: add more iocall struct funcs
-
-struct IOCallTerminalSizeArgs 
+struct IOCallTerminalSizeArgs
 {
     int width;
     int height;
-}
+};
 
-struct IOCallNetworkStateArgs
+struct IOCallDisplayModeArgs
 {
-    MacAddress mac_address;
-}
+    int width;
+    int height;
+};
 
-struct IOCallKeyboardSetKeymapArgs 
+struct IOCallDisplayBlitArgs
+{
+    uint32_t *buffer;
+    int buffer_width;
+    int buffer_height;
+
+    int blit_x;
+    int blit_y;
+    int blit_width;
+    int blit_height;
+};
+
+struct IOCallKeyboardSetKeymapArgs
 {
     void *keymap;
     size_t size;
-}
+};
 
-// FIXME: add more iocall enum funcs
+struct IOCallTextModeStateArgs
+{
+    int width;
+    int height;
+    int cursor_x;
+    int cursor_y;
+};
+
+struct IOCallNetworkSateAgs
+{
+    MacAddress mac_address;
+};
 
 enum IOCall
 {
-    IOCALL_TERMINAL_GETSIZE,
-    IOCALL_TERMINAL_GETSIZE,
-}
+    IOCALL_TERMINAL_GET_SIZE,
+    IOCALL_TERMINAL_SET_SIZE,
+
+    IOCALL_DISPLAY_GET_MODE,
+    IOCALL_DISPLAY_SET_MODE,
+    IOCALL_DISPLAY_BLIT,
+
+    IOCALL_KEYBOARD_SET_KEYMAP,
+    IOCALL_KEYBOARD_GET_KEYMAP,
+
+    IOCALL_TEXTMODE_GET_STATE,
+    IOCALL_TEXTMODE_SET_STATE,
+
+    IOCALL_NETWORK_GET_STATE,
+
+    __IOCALL_COUNT,
+};
