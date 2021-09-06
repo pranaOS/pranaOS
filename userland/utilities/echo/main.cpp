98bcdf775e92f92a0de244684b2933b393a132c8
krishpranav <krisna.pranav@gmail.com>
2021-09-06 11:46:40 +0530

    useralnd: utilities echo
    echo command added for pranaOS its working great
    
    $ echo helloworld

--- userland/utilities/echo/main.cpp
+++ userland/utilities/echo/main.cpp
@@ -1,7 +1,18 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
 #include <stdio.h>
 
-int main()
-{
-    printf("Hello world");
+int main(int argc, char** argv){
+    for(int i = 1; i < argc; i++){
+        printf("%s", argv[i]);
+
+        if(i < argc - 1){ 
+        }
+    }
+
     return 0;
 }
