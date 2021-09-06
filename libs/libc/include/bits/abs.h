84514ce810557be2ffb11e591295087cb9c72f3c
krishpranav <krisna.pranav@gmail.com>
2021-09-06 13:06:50 +0530

    libs: libc include bits abs header
    FIXME

---
+++ libs/libc/include/bits/abs.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <__libc__.h>
+
+__BEGIN_HEADER
+
+int abs(int x);
+long labs(long j);
+long long llabs(long long x);
+
+// FIXME: float
+
+double abs(double x);
+float fabsf(float y);
+long double fabsl(long double x);
+
+
+__END_HEADER;
