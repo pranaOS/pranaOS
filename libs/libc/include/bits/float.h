a2366eadeb5b8af7a93e83cdd2c305b4c19fa825
Williams <82570807+NukeWilliams@users.noreply.github.com>
2021-09-06 13:19:23 +0530

    float header
    fixed

--- libs/libc/include/bits/float.h
+++ libs/libc/include/bits/float.h
@@ -1,12 +1,14 @@
 /*
- * Copyright (c) 2021, Krisna Pranav
+ * Copyright (c) 2021, Krisna Pranav, NukeWilliams
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
 #pragma once
 
-// FIXME: add more float define values 
-
 #define NAN (0.0 / 0.0)
 #define INFINITY (1.0 / 0.0)
+
+#define INF (1.0 / 0.0)
+#define POS_INF (1.0 / 0.0)
+#define NEG_INF (-1.0 / 0.0)
