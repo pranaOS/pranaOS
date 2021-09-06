70425703f3306cd7c5538042b32bedd11b4d19aa
krishpranav <krisna.pranav@gmail.com>
2021-09-06 10:57:57 +0530

    kernel: include libkern atomic header
    atomic_add __ATOMIC_SEQ_CST FIX

--- kernel/include/libkern/atomic.h
+++ kernel/include/libkern/atomic.h
@@ -4,12 +4,13 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#pragma once 
+#pragma once
 
+// includes
 #include <libkern/c_attrs.h>
 #include <libkern/kassert.h>
 #include <libkern/types.h>
 
-#define atomic_add(x, val) (__atomic_add_fetch(x, val, __ATOMIC_SEQ_CST) + val)
+#define atomic_add(x, val) (__atomic_add_fetch(x, val, __ATOMIC_SEQ_CST))
 #define atomic_store(x, val) (__atomic_store_n(x, val, __ATOMIC_SEQ_CST))
 #define atomic_load(x) (__atomic_load_n(x, __ATOMIC_SEQ_CST))
