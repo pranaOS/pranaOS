dbfa9deff91a0ff5bb725ea1b5730559afcbe8e8
krishpranav <krisna.pranav@gmail.com>
2021-09-06 13:07:27 +0530

    libs: libc include bits abs header
    FIXED

--- libs/libc/include/bits/abs.h
+++ libs/libc/include/bits/abs.h
@@ -1,5 +1,12 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
 #pragma once
 
+// includes
 #include <__libc__.h>
 
 __BEGIN_HEADER
@@ -8,11 +15,8 @@ int abs(int x);
 long labs(long j);
 long long llabs(long long x);
 
-// FIXME: float
-
-double abs(double x);
-float fabsf(float y);
+double fabs(double x);
+float fabsf(float x);
 long double fabsl(long double x);
-
 
 __END_HEADER;
