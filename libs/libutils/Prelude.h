379038f85e314f5beb4da46f8c6ff7e931b56ad0
krishpranav <krisna.pranav@gmail.com>
2021-09-06 12:42:08 +0530

    libs: libutils Prelude header

--- libs/libutils/Prelude.h
+++ libs/libutils/Prelude.h
@@ -6,24 +6,25 @@
 
 #pragma once
 
-// includes
 #include <stdarg.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <libabi/Process.h>
-#include <Macros.h>
+
+#include <abi/Process.h>
+#include <libutils/Macros.h>
 
 typedef uint64_t size64_t;
 typedef int64_t ssize64_t;
 
 #ifdef __cplusplus
-#   ifdef NO_PRANAOS_USING_UTILS_NAMESPACE
+#    ifndef NO_PRANAOS_USING_UTILS_NAMESPACE
 
-
-namespace Utils 
+namespace Utils
 {
 }
-#endif 
 
+using namespace Utils;
+
+#    endif
 #endif
