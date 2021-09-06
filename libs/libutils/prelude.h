dd1cd22711d2ee425aba3aac07646d94a8d6bc5e
krishpranav <krisna.pranav@gmail.com>
2021-09-06 16:30:12 +0530

    libs: libutils Predue h

---
+++ libs/libutils/prelude.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include <libabi/Process.h>
+#include <libutils/Macros.h>
+
+typedef uint64_t size64_t;
+typedef int64_t ssize64_t;
+
+#ifdef __cplusplus
+#    ifndef NO_PRANAOS_USING_UTILS_NAMESPACE
+
+namespace Utils
+{
+}
+
+using namespace Utils;
+
+#    endif
+#endif
