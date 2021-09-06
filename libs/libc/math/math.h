7660bbd0ac946d2cc8aec6757718443dfd91c995
krishpranav <krisna.pranav@gmail.com>
2021-09-06 13:25:31 +0530

    libs: libc math header
    // fixed: add some math double, void, long, int interger function but
    // fixme: add more function

--- libs/libc/math/math.h
+++ libs/libc/math/math.h
@@ -14,12 +14,53 @@
 
 __BEGIN_HEADER
 
-
 extern int signgam;
 
 double j0(double);
 double j1(double);
 double jn(int, double);
+
+void sincos(double, double *, double *);
+void sincosf(float, float *, float *);
+void sincosl(long double, long double *, long double *);
+
+double exp10(double);
+float exp10f(float);
+long double exp10l(long double);
+
+double pow10(double);
+float pow10f(float);
+long double pow10l(long double);
+
+double acos(double x);
+float acosf(float x);
+long double acosl(long double x);
+double asin(double x);
+float asinf(float x);
+long double asinl(long double x);
+double atan(double x);
+float atanf(float x);
+long double atanl(long double x);
+double atan2(double x, double y);
+float atan2f(float x, float y);
+long double atan2l(long double x, long double y);
+double cos(double x);
+float cosf(float x);
+long double cosl(long double x);
+double sin(double x);
+float sinf(float x);
+long double sinl(long double x);
+double tan(double x);
+float tanf(float x);
+long double tanl(long double x);
+
+double acosh(double x);
+float acoshf(float x);
+long double acoshl(long double x);
+
+double asinh(double x);
+float asinhf(float x);
+long double asinhl(long double x);
 
 double rint(double x);
 float rintf(float x);
