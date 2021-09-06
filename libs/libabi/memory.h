a80499c8c9b991c87040df0157ed5902308d5d67
krishpranav <krisna.pranav@gmail.com>
2021-09-06 17:13:24 +0530

    libs: libabi memory
    // fixed: memory allocations

--- libs/libabi/memory.h
+++ libs/libabi/memory.h
@@ -6,7 +6,7 @@
 
 #pragma once
 
-// FIXME: memory funcs 
-
 #define MEMORY_NONE (0)
-typedef unsigned int MemoryFlags 
+#define MEMORY_USER (1 << 0)
+#define MEMORY_CLEAR (1 << 1)
+typedef unsigned int MemoryFlags;
