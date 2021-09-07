04a47fb86ce05dcf6bc86ec63c23b4b36a9710cc
krishpranav <krisna.pranav@gmail.com>
2021-09-07 16:34:45 +0530

    userland: tests objc forloop

--- userland/tests/objc/forloop.m
+++ userland/tests/objc/forloop.m
@@ -3,3 +3,16 @@
  *
  * SPDX-License-Identifier: BSD-2-Clause
 */
+
+#import <libfoundation/NSObject.h>
+#import <libobjc/helpers.h>
+#import <stdio.h>
+
+int main()
+{
+    for (;;) {
+        printf("This loop will run forever.\n");
+    }
+    
+    return 0;
+}
