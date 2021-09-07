1fdb0ac3583c4bc97aa1683dbb47a6895f05cc06
krishpranav <krisna.pranav@gmail.com>
2021-09-07 11:20:55 +0530

    libs: libutils std.h
    // FIXME

--- libs/libutils/std.h
+++ libs/libutils/std.h
@@ -16,6 +16,11 @@ inline void *operator new(size_t, void *ptr)
     return ptr;
 }
 
+inline void *operator new[](size_t, void *ptr)
+{
+    return ptr;
+}
+
 #else
 #   include <new>
 #endif
@@ -26,5 +31,50 @@ namespace std
 // FIXME: add more templates
 
 using nullptr_t = decltype(nullptr);
+
+template <typename T>
+void swap(T &left, T &right)
+{
+    T tmp = move(left);
+    left = move(right);
+    right = move(tmp);
+}
+
+template <typename T>
+struct initialize_list
+{
+private:
+    T *_data;
+
+size_t _size;
+
+constexpr initialize_list(const T *data, size_t size)
+    : _data(data), _size(size)
+{
+}
+
+public:
+    size_t size() const { return _size;}
+
+    constexpr initialize_list()
+        : _data(0), _size(0)
+    {
+    }
+
+    const T &operator[](size_t index) const
+    {
+        return _data[index];
+    }
+
+    T &operator[](size_t index)
+    {
+        return _data[index];
+    }
+
+    constexpr const T *begin() const { return _data; }
+
+    constexpr const T *end() const { return _data + _size; }
+};
+
 
 }
