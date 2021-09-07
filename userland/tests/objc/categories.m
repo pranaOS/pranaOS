1ffc99f6e3f57a55cf434cc7f89935fdd4c141e3
krishpranav <krisna.pranav@gmail.com>
2021-09-07 17:43:53 +0530

    userland: tests objc categories

---
+++ userland/tests/objc/categories.m
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#import <libfoundation/NSObject.h>
+#import <libobjc/helpers.h>
+#import <stdio.h>
+
+@interface ClassName(CategoryName)
+@end
