03de5ca27febca679e612802d23a0fea709e6393
krishpranav <krisna.pranav@gmail.com>
2021-09-07 15:55:35 +0530

    libs: libutils stringstorage header

--- libs/libutils/stringstorage.h
+++ libs/libutils/stringstorage.h
@@ -4,6 +4,7 @@
 * SPDX-License-Identifier: BSD-2-Clause
 */
 
+
 #pragma once
 
 // includes
@@ -14,7 +15,8 @@
 namespace Utils
 {
 
-struct StringStorage final : public Storage
+struct StringStorage final :
+    public Storage
 {
 private:
     char *_buffer;
@@ -26,19 +28,21 @@ public:
 
     const char *cstring() { return _buffer; }
 
-    void *start()
-    {
-        return _buffer;
-    }
+    void *start() override { return _buffer; }
 
-    void *end()
-    {
-        return _length;
-    }
+    void *end() override { return reinterpret_cast<char *>(start()) + _length; }
 
     StringStorage(CopyTag, const char *cstring)
         : StringStorage(COPY, cstring, strlen(cstring))
     {
+    }
+
+    StringStorage(CopyTag, const char *cstring, size_t length)
+    {
+        _length = strnlen(cstring, length);
+        _buffer = new char[_length + 1];
+        memcpy(_buffer, cstring, _length);
+        _buffer[_length] = '\0';
     }
 
     StringStorage(AdoptTag, char *cstring)
@@ -56,7 +60,6 @@ public:
     {
         delete[] _buffer;
     }
-
 };
 
-}
+} 
