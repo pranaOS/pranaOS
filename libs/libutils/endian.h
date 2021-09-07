ed3bfa08a1f4352edd584e610fa9bd98eb514294
krishpranav <krisna.pranav@gmail.com>
2021-09-07 19:05:55 +0530

    libs: libutils endian header

---
+++ libs/libutils/endian.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <libutils/prelude.h>
+
+namespace Utils
+{
+
+template <typename TValue>
+constexpr TValue swap_little_endian(TValue value)
+{
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+    return value;
+#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+    if constexpr (sizeof(TValue) == 8)
+        return __builtin_bswap64(value);
+    if constexpr (sizeof(TValue) == 4)
+        return __builtin_bswap32(value);
+    if constexpr (sizeof(TValue) == 2)
+        return __builtin_bswap16(value);
+    if constexpr (sizeof(TValue) == 1)
+        return value;
+#endif
+}
+
+}
