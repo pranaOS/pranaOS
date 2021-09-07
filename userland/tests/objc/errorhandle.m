017b940506627f7b92177cc02e9c6198fe4036ee
krishpranav <krisna.pranav@gmail.com>
2021-09-07 18:27:57 +0530

    userland: tests objc errorhandle

---
+++ userland/tests/objc/errorhandle.m
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#import <libfoundation/NSObject.h>
+#import <libobjc/helpers.h>
+#import <stdio.h>
+
+@interface SampleClass:NSObject
+-(NSString *) getEmployeeNameForID:(int) id withError:(NSError **)errorPtr;
+@end
+
+int main() {
+    if (error) {
+        printf("Error finding name: ", error);
+    } else {
+        printf("Name1: %@", name1)
+    }
+
+    [pool darin]
+    return 0;
+}
