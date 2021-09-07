4bb2368513ed088f8f9c9d8b70735ec84c48ec5f
krishpranav <krisna.pranav@gmail.com>
2021-09-07 14:01:31 +0530

    libs: libutils variant header

--- libs/libutils/variant.h
+++ libs/libutils/variant.h
@@ -20,6 +20,23 @@ struct Dispatch<>
 };
 
 template <typename... Ts>
+struct Variant
+{
+
+private:
+    static const size_t data_size = Max<sizeof(Ts)...>::value;
+    static const size_t data_align = Max<alignof(Ts)...>::value;
+
+public:
+    Variant()
+    {
+        _index = 0
+        new (&_storage) typename First<Ts...>::Type();
+    }
+
+};
+
+template <typename... Ts>
 struct Visitor : Ts...
 {
     using Ts::operator()...;
