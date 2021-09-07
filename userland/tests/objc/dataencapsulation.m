8348c4be866df4950bafd06520c054d09211153b
krishpranav <krisna.pranav@gmail.com>
2021-09-07 16:57:32 +0530

    userland: tests objc BUILD

---
+++ userland/tests/objc/dataencapsulation.m
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#import <libfoundation/NSObject.h>
+#import <libobjc/helpers.h>
+#import <stdio.h>
+
+int main()
+{
+    printf("Hello world from Data Encapsulation obj-c tests")
+    return 0;
+}
