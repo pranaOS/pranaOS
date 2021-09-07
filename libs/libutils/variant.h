2ffa275ca992a3ef192a2c233a1932eec044a315
krishpranav <krisna.pranav@gmail.com>
2021-09-07 14:01:50 +0530

    libs: libutils variant header

--- libs/libutils/variant.h
+++ libs/libutils/variant.h
@@ -34,6 +34,52 @@ public:
         new (&_storage) typename First<Ts...>::Type();
     }
 
+
+    template <typename T>
+    requires In<T, Ts...>
+    Variant(T value)
+    {
+        _index = IndexOf<T, Ts...>();
+        new (&_storage) T(value);
+    }
+
+    Variant(const Variant<Ts...> &other)
+    {
+        _index = other._index;
+        Operations::copy(other._index, &other._storage, &_storage);
+    }
+
+    Variant(Variant<Ts...> &&other)
+    {
+        _index = other._index;
+        Operations::move(other._index, &other._storage, &_storage);
+    }
+
+    ~Variant()
+    {
+        Operations::destroy(_index, &_storage);
+    }
+
+    Variant<Ts...> &operator=(const Variant<Ts...> &other)
+    {
+        Operations::destroy(_index, &_storage);
+        _index = other._index;
+        Operations::copy(_index, &_storage, &other._storage);
+
+        return *this;
+    }
+
+    Variant<Ts...> &operator=(Variant<Ts...> &&other)
+    {
+        if (this != &other)
+        {
+            std::swap(_index, other._index);
+            std::swap(_storage, other._storage);
+        }
+
+        return *this;
+    }
+
 };
 
 template <typename... Ts>
