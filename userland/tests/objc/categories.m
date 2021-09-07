1c0ad59b94efd7879fb41389c846a6fde0df9870
krishpranav <krisna.pranav@gmail.com>
2021-09-07 17:45:26 +0530

    userland: tests objc categories

--- userland/tests/objc/categories.m
+++ userland/tests/objc/categories.m
@@ -12,11 +12,19 @@
 +(NSString *)getCopyRightString;
 @end
 
+@implementation NSString(MyAdditions)
+
++(NSString *)getCopyRightString {
+   return @"Copyright TutorialsPoint.com 2013";
+}
+
+@end
+
 int main(int argc, const char * argv[]) {
-    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-    NSString *copyrightString = [NSString getCopyRightString];
-    printf(@"Accessing Category: %@",copyrightString);
-    
-    [pool drain];
-    return 0;
- }
+   NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+   NSString *copyrightString = [NSString getCopyRightString];
+   printf("Accessing Category: %@",copyrightString);
+   
+   [pool drain];
+   return 0;
+}
