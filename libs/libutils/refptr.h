34404f78b95e91c06692f93596792b0a2e6fbf1e
krishpranav <krisna.pranav@gmail.com>
2021-09-07 12:38:11 +0530

    libs: libutils refptr header

--- libs/libutils/refptr.h
+++ libs/libutils/refptr.h
@@ -66,6 +66,18 @@ public:
         return *_ptr;
     }
 
+    T &operator*() { return *_ptr; }
+
+    const T &operator*() const { return *_ptr; }
+
+    bool operator==(const RefPtr<T> other) const { return _ptr == other._ptr; }
+
+    bool operator!=(const RefPtr<T> other) const { return _ptr != other._ptr; }
+
+    bool operator==(const T *other) const { return _ptr == other; }
+
+    bool operator!=(const T *other) const { return _ptr != other; }
+
     operator bool() const
     {
         return _ptr != nullptr;
