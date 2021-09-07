1f4099cf94877132d6228c4731391be114834c20
krishpranav <krisna.pranav@gmail.com>
2021-09-07 19:14:24 +0530

    libs: libutils inlineringbuffer header

--- libs/libutils/inlineringbuffer.h
+++ libs/libutils/inlineringbuffer.h
@@ -5,6 +5,17 @@
 namespace Utils
 {
 
+template <typename T, size_t N>
+struct InlineRingBuffer
+{
 
+private:
+    size_t _head = 0;
+    size_t _tail = 0;
+    size_t _used = 0;
+
+    T _buffer[N] = {};
+
+};
 
 }
