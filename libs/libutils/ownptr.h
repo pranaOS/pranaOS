891ef00e5e0a93310045c9b0c2ab63473a838259
krishpranav <krisna.pranav@gmail.com>
2021-09-07 12:57:23 +0530

    libs: libutils ownptr header

--- libs/libutils/ownptr.h
+++ libs/libutils/ownptr.h
@@ -1,5 +1,12 @@
+/*
+* Copyright (c) 2021, Krisna Pranav
+*
+* SPDX-License-Identifier: BSD-2-Clause
+*/
+
 #pragma once
 
+// includes
 #include <assert.h>
 #include <libutils/std.h>
 
@@ -14,7 +21,19 @@ private:
 
 public:
     OwnPtr() {}
-    OwnPtr(nullptr) {}
+    OwnPtr(nullptr_t) {}
+
+    OwnPtr(T *ptr) : _ptr(ptr) {}
+
+    OwnPtr(const OwnPtr &other) : _ptr(const_cast<OwnPtr &>(other).give_ref()) {}
+
+    OwnPtr(OwnPtr &&other) : _ptr(other.give_ref()) {}
+
+    template <typename U>
+    OwnPtr(const OwnPtr<U> &other) : _ptr(static_cast<U *>(const_cast<OwnPtr<U>>(other).give_ref())) {}
+
+    template <typename U>
+    OwnPtr(OwnPtr<U> &&other) : _ptr(static_cast<U *>(other.give_ref())) {}
 
     ~OwnPtr()
     {
@@ -36,10 +55,12 @@ public:
 
             _ptr = other.give_ref();
         }
+
+        return *this;
     }
 
     template <typename U>
-    OwnPtr &operator=(OwnPtr<U> &&other)
+    OwnPtr &operator=(OwnPtr<U> &other)
     {
         if (naked() != other.naked())
         {
@@ -50,6 +71,39 @@ public:
 
             _ptr = other.give_ref();
         }
+
+        return *this;
+    }
+
+    OwnPtr &operator=(OwnPtr &&other)
+    {
+        if (this != &other)
+        {
+            if (_ptr)
+            {
+                delete _ptr;
+            }
+
+            _ptr = other.give_ref();
+        }
+
+        return *this;
+    }
+
+    template <typename U>
+    OwnPtr &operator=(OwnPtr<U> &&other)
+    {
+        if (this != static_cast<void *>(&other))
+        {
+            if (_ptr)
+            {
+                delete _ptr;
+            }
+
+            _ptr = other.give_ref();
+        }
+
+        return *this;
     }
 
     T *operator->() const
@@ -58,12 +112,32 @@ public:
         return _ptr;
     }
 
+    T &operator*() { return *_ptr; }
+
+    const T &operator*() const { return *_ptr; }
+
+    bool operator==(const OwnPtr<T> &other) const
+    {
+        return _ptr == other._ptr;
+    }
+
+    template <typename U>
+    bool operator==(const OwnPtr<U> &other) const
+    {
+        return _ptr == static_cast<U *>(other._ptr);
+    }
+
+    bool operator==(T *other) const
+    {
+        return _ptr == other;
+    }
+
     operator bool() const
     {
         return _ptr != nullptr;
     }
 
-    bool operator() const
+    bool operator!() const
     {
         return _ptr == nullptr;
     }
@@ -83,7 +157,7 @@ public:
 };
 
 template <typename Type, typename... Args>
-inline OwnPtr<Type> own(Args &&... args)
+inline OwnPtr<Type> own(Args &&...args)
 {
     return OwnPtr<Type>(new Type(std::forward<Args>(args)...));
 }
