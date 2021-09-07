c4f9c92344f6eeb5f23e9eca4c8213a6aa2a28f0
krishpranav <krisna.pranav@gmail.com>
2021-09-07 18:40:05 +0530

    userland: tests objc protocols

---
+++ userland/tests/objc/protocols.m
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#import <libfoundation/NSObject.h>
+#import <libobjc/helpers.h>
+#import <stdio.h>
+
+@protocol PrintProtocolDelegate
+- (void)processCompleted;
+@end
+
+@interface PrintClass :NSObject {
+    id delegate;
+}
+
+- (void) printDetails;
+
+
+@end
