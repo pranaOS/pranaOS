0cfc07479e0fb9fa31a05fca53fe91713369f700
krishpranav <krisna.pranav@gmail.com>
2021-09-07 10:57:33 +0530

    libs: libabi system header
    // fixed

--- libs/libabi/system.h
+++ libs/libabi/system.h
@@ -6,19 +6,19 @@
 
 #pragma once
 
-#include <libabi/time.h>
-#include <libutils/prelude.h>
+#include <abi/Time.h>
+#include <libutils/Prelude.h>
 
 #define SYSTEM_INFO_FIELD_SIZE 65
 
 struct SystemInfo
 {
-    // FIXME: kerenl release + system info release :) needed
     char kernel_name[SYSTEM_INFO_FIELD_SIZE];
+    char kernel_release[SYSTEM_INFO_FIELD_SIZE];
     char kernel_build[SYSTEM_INFO_FIELD_SIZE];
+    char system_name[SYSTEM_INFO_FIELD_SIZE];
     char machine[SYSTEM_INFO_FIELD_SIZE];
 };
-
 
 struct SystemStatus
 {
