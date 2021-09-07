11806662d8be434585989794bbb45106b8d0229b
krishpranav <krisna.pranav@gmail.com>
2021-09-07 19:14:46 +0530

    libs: libutils inline ring buffer header

--- libs/libutils/inlineringbuffer.h
+++ libs/libutils/inlineringbuffer.h
@@ -16,6 +16,29 @@ private:
 
     T _buffer[N] = {};
 
+public:
+    InlineRingBuffer() {}
+
+    InlineRingBuffer(const InlineRingBuffer &other)
+        : _head(other._head),
+          _tail(other._tail),
+          _used(other._used)
+    {
+        memcpy(_buffer, other._buffer, N);
+    }
+
+    InlineRingBuffer(InlineRingBuffer &&other)
+        : _head(other._head),
+          _tail(other._tail),
+          _used(other._used),
+          _buffer(other._buffer)
+    {
+        other._head = 0;
+        other._tail = 0;
+        other._used = 0;
+        other._buffer = nullptr;
+    }
+
 };
 
 }
