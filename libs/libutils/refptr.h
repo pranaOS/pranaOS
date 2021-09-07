c63d7c7ef3b09ce26cde69cd4c558f404a51a5ba
krishpranav <krisna.pranav@gmail.com>
2021-09-07 12:33:54 +0530

    libs: libutils refptr header

---
+++ libs/libutils/refptr.h
@@ -0,0 +1,111 @@
+/*
+* Copyright (c) 2021, Krisna Pranav
+*
+* SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+#include <libutils/refcounted.h>
+#include <libutils/std.h>
+#include <libutils/tags.h>
+#include <libutils/traits.h>
+
+
+namespace Utils
+{
+
+template<typename T>
+struct RefPtr
+{
+private:
+    T *_ptr = nullptr;
+
+public:
+    RefPtr()
+    {
+    }
+
+    RefPtr(nullptr_t)
+    {
+    }
+
+    ~RefPtr()
+    {
+        deref_if_not_null(_ptr);
+        _ptr = nullptr;
+    }
+
+    RefPtr &operator=(nullptr_t)
+    {
+        deref_if_not_null(_ptr);
+        _ptr = nullptr;
+
+        return *this;
+    }
+
+    RefPtr &operator=(RefPtr &&other)
+    {
+        if (this != &other)
+        {
+            deref_if_not_null(_ptr);
+            _ptr = other.give_ref();
+        }
+
+        return *this;
+    }
+
+    T *operator->() const
+    {
+        assert(ptr);
+        return _ptr;
+    }
+
+    T &operator*()
+    {
+        return *_ptr;
+    }
+
+    operator bool() const
+    {
+        return _ptr != nullptr;
+    }
+
+    bool operator!() const
+    {
+        return _ptr == nullptr;
+    }
+
+    int refcount()
+    {
+        if (_ptr)
+        {
+            return _ptr->refcount();
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    [[nodiscard]] T *give_ref()
+    {
+        T *ptr = _ptr;
+        _ptr = nullptr;
+        return ptr;
+    }
+
+};
+
+template <typename T>
+struct CallbackRefPtr : public RefPtr<T>
+{
+public:
+    template <typename... TArgs>
+    auto operator()(TArgs &&...args)
+    {
+        return (*this->naked())
+    }
+};
+
+}
