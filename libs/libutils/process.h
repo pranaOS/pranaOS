916994934b140048f82d3503fba6b6a03e84c160
krishpranav <krisna.pranav@gmail.com>
2021-09-08 18:41:18 +0530

    libs: libutils process header

--- libs/libutils/process.h
+++ libs/libutils/process.h
@@ -10,7 +10,7 @@
 #include <libabi/process.h>
 #include <libabi/task.h>
 #include <libabi/result.h>
-#include <libutils/strings.h>
+#include <libutils/string.h>
 
 int process_this();
 
@@ -21,3 +21,15 @@ JResult process_run(const char *command, int *pid, TaskFlags flags);
 void NO_RETURN process_abort();
 
 void NO_RETURN process_exit(int code);
+
+JResult process_cancel(int pid);
+
+JResult process_get_directory(char *buffer, size_t size);
+
+JResult process_set_directory(const char *directory);
+
+String process_resolve(String path);
+
+JResult process_sleep(int time);
+
+JResult process_wait(int pid, int *exit_value);
