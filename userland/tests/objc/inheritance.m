1c2498225052326ccab1b4f1e11beac2fb05aa9c
krishpranav <krisna.pranav@gmail.com>
2021-09-08 17:33:06 +0530

    userland: tests objc inheritance

--- userland/tests/objc/inheritance.m
+++ userland/tests/objc/inheritance.m
@@ -4,25 +4,72 @@
  * SPDX-License-Identifier: BSD-2-Clause
 */
 
+// includes
 #import <libfoundation/NSObject.h>
 #import <libobjc/helpers.h>
 #import <stdio.h>
 
 @interface Person : NSObject {
-    NSString *personName;
-    NSInterger personAge;
+   NSString *personName;
+   NSInteger personAge;
 }
 
-- (id) initWithName:(NSString *) name andAge: (NSString) age;
+- (id)initWithName:(NSString *)name andAge:(NSInteger)age;
 - (void)print;
 
 @end
 
 @implementation Person
 
-- (void)print {
-    printf("Name: %", personName);
-    printf("Name: %", personName);
+- (id)initWithName:(NSString *)name andAge:(NSInteger)age {
+   personName = name;
+   personAge = age;
+   return self;
 }
 
-@end 
+- (void)print {
+   printf("Name: %", personName);
+   printf("Age: %ld", personAge);
+}
+
+@end
+
+@interface Employee : Person {
+   NSString *employeeEducation;
+}
+
+- (id)initWithName:(NSString *)name andAge:(NSInteger)age 
+  andEducation:(NSString *)education;
+- (void)print;
+@end
+
+@implementation Employee
+
+- (id)initWithName:(NSString *)name andAge:(NSInteger)age 
+   andEducation: (NSString *)education {
+      personName = name;
+      personAge = age;
+      employeeEducation = education;
+      return self;
+   }
+
+- (void)print {
+   printf("Name: %", personName);
+   printf("Age: %ld", personAge);
+   printf("Education: %", employeeEducation);
+}
+
+@end
+
+int main(int argc, const char * argv[]) {
+   NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];        
+   printf("Base class Person Object");
+   Person *person = [[Person alloc]initWithName:"test" andAge:5];
+   [person print];
+   printf("Inherited Class Employee Object");
+   Employee *employee = [[Employee alloc]initWithName:"test" 
+   andAge:5 andEducation:"MBA"];
+   [employee print];        
+   [pool drain];
+   return 0;
+}
