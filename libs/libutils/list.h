3fd8201af36da323d0ca9e69422233467bea02b8
krishpranav <krisna.pranav@gmail.com>
2021-09-08 16:21:56 +0530

    libs: libutils list header

--- libs/libutils/list.h
+++ libs/libutils/list.h
@@ -16,7 +16,7 @@ namespace Utils
 {
 
 template <typename T>
-struct List 
+struct List
 {
 private:
     struct Node
@@ -26,10 +26,10 @@ private:
         Node *prev = nullptr;
         Node *next = nullptr;
 
-        Node(const T &v) : value {v} {}
+        Node(const T &v) : value{v} {}
     };
 
-    size_t count = 0;
+    size_t _count = 0;
     Node *_head = nullptr;
     Node *_tail = nullptr;
 
@@ -58,10 +58,12 @@ public:
 
     List(const List &other)
     {
-        other.foreach([this](const T &el){
+        other.foreach([this](const T &el) {
             push_back(el);
         });
     }
+
+    List(List &&other) { *this = std::move(other); }
 
     List &operator=(const List &other)
     {
@@ -77,6 +79,17 @@ public:
         return *this;
     }
 
+    List &operator=(List &&other)
+    {
+        if (this != &other)
+        {
+            std::swap(_count, other._count);
+            std::swap(_head, other._head);
+            std::swap(_tail, other._tail);
+        }
+
+        return *this;
+    }
 
     void clear()
     {
@@ -92,9 +105,70 @@ public:
         _count = 0;
         _head = nullptr;
         _tail = nullptr;
-    };
+    }
 
-    void push(const T& value)
+    void remove(const T &value)
+    {
+        Node *current = _head;
+
+        while (current)
+        {
+            Node *next = current->next;
+
+            if (current->value == value)
+            {
+                if (current->prev)
+                {
+                    current->prev->next = current->next;
+                }
+                else
+                {
+                    _head = current->next;
+                }
+
+                if (current->next)
+                {
+                    current->next->prev = current->prev;
+                }
+                else
+                {
+                    _tail = current->prev;
+                }
+
+                _count--;
+                delete current;
+                return;
+            }
+
+            current = next;
+        }
+    }
+
+    Optional<T> requeue()
+    {
+        if (_count == 0)
+        {
+            return NONE;
+        }
+
+        if (_count == 1)
+        {
+            return _head->value;
+        }
+
+        Node *node = _head;
+        _head = node->next;
+        node->next->prev = nullptr;
+
+        _tail->next = node;
+        node->prev = _tail;
+        node->next = nullptr;
+        _tail = node;
+
+        return node->value;
+    }
+
+    void push(const T &value)
     {
         Node *node = new Node{value};
 
@@ -110,7 +184,142 @@ public:
 
         _count++;
         _head = node;
-    };
+    }
+
+    void push_back(const T &value)
+    {
+        Node *node = new Node{value};
+
+        if (_tail == nullptr)
+        {
+            _head = node;
+        }
+        else
+        {
+            _tail->next = node;
+            node->prev = _tail;
+        }
+
+        _count++;
+        _tail = node;
+    }
+
+    T pop()
+    {
+        Assert::truth(any());
+
+        Node *node = _head;
+
+        if (_count == 1)
+        {
+            _head = nullptr;
+            _tail = nullptr;
+        }
+        else if (_count > 1)
+        {
+            node->next->prev = nullptr;
+            _head = node->next;
+        }
+
+        _count--;
+
+        T value = std::move(node->value);
+        delete node;
+        return value;
+    }
+
+    T pop_back()
+    {
+        Assert::truth(any());
+
+        Node *node = _tail;
+
+        if (_count == 1)
+        {
+            _head = nullptr;
+            _tail = nullptr;
+        }
+        else if (_count > 1)
+        {
+            node->prev->next = nullptr;
+            _tail = node->prev;
+        }
+
+        _count--;
+
+        T value = std::move(node->value);
+        delete node;
+        return value;
+    }
+
+    T &peek()
+    {
+        Assert::truth(any());
+        return _head->value;
+    }
+
+    T &peek_back()
+    {
+        Assert::truth(any());
+        return _tail->value;
+    }
+
+    bool contains(const T &value)
+    {
+        bool result = false;
+
+        foreach([&](auto &el) {
+            if (el == value)
+            {
+                result = true;
+                return Iteration::STOP;
+            }
+            else
+            {
+                return Iteration::CONTINUE;
+            }
+        });
+
+        return result;
+    }
+
+    template <typename TCallback>
+    Iteration foreach(TCallback callback) const
+    {
+        Node *current = _head;
+
+        while (current)
+        {
+            Node *next = current->next;
+
+            if (callback(current->value) == Iteration::STOP)
+            {
+                return Iteration::STOP;
+            }
+
+            current = next;
+        }
+
+        return Iteration::CONTINUE;
+    }
+
+    template <typename TCallback>
+    Iteration foreach_reversed(TCallback callback) const
+    {
+        Node *current = _tail;
+
+        while (current)
+        {
+            Node *next = current->prev;
+
+            if (callback(current->value) == Iteration::STOP)
+            {
+                return Iteration::STOP;
+            }
+
+            current = next;
+        }
+    }
 
     struct Iterator
     {
@@ -118,6 +327,10 @@ public:
         Node *_node;
 
     public:
+        Iterator(Node *node) : _node{node}
+        {
+        }
+
         Iterator operator++()
         {
             _node = _node->next;
@@ -135,6 +348,8 @@ public:
         }
     };
 
+    Iterator begin() const { return _head; }
+    Iterator end() const { return nullptr; }
 };
 
-}
+} 
