f7510b41e2deb8713ed40d2831b9fe9fb2e39152
krishpranav <krisna.pranav@gmail.com>
2021-09-08 17:24:52 +0530

    userland: tests objc memorymanager

--- userland/tests/objc/memorymanager.m
+++ userland/tests/objc/memorymanager.m
@@ -9,19 +9,36 @@
 #import <stdio.h>
 
 @interface SampleClass:NSObject
-- (void) sampleMethod;
+- (void)sampleMethod;
 @end
 
 @implementation SampleClass
-- (void) sampleMethod {
-    printf("Hello, World\n");
+- (void)sampleMethod {
+   printf("Hello, World! \n");
+}
+
+- (void)dealloc  {
+  printf("Object deallocated");
+  [super dealloc];
 }
 
 @end
 
-int main()
-{
-    SampleClass *sampleClass = [[SampleClass alloc]init];
-    printf("objc memeory manager test");
-    return 0;
+int main() {
+   
+   SampleClass *sampleClass = [[SampleClass alloc]init];
+   [sampleClass sampleMethod];
+   
+   printf("Retain Count after initial allocation: %d", 
+   [sampleClass retainCount]);
+   [sampleClass retain];
+   
+   printf("Retain Count after retain: %d", [sampleClass retainCount]);
+   [sampleClass release];
+   printf("Retain Count after release: %d", [sampleClass retainCount]);
+   [sampleClass release];
+   printf("SampleClass dealloc will be called before this");
+   
+   sampleClass = nil;
+   return 0;
 }
