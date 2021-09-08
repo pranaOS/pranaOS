a54c02737da02123e10d199f78f0251fa6f41e67
krishpranav <krisna.pranav@gmail.com>
2021-09-08 17:22:28 +0530

    userland: tests objc memorymanager

---
+++ userland/tests/objc/memorymanager.m
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#import <libfoundation/NSObject.h>
+#import <libobjc/helpers.h>
+#import <stdio.h>
+
+int main()
+{
+    printf("objc memeory manager test");
+    return 0;
+}
