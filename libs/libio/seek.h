3eead0b67bd1b2c2dfee3e4a5e5b7a901245464e
krishpranav <krisna.pranav@gmail.com>
2021-09-08 18:21:13 +0530

    libs: libio seek
    seekfrom [start, current, end]

--- libs/libio/seek.h
+++ libs/libio/seek.h
@@ -17,16 +17,25 @@ enum struct Whence : uint8_t
     START = J_WHENCE_START,
     CURRENT = J_WHENCE_CURRENT,
     END = J_WHENCE_END,
-}
+};
 
 struct SeekFrom
 {
     Whence whence;
     ssize64_t position;
-        
+
     static SeekFrom start(ssize64_t position = 0)
     {
-        return {position};
+        return {Whence::START, position};
     }
 
+    static SeekFrom current(ssize64_t position = 0)
+    {
+        return {Whence::CURRENT, position};
+    }
+
+    static SeekFrom end(ssize64_t position = 0)
+    {
+        return {Whence::END, position};
+    }
 };
