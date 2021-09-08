f81454fbcbaebae98e6e6cb52043deeeebf12ace
krishpranav <krisna.pranav@gmail.com>
2021-09-08 18:00:24 +0530

    libs: libutils slice header

--- libs/libutils/slice.h
+++ libs/libutils/slice.h
@@ -20,5 +20,42 @@ private:
     const void *_start = nullptr;
     size_t _size = 0;
 
+public:
+    bool any() const
+    {
+        return _size > 0;
+    }
 
-}
+    size_t size() const
+    {
+        return _size;
+    }
+
+    const void *start() const
+    {
+        return _start;
+    }
+
+    const void *end() const
+    {
+        return reinterpret_cast<const char *>(start()) + _size;
+    }
+
+    Slice slice(size_t offset, size_t size) const
+    {
+        if (_storage != nullptr)
+        {
+            return {_storage, offset, size};
+        }
+        else
+        {
+            auto start = static_cast<const void *>(static_cast<const char *>(_start) + offset);
+            return {start, size};
+        }
+    }
+
+    RefPtr<Storage> storage() override
+    {
+        return _storage;
+    }
+};
