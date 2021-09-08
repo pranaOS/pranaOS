f81533f683b021586be85ef426e3d07830dca1c5
krishpranav <krisna.pranav@gmail.com>
2021-09-08 18:28:52 +0530

    libs: libutils array header

--- libs/libutils/array.h
+++ libs/libutils/array.h
@@ -1,8 +1,8 @@
 /*
-* Copyright (c) 2021, Krisna Pranav
-*
-* SPDX-License-Identifier: BSD-2-Clause
- */
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
 
 #pragma once
 
