f55c945f5bdfbf1fe7bacf34a5916343ef3633d2
krishpranav <krisna.pranav@gmail.com>
2021-09-08 18:01:18 +0530

    libs: libutils slice header

--- libs/libutils/slice.h
+++ libs/libutils/slice.h
@@ -1,8 +1,8 @@
 /*
-* Copyright (c) 2021, Krisna Pranav
-*
-* SPDX-License-Identifier: BSD-2-Clause
-*/
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
 
 #pragma once
 
@@ -12,7 +12,8 @@
 #include <libutils/sliceStorage.h>
 #include <libutils/std.h>
 
-struct Slice : public RawStorage
+struct Slice :
+    public RawStorage
 {
 private:
     RefPtr<Storage> _storage;
@@ -21,15 +22,9 @@ private:
     size_t _size = 0;
 
 public:
-    bool any() const
-    {
-        return _size > 0;
-    }
+    bool any() const { return _size > 0; }
 
-    size_t size() const
-    {
-        return _size;
-    }
+    size_t size() const { return _size; }
 
     const void *start() const
     {
@@ -39,6 +34,46 @@ public:
     const void *end() const
     {
         return reinterpret_cast<const char *>(start()) + _size;
+    }
+
+    Slice()
+    {
+    }
+
+    Slice(RefPtr<Storage> storage)
+    {
+        _storage = storage;
+        _start = _storage->start();
+        _size = _storage->size();
+    }
+
+    Slice(RefPtr<Storage> storage, size_t offset, size_t size)
+    {
+        _storage = storage;
+        _start = _storage->start();
+        _start = static_cast<const void *>(static_cast<const char *>(_start) + offset);
+        _size = size;
+    }
+
+    Slice(const char *start)
+    {
+        _storage = nullptr;
+        _start = start;
+        _size = strlen(start);
+    }
+
+    Slice(const void *start, size_t size)
+    {
+        _storage = nullptr;
+        _start = start;
+        _size = size;
+    }
+
+    Slice(RawStorage &obj)
+    {
+        _storage = obj.storage();
+        _start = _storage->start();
+        _size = _storage->size();
     }
 
     Slice slice(size_t offset, size_t size) const
