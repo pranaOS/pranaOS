a40bd518036fb73dfa5fb6d1b38d44eba73766fc
krishpranav <krisna.pranav@gmail.com>
2021-09-10 11:27:45 +0530

    servers: window server

--- servers/window_server/src/Components/Base/BaseWindow.h
+++ servers/window_server/src/Components/Base/BaseWindow.h
@@ -5,6 +5,7 @@
  */
 
 #pragma once
+#include "../../../shared/MessageContent/MenuBar.h"
 #include "../../../shared/MessageContent/Window.h"
 #include "../../Connection.h"
 #include "../MenuBar/MenuItem.h"
