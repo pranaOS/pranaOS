eb4641086288f8b90fbf25b9ea1407eade6cbf01
krishpranav <krisna.pranav@gmail.com>
2021-09-12 13:19:10 +0530

    libs: libabi result header
    static inline funcs

---
+++ libs/libabi/result.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+enum JResult
+{
+#define RESULT_ENUM_ENTRY(__name, __description) __name,
+    RESULT_ENUM(RESULT_ENUM_ENTRY)
+#undef RESULT_ENUM_ENTRY
+    __RESULT_COUNT
+};
+
+#define result_is_error(__result) ((__result) != SUCCESS && (__result) != TIMEOUT)
+
+static inline const char *result_to_string(JResult error)
+{
+#define RESULT_ENUM_ENTRY_STRING(__entry, __description) #__entry,
+    const char *RESULT_NAMES[] = {RESULT_ENUM(RESULT_ENUM_ENTRY_STRING)};
+#undef RESULT_ENUM_ENTRY
+
+    if (error < __RESULT_COUNT && error >= 0)
+    {
+        return RESULT_NAMES[error];
+    }
+    else
+    {
+        return "INVALID_RESULT_CODE";
+    }
+}
+
+
+static inline const char *get_result_description(JResult result)
+{
+    if (result < __RESULT_COUNT && result >= 0)
+    {
+        return RESULT_DESCRIPTIONS[result];
+    }
+    else
+    {
+        return "INVALID_RESULT_CODE";
+    }
+}
+
+
