e4080bea9a7b3efdfb1f6b81b0a08d5f5438aed1
krishpranav <krisna.pranav@gmail.com>
2021-09-12 12:10:52 +0530

    libs: libabi time header

---
+++ libs/libabi/time.h
@@ -0,0 +1,41 @@
+#pragma once
+
+
+#include <libutils/prelude.h>,
+
+typedef uint32_t ElapsedTime;
+
+typedef uint32_t Tick;
+
+struct Time
+{
+    int second;
+    int minute;
+    int hour;
+};
+
+struct Date
+{
+    int day;
+    int month;
+    int year;
+};
+
+union DateTime
+{
+    struct
+    {
+        Time time;
+        Date date;
+    };
+
+    struct
+    {
+        int second;
+        int minute;
+        int hour;
+        int day;
+        int month;
+        int year;
+    };
+};
