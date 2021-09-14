679352434feb21db34b2be7989069b19588913c2
krishpranav <krisna.pranav@gmail.com>
2021-09-14 17:30:13 +0530

    libs: libabi syscalls
    syscalls return

---
+++ libs/libabi/syscalls.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#include <libabi/syscalls.h>
+
+static int _pid_cache = -1;
+
+JResult j_process_this(int *pid)
+{
+    if (_pid_cache == -1)
+    {
+        __syscall(J_PROCESS_THIS, (uintptr_t)&_pid_cache);
+    }
+
+    *pid = _pid_cache;
+
+    return SUCCESS;
+}
+
+JResult j_filesystem_link(const char *raw_old_path, size_t old_size, const char *raw_new_path, size_t new_size)
+{
+    return __syscall(
+            J_FILESYSTEM_LINK,
+            (uintptr_t)raw_old_path,
+            (uintptr_t)old_size,
+            (uintptr_t)raw_new_path,
+            (uintptr_t)new_size);
+}
+
+JResult j_handle_connect(int *handle, const char *raw_path, size_t size)
+{
+    return __syscall(J_HANDLE_CONNECT, (uintptr_t)handle, (uintptr_t)raw_path, (uintptr_t)size);
+}
