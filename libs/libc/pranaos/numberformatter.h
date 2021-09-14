4be8dc7109d26f1bf4c6a6ed28c8bb61e2c85a86
krishpranav <krisna.pranav@gmail.com>
2021-09-14 17:40:17 +0530

    libs: libc numberformatter header

--- libs/libc/pranaos/numberformatter.h
+++ libs/libc/pranaos/numberformatter.h
@@ -6,15 +6,21 @@
 
 #pragma once
 
-#include <libutils/prelude.h>
+// includes
+#include <libutils/Prelude.h>
 
 struct NumberFormater
 {
     int base;
     int after_point;
     bool padded_with_zero;
-    bool captialized;
+    bool capitalized;
 };
+
+#define FORMAT_BINARY ((NumberFormater){2, 4, true, false})
+#define FORMAT_OCTAL ((NumberFormater){8, 4, true, false})
+#define FORMAT_DECIMAL ((NumberFormater){10, 4, false, false})
+#define FORMAT_HEXADECIMAL ((NumberFormater){16, 4, true, false})
 
 size_t format_uint(NumberFormater formater, unsigned long value, char *str, size_t size);
 
