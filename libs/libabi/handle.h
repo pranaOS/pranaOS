b9ac12336771d8c163be5f13cdc8a27488d987ac
krishpranav <krisna.pranav@gmail.com>
2021-09-14 17:00:42 +0530

    libs: libabi handle header

--- libs/libabi/handle.h
+++ libs/libabi/handle.h
@@ -1,7 +1,19 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
 #pragma once
 
-#include <libabi/filesystem.h>
-#include <libabi/result.h>
+// includes
+#include <libabi/Filesystem.h>
+#include <libabi/Result.h>
+
+#define POLL_READ (1 << 0)
+#define POLL_WRITE (1 << 1)
+#define POLL_CONNECT (1 << 2)
+#define POLL_ACCEPT (1 << 3)
 
 typedef unsigned int PollEvent;
 
@@ -12,13 +24,19 @@ struct Handle
     JResult result;
 };
 
-struct HandlePool
+struct HandlePoll
 {
     int handle;
-    PoolEvent events;
-    PoolEvent result;
+    PollEvent events;
+    PollEvent result;
 };
 
 #define HANDLE_INVALID_ID (-1)
 
 #define HANDLE(__subclass) ((Handle *)(__subclass))
+
+#define handle_has_error(__handle) (HANDLE(__handle)->result != SUCCESS)
+
+#define handle_error_string(__handle) result_to_string(HANDLE(__handle)->result)
+
+#define handle_get_error(__handle) (HANDLE(__handle)->result)
