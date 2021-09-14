1e72302e4e5eb8e25288fc0486a59d6b2593e105
krishpranav <krisna.pranav@gmail.com>
2021-09-14 17:15:22 +0530

    libs: libc __libc__ header

--- libs/libc/__libc__.h
+++ libs/libc/__libc__.h
@@ -1,18 +1,24 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
 #pragma once
 
-#ifndef __cplusplus
+#ifdef __cplusplus
 
-#   define __BEGIN_HEADER \
-    extern "C"            \
-    {
+#    define __BEGIN_HEADER \
+        extern "C"         \
+        {
 
-#   define __END_HEADER   \
-    }
+#    define __END_HEADER \
+        }
 
 #else
 
-#   define __BEGIN_HEADER \
+#    define __BEGIN_HEADER
 
-#   define __END_HEADER   \
+#    define __END_HEADER
 
-#endif 
+#endif
