e1bf1a7e545c3bee592026d0b66cf262f68432c6
krishpranav <krisna.pranav@gmail.com>
2021-09-15 16:57:01 +0530

    libs: libutils std templates, namespace std

--- libs/libutils/std.h
+++ libs/libutils/std.h
@@ -4,6 +4,32 @@
 
 #if __CONFIG_IS_HOSTED__ == 0
 
+inline void *operator new(size_t, void *ptr)
+{
+    return ptr;
+}
+
+inline void *operator new[](size_t, void *ptr)
+{
+    return ptr;
+}
+
 #else
 #   include <new>
 #endif
+
+namespace Std
+{
+
+using nullptr_t = decltype(nullptr);
+
+template <typename T>
+struct initialize_list
+{
+private:
+    T *_data;
+    size_t size;   
+ 
+};
+
+}
