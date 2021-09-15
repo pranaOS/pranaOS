2e061bc8d3980a7979c4686f3b6c12f0bda4cd61
krishpranav <krisna.pranav@gmail.com>
2021-09-15 19:22:40 +0530

    libs: libutils minmax headers

--- libs/libutils/minmax.h
+++ libs/libutils/minmax.h
@@ -1,12 +1,18 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
 #pragma once
 
 namespace Math
 {
 
-#define clamp(__v, __lower, __upper) 
+#define clamp(__v, __lower, __upper) (MAX(MIN((__v), (__upper)), (__lower)))
 
-#define MIN(__x, __y)
+#define MIN(__x, __y) ((__x) < (__y) ? (__x) : (__y))
 
-#define MAX(__x, __y)
+#define MAX(__x, __y) ((__x) > (__y) ? (__x) : (__y))
 
-}
+} 
