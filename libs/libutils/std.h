3e208f569c0e14f4f240024c2849280354740d91
krishpranav <krisna.pranav@gmail.com>
2021-09-15 17:15:07 +0530

    libs: libutils std header
    updated templates and swap has been updated!!

--- libs/libutils/std.h
+++ libs/libutils/std.h
@@ -24,12 +24,67 @@ namespace Std
 using nullptr_t = decltype(nullptr);
 
 template <typename T>
+constexpr typename RemoveReference<T>::Type &&move(T &&arg)
+{
+    return static_cast<typename RemoveReference<T>::Type &&>(arg);
+}
+
+
+template <typename T>
+void swap(T &left, T &right)
+{
+    T tmp = move(left);
+    left = move(right);
+    right = move(tmp);
+}
+
+template <typename T, typename U = T>
+constexpr T exchange(T &slot, U &&value)
+{
+    T old = move(slot);
+    slot = forward<U>(value);
+    return old;
+}
+
+template <typename T>
 struct initialize_list
 {
 private:
     T *_data;
     size_t size;   
- 
+    
+    constexpr initializer_list(const T *data, size_t size)
+    {
+    }
+
+public:
+    size_t size() const { return _size; }
+
+    constexpr initializer_list()
+        : _data(0), _size(0)
+    {        
+    }
+
+    const T &operator[](size_t index) const
+    {
+        return _data[index];
+    }
+
+    T &operator[](size_t index)
+    {
+        return _data[index];
+    }
+
+    constexpr const T *begin() const
+    {
+        return _data;
+    }
+
+    constexpr const T *end() const
+    {
+        return _data;
+    }
+
 };
 
 }
