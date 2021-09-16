8fff26c3f97c77946cbec2c33cca74ebc7dc25c8
krishpranav <krisna.pranav@gmail.com>
2021-09-16 11:20:05 +0530

    libs: libio seek header

---
+++ libs/libio/seek.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+#include <libabi/handle.h>
+// FIXME: add this library
+// #include <libutils/resultor.h>
+
+namespace IO
+{
+
+}
