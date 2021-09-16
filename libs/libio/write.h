01da206b4c65e66892bd9e285575c90bfbae7bbd
krishpranav <krisna.pranav@gmail.com>
2021-09-16 11:42:51 +0530

    libs: libio write header

--- libs/libio/write.h
+++ libs/libio/write.h
@@ -1,7 +1,29 @@
 #pragma once
 
 #include <string.h>
+#include <libio/writer.h>
+#include <libutils/string.h>
 
 namespace IO
 {
+
+
+static inline ResultOr<size_t> write(Writer &write, char value)
+{
+    return writer.write(&value, sizeof(value));
+}
+
+static inline ResultOr<size_t> write(Writer &writer, Slice slice)
+{
+    return writer.write(slice.start(), slice.size());
+}
+
+
+template <typenmae T>
+static inline ResultOr<size_t> write_struct(Writer &writer, const T &data)
+{
+    const uint8_t *bytes = reinterpret_cast<const uint8_t *>(&data);
+    return writer.write(bytes, sizeof(T));
+}
+
 }
