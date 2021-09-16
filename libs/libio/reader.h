cfeb954fbabd33bb698f851775c5b180eeb9ff54
krishpranav <krisna.pranav@gmail.com>
2021-09-16 12:22:59 +0530

    libs: libio reader header

--- libs/libio/reader.h
+++ libs/libio/reader.h
@@ -1,6 +1,25 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
 #pragma once
+
+// includes
+#include <libio/seek.h>
 
 namespace IO
 {
-    
-}
+
+struct Reader
+{
+    virtual ~Reader() {}
+
+    virtual ResultOr<size_t> read(void *buffer, size_t size) = 0;
+};
+
+template <typename T>
+concept SeekableReader = IsBaseOf<Reader, T>::value &&IsBaseOf<Seek, T>::value;
+
+} 
