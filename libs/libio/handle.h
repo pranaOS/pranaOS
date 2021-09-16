dd6c46677f03d669cb39ca8d87ed1043d8f96588
krishpranav <krisna.pranav@gmail.com>
2021-09-16 12:16:13 +0530

    libs: libio handle header

--- libs/libio/handle.h
+++ libs/libio/handle.h
@@ -37,6 +37,40 @@ public:
         auto resolved_path = process_resolve(path);
         _result = j_handle_open(&_handle, resolved_path.cstring(), resolved_path.length(), flags);
     }
+
+    Handle &operator=(Handle &&other)
+    {
+        std::swap(_handle, other._handle);
+        std::swap(_result, other._result);
+
+        return *this;
+    }
+
+    ~Handle()
+    {
+        if (_handle !- HANDLE_INVALID_ID)
+        {
+            j_handle_close(_handle);
+            _handle = -1;
+        }
+    }
+
+    bool valid()
+    {
+        return _handle != HANDLE_INVALID_ID;
+    }
+
+    JResult result()
+    {
+        return _result;
+    }
 }
+
+struct RawHandle
+{
+    virtual ~RawHandle() {}
+
+    virtual RefPtr<Handle> handle() = 0;
+};
 
 }
