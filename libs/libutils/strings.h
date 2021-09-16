0f18fd119cccb1cdb08658b1231b70075f4d3b06
krishpranav <krisna.pranav@gmail.com>
2021-09-16 11:00:05 +0530

    libs: libutils strings header

---
+++ libs/libutils/strings.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+#include <libutils/prelude.h>
+
+namespace Utils::Strings
+{
+
+static constexpr auto WHITESPACES = "\n\r\t";
+
+}
