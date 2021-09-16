f4221bcc8065fca265006747af3c55c12fbae193
krishpranav <krisna.pranav@gmail.com>
2021-09-16 11:39:50 +0530

    libs: libio writer header

--- libs/libio/writer.h
+++ libs/libio/writer.h
@@ -1,5 +1,12 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
 #pragma once
 
+// includes
 #include <libio/seek.h>
 
 namespace IO
@@ -9,13 +16,12 @@ struct Writer
 {
     virtual ~Writer() {}
 
-    virtual JResult flush()
-    {
-        return SUCCESS;
-    }
+    virtual ResultOr<size_t> write(const void *buffer, size_t size) = 0;
+
+    virtual JResult flush() { return SUCCESS; }
 };
 
 template <typename T>
 concept SeekableWriter = IsBaseOf<Writer, T>::value &&IsBaseOf<Seek, T>::value;
 
-}
+} 
