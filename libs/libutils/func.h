a48ef15f06a87d3f7dac2e752d3203867ff328a9
krishpranav <krisna.pranav@gmail.com>
2021-09-16 17:14:32 +0530

    libs: libutils func header

--- libs/libutils/func.h
+++ libs/libutils/func.h
@@ -1,11 +1,45 @@
 #pragma once
 
 #include <assert.h>
+#include <libutils/ownptr.h>
+#include <libutils/std.h>
+#include <libutils/traits.h>
 
 namespace Utils
 {
 
 template <typename T>
 struct Func;
+
+template <typename Out, typename... In>
+struct Func<Out(In...)>
+{
+private:
+    struct CallableWrapperBase
+    {
+        virtual ~CallableWrapperBase() {}
+        virtual Out call(In...) const = 0;
+    };
+
+    template <typename TCallable>
+    struct CallableWrapper final : public CallableWrapperBase
+    {
+        TCallable callable;
+
+        NONCOPYABLE(CallableWrapper);
+
+        explicit CallableWrapper(TCallable &&callable)
+            : callable{std::move(callable)}
+        {
+        }
+
+        Out call(In... in) const final override
+        {
+            return callable(std::forward<In>(in)...);
+        }
+    };
+
+    OwnPtr<CallableWrapperBase> _wrapper;
+};
 
 }
