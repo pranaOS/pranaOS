d5ad8dc70563a87e7cb09239486549e6567d0a98
krishpranav <krisna.pranav@gmail.com>
2021-09-16 17:19:25 +0530

    libs: libutils iteration header

--- libs/libutils/iteration.h
+++ libs/libutils/iteration.h
@@ -11,4 +11,34 @@ enum struct Iteration
     STOP,
 };
 
+template <typename T>
+using IterFunc = Func<Iteration(T)>;
+
+template <typename T>
+struct ContiguousIterator
+{
+private:
+    T *_ptr;
+
+public:
+    ContiguousIterator(T *ptr) : _ptr(ptr) {}
+
+    ContiguousIterator<T> operator++()
+    {
+        ++_ptr;
+        return *this;
+    }
+
+    bool operator!=(const &other) const
+    {
+        return _ptr != other._ptr;
+    }
+
+    const T &operator*() const
+    {
+        return *_ptr;
+    }
+
+};
+
 }
