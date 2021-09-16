dd0a20d47d1b7f5857c73cf86cf3ad139de2b80e
krishpranav <krisna.pranav@gmail.com>
2021-09-16 11:28:02 +0530

    libs: libio seek header
    // FIXED

--- libs/libio/seek.h
+++ libs/libio/seek.h
@@ -7,10 +7,37 @@
 #pragma once
 
 #include <libabi/handle.h>
-// FIXME: add this library
-// #include <libutils/resultor.h>
+#include <libutils/resultor.h>
 
 namespace IO
 {
+
+enum struct Whence : uint8_t
+{
+    START = J_WHENCE_START,
+    CURRENT = J_WHENCE_CURRENT,
+    END = J_WHENCE_END,
+};
+
+struct SeekFrom
+{
+    Whence whence;
+    ssize64_t position;
+
+    static SeekFrom start(ssize64_t position = 0)
+    {
+        return {Whence::START, position};
+    }
+
+    static SeekFrom current(ssize64_t position = 0)
+    {
+        return {Whence::CURRENT, position};
+    }
+
+    static SeekFrom end(ssize64_t position = 0)
+    {
+        return {Whence::END, position};
+    }
+};
 
 }
