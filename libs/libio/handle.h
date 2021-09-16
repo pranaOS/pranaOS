4ac838af78d6a7110823a2a938db14208f817cba
krishpranav <krisna.pranav@gmail.com>
2021-09-16 12:16:51 +0530

    libs: libio handle header

--- libs/libio/handle.h
+++ libs/libio/handle.h
@@ -6,6 +6,7 @@
 
 #pragma once
 
+// includes
 #include <libabi/syscalls.h>
 #include <libio/seek.h>
 #include <libutils/process.h>
@@ -14,28 +15,32 @@
 namespace IO
 {
 
-struct Handle : public RefCounted<Handle>
+struct Handle :
+    public RefCounted<Handle>
 {
 private:
     int _handle = HANDLE_INVALID_ID;
-    JResult _result = ERR_BAD_HANDLE;
+    HjResult _result = ERR_BAD_HANDLE;
 
     NONCOPYABLE(Handle);
 
 public:
-    int id() const
-    {
-        return _handle;
-    }
+    int id() const { return _handle; }
 
-    Handle(int handle) : _handle(handle), _result(handle != HANDLE_INVALID_ID ? SUCCESS : ERR_BAD_ADDRESS)
+    Handle(int handle) : _handle(handle), _result(handle != HANDLE_INVALID_ID ? SUCCESS : ERR_BAD_HANDLE)
     {
     }
 
-    Handle(const String path, JOpenFlag flags)
+    Handle(const String path, HjOpenFlag flags)
     {
         auto resolved_path = process_resolve(path);
-        _result = j_handle_open(&_handle, resolved_path.cstring(), resolved_path.length(), flags);
+        _result = hj_handle_open(&_handle, resolved_path.cstring(), resolved_path.length(), flags);
+    }
+
+    Handle(Handle &&other)
+    {
+        _handle = std::exchange(other._handle, HANDLE_INVALID_ID);
+        _result = std::exchange(other._result, ERR_BAD_HANDLE);
     }
 
     Handle &operator=(Handle &&other)
@@ -48,11 +53,60 @@ public:
 
     ~Handle()
     {
-        if (_handle !- HANDLE_INVALID_ID)
+        if (_handle != HANDLE_INVALID_ID)
         {
-            j_handle_close(_handle);
+            hj_handle_close(_handle);
             _handle = -1;
         }
+    }
+
+    ResultOr<size_t> read(void *buffer, size_t size)
+    {
+        size_t data_read = 0;
+        _result = TRY(hj_handle_read(_handle, buffer, size, &data_read));
+        return data_read;
+    }
+
+    ResultOr<size_t> write(const void *buffer, size_t size)
+    {
+        size_t data_written = 0;
+        _result = TRY(hj_handle_write(_handle, buffer, size, &data_written));
+        return data_written;
+    }
+
+    HjResult call(IOCall request, void *args)
+    {
+        _result = hj_handle_call(_handle, request, args);
+        return _result;
+    }
+
+    ResultOr<ssize64_t> seek(IO::SeekFrom from)
+    {
+        ssize64_t result_offset = 0;
+        _result = TRY(hj_handle_seek(_handle, &from.position, (HjWhence)from.whence, &result_offset));
+        return result_offset;
+    }
+
+    ResultOr<ssize64_t> tell()
+    {
+        ssize64_t position = 0;
+        ssize64_t result_offset = 0;
+        _result = TRY(hj_handle_seek(_handle, &position, HJ_WHENCE_CURRENT, &result_offset));
+        return result_offset;
+    }
+
+    ResultOr<HjStat> stat()
+    {
+        HjStat stat{};
+        _result = TRY(hj_handle_stat(_handle, &stat));
+        return stat;
+    }
+
+    ResultOr<RefPtr<Handle>> accept()
+    {
+        int connection_handle;
+        _result = TRY(hj_handle_accept(_handle, &connection_handle));
+        return make<Handle>(connection_handle);
     }
 
     bool valid()
@@ -60,11 +114,11 @@ public:
         return _handle != HANDLE_INVALID_ID;
     }
 
-    JResult result()
+    HjResult result()
     {
         return _result;
     }
-}
+};
 
 struct RawHandle
 {
@@ -73,4 +127,4 @@ struct RawHandle
     virtual RefPtr<Handle> handle() = 0;
 };
 
-}
+} 
