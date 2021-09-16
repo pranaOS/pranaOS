1d9f3d824eba96f2f6dd5a61a28973cea31752b7
krishpranav <krisna.pranav@gmail.com>
2021-09-16 14:15:16 +0530

    userland: tests testobjc print
    objc-c

---
+++ userland/tests/testobjc/print.m
@@ -0,0 +1,11 @@
+#include <libfoundation/NSObject.h>
+#include <libobjc/helpers.h>
+#include <stdio.h>
+
+int main()
+{
+    // NSLog(@"hello world")
+    printf("hello world")
+    
+    return 0;
+}
