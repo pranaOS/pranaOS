483d37cc719352654853ff5e9a9c3b4bb65e7f97
krishpranav <krisna.pranav@gmail.com>
2021-09-16 12:11:23 +0530

    libs: libio handle headers

---
+++ libs/libio/handle.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+#include <libabi/syscalls.h>
+#include <libio/seek.h>
+#include <libutils/process.h>
+#include <libutils/string.h>
+
+namespace IO
+{
+
+struct Handle : public RefCounted<Handle>
+{
+private:
+    int _handle = HANDLE_INVALID_ID;
+    JResult _result = ERR_BAD_HANDLE;
+
+    NONCOPYABLE(Handle);
+
+public:
+    int id() const
+    {
+        return _handle;
+    }
+
+    Handle(int handle) : _handle(handle), _result(handle != HANDLE_INVALID_ID ? SUCCESS : ERR_BAD_ADDRESS)
+    {
+    }
+
+    Handle(const String path, JOpenFlag flags)
+    {
+        auto resolved_path = process_resolve(path);
+        _result = j_handle_open(&_handle, resolved_path.cstring(), resolved_path.length(), flags);
+    }
+}
+
+}
