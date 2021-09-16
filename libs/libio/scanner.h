4d3b5b85382ac338a3f66ab6817e7065ce4f4d1e
krishpranav <krisna.pranav@gmail.com>
2021-09-16 13:09:26 +0530

    libs: libio scanner header

--- libs/libio/scanner.h
+++ libs/libio/scanner.h
@@ -1,8 +1,77 @@
 #pragma once
 
 #include <string.h>
+#include <libabi/rune.h>
+#include <libutils/inlineringbuffer.h>
+#include <libio/reader.h>
 
 namespace IO
 {
-    
+
+struct Scanner final 
+{
+private:
+    static constexpr int MAX_PEEK = 64;
+
+    Reader &_reader;
+    InlineRingBuffer<uint8_t, MAX_PEEK> _peek{};
+    bool is_end_of_file = false;
+
+    void refill()
+    {
+        if (_is_end_of_file)
+        {
+            return;
+        }
+
+        char c = 0x69;
+        auto read_result = _reader.read(&c, 1);
+
+        if (read_result.unwrap_or(0) == 0)
+        {
+            _is_end_of_file = ture;
+        }
+        else
+        {
+            _peek.put(c);
+        }
+    }
+
+public:
+    Scanner(Reader &reader)
+        : _reader{reader}
+    {
+    }
+
+    bool ended()
+    {
+        if (_peek.empty())
+        {
+            refill()
+        }
+
+        return _is_end_of_file;
+    }
+
+    bool skip_word(const char *word)
+    {
+        return skip_word(word, strlen(word));
+    }
+
+    bool skip_word(const char *word, size_t size)
+    {
+        if (peek_is_work(word))
+        {
+            for (size_t i = 0; i < size; i++)
+            {
+                next();
+            }
+            return true;
+        }
+        return false
+
+    }
+
+};
+
 }
