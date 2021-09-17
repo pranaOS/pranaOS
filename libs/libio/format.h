88f21620f7f3f932a36a89562d9327ad09b0c99f
krishpranav <krisna.pranav@gmail.com>
2021-09-17 16:54:44 +0530

    libs: libio format headers

--- libs/libio/format.h
+++ libs/libio/format.h
@@ -1,5 +1,12 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
 #pragma once
 
+// includes
 #include <libio/formatter.h>
 #include <libio/memoryreader.h>
 #include <libio/memorywriter.h>
@@ -17,7 +24,7 @@ static inline ResultOr<size_t> format(IO::Writer &writer, Scanner &scan)
 
     while (!scan.ended())
     {
-        writer += TRY(IO::write(writer, scan.next()));
+        written += TRY(IO::write(writer, scan.next()));
     }
 
     return written;
@@ -28,6 +35,44 @@ static inline ResultOr<size_t> format(Writer &writer, Scanner &scan, First first
 {
     size_t written = 0;
 
+    while (!scan.ended() && scan.peek() != '{')
+    {
+        written += TRY(IO::write(writer, scan.next()));
+    }
+
+    if (scan.peek() == '{')
+    {
+        auto fmt = Formatter::parse(scan);
+        written += TRY(fmt.format(writer, first));
+    }
+
+    if (!scan.ended())
+    {
+        written += TRY(format(writer, scan, std::forward<Args>(args)...));
+    }
+
+    return written;
 }
 
+template <typename... Args>
+static inline ResultOr<size_t> format(Writer &writer, const char *fmt, Args... args)
+{
+    MemoryReader memory{fmt};
+    Scanner scan{memory};
+    return format(writer, scan, std::forward<Args>(args)...);
 }
+
+static inline ResultOr<size_t> format(Writer &writer, const char *fmt)
+{
+    return IO::write(writer, fmt);
+}
+
+template <typename... Args>
+static inline String format(const char *fmt, Args... args)
+{
+    MemoryWriter memory{};
+    format(memory, fmt, std::forward<Args>(args)...);
+    return memory.string();
+}
+
+} 
