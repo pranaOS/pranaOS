0b0a5c85358e8c6ff68933620184e51c523acafc
krishpranav <krisna.pranav@gmail.com>
2021-09-18 19:23:07 +0530

    libs: libutils types header

--- libs/libutils/types.h
+++ libs/libutils/types.h
@@ -23,10 +23,29 @@ using ssize_t = MakeSigned<size_t>;
 using __ptrdiff_t = __PTRDIFF_TYPE__;
 #   endif
 
+#endif 
+
 using FlatPtr = Conditional<sizeof(void*) == 8, u64, u32>;
 
 namespace std {
 using nullptr_t decltype(nullptr);
 }
 
-#endif 
+static constexpr FlatPtr explode_byte(u8 b)
+{
+    FlatPtr value = b;
+    if constexpr (sizeof(FlatPtr) == 4)
+        return value << 24 | value << 16 | value << 8 | value;
+    else if (sizeof(FlatPtr) == 8)
+        return value << 56 | value << 48 | value << 40 | value << 32 | value << 24 | value << 16 | value << 8 | value;
+}
+
+constexpr size_t alignt_up_to(const size_t value, const size_t alignment)
+{
+    return (value);
+}
+
+enum class [[nodiscard]] TriState : u8 {
+    False,
+    True,
+}
