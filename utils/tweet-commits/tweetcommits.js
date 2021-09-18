51ab4f5303f1fd915aa66565e7fe8dc739eb0e98
krishpranav <krisna.pranav@gmail.com>
2021-09-18 19:12:58 +0530

    utils: tweet-commits

---
+++ utils/tweet-commits/tweetcommits.js
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// imports
+const fs = require('fs');
+const Twit = require("twit");
+
+// const values
+const { CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET } = process.env;
+const tweetLength = 300;
+const twitterLinkLneght = 25;
+
