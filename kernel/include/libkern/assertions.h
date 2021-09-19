48711c47db23838270b2ce6dee1433170c88d584
krishpranav <krisna.pranav@gmail.com>
2021-09-19 12:23:07 +0530

    kernel: include libkern assertions header

--- kernel/include/libkern/assertions.h
+++ kernel/include/libkern/assertions.h
@@ -1,7 +1,30 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
 #pragma once
 
 #define __STRINGIFY_HELPER(x) #x
 #define __STRINGIFY(x) __STRINGIFY_HELPER(x)
+
+[[noreturn]] void __assertion_failed(const char* msg, const char* file, unsigned line, const char* func);
+#define VERIFY(expr)                                                            \
+    do {                                                                        \
+        if (!static_cast<bool>(expr)) [[unlikely]]                              \
+            __assertion_failed(#expr, __FILE__, __LINE__, __PRETTY_FUNCTION__); \
+    } while (0)
+
+#define VERIFY_NOT_REACHED() VERIFY(false)
+
+extern "C" {
+void _abort();
+[[noreturn]] void abort();
+}
+
+#define VERIFY_INTERRUPTS_DISABLED()
+#define VERIFY_INTERRUPTS_ENABLED
 
 static constexpr bool TODO = false;
 #define TODO() VERIFY(TODO)
