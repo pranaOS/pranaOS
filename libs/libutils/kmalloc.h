91393ffae6f51a9cb2e2cedba6da0ee921095025
krishpranav <krisna.pranav@gmail.com>
2021-09-19 16:05:58 +0530

    libs: libutils kmalloc header

--- libs/libutils/kmalloc.h
+++ libs/libutils/kmalloc.h
@@ -1,30 +1,62 @@
+
 #pragma once
 
-#if defined(KERNEL)
-#   include <kernel/include/mem/kmalloc.h>
-#else
-#   include <new>
-#   include <stdlib.h>
+// includes
+// FIXME: add checked library 
+// #include <libutils/Checked.h>
 
-#   define kmalloc calloc
-#   define kmalloc malloc
-#   define kfree free
+#if defined(KERNEL)
+#       include <kernel/include/mem/kmalloc.h>
+#else
+#    include <new>
+#    include <stdlib.h>
+
+#    define kcalloc calloc
+#    define kmalloc malloc
+#    define kmalloc_good_size malloc_good_size
+#    define kfree free
 
 inline void kfree_sized(void* ptr, size_t)
 {
     kfree(ptr);
 }
-
 #endif
 
 #ifndef __pranaOS__
 #   include <libutils/types.h>
 
-#   ifndef UTILS_OS_MACOS
+#    ifndef UTILS_OS_MACOS
 extern "C" {
-inline size_t malloc_good_size(size_t size)
+inline size_t malloc_good_size(size_t size) { return size; }
 }
-#   else
-#       include <malloc/malloc.h>
-#   endif
+#    else
+#        include <malloc/malloc.h>
+#    endif
 #endif
+
+#ifdef KERNEL
+#    define UTILS_MAKE_ETERNAL                                               \
+    public:                                                               \
+        void* operator new(size_t size) { return kmalloc_eternal(size); } \
+        void operator delete(void*, size_t) { VERIFY_NOT_REACHED(); }     \
+                                                                          \
+    private:
+#else
+#    define UTILS_MAKE_ETERNAL
+#endif
+
+using std::nothrow;
+
+inline void* kmalloc_array(Checked<size_t> a, Checked<size_t> b)
+{
+    auto size = a * b;
+    VERIFY(!size.has_overflow());
+    return kmalloc(size.value());
+}
+
+inline void* kmalloc_array(Checked<size_t> a, Checked<size_t> b, Checked<size_t> c)
+{
+    auto size = a * b * c;
+    VERIFY(!size.has_overflow());
+    return kmalloc(size.value());
+}
