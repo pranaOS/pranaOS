31513a1cf01bbab943bbaca220957bdd3d419eb5
krishpranav <krisna.pranav@gmail.com>
2021-09-19 19:44:17 +0530

    build script

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ chmod +x out/bench.sh
 chmod +x out/debug.sh
 chmod +x out/dll.sh
 
-IMAGE_SIZE=50M
+IMAGE_SIZE=64M
 qemu-img create -f raw out/pranaos.img $IMAGE_SIZE
 if [ $? -ne 0 ]; then echo -e "${ERROR} Can't create an out/pranaos.img" && exit 1; fi
 MKFS="" # Provide path here
