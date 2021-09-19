04fc695fbdba2659460af851e33d5153b6812109
krishpranav <krisna.pranav@gmail.com>
2021-09-19 12:55:55 +0530

    libs: libutils concepts headers

--- libs/libutils/concepts.h
+++ libs/libutils/concepts.h
@@ -1,23 +1,41 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
 #pragma once
 
+// includes
 #include <libutils/iterationdecision.h>
 #include <libutils/stdextras.h>
 
 namespace Utils::Concepts {
 
-template <typename T>
+template<typename T>
 concept Integral = IsIntegral<T>;
 
-template <typename T>
+template<typename T>
 concept FloatingPoint = IsFloatingPoint<T>;
 
-template <typename T>
+template<typename T>
 concept Arithmetic = IsArithmetic<T>;
 
+template<typename T>
+concept Signed = IsSigned<T>;
 
-// FIXME: remove clang formats these properly.
-// clang-format: off
-template <typename Func, typename... Args>
+template<typename T>
+concept Unsigned = IsUnsigned<T>;
+
+template<typename T>
+concept Enum = IsEnum<T>;
+
+template<typename T, typename U>
+concept SameAs = IsSame<T, U>;
+
+// FIXME: remove once Clang formats these properly.
+// clang-format off
+template<typename Func, typename... Args>
 concept VoidFunction = requires(Func func, Args... args)
 {
     {
@@ -26,6 +44,39 @@ concept VoidFunction = requires(Func func, Args... args)
     -> SameAs<void>;
 };
 
+template<typename Func, typename... Args>
+concept IteratorFunction = requires(Func func, Args... args)
+{
+    {
+        func(args...)
+    }
+    -> SameAs<IterationDecision>;
+};
+
+template<typename T, typename EndT>
+concept IteratorPairWith = requires(T it, EndT end)
+{
+    *it;
+    { it != end } -> SameAs<bool>;
+    ++it;
+};
+
+template<typename T>
+concept IterableContainer = requires
+{
+    { declval<T>().begin() } -> IteratorPairWith<decltype(declval<T>().end())>;
+};
+
+// clang-format on
 }
 
 using Utils::Concepts::Arithmetic;
+using Utils::Concepts::Enum;
+using Utils::Concepts::FloatingPoint;
+using Utils::Concepts::Integral;
+using Utils::Concepts::IterableContainer;
+using Utils::Concepts::IteratorFunction;
+using Utils::Concepts::IteratorPairWith;
+using Utils::Concepts::Signed;
+using Utils::Concepts::Unsigned;
+using Utils::Concepts::VoidFunction;
