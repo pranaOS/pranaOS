47df59e31402a11f4ccc61c1f8127fdcbe9b2960
krishpranav <krisna.pranav@gmail.com>
2021-09-19 09:40:03 +0530

    libs: libc include unistd
    added getlogin for identity
    convert: #ifndef _LIBC_UNISTD_H -> #pragma once
    
    author: @krishpranav

--- libs/libc/include/unistd.h
+++ libs/libc/include/unistd.h
@@ -1,6 +1,15 @@
-#ifndef _LIBC_UNISTD_H
-#define _LIBC_UNISTD_H
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
 
+//#ifndef _LIBC_UNISTD_H
+//#define _LIBC_UNISTD_H
+
+#pragma once
+
+// includes
 #include <fcntl.h>
 #include <stddef.h>
 #include <sys/cdefs.h>
@@ -8,7 +17,6 @@
 
 __BEGIN_DECLS
 
-/* tasking */
 int fork();
 int execve(const char* path, char** argv, char** env);
 int wait(int pid);
@@ -16,7 +24,6 @@ pid_t getpid();
 int setpgid(pid_t cmd, pid_t arg);
 pid_t getpgid(pid_t arg);
 
-/* fs */
 int close(int fd);
 ssize_t read(int fd, char* buf, size_t count);
 ssize_t write(int fd, const void* buf, size_t count);
@@ -25,12 +32,11 @@ int chdir(const char* path);
 int unlink(const char* path);
 off_t lseek(int fd, off_t off, int whence);
 
-/* identity */
 uid_t getuid();
+char* getlogin();
 
-/* sched */
 int nice(int inc);
 
 __END_DECLS
 
-#endif // _LIBC_UNISTD_H
+//#endif
