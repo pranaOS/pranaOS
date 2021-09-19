e6d03569867fe43eec1511c15d035a9aced152da
krishpranav <krisna.pranav@gmail.com>
2021-09-19 13:52:52 +0530

    libs: libutils allof header

--- libs/libutils/allof.h
+++ libs/libutils/allof.h
@@ -6,34 +6,31 @@
 
 #pragma once
 
-#include <libutils/concepts.h>
+// includes
+#include <libutils/foncepts.h>
 #include <libutils/find.h>
 #include <libutils/iterator.h>
 
 namespace Utils {
 
 template<typename TEndIterator, IteratorPairWith<TEndIterator> TIterator>
-constexpr bool all_of (
+constexpr bool all_of(
     TIterator const& begin,
     TEndIterator const& end,
     auto const& predicate)
 {
     constexpr auto negated_predicate = [](auto const& pred) {
-        return [&](auto const& elem)
-        {
-            return !pred(elem);
-        };
-        
-        return !(find_if(begin, end, negated_predicate));
-    }
+        return [&](auto const& elem) { return !pred(elem); };
+    };
+    return !(find_if(begin, end, negated_predicate(predicate)) != end);
 }
 
-constexpr bool all_of(Container&& container)
+template<IterableContainer Container>
+constexpr bool all_of(Container&& container, auto const& predicate)
 {
-    return all_of(container.begin());
+    return all_of(container.begin(), container.end(), predicate);
 }
 
 }
-
 
 using Utils::all_of;
