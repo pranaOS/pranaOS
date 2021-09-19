538903b761fedb3fc5d742933f85b4d416869fae
krishpranav <krisna.pranav@gmail.com>
2021-09-19 12:25:26 +0530

    libs: libutils assertions header

--- libs/libutils/assertions.h
+++ libs/libutils/assertions.h
@@ -1,12 +1,11 @@
 #pragma once
 
 #if defined(KERNEL)
-#       include <libkern/assertions.h>
+#    include <libkern/assertions.h>
 #else
-#       include <assert.h>
-#       define VERIFY assert
-
+#    include <assert.h>
+#    define VERIFY assert
+#    define VERIFY_NOT_REACHED() assert(false)
 static constexpr bool TODO = false;
-#       define TODO() VERIFY(TODO)
-
-#endif 
+#    define TODO() VERIFY(TODO)
+#endif
