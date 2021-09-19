f6e272b6a4f9cc5f4043f478f1cacd1d4ffb1b06
krishpranav <krisna.pranav@gmail.com>
2021-09-19 09:26:29 +0530

    libs: libutils numericlimits header

--- libs/libutils/numericlimits.h
+++ libs/libutils/numericlimits.h
@@ -8,6 +8,37 @@ template <typename T>
 struct NumericLimits {
 };
 
+template <>
+struct NumericLimits<bool> {
+    static constexpr char min()
+    {
+        return false;
+    }
+
+    static constexpr char max()
+    {
+        return true;
+    }
+
+    static constexpr bool is_signed()
+    {
+        return false;
+    }
+}
+
+#ifndef KERNEL
+
+template<>
+struct NumericLimits<long double> {
+    static constexpr long double lowest() { return -__LDBL_MAX__; }
+    static constexpr long double min() { return __LDBL_MIN__; }
+    static constexpr long double max() { return __LDBL_MAX__; }
+    static constexpr long double epsilon() { return __LDBL_EPSILON__; }
+    static constexpr bool is_signed() { return true; }
+};
+
+#endif
+
 }
 
 using Utils::NumericLimits;
