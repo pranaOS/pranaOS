492d0a4e4215fd33c8e8313ddbb7552c1bf04af7
krishpranav <krisna.pranav@gmail.com>
2021-09-19 15:56:19 +0530

    kernel: include mem kmalloc header
    added kfree_sizned for signing some kmalloc modules + kmalloc enabled expanded func has been added
    author: @krishpranav

--- kernel/include/mem/kmalloc.h
+++ kernel/include/mem/kmalloc.h
@@ -12,6 +12,8 @@
 
 #define KMALLOC_SPACE_SIZE (4 * MB)
 #define KMALLOC_BLOCK_SIZE 32
+// #define KMALLOC_SCRUB_BYTE 0xbb
+// #define KMALLOC_SCRUB_BYTE 0xaa
 
 void kmalloc_init();
 void* kmalloc(uint32_t size);
@@ -19,6 +21,9 @@ void* kmalloc_aligned(uint32_t size, uint32_t alignment);
 void* kmalloc_page_aligned();
 void kfree(void* ptr);
 void kfree_aligned(void* ptr);
+void kfree_sized(void*, size_t);
 void* krealloc(void* ptr, uint32_t size);
+
+void kmalloc_enabled_expand();
 
 #endif // _KERNEL_MEM_KMALLOC_H
