00c5cf0aed5dc0f72a34dcf1c3dbe017815d7085
krishpranav <krisna.pranav@gmail.com>
2021-09-19 16:25:38 +0530

    libs: libutils noncopyable

--- libs/libutils/noncopyable.h
+++ libs/libutils/noncopyable.h
@@ -1,6 +1,17 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
 #pragma once
 
-#define UTILS_MAKE_NONCOPYABLE(c)    \
-private:
-    c(const c&) = delete;            \
-    c& operator=(const c&) = delete  \
+#define MAKE_NONCOPYABLE(c) \
+private:                       \
+    c(const c&) = delete;      \
+    c& operator=(const c&) = delete
+
+#define MAKE_NONMOVABLE(c) \
+private:                      \
+    c(c&&) = delete;          \
+    c& operator=(c&&) = delete
