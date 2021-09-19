7bf68a959c5a0c539ea3d5ca810c1af7d5b5bd24
krishpranav <krisna.pranav@gmail.com>
2021-09-19 13:44:20 +0530

    libs: libutils anyof header

--- libs/libutils/anyof.h
+++ libs/libutils/anyof.h
@@ -3,3 +3,24 @@
  *
  * SPDX-License-Identifier: BSD-2-Clause
 */
+
+#pragma once
+
+#include <libutils/concepts.h>
+#include <libutils/find.h>
+#include <libutils/iterator.h>
+
+namespace Utils {
+
+template<typename TEndIterator, IteratorPairWith<TEndIterator> TIterator>
+constexpr bool any_of(
+    TIterator const& begin,
+    TEndIterator const& end,
+auto const& predicate)
+{
+return find_if(begin, end, predicate) != end;
+}
+
+}
+
+using Utils::any_of;
