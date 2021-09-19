2326e25bd02065c85e43670b2a5539f584ccc9fa
krishpranav <krisna.pranav@gmail.com>
2021-09-19 16:01:22 +0530

    kernel: include mem kmalloc
    kernel: fix kmalloc stats + kmalloc good size
    fix: #ifndef -> #pragma once
    author: @krishpranav

--- kernel/include/mem/kmalloc.h
+++ kernel/include/mem/kmalloc.h
@@ -4,16 +4,14 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#ifndef _KERNEL_MEM_KMALLOC_H
-#define _KERNEL_MEM_KMALLOC_H
+#pragma once
 
+// includes
 #include <libkern/types.h>
 #include <mem/vmm/vmm.h>
 
 #define KMALLOC_SPACE_SIZE (4 * MB)
 #define KMALLOC_BLOCK_SIZE 32
-// #define KMALLOC_SCRUB_BYTE 0xbb
-// #define KMALLOC_SCRUB_BYTE 0xaa
 
 void kmalloc_init();
 void* kmalloc(uint32_t size);
@@ -23,7 +21,14 @@ void kfree(void* ptr);
 void kfree_aligned(void* ptr);
 void kfree_sized(void*, size_t);
 void* krealloc(void* ptr, uint32_t size);
-
 void kmalloc_enabled_expand();
 
-#endif // _KERNEL_MEM_KMALLOC_H
+size_t kmalloc_good_size(size_t);
+
+struct kmalloc_stats {
+    size_t bytes_allocated;
+    size_t bytes_free;
+    size_t bytes_eternal;
+    size_t kmallc_call_count;
+    size_t kfree_call_count;
+};
