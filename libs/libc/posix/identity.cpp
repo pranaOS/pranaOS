475fb2bdc01c19d8aeecb5303685b246c3c7f543
krishpranav <krisna.pranav@gmail.com>
2021-09-19 09:43:37 +0530

    libs: libc posix identity

---
+++ libs/libc/posix/identity.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+// includes
+#include <assert.h>
+#include <pwd.h>
+#include <string.h>
+#include <sysdep.h>
+#include <unistd.h>
+
+uid_t getuid()
+{
+    return (uid_t)DO_SYSCALL_0(SYS_GETUID);
+}
+
+uid_t geteuid()
+{
+    return 0;
+}
+
+static char loginbuf[128];
+
+char* getlogin()
+{
+    passwd_t* passwd = getpwuid(getuid());
+    strncpy(loginbuf, passwd->pw_name, 128);
+    endpwent();
+    return loginbuf;
+}
