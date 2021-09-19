96b845eea4ebfdac0b25d5f371857c5010df012f
krishpranav <krisna.pranav@gmail.com>
2021-09-19 13:44:50 +0530

    libs: libutils anyof header

--- libs/libutils/anyof.h
+++ libs/libutils/anyof.h
@@ -6,9 +6,10 @@
 
 #pragma once
 
-#include <libutils/concepts.h>
-#include <libutils/find.h>
-#include <libutils/iterator.h>
+// includes
+#include <libutils/Concepts.h>
+#include <libutils/Find.h>
+#include <libutils/Iterator.h>
 
 namespace Utils {
 
@@ -16,9 +17,15 @@ template<typename TEndIterator, IteratorPairWith<TEndIterator> TIterator>
 constexpr bool any_of(
     TIterator const& begin,
     TEndIterator const& end,
-auto const& predicate)
+    auto const& predicate)
 {
-return find_if(begin, end, predicate) != end;
+    return find_if(begin, end, predicate) != end;
+}
+
+template<IterableContainer Container>
+constexpr bool any_of(Container&& container, auto const& predicate)
+{
+    return any_of(container.begin(), container.end(), predicate);
 }
 
 }
