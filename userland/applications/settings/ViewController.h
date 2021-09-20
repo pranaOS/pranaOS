a7709cc3c5ffcc05bd33b7d6ad84a1e6cc77c86e
krishpranav <krisna.pranav@gmail.com>
2021-09-20 16:26:41 +0530

    userland: applications settings viewcontroller
    update controller

--- userland/applications/settings/ViewController.h
+++ userland/applications/settings/ViewController.h
@@ -38,17 +38,24 @@ public:
         label.set_font(LG::Font::system_bold_font());
         label.set_width(label.preferred_width());
 
-        auto& button = view().add_subview<UI::Button>(LG::Rect(0, 0, 10, 10));
-        button.set_background_color(LG::Color::LightSystemButton);
-        button.set_title("Theme Switch");
-        button.set_title_color(LG::Color::LightSystemText);
+        auto& themeswitchbutton = view().add_subview<UI::Button>(LG::Rect(0, 0, 10, 10));
+        themeswitchbutton.set_background_color(LG::Color::LightSystemButton);
+        themeswitchbutton.set_title("Theme Switch");
+        themeswitchbutton.set_title_color(LG::Color::LightSystemText);
+        
+        auto& generalbutton = view().add_subview<UI::Button>(LG::Rect(0, 0, 10, 10));
+        generalbutton.set_background_color(LG::Color::LightSystemButton);
+        generalbutton.set_title("General");
+        generalbutton.set_title_color(LG::Color::LightSystemText);
+        
 
         view().add_constraint(UI::Constraint(label, UI::Constraint::Attribute::Left, UI::Constraint::Relation::Equal, UI::SafeArea::Left));
         view().add_constraint(UI::Constraint(label, UI::Constraint::Attribute::Top, UI::Constraint::Relation::Equal, UI::SafeArea::Top));
 
-        view().add_constraint(UI::Constraint(button, UI::Constraint::Attribute::Left, UI::Constraint::Relation::Equal, UI::SafeArea::Left));
-        view().add_constraint(UI::Constraint(button, UI::Constraint::Attribute::Top, UI::Constraint::Relation::Equal, button, UI::Constraint::Attribute::Bottom, 1, 8));
+        view().add_constraint(UI::Constraint(themeswitchbutton, UI::Constraint::Attribute::Left, UI::Constraint::Relation::Equal, UI::SafeArea::Left));
+        view().add_constraint(UI::Constraint(themeswitchbutton, UI::Constraint::Attribute::Top, UI::Constraint::Relation::Equal, themeswitchbutton, UI::Constraint::Attribute::Bottom, 1, 8));
 
+        view().add_constraint(UI::Constraint(generalbutton, UI::Constraint::Attribute::Left, UI::Constraint::Relation::Equal, UI::SafeArea::Left));
 
         view().set_needs_layout();
     }
