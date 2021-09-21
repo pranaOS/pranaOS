5ccdda3c907936ef9b64e1dc5059a5b529ee3ba3
krishpranav <krisna.pranav@gmail.com>
2021-09-21 17:03:13 +0530

    libs: libc include signal header
    fix: #ifndef -> #pragma once
    issue: #99
    author: @krishpranav

--- libs/libc/include/signal.h
+++ libs/libc/include/signal.h
@@ -1,5 +1,10 @@
-#ifndef _LIBC_SIGNAL_H
-#define _LIBC_SIGNAL_H
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
 
 #include <sys/cdefs.h>
 #include <sys/types.h>
@@ -11,5 +16,3 @@ int sigaction(int signo, void* callback);
 int raise(int sig);
 
 __END_DECLS
-
-#endif // _LIBC_SIGNAL_H
