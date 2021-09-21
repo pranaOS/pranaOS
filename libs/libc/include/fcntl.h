38e6f80634061297753599fbe5e6738b4cf3e968
krishpranav <krisna.pranav@gmail.com>
2021-09-21 17:02:33 +0530

    libs: libc include fcntl header
    fix: #ifndef -> #pragma once
    issue: #99
    author: @krishpranav

--- libs/libc/include/fcntl.h
+++ libs/libc/include/fcntl.h
@@ -1,6 +1,12 @@
-#ifndef _LIBC_FCNTL_H
-#define _LIBC_FCNTL_H
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
 
+#pragma once
+
+// includes
 #include <bits/fcntl.h>
 #include <sys/cdefs.h>
 #include <sys/types.h>
@@ -11,5 +17,3 @@ int open(const char* pathname, int flags);
 int creat(const char* path, mode_t mode);
 
 __END_DECLS
-
-#endif // _LIBC_FCNTL_H
