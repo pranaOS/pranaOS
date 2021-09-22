3a4d7397aeb0e00b26cc064411c9d78b45c0003d
krishpranav <krisna.pranav@gmail.com>
2021-09-22 19:31:23 +0530

    libs: libc include shadow header

--- libs/libc/include/shadow.h
+++ libs/libc/include/shadow.h
@@ -1,5 +1,12 @@
+/*
+* Copyright (c) 2021, Krisna Pranav
+*
+* SPDX-License-Identifier: BSD-2-Clause
+*/
+
 #pragma once
 
+// includes
 #include <sys/cdefs.h>
 #include <sys/types.h>
 
@@ -10,6 +17,7 @@ struct spwd {
     char* sp_pwdp;
     int sp_lstchg;
     int sp_min;
+    int sp_max;
     int sp_warn;
     int sp_inact;
     int sp_expire;
@@ -19,5 +27,8 @@ struct spwd {
 typedef struct spwd spwd_t;
 
 void setspent();
+void endspent();
+spwd_t* getspent();
+spwd_t* getspnam(const char* name);
 
 __END_DECLS
