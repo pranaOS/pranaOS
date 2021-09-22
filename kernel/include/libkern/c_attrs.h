80c5ba2a7d306a053ac904f4e2ea49d79214f23f
Williams <82570807+NukeWilliams@users.noreply.github.com>
2021-09-22 10:55:20 +0530

    c_attrs header
    fix: #ifndef -> #pragma once
    issue: #99
    author: @krishpranav
    co-author: @NukeWilliams

--- kernel/include/libkern/c_attrs.h
+++ kernel/include/libkern/c_attrs.h
@@ -1,11 +1,10 @@
 /*
- * Copyright (c) 2021, Krisna Pranav
+ * Copyright (c) 2021, Krisna Pranav, NukeWilliams
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#ifndef _KERNEL_LIBKERN_C_ATTRS_H
-#define _KERNEL_LIBKERN_C_ATTRS_H
+#pragma once
 
 #ifndef PACKED
 #define PACKED __attribute__((packed))
@@ -18,5 +17,3 @@
 #ifndef NORETURN
 #define NORETURN __attribute__((noreturn))
 #endif // NORETURN
-
-#endif // _KERNEL_LIBKERN_C_ATTRS_H
