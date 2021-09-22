b1dcc60d75c8cdc68256810024d86ad8d4d9e6cd
krishpranav <krisna.pranav@gmail.com>
2021-09-22 11:48:57 +0530

    fix: kernel: include time #ifndef -> #pragma once
    fix: #ifndef -> #pragma once
    issue: #99
    author: @krishpranav

--- kernel/include/time/time_manager.h
+++ kernel/include/time/time_manager.h
@@ -4,8 +4,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#ifndef _KERNEL_TIME_TIME_MANAGER_H
-#define _KERNEL_TIME_TIME_MANAGER_H
+#pragma once
 
 #include <drivers/generic/timer.h>
 #include <libkern/atomic.h>
@@ -29,5 +28,3 @@ time_t timeman_seconds_since_boot();
 time_t timeman_get_ticks_from_last_second();
 static inline time_t timeman_ticks_per_second() { return TIMER_TICKS_PER_SECOND; };
 static inline time_t timeman_ticks_since_boot() { return THIS_CPU->stat_ticks_since_boot; };
-
-#endif /* _KERNEL_TIME_TIME_MANAGER_H */
