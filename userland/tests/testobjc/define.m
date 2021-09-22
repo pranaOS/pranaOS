8e469840f361ec7aa229886aa4ab6288e633862a
krishpranav <krisna.pranav@gmail.com>
2021-09-22 13:36:28 +0530

    userland: tests testobjc define

---
+++ userland/tests/testobjc/define.m
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <libfoundation/NSObject.h>
+#include <libobjc/helpers.h>
+#include <stdio.h>
+
+#define HELLO 1
