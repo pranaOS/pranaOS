6698a5eae0fd4df8984a55fc06ff8439f572ed34
krishpranav <krisna.pranav@gmail.com>
2021-09-22 13:31:30 +0530

    userland: tests testobjc typedef

---
+++ userland/tests/testobjc/typedef.m
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
+#include <libfoundation/NSObject.h>
+#include <libobjc/helpers.h>
+#include <stdio.h>
+
+
+int main() {
+    
+}
