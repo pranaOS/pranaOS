801cc787ff914fe0daa2818868e0ffb8cdaac849
krishpranav <krisna.pranav@gmail.com>
2021-09-22 16:24:44 +0530

    docs: docdep build-docdep pl

--- docs/docdep/build-docdep.pl
+++ docs/docdep/build-docdep.pl
@@ -1,5 +1,7 @@
-use %include = ();
-use %included = ();
+#!/usr/bin/perl
+
+my %include = ();
+my %included = ();
 
 for my $text (<*.txt>) {
     open I, '<', $text || die "cannot read: $text";
@@ -13,4 +15,29 @@ for my $text (<*.txt>) {
 	}
     }
     close I;
+}
+
+my $changed = 1;
+while ($changed) {
+    $changed = 0;
+    while (my ($text, $included) = each %include) {
+	for my $i (keys %$included) {
+	    if (exists $include{$i}) {
+		for my $j (keys %{$include{$i}}) {
+		    if (!exists $include{$text}{$j}) {
+			$include{$text}{$j} = 1;
+			$included{$j} = 1;
+			$changed = 1;
+		    }
+		}
+	    }
+	}
+    }
+}
+
+while (my ($text, $included) = each %include) {
+    if (! exists $included{$text} &&
+	(my $base = $text) =~ s/\.txt$//) {
+	print "$base.html $base.xml : ", join(" ", keys %$included), "\n";
+    }
 }
