08c116d3c5b833a50f581c241eba7fb9536a1f3d
krishpranav <krisna.pranav@gmail.com>
2021-09-22 13:34:49 +0530

    userland: tests testobjc typedef

--- userland/tests/testobjc/typedef.m
+++ userland/tests/testobjc/typedef.m
@@ -10,6 +10,22 @@
 #include <stdio.h>
 
 
+typedef struct About {
+    NSString *author;
+    NSString *language;
+    NSString *operatingsystem
+} About;
+
 int main() {
+    About about;
     
+    about.author = "Krisna Pranav";
+    about.language = "C / C++";
+    about.operatingsystem = "pranaOS";
+    
+    printf("Author Name: %@\n", about.author);
+    printf("Language: %@\n", about.language);
+    printf("Operating System: %@\n", about.operatingsystem);
+    
+    return 0;
 }
