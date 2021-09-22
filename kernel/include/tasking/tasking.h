2c61f93f69e3ec32d643cc4394a46436729ea2aa
krishpranav <krisna.pranav@gmail.com>
2021-09-22 11:45:17 +0530

    fix: kernel include tasking #ifndef -> #pragma once
    fix: #ifndef -> #pragma once
    issue: #99
    author: @krishpranav

--- kernel/include/tasking/tasking.h
+++ kernel/include/tasking/tasking.h
@@ -4,7 +4,8 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#pragma once
+#ifndef _KERNEL_TASKING_TASKING_H
+#define _KERNEL_TASKING_TASKING_H
 
 #include <drivers/generic/fpu.h>
 #include <fs/vfs.h>
@@ -72,3 +73,5 @@ int signal_rem_pending(thread_t* thread, int signo);
 
 int signal_restore_thread_after_handling_signal(thread_t* thread);
 int signal_dispatch_pending(thread_t* thread);
+
+#endif // _KERNEL_TASKING_TASKING_H
