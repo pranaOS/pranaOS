f3c56eb49303e38b64cd4c0ab110ec2e14f0d1c9
krishpranav <krisna.pranav@gmail.com>
2021-09-22 13:51:08 +0530

    userland: utilities osfetch

--- userland/utilities/osfetch/main.cpp
+++ userland/utilities/osfetch/main.cpp
@@ -2,7 +2,9 @@
 
 int main()
 {
-    printf("hello world");
+    printf("pranaOS leopard version\n");
+    printf("stable release\n");
+    printf("check out: https://github.com/pranaOS/pranaOS\n");
     
     return 0;
 }
