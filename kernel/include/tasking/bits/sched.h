2893d371a8d3c39d45ab5f089f0cd8742fa585f1
krishpranav <krisna.pranav@gmail.com>
2021-09-22 11:48:43 +0530

    fix: kernel: include tasking #ifndef -> #pragma once
    fix: #ifndef -> #pragma once
    issue: #99
    author: @krishpranav

--- kernel/include/tasking/bits/sched.h
+++ kernel/include/tasking/bits/sched.h
@@ -4,8 +4,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#ifndef _KERNEL_TASKING_BITS_SCHED_H
-#define _KERNEL_TASKING_BITS_SCHED_H
+#pragma once
 
 #define MAX_PRIO 0
 #define MIN_PRIO 11
@@ -31,5 +30,3 @@ struct sched_data {
     int enqueued_tasks;
 };
 typedef struct sched_data sched_data_t;
-
-#endif // _KERNEL_TASKING_BITS_SCHED_H
