a4b2b3a16e756ac2bdf4a2cdecffcf4d22eda67e
krishpranav <krisna.pranav@gmail.com>
2021-09-24 17:08:00 +0530

    libs: libutils never destroyed header

--- libs/libutils/neverdestroyed.h
+++ libs/libutils/neverdestroyed.h
@@ -1,28 +1,42 @@
+/*
+* Copyright (c) 2021, Krisna Pranav
+*
+* SPDX-License-Identifier: BSD-2-Clause
+*/
+
 #pragma once
 
+// includes
 #include <libutils/noncopyable.h>
 #include <libutils/types.h>
 
 namespace Utils {
 
+template<typename T>
 class NeverDestroyed {
-    MAKE_NONCOPYABLE(NeverDestroyed);
-    MAKE_NONCOPYABLE(NeverDestroyed);
+   MAKE_NONCOPYABLE(NeverDestroyed);
+   MAKE_NONMOVABLE(NeverDestroyed);
 
 public:
-    template <typename... Args>
-    NeverDestroyed(Args&&... args)
-    {
-        new (storage) T(forward<Args>(args)...);
-    }
+   template<typename... Args>
+   NeverDestroyed(Args&&... args)
+   {
+       new (storage) T(forward<Args>(args)...);
+   }
 
-    ~NeverDestroyed() = default;
+   ~NeverDestroyed() = default;
 
-    T* operator->() { return &get(); }
-    const T* operator->() const { return &get(); }
+   T* operator->() { return &get(); }
+   const T* operator->() const { return &get(); }
+
+   T& operator*() { return get(); }
+   const T& operator*() const { return get(); }
+
+   T& get() { return reinterpret_cast<T&>(storage); }
+   const T& get() const { return reinterpret_cast<T&>(storage); }
 
 private:
-    alignas(T) u8 storage[sizeof(T)];
+   alignas(T) u8 storage[sizeof(T)];
 };
 
 }
