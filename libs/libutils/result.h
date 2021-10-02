932739bcd4cc8d50a21bde7e5406983a96a31930
krishpranav <krisna.pranav@gmail.com>
2021-10-02 17:12:22 +0530

    libs: libutils result header

--- libs/libutils/result.h
+++ libs/libutils/result.h
@@ -1,9 +1,47 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
 #pragma once
+
+// includes
+#include <libutils/assertions.h>
+#include <libutils/optional.h>
 
 namespace Utils {
 
 template <typename ValueT, typename ErrorT>
 class [[nodiscard]] Result {
+
+public:
+    using ValueType = ValueT;
+    using ErrorType = ErrorT;
+
+    Result(const ValueType& res)
+        : m_result(res)
+    {
+    }
+
+    Result(ValueType&& res)
+        : m_result(move(res))
+    {
+    }
+
+    Result(const ErrorType& error)
+        : m_error(error)
+    {
+    }
+
+    Result(ErrorType&& error)
+        : m_error(move(error))
+    {
+    }
+
+    Result(Result&& other) = default;
+    Result(const Result&& other) = default;
+    ~Result() = default;
 
 };
 
