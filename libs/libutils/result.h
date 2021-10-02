8dfea150573deaf07d62814f3a2b577b326a01f7
krishpranav <krisna.pranav@gmail.com>
2021-10-02 17:16:28 +0530

    libs: libutils result header

--- libs/libutils/result.h
+++ libs/libutils/result.h
@@ -12,9 +12,8 @@
 
 namespace Utils {
 
-template <typename ValueT, typename ErrorT>
+template<typename ValueT, typename ErrorT>
 class [[nodiscard]] Result {
-
 public:
     using ValueType = ValueT;
     using ErrorType = ErrorT;
@@ -40,9 +39,80 @@ public:
     }
 
     Result(Result&& other) = default;
-    Result(const Result&& other) = default;
+    Result(const Result& other) = default;
     ~Result() = default;
 
+    ValueType& value()
+    {
+        return m_result.value();
+    }
+
+    ErrorType& error()
+    {
+        return m_error.value();
+    }
+
+    bool is_error() const
+    {
+        return m_error.has_value();
+    }
+
+    ValueType release_value()
+    {
+        return m_result.release_value();
+    }
+
+    ErrorType release_error()
+    {
+        return m_error.release_value();
+    }
+
+private:
+    Optional<ValueType> m_result;
+    Optional<ErrorType> m_error;
+};
+
+// Partial specialization for void value type
+template<typename ErrorT>
+class [[nodiscard]] Result<void, ErrorT> {
+public:
+    using ValueType = void;
+    using ErrorType = ErrorT;
+
+    Result(const ErrorType& error)
+        : m_error(error)
+    {
+    }
+
+    Result(ErrorType&& error)
+        : m_error(move(error))
+    {
+    }
+
+    Result() = default;
+    Result(Result&& other) = default;
+    Result(const Result& other) = default;
+    ~Result() = default;
+
+    ErrorType& error()
+    {
+        return m_error.value();
+    }
+
+    bool is_error() const
+    {
+        return m_error.has_value();
+    }
+
+    ErrorType release_error()
+    {
+        return m_error.release_value();
+    }
+
+private:
+    Optional<ErrorType> m_error;
 };
 
 }
+
+using Utils::Result;
