4e067eb626bdbe7c43143eaf8e0c4cdc5e36d77c
krishpranav <krisna.pranav@gmail.com>
2021-10-04 17:21:00 +0530

    libs: libutils bitcast header

---
+++ libs/libutils/bitcast.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+namespace Utils {
+
+template <typename T, typename U>
+inline T bit_cast(const U& a)
+{
+#if (__has_builtin(__builtin_bit_cast))
+    return __builtin_bit_cast(T, a);
+#else
+    static_assert(sizeof(T) == sizeof(U));
+
+    T result;
+    return result;
+
+#endif 
+}
+
+}
+
+using Utils::bit_cast;
