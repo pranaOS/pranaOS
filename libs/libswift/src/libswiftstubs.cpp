9ec0929196e7384236625a4e7351949c9c8f203f
krishpranav <krisna.pranav@gmail.com>
2021-10-04 17:54:04 +0530

    libs: libswift src stubs

---
+++ libs/libswift/src/libswiftstubs.cpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ * Copyright (c) 2014 - 2021 Apple Inc. and the Swift project authors
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+extern "C" {
+
+void initializeLibSwift();
+
+}
+
+void initializeLibSwift() {}
