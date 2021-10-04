91d0b5b19be6b2f578c29823243a64c230282fa9
krishpranav <krisna.pranav@gmail.com>
2021-10-04 17:31:05 +0530

    libs: libutils typelist header

--- libs/libutils/typelist.h
+++ libs/libutils/typelist.h
@@ -11,10 +11,10 @@
 
 namespace Utils {
 
-template <typename... Types>
+template<typename... Types>
 struct TypeList;
 
-template <unsigned Index, typename List>
+template<unsigned Index, typename List>
 struct TypeListElement;
 
 template<unsigned Index, typename Head, typename... Tail>
@@ -22,22 +22,53 @@ struct TypeListElement<Index, TypeList<Head, Tail...>>
     : TypeListElement<Index - 1, TypeList<Tail...>> {
 };
 
-template <typename Head, typename... Tail>
+template<typename Head, typename... Tail>
 struct TypeListElement<0, TypeList<Head, Tail...>> {
     using Type = Head;
 };
 
-template <typename... Types>
+template<typename... Types>
 struct TypeList {
     static constexpr unsigned size = sizeof...(Types);
 
-    template <unsigned N>
+    template<unsigned N>
+    using Type = typename TypeListElement<N, TypeList<Types...>>::Type;
 };
 
-template <typename T>
+template<typename T>
 struct TypeWrapper {
     using Type = T;
 };
 
+template<typename List, typename F, unsigned... Indices>
+constexpr void for_each_type_impl(F&& f, IndexSequence<Indices...>)
+{
+    (forward<F>(f)(TypeWrapper<typename List::template Type<Indices>> {}), ...);
+}
+
+template<typename List, typename F>
+constexpr void for_each_type(F&& f)
+{
+    for_each_type_impl<List>(forward<F>(f), MakeIndexSequence<List::size> {});
+}
+
+template<typename ListA, typename ListB, typename F, unsigned... Indices>
+constexpr void for_each_type_zipped_impl(F&& f, IndexSequence<Indices...>)
+{
+    (forward<F>(f)(TypeWrapper<typename ListA::template Type<Indices>> {}, TypeWrapper<typename ListB::template Type<Indices>> {}), ...);
+}
+
+template<typename ListA, typename ListB, typename F>
+constexpr void for_each_type_zipped(F&& f)
+{
+    static_assert(ListA::size == ListB::size, "Can't zip TypeLists that aren't the same size!");
+    for_each_type_zipped_impl<ListA, ListB>(forward<F>(f), MakeIndexSequence<ListA::size> {});
+}
 
 }
+
+using Utils::for_each_type;
+using Utils::for_each_type_zipped;
+using Utils::TypeList;
+using Utils::TypeListElement;
+using Utils::TypeWrapper;
