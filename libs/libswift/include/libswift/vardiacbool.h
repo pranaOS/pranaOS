747adc058ce498295f99b4e44287f1efa678b894
krishpranav <krisna.pranav@gmail.com>
2021-10-04 19:44:29 +0530

    libs: libswift include vardiacbool header

---
+++ libs/libswift/include/libswift/vardiacbool.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+extern int numberOfTrues(int count, va_list arguments);
