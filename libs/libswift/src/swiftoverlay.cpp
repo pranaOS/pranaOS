1cab06807183999583158c2cb81955d7d730bd3f
krishpranav <krisna.pranav@gmail.com>
2021-10-06 17:11:17 +0530

    libs: libswift src swiftoverlay

--- libs/libswift/src/swiftoverlay.cpp
+++ libs/libswift/src/swiftoverlay.cpp
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// includes
 #include <stdio.h>
 #include <errno.h>
 #include <sys/types.h>
@@ -14,3 +21,10 @@ static inline int _swift_stdlib_getErrno() {
     return errno;
 }
 
+static inline void _swift_stdlib_setErrno(int value) {
+    errno = value;
+}
+
+#if __has_feature(nullability)
+#pragma clang assume_nonnull end
+#endif
