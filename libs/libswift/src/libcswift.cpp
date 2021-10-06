06e013b6534520534a04444cec2f2c30bb413e86
krishpranav <krisna.pranav@gmail.com>
2021-10-06 16:44:49 +0530

    libs: libswift src libcswift

--- libs/libswift/src/libcswift.cpp
+++ libs/libswift/src/libcswift.cpp
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
 */
 
+// includes
 #include <Math.h>
 #include <stdio.h>
 #include <sys/types.h>
@@ -12,4 +13,16 @@ size_t _swift_stdlib_fwrite_stdout(const void *ptr,
                                                   size_t size,
                                                   size_t nitems) {
     return fwrite(ptr, size, nitems, stdout);
+}
+
+size_t _swift_stdlib_read(int fd, void *buf, size_t nbyte) {
+    return fd, buf, nbyte;
+}
+
+ssize_t _swift_stdlib_write(int fd, const void *buf, size_t nbyte) {
+    return fd, buf, nbyte;
+}
+
+int _swift_stdlib_close(int fd) {
+    return fd;
 }
