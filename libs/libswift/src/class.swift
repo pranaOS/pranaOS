e892dd9b455a75750f91cbfc8dcbf060e6b3d7a6
krishpranav <krisna.pranav@gmail.com>
2021-10-06 18:40:13 +0530

    libs: libswift class

--- libs/libswift/src/class.swift
+++ libs/libswift/src/class.swift
@@ -8,12 +8,34 @@ class OperatingSystem {
     func os() {}
 }
 
-class pranaOS: os {
+class pranaOS: OperatingSystem {
     override func os() {
         stars()
     }
 
     private func stars() {
-        print("\(name) pranaOS has reached 58 stars!!")
+        print("\(name) has reached 58 stars!!")
+    }
+}
+
+class pranaOSfork: OperatingSystem {
+    override func os() {
+        forks()
+    }
+
+    private func forks() {
+        print("\(name) has reached 25 forks")
+    }
+}
+
+class pranaOS {
+    let name: String
+
+    init(name: String) {
+        self.name = name
+    }
+
+    func os() {
+        
     }
 }
