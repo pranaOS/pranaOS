4857839f59bc88fd165c3c063363a156bd048f7a
krishpranav <krisna.pranav@gmail.com>
2021-10-08 18:07:06 +0530

    libs: libutils kstdio header

--- libs/libutils/kstdio.h
+++ libs/libutils/kstdio.h
@@ -3,3 +3,15 @@
  *
  * SPDX-License-Identifier: BSD-2-Clause
 */
+
+#pragma once
+
+// includes
+#include <libutils/types.h>
+#include <stdarg.h>
+
+extern "C" {
+
+void dbgputstr(const char*, size_t);
+
+}
