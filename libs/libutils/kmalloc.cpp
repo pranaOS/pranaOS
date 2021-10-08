e15b73a6d09aee292651f83a6f9c207db8c50b6b
krishpranav <krisna.pranav@gmail.com>
2021-10-08 18:01:16 +0530

    libs: libutils kmalloc

--- libs/libutils/kmalloc.cpp
+++ libs/libutils/kmalloc.cpp
@@ -9,6 +9,28 @@
 #   include <libutils/assertions.h>
 #   include <libutils/kmalloc.h>
 
+void* operator new(size_t size)
+{
+    void* ptr = malloc(size);
+    VERIFY(ptr);
+    return ptr;
+}
+
+void* operator new(size_t size, const std::nothrow_t&) noexcept
+{
+    return malloc(size);
+}
+
+void operator delete(void* ptr) noexcept
+{
+    return free(ptr);
+}
+
+void* operator new[](size_t size)
+{
+    void* ptr = malloc(size);
+    return ptr;
+}
 
 
 #endif
