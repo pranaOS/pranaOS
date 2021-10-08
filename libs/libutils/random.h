5135f322b26b93a4f3ebb60269ce76bf16144b87
krishpranav <krisna.pranav@gmail.com>
2021-10-08 17:25:09 +0530

    libs: libutils random header

--- libs/libutils/random.h
+++ libs/libutils/random.h
@@ -6,34 +6,46 @@
 
 #pragma once
 
+// includes
 #include <libutils/platform.h>
 #include <libutils/types.h>
 
 #if defined(__pranaOS__)
-#   include <stdlib.h>
+#    include <stdlib.h>
 #endif
 
 #if defined(__unix__)
-#   include <unistd.h>
+#    include <unistd.h>
 #endif
 
 #if defined(UTILS_OS_MACOS)
-#   include <sys/random.h>
+#    include <sys/random.h>
 #endif
 
 namespace Utils {
 
-
 inline void fill_with_random([[maybe_unused]] void* buffer, [[maybe_unused]] size_t length)
 {
-    #if defined(__pranaOS__)
-        arc4random_buf(buffer, length);
-    #elif defined(OSS_FUZZ)
-    #elif defined(__unix__) or defined(AK_OS_MACOS)
-         [[maybe_unused]] int rc = getentropy(buffer, length);
-    #endif
+#if defined(__serenity__)
+    arc4random_buf(buffer, length);
+#elif defined(OSS_FUZZ)
+#elif defined(__unix__) or defined(UTILS_OS_MACOS)
+    [[maybe_unused]] int rc = getentropy(buffer, length);
+#endif
 }
+
+template<typename T>
+inline T get_random()
+{
+    T t;
+    fill_with_random(&t, sizeof(T));
+    return t;
+}
+
+u32 get_random_uniform(u32 max_bounds);
 
 }
 
 using Utils::fill_with_random;
+using Utils::get_random;
+using Utils::get_random_uniform;
