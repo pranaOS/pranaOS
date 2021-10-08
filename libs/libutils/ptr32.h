641b01afc3d50d2d546c406a18404dcfc40a069f
krishpranav <krisna.pranav@gmail.com>
2021-10-08 16:52:36 +0530

    libs: libutils ptr32 header

---
+++ libs/libutils/ptr32.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+#include <libutils/assertions.h>
+#include <libutils/types.h>
+
+namespace Utils {
+    
+}
