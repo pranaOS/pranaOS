ae00647a259990ccf1e0cd585b6ad44a2d56a3fb
krishpranav <krisna.pranav@gmail.com>
2021-10-08 16:42:00 +0530

    libs: libutils stringutils header

--- libs/libutils/stringutils.h
+++ libs/libutils/stringutils.h
@@ -9,7 +9,6 @@
 // includes
 #include <libutils/forward.h>
 
-
 namespace Utils {
 
 enum class CaseSensitivity {
@@ -44,10 +43,38 @@ struct MaskSpan {
 
 namespace StringUtils {
 
-enum class SearchDirection {
-    Forward,
-    Backward
-};
+    bool matches(const StringView& str, const StringView& mask, CaseSensitivity = CaseSensitivity::CaseInsensitive, Vector<MaskSpan>* match_spans = nullptr);
+    template<typename T = int>
+    Optional<T> convert_to_int(const StringView&, TrimWhitespace = TrimWhitespace::Yes);
+    template<typename T = unsigned>
+    Optional<T> convert_to_uint(const StringView&, TrimWhitespace = TrimWhitespace::Yes);
+    template<typename T = unsigned>
+    Optional<T> convert_to_uint_from_hex(const StringView&, TrimWhitespace = TrimWhitespace::Yes);
+    bool equals_ignoring_case(const StringView&, const StringView&);
+    bool ends_with(const StringView& a, const StringView& b, CaseSensitivity);
+    bool starts_with(const StringView&, const StringView&, CaseSensitivity);
+    bool contains(const StringView&, const StringView&, CaseSensitivity);
+    bool is_whitespace(const StringView&);
+    StringView trim(const StringView& string, const StringView& characters, TrimMode mode);
+    StringView trim_whitespace(const StringView& string, TrimMode mode);
+
+    Optional<size_t> find(StringView const& haystack, char needle, size_t start = 0);
+    Optional<size_t> find(StringView const& haystack, StringView const& needle, size_t start = 0);
+    Optional<size_t> find_last(StringView const& haystack, char needle);
+    Vector<size_t> find_all(StringView const& haystack, StringView const& needle);
+
+    enum class SearchDirection {
+        Forward,
+        Backward
+    };
+
+    Optional<size_t> find_any_of(StringView const& haystack, StringView const& needles, SearchDirection);
+
+    String to_snakecase(const StringView&);
+    String to_titlecase(StringView const&);
+
+    String replace(StringView const&, StringView const& needle, StringView const& replacement, bool all_occurrences = false);
+    size_t count(StringView const&, StringView const& needle);
 
 }
 
