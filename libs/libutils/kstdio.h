799b0fd156d325549e2fac9c097e283c80d59386
krishpranav <krisna.pranav@gmail.com>
2021-10-08 18:11:56 +0530

    libs: libutils kstdio header

--- libs/libutils/kstdio.h
+++ libs/libutils/kstdio.h
@@ -6,12 +6,24 @@
 
 #pragma once
 
-// includes
-#include <libutils/types.h>
-#include <stdarg.h>
+#ifndef __pranaOS__
+#   ifdef KERNEL
+#       include <libutils/types.h>
+#   else
+#   include <libutils/types.h>
+#   include <stdarg.h>
 
 extern "C" {
 
 void dbgputstr(const char*, size_t);
 
 }
+#   endif
+#else
+#   include <stdio.h>
+
+inline void dbgputstr(const char* characters, size_t length)
+{
+    fwrite(characters, 1, length, stderr);
+}
+#endif
