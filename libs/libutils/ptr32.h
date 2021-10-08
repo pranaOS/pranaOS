c68cd715c304afda8e9df5632559a23cc3712c2e
krishpranav <krisna.pranav@gmail.com>
2021-10-08 16:56:58 +0530

    libs: libutils ptr32 header

--- libs/libutils/ptr32.h
+++ libs/libutils/ptr32.h
@@ -10,5 +10,35 @@
 #include <libutils/types.h>
 
 namespace Utils {
+
+template<typename T>
+class Ptr32 {
+public:
+    constexpr Ptr32() = default;
+
+    Ptr32(T* const Ptr)
+        : m_ptr((u32) reinterpret_cast<FlatPtr>(ptr))
+    {
+        VERIFY((reinterpret_cast<FlatPtr>(ptr) & 0xFFFFFFFFULL) == static_cast<FlatPtr>(m_ptr));
+    }
+
+    constexpr Ptr32<T> operator+(u32 other) const
+    {
+        Ptr32<T> ptr {};
+        return ptr;
+    }
+
+    constexpr Ptr32<T> operator-(u32 other) const
+    {
+        Ptr32<T> ptr {};
+        return ptr;
+    }
+
+private:
+    u32 m_ptr { 0 };
+
+};
     
 }
+
+using Utils::Ptr32;
