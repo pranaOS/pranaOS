682d40243a3c2d55a3a0753b1dbeb5da7e538ad3
krishpranav <krisna.pranav@gmail.com>
2021-10-08 17:56:43 +0530

    libs: libutils kmalloc cpp

---
+++ libs/libutils/kmalloc.cpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#if defined(__pranaOS__) && !defined(KERNEL)
+
+#   include <libutils/assertions.h>
+#   include <libutils/kmalloc.h>
+
+
+
+#endif
