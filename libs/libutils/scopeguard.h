257f971a8fafbf2452c34eed2b9cb5d8f77b423e
krishpranav <krisna.pranav@gmail.com>
2021-10-08 17:40:46 +0530

    libs: libutils scopeguard header

--- libs/libutils/scopeguard.h
+++ libs/libutils/scopeguard.h
@@ -11,11 +11,12 @@
 
 namespace Utils {
 
+template<typename Callback>
 class ScopeGuard {
 public:
     ScopeGuard(Callback callback)
         : m_callback(move(callback))
-    {        
+    {
     }
 
     ~ScopeGuard()
@@ -27,6 +28,28 @@ private:
     Callback m_callback;
 };
 
+template<typename Callback>
+class ArmedScopeGuard {
+public:
+    ArmedScopeGuard(Callback callback)
+        : m_callback(move(callback))
+    {
+    }
+
+    ~ArmedScopeGuard()
+    {
+        if (m_armed)
+            m_callback();
+    }
+
+    void disarm() { m_armed = false; }
+
+private:
+    Callback m_callback;
+    bool m_armed { true };
+};
+
 }
 
+using Utils::ArmedScopeGuard;
 using Utils::ScopeGuard;
