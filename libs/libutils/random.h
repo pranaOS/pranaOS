9e6a6108f751b4f78f2164e67d38d8a39c203da6
krishpranav <krisna.pranav@gmail.com>
2021-10-08 17:20:30 +0530

    libs: libutils random header

--- libs/libutils/random.h
+++ libs/libutils/random.h
@@ -20,3 +20,20 @@
 #if defined(UTILS_OS_MACOS)
 #   include <sys/random.h>
 #endif
+
+namespace Utils {
+
+
+inline void fill_with_random([[maybe_unused]] void* buffer, [[maybe_unused]] size_t length)
+{
+    #if defined(__pranaOS__)
+        arc4random_buf(buffer, length);
+    #elif defined(OSS_FUZZ)
+    #elif defined(__unix__) or defined(AK_OS_MACOS)
+         [[maybe_unused]] int rc = getentropy(buffer, length);
+    #endif
+}
+
+}
+
+using Utils::fill_with_random;
