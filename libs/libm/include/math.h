a7abe8f19d390039383db40c098f4d882176e631
krishpranav <krisna.pranav@gmail.com>
2021-10-11 17:47:10 +0530

    libs: libm include math header
    if target running using cplusplus NOEXCEPT noexcept

---
+++ libs/libm/include/math.h
@@ -0,0 +1,16 @@
+/*
+* Copyright (c) 2021, Krisna Pranav
+*
+* SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+#include <limits.h>
+#include <sys/cdefs.h>
+
+#if __cplusplus >= 201103L
+#   define NOEXCEPT noexcept
+#else
+#   define NOEXCEPT
+#endif 
