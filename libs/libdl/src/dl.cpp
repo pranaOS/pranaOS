f08f113ac7f5a7fcc8e9ef95d3c3297a06d19f84
krishpranav <krisna.pranav@gmail.com>
2021-10-11 18:37:16 +0530

    libs: libdl src dl cpp

--- libs/libdl/src/dl.cpp
+++ libs/libdl/src/dl.cpp
@@ -4,14 +4,23 @@
 * SPDX-License-Identifier: BSD-2-Clause
 */
 
+// includes
 #include <libc/include/string.h>
 #include <libutils/types.h>
 #include <libdl/include/dl.h>
 #include <libdl/include/dl_integeration.h>
 #include <string.h>
 
+/* dlerror text + retrived thread*/
 __thread char* s_dlerror_text = NULL;
-__thread bool s_dlerror_retrived = NULL;
+__thread bool s_dlerror_retrieved = false;
+
+static void store_error(const String& error)
+{
+    free(s_dlerror_text);
+    s_dlerror_text = strdup(error.characters());
+    s_dlerror_retrieved = false;
+}
 
 int dlclose(void* handle)
 {
@@ -20,17 +29,45 @@ int dlclose(void* handle)
         store_error(result.error().text);
         return -1;
     }
-
     return 0;
 }
 
-void* dlsym(void* handle)
+char* dlerror()
 {
-    auto result = __dlsym(handle);
+    if (s_dlerror_retrieved) {
+        free(s_dlerror_text);
+        s_dlerror_text = nullptr;
+    }
+    s_dlerror_retrieved = true;
+    return const_cast<char*>(s_dlerror_text);
+}
+
+void* dlopen(const char* filename, int flags)
+{
+    auto result = __dlopen(filename, flags);
+    if (result.is_error()) {
+        store_error(result.error().text);
+        return nullptr;
+    }
+    return result.value();
+}
+
+void* dlsym(void* handle, const char* symbol_name)
+{
+    auto result = __dlsym(handle, symbol_name);
+    if (result.is_error()) {
+        store_error(result.error().text);
+        return nullptr;
+    }
+    return result.value();
+}
+
+int dladdr(void* addr, Dl_info* info)
+{
+    auto result = __dladdr(addr, info);
     if (result.is_error()) {
         store_error(result.error().text);
         return 0;
     }
-
-    return result.value();
+    return 1;
 }
