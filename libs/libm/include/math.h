94b4a392884b70c39271f86b6e9eee51002b188c
krishpranav <krisna.pranav@gmail.com>
2021-10-11 17:49:21 +0530

    libs: libm include math header
    defined some math funcs

--- libs/libm/include/math.h
+++ libs/libm/include/math.h
@@ -13,4 +13,21 @@
 #   define NOEXCEPT noexcept
 #else
 #   define NOEXCEPT
-#endif 
+#endif
+
+__BEGIN_DECLS
+
+/* FIXME: define some math funcs + float funcs */
+
+#define MATH_ERRNO 1
+#define MATH_ERREXCEPT 2
+#define math_errhandling MATH_ERREXCEPT
+
+#define HUGE_VALF __builtin_huge_valf()
+#define HUGE_VAL __builtin_huge_val()
+#define HUGE_VALL __builtin_huge_vall()
+#define INFINITY __builtin_huge_valf()
+#define NAN __builtin_nan("")
+#define MAXFLOAT FLT_MAX
+
+__END_DECLS
