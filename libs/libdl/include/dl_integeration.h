ce3aae2789ddc6e049de5cffdd8aa9d5a0ee39b9
krishpranav <krisna.pranav@gmail.com>
2021-10-11 18:25:21 +0530

    libs: libdl include dl_integeration
    typedef result added dl error message

--- libs/libdl/include/dl_integeration.h
+++ libs/libdl/include/dl_integeration.h
@@ -17,3 +17,8 @@ struct DlErrorMessage {
 struct __Dl_info;
 typedef struct __Dl_info Dl_info;
 
+typedef Result<void, DlErrorMessage> (*DlCloseFunction)(void*);
+typedef Result<void*, DlErrorMessage> (*DlOpenFunction)(const char*, int);
+typedef Result<void*, DlErrorMessage> (*DlSymFunction)(void*, const char*);
+typedef Result<void, DlErrorMessage> (*DlAddrFunction)(void*, Dl_info*);
+
