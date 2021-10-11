8f188e2c5294ea940bc23653f8a7d07dc094e95c
krishpranav <krisna.pranav@gmail.com>
2021-10-11 18:26:20 +0530

    libs: libdl include dl integeration header

--- libs/libdl/include/dl_integeration.h
+++ libs/libdl/include/dl_integeration.h
@@ -1,24 +1,39 @@
+/*
+* Copyright (c) 2021, Krisna Pranav
+*
+* SPDX-License-Identifier: BSD-2-Clause
+ */
+
 #pragma once
 
-#include <libc/include/string.h>
+// includes
 #include <libutils/result.h>
+#include <libc/include/string.h>
 
 struct DlErrorMessage {
-    DlErrorMessage(String&& other)
-        : text(move(other))
-    {
-    }
+   DlErrorMessage(String&& other)
+       : text(move(other))
+   {
+   }
 
-    virtual ~DlErrorMessage() {}
+   virtual ~DlErrorMessage() { }
 
-    String text;
+   String text;
 };
 
 struct __Dl_info;
 typedef struct __Dl_info Dl_info;
 
+/* Error Message */
 typedef Result<void, DlErrorMessage> (*DlCloseFunction)(void*);
 typedef Result<void*, DlErrorMessage> (*DlOpenFunction)(const char*, int);
 typedef Result<void*, DlErrorMessage> (*DlSymFunction)(void*, const char*);
 typedef Result<void, DlErrorMessage> (*DlAddrFunction)(void*, Dl_info*);
 
+/* call */
+extern "C" {
+extern DlCloseFunction __dlclose;
+extern DlOpenFunction __dlopen;
+extern DlSymFunction __dlsym;
+extern DlAddrFunction __dladdr;
+}
