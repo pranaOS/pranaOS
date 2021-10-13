346fa6c5b21d2e2d86b6994cfed5937e202862d4
krishpranav <krisna.pranav@gmail.com>
2021-10-13 17:19:55 +0530

    libs: libutils hex header

--- libs/libutils/hex.h
+++ libs/libutils/hex.h
@@ -6,11 +6,11 @@
 
 #pragma once
 
-// includes
+/* includes */
 #include <libutils/bytebuffer.h>
 #include <libutils/optional.h>
-#include <string>
 #include <libutils/stringutils.h>
+#include <string>
 
 namespace Utils {
 
@@ -18,12 +18,19 @@ constexpr u8 decode_hex_digit(char digit)
 {
     if (digit >= '0' && digit <= '9')
         return digit - '0';
+    if (digit >= 'a' && digit <= 'f')
+        return 10 + (digit - 'a');
+    if (digit >= 'A' && digit <= 'F')
+        return 10 + (digit - 'A');
     return 255;
 }
+
+Optional<ByteBuffer> decode_hex(const StringView&);
 
 String encode_hex(ReadonlyBytes);
 
 }
 
+using Utils::decode_hex;
 using Utils::decode_hex_digit;
 using Utils::encode_hex;
