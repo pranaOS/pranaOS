f6cee04948a80f30bec4c4581dba12075605568a
Olivia <88483682+OliviaCE@users.noreply.github.com>
2021-10-13 17:01:20 +0530

    window_server: MenuBar header
    added inline bool values + private funcs upgraded

--- servers/window_server/shared/MessageContent/MenuBar.h
+++ servers/window_server/shared/MessageContent/MenuBar.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Krisna Pranav
+ * Copyright (c) 2021, Krisna Pranav, OliviaCE
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -43,32 +43,81 @@ public:
         : m_flags(uint32_t(attr))
         , m_color(clr)
     {
-
     }
 
-    explicit StatusBarStyle(Mode attr, const LG::Color& clr)
-        : m_flags(uint32_t(attr))
+    explicit StatusBarStyle(const LG::Color& clr)
+        : m_flags(0)
         , m_color(clr)
     {
     }
 
     struct StandardLightType {
     };
+    static const StandardLightType StandardLight;
+    StatusBarStyle(StandardLightType)
+        : m_flags(0)
+        , m_color(LG::Color::LightSystemBackground)
+    {
+    }
+
+    struct StandardOpaqueType {
+    };
     static const StandardOpaqueType StandardOpaque;
-    StatusBarStyle(StandardOpaque)
+    StatusBarStyle(StandardOpaqueType)
         : m_flags(0)
         , m_color(LG::Color::Opaque)
     {
     }
 
-    inline bool hide_text() const { return has_attr(Mode::HideText); }
-
-
     ~StatusBarStyle() = default;
 
+    inline bool hide_text() const { return has_attr(Mode::HideText); }
+    inline bool show_text() const { return !hide_text(); }
+
+    inline bool light_text() const { return has_attr(Mode::LightText); }
+    inline bool dark_text() const { return !light_text(); }
+
+    inline StatusBarStyle& set_light_text()
+    {
+        set_attr(Mode::LightText);
+        return *this;
+    }
+
+    inline StatusBarStyle& set_dark_text()
+    {
+        rem_attr(Mode::LightText);
+        return *this;
+    }
+
+    inline StatusBarStyle& set_hide_text()
+    {
+        set_attr(Mode::HideText);
+        return *this;
+    }
+
+    inline StatusBarStyle& set_show_text()
+    {
+        rem_attr(Mode::HideText);
+        return *this;
+    }
+
+    inline uint32_t flags() const { return m_flags; }
+    inline void set_flags(uint32_t attr) { m_flags = attr; }
+
+    inline StatusBarStyle& set_mode(Mode attr)
+    {
+        m_flags = (uint32_t)attr;
+        return *this;
+    }
+
+    inline const LG::Color& color() const { return m_color; }
+    inline void set_color(const LG::Color& clr) { m_color = clr; }
+
 private:
-    inline bool has_attr(Mode mode) const { return m_flags; }
+    inline bool has_attr(Mode mode) const { return ((m_flags & (uint32_t)mode) == (uint32_t)mode); }
+    inline void set_attr(Mode mode) { m_flags |= (uint32_t)mode; }
+    inline void rem_attr(Mode mode) { m_flags = m_flags & (~(uint32_t)mode); }
 
     uint32_t m_flags { 0 };
-
+    LG::Color m_color { LG::Color::LightSystemBackground };
 };
