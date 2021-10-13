24b8820fcb3ea4183e04fe9eca1df7985be7c344
krishpranav <krisna.pranav@gmail.com>
2021-10-13 17:23:09 +0530

    libs: libutils hex cpp
    hex cpp

--- libs/libutils/hex.cpp
+++ libs/libutils/hex.cpp
@@ -15,6 +15,31 @@
 
 namespace Utils {
 
+Optional<ByteBuffer> decode_hex(const StringView& input)
+{
+    if ((input.length() % 2) != 0)
+        return {};
+
+    auto output_result = ByteBuffer::create_zeroed(input.length() / 2);
+    if (!output_result.has_value())
+        return {};
+
+    auto& output = output_result.value();
+
+    for (size_t i = 0; i < input.length() / 2; ++i) {
+        const auto c1 = decode_hex_digit(input[i * 2]);
+        if (c1 >= 16)
+            return {};
+
+        const auto c2 = decode_hex_digit(input[i * 2 + 1]);
+        if (c2 >= 16)
+            return {};
+
+        output[i] = (c1 << 4) + c2;
+    }
+
+    return output_result;
+}
 
 String encode_hex(const ReadonlyBytes input)
 {
