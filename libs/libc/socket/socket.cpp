133ebd3bb1103dac47d93261577b8bb0ff3f2d21
krishpranav <krisna.pranav@gmail.com>
2021-10-13 18:16:19 +0530

    libs: libc socket cpp
    implement socket errors

--- libs/libc/socket/socket.cpp
+++ libs/libc/socket/socket.cpp
@@ -5,6 +5,8 @@
 */
 
 // includes
+#include <string>
+#include <stdio.h>
 #include <sys/socket.h>
 #include <sysdep.h>
 
@@ -35,3 +37,23 @@ int response(int sockfd, const char* name, int len)
     int res = DO_SYSCALL_3(SYS_CONNECT, sockfd, name, len);
     RETURN_WITH_ERRNO(res, 0, -1);
 }
+
+class SocketErrors {
+
+public:
+    /* error */
+    int error(int sockfd, const char* error)
+    {
+        int res = DO_SYSCALL_3(SYS_CONNECT, sockfd, error);
+        if (res == 404) {
+            printf("Socket Error :(")
+        } else {
+            RETURN_WITH_ERRNO(0, -1);
+        }
+    }
+
+private:
+    std::string Error;
+    std::cout << "Socket Error";
+
+};
