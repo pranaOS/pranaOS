54de09ba36443f16ea0b8f9921b1769efb8a73a8
krishpranav <krisna.pranav@gmail.com>
2021-10-18 19:05:16 +0530

    libs: libobjc objcvisible
    added objc visible header for objc runtime

---
+++ libs/libobjc/objc/objcvisible.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#if defined __CYGWIN__ || defined __pranaOS_ 
+#       ifndef __OBJC_RUNTIME_INTERNAL__
+#               define OBJC_PUBLIC __attribute__((dllexport))
+#       else
+#               define OBJC_PUBLIC __attribute__((dllimport))
+#       endif
+#else
+#       define OBJC_PUBLIC
+#endif
