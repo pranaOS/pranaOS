2e7e5f4870e68f9bb648953055d14883e3c2dfe8
krishpranav <krisna.pranav@gmail.com>
2021-10-19 11:28:02 +0530

    libs: libobjc objc availabilty header

--- libs/libobjc/objc/availabilty.h
+++ libs/libobjc/objc/availabilty.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
 #if defined(__clang__) && !defined(__OBJC_RUNTIME_INTERNAL__)
 #pragma clang system_header
 #endif
@@ -10,4 +16,8 @@
 #    define OBJC_NONPORTABLE
 #endif
 
-
+#if !defined(__DEPRECATE_DIRECT_ACCESS) || defined(__OBJC_LEGACY_GNU_MODE__) || defined(__OBJC_RUNTIME_INTERNAL__)
+#    define OBJC_DEPRECATED
+#else
+#    define OBJC_DEPRECATED __attribute__((deprecated))
+#endif
