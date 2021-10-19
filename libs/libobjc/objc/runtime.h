0a8cdea3bbc89ce43ff83ca9f8a14bce1e334182
krishpranav <krisna.pranav@gmail.com>
2021-10-19 11:36:52 +0530

    libobjc: gnu step runtime + feature request

--- libs/libobjc/objc/runtime.h
+++ libs/libobjc/objc/runtime.h
@@ -3,3 +3,25 @@
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
+
+#if defined(__clang__) && !defined(__OBJC_RUNTIME_INTERNAL__)
+#pragma clang system_header
+#endif
+
+#pragma once
+
+#ifndef __cplusplus
+extern "C" {
+#endif
+
+#ifndef __GNU_RUNTIME__
+#   define __GNU_RUNTIME__
+#endif
+
+#ifndef __has_feature__
+#   define __has_feature__(x) 0
+#endif
+
+typedef struct objc_class *Class;
+
+}
