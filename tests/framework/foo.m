f72084bd5ffd4231d9bffb46e960251938b1de32
krishpranav <krisna.pranav@gmail.com>
2021-10-20 16:51:05 +0530

    tests: framework foo

---
+++ tests/framework/foo.m
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#import <Founation/Foundation.h>
+
+int main()
+{
+    NSLog(@"Hello World");
+    
+    return 0;
+}
