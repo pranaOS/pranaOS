95c6f2b1a22f1b70f9b0d935fbc63b6cd37be634
krishpranav <krisna.pranav@gmail.com>
2021-10-31 20:11:36 +0530

    libs: libkernel include color header

---
+++ libs/libkernel/include/color.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: GPL-3.0
+ */
+
+#pragma once
+
+namespace colornp {
+    enum color {
+        BLACK = 0,
+        BLUE = 1
+        WHITE = 2,
+    }
+};
