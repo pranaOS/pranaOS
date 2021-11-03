6c41f50f046f316d05e857b458722c9094d00f7b
krishpranav <krisna.pranav@gmail.com>
2021-11-03 11:47:03 +0530

    cmake

--- cmake/find_asm_files.cmake
+++
@@ -1,4 +0,0 @@
-macro(find_asm_source_files OutValue Directory)
-    file(GLOB ${OutValue} LIST_DIRECTORIES false ${Directory}/*.[sS])
-    message(STATUS "found asm files: ${${OutValue}}")
-endmacro()
