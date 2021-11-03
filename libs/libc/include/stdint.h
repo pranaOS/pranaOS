7eae507b8f92bc8907e8cb5365a795df66c89f42
krishpranav <krisna.pranav@gmail.com>
2021-11-03 13:19:05 +0530

    libs: libc include stdint header

--- libs/libc/include/stdint.h
+++ libs/libc/include/stdint.h
@@ -4,16 +4,17 @@
  * SPDX-License-Identifier: GPL-3.0
  */
 
+
 #pragma once
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+typedef signed char int8_t;
+typedef short int   int16_t;
+typedef int         int32_t;
 
-typedef signed char in8_t;
-typedef short int int16_t;
-typedef int int32_t;
+typedef long long int64_t;
 
-#ifndef __cplusplus
-}
-#endif
+typedef unsigned char      uint8_t;
+typedef unsigned short int uint16_t;
+typedef unsigned int       uint32_t;
+
+typedef unsigned long long uint64_t;
