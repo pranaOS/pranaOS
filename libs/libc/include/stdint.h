7c8697d6b5630aaf3bfe32012d20f112f16b97d7
krishpranav <krisna.pranav@gmail.com>
2021-11-03 09:58:03 +0530

    libs: libc include stdint header
    libs for kernel

---
+++ libs/libc/include/stdint.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: GPL-3.0
+ */
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef signed char in8_t;
+typedef short int int16_t;
+typedef int int32_t;
+
+#ifndef __cplusplus
+}
+#endif
