e069260dccf82b09f8d9846ba3d8e8ede462cb51
krishpranav <krisna.pranav@gmail.com>
2021-11-06 12:31:11 +0530

    libs: libc include assert header

--- libs/libc/include/assert.h
+++ libs/libc/include/assert.h
@@ -1,10 +1,11 @@
-/*
- * Copyright (c) 2021, Krisna Pranav
- *
- * SPDX-License-Identifier: GPL-3.0
- */
-
 #pragma once
 
-namespace Assert {
+#include "io.h"
+
+namespace ASSERT {
+    static IO io;
 };
+
+#define assert(e) ((void)((e) ? ((void)0) : __assert(#e, __FILE__, __LINE__)))
+#define __assert(e, file, line)                                                \
+    ((void)ASSERT::io.printf("%s:%d: failed assertion `%s'\n", file, line, e))
