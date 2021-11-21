d810338147cb91d4e7c66030f98b21cbd77ae141
krishpranav <krisna.pranav@gmail.com>
2021-11-21 15:21:09 +0530

    boot: boot 64 main

--- boot/boot64main.c
+++ boot/boot64main.c
@@ -1,3 +1,12 @@
+/**
+ * @file boot64main.c
+ * @version 0.1
+ * @date 2021-11-21
+ * 
+ * @copyright Copyright (c) 2021 Krisna Pranav
+ * 
+ */
+
 typedef unsigned short uint16;
 typedef unsigned int uint32;
 typedef unsigned long uint64;
@@ -6,7 +15,11 @@ typedef void (*go_entry_t)(uint32, uint32);
 
 void boot64main(uint32 gomain, uint32 magic, uint32 mbinfo) {
     go_entry_t go_entry;
+    
+    go_entry = (go_entry_t)((uint64)gomain);
+    go_entry(magic, mbinfo);
+
     for (;;) {
-        
+
     }
 }
