603547e636ff40a3f3a66015922d45d94f795d8d
krishpranav <krisna.pranav@gmail.com>
2021-11-21 20:10:16 +0530

    drivers: clock cmosclock

--- drivers/clock/cmosclock.go
+++ drivers/clock/cmosclock.go
@@ -2,25 +2,27 @@ package clock
 
 import (
 	"github.com/pranaOS/pranaOS/kernel/sys"
+
 	"time"
 )
 
-/* cmos time */
 type CmosTime struct {
 	Second int
 	Minute int
-	Hour int
-	Day int
-	Month int
-	Year int
+	Hour   int
+	Day    int
+	Month  int
+	Year   int
 }
 
 func ReadCmosTime() CmosTime {
 	var t CmosTime
 	for {
 		readCmosTime(&t)
+		if bcdDecode(readCmosSecond()) == t.Second {
+			break
+		}
 	}
-
 	return t
 }
 
@@ -35,4 +37,17 @@ func readCmosTime(t *CmosTime) {
 	t.Hour = bcdDecode(readCmosReg(0x04))
 	t.Minute = bcdDecode(readCmosReg(0x02))
 	t.Second = bcdDecode(readCmosReg(0x00))
+}
+
+func readCmosSecond() int {
+	return readCmosReg(0x00)
+}
+
+func bcdDecode(v int) int {
+	return v&0x0F + v/16*10
+}
+
+func readCmosReg(reg uint16) int {
+	sys.Outb(0x70, 0x80|byte(reg))
+	return int(sys.Inb(0x71))
 }
