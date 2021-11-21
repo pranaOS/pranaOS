ec7d9339348e4be6e268c138183d0b9b3032bf41
krishpranav <krisna.pranav@gmail.com>
2021-11-21 15:17:36 +0530

    kernel: mm sys

--- kernel/mm/sys.go
+++ kernel/mm/sys.go
@@ -6,17 +6,16 @@ const (
 	_SYS_FIXED_MMAP = 502
 )
 
-func SysMap(vaddr, size uintptr) uintptr {
+func SysMmap(vaddr, size uintptr) uintptr {
 	mem, _, err := syscall.Syscall6(syscall.SYS_MMAP, uintptr(vaddr), size, syscall.PROT_READ|syscall.PROT_WRITE, 0, 0, 0)
 	if err != 0 {
 		panic(err.Error())
 	}
-
 	return mem
 }
 
-func SysFixedMmap(vaddr, uintptr) {
-	_, _, err := syscall.Syscall(SYS_FIXED_MMAP, vaddr)
+func SysFixedMmap(vaddr, paddr, size uintptr) {
+	_, _, err := syscall.Syscall(_SYS_FIXED_MMAP, vaddr, paddr, size)
 	if err != 0 {
 		panic(err.Error())
 	}
