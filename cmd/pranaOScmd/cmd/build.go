5438416629977722387bc137427372885320adb5
krishpranav <krisna.pranav@gmail.com>
2021-11-21 19:26:07 +0530

    pranaOScmd: build

--- cmd/pranaOScmd/cmd/build.go
+++ cmd/pranaOScmd/cmd/build.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"log"
 
+	"github.com/pranaOS/pranaOS/cmd/egg/build"
 	"github.com/spf13/cobra"
 )
 
@@ -11,15 +12,28 @@ var (
 )
 
 var buildCmd = &cobra.Command{
-	Use: "build",
-	Short: "go build like command",
+	Use:                "build",
+	Short:              "go build like command",
 	DisableFlagParsing: true,
-	Run: func(cmd *cobra.Command, args []string {
+	Run: func(cmd *cobra.Command, args []string) {
 		err := runBuild(args)
 		if err != nil {
 			log.Fatal(err)
 		}
+	},
+}
+
+func runBuild(args []string) error {
+	b := build.NewBuilder(build.Config{
+		GoRoot:       goroot,
+		EggosVersion: pranaosversion,
+		GoArgs:       args,
 	})
+	err := b.Build()
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func init() {
