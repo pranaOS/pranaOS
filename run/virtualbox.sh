883c5e72bbda14f147d67f7247a9b80f6ba454c3
krishpranav <krisna.pranav@gmail.com>
2021-12-03 19:48:02 +0530

    run: virtualbox
    virtual box support to pranaOS

---
+++ run/virtualbox.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -e
+
+make image output=video keyboard=dvorak nic=pcnet
+qemu-img convert -f raw -O vdi disk.img disk.vdi -o size=32M
+VBoxManage internalcommands sethduuid disk.vdi dbbfad68-c3d1-4c9a-828f-7e4db4e9488e
+VBoxManage startvm pranaOS
