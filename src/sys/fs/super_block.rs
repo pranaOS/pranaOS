fd152ea4fff9d669a660c4b27b2ae5d9ccb76001
krishpranav <krisna.pranav@gmail.com>
2021-12-03 20:09:00 +0530

    src: sys super block
    file system check ata + new self function

--- src/sys/fs/super_block.rs
+++ src/sys/fs/super_block.rs
@@ -15,3 +15,25 @@ pub struct SuperBlock {
     pub block_count: u32,
     pub alloc_count: u32,
 }
+
+impl SuperBlock {
+    pub fn check_ata(bus: u8, dsk: u8) -> bool {
+        let mut buf = [0u8; super::BLOCK_SIZE];
+        sys::ata::read(bus, dsk, SUPERBLOCK_ADDR, &mut buf);
+        &buf[0..8] == SIGNATURE
+    }
+
+    pub fn new() -> Option<Self> {
+        if let Some(ref dev) = *super::block_device::BLOCK_DEVICE.lock() {
+            Some(Self {
+                signature: SIGNATURE,
+                version: super::VERSION,
+                block_size: dev.block_size() as u32,
+                block_count: dev.block_count() as u32,
+                alloc_count: 0,
+            })
+        } else {
+            None
+        }
+    }
+}
