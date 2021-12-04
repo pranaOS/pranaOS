3090c2c31169de65566b957be0f60013f25989c3
krishpranav <krisna.pranav@gmail.com>
2021-12-04 19:08:44 +0530

    filesystem: block
    read + write + alloc in Block

--- src/sys/fs/block.rs
+++ src/sys/fs/block.rs
@@ -9,3 +9,43 @@ pub struct Block {
     addr: u32,
     buf: [u8; super::BLOCK_SIZE],
 }
+
+impl Block {
+    pub fn new(addr: u32) -> Self {
+        let buf = [0; super::BLOCK_SIZE];
+        Self { addr, buf }
+    }
+
+    pub fn alloc() -> Option<Self> {
+        match BitmapBlock::next_free_addr() {
+            None => {
+                None
+            }
+            Some(addr) => {
+                BitmapBlock::alloc(addr);
+
+                let mut block = Block::read(addr);
+                for i in 0..super::BLOCK_SIZE {
+                    block.buf[i] = 0;
+                }
+                block.write();
+
+                some(block)
+            }
+        }
+    }
+
+    pub fn read(addr: u32) -> Self {
+        let mut buf = [0; super::BLOCK_SIZE];
+        if let Some(ref block_device) = *super::block_device::BLOCK_DEVICE.lock() {
+            block_device.read(addr, &mut buf);
+        }
+        Self { addr, buf }
+    }
+
+    pub fn write(&self) {
+        if let Some(ref mut block_device) = *super::block_device::BLOCK_DEVICE.lock() {
+            block_device.write(self.addr, &self.buf);
+        }
+    }
+}
