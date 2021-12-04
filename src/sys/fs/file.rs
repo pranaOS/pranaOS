8b233404545d7f6acd3d1a08e3139979d39d3c58
krishpranav <krisna.pranav@gmail.com>
2021-12-04 18:46:05 +0530

    filesystem: file
    read to string + create + size of the file + name of the file functions
    added
    author: @krishpranav

--- src/sys/fs/file.rs
+++ src/sys/fs/file.rs
@@ -32,3 +32,51 @@ impl From<DirEntry> for File {
         }
     }
 }
+
+impl File {
+    pub fn create(pathname: &str) -> Option<Self> {
+        let pathname = realpath(pathname);
+        let dirname = dirname(&pathname);
+        let filename = filename(&pathname);
+        if let Some(dir) = Dir::open(dirname) {
+            if let Some(dir_entry) = dir.create_file(filename) {
+                return Some(dir_entry.into());
+            }
+        }
+        None
+    }
+
+    pub fn name(&self) -> String {
+        self.name.clone()
+    }
+
+    pub fn size(&self) -> usize {
+        self.size as usize
+    }
+
+    pub fn seek(&mut self, pos: SeekFrom) -> Result<u32, ()> {
+        let offset = match pos {
+            SeekFrom::Start(i) => i as i32,
+            SeekFrom::Current(i) => i + self.offset as i32,
+            SeekFrom::End(i) => i + self.size  as i32 - 1,
+        };
+        if offset < 0 || offset > self.size as i32 {
+            return Err(())
+        }
+        self.offset = offset as u32;
+
+        Ok(self.offset);
+    }
+
+    pub fn read_to_string(&mut self) -> String {
+        let mut buf = vec![0; self.size()];
+        if let Ok(bytes) = self.read(&mut buf) {
+            buf.resize(bytes, 0);
+        }
+        String::from_utf8_lossy(&buf).to_string()
+    }
+
+    pub fn addr(&self) -> u32 {
+        self.addr
+    }
+}
