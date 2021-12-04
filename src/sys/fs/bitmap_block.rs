adb9c76959a76cfba252536fb5b0c7bfd06cc0e0
krishpranav <krisna.pranav@gmail.com>
2021-12-04 18:13:52 +0530

    filesystem: bitmap block
    add: buffer index + block index

--- src/sys/fs/bitmap_block.rs
+++ src/sys/fs/bitmap_block.rs
@@ -3,12 +3,23 @@ use super::super_block;
 use super::super_block::SuperBlock;
 use bit_field::BitField;
 
-const BITMAP_SIZE: usize = 8 * super::BLOCK_SIZE;
+pub const BITMAP_SIZE: usize = 8 * super::BLOCK_SIZE;
 
 pub struct BitmapBlock {}
 
 impl BitmapBlock {
+    fn block_index(addr: u32) -> u32 {
+        let sb = SuperBlock::read();
+        let size = sb.block_size();
+        let i = addr - sb.data_area();
+        sb.bitmap_area() + (i / size / 8)
+    }
 
+    fn buffer_index(addr: u32) -> usize {
+        let sb = SuperBlock::read();
+        let i = (addr - sb.data_area()) as usize;
+        i % sb.block_size() as usize
+    }
 }
 
 pub fn free_all() {
