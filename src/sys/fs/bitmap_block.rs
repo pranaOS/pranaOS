c8449bd3c2f23f384e6b20de4aa065716290055f
krishpranav <krisna.pranav@gmail.com>
2021-12-04 18:17:44 +0530

    filesystem: bitmap block
    free + allocation

--- src/sys/fs/bitmap_block.rs
+++ src/sys/fs/bitmap_block.rs
@@ -20,6 +20,26 @@ impl BitmapBlock {
         let i = (addr - sb.data_area()) as usize;
         i % sb.block_size() as usize
     }
+
+    pub fn alloc(addr: u32) {
+        let mut block = Block::read(BitmapBlock::block_index(addr));
+        let bitmap = block.data_mut();
+        let i = BitmapBlock::buffer_index(addr);
+        if !bitmap[i / 8].get_bit(i % 8) {
+            bitmap[i / 8].set_bit(i % 8, true);
+            blcok.write();
+            super_block::inc_alloc_count();
+        }
+    }
+
+    pub fn free(addr: u32) {
+        let mut block = Block::read(BitmapBlock::block_index(addr));
+        let bitmap = block.data_mut();
+        let i = BitmapBlock::buffer_index(addr);
+        bitmap[i / 8].set_bit(i % 8, false);
+        block.write();
+        super_block::dec_alloc_count();
+    }
 }
 
 pub fn free_all() {
