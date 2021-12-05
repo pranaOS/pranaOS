843b9b068f7d2daaef64a7d34b78d5517ff87a0f
krishpranav <krisna.pranav@gmail.com>
2021-12-05 12:24:31 +0530

    filesystem: dir

--- src/sys/fs/dir.rs
+++ src/sys/fs/dir.rs
@@ -6,6 +6,7 @@ use super::bitmap_block::BitmapBlock;
 use super::FileType;
 use super::block::LinkedBlock;
 use crate::sys;
+
 use alloc::string::String;
 use core::convert::From;
 
@@ -26,7 +27,7 @@ impl Dir {
     }
 
     pub fn create(pathname: &str) -> Option<Self> {
-        let pathname: realpath(pathname);
+        let pathname = realpath(pathname);
         let dirname = dirname(&pathname);
         let filename = filename(&pathname);
         if let Some(dir) = Dir::open(dirname) {
@@ -41,12 +42,12 @@ impl Dir {
         if !super::is_mounted() {
             return None;
         }
-        
+
         let mut dir = Dir::root();
         let pathname = realpath(pathname);
 
         if pathname == "/" {
-            return  Some(dir);
+            return Some(dir);
         }
 
         for name in pathname.trim_start_matches('/').split('/') {
@@ -76,7 +77,118 @@ impl Dir {
                 return Some(entry);
             }
         }
-        None 
+        None
+    }
+
+    pub fn create_file(&self, name: &str) -> Option<DirEntry> {
+        self.create_entry(FileType::File, name)
+    }
+
+    pub fn create_dir(&self, name: &str) -> Option<DirEntry> {
+        self.create_entry(FileType::Dir, name)
+    }
+
+    pub fn create_device(&self, name: &str) -> Option<DirEntry> {
+        self.create_entry(FileType::Device, name)
+    }
+
+    fn create_entry(&self, kind: FileType, name: &str) -> Option<DirEntry> {
+        if self.find(name).is_some() {
+            return None;
+        }
+
+        let mut entries = self.entries();
+        while entries.next().is_some() {}
+
+        let space_left = entries.block.data().len() - entries.block_data_offset();
+        let entry_len = DirEntry::empty_len() + name.len();
+        if entry_len > space_left {
+            match entries.block.alloc_next() {
+                None => return None, 
+                Some(new_block) => {
+                    entries.block = new_block;
+                    entries.block_data_offset = 0;
+                },
+            }
+        }
+
+        let entry_block = LinkedBlock::alloc().unwrap();
+        let entry_kind = kind as u8;
+        let entry_addr = entry_block.addr();
+        let entry_size = 0u32;
+        let entry_time = sys::clock::realtime() as u64;
+        let entry_name = truncate(name, u8::MAX as usize);
+        let n = entry_name.len();
+        let i = entries.block_data_offset();
+        let data = entries.block.data_mut();
+
+        data[i] = entry_kind;
+        data[(i + 1)..(i + 5)].clone_from_slice(&entry_addr.to_be_bytes());
+        data[(i + 5)..(i + 9)].clone_from_slice(&entry_size.to_be_bytes());
+        data[(i + 9)..(i + 17)].clone_from_slice(&entry_time.to_be_bytes());
+        data[i + 17] = n as u8;
+        data[(i + 18)..(i + 18 + n)].clone_from_slice(&entry_name.as_bytes());
+
+        entries.block.write();
+
+        Some(DirEntry::new(*self, kind, entry_addr, entry_size, entry_time, &entry_name))
+    }
+
+    pub fn delete_entry(&mut self, name: &str) -> Result<(), ()> {
+        let mut entries = self.entries();
+        for entry in &mut entries {
+            if entry.name() == name {
+                let i = entries.block_data_offset() - entry.len();
+                let data = entries.block.data_mut();
+                data[i + 1] = 0;
+                data[i + 2] = 0;
+                data[i + 3] = 0;
+                data[i + 4] = 0;
+                entries.block.write();
+
+                let mut entry_block = LinkedBlock::read(entry.addr());
+                loop {
+                    BitmapBlock::free(entry_block.addr());
+                    match entry_block.next() {
+                        Some(next_block) => entry_block = next_block,
+                        None => break,
+                    }
+                }
+
+                return Ok(());
+            }
+        }
+        Err(())
+    }
+
+    pub fn update_entry(&mut self, name: &str, size: u32) {
+        let time = sys::clock::realtime() as u64;
+        let mut entries = self.entries();
+        for entry in &mut entries {
+            if entry.name() == name {
+                let i = entries.block_data_offset() - entry.len();
+                let data = entries.block.data_mut();
+                data[(i + 5)..(i + 9)].clone_from_slice(&size.to_be_bytes());
+                data[(i + 9)..(i + 17)].clone_from_slice(&time.to_be_bytes());
+                entries.block.write();
+                break;
+            }
+        }
+    }
+
+    pub fn entries(&self) -> ReadDir {
+        ReadDir::from(self.clone())
+    }
+
+    pub fn delete(pathname: &str) -> Result<(), ()> {
+        let pathname = realpath(pathname);
+        let dirname = dirname(&pathname);
+        let filename = filename(&pathname);
+        if let Some(mut dir) = Dir::open(dirname) {
+            dir.delete_entry(filename)
+        } else {
+            Err(())
+        }
     }
 }
 
@@ -91,4 +203,30 @@ impl FileIO for Dir {
 
 fn truncate(s: &str, max: usize) -> String {
     s.char_indices().take_while(|(i, _)| *i <= max).map(|(_, c)| c).collect()
+}
+
+#[test_case]
+fn test_dir_create() {
+    super::mount_mem();
+    super::format_mem();
+    assert!(Dir::open("/test").is_none());
+    assert!(Dir::create("/test").is_some());
+    assert!(Dir::open("/test").is_some());
+
+    assert!(Dir::open("/test/test").is_none());
+    assert!(Dir::create("/test/test").is_some());
+    assert!(Dir::open("/test/test").is_some());
+    super::dismount();
+}
+
+#[test_case]
+fn test_dir_delete() {
+    super::mount_mem();
+    super::format_mem();
+    assert!(Dir::open("/test").is_none());
+    assert!(Dir::create("/test").is_some());
+    assert!(Dir::open("/test").is_some());
+    assert!(Dir::delete("/test").is_ok());
+    assert!(Dir::open("/test").is_none());
+    super::dismount();
 }
