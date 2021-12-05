bcaaadfb26b178a814c954baf69c42f6232bd496
krishpranav <krisna.pranav@gmail.com>
2021-12-05 12:22:49 +0530

    filesystem: dir

--- src/sys/fs/dir.rs
+++ src/sys/fs/dir.rs
@@ -24,6 +24,51 @@ impl Dir {
     pub fn root() -> Self {
         Self { addr: SuperBlock::read().data_area() }
     }
+
+    pub fn create(pathname: &str) -> Option<Self> {
+        let pathname: realpath(pathname);
+        let dirname = dirname(&pathname);
+        let filename = filename(&pathname);
+        if let Some(dir) = Dir::open(dirname) {
+            if let Some(dir_entry) = dir.create_dir(filename) {
+                return Some(dir_entry.into());
+            }
+        }
+        None
+    }
+
+    pub fn open(pathname: &str) -> Option<Self> {
+        if !super::is_mounted() {
+            return None;
+        }
+        
+        let dir = Dir::root();
+        let pathname = realpath(pathname);
+
+        if pathname == "/" {
+            return  Some(dir);
+        }
+
+        for name in pathname.trim_start_matches('/').split('/') {
+            match dir.find(name) {
+                Some(dir_entry) => {
+                    if dir_entry.is_dir() {
+                        dir = dir_entry.into()
+                    } else {
+                        return None;
+                    }
+                },
+                None => {
+                    return None
+                },
+            }
+        }
+        Some(dir)
+    }
+
+    pub fn addr(&self) -> u32 {
+        self.addr
+    }
 }
 
 impl FileIO for Dir {
