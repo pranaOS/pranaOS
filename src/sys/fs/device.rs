b4b5deab568f58fe1afee9b370ddb61315e66b12
krishpranav <krisna.pranav@gmail.com>
2021-12-05 11:28:17 +0530

    filesystem: device
    fileio read + create + new

--- src/sys/fs/device.rs
+++ src/sys/fs/device.rs
@@ -6,23 +6,47 @@ use crate::sys::console::Console;
 use crate::sys::random::Random;
 
 #[repr(u8)]
-enum DeviceType {
+pub enum DeviceType {
     File = 0,
     Console = 1,
     Random = 2,
 }
 
 #[derive(Debug, Clone)]
-enum Device {
+pub enum Device {
     File(File),
     Console(Console),
     Random(Random),
 }
 
 impl Device {
+    fn new(i: u8) -> Self {
+        match i {
+            i if i == DeviceType::Console as u8 => Device::Console(Console::new()),
+            i if i == DeviceType::Random as u8 => Device::Random(Random::new()),
+            _ => unimplemented!(),
+        }
+    }
 
+    pub fn create(pathname: &str) -> Option<Self> {
+        let pathname = realpath(pathname);
+        let dirname = dirname(&pathname);
+        let filename = filename(&pathname);
+        if let Some(dir) = Dir::open(dirname) {
+            if let Some(dir_entry) = dir.create_device(filename) {
+                return Some(Device::File(dir_entry.into()))
+            }
+        }
+        None
+    }
 }
 
 impl FileIO for Device {
-    
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize, ()> {
+        match self {
+            Device::File(io) => io.read(buf),
+            Device::Console(io) => io.read(buf),
+            Device::Random(io) => io.read(buf),
+        }
+    }
 }
