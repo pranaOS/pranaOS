f9828d9bf442fabca507b0548eeabf891481e420
krishpranav <krisna.pranav@gmail.com>
2021-12-15 21:47:05 +0530

    pranaOS

--- dsk/ini/boot.sh
+++
@@ -1,5 +0,0 @@
-vga set palette /ini/palette.csv
-vga set font /ini/fonts/zap-light-8x16.psf
-read /ini/banner.txt
-user login
-shell
--- run/fuse.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-img="disk.img"
-path="/tmp/pranaos"
-
-mkdir -p $path
-echo "Mounting $img in $path"
-python run/fuse.py $img $path
