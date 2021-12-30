1af401e6d0959f9085cfb66f5897f4b0c12ff414
krishpranav <krisna.pranav@gmail.com>
2021-12-30 18:17:43 +0530

    libpranaos: utils assert cpp

--- lib/libpranaos/utils/assert.cpp
+++ lib/libpranaos/utils/assert.cpp
@@ -1,11 +1,13 @@
 #include "libpranaos/utils/assert.h"
 #include <cstdio>
 
-namespace utils {
+namespace utils
+{
     static void assert_fail(const char* filename, const char* function, int line, const char* message)
     {
         fprintf(stderr, "%s() in %s:%d: assertion failed. %s\n", function, filename, line, message);
-        while (true);
+
+        while(true); 
     }
 
     void assert_if_reached(const char* message, std::source_location source)
@@ -16,6 +18,14 @@ namespace utils {
     void assert_if_equals_true(bool condition, const char* message, std::source_location source)
     {
         if(condition == true)
+        {
+            assert_fail(source.file_name(), source.function_name(), source.line(), message);
+        }
+    }
+
+    void assert_if_equals_false(bool condition, const char* message, std::source_location source)
+    {
+        if(condition == false)
         {
             assert_fail(source.file_name(), source.function_name(), source.line(), message);
         }
