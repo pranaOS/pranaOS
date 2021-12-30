6367dc16a458061cabcc1848e1635dfa5c3a5b19
krishpranav <krisna.pranav@gmail.com>
2021-12-30 20:03:30 +0530

    libc: stdlib ctype cpp

--- lib/libc/stdlib/ctype.cpp
+++ lib/libc/stdlib/ctype.cpp
@@ -24,3 +24,33 @@ int islower(int c)
 {
     return c >= 'a' && c <= 'z';
 }
+
+int isprint(int c)
+{
+    return c >= 0x20 && c <= 0x7e;
+}
+
+int isalnum(int c)
+{
+    return isalpha(c) || isdigit(c);
+}
+
+int iscntrl(int c)
+{
+    return c == 0x7f || (c >= 0 && c <= 0x1f);
+}
+
+int isxdigit(int c)
+{
+    return isdigit(c) || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f');
+}
+
+int tolower(int c)
+{
+    return isupper(c) ? c - 'A' + 'a' : c;
+}
+
+int toupper(int c)
+{
+    return islower(c) ? c - 'a' + 'A' : c;
+}
