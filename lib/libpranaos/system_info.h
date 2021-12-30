073a343b5c1fdae4e7d7d19e2544cc88948bf3cf
krishpranav <krisna.pranav@gmail.com>
2021-12-30 18:10:04 +0530

    libpranaos: system_info header

--- lib/libpranaos/system_info.h
+++ lib/libpranaos/system_info.h
@@ -2,11 +2,14 @@
 
 #include <string>
 
-namespace pranaos {
-    struct system_info {
+namespace ackos
+{
+    struct system_info
+    {
         std::string os_name;
         std::string os_version;
         std::string os_release;
+        std::string build_date;
         std::string copyright;
     };
 }
