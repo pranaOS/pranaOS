1acadf7ed25dc8021d72c5cee1063ec79acf575d
krishpranav <krisna.pranav@gmail.com>
2021-12-31 12:59:10 +0530

    libc: ctype header

---
+++ lib/libc/ctype.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <sys/cdefs.h>
+#include <cstdarg>
+#include <cstddef>
+
+__BEGIN_DECLS
+int isalpha(int c);
+
+int isspace(int c);
+
+int isdigit(int c);
+
+int isupper(int c);
+
+int islower(int c);
+
+int isprint(int c);
+__END_DECLS
