d3c0f577479c1f2488a4039a133855d48a353da3
krishpranav <krisna.pranav@gmail.com>
2021-12-31 12:54:56 +0530

    libc: assert header

--- lib/libc/assert.h
+++ lib/libc/assert.h
@@ -6,4 +6,6 @@ __BEGIN_DECLS
 
 __attribute__((noreturn)) void _assert_fail(const char* expression, const char* filename, const char* function, int line);
 
+#define assert(__condition) ((__condition) ? (void)0 : _assert_fail(#__condition, __FILE__, __FUNCTION__, __LINE__))
+
 __END_DECLS
