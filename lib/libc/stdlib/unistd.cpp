3ce262f9c5d76d4793787bc735164aad1156ff11
krishpranav <krisna.pranav@gmail.com>
2021-12-31 12:45:30 +0530

    libc: stdlib unistd cpp

--- lib/libc/stdlib/unistd.cpp
+++ lib/libc/stdlib/unistd.cpp
@@ -5,7 +5,7 @@
 ssize_t read(int fd, void* buff, size_t count)
 {
     size_t read = 0;
-    utils::result result = ackos::sys_wrapper::stream_read(fd, buff, count, &read);
+    utils::result result = pranaos::sys_wrapper::stream_read(fd, buff, count, &read);
 
     if(result == utils::result::ERR_INVALID_FD)
     {
@@ -36,7 +36,7 @@ ssize_t read(int fd, void* buff, size_t count)
 ssize_t write(int fd, const void* buff, size_t count)
 {
     size_t written = 0;
-    utils::result result = ackos::sys_wrapper::stream_write(fd, buff, count, &written);
+    utils::result result = pranaos::sys_wrapper::stream_write(fd, buff, count, &written);
 
     if(result == utils::result::ERR_INVALID_FD)
     {
@@ -48,4 +48,75 @@ ssize_t write(int fd, const void* buff, size_t count)
     }
 
     return (ssize_t)written;
+}
+
+int dup(int old_fd)
+{
+    int new_fd = -1;
+
+    utils::result result = pranaos::sys_wrapper::stream_clone(&new_fd, old_fd);
+
+    switch((int)result)
+    {
+    case utils::result::ERR_INVALID_FD:
+    {
+        errno = EBADF;
+
+        break;
+    }
+    case utils::result::ERR_OUT_OF_BOUNDS:
+    {
+        errno = EBADF;
+
+        break;
+    }
+    case utils::result::ERR_INVALID_ARGUMENT:
+    {
+        errno = EINVAL;
+
+        break;
+    }
+    default:
+        errno = 0;
+
+        break;
+    }
+
+    return new_fd;
+}
+
+int dup2(int old_fd, int new_fd)
+{
+    utils::result result = pranaos::sys_wrapper::stream_clone(old_fd, new_fd);
+
+    switch((int)result)
+    {
+    case utils::result::ERR_INVALID_FD:
+    {
+        errno = EBADF;
+        return -1;
+
+        break;
+    }
+    case utils::result::ERR_OUT_OF_BOUNDS:
+    {
+        errno = EBADF;
+        return -1;
+
+        break;
+    }
+    case utils::result::ERR_INVALID_ARGUMENT:
+    {
+        errno = EINVAL;
+        return -1;
+
+        break;
+    }
+    default:
+        errno = 0;
+
+        break;
+    }
+
+    return new_fd;
 }
