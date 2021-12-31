4fad47312236bc50b5cd015a77be6611a747d632
krishpranav <krisna.pranav@gmail.com>
2021-12-31 09:40:10 +0530

    libc: stdlib heap cpp

--- lib/libc/stdlib/heap.cpp
+++ lib/libc/stdlib/heap.cpp
@@ -23,11 +23,40 @@ uint8_t heap[HEAP_SIZE];
 
 static void heap_merge_blocks(heap_block* a, heap_block* b)
 {
-    if (a == nullptr) return;
-    if (b == nullptr) return;
+    if(a == nullptr) return;
+    if(b == nullptr) return;
 
-    if (a < b)
+    if(a < b)
     {
-        
+        a->size += b->size + sizeof(heap_block);
+        a->next = b->next;
+        a->next_free = b->next_free;
+
+        b->next->previous = a;
+        b->next->previous_free = a;
+        b->next_free->previous_free = a;
     }
+    else
+    {
+        b->size += a->size + sizeof(heap_block);
+        b->next = a->next;
+        b->next_free = a->next_free;
+
+        a->next->previous = b;
+        a->next->previous_free = b;
+        a->next_free->previous_free = b;
+    }
+}
+
+void __stdlib_heap_initialise()
+{
+    heap_root = (heap_block*)heap;
+    heap_root->size = (uint64_t)HEAP_SIZE - sizeof(heap_block);
+
+    heap_root->next = nullptr;
+    heap_root->next_free = nullptr;
+    heap_root->previous = nullptr;
+    heap_root->previous_free = nullptr;
+
+    heap_root->is_free = true;
 }
