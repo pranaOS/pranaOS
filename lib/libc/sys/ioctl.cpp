654f62ae218fe1734c5fe81f922b1306a4ced5bc
krishpranav <krisna.pranav@gmail.com>
2021-12-31 12:49:11 +0530

    libc: sys ioctl cpp

--- lib/libc/sys/ioctl.cpp
+++ lib/libc/sys/ioctl.cpp
@@ -1,1 +1,20 @@
 // TODO: implement syscalls
+#include <sys/ioctl.h>
+#include <cstdarg>
+#include <cerrno>
+#include <libpranaos/syscalls.h>
+
+int ioctl(int fd, int request, ...)
+{
+    va_list va;
+
+    va_start(va, request);
+    int res = 0;
+    void* arg = va_arg(va, void*);
+
+    auto result = pranaos::sys_wrapper::stream_iocall(fd, request, arg);
+
+    va_end(va);
+
+    return res;
+}
