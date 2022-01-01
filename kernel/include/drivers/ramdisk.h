11d3029bf42b92eb97141353e3f37072e7440515
krishpranav <krisna.pranav@gmail.com>
2022-01-01 18:50:01 +0530

    kernel: include drivers ramdisk header

--- kernel/include/drivers/ramdisk.h
+++ kernel/include/drivers/ramdisk.h
@@ -1,1 +1,10 @@
 #include <drivers/disk.h>
+#include <sys/types.h>
+#include <stddef.h>
+#include <stdint.h>
+
+void init_ramdisk(offset_t location, size_t size);
+
+ssize_t ramdisk_read(unsigned long offset, void *destiniation, size_t size, disk_t *disk_info);
+
+ssize_t ramdisk_write(unsigned long offset);
