d50ed45d15e3f9e072424afb891beae5c42eeeac
krishpranav <krisna.pranav@gmail.com>
2022-01-01 12:26:17 +0530

    kernel: include core timer header

--- kernel/include/core/timer.h
+++ kernel/include/core/timer.h
@@ -1,4 +1,4 @@
-#pragma once
+#pragma once 
 
 #include <sys/types.h>
 
@@ -7,3 +7,8 @@ unsigned long timer_get_frequency();
 void init_timer();
 
 unsigned long timer_get_cur_ticks();
+
+unsigned long timer_get_period();
+
+unsigned long timer_secs_since_boot();
+void          time_since_boot(time_t *sec, time_t *microsec);
