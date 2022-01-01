98ce048bd2aceae7b90f51d5b137676a3043e127
krishpranav <krisna.pranav@gmail.com>
2022-01-01 18:48:27 +0530

    kernel: include drivers disk header

--- kernel/include/drivers/disk.h
+++ kernel/include/drivers/disk.h
@@ -4,6 +4,9 @@
 #include <stdint.h>
 #include <sys/types.h>
 
+#define DISK_TYPE_RAMDISK  0x00
+#define DISK_TYPE_HARDDISK 0x01
+
 struct disk_s;
 
 typedef ssize_t (*read_drive_fp)(unsigned long offset, void *buf, size_t count,
@@ -13,13 +16,19 @@ typedef ssize_t (*write_drive_fp)(unsigned long offset, const void *buf,
 
 typedef struct disk_s
 {
-    int type;
-    char *name;
-    void *drive_info;
-    
+	int   type;
+	char *name;
+	void *drive_info; 
 
-    unsigned int block_size;  
-    struct disk_s *next;
+	unsigned int   block_size;
+	read_drive_fp  read;
+	write_drive_fp write;
+
+	struct disk_s *next;
 } disk_t;
 
 void add_disk(disk_t *disk);
+
+ssize_t disk_read(unsigned long offset, void *buf, size_t size, disk_t *disk);
+ssize_t disk_write(unsigned long offset, const void *buf, size_t size,
+                   disk_t *disk);
