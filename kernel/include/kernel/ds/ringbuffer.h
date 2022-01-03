a08f4a95a52addc0f9205622f37103e3fcbee915
krishpranav <krisna.pranav@gmail.com>
2022-01-03 10:21:26 +0530

    kernel: include ds ringbuffer header

--- kernel/include/kernel/ds/ringbuffer.h
+++ kernel/include/kernel/ds/ringbuffer.h
@@ -10,19 +10,30 @@
 typedef uint32_t cb_flags_t;
 
 struct ringbuffer {
-    unsigned long size;
+	unsigned long size;
 
-    volatile unsigned long virtual_end;
-    volatile unsigned long virtual_begin;
+	volatile unsigned long virtual_end;
+	volatile unsigned long virtual_begin;
 
-    char *buffer_start;
-    char *buffer_end;
+	char *buffer_start;
+	char *buffer_end;
 
-    volatile unsigned int lock;
-    struct spinlock spinlock;
+	volatile unsigned int lock;
+	struct spinlock spinlock;
 
-    cb_flags_t flags;
+	cb_flags_t flags;
 };
 
 struct ringbuffer *create_ringbuffer(size_t size, cb_flags_t flags);
-void ringbuffer_destroy(struct ringbuffer *rb);
+void               ringbuffer_destroy(struct ringbuffer *rb);
+
+ssize_t ringbuffer_read_index(char *buffer, size_t size, unsigned long index,
+                              struct ringbuffer *circbuf);
+ssize_t ringbuffer_read(char *buffer, size_t size, struct ringbuffer *circbuf);
+
+ssize_t ringbuffer_write_index(char *buffer, size_t size, unsigned long index,
+                               struct ringbuffer *circbuf);
+ssize_t ringbuffer_write(char *buffer, size_t size, struct ringbuffer *circbuf);
+void    ringbuffer_block(struct ringbuffer *circbuf);
+void    ringbuffer_flush(struct ringbuffer *circbuf);
+int     ringbuffer_remove(int location, struct ringbuffer *circbuf);
