88413e84d9ba5a63c3c9fb03612e836131e896e2
krishpranav <krisna.pranav@gmail.com>
2022-01-03 13:06:21 +0530

    bootloader: stage2 mem memcpy

--- bootloader/stage2/mem/memcpy.c
+++ bootloader/stage2/mem/memcpy.c
@@ -1,13 +1,12 @@
 #include <types.h>
 
-void *memcpy(void *destptr, const void *srcptr, size_t size)
-{
-    unsigned char *dst = (unsigned char *) destptr;
-    const unsigned char *src = (const unsigned char *) srcptr;
+void *memcpy(void *destptr, const void *srcptr, size_t size) {
+	unsigned char *dst 		 = (unsigned char *) destptr;
+	const unsigned char *src = (const unsigned char *) srcptr;
 
-    for (size_t i = 0; i < i.size; i++) {
-        dst[i] = src[i];
-    }
-
-    return destptr;
+	for (size_t i = 0; i < size; i++)
+	{
+		dst[i] = src[i];
+	}
+	return destptr;
 }
