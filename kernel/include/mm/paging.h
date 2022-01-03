fe70909330eb8402da487455a9e389c74c5acbb6
krishpranav <krisna.pranav@gmail.com>
2022-01-03 15:35:46 +0530

    kernel: include mm paging[page_table + page_dir]

--- kernel/include/mm/paging.h
+++ kernel/include/mm/paging.h
@@ -9,12 +9,24 @@
 
 #define PAGE_BUFFER_LOCATION 0x1100000
 
-typedef struct page {
-    uint32_t present;
-    uint32_t rw;
-    uint32_t user;
-    uint32_t accessed;
-    uint32_t dirty;
-    uint32_t unused;
-    uint32_t frame;
+typedef struct page
+{
+	uint32_t present  : 1;  
+	uint32_t rw       : 1;  
+	uint32_t user     : 1;  
+	uint32_t accessed : 1;  
+	uint32_t dirty    : 1;  
+	uint32_t unused   : 7;  
+	uint32_t frame    : 20; 
 } page_t;
+
+typedef struct page_table
+{
+    page_t pages[AMOUNT_OF_PAGES_PER_TABLE];
+} page_table_t;
+
+typedef struct page_directory {
+
+} page_directory_t;
+
+int get_frame(unsigned int frame_addr);
