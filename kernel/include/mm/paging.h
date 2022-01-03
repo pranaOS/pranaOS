cf09203a6774831eb0ed593c3907fa899ae8e1f7
krishpranav <krisna.pranav@gmail.com>
2022-01-03 15:42:45 +0530

    kernel: include mm paging header

--- kernel/include/mm/paging.h
+++ kernel/include/mm/paging.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include <sys/types.h>
 
-#define AMOUNT_OF_PAGES_PER_TABLE 1024 
+#define AMOUNT_OF_PAGES_PER_TABLE     1024
 #define AMOUNT_OF_PAGE_TABLES_PER_DIR 1024
 
 #define PAGE_BUFFER_LOCATION 0x1100000
@@ -22,21 +22,24 @@ typedef struct page
 
 typedef struct page_table
 {
-    page_t pages[AMOUNT_OF_PAGES_PER_TABLE];
+	page_t pages[AMOUNT_OF_PAGES_PER_TABLE];
 } page_table_t;
 
-typedef struct page_directory {
-    page_t pages[AMOUNT_OF_PAGES_PER_TABLE];
+typedef struct page_directory
+{
+	page_table_t *tables[AMOUNT_OF_PAGE_TABLES_PER_DIR];
+	uint32_t      tablesPhysical[AMOUNT_OF_PAGE_TABLES_PER_DIR];
+	uint32_t      physicalAddress;
 } page_directory_t;
 
 int get_frame(unsigned int frame_addr);
 
 page_directory_t *get_kernel_dir();
-page_directory_t *get_current_dir(); 
-void set_current_dir(page_directory_t *new);
+page_directory_t *get_current_dir();
+void              set_current_dir(page_directory_t *new);
 
 unsigned long phys_freeram_amount();
-unsigned long phys_freeswap_amout();
+unsigned long phys_freeswap_amount();
 unsigned long sharedram_amount();
 unsigned long bufferram_amount();
 
@@ -52,10 +55,30 @@ phys_addr_t virt_to_phys(void *virtual_address);
 void switch_page_directory(page_directory_t *dir);
 
 page_t *get_page(uint32_t virtual_address, int make_page_table,
-                page_directory_t *dir);
+                 page_directory_t *dir);
 
 void alloc_virt(offset_t start, size_t size, int user);
 
 page_directory_t *duplicate_current_page_directory();
 
 int init_paging();
+
+int map_physical_to_virtual(phys_addr_t *physical_address,
+                            void *virtual_address, size_t size, int is_kernel,
+                            int               is_writable_from_userspace,
+                            page_directory_t *dir);
+
+void copy_stack_to_new_addressspace(page_directory_t *newdir);
+
+int map_mem(uint32_t startaddr, uint32_t endaddr, int is_kernel,
+            int is_writable_from_userspace);
+int remap_mem(uint32_t startaddr, uint32_t endaddr, int is_kernel,
+              int is_writable_from_userspace);
+
+void clear_page_directory(page_directory_t *dir);
+
+int identity_map_memory_block(uint32_t startaddr, uint32_t endaddr,
+                              int is_kernel, int is_writable_from_userspace,
+                              page_directory_t *dir);
+
+void debug_paging_print(page_directory_t *dir);
