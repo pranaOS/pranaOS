e9883ceafccf3a95747b2a8b929ac1eb33398f51
krishpranav <krisna.pranav@gmail.com>
2022-01-03 15:48:22 +0530

    kernel: include mm linkedlist header

--- kernel/include/mm/linkedlist.h
+++ kernel/include/mm/linkedlist.h
@@ -5,25 +5,30 @@
 
 #define NODESIZE_MAX 32
 
-struct ll_node {
-    size_t size;
+struct ll_node
+{
+	size_t size;
 
-    struct ll_node *next;
-    struct ll_node *prev;
+	struct ll_node *next;
+	struct ll_node *prev;
 };
 
-struct linkedlist {
-    offset_t memstart;
-    size_t maxsize;
-    size_t cursize;
-    
-    int user;
+struct linkedlist
+{
+	offset_t memstart; 
+	size_t   maxsize;  
+	size_t   cursize;
+	int user;
 
-    struct ll_node *start_used;
-    struct ll_node *start_free;
+	struct ll_node *start_used;
+	struct ll_node *start_free;
 };
 
 struct linkedlist *create_linkedlist(offset_t memorystart, size_t maxsize,
-                                    int user);
+                                     int user);
 
-int free_node(struct ll_node *node, struct linkedlist *list);
+int             free_node(struct ll_node *node, struct linkedlist *list);
+struct ll_node *get_free(size_t searchsize, struct linkedlist *list,
+                         int pagealigned);
+
+int dbg_is_heapblock_free(struct linkedlist *list, struct ll_node *node);
