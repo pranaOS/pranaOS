80dc82faa231e97ae44547ae8615b98f7c9213b2
krishpranav <krisna.pranav@gmail.com>
2022-01-03 15:04:46 +0530

    external: clib vector header

--- external/clib/vector.h
+++ external/clib/vector.h
@@ -18,3 +18,34 @@
 	type       name##_pop(name vec);                           \
 	int name##_set(name vec, unsigned int index, type object); \
 	int name##_iter(name vec);
+
+#define define_vector_type(name) \
+	struct vec_s_##name          \
+	{                            \
+		void * buffer;           \
+		size_t size;             \
+		size_t capacity;         \
+		int    destroyed;        \
+		int    error;            \
+		int    iter;             \
+	};                           \
+	typedef struct vec_s_##name *name;
+
+#define define_vector_functions(type, name) \
+	_define_vector_create(type, name);      \
+	_define_vector_get(type, name);         \
+	_define_vector_destroy(type, name);     \
+	_define_vector_wipe(type, name);        \
+	_define_vector_copy(type, name);        \
+	_define_vector_reserve(type, name);     \
+	_define_vector_push(type, name);        \
+	_define_vector_set(type, name);         \
+	_define_vector_pop(type, name);         \
+	_define_vector_iter(type, name);        \
+	_define_vector_find_int(type, name);
+
+#define _ret_error(err, vec, type) \
+	vec->error = err;              \
+	type x;                        \
+	memset(&x, 0, sizeof(type));   \
+	return x;
