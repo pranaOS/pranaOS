35845f902ded2a91c9164867023000904c949ff8
krishpranav <krisna.pranav@gmail.com>
2022-01-03 15:28:25 +0530

    kernel: arch i386 include boot multiboot header

--- kernel/arch/i386/include/boot/multiboot.h
+++ kernel/arch/i386/include/boot/multiboot.h
@@ -1,8 +1,19 @@
 #pragma once
 
 struct multiboot {
+	uint32_t flags;
+	uint32_t mem_lower;
+	uint32_t mem_upper;
+	uint32_t boot_device;
+	uint32_t cmdline;
+	uint32_t mods_count;
+	uint32_t mods_addr;
+	uint32_t num;
+	uint32_t size;
+	uint32_t addr;
+	uint32_t shndx;
 
-};
+} __attribute__((packed));
 
 struct vbe_info {
 
