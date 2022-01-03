c647c143ee1241e79bbf5365268c55a581670c8f
krishpranav <krisna.pranav@gmail.com>
2022-01-03 12:40:53 +0530

    bootloader: stage2 types header

--- bootloader/stage2/types.h
+++ bootloader/stage2/types.h
@@ -1,6 +1,6 @@
-#pragma one
+#pragma once
 
-typedef unsigned int size_t;
-typedef unsigned long uint32_t;
-typedef char uint8_t;
-typedef short uint16_t;
+typedef unsigned int 	size_t;
+typedef unsigned long	uint32_t;
+typedef unsigned short 	uint16_t;
+typedef unsigned char 	uint8_t;
