bba6c98976067658b81b812c2607123bf3ecab28
krishpranav <krisna.pranav@gmail.com>
2022-01-03 10:10:38 +0530

    kernel: arch i386 cpi apic header

---
+++ kernel/arch/i386/include/cpu/apic.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <stdint.h>
+
+struct madt
+{
+	uint32_t signature;
+	uint32_t length;
+	uint8_t  revistion;
+	uint8_t  checksum;
+	uint8_t  OEMID[6];
+	uint8_t  OEM_table_id[8];
+	uint32_t OEM_rev;
+	uint32_t creator_id;
+	uint32_t creator_rev;
+	uint32_t local_apic;
+	uint32_t flags;
+};
+
+int init_apic();
+void apic_send_eio(int inter);
