a8cb38845e1a46185b0e4a38b5306e3e7e1abc9f
krishpranav <krisna.pranav@gmail.com>
2022-01-03 12:26:43 +0530

    bootloader: stage2 gdt asm

---
+++ bootloader/stage2/gdt.asm
@@ -0,0 +1,32 @@
+gdt_start:
+    dd 0x0
+    dd 0x0
+
+gdt_code:
+    dw 0xffff
+    dw 0x0
+    db 0x0
+    db 10011010b
+    db 11001111b
+    db 0x0
+
+gdt_data:
+    dw 0xffff
+    dw 0x0
+    db 0x0
+    db 10010010b
+    db 11001111b
+    db 0x0
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+global CODE_SEG
+global DATA_SEG
+
+CODE_SEG:   equ gdt_code - gdt_start
+DATA_SEG:   equ gdt_data - gdt_start
+
