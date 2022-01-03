88fa1f0e0313148ff626df710d66c31591048040
krishpranav <krisna.pranav@gmail.com>
2022-01-03 10:30:19 +0530

    kernel: include fs vfs_node header

--- kernel/include/fs/vfs_node.h
+++ kernel/include/fs/vfs_node.h
@@ -11,8 +11,17 @@ typedef struct DIR_s DIR;
 #include <stddef.h>
 #include <stdint.h>
 
-#define VFS_FILE 0x01
-#define VFS_READ 0
+#define VFS_FILE        0x01
+#define VFS_DIRECTORY   0x02
+#define VFS_CHARDEVICE  0x03
+#define VFS_BLOCKDEVICE 0x04
+#define VFS_PIPE        0x05
+#define VFS_SYMLINK     0x06
+
+#define VFS_MOUNTPOINT 0x08
+#define VFS_SOCKET     0x09
+
+#define VFS_READ  0
 #define VFS_WRITE 1
 
 struct dirent;
