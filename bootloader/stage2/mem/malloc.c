45d43136f36af977d57ce164c6c2f1165871d718
krishpranav <krisna.pranav@gmail.com>
2022-01-03 13:08:46 +0530

    bootloader: stage2 mem malloc

--- bootloader/stage2/mem/malloc.c
+++ bootloader/stage2/mem/malloc.c
@@ -1,18 +1,20 @@
 #include <types.h>
 
 extern uint32_t end_bin;
-uint32_t placement_address = ((uint32_t) &end_bin);
+uint32_t placement_address = ((uint32_t) &end_bin) + 0x1000;
 
 void *malloc(size_t size, int aligned)
-{   
-    if (aligned) {
-        placement_address += 0x1000;
-    }
-    placement_address += size;
-    return (void*) (placement_address - size);
+{
+	if (aligned)
+	{
+		placement_address &= 0xFFFFF000 ;
+		placement_address += 0x1000;
+	}
+	placement_address += size;
+	return (void*) (placement_address - size);
 }
 
 void free(size_t size)
 {
-    placement_address -= size;
+	placement_address -= size;
 }
