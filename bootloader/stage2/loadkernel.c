bbf82728ab9eecf9a0a1f596ca8cc6ca2fa4ab26
krishpranav <krisna.pranav@gmail.com>
2022-01-03 12:49:51 +0530

    bootloader: stage2 loadkernel c

--- bootloader/stage2/loadkernel.c
+++ bootloader/stage2/loadkernel.c
@@ -2,19 +2,19 @@
 #include <types.h>
 #include <fs/ext2.h>
 
-#define PART_OFFSET (0x7c00 + 0x1BE)
+#define PART_OFFSET 	(0x7c00 + 0x1BE) 
 
-#define FLAG_ACTIVE (1 << 7)
+#define FLAG_ACTIVE 	(1 << 7)
 
-#define FS_TYPE_EXT2 0x01
-#define FS_TYPE_FAT16 0x02
-#define FS_TYPE_FAT32 0x03
-#define FS_TYPE_EXT3 0x04 
-#define FS_TYPE_EXT4 0x05
+#define FS_TYPE_EXT2 	0x01
+#define FS_TYPE_FAT16 	0x02
+#define FS_TYPE_FAT32 	0x03
+#define FS_TYPE_EXT3 	0x04
+#define FS_TYPE_EXT4 	0x05
 
-#define KERNEL_NAME "kernel"
+#define KERNEL_NAME 	"kernel"
 
-void jump_kernel();
+extern void jump_kernel();
 
 #pragma pack(1)
 typedef struct partition_s
@@ -39,27 +39,60 @@ typedef struct partition_s
 
 int determine_fs(unsigned int startlba, unsigned int total_sects, unsigned int disk, unsigned int blocksize)
 {
-    return FS_TYPE_EXT2;
+	return FS_TYPE_EXT2;
 }
 
 int load_kernel(unsigned int startlba, unsigned int total_sects, unsigned int disk, unsigned int blocksize)
 {
-    switch (determine_fs(startlba, total_sects, disk, blocksize)) {
-        case FS_TYPE_EXT2:
-            load_ext2_kernel(KERNEL_NAME, startlba, total_sects, disk, blocksize);
-            return 0;
-        
-        default:
-            return 1;
-    }
-    return 0;
+	switch (determine_fs(startlba, total_sects, disk, blocksize)){
+		case FS_TYPE_EXT2:
+			load_ext2_kernel(KERNEL_NAME, startlba, total_sects, disk, blocksize);
+			return 0;
+
+		default:
+			return 1;
+	}
+	return 0;
 }
 
 void findkernel(unsigned int disk, unsigned int blocksize)
 {
-    print("Finding bootable partition table\n");
 
-    print("disk: "); print_int(disk); print(" bs: "); print_int(blocksize); print("\n");
+	print("Finding bootable partition\n");
 
-    int jump = 0;
-}
+	print("disk: "); print_int(disk); print(" bs: "); print_int(blocksize); print("\n");
+
+	int jump = 0;
+
+	partition_t *part_table = (partition_t*) PART_OFFSET;
+	for (size_t i = 0; i < 4; i++)
+	{
+		if (part_table[i].flags & FLAG_ACTIVE)
+		{
+			print("Bootable: "); print_int(i); print("\n");
+
+			if (load_kernel(part_table[i].part_start_lba, part_table[i].total_sects, disk, blocksize) != 0)
+			{
+				print("Loading kernel failed\n");
+				for (;;);
+			}
+
+			print("Successfully loaded kernel\n");
+			jump = 1;
+			goto go_asm;
+		}
+
+	}
+
+go_asm:
+	
+	if (!jump)
+	{
+		print("Error, no bootable partition found\nCheck if the parititon you want to boot from is actually flagged for boot");
+		for(;;);
+	}
+
+	jump_kernel();
+
+	return;
+} 
