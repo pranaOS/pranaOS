f512d58e06243001b0b3b10c010314eeb8940c0b
krishpranav <krisna.pranav@gmail.com>
2022-01-03 15:10:00 +0530

    external: config header

--- external/config.h
+++ external/config.h
@@ -1,6 +1,9 @@
 #pragma once
 
-#define HAVE_OWN_STDLIB 1 
+#define HAVE_OWN_STDLIB 1
 #define HAVE_OWN_STRING 1
 
 #define ECLIB_PROV_MALLOC kmalloc
+#define ECLIB_PROV_FREE   kfree
+#define ECLIB_PROV_MEMSET memset
+#define ECLIB_PROV_MEMCPY memcpy
