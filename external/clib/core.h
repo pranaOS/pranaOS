e50354506e62459cd499048dd2c7bddd5a2c8253
krishpranav <krisna.pranav@gmail.com>
2022-01-03 13:49:14 +0530

    external: clib core header

--- external/clib/core.h
+++ external/clib/core.h
@@ -12,5 +12,14 @@
 	#define eclib_free   free
 #endif 
 
+#ifdef HAVE_OWN_STRING
+	#define eclib_memcpy ECLIB_PROV_MEMCPY
+	#define eclib_memset ECLIB_PROV_MEMSET
+#else
+	#include <string.h>
+	#define eclib_memcpy memcpy
+	#define eclib_memset memset
+#endif
+
 #define ECLIB_EIND   1
 #define ECLIB_EDESTR 2
