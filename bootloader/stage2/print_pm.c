900d7d2b9f7ac9a32014b55132005d814087ad27
krishpranav <krisna.pranav@gmail.com>
2022-01-03 12:33:57 +0530

    bootloader: stage2 print_pm clang

--- bootloader/stage2/print_pm.c
+++ bootloader/stage2/print_pm.c
@@ -1,19 +1,19 @@
 #include <types.h>
 
 #define VIDEO_MEM 0xb8000
-#define VIDEO_COLS 80
-#define VIDEO_ROWS 25 
+
+#define VIDEO_COLS	80
+#define VIDEO_ROWS 	25
 
 size_t loc = 0;
 
 void print_char(char c)
 {
-    char *videoMem = (char*) VIDEO_MEM;
-    videoMem[loc] = c;
-    videoMem[loc+1] = 0x0F;
-    loc += 2;
+	char *videoMem = (char*) VIDEO_MEM;
+	videoMem[loc] = c;
+	videoMem[loc+1] = 0x0F;
+	loc += 2;
 }
-
 
 void print(char *txt)
 {
@@ -38,9 +38,40 @@ void print(char *txt)
 
 int strlen(char *str)
 {
-    int size = 0;
-    while (str[size])
-        size++;
-    
-    return size;
+	int size = 0;
+	while (str[size])
+		size++;
+	return size;
+}
+
+void print_int(int integer)
+{
+	int tmp = integer;
+	int rev_tmp = 0;
+	unsigned int z_b = 0;
+	while (tmp > 0)
+	{
+		rev_tmp = rev_tmp * 10 + tmp % 10;
+		if ((rev_tmp == 0) && ((tmp % 10) == 0))
+		{
+			z_b++;
+		}
+		tmp /= 10;
+	}
+
+	if (rev_tmp == 0)
+	{
+		print_char('0');
+	}
+
+	while (rev_tmp > 0)
+	{
+		print_char('0' + (rev_tmp % 10));
+		rev_tmp /= 10;
+	}
+
+	for (int i = 0; i < z_b; i++)
+	{
+		print_char('0');
+	}
 }
