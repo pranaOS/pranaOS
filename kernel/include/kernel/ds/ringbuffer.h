6ecdc5c387c196c020b0b276677e7210970ff814
krishpranav <krisna.pranav@gmail.com>
2022-01-03 10:19:17 +0530

    kernel: include ds ringbuffer header

---
+++ kernel/include/kernel/ds/ringbuffer.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <kernel/atomic.h>
+
+#define RINGBUFFER_OPTIMIZE_USHORTINT (1 << 0)
+
+typedef uint32_t cb_flags_t;
+
+struct ringbuffer {
+    unsigned long size;
+
+    char *buffer_start;
+    char *buffer_end;
+
+    volatile unsigned int lock;
+    struct spinlock spinlock;
+
+    cb_flags_t flags;
+};
+
+struct ringbuffer *create_ringbuffer(size_t size, cb_flags_t flags);
+void ringbuffer_destroy(struct ringbuffer *rb);
