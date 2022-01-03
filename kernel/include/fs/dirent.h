5cc085b24afa71a1edbe80fe7156ceabfe4fc7ca
krishpranav <krisna.pranav@gmail.com>
2022-01-03 09:10:14 +0530

    kernel: include fs dirent header

--- kernel/include/fs/dirent.h
+++ kernel/include/fs/dirent.h
@@ -5,22 +5,23 @@
 struct filesystem_s;
 typedef struct filesystem_s filesystem_t;
 
-struct dirent 
+struct dirent
 {
-    unsigned long d_ino;
-    unsigned long d_off;
-    unsigned short d_reclen;
+	unsigned long  d_ino;    
+	unsigned long  d_off;    
+	unsigned short d_reclen; 
 
-    char d_type;
-    char d_name[];
+	char d_type;   
+	char d_name[]; 
 };
 
 typedef struct DIR_s
 {
-    filesystem_t *fs_info;
-    offset_t next_direntry_offset;
-    ino_t inode;
-    blkcnt_t blockpointerindex;
-    size_t lastentrysize;
-    void * filebuffer;
+	filesystem_t *fs_info;
+	offset_t      next_direntry_offset; 
+	ino_t         inode;
+	blkcnt_t      blockpointerindex;
+	size_t        lastentrysize;
+	void *        filebuffer;
+	struct dirent dirent;
 } DIR;
