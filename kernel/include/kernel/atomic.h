88c261fe3393a7561f6c86921725654315ef4048
krishpranav <krisna.pranav@gmail.com>
2022-01-03 10:16:29 +0530

    kernel: include kernel atomic header

--- kernel/include/kernel/atomic.h
+++ kernel/include/kernel/atomic.h
@@ -3,16 +3,18 @@
 #include <cpu/atomic.h>
 
 struct spinlock {
-    int locked;
+	int locked;
 };
 
-#define SPINLOCK_INIT {0};
-#define SPINLOCK_LOCK 1;
+#define SPINLOCK_INIT   {0};
+#define SPINLOCK_LOCK   1
 #define SPINLOCK_UNLOCK 0
 
 #define atomic_test_and_set(mem, bit) arch_atomic_test_and_set(mem, bit)
 #define atomic_compare_exchange(mem, bitunset, bitset) \
-arch_atomic_compare_exchange(mem, bitunset, bitset)
+	arch_atomic_compare_exchange(mem, bitunset, bitset)
+
+#define atomic_store(mem, bit) arch_atomic_store(mem, bit)
 
 void spinlock_lock(struct spinlock *sl);
 void spinlock_unlock(struct spinlock *sl);
