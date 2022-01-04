9f907750c64378c1071519c3723cb9ab040c8680
krishpranav <krisna.pranav@gmail.com>
2022-01-04 19:47:20 +0530

    kernel: include drivers time header

---
+++ kernel/include/drivers/time.h
@@ -0,0 +1,18 @@
+#pragma once
+
+struct time_s {
+	int tm_sec;
+	int tm_min;
+	int tm_hour;
+	int tm_mday;
+	int tm_day;
+	int tm_mon;
+	int tm_year;
+	int tm_wdag;
+	int tm_yday;
+	int tm_isdst;
+};
+
+int init_time();
+
+struct time_s *get_time();
