627e46e36183e8cd87cf8302a88d610ed88f974a
krishpranav <krisna.pranav@gmail.com>
2022-01-04 19:46:04 +0530

    kernel: include drivers keyboard header

---
+++ kernel/include/drivers/keyboard.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define KEYMAP_FILE "/etc/default/keymap"
+#define KEYMAP_FILE_PREFIX "/etc/keyboard"
+#define KEYMAP_SIZE 128
+#define KEYMAP_FILE_SIZE 256*2
+
+struct key_mod {
+    int ctrl;
+    int shift;
+    int alt;
+    int altgr;
+};
+
+int init_keyboard();
