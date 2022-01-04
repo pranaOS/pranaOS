2637cc644be2c5dfc2fe9882ee034a972fd853c3
krishpranav <krisna.pranav@gmail.com>
2022-01-04 19:54:12 +0530

    kernel: include drivers ps2 mouse header

--- kernel/include/drivers/ps2/mouse.h
+++ kernel/include/drivers/ps2/mouse.h
@@ -1,11 +1,14 @@
-#pragma once 
+#pragma once
 
 #include <stdint.h>
-
 
 typedef void (*mousehook_fpointer)(int16_t xrel, int16_t yrel, uint16_t xloc,
                                    uint16_t yloc, int lbut, int rbut, int mbut);
 
-int init_mouse();
-void mouse_poll();
-void mouse_loop();
+int     init_mouse();
+void    mouse_poll();
+uint8_t register_mouse_hook(mousehook_fpointer hook);
+uint8_t remove_mouse_hook(mousehook_fpointer hook);
+void    mouse_loop();
+
+#endif
