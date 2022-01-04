31dc555cf655929ae2ab8355b7ae227050930d21
krishpranav <krisna.pranav@gmail.com>
2022-01-04 19:43:28 +0530

    kernel: include drivers drive header

--- kernel/include/drivers/drive.h
+++ kernel/include/drivers/drive.h
@@ -6,12 +6,15 @@
 struct drive_s;
 
 typedef ssize_t (*read_drive_fp)(unsigned long offset, size_t count, void *buf,
-                                struct drive_s drive);
+                                 struct drive_s drive);
+typedef ssize_t (*write_drive_fp)(unsigned long offset, size_t count, void *buf,
+                                  struct drive_s drive);
 
 typedef struct drive_s {
-    int type;
-    char *name;
-    void *drive_info;
+	int   type;
+	char *name;
+	void *drive_info; 
 
-    read_drive_fp read;
+	read_drive_fp  read;
+	write_drive_fp write;
 } drive_t;
