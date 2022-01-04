f86222f094b26585c02604c775db12d5678b116c
krishpranav <krisna.pranav@gmail.com>
2022-01-04 19:50:13 +0530

    kernel: include drivers networking rtl8139 header

---
+++ kernel/include/drivers/networking/rtl8139.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <drivers/pci.h>
+
+typedef struct {
+    pci_device_t *pci_dev;
+    int bar_type;
+    uint32_t io_base;
+    uint32_t mem_base;
+    void* rx_buffer;
+    int tx_current
+    int current_packet_offset;
+} rtl8139_dev_t;
