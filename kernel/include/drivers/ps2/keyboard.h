74a759d2b27352637a144ae7f249e280c361cfbe
krishpranav <krisna.pranav@gmail.com>
2022-01-04 19:52:46 +0530

    kernel: include drivers ps2 keyboard header

--- kernel/include/drivers/ps2/keyboard.h
+++ kernel/include/drivers/ps2/keyboard.h
@@ -2,8 +2,10 @@
 
 #include <stdint.h>
 
+typedef void (*keyhook_fpointer)(uint8_t);
+
 int init_ps2_keyboard();
 
 void register_keyboard_hook(keyhook_fpointer hook);
 
-void remove_keyboard_hook();
+void remove_keyboard_hook(keyhook_fpointer hook);
