65aed7e1f03058fe10a635eb64a3decab4969413
krishpranav <krisna.pranav@gmail.com>
2022-01-04 19:31:55 +0530

    kernel: include fs ext2 header

--- kernel/include/fs/ext2/ext2.h
+++ kernel/include/fs/ext2/ext2.h
@@ -27,7 +27,7 @@ typedef struct {
 
 	uint16_t ext2_signature; 
 	                         
-	uint16_t fs_state;      
+	uint16_t fs_state;       
 	uint16_t
 		when_error; 
 	uint16_t minor_version;
@@ -35,7 +35,7 @@ typedef struct {
 	uint32_t last_check_time;       
 	uint32_t forced_check_interval; 
 	uint32_t os_creator_id; 
-	                        
+
 	uint32_t major_version;
 
 	uint16_t uid;
@@ -61,7 +61,7 @@ typedef struct {
 
 	uint16_t UNUSED;
 
-	uint8_t  journal_id[16]; 
+	uint8_t  journal_id[16];
 	uint32_t journal_inode;
 	uint32_t journal_device;
 	uint32_t orphan_inode_list_head;
@@ -69,53 +69,55 @@ typedef struct {
 } __attribute__((packed)) ext2_superblock_t;
 
 typedef struct {
-    uint32_t block_bitmap;
-    uint32_t inode_bitmap;
-    uint32_t start_inode_table;
-    uint32_t unallocated_block_count;
-    uint16_t unallocated_inode_count;
-    uint16_t directory_count;
-    uint8_t unused[14];
+	uint32_t block_bitmap;            
+	uint32_t inode_bitmap;            
+	uint32_t start_inode_table;       
+	uint16_t unallocated_block_count; 
+	uint16_t unallocated_inode_count; 
+	uint16_t directory_count;
+	uint8_t  unused[14];
 } __attribute__((packed)) ext2_block_group_descriptor_t;
 
 typedef struct {
+	uint16_t type_permissions; 
+	uint16_t uid;
+	uint32_t size;             
+	uint32_t last_access_time; 
+	uint32_t creation_time;    
+	uint32_t last_mod_time;    
+	uint32_t deletion_time;    
 
-    uint16_t type_permissions;
-    uint16_t uid;
-    uint32_t size;
-    uint32_t last_access_time;
-    uint32_t creation_time;
-    uint32_t last_mod_time;
-    uint32_t deletion_time;
+	uint16_t gid;
 
-    uint16_t gid;
+	uint16_t hard_link_count;
+	uint32_t disk_sectors; 
 
-    uint16_t hard_link_count;
-    uint32_t disk_sectors;
+	uint32_t flags;
+	uint32_t os_specific_value_1;
 
-    uint32_t flags;
-    uint32_t os_specific_value_1;
+	uint32_t direct_block_pointer[12];
+	uint32_t single_indirect_block_pointer;
+	uint32_t double_indirect_block_pointer;
+	uint32_t triple_indirect_block_pointer;
 
-    uint32_t direct_block_pointer[12];
-    uint32_t single_indirect_block_pointer;
-    uint32_t double_indirect_block_pointer;
-    uint32_t triple_indirect_block_pointer;
-
-    uint32_t gen_number;
-    uint32_t reserved;
-    uint32_t reserved2;
-    uint32_t block_addr_fragment;
-    uint32_t os_specific_value_2[3];
+	uint32_t gen_number;
+	uint32_t reserved;
+	uint32_t reserved2;
+	uint32_t block_addr_fragment;
+	uint32_t os_specific_value_2[3]; 
 
 } __attribute__((packed)) ext2_inode_t;
 
 typedef struct {
-    uint32_t inode;
-    uint16_t total_size_of_entry;
-    uint8_t name_length;
-
-    uint8_t type_indicator;
-    char name;
+	uint32_t inode;
+	uint16_t total_size_of_entry; 
+	uint8_t  name_length; 
+	uint8_t type_indicator;
+	char    name; 
+				  
 } __attribute__((packed)) ext2_directory_entry_t;
 
-filesystem_t *init_ext2_filesystem(char *name, unsigned int ext2_start, disk_t *disk_info);
+filesystem_t *init_ext2_filesystem(char *name, unsigned int ext2_start,
+                                   disk_t *disk_info);
+
+#endif
