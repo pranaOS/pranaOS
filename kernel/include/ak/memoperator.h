3edcebe660d168f068521dd426d78eb08d9236b4
krishpranav <krisna.pranav@gmail.com>
2022-01-05 19:50:45 +0530

    kernel: include ak memoperator header size_t -> uint32_t
    change the data function

--- kernel/include/ak/memoperator.h
+++ kernel/include/ak/memoperator.h
@@ -3,18 +3,17 @@
 #include "types.h"
 
 namespace pranaOS {
-    
-    namespace ak
-    {
+    namespace ak {
         #define phys2virt(x) ((x) + 3_GB)
         #define virt2phys(x) ((x) - 3_GB)
 
-        class MemOperator {
+        class MemoryOperations
+        {
         public:
             static void* memmove(void* dstptr, const void* srcptr, uint32_t size);
             static int memcmp(const void* aptr, const void* bptr, uint32_t size);
-            static void* memset(void* bufptr, char value, size_t size);
-            static void* memcpy(void* dstptr, const void* srcptr, size_t size);
+            static void* memset(void* bufptr, char value, uint32_t size);
+            static void* memcpy(void* dstptr, const void* srcptr, uint32_t size);
         };
-    } 
+    }
 }
