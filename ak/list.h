f64215a092d85f0a1a57c7004f1c80986eb0fd62
krishpranav <krisna.pranav@gmail.com>
2022-01-05 20:17:12 +0530

    ak: list header

--- ak/list.h
+++ ak/list.h
@@ -3,18 +3,23 @@
 #include <system/tasking/lock.h>
 
 namespace pranaOS {
-    namespace ak {
+    namespace ak
+    {
         template <typename T>
-        struct ListNode {
-            ListNode(const T &e) : data(e), next(0)
+        struct ListNode
+        {
+            ListNode(const T &e) : data(e), next(0), prev(0)
             {}
 
             T data;
             ListNode<T>* next;
+            ListNode<T>* prev;
         };
 
+
         template <typename T>
-        class List {
+        class List
+        {
         public:
             List() : head_(0), tail_(0), size_(0)
             {}
@@ -24,11 +29,10 @@ namespace pranaOS {
                 this->clear();
             }
 
-            int size() { 
+            int size() {
                 return size_;
             }
-
-            void push_back(const T &e)
+            void push_back(const T &e);
             void push_front(const T &e);
             void clear();
 
@@ -38,7 +42,7 @@ namespace pranaOS {
 
             void remove(int index);
             void remove(const T &e);
-        
+
         private:
             ListNode<T>* head_;
             ListNode<T>* tail_;
@@ -46,39 +50,47 @@ namespace pranaOS {
 
             int size_;
 
-            ListNode<t>* insertInternal(const T &e, ListNode<T>* pos);
+            ListNode<T>* insertInternal(const T &e, ListNode<T>* pos);
             void removeInternal(ListNode<T> *pos);
 
         public:
             class iterator {
             public:
-                iterator(ListNode<T> *p=0) : pos_(p) {}
-
+                iterator(ListNode<T> *p=0) : pos_(p) { }
+                
                 T &operator*() {
                     return pos_->data;
                 }
-
+    
                 T *operator->() {
                     return &(pos_->data);
                 }
-
+    
                 bool operator!=(const iterator &rhs) {
                     return this->pos_ != rhs.pos_;
                 }
-
+    
                 iterator operator++() {
-                    pos_ = pos_->next;
-                    return *this;
+                    pos_ = pos_->next; return *this;
                 }
-
+    
                 iterator operator--() {
-                    pos_ = pos_->prev;
-                    return *this;
+                    pos_ = pos_->prev; return *this;
                 }
-
+    
             private:
                 ListNode<T> *pos_;
             };
-        }
+
+            iterator begin()
+            {
+                return iterator(head_);
+            }
+
+            iterator end()
+            {
+                return iterator(0);
+            }
+        };
     }
 }
