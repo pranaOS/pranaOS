c5a27600951ba2e68c5f489bbd7fc002c4533d2b
krishpranav <krisna.pranav@gmail.com>
2022-01-05 19:31:59 +0530

    kernel: include ak types header

---
+++ kernel/include/ak/types.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace pranaos {
+    namespace ak {
+        typedef char                        int8_t;
+        typedef unsigned char               uint8_t;
+        typedef short                       int16_t;
+        typedef unsigned short              uint16_t;
+        typedef int                         int32_t;
+        typedef unsigned int                uint32_t;
+        typedef long long int               int64_t;
+        typedef unsigned long long int      uint64_t;
+        typedef unsigned long long          uintptr_t;
+
+    }
+}
