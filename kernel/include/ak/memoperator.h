57762721ec1ae2a84030ca9349a1365f0a31e691
krishpranav <krisna.pranav@gmail.com>
2022-01-05 19:50:05 +0530

    kernel: include ak memoperator header

---
+++ kernel/include/ak/memoperator.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "types.h"
+
+namespace pranaOS {
+    
+    namespace ak
+    {
+        #define phys2virt(x) ((x) + 3_GB)
+        #define virt2phys(x) ((x) - 3_GB)
+
+        class MemOperator {
+        public:
+            static void* memmove(void* dstptr, const void* srcptr, uint32_t size);
+            static int memcmp(const void* aptr, const void* bptr, uint32_t size);
+            static void* memset(void* bufptr, char value, size_t size);
+            static void* memcpy(void* dstptr, const void* srcptr, size_t size);
+        };
+    } 
+}
