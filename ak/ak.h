c049bc70ad201a4e79d188bb720246ef381083fe
krishpranav <krisna.pranav@gmail.com>
2022-01-06 16:48:58 +0530

    ak: ak header

--- ak/ak.h
+++ ak/ak.h
@@ -1,9 +1,9 @@
-#include "convert.h"
-#include "list.h"
-#include "memoperator.h"
-#include "print.h"
-#include "random.h"
-#include "string.h"
-#include "types.h"
+#include <convert.h>
+#include <list.h>
+#include <memoperator.h>
+#include <print.h>
+#include <random.h>
+#include <string.h>
+#include <types.h>
 
 using namespace pranaOS::ak;
