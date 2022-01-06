d204e3237954832b09ed10e2fbca001ce8e38dc3
krishpranav <krisna.pranav@gmail.com>
2022-01-06 10:56:12 +0530

    ak: convert cpp

--- ak/convert.cpp
+++ ak/convert.cpp
@@ -3,7 +3,28 @@
 
 using namespace pranaOS::ak;
 
-int isspace(char c)
-{
+int isspace(char c) {
     return c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r';
+}
+
+char convert::intToString(int n) {
+    static char ret[24];
+    int numChars = 0;
+    bool isNegative = false;
+
+    if (n < 0) {
+        n = -n;
+        isNegative = true;
+        numChars++;
+    }
+    int temp = n 
+
+    do {
+        numChars++;
+        temp /= 10;
+    } while (temp);
+
+    ret[numChars] = 0;
+
+    
 }
