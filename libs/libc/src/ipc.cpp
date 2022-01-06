5f2747e94b7d642a2149504981f77cd7a1e3b45f
krishpranav <krisna.pranav@gmail.com>
2022-01-06 16:32:13 +0530

    libs: libc ipc

--- libs/libc/src/ipc.cpp
+++ libs/libc/src/ipc.cpp
@@ -1,22 +1,23 @@
 #include <ipc.h>
+#include <proc.h>
 #include <string.h>
 
 using namespace IPC;
 
 int IPC::IPCSend(int dest, int type, unsigned int arg1, unsigned int arg2, unsigned int arg3, unsigned int arg4, unsigned int arg5, unsigned int arg6) {
     IPCMessage message;
-
+    
     message.dest = dest;
     message.type = type;
     message.source = Process::ID;
-
+    
     message.arg1 = arg1;
     message.arg2 = arg2;
     message.arg3 = arg3;
     message.arg4 = arg4;
     message.arg5 = arg5;
     message.arg6 = arg6;
-
+    
     return IPCSend(message);
 }
 
@@ -28,7 +29,8 @@ int IPC::IPCAvailable() {
     return DoSyscall(SYSCALL_IPC_AVAILABLE);
 }
 
-IPCMessage IPC::ICPReceive(int formID, int* errOut, int type) {
+IPCMessage IPC::ICPReceive(int fromID, int* errOut, int type) {
     IPCMessage result;
+    DoSyscall(SYSCALL_IPC_RECEIVE, (uint32_t)&result, fromID, (uint32_t)errOut, type);
     return result;
 }
