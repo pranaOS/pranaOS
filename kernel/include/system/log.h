de7e3942cc001f01eef07d57bef3a33f3cc4e0e3
krishpranav <krisna.pranav@gmail.com>
2022-01-06 13:21:18 +0530

    kernel: include system log header

--- kernel/include/system/log.h
+++ kernel/include/system/log.h
@@ -3,16 +3,19 @@
 #include <ak/types.h>
 
 namespace pranaOS {
+
     namespace system {
+
         #define LOG_SHOW_MS 1
-        
-        enum LogLevel { 
+
+        enum LogLevel
+        {
             Info,
             Warning,
             Error
         };
 
         void log(LogLevel level, const char* __restrict__ format, ...);
-        void print(const char* data);
+        void print(const char* data, ak::uint32_t length);
     }
 }
