16999545bfc9482c83a93c882e38ea2c76c878cd
krishpranav <krisna.pranav@gmail.com>
2022-01-06 12:38:34 +0530

    ak: random cpp

--- ak/random.cpp
+++ ak/random.cpp
@@ -4,13 +4,19 @@ using namespace pranaOS::ak;
 
 uint32_t Random::next;
 
-
-int Random::next(uint32_t max) {
+int Random::next(uint32_t max)
+{
     next = next * 1103515245 + 12345;
 
     return (unsigned int)(next / 65536) % (max+1);
 }
 
-void Random::setSeed(uint32_t seed) {
+int Random::next(uint32_t min, uint32_t max)
+{
+    return next(max-min) + min;
+}
+
+void Random::setSeed(uint32_t seed)
+{
     next = seed;
 }
