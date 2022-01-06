112282be5031442aa43f3724c87ab048dc189537
krishpranav <krisna.pranav@gmail.com>
2022-01-06 18:12:09 +0530

    libc: include bitreader header

--- libs/libc/include/bitreader.h
+++ libs/libc/include/bitreader.h
@@ -2,7 +2,7 @@
 
 #include <types.h>
 
-namespace pranaOSBitreader {
+namespace bitreader {
     class BitReader {
     public:
         BitReader(uint8_t* data) {
@@ -20,15 +20,38 @@ namespace pranaOSBitreader {
         }
 
         uint8_t ReadBit() {
-            if (this->numBits <= 0) {
+            if(this->numBits <= 0) {
                 this->byte = this->ReadByte();
                 this->numBits = 8;
             }
-
             this->numBits -= 1;
             uint8_t bit = this->byte & 1;
             this->byte >>= 1;
             return bit;
         }
+
+        template<typename T>
+        T ReadBits(uint32_t n) {
+            T ret = 0;
+            for(uint32_t i = 0; i < n; i++)
+                ret |= (this->ReadBit() << i);
+            
+            return ret;            
+        }
+
+        template<typename T>
+        T ReadBytes(uint32_t n) {
+            T ret = 0;
+            for(uint32_t i = 0; i < n; i++)
+                ret |= (this->ReadByte() << (i*8));
+            
+            return ret;
+        }
+    
+    private:
+        uint8_t* dataPtr = 0;
+        uint32_t pos = 0;
+        uint8_t  byte = 0;
+        uint32_t numBits = 0;
     };
 }
