883d2af4ab94e36e0a38641448f77474c29b93ce
krishpranav <krisna.pranav@gmail.com>
2022-01-06 12:49:53 +0530

    ak: string cpp

--- ak/string.cpp
+++ ak/string.cpp
@@ -12,11 +12,43 @@ int String::strlen(const char* str) {
 }
 
 bool String::strcmp(const char* strA, const char* strB) {
-    while(*strA = strB) {
-        if (*strA == '\0')
-            return true;
-        strA++;
-        strB++;
+    while(*strA == *strB)
+    {
+        if(*strA == '\0')
+            return true; 
+        ++strA;
+        ++strB;
     }
+    return false;
+}
+
+bool String::strncmp(const char* s1, const char* s2, int n) {
+    while(n--)
+        if(*s1++ != *s2++)
+            return false;
+
+    return true; 
+}
+
+int String::indexof(const char* str, char c, uint32_t skip) {
+    uint32_t hits = 0;
+    int i = 0;
+    while(str[i]) {
+        if (str[i] == c && hits++ == skip)
+            return i;
+        i++;
+    }
+
+    return -1;
+}
+
+bool String::contains(const char* str, char c) {
+    int i = 0;
+    while (str[i]) {
+        if (str[i] == c)
+            return true;
+            i++;
+    }
+
     return false;
 }
