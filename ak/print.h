9b6bcdbf94fd4dc942432a1e0c67b93161da1d42
krishpranav <krisna.pranav@gmail.com>
2022-01-06 10:35:59 +0530

    ak: print header

--- ak/print.h
+++ ak/print.h
@@ -2,16 +2,22 @@
 
 #include "types.h"
 #include "convert.h"
+#include <system/console.h>
 
-namespace pranaOS {
-    namespace ak {
-        class Print {
+namespace CactusOS
+{
+    namespace common
+    {
+        class Print
+        {
         public:
-            static void printfhex(uint8_t key);
-            static void printfhex16(uint16_t key);
-            static void printfhex32(uint32_t key);
+            static void printfHex(uint8_t key);
+            static void printfHex16(uint16_t key);
+            static void printfHex32(uint32_t key);
             static void printbits(uint8_t key);
-            static void printbits(uint16_t key);  
+            static void printbits(uint16_t key);
+            static void printbits(uint32_t key);
+            static void printbits(uint64_t key);
         };
     }
 }
