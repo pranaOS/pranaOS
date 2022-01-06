8f5fb43d75bcd3ba43189e63921404b5306e2c81
krishpranav <krisna.pranav@gmail.com>
2022-01-06 16:06:43 +0530

    tests: dirtest

--- tests/dirtest.js
+++ tests/dirtest.js
@@ -7,19 +7,3 @@ exec('find kernel', function(error, stdout, stderr) {
         console.log('error: ' + error);
     }
 });
-
-exec('find libs', function(error, stdout, stderr) {
-    console.log('libs stdout: ' + stdout);
-    console.log('libs stderr: ' + stderr);
-    if (error !== null) {
-        console.log('error: ' + error);
-    }
-});
-
-exec('find ak', function(error, stdout, stderr) {
-    console.log('ak stdout: ' + stdout);
-    console.log('ak stderr: ' + stderr);
-    if (error !== null) {
-        console.log('error: ' + error);
-    }
-});
