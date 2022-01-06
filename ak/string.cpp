d0731efcd921588462fb415d61690ba2fac814cc
krishpranav <krisna.pranav@gmail.com>
2022-01-06 13:12:59 +0530

    ak: string cpp

--- ak/string.cpp
+++ ak/string.cpp
@@ -1,14 +1,13 @@
-#include <ak/string.h>
+#include "string.h"
 
-using namespace pranaOS::ak;
 using namespace pranaOS;
+using namespace pranaOS::ak;
 
 int String::strlen(const char* str) {
     uint32_t len = 0;
-    while (str[len])
-        len++;
-    
-    return len;
+	while (str[len])
+		len++;
+	return len;
 }
 
 bool String::strcmp(const char* strA, const char* strB) {
@@ -26,34 +25,33 @@ bool String::strncmp(const char* s1, const char* s2, int n) {
     while(n--)
         if(*s1++ != *s2++)
             return false;
-
-    return true; 
+    return true;
 }
 
 int String::indexof(const char* str, char c, uint32_t skip) {
     uint32_t hits = 0;
     int i = 0;
-    while(str[i]) {
-        if (str[i] == c && hits++ == skip)
+    while(str[i])
+    {
+        if(str[i] == c && hits++ == skip)
             return i;
         i++;
     }
-
     return -1;
 }
 
 bool String::contains(const char* str, char c) {
     int i = 0;
-    while (str[i]) {
-        if (str[i] == c)
+	while (str[i])
+    {
+        if(str[i] == c)
             return true;
-            i++;
+		i++;
     }
-
     return false;
 }
 
-List<char*> String::Split(const char* str, char d) {
+List<char*> String::split(const char* str, char d) {
     List<char*> result = List<char*>();
     int len = String::strlen(str);
     int pos = 0;
@@ -76,7 +74,7 @@ List<char*> String::Split(const char* str, char d) {
         int lastLen = len - pos;
         if(lastLen > 0) {
             char* part = new char[lastLen + 1];
-            MemoryOperations::memcpy(part, str + pos, lastLen);
+            MemOperator::memcpy(part, str + pos, lastLen);
             part[lastLen] = '\0';
             result.push_back(part);
         }
@@ -106,28 +104,49 @@ char* String::lowercase(char* str) {
 		i++;
 	}
     return str;
-} 
-
-char* String::uppercase(char* str) { 
-    int len = strlen(str);
-    int i = 0;
-	while (i < len)
-	{
-		if ((short)str[i] >= 97 && (short)str[i] <= 122)
-			str[i] -= 32;
-		i++;
-	}
-    return str;
 }
 
-char* String::lowercase(char* str) {
-    int len = strlen(str);
-    int i = 0;
-	while (i < len)
+char String::uppercase(char c) {
+    if (c >= 97 && c <= 122)
+		return c - 32;
+    
+    return c;
+}
+
+char String::lowercase(char c) {
+	if (c >= 65 && c <= 90)
+		return c + 32;
+    
+    return c;
+}
+
+char* String::strcpy(char *s1, const char *s2) {
+	strncpy(s1, s2, strlen(s2) + 1);
+	s1[strlen(s2)] = '\0'; 
+	return s1;
+}
+
+char* String::strncpy(char *s1, const char *s2, unsigned int n) {
+	unsigned int extern_iter = 0;
+
+	unsigned int iterator = 0;
+	for (iterator = 0; iterator < n; iterator++) 
 	{
-		if ((short)str[i] >= 65 && (short)str[i] <= 90)
-			str[i] += 32;
-		i++;
+		if (s2[iterator] != '\0')
+			s1[iterator] = s2[iterator];
+		else 
+		{
+			s1[iterator] = s2[iterator];
+			extern_iter = iterator + 1;
+			break;
+		}
 	}
-    return str;
+
+	while (extern_iter < n) 
+	{
+		s1[extern_iter] = '\0';
+		extern_iter++;
+	}
+
+	return s1;
 }
