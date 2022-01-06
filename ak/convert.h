9cf6dd9333a3e99286476bb10f77899de92a3e47
krishpranav <krisna.pranav@gmail.com>
2022-01-06 11:04:28 +0530

    ak: convert header[char funcs to covnert hex to string + int to string]

--- ak/convert.h
+++ ak/convert.h
@@ -8,6 +8,13 @@ namespace pranaOS {
         class Convert {
         public:
             static char* intToString(int i);
+            static char* intToString32(uint32_t i);
+
+            static char intToHexString(ak::uint8_t w);
+            static char intToHexString(ak::uint16_t w);
+            static char intToHexString(ak::uint32_t w);
+
+            static int stringToInt(char* string);
         }
     }
 }
