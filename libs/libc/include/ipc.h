80e8c1a85250a43d4c692b7751d0cc90c3dfd61a
krishpranav <krisna.pranav@gmail.com>
2022-01-06 13:47:17 +0530

    libs: libc include ipc header

--- libs/libc/include/ipc.h
+++ libs/libc/include/ipc.h
@@ -1,6 +1,6 @@
 #pragma once
 
-namespace Ipc {
+namespace IPC {
 
     enum IPCMessageType : int
     {
@@ -8,11 +8,11 @@ namespace Ipc {
         GUIRequest = 1,
         GUIEvent = 2
     };
-    
+
     struct IPCMessage {
-        int source;
-        int dest;
-        int type;
+        int source; 
+        int dest;   
+        int type; 
 
         unsigned int arg1;
         unsigned int arg2;
@@ -23,8 +23,8 @@ namespace Ipc {
     };
 
     int IPCSend(int dest, int type = IPCMessageType::None, unsigned int arg1 = 0, unsigned int arg2 = 0, unsigned int arg3 = 0, unsigned int arg4 = 0, unsigned int arg5 = 0, unsigned int arg6 = 0);
-
     int IPCSend(IPCMessage message);
-
     int IPCAvailable();
+
+    IPCMessage ICPReceive(int fromID = -1, int* errOut = 0, int type = -1);
 }
