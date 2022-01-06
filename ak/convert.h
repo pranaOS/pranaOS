3addb111be06eefc2b966fb43558fde6e71e1b80
krishpranav <krisna.pranav@gmail.com>
2022-01-06 10:58:25 +0530

    ak: convert header

--- ak/convert.h
+++ ak/convert.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include "types.h"
+#include "memoperator.h"
+
+namespace pranaOS {
+    namespace ak {
+        class Convert {
+        public:
+            static char* intToString(int i);
+        }
+    }
+}
