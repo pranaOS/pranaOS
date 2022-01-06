e3ab37eb11d755e1381ef6cf2ce0e0b06b41ec97
krishpranav <krisna.pranav@gmail.com>
2022-01-06 13:19:22 +0530

    kernel: include system log header

---
+++ kernel/include/system/log.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <ak/types.h>
+
+namespace pranaOS {
+    namespace system {
+        #define LOG_SHOW_MS 1
+        
+        enum LogLevel { 
+            Info,
+            Warning,
+            Error
+        };
+    }
+}
