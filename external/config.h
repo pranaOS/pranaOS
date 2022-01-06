465b8c88f45e9f0f2a8f6f49bbeb1bf38a21eab5
krishpranav <krisna.pranav@gmail.com>
2022-01-06 17:21:02 +0530

    remove external

--- external/config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define HAVE_OWN_STDLIB 1
-#define HAVE_OWN_STRING 1
-
-#define ECLIB_PROV_MALLOC kmalloc
-#define ECLIB_PROV_FREE   kfree
-#define ECLIB_PROV_MEMSET memset
-#define ECLIB_PROV_MEMCPY memcpy
