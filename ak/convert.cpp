cf25bed8e80aed20ebfb24d729c6b0e0babc8f9b
krishpranav <krisna.pranav@gmail.com>
2022-01-06 10:56:58 +0530

    ak: convert cpp

--- ak/convert.cpp
+++ ak/convert.cpp
@@ -7,24 +7,35 @@ int isspace(char c) {
     return c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r';
 }
 
-char convert::intToString(int n) {
+
+char* convert::inttostring(int n)
+{
     static char ret[24];
     int numChars = 0;
     bool isNegative = false;
-
-    if (n < 0) {
+    if (n < 0)
+    {
         n = -n;
         isNegative = true;
         numChars++;
     }
-    int temp = n 
 
-    do {
+    int temp = n;
+    do
+    {
         numChars++;
         temp /= 10;
     } while (temp);
 
     ret[numChars] = 0;
 
-    
+    if (isNegative)
+        ret[0] = '-';
+    int i = numChars - 1;
+    do
+    {
+        ret[i--] = n % 10 + '0';
+        n /= 10;
+    } while (n);
+    return ret;
 }
