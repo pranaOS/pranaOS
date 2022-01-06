18834a696940e0d9b59837a7d85399f682885116
krishpranav <krisna.pranav@gmail.com>
2022-01-06 12:28:37 +0530

    ak: print cpp

---
+++ ak/print.cpp
@@ -0,0 +1,10 @@
+#include "print.h"
+
+using namespace pranaOS;
+using namespace pranaOS::system;
+using namespace pranaOS::ak;
+using namespace pranaOS::core;
+
+void Print::printfHex(uint8_t key) {
+    char *foo = "00";
+}
