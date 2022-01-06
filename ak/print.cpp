46d42fceff82d71b4e50b33ce76c6a81edcb02c2
krishpranav <krisna.pranav@gmail.com>
2022-01-06 12:30:33 +0530

    ak: print cpp

--- ak/print.cpp
+++ ak/print.cpp
@@ -7,4 +7,21 @@ using namespace pranaOS::core;
 
 void Print::printfHex(uint8_t key) {
     char *foo = "00";
+    char *hex = "0123456789";
+    foo[0] = hex[(key >> 4) & 0xF];
+    foo[1] = hex[key & 0xF];
+    system::BootConsole::write(foo);
+}
+
+void Print::printfHex16(uint16_t key) {
+    printfHex((key >> 8) & 0xFF);
+    printfHex(key & 0xFF);
+}
+
+void Print::printfHex32(uint32_t key)
+{
+    printfHex((key >> 24) & 0xFF);
+    printfHex((key >> 16) & 0xFF);
+    printfHex((key >> 8) & 0xFF);
+    printfHex(key & 0xFF);
 }
