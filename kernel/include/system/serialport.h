6e19bd500233508e882d2d5b84b92b926396d73e
krishpranav <krisna.pranav@gmail.com>
2022-01-06 10:40:31 +0530

    kernel: include system serialport header

--- kernel/include/system/serialport.h
+++ kernel/include/system/serialport.h
@@ -1,26 +1,33 @@
 #pragma once
 
-#include "types.h"
+#include <ak/types.h>
 #include <core/port.h>
 
 namespace pranaOS {
     namespace system {
-        enum COMPort {
+
+        enum COMPort
+        {
             COM1 = 0x3F8,
             COM2 = 0x2F8,
             COM3 = 0x3E8,
-            COM4 = 0x2E8,
+            COM4 = 0x2E8
         };
 
         class Serialport {
         public:
             static int serialreceiveready();
-            static serialsendready();
+            static int serialsendready();
 
             static bool initialized;
             static void init(COMPort port);
 
             static char read();
+            static void write(char a);
+            static void writestr(char* str);
+
+        private:
+            static COMPort portaddress;
         };
     }
 }
