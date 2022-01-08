640754be96a8960b04270fbb413ca8073b3c85c5
krishpranav <krisna.pranav@gmail.com>
2022-01-08 18:40:41 +0530

    libc: include systeminfo header

--- libs/libc/include/systeminfo.h
+++ libs/libc/include/systeminfo.h
@@ -1,15 +1,16 @@
 #pragma once
 
 #ifndef __KERNEL__
-#include <ak/types.h>
+#include <common/types.h>
 #else
 #include <types.h>
 #endif
 
 namespace pranaOSsystemInfo {
+
     #define SYSTEM_INFO_ADDR 0xBFFEE000
 
-    enum siPropertyIdentifer {
+    enum siPropertyIdentifier {
         String,
         Index
     };
@@ -18,8 +19,9 @@ namespace pranaOSsystemInfo {
     public:
         char* id = 0;
         int index = 0;
-        siPropertyIdentifer type = String;
-        siPropertyIdentifer* parent = 0;
+        
+        siPropertyIdentifier type = String;
+        siPropertyProvider* parent = 0;
     public:
         siPropertyProvider(char* property);
         siPropertyProvider(int index);
@@ -29,35 +31,35 @@ namespace pranaOSsystemInfo {
         siPropertyProvider operator[](const int index);
         siPropertyProvider operator[](const char* element);
 
-        template <typename T>
-        T receiveValue(const bool getsize) const 
+        template<typename T>
+        T receiveValue(const bool getsize) const;
 
-        template <typename T>
+        template<typename T>
         T receiveBuffer() const;
 
-        operator int() const;
-        operator bool() const;
-        operator double() const;
-        operator char*() const;
-        operator uint64_t() const;
-        operator uint32_t() const;
-        operator uint16_t() const;
-        operator uint8_t() const;
+        operator int() const;       
+        operator bool() const;      
+        operator double() const;    
+        operator char*() const;     
+        operator uint64_t() const;  
+        operator uint32_t() const;  
+        operator uint16_t() const;  
+        operator uint8_t() const;  
     } __attribute__((packed));
 
     class systemInfo {
     public:
         static siPropertyProvider properties;
-        static bool requestSystemInfo();  
+        static bool requestSystemInfo();
     };
 
     struct sharedSystemInfo {
         unsigned int mousex;
         unsigned int mousey;
-        signed int mousez;
+        signed int   mousez;
 
         bool mouseLeftButton;
         bool mouseRightButton;
-        bool mouseScroolButton;
+        bool mouseMiddleButton;
     } __attribute__((packed));
 }
