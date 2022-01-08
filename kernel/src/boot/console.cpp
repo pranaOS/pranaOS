125da160a23255d7001504439572e58dc0a35b97
krishpranav <krisna.pranav@gmail.com>
2022-01-08 20:06:31 +0530

    kernel: src boot console cepp

---
+++ kernel/src/boot/console.cpp
@@ -0,0 +1,8 @@
+#include <system/console.h>
+
+using namespace pranaOSConsole;
+using namespace pranaOS::ak;
+using namespace pranaOS::system;
+
+int bootConsole:xOffset = 0;
+int bootConsole:yOffset = 0;
