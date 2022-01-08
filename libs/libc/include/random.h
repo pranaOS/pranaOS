3cb3677cb478a2015c31092a28303c9bc8b3d837
krishpranav <krisna.pranav@gmail.com>
2022-01-08 19:41:54 +0530

    libc: include random header

--- libs/libc/include/random.h
+++ libs/libc/include/random.h
@@ -3,9 +3,10 @@
 #include <types.h>
 
 namespace pranaOSRandom {
-    class random {
+    class Random {
     public:
         static int next(uint32_t max = 32767);
-        static int next(uint32_t min, uint32_t max);
+        static int next(uint32_t min, uint32_t max);             
+        static void setSeed(uint32_t seed);
     };
 }
