7016c2ab7df551070567770d6be17c27171e8258
krishpranav <krisna.pranav@gmail.com>
2022-01-09 13:05:05 +0530

    bootloader: stage2 fs header

--- bootloader/stage2/fs/fs.h
+++ bootloader/stage2/fs/fs.h
@@ -1,3 +1,6 @@
 #pragma once
 
-int load_ext2_kernel(char *kernel_name, unsigned int startlba, unsigned int total_lba, unsigned int disk, unsigned int blocksize_phys);
+
+namespace bootloader {
+    int (char *kernel_name, unsigned int startlba, unsigned int total_lba, unsigned int disk, unsigned int blocksize_phys);
+}
