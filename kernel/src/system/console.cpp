d5370b2b93ae5a3d332273453d61ababb4f8e31e
krishpranav <krisna.pranav@gmail.com>
2022-01-09 11:24:59 +0530

    kernel: system console cpp

--- kernel/src/system/console.cpp
+++ kernel/src/system/console.cpp
@@ -1,26 +1,26 @@
 #include <system/console.h>
 
 using namespace Kernel;
-using namespace Kernel::system;
 using namespace Kernel::ak;
+using namespace Kernel::system;
 
-
-int bootConsole::xOffset = 0;
-int bootConsole::yOffset = 0;
-uint8_t bootConsole::backgroundcolor = VGA_COLOR_BLACK;
-uint8_t bootConsole::foregroundcolor = VGA_COLOR_WHITE;
+int bootConsole::xoffset = 0;
+int bootConsole::yoffset = 0;
+uint8_t bootConsole::backgroundcolor = VGA_COLOR_BLACK; 
+uint8_t bootConsole::foregroundcolor = VGA_COLOR_WHITE; 
 bool bootConsole::writeToSerial = false;
 
 static uint16_t* videoMemory = (uint16_t*)0xC00B8000;
 
-void bootSonsole::scroll() {
+void bootConsole::scroll() {
     for(int i = 0; i < 24; i++){
         for (int m = 0; m < 80; m++){
             videoMemory[i * 80 + m] = videoMemory[(i + 1) * 80 + m];
         }
     }
 
-    for(int x = 0; x < 80; x++) {
+    for(int x = 0; x < 80; x++)
+    {
         uint16_t attrib = (backgroundcolor << 4) | (foregroundcolor & 0x0F);
         volatile uint16_t * where;
         where = (volatile uint16_t *)videoMemory + (24 * VGA_WIDTH + x) ;
@@ -28,12 +28,12 @@ void bootSonsole::scroll() {
     }
 }
 
-void bootConsole::init(bool enableSerial) {    
+void bootConsole::init(bool enableSerial) {
     bootConsole::writeToSerial = enableSerial;
-
-    if (enableSerial) {
-        Serialport::Init(COMPort::COM1);
-        bootConsole::writeLine()
+    if(enableSerial)
+    {
+        serialPort::init(COMPort::COM1);
+        bootConsole::writeLine("Start of serial log for CactusOS");
     }
 }
 
@@ -45,54 +45,71 @@ void bootConsole::write(char c) {
 
 void bootConsole::write(char* str) {
     if (writeToSerial)
-        Serialport::WriteStr(str);
+        serialPort::writeStr(str);
 
     for(int i = 0; str[i] != '\0'; ++i)
     {
         switch(str[i])
         {
             case '\n':
-                XOffset = 0;
-                YOffset++;
+                xoffset = 0;
+                yoffset++;
                 break;
             case '\t':
-                Write("    "); //4 spaces for tab
+                write("    "); 
                 break;
             default:
-                uint16_t attrib = (BackgroundColor << 4) | (ForegroundColor & 0x0F);
+                uint16_t attrib = (backgroundcolor << 4) | (foregroundcolor & 0x0F);
                 volatile uint16_t * where;
-                where = (volatile uint16_t *)videoMemory + (YOffset * 80 + XOffset) ;
+                where = (volatile uint16_t *)videoMemory + (yoffset * 80 + xoffset) ;
                 *where = str[i] | (attrib << 8);
-                XOffset++;
+                xoffset++;
                 break;
         }
 
-        if(XOffset >= 80)
-        {
-            XOffset = 0;
-            YOffset++;
+        if(xoffset >= 80) {
+            xoffset = 0;
+            yoffset++;
         }
 
-        if(YOffset >= 25)
-        {
-            Scroll();
-            XOffset = 0;
-            YOffset = 24;
+        if(yoffset >= 25) {
+            scroll();
+            xoffset = 0;
+            yoffset = 24;
         }
     }
 }
 
-void BootConsole::writeLine(char* str) {
-    BootConsole::write(str);
-    BootConsole::write("\n");
+void bootConsole::writeLine(char* str) {
+    bootConsole::write(str);
+    bootConsole::write("\n");
+}
+
+void bootConsole::writeLine() {
+    bootConsole::write("\n");
+}
+
+void bootConsole::clear() {
+    for(int y = 0; y < VGA_HEIGHT; y++)
+        for(int x = 0; x < VGA_WIDTH; x++) {
+                uint16_t attrib = (backgroundcolor << 4) | (foregroundcolor & 0x0F);
+                volatile uint16_t * where;
+                where = (volatile uint16_t *)videoMemory + (y * VGA_WIDTH + x) ;
+                *where = ' ' | (attrib << 8);
+        }
+
+    xoffset = 0;
+    yoffset = 0;
 }
 
 uint16_t* bootConsole::getBuffer() {
     return videoMemory;
 }
 
-void bootConsole::setX(int x) {
-    xOffset = x;
+void bootConsole::setx(int x) {
+    xoffset = x;
 }
 
-void bootConsole
+void bootConsole::sety(int y) {
+    yoffset = y;
+}
