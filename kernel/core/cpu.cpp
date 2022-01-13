b3bc1834b2a92d722ac757dce304fb5dff253832
krishpranav <krisna.pranav@gmail.com>
2022-01-13 16:47:20 +0530

    kernel: core cpu

--- kernel/core/cpu.cpp
+++ kernel/core/cpu.cpp
@@ -5,4 +5,15 @@
 //  Created by Apple on 13/01/22.
 //
 
-#include <stdio.h>
+#include "cpu.h"
+#include <system/console.h>
+
+using namespace Kernel;
+
+extern "C" void enableSSE();
+
+static inline void cpuid(uint32_t reg, uint32_t *eax, uint32_t *ebx, uint32_t *ecx, uint32_t *edx) {
+    asm volatile("cpuid"
+        : "=a" (*eax), "=b" (*ebx), "=c" (*ecx), "=d" (*edx)
+        : "0" (reg));
+}
