458ee964ee463a7664fbc19326cf02c56f4893fe
krishpranav <krisna.pranav@gmail.com>
2022-01-13 16:39:59 +0530

    kernel: core memory header

---
+++ kernel/core/memory.h
@@ -0,0 +1,23 @@
+//
+//  memory.h
+//  pranaOS
+//
+//  Created by Apple on 13/01/22.
+//
+
+#pragma once
+
+#include <ak/types.h>
+#include <ak/convert.h>
+#include <ak/memoperator.h>
+#include <system/console.h>
+#include <multiboot/multiboot.h>
+
+namespace Kernel {
+    namespace core {
+        /**
+                TODO: PHYSICAL MEMORY OPERATIONS NEED TO BE ADDED
+                BY: @KRISHPRANAV
+         */
+    }
+}
