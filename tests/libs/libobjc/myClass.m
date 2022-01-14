5a9d55dd73fa662f3ab69cc0d77374f8d22981b7
krishpranav <krisna.pranav@gmail.com>
2022-01-14 14:29:54 +0530

    tests: libs libobjc myClass

---
+++ tests/libs/libobjc/myClass.m
@@ -0,0 +1,32 @@
+//
+//  myClass.m
+//  pranaOS
+//
+//  Created by Apple on 14/01/22.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface myClass : XCTestCase
+
+@end
+
+@implementation myClass
+
+- (void)setUp {
+}
+
+- (void)tearDown {
+}
+
+- (void)testExample {
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
