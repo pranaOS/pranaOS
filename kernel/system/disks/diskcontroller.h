0e904147a500371fa25d241a2728f3b9f1276791
krishpranav <krisna.pranav@gmail.com>
2022-01-14 13:33:56 +0530

    kernel: disks diskcontroller header

--- kernel/system/disks/diskcontroller.h
+++ kernel/system/disks/diskcontroller.h
@@ -12,3 +12,16 @@
 #include <ak/memoperator.h>
 #include <system/disks/disk.h>
 #include <system/disks/diskmanager.h>
+
+namespace Kernel {
+    namespace system {
+        class diskController {
+        public:
+            diskController();
+
+            char readSector(ak::uint16_t drive, ak::uint32_t lba, ak::uint8_t* buf);
+            char writeSector();
+            bool ejectDrive(ak::uint8_t drive);
+        };
+    }
+}
