15091ed41f6efbd948b6206030cfbc5677c82301
krishpranav <krisna.pranav@gmail.com>
2022-01-14 11:30:17 +0530

    kernel: disks disk header

--- kernel/system/disks/disk.h
+++ kernel/system/disks/disk.h
@@ -12,15 +12,16 @@
 
 namespace Kernel {
     namespace system {
+    
         class diskController;
-        
+
         enum diskType {
             hardDisk,
             usbDisk,
             floppy,
             cdROM
         };
-    
+
         class disk {
         public:
             diskController* controller;
@@ -30,10 +31,13 @@ namespace Kernel {
             ak::uint64_t size;
             ak::uint32_t numBlocks;
             ak::uint32_t blockSize;
+
+            disk(ak::uint32_t controllerIndex, diskController* controller, diskType type, ak::uint64_t size, ak::uint32_t blocks, ak::uint32_t blocksize);
             
-            disk(ak::uint32_t controllerIndex, DiskController* controller, DiskType type, ak::uint64_t size, ak::uint32_t blocks, ak::uint32_t blocksize);
-        
-            virtual char readSector(ak::uint32_t, ak::uint8_t* buf);
+            virtual char readSector(ak::uint32_t lba, ak::uint8_t* buf);
+            virtual char writeSector(ak::uint32_t lba, ak::uint8_t* buf);
         };
     }
 }
+
+#endif
