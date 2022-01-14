9078a8b2d22278382f8ff5dc37e46f6e8b5279ff
krishpranav <krisna.pranav@gmail.com>
2022-01-14 13:37:15 +0530

    kernel: system disks diskmanager header

--- kernel/system/disks/diskmanager.h
+++ kernel/system/disks/diskmanager.h
@@ -16,5 +16,27 @@
 #include <system/disks/disk.h>
 
 namespace Kernel {
-    
+    namespace system {
+        struct biosDriveParameters {
+            ak::uint16_t bufLen;
+            ak::uint16_t infoFlags;
+            ak::uint32_t cilinders;
+            ak::uint32_t heads;
+            ak::uint32_t sectorsPerTrack;
+            ak::uint64_t totalSectors;
+            ak::uint16_t bytesPerSector;
+
+            ak::uint32_t eddParameters;
+
+            ak::uint16_t signature;
+            ak::uint8_t  devPathLen;
+            ak::uint8_t  reserved1[3];
+            char             hostBusName[4];
+            char             interfaceName[8];
+            ak::uint8_t  interfacePath[8];
+            ak::uint8_t  devicePath[8];
+            ak::uint8_t  reserved2;
+            ak::uint8_t  checksum;
+        } __attribute__((packed));
+    }
 }
