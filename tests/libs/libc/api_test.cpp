861943e00714aa5abb5197bce829081517b25df9
krishpranav <krisna.pranav@gmail.com>
2022-01-14 14:15:28 +0530

    tests: libc api_test cpp

---
+++ tests/libs/libc/api_test.cpp
@@ -0,0 +1,16 @@
+//
+//  api_test.cpp
+//  pranaOS
+//
+//  Created by Krisna Pranav on 14/01/22.
+//
+
+#include <api.h>
+#include <log.h>
+
+using namespace pranaOSApi;
+using namespace pranaOSLog;
+
+void API::initialize() {
+    log(Info, "YEEEEE api test succeded");
+}
