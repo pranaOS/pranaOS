7b79150fed80e0b461f8af50c8ad246fb8f2cf7d
krishpranav <krisna.pranav@gmail.com>
2022-01-14 11:27:24 +0530

    kernel: disks

---
+++ kernel/system/disks/disk.h
@@ -0,0 +1,39 @@
+//
+//  disk.h
+//  pranaOS
+//
+//  Created by Apple on 14/01/22.
+//
+
+#pragma once
+
+#include <ak/types.h>
+#include "diskcontroller.h"
+
+namespace Kernel {
+    namespace system {
+        class diskController;
+        
+        enum diskType {
+            hardDisk,
+            usbDisk,
+            floppy,
+            cdROM
+        };
+    
+        class disk {
+        public:
+            diskController* controller;
+            ak::uint32_t controllerIndex;
+            char* identifier = 0;
+            diskType type;
+            ak::uint64_t size;
+            ak::uint32_t numBlocks;
+            ak::uint32_t blockSize;
+            
+            disk(ak::uint32_t controllerIndex, DiskController* controller, DiskType type, ak::uint64_t size, ak::uint32_t blocks, ak::uint32_t blocksize);
+        
+            virtual char readSector(ak::uint32_t, ak::uint8_t* buf);
+        };
+    }
+}
---
+++ kernel/system/disks/diskcontroller.h
@@ -0,0 +1,12 @@
+//
+//  diskcontroller.h
+//  pranaOS
+//
+//  Created by Apple on 14/01/22.
+//
+
+#ifndef diskcontroller_h
+#define diskcontroller_h
+
+
+#endif /* diskcontroller_h */
