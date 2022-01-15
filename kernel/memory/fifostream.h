0b2ec1487dae1da8301f651ec3afb3cca8ee99a7
krishpranav <krisna.pranav@gmail.com>
2022-01-15 15:23:08 +0530

    kernel: memroy fifostream header

---
+++ kernel/memory/fifostream.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <ak/types.h>
+#include <ak/memoperator.h>
+#include "stream.h"
+
+namespace Kernel {
+    namespace system {
+        class FIFOStream : public Stream {
+        public:
+            FIFOStream(int capacity = 100);
+            ~FIFOStream();
+
+            char read();
+            void write(char byte);
+            
+        private:
+            char* buffer;
+            char* buffer_end;
+            int count;
+            int capacity;
+            char* head;
+            char* tail;
+        };
+    }
+}
