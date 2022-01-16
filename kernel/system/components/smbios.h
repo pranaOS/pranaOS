64e4e58da29e443a2efa7039dd6da1ab2ebd9185
krishpranav <krisna.pranav@gmail.com>
2022-01-16 11:56:38 +0530

    kernel: system components smbios header

--- kernel/system/components/smbios.h
+++ kernel/system/components/smbios.h
@@ -7,8 +7,30 @@
 
 namespace Kernel {
     namespace system {
-        enum smbiosTableType {
+        enum smbiosTableType  {
             BIOSInformation = 0,
+            SystemInformation = 1,
+            BaseBoardInformation = 2,
+            EnclosureInformation = 3,
+            ProcessorInformation = 4,
+            MemoryControllerInformation = 5,
+            MemoryModuleInformation = 6,
+            CacheInformation = 7,
+            PortConnectorInformation = 8,
+            SystemSlotsInformation = 9,
+            OnBoardDevicesInformation = 10,
+            OEMStrings = 11,
+            SystemConfigurationOptions = 12,
+            BIOSLanguageInformation = 13,
+            GroupAssociations = 14,
+            SystemEventLog = 15,
+            PhysicalMemoryArray = 16,
+            MemoryDevice = 17,
+            MemoryErrorInformation = 18,
+            MemoryArrayMappedAddress = 19,
+            MemoryDeviceMappedAddress = 20,
+            SystemBootInformation = 32,
+            EndOfTable = 127
         };
 
         struct smbiosTag {
