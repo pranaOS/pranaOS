799698d93408b177894aa360f8159d93201a8052
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-17 12:48:14 +0530

    kernel: tasking lock header

--- kernel/tasking/lock.h
+++ kernel/tasking/lock.h
@@ -1,1 +1,22 @@
 #pragma once
+
+#include <ak/types.h>
+
+/**
+ * @brief mutex lock
+ */
+
+namespace Kernel {
+    class mutexLock {
+
+    private:
+        int value = 0;
+
+    public:
+        mutexLock();
+
+        void lock();
+        void unlock();
+        void load();
+    };
+}
