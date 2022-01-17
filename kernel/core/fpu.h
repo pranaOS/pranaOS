0f294d02bded5e3c7a1d3f8d165da2c1ffe42f29
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-17 12:39:18 +0530

    kernel: core fpu header

--- kernel/core/fpu.h
+++ kernel/core/fpu.h
@@ -10,25 +10,25 @@
 #include <ak/types.h>
 
 namespace Kernel {
-    namespace core {
-        struct FPU {
-            ak::uint8_t invalidOperand : 1;
-            ak::uint8_t denormalOperand : 1;
-            ak::uint8_t zeroDevide : 1;
-            ak::uint8_t overflow : 1;
-            ak::uint8_t underflow : 1;
-            ak::uint8_t precision : 1;
-            ak::uint8_t reserved1 : 1;
-            ak::uint8_t reserved2 : 1;
-            ak::uint8_t precisionControl : 2;
-            ak::uint8_t roundingControl : 2;
-            ak::uint8_t infinityControl : 1;
-            ak::uint8_t reserved3 : 3;
-        } __attribute__((packed));
+    
+    struct FPU {
+        ak::uint8_t invalidOperand : 1;
+        ak::uint8_t denormalOperand : 1;
+        ak::uint8_t zeroDevide : 1;
+        ak::uint8_t overflow : 1;
+        ak::uint8_t underflow : 1;
+        ak::uint8_t precision : 1;
+        ak::uint8_t reserved1 : 1;
+        ak::uint8_t reserved2 : 1;
+        ak::uint8_t precisionControl : 2;
+        ak::uint8_t roundingControl : 2;
+        ak::uint8_t infinityControl : 1;
+        ak::uint8_t reserved3 : 3;
+    } __attribute__((packed));
 
-        class Fpu {
-        public:
-            static void enable();
-        };
-    }
+    class Fpu {
+    public:
+        static void enable();
+    };
+
 }
