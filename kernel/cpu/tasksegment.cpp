15d594b1ffba6e1e072ab7c80d9a77d7afb515e4
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-17 18:57:03 +0530

    kernel: cpu tasksegment cpp

--- kernel/cpu/tasksegment.cpp
+++ kernel/cpu/tasksegment.cpp
@@ -1,21 +1,32 @@
 #include "tasksegment.h"
 
 using namespace Kernel;
-using namespace Kernel::ak;
+using namespace Kernel::core;
 
-static taskSegmentEntry tasksegment;
+static taskSegmentEntry taskSegment;
 
-extern "C" void flish_tasksegment();
+extern "C" void flush_tasksegment();
 
-void taskSegment::install(int idx, int kernelSS, int kernelESP) {
-    uint32_t base = (uint32_t) &tasksegment;
+void taskSegment::install(uint32_t idx, uint32_t kernelSS, uint32_t kernelESP) {
+	memOperator::memset(&taskSegment, 0, sizeof(taskSegmentEntry));
+
+	uint32_t base = (uint32_t) &taskSegment;
+	globalDescriptorTable::setDescriptor (idx, base, base + sizeof (taskSegmentEntry), 0xE9, 0);
+
+    memOperator::memset ((void*) &taskSegment, 0, sizeof (taskSegmentEntry));
+
+	taskSegment.ss0 = kernelSS;
+	taskSegment.esp0 = kernelESP;
+	taskSegment.iomap = sizeof(taskSegmentEntry);
+
+	flush_tasksegment();
 }
 
-void taskSegment::setStack(int kernelSS, int kernelESP) {
-    tasksegment.ss0 = kernelSS;
-    tasksegment.esp0 = kernelESP;
+void taskSegment::setStack(uint32_t kernelSS, uint32_t kernelESP) {
+    taskSegment.ss0 = kernelSS;
+    taskSegment.esp0 = kernelESP;
 }
 
 taskSegmentEntry* taskSegment::getCurrent() {
-    return &tasksegment;
+	return &taskSegment;
 }
