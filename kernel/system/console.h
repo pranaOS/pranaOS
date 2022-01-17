09b3dc7c0c0c452ee18f2bc4f539b1a8c26d5440
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-17 11:54:51 +0530

    kernel: system console header

--- kernel/system/console.h
+++ kernel/system/console.h
@@ -5,54 +5,52 @@
 #include <stdarg.h>
 
 namespace Kernel {
-    namespace system {
-        #define VGA_WIDTH 80
-        #define VGA_HEIGHT 25
+    #define VGA_WIDTH 80
+    #define VGA_HEIGHT 25
 
-        enum vga_color {
-            VGA_COLOR_BLACK = 0,
-            VGA_COLOR_BLUE = 1,
-            VGA_COLOR_GREEN = 2,
-            VGA_COLOR_CYAN = 3,
-            VGA_COLOR_RED = 4,
-            VGA_COLOR_MAGENTA = 5,
-            VGA_COLOR_BROWN = 6,
-            VGA_COLOR_LIGHT_GREY = 7,
-            VGA_COLOR_DARK_GREY = 8,
-            VGA_COLOR_LIGHT_BLUE = 9,
-            VGA_COLOR_LIGHT_GREEN = 10,
-            VGA_COLOR_LIGHT_CYAN = 11,
-            VGA_COLOR_LIGHT_RED = 12,
-            VGA_COLOR_LIGHT_MAGENTA = 13,
-            VGA_COLOR_LIGHT_BROWN = 14,
-            VGA_COLOR_WHITE = 15
-        };
+    enum vga_color {
+        VGA_COLOR_BLACK = 0,
+        VGA_COLOR_BLUE = 1,
+        VGA_COLOR_GREEN = 2,
+        VGA_COLOR_CYAN = 3,
+        VGA_COLOR_RED = 4,
+        VGA_COLOR_MAGENTA = 5,
+        VGA_COLOR_BROWN = 6,
+        VGA_COLOR_LIGHT_GREY = 7,
+        VGA_COLOR_DARK_GREY = 8,
+        VGA_COLOR_LIGHT_BLUE = 9,
+        VGA_COLOR_LIGHT_GREEN = 10,
+        VGA_COLOR_LIGHT_CYAN = 11,
+        VGA_COLOR_LIGHT_RED = 12,
+        VGA_COLOR_LIGHT_MAGENTA = 13,
+        VGA_COLOR_LIGHT_BROWN = 14,
+        VGA_COLOR_WHITE = 15
+    };
 
-        class bootConsole {
-        public:
-            static ak::uint8_t foregroundcolor;
-            static ak::uint8_t backgroundcolor;
+    class bootConsole {
+    public:
+        static ak::uint8_t foregroundcolor;
+        static ak::uint8_t backgroundcolor;
 
-            static void init(bool enableSerial = false);
+        static void init(bool enableSerial = false);
 
-            static void write(char c);
-            static void write(char* str);
+        static void write(char c);
+        static void write(char* str);
 
-            static void writeLine(char* str);
-            static void writeLine();
+        static void writeLine(char* str);
+        static void writeLine();
 
-            static void clear();
-            static void setx(int x);
-            static void sety(int y);
+        static void clear();
+        static void setx(int x);
+        static void sety(int y);
 
-            static ak::uint16_t* getBuffer();
+        static ak::uint16_t* getBuffer();
         
-        private:
-            static int xoffset;
-            static int yoffset;
-            static bool writeToSerial;
+    private:
+        static int xoffset;
+        static int yoffset;
+        static bool writeToSerial;
 
-            static void scroll();
-        };
-    }
+        static void scroll();
+    };
 }
