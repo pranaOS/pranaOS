928a73336ae691731b173ff5a829fd7c30e8bba5
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-17 16:02:25 +0530

    kernel: disks diskmanager header

--- kernel/disks/diskmanager.h
+++ kernel/disks/diskmanager.h
@@ -1,10 +1,3 @@
-//
-//  diskmanager.h
-//  pranaOS
-//
-//  Created by Krisna Pranav on 14/01/22.
-//
-
 #pragma once
 
 #include <ak/types.h>
@@ -16,43 +9,39 @@
 #include "disk.h"
 
 namespace Kernel {
-    namespace system {
-        struct biosDriveParameters {
-            ak::uint16_t bufLen;
-            ak::uint16_t infoFlags;
-            ak::uint32_t cilinders;
-            ak::uint32_t heads;
-            ak::uint32_t sectorsPerTrack;
-            ak::uint64_t totalSectors;
-            ak::uint16_t bytesPerSector;
+    struct biosDriveParameters {
+        ak::uint8_t  interfacePath[8];
+        ak::uint8_t  devicePath[8];
+        ak::uint8_t  reserved2;
+        ak::uint8_t  checksum;
+        ak::uint8_t  devPathLen;
+        ak::uint8_t  reserved1[3];
+        ak::uint16_t bufLen;
+        ak::uint16_t bytesPerSector;
+        ak::uint16_t infoFlags;
+        ak::uint16_t signature;
+        ak::uint32_t cilinders;
+        ak::uint32_t heads;
+        ak::uint32_t sectorsPerTrack;
+        ak::uint32_t eddParameters;
+        ak::uint64_t totalSectors;
+        char             hostBusName[4];
+        char             interfaceName[8];
+    } __attribute__((packed));
 
-            ak::uint32_t eddParameters;
+    class Disk;
 
-            ak::uint16_t signature;
-            ak::uint8_t  devPathLen;
-            ak::uint8_t  reserved1[3];
-            char             hostBusName[4];
-            char             interfaceName[8];
-            ak::uint8_t  interfacePath[8];
-            ak::uint8_t  devicePath[8];
-            ak::uint8_t  reserved2;
-            ak::uint8_t  checksum;
-        } __attribute__((packed));
+    class diskManager {
+    public:
+        List<Disk*> allDisks;
+        diskManager();
 
-        class Disk;
+        void addDisk(Disk* disk);
+        void removeDisk(Disk* disk);
 
-        class diskManager {
-        public:
-            List<Disk*> allDisks;
-            diskManager();
+        char readSector(ak::uint16_t drive, ak::uint32_t lba, ak::uint8_t* buf);
+        char writeSector(ak::uint16_t drive, ak::uint32_t lba, ak::uint8_t* buf);
 
-            void addDisk(Disk* disk);
-            void removeDisk(Disk* disk);
-
-            char readSector(ak::uint16_t drive, ak::uint32_t lba, ak::uint8_t* buf);
-            char writeSector(ak::uint16_t drive, ak::uint32_t lba, ak::uint8_t* buf);
-
-            biosDriveParameters* getDriveInfoBios(ak::uint8_t drive);
-        };
-    }
+        biosDriveParameters* getDriveInfoBios(ak::uint8_t drive);
+    };
 }
