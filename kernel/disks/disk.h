f0ca27d1a104490ac4893b9a1125f8679ce3c577
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-17 14:46:55 +0530

    kernel: disks disk header

--- kernel/disks/disk.h
+++ kernel/disks/disk.h
@@ -1,41 +1,34 @@
-//
-//  disk.h
-//  pranaOS
-//
-//  Created by Krisna Pranav on 14/01/22.
-//
-
 #pragma once
 
 #include <ak/types.h>
 #include "diskcontroller.h"
 
 namespace Kernel {
-    namespace system {
     
-        class diskController;
+    class diskController;
 
-        enum diskType {
-            hardDisk,
-            usbDisk,
-            floppy,
-            cdROM
-        };
+    enum diskType {
+        hardDisk,
+        usbDisk,
+        floppy,
+        cdROM
+    };
 
-        class disk {
-        public:
-            diskController* controller;
-            ak::uint32_t controllerIndex;
-            char* identifier = 0;
-            diskType type;
-            ak::uint64_t size;
-            ak::uint32_t numBlocks;
-            ak::uint32_t blockSize;
+    class disk {
+    public:
 
-            disk(ak::uint32_t controllerIndex, diskController* controller, diskType type, ak::uint64_t size, ak::uint32_t blocks, ak::uint32_t blocksize);
+        diskController* controller;
+        ak::uint32_t controllerIndex;
+        char* identifier = 0;
+        diskType type;
+        ak::uint64_t size;
+        ak::uint32_t numBlocks;
+        ak::uint32_t blockSize;
+
+        disk(ak::uint32_t controllerIndex, diskController* controller, diskType type, ak::uint64_t size, ak::uint32_t blocks, ak::uint32_t blocksize);
             
-            virtual char readSector(ak::uint32_t lba, ak::uint8_t* buf);
-            virtual char writeSector(ak::uint32_t lba, ak::uint8_t* buf);
-        };
-    }
+        virtual char readSector(ak::uint32_t lba, ak::uint8_t* buf);
+        virtual char writeSector(ak::uint32_t lba, ak::uint8_t* buf);
+    };
+    
 }
