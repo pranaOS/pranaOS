471fc7487d61f3b04b48f02bbb0a131d85093748
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-18 11:50:02 +0530

    kernel: tasking thread header

--- kernel/tasking/thread.h
+++ kernel/tasking/thread.h
@@ -11,7 +11,7 @@
 
 namespace Kernel {
     #define THREAD_STACK_SIZE 4_KB
-
+        
     #define SEG_USER_DATA 0x23
     #define SEG_USER_CODE 0x1B
     #define SEG_KERNEL_DATA 0x10
@@ -21,32 +21,33 @@ namespace Kernel {
         Blocked,
         Ready,
         Stopped,
-        Started
+        Started,
     };
 
     enum blockedState {
-        Unknown,
+        Unkown,
         Sleep,
         ReceiveIPC
     };
 
-    struct process;
+    struct Process;
 
     struct Thread {
         Process* parent;
         ak::uint8_t* stack;
-        ak::uint8_t userStack;
+        ak::uint8_t* userStack;
         ak::uint32_t userStackSize;
         threadState state;
         blockedState blockedstate;
         Kernel::CPUState* regsPtr;
-
+            
         ak::uint32_t timeDelta;
-        ak::uint8_t FPUBuffer;
+        ak::uint8_t* FPUBuffer;
     };
 
     class threadHelper {
       public:
+        static thread* createFromFunction(void (*entryPoint)(), bool isKernel = false, ak::uint32_t flags = 0x202, Process* parent = 0);
         static void removeThread(Thread* thread);
       private:
         threadHelper();
