448e116a0a99fdb7255d843087f352d1c9c55093
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-18 10:43:18 +0530

    ak: types header

--- ak/types.h
+++ ak/types.h
@@ -1,43 +1,42 @@
 #pragma once
 
-namespace pranaOS {
+namespace ak {
+    typedef char                        int8_t;
+    typedef unsigned char               uint8_t;
+    typedef short                       int16_t;
+    typedef unsigned short              uint16_t;
+    typedef int                         int32_t;
+    typedef unsigned int                uint32_t;
+    typedef long long int               int64_t;
+    typedef unsigned long long int      uint64_t;
+    typedef unsigned long long          uintptr_t;
 
-    namespace ak {
-        typedef char                        int8_t;
-        typedef unsigned char               uint8_t;
-        typedef short                       int16_t;
-        typedef unsigned short              uint16_t;
-        typedef int                         int32_t;
-        typedef unsigned int                uint32_t;
-        typedef long long int               int64_t;
-        typedef unsigned long long int      uint64_t;
-        typedef unsigned long long          uintptr_t;
+    constexpr uint64_t divide64(uint64_t n, uint32_t base, uint32_t* r = 0) {
+        uint64_t rem = n;
+        uint64_t b = base;
+        uint64_t res = 0, d = 1;
+        uint32_t high = rem >> 32;
 
-        constexpr uint64_t divide64(uint64_t n, uint32_t base, uint32_t* r = 0)
-        {
-            uint64_t rem = n;
-            uint64_t b = base;
-            uint64_t res = 0, d = 1;
-            uint32_t high = rem >> 32;
+        if (high >= base) {
+                high /= base;
+                res = (uint64_t) high << 32;
+                rem -= (uint64_t) (high*base) << 32;
+        }
 
-            if (high >= base) {
-                    high /= base;
-                    res = (uint64_t) high << 32;
-                    rem -= (uint64_t) (high*base) << 32;
+        while ((int64_t)b > 0 && b < rem) {
+                b = b+b;
+                d = d+d;
+        }
+
+        do {
+            if (rem >= b) {
+                rem -= b;
+                res += d;
             }
 
-            while ((int64_t)b > 0 && b < rem) {
-                    b = b+b;
-                    d = d+d;
-            }
+            b >>= 1;
+            d >>= 1;
 
-            do {
-                    if (rem >= b) {
-                            rem -= b;
-                            res += d;
-                    }
-                    b >>= 1;
-                    d >>= 1;
             } while (d);
 
             if(r)
@@ -46,18 +45,16 @@ namespace pranaOS {
             return res;
         }
     }
-    constexpr ak::uint32_t operator"" _KB(unsigned long long no)
-    {
+
+    constexpr ak::uint32_t operator"" _KB(unsigned long long no) {
         return no * 1024;
     }
 
-    constexpr ak::uint32_t operator"" _MB(unsigned long long no)
-    {
+    constexpr ak::uint32_t operator"" _MB(unsigned long long no {
         return no * (1024_KB);
     }
     
-    constexpr ak::uint32_t operator"" _GB(unsigned long long no)
-    {
+    constexpr ak::uint32_t operator"" _GB(unsigned long long no) {
         return no * (1024_MB);
     }
 }
