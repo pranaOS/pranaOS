0a8bb8853d0ee82b932d1bda6646f58c2c270d42
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-18 11:09:53 +0530

    kernel: memory stream header

--- kernel/memory/stream.h
+++ kernel/memory/stream.h
@@ -3,15 +3,13 @@
 #include <ak/types.h>
 
 namespace Kernel {
-    namespace system {
-        class Stream {
-        public:
-            Stream();
-            virtual ~Stream();
+    class Stream {
+    public:
+        Stream();
+        virtual ~Stream();
 
-            virtual char read();
-            virtual void write(char byte);
-            virtual int available();
-        };
-    }
+        virtual char read();
+        virtual void write(char byte);
+        virtual int available();
+    };
 }
