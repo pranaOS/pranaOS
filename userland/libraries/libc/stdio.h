5407d8624a1d55d6577ead3127b6cf2ea4e29e06
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-18 13:11:03 +0530

    libc: stdio header

--- userland/libraries/libc/stdio.h
+++ userland/libraries/libc/stdio.h
@@ -2,14 +2,9 @@
 
 #include <stdint.h>
 
-namespace pranaOSStdio {
-
-    class Stdio {
-
-    #define BUFSIZ 0x400
-    #define EOF    -1
-
-    public:
+namespace Stdio {
+    class Std {
+      public:
         int putchar(int);
         int puts(const char *);
         int readline(char *data, int dataend, char *buf, int size);
