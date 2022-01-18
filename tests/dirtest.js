f9f4f72b3611a8b4a4d5bca053210cd22b379113
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-18 13:17:38 +0530

    tests: dirtest

--- tests/dirtest.js
+++ tests/dirtest.js
@@ -1,25 +1,37 @@
 var exec = require('child_process').exec;
 
-exec('find kernel', function(error, stdout, stderr) {
-    console.log('kernel stdout: ' + stdout);
-    console.log('kernel stderr: ' + stderr);
-    if (error !== null) {
-        console.log('error: ' + error);
-    }
-});
+function dirtest() {
 
-exec('find libs', function(error, stdout, stderr) {
-    console.log('libs stdout: ' + stdout);
-    console.log('libs stderr: ' + stderr);
-    if (error !== null) {
-        console.log('error: ' + error);
-    }
-});
+    exec('find ../kernel', function (error, stdout, stderr) {
+        console.log('kernel stdout: ' + stdout);
+        console.log('kernel stderr: ' + stderr);
+        if (error !== null) {
+            console.log('error: ' + error);
+        }
+    });
 
-exec('find ak', function(error, stdout, stderr) {
-    console.log('ak stdout: ' + stdout);
-    console.log('ak stderr: ' + stderr);
-    if (error !== null) {
-        console.log('error: ' + error);
-    }
-});
+    exec('find ../userland/libraries', function (error, stdout, stderr) {
+        console.log('libs stdout: ' + stdout);
+        console.log('libs stderr: ' + stderr);
+        if (error !== null) {
+            console.log('error: ' + error);
+        }
+    });
+
+    exec('find ../ak', function (error, stdout, stderr) {
+        console.log('ak stdout: ' + stdout);
+        console.log('ak stderr: ' + stderr);
+        if (error !== null) {
+            console.log('error: ' + error);
+        }
+    });
+
+}
+
+function main() {
+    console.log('Running dirtest');
+
+    dirtest();
+}
+
+main();
