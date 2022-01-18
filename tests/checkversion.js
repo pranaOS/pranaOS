241d20bb65c121427de300ad1df3a920b5affa84
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-18 13:15:27 +0530

    checkversion js test

--- tests/checkversion.js
+++ tests/checkversion.js
@@ -1,25 +1,43 @@
 var exec = require('child_process').exec;
 
-exec('i686-elf-gcc --version', function(error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if (error !== null) {
-        console.log('error: ' + error);
-    }
-});
+function checkversion() {
+    exec('i686-elf-gcc --version', function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+            console.log('error: ' + error);
+        }
+    });
 
-exec('nasm --version', function(error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if (error !== null) {
-        console.log('error: ' + error);
-    }
-});
+    exec('nasm --version', function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+            console.log('error: ' + error);
+        }
+    });
 
-exec('make --version', function(error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if (error !== null) {
-        console.log('error: ' + error);
-    }
-});
+    exec('make --version', function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+            console.log('error: ' + error);
+        }
+    });
+
+    exec('which ld', function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+            console.log('error: ' + error);
+        }
+    });
+
+}
+
+function main() {
+    console.log('Running check version');
+    checkversion();
+}
+
+main();
