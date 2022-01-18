89045de33ec5a9ed5c6dd78484429a9a584da657
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-18 11:10:22 +0530

    kernel: tasking process header

---
+++ kernel/tasking/process.h
@@ -0,0 +1,15 @@
+//
+// Created by Apple on 18/01/22.
+//
+
+#pragma once
+
+#include "thread.h"
+#include <ak/list.h>
+#include <ak/types.h>
+#include <libc/ipc.h>
+#include <memory/stream.h>
+
+namespace Kernel {
+
+}
