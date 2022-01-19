df09319b83dda6f2561fdd19cfb436c09d91d8cc
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-19 16:19:44 +0530

    kernel: drivers video header

--- kernel/drivers/video/video.h
+++ kernel/drivers/video/video.h
@@ -4,42 +4,42 @@
 
 #pragma once
 
-#include <kernel/drivers/driver.h>
-#include <kernel/internal/pci.h>
-#include <kernel/internal/graphics.h>
-#include <kernel/system/interruptmanager.h>
+#include <drivers/driver.h>
+#include <internal/pci.h>
+#include <system/interrupthandler.h>
+#include <internal/graphics.h>
 
-namespace Kernel::Driver {
-    #define VIDEO_VENDORID 0x15AD
-    #define VIDEO_DEVICEID 0x0405
+namespace Kernel {
 
-    #define SVGA_INDEX_PORT         0x0
-    #define SVGA_VALUE_PORT         0x1
-    #define SVGA_BIOS_PORT          0x2
-    #define SVGA_IRQSTATUS_PORT     0x8
+  #define VMWARESVGAII_VENDORID 0x15AD
+  #define VMWARESVGAII_DEVICEID 0x0405
 
-    #define SVGA_MAGIC         0x900000UL
-    #define SVGA_MAKE_ID(ver)  (SVGA_MAGIC << 8 | (ver))
+  #define SVGA_INDEX_PORT         0x0
+  #define SVGA_VALUE_PORT         0x1
+  #define SVGA_BIOS_PORT          0x2
+  #define SVGA_IRQSTATUS_PORT     0x8
+  #define SVGA_MAGIC         0x900000UL
+  #define SVGA_MAKE_ID(ver)  (SVGA_MAGIC << 8 | (ver))
 
-    #define SVGA_VERSION_2     2
-    #define SVGA_ID_2          SVGA_MAKE_ID(SVGA_VERSION_2)
+  #define SVGA_VERSION_2     2
+  #define SVGA_ID_2          SVGA_MAKE_ID(SVGA_VERSION_2)
 
-    #define SVGA_REG_ID 0
-    #define SVGA_REG_FB_START 13
-    #define SVGA_REG_FB_ENABLE 1
-    #define SVGA_REG_FB_WIDTH 2
-    #define SVGA_REG_FB_HEIGHT 3
-    #define SVGA_REG_FB_BITSPERPIXEL 7
+  #define SVGA_REG_ID 0
+  #define SVGA_REG_FB_START 13
+  #define SVGA_REG_FB_Enable 1
+  #define SVGA_REG_FB_Width 2
+  #define SVGA_REG_FB_Height 3
+  #define SVGA_REG_FB_BitsPerPixel 7
 
-    class video : public Driver, public graphicsDevice {
-      public:
-        video(pciDevice* pciDev);
-
+  class video : public driver, public graphicsDevice {
+    public:
+        video(PCIDevice* pciDev);
         bool initialize();
-      private:
+        bool selectBestVideoMode();
+        
+    private:
         pciDevice* pcidevice;
-
         void writeRegister(ak::uint32_t reg, ak::uint32_t value);
-        ak::uint32_t readRegister(ak::uin32_t reg);
-    };
+        ak::uint32_t readRegister(ak::uint32_t reg);
+  };
 }
