a3832bc642bdd321a71eb867f01e41c372fbf457
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-19 11:40:45 +0530

    kernel: drivers video header
    impl: #include header

---
+++ kernel/drivers/video/video.h
@@ -0,0 +1,10 @@
+//
+// Created by KrisnaPranav on 19/01/22.
+//
+
+#pragma once
+
+#include <kernel/drivers/driver.h>
+#include <kernel/internal/pci.h>
+#include <kernel/internal/graphics.h>
+#include <kernel/system/interruptmanager.h>
