e4d999d68a310055c082c26ce02622618e9e481a
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-19 11:12:30 +0530

    kernel: drivers pcidriver header

---
+++ kernel/drivers/pcidriver.h
@@ -0,0 +1,17 @@
+//
+// Created by KrisnaPranav on 19/01/22.
+//
+
+#pragma once
+
+#include "manager.h"
+
+namespace Kernel::Driver {
+    struct pciDriverEntry {
+        ak::uint16_t vendorID;
+        ak::uint16_t deviceID;
+        char* driverString;
+    } __attribute__((packed));
+
+    
+}
