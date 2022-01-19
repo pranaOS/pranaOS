b27854a59cb5c114aae135161a6c1ab6975be383
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-19 11:43:44 +0530

    kernel: drivers video header
    impl: #define new functions

--- kernel/drivers/video/video.h
+++ kernel/drivers/video/video.h
@@ -8,3 +8,26 @@
 #include <kernel/internal/pci.h>
 #include <kernel/internal/graphics.h>
 #include <kernel/system/interruptmanager.h>
+
+namespace Kernel::Driver {
+    #define VIDEO_VENDORID 0x15AD
+    #define VIDEO_DEVICEID 0x0405
+
+    #define SVGA_INDEX_PORT         0x0
+    #define SVGA_VALUE_PORT         0x1
+    #define SVGA_BIOS_PORT          0x2
+    #define SVGA_IRQSTATUS_PORT     0x8
+
+    #define SVGA_MAGIC         0x900000UL
+    #define SVGA_MAKE_ID(ver)  (SVGA_MAGIC << 8 | (ver))
+
+    #define SVGA_VERSION_2     2
+    #define SVGA_ID_2          SVGA_MAKE_ID(SVGA_VERSION_2)
+
+    #define SVGA_REG_ID 0
+    #define SVGA_REG_FB_START 13
+    #define SVGA_REG_FB_ENABLE 1
+    #define SVGA_REG_FB_WIDTH 2
+    #define SVGA_REG_FB_HEIGHT 3
+    #define SVGA_REG_FB_BITSPERPIXEL 7
+}
