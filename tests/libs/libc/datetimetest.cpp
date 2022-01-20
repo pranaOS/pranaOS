a3a11efa80a8bc6dd4e9d5a67edec3fd82e6c064
Lalucas20 <98066749+Lalucas20@users.noreply.github.com>
2022-01-20 09:31:33 +0530

    datetime test case

--- tests/libs/libc/datetimetest.cpp
+++ tests/libs/libc/datetimetest.cpp
@@ -6,6 +6,6 @@ using namespace Syscall;
 
 Time dateTime::current() {
     Time result;
-    doSyscall(SYSCALL_GET_DATETIME, (uint8_t)&result);
+    doSyscall(SYSCALL_GET_DATETIME, (uint32_t)&result);
     return result;
 }
