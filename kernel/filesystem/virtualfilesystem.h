db59183daa977f3efba9fe16905673cc1b9ff526
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-20 12:11:33 +0530

    kernel: filesystem virtualfilesystem header

--- kernel/filesystem/virtualfilesystem.h
+++ kernel/filesystem/virtualfilesystem.h
@@ -4,38 +4,42 @@
 
 #pragma once
 
-#include <ak/types>
+#include <ak/types.h>
 #include <ak/list.h>
 #include <kernel/disks/disk.h>
 #include <libc/shared.h>
 
 namespace Kernel {
-    #define PATH_SEPERATOR_C '\\'
-    #define PATH_SEPERATOR_S "\\"
 
-    class virtualFileSystem {
-        friend class virtualFileSystemManager;
-      public:
-        Disk* disk;
+  #define PATH_SEPERATOR_C '\\' 
+  #define PATH_SEPERATOR_S "\\" 
 
-      public:
-        virtualFileSystem(Disk* disk, ak::uint32_t start, ak::uint32_t size, char* name = 0);
-        virtual ~virtualFileSystem();
+  class virtualFileSystem {
+    friend class virtualFileSystemManager
+    public:
+      Disk* disk;
 
-        virtual int readFile(const char* filename, uint8_t* buffer, uint32_t offset = 0, uint32_t len = -1);
-        virtual int writeFile(const char* filename, uint8_t* buffer, uint32_t len, bool create = true);
-        virtual int createFile(const char* path);
-        virtual int createDirectory(const char* path);
+    protected:
+      ak::uint32_t startLBA;
+      ak::uint32_t sizeInSectors;      
+      char* Name = "Unkown";
 
-        virtual bool fileExists(const char* filename);
-        virtual bool directoryExists(const char* filename);
+    public:
+      virtualFileSystem(Disk* disk, ak::uint32_t start, ak::uint32_t size, char* name = 0);
+      virtual ~virtualFileSystem();
+            
+      virtual bool initialize();
+            
+      virtual int readFile(const char* filename, uint8_t* buffer, uint32_t offset = 0, uint32_t len = -1);
+      virtual int writeFile(const char* filename, uint8_t* buffer, uint32_t len, bool create = true);
 
-        virtual bool initialize();
+      virtual bool fileExists(const char* filename);
+      virtual bool directoryExists(const char* filename);
 
-      protected:
-        ak::uint32_t startLBA;
-        ak::uint32_t sizeInSecotrs;
+      virtual int createFile(const char* path);
+      virtual int createDirectory(const char* path);
 
-        char* name = "Unknown";
-    };
+      virtual uint32_t getFileSize(const char* filename);
+      virtual List<LibC::vfsEntry>* directoryList(const char* path);
+  };
 }
