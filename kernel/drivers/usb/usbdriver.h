e1ecd2cb0b0dec01434932568e0522d276e41b20
Williams <82570807+NukeWilliams@users.noreply.github.com>
2022-01-20 05:48:47 +0530

    kernel: usbdriver header

---
+++ kernel/drivers/usb/usbdriver.h
@@ -0,0 +1,30 @@
+#ifndef KERNEL_DRIVER_USB
+#define KERNEL_DRIVER_USB
+
+#include <kernel/drivers/driver.h>
+
+namespace Kernel {
+    
+    typedef struct interruptTransfer {
+        uint8_t* bufferPointer;
+        uint32_t bufferPhys;
+        int bufferLen;
+
+        void* td;
+        int numTd;
+        void* qh;
+        int endPoint;
+    } interruptTransfer_t;
+
+    class usbDriver : public Kernel::driver {
+    public:
+        virtual ~usbDriver();
+
+        virtual void initialize();
+        void deInitialize();
+
+        virtual bool handleInterruptPacket(interruptTransfer_t* transfer);
+    }
+}
+
+#endif
