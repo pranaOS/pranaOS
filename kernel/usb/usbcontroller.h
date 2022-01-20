208c4ace6e9ad8dc8ec2bf1f7134beab26d4b14f
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-20 08:40:21 +0530

    kernel: usb usbcontroller header

--- kernel/usb/usbcontroller.h
+++ kernel/usb/usbcontroller.h
@@ -8,3 +8,35 @@
 #include <kernel/internal/pci.h>
 #include <kernel/system/interrupthandler.h>
 #include "usbdevice.h"
+
+namespace Kernel {
+    enum usbControllerType {
+        UHCI,
+        OHCI,
+        EHCI,
+        xHCI
+    };
+
+    class usbDriver;
+    typedef struct interruptTransfer {
+        uint8_t* bufferPointer;
+        uint32_t bufferPhys;
+        uint32_t tdPhys;
+
+        usbDriver* handler;
+
+        int bufferLen;
+        int queueIndex;
+        int endpoint;
+        int numTd;
+
+        void* qh;
+        void* td
+    };
+
+    class usbDevice;
+
+    class usbController {
+
+    };
+}
