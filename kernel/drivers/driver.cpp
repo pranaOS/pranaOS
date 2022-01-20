08f7ac41f3c9f1bb7f4f0e4c7c59eb11b297e2ce
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-20 11:24:35 +0530

    kernel: drivers driver cpp

--- kernel/drivers/driver.cpp
+++ kernel/drivers/driver.cpp
@@ -1,3 +1,18 @@
 //
 // Created by KrisnaPranav on 20/01/22.
 //
+
+#include "driver.h"
+#include <kernel/system/log.h>
+
+using namespace Kernel;
+using namespace Kernel::driver;
+
+driver::Driver(char* name, char* description) {
+    this->driverName = name;
+    this->driverDescription = description;
+}
+
+char* driver::getDriverName() {
+    return this->driverName;
+}
