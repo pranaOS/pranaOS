288cb26aa74d22430b072f6eb079b7c3409f8d5d
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-20 11:36:44 +0530

    kernel: drivers manager cpp

---
+++ kernel/drivers/manager.cpp
@@ -0,0 +1,20 @@
+//
+// Created by KrisnaPranav on 20/01/22.
+//
+
+#include "manager.h"
+#include <kernel/system/log.h>
+
+using namespace Kernel;
+
+driverManager::driverManager() {
+    this->driverList.clear();
+}
+
+void driverManager::addDriver(driver* drv) {
+
+}
+
+void driverManager::activate() {
+
+}
