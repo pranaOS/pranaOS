61a8efb28a21af721c5f43d18c9d83d5c9019a81
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-20 11:53:45 +0530

    kernel: filesystem virtualfilesystem header

---
+++ kernel/filesystem/virtualfilesystem.h
@@ -0,0 +1,15 @@
+//
+// Created by KrisnaPranav on 20/01/22.
+//
+
+#pragma once
+
+#include <ak/types>
+#include <ak/list.h>
+#include <kernel/disks/disk.h>
+#include <libc/shared.h>
+
+namespace Kernel {
+    #define PATH_SEPERATOR_C '\\'
+    #define PATH_SEPERATOR_S "\\"
+}
