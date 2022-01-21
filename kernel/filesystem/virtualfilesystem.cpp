90ea8883c91865eb7d3cec799fdec5dd88b9cf04
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-21 10:38:20 +0530

    kernel: filesystem virtualfilesystem cpp

--- kernel/filesystem/virtualfilesystem.cpp
+++ kernel/filesystem/virtualfilesystem.cpp
@@ -4,3 +4,20 @@
 
 #include "virtualfilesystem.h"
 #include <kernel/system/log.h>
+
+using Kernel;
+
+Kernel::virtualFileSystem::virtualFileSystem(Disk* disk, ak::uint32_t start, ak::uint32_t size, char* name) {
+    this->disk = disk;
+    this->sizeInSectors = size;
+    this->startLBA = start;
+    this->Name = name;
+}
+
+Kernel::virtualFileSystem::~virtualFileSystem() {
+    Log(Error, "filesystem failed %s%d", __FILE__, __LINE__);
+}
+
+bool Kernel::virtualFileSystem::initialize() {
+    return false;
+}
