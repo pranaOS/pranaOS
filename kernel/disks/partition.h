fe046fc7111c5954af42189148c0920ad812ce63
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-21 18:01:39 +0530

    kernel: disks partition header

--- kernel/disks/partition.h
+++ kernel/disks/partition.h
@@ -8,12 +8,21 @@
 #include <kernel/filesystem/vfsmanager.h>
 
 namespace Kernel {
-    struct partitionTableEntry {
+    struct PartitionTableEntry {
         ak::uint8_t bootable;
+
         ak::uint8_t startHead;
         ak::uint8_t startSector : 6;
         ak::uint16_t startCylinder : 10;
+
         ak::uint8_t partitionId;
+
+        ak::uint8_t endHead;
+        ak::uint8_t endSector : 6;
+        ak::uint16_t endCylinder : 10;
+
+        ak::uint32_t startLba;
+        ak::uint32_t length;
     } __attribute__((packed));
 
     struct masterBootRecord {
