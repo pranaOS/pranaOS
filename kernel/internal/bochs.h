f59b739158fe38369c2984a2e9620a3bc49661e0
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-21 11:14:25 +0530

    kernel: internal bochs header
    impl: #define functions

---
+++ kernel/internal/bochs.h
@@ -0,0 +1,14 @@
+//
+// Created by KrisnaPranav on 21/01/22.
+//
+
+#pragma once
+
+#include "graphics.h"
+
+namespace Kernel {
+    #define VBE_DISPI_BANK_ADDRESS 0x10000
+    #define VBE_DISPI_BANK_SIZE_KB 64
+    #define VBE_DISPI_MAX_XRES 1024
+    #define VBE_DISPI_MAX_YRES 768
+}
