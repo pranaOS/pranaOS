0afc3d151a453024a67beb28b1453becbdfa3c15
Chester <97346921+biggboy021@users.noreply.github.com>
2022-01-21 08:51:14 +0530

    virtual filesystem manager

--- kernel/filesystem/vfsmanager.h
+++ kernel/filesystem/vfsmanager.h
@@ -7,5 +7,25 @@
 #include "virtualfilesystem.h"
 
 namespace Kernel {
+    class vfsManager {
+    public:
+        List<virtualFileSystem>* Filesystem;
+    public:
+        int bootPartitionID = -1;
 
+        vfsManager();
+        virtual ~vfsManager();
+
+        void mount(virtualFileSystem* vfs);
+        void unmount(virtualFileSystem* vfs);
+        void unmountByDisk(Disk* disk);
+
+        int extractDiskNumber(const char* path, ak::uint8_t* idSizeReturn);
+        bool searchBootPartition();
+
+        int readFile(const char* filename, uint8_t* buffer, uint32_t offset = 0, uint32_t len = -1);
+        int writeFile(const char* filename, uint8_t* buffer, uint32_t len, bool create);
+        bool fileExists(const char* filename);
+        bool directoryExists(const char* filename);
+    };
 }
