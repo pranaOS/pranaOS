9383cff801debc7842261521f6ad3928603a9a29
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-21 11:10:26 +0530

    kerenl: internal rtc header

--- kernel/internal/rtc.h
+++ kernel/internal/rtc.h
@@ -8,19 +8,21 @@
 #include "systemcomponent.h"
 
 namespace Kernel {
-    #define CURRET_YEAR 1000
 
-    class RTC : public systemComponent {
-      public:
-        RTC();
+  #define CURRENT_YEAR 2020
 
-        ak::uint32_t getSecond();
-        ak::uint32_t getMinute();
-        ak::uint32_t getHour();
-        ak::uint32_t getDay();
-        
-      private:
-        bool updateInProgress();
-        ak::uint8_t readRegister(int reg);
-    };
+  class RTC : public systemComponent {
+    private:
+      bool updateInProgress();
+      ak::uint8_t readRegister(int reg);
+
+    public:
+      RTC();
+      ak::uint32_t getSecond();
+      ak::uint32_t getMinute();
+      ak::uint32_t getHour();
+      ak::uint32_t getDay();
+      ak::uint32_t getMonth();
+      ak::uint32_t getYear();
+  };  
 }
