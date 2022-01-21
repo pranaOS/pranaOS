259998e47c7176ef55d6fcbad3088dfdc132ba01
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-21 19:29:58 +0530

    kernel: filesystem fat header

--- kernel/filesystem/fat.h
+++ kernel/filesystem/fat.h
@@ -4,7 +4,7 @@
 
 #pragma once
 
-#include "virtualfilesystem.h
+#include "virtualfilesystem.h"
 
 namespace Kernel {
 
@@ -26,7 +26,7 @@ namespace Kernel {
         ak::uint32_t    sectorsPerFat32;
         ak::uint16_t    flags;
         ak::uint16_t    fatVersionNum;
-        ak::uint32_t    footDirCluster;
+        ak::uint32_t    rootDirCluster;
         ak::uint16_t    fsInfoSector;
         ak::uint16_t    backupBootSector;
         ak::uint8_t     reserved[12];
@@ -50,7 +50,6 @@ namespace Kernel {
         ak::uint32_t    signature3;
     } __attribute__((packed));
 
-    
     struct directoryEntry {
         ak::uint8_t     fileName[11];       
         ak::uint8_t     attributes;         
@@ -63,34 +62,27 @@ namespace Kernel {
         ak::uint16_t    modifyTime;         
         ak::uint16_t    modifyDate;         
         ak::uint16_t    lowFirstCluster;    
-        ak::uint32_t    fileSize;          
+        ak::uint32_t    fileSize;           
     } __attribute__((packed));
 
     struct lfnEntry {
         ak::uint8_t entryIndex;             
-        ak::uint8_t namePart1[10];          
-        ak::uint8_t Attributes;             
-        ak::uint8_t reserved_1;             
-        ak::uint8_t checksum;               
-        ak::uint8_t namePart2[12];          
-        ak::uint16_t reserved_2;            
-        ak::uint8_t namePart3[4];           
+        ak::uint8_t namePart1[10];         
+        ak::uint8_t Attributes;            
+        ak::uint8_t reserved_1;            
+        ak::uint8_t checksum;              
+        ak::uint8_t namePart2[12];         
+        ak::uint16_t reserved_2;           
+        ak::uint8_t namePart3[4];          
     } __attribute__((packed));
-
 
     struct fatEntryInfo {
         directoryEntry entry;                   
         char* filename;                         
-        ak::uint32_t sector;                
-        ak::uint32_t offsetInSector;        
+        ak::uint32_t sector;               
+        ak::uint32_t offsetInSector;       
     } __attribute__((packed));
-
-    enum fatType {
-        FAT12,
-        FAT16,
-        FAT32,
-    };
-
+    
     #define CLUSTER_END_32  0x0FFFFFF8
     #define CLUSTER_BAD_32  0x0FFFFFF7
     #define CLUSTER_FREE_32 0x00000000
@@ -98,14 +90,14 @@ namespace Kernel {
     #define CLUSTER_END_16  0xFFF8
     #define CLUSTER_BAD_16  0xFFF7
     #define CLUSTER_FREE_16 0x0000
-
+        
     #define CLUSTER_END_12  0xFF8
     #define CLUSTER_BAD_12  0xFF7
     #define CLUSTER_FREE_12 0x000
 
-    #define CLUSTER_END     (this->fatType == FAT12 ? CLUSTER_END_12 : (this->fatType == FAT16 ? CLUSTER_END_16 : CLUSTER_END_32))
-    #define CLUSTER_FREE    (this->fatType == FAT12 ? CLUSTER_FREE_12 : (this->fatType == FAT16 ? CLUSTER_FREE_16 : CLUSTER_FREE_32))
-    #define CLUSTER_BAD     (this->fatType == FAT12 ? CLUSTER_BAD_12 : (this->fatType == FAT16 ? CLUSTER_BAD_16 : CLUSTER_BAD_32))
+    #define CLUSTER_END     (this->FatType == FAT12 ? CLUSTER_END_12 : (this->FatType == FAT16 ? CLUSTER_END_16 : CLUSTER_END_32))
+    #define CLUSTER_FREE    (this->FatType == FAT12 ? CLUSTER_FREE_12 : (this->FatType == FAT16 ? CLUSTER_FREE_16 : CLUSTER_FREE_32))
+    #define CLUSTER_BAD     (this->FatType == FAT12 ? CLUSTER_BAD_12 : (this->FatType == FAT16 ? CLUSTER_BAD_16 : CLUSTER_BAD_32))
 
     #define ATTR_READ_ONLY  0x01
     #define ATTR_HIDDEN 	0x02
@@ -119,27 +111,71 @@ namespace Kernel {
     #define ENTRY_UNUSED    0xE5
     #define LFN_ENTRY_END   0x40
 
-    #define GET_CLUSTER(e) (e.LowFirstCluster | (e.HighFirstCluster << (16)))
+	#define GET_CLUSTER(e) (e.LowFirstCluster | (e.HighFirstCluster << (16)))
 
-    class fat : public Kernel::virtualFileSystem {
-      private:
-        fatType fattype;
-        char* fatTypeString = 0;
+    enum fatType {
+        FAT12,
+        FAT16,
+        FAT32
+    };
+        
+    class fat : public VirtualFileSystem {
+    private: 
+        fatType fattpye;                    
+        char* FatTypeString = 0;            
 
-        uint8_t* readBuffer = 0;
-        uint8_t sectorsPerCluster = 0;
+        uint16_t bytesPerSector = 0;        
+        uint32_t rootDirSectors = 0;        
+        uint8_t sectorsPerCluster = 0;     
+        uint32_t clusterSize = 0;          
 
-        uint16_t bytesPerSector = 0;
+        uint32_t firstDataSector = 0;      
+        uint32_t firstFatSector = 0;       
+        uint32_t rootDirCluster = 0;       
+        uint32_t totalClusters = 0;        
 
-        uint32_t rootDirSectors = 0;
-        uint32_t clusterSize = 0;
-        uint32_t firstDataSector = 0;
-        uint32_t firstFatSector = 0;
-        uint32_t rootDirCluster = 0;
-        uint32_t totalClusters = 0;
+        uint8_t* readBuffer = 0;            
+        fat32Info fsInfo;                
 
-        fat32Info fsInfo;
-      public:
+    private:
+        ak::uint32_t clusterToSector(ak::uint32_t cluster);
+        ak::uint32_t readTable(ak::uint32_t cluster);
+        void writeTable(ak::uint32_t cluster, ak::uint32_t value);
+        ak::uint32_t allocateCluster();
+
+        void clearCluster(ak::uint32_t cluster);
+        char* parseLFNEntries(List<lfnEntry>* entries, directoryEntry sfnEntry);
+        char* parseShortFilename(char* fatName);
+
+        ak::uint8_t checksum(char* filename);
+
+        List<fatEntryInfo> getDirectoryEntries(ak::uint32_t dirCluster, bool rootDirectory = false);
+
+        fatEntryInfo* seachInDirectory(char* name, ak::uint32_t dirCluster, bool rootDirectory = false);
+
+        fatEntryInfo* getEntryByPath(char* path);
+
+        List<lfnEntry> createLFNEntriesFromName(char* name, int num, ak::uint8_t checksum);
+
+        char* createShortFilename(char* name);
+
+        bool writeLongFilenameEntries(List<LFNEntry>* entries, ak::uint32_t targetCluster, ak::uint32_t targetSector, ak::uint32_t sectorOffset, bool rootDirectory);
+
+        bool writeDirectoryEntry(directoryEntry entry, ak::uint32_t targetSector, ak::uint32_t sectorOffset, bool rootDirectory);
+
+        bool findEntryStartpoint(ak::uint32_t cluster, ak::uint32_t entryCount, bool rootdir, ak::uint32_t* targetCluster, ak::uint32_t* targetSector, ak::uint32_t* sectorOffset);
+
+        directoryEntry* createEntry(ak::uint32_t parentCluster, char* name, ak::uint8_t attr, bool rootdir, ak::uint32_t targetCluster, ak::uint32_t* sectorPlaced);
+
+        int createNewDirFileEntry(const char* path, ak::uint8_t attributes);
+
+        bool modifyEntry(fatEntryInfo* entry, directoryEntry newVersion);
+
+        ak::uint16_t fatTime();
+
+        ak::uint16_t fatDate();
+
+    public:
         fat(Disk* disk, ak::uint32_t start, ak::uint32_t size);
         ~fat();
 
@@ -147,14 +183,14 @@ namespace Kernel {
 
         int readFile(const char* filename, uint8_t* buffer, uint32_t offset = 0, uint32_t len = -1);
         int writeFile(const char* filename, uint8_t* buffer, uint32_t len, bool create = true);
-        int createFile(const char* path);
-        int createDirectory(const char* paht);
 
         bool fileExists(const char* filename);
         bool directoryExists(const char* filename);
 
+        int createFile(const char* path);
+        int createDirectory(const char* path);
+
         uint32_t getFileSize(const char* filename);
         List<LibC::vfsEntry>* directoryList(const char* path);
     };
-
 }
