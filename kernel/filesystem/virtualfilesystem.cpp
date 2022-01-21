5f8b339290226cccfafd43251652a694c506c5e8
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-21 11:02:06 +0530

    kernel: filesystem virtualfilesystem cpp

--- kernel/filesystem/virtualfilesystem.cpp
+++ kernel/filesystem/virtualfilesystem.cpp
@@ -5,56 +5,60 @@
 #include "virtualfilesystem.h"
 #include <kernel/system/log.h>
 
-using Kernel;
+using namespace Kernel::ak;
+using namespace Kernel;
 
-Kernel::virtualFileSystem::virtualFileSystem(Disk* disk, ak::uint32_t start, ak::uint32_t size, char* name) {
+virtualFileSystem::virtualFileSystem(Disk* disk, ak::uint32_t start, ak::uint32_t size, char* name) {
     this->disk = disk;
     this->sizeInSectors = size;
     this->startLBA = start;
     this->Name = name;
 }
 
-Kernel::virtualFileSystem::~virtualFileSystem() {
-    Log(Error, "filesystem failed %s%d", __FILE__, __LINE__);
+virtualFileSystem::~virtualFileSystem() {
+    Log(Error, "Virtual function called directly %s:%d", __FILE__, __LINE__);
 }
 
-bool Kernel::virtualFileSystem::initialize() {
+bool virtualFileSystem::initialize() {
     return false;
 }
 
-int Kernel::virtualFileSystem::readFile(const char* filename, int* buffer, int offset, int len) {
-    Log(Error, "virtual filesystem failed %s%d", __FILE__, __LINE__);
+int virtualFileSystem::readFile(const char* filename, uint8_t* buffer, uint32_t offset, uint32_t len) {
+    Log(Error, "Virtual function called directly %s:%d", __FILE__, __LINE__);
     return -1;
 }
 
-int Kernel::virtualFileSystem::writeFile(const char* filename, int* buffer, int len, bool create) {
-    Log(Error, "virtual filesystem failed %s%d", __FILE__, __LINE__);
+int virtualFileSystem::writeFile(const char* filename, uint8_t* buffer, uint32_t len, bool create) {
+    Log(Error, "Virtual function called directly %s:%d", __FILE__, __LINE__);
+    return -1;
 }
 
-bool Kernel::virtualFileSystem::fileExists(const char* filename) {
-    Log(Error, "virtual filesystem failed %s:%d", __FILE__, __LINE__);
-
+bool virtualFileSystem::fileExists(const char* filename) {
+    Log(Error, "Virtual function called directly %s:%d", __FILE__, __LINE__);
     return false;
 }
 
-bool Kernel::virtualFileSystem::directoryExists(const char* filename) {
-    Log(Error, "virtual filesystem failed %s:%d", __FILE__, __LINE__);
-
+bool virtualFileSystem::directoryExists(const char* filename) {
+    Log(Error, "Virtual function called directly %s:%d", __FILE__, __LINE__);
     return false;
 }
 
-int Kernel::virtualFileSystem::createFile(const char* path) {
-    Log(Error, "virtual filesystem failed %s:%d", __FILE__, __LINE__);
+int virtualFileSystem::createFile(const char* path) {
+    Log(Error, "Virtual function called directly %s:%d", __FILE__, __LINE__);
     return -1;
 }
 
-int Kernel::virtualFileSystem::createDirectory(const char* path) {
-    Log(Error, "virtaul filesystem failed %s:%d", __FILE__, __LINE__);
-
+int virtualFileSystem::createDirectory(const char* path) {
+    Log(Error, "Virtual function called directly %s:%d", __FILE__, __LINE__);
     return -1;
 }
 
-uint32_t Kernel::virtualFileSystem::getFileSize(const char* filename) {
-    Log(Error, "virtual filesystem failed %s:%d", __FILE__, __LINE__);
+uint32_t virtualFileSystem::getFileSize(const char* filename) {
+    Log(Error, "Virtual function called directly %s:%d", __FILE__, __LINE__);
     return -1;
+}
+
+List<LibC::vfsEntry>* virtualFileSystem::directoryList(const char* path) {
+    Log(Error, "Virtual function called directly %s:%d", __FILE__, __LINE__);
+    return 0;
 }
