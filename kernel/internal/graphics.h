38d8675076dfdd9951a951b628371b84f27b717e
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-21 12:45:12 +0530

    kernel: internal graphics header

--- kernel/internal/graphics.h
+++ kernel/internal/graphics.h
@@ -3,22 +3,24 @@
 #include "systemcomponent.h"
 
 namespace Kernel {
-    namespace system {
-        class graphicsDevice {
-        public:
-            ak::uint32_t width;
-            ak::uint32_t height;
-            ak::uint8_t bpp;
-            ak::uint32_t framebufferPhys;
-            char* identifier = 0;
+    class graphicsDevice {
+    public:
+        ak::uint32_t width;
+        ak::uint32_t height;
+        ak::uint8_t bpp;
+        ak::uint32_t framebufferPhys;
+        char* identifier = 0;
 
-            graphicsDevice(char* name);
-            virtual ~graphicsDevice();
-            virtual bool selectBestVideoMode();
+        graphicsDevice(char* name);
+        virtual ~graphicsDevice();
+        virtual bool selectBestVideoMode();
             
-            ak::uint32_t getBufferSize();
+        ak::uint32_t getBufferSize();
             
-            static graphicsDevice* getBestDevice();
-        };
-    }
+        static graphicsDevice* getBestDevice();
+    
+    private:
+        graphicsDevice();
+        static graphicsDevice* device();
+    };
 }
