00147849697ffc8a5bbf2e0ea6274d55bd833de5
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-22 17:17:15 +0530

    kernel: internal dma header

---
+++ kernel/internal/dma.h
@@ -0,0 +1,16 @@
+//
+// Created by KrisnaPranav on 22/01/22.
+//
+
+#pragma once
+
+#include <ak/types.h>
+#include "systemcomponent.h"
+
+namespace Kernel {
+    
+
+    class dmaController : public systemComponent {
+      public:
+    };
+}
