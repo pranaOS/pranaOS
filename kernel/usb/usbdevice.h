d0c110443877bb32283dbd34abddee6883b3a6c4
ひろとひんた <98206936+HirotoHinta@users.noreply.github.com>
2022-01-22 16:19:23 +0530

    kernel: [usb] usbdevice header

--- kernel/usb/usbdevice.h
+++ kernel/usb/usbdevice.h
@@ -5,3 +5,14 @@
 #pragma once
 
 #include "usbcontroller.h"
+
+namespace Kernel {
+    class usbController;
+    class usbDriver;
+
+    class usbDevice {
+    public:
+        bool assignDriver();
+        bool onUnplugged();
+    };
+}
