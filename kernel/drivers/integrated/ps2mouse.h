097a2976d2c56d25b2dc60b5ab56afe4540db45a
lucas <98066749+Lalucas20@users.noreply.github.com>
2022-01-22 16:55:31 +0530

    changes: ps2mouse remove driver namespace

--- kernel/drivers/integrated/ps2mouse.h
+++ kernel/drivers/integrated/ps2mouse.h
@@ -4,44 +4,40 @@
 #include <system/interrupthandler.h>
 
 namespace Kernel {
-    namespace drivers {
-        struct mousePacket {
-
-            unsigned char leftBTN : 1;
-            unsigned char rightBTN : 1;
-            unsigned char middleBTN : 1;
-            unsigned char always1 : 1;
-            unsigned char xSign : 1;
-            unsigned char ySign : 1;
-            unsigned char xOverflow : 1;
-            unsigned char yOverflow : 1;
-            signed char xMovement;
-            signed char yMovement;
-            signed char zMovement; 
-        };
+    struct mousePacket {
+        unsigned char leftBTN : 1;
+        unsigned char rightBTN : 1;
+        unsigned char middleBTN : 1;
+        unsigned char always1 : 1;
+        unsigned char xSign : 1;
+        unsigned char ySign : 1;
+        unsigned char xOverflow : 1;
+        unsigned char yOverflow : 1;
+        signed char xMovement;
+        signed char yMovement;
+        signed char zMovement; 
+    };
             
 
-        class ps2MouseDriver : public interruptHandler, public driver {
-        public:
-            ps2MouseDriver();
+    class ps2MouseDriver : public interruptHandler, public driver {
+    public:
+        ps2MouseDriver();
+        bool initialize();
+        ak::uint32_t handleInterrupt(ak::uint32_t esp);
 
-            bool initialize();
-            ak::uint32_t handleInterrupt(ak::uint32_t esp);
+        bool enableScrollWheel();
+        bool setSampleRate(ak::uint8_t value);
+        void processPacket();
 
-            bool enableScrollWheel();
-            bool setSampleRate(ak::uint8_t value);
-            void processPacket();
+    private:
+        #define MOUSE_DATA 0x60
+        #define MOUSE_COMMAND 0x64
+        #define MOUSE_ACK 0xFA
 
-        private:
-            #define MOUSE_DATA 0x60
-            #define MOUSE_COMMAND 0x64
-            #define MOUSE_ACK 0xFA
+        ak::uint8_t mouseID = 0;
+        ak::uint8_t mouseCycle = 0;
+        signed char* packetBuffer;
 
-            ak::uint8_t mouseID = 0;
-            ak::uint8_t mouseCycle = 0;
-            signed char* packetBuffer;
-
-            bool ready = false;
-        };
-    }
+        bool ready = false;
+    };
 }
