52154c35d74b9d33602d2194b997c0a5992d8d3d
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-23 15:47:44 +0530

    kernel: cpu vm header

--- kernel/cpu/vm.h
+++ kernel/cpu/vm.h
@@ -14,11 +14,37 @@ namespace Kernel {
 
     #define KERNEL_VIRT_ADDR 3_GB
     #define USER_STACK_SIZE 32_KB
-    #define USER_STAcK_TOP (KERNEL_VIRT_ADDR)
+    #define USER_STACK_TOP (KERNEL_VIRT_ADDR)
     #define USER_STACK (USER_STACK_TOP - USER_STACK_SIZE)
 
     #define PAGE_SIZE 4_KB
-    #define KERNEL_PTNUM 768 
+    #define KERNEL_PTNUM 768
     #define PAGE_TABLE_ADDRESS 0xFFC00000
     #define PAGE_DIRECTORY_ADDRESS 0xFFFFF000
+    #define PAGE_OFFSET_BITS 12
+    #define PAGEDIR_INDEX(addr) (((uint32_t)addr) >> 22)
+    #define PAGETBL_INDEX(addr) ((((uint32_t)addr) >> 12) & 0x3ff)
+    #define PAGEFRAME_INDEX(addr) (((uint32_t)addr) & 0xfff)
+
+    /* todo */
+    struct pageDirectoryEntry {
+        ak::uint32_t present;
+    };
+
+    struct pageTableEntry {
+    };
+
+    struct pageTable {
+    };
+
+    struct pageDirectory {
+    };
+
+    static inline void invlpg(void* addr) {
+        asm volatile("invlpg (%0)" ::"r" (addr) : "memory");
+    }
+
+    class virtualMemoryManager {
+        
+    };
 }
