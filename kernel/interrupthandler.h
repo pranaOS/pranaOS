84e04aab5a68ec00a0823ac54d0fc2472115a2b5
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-23 17:18:31 +0530

    kernel: interrupthandler header
    interrupt handler update namespace

--- kernel/interrupthandler.h
+++ kernel/interrupthandler.h
@@ -11,24 +11,22 @@
 #include <ak/types.h>
 
 namespace Kernel {
-    namespace system {
-        class interruptHandler {
-        public:
-            interruptHandler(ak::uint8_t intNumber);
-            virtual ak::uint32_t handleInterrupt(ak::uint32_t esp);
-        };
+    class interruptHandler {
+    public:
+        interruptHandler(ak::uint8_t intNumber);
+        virtual ak::uint32_t handleInterrupt(ak::uint32_t esp);
+    };
 
 
-        class interruptManager {
-        public:
-            static void initialize();
-            static ak::uint32_t handleInterrupt(ak::uint8_t interrupt, ak::uint32_t esp);
+    class interruptManager {
+    public:
+        static void initialize();
+        static ak::uint32_t handleInterrupt(ak::uint8_t interrupt, ak::uint32_t esp);
 
-            static void addHandler(interruptHandler* handler, ak::uint8_t interrupt);
-            static void removeHandler(interruptHandler* handler, ak::uint8_t interrupt);
-            
-        private:
-            static List<interruptHandler*>* interruptCallbacks[256];
-        };
-    }
+        static void addHandler(interruptHandler* handler, ak::uint8_t interrupt);
+        static void removeHandler(interruptHandler* handler, ak::uint8_t interrupt);
+    
+    private:
+        static List<interruptHandler*>* interruptCallbacks[256];
+    };
 }
