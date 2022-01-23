876cec6c98f58b953fd33b6c649ccd484fe756c2
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-23 19:19:14 +0530

    kernel entry point header

--- kernel/kernel.h
+++ kernel/kernel.h
@@ -4,6 +4,12 @@
 
 #pragma once
 
+#include "console.h"
+#include <kernel/internal/systemcomponent.h>
+#include <kernel/internal/rtc.h>
+#include <kernel/internal/smbios.h>
+#include <kernel/internal/graphics.h>
+
 namespace Kernel {
     #define DEFAULT_SCREEN_WIDTH 1024
     #define DEFAULT_SCREEN_HEIGHT 876
