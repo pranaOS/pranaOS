85b88e722adab05d11a6a821d6f5670a0beadbf3
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-23 19:01:29 +0530

    kernel: cpu idt cpp

--- kernel/cpu/idt.cpp
+++ kernel/cpu/idt.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "idt.h"
+#include <ak/memoperator.h>
 
 using namespace Kernel;
 
@@ -10,7 +11,7 @@ idtEntry idtEntries[IDT_ENTRY_SIZE];
 idtPointer idtpointer;
 
 
-void InterruptDescriptorTable::SetDescriptor(uint32_t number, void (*handler)(), int accesLevel) {
+void interruptDescriptorTable::setDescriptor(uint32_t number, void (*handler)(), int accesLevel) {
     uint32_t callerBase = (uint32_t)handler;
 
     idtEntries[number].handlerLowBits = (uint16_t)(callerBase & 0xFFFF);
@@ -18,4 +19,9 @@ void InterruptDescriptorTable::SetDescriptor(uint32_t number, void (*handler)(),
     idtEntries[number].reserved = 0;
     idtEntries[number].access = IDT_PRESENT | ((accesLevel & 3) << 5) | IDT_INTERRUPT;
     idtEntries[number].selector = 0x8;
+}
+
+void interruptDescriptorTable::install() {
+    idtpointer.size = sizeof(idtEntry) * IDT_ENTRY_SIZE - 1;
+    idtpointer.base = (uint32_t)&idtEntries[0];
 }
