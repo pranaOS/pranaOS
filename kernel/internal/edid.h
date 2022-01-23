4de77274a0a414e658f477212eb6a114ce5d7f67
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-23 19:33:49 +0530

    kernel: internal edid header

--- kernel/internal/edid.h
+++ kernel/internal/edid.h
@@ -9,7 +9,13 @@
 namespace Kernel {
     struct edidInfoBlock {
         ak::uint8_t header[8];
-        ak::uint16_t manufacturerid;
+        ak::uint16_t manufacturerId;
+        ak::uint16_t productId;
+        ak::uint32_t serialNumber;
+        ak::uint8_t weekOfManufacture
+        ak::uint8_t yearOfManufacture;
+        ak::uint8_t version;
+        ak::uint8_t revision;
     };
 
     #define EDID_SUCCESS_CODE 0x004F
