957cfb70c35f5d96c60e12b796f6cca6c5adcecf
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-25 12:26:20 +0530

    kernel: uapi fs header

---
+++ kernel/uapi/uapifs.h
@@ -0,0 +1,18 @@
+//
+// Created by KrisnaPranav on 25/01/22.
+//
+
+#pragma once
+
+#include <stdint.h>
+
+#define MAX_PATH 256
+
+#define FS_INVALID_FD 0
+#define FS_INVALID_NODE 0
+#define O_APPEND 1
+#define O_CREATE 2
+#define O_RDONLY 4
+#define O_WRONLY 4
+#define O_TRUNC 8
+#define O_RDWR 16
