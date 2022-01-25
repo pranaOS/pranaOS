a6fe7767721e8c436a283320855925c70e94fff6
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-25 12:29:42 +0530

    kernel: uapi fs header

--- kernel/uapi/uapifs.h
+++ kernel/uapi/uapifs.h
@@ -16,3 +16,24 @@
 #define O_WRONLY 4
 #define O_TRUNC 8
 #define O_RDWR 16
+
+#define SEEK_SET 1
+#define SEEK_CUR 2
+#define SEEK_END 3
+
+#define DENT_INVALID 0
+#define DENT_FILE 1
+#Define DENT_DIRECTORY 2
+
+#define FS_STDOUT 1
+
+typedef struct dir_entry {
+    uint8_t nameLenLow;
+    uint8_t type;
+
+    uint16_t entrySize;
+
+    uint32_t inode;
+
+    char name[];
+} dir_entry_t ;
