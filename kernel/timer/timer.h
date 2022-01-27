929b4c7f6c806d94c285836340dca1db11d1c9b9
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-27 12:06:05 +0530

    kernel: timer header
    timer functionality

--- kernel/timer/timer.h
+++ kernel/timer/timer.h
@@ -5,3 +5,28 @@
  */
 
 #pragma once
+
+#include <kernel/interruptservice.h>
+
+/**
+ * @brief timer functionality
+ * 
+ */
+void timerCallback();
+uint32_t timerGetTick();
+float timerGetTime();
+void timerRegisterCallback(handler_t handler);
+
+/**
+ * @brief set timer freq.
+ * 
+ */
+#define TIMERFREQ 50
+#define TIMER_QUOTIENT 1193180
+#define PIT_0 0x40
+#define PIT_1 0x41
+#define PIT_2 0x42
+#define PIT_CMD 0x43
+#define PIT_SET 0x36
+
+void init_timer();
