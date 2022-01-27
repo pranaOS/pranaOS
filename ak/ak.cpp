a26212a97821dca1e8ce798ef69719b67cc59537
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-27 10:53:07 +0530

    ak: all kind of files

--- ak/ak.cpp
+++ ak/ak.cpp
@@ -1,10 +1,19 @@
+//
+//  ak.cpp
+//  pranaOS
+//
+//  Created by KrisnaPranav on 27/01/22.
+//
+
 #include "convert.h"
-#include "list.h"
 #include "memoperator.h"
 #include "print.h"
 #include "random.h"
+#include "ringbuffer.h"
 #include "string.h"
 #include "types.h"
 #include "utils.h"
 
+#ifndef _TESTING_
 using namespace ak;
+#endif
