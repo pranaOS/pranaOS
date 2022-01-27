3f6412053b96accff54594279a9fbcc00cbac23b
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-27 11:58:45 +0530

    kerenl: interruptservice header

--- kernel/interruptservice.h
+++ kernel/interruptservice.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2020 - 2022, the pranaOS Developers & Krisna Pranav
+ *
+ * SPDX-License-Identifier: GPL-3.0 License
+ */
+
 #pragma once
 
 #include <stdint.h>
@@ -7,6 +13,7 @@ typedef struct {
     uint32_t fs;
     uint32_t es;
     uint32_t ds;
+    uint32_t ss;
     uint32_t edi;
     uint32_t esi;
     uint32_t ebi;
@@ -26,3 +33,38 @@ typedef struct {
 typedef void (*handler_t)(register_t*);
 
 void init_isr();
+void isr_register_handle(uint32_t num, handler_t handler);
+
+extern void isr0();
+extern void isr1();
+extern void isr2();
+extern void isr3();
+extern void isr4();
+extern void isr5();
+extern void isr6();
+extern void isr7();
+extern void isr8();
+extern void isr9();
+extern void isr10();
+extern void isr11();
+extern void isr12();
+extern void isr13();
+extern void isr14();
+extern void isr15();
+extern void isr16();
+extern void isr17();
+extern void isr18();
+extern void isr19();
+extern void isr20();
+extern void isr21();
+extern void isr22();
+extern void isr23();
+extern void isr24();
+extern void isr25();
+extern void isr26();
+extern void isr27();
+extern void isr28();
+extern void isr29();
+extern void isr30();
+extern void isr31();
+extern void isr48();
