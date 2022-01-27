fa23d148190d80c359cf114e225d413e0445a9f4
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-27 12:31:30 +0530

    libc: math header[updating math functionalities]

---
+++ userland/libraries/libc/math/math.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <types.h>
+
+namespace LibC {
+    // reference: https://www.geeksforgeeks.org/pi-in-c-with-examples/
+    #define MATH_PI 3.14159265358979323846
+
+    class math {
+    public:     
+        /**
+         * @brief float values
+         * 
+         * @param a 
+         * @param b 
+         * @return float 
+         */
+        float fmaxf(float a, float b);
+        float fminf(float a, float b);
+        
+        
+        /**
+         * @brief double functions 
+         * 
+         * @param a 
+         * @param b 
+         * @return double 
+         */
+        double fmax(double a, double b);
+        double fmin(double a, double b);
+        
+
+        /**
+         * @brief integers
+         * 
+         * @param a 
+         * @param b 
+         * @return int 
+         */
+
+        int min(int a, int b);
+        int max(int a, int b);
+        int powi(int x, int y);
+        
+    };
+}
