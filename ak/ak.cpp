6eae9a074698f9cb884bada338faa6ae7f396beb
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-27 11:07:32 +0530

    ak: ak cpp
    entry point cpp file

---
+++ ak/ak.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2020 - 2022, the pranaOS Developers & Krisna Pranav
+ *
+ * SPDX-License-Identifier: GPL-3.0 License
+ */
+
+#include "convert.h"
+#include "list.h"
+#include "memoperator.h"
+#include "print.h"
+#include "random.h"
+#include "string.h"
+#include "types.h"
+#include "utils.h"
+
+using namespace ak;
