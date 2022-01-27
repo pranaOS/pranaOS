afeb4ccda91bddb5309fa40d954f711c1d49a89d
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-27 09:22:47 +0530

    libc: sys

---
+++ userland/libraries/libc/sys/types.h
@@ -0,0 +1,31 @@
+#pragma once
+
+namespace LibC {
+
+    #define STATIC_ASSERT(condition) typedef char p__LINE__[ (condition) ? 1 : -1];
+
+    typedef char                        int8_t;
+    typedef unsigned char               uint8_t;
+    typedef short                       int16_t;
+    typedef unsigned short              uint16_t;
+    typedef int                         int32_t;
+    typedef unsigned int                uint32_t;
+    typedef long long int               int64_t;
+    typedef unsigned long long int      uint64_t;
+    typedef unsigned long long          uintptr_t;
+
+    constexpr uint32_t operator"" _KB(unsigned long long no)
+    {
+        return no * 1024;
+    }
+
+    constexpr uint32_t operator"" _MB(unsigned long long no)
+    {
+        return no * (1024_KB);
+    }
+    
+    constexpr uint32_t operator"" _GB(unsigned long long no)
+    {
+        return no * (1024_MB);
+    }
+}
