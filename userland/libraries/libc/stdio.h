54804b283d2e7c4f7bcb1ba96643cbb7234f0ae8
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-27 09:02:54 +0530

    libc: stdio header
    update stdio header functionalities

--- userland/libraries/libc/stdio.h
+++ userland/libraries/libc/stdio.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include <unistd.h>
 #include <stdarg.h>
 #include <stdint.h>
+#include <unistd.h>
 
 #define EOF -1
 
@@ -16,20 +16,30 @@ extern FILE* stderr;
 
 int putchar(int);
 int puts(const char*);
+
 int printf(const char* format, ...);
 int sprintf(char* str, const char* format, ...);
+int snprintf(char* str, size_t size, const char* format, ...);
+int vsprintf(char* str, const char* format, va_list ap);
+int vsnprintf(char* str, size_t size, const char* format, va_list ap);
 
 
 /**
  * @brief use these functions when pranaOS kernel mode enabled
  * 
  */
-#ifndef __pranaOS__
+#ifndef _pranaOS_
 FILE* fopen(const char* path, const char* mode);
 int fclose(FILE* stream);
-int fread(void* ptr, size_t size, size_t memb, FILE* stream);
+int fread(void* ptr, size_t size, size_t nmemb, FILE* stream);
 int fgetc(FILE* stream);
-int fwrite(const void* ptr, size_t size, size_t memb, FILE* stream);
+int fwrite(const void* ptr, size_t size, size_t nmemb, FILE* stream);
 int fputc(int c, FILE* stream);
 int fprintf(FILE* stream, const char* format, ...);
+int vfprintf(FILE* stream, const char* format, va_list ap);
+int fseek(FILE* stream, long offset, int whence);
+long ftell(FILE* stream);
+int fflush(FILE* stream);
+int rename(const char* old, const char* new);
+int remove(const char* pathname);
 #endif
