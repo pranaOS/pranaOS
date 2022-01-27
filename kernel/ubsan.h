6a4142d2ea5c183a6007023a5c432389f9f0f105
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-27 19:38:20 +0530

    kernel: ubsan header

--- kernel/ubsan.h
+++ kernel/ubsan.h
@@ -1,37 +1,47 @@
-//
-// Created by KrisnaPranav on 27/01/22.
-//
+/*
+ * Copyright (c) 2021 - 2022, the pranaOS Developers & Krisna Pranav
+ *
+ * SPDX-License-Identifier: GPL-3.0 License
+ */
 
 #pragma once
 
 #include <stdint.h>
 
-typedef struct {
-    uint16_t kind;
-    uint16_t info;
-    char name[];
-} ubsan_type_t;
+namespace Kernel {
+    typedef struct {
+        uint16_t kind;
+        uint16_t info;
+        char name[];
+    } ubsan_type_t;
 
-typedef struct {
-    uint32_t line;
-    uint32_t column;
-    const char* file;
-} ubsan_source_location_t;
+    typedef struct {
+        const char* file;
+        uint32_t line;
+        uint32_t column;
+    } ubsan_source_location_t;
 
-typedef struct {
-    ubsan_source_location_t location
-    ubsan_type_t* type;
-    uint8_t align;
-    uint8_t kind;
-} ubsan_mismatch_data_t;
+    typedef struct {
+        ubsan_source_location_t location;
+        ubsan_type_t* type;
+        uint8_t align;
+        uint8_t kind;
+    } ubsan_mismatch_data_t;
 
-typedef struct {
-    ubsan_source_location_t location;
-    ubsan_type_t* type;
-} ubsan_overflow_data_t;
+    typedef struct {
+        ubsan_source_location_t location;
+        ubsan_type_t* type;
+    } ubsan_overflow_data_t;
 
-typedef struct {
-    ubsan_source_location_t location ;
-    ubsan_type_t* lhsType;
-    ubsan_type_t* rhsType;
-} ubsan_shift_out_of_bounds_data_t;
+    typedef struct {
+        ubsan_source_location_t location;
+        ubsan_type_t* lhs_type;
+        ubsan_type_t* rhs_type;
+    } ubsan_shift_out_of_bounds_data_t;
+
+    typedef struct {
+        ubsan_source_location_t location;
+        ubsan_type_t* array_type;
+        ubsan_type_t* index_type;
+    } ubsan_out_of_bounds_data_t;
+}
