340fe67ab34c6c424047c8cc048c10e8e9fcbc48
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-27 19:15:07 +0530

    kernel: bus address header

--- kernel/bus/address.h
+++ kernel/bus/address.h
@@ -1,10 +1,20 @@
-//
-// Created by KrisnaPranav on 24/01/22.
-//
+/*
+ * Copyright (c) 2021 - 2022, the pranaOS Developers & Krisna Pranav
+ *
+ * SPDX-License-Identifier: GPL-3.0 License
+ */
+
+#pragma once
 
 namespace Kernel {
     struct Address {
         Address() = default;
         Address(const Address& address) = default;
+    };
+
+    class busAddress {
+      public:
+        busAddress();
+        Address address_t;
     };
 }
