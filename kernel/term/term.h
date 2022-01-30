b8269b12dc096a8154cd16a4e110a480e54dc851
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-30 16:42:17 +0530

    kernel: term header

--- kernel/term/term.h
+++ kernel/term/term.h
@@ -14,6 +14,33 @@
 #define TERM_MEMORY 0xfffff
 
 typedef enum {
+    TERM_COLOR_BLACK,
+    TERM_COLOR_BLUE,
+    TERM_COLOR_GREEN,
+    TERM_COLOR_CYAN,
+    TERM_COLOR_RED,
+    TERM_COLOR_MAGENTA,
+    TERM_COLOR_BROWN,
+    TERM_COLOR_LIGHT_GREY,
+    TERM_COLOR_DARK_GREY,
+    TERM_COLOR_LIGHT_BLUE,
+    TERM_COLOR_LIGHT_GREEN,
+    TERM_COLOR_LIGHT_CYAN,
+    TERM_COLOR_LIGHT_RED,
+    TERM_COLOR_LIGHT_MAGENTA,
+    TERM_COLOR_LIGHT_BROWN,
     TERM_COLOR_WHITE,
-    TERM_COLOR_CYAN
 } term_color_t;
+
+namespace Kernel {
+
+    class Term {
+      public:
+        void init_term();
+        void term_change_bg_color(term_color_t bg);
+        void term_set_blink(int blink);
+        void term_scrolldown();
+        void term_new_line();
+        void term_putchar_at(char c, uint32_t x, uint32_t y);
+    };
+}
