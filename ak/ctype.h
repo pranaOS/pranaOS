1c13eb10d054a0c026e116a1eb3f3802945365d1
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-31 19:20:47 +0530

    ak: ctype header

---
+++ ak/ctype.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define isspace(c)
+#define isupper(c)
+#define islower(c)
+#define isalpha(c)
+#define isdigit(c)
+#define isxdigit(c)
+#define isprint(c)
+#define toupper(c)
+#define tolower(c)
+#define isascii(c)
+#define toascii(c)
