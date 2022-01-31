4a595b09d7af46fa60ffa42b3a8cd3e2adae85c8
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-31 18:18:27 +0530

    ak: atomic header

--- ak/atomic.h
+++ ak/atomic.h
@@ -4,10 +4,22 @@ typedef struct {
     int counter;
 } atomic_t;
 
+#define atomic_read(v) ((v)->counter)
+#define atomic_set(v, i) (((v)->counter) = (i))
+
+
 static inline void atomic_add(int i, atomic_t* v) {
     __asm__ volatile(
         "addl %1 %0"
         : "=m"(v->counter)
         : "ir"(i), "m"(v->counter)
+    );
+}
+
+static inline void atomic_inc(atomic_t* v) {
+    __asm__ volatile(
+        "incl %0"
+        : "=m"(v->counter)
+        : "m" (v->counter)
     );
 }
