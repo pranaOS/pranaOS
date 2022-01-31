02454a25ab0d08eb620b03455364208ca8fa4f05
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-31 08:53:43 +0530

    libc: string header

--- userland/libraries/libc/string.h
+++ userland/libraries/libc/string.h
@@ -21,6 +21,17 @@ extern "C" {
 size_t strlen(const char* str);
 size_t strnlen(const char* s, size_t maxlen);
 
+/**
+ * @brief comparing string 1 and string 2 function
+ * 
+ * @param a 
+ * @param b 
+ * @param n 
+ * @return int 
+ */
+int strncasecmp(const char* a, const char* b, size_t n);
+int strcasecmp(const char* a, const char* b);
+
 #ifndef __cplusplus
 }
 
