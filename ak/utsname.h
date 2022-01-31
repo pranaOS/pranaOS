f6be2cdc025e6a9623f5bb69694a830484e9e6ca
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-31 17:27:30 +0530

    ak: utsname header

--- ak/utsname.h
+++ ak/utsname.h
@@ -1,9 +1,8 @@
 #pragma once
 
 #define UTSNAME_LENGTH 65
+
 #define UTSNAME_DOMAIN_LENGTH UTSNAME_LENGTH
-#define UTSNAME_NODENAME_LENGTH
-#define UTSNAME_VERSION_LENGTH
 
 #ifndef UTSNAME_SYSNAME_LENGTH
 #define UTSNAME_SYSNAME_LENGTH UTSNAME_LENGTH
@@ -21,13 +20,16 @@
 #define UTSNAME_MACHINE_LENGTH UTSNAME_LENGTH
 #endif
 
-
 /**
- * @brief uts name
+ * @brief utsname[]
  * 
  */
 struct utsname
 {
-    char sysname[UTSNAME_SYSNAME_LENGTH];
-    char version[UTSNAME_VERSION_LENGTH];
+	char sysname[UTSNAME_SYSNAME_LENGTH];
+	char nodename[UTSNAME_NODENAME_LENGTH];
+	char release[UTSNAME_RELEASE_LENGTH];
+	char version[UTSNAME_VERSION_LENGTH];
+	char machine[UTSNAME_MACHINE_LENGTH];
+	char domainname[UTSNAME_DOMAIN_LENGTH];
 };
