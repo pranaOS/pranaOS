2fe200b14a0ea8f37ad724d5cc18f2110543b45d
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-31 17:35:45 +0530

    ak: types header

--- ak/types.h
+++ ak/types.h
@@ -26,6 +26,20 @@ namespace ak {
     typedef unsigned long long ino_t;
     typedef unsigned int mode_t;
     typedef long ssize_t;
+    typedef unsigned long sector_t;
+    typedef int pid_t;
+    typedef int tid_t;
+    typedef int uid_t;
+    typedef int sid_t;
+    typedef unsigned int sigset_t;
+    typedef int idtype_t;
+    typedef int id_t;
+    typedef long long time_t;
+    typedef int clockid_t;
+    typedef int fops_t;
+    typedef unsigned int fmode_t;
+    typedef unsigned int nlink_t;
+    typedef int blksize_t;
 
     constexpr uint64_t divide64(uint64_t n, uint32_t base, uint32_t* r = 0) {
         uint64_t rem = n;
