b036ef79c895a75f199f42aa5c0ed46d4333b84c
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-31 19:21:52 +0530

    ak: ctype header

--- ak/ctype.h
+++ ak/ctype.h
@@ -1,13 +1,18 @@
 #pragma once
 
-#define isspace(c)
-#define isupper(c)
-#define islower(c)
-#define isalpha(c)
-#define isdigit(c)
-#define isxdigit(c)
-#define isprint(c)
-#define toupper(c)
-#define tolower(c)
-#define isascii(c)
-#define toascii(c)
+/**
+ * @brief space, upper, lower, alpha, digit, toupper, tolower, isascii, toascii
+ * 
+ */
+#define isspace(c) ((c) == ' ' || ((c) >= '\t' && (c) <= '\r'))
+#define isupper(c) ((c) >= 'A' && (c) <= 'Z')
+#define islower(c) ((c) >= 'a' && (c) <= 'z')
+#define isalpha(c) (isupper(c) || islower(c))
+#define isdigit(c) ((c) >= '0' && (c) <= '9')
+#define isdigits(c) (isdigit(c) || ((c) >= 'A' && (c) <= 'F') || ((c) >= 'a' && (c) <= 'f'))
+#define isprint(c) ((c) >= ' ' && (c) <= '~')
+#define isascii(c) ((unsigned)(c) <= 0x7F)
+
+#define toupper(c) ((c)-0x20 * (((c) >= 'a') && ((c) <= 'z')))
+#define tolower(c) ((c) + 0x20 * (((c) >= 'A') && ((c) <= 'Z')))
+#define toascii(c) ((unsigned)(c)&0x7F)
