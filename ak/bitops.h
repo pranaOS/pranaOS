ded248792a444e46142446c9d30c9f6b56f43897
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-31 19:09:30 +0530

    ak: bitops header

--- ak/bitops.h
+++ ak/bitops.h
@@ -2,17 +2,58 @@
 
 #define ADDR (*(volatile int*)addr)
 
-static inline unsigned int ffz(unsigned int word) {
-    __asm__("bsfl %1,%0"
-            : "=r"(word)
-            : "r"(~word));
+/**
+ * @brief fzz
+ * 
+ * @param word 
+ * @return unsigned int 
+ */
 
-    return word
+static inline unsigned int ffz(unsigned int word) {
+	__asm__("bsfl %1,%0"
+			: "=r"(word)
+			: "r"(~word));
+	return word;
 }
 
-static inline test_bit(int nr, const unsigned int* addr) {
-    int mask;
-    addr += nr >> 5;
-    mask = 1 << (nr & 0x1f);
-    return ((mask & *addr) != 0);
+/**
+ * @brief Set the bit object
+ * 
+ * @param nr 
+ * @param addr 
+ */
+
+static inline void set_bit(int nr, volatile unsigned int* addr) {
+	__asm__(
+		"btsl %1,%0"
+		: "=m"(ADDR)
+		: "Ir"(nr));
+}
+
+/**
+ * @brief clear bit function 
+ * 
+ * @param nr 
+ * @param addr 
+ */
+static inline void clear_bit(int nr, volatile unsigned int* addr) {
+	__asm__ __volatile__(
+		"btrl %1,%0"
+		: "=m"(ADDR)
+		: "Ir"(nr));
+}
+
+/**
+ * @brief mask addr test bit function 
+ * 
+ * @param nr 
+ * @param addr 
+ * @return int 
+ */
+static inline int test_bit(int nr, const unsigned int* addr) {
+	int mask;
+
+	addr += nr >> 5;
+	mask = 1 << (nr & 0x1f);
+	return ((mask & *addr) != 0);
 }
