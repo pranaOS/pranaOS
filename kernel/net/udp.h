19276cde6e8558c1eb5c35d8e25fa9fba856bd82
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-01 11:18:44 +0530

    kernel: net udp header

--- kernel/net/udp.h
+++ kernel/net/udp.h
@@ -2,13 +2,36 @@
 
 #include <stdint.h>
 
+/**
+ * @brief udp packet struct
+ * 
+ */
 struct __attribute__((packed)) udp_packet {
-    uint16_t source_port;
-    uint16_t dest_port;
-    uint16_t length;
-    uint16_t checksum
-    uint16_t payload[];
-}
+	uint16_t source_port;
+	uint16_t dest_port;
+	uint16_t length;
+	uint16_t checksum;
+	uint8_t payload[];
+};
 
+/**
+ * @brief udp build function
+ * 
+ * @param udp 
+ * @param msg_len 
+ * @param source_ip 
+ * @param source_port 
+ * @param dest_ip 
+ * @param dest_port 
+ */
+void udp_build(struct udp_packet *udp, uint16_t msg_len, uint32_t source_ip, uint16_t source_port, uint32_t dest_ip, uint16_t dest_port);
+
+/**
+ * @brief udp validate
+ * 
+ * @param udp 
+ * @param source_ip 
+ * @param dest_ip 
+ * @return int 
+ */
 int udp_validate(struct udp_packet *udp, uint32_t source_ip, uint32_t dest_ip);
-void udp_build(struct udp_packet* udp, uint16_t msg_len, uint32_t source_ip);
