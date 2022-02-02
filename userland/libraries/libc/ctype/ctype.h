fbbe657d6047ca4f4b32cf032ded86712cdc923c
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-02 12:25:12 +0530

    libc: ctpye header

--- userland/libraries/libc/ctype/ctype.h
+++ userland/libraries/libc/ctype/ctype.h
@@ -6,11 +6,32 @@
 
 #include <sys/types.h>
 
-#ifndef locale_t_defined
-#define locale_t_defined
-typedef _locale_t locale_t;
+/**
+ * @brief locale_t
+ *
+ */
+#ifndef LOCALE_T
+#define LOCALE_T
+typedef __locale_t locale_t;
 #endif
 
 /**
  * @brief alpha, alnum, ascii, blank, cntrl, digit, graph, lower, print, space, punct, upper, xdigit, ascii, lower
  */
+
+int isalnum(int);
+int isalpha(int);
+int isascii(int);
+int isblank(int);
+int iscntrl(int);
+int isdigit(int);
+int isgraph(int);
+int islower(int);
+int isprint(int);
+int ispunct(int);
+int isspace(int);
+int isupper(int);
+int isxdigit(int);
+int toascii(int);
+int tolower(int);
+int toupper(int);
