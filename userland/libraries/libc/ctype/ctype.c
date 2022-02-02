c8c496115c3e28688a9295a27308150a5e7d553a
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-02 12:27:09 +0530

    libc: ctype

--- userland/libraries/libc/ctype/ctype.c
+++ userland/libraries/libc/ctype/ctype.c
@@ -1,1 +1,25 @@
 #include <ctype.h>
+
+int isalnum(int c) {
+    return isalpha(c) || isdigit(c);
+}
+
+int isalpha(int c) {
+    return isupper(c) || islower(c);
+}
+
+int isascii(int c) {
+    return (unsigned )c <= 0x7F;
+}
+
+int isblank(int c) {
+    return c == ' ' || c == '\t';
+}
+
+int iscntrl(int c) {
+    return c >= '0' && c < 32;
+}
+
+int toascii(int c) {
+    return (unsigned)c & 0x7F;
+}
