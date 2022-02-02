1f0f151e7cec22441c4ff64c4cdb394e0da47dc6
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-02 12:12:36 +0530

    libc: sys types header

--- userland/libraries/libc/sys/types.h
+++ userland/libraries/libc/sys/types.h
@@ -1,31 +1,43 @@
 #pragma once
 
+#include <sys/cdefs.h>
+
 namespace LibC {
+    typedef unsigned int dev_t;
+    typedef unsigned short umode_t;
 
-    #define STATIC_ASSERT(condition) typedef char p__LINE__[ (condition) ? 1 : -1];
+    typedef long long loff_t;
+    typedef long off_t;
+    typedef unsigned long long ino_t;
+    typedef unsigned int mode_t;
+    typedef unsigned long ssize_t;
+    typedef unsigned long sector_t;
 
-    typedef char                        int8_t;
-    typedef unsigned char               uint8_t;
-    typedef short                       int16_t;
-    typedef unsigned short              uint16_t;
-    typedef int                         int32_t;
-    typedef unsigned int                uint32_t;
-    typedef long long int               int64_t;
-    typedef unsigned long long int      uint64_t;
-    typedef unsigned long long          uintptr_t;
+    /**
+     * @brief pid, tid, uid, gid, sid
+     * 
+     */
+    typedef int pid_t;
+    typedef int tid_t;
+    typedef int uid_t;
+    typedef int gid_t;
+    typedef int sid_t;
 
-    constexpr uint32_t operator"" _KB(unsigned long long no)
-    {
-        return no * 1024;
-    }
+    typedef unsigned int sigset_t;
+    typedef int idtype_t;
+    typedef int id_t;
+    typedef int clockid_t;
+    typedef long long clock_t;
+    typedef int fpos_t;
+    typedef unsigned int nlink_t;
+    typedef int blksize_t;
+    typedef int blkcnt_t;
+    typedef unsigned int uint;
+    typedef int seconds_t;
+    typedef unsigned int useconds_t;
 
-    constexpr uint32_t operator"" _MB(unsigned long long no)
-    {
-        return no * (1024_KB);
-    }
-    
-    constexpr uint32_t operator"" _GB(unsigned long long no)
-    {
-        return no * (1024_MB);
-    }
+    typedef unsigned char u_char;
+    typedef unsigned short u_short;
+    typedef unsigned int u_int;
+    typedef unsigned long u_long;
 }
