031fe702c31861491efe08a66b3d75ca7667f8b2
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-03 19:20:43 +0530

    kernel: null

--- kernel/null.c
+++
@@ -1,6 +0,0 @@
-#include <null.h>
-#include <stdint.h>
-
-static null_initialize(uint32_t flags) {
-    return flags;
-}
---
+++ kernel/null/null.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021 - 2022, the pranaOS Developers & Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <null.h>
+#include <stdint.h>
+
+static null_initialize(uint32_t flags) {
+    return flags;
+}
