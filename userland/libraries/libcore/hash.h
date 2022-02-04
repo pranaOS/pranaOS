510003266c4fb6901636d205e0973619c8e13de2
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-04 19:12:09 +0530

    libcore: hash header

--- userland/libraries/libcore/hash.h
+++ userland/libraries/libcore/hash.h
@@ -12,6 +12,10 @@
 struct hashmap_iter;
 struct hashmap_entry;
 
+/**
+ * @brief hash map struct [table size, num entries]
+ * 
+ */
 struct hashmap {
 	size_t table_size_init;
 	size_t table_size;
@@ -22,3 +26,28 @@ struct hashmap {
 	void *(*key_alloc)(const void *);
 	void (*key_free)(void *);
 };
+
+/**
+ * @brief hash map init function
+ * 
+ * @param map 
+ * @param hash_func 
+ * @param key_compare_func 
+ * @param initial_size 
+ * @return int 
+ */
+int hashmap_init(struct hashmap *map, size_t (*hash_func)(const void *), int (*key_compare_func)(const void *, const void *), size_t initial_size);
+
+/**
+ * @brief hashmap destroy
+ * 
+ * @param map 
+ */
+void hashmap_destroy(struct hashmap *map);
+
+/**
+ * @brief hash map clear 
+ * 
+ * @param map 
+ */
+void hashmap_clear(struct hashmap *map);
