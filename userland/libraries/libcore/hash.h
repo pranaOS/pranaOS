723a0e9b9744e1d6972cde916fd49a869efd932e
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-04 19:08:55 +0530

    libcore: hash header

--- userland/libraries/libcore/hash.h
+++ userland/libraries/libcore/hash.h
@@ -9,23 +9,6 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#define HASHMAP_FUNCS_DECLARE(name, key_type, data_type)                 \
-	data_type *name##_hashmap_put(struct hashmap *map,                   \
-								  const key_type *key, data_type *data); \
-	data_type *name##_hashmap_get(const struct hashmap *map,             \
-								  const key_type *key);                  \
-	data_type *name##_hashmap_remove(struct hashmap *map,                \
-									 const key_type *key);               \
-	const key_type *name##_hashmap_iter_get_key(                         \
-		const struct hashmap_iter *iter);                                \
-	data_type *name##_hashmap_iter_get_data(                             \
-		const struct hashmap_iter *iter);                                \
-	void name##_hashmap_iter_set_data(const struct hashmap_iter *iter,   \
-									  data_type *data);                  \
-	int name##_hashmap_foreach(const struct hashmap *map,                \
-							   int (*func)(const key_type *, data_type *, void *), void *arg);
-
-
 struct hashmap_iter;
 struct hashmap_entry;
 
