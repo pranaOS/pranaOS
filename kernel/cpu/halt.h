6658c3a6e160b9b534ecd809c0f840b1e05bd45e
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-07 19:42:53 +0530

    kernel: cpu halt header

--- kernel/cpu/halt.h
+++ kernel/cpu/halt.h
@@ -20,6 +20,22 @@ static __inline void enable_interrupt() {
     __asm__ __volatile__("sti");
 }
 
-static inline void disable_interrupt() {
+static __inline void disable_interrupt() {
     __asm__ __volatile__("cli");
 }
+
+static __inline unsigned char inportb(unsigned short _port) {
+    unsigned char rv;
+    asm volatile("inb %1, %0"
+                 : "=a"(rv)
+                 : "dN"(_port));
+}
+
+static __inline void outportw(unsigned short _port, unsigned short _data) {
+    asm volatile("outw %1, %0"
+                            :
+                            : "dn"(_port), "a"(_data));
+}
+
+void cpuid(int code, uint32_t *a, uint32_t *b);
+const char *get_cpu_vender();
