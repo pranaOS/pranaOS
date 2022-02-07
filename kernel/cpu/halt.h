8d5804a978a56b4e8bc8a3448ca8fa3c4a1951e0
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-07 19:45:00 +0530

    kernel: cpu halt header

--- kernel/cpu/halt.h
+++ kernel/cpu/halt.h
@@ -10,32 +10,193 @@
 #include <stddef.h>
 #include <stdint.h>
 
+/**
+ * @brief io_wait
+ * 
+ * @return __inline 
+ */
 static __inline void io_wait() {
 	__asm__ __volatile__("outb %%al, $0x80"
 						 :
 						 : "a"(0));
 }
 
-static __inline void enable_interrupt() {
-    __asm__ __volatile__("sti");
+/**
+ * @brief enable_interrupts
+ * 
+ * @return __inline 
+ */
+static __inline void enable_interrupts() {
+	__asm__ __volatile__("sti");
 }
 
-static __inline void disable_interrupt() {
-    __asm__ __volatile__("cli");
+/**
+ * @brief disable_interrupts
+ * 
+ * @return __inline 
+ */
+static __inline void disable_interrupts() {
+	__asm__ __volatile__("cli");
 }
 
+/**
+ * @brief halt
+ * 
+ * @return __inline 
+ */
+static __inline void halt() {
+	__asm__ __volatile__("hlt");
+}
+
+/**
+ * @brief inportb
+ * 
+ * @param _port 
+ * @return __inline 
+ */
 static __inline unsigned char inportb(unsigned short _port) {
-    unsigned char rv;
-    asm volatile("inb %1, %0"
-                 : "=a"(rv)
-                 : "dN"(_port));
+	unsigned char rv;
+	asm volatile("inb %1, %0"
+				 : "=a"(rv)
+				 : "dN"(_port));
+	return rv;
 }
 
+/**
+ * @brief outportb
+ * 
+ * @param _port 
+ * @param _data 
+ * @return __inline 
+ */
+static __inline void outportb(unsigned short _port, unsigned char _data) {
+	asm volatile("outb %1, %0"
+				 :
+				 : "dN"(_port), "a"(_data));
+}
+
+/**
+ * @brief inportw
+ * 
+ * @param _port 
+ * @return __inline 
+ */
+static __inline unsigned short inportw(unsigned short _port) {
+	unsigned short rv;
+	asm volatile("inw %1, %0"
+				 : "=a"(rv)
+				 : "dN"(_port));
+	return rv;
+}
+
+/**
+ * @brief outportw
+ * 
+ * @param _port 
+ * @param _data 
+ * @return __inline 
+ */
 static __inline void outportw(unsigned short _port, unsigned short _data) {
-    asm volatile("outw %1, %0"
-                            :
-                            : "dn"(_port), "a"(_data));
+	asm volatile("outw %1, %0"
+				 :
+				 : "dN"(_port), "a"(_data));
 }
 
-void cpuid(int code, uint32_t *a, uint32_t *b);
+/**
+ * @brief inportl
+ * 
+ * @param _port 
+ * @return __inline 
+ */
+static __inline unsigned int inportl(unsigned short _port) {
+	unsigned int rv;
+	asm volatile("inl %%dx, %%eax"
+				 : "=a"(rv)
+				 : "dN"(_port));
+	return rv;
+}
+
+/**
+ * @brief outportl
+ * 
+ * @param _port 
+ * @param _data 
+ * @return __inline 
+ */
+static __inline void outportl(unsigned short _port, unsigned int _data) {
+	asm volatile("outl %%eax, %%dx"
+				 :
+				 : "dN"(_port), "a"(_data));
+}
+
+/**
+ * @brief inports
+ * 
+ * @param _port 
+ * @return __inline 
+ */
+static __inline unsigned short inports(unsigned short _port) {
+	unsigned short rv;
+	asm volatile("inw %1, %0"
+				 : "=a"(rv)
+				 : "dN"(_port));
+	return rv;
+}
+
+/**
+ * @brief outports
+ * 
+ * @param _port 
+ * @param _data 
+ * @return __inline 
+ */
+static __inline void outports(unsigned short _port, unsigned short _data) {
+	asm volatile("outw %1, %0"
+				 :
+				 : "dN"(_port), "a"(_data));
+}
+
+/**
+ * @brief inportsw
+ * 
+ * @param portid 
+ * @param addr 
+ * @param count 
+ * @return __inline 
+ */
+static __inline void inportsw(uint16_t portid, void *addr, size_t count) {
+	__asm__ __volatile__("rep insw"
+						 : "+D"(addr), "+c"(count)
+						 : "d"(portid)
+						 : "memory");
+}
+
+/**
+ * @brief outportsw
+ * 
+ * @param portid 
+ * @param addr 
+ * @param count 
+ * @return __inline 
+ */
+static __inline void outportsw(uint16_t portid, const void *addr, size_t count) {
+	__asm__ __volatile__("rep outsw"
+						 : "+S"(addr), "+c"(count)
+						 : "d"(portid));
+}
+
+/**
+ * @brief cpuid 
+ * 
+ * @param code 
+ * @param a 
+ * @param d 
+ */
+void cpuid(int code, uint32_t *a, uint32_t *d);
+
+/**
+ * @brief Get the cpu vender object
+ * 
+ * @return const char* 
+ */
 const char *get_cpu_vender();
