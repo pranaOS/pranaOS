fecdeefac378d901cfeb64fd41219655cb04ad74
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-08 19:50:40 +0530

    kernel: cpu gdtentry header

--- kernel/cpu/gdtentry.h
+++ kernel/cpu/gdtentry.h
@@ -1,28 +1,9 @@
+/*
+ * Copyright (c) 2021 - 2022, the pranaOS Developers & Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
 #pragma once
 
-#include <ak/types.h>
-
-using namespace ak;
-
-namespace Kernel {
-    struct gdtEntry {
-        ak::uint16_t limitLow;           
-        ak::uint16_t baseLow;            
-        ak::uint8_t  baseMiddle;         
-        ak::uint8_t  access;              
-        ak::uint8_t  granularity;
-        ak::uint8_t  baseHigh;           
-    } __attribute__((packed));
-
-    struct gdtPointer {
-        ak::uint16_t limit;               
-        ak::uint32_t base;                
-    } __attribute__((packed));
-
-    class globalDescriptorTable {        
-    public:
-        static void setDescriptor(int number, ak::uint32_t base, ak::uint32_t limit, ak::uint8_t access, ak::uint8_t gran);
-        static gdtEntry* getDescriptor(int number);
-        static void init();
-    };
-}
+#include <stdint.h>
