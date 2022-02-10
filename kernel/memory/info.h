5cb0fd9cc604b7797c0c39d87c9cb1aa874fb0f6
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-10 19:27:37 +0530

    kernel: memory info header

--- kernel/memory/info.h
+++ kernel/memory/info.h
@@ -3,3 +3,17 @@
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
+
+#pragma once
+
+#include <stdint.h>
+
+extern void *kernel_start;
+extern void *kernel_boot;
+extern void *kernel_higher_half;
+extern void *kernel_start;
+extern void *kernel_text_start;
+extern void *kernel_text_end;
+extern void *kernel_data_start;
+extern void *kernel_data_end;
+extern void *kernel_end;
