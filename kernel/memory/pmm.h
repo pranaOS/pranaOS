f26d12bc1d2132943d420863af6a5fe5fd438ba4
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-11 19:11:58 +0530

    kernel: memory pmm header

--- kernel/memory/pmm.h
+++ kernel/memory/pmm.h
@@ -24,3 +24,14 @@
  * 
  */
 #define PMM_FRAME_SIZE 4096
+
+#define PMM_FRAME_ALIGN PMM_FRAME_SIZE
+
+#define PAGE_MASK (~(PMM_FRAME_SIZE - 1))
+
+#define PAGE_ALIGN(addr) (((addr) + PMM_FRAME_SIZE - 1) & PAGE_MASK)
+
+void pmm_init(struct multiboot_tag_basic_meminfo *, struct multiboot_tag_mmap *);
+void *pmm_alloc_block();
+void *pmm_alloc_blocks(size_t num);
+uint32_t get_total_frames();
