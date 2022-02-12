eadeab3abe4e9776df2c2f0bd5001e162bc6f07e
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-12 18:47:24 +0530

    ak: limits header

--- ak/limits.h
+++ ak/limits.h
@@ -9,19 +9,19 @@
 #include <stdint.h>
 
 /**
- * @brief name max
+ * @brief name maximum
  * 
  */
 #define NAME_MAX 255
 
 /**
- * @brief path max
+ * @brief path maxmimum
  * 
  */
-#define PATH_MAX 4096 
+#define PATH_MAX 4096
 
 /**
- * @brief check maxpathlen + path max
+ * @brief check maximum path len + path max
  * 
  */
 #if !defined MAXPATHLEN && defined PATH_MAX
@@ -29,14 +29,45 @@
 #endif
 
 /**
- * @brief name maximum length
- * 
- */
-#define NAME_MAX 255
-
-/**
- * @brief int maximum and minimum rule
+ * @brief int max and minimum rule
  * 
  */
 #define INT_MAX ((int)(~0U >> 1))
-#define INT_MIN (~INT_MAX - 1)
+#define INT_MIN (-INT_MAX - 1)
+
+/**
+ * @brief uint max rule 
+ * 
+ */
+#define UINT_MAX (~0U)
+
+/**
+ * @brief [long] maximum and minimum
+ * 
+ */
+#define LONG_MAX ((long)(~0UL >> 1))
+#define LONG_MIN (-LONG_MAX - 1)
+
+/**
+ * @brief ulong max
+ * 
+ */
+#define ULONG_MAX (~0UL)
+
+/**
+ * @brief ushrt max
+ * 
+ */
+#define USHRT_MAX 65535
+
+/**
+ * @brief shrt max
+ * 
+ */
+#define SHRT_MAX 32767
+
+/**
+ * @brief ullong max
+ * 
+ */
+#define ULLONG_MAX (~0ULL)
