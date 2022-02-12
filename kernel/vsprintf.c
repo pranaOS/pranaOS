3401140589314791a800a47e31d610520eca6dee
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-12 18:57:09 +0530

    kernel: vsprintf

--- kernel/vsprintf.c
+++ kernel/vsprintf.c
@@ -44,3 +44,42 @@ size_t simple_strlen(const char *s, size_t count) {
 
     return sc - s;
 }
+
+/**
+ * @brief simple strtoul
+ * 
+ * @param cp 
+ * @param endp 
+ * @param base 
+ * @return unsigned long 
+ */
+unsigned long simple_strtoul(const char *cp, char **endp, unsigned int base) {
+	unsigned long result = 0, value;
+
+	if (!base) {
+		base = 10;
+		if (*cp == '0') {
+			base = 8;
+			cp++;
+			if ((toupper(*cp) == 'X') && isxdigit(cp[1]))
+			{
+				cp++;
+				base = 16;
+			}
+		}
+	}
+	else if (base == 16) {
+		if (cp[0] == '0' && toupper(cp[1]) == 'X')
+			cp += 2;
+	}
+	while (isxdigit(*cp) &&
+		   (value = isdigit(*cp) ? *cp - '0' : toupper(*cp) - 'A' + 10) < base) {
+		result = result * base + value;
+		cp++;
+	}
+
+
+	if (endp)
+		*endp = (char *)cp;
+	return result;
+}
