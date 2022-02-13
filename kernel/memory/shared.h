04b62ffcd5db71e2b27a9504754fc8f82f598dbd
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-13 12:27:42 +0530

    kernel: memory shared header

--- kernel/memory/shared.h
+++ kernel/memory/shared.h
@@ -1,6 +1,8 @@
-//
-// Created by KrisnaPranav on 13/02/22.
-//
+/*
+ * Copyright (c) 2021 - 2022, the pranaOS Developers & Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
 
 #pragma once
 
@@ -8,9 +10,38 @@
 #include <stdint.h>
 
 namespace Kernel {
-    class sharedMemory {
-      public:
-        static bool CreateSharedRegion(Process* proc1, Process* proc2, uint32_t virtStart, uint32_t len);
 
-    };
+  /**
+   * @brief shared memory class
+   * 
+   */
+  class sharedMemory {
+  public:
+
+    /**
+     * @brief Create a Shared Region object
+     * 
+     * @param proc1 
+     * @param proc2 
+     * @param virtStart 
+     * @param len 
+     * @return true 
+     * @return false 
+     */
+    static bool createSharedRegion(Process* proc1, Process* proc2, uint32_t virtStart, uint32_t len);
+    static bool createSharedRegion(Process* proc1, Process* proc2, uint32_t virtStart1, uint32_t virtStart2, uint32_t len);
+
+    /**
+     * @brief remove shared region object
+     * 
+     * @param proc1 
+     * @param proc2 
+     * @param virtStart 
+     * @param len 
+     * @return true 
+     * @return false 
+     */
+    static bool removeSharedRegion(Process* proc1, Process* proc2, uint32_t virtStart, uint32_t len);
+    static bool removeSharedRegion(Process* proc1, Process* proc2, uint32_t virtStart1, uint32_t virtStart2, uint32_t len);
+  };
 }
