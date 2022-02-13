708892ab5ca4fa6e2b2b09e7818d4b974b2d3d19
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-13 11:19:53 +0530

    kernel: memory vmm header

--- kernel/memory/vmm.h
+++ kernel/memory/vmm.h
@@ -55,3 +55,29 @@ enum PAGE_PTE_FLAGS {
 	I86_PTE_LV4_GLOBAL = 0x200,	   
 	I86_PTE_FRAME = 0x7FFFF000	   
 };
+
+
+/**
+ * @brief pt entry
+ * 
+ */
+typedef uint32_t pt_entry;
+
+/**
+ * @brief PDE[PRESENT, WRITABLE, USER, PWT, PCD, ACCESSED, DIRTY, 4MB, CPUGLOBAL, LV4GLOBAL, FRAME]
+ * 
+ */
+enum PAGE_PDE_FLAGS {
+
+	I86_PDE_PRESENT = 1,		 
+	I86_PDE_WRITABLE = 2,		 
+	I86_PDE_USER = 4,			 
+	I86_PDE_PWT = 8,			 
+	I86_PDE_PCD = 0x10,			 
+	I86_PDE_ACCESSED = 0x20,	 
+	I86_PDE_DIRTY = 0x40,		 
+	I86_PDE_4MB = 0x80,			 
+	I86_PDE_CPU_GLOBAL = 0x100,	 
+	I86_PDE_LV4_GLOBAL = 0x200,	 
+	I86_PDE_FRAME = 0x7FFFF000	 
+};
