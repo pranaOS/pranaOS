927c163e3a9d22abf69b8c5a379fddba0b2eca76
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-13 10:55:29 +0530

    kernel: queue header

--- kernel/queue.h
+++ kernel/queue.h
@@ -6,19 +6,20 @@
 
 #pragma once
 
-#include <listing/list.h>
+#include <include/list.h>
 #include <stdint.h>
 
 /**
- * @brief struct queue
+ * @brief queue struct 
  * 
  */
 struct queue {
-    struct list_head *qhead;
+	struct list_head *qhead;
+	uint32_t number_of_items;
 };
 
 /**
- * @brief queue push
+ * @brief queue push 
  * 
  * @param q 
  * @param data 
@@ -32,3 +33,11 @@ void queue_push(struct queue *q, void *data);
  * @return void* 
  */
 void *queue_pop(struct queue *q);
+
+/**
+ * @brief queue peek
+ * 
+ * @param q 
+ * @return void* 
+ */
+void *queue_peek(struct queue *q);
