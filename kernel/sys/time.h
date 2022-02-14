9f8a11fa2387541570f379ed8d7d652eb07acbac
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-14 18:59:32 +0530

    kernel: sys time header

--- kernel/sys/time.h
+++ kernel/sys/time.h
@@ -3,3 +3,64 @@
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
+
+#pragma once
+
+#include <sys/types.h>
+#include <stdint.h>
+
+/**
+ * @brief time spec
+ * 
+ */
+struct timespec {
+    time_t tv_sec;  
+    long tv_nsec;
+};
+
+/**
+ * @brief tms
+ * 
+ */
+struct tms {
+    clock_t tms_utime;
+    clock_t tms_stime;
+    clock_t tms_cutime;
+    clock_t tms_cstime;
+};
+
+/**
+ * @brief time value
+ * 
+ */
+struct timeval {
+    time_t tv_sec;
+    suseconds_t tv_usec;
+};
+
+/**
+ * @brief Set the boot seconds object
+ * 
+ * @param bs 
+ */
+void set_boot_seconds(uint64_t bs);
+
+/**
+ * @brief Set the current time object
+ * 
+ * @param year 
+ * @param month 
+ * @param day 
+ * @param hour 
+ * @param minute 
+ * @param second 
+ */
+void set_current_time(uint16_t year, uint8_t month, uint8_t day, uint8_t hour, uint8_t minute, uint8_t second);
+
+/**
+ * @brief Get the time object
+ * 
+ * @param seconds 
+ * @return struct time* 
+ */
+struct time *get_time(int32_t seconds);
