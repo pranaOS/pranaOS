c04f9e232501607d6df48c7c74431ab98c949ade
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-16 18:56:07 +0530

    libc: init header

--- userland/libraries/libc/init.h
+++ userland/libraries/libc/init.h
@@ -1,8 +1,47 @@
-//
-// Created by KrisnaPranav on 16/02/22.
-//
+/*
+ * Copyright (c) 2021 - 2022, the pranaOS Developers & Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
 
 #pragma once
 
 #include <stdint.h>
 #include <sys/types.h>
+
+#ifndef __wchar_t_defined
+#define __wchar_t_defined
+#define __need_wchar_t
+#endif
+
+typedef struct {
+    intmax_t quot;
+    intmax_t rem;
+} imaxdiv_t;
+
+/**
+ * @brief imaxabs
+ * 
+ * @param val 
+ * @return intmax_t 
+ */
+intmax_t imaxabs(intmax_t val);
+
+/**
+ * @brief imaxdiv
+ * 
+ * @param numer 
+ * @param denom 
+ * @return imaxdiv_t 
+ */
+imaxdiv_t imaxdiv(intmax_t numer, intmax_t denom);
+
+/**
+ * @brief strtoimax
+ * 
+ * @param nptr 
+ * @param endptr 
+ * @param base 
+ * @return intmax_t 
+ */
+intmax_t strtoimax(const char *restrict nptr, char** endptr, int base);
