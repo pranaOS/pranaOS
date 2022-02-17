57066cc334f06ad709e71f645122c846837ee2f4
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-17 18:43:28 +0530

    kernel: utilities math header

--- kernel/utilities/math.h
+++ kernel/utilities/math.h
@@ -9,22 +9,100 @@
 #include <stdint.h>
 
 /**
- * @brief align down
+ * @brief ALIGN DOWN 
  * 
  */
 #define ALIGN_DOWN(base, size) ((base) & -((__typeof__(base))(size)))
 
 /**
- * @brief rand
+ * @brief align up
+ * 
+ */
+#define ALIGN_UP(base, size) ALIGN_DOWN((base) + (size)-1, (size))
+
+/**
+ * @brief ptr align down
+ * 
+ */
+#define PTR_ALIGN_DOWN(base, size) \
+	((__typeof__(base))ALIGN_DOWN((uintptr_t)(base), (size)))
+
+/**
+ * @brief ptr align up
+ * 
+ */
+#define PTR_ALIGN_UP(base, size) \
+	((__typeof__(base))ALIGN_UP((uintptr_t)(base), (size)))
+
+/**
+ * @brief log2
+ * 
+ */
+#define log2(X) ((unsigned)(8 * sizeof(unsigned long long) - __builtin_clzll((X)) - 1))
+
+/**
+ * @brief min
+ * 
+ */
+#define min(x, y) ({ \
+	typeof(x) _x = (x);	\
+	typeof(y) _y = (y);	\
+	(void) (&_x == &_y);		\
+	_x < _y ? _x : _y; })
+
+/**
+ * @brief max
+ * 
+ */
+#define max(x, y) ({ \
+	typeof(x) _x = (x);	\
+	typeof(y) _y = (y);	\
+	(void) (&_x == &_y);		\
+	_x > _y ? _x : _y; })
+
+/**
+ * @brief min_t
+ * 
+ */
+#define min_t(type, x, y) \
+	({ type __x = (x); type __y = (y); __x < __y ? __x: __y; })
+
+/**
+ * @brief max_t 
+ * 
+ */
+#define max_t(type, x, y) \
+	({ type __x = (x); type __y = (y); __x > __y ? __x: __y; })
+
+/**
+ * @brief div ceil
+ * 
+ */
+#define div_ceil(x, y) ({__auto_type _x = (x); __auto_type _y = (y); _x / _y + (_x % _y > 0); })
+
+/**
+ * @brief round
+ * 
+ */
+#define round(x, y) ({__auto_type _x = (x); __auto_type _y = (y); (_x / _y) * _y ; })
+
+/**
+ * @brief abs
+ * 
+ */
+#define abs(x) ({__auto_type _x = (x); _x >=0 ? _x : -_x; })
+
+/**
+ * @brief rand 
  * 
  * @return uint32_t 
  */
 uint32_t rand();
 
 /**
- * @brief strand
+ * @brief srand
  * 
  * @param seed 
  * @return uint32_t 
  */
-uint32_t strand(uint32_t seed);
+uint32_t srand(uint32_t seed);
