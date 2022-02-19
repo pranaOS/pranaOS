08e45de8e595fa52dd16d73c6fd503b2b5bc8fe8
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-19 10:42:12 +0530

    kernel: utilities queue

--- kernel/utilities/queue.c
+++ kernel/utilities/queue.c
@@ -4,25 +4,4 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-
-#include <listing/list.h>
-#include <memory/vmm.h>
 #include "queue.h"
-
-struct qelement
-{
-    void *data;
-    struct list_head sibling;
-};
-
-/**
- * @brief queue push 
- * 
- * @param q 
- * @param data 
- */
-void queue_push(struct queue *q, void *data) {
-	struct qelement *qe = kcalloc(1, sizeof(struct qelement));
-	qe->data = data;
-	list_add_tail(&qe->sibling, q->qhead);
-}
