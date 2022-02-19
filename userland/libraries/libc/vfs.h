7fbe39e85646d89d42db1b92727c1e054f80dcf3
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-19 13:17:22 +0530

    userland: libc vfs header

--- userland/libraries/libc/vfs.h
+++ userland/libraries/libc/vfs.h
@@ -1,12 +1,14 @@
 #pragma once
 
 #include <types.h>
-#include "list/list.h"
+#include <list/list.h>
 #include <shared.h>
 
+using namespace LibC;
+
 namespace LibC {
-    int readFile(char* filename, uint8_t* buffer, uint32_t offset = 0, uint32_t len = -1);
-    int writeFile(char* filename, uint8_t* buffer, uint32_t len, bool create = true);
+    int readFile(char* filename, ak::uint8_t* buffer, ak::uint32_t offset = 0, ak::uint32_t len = -1);
+    int writeFile(char* filename, ak::uint8_t* buffer, ak::uint32_t len, bool create = true);
 
     bool fileExists(char* filename);
     bool dirExists(char* filename);
@@ -14,8 +16,8 @@ namespace LibC {
     int createFile(char* path);
     int createDirectory(char* path);
 
-    uint32_t getFileSize(char* filename);
-    List<vfsEntry> dirListing(char* path);
+    ak::uint32_t getFileSize(char* filename);
+    list<vfsEntry> dirListing(char* path);
 
     bool ejectDisk(char* path);
 }
