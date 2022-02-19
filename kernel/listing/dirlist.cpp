e70e2bf0a006167a6aa7d8faa19ece23b273d29e
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-19 13:16:43 +0530

    kernel: listing dirlist cpp

---
+++ kernel/listing/dirlist.cpp
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2021 - 2022, the pranaOS Developers & Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <listing/dirlist.h>
+
+using namespace Kernel;
