9d04587c6bf485680f78dd6b14acdf573349a2a5
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-19 13:43:46 +0530

    kernel: listing dirlist cpp

--- kernel/listing/dirlist.cpp
+++ kernel/listing/dirlist.cpp
@@ -7,3 +7,65 @@
 #include <listing/dirlist.h>
 
 using namespace Kernel;
+using namespace LibC;
+
+ak::List<vfsEntry>* currentDirList = 0;
+
+/**
+ * @brief Construct a new dir List::dir List object
+ * 
+ */
+dirList::dirList()
+: controller() { }
+
+/**
+ * @brief begin listing 
+ * 
+ * @param thread 
+ * @param pathPtr 
+ * @return int 
+ */
+int dirList::beginListing(Thread* thread, ak::uint32_t pathPtr) {
+    char* path = (char*)pathPtr;
+    if(requestBusy) {
+        waitingQueue.push_back(thread);
+    }
+
+    requestBusy = true;
+    currentReqThread = thread;
+
+    return currentDirList->size();
+}
+
+/**
+ * @brief get entry object
+ * 
+ * @param thread 
+ * @param entry 
+ * @param bufPtr 
+ * @return int 
+ */
+int dirList::getEntry(Thread* thread, int entry, ak::uint32_t bufPtr) {
+    char* buf = (char*)bufPtr;
+
+    if (currentReqThread != thread) {
+        return 0;
+    }
+
+    return 0;
+}
+
+/**
+ * @brief end listing
+ * 
+ * @param thread 
+ */
+void dirList::endListing(Thread* thread) {
+    if(currentReqThread != thread || currentReqThread == 0) {
+        return;
+    }
+
+    requestBusy = false;
+    currentReqThread = 0;
+    delete currentDirList;
+}
