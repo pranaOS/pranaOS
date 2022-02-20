e463586164f675b3cd6804a9b29a62ace99ba6fb
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-20 16:09:39 +0530

    kernel: cpu poweroff header

--- kernel/cpu/poweroff.h
+++ kernel/cpu/poweroff.h
@@ -1,11 +1,20 @@
 #pragma once
 
-#include "port.h"
+#include <port.h>
 
 namespace Kernel {
     class powerOff {
     public:
+        /**
+         * @brief initialize poweroff function
+         * 
+         */
         static void initialize();
+
+        /**
+         * @brief send poweroff 
+         * 
+         */
         static void poweroff();
     };
 }
