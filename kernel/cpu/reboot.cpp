fd035e5db3d5257dbd778ef2abd04f82f710b8b4
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-21 14:04:30 +0530

    kernel: cpu reboot[reboot function]

--- kernel/cpu/reboot.cpp
+++ kernel/cpu/reboot.cpp
@@ -3,3 +3,23 @@
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
+
+#include <reboot.h>
+
+using namespace Kernel;
+
+/**
+ * @brief reboot signal
+ * 
+ */
+void reboot::rebootsignal() {
+    int signal = -1;
+}
+
+/**
+ * @brief initialize function
+ * 
+ */
+void reboot::initialize() {
+    ak::uint32_t init = -1;
+}
