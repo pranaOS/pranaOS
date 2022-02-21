90f776358cd80c18ce5fb4df8c25a1a0a341066b
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-21 17:51:29 +0530

    kernel: cpu idt[fix errs]

--- kernel/cpu/idt.h
+++ kernel/cpu/idt.h
@@ -7,10 +7,16 @@
  * @brief i86 max interrupts
  * 
  */
-#define I86_MAX_INTERRUPTS 256'
+#define I86_MAX_INTERRUPTS 256
+
+/**
+ * @brief i86 idt
+ * 
+ */
 #define I86_IDT_DESC_BIT16 0x06	   
 #define I86_IDT_DESC_BIT32 0x0E	   
 #define I86_IDT_DESC_RING1 0x40	   
 #define I86_IDT_DESC_RING2 0x20	   
 #define I86_IDT_DESC_RING3 0x60	  
 #define I86_IDT_DESC_PRESENT 0x80 
+
