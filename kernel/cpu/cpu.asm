df4fbd5cb2b3fab83dddf43bdfc7798d66360cea
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-21 17:44:27 +0530

    kernel: cpu[cpu asm]

--- kernel/cpu/cpu.asm
+++ kernel/cpu/cpu.asm
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2021 - 2022, the pranaOS Developers & Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
 [global gdt_flush]    
 gdt_flush:
     mov eax, [esp+4]  
