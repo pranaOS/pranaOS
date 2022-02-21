0a5a6da17fdb42cf33073aada276fac7b4919b23
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-21 17:42:54 +0530

    kernel: cpu[exception]

---
+++ kernel/cpu/exception.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021 - 2022, the pranaOS Developers & Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <stdarg.h>
+#include <stdint.h>
+
+void exception_init();
