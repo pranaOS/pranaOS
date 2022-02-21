ea18601243370c6263f5390f12f7fead29876064
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-21 13:56:15 +0530

    kernel: cpu reboot header

--- kernel/cpu/reboot.h
+++ kernel/cpu/reboot.h
@@ -1,11 +1,26 @@
+/*
+ * Copyright (c) 2021 - 2022, the pranaOS Developers & Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
 #pragma once
 
-#include "port.h"
+#include <port.h>
 
 namespace Kernel {
     class reboot {
     public:
+        /**
+         * @brief initialize function
+         * 
+         */
         static void initialize();
-        static void reboot();
+
+        /**
+         * @brief send reboot signal
+         * 
+         */
+        static void rebootsignal();
     };
 }
