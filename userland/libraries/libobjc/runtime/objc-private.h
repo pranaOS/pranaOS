2370974cccbc5299ef86505b6bbb14838a96e74a
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-25 19:47:37 +0530

    libobjc: runtime[objc-private(id, class, isa_t)]

--- userland/libraries/libobjc/runtime/objc-private.h
+++ userland/libraries/libobjc/runtime/objc-private.h
@@ -8,7 +8,53 @@
 #define _objc_private_h_
 
 #ifndef _OBJC_OBJC_H
-// todo.
+#error include objc-private.h
 #endif
+
+#define OBJC_TTYPES_DEFIEND 1
+#define OBJC_OLD_DISPATCH_PROTOTYPES 1
+
+#include <stdint.h>
+#include <assert.h>
+
+/**
+ * @brief objc class
+ * 
+ */
+struct objc_class;
+
+/**
+ * @brief objc object
+ * 
+ */
+struct objc_object;
+
+/**
+ * @brief objc class
+ * 
+ */
+typedef struct objc_class *Class;
+
+/**
+ * @brief objc object
+ * 
+ */
+typedef struct objc_object *id;
+
+/**
+ * @brief side table 
+ * 
+ */
+namespace {
+    struct SideTable;
+};
+
+/**
+ * @brief isa_t union func
+ * 
+ */
+union isa_t {
+
+};
 
 #endif /* objc_private_h */
