b51eb46d7504462173073dcbbb1fd3f7baf148bd
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-25 19:38:45 +0530

    libobjc: private header[contains isa, id classess]

---
+++ userland/libraries/libobjc/runtime/objc-private.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021 - 2022, the pranaOS Developers & Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef _objc_private_h_
+#define _objc_private_h_
+
+#ifndef _OBJC_OBJC_H
+// todo..
+#endif
+
+#endif 
