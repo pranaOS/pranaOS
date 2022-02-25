f757aec55b2540a184dc007361b0c96acfe6b209
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-25 19:52:04 +0530

    libobjc: runtime[runtime impl]

--- userland/libraries/libobjc/runtime/runtime.h
+++ userland/libraries/libobjc/runtime/runtime.h
@@ -1,21 +1,51 @@
-//
-//  runtime.h
-//  libobjc
-//
-//  Created by KrisnaPranav on 25/02/22.
-//
+/*
+ * Copyright (c) 2021 - 2022, the pranaOS Developers & Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
 
 #ifndef _runtime_h_
 #define _runtime_h_
 
+/**
+ * @brief objc class
+ * 
+ */
 typedef struct objc_class *Class;
+
+/**
+ * @brief objc object
+ * 
+ */
 typedef struct objc_object *id;
 
+/**
+ * @brief objc selector
+ * 
+ */
 typedef struct objc_selector *SEL;
 
-#ifndef __cplusplus
+#ifdef __cplusplus
+extern "C" {
+#endif
+    /**
+     * @brief object get class
+     * 
+     * @param object 
+     * @return Class 
+     */
+    Class object_getClass(id object);
+
+    /**
+     * @brief objc get class
+     * 
+     * @param aClassName 
+     * @return Class 
+     */
+    Class objc_getClass(const char *aClassName);
+    
+#ifdef __cplusplus
+}
 
 #endif
-
-
 #endif /* runtime_h */
