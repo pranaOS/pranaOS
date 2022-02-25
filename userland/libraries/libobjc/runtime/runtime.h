efff81b8a851cf0222a7bf1b711c332e9cc4f34f
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-25 19:50:54 +0530

    libobjc: runtime[runtime header(class, id)]

---
+++ userland/libraries/libobjc/runtime/runtime.h
@@ -0,0 +1,21 @@
+//
+//  runtime.h
+//  libobjc
+//
+//  Created by KrisnaPranav on 25/02/22.
+//
+
+#ifndef _runtime_h_
+#define _runtime_h_
+
+typedef struct objc_class *Class;
+typedef struct objc_object *id;
+
+typedef struct objc_selector *SEL;
+
+#ifndef __cplusplus
+
+#endif
+
+
+#endif /* runtime_h */
