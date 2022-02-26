788d680a40e975228aed92f213a5c4da8ca9b114
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-26 18:54:24 +0530

    libcpp: osruntime[extern c block]

--- userland/libraries/libcpp/osruntime.cpp
+++ userland/libraries/libcpp/osruntime.cpp
@@ -1,8 +1,17 @@
-//
-//  osruntime.cpp
-//  libcpp
-//
-//  Created by KrisnaPranav on 26/02/22.
-//
+/*
+ * Copyright (c) 2021 - 2022, the pranaOS Developers & Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
 
+#include "osruntime.h"
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
 
+extern "C" {
+    void* kalloc_(uint32_t size);
+    void free_(void* data, uint32_t size);
+    void panic(const char* fmt, ...);
+}
