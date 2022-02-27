2155baba38ed973dfe94421460f6a21a8977de26
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-27 10:41:38 +0530

    libobjc: runtime[objc]

---
+++ userland/libraries/libobjc/runtime/objc.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021 - 2022, the pranaOS Developers & Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef objc_h
+#define objc_h
+
+
+#endif /* objc_h */
