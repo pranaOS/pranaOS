e00a2683013f2423ca59d954edda17849b7fd1b8
Krisna Pranav <krisna.pranav@gmail.com>
2022-03-02 19:24:58 +0530

    libobjc: for loop

--- userland/libraries/libobjc/for.m
+++ userland/libraries/libobjc/for.m
@@ -1,8 +1,14 @@
+/*
+ * Copyright (c) 2021 - 2022, the pranaOS Developers & Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
 #import <Foundation/Foundation.h>
 
 int main() {
     for(;;) {
-        NSLog(@"This Log Will Run Forever\n");
+        NSLog(@"YEEE\n");
     }
 
     return 0;
