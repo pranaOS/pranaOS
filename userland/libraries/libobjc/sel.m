39b83e42bca67674f70d556bdd44a9cb66cd3642
Krisna Pranav <krisna.pranav@gmail.com>
2022-03-02 18:51:49 +0530

    libobjc: sel[sel_alloc, registerName]

--- userland/libraries/libobjc/sel.m
+++ userland/libraries/libobjc/sel.m
@@ -7,8 +7,7 @@
 #include "runtime/objc.h"
 #include <string.h>
 
-const char * _Nonnull
-set_getName(SEL _Nonnull sel) {
+const char * _Nonnull set_getname(SEL _Nonnull sel) {
     if (!sel) {
         return "<null selector>";
     }
@@ -16,6 +15,15 @@ set_getName(SEL _Nonnull sel) {
     return (const char *)(const void*)sel;
 }
 
-BOOL sel_isEqual(SEL lhs, SEL rhs) {
+BOOL sel_isequal(SEL lhs, SEL rhs) {
     return lhs == rhs;
 }
+
+static SEL sel_alloc(const char *name, BOOL copy) {
+    //
+}
+
+SEL sel_registername(const char *name) {
+    return sel_alloc(name, YES);
+}
+
