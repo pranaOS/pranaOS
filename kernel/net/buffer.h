c9e5d2484cf2a2af8ea6a0ce4619633faca1b22d
Krisna Pranav <krisna.pranav@gmail.com>
2022-03-03 18:56:05 +0530

    kernel: net buffer[udp, tcp, icmp, ip4, arp, ethernet]

---
+++ kernel/net/buffer.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2021 - 2022, the pranaOS Developers & Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <listing/list.h>
+#include <stdint.h>
+
+/**
+ * @brief udp packet
+ * 
+ */
+struct udp_packet;
+
+/**
+ * @brief tcp_packet
+ * 
+ */
+struct tcp_packet;
+
+/**
+ * @brief icmp packet
+ * 
+ */
+struct icmp_packet;
+
+/**
+ * @brief ip4 packet
+ * 
+ */
+struct ip4_packet;
+
+/**
+ * @brief arp packet
+ * 
+ */
+struct arp_packet;
+
+/**
+ * @brief ethernet packet
+ * 
+ */
+struct ethernet_packet;
