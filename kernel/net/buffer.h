97cfc38706e39d182b9455a9f567b6bf2688798e
Krisna Pranav <krisna.pranav@gmail.com>
2022-03-03 19:07:15 +0530

    kernel: net buffer header

--- kernel/net/buffer.h
+++ kernel/net/buffer.h
@@ -50,12 +50,35 @@ struct ethernet_packet;
  * 
  */
 struct sk_buff {
+    /**
+     * @brief socket
+     * 
+     */
     struct sock *sk;
+
+    /**
+     * @brief net device
+     * 
+     */
     struct net_device *dev;
+
+    /**
+     * @brief list head
+     * 
+     */
     struct list_head sibling;
 
-    uint32_t len, true_size;
+    /**
+     * @brief length, true_size
+     * 
+     */
+    uint32_t len;
+    uint32_t true_size;
 
+    /**
+     * @brief udp packet, tcp packet, icmp packet
+     * 
+     */
     union {
         struct udp_packet *udpht;
         struct tcp_packet *tcpht;
@@ -63,17 +86,29 @@ struct sk_buff {
 
     } t; 
 
+    /**
+     * @brief nt[ipv4 packet, arp packet]
+     * 
+     */
     union {
         struct ip4_packet *ipnt;
         struct arp_packet *arpnt;
         uint8_t *raw;
     } nt;
 
+    /**
+     * @brief mac addr 
+     * 
+     */
     union {
         struct ethernet_packet *eh;
         uint8_t *raw;
     } mac;
 
+    /**
+     * @brief head, data, tail, end
+     * 
+     */
     uint8_t *head;
     uint8_t *data;
     uint8_t *tail;
@@ -103,3 +138,49 @@ static inline void skb_put(struct sk_buff *skb, uint32_t len) {
     skb->tail += len;
     skb->len += len;
 }
+
+/**
+ * @brief skb push
+ * 
+ * @param skb 
+ * @param len 
+ */
+static inline void skb_push(struct sk_buff *skb, uint32_t len) {
+	skb->data -= len;
+	skb->len += len;
+}
+
+/**
+ * @brief skb pull
+ * 
+ * @param skb 
+ * @param len 
+ */
+static inline void skb_pull(struct sk_buff *skb, uint32_t len) {
+	skb->data += len;
+	skb->len -= len;
+}
+
+/**
+ * @brief skb allocation
+ * 
+ * @param header_size 
+ * @param payload_size 
+ * @return struct sk_buff* 
+ */
+struct sk_buff *skb_alloc(uint32_t header_size, uint32_t payload_size);
+
+/**
+ * @brief skb clone 
+ * 
+ * @param skb 
+ * @return struct sk_buff* 
+ */
+struct sk_buff *skb_clone(struct sk_buff *skb);
+
+/**
+ * @brief skb free
+ * 
+ * @param skb 
+ */
+void skb_free(struct sk_buff *skb);
