deb2dd4feeaa08d3cdd1d51fd8146b5c0e2eaccd
Krisna Pranav <krisna.pranav@gmail.com>
2022-03-04 19:05:13 +0530

    kernel: proc wait[define]

--- kernel/proc/wait.h
+++ kernel/proc/wait.h
@@ -10,6 +10,30 @@
 #include <utilities/list.h>
 #include <libc/sys/types.h>
 
+#define WNOHANG 0x00000001
+#define WUNTRACED 0x00000002
+#define WSTOPPED WUNTRACED
+#define WEXITED 0x00000004
+#define WCONTINUED 0x00000008
+#define WNOWAIT 0x01000000 
+
+#define CLD_EXITED 1	
+#define CLD_KILLED 2	
+#define CLD_DUMPED 3	
+#define CLD_TRAPPED 4	
+#define CLD_STOPPED 5	
+#define CLD_CONTINUED 6 
+
+#define P_ALL 0
+#define P_PID 1
+#define P_PGID 2
+
+#define WSEXITED 0x0001
+#define WSSIGNALED 0x0002
+#define WSCOREDUMP 0x0004
+#define WSSTOPPED 0x0008
+#define WSCONTINUED 0x0010
+
 /**
  * @brief infop
  * 
