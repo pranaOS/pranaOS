c9e4229e12ade1b609da66817250964b04ecb4a6
Krisna Pranav <krisna.pranav@gmail.com>
2022-03-05 18:32:03 +0530

    kernel: cpu rtc[rtc(init + get_datetime)]

--- kernel/cpu/rtc.h
+++ kernel/cpu/rtc.h
@@ -5,3 +5,23 @@
  */
 
 #pragma once
+
+#include <stdint.h>
+
+/**
+ * @brief rtc[get datetime]
+ * 
+ * @param year 
+ * @param month 
+ * @param day 
+ * @param hour 
+ * @param minute 
+ * @param second 
+ */
+void rtc_get_datetime(uint16_t *year, uint8_t *month, uint8_t *day, uint8_t *hour, uint8_t *minute, uint8_t *second);
+
+/**
+ * @brief init function
+ * 
+ */
+void rtc_init();
