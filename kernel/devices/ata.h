d5c128dff036d842b9acf917781cb34528f46b23
Krisna Pranav <krisna.pranav@gmail.com>
2022-03-06 12:56:36 +0530

    kernel: devices ata[read, init]

--- kernel/devices/ata.h
+++ kernel/devices/ata.h
@@ -27,3 +27,54 @@
 #define ATA_IDENTIFY_ERR 0
 #define ATA_IDENTIFY_SUCCESS 1
 #define ATA_IDENTIFY_NOT_FOUND 2
+
+/**
+ * @brief ata device
+ * 
+ */
+struct ata_device {
+
+    /**
+     * @brief io base + associated io base
+     * 
+     */
+    uint16_t io_base;
+    uint16_t associated_io_base;
+
+    /**
+     * @brief irq 
+     * 
+     */
+    uint8_t irq;
+
+    /**
+     * @brief dev name
+     * 
+     */
+    char *dev_name;
+
+    /**
+     * @brief is master, harddisk
+     * 
+     */
+    bool is_master;
+    bool is_harddisk;
+};
+
+/**
+ * @brief ata read
+ * 
+ * @param device 
+ * @param lba 
+ * @param n_sectors 
+ * @param buffer 
+ * @return int8_t 
+ */
+int8_t ata_read(struct ata_device *device, uint32_t lba, uint32_t n_sectors, uint16_t *buffer);
+
+/**
+ * @brief initialize function
+ * 
+ * @return uint8_t 
+ */
+uint8_t ata_init();
