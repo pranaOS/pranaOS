7ed165f53503f85217870c25c0c0141d0aebe171
Krisna Pranav <krisna.pranav@gmail.com>
2022-03-06 16:35:00 +0530

    libobjc: runtime

--- userland/libraries/libobjc/runtime.m
+++ userland/libraries/libobjc/runtime.m
@@ -5,44 +5,42 @@
 //  Created by KrisnaPranav on 06/03/22.
 //
 
-#include "runtime/NSObject.h"
-#include "runtime/runtime.h"
-#include "runtime/objc.h"
+#import "runtime/NSObject.h"
+#import "runtime/runtime.h"
+#import "runtime/objc.h"
 
 void objc_release(id);
-
 id objc_autorelease(id);
 id objc_retain(id);
 
 /**
- * @brief objc autorelease
+ * @brief objc retain
  */
 id objc_retain_autorelease(id obj) {
     return objc_autorelease(objc_retain(obj));
 }
 
 /**
- * @brief retain autorelease
+ * @brief objc retain autorelease
  */
 id objc_retainAutorelease(id obj) {
     return objc_retain_autorelease(obj);
 }
 
 __attribute__((noinline))
-static id objc_retainAutoreleaseandReturn(id obj) {
+static id objc_retainAutoreleaseAndReturn(id obj) {
     return objc_retainAutorelease(obj);
 }
 
-
 /**
- * @brief: retian autorelease and returns value
+ * @brief objc retain
  */
-id objc_retainAutoreleaseReturnvalue(id obj) {
-    return objc_retainAutoreleaseandReturn(obj);
+id objc_retainAutoreleaseReturnValue(id obj) {
+    return objc_retainAutoreleaseAndReturn(obj);
 }
 
 /**
- * @brief objc store
+ @brief objc store
  */
 void objc_storeStrong(id *location, id obj) {
     id prev = *location;
@@ -55,9 +53,59 @@ void objc_storeStrong(id *location, id obj) {
     objc_release(prev);
 }
 
+/**
+ * @brief objc alloc
+ */
 id objc_alloc(Class cls) {
     if (!cls) {
         return Nil;
     }
     return [cls alloc];
+}
+
+/**
+ * @brief objc alloc with zone
+ */
+id objc_allocWithZone(Class cls) {
+    if (!cls) {
+        return Nil;
+    }
+    return [cls alloc];
+}
+
+/**
+ * @brief objc storeweak
+ */
+id objc_storeWeak(id *location, id obj) {
+    *location = obj;
+    return obj;
+}
+
+/**
+ * @brief objc get property
+ */
+void objc_getProperty() {
+    
+}
+
+/**
+ * @brief objc init week
+ */
+id objc_initWeak(id *location, id newObj) {
+    if (!newObj) {
+        *location = nil;
+        return nil;
+    }
+    *location = [[newObj alloc] init];
+    return newObj;
+}
+
+/**
+ * @brief objc get class
+ */
+Class objc_getClass(const char* aClassName) {
+    if (!aClassName) {
+        return Nil;
+    }
+    return objc_lookUpClass(aClassName);
 }
