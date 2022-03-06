60e57a2fab849d73ba7b06840ad1921e8e7f9b7d
Krisna Pranav <krisna.pranav@gmail.com>
2022-03-06 13:13:02 +0530

    libobjc: runtime

---
+++ userland/libraries/libobjc/runtime.m
@@ -0,0 +1,14 @@
+//
+//  runtime.m
+//  libobjc
+//
+//  Created by KrisnaPranav on 06/03/22.
+//
+
+#include "runtime/NSObject.h"
+#include "runtime/runtime.h"
+#include "runtime/objc.h"
+
+void objc_release(id);
+
+id objc_autorelease(id);
