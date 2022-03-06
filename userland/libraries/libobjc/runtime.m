22104a573d58e1c0cad287f82fd5771406bbf487
Krisna Pranav <krisna.pranav@gmail.com>
2022-03-06 16:28:12 +0530

    libobjc: runtime[autorelease]

--- userland/libraries/libobjc/runtime.m
+++ userland/libraries/libobjc/runtime.m
@@ -14,6 +14,29 @@ void objc_release(id);
 id objc_autorelease(id);
 id objc_retain(id);
 
+/**
+ * @brief objc autorelease
+ */
 id objc_retain_autorelease(id obj) {
     return objc_autorelease(objc_retain(obj));
+}
+
+/**
+ * @brief retain autorelease
+ */
+id objc_retainAutorelease(id obj) {
+    return objc_retain_autorelease(obj);
+}
+
+__attribute__((noinline))
+static id objc_retainAutoreleaseandReturn(id obj) {
+    return objc_retainAutorelease(obj);
+}
+
+
+/**
+ * @brief: retian autorelease and returns value
+ */
+id objc_retainAutoreleaseReturnvalue(id obj) {
+    return objc_retainAutoreleaseandReturn(obj);
 }
