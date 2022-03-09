7fb90a5512c7f67ba4339b9bc2e2cf5a8a4d8bbb
Krisna Pranav <krisna.pranav@gmail.com>
2022-03-09 15:14:59 +0530

    objc++, runtime-private

---
+++ userland/libraries/libobjc/objc++.mm
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2021 - 2022, the pranaOS Developers & Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include "runtime/objc-private.h"
+
---
+++ userland/libraries/libobjc/runtime/runtime-private.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021 - 2022, the pranaOS Developers & Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef runtime_private_h
+#define runtime_private_h
+
+
+#endif /* runtime_private_h */
