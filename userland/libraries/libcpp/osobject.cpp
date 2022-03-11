c04051c7c40bcab9c193f3a2c114ca3367d0fc5f
Krisna Pranav <krisna.pranav@gmail.com>
2022-03-11 19:10:14 +0530

    libcpp: osobject[hpp + cpp]

---
+++ userland/libraries/libcpp/osobject.cpp
@@ -0,0 +1,8 @@
+//
+//  osobject.cpp
+//  libcpp
+//
+//  Created by KrisnaPranav on 11/03/22.
+//
+
+#include "osobject.hpp"
---
+++ userland/libraries/libcpp/osobject.hpp
@@ -0,0 +1,13 @@
+//
+//  osobject.hpp
+//  libcpp
+//
+//  Created by KrisnaPranav on 11/03/22.
+//
+
+#ifndef osobject_hpp
+#define osobject_hpp
+
+#include <stdio.h>
+
+#endif /* osobject_hpp */
