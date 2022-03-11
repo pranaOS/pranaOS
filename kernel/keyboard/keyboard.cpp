0e927cbaf84aeafb8ac9610843e6fcf2304c1dcd
Krisna Pranav <krisna.pranav@gmail.com>
2022-03-11 19:37:13 +0530

    kerenl: keyboard

--- kernel/keyboard/keyboard.cpp
+++ kernel/keyboard/keyboard.cpp
@@ -1,1 +1,13 @@
-// 
+#include "keyboard.h"
+
+
+using namespace Kernel;
+
+struct keyboard_status {
+    unsigned char A : 1;
+    unsigned char B : 2;
+    unsigned char C : 3;
+} keyboard_status_t;
+
+void keyboardStatus() {
+}
--- kernel/keyboard/keyboard.h
+++ kernel/keyboard/keyboard.h
@@ -2,3 +2,11 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+
+namespace Kernel {
+    class keyboard {
+      public:
+        static void alphabet_a();
+
+    };
+}
