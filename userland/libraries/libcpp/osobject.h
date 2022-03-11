307e0a0e08bcca7483d66392bbb5db0d0825c084
Krisna Pranav <krisna.pranav@gmail.com>
2022-03-11 19:16:03 +0530

    libcpp: osobject[list_append_elem, list_append]

--- userland/libraries/libcpp/osobject.h
+++ userland/libraries/libcpp/osobject.h
@@ -27,3 +27,32 @@ typedef struct {
     delement_t* head;
     delement_t* tail;
 } list_t;
+
+#define list_init() {0, 0}
+
+/**
+ * @brief list append elem
+ */
+static inline void list_append_elem(list_t* list, delement_t* elem) {
+    elem->next = 0;
+    elem->prev = list->tail;
+    if (list->head == 0) {
+        list->head = elem;
+    } else {
+        list->tail->next = elem;
+    }
+    list->tail = elem;
+}
+
+/**
+ * @brief list append
+ */
+static inline delement_t* list_append(list_t* list, void* data) {
+    delement_t* newElement = new delement_t();
+    if (newElement) {
+        newElement->data = data;
+        list_append_elem(list, newElement);
+    }
+    
+    return newElement;
+}
