dea4008320fda53fa70b9a8c2f83c9ff4d46fb51
Krisna Pranav <krisna.pranav@gmail.com>
2022-03-19 19:18:18 +0530

    kernel: kernel xcode proj

---
+++ kernel/kernel/kernel/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  kernel
+//
+//  Created by KrisnaPranav on 19/03/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
