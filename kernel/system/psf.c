e5573fd45f0553a70f97846879a56a832252b683
Krisna Pranav <krisna.pranav@gmail.com>
2022-03-19 19:13:30 +0530

    kernel: system psf

--- kernel/system/psf.c
+++ kernel/system/psf.c
@@ -4,4 +4,25 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <ak/limits.h>
+#include <memory/vmm.h>
+#include <libkernel/math.h>
+#include <libkernel/string.h>
 #include "psf.h"
+
+#define MAGIC 0x864ab572
+
+static uint16_t *unicode;
+static char *psf_start;
+
+/**
+ * @brief psf init 
+ * 
+ * @param buff 
+ * @param size 
+ */
+void psf_init(char *buff, size_t size) {
+    psf_start = buff;
+    uint16_t glyph = 0;
+    char *psf_end = psf_start + size;
+}
