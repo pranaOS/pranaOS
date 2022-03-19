61880027d07604c9eda0a49cb3a0ad3e711af5fc
Krisna Pranav <krisna.pranav@gmail.com>
2022-03-19 19:26:16 +0530

    libcpp: osobject[append lists(cpp rendering)]

--- userland/libraries/libcpp/osobject.cpp
+++ userland/libraries/libcpp/osobject.cpp
@@ -6,3 +6,20 @@
 //
 
 #include "osobject.h"
+
+namespace OsObject {
+    class object {
+    public:
+        /**
+         * @brief list append
+         */
+        void listappend(list_t* list, delement_t* elem) {
+            elem->next = 0;
+            list_t headnew;
+        }
+        
+    protected:
+        ~object();
+        object();
+    };
+}
