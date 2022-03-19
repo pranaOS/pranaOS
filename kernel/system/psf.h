cc2142e6b7ab72c92f4dd5960ead1d87fcadb783
Krisna Pranav <krisna.pranav@gmail.com>
2022-03-19 19:09:58 +0530

    kerenl: system psf

--- kernel/system/psf.h
+++ kernel/system/psf.h
@@ -4,3 +4,29 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+
+#define FONT_MAGIC 0x864ab572
+#define UNICODE_TABLE 0x01
+
+struct psf_t {
+	uint32_t magic;
+	uint32_t version;
+	uint32_t headersize;
+	uint32_t flags;
+	uint32_t numglyph;
+	uint32_t bytesperglyph;
+	uint32_t height;
+	uint32_t width;
+};
+
+/**
+ * @brief psf init 
+ * 
+ * @param buff 
+ * @param size 
+ */
+void psf_init(char *buff, size_t size);
