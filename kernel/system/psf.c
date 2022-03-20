f09419ca2f0ff7f4336d618fed4bfc114641adaa
Krisna Pranav <krisna.pranav@gmail.com>
2022-03-20 12:58:03 +0530

    kernel: system psf[putchar]

--- kernel/system/psf.c
+++ kernel/system/psf.c
@@ -17,9 +17,37 @@ static char *psf_start;
 
 /**
  * @brief putchar
- * @returns nothing
+ * 
+ * @param c 
+ * @param cx 
+ * @param cy 
+ * @param fg 
+ * @param bg 
+ * @param fb 
+ * @param scanline 
  */
 void putchar(uint32_t c, uint32_t cx, uint32_t cy, uint32_t fg, uint32_t bg, char *fb, uint32_t scanline) {
+    struct psf_t *font = (struct psf_t *)psf_start;
+    int bytesperline = div_ceil(font->width, 8);
+    if (unicode != NULL) {
+        c = unicode[c];
+    }
+
+    unsigned char *glyph = (unsigned char *)psf_start + font->headersize + (c > 0 && c < font->numglyph ? c : 0) * font->bytesperglyph;
+
+    int offs = cy * scanline + cx * 4;
+    uint32_t x, y, line, mask;
+    for (y = 0; y < font->height; y++) {
+		line = offs;
+		mask = 1 << (font->width - 1);
+		for (x = 0; x < font->width; x++) {
+			*((uint32_t *)(fb + line)) = ((int)*glyph) & (mask) ? fg : bg;
+			mask >>= 1;
+			line += 4;
+		}
+		glyph += bytesperline;
+		offs += scanline;
+	}
 }
 
 /**
