ffd3dbd17f88b564c8afd4f74279b6e3f8b17052
Krisna Pranav <krisna.pranav@gmail.com>
2022-04-01 18:55:48 +0530

    kernel: kernel[filesystem type]
    TODO: setup window server

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -6,6 +6,22 @@
  * @brief main kernel entry point 
  * 
  */
-void kernel() {
-    // TODO
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include "cpu/exception.h"
+#include "cpu/gdtentry.h"
+#include "cpu/halt.h"
+#include "cpu/idt.h"
+#include "cpu/rtc.h"
+#include "cpu/tss.h"
+#include "cpu/tasksegment.h"
+#include "devices/ata.h"
+#include "devices/memory/memory.h"
+#include "filesystem/vfs.h"
+
+extern struct vfs_file_system_type ext2_file_type;
+
+// TODO: setup window server[while booting]
+void setup_server() {
 }
