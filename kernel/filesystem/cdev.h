a1ba85359920ba6a13e297806efebfaac8d6a748
Krisna Pranav <krisna.pranav@gmail.com>
2022-04-05 19:28:12 +0530

    kernel: filesystem cdev

---
+++ kernel/filesystem/cdev.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2021 - 2022, the pranaOS Developers & Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <stdint.h>
+#include "vfs.h"
+
+#define MINORBITS 20
+#define MINORMASK ((1U << MINORBITS) - 1)
+
+#define MAJOR(dev) ((unsigned int)((dev) >> MINORBITS))
+#define MINOR(dev) ((unsigned int)((dev)&MINORMASK))
+#define MKDEV(ma, mi) (((ma) << MINORBITS) | (mi))
+
+#define PATH_DEV "/dev/"
+#define SPECNAMELEN 255
+
+/**
+ * @brief c devices 
+ * 
+ */
+struct cdevices {
+    const char *name;
+    uint32_t major;
+    uint32_t baseminor;
+    int32_t minorct;
+
+    dev_t dev;
+    struct list_head sibling;
+    struct vfs_file_operations *f_ops;
+};
