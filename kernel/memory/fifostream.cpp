7d2b55fce6ac54990f30dd6514a74e028b83193b
Krisna Pranav <krisna.pranav@gmail.com>
2022-04-14 12:37:28 +0530

    kernel: memory fifiostream

--- kernel/memory/fifostream.cpp
+++ kernel/memory/fifostream.cpp
@@ -1,5 +1,6 @@
 #include "fifostream.h"
 #include <kernel/kernel.h>
+#include <kernel/log.h>
 
 using namespace Kernel;
 
@@ -10,6 +11,11 @@ using namespace Kernel;
  */
 FIFOStream::FIFOStream(int capacity) {
     this->buffer = new char[capacity * sizeof(char)];
+    this->buffer_end = (char*)this->buffer + capacity * sizeof(char);
+    this->count = 0;
+    this->capacity = capacity;
+    this->head = this->buffer;
+    this->tail = this->buffer;
 }
 
 /**
@@ -18,4 +24,22 @@ FIFOStream::FIFOStream(int capacity) {
  */
 FIFOStream::~FIFOStream() {
     delete this->buffer;
+}
+
+/**
+ * @brief fifostream writing object
+ * 
+ * @param item 
+ */
+void FIFOStream::write(char item) {
+    if(this->count == this->capacity) {
+        sendLog(Error, "Item count has reached its capacity", this->capacity);
+        return;
+    }
+    memcpy((void*)this->head, (void*)&item, sizeof(char));
+    this->head = (char*)(this->head + sizeof(char));
+    if(this->head == this->buffer_end)
+        this->head = this->buffer;
+
+    this->count++;
 }
