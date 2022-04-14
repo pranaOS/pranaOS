f612834100a9513772431a67e45f359555a0b40d
Krisna Pranav <krisna.pranav@gmail.com>
2022-04-14 17:45:28 +0530

    kernel: system framebuffer

--- kernel/system/framebuffer.c
+++ kernel/system/framebuffer.c
@@ -12,3 +12,23 @@
 #define BACKGROUND_COLOR 0x000000
 
 static struct framebuffer *current_buffer;
+
+struct framebuffer *get_framebuffer() {
+    return current_buffer;
+}
+
+void framebuffer_init(struct multiboot_tag_framebuffer *multiboot_framebuffer) {
+    current_fb = kcalloc(1, sizeof(struct framebuffer));
+    current_fb->addr = multiboot_framebuffer->common.framebuffer_addr;
+    current_fb->bpp = multiboot_framebuffer->common.framebuffer_pitch;
+
+    uint32_t screen_size = current_fb->height * current_fb->pitch;
+    uint32_t blocks = div_ceil(screen_size, PMM_FRAME_SIZE);
+    for (uint32_t i = 0; i < blocks; i++)
+        vmm_map_address(
+            vm_get_directory(),
+            VIDEO_VADDR + i * PMM_FRAME_ALIGN,
+            current_fb->addr + i * PMM_FRAME_SIZE,
+            I86_PTE_PRESENT | I86_PTE_WRITABLE
+        );
+}
