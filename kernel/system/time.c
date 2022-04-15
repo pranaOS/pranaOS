1b687ee2c850703b474209be8d7c2f60940a5927
Krisna Pranav <krisna.pranav@gmail.com>
2022-04-15 09:12:35 +0530

    kernel: system time

--- kernel/system/time.c
+++ kernel/system/time.c
@@ -1,9 +1,8 @@
-#pragma once
-
 #include <memory/vmm.h>
 #include "time.h"
 
 extern volatile uint64_t jiffies;
+
 volatile uint64_t boot_seconds, current_seconds;
 
 struct time epoch_time = {
@@ -16,3 +15,17 @@ struct time epoch_time = {
 };
 
 struct time current_time;
+
+void set_boot_seconds(uint64_t bs) {
+	boot_seconds = bs;
+}
+
+void set_current_time(uint16_t year, uint8_t month, uint8_t day, uint8_t hour, uint8_t minute, uint8_t second) {
+	current_time.year = year;
+	current_time.month = month;
+	current_time.day = day;
+	current_time.hour = hour;
+	current_time.minute = minute;
+	current_time.second = second;
+	current_seconds = get_seconds(NULL);
+}
