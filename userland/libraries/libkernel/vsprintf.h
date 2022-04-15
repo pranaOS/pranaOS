98b0ab2ea99502ad3d4258b415f60cd1a244411b
Krisna Pranav <krisna.pranav@gmail.com>
2022-04-15 10:36:50 +0530

    libkernel: vsprintf

---
+++ userland/libraries/libkernel/vsprintf.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021 - 2022, the pranaOS Developers & Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stddef.h>
