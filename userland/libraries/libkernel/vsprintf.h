d081cc1b3fea491cdd3b1fb9ee699ae09a99573f
Krisna Pranav <krisna.pranav@gmail.com>
2022-04-15 10:49:43 +0530

    libkernel: vsprintf

--- userland/libraries/libkernel/vsprintf.h
+++ userland/libraries/libkernel/vsprintf.h
@@ -11,8 +11,89 @@
 #include <stdbool.h>
 #include <stddef.h>
 
+/**
+ * @brief vsnprintf
+ * 
+ * @param buf 
+ * @param size 
+ * @param fmt 
+ * @param args 
+ * @return int 
+ */
 int vsnprintf(char *buf, size_t size, const char *fmt, va_list args);
+
+/**
+ * @brief vscnprintf
+ * 
+ * @param buf 
+ * @param size 
+ * @param fmt 
+ * @param args 
+ * @return int 
+ */
 int vscnprintf(char *buf, size_t size, const char *fmt, va_list args);
+
+/**
+ * @brief snprintf
+ * 
+ * @param buf 
+ * @param size 
+ * @param fmt 
+ * @param ... 
+ * @return int 
+ */
 int snprintf(char *buf, size_t size, const char *fmt, ...);
+
+/**
+ * @brief scnprintf
+ * 
+ * @param buf 
+ * @param size 
+ * @param fmt 
+ * @param ... 
+ * @return int 
+ */
+int scnprintf(char *buf, size_t size, const char *fmt, ...);
+
+/**
+ * @brief vsprintf
+ * 
+ * @param buf 
+ * @param fmt 
+ * @param args 
+ * @return int 
+ */
+int vsprintf(char *buf, const char *fmt, va_list args);
+
+/**
+ * @brief sprintf
+ * 
+ * @param buf 
+ * @param fmt 
+ * @param ... 
+ * @return int 
+ */
+int sprintf(char *buf, const char *fmt, ...);
+
+/**
+ * @brief vsscanf
+ * 
+ * @param buf 
+ * @param fmt 
+ * @param args 
+ * @return int 
+ */
+int vsscanf(const char *buf, const char *fmt, va_list args);
+
+/**
+ * @brief sscanf
+ * 
+ * @param buf 
+ * @param fmt 
+ * @param ... 
+ * @return int 
+ */
+int sscanf(const char *buf, const char *fmt, ...);
+
 
 #endif 
