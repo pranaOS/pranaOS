17af8384c6afbb1afcd4316318ed59d0e384670d
Krisna Pranav <krisna.pranav@gmail.com>
2022-04-17 08:50:55 +0530

    libkernel: string[memcmp]

--- userland/libraries/libkernel/string/memcmp.c
+++ userland/libraries/libkernel/string/memcmp.c
@@ -116,3 +116,98 @@ do0:
 		return CMP_LT_OR_GT(a1, b1);
 	return 0;
 }
+
+/**
+ * @brief memcmp not common alignment 
+ * 
+ * @param srcp1 
+ * @param srcp2 
+ * @param len 
+ * @return int 
+ */
+static int memcmp_not_common_alignment(long int srcp1, long int srcp2, size_t len) {
+	op_t a0, a1, a2, a3;
+	op_t b0, b1, b2, b3;
+	op_t x;
+	int shl, shr;
+
+	shl = 8 * (srcp1 % OPSIZ);
+	shr = 8 * OPSIZ - shl;
+
+	srcp1 &= -OPSIZ;
+
+	switch (len % 4) {
+	default: 
+	case 2:
+		a1 = ((op_t *)srcp1)[0];
+		a2 = ((op_t *)srcp1)[1];
+		b2 = ((op_t *)srcp2)[0];
+		srcp1 -= 1 * OPSIZ;
+		srcp2 -= 2 * OPSIZ;
+		len += 2;
+		goto do1;
+	case 3:
+		a0 = ((op_t *)srcp1)[0];
+		a1 = ((op_t *)srcp1)[1];
+		b1 = ((op_t *)srcp2)[0];
+		srcp2 -= 1 * OPSIZ;
+		len += 1;
+		goto do2;
+	case 0:
+		if (OP_T_THRES <= 3 * OPSIZ && len == 0)
+			return 0;
+		a3 = ((op_t *)srcp1)[0];
+		a0 = ((op_t *)srcp1)[1];
+		b0 = ((op_t *)srcp2)[0];
+		srcp1 += 1 * OPSIZ;
+		goto do3;
+	case 1:
+		a2 = ((op_t *)srcp1)[0];
+		a3 = ((op_t *)srcp1)[1];
+		b3 = ((op_t *)srcp2)[0];
+		srcp1 += 2 * OPSIZ;
+		srcp2 += 1 * OPSIZ;
+		len -= 1;
+		if (OP_T_THRES <= 3 * OPSIZ && len == 0)
+			goto do0;
+	}
+
+	do {
+		a0 = ((op_t *)srcp1)[0];
+		b0 = ((op_t *)srcp2)[0];
+		x = MERGE(a2, shl, a3, shr);
+		if (x != b3)
+			return CMP_LT_OR_GT(x, b3);
+
+	do3:
+		a1 = ((op_t *)srcp1)[1];
+		b1 = ((op_t *)srcp2)[1];
+		x = MERGE(a3, shl, a0, shr);
+		if (x != b0)
+			return CMP_LT_OR_GT(x, b0);
+
+	do2:
+		a2 = ((op_t *)srcp1)[2];
+		b2 = ((op_t *)srcp2)[2];
+		x = MERGE(a0, shl, a1, shr);
+		if (x != b1)
+			return CMP_LT_OR_GT(x, b1);
+
+	do1:
+		a3 = ((op_t *)srcp1)[3];
+		b3 = ((op_t *)srcp2)[3];
+		x = MERGE(a1, shl, a2, shr);
+		if (x != b2)
+			return CMP_LT_OR_GT(x, b2);
+
+		srcp1 += 4 * OPSIZ;
+		srcp2 += 4 * OPSIZ;
+		len -= 4;
+	} while (len != 0);
+
+do0:
+	x = MERGE(a2, shl, a3, shr);
+	if (x != b3)
+		return CMP_LT_OR_GT(x, b3);
+	return 0;
+}
