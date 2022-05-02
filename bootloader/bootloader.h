da62042b625688d239b5851bd1e0fd935eede448
Krisna Pranav <krisna.pranav@gmail.com>
2022-05-02 19:04:42 +0530

    bootloader: bootloader header

---
+++ bootloader/bootloader.h
@@ -0,0 +1,14 @@
+#pragma once 
+
+#include <ak/types.h>
+
+namespace Bootloader {
+    class bootingMsg {
+        public:
+            static uint32_t bootingTime;
+    
+        private:
+            ~bootingMsg();
+ 
+    };
+}
