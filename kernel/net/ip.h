96fd0cb74cadf2dd54147cf98a0205d171bffe2e
Krisna Pranav <krisna.pranav@gmail.com>
2022-05-06 18:00:05 +0530

    kernel: net ip

--- kernel/net/ip.h
+++ kernel/net/ip.h
@@ -2,5 +2,21 @@
 
 #include <stdint.h>
 
-struct socket;
-struct sk_buff;
+#define IP4_PROTOCAL_ICMP 1
+#define IP4_PROTOCAL_TCP 6
+#define IP4_PROTOCAL_UDP 17
+
+
+namespace Kernel {
+    struct socket;
+    struct sk_buff;
+
+    struct __attribute__((packed)) ip4_pseudo {
+        // TODO
+    };
+
+    struct __attribute__((packed)) ip4_packet {
+        // todo
+    };
+
+}
