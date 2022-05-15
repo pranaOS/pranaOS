4da67c9466d18ace845c00a2d0ac64bfa0db2c1a
krishpranav <krisna.pranav@gmail.com>
2022-05-15 10:53:35 +0530

    libc: bits config_name

---
+++ libraries/libc/bits/conf.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2021 - 2022, the pranaOS Developers & Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef _BITS_CONF_H
+#define _BITS_CONF_H
+
+enum {
+    _SC_ARG_MAX,
+#define _SC_ARG_MAX _SC_ARG_MAX
+	_SC_CHILD_MAX,
+#define _SC_CHILD_MAX _SC_CHILD_MAX
+	_SC_CLK_TCK,
+#define _SC_CLK_TCK _SC_CLK_TCK
+	_SC_NGROUPS_MAX,
+#define _SC_NGROUPS_MAX _SC_NGROUPS_MAX
+	_SC_OPEN_MAX,
+};
+
+#endif 
