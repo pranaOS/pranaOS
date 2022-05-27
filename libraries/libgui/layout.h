299b911e8bc692c8d5d4c6dfbcd890e38e3faa86
krishpranav <krisna.pranav@gmail.com>
2022-05-27 19:10:10 +0530

    libgui: layout

--- libraries/libgui/layout.h
+++ libraries/libgui/layout.h
@@ -10,4 +10,27 @@
 #include <stdint.h>
 #include <sys/cdefs.h>
 
+struct window;
+
+typedef void (*EVENT_HANDLER)(struct window *win);
+
+struct graphic {
+    char *buf;
+    int32_t x, y;
+    uint16_t width, height;
+    bool transparent;
+};
+
+struct icon {
+    char *label;
+    char *exec_path;
+    char *icon_path;
+
+    bool active;
+};
+
+int get_character_width(char ch);
+int get_character_height(char ch);
+void init_fonts();
+
 #endif 
