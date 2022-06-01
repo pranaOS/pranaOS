b6198464d8909dd0580c4f21157b8eb7d53f97e7
krishpranav <krisna.pranav@gmail.com>
2022-06-01 13:02:10 +0530

    libc: file

--- libraries/libc/file.h
+++ libraries/libc/file.h
@@ -4,15 +4,24 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#pragma once
+#ifndef _LIBC_FILE_H
+#define _LIBC_FILE_H
 
 #include <ak/list.h>
 
 struct __FILE {
-    int fd;
-    int _flags;
-    int _offset;
-    int bkup_chr;
-    int blksize;
-    struct list_head sibling;
+	int fd;
+	int _flags;
+	int _offset;
+	char *_IO_read_ptr, *_IO_read_base, *_IO_read_end;
+	char *_IO_write_ptr, *_IO_write_base, *_IO_write_end;
+	int bkup_chr; 
+	int blksize;
+	struct list_head sibling;
+
+	char *_IO_save_base;   
+	char *_IO_backup_base; 
+	char *_IO_save_end;	   
 };
+
+#endif
