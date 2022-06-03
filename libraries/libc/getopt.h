49dae6d7202adf980137fda38418582bc79bff07
krishpranav <krisna.pranav@gmail.com>
2022-06-03 15:45:35 +0530

    libraries: libc getopt

--- libraries/libc/getopt.h
+++ libraries/libc/getopt.h
@@ -1,16 +1,27 @@
-#ifndef LIBC_GETOPT_H 
+#ifndef LIBC_GETOPT_H
 #define LIBC_GETOPT_H 
 
 struct option {
-    const char *name;
-    int has_arg;
-    int *flag;
-    int val;
+	const char *name;
+	int has_arg;
+	int *flag;
+	int val;
 };
 
-int getopt_long(int argc, char *const argv[], const char *optstring, const struct option);
+/**
+ * @brief Get the opt long object
+ * 
+ * @param argc 
+ * @param argv 
+ * @param optstring 
+ * @param longopts 
+ * @param longindex 
+ * @return int 
+ */
+int getopt_long(int argc, char *const argv[], const char *optstring, const struct option *longopts, int *longindex);
 
 #define no_argument 0
 #define required_argument 1
+#define optional_argument 2 
 
-#endif 
+#endif
