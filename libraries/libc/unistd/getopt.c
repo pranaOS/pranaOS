964e1e15999479215c58ca2c41da0e58e6a12337
krishpranav <krisna.pranav@gmail.com>
2022-06-03 15:38:33 +0530

    libc: unistd getopt

---
+++ libraries/libc/unistd/getopt.c
@@ -0,0 +1,13 @@
+#include <getopt.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+char *optarg = NULL;
+int optind = 1;
+int opterr = 1;
+int optopt = 0;
+
+int getopt(int argc, char *const argv[], const char *optstring) {
+    return getopt_long(argc, argv, optstring, NULL, 0);
+}
