d93278156654133e531ccc1be29e424bc9264de5
krishpranav <krisna.pranav@gmail.com>
2022-06-08 12:12:34 +0530

    builder: prana utils lock

--- builder/prana/utils/lock.h
+++ builder/prana/utils/lock.h
@@ -2,10 +2,15 @@
 
 #include <atomic>
 
+/**
+ * @brief ScopedLocker
+ * 
+ * @tparam Lock 
+ */
 template <class Lock>
 class ScopedLocker {
 public:
-    explicit ScopedLocker(Lock& lock) 
+    explicit ScopedLocker(Lock& lock)
         : m_lock(lock)
     {
         m_lock.lock();
@@ -14,17 +19,27 @@ public:
     ~ScopedLocker() {
         m_lock.unlock();
     }
+
+private:
+    Lock& m_lock;
 };
 
+/**
+ * @brief Spin Lock
+ * 
+ */
 class SpinLock {
 public:
     SpinLock() = default;
 
     void lock() {
-        while (m_locked.exchange(true)) {}
+        while (m_locked.exchange(true)) { }
     }
 
     void unlock() {
         m_locked.store(false);
     }
-}
+
+private:
+    std::atomic<bool> m_locked {};
+};
