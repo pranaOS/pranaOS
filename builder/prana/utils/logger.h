c3330d769c469fe3d1f444f3b01cca1ce9fe32b5
krishpranav <krisna.pranav@gmail.com>
2022-06-08 12:15:22 +0530

    builder: prana utils logger

--- builder/prana/utils/logger.h
+++ builder/prana/utils/logger.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <iostream>
 #include "lock.h"
 
 enum class Color {
@@ -7,6 +8,7 @@ enum class Color {
     Green = 32,
     Yellow = 33,
     Blue = 34,
+    Magenta = 35,
 };
 
 static SpinLock m_lock;
@@ -14,4 +16,7 @@ static SpinLock m_lock;
 template <class... Types>
 void Log(Color color, Types... args) {
     auto _ = ScopedLocker(m_lock);
+    std::cout << "\033[1;" << static_cast<uint32_t>(color) << "m";
+    auto print_args = { (std::cout << args << " ", 0)... };
+    std::cout << "\033[0m\n";
 }
