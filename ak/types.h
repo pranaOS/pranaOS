499b6e5593f21fe1ed0e879fcc7d927ee97b04c1
krishpranav <krisna.pranav@gmail.com>
2022-06-13 12:34:17 +0530

    ak: types

--- ak/types.h
+++ ak/types.h
@@ -6,6 +6,19 @@
 
 #pragma once
 
+#include <limits.h>
+#include <stdint.h>
+#include <stddef.h>
+
+#define asm __asm__
+#define volatile __volatile__
+
+#define ALIGN (sizeof(size_t))
+
+#define ONES ((size_t)-1/UCHAR_MAX)
+#define HIGHS (ONES * (UCHAR_MAX/2+1))
+#define HASZERO(X) (((X)-ONES) & ~(X) & HIGHS)
+
 typedef char                        int8_t;
 typedef unsigned char               uint8_t;
 typedef short                       int16_t;
