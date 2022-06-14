b837d30fa84e21bc637f770db8c7a159f41d2448
krishpranav <krisna.pranav@gmail.com>
2022-06-14 12:09:34 +0530

    libraries: libc cheader

--- libraries/libc/cheader.h
+++ libraries/libc/cheader.h
@@ -1,7 +1,9 @@
 #pragma once
 
-#ifndef __cplusplus
+#ifdef __cplusplus
 #   define _Begin_CHeader extern "C" {
 #   define _End_CHeader }
 #else
-#endif 
+#   define _Begin_CHeader
+#   define _End_CHeader
+#endif
