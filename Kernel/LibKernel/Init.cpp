4c6400f5bd5b66d1387083147d8fdab56fa676d9
krishpranav <krisna.pranav@gmail.com>
2022-06-28 17:19:07 +0530

    Kernel: LibKernel Init

--- Kernel/LibKernel/Init.cpp
+++ Kernel/LibKernel/Init.cpp
@@ -6,3 +6,12 @@
 //
 
 #include "Init.hpp"
+#include <pranaLib/Common.hpp>
+
+namespace LibKernel {
+    typedef void (*constructor)();
+    extern "C" constructor start_ctors;
+    extern "C" constructor end_ctors;
+}
+
+
