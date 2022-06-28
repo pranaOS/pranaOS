6eb3bb0bb0985358497e613e46018e531228e314
krishpranav <krisna.pranav@gmail.com>
2022-06-28 17:15:46 +0530

    Kernel

---
+++ Kernel/LibKernel/Init.cpp
@@ -0,0 +1,8 @@
+//
+//  Init.cpp
+//  Kernel
+//
+//  Created by Pranav on 28/06/22.
+//
+
+#include "Init.hpp"
---
+++ Kernel/LibKernel/Init.hpp
@@ -0,0 +1,12 @@
+//
+//  Init.hpp
+//  Kernel
+//
+//  Created by Pranav on 28/06/22.
+//
+
+#pragma once
+
+namespace LibKernel {
+extern "C" void CallConstructors();
+}
