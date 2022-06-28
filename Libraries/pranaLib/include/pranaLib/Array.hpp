ef2e2183df34fdce4fab7c502c06489f27b692f7
krishpranav <krisna.pranav@gmail.com>
2022-06-28 18:05:08 +0530

    pranaLib: Array

---
+++ Libraries/pranaLib/include/pranaLib/Array.hpp
@@ -0,0 +1,24 @@
+//
+//  Array.hpp
+//  Libraries
+//
+//  Created by Pranav on 28/06/22.
+//
+
+#pragma once
+
+#include "Common.hpp"
+
+template <typename T, size_t S>
+class Array {
+public:
+    constexpr size_t size() const { return S; }
+    
+    T& operator[](size_t index)
+    {
+        return m_data[index];
+    }
+    
+private:
+    T m_data[S];
+};
