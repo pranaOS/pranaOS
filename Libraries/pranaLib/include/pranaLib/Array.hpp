ee453df14b51d11c6fed195cfdfec0fd0019fa0c
krishpranav <krisna.pranav@gmail.com>
2022-06-28 18:05:35 +0530

    pranaLib: Array

--- Libraries/pranaLib/include/pranaLib/Array.hpp
+++ Libraries/pranaLib/include/pranaLib/Array.hpp
@@ -13,12 +13,17 @@ template <typename T, size_t S>
 class Array {
 public:
     constexpr size_t size() const { return S; }
-    
+
     T& operator[](size_t index)
     {
         return m_data[index];
     }
-    
+
+    const T& operator[](size_t index) const
+    {
+        return m_data[index];
+    }
+
 private:
     T m_data[S];
 };
