29e223662b1c511440b1da39474ffa465d1a90e6
krishpranav <krisna.pranav@gmail.com>
2022-06-29 12:55:13 +0530

    PranaBuilder: Finder

--- Builder/Prana/Finder/Finder.h
+++ Builder/Prana/Finder/Finder.h
@@ -8,6 +8,7 @@
 #include <string>
 #include <vector>
 
+
 class Finder {
     static constexpr auto PranaExtensionPattern = "*.prana";
 
@@ -16,14 +17,30 @@ public:
     {
         return Glob(PranaExtensionPattern).result();
     }
+    
+    static inline auto FindPranaFiles(const std::string& directory, const std::string& pattern = "")
+    {
+        auto path = std::filesystem::path(directory) / std::filesystem::path(pattern) / PranaExtensionPattern;
+        return Glob(path).result();
+    }
 
-    static inline auto FindPranaFiles(const std::string& directory, const std::string& patter = "")
+    static inline auto FindFiles(const std::string& directory, const std::string& pattern)
     {
         auto path = std::filesystem::path(directory) / std::filesystem::path(pattern);
         return Glob(path).result();
     }
 
-    static inline void CreateDirectory(const std::string& directory) 
+    static inline auto GetExtension(const std::string& filename)
+    {
+        return std::filesystem::path(filename).extension();
+    }
+
+    static inline auto GetFolder(const std::string& path)
+    {
+        return std::filesystem::path(path).parent_path();
+    }
+
+    static inline void CreateDirectory(const std::string& directory)
     {
         auto dirs = Utils::Split(directory, "/");
         int begin = 0;
@@ -31,15 +48,18 @@ public:
             begin++;
         }
 
-        std::filesystem::path current_path = dirs[begin];
+        std::filesystem::path cur_path = dirs[begin];
 
         for (size_t at = begin + 1; at < dirs.size(); at++) {
             if (!std::filesystem::exists(cur_path)) {
-                std::filesystem::create_directoy(cur_path);
+                std::filesystem::create_directory(cur_path);
             }
 
             cur_path = cur_path / dirs[at];
         }
-        
+
+        if (!std::filesystem::exists(cur_path)) {
+            std::filesystem::create_directory(cur_path);
+        }
     }
 };
