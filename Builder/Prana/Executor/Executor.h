d7919b49d8a067ba48fe25e0fa31e491666c9222
krishpranav <krisna.pranav@gmail.com>
2022-06-29 11:39:06 +0530

    PranaBuildr: Exectuor

--- Builder/Prana/Executor/Executor.h
+++ Builder/Prana/Executor/Executor.h
@@ -15,7 +15,7 @@ class Context;
 
 class Executor {
 public:
-    static Executor& the() 
+    static Executor& the()
     {
         static auto instance = Executor();
         return instance;
@@ -28,12 +28,20 @@ public:
 
     static inline void blocking_cmd(const std::string& cmd)
     {
-        Log(Color::Blue, "Command: ", cmd);
+        Log(Color::Blue, "Command:", cmd);
         system(cmd.c_str());
     }
 
 private:
     Executor() = default;
 
+private:
+    static void process_unit(const std::shared_ptr<ExecUnit>& u3, Command& cmd);
 
+private:
+    std::thread* m_thread {};
+    bool m_running { true };
+    size_t m_free_processes { std::max((uint32_t)1, std::thread::hardware_concurrency()) };
+    std::vector<Command> m_commands { std::vector<Command>(m_free_processes) };
+    ThreadQueue<std::shared_ptr<ExecUnit>> m_units {};
 };
