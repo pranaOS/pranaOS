1fa8662825f86b5f34105b52f5858c713915a937
krishpranav <krisna.pranav@gmail.com>
2022-06-29 16:27:10 +0530

    Prana: Parser[IncludeField]

--- Builder/Prana/Parser/Field/IncludeField.h
+++ Builder/Prana/Parser/Field/IncludeField.h
@@ -1,15 +1,16 @@
 #pragma once
 
 #include <memory>
+#include <string>
 #include <vector>
 
 class IncludeField {
 public:
+
     IncludeField() = default;
-    auto& paths() const  {
-        return m_paths;
-    }
+    void add_path(const std::shared_ptr<std::string>& path) { m_paths.push_back(path); }
+    auto& paths() const { return m_paths; }
 
 private:
-    IncludeField::~IncludeField();
-}
+    std::vector<std::shared_ptr<std::string>> m_paths {};
+};
