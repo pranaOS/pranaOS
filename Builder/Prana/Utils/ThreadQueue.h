ee10f21bf66b1e6a2069e9e43943cdfcb614183c
krishpranav <krisna.pranav@gmail.com>
2022-06-29 11:15:32 +0530

    Builder: Prana Utils[ThreadQueue]

--- Builder/Prana/Utils/ThreadQueue.h
+++ Builder/Prana/Utils/ThreadQueue.h
@@ -13,7 +13,18 @@ public:
         return m_queue.size();
     }
 
+public:
+    void enqueue(const ValueType& value);
+    bool dequeue(ValueType& value);
+    void enqueue(ValueType&& value);
+
 private:
     SpinLock m_lock {};
     std::queue<ValueType> m_queue;
 };
+
+template <typename ValueType>
+void ThreadQueue<ValueType>::enqueue(const ValueType& value)
+{
+    enqueu(ValueType(value));
+}
