0a975ebf71c30f7ed09767777c0ac30ce945b9ea
krishpranav <krisna.pranav@gmail.com>
2022-07-10 17:52:20 +0530

    Builder: Prana Parser

--- Builder/Prana/Parser/Parser.h
+++ Builder/Prana/Parser/Parser.h
@@ -3,9 +3,11 @@
 #include "../Utils/Logger.h"
 #include "Lexer/Lexer.h"
 #include "Lexer/Token.h"
+#include <functional>
 
 #pragma once
 using TokenProcessor = const std::function<void(const Token&)>;
+using TokenContentProcessor = const std::function<void(const std::shared_ptr<std::string>& content)>&;
 
 class Context;
 
@@ -13,4 +15,13 @@ class Parser {
 public:
     Parser() = default;
     Parser(const Parser&) = default;
+    
+    void run();
+    
+private:
+    void parse_include();
+    void parse_defines();
+    void parse_commands();
+    void parse_build();
+    void parse_deault();
 }
