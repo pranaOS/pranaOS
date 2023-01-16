efc758ca7ad34bd770ca80e3fe67ec616665147e
krishpranav <krisna.pranav@gmail.com>
2023-01-16 18:08:16 +0530

    Libraries: Base[std]

---
+++ Libraries/Base/std.h
@@ -0,0 +1,16 @@
+//
+//  std.h
+//  Libraries
+//
+//  Created by krisna pranav on 16/01/23.
+//
+
+#pragma once
+
+#include <initializer_list>
+#include <new>
+#include <utility>
+#include "_prelude.h"
+#import <Meta/sign.h>
+
+using ssize_t = Prana::Meta::MakeSigned<size_t>
