abf44fac089faeb5cdc470a89374ef3748869a3b
krishpranav <krisna.pranav@gmail.com>
2023-01-16 18:06:35 +0530

    Libraries: Base

---
+++ Libraries/Base/_prelude.h
@@ -0,0 +1,24 @@
+//
+//  _prelude.h
+//  Libraries
+//
+//  Created by krisna pranav on 16/01/23.
+//
+
+#pragma once
+
+#include <ctype.h>
+#include <math.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+
+#ifndef NO_TOP_LEVEL_USING
+
+namespace Prana {
+
+};
+
+using namespace Prana;
+
+#endif
