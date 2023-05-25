7226d673a714db5bcebe2fa6b4c1b7549bb8efe7
krishpranav <krisna.pranav@gmail.com>
2023-05-25 06:34:07 +0530

    errno + asm: gdt + int

---
+++ kernel/api/errno.h
@@ -0,0 +1,100 @@
+/**
+ * @file errno.h
+ * @author Krisna Pranav
+ * @brief Errno
+ * @version 0.1
+ * @date 2023-05-25
+ * 
+ * @copyright Copyright (c) 2023 pranaOS Developers, Krisna Pranav
+ * 
+*/
+
+#pragma once
+
+#define EPEM
+#define	EPERM 1		
+#define	ENOENT 2	
+#define	ESRCH 3		
+#define	EINTR 4		
+#define	EIO 5		
+#define	ENXIO 6		
+#define	E2BIG 7		
+#define	ENOEXEC 8	
+#define	EBADF 9		
+#define	ECHILD 10	
+#define	EAGAIN 11	
+#define	ENOMEM 12	
+#define	EACCES 13	
+#define	EFAULT 14	
+#define	EBUSY 16	
+#define	EEXIST 17	
+#define	EXDEV 18	
+#define	ENODEV 19	
+#define	ENOTDIR 20	
+#define	EISDIR 21	
+#define	EINVAL 22	
+#define	ENFILE 23	
+#define	EMFILE 24	
+#define	ENOTTY 25	
+#define	ETXTBSY 26	
+#define	EFBIG 27	
+#define	ENOSPC 28	
+#define	ESPIPE 29	
+#define	EROFS 30	
+#define	EMLINK 31	
+#define	EPIPE 32	
+#define	EDOM 33		
+#define	ERANGE 34	
+#define	ENOMSG 35	
+#define	EIDRM 36	
+#define	EDEADLK 45	
+#define	ENOLCK 46	
+#define ENOSTR 60	
+#define ENODATA 61	
+#define ETIME 62	
+#define ENOSR 63	
+#define ENOLINK 67	
+#define EPROTO 71	
+#define	EMULTIHOP 74	
+#define EBADMSG 77	
+#define EFTYPE 79	
+#define ENOSYS 88	
+#define ENOTEMPTY 90	
+#define ENAMETOOLONG 91	
+#define ELOOP 92	
+#define EOPNOTSUPP 95	
+#define EPFNOSUPPORT 96 
+#define ECONNRESET 104  
+#define ENOBUFS 105	
+#define EAFNOSUPPORT 106 
+#define EPROTOTYPE 107	
+#define ENOTSOCK 108	
+#define ENOPROTOOPT 109	
+#define ECONNREFUSED 111	
+#define EADDRINUSE 112		
+#define ECONNABORTED 113	
+#define ENETUNREACH 114		
+#define ENETDOWN 115
+#define ENETPOSIX		
+#define ETIMEDOUT 116		
+#define EHOSTDOWN 117		
+#define EHOSTUNREACH 118	
+#define EINPROGRESS 119		
+#define EALREADY 120		
+#define EDESTADDRREQ 121	
+#define EMSGSIZE 122		
+#define EPROTONOSUPPORT 123	
+#define EADDRNOTAVAIL 125	
+#define ENETRESET 126		
+#define EISCONN 127		
+#define ENOTCONN 128		
+#define ETOOMANYREFS 129
+#define EDQUOT 132
+#define ESTALE 133
+#define ENOTSUP 134		
+#define EILSEQ 138		
+#define EOVERFLOW 139	
+#define ECANCELED 140	
+#define ENOTRECOVERABLE 141	
+#define EOWNERDEAD 142	
+#define EWOULDBLOCK EAGAIN	
