155fbf9f3904993ee4609d03ac63d8ba2593c27a
krishpranav <krisna.pranav@gmail.com>
2023-05-29 18:19:47 +0530

    kernel: device[vgaDevice]

---
+++ kernel/device/vgadevice.h
@@ -0,0 +1,11 @@
+/**
+ * @file vgadevice.h
+ * @author Krisna Pranav
+ * @brief VGA Device
+ * @version 0.1
+ * @date 2023-05-29
+ * 
+ * @copyright Copyright (c) 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
