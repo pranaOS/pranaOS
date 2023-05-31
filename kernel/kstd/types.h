20ac99b13fb610b5ed40eb088e1659c1376d07f7
krishpranav <krisna.pranav@gmail.com>
2023-05-31 21:08:28 +0530

    kernel: kstd[types]

---
+++ kernel/kstd/types.h
@@ -0,0 +1,17 @@
+/**
+ * @file types.h
+ * @author Krisna Pranav
+ * @brief Types
+ * @version 0.1
+ * @date 2023-05-31
+ * 
+ * @copyright Copyright (c) 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#define NULL 0
+
+#include "api/stdint.h"
+#include "api/stddef.h"
