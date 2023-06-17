2b4263e6b4603949b7ab773cf014538a7a180476
krishpranav <krisna.pranav@gmail.com>
2023-06-17 15:36:33 +0530

    mobilesOS: kernel[memory(non_cached_mem_allocations)]

---
+++ mobileOS/kernel/memory/non_cached_mem_allocations.hpp
@@ -0,0 +1,43 @@
+/**
+ * @file non_cached_mem_allocations.hpp
+ * @author Krisna Pranav
+ * @brief NonCachedMemAllocations
+ * @version 0.1
+ * @date 2023-06-17
+ * 
+ * @copyright Copyright (c) 2021-2023, pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <cstdint>
+#include <cassert>
+#include <FreeRTOS.h>
+
+/**
+ * @brief nonCachedMemAllocator 
+ * 
+ * @tparam T 
+ */
+template <typename T> struct nonCachedMemAllocator {
+    using value_type = T;
+
+    T *allocate(std::size_t num);
+    void deallocate(T *ptr, std::size_t num);
+
+    nonCachedMemAllocator() = default;
+    nonCachedMemAllocator(const nonCachedMemAllocator &) = default;
+};  
+
+/**
+ * @brief nonCachedMemAllocator
+ * 
+ * @tparam T 
+ * @param num 
+ * @return T* 
+ */
+template <typename T> T *nonCachedMemAllocator<T>::allocate(std::size_t num) {
+    T *ptr = reinterpret_cast<T *>(pvPortMalloc(sizeof(T) * num));
+    return ptr;
+}
