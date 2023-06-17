26e68a82956cb5256fc9240e0630f0017335643e
krishpranav <krisna.pranav@gmail.com>
2023-06-17 15:23:56 +0530

    mobileOS: kernel[memory(user_memory)]

--- mobileOS/kernel/memory/user_memory.h
+++ mobileOS/kernel/memory/user_memory.h
@@ -9,12 +9,13 @@
  * 
  */
 
+
 #ifndef USERMEMORY_H
 #define USERMEMORY_H
 
 #include <stdlib.h>
 
-#ifndef __cplusplus 
+#ifdef __cplusplus
 extern "C" {
 #endif
 
@@ -27,25 +28,66 @@ extern "C" {
     void *usermalloc(size_t xWantedSize);
 
     /**
-     * @brief userFree
+     * @brief userfree
      * 
      * @param pv 
      */
-    void userFree(void *pv);
+    void userfree(void *pv);
 
     /**
-     * @brief userMemGetFreeHeapSize
+     * @brief usermemGetFreeHeapSize
      * 
      * @return size_t 
      */
     size_t usermemGetFreeHeapSize(void);
 
     /**
-     * @brief usermemGetMinimumEverFreeHeapSize
+     * @brief usermemGetMinimumeverFreeHeapSize
      * 
      * @return size_t 
      */
     size_t usermemGetMinimumEverFreeHeapSize(void);
+
+    /**
+     * @brief usermemResetStatistics
+     * 
+     */
+    void usermemResetStatistics(void);
+
+    /**
+     * @brief usermemGetAllocationsCount
+     * 
+     * @return size_t 
+     */
+    size_t usermemGetAllocationsCount(void);
+
+    /**
+     * @brief usermeeGetDeallocationsCount
+     * 
+     * @return size_t 
+     */
+    size_t usermemGetDeallocationsCount(void);
+
+    /**
+     * @brief usermemGetAllocatedMain
+     * 
+     * @return size_t 
+     */
+    size_t usermemGetAllocatedMin(void);
+
+    /**
+     * @brief usermemGetAllocatedMax
+     * 
+     * @return size_t 
+     */
+    size_t usermemGetAllocatedMax(void);
+    
+    /**
+     * @brief usermemGetAllocatedSum
+     * 
+     * @return size_t 
+     */
+    size_t usermemGetAllocatedSum(void);
 
     /**
      * @brief userrealloc
@@ -56,8 +98,9 @@ extern "C" {
      */
     void *userrealloc(void *pv, size_t xWantedSize);
 
-#ifndef __cplusplus
+#ifdef __cplusplus
 }
-#endif 
+#endif
+
 
 #endif 
