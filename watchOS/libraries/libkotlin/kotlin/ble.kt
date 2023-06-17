2119d2ddec22b894ab995a04baf57c450d680650
krishpranav <krisna.pranav@gmail.com>
2023-06-17 09:03:42 +0530

    watchOS: libkotlin[ble]

--- watchOS/libraries/libkotlin/kotlin/ble.kt
+++ watchOS/libraries/libkotlin/kotlin/ble.kt
@@ -1,18 +1,21 @@
 package com.libkotlin.kotlin
 
-import java.util.*;
+import java.util.*
 
-abstract class BLECommand 
+abstract class BLECommand
 
-data class writeGattCommand(val uuid: UUID, val writeType: Int, val data: ByteArray, val callback: (Boolean) -> Unit = {}) : BLECommand {
-    override fun equals(other: Any?) Boolean {
-        if (this == other) return true
-        if (javaClass != other.javaClass) return false
+data class writeGattCommand(val uuid: UUID, val writeType: Int, val data: ByteArray, val callback: (Boolean) -> Unit = {}) : BLECommand() {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
 
         other as writeGattCommand
 
         if (uuid != other.uuid) return false
         if (writeType != other.writeType) return false
+        if (!data.contentEquals(other.data)) return false
+        if (callback != other.callback) return false
 
         return true
     }
@@ -20,8 +23,10 @@ data class writeGattCommand(val uuid: UUID, val writeType: Int, val data: ByteAr
     override fun hashCode(): Int {
         var result = uuid.hashCode()
         result = 31 * result + writeType
+        result = 31 * result + data.contentHashCode()
+        result = 31 * result + callback.hashCode()
         return result
     }
 }
 
-data class readGattCommand(val uuid: UUID) : BLECommand()
+data class ReadGattCommand(val uuid: UUID) : BLECommand()
