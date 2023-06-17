001ee4d33988e0698477a55908349adf7e3efc84
krishpranav <krisna.pranav@gmail.com>
2023-06-17 08:25:06 +0530

    remove unused license + libtar[tar]

---
+++ mobileOS/libraries/libtar/tar.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file tar.cpp
+ * @author Krisna Pranav
+ * @brief TAR
+ * @version 0.1
+ * @date 2023-06-17
+ * 
+ * @copyright Copyright (c) 2021-2023, pranaOS Developers, Krisna Pranav
+ * 
+ */
