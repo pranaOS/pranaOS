8ddaf25f10e89de93b476190273b374334d2fd1e
krishpranav <krisna.pranav@gmail.com>
2023-06-17 08:28:51 +0530

    mobileOS: libtar[tar(Entry + Iterator for microTar)]

--- mobileOS/libraries/libtar/tar.hpp
+++ mobileOS/libraries/libtar/tar.hpp
@@ -8,3 +8,55 @@
  * @copyright Copyright (c) 2021-2023, pranaOS Developers, Krisna Pranav
  * 
  */
+
+#pragma once
+
+#include <filesystem>
+#include <optional>
+#include <vector>
+#include <microtar.hpp>
+
+namespace Tar {
+    
+    class Entry {
+    public:
+        ~entry();
+        
+        std::size_t size() const
+        std::filesystem::path name() const;
+        bool isFile() const;
+        bool isDirectory() const;
+        
+        void read(const std::byte *data, std::size_t size) const;
+        
+    private:
+        explicit entry(const std::filesystem::path &path);
+        friend class iterator;
+        mutable mtar_t handle{};
+        mtar_header_t tar_header;
+    };
+
+    class iterator {
+    public:
+        using iterator_category = std::input_iterator_tag;
+        using difference_type = std::ptrdiff_t;
+        using value_type = entry;
+        using pointer = entry *;
+        using reference = entry &;
+        
+        Iterator() = default;
+        
+        explicit Iterator(const std::filesystem::path &path);
+        
+        reference operator*() const;
+        pointer operator->() const;
+    }
+
+    inline iterator begin(const iterator &it) noexcept {
+        return it
+    }
+
+    inline iterator end(iterator) noexcept {
+        return iterator {};
+    }
+}
