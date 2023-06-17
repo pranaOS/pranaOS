67283cdc04d631729e4dd1a568f3e17f649b3645
krishpranav <krisna.pranav@gmail.com>
2023-06-17 09:01:23 +0530

    watchOS: libkotlin[ble]

--- watchOS/libraries/libkotlin/kotlin/ble.kt
+++ watchOS/libraries/libkotlin/kotlin/ble.kt
@@ -4,6 +4,24 @@ import java.util.*;
 
 abstract class BLECommand 
 
-data class WriteGattCommand(val uuid: UUID, val writeType: Int, val data: ByteArray, val callback: (Boolean) -> Unit = {}) : BLECommand {
-    
+data class writeGattCommand(val uuid: UUID, val writeType: Int, val data: ByteArray, val callback: (Boolean) -> Unit = {}) : BLECommand {
+    override fun equals(other: Any?) Boolean {
+        if (this == other) return true
+        if (javaClass != other.javaClass) return false
+
+        other as writeGattCommand
+
+        if (uuid != other.uuid) return false
+        if (writeType != other.writeType) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = uuid.hashCode()
+        result = 31 * result + writeType
+        return result
+    }
 }
+
+data class readGattCommand(val uuid: UUID) : BLECommand()
