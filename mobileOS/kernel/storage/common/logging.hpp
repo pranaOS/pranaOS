8a0f11b527bc409bf8c9728bbc1cf72f9f8fd95c
krishpranav <krisna.pranav@gmail.com>
2023-06-17 16:19:45 +0530

    mobileOS: kernel[storage]

---
+++ mobileOS/kernel/storage/common/logging.hpp
@@ -0,0 +1,20 @@
+/**
+ * @file logging.hpp
+ * @author Krisna Pranav
+ * @brief Logging
+ * @version 1.0
+ * @date 2023-06-17
+ * 
+ * @copyright Copyright (c) 2021-2023, pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#if DEBUG_DB_MODEL_DATA == 1
+#define debug_db_data(...) LOG_DEBUG(__VA_ARGS__)
+#define error_db_data(...) LOG_ERROR(__VA_ARGS__)
+#else
+#define debug_db_data(...)
+#define error_db_data(...)
+#endif 
