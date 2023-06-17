d70304aec6f70b74a89bbaf528926b8e831c00d6
krishpranav <krisna.pranav@gmail.com>
2023-06-17 19:48:47 +0530

    mobileOS: kernel[storage(queryresult)]

---
+++ mobileOS/kernel/storage/queryresult.cpp
@@ -0,0 +1,39 @@
+/**
+ * @file queryresult.cpp
+ * @author Krisna Pranav
+ * @brief QueryResult
+ * @version 1.0
+ * @date 2023-06-17
+ * 
+ * @copyright Copyright (c) 2021-2023, pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include "queryresult.hpp"
+
+/**
+ * @brief Construct a new Query Result:: Query Result object
+ * 
+ */
+QueryResult::QueryResult() : currentRow(0) {}
+
+/**
+ * @brief addRow
+ * 
+ * @param row 
+ */
+void QueryResult::addRow(const std::vector<Field> &row) {
+    rows.push_back(row);
+}
+
+/**
+ * @brief nextRow
+ * 
+ * @return true 
+ * @return false 
+ */
+bool QueryResult::nextRow() {
+    ++currentRow;
+
+    return (currentRow < rows.size());
+}
