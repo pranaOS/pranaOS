60458a959041b104878b49b47144d53eba0e5a08
krishpranav <krisna.pranav@gmail.com>
2023-06-17 18:14:58 +0530

    mobileOS: kernel[storage(common)]

---
+++ mobileOS/kernel/storage/common/types.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file types.hpp
+ * @author Krisna Pranav
+ * @brief Types
+ * @version 0.1
+ * @date 2023-06-17
+ * 
+ * @copyright Copyright (c) 2021-2023, pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#define u32_ "%" PRIu32
+#define u32_c "%" PRIu32 ","
+#define str_ "%Q"
+#define str_c "%Q,"
