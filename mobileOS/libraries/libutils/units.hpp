08cbc3b10ba49199f09e1a2990d48c49f40108cc
krishpranav <krisna.pranav@gmail.com>
2023-06-17 13:35:39 +0530

    mobileOS: libutils[units]

---
+++ mobileOS/libraries/libutils/units.hpp
@@ -0,0 +1,21 @@
+/**
+ * @file units.hpp
+ * @author Krisna Pranav
+ * @brief UNITS 
+ * @version 0.1
+ * @date 2023-06-17
+ * 
+ * @copyright Copyright (c) 2021-2023, pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <cstdint>
+
+namespace Units {
+    using Voltage = std::uint32_t;
+    using SOC = std::uint8_t;
+    using Temperature = float;
+    using Percent = float;
+}
