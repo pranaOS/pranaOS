37b25f0587ec374337d0513720a521df80a061da
krishpranav <krisna.pranav@gmail.com>
2023-06-18 17:32:28 +0530

    watchOS: kernel[battery]

---
+++ watchOS/kernel/battery.hpp
@@ -0,0 +1,18 @@
+/**
+ * @file battery.hpp
+ * @author Krisna Pranav
+ * @brief Battery
+ * @version 1.0
+ * @date 2023-06-18
+ * 
+ * @copyright Copyright (c) 2021-2023, pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+float batteryVoltage();
+
+float batteryPercentage();
+
+float batteryCapacity();
