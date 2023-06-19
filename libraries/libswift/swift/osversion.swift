52adc88e97ac2769aa4482e54fb034bd1057e562
krishpranav <krisna.pranav@gmail.com>
2023-06-19 20:09:33 +0530

    libraries: libswift

---
+++ libraries/libswift/swift/osversion.swift
@@ -0,0 +1,7 @@
+// osVersion and all other stuffs would be here, related to operating systems.
+
+public let name = "pranaOS"
+public let versionMajor = 0
+public let versionMinor = 10
+public let versionXMinor = 0
+public let buildNumber = 1
