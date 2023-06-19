a25bcdce1b13a424fd17cfdb0d62fcd5bef064f2
krishpranav <krisna.pranav@gmail.com>
2023-06-19 06:00:34 +0530

    kernel: kstd[refcount]

--- kernel/kstd/refcount.h
+++ kernel/kstd/refcount.h
@@ -11,64 +11,104 @@
 
 #pragma once
 
-#include "../../atomic.h"
+#include "../atomic.h"
 #include "utility.h"
-#include "kstdio.h" 
+#include "kstdio.h"
 
 namespace kstd {
-    enum class PtrReleaseAction {
-        Keep, 
-        Destroy 
-    };
+	enum class PtrReleaseAction {
+		Keep, 
+        Destroy
+	};
 
-    class RefCount {
-    public:
+	class RefCount {
+	public:
+
         /**
          * @brief Construct a new Ref Count object
          * 
          * @param strong_count 
          */
-        explicit RefCount(int strong_count);
+		explicit RefCount(int strong_count);
 
         /**
          * @brief Construct a new Ref Count object
          * 
          * @param other 
          */
-        RefCount(RefCount&& other);
+		RefCount(RefCount&& other);
 
         /**
          * @brief Construct a new Ref Count object
          * 
          * @param other 
          */
-        RefCount(const RefCount& other) = delete;
+		RefCount(const RefCount& other) = delete;
 
         /**
          * @brief Destroy the Ref Count object
          * 
          */
-        ~RefCount();
+		~RefCount();
+
+		/**
+		 * @brief strong_count
+		 * 
+		 * @return long 
+		 */
+		long strong_count() const;
+
+		/**
+		 * @brief weak_count
+		 * 
+		 * @return long 
+		 */
+		long weak_count() const;
+
+		/**
+		 * @brief make_strong
+		 * 
+		 * @return true 
+		 * @return false 
+		 */
+		bool make_strong();
+
+		/**
+		 * @brief acqure_strong
+		 * 
+		 */
+		void acquire_strong();
+
+		/**
+		 * @brief acquire_weak
+		 * 
+		 */
+		void acquire_weak();
+
+		/**
+		 * @brief release_strong
+		 * 
+		 * @return PtrReleaseAction 
+		 */
+		PtrReleaseAction release_strong();
+
+		/**
+		 * @brief release_weak
+		 * 
+		 */
+		void release_weak();
+
+	private:
+        /**
+         * @brief atomic m_strong_cout
+         * 
+         */
+		Atomic<long, MemoryOrder::SeqCst> m_strong_count = 0;
 
         /**
-         * @brief strong_count
+         * @brief m_weak_cout
          * 
-         * @return long 
          */
-        long strong_count() const;
-
-        long weak_count() const;
-
-        bool make_stong();
-
-        void acquire_strong();
-
-        /**
-         * @brief release_String
-         * 
-         * @return PtrReleaseAction 
-         */
-        PtrReleaseAction release_strong();
-
-    };  
+		Atomic<long, MemoryOrder::SeqCst> m_weak_count = 0;
+	};
 }
