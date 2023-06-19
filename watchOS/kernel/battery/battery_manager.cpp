5716573d1d97c29762ac53316a7aaed638d87aa0
krishpranav <krisna.pranav@gmail.com>
2023-06-19 05:41:56 +0530

    watchOS: kernel

--- watchOS/kernel/battery/battery_manager.cpp
+++ watchOS/kernel/battery/battery_manager.cpp
@@ -2,9 +2,11 @@
  * @file battery_manager.cpp
  * @author Krisna Pranav
  * @brief BatteryManager
- * @version 0.1
+ * @version 1.0
  * @date 2023-06-18
  * 
  * @copyright Copyright (c) 2021-2023, pranaOS Developers, Krisna Pranav
  * 
  */
+
+#include "battery_manager.hpp"
