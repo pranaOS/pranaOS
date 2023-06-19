dd7b24722d4b17db18dc020292fd6973eb3133ff
krishpranav <krisna.pranav@gmail.com>
2023-06-19 19:51:41 +0530

    libraries: libobjc[mobile]

---
+++ libraries/libobjc/mobile.m
@@ -0,0 +1,10 @@
+//
+//  mobile.m
+//  libobjc
+//
+//  Created by krisna pranav on 19/06/23.
+//
+
+#import <objc/objc.h>
+
+bool isMobileUnicodePresent = false;
---
+++ libraries/libobjc/unicode.m
@@ -0,0 +1,8 @@
+//
+//  unicode.m
+//  libobjc
+//
+//  Created by krisna pranav on 19/06/23.
+//
+
+#import <Foundation/Foundation.h>
