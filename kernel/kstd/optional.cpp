1f0499a5734c09a52f01fe2591f4ca9ce95e6306
krishpranav <krisna.pranav@gmail.com>
2023-06-19 06:45:28 +0530

    kernel: kstd[optional]

---
+++ kernel/kstd/optional.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file optional.cpp
+ * @author Krisna Pranav
+ * @brief optional
+ * @version 1.0
+ * @date 2023-06-19
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav 
+ * 
+ */
+
+#include "optional.h"
+
+/**
+ * @brief nullopt
+ * 
+ */
+kstd::nullopt_t kstd::nullopt {};
