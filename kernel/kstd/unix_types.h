cd1d37d480060db94a888378f1b214920780cc1e
krishpranav <krisna.pranav@gmail.com>
2023-06-19 06:35:41 +0530

    kernel: kstd[unix_types]

---
+++ kernel/kstd/unix_types.h
@@ -0,0 +1,25 @@
+/**
+ * @file unix_types.h
+ * @author Krisna Pranav
+ * @brief UnixTypes
+ * @version 1.0
+ * @date 2023-06-19
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include "../api/fcntl.h"
+#include "../api/errno.h"
+#include "../api/types.h"
+#include "../api/time.h"
+#include "../api/signal.h"
+#include "../api/shm.h"
+#include "../api/stdarg.h"
+#include "../api/stddef.h"
+#include "../api/unistd.h"
+#include "types.h"
+
+#define SUCCESS 0
