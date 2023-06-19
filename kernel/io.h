7d940823c5d0ee9cb6d0a16bce421e85ece01eee
krishpranav <krisna.pranav@gmail.com>
2023-06-19 06:39:32 +0530

    kernel: io

--- kernel/io.h
+++ kernel/io.h
@@ -14,13 +14,67 @@
 #include "kstd/unix_types.h"
 
 namespace IO {
-    void wait();
+    /**
+     * @brief wait
+     * 
+     */
+	void wait();
 
-    uint8_t inb(uint16_t port);
+    /**
+     * @brief outb
+     * 
+     * @param port 
+     * @param value 
+     */
+	void outb(uint16_t port, uint8_t value);
 
-    inline void wait(size_t us) {
-        while(us--) {
-            inb(0x80)
-        }
-    }
+    /**
+     * @brief outw
+     * 
+     * @param port 
+     * @param value 
+     */
+	void outw(uint16_t port, uint16_t value);
+
+    /**
+     * @brief outl
+     * 
+     * @param port 
+     * @param value 
+     */
+	void outl(uint16_t port, uint32_t value);
+
+    /**
+     * @brief inb
+     * 
+     * @param port 
+     * @return uint8_t 
+     */
+	uint8_t inb(uint16_t port);
+
+    /**
+     * @brief inw
+     * 
+     * @param port 
+     * @return uint16_t 
+     */
+	uint16_t inw(uint16_t port);
+
+    /**
+     * @brief inl
+     * 
+     * @param port 
+     * @return uint32_t 
+     */
+	uint32_t inl(uint16_t port);
+
+    /**
+     * @brief wait
+     * 
+     * @param us 
+     */
+	inline void wait(size_t us) {
+		while(us--)
+			inb(0x80);
+	}
 };
