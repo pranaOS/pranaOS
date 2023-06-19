70fb89ef27390ca703c28e6f59572bb1c599c9ae
krishpranav <krisna.pranav@gmail.com>
2023-06-19 06:08:57 +0530

    kernel: api[shm]

--- kernel/api/shm.h
+++ kernel/api/shm.h
@@ -14,12 +14,15 @@
 #include "types.h"
 
 #define SHM_READ 0x1u
+#define SHM_WRITE 0x2u
+#define SHM_SHARE 0x4u
 
 __DECL_BEGIN
 
 struct shm {
-    void* ptr;
-    size_t size;
+	void* ptr;
+	size_t size;
+	int id;
 };
 
 __DECL_END
