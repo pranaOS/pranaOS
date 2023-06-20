093297588905c85f643a7b808a35a9e33ff61d05
krishpranav <krisna.pranav@gmail.com>
2023-06-20 19:29:41 +0530

    mobileOS: kernel[sys(phone)]

---
+++ mobileOS/kernel/sys/phone.hpp
@@ -0,0 +1,21 @@
+/**
+ * @file phone.hpp
+ * @author Krisna Pranav
+ * @brief Phone Modes
+ * @version 1.0
+ * @date 2023-06-20
+ * 
+ * @copyright Copyright (c) 2021-2023, pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+namespace sys::phonemodes {
+    enum class PhoneMode {
+        Connected,
+        DoNotDisturb,
+        Offline,
+        Unintialized
+    };
+}
