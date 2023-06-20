8fee7b3ff32a163bb88dd7014652f68c868977f4
krishpranav <krisna.pranav@gmail.com>
2023-06-20 19:50:32 +0530

    mobileOS: kernel[sys(service(message))]

---
+++ mobileOS/kernel/sys/service/message.hpp
@@ -0,0 +1,22 @@
+/**
+* @file message.hpp
+* @author Krisna Pranav
+* @brief Message Functionalities
+* @version 1.0
+* @date 2023-06-20
+*
+* @copyright Copyright (c) 2021-2023, pranaOS Developers, Krisna Pranav
+*
+*/
+
+#pragma once
+
+#include "message_forward.hpp"
+//#include <>
+#include <cstdint>
+#include <memory>
+#include <string>
+
+namespace sys {
+
+}
