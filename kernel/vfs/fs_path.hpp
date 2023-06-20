ad57d5a1a9b89b5d472ae840e30e9618b13c7eed
krishpranav <krisna.pranav@gmail.com>
2023-06-20 19:25:55 +0530

    kernel: vfs[fs_path]

--- kernel/vfs/fs_path.hpp
+++ kernel/vfs/fs_path.hpp
@@ -14,8 +14,29 @@
 #include <filesystem>
 
 namespace FSPath {
-    namespace dir {
+
+    namespace Dir {
         std::filesystem::path getSystemDiskPath() noexcept;
-        // std::filesystem::path 
+        std::filesystem::path getUserDiskPath() noexcept;
+        std::filesystem::path getMfgConfPath() noexcept;
+        std::filesystem::path getDatabasesPath() noexcept;
+        std::filesystem::path getLogsPath() noexcept;
+        std::filesystem::path getCrashDumpsPath() noexcept;
+        std::filesystem::path getUserMediaPath() noexcept;
+        std::filesystem::path getTemporaryPath() noexcept;
+        std::filesystem::path getBootJSONPath() noexcept;
+        std::filesystem::path getAssetsDirPath() noexcept;
+        std::filesystem::path getSystemDataDirPath() noexcept;
+        std::filesystem::path getSystemVarDirPath() noexcept;
+    } 
+
+    namespace File {
+        constexpr inline auto boot_json = "bootcfg.json";
+        constexpr inline auto os_bin = "os.bin";
+        constexpr inline auto version_json = "version.json";
+    }
+
+    namespace Buffer {
+        constexpr inline auto tar_buf = 1024;
     }
 }
