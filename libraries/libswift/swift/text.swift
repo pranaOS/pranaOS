9a6264576da3d35cba1b1c9bf899cf9f7fe0ff43
krishpranav <krisna.pranav@gmail.com>
2023-06-20 18:53:57 +0530

    libraries: libswift[text]

---
+++ libraries/libswift/swift/text.swift
@@ -0,0 +1,24 @@
+import Loggable
+
+class TextFile: File, Loggable {
+    let name: String = "Text File"
+    var text: String {
+        get {
+            return String(utf8Characters: file.Data) ?? ""
+        }
+        set {
+            var buf = ContiguousArray<UInt8>(newValue.utf8)
+            file.Info.Size = newValue.utf8.count
+            file.Data.deallocate()
+            file.Data = MutableData(start: &buf[0], count: buf.count)
+            writeToDisk()
+        }
+    }
+
+    required init?(partition: Partition, path: String) {
+        super.init(partition: partition, path: path)
+        guard file.Info.Extension == "TXT" || file.Info.Extension == "PLI" || file.Info.Extension == "XML"  else {
+            
+        }
+    }
+}
