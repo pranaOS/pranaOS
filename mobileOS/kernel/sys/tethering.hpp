ca5ce1ce43402348410c962016e5af0dec3c0115
krishpranav <krisna.pranav@gmail.com>
2023-06-20 19:30:39 +0530

    mobileOS: kernel[sys(tethering)]

---
+++ mobileOS/kernel/sys/tethering.hpp
@@ -0,0 +1,19 @@
+/**
+ * @file tethering.hpp
+ * @author Krisna Pranav
+ * @brief Tethering
+ * @version 1.0
+ * @date 2023-06-20
+ * 
+ * @copyright Copyright (c) 2021-2023, pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+namespace sys::phonemodes {
+    enum class Tethering {
+        Off,
+        On
+    };
+}
