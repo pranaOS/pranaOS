e22da52f6a891df6f99d913ceb8f5df7fec3ba48
krishpranav <krisna.pranav@gmail.com>
2023-06-20 19:40:03 +0530

    mobileOS: kernel[service(message_forward)]

---
+++ mobileOS/kernel/sys/service/message_forward.hpp
@@ -0,0 +1,24 @@
+/**
+ * @file message_forward.hpp
+ * @author Krisna Pranav
+ * @brief Message Forward
+ * @version 1.0
+ * @date 2023-06-20
+ * 
+ * @copyright Copyright (c) 2021-2023, pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <memory>
+#include "../sys_return_codes.hpp"
+
+namespace sys {
+    class Service;
+    class Message;
+
+    using MessagePointer = std::shared_ptr<Message>;
+    using MessageNone = std::nullptr_t;
+    using SendResult     = std::pair<ReturnCodes, std::shared_ptr<Message>>;
+}
