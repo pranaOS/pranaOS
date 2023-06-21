b8c6c6e86dd251ff7d647bbd1016fcb251fb9b21
krishpranav <krisna.pranav@gmail.com>
2023-06-21 06:10:38 +0530

    kernel: pci[pci funcs]

--- kernel/pci/pci.cpp
+++ kernel/pci/pci.cpp
@@ -67,4 +67,42 @@ namespace PCI {
         comm.attrs.bus_master = false;
         write_word(address, PCI_COMMAND, comm.value);
     }
+
+    void enumerate_devices(PCIEnumerationCallback callback, void* dataPtr) {
+        if((read_byte({0,0,0}, PCI_HEADER_TYPE) & PCI_MULTIFUNCTION) == 0) {
+            enumerate_bus(0, callback, dataPtr);
+        } else {
+            for(uint8_t func = 0; func < 8; func++) {
+                if(read_word({0,0, func}, PCI_VENDOR_ID) != PCI_NONE)
+                    enumerate_bus(func, callback, dataPtr);
+            }
+        }
+    }
+
+    void enumerate_bus(uint8_t bus, PCIEnumerationCallback callback, void* dataPtr) {
+        for (uint8_t slot = 0; slot < 32; slot++) {
+            enumerate_slot(bus, slot, callback, dataPtr);
+        }
+    }
+
+    void enumerate_slot(uint8_t bus, uint8_t slot, PCIEnumerationCallback callback, void* dataPtr) {
+        Address addr = {bus, slot, 0};
+        if(read_word(addr, PCI_VENDOR_ID) == PCI_NONE) return;
+        enumerate_functions(bus, slot, 0, callback, dataPtr);
+        if(!read_byte(addr, PCI_HEADER_TYPE)) return;
+        for(uint8_t func = 1; func < 8; func++) {
+            if(read_word({bus, slot, func}, PCI_VENDOR_ID) != PCI_NONE) {
+                enumerate_functions(bus, slot, func, callback, dataPtr);
+            }
+        }
+    }
+
+    void enumerate_functions(uint8_t bus, uint8_t slot, uint8_t function, PCIEnumerationCallback callback, void* dataPtr) {
+        Address addr = {bus, slot, function};
+        if(get_type(addr) == PCI_TYPE_BRIDGE) {
+            uint8_t second_bus = read_byte(addr, PCI_SECONDARY_BUS);
+            enumerate_bus(second_bus, callback, dataPtr);
+        }
+        callback(addr, {read_word(addr, PCI_VENDOR_ID), read_word(addr, PCI_DEVICE_ID)}, get_type(addr), dataPtr);
+    }
 }
