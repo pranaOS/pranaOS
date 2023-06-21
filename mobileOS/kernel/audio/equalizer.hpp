a5393d4e16bbc1d3e2d8ee31483c13dbd58f244b
krishpranav <krisna.pranav@gmail.com>
2023-06-21 18:48:46 +0530

    mobileOS: kernel[audio(equalizer)]

--- mobileOS/kernel/audio/equalizer.hpp
+++ mobileOS/kernel/audio/equalizer.hpp
@@ -8,3 +8,44 @@
 * @copyright Copyright (c) 2021-2023, pranaOS Developers, Krisna Pranav
 *
  */
+
+#pragma once
+
+#include <cstdint>
+#include <tuple>
+
+namespace audio::equalizer {
+    constexpr inline auto bands = 5;
+
+    struct QFilterCoefficients {
+        float b0;
+        float b1;
+        float b2;
+        float a1;
+        float a2;
+
+        inline bool operator==(const QFilterCoefficients &rhs) const {
+            if (b0 != rhs.b0) {
+                return false;
+            }
+
+            if (b1 != rhs.b1) {
+                return false;
+            }
+
+            if (b2 != rhs.b2) {
+                return false;
+            }
+
+            if (a1 != rhs.a1) {
+                return false;
+            }
+
+            if (a2 != rhs.a2) {
+                return false;
+            }
+
+            return true;
+        }
+    };
+}
