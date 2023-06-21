cb803c24346bc5b97eeeef358754afb4cbc5583b
krishpranav <krisna.pranav@gmail.com>
2023-06-21 06:57:59 +0530

    watchOS: kernel[event_listener]

---
+++ watchOS/kernel/event/event_listener.h
@@ -0,0 +1,20 @@
+/**
+* @file event_listener.h
+* @author Krisna Pranav
+* @brief Event Listener
+* @version 1.0
+* @date 2023-06-20
+*
+* @copyright Copyright (c) 2021-2023, pranaOS Developers, Krisna Pranav
+*
+ */
+
+#pragma once
+
+#include <Arduino.h>
+
+class EventListener {
+  public:
+    virtual bool listen(uint8_t event);
+    virtual void handle();
+};
