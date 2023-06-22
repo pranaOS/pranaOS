ae269a867e7dbe9a8a6da784aecb871e57319be1
krishpranav <krisna.pranav@gmail.com>
2023-06-22 19:18:24 +0530

    kernel: tasking[waitblocker]

---
+++ kernel/tasking/waitblocker.h
@@ -0,0 +1,15 @@
+/**
+* @file waitblocker.h
+* @author Krisna Pranav
+* @brief WaitBlocker
+* @version 1.0
+*
+* @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+*
+ */
+
+#pragma once
+
+#include "blocker.h"
+#include "../kstd/unix_types.h"
+#include "../kstd/arc.h"
