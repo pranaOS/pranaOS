c4d09ed44aaa2823eae1a24a6b4dfbd14648915b
krishpranav <krisna.pranav@gmail.com>
2023-06-22 18:41:40 +0530

    kernel: interrupt[irq]

--- kernel/interrupt/irq.h
+++ kernel/interrupt/irq.h
@@ -12,14 +12,34 @@
 
 #define PIC1 0x20
 #define PIC2 0xA0
+#define PIC1_COMMAND PIC1
+#define PIC1_DATA (PIC1+1)
+#define PIC2_COMMAND PIC2
+#define PIC2_DATA (PIC2+1)
 
 class IRQHandler;
 
 namespace Interrupt {
-    extern "C" void irq_handler(struct Registers* reg);
+    extern "C" void irq0();
+    extern "C" void irq1();
+    extern "C" void irq2();
+    extern "C" void irq3();
+    extern "C" void irq4();
+    extern "C" void irq5();
+    extern "C" void irq6();
+    extern "C" void irq7();
+    extern "C" void irq8();
+    extern "C" void irq9();
+    extern "C" void irq10();
+    extern "C" void irq11();
+    extern "C" void irq12();
+    extern "C" void irq13();
+    extern "C" void irq14();
+    extern "C" void irq15();
+    extern "C" void irq_handler(struct Registers *r);
 
     void irq_set_handler(int irq, IRQHandler* handler);
-    void irq_remove_handler(int req);
+    void irq_remove_handler(int irq);
     void irq_remap();
     void irq_init();
     bool in_irq();
