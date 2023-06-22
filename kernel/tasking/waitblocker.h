fe2e577e48db58a2b14adb1cf18739bd996fe842
krishpranav <krisna.pranav@gmail.com>
2023-06-22 19:22:50 +0530

    kernel: tasking[waitblocker]

--- kernel/tasking/waitblocker.h
+++ kernel/tasking/waitblocker.h
@@ -13,3 +13,24 @@
 #include "blocker.h"
 #include "../kstd/unix_types.h"
 #include "../kstd/arc.h"
+
+class Thread;
+
+class WaitBlocker: public Blocker {
+  public:
+    WaitBlocker(kstd::Arc<Thread> thread, pid_t wait_for);
+    bool is_ready() override;
+
+    pid_t waited_pid();
+    Process* waited_process();
+    pid_t error();
+    pid_t exit_status();
+
+  private:
+    int _err = 0;
+    int _exit_status = 0;
+    Process* _waited_process = nullptr;
+    pid_t _wait_pid;
+    pid_t _wait_pgid;
+    kstd::Arc<Thread> _thread;
+};
