cd519875abbda1a5cacadcab8b678bf1c4555925
krishpranav <krisna.pranav@gmail.com>
2023-06-22 20:33:31 +0530

    kernel: time[PIT]

---
+++ kernel/time/PIT.h
@@ -0,0 +1,30 @@
+/**
+* @file PIT.h
+* @author Krisna Pranav
+* @brief PIT
+* @version 1.0
+*
+* @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+*
+ */
+
+#pragma once
+
+#define PIT_COUNTER0 0x40
+#define PIT_COUNTER1 0x41
+#define PIT_COUNTER2 0x42
+#define PIT_CMD  0x43
+#define PIT_IRQ 0
+#define PIT_FREQUENCY 1000
+
+#include "../interrupt/irqhandler.h"
+#include "timekeeper.h"
+
+class PIT: public TimeKeeper : public IRQHandler {
+  public:
+    PIT(TimeManager* manager);
+    void handle_irq(Registers* regs) override;
+    bool mark_in_req() override;
+
+    
+};
