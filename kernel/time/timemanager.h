777d713c28e815c4379cebca6fbe4ff037fdd99b
krishpranav <krisna.pranav@gmail.com>
2023-06-22 19:41:17 +0530

    kernel: time[timemanager]

---
+++ kernel/time/timemanager.h
@@ -0,0 +1,18 @@
+/**
+* @file timemanager.h
+* @author Krisna Pranav
+* @brief TimeManager
+* @version 1.0
+*
+* @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+*
+ */
+
+#pragma once
+
+#include "../kstd/unix_types.h"
+#include "timekeeper.h"
+
+class TimeManager {
+
+};
