d160da497454801da619daa4d4ce3be196cf9a78
krishpranav <krisna.pranav@gmail.com>
2023-06-22 20:46:45 +0530

    kernel: time[CMOS]

---
+++ kernel/time/CMOS.h
@@ -0,0 +1,35 @@
+/**
+ * @file CMOS.h
+ * @author Krisna Pranav
+ * @brief CMOS 
+ * @version 1.0
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#pragma once
+
+#include "../kstd/kstddef.h"
+
+#define CMOS_ADDRESS 0x70
+#define CMOS_DATA 0x71
+
+class CMOS {
+public:
+    /**
+     * @brief write
+     * 
+     * @param reg 
+     * @param val 
+     */
+    static void write(uint8_t reg, uint8_t val);
+
+    /**
+     * @brief read
+     * 
+     * @param reg 
+     * @return uint8_t 
+     */
+    static uint8_t read(uint8_t reg);
+};
