26f180a2d770a7893f68f1aa4ab678e03b493878
krishpranav <krisna.pranav@gmail.com>
2023-06-22 19:00:53 +0530

    kernel: tasking[blocker]

---
+++ kernel/tasking/blocker.h
@@ -0,0 +1,28 @@
+/**
+* @file blocker.h
+* @author Krisna Pranav
+* @brief Blocker
+* @version 1.0
+*
+* @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+*
+*/
+
+#pragma once
+
+class Process;
+class Thread;
+
+class Blocker {
+  public:
+    virtual bool is_ready() = 0;
+    virtual bool can_be_interrupted();
+    virtual bool is_lock();
+    virtual Thread* responsible_thread();
+
+  protected:
+    virtual void on_interrupted();
+
+  private:
+    bool _interrupted = false;
+};
