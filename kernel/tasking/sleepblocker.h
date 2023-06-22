b9dc6a493597544ba1a5b6703934efd518fba88f
krishpranav <krisna.pranav@gmail.com>
2023-06-22 19:31:29 +0530

    kernel: tasking[sleepblocker]

---
+++ kernel/tasking/sleepblocker.h
@@ -0,0 +1,15 @@
+/**
+* @file sleepblocker.h
+* @author Krisna Pranav
+* @brief SleepBlocker
+* @version 1.0
+*
+* @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+*
+ */
+
+#pragma once
+
+#include "blocker.h"
+#include "../kstd/kstddef.h"
+#include "../time/Time.h"
