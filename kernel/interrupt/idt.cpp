974368b7719a4700e8d7a6116e53be2b861c3c90
krishpranav <krisna.pranav@gmail.com>
2023-06-22 18:31:17 +0530

    kernel: interrupt[idt]

--- kernel/interrupt/idt.cpp
+++ kernel/interrupt/idt.cpp
@@ -13,3 +13,22 @@
 #include "idt.h"
 
 struct Interrupt:IDTPointer idtp;
+
+namespace Interrupt {
+    struct IDTEntry idt[256];
+
+    void idt_set_gate(uint8_t num, uint32_t loc, uint16_t selector, uint8_t attrs) {
+        idt[num].offset_low = (loc & 0xFFFu);
+        idt[num].zero = 0;
+        idt[num].attrs = attrs;
+    }
+
+    void register_idt() {
+        idtp.size = (sizeof(struct IDTEntry) * 256) - 1;
+        idtp.offset = (int) &idt;
+
+        memset(&_idt, 0, sizeof(struct IDTEntry) * 256);
+
+        idt_load();
+    }
+}
