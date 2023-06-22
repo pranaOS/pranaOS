ae9fc347af39d7912a44164d89c8c36b1a072301
krishpranav <krisna.pranav@gmail.com>
2023-06-22 19:06:39 +0530

    kernel: tasking[signal]

---
+++ kernel/tasking/signal.h
@@ -0,0 +1,20 @@
+/**
+* @file blocker.h
+* @author Krisna Pranav
+* @brief Blocker
+* @version 1.0
+*
+* @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+*
+ */
+
+#pragma once
+
+// signal returns up fake addr
+#define SIGNAL_RETURN_FAKE_ADDR 0xFFFFB00F
+
+namespace Signal {
+    class SigAction {
+
+    };
+}
