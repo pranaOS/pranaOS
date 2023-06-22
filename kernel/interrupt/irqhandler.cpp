b10c3fc19445afd70ad816ab581a7d2c0fdd5edc
krishpranav <krisna.pranav@gmail.com>
2023-06-22 18:44:18 +0530

    kernel: interrupt[irqhandler]

--- kernel/interrupt/irqhandler.cpp
+++ kernel/interrupt/irqhandler.cpp
@@ -11,3 +11,22 @@
 #include "../kstd/kstdio.h"
 #include "irqhandler.h"
 #include "irq.h"
+
+IRQHandler::IRQHandler() {}
+
+void IRQHandler::handle(Registers* regs) {
+    _sent_eoi = false;
+    handle_irq(regs);
+}
+
+bool IRQHandler::sent_eoi() {
+    return _sent_eoi;
+}
+
+IRQHandler::IRQHandler(int irq) : _irq(irq) {
+    Interrupt::irq_set_handler(irq, this);
+}
+
+void IRQHandler::set_irq(int irg) {
+    _irq = irq;
+}
