8b988392839a91f66333a314a8477bde2f2a2c17
krishpranav <krisna.pranav@gmail.com>
2023-06-22 18:25:42 +0530

    kernel: interrupt[idt]

---
+++ kernel/interrupt/idt.h
@@ -0,0 +1,26 @@
+/**
+* @file idt.h
+* @author Krisna Pranav
+* @brief IDT
+* @version 0.1
+* @date 2023-05-29
+*
+* @copyright Copyright (c) 2023 pranaOS Developers, Krisna Pranav
+*
+*/
+
+#pragma once
+
+namespace Interrupt {
+    struct IDTEntry {
+        unsigned short offset_low;
+        unsigned short selector;
+        unsigned char zero;
+        unsigned char attrs;
+        unsigned short offset_high;
+    } __attribute__((packed));
+
+    extern "C" void idt_load();
+
+    void register_idt();
+}
