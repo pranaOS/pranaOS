1be29535ec551d9dc9fb0aa2748bed354fe4dabb
krishpranav <krisna.pranav@gmail.com>
2023-06-22 18:40:46 +0530

    kernel: interrupt[irqhandler]

---
+++ kernel/interrupt/irqhandler.cpp
@@ -0,0 +1,13 @@
+/**
+* @file irqhandler.cpp
+* @author Krisna Pranav
+* @brief irqhandler
+* @version 1.0
+*
+* @copyright Copyright (c) 2023 pranaOS Developers, Krisna Pranav
+*
+ */
+
+#include "../kstd/kstdio.h"
+#include "irqhandler.h"
+#include "irq.h"
