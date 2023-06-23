96721329e7b1a2cdacac4d75b780efd1ab6a2273
krishpranav <krisna.pranav@gmail.com>
2023-06-23 19:35:43 +0530

    kernel: time[CMOS]

---
+++ kernel/time/CMOS.cpp
@@ -0,0 +1,22 @@
+/**
+* @file CMOS.cpp
+* @author Krisna Pranav
+* @brief CMOS
+* @version 1.0
+*
+* @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+*
+ */
+
+#include "CMOS.h"
+#include "../io.h"
+
+void CMOS::write(uint8_t reg, uint8_t val) {
+    IO::outb(CMOS_ADDRESS, reg);
+    IO::outb(CMOS_DATA, val);
+}
+
+uint8_t CMOS::read(uint8_t reg) {
+    IO::outb(CMOS_ADDRESS, reg);
+    return IO::inb(CMOS_DATA);
+}
