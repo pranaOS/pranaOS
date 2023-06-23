6f9a3fcef5f18c506f56238639e881b09a9a5d86
krishpranav <krisna.pranav@gmail.com>
2023-06-23 20:25:54 +0530

    kernel: time[timemanager]

---
+++ kernel/time/timemanager.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file timemanager.cpp
+ * @author Krisna Pranav
+ * @brief TimeManager
+ * @version 1.0
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include "timemanager.h"
+#include "PIT.h"
+#include "RTC.h"
+
+TimeManager* TimeManager::_inst = nullptr;
+
+void TimeManager::init() {
+    if(_inst)
+        return;
+    
+    _inst = new TimeManager();
+    _inst->_keeper->enable();
+}
