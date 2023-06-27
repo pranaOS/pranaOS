c40ba27e3395bf231bb28dbb8db536160345ab3f
krishpranav <krisna.pranav@gmail.com>
2023-06-27 20:10:07 +0530

    mods: iterdecision

---
+++ mods/iterdecision.h
@@ -0,0 +1,21 @@
+/**
+ * @file iterdecision.h
+ * @author Krisna Pranav
+ * @brief Iterator Decicion Maker
+ * @version 1.0
+ * @date 2023-06-27
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+namespace Mods {
+    enum class IterDecision {
+        Continue,
+        Break,
+    };
+}
+
+using Mods::IterDecision;
