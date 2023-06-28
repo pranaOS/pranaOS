82fae8c53d3e9d4aeb40b2efbdd4f77747044ac0
krishpranav <krisna.pranav@gmail.com>
2023-06-28 22:02:37 +0530

    libc: alloc

---
+++ libraries/libc/alloca.h
@@ -0,0 +1,14 @@
+/**
+ * @file alloc.h
+ * @author Krisna Pranav
+ * @brief alloc
+ * @version 1.0
+ * @date 2023-06-28
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#define alloca __builtin_alloca
