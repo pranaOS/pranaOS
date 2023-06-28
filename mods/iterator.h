95769408465f4f5889f8ccf6e11d236da75ba4a9
krishpranav <krisna.pranav@gmail.com>
2023-06-28 22:14:50 +0530

    mods: iterator

--- mods/iterator.h
+++ mods/iterator.h
@@ -26,7 +26,7 @@ namespace Mods {
          * @return true 
          * @return false 
          */
-        constexpr bool is_end() const {
+        constexpr bool is_end() const { 
             return m_index == SimpleIterator::end(m_container).m_index; 
         }
 
@@ -35,9 +35,161 @@ namespace Mods {
          * 
          * @return constexpr size_t 
          */
-        constexpr size_t index() const {
+        constexpr size_t index() const { 
             return m_index;
         }
+
+        /**
+         * @brief operator==
+         * 
+         * @param other 
+         * @return true 
+         * @return false 
+         */
+        constexpr bool operator==(SimpleIterator other) const { 
+            return m_index == other.m_index; 
+        }
+
+        /**
+         * @brief operator!=
+         * 
+         * @param other 
+         * @return true 
+         * @return false 
+         */
+        constexpr bool operator!=(SimpleIterator other) const { 
+            return m_index != other.m_index;
+        }
+
+        /**
+         * @brief operator<
+         * 
+         * @param other 
+         * @return true 
+         * @return false 
+         */
+        constexpr bool operator<(SimpleIterator other) const { 
+            return m_index < other.m_index; 
+        }
+
+        /**
+         * @brief operator>
+         * 
+         * @param other 
+         * @return true 
+         * @return false 
+         */
+        constexpr bool operator>(SimpleIterator other) const { 
+            return m_index > other.m_index; 
+        }
+
+        /**
+         * @brief <=
+         * 
+         * @param other 
+         * @return true 
+         * @return false 
+         */
+        constexpr bool operator<=(SimpleIterator other) const { 
+            return m_index <= other.m_index; 
+        }
+
+        /**
+         * @brief >=
+         * 
+         * @param other 
+         * @return true 
+         * @return false 
+         */
+        constexpr bool operator>=(SimpleIterator other) const { 
+            return m_index >= other.m_index; 
+        }
+
+        /**
+         * @brief +
+         * 
+         * @param delta 
+         * @return constexpr SimpleIterator 
+         */
+        constexpr SimpleIterator operator+(ptrdiff_t delta) const { 
+            return SimpleIterator { m_container, m_index + delta };
+        }
+
+        /**
+         * @brief -
+         * 
+         * @param delta 
+         * @return constexpr SimpleIterator 
+         */
+        constexpr SimpleIterator operator-(ptrdiff_t delta) const { 
+            return SimpleIterator { m_container, m_index - delta }; 
+        }
+
+        /**
+         * @brief -
+         * 
+         * @param other 
+         * @return constexpr ptrdiff_t 
+         */
+        constexpr ptrdiff_t operator-(SimpleIterator other) const { 
+            return static_cast<ptrdiff_t>(m_index) - other.m_index; 
+        }
+        
+        /**
+         * @brief ++
+         * 
+         * @return constexpr SimpleIterator 
+         */
+        constexpr SimpleIterator operator++() {
+            ++m_index;
+            return *this;
+        }
+
+        /**
+         * @brief ++
+         * 
+         * @return constexpr SimpleIterator 
+         */
+        constexpr SimpleIterator operator++(int) {
+            ++m_index;
+            return SimpleIterator { m_container, m_index - 1 };
+        }
+
+        /**
+         * @brief --
+         * 
+         * @return constexpr SimpleIterator 
+         */
+        constexpr SimpleIterator operator--() {
+            --m_index;
+            return *this;
+        }
+
+        /**
+         * @brief --
+         * 
+         * @return constexpr SimpleIterator 
+         */
+        constexpr SimpleIterator operator--(int) {
+            --m_index;
+            return SimpleIterator { m_container, m_index + 1 };
+        }
+
+        /**
+         * @brief *
+         * 
+         * @return ALWAYS_INLINE constexpr const& 
+         */
+        ALWAYS_INLINE constexpr const ValueType& operator*() const { return m_container[m_index]; }
+        ALWAYS_INLINE constexpr ValueType& operator*() { return m_container[m_index]; }
+
+        /**
+         * @brief ->
+         * 
+         * @return constexpr const ValueType* 
+         */
+        constexpr const ValueType* operator->() const { return &m_container[m_index]; }
+        constexpr ValueType* operator->() { return &m_container[m_index]; }
 
     private:
         /**
@@ -46,20 +198,37 @@ namespace Mods {
          * @param container 
          * @return constexpr SimpleIterator 
          */
-        static constexpr SimpleIterator begin(Container& container) {
-            return { continer, 0 };
-        };
+        static constexpr SimpleIterator begin(Container& container) { 
+            return { container, 0 }; 
+        }
+        
+        /**
+         * @brief end
+         * 
+         * @param container 
+         * @return constexpr SimpleIterator 
+         */
+        static constexpr SimpleIterator end(Container& container) {
+            using RawContainerType = typename RemoveCV<Container>::Type;
+
+            if constexpr (IsSame<StringView, RawContainerType>::value || IsSame<String, RawContainerType>::value)
+                return { container, container.length() };
+            else
+                return { container, container.size() };
+        }
 
         /**
-         * @brief SimpleIterator
+         * @brief simpleiterator
          * 
          */
-        constexpr SimpleIterator(Container& container, size_t index) 
+        constexpr SimpleIterator(Container& container, size_t index)
             : m_container(container)
             , m_index(index)
-        {}
+        {
+        }
 
         Container& m_container;
-        size_T m_index;
+        size_t m_index;
     };
+
 }
