7bf354612bf9a1b22eb97de6947f2cdf2f38cf74
krishpranav <krisna.pranav@gmail.com>
2023-06-28 20:39:39 +0530

    libc: bits[stdint]

---
+++ libraries/libc/bits/stdint.h
@@ -0,0 +1,20 @@
+/**
+ * @file stdint.h
+ * @author Krisna Pranav
+ * @brief stdint
+ * @version 1.0
+ * @date 2023-06-28
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
+#define SIZE_MAX ((size_t)-1)
+
+__END_DECLS
