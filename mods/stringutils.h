fa87187cefa0025961c2044048f2f383556ece28
krishpranav <krisna.pranav@gmail.com>
2023-06-28 23:14:13 +0530

    mods: stringutils

--- mods/stringutils.h
+++ mods/stringutils.h
@@ -14,27 +14,48 @@
 #include "forward.h"
 
 namespace Mods {
+
+    /**
+     * @brief CaseSensitivity
+     * 
+     */
     enum class CaseSensitivity {
         CaseInsensitive,
-        Casesensitive,
+        CaseSensitive,
     };
 
+    // TrimMode
     enum class TrimMode {
         Left,
         Right,
         Both
     };
 
-
-    struct MarkSpan {
+    // MarkSpan
+    struct MaskSpan {
         size_t start;
         size_t length;
 
-        bool operator==(const MarkSpan& other) const {
+        /**
+         * @brief ==
+         * 
+         * @param other 
+         * @return true 
+         * @return false 
+         */
+        bool operator==(const MaskSpan& other) const {
             return start == other.start && length == other.length;
         }
 
-        bool operator!=(const MarkSpan& other) const {
+        /**
+         * @brief !=
+         * 
+         * @param other 
+         * @return true 
+         * @return false 
+         */
+        bool operator!=(const MaskSpan& other) const
+        {
             return !(*this == other);
         }
     };
@@ -50,6 +71,45 @@ namespace Mods {
          * @return false 
          */
         bool matches(const StringView& str, const StringView& mask, CaseSensitivity = CaseSensitivity::CaseInsensitive, Vector<MaskSpan>* match_spans = nullptr);
+
+        /**
+         * @brief convert_to_int
+         * 
+         * @return Optional<int> 
+         */
+        Optional<int> convert_to_int(const StringView&);
+
+        /**
+         * @brief convert_to_uint
+         * 
+         * @return Optional<unsigned> 
+         */
+        Optional<unsigned> convert_to_uint(const StringView&);
+
+        /**
+         * @brief convert_to_uint_from_hex
+         * 
+         * @return Optional<unsigned> 
+         */
+        Optional<unsigned> convert_to_uint_from_hex(const StringView&);
+
+        /**
+         * @brief equals_ignoring_case
+         * 
+         * @return true 
+         * @return false 
+         */
+        bool equals_ignoring_case(const StringView&, const StringView&);
+
+        /**
+         * @brief ends_with
+         * 
+         * @param a 
+         * @param b 
+         * @return true 
+         * @return false 
+         */
+        bool ends_with(const StringView& a, const StringView& b, CaseSensitivity);
 
         /**
          * @brief starts_with
@@ -68,11 +128,16 @@ namespace Mods {
         bool contains(const StringView&, const StringView&, CaseSensitivity);
 
         /**
-         * @brief trim_whitespaces
+         * @brief trim_whitespace
          * 
          * @param mode 
          * @return StringView 
          */
-        StringView trim_whitespaces(const StringView&, TrimMode mode);
+        StringView trim_whitespace(const StringView&, TrimMode mode);
     }
+
 }
+
+// using mods
+using Mods::CaseSensitivity;
+using Mods::TrimMode;
