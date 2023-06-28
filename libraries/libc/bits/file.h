9c1fed9b1bef2ed3d059fcee3ea3b92f418e1819
krishpranav <krisna.pranav@gmail.com>
2023-06-28 22:05:25 +0530

    libc: bits[file]

---
+++ libraries/libc/bits/file.h
@@ -0,0 +1,23 @@
+/**
+ * @file file.h
+ * @author Krisna Pranav
+ * @brief File
+ * @version 1.0
+ * @date 2023-06-28
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <sys/cdefs.h>
+#include <sys/types.h>
+
+#define BUFSIZ 1024
+
+__BEGIN_DECLS
+
+typedef struct FILE FILE;
+
+__END_DECLS
