05213039d4d08649b175f3a453b8c13df262a8fd
krishpranav <krisna.pranav@gmail.com>
2023-07-01 15:28:02 +0530

    mods: shared_buffer

---
+++ mods/shared_buffer.h
@@ -0,0 +1,18 @@
+/**
+ * @file shared_buffer.h
+ * @author Krisna Pranav
+ * @brief Shared_Buffer
+ * @version 1.0
+ * @date 2023-07-01
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#if defined(__pranaos__) || defined(__linux__)
+
+#include "refcounted.h"
+
+#endif 
