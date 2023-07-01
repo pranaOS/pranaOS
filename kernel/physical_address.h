42932143db4dd6fba06ac86c8cff36682470d29d
krishpranav <krisna.pranav@gmail.com>
2023-07-01 17:06:25 +0530

    kernel: physical_address

--- kernel/physical_address.h
+++ kernel/physical_address.h
@@ -14,46 +14,148 @@
 #include <mods/types.h>
 #include <mods/logstream.h>
 
-class PhyscialAddress {
+class PhysicalAddress {
 public:
-    PhyscialAddress() {}
+    PhysicalAddress() {}
 
-    explicit PhyscialAddress(FlatPtr address)
+    /**
+     * @brief Construct a new Physical Address object
+     * 
+     * @param address 
+     */
+    explicit PhysicalAddress(FlatPtr address)
         : m_address(address)
-    {}
-
-    PhyscialAddress offset(FlatPtr 0) const {
-        return PhyscialAddress(m_address + o);
+    {
     }
 
-    FlatPtr get() const {
-        return m_address;
+    /**
+     * @param o 
+     * @return PhysicalAddress 
+     */
+    PhysicalAddress offset(FlatPtr o) const { 
+        return PhysicalAddress(m_address + o); 
     }
 
+    /**
+     * @return FlatPtr 
+     */
+    FlatPtr get() const { 
+        return m_address; 
+    }
+
+    /**
+     * @param address 
+     */
     void set(FlatPtr address) {
-        m_address = address;
+        m_address = address; 
     }
 
-    void mask(FlatPtr m) {
-        m_address = m;
+    /**
+     * @param m 
+     */
+    void mask(FlatPtr m) { 
+        m_address &= m; 
+    }   
+
+    /**
+     * @return true 
+     * @return false 
+     */
+    bool is_null() const { 
+        return m_address == 0; 
     }
 
-    bool is_null() const {
-        return m_address == 0;
+    /**
+     * @return u8* 
+     */
+    u8* as_ptr() { 
+        return reinterpret_cast<u8*>(m_address); 
     }
 
-    u8* as_ptr() {
-        return reinterpret_cast<u8*>(m_address);
+    /**
+     * @return const u8* 
+     */
+    const u8* as_ptr() const { 
+        return reinterpret_cast<const u8*>(m_address); 
     }
 
-    const u8* as_ptr() const {
-        return PhyscialAddress(m_address & 0xfffffff);
+    /**
+     * @return PhysicalAddress 
+     */
+    PhysicalAddress page_base() const { 
+        return PhysicalAddress(m_address & 0xfffff000); 
+    }
+
+    /**
+     * @return FlatPtr 
+     */
+    FlatPtr offset_in_page() const { 
+        return PhysicalAddress(m_address & 0xfff).get(); 
+    }
+
+    /**
+     * @param other 
+     * @return true 
+     * @return false 
+     */
+    bool operator==(const PhysicalAddress& other) const { 
+        return m_address == other.m_address; 
+    }
+
+    /**
+     * @param other 
+     * @return true 
+     * @return false 
+     */
+    bool operator!=(const PhysicalAddress& other) const { 
+        return m_address != other.m_address; 
+    }
+
+    /**
+     * @param other 
+     * @return true 
+     * @return false 
+     */
+    bool operator>(const PhysicalAddress& other) const { 
+        return m_address > other.m_address; 
+    }
+
+    /**
+     * @param other 
+     * @return true 
+     * @return false 
+     */
+    bool operator>=(const PhysicalAddress& other) const { 
+        return m_address >= other.m_address; 
+    }
+
+    /**
+     * @param other 
+     * @return true 
+     * @return false 
+     */
+    bool operator<(const PhysicalAddress& other) const { 
+        return m_address < other.m_address; 
+    }
+
+    /**
+     * @param other 
+     * @return true 
+     * @return false 
+     */
+    bool operator<=(const PhysicalAddress& other) const { 
+        return m_address <= other.m_address; 
     }
 
 private:
     FlatPtr m_address { 0 };
 };
 
-inline const LogStream& operator<<<(const LogStream& stream) {
-    return stream;
+/**
+ * @param stream 
+ * @param value 
+ * @return const LogStream& 
+ */
+inline const LogStream& operator<<(const LogStream& stream, PhysicalAddress value) {
+    return stream << 'P' << value.as_ptr();
 }
