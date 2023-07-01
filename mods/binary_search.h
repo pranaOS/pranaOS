d6fefcdff33318453d06c4e568d9e4c31f506bf0
krishpranav <krisna.pranav@gmail.com>
2023-07-01 12:39:01 +0530

    mods: binary_search

--- mods/binary_search.h
+++ mods/binary_search.h
@@ -16,13 +16,29 @@
 #include "types.h"
 
 namespace Mods {
+
+    /**
+     * @tparam T 
+     * @param a 
+     * @param b 
+     * @return constexpr int 
+     */
     template<typename T>
     constexpr int integral_compare(const typename RemoveConst<T>::Type& a, const typename RemoveConst<T>::Type& b) {
         return a - b;
     }
 
-    template<typename T, typename Compare> 
-    constexpr T* binary_search(Span<T> haystack, const typename RemoveConst<T>::Type& needle, Compare compare = integral_compare, size_t* nearby_index = nullptr)  {
+    /**
+     * @tparam T 
+     * @tparam Compare 
+     * @param haystack 
+     * @param needle 
+     * @param compare 
+     * @param nearby_index 
+     * @return constexpr T* 
+     */
+    template<typename T, typename Compare>
+    constexpr T* binary_search(Span<T> haystack, const typename RemoveConst<T>::Type& needle, Compare compare = integral_compare, size_t* nearby_index = nullptr) {
         if (haystack.size() == 0) {
             if (nearby_index)
                 *nearby_index = 0;
@@ -31,23 +47,29 @@ namespace Mods {
 
         size_t low = 0;
         size_t high = haystack.size() - 1;
-
         while (low <= high) {
             size_t middle = low + ((high - low) / 2);
-            int comparision = compare(needle, haystack[front]);
-            if (comparision < 0)
-                if (front != 0)
+            int comparison = compare(needle, haystack[middle]);
+            if (comparison < 0)
+                if (middle != 0)
                     high = middle - 1;
                 else
                     break;
+            else if (comparison > 0)
+                low = middle + 1;
             else {
                 if (nearby_index)
                     *nearby_index = middle;
+                return &haystack[middle];
             }
         }
 
+        if (nearby_index)
+            *nearby_index = min(low, high);
+
         return nullptr;
     }
+
 }
 
 using Mods::binary_search;
