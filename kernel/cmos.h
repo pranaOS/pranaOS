d8034c8b4319db3f5f6aafb439b172714ca6f665
krishpranav <krisna.pranav@gmail.com>
2023-07-04 18:00:00 +0530

    kernel: cmos

---
+++ kernel/cmos.h
@@ -0,0 +1,29 @@
+/**
+ * @file cmos.h
+ * @author Krisna Pranav
+ * @brief CMOS
+ * @version 1.0
+ * @date 2023-07-04
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <mods/types.h>
+
+namespace CMOS {
+    /**
+     * 
+     * @param index 
+     * @return u8 
+     */
+    u8 read(u8 index);
+
+    /**
+     * @param index 
+     * @param data 
+     */
+    void write(u8 index, u8 data);
+}
