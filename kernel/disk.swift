d0e9b5f44fd17281cda468378c42e0198e0136d7
krishpranav <krisna.pranav@gmail.com>
2023-07-04 12:05:43 +0530

    kernel: disk[read & write]

---
+++ kernel/disk.swift
@@ -0,0 +1,20 @@
+protocol Disk: CustomStringConverable {
+    func read(lba: UInt64, count: Int, buffer: UnsafeMutbleBuffer<UInt8>) -> MutableData
+    func write(lba: UInt64, count: Int, buffer: UnsafeMutblePointer<UInt8>)
+    func PutDriveToSleep()
+}
+
+extension Disk {
+    func read(lba: UInt64, count: Int) -> MutableData {
+        let LBA = UnsafeMutblePointer<UInt8>(start: UnsafeMutblePointer<UInt8>.allocate(capacity: 512 * count), count: 512 * count)
+        return read(lba: lba, count: count, buffer: LBA)
+    }
+
+    func read(lba: UInt64) -> MutableData {
+        return read(lba: lba, count: 1)
+    }
+
+    func write(lba: UInt64, buffer: UnsafeMutblePointer<UInt8>) {
+        return write(lba: lba, count: 1, buffer: buffer)
+    }
+}
