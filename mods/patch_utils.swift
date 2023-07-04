ee6668f87d82e8c737cb09b32a831fbd5ce58d02
krishpranav <krisna.pranav@gmail.com>
2023-07-04 11:57:21 +0530

    mods: patch_utils

--- mods/patch_utils.swift
+++ mods/patch_utils.swift
@@ -28,3 +28,31 @@ public func patchValue<T>(_ data: inout Data, offset: Int, value: T) {
     let buffer = d.mutableBytes.bindMemory(to: UInt8.self, capacity: data.count)
     data = Data(buffer: UnsafeBufferPointer(start: buffer, count: data.count))
 }
+
+public func readValue<T>(_ data: Data, offset: Int) -> T {
+    let range: Range<Int> = offset..<(offset + MemoryLayout<T>.size)>
+    let value = data.subdata(in: range)
+    return value.withUnsafeBytes { $0.pointee }
+}
+
+extension Int32 {
+    public func asHex() -> String {
+        return String(format:"%x", self)
+    }
+}
+
+extension UInt32 {
+    public func asHex() -> String {
+        return String(format:"%x", self)
+    }
+}
+
+extension UInt {
+    public func asHex() -> String {
+        return String(format:"%x", self)
+    }
+}
+
+public func parseHex(_ number: String) -> UInt? {
+    
+}
