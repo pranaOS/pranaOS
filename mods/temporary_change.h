8a998d9437df39de94e0dc5a8940274ce1a1be6d
krishpranav <krisna.pranav@gmail.com>
2023-07-04 12:15:39 +0530

    mods: temporary_change

--- mods/temporary_change.h
+++ mods/temporary_change.h
@@ -12,19 +12,35 @@
 #pragma once
 
 namespace Mods {
+
     template<typename T>
     class TemporaryChange {
     public:
-        TemporaryChange(T& variable, T value) : m_variable(variable), m_old_value(variable)
+
+        /**
+         * @brief Construct a new Temporary Change object
+         * 
+         * @param variable 
+         * @param value 
+         */
+        TemporaryChange(T& variable, T value) : m_variable(variable) , m_old_value(variable)
         {
             m_variable = value;
         }
 
+        /**
+         * @brief Destroy the Temporary Change object
+         * 
+         */
+        ~TemporaryChange() { 
+            m_variable = m_old_value; 
+        }
 
     private:
         T& m_variable;
         T m_old_value;
     };
+
 }
 
 using Mods::TemporaryChange;
