75f825417edac33dec1c4ffbaa3a5c0aa8ba8276
krishpranav <krisna.pranav@gmail.com>
2023-07-04 12:26:42 +0530

    mods: time

--- mods/time.h
+++ mods/time.h
@@ -12,6 +12,7 @@
 #pragma once
 
 namespace Mods {
+
     /**
      * @param year 
      * @param month 
@@ -51,20 +52,17 @@ namespace Mods {
     inline unsigned days_in_year(int year) {
         return 365 + is_leap_year(year);
     }
-    
+
     /**
      * @param year 
      * @return int 
      */
     inline int years_to_days_since_epoch(int year) {
         int days = 0;
-
         for (int current_year = 1970; current_year < year; ++current_year)
             days += days_in_year(current_year);
-        
-        for(int current_year = year; current_year < 1970; ++current_year)
+        for (int current_year = year; current_year < 1970; ++current_year)
             days -= days_in_year(current_year);
-        
         return days;
     }
 
@@ -77,9 +75,211 @@ namespace Mods {
     template<typename TimevalType>
     inline void timeval_sub(const TimevalType& a, const TimevalType& b, TimevalType& result) {
         result.tv_sec = a.tv_sec - b.tv_sec;
-        result.tv_usec = a.tv_usec = b.tv_usec;
-
-        if (result.tv_usec < 0)
+        result.tv_usec = a.tv_usec - b.tv_usec;
+        if (result.tv_usec < 0) {
             --result.tv_sec;
+            result.tv_usec += 1'000'000;
+        }
+    }   
+
+    /**
+     * @tparam TimevalType 
+     * @param a 
+     * @param b 
+     * @param result 
+     */
+    template<typename TimevalType>
+    inline void timeval_add(const TimevalType& a, const TimevalType& b, TimevalType& result) {
+        result.tv_sec = a.tv_sec + b.tv_sec;
+        result.tv_usec = a.tv_usec + b.tv_usec;
+        if (result.tv_usec >= 1'000'000) {
+            ++result.tv_sec;
+            result.tv_usec -= 1'000'000;
+        }
+    }   
+
+    /**
+     * @tparam TimespecType 
+     * @param a 
+     * @param b 
+     * @param result 
+     */
+    template<typename TimespecType>
+    inline void timespec_sub(const TimespecType& a, const TimespecType& b, TimespecType& result) {
+        result.tv_sec = a.tv_sec - b.tv_sec;
+        result.tv_nsec = a.tv_nsec - b.tv_nsec;
+        if (result.tv_nsec < 0) {
+            --result.tv_sec;
+            result.tv_nsec += 1'000'000'000;
+        }
     }
+
+    /**
+     * @tparam TimespecType 
+     * @tparam TimevalType 
+     * @param a 
+     * @param b 
+     * @param result 
+     */
+    template<typename TimespecType, typename TimevalType>
+    inline void timespec_sub_timeval(const TimespecType& a, const TimevalType& b, TimespecType& result) {
+        result.tv_sec = a.tv_sec - b.tv_sec;
+        result.tv_nsec = a.tv_nsec - b.tv_usec * 1000;
+        if (result.tv_nsec < 0) {
+            --result.tv_sec;
+            result.tv_nsec += 1'000'000'000;
+        }
+    }   
+
+    /**
+     * 
+     * @tparam TimespecType 
+     * @param a 
+     * @param b 
+     * @param result 
+     */
+    template<typename TimespecType>
+    inline void timespec_add(const TimespecType& a, const TimespecType& b, TimespecType& result) {
+        result.tv_sec = a.tv_sec + b.tv_sec;
+        result.tv_nsec = a.tv_nsec + b.tv_nsec;
+        if (result.tv_nsec >= 1000'000'000) {
+            ++result.tv_sec;
+            result.tv_nsec -= 1000'000'000;
+        }
+    }
+
+    /**
+     * @tparam TimespecType 
+     * @tparam TimevalType 
+     * @param a 
+     * @param b 
+     * @param result 
+     */
+    template<typename TimespecType, typename TimevalType>
+    inline void timespec_add_timeval(const TimespecType& a, const TimevalType& b, TimespecType& result) {
+        result.tv_sec = a.tv_sec + b.tv_sec;
+        result.tv_nsec = a.tv_nsec + b.tv_usec * 1000;
+        if (result.tv_nsec >= 1000'000'000) {
+            ++result.tv_sec;
+            result.tv_nsec -= 1000'000'000;
+        }
+    }
+
+    /**
+     * @tparam TimevalType 
+     * @tparam TimespecType 
+     * @param tv 
+     * @param ts 
+     */
+    template<typename TimevalType, typename TimespecType>
+    inline void timeval_to_timespec(const TimevalType& tv, TimespecType& ts) {
+        ts.tv_sec = tv.tv_sec;
+        ts.tv_nsec = tv.tv_usec * 1000;
+    }
+
+    /**
+     * @tparam TimespecType 
+     * @tparam TimevalType 
+     * @param ts 
+     * @param tv 
+     */
+    template<typename TimespecType, typename TimevalType>
+    inline void timespec_to_timeval(const TimespecType& ts, TimevalType& tv) {
+        tv.tv_sec = ts.tv_sec;
+        tv.tv_usec = ts.tv_nsec / 1000;
+    }
+
+    /**
+     * @tparam TimespecType 
+     * @param a 
+     * @param b 
+     * @return true 
+     * @return false 
+     */
+    template<typename TimespecType>
+    inline bool operator>=(const TimespecType& a, const TimespecType& b) {
+        return a.tv_sec > b.tv_sec || (a.tv_sec == b.tv_sec && a.tv_nsec >= b.tv_nsec);
+    }
+
+    /**
+     * @tparam TimespecType 
+     * @param a 
+     * @param b 
+     * @return true 
+     * @return false 
+     */
+    template<typename TimespecType>
+    inline bool operator>(const TimespecType& a, const TimespecType& b) {
+        return a.tv_sec > b.tv_sec || (a.tv_sec == b.tv_sec && a.tv_nsec > b.tv_nsec);
+    }
+
+    /**
+     * @tparam TimespecType 
+     * @param a 
+     * @param b 
+     * @return true 
+     * @return false 
+     */
+    template<typename TimespecType>
+    inline bool operator<(const TimespecType& a, const TimespecType& b) {
+        return a.tv_sec < b.tv_sec || (a.tv_sec == b.tv_sec && a.tv_nsec < b.tv_nsec);
+    }
+
+    /**
+     * @tparam TimespecType 
+     * @param a 
+     * @param b 
+     * @return true 
+     * @return false 
+     */
+    template<typename TimespecType>
+    inline bool operator<=(const TimespecType& a, const TimespecType& b) {
+        return a.tv_sec < b.tv_sec || (a.tv_sec == b.tv_sec && a.tv_nsec <= b.tv_nsec);
+    }
+
+    /**
+     * @tparam TimespecType 
+     * @param a 
+     * @param b 
+     * @return true 
+     * @return false 
+     */
+    template<typename TimespecType>
+    inline bool operator==(const TimespecType& a, const TimespecType& b) {
+        return a.tv_sec == b.tv_sec && a.tv_nsec == b.tv_nsec;
+    }
+
+    /**
+     * @tparam TimespecType 
+     * @param a 
+     * @param b 
+     * @return true 
+     * @return false 
+     */
+    template<typename TimespecType>
+    inline bool operator!=(const TimespecType& a, const TimespecType& b) {
+        return a.tv_sec != b.tv_sec || a.tv_nsec != b.tv_nsec;
+    }
+
 }
+
+using Mods::day_of_week;
+using Mods::day_of_year;
+using Mods::days_in_month;
+using Mods::days_in_year;
+using Mods::is_leap_year;
+using Mods::timespec_add;
+using Mods::timespec_add_timeval;
+using Mods::timespec_sub;
+using Mods::timespec_sub_timeval;
+using Mods::timespec_to_timeval;
+using Mods::timeval_add;
+using Mods::timeval_sub;
+using Mods::timeval_to_timespec;
+using Mods::years_to_days_since_epoch;
+using Mods::operator<=;
+using Mods::operator<;
+using Mods::operator>;
+using Mods::operator>=;
+using Mods::operator==;
+using Mods::operator!=;
