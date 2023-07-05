6c269aaacd3a6c92bd386f801cf180a7a688ace1
krishpranav <krisna.pranav@gmail.com>
2023-07-05 19:33:31 +0530

    libc: sys[un]

---
+++ libraries/libc/sys/un.h
@@ -0,0 +1,25 @@
+/**
+ * @file un.h
+ * @author Krisna Pranav
+ * @brief UN
+ * @version 1.0
+ * @date 2023-07-05
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
+#define UNIX_PATH_MAX 108
+
+struct sockaddr_un {
+    uint16_t sun_family;
+    char sun_path[UNIX_PATH_MAX];
+};
+
+__END_DECLS
