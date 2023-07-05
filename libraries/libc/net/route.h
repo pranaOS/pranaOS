f297266c54dcdfa97a0c016321b7fc17baac5cea
krishpranav <krisna.pranav@gmail.com>
2023-07-05 21:49:36 +0530

    libraries: libc[net(route)]

---
+++ libraries/libc/net/route.h
@@ -0,0 +1,23 @@
+/**
+ * @file route.h
+ * @author Krisna Pranav
+ * @brief route
+ * @version 1.0
+ * @date 2023-07-05
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <sys/socket.h>
+
+struct rtentry {
+    struct sockaddr rt_gateway;
+    struct sockaddr rt_genmask;
+    char* rt_dev;
+};
+
+#define RTF_UP 0x1
+#define RTF_GATEWAY 0x2
