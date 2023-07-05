7eb7efebdd91d965d6ba1b3958a2d08974fa3e8f
krishpranav <krisna.pranav@gmail.com>
2023-07-05 18:52:14 +0530

    mods: circularduplex_stream

--- mods/circularduplex_stream.h
+++ mods/circularduplex_stream.h
@@ -15,10 +15,11 @@
 #include "stream.h"
 
 namespace Mods {
-    template<size_t Capacity>
-    class CircularDuplexStream: public Mods::DuplexStream{ 
-    public:
 
+    template<size_t Capacity>
+    class CircularDuplexStream : public Mods::DuplexStream {
+    public:
+         
         /**
          * @param bytes 
          * @return size_t 
@@ -26,9 +27,9 @@ namespace Mods {
         size_t write(ReadonlyBytes bytes) override {
             const auto nwritten = min(bytes.size(), Capacity - m_queue.size());
 
-            for(size_t idx = 0; idx < nwritten; ++idx)
+            for (size_t idx = 0; idx < nwritten; ++idx)
                 m_queue.enqueue(bytes[idx]);
-            
+
             m_total_written += nwritten;
             return nwritten;
         }
@@ -45,7 +46,6 @@ namespace Mods {
             }
 
             write(bytes);
-
             return true;
         }
 
@@ -56,14 +56,14 @@ namespace Mods {
         size_t read(Bytes bytes) override {
             if (has_any_error())
                 return 0;
-            
+
             const auto nread = min(bytes.size(), m_queue.size());
 
             for (size_t idx = 0; idx < nread; ++idx)
                 bytes[idx] = m_queue.dequeue();
-            
+
             return nread;
-        };
+        }
 
         /**
          * @param bytes 
@@ -84,7 +84,7 @@ namespace Mods {
             }
 
             return nread;
-        }       
+        }
 
         /**
          * @param bytes 
@@ -97,8 +97,24 @@ namespace Mods {
                 return false;
             }
 
-            for(size_t idx = 0; idx < count; ++idx)
+            read(bytes);
+            return true;
+        }
+
+        /**
+         * @param count 
+         * @return true 
+         * @return false 
+         */
+        bool discard_or_error(size_t count) override {
+            if (m_queue.size() < count) {
+                set_recoverable_error();
+                return false;
+            }
+
+            for (size_t idx = 0; idx < count; ++idx)
                 m_queue.dequeue();
+
             return true;
         }
 
@@ -106,20 +122,45 @@ namespace Mods {
          * @return true 
          * @return false 
          */
-        bool unreliable_eof() const override {
-            return eof();
+        bool unreliable_eof() const override { 
+            return eof(); 
         }
-
+        
         /**
          * @return true 
          * @return false 
          */
-        bool eof() const {
-            return m_queue.size() == 0;
+        bool eof() const { 
+            return m_queue.size() == 0; 
+        }
+
+        /**
+         * @return size_t 
+         */
+        size_t remaining_contigous_space() const {
+            return min(Capacity - m_queue.size(), m_queue.capacity() - (m_queue.head_index() + m_queue.size()) % Capacity);
+        }
+
+        /**
+         * @param count 
+         * @return Bytes 
+         */
+        Bytes reserve_contigous_space(size_t count) {
+            ASSERT(count <= remaining_contigous_space());
+
+            Bytes bytes { m_queue.m_storage + (m_queue.head_index() + m_queue.size()) % Capacity, count };
+
+            m_queue.m_size += count;
+            m_total_written += count;
+
+            return bytes;
         }
 
     private:
         CircularQueue<u8, Capacity> m_queue;
         size_t m_total_written { 0 };
     };
+
 }
+
+using Mods::CircularDuplexStream;
