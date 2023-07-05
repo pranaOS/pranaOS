bff5c990a2fa39779d2999588de11b55fa7a119a
krishpranav <krisna.pranav@gmail.com>
2023-07-05 21:46:08 +0530

    libc: netinet[tcp]

---
+++ libraries/libc/netinet/tcp.h
@@ -0,0 +1,14 @@
+/**
+ * @file tcp.h
+ * @author Krisna Pranav
+ * @brief tcp
+ * @version 1.0
+ * @date 2023-07-05
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#define TCP_NODELAY 10
