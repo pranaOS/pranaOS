c32395ab267b4902defb4790782548acff466bf3
krishpranav <krisna.pranav@gmail.com>
2023-07-06 19:44:22 +0530

    libc: dirent

--- libraries/libc/dirent.h
+++ libraries/libc/dirent.h
@@ -18,12 +18,26 @@ __BEGIN_DECLS
 
 enum {
     DT_UNKNOWN = 0,
+#define DT_UNKNOWN DT_UNKNOWN
     DT_FIFO = 1,
+#define DT_FIFO DT_FIFO
     DT_CHR = 2,
+#define DT_CHR DT_CHR
     DT_DIR = 4,
+#define DT_DIR DT_DIR
+    DT_BLK = 6,
+#define DT_BLK DT_BLK
     DT_REG = 8,
+#define DT_REG DT_REG
+    DT_LNK = 10,
+#define DT_LNK DT_LNK
+    DT_SOCK = 12,
+#define DT_SOCK DT_SOCK
+    DT_WHT = 14
+#define DT_WHT DT_WHT
 };
 
+/// @brief DIRENT
 struct dirent {
     ino_t d_ino;
     off_t d_off;
@@ -32,6 +46,7 @@ struct dirent {
     char d_name[256];
 };
 
+/// @brief DIR
 struct __DIR {
     int fd;
     struct dirent cur_ent;
@@ -40,6 +55,7 @@ struct __DIR {
     char* nextptr;
 };
 
+/// @brief __DIR -> DIR
 typedef struct __DIR DIR;
 
 /**
