2c11e38ec8e97b992b80688a56df4a30a9422e93
krishpranav <krisna.pranav@gmail.com>
2023-07-07 06:06:27 +0530

    kernel: api[inodewatcherevent]

---
+++ kernel/api/inodewatcherevent.h
@@ -0,0 +1,23 @@
+/**
+ * @file inodewatcherevent.h
+ * @author Krisna Pranav
+ * @brief inode watcher event
+ * @version 1.0
+ * @date 2023-07-07
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+#include <mods/types.h>
+
+struct [[gnu::packed]] InodeWatcherEvent {
+    enum class Type {
+        Invalid = 0
+    };
+
+    Type type {Type::Invalid};
+    unsigned inode_index { 0 };
+};
