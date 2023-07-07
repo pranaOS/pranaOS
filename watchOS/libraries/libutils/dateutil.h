83c6f0b3e11b5d35ec8d6083e2c7ed8cf7d198c4
krishpranav <krisna.pranav@gmail.com>
2023-07-07 22:24:29 +0530

    watchOS: libutils[dateutil]

---
+++ watchOS/libraries/libutils/dateutil.h
@@ -0,0 +1,29 @@
+/**
+ * @file dateutil.h
+ * @author Krisna Pranav
+ * @brief DateUtil
+ * @version 1.0
+ * @date 2023-07-04
+ *
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ *
+ */
+
+#pragma once
+
+
+class DateUtil {
+public:
+
+	// weekdays
+	static uint8_t weekday(uint year, uint8_t month, uint8_t day);
+
+	// get days in mnth
+	static uin8_t getDaysInMonth(uint year, int month);
+
+	// week days
+	static void weekdayName(char *dayInWeekStr, uint year, uint8_t month, uint8_t day);
+
+	// month name short
+	static void monthNameShort(char (&name)[4], int month);
+};
