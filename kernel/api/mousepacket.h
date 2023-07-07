ec975dcebcba3a6bf73c5bc7cd3c0a46a6e5f29e
krishpranav <krisna.pranav@gmail.com>
2023-07-07 06:29:45 +0530

    kernel: api[mousepacket]

--- kernel/api/mousepacket.h
+++ kernel/api/mousepacket.h
@@ -19,10 +19,13 @@ struct MousePacket {
     int z { 0 };
 
     enum Button {
-        LefButton = 0x01,
-        RightButton = 0x02
+        LeftButton = 0x01,
+        RightButton = 0x02,
+        MiddleButton = 0x04,
+        BackButton = 0x08,
+        ForwardButton = 0x10,
     };
 
     unsigned char buttons { 0 };
-    bool is_relatie {true};
+    bool is_relative { true };
 };
