ab7d4d9bfa008cd972d765235dd21996b799869e
krishpranav <krisna.pranav@gmail.com>
2023-07-07 06:18:38 +0530

    kernel: api[mousepacket]

---
+++ kernel/api/mousepacket.h
@@ -0,0 +1,10 @@
+/**
+ * @file mousepacket.h
+ * @author Krisna Pranav
+ * @brief mouse_packet
+ * @version 1.0
+ * @date 2023-07-07
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
