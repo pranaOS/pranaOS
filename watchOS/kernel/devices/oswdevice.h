67fd0d0d5c5d02b50cdae05c1222cb25db0a773c
krishpranav <krisna.pranav@gmail.com>
2023-07-07 22:08:09 +0530

    watchOS: kernel[device]

--- watchOS/kernel/devices/oswdevice.h
+++ watchOS/kernel/devices/oswdevice.h
@@ -17,7 +17,15 @@
 class OswDevice {
 public:
   virtual void setup() = 0;
-  
+  virtual void update() = 0;
+  virtual void reset() = 0;
+  virtual void stop() = 0;
+
+  virtual const char* getName() = 0;
+
+  static const std::set<OswDevice*>* getAllDevice() {
+      return &allDevice;
+  }
 
 private:
   static std::set<OswDevice*> allDevice;
