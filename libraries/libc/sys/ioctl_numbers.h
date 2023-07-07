9ea6fbce0e6b9186445168dbdaef07f04eeac43d
krishpranav <krisna.pranav@gmail.com>
2023-07-07 05:53:56 +0530

    libraries: libc[sys(ioctl_numbers)]

--- libraries/libc/sys/ioctl_numbers.h
+++ libraries/libc/sys/ioctl_numbers.h
@@ -15,7 +15,7 @@
 
 __BEGIN_DECLS
 
-
+/// @brief winsize
 struct winsize {
     unsigned short ws_row;
     unsigned short ws_col;
@@ -23,6 +23,7 @@ struct winsize {
     unsigned short ws_ypixel;
 };
 
+/// @brief fbresolution
 struct FBResolution {
     unsigned pitch;
     unsigned width;
@@ -31,9 +32,51 @@ struct FBResolution {
 
 __END_DECLS
 
+/// @brief ioctl number codes
 enum IOCtlNumber {
     TIOCGPGRP,
     TIOCSPGRP,
     TCGETS,
-    TCSETS  
+    TCSETS,
+    TCSETSW,
+    TCSETSF,
+    TCFLSH,
+    TIOCGWINSZ,
+    TIOCSCTTY,
+    TIOCNOTTY,
+    TIOCSWINSZ,
+    FB_IOCTL_GET_SIZE_IN_BYTES,
+    FB_IOCTL_GET_RESOLUTION,
+    FB_IOCTL_SET_RESOLUTION,
+    FB_IOCTL_GET_BUFFER,
+    FB_IOCTL_SET_BUFFER,
+    SIOCSIFADDR,
+    SIOCGIFADDR,
+    SIOCGIFHWADDR,
+    SIOCSIFNETMASK,
+    SIOCADDRT,
+    SIOCDELRT
 };
+
+#define TIOCGPGRP TIOCGPGRP
+#define TIOCSPGRP TIOCSPGRP
+#define TCGETS TCGETS
+#define TCSETS TCSETS
+#define TCSETSW TCSETSW
+#define TCSETSF TCSETSF
+#define TCFLSH TCFLSH
+#define TIOCGWINSZ TIOCGWINSZ
+#define TIOCSCTTY TIOCSCTTY
+#define TIOCNOTTY TIOCNOTTY
+#define TIOCSWINSZ TIOCSWINSZ
+#define FB_IOCTL_GET_SIZE_IN_BYTES FB_IOCTL_GET_SIZE_IN_BYTES
+#define FB_IOCTL_GET_RESOLUTION FB_IOCTL_GET_RESOLUTION
+#define FB_IOCTL_SET_RESOLUTION FB_IOCTL_SET_RESOLUTION
+#define FB_IOCTL_GET_BUFFER FB_IOCTL_GET_BUFFER
+#define FB_IOCTL_SET_BUFFER FB_IOCTL_SET_BUFFER
+#define SIOCSIFADDR SIOCSIFADDR
+#define SIOCGIFADDR SIOCGIFADDR
+#define SIOCGIFHWADDR SIOCGIFHWADDR
+#define SIOCSIFNETMASK SIOCSIFNETMASK
+#define SIOCADDRT SIOCADDRT
+#define SIOCDELRT SIOCDELRT
