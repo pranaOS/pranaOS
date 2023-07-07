11287762f505d777f50c0800f5289d72b3b2787f
krishpranav <krisna.pranav@gmail.com>
2023-07-07 22:11:52 +0530

    watchOS: kernel[ulp]

---
+++ watchOS/kernel/ulp.h
@@ -0,0 +1,18 @@
+/**
+* @file ulp.h
+* @author Krisna Pranav
+* @brief ULP
+* @version 1.0
+* @date 2023-07-07
+*
+* @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+*
+ */
+
+#pragma once
+
+// init
+void init_ulp(void);
+
+// loop
+void loop_ulp(void);
