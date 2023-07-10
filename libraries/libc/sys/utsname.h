6c84bc6b7e7c0e6049e85e14788225c3a4e9ca42
krishpranav <krisna.pranav@gmail.com>
2023-07-10 19:54:07 +0530

    libraries: libc[sys(utsname)]

--- libraries/libc/sys/utsname.h
+++ libraries/libc/sys/utsname.h
@@ -13,16 +13,23 @@
 
 #include <sys/cdefs.h>
 
+/// ENTRY_LENGTH
 #define UTSNAME_ENTRY_LEN 65
 
 __BEGIN_DECLS
 
+/// @brief utsname
 struct utsname {
     char sysname[UTSNAME_ENTRY_LEN];
     char nodename[UTSNAME_ENTRY_LEN];
     char release[UTSNAME_ENTRY_LEN];
+    char version[UTSNAME_ENTRY_LEN];
+    char machine[UTSNAME_ENTRY_LEN];
 };
 
+/**
+ * @return int 
+ */
 int uname(struct utsname*);
 
 __END_DECLS
