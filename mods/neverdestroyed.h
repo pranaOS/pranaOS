606cd0a752257947ab32309f18617dc51342ac41
krishpranav <krisna.pranav@gmail.com>
2023-07-11 22:04:31 +0530

    mods: neverdestroyed

---
+++ mods/neverdestroyed.h
@@ -0,0 +1,20 @@
+/**
+ * @file neverdestroyed.h
+ * @author Krisna Pranav
+ * @brief neverdestroyed
+ * @version 1.0
+ * @date 2023-07-11
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include "noncopyable.h"
+#include "types.h"
+
+namespace Mods
+{
+    
+}
