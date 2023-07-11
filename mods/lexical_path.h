87dd835af4ebdb6d6ebf818ccd83cd0bf0552ea6
krishpranav <krisna.pranav@gmail.com>
2023-07-11 21:58:51 +0530

    mods: lexical_path

--- mods/lexical_path.h
+++ mods/lexical_path.h
@@ -11,42 +11,99 @@
 
 #pragma once
 
-// #include <mods/string.h>
-// #include <mods/vector.h>
 #include "string.h"
 #include "vector.h"
 
+
 namespace Mods 
 {
-    class LexicalPath
-    {
 
+    class LexicalPath 
+    {
     public:
-        LexicalPath() {}
+        LexicalPath() { }
         explicit LexicalPath(const StringView&);
 
-        bool is_valid() const
-        {
-            return m_is_valid;
-        };
-
-        bool is_absolute() const
-        {
-            return m_is_absolute;
+        /**
+         * @return true 
+         * @return false 
+         */
+        bool is_valid() const 
+        { 
+            return m_is_valid; 
         }
 
-
-        const String& string() const
-        {
-            return m_string;
+        /**
+         * @return true 
+         * @return false 
+         */
+        bool is_absolute() const 
+        { 
+            return m_is_absolute; 
         }
 
-        const String& dirname() 
-        {
-            return m_dirname;
+        /**
+         * @return const String& 
+         */
+        const String& string() const 
+        { 
+            return m_string; 
         }
-    
+
+        /**
+         * @return const String& 
+         */
+        const String& dirname() const 
+        { 
+            return m_dirname; 
+        }
+
+        /**
+         * @return const String& 
+         */
+        const String& basename() const 
+        { 
+            return m_basename; 
+        }
+
+        /**
+         * @return const String& 
+         */
+        const String& title() const 
+        { 
+            return m_title; 
+        }
+
+        /**
+         * @return const String& 
+         */
+        const String& extension() const 
+        { 
+            return m_extension; 
+        }
+
+        /**
+         * @return const Vector<String>& 
+         */
+        const Vector<String>& parts() const 
+        { 
+            return m_parts; 
+        }
+
+        /**
+         * @return true 
+         * @return false 
+         */
+        bool has_extension(const StringView&) const;
+
+        /**
+         * @return String 
+         */
+        static String canonicalized_path(const StringView&);
+
     private:
+        void canonicalize();
+
         Vector<String> m_parts;
         String m_string;
         String m_dirname;
@@ -56,7 +113,15 @@ namespace Mods
         bool m_is_valid { false };
         bool m_is_absolute { false };
     };
-}
 
+    template<>
+    struct Formatter<LexicalPath> : Formatter<StringView> {
+        void format(TypeErasedFormatParams& params, FormatBuilder& builder, const LexicalPath& value)
+        {
+            Formatter<StringView>::format(params, builder, value.string());
+        }
+    };
+
+};
 
 using Mods::LexicalPath;
