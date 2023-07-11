a5690858a08a86bead56cb2e0f0fbc50b5acae61
krishpranav <krisna.pranav@gmail.com>
2023-07-11 19:37:34 +0530

    libdiff: format

---
+++ libraries/libdiff/format.h
@@ -0,0 +1,22 @@
+/**
+ * @file format.h
+ * @author Krisna Pranav
+ * @brief format
+ * @version 1.0
+ * @date 2023-07-11
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+#include <mods/string.h>
+
+namespace Diff
+{  
+    /**
+     * @return String 
+     */
+    String generate_only_additions(const String&);
+} // namespace Diff
