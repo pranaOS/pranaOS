2086acc2b57ec51c0967f9392ac96bc55328de0a
krishpranav <krisna.pranav@gmail.com>
2023-07-12 21:44:44 +0530

    kernel: net[ipv4]

--- kernel/net/ipv4.h
+++ kernel/net/ipv4.h
@@ -13,22 +13,23 @@
 
 #include <mods/assertions.h>
 #include <mods/endian.h>
+#include <mods/ipv4address.h>
 #include <mods/string.h>
 #include <mods/types.h>
 #include <mods/platform.h>
+#include <mods/string_view.h>
 
-// IPV4
-
-namespace Kernel
-{
-    enum class IPv4Protocol: u16
+namespace Kernel {
+    /// @brief IPv4Protocol
+    enum class IPv4Protocol : u16 
     {
         ICMP = 1,
         TCP = 6,
         UDP = 17,
     };
 
-    enum class IPv4PacketFlags: u16
+    /// @brief IPv4PacketFlags
+    enum class IPv4PacketFlags : u16 
     {
         DontFragment = 0x4000,
         MoreFragments = 0x2000,
@@ -42,61 +43,232 @@ namespace Kernel
     class [[gnu::packed]] IPv4Packet
     {
     public:
-        u8 version() const
-        {
-            return (m_version_and_ihl >> 4);
-        };
 
-        void set_version(u8 version)
-        {
-            m_version_and_ihl = (m_version_and_ihl & 0x0f);
+        /**
+         * @return u8 
+         */
+        u8 version() const 
+        { 
+            return (m_version_and_ihl >> 4) & 0xf; 
         }
 
-        u8 internet_header_length() const
-        {
-            return m_version_and_ihl & 0x0f;
-        };
-
-        void set_internet_header_length(u8 ihl)
-        {
-            m_version_and_ihl = (m_version_and_ihl & 0x0f);
+        /**
+         * @param version 
+         */
+        void set_version(u8 version) 
+        { 
+            m_version_and_ihl = (m_version_and_ihl & 0x0f) | (version << 4); 
         }
 
-        u16 length() const
-        {
-            return m_length;
+        /**
+         * @return u8 
+         */
+        u8 internet_header_length() const 
+        { 
+            return m_version_and_ihl & 0xf; 
         }
 
+        /**
+         * @param ihl 
+         */
+        void set_internet_header_length(u8 ihl) 
+        { 
+            m_version_and_ihl = (m_version_and_ihl & 0xf0) | (ihl & 0x0f); 
+        }
+
+        /**
+         * @return u16 
+         */
+        u16 length() const 
+        { 
+            return m_length; 
+        }
+
+        /**
+         * @param length 
+         */
         void set_length(u16 length) 
-        {
-            return m_length = length;
+        { 
+            m_length = length; 
         }
 
-        u16 ident() const
-        {
-            return m_ident;
+        /**
+         * @return u16 
+         */
+        u16 ident() const 
+        { 
+            return m_ident; 
         }
 
-        void set_ident(u16 ident) const
-        {
-            return m_ident = ident;
+        /**
+         * @param ident 
+         */
+        void set_ident(u16 ident) 
+        { 
+            m_ident = ident; 
         }
 
+        /**
+         * @return u8 
+         */
+        u8 ttl() const 
+        { 
+            return m_ttl; 
+        }
+
+        /**
+         * @param ttl 
+         */
+        void set_ttl(u8 ttl) 
+        { 
+            m_ttl = ttl; 
+        }
+
+        /**
+         * @return u8 
+         */
+        u8 protocol() const 
+        { 
+            return m_protocol; 
+        }
+
+        /**
+         * @param protocol 
+         */
+        void set_protocol(u8 protocol) 
+        { 
+            m_protocol = protocol; 
+        }
+
+        /**
+         * @return u16 
+         */
+        u16 checksum() const 
+        { 
+            return m_checksum; 
+        }
+
+        /**
+         * @param checksum 
+         */
+        void set_checksum(u16 checksum) 
+        { 
+            m_checksum = checksum; 
+        }
+
+        /**
+         * @return const IPv4Address& 
+         */
+        const IPv4Address& source() const 
+        { 
+            return m_source; 
+        }
+
+        /**
+         * @param address 
+         */
+        void set_source(const IPv4Address& address) 
+        { 
+            m_source = address; 
+        }
+
+        /**
+         * @return const IPv4Address& 
+         */
+        const IPv4Address& destination() const 
+        { 
+            return m_destination; 
+        }
+
+        /**
+         * @param address 
+         */
+        void set_destination(const IPv4Address& address) 
+        { 
+            m_destination = address; 
+        }
+
+        /**
+         * @return void* 
+         */
+        void* payload() 
+        { 
+            return this + 1; 
+        }
+
+        /**
+         * @return const void* 
+         */
+        const void* payload() const 
+        { 
+            return this + 1; 
+        }
+
+        /**
+         * @return u16 
+         */
+        u16 flags_and_fragment() const 
+        { 
+            return m_flags_and_fragment; 
+        }
+
+        /**
+         * @return u16 
+         */
+        u16 fragment_offset() const { 
+            return ((u16)m_flags_and_fragment & 0x1fff); 
+        }
+
+        /**
+         * @return u16 
+         */
+        u16 flags() const { 
+            return (((u16)m_flags_and_fragment) & (((u16)IPv4PacketFlags::MoreFragments) | ((u16)IPv4PacketFlags::DontFragment))); 
+        }
+
+        /**
+         * @param more_fragments 
+         */
         void set_has_more_fragments(bool more_fragments)
         {
             if (more_fragments)
-                m_flags_and_fragment = (u16)m_flags_and_fragment;
+                m_flags_and_fragment = (u16)m_flags_and_fragment | ((u16)IPv4PacketFlags::MoreFragments);
             else
-                m_flags_and_fragment = (u16)m_flags_and_fragment;
+                m_flags_and_fragment = (u16)m_flags_and_fragment & ((u16)IPv4PacketFlags::MoreFragments);
         }
 
-        void set_fragments_offset(u16 offset)
+        /**
+         * @param offset 
+         */
+        void set_fragment_offset(u16 offset)
         {
-            m_flags_and_fragment = flags() | (offset & 0x1ffff);
+            m_flags_and_fragment = flags() | (offset & 0x1fff);
         }
 
-        bool is_fragment() const
+        /**
+         * @return true 
+         * @return false 
+         */
+        bool is_a_fragment() const
         {
+            return (((u16)m_flags_and_fragment) & ((u16)IPv4PacketFlags::MoreFragments)) || ((u16)m_flags_and_fragment & 0x1fff);
+        }
+
+        /**
+         * @return u16 
+         */
+        u16 payload_size() const 
+        { 
+            return m_length - sizeof(IPv4Packet); 
+        }
+
+        /**
+         * @return NetworkOrdered<u16> 
+         */
+        NetworkOrdered<u16> compute_checksum() const
+        {
+            ASSERT(!m_checksum);
+            return internet_checksum(this, sizeof(IPv4Packet));
         }
 
     private:
@@ -112,4 +284,37 @@ namespace Kernel
         IPv4Address m_destination;
     };
 
-} // namespace Kernel
+    /**
+     * @brief Construct a new static assert object
+     * 
+     */
+    static_assert(sizeof(IPv4Packet) == 20);
+
+    /**
+     * @param stream 
+     * @param packet 
+     * @return const LogStream& 
+     */
+    const LogStream& operator<<(const LogStream& stream, const IPv4Packet& packet);
+
+    /**
+     * @param ptr 
+     * @param count 
+     * @return NetworkOrdered<u16> 
+     */
+    inline NetworkOrdered<u16> internet_checksum(const void* ptr, size_t count)
+    {
+        u32 checksum = 0;
+        auto* w = (const u16*)ptr;
+        while (count > 1) {
+            checksum += AK::convert_between_host_and_network_endian(*w++);
+            if (checksum & 0x80000000)
+                checksum = (checksum & 0xffff) | (checksum >> 16);
+            count -= 2;
+        }
+        while (checksum >> 16)
+            checksum = (checksum & 0xffff) + (checksum >> 16);
+        return ~checksum & 0xffff;
+    }
+
+}
