d83fdf9a843d628c32f9068fbef41fe555c56f92
krishpranav <krisna.pranav@gmail.com>
2023-07-12 20:37:30 +0530

    kernel: modules[syms]

---
+++ kernel/modules/syms.h
@@ -0,0 +1,16 @@
+/**
+ * @file syms.h
+ * @author Krisna Pranav
+ * @brief syms
+ * @version 1.0
+ * @date 2023-07-12
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+extern "C" const char module_name[];
+extern "C" void module_init();
+extern "C" void module_fini();
