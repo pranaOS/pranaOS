8979fac204b3461b6babd771ab570558cc1f5470
krishpranav <krisna.pranav@gmail.com>
2023-07-15 12:38:13 +0530

    kerenl: tasks[finalizetask]

---
+++ kernel/tasks/finalizertask.h
@@ -0,0 +1,22 @@
+/**
+ * @file finalizertask.h
+ * @author Krisna Pranav
+ * @brief Finalizer Task
+ * @version 1.0
+ * @date 2023-07-15
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+namespace Kernel 
+{
+    class FinalizerTask 
+    {
+    public:
+        /// @brief spawn task.
+        static void spawn();
+    };
+}
