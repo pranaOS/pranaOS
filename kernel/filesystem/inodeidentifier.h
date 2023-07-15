84a930c07b121ef149768fdb488d3b57bd898136
krishpranav <krisna.pranav@gmail.com>
2023-07-15 15:42:10 +0530

    kernel: filesystem[inodeidentifier]

--- kernel/filesystem/inodeidentifier.h
+++ kernel/filesystem/inodeidentifier.h
@@ -15,41 +15,49 @@
 #include <mods/string.h>
 #include <mods/types.h>
 
-namespace Kernel
+namespace Kernel 
 {
     class FS;
-    class InodeMetadata;
+    struct InodeMetadata;
 
-    class InodeIdentifier {
+    class InodeIdentifier 
+    {
     public:
-        InodeIdentifier() {}
+        InodeIdentifier() { }
 
-        InodeIdentifier(u32 fsid, u32 inode) : m_fsid(fsid), m_index(index)
-        {}
+        /**
+         * @param fsid 
+         * @param inode 
+         */
+        InodeIdentifier(u32 fsid, u32 inode)
+            : m_fsid(fsid)
+            , m_index(inode)
+        {
+        }
 
-        /** 
+        /**
          * @return true 
          * @return false 
          */
-        bool is_valid() const
-        {
-            return m_fsid;
+        bool is_valid() const 
+        { 
+            return m_fsid != 0 && m_index != 0; 
         }
 
         /**
          * @return u32 
          */
-        u32 fsid() const
-        {
-            return m_fsid;
+        u32 fsid() const 
+        { 
+            return m_fsid; 
         }
 
         /**
          * @return u32 
          */
-        u32 index() const
-        {
-            return m_index;
+        u32 index() const 
+        { 
+            return m_index; 
         }
 
         /**
@@ -73,15 +81,37 @@ namespace Kernel
         }
 
         /**
+         * @param other 
+         * @return true 
+         * @return false 
+         */
+        bool operator!=(const InodeIdentifier& other) const
+        {
+            return m_fsid != other.m_fsid || m_index != other.m_index;
+        }
+
+        /**
          * @return String 
          */
-        String to_string() const
-        {
-            return String::format("%u:%u", m_fsid, m_index);
+        String to_string() const 
+        { 
+            return String::format("%u:%u", m_fsid, m_index); 
         }
 
     private:
         u32 m_fsid { 0 };
         u32 m_index { 0 };
     };
+
+    /**
+     * @param stream 
+     * @param value 
+     * @return const LogStream& 
+     */
+    inline const LogStream& operator<<(const LogStream& stream, const InodeIdentifier& value)
+    {
+        stream << value.fsid() << ':' << value.index();
+        return stream;
+    }
+
 }
