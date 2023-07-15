bd9692a90f1f038552cc3cb19e918a297f0b6e6e
krishpranav <krisna.pranav@gmail.com>
2023-07-15 12:39:42 +0530

    kernel: tasks[synctasks]

---
+++ kernel/tasks/synctask.h
@@ -0,0 +1,21 @@
+/**
+ * @file synctask.h
+ * @author Krisna Pranav
+ * @brief synctask
+ * @version 1.0
+ * @date 2023-07-15
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+namespace Kernel 
+{
+    class SyncTask 
+    {
+    public:
+        static void spawn();
+    };
+}
