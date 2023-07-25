0d3a69bc7311fff23778836b628d9dca5f7723c4
krishpranav <krisna.pranav@gmail.com>
2023-07-25 20:26:49 +0530

    mods: stackinfo

---
+++ mods/stackinfo.h
@@ -0,0 +1,54 @@
+/**
+ * @file stackinfo.h
+ * @author Krisna Pranav
+ * @brief StackInfo
+ * @version 1.0
+ * @date 2023-07-25
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+#include "types.h"
+
+namespace Mods
+{
+
+    class StackInfo
+    {
+    public:
+        StackInfo();
+
+        /**
+         * @return FlatPtr 
+         */
+        FlatPtr base() const
+        {
+            return m_base;
+        }
+
+        /**
+         * @return FlatPtr 
+         */
+        FlatPtr top() const
+        {
+            return m_top;
+        }
+
+        /**
+         * @return size_t 
+         */
+        size_t size() const 
+        {
+            return m_size;
+        }
+
+    private:
+        FlatPtr m_base;
+        FlatPtr m_top;
+        size_t m_size;
+    }; // class StackInfo
+
+} // namespace Mods
