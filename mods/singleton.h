22e81002e8a23c598274c20d728915b92027e67e
krishpranav <krisna.pranav@gmail.com>
2023-07-26 18:39:29 +0530

    mods: singleton

--- mods/singleton.h
+++ mods/singleton.h
@@ -26,5 +26,84 @@
 
 namespace Mods
 {
-    
+    template <typename T>
+    struct SingletonInstanceCreator
+    {
+        /**
+         * @return T* 
+         */
+        static T* create()
+        {
+            return new T();
+        }
+    };
+
+    template<typename T, T* (*InitFunction)() = SingletonInstanceCreator<T>::create>
+    class Singleton
+    {
+        MOD_MAKE_NONCOPYABLE(Singleton);
+        MOD_MAKE_NONMOVABLE(Singleton);
+
+    public:
+        Singleton() = default;
+
+        /**
+         * @return T* 
+         */
+        T* ptr() const
+        {
+            T* obj = Mods::atomic_load(&m_obj, Mods::memory_order_consume);
+            return obj;
+        }
+
+        /**
+         * @return T* 
+         */
+        T* operator->() const
+        {
+            return ptr();
+        }
+
+        /**
+         * @return T& 
+         */
+        T& operator*() const
+        {
+            return *ptr();
+        }
+
+        /**
+         * @return T* 
+         */
+        operator T*() const
+        {
+            return ptr();
+        }
+
+        /**
+         * @return T& 
+         */
+        operator T&() const
+        {
+            return *ptr();
+        }
+
+        /**
+         * @return true 
+         * @return false 
+         */
+        bool is_initialized() const
+        {
+            T* obj = Mods::atomic_load(m_obj);
+            return FlatPtr(obj) > 0x1;
+        }
+
+        void enusure_instance()
+        {
+            (void)ptr();
+        }
+
+    private:
+        mutable T* m_obj { nullptr };
+    };
 }
