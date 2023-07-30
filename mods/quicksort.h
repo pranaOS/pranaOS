e77f6eeea910c709ed2f9b2520ec4cb696bbf0fd
krishpranav <krisna.pranav@gmail.com>
2023-07-30 12:10:40 +0530

    mods: quicksort

--- mods/quicksort.h
+++ mods/quicksort.h
@@ -13,7 +13,7 @@
 
 #include "stdlibextra.h"
 
-namespace Mods
+namespace Mods 
 {
 
     /**
@@ -32,7 +32,6 @@ namespace Mods
         }
 
         int left_pointer, right_pointer;
-
         if (!less_than(col[start], col[end])) {
             swap(col[start], col[end]);
         }
@@ -43,6 +42,69 @@ namespace Mods
 
         auto&& left_pivot = col[start];
         auto&& right_pivot = col[end];
+
+        while (k <= g) {
+            if (less_than(col[k], left_pivot)) {
+                swap(col[k], col[j]);
+                j++;
+            } else if (!less_than(col[k], right_pivot)) {
+                while (!less_than(col[g], right_pivot) && k < g) {
+                    g--;
+                }
+                swap(col[k], col[g]);
+                g--;
+                if (less_than(col[k], left_pivot)) {
+                    swap(col[k], col[j]);
+                    j++;
+                }
+            }
+            k++;
+        }
+        j--;
+        g++;
+
+        swap(col[start], col[j]);
+        swap(col[end], col[g]);
+
+        left_pointer = j;
+        right_pointer = g;
+
+        dual_pivot_quick_sort(col, start, left_pointer - 1, less_than);
+        dual_pivot_quick_sort(col, left_pointer + 1, right_pointer - 1, less_than);
+        dual_pivot_quick_sort(col, right_pointer + 1, end, less_than);
+    }
+
+    /**
+     * @tparam Iterator 
+     * @tparam LessThan 
+     * @param start 
+     * @param end 
+     * @param less_than 
+     */
+    template<typename Iterator, typename LessThan>
+    void quick_sort(Iterator start, Iterator end, LessThan less_than)
+    {
+        int size = end - start;
+        if (size <= 1)
+            return;
+
+        int pivot_point = size / 2;
+        auto pivot = *(start + pivot_point);
+
+        if (pivot_point)
+            swap(*(start + pivot_point), *start);
+
+        int i = 1;
+        for (int j = 1; j < size; ++j) {
+            if (less_than(*(start + j), pivot)) {
+                swap(*(start + j), *(start + i));
+                ++i;
+            }
+        }
+
+        swap(*start, *(start + i - 1));
+        quick_sort(start, start + i - 1, less_than);
+        quick_sort(start + i, end, less_than);
     }
 
     /**
@@ -53,7 +115,7 @@ namespace Mods
     template<typename Iterator>
     void quick_sort(Iterator start, Iterator end)
     {
-        quick_sort(start, end, [](auto& a, auto& b) { return a < b });
+        quick_sort(start, end, [](auto& a, auto& b) { return a < b; });
     }
 
     /**
@@ -75,7 +137,10 @@ namespace Mods
     template<typename Collection>
     void quick_sort(Collection& collection)
     {
-        dual_pivot_quick_sort(collection, 0, collection.size() - 1, [](auto& a, auto& b) { return a < b});
+        dual_pivot_quick_sort(collection, 0, collection.size() - 1,
+            [](auto& a, auto& b) { return a < b; });
     }
 
-} // namespace Mods
+}
+
+using Mods::quick_sort;
