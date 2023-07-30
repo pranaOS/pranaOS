718df25b1f79af64289705a14144e9801b301121
krishpranav <krisna.pranav@gmail.com>
2023-07-30 15:10:17 +0530

    kernel: mob

---
+++ kernel/mob.m
@@ -0,0 +1,14 @@
+/**
+ * @file mob.m
+ * @author Krisna Pranav
+ * @brief Mobile
+ * @version 1.0
+ * @date 2023-07-30
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+*/
+
+#import <objc/objc.h>
+
+bool isMobileUnicodePresent = false;
