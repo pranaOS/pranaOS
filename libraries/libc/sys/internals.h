daa42fe905d8a7711c2f5dd5aa69a86b99b31bc0
krishpranav <krisna.pranav@gmail.com>
2023-07-31 20:11:45 +0530

    libraries: libc[sys(internals)]

---
+++ libraries/libc/sys/internals.h
@@ -0,0 +1,30 @@
+/**
+ * @file internals.h
+ * @author Krisna Pranav
+ * @brief internals
+ * @version 1.0
+ * @date 2023-07-31
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
+typedef void (*AtExitFunction)(void*);
+
+extern void __libc_init();
+extern void __malloc_init();
+extern void __stdio_init();
+extern void _init();
+extern bool __environ_is_malloced;
+extern bool __stdio_is_initialized;
+
+int __cxa_atexit(AtExitFunction exit_function, void* paramter, void* dso_handle);
+void __cxa_finalize(void* dso_handle);
+
+__END_DECLS
