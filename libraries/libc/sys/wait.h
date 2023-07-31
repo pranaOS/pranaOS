b2b6be8b79499b1648463b32aa477ff001cbc2ad
krishpranav <krisna.pranav@gmail.com>
2023-07-31 22:11:50 +0530

    libraries: libc[sys(wait)]

--- libraries/libc/sys/wait.h
+++ libraries/libc/sys/wait.h
@@ -17,17 +17,28 @@
 
 __BEGIN_DECLS
 
+// STATUS
+#define WEXITSTATUS(status) (((status)&0xff00) >> 8)
+#define WSTOPSIG(status) WEXITSTATUS(status)
+#define WTERMSIG(status) ((status)&0x7f)
+#define WIFEXITED(status) (WTERMSIG(status) == 0)
+#define WIFSTOPPED(status) (((status)&0xff) == 0x7f)
+#define WIFSIGNALED(status) (((char)(((status)&0x7f) + 1) >> 1) > 0)
+
+// TRACED
 #define WNOHANG 1
-#define WUNTRACED 2 
+#define WUNTRACED 2
 #define WSTOPPED WUNTRACED
 #define WEXITED 4
 #define WCONTINUED 8
+#define WNOWAIT 0x1000000
 
+/// @brief: idtype_t
 typedef enum 
 {
     P_ALL = 1,
     P_PID,
-    P_GID
+    P_PGID
 } idtype_t;
 
 /**
