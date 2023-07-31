8667d1696ce8c9eec97f953201eadb3af9e36712
krishpranav <krisna.pranav@gmail.com>
2023-07-31 20:14:42 +0530

    libraries: libc[sys(param)]

---
+++ libraries/libc/sys/param.h
@@ -0,0 +1,15 @@
+/**
+ * @file param.h
+ * @author Krisna Pranav
+ * @brief Para
+ * @version 1.0
+ * @date 2023-07-31
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <limits.h>
+#include <endian.h>
