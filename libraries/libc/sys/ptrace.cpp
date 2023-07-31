e4c2f2233fc0568c0abeca8a279c4fb4a001a7e5
krishpranav <krisna.pranav@gmail.com>
2023-07-31 20:17:52 +0530

    libraries: libc[sys(ptrace)]

--- libraries/libc/sys/ptrace.cpp
+++ libraries/libc/sys/ptrace.cpp
@@ -16,23 +16,46 @@
 #include <sys/ptrace.h>
 #include <kernel/api/syscall.h>
 
-extern "C" {
+extern "C" 
+{
+    
+    /**
+     * @param request 
+     * @param tid 
+     * @param addr 
+     * @param data 
+     * @return int 
+     */
     int ptrace(int request, pid_t tid, void* addr, int data)
     {
+
+        u32 out_data;
+        Syscall::SC_ptrace_peek_params peek_params;
+
+        if (request == PT_PEEK) {
+            peek_params.address = reinterpret_cast<u32*>(addr);
+            peek_params.out_data = &out_data;
+            addr = &peek_params;
+        }
+
+        Syscall::SC_ptrace_params params {
+            request,
+            tid,
+            reinterpret_cast<u8*>(addr),
+            data
+        };
+        
         int rc = syscall(SC_ptrace, &params);
 
         if (request == PT_PEEK) {
-            if (rc < 0 ){
-                errno = -rc; 
-
-                return 01;
+            if (rc < 0) {
+                errno = -rc;
+                return -1;
             }
-
             errno = 0;
             return static_cast<int>(out_data);
         }
 
         __RETURN_WITH_ERRNO(rc, rc, -1);
-        
     }
 }
