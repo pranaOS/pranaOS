3c4c87cade0e50455c3689ef9b02f1d3434163b5
krishpranav <krisna.pranav@gmail.com>
2023-07-31 20:14:30 +0530

    libraries: libc[sys(mman)]

---
+++ libraries/libc/sys/mman.h
@@ -0,0 +1,14 @@
+/**
+ * @file mman.h
+ * @author Krisna Pranav
+ * @brief MMAN
+ * @version 1.0
+ * @date 2023-07-31
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+#include <mman.h>
