f8588c9e5c328412d48e64bb96973a6ca035f62e
krishpranav <krisna.pranav@gmail.com>
2023-08-01 19:33:54 +0530

    libraries: libc[ctype]

---
+++ libraries/libc/ctype.cpp
@@ -0,0 +1,35 @@
+/**
+ * @file ctype.cpp
+ * @author Krisna Pranav
+ * @brief ctype
+ * @version 1.0
+ * @date 2023-08-01
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include <ctype.h>
+
+extern "C"
+{
+    const char _ctype_[256] = 
+    {
+        _C, _C, _C, _C, _C, _C, _C, _C,
+        _C, _C | _S, _C | _S, _C | _S, _C | _S, _C | _S, _C, _C,
+        _C, _C, _C, _C, _C, _C, _C, _C,
+        _C, _C, _C, _C, _C, _C, _C, _C,
+        (char)(_S | _B), _P, _P, _P, _P, _P, _P, _P,
+        _P, _P, _P, _P, _P, _P, _P, _P,
+        _N, _N, _N, _N, _N, _N, _N, _N,
+        _N, _N, _P, _P, _P, _P, _P, _P,
+        _P, _U | _X, _U | _X, _U | _X, _U | _X, _U | _X, _U | _X, _U,
+        _U, _U, _U, _U, _U, _U, _U, _U,
+        _U, _U, _U, _U, _U, _U, _U, _U,
+        _U, _U, _U, _P, _P, _P, _P, _P,
+        _P, _L | _X, _L | _X, _L | _X, _L | _X, _L | _X, _L | _X, _L,
+        _L, _L, _L, _L, _L, _L, _L, _L,
+        _L, _L, _L, _L, _L, _L, _L, _L,
+        _L, _L, _L, _P, _P, _P, _P, _C
+    };
+}
