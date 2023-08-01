b9a17c7362e1705de0d2d91f09b9e0e893cba8d3
krishpranav <krisna.pranav@gmail.com>
2023-08-01 19:03:03 +0530

    libraries: libc[assert]

--- libraries/libc/assert.h
+++ libraries/libc/assert.h
@@ -15,12 +15,27 @@
 
 __BEGIN_DECLS
 
-#define CRASH()                 \
-    do {                        \
-        asm volatile("ud2");    \
-    } while (0) 
+#ifdef DEBUG
+__attribute__((noreturn)) void __assertion_failed(const char* msg);
+#    define __stringify_helper(x) #    x
+#    define __stringify(x) __stringify_helper(x)
+#    define assert(expr)                                                           \
+        do {                                                                       \
+            if (__builtin_expect(!(expr), 0))                                      \
+                __assertion_failed(#expr "\n" __FILE__ ":" __stringify(__LINE__)); \
+        } while (0)
+#    define ASSERT_NOT_REACHED() assert(false)
+#else
+#    define assert(expr) ((void)0)
+#    define ASSERT_NOT_REACHED() CRASH()
+#endif
 
+#define CRASH()              \
+    do {                     \
+        asm volatile("ud2"); \
+    } while (0)
 
+/// @brief: ASSERT functionalities.
 #define ASSERT assert
 #define RELEASE_ASSERT assert
 #define TODO ASSERT_NOT_REACHED
