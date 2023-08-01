d2c6161c7409e8732ad8e4494d5089776a2305d2
krishpranav <krisna.pranav@gmail.com>
2023-08-01 18:06:38 +0530

    libraries: libc[sys(sysmacro)]

---
+++ libraries/libc/sys/sysmacro.h
@@ -0,0 +1,12 @@
+/**
+ * @file sysmacro.h
+ * @author Krisna Pranav
+ * @brief sysmacros
+ * @version 1.0
+ * @date 2023-08-01
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
