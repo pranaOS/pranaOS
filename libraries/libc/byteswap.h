dda23f7320ae8016dad2cb7ae28ae71536ca2ebd
krishpranav <krisna.pranav@gmail.com>
2023-08-01 19:13:49 +0530

    libraries: libc[byteswap]

---
+++ libraries/libc/byteswap.h
@@ -0,0 +1,23 @@
+/**
+ * @file byteswap.h
+ * @author Krisna Pranav
+ * @brief byteswp
+ * @version 1.0
+ * @date 2023-08-01
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include <sys/cdefs.h>
+
+#pragma once
+
+__BEGIN_DECLS
+
+/// @brief: bswap[16, 32, 64]
+#define bswap_16(x) (__builtin_bswap16(x))
+#define bswap_32(x) (__builtin_bswap32(x))
+#define bswap_64(x) (__builtin_bswap64(x))
+
+__END_DECLS
