280b2a5426de2b296b0d14cbc15e7c0988075458
krishpranav <krisna.pranav@gmail.com>
2023-08-01 21:19:09 +0530

    libraries: libc[dirent]

---
+++ libraries/libc/dirent.cpp
@@ -0,0 +1,81 @@
+/**
+ * @file dirent.cpp
+ * @author Krisna Pranav
+ * @brief dirent
+ * @version 1.0
+ * @date 2023-08-01
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <mods/assertions.h>
+#include <mods/stdlibextra.h>
+#include <kernel/api/syscall.h>
+#include <sys/stat.h>
+
+extern "C"
+{
+    /**
+     * @param name 
+     * @return DIR* 
+     */
+    DIR* opendir(const char* name)
+    {
+        int fd = open(name, O_RDONLY | O_DIRECTORY);
+
+        if (fd == -1)
+            return nullptr;
+        
+        DIR* dirp = (DIR*)malloc(sizeof(DIR));
+
+        dirp->fd = fd;
+        dirp->buffer = nullptr;
+        dirp->buffer_size = 0;
+        dirp->nextptr = nullptr;
+
+        return dirp;
+    }
+
+    /**
+     * @param dirp 
+     * @return int 
+     */
+    int closedir(DIR* dirp)
+    {
+        if (!dirp || dirp->fd == -1)
+            return -EBADF;
+        
+        if (dirp->buffer)
+            free(dirp->buffer);
+        
+        int rc = close(dirp->rc);
+
+        if (rc == 0)
+            dirp->fd = -1;
+        
+        free(dirp);
+
+        return rc;
+    }
+
+    struct [[gnu::packed]] sys_dirent
+    {
+        ino_t ino;
+        u8 file_type;
+        size_t namelen;
+        char name[];
+
+        size_t total_size()
+        {
+            return sizeof(ino_t) + sizeof(u8) + sizeof(size);
+        }
+    }; // struct sys_dirent
+}
