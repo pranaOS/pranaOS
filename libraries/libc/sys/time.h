7e86b8de818fedbc51f958d21ff6f2470b1a69b4
krishpranav <krisna.pranav@gmail.com>
2023-08-01 18:06:47 +0530

    libraries: libc[sys(time)]

---
+++ libraries/libc/sys/time.h
@@ -0,0 +1,23 @@
+/**
+ * @file time.h
+ * @author Krisna Pranav
+ * @brief time
+ * @version 1.0
+ * @date 2023-08-01
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+#include <time.h>
+#include <sys/cdefs.h>
+#include <sys/types.h>
+
+__BEGIN_DECLS
+
+
+
+
+__END_DECLS
