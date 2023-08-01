29182d360cdbb090b3bae735958af38ae103c9d5
krishpranav <krisna.pranav@gmail.com>
2023-08-01 19:26:07 +0530

    libraries: libc[ctype]

--- libraries/libc/ctype.h
+++ libraries/libc/ctype.h
@@ -15,10 +15,27 @@
 
 __BEGIN_DECLS
 
+/// @brief: values
+#define _U 01
+#define _L 02
+#define _N 04
+#define _S 010
+#define _P 020
+#define _C 040
+#define _X 0100
+#define _B 0200
 
+/// @brief ctype[256]
 extern const char _ctype_[256];
 
+/**
+ * @return int 
+ */
 int tolower(int);
+
+/**
+ * @return int 
+ */
 int toupper(int);
 
 /**
@@ -27,10 +44,103 @@ int toupper(int);
  */
 static inline int isalnum(int c)
 {
-    return (_ctype_[(unsigned char)(c)]);
+    return (_ctype_[(unsigned char)(c)] & (_U | _L | _N));
 }
 
-#define toascii(c) ((unsigned)c <= 127)
-#define toascii(c) ((C)&127)
+/**
+ * @param c 
+ * @return int 
+ */
+static inline int isalpha(int c)
+{
+    return (_ctype_[(unsigned char)(c)] & (_U | _L));
+}
+
+/**
+ * @param c 
+ * @return int 
+ */
+static inline int iscntrl(int c)
+{
+    return (_ctype_[(unsigned char)(c)] & (_C));
+}
+
+/**
+ * @param c 
+ * @return int 
+ */
+static inline int isdigit(int c)
+{
+    return (_ctype_[(unsigned char)(c)] & (_N));
+}
+
+/**
+ * @param c 
+ * @return int 
+ */
+static inline int isxdigit(int c)
+{
+    return (_ctype_[(unsigned char)(c)] & (_N | _X));
+}
+
+/**
+ * @param c 
+ * @return int 
+ */
+static inline int isspace(int c)
+{
+    return (_ctype_[(unsigned char)(c)] & (_S));
+}
+
+/**
+ * @param c 
+ * @return int 
+ */
+static inline int ispunct(int c)
+{
+    return (_ctype_[(unsigned char)(c)] & (_P));
+}
+
+/**
+ * @param c 
+ * @return int 
+ */
+static inline int isprint(int c)
+{
+    return (_ctype_[(unsigned char)(c)] & (_P | _U | _L | _N | _B));
+}
+
+/**
+ * @param c 
+ * @return int 
+ */
+static inline int isgraph(int c)
+{
+    return (_ctype_[(unsigned char)(c)] & (_P | _U | _L | _N));
+}
+
+/**
+ * @param c 
+ * @return int 
+ */
+static inline int islower(int c)
+{
+    return ((_ctype_[(unsigned char)(c)] & (_U | _L)) == _L);
+}
+
+/**
+ * @param c 
+ * @return int 
+ */
+static inline int isupper(int c)
+{
+    return ((_ctype_[(unsigned char)(c)] & (_U | _L)) == _U);
+}
+
+/// @brief: isascii
+#define isascii(c) ((unsigned)c <= 127)
+
+/// @brief: toascii
+#define toascii(c) ((c)&127)
 
 __END_DECLS
