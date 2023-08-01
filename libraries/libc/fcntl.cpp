a5f62ded58aaa9edbb799e94d51caac383e04b2e
krishpranav <krisna.pranav@gmail.com>
2023-08-01 21:38:44 +0530

    libraries: libc[fcntl]

--- libraries/libc/fcntl.cpp
+++ libraries/libc/fcntl.cpp
@@ -15,8 +15,9 @@
 #include <string.h>
 #include <kernel/api/syscall.h>
 
-extern "C"
+extern "C" 
 {
+
     /**
      * @param fd 
      * @param cmd 
@@ -38,7 +39,7 @@ extern "C"
      * @param path_length 
      * @return int 
      */
-    int watch_file(const chr* path, size_t path_length)
+    int watch_file(const char* path, size_t path_length)
     {
         int rc = syscall(SC_watch_file, path, path_length);
         __RETURN_WITH_ERRNO(rc, rc, -1);
@@ -56,6 +57,52 @@ extern "C"
 
     /**
      * @param path 
+     * @param path_length 
+     * @param mode 
+     * @return int 
+     */
+    int creat_with_path_length(const char* path, size_t path_length, mode_t mode)
+    {
+        return open_with_path_length(path, path_length, O_CREAT | O_WRONLY | O_TRUNC, mode);
+    }
+
+    /**
+     * @param path 
+     * @param path_length 
+     * @param options 
+     * @param mode 
+     * @return int 
+     */
+    int open_with_path_length(const char* path, size_t path_length, int options, mode_t mode)
+    {
+        return openat_with_path_length(AT_FDCWD, path, path_length, options, mode);
+    }
+
+    /**
+     * @param dirfd 
+     * @param path 
+     * @param path_length 
+     * @param options 
+     * @param mode 
+     * @return int 
+     */
+    int openat_with_path_length(int dirfd, const char* path, size_t path_length, int options, mode_t mode)
+    {
+        if (!path) {
+            errno = EFAULT;
+            return -1;
+        }
+        if (path_length > INT32_MAX) {
+            errno = EINVAL;
+            return -1;
+        }
+        Syscall::SC_open_params params { dirfd, { path, path_length }, options, mode };
+        int rc = syscall(SC_open, &params);
+        __RETURN_WITH_ERRNO(rc, rc, -1);
+    }
+
+    /**
+     * @param path 
      * @param options 
      * @param ... 
      * @return int 
@@ -66,14 +113,30 @@ extern "C"
             errno = EFAULT;
             return -1;
         }
-
         va_list ap;
         va_start(ap, options);
-
         auto mode = (mode_t)va_arg(ap, unsigned);
-
         va_end(ap);
-
         return open_with_path_length(path, strlen(path), options, mode);
+    }
+
+    /**
+     * @param dirfd 
+     * @param path 
+     * @param options 
+     * @param ... 
+     * @return int 
+     */
+    int openat(int dirfd, const char* path, int options, ...)
+    {
+        if (!path) {
+            errno = EFAULT;
+            return -1;
+        }
+        va_list ap;
+        va_start(ap, options);
+        auto mode = (mode_t)va_arg(ap, unsigned);
+        va_end(ap);
+        return openat_with_path_length(dirfd, path, strlen(path), options, mode);
     }
 }
