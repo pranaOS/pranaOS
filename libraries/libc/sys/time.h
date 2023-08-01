4fc24e5ec2064826df09f4c97653f9e82ac66963
krishpranav <krisna.pranav@gmail.com>
2023-08-01 18:11:36 +0530

    libraries: libc[sys(time)]

--- libraries/libc/sys/time.h
+++ libraries/libc/sys/time.h
@@ -17,7 +17,89 @@
 
 __BEGIN_DECLS
 
+struct timeval
+{
+    time_t tv_sec;
+    suseconds_t tv_usec;
+}; // struct timeval
 
+struct timezone
+{
+    int tz_minuteswest;
+    int tz_dsttime;
+}; // struct timezone
 
+/**
+ * @param delta 
+ * @param old_delta 
+ * @return int 
+ */
+int adjtime(const struct timeval* delta, struct timeval* old_delta);
+
+/**
+ * @param a 
+ * @param b 
+ * @param out 
+ */
+static inline void timeradd(const struct timeval* a, const struct timeval* b, struct timeval* out)
+{
+    out->tv_sec = a->tv_sec + b->tv_sec;
+    out->tv_usec = a->tv_usec + b ->tv_usec;
+
+    if (out->tv_usec >= 1000 * 1000) {
+        out->tv_sec++;
+        out->tv_usec -= 1000 * 1000;
+    }
+} 
+
+/**
+ * @param a 
+ * @param b 
+ * @param out 
+ */
+static inline void timersub(const struct timeval* a, const struct timeval* b, struct timeval* out)
+{
+    out->tv_sec = a->tv_sec - b->tv_sec;
+    out->tv_sec = a->tv_usec - b->tv_usec;
+
+    if (out->tv_usec < 0) {
+        out->tv_usec--;
+        out->tv_usec += 1000 * 1000;
+    }
+}
+
+/**
+ * @param out 
+ */
+static inline void timerclear(struct timeval* out)
+{
+    out->tv_sec = out->tv_usec = 0;
+}
+
+/**
+ * @param tv 
+ * @return int 
+ */
+static inline int timerisset(const struct timeval* tv)
+{
+    return tv->tv_sec || tv->tv_usec;
+}
+
+/**
+ * @param out 
+ */
+static inline void timespecclear(struct timespec* out)
+{
+    out->tv_sec = out->tv_nsec = 0;
+}
+
+/**
+ * @param ts 
+ * @return int 
+ */
+static inline int timespecisset(const struct timespec* ts)
+{
+    return ts->tv_sec || ts->tv_nsec;
+}
 
 __END_DECLS
