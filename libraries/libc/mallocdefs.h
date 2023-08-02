856bc1d24237a36426862c4ec651adcd086389c8
krishpranav <krisna.pranav@gmail.com>
2023-08-02 19:30:29 +0530

    libraries: libc[mallocdefs]

---
+++ libraries/libc/mallocdefs.h
@@ -0,0 +1,16 @@
+/**
+ * @file mallocdefs.h
+ * @author Krisna Pranav
+ * @brief mallocdefs
+ * @version 1.0
+ * @date 2023-08-02
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+#include <mods/inlinelinkedlist.h>
+#include <mods/types.h>
+
