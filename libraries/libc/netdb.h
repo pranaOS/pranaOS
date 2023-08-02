6076be2f3f090133e498fe6f1e6480ccdb10de74
krishpranav <krisna.pranav@gmail.com>
2023-08-02 22:06:06 +0530

    libraries: libc[netdb]

---
+++ libraries/libc/netdb.h
@@ -0,0 +1,104 @@
+/**
+ * @file netdb.h
+ * @author Krisna Pranav
+ * @brief netdb
+ * @version 1.0
+ * @date 2023-08-02
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+#include <sys/types.h>
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
+struct hostent 
+{
+    char* h_name;
+    char** h_aliases;
+    int h_addrtype;
+    int h_length;
+    #define h_addr h_addr_list[0]
+};
+
+/**
+ * @return struct hostent* 
+ */
+struct hostent* gethostbyname(const char*);
+
+/**
+ * @param addr 
+ * @param len 
+ * @param type 
+ * @return struct hostent* 
+ */
+struct hostent* gethostbyaddr(const  void* addr, socklen_t len, int type);
+
+struct servent 
+{
+    char* s_name;
+    char** s_aliases;
+    int s_port;
+    char* s_proto;
+};
+
+/**
+ * @return struct servent* 
+ */
+struct servent* getservent();
+
+/**
+ * @param name 
+ * @param protocol 
+ * @return struct servent* 
+ */
+struct servent* getservbyname(const char* name, const char* protocol);
+
+/**
+ * @param port 
+ * @param protocol 
+ * @return struct servent* 
+ */
+struct servent* getservbyport(int port, const char* protocol);
+
+/**
+ * @param stay_open 
+ */
+void setservent(int stay_open);
+
+struct protoent 
+{
+    char* p_name;
+    char** p_aliases;
+    int p_proto;
+}; // struct protoent
+
+/**
+ * @param name 
+ * @return struct protoent* 
+ */
+struct protoent* getprotobyname(const char* name);
+
+/**
+ * @param proto 
+ * @return struct protoent* 
+ */
+struct protoent* getprotobynumber(int proto);
+
+/**
+ * @param stay_open 
+ */
+void setprotoent(int stay_open);
+
+extern int h_errno;
+
+#define HOST_NOT_FOUND 101
+#define NO_DATA 102
+#define NO_RECOVERY 103
+#define TRY_AGAIN 104
+
+__END_DECLS
