7a3b1691c5a1ad127bedb7b2a5150c7bea127d6a
krishpranav <krisna.pranav@gmail.com>
2023-08-02 18:56:13 +0530

    libraries: libc[libgen]

--- libraries/libc/libgen.cpp
+++ libraries/libc/libgen.cpp
@@ -29,8 +29,8 @@ char* dirname(char* path)
 
     if (len == 0)
         return dot;
-    
-    while (len > 1 && path[len -1] == '/') {
+
+    while (len > 1 && path[len - 1] == '/') {
         path[len - 1] = 0;
         len--;
     }
@@ -39,9 +39,44 @@ char* dirname(char* path)
 
     if (last_slash == nullptr)
         return dot;
-    
+
     if (last_slash == path)
         return slash;
-    
+
+    *last_slash = 0;
+
     return path;
+}
+
+/**
+ * @param path 
+ * @return char* 
+ */
+char* basename(char* path)
+{
+    if (path == nullptr)
+        return dot;
+
+    int len = strlen(path);
+
+    if (len == 0)
+        return dot;
+
+    while (len > 1 && path[len - 1] == '/') {
+        path[len - 1] = 0;
+        len--;
+    }
+
+    char* last_slash = strrchr(path, '/');
+    
+    if (last_slash == nullptr)
+        return path;
+
+    if (len == 1) {
+        ASSERT(last_slash == path);
+        ASSERT(path[0] == '/');
+        return slash;
+    }
+
+    return last_slash + 1;
 }
