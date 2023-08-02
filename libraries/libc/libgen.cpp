646a635e410fed8459d217fe8140952212f7cb0f
krishpranav <krisna.pranav@gmail.com>
2023-08-02 18:55:28 +0530

    libraries: libc[libgen]

---
+++ libraries/libc/libgen.cpp
@@ -0,0 +1,47 @@
+/**
+ * @file libgen.cpp
+ * @author Krisna Pranav
+ * @brief libgen
+ * @version 1.0
+ * @date 2023-08-02
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include <libgen.h>
+#include <string.h>
+#include <mods/assertions.h>
+
+static char dot[] = ".";
+static char slash[] = "/";
+
+/**
+ * @param path 
+ * @return char* 
+ */
+char* dirname(char* path)
+{
+    if (path == nullptr)
+        return dot;
+
+    int len = strlen(path);
+
+    if (len == 0)
+        return dot;
+    
+    while (len > 1 && path[len -1] == '/') {
+        path[len - 1] = 0;
+        len--;
+    }
+
+    char* last_slash = strrchr(path, '/');
+
+    if (last_slash == nullptr)
+        return dot;
+    
+    if (last_slash == path)
+        return slash;
+    
+    return path;
+}
