dce32e66c69a0fe4e63a9679e48e40c5b756fe77
krishpranav <krisna.pranav@gmail.com>
2023-08-02 19:58:26 +0530

    libraries: libc[mallocdefs]

--- libraries/libc/mallocdefs.h
+++ libraries/libc/mallocdefs.h
@@ -14,3 +14,34 @@
 #include <mods/inlinelinkedlist.h>
 #include <mods/types.h>
 
+#define MAGIC_PAGE_HEADER 0x42657274     
+#define MAGIC_BIGALLOC_HEADER 0x42697267 
+#define MALLOC_SCRUB_BYTE 0xdc
+#define FREE_SCRUB_BYTE 0xed
+
+static constexpr unsigned short size_classes[] = { 8, 16, 32, 64, 128 };
+
+struct CommonHeader
+{
+    size_t m_magic;
+    size_t m_size;
+}; // struct CommonHeader
+
+struct BigAllocationBlock : public CommonHeader
+{
+    /**
+     * @param size 
+     */
+    BigAllocationBlock(size_t size)
+    {
+        m_magic = MAGIC_BIGALLOC_HEADER;
+        m_size = size;
+    }
+
+    unsigned char* m_slot[0];
+}; // struct BigAllocationBlock
+
+struct FreelistEntry 
+{
+    FreelistEntry* next;
+}; // struct FreelistEntry
