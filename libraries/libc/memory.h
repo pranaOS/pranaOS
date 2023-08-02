f8653ae2ae00f2168883a59a928221aae35ab2d2
krishpranav <krisna.pranav@gmail.com>
2023-08-02 21:47:49 +0530

    libraries: libc[memory]

---
+++ libraries/libc/memory.h
@@ -0,0 +1,14 @@
+/**
+ * @file memory.h
+ * @author Krisna Pranav
+ * @brief Memory
+ * @version 1.0
+ * @date 2023-08-02
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+#include <string.h>
