d029fd9f83d08303ad6a50465c1dd119a9ae1fd1
krishpranav <krisna.pranav@gmail.com>
2023-08-03 22:22:17 +0530

    libraries: libc[stdint]
    <bits/stdint.h> -> <libc/stdint.h>

---
+++ libraries/libc/stdint.h
@@ -0,0 +1,15 @@
+/**
+ * @file stdint.h
+ * @author Krisna Pranav
+ * @brief stdint
+ * @version 1.0
+ * @date 2023-08-03
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+#include <sys/cdefs.h>
+#include <bits/stdint.h>
