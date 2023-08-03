15c67543b20202429bd4f77451cc0d591a2e3157
krishpranav <krisna.pranav@gmail.com>
2023-08-03 18:53:19 +0530

    libraries: libc[paths]

---
+++ libraries/libc/paths.h
@@ -0,0 +1,14 @@
+/**
+ * @file paths.h
+ * @author Krisna Pranav
+ * @brief paths
+ * @version 1.0
+ * @date 2023-08-03
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#define _PATH_MAILDIR "/var/mail"
