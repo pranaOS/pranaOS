5514a45806b93dfbbb5cf5b3b8c2f74e136a944a
krishpranav <krisna.pranav@gmail.com>
2023-08-03 22:18:36 +0530

    libraries: libc[stdbool]

--- libraries/libc/stdbool.h
+++ libraries/libc/stdbool.h
@@ -9,18 +9,18 @@
  * 
  */
 
-#pragma once 
+#pragma once
 
 #ifndef __cplusplus
-#   include <sys/cdefs.h>
+#    include <sys/cdefs.h>
 
 __BEGIN_DECLS
 
-#   define bool _Bool
-#   define true 1
-#   define false 0
+#    define bool _Bool
+#    define true 1
+#    define false 0
+#    define __bool_true_false_are_Defined 1
 
-__END_DECLS 
+__END_DECLS
 
-
-#endif 
+#endif
