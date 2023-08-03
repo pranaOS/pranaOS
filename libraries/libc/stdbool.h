05c77a1fab3ce874d6afff36c2f62311b27d7952
krishpranav <krisna.pranav@gmail.com>
2023-08-03 22:17:11 +0530

    libraries: libc[stdbool]

---
+++ libraries/libc/stdbool.h
@@ -0,0 +1,10 @@
+/**
+ * @file stdbool.h
+ * @author Krisna Pranav
+ * @brief stdbool
+ * @version 1.0
+ * @date 2023-08-03
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
