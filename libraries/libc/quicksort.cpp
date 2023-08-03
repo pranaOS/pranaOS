5ee420185490e7eca42ef2cd6bfe64e63e6f5487
krishpranav <krisna.pranav@gmail.com>
2023-08-03 19:29:21 +0530

    libraries: libc[quicksort]

--- libraries/libc/quicksort.cpp
+++ libraries/libc/quicksort.cpp
@@ -14,10 +14,9 @@
 #include <mods/assertions.h>
 #include <mods/quicksort.h>
 
-class SizedObject
-{
+class SizedObject {
 public:
-    SizedObject() = default;
+    SizedObject() = delete;
 
     /**
      * @param data 
@@ -28,18 +27,29 @@ public:
         , m_size(size)
     {}
 
-    void* data() const
-    {
-        return m_data;
+    /**
+     * @return void* 
+     */
+    void* data() const 
+    { 
+        return m_data; 
+    }
+
+    /**
+     * @return size_t 
+     */
+    size_t size() const 
+    { 
+        return m_size; 
     }
 
 private:
     void* m_data;
     size_t m_size;
-}; // class SizedObject
+};
 
-namespace Mods
-{
+namespace Mods {
+
     /**
      * @tparam  
      * @param a 
@@ -48,23 +58,25 @@ namespace Mods
     template<>
     inline void swap(const SizedObject& a, const SizedObject& b)
     {
-        ASSERT(a.size() == b.size);
-
+        ASSERT(a.size() == b.size());
         const size_t size = a.size();
         const auto a_data = reinterpret_cast<char*>(a.data());
         const auto b_data = reinterpret_cast<char*>(b.data());
-
         for (auto i = 0u; i < size; ++i) {
             swap(a_data[i], b_data[i]);
         }
     }
+
 } // namespace Mods
 
-class SizedObjectSlice 
-{
+class SizedObjectSlice {
 public:
     SizedObjectSlice() = delete;
-
+    
+    /**
+     * @param data 
+     * @param element_size 
+     */
     SizedObjectSlice(void* data, size_t element_size)
         : m_data(data)
         , m_element_size(element_size)
@@ -76,10 +88,45 @@ public:
      */
     const SizedObject operator[](size_t index)
     {
-        return { static_cast<char*>(m_data) + index }; 
+        return { static_cast<char*>(m_data) + index * m_element_size, m_element_size };
     }
 
 private:
     void* m_data;
     size_t m_element_size;
 }; // class SizedObjectSlice
+
+/**
+ * @param bot 
+ * @param nmemb 
+ * @param size 
+ * @param compar 
+ */
+void qsort(void* bot, size_t nmemb, size_t size, int (*compar)(const void*, const void*))
+{
+    if (nmemb <= 1) {
+        return;
+    }
+
+    SizedObjectSlice slice { bot, size };
+
+    Mods::dual_pivot_quick_sort(slice, 0, nmemb - 1, [=](const SizedObject& a, const SizedObject& b) { return compar(a.data(), b.data()) < 0; });
+} // void qsort
+
+/**
+ * @param bot 
+ * @param nmemb 
+ * @param size 
+ * @param compar 
+ * @param arg 
+ */
+void qsort_r(void* bot, size_t nmemb, size_t size, int (*compar)(const void*, const void*, void*), void* arg)
+{
+    if (nmemb <= 1) {
+        return;
+    }
+
+    SizedObjectSlice slice { bot, size };
+
+    Mods::dual_pivot_quick_sort(slice, 0, nmemb - 1, [=](const SizedObject& a, const SizedObject& b) { return compar(a.data(), b.data(), arg) < 0; });
+} // void qsort_r
