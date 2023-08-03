88544eccfef3bfb14e00dc067d7f71bb9d19c75f
krishpranav <krisna.pranav@gmail.com>
2023-08-03 22:20:59 +0530

    libraries: libc[stddef]

--- libraries/libc/stddef.h
+++ libraries/libc/stddef.h
@@ -9,16 +9,17 @@
  * 
  */
 
-#pragma once 
+#pragma once
 
 #ifndef KERNEL
+#    include <sys/cdefs.h>
+#    ifdef __cplusplus
+#        define NULL nullptr
+#    else
+#        define NULL ((void*)0)
+#    endif
 
-#   ifdef __cplusplus
-#       define NULL nullptr
-#   else
-#       define NULL ((void*)0)
-#   endif 
-
+typedef __PTRDIFF_TYPE__ ptrdiff_t;
 typedef __SIZE_TYPE__ size_t;
 
 #endif
