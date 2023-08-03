5d088be20239359cd855e740dedea59048668d30
krishpranav <krisna.pranav@gmail.com>
2023-08-03 22:24:03 +0530

    libraries: libc[stdio]

---
+++ libraries/libc/stdio.h
@@ -0,0 +1,21 @@
+/**
+ * @file stdio.h
+ * @author Krisna Pranav
+ * @brief STDIO
+ * @version 1.0
+ * @date 2023-08-03
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+#define _STDIO_H
+
+#include <limits.h>
+#include <stdarg.h>
+#include <sys/cdefs.h>
+#include <sys/types.h>
+
+#define FILENAME_MAX 1024
