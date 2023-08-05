067fc2448fae49b011929f22905c4d224fc12a80
krishpranav <krisna.pranav@gmail.com>
2023-08-05 20:01:09 +0530

    libraries: libc[termcap]

---
+++ libraries/libc/termcap.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file termcap.cpp
+ * @author Krisna Pranav
+ * @brief termcap
+ * @version 1.0
+ * @date 2023-08-05
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include <mods/hashmap.h>
+#include <mods/string.h>
+
+// termcap
