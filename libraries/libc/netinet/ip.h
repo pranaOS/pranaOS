5a05b61cd111517c0b93ee5ea8bed78a0622e44e
krishpranav <krisna.pranav@gmail.com>
2023-08-06 16:03:51 +0530

    libraries: libc[netinet(ip)]

---
+++ libraries/libc/netinet/ip.h
@@ -0,0 +1,14 @@
+/**
+ * @file ip.h
+ * @author Krisna Pranav
+ * @brief ip
+ * @version 1.0
+ * @date 2023-08-06
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+#include "in.h"
