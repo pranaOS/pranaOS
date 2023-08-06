d63afae8c3fedd6c9124ae1279d60d36a242bf79
krishpranav <krisna.pranav@gmail.com>
2023-08-06 13:16:13 +0530

    libraries: libc[ulimit]

---
+++ libraries/libc/ulimit.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file ulimit.cpp
+ * @author Krisna Pranav
+ * @brief ulimit
+ * @version 1.0
+ * @date 2023-08-06
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include <assert.h>
+#include <ulimit.h>
+#include <sys/resource.h>
+#include <mods/logstream.h>
+
+extern "C"
+{
+    /**
+     * @param cmd 
+     * @param newlimit 
+     * @return long 
+     */
+    long ulimit(int cmd, long newlimit)
+    {
+        (void)cmd;
+        (void)newlimit;
+
+        ASSERT_NOT_REACHED();
+        
+        return -1;
+    }
+}
