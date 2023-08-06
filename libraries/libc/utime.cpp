1e4f65c72c5bff39da6c5042c41f16f144bd2939
krishpranav <krisna.pranav@gmail.com>
2023-08-06 15:43:43 +0530

    libraries: libc[utime]

---
+++ libraries/libc/utime.cpp
@@ -0,0 +1,34 @@
+/**
+ * @file utime.cpp
+ * @author Krisna Pranav
+ * @brief utime
+ * @version 1.0
+ * @date 2023-08-06
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include <errno.h>
+#include <string.h>
+#include <utime.h>
+#include <kernel/api/syscall.h>
+
+extern "C"
+{
+
+    /**
+     * @param pathname 
+     * @param buf 
+     * @return int 
+     */
+    int utime(const char* pathname, const struct utimbuf* buf)
+    {
+        if (!pathname)
+        {
+            errno = EFAULT;
+            return -1;
+        }
+    }
+
+} // extern
