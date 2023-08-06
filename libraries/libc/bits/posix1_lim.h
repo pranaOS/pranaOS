7a7504430536c11d38285df4da2d829aa55feece
krishpranav <krisna.pranav@gmail.com>
2023-08-06 15:58:10 +0530

    libraries: libc[arpa(posix)]

---
+++ libraries/libc/bits/posix1_lim.h
@@ -0,0 +1,50 @@
+/**
+ * @file posix1_lim.h
+ * @author Krisna Pranav
+ * @brief posix1 lim
+ * @version 1.0
+ * @date 2023-08-06
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#define _POSIX_AIO_LISTIO_MAX 2               
+#define _POSIX_AIO_MAX 1                      
+#define _POSIX_ARG_MAX 4096                   
+#define _POSIX_CHILD_MAX 25                   
+#define _POSIX_DELAYTIMER_MAX 32              
+#define _POSIX_HOST_NAME_MAX 255              
+#define _POSIX_LINK_MAX 8                     
+#define _POSIX_LOGIN_NAME_MAX 9               
+#define _POSIX_MAX_CANON 255                  
+#define _POSIX_MAX_INPUT 255                  
+#define _POSIX_MQ_OPEN_MAX 8                  
+#define _POSIX_MQ_PRIO_MAX 32                 
+#define _POSIX_NAME_MAX 14                    
+#define _POSIX_NGROUPS_MAX 8                  
+#define _POSIX_OPEN_MAX 20                    
+#define _POSIX_PATH_MAX 256                   
+#define _POSIX_PIPE_BUF 512                   
+#define _POSIX_RE_DUP_MAX 255                 
+#define _POSIX_RTSIG_MAX 8                    
+#define _POSIX_SEM_NSEMS_MAX 256              
+#define _POSIX_SEM_VALUE_MAX 32767            
+#define _POSIX_SIGQUEUE_MAX 32                
+#define _POSIX_SSIZE_MAX 32767                
+#define _POSIX_SS_REPL_MAX 4                  
+#define _POSIX_STREAM_MAX 8                   
+#define _POSIX_SYMLINK_MAX 255                
+#define _POSIX_SYMLOOP_MAX 8                  
+#define _POSIX_THREAD_DESTRUCTOR_ITERATIONS 4 
+#define _POSIX_THREAD_KEYS_MAX 128            
+#define _POSIX_THREAD_THREADS_MAX 64          
+#define _POSIX_TIMER_MAX 32                   
+#define _POSIX_TRACE_EVENT_NAME_MAX 30        
+#define _POSIX_TRACE_NAME_MAX 8               
+#define _POSIX_TRACE_SYS_MAX 8                
+#define _POSIX_TRACE_USER_EVENT_MAX 32        
+#define _POSIX_TTY_NAME_MAX 9                 
+#define _POSIX_TZNAME_MAX 6                   
