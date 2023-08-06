1177d933f8bcb865ae6abe11f3f139c7f515fe88
krishpranav <krisna.pranav@gmail.com>
2023-08-06 18:49:53 +0530

    libraries: libelf[validation]

--- libraries/libelf/validation.cpp
+++ libraries/libelf/validation.cpp
@@ -9,6 +9,7 @@
  * 
  */
 
+#include "validation.h"
 #include <libelf/validation.h>
 #include <libelf/exec_elf.h>
 #include <mods/assertions.h>
@@ -56,6 +57,34 @@ namespace ELF
                 dbgputstr("File has unknown SYSV ABI version (%d)\n", elf_header.e_ident[EI_ABIVERSION]);
             return false;
         }
+    }
+
+    /**
+     * @param elf_header 
+     * @param file_size 
+     * @param buffer 
+     * @param buffer_size 
+     * @param interpreter_path 
+     * @param verbose 
+     * @return true 
+     * @return false 
+     */
+    bool validate_program_headers(const Elf32_Ehdr& elf_header, size_t file_size, const u8* buffer, size_t buffer_size, String* interpreter_path, bool verbose)
+    {
+        size_t end_of_last_program_header = elf_header.e_phoff + (elf_header.e_phnum * elf_header.e_phentsize);
+
+        if (end_of_last_program_header > buffer_size) {
+            if (verbose)
+                dbgprintf("Unable to parse program headers");
+            
+            return false;
+        }        
+
+        if (file_size < buffer_size) {
+            ASSERT_NOT_REACHED();
+        }
+
+        size_t num_program_headers = elf_header.e_phnum;
     }
 
 } // namespace ELF
