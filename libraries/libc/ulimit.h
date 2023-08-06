3e3f802b22cb8cef35b38b98608721270749d9a8
krishpranav <krisna.pranav@gmail.com>
2023-08-06 13:15:01 +0530

    libraries: libc[ulimit]

---
+++ libraries/libc/ulimit.h
@@ -0,0 +1,25 @@
+/**
+ * @file ulimit.h
+ * @author Krisna Pranav
+ * @brief ulimit
+ * @version 1.0
+ * @date 2023-08-06
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
+/**
+ * @param cmd 
+ * @param newlimit 
+ * @return long 
+ */
+long ulimit(int cmd, long newlimit);
+
+__END_DECLS
