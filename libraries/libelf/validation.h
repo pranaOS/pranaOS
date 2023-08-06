49b632b61be1019bc2262405cf1407b93e0ed9f5
krishpranav <krisna.pranav@gmail.com>
2023-08-06 18:37:48 +0530

    libraries: libelf[validation]

---
+++ libraries/libelf/validation.h
@@ -0,0 +1,26 @@
+/**
+ * @file validation.h
+ * @author Krisna Pranav
+ * @brief validation
+ * @version 1.0
+ * @date 2023-08-06
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+#include <libelf/exec_elf.h>
+
+namespace ELF
+{
+    /**
+     * @param elf_header 
+     * @param file_size 
+     * @param verbose 
+     * @return true 
+     * @return false 
+     */
+    bool validate_elf_header(const Elf32_Ehdr& elf_header, size_t file_size, bool verbose = true);
+}
