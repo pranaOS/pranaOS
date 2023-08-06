f57824359643647ee24159f561a212a28976b022
krishpranav <krisna.pranav@gmail.com>
2023-08-06 16:04:02 +0530

    libraries: libc[sys(file)]

---
+++ libraries/libc/sys/file.h
@@ -0,0 +1,12 @@
+/**
+ * @file file.h
+ * @author Krisna Pranav
+ * @brief file
+ * @version 1.0
+ * @date 2023-08-06
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
