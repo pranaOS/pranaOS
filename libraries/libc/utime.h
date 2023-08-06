3011a0f6a58bdc3a43698e3571abe8e2483fbcbe
krishpranav <krisna.pranav@gmail.com>
2023-08-06 15:41:30 +0530

    libraries: libc[utime]

---
+++ libraries/libc/utime.h
@@ -0,0 +1,24 @@
+/**
+ * @file utime.h
+ * @author Krisna Pranav
+ * @brief utime
+ * @version 1.0
+ * @date 2023-08-06
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
+/**
+ * @param pathname 
+ * @return int 
+ */
+int utime(const char* pathname, const struct utimbuf*);
+
+__END_DECLS
