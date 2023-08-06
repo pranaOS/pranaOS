62defe39f43243887dc0c966fda33a744ef6521c
krishpranav <krisna.pranav@gmail.com>
2023-08-06 13:11:44 +0530

    libraries: libc[times]

---
+++ libraries/libc/times.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file times.cpp
+ * @author Krisna Pranav
+ * @brief times
+ * @version 1.0
+ * @date 2023-08-06
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+
+#include <errno.h>
+#include <sys/times.h>
+#include <kernel/api/syscall.h>
+
+/**
+ * @param buf 
+ * @return clock_t 
+ */
+clock_t times(struct tms* buf)
+{
+    int rc = syscall(SC_times, buf);
+    __RETURN_WITH_ERRNO(rc, rc, (clock_t)-1);
+}
