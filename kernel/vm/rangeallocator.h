b8231c9763e5bea84d5ed2c311a6bb0493062666
krishpranav <krisna.pranav@gmail.com>
2023-08-07 19:05:03 +0530

    kernel: vm[rangeallocator]

--- kernel/vm/rangeallocator.h
+++ kernel/vm/rangeallocator.h
@@ -17,43 +17,49 @@
 #include <mods/traits.h>
 #include <mods/vector.h>
 
-namespace Kernel
+namespace Kernel 
 {
-    class Range
+
+    class Range 
     {
         friend class RangeAllocator;
-    
-    public:
-        Range() {}
 
+    public:
+        Range() { }
+
+        /**
+         * @param base 
+         * @param size 
+         */
         Range(VirtualAddress base, size_t size)
             : m_base(base)
             , m_size(size)
-        {}
+        {
+        }
 
         /**
          * @return VirtualAddress 
          */
-        VirtualAddress base() const
-        {
-            return m_base;
+        VirtualAddress base() const 
+        { 
+            return m_base; 
         }
 
         /**
          * @return size_t 
          */
-        size_t size() const
+        size_t size() const 
         {
-            return m_size;
+            return m_size; 
         }
 
         /**
          * @return true 
          * @return false 
          */
-        bool is_valid() const
-        {
-            return !m_base.is_null();
+        bool is_valid() const 
+        { 
+            return !m_base.is_null(); 
         }
 
         /**
@@ -61,17 +67,17 @@ namespace Kernel
          * @return true 
          * @return false 
          */
-        bool contains(VirtualAddress vaddr) const
-        {
-            return vaddr >= base() && vaddr < end();
+        bool contains(VirtualAddress vaddr) const 
+        { 
+            return vaddr >= base() && vaddr < end(); 
         }
 
         /**
          * @return VirtualAddress 
          */
-        VirtualAddress end() const
-        {
-            return m_base.offset(m_size);
+        VirtualAddress end() const 
+        { 
+            return m_base.offset(m_size); 
         }
 
         /**
@@ -94,7 +100,7 @@ namespace Kernel
         {
             if (base.offset(size) < base)
                 return false;
-            
+
             return base >= m_base && base.offset(size) <= end();
         }
 
@@ -118,13 +124,13 @@ namespace Kernel
         size_t m_size { 0 };
     }; // class Range
 
-
-    class RangeAllocator
+    class RangeAllocator 
     {
     public:
         RangeAllocator();
         ~RangeAllocator();
 
+        /// @brief: initialize_with_range
         void initialize_with_range(VirtualAddress, size_t);
         void initialize_from_parent(const RangeAllocator&);
 
@@ -138,8 +144,11 @@ namespace Kernel
          * @return Range 
          */
         Range allocate_specific(VirtualAddress, size_t);
-        
+
+        /// @brief: deallocate
         void deallocate(Range);
+
+        void dump() const;
 
         /**
          * @param range 
@@ -156,8 +165,35 @@ namespace Kernel
         void carve_at_index(int, const Range&);
 
         Vector<Range> m_available_ranges;
+
         Range m_total_range;
         mutable SpinLock<u8> m_lock;
     }; // class RangeAllocator
 
+    /**
+     * @param stream 
+     * @param value 
+     * @return const LogStream& 
+     */
+    inline const LogStream& operator<<(const LogStream& stream, const Range& value)
+    {
+        return stream << String::format("Range(%x-%x)", value.base().get(), value.end().get() - 1);
+    }
+
 } // namespace Kernel
+
+namespace Mods 
+{
+    template<>
+    struct Traits<Kernel::Range> : public GenericTraits<Kernel::Range> 
+    {
+        /**
+         * @return true 
+         * @return false 
+         */
+        static constexpr bool is_trivial() 
+        { 
+            return true; 
+        }
+    };
+} // namespace Mods
