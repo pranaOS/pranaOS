ac49bf2f5fc3fc7ae96e854449d5c5af30d2173d
krishpranav <krisna.pranav@gmail.com>
2023-08-08 19:33:08 +0530

    kernel: vm[inodevmobject]

--- kernel/vm/inodevmobject.h
+++ kernel/vm/inodevmobject.h
@@ -16,8 +16,9 @@
 #include <mods/bitmap.h>
 #include <mods/typecast.h>
 
-namespace Kernel
+namespace Kernel 
 {
+
     class InodeVMObject : public VMObject 
     {
     public:
@@ -27,50 +28,76 @@ namespace Kernel
          * @return Inode& 
          */
         Inode& inode() 
-        {
-            return *m_inode;
+        { 
+            return *m_inode; 
         }
 
         /**
          * @return const Inode& 
          */
-        const Inode& inode() const
-        {
-            return *m_inode;
-        }   
+        const Inode& inode() const 
+        { 
+            return *m_inode; 
+        }
 
-        void inode_contents_changed(Badge<Inode>, off_t, ssize_t);
+        void inode_contents_changed(Badge<Inode>, off_t, ssize_t, const UserOrKernelBuffer&);
 
         /**
          * @param old_size 
          * @param new_size 
          */
-        void inode_size_changes(Badge<Inode>, size_t old_size, size_t new_size);
+        void inode_size_changed(Badge<Inode>, size_t old_size, size_t new_size);
 
+        /**
+         * @return size_t 
+         */
         size_t amount_dirty() const;
-        size_t amount_clear() const;
+        size_t amount_clean() const;
 
+        int release_all_clean_pages();
+
+        /**
+         * @return u32 
+         */
+        u32 writable_mappings() const;
+        u32 executable_mappings() const;
 
     protected:
         explicit InodeVMObject(Inode&, size_t);
         explicit InodeVMObject(const InodeVMObject&);
 
-        virtual bool is_inode() const final
-        {
-            return true;
+        /**
+         * @return InodeVMObject& 
+         */
+        InodeVMObject& operator=(const InodeVMObject&) = delete;
+        InodeVMObject& operator=(InodeVMObject&&) = delete;
+
+        /**
+         * @brief Construct a new Inode VM Object object
+         * 
+         */
+        InodeVMObject(InodeVMObject&&) = delete;
+
+        /**
+         * @return true 
+         * @return false 
+         */
+        virtual bool is_inode() const final 
+        { 
+            return true; 
         }
 
         int release_all_clean_pages_impl();
 
         NonnullRefPtr<Inode> m_inode;
         Bitmap m_dirty_pages;
-
     }; // class InodeVMObject
+
 } // namespace Kernel
 
 MOD_BEGIN_TYPE_TRAITS(Kernel::InodeVMObject)
-static bool is_type(const Kernel::VMObject& vmobject)
-{
-    return vmobject.is_inode();
+static bool is_type(const Kernel::VMObject& vmobject) 
+{ 
+    return vmobject.is_inode(); 
 }
 MOD_END_TYPE_TRAITS()
