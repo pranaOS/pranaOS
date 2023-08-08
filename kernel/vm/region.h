6d96fcdc44832c6b61af4e4a2dd03732db8f7eb5
krishpranav <krisna.pranav@gmail.com>
2023-08-08 20:28:50 +0530

    kernel: vm[region]

--- kernel/vm/region.h
+++ kernel/vm/region.h
@@ -70,5 +70,64 @@ namespace Kernel
          * @return NonnullOwnPtr<Region> 
          */
         static NonnullOwnPtr<Region> create_kernel_only(const Range&, NonnullRefPtr<VMObject>, size_t offset_in_vmobject, const StringView& name, u8 access, bool cacheable = true);
-    }
+
+        ~Region();
+
+    private:
+        Bitmap& ensure_cow_map() const;
+
+        void set_access_bit(Access access, bool b)
+        {
+        }
+
+        /**
+         * @param page_index 
+         * @return true 
+         * @return false 
+         */
+        bool commit(size_t page_index);
+
+        /**
+         * @param index 
+         * @param with_flush 
+         * @return true 
+         * @return false 
+         */
+        bool remap_page(size_t index, bool with_flush = true);
+
+        /**
+         * @param page_index 
+         * @return PageFaultResponse 
+         */
+        PageFaultResponse handle_cow_fault(size_t page_index);
+
+        /**
+         * @param page_index 
+         * @return PageFaultResponse 
+         */
+        PageFaultResponse handle_inode_fault(size_t page_index);
+
+        /**
+         * @param page_index 
+         * @return PageFaultResponse 
+         */
+        PageFaultResponse handle_zero_fault(size_t page_index);
+
+        /**
+         * @param page_index 
+         * @return true 
+         * @return false 
+         */
+        bool map_individual_page_impl(size_t page_index);   
+
+        RefPtr<PageDirectory> m_page_directory;
+        Range m_range;
+        size_t m_offset_in_vmobject { 0 };
+        NonnullRefPtr<VMObject> m_vmobject;
+        String m_name;
+        u8 m_access { 0 };
+        InheritMode m_inherid_mode : 3 { InheritMode::Default };
+        bool m_shared : 1 { false };
+        bool m_user_accessible : 1 { false };
+    };
 } // namespace Kernel   
