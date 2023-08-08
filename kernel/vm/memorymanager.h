c082d9781bc4bf55d85e6ba931391ba4ab8efa60
krishpranav <krisna.pranav@gmail.com>
2023-08-08 21:52:54 +0530

    kernel: vm[memorymanager]

--- kernel/vm/memorymanager.h
+++ kernel/vm/memorymanager.h
@@ -11,6 +11,7 @@
 
 #pragma once 
 
+#include "vmobject.h"
 #include <kernel/forward.h>
 #include <kernel/spinlock.h>
 #include <kernel/arch/i386/cpu.h>
@@ -23,5 +24,59 @@
 
 namespace Kernel
 {
+    #define PAGE_ROUND_UP(x)
+
+    /**
+     * @tparam T 
+     * @param physical 
+     * @return T* 
+     */
+    template<typename T>
+    inline T* low_physical_to_virtual(T* physical)
+    {
+        return (T*) (((u8*)physical) + 0xc00000000);
+    }
+
+    class KBuffer;
+    class SynthFSInode;
+
+    #define MM Kernel::MemoryManager::the()
+
+    struct MemoryManagerData 
+    {
+        SpinLock<u8> m_quickmap_in_use;
+        u32 m_quickmap_prev_flags;
+        PhysicalAddress m_last_quickmap_pd;
+    };
+
+    extern RecursiveSpinLock s_mm_lock;
+
+    class MemoryManager
+    {
+        MOD_MAKE_ETERNAL
+        friend class PageDirectory;
+        friend class PhysicalPage;
+        friend class PhysicalRange;
+        friend class Region;
+        friend class VMObject;
+
+    public:
+        static MemoryManager& the();
+
+        static bool is_initialized();
+        
+        /**
+         * @param cpu 
+         */
+        static void initialize(u32 cpu);
+
+        /**
+         * @return MemoryManagerData& 
+         */
+        static inline MemoryManagerData& get_data()
+        {
+            return Processor::current().get_mm_data();
+        }
+    }
 
 } // namespace Kernel
