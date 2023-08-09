a2ed2ef633234f30dea87da5a1f5f82a678003cd
krishpranav <krisna.pranav@gmail.com>
2023-08-09 19:54:37 +0530

    kernel: vm[purgeablevmobject]

--- kernel/vm/purgeablevmobject.h
+++ kernel/vm/purgeablevmobject.h
@@ -13,11 +13,13 @@
 
 #include <kernel/vm/anonymousvmobject.h>
 
-namespace Kernel
+namespace Kernel 
 {
+
     class PurgeableVMObject final : public AnonymousVMObject 
     {
     public:
+        /// @brief Destroy the PurgeableVMObject object
         virtual ~PurgeableVMObject() override;
 
         /**
@@ -25,10 +27,16 @@ namespace Kernel
          */
         static NonnullRefPtr<PurgeableVMObject> create_with_size(size_t);
 
+        /**
+         * @return NonnullRefPtr<VMObject> 
+         */
         virtual NonnullRefPtr<VMObject> clone() override;
 
         int purge();
 
+        /**
+         * @return int 
+         */
         int purge_with_interrupts_disabled(Badge<MemoryManager>);
 
         /**
@@ -36,60 +44,78 @@ namespace Kernel
          * @return false 
          */
         bool was_purged() const 
-        {
-            return m_was_purged;
+        { 
+            return m_was_purged; 
         }
-
+        
         /**
          * @param b 
          */
-        void set_was_purged(bool b)
-        {
-            m_was_purged = b;
+        void set_was_purged(bool b) 
+        { 
+            m_was_purged = b; 
         }
 
         /**
          * @return true 
          * @return false 
          */
-        bool is_volatile() const
-        {
-            return m_volatile;
+        bool is_volatile() const 
+        { 
+            return m_volatile; 
         }
 
         /**
          * @param b 
          */
         void set_volatile(bool b) 
-        {
-            m_volatile = b;
+        { 
+            m_volatile = b; 
         }
 
-
     private:
+        /// * @brief Construct a new PurgeableVMObject object
         explicit PurgeableVMObject(size_t);
         explicit PurgeableVMObject(const PurgeableVMObject&);
 
         /**
          * @return const char* 
          */
-        virtual const char* class_name() const override
-        {
-            return "PurgeableVMObject";
+        virtual const char* class_name() const override 
+        { 
+            return "PurgeableVMObject"; 
         }
 
         int purge_impl();
 
         /**
+         * @return PurgeableVMObject& 
+         */
+        PurgeableVMObject& operator=(const PurgeableVMObject&) = delete;
+
+        /**
+         * @return PurgeableVMObject& 
+         */
+        PurgeableVMObject& operator=(PurgeableVMObject&&) = delete;
+
+        PurgeableVMObject(PurgeableVMObject&&) = delete;
+
+        /**
          * @return true 
          * @return false 
          */
-        virtual bool is_purgeable() const override
-        {
-            return true;
+        virtual bool is_purgeable() const override 
+        { 
+            return true; 
         }
 
         bool m_was_purged { false };
         bool m_volatile { false };
-    }; 
+
+    }; // class PurgeableVMObject
+
 } // namespace Kernel
+
+MOD_BEGIN_TYPE_TRAITS(Kernel::PurgeableVMObject)
+static bool is_type(const Kernel::VMObject& vmobject) { return vmobject.is_purgeable(); }
+MOD_END_TYPE_TRAITS()
