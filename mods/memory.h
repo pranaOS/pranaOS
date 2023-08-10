5ca7f49fe3cff2ce79154deae6cd180f036efdd3
krishpranav <krisna.pranav@gmail.com>
2023-08-10 19:52:45 +0530

    mods: memory

--- mods/memory.h
+++ mods/memory.h
@@ -16,11 +16,34 @@
 #if defined(KERNEL)
 #   include <kernel/stdlib.h>
 #else
-#   include <stdlib.h>
-#   include <string.h>
+#    include <stdlib.h>
+#    include <string.h>
 #endif
 
+/**
+ * @param dest 
+ * @param src 
+ * @param count 
+ * @return ALWAYS_INLINE 
+ */
 ALWAYS_INLINE void fast_u32_copy(u32* dest, const u32* src, size_t count)
 {
-    
+    asm volatile(
+        "rep movsl\n"
+        : "+S"(src), "+D"(dest), "+c"(count)::"memory");
+}
+
+/**
+ * @param dest 
+ * @param value 
+ * @param count 
+ * @return ALWAYS_INLINE 
+ */
+ALWAYS_INLINE void fast_u32_fill(u32* dest, u32 value, size_t count)
+{
+    asm volatile(
+        "rep stosl\n"
+        : "=D"(dest), "=c"(count)
+        : "D"(dest), "c"(count), "a"(value)
+        : "memory");
 }
