eab241b27b0b664e34ccfc79eb7e731792f3c452
krishpranav <krisna.pranav@gmail.com>
2023-08-10 20:05:45 +0530

    kernel: doublebuffer

---
+++ kernel/doublebuffer.h
@@ -0,0 +1,26 @@
+/**
+ * @file doublebuffer.h
+ * @author Krisna Pranav
+ * @brief double buffer
+ * @version 1.0
+ * @date 2023-08-10
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+#include <mods/types.h>
+#include <kernel/lock.h>
+#include <kernel/thread.h>
+#include <kernel/userorkernelbuffer.h>
+#include <kernel/kbuffer.h>
+
+namespace Kernel
+{
+    class DoubleBuffer
+    {
+        
+    }; // class DoubleBuffer
+}
