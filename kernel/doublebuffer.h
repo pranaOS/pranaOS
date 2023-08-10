fbf886f054448c95c85c26b3f26fad4a9501d416
krishpranav <krisna.pranav@gmail.com>
2023-08-10 20:12:55 +0530

    kernel: doublebuffer

--- kernel/doublebuffer.h
+++ kernel/doublebuffer.h
@@ -20,7 +20,68 @@
 namespace Kernel
 {
     class DoubleBuffer
-    {
+    {   
+    public:
+        explicit DoubleBuffer(size_t capacity = 65536);
+
+        /**
+         * @return ssize_t 
+         */
+        [[nodiscard]] ssize_t write(const UserOrKernelBuffer&, size_t);
+
+        /**
+         * @param data 
+         * @param size 
+         * @return ssize_t 
+         */
+        [[nodiscard]] ssize_t write(const u8* data, size_t size)
+        {
+            return write(UserOrKernelBuffer::for_kernel_buffer(const_cast<u8>*(data)));
+        }
+
+        /**
+         * @return ssize_t 
+         */
+        [[nodiscard]] ssize_t read(UserOrKernelBuffer&, size_t);
+
+        /**
+         * @param data 
+         * @param size 
+         * @return ssize_t 
+         */
+        [[nodiscard]] ssize_t read(u8* data, size_t size)
+        {
+            auto buffer = UserOrKernelBuffer::for_kernel_buffer(data);
+
+            return read(buffer, size);
+        }
         
+
+    private:
+        void flip();
+        void compute_lockfree_metadata();
+
+        struct InnerBuffer
+        {
+            u8* data { nullptr };
+            size_t size;
+        }; 
+
+        InnerBuffer* m_write_buffer { nullptr };
+        InnerBuffer* m_read_buffer { nullptr };
+        InnerBuffer m_buffer1;
+        InnerBuffer m_buffer2;
+        
+        KBuffer m_storage;
+
+        Function<void()> m_unlock_callback;
+        
+        size_t m_capacity { 0 };
+        size_t m_read_buffer_index { 0 };
+        size_t m_space_for_writing { 0 };
+
+        bool m_empty { true };
+
+        mutable Lock m_lock { "DoubleBuffer" };
     }; // class DoubleBuffer
 }
