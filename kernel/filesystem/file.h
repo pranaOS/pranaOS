70eee3278e79488a223cabc67c6c7a32c0c7b8d1
krishpranav <krisna.pranav@gmail.com>
2023-08-10 19:36:17 +0530

    kernel: filesystem[file]

--- kernel/filesystem/file.h
+++ kernel/filesystem/file.h
@@ -60,7 +60,66 @@ namespace Kernel
     class File 
         : public RefCounted<File>
         , public Weakable<File> {
-        
+
+    public:
+        virtual ~File();
+
+        /**
+         * @param options 
+         * @return KResultOr<NonnullRefPtr<FileDescription>> 
+         */
+        virtual KResultOr<NonnullRefPtr<FileDescription>> open(int options);
+        virtual KResult close();
+
+        /**
+         * @return true 
+         * @return false 
+         */
+        virtual bool can_read(const FileDescription&, size_t) const = 0;
+
+        /**
+         * @return true 
+         * @return false 
+         */
+        virtual bool can_write(const FileDescription&, size_t) const = 0;
+
+        /**
+         * @return KResult<size_t> 
+         */
+        virtual KResult<size_t> read(FileDescription&, size_t, UserOrKernelBuffer&, size_t) = 0;
+
+        /**
+         * @return KResult<size_t> 
+         */
+        virtual KResult<size_t> write(FileDescription&, size_t, const UserOrKernelBuffer&, size_t) = 0;
+
+        /**
+         * @param request 
+         * @param arg 
+         * @return int 
+         */
+        virtual int ioctl(FileDescription&, unsigned request, FlatPtr arg);
+
+        /**
+         * @return String 
+         */
+        virtual String absolute_path(const FileDescription&) const = 0; 
+
+    protected:
+        File();
+
+        void evaluate_block_conditions()
+        {
+            if (Processor::current().in_irq()) {
+                Processor::deferred_call_queue([self = make_weak_ptr()]() {
+                    if (auto file = self.strong_ref())
+                        file->do_evaluate_block_conditions();
+                });
+            } else {
+                do_evaluate_block_conditions();
+            }
+        }
+
     private:
         /**
          * @return ALWAYS_INLINE 
