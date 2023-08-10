410ec6cd8b49c5413e5b6d3199300dc4ae18f407
krishpranav <krisna.pranav@gmail.com>
2023-08-10 18:40:26 +0530

    kernel: vm[privateinodevmobject]

--- kernel/vm/privateinodevmobject.h
+++ kernel/vm/privateinodevmobject.h
@@ -15,13 +15,22 @@
 #include <kernel/unixtypes.h>
 #include <kernel/vm/inodevmobject.h>
 
-namespace Kernel
+namespace Kernel 
 {
+
     class PrivateInodeVMObject final : public InodeVMObject 
     {
         MOD_MAKE_NONMOVABLE(PrivateInodeVMObject);
 
-    public: 
+    public:
+        /// @brief Destroy the Private Inode V M Object object
+        virtual ~PrivateInodeVMObject() override;
+
+        /**
+         * @return NonnullRefPtr<PrivateInodeVMObject> 
+         */
+        static NonnullRefPtr<PrivateInodeVMObject> create_with_inode(Inode&);
+
         /**
          * @return NonnullRefPtr<VMObject> 
          */
@@ -32,20 +41,21 @@ namespace Kernel
          * @return true 
          * @return false 
          */
-        virtual bool is_private_inode() const override
-        {
-            return true;
+        virtual bool is_private_inode() const override 
+        { 
+            return true; 
         }
 
+        /// @brief Construct a new Private Inode V M Object object
         explicit PrivateInodeVMObject(Inode&, size_t);
         explicit PrivateInodeVMObject(const PrivateInodeVMObject&);
 
         /**
          * @return const char* 
          */
-        virtual const char* class_name() const override
-        {
-            return "PrivateInodeVMObject";
+        virtual const char* class_name() const override 
+        { 
+            return "PrivateInodeVMObject"; 
         }
 
         /**
@@ -53,4 +63,5 @@ namespace Kernel
          */
         PrivateInodeVMObject& operator=(const PrivateInodeVMObject&) = delete;
     }; // class PrivateInodeVMObject
-} // namespace Kernel
+
+} // namespace Kernel 
