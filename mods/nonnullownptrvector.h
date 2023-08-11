172fce124aea6c5520a8d8f2fde46fa2f4c537bb
krishpranav <krisna.pranav@gmail.com>
2023-08-11 20:02:20 +0530

    mods: nonnullownptrvector

---
+++ mods/nonnullownptrvector.h
@@ -0,0 +1,32 @@
+/**
+ * @file nonnullownptrvector.h
+ * @author Krisna Pranav
+ * @brief NonNull_OwnPtr_Vector
+ * @version 1.0
+ * @date 2023-08-11
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+#include <mods/nonnullownptr.h>
+#include <mods/nonnullptrvector.h>
+
+namespace Mods 
+{
+
+    /**
+     * @tparam T 
+     * @tparam inline_capacity 
+     */
+    template<typename T, int inline_capacity>
+    class NonnullOwnPtrVector : public NonnullPtrVector<NonnullOwnPtr<T>, inline_capacity> 
+    {
+    };
+
+} // namespace Mods
+
+// using the mods.
+using Mods::NonnullOwnPtrVector;
