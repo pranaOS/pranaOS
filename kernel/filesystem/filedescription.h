4869882718aadb3021acfc5afc906afa72e38f3c
krishpranav <krisna.pranav@gmail.com>
2023-08-11 22:05:14 +0530

    kernel: filesystem[filedescription]

--- kernel/filesystem/filedescription.h
+++ kernel/filesystem/filedescription.h
@@ -21,19 +21,293 @@
 #include <kernel/filesystem/inodemetadata.h>
 #include <kernel/filesystem/virtualfilesystem.h>
 
-namespace Kernel
+namespace Kernel 
 {
 
     class FileDescription : public RefCounted<FileDescription> 
     {
         MAKE_SLAB_ALLOCATED(FileDescription)
+    public:
+
+        /**
+         * @return NonnullRefPtr<FileDescription> 
+         */
+        static NonnullRefPtr<FileDescription> create(Custody&);
+        static NonnullRefPtr<FileDescription> create(File&);
+
+        /// @brief Destroy the File Description object
+        ~FileDescription();
+
+        /**
+         * @return Thread::FileBlocker::BlockFlags 
+         */
+        Thread::FileBlocker::BlockFlags should_unblock(Thread::FileBlocker::BlockFlags) const;
+
+        /**
+         * @return true 
+         * @return false 
+         */
+        bool is_readable() const 
+        { 
+            return m_readable; 
+        }
+
+        bool is_writable() const 
+        { 
+            return m_writable; 
+        }
+
+        /**
+         * @param b 
+         */
+        void set_readable(bool b) 
+        { 
+            m_readable = b; 
+        }
+
+        void set_writable(bool b) 
+        { 
+            m_writable = b; 
+        }
+
+        /**
+         * @param options 
+         */
+        void set_rw_mode(int options)
+        {
+            set_readable(options & O_RDONLY);
+            set_writable(options & O_WRONLY);
+        }
+
+        /**
+         * @return KResult 
+         */
+        KResult close();
+
+        /**
+         * @param whence 
+         * @return off_t 
+         */
+        off_t seek(off_t, int whence);
+
+        /**
+         * @return KResultOr<size_t> 
+         */
+        KResultOr<size_t> read(UserOrKernelBuffer&, size_t);
+        KResultOr<size_t> write(const UserOrKernelBuffer& data, size_t);
+
+        /**
+         * @return KResult 
+         */
+        KResult stat(::stat&);
+        KResult chmod(mode_t);
+
+        /**
+         * @return true 
+         * @return false 
+         */
+        bool can_read() const;
+        bool can_write() const;
+
+        /**
+         * @param buffer 
+         * @return ssize_t 
+         */
+        ssize_t get_dir_entries(UserOrKernelBuffer& buffer, ssize_t);
+
+        /**
+         * @return KResultOr<KBuffer> 
+         */
+        KResultOr<KBuffer> read_entire_file();
+
+        /**
+         * @return String 
+         */
+        String absolute_path() const;
+
+        /**
+         * @return true 
+         * @return false 
+         */
+        bool is_direct() const 
+        { 
+            return m_direct; 
+        }
+
+        /**
+         * @return true 
+         * @return false 
+         */
+        bool is_directory() const 
+        { 
+            return m_is_directory; 
+        }
+
+        /**
+         * @return File& 
+         */
+        File& file() 
+        { 
+            return *m_file; 
+        }
+
+        /**
+         * @return const File& 
+         */
+        const File& file() const 
+        { 
+            return *m_file; 
+        }
+
+        bool is_device() const;
+
+        /**
+         * @return const Device* 
+         */
+        const Device* device() const;
+
+        /**
+         * @return Device* 
+         */
+        Device* device();
+
+        /// @brief: tty
+        bool is_tty() const;
+        const TTY* tty() const;
+        TTY* tty();
+
+        
+        /// @breif: masterptty
+        bool is_master_pty() const;
+        const MasterPTY* master_pty() const;
+        MasterPTY* master_pty();
+
+        /**
+         * @return InodeMetadata 
+         */
+        InodeMetadata metadata() const;
+
+        /**
+         * @return Inode* 
+         */
+        Inode* inode() 
+        { 
+            return m_inode.ptr(); 
+        }
+
+        /**
+         * @return const Inode* 
+         */
+        const Inode* inode() const 
+        { 
+            return m_inode.ptr(); 
+        }
+
+        /**
+         * @return Custody* 
+         */
+        Custody* custody() 
+        { 
+            return m_custody.ptr(); 
+        }
+
+        /**
+         * @return const Custody* 
+         */
+        const Custody* custody() const 
+        { 
+            return m_custody.ptr(); 
+        }   
+
+        /**
+         * @param offset 
+         * @param prot 
+         * @param shared 
+         * @return KResultOr<Region*> 
+         */
+        KResultOr<Region*> mmap(Process&, VirtualAddress, size_t offset, size_t, int prot, bool shared);
+
+        /**
+         * @return true 
+         * @return false 
+         */
+        bool is_blocking() const 
+        { 
+            return m_is_blocking; 
+        }
+
+        /**
+         * @param b 
+         */
+        void set_blocking(bool b) 
+        { 
+            m_is_blocking = b; 
+        }
+
+        /**
+         * @return true 
+         * @return false 
+         */
+        bool should_append() const 
+        { 
+            return m_should_append; 
+        }
+
+        /**
+         * @param s 
+         */
+        void set_should_append(bool s) 
+        { 
+            m_should_append = s; 
+        }
+
+        u32 file_flags() const 
+        { 
+            return m_file_flags; 
+        }
+
+        void set_file_flags(u32);
+
+        bool is_socket() const;
+        Socket* socket();
+        const Socket* socket() const;
+
+        bool is_fifo() const;
+        FIFO* fifo();
+        FIFO::Direction fifo_direction() 
+        { 
+            return m_fifo_direction; 
+        }
+
+        void set_fifo_direction(Badge<FIFO>, FIFO::Direction direction) 
+        { 
+            m_fifo_direction = direction; 
+        }
+
+        Optional<KBuffer>& generator_cache() 
+        { 
+            return m_generator_cache; 
+        }
+
+        void set_original_inode(Badge<VFS>, NonnullRefPtr<Inode>&& inode) 
+        { 
+            m_inode = move(inode); 
+        }
+
+        KResult truncate(u64);
+
+        off_t offset() const 
+        { 
+            return m_current_offset; 
+        }
+
+        KResult chown(uid_t, gid_t);
+
+        FileBlockCondition& block_condition();
 
     private:
         friend class VFS;
-
-        /// @brief Construct a new File Description object
         explicit FileDescription(File&);
-
         FileDescription(FIFO&, FIFO::Direction);
 
         void evaluate_block_conditions()
@@ -62,8 +336,6 @@ namespace Kernel
         FIFO::Direction m_fifo_direction { FIFO::Direction::Neither };
 
         Lock m_lock { "FileDescription" };
-
-
-    } // class FileDescription
+    };
 
 } // namespace Kernel
