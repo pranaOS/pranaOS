109e9397474ebcef70ce1f9756366800c2d07f08
krishpranav <krisna.pranav@gmail.com>
2023-08-11 18:31:20 +0530

    kernel: lock

--- kernel/lock.h
+++ kernel/lock.h
@@ -19,41 +19,55 @@
 #include <mods/types.h>
 #include <mods/assertions.h>
 
-namespace Kernel
+namespace Kernel 
 {
-    class Lock
+
+    class Lock 
     {
     public:
+        /**
+         * @param name 
+         */
         Lock(const char* name = nullptr)
             : m_name(name)
-        {}
+        {
+        }
 
-        ~Lock() {}
+        ~Lock() { }
 
-        enum class Mode
+        enum class Mode 
         {
             Unlocked,
             Shared,
             Exclusive
-        };
-
+        }; // enum 
+        
         void lock(Mode = Mode::Exclusive);
 
-        #ifdef LOCK_DEBUG
-            void lock(const char* file, int line, Mode mode = Mode::Exclusive);
-        #endif
+    #ifdef LOCK_DEBUG
+        /**
+         * @param file 
+         * @param line 
+         * @param mode 
+         */
+        void lock(const char* file, int line, Mode mode = Mode::Exclusive);
+    #endif
 
         void unlock();
 
+        /**
+         * @return true 
+         * @return false 
+         */
         bool force_unlock_if_locked();
 
         /**
          * @return true 
          * @return false 
          */
-        bool is_locked() const
-        {
-            return m_holder;
+        bool is_locked() const 
+        { 
+            return m_holder; 
         }
 
         void clear_waiters();
@@ -61,15 +75,16 @@ namespace Kernel
         /**
          * @return const char* 
          */
-        const char* name() const
-        {
-            return m_name;
+        const char* name() const 
+        { 
+            return m_name; 
         }
 
     private:
         Atomic<bool> m_lock { false };
-        const char* m_name { nullptr };
 
+        const char* m_name { nullptr };
+        
         WaitQueue m_queue;
 
         Mode m_mode { Mode::Unlocked };
@@ -77,25 +92,26 @@ namespace Kernel
         u32 m_times_locked { 0 };
 
         RefPtr<Thread> m_holder;
-    }; // class Lock
+    }; // class Lock 
 
-    class Locker
+    class Locker 
     {
     public:
-        #ifdef LOCK_DEBUG
-            /**
-             * @param file 
-             * @param line 
-             * @param l 
-             * @param mode 
-             * @return ALWAYS_INLINE 
-             */
-            ALWAYS_INLINE explicit Locker(const char* file, int line, Lock& l, Lock::Mode mode = Lock::Mode::Exclusive)
-                : m_lock(l)
-            {
-                m_lock.lock(mode);
-            }
-        #endif 
+
+    #ifdef LOCK_DEBUG
+        /**
+         * @param file 
+         * @param line 
+         * @param l 
+         * @param mode 
+         * @return ALWAYS_INLINE 
+         */
+        ALWAYS_INLINE explicit Locker(const char* file, int line, Lock& l, Lock::Mode mode = Lock::Mode::Exclusive)
+            : m_lock(l)
+        {
+            m_lock.lock(file, line, mode);
+        }
+    #endif
 
         /**
          * @param l 
@@ -112,29 +128,82 @@ namespace Kernel
          * @return ALWAYS_INLINE 
          */
         ALWAYS_INLINE ~Locker() 
-        {
-            unlock();
+        { 
+            unlock(); 
         }
 
         /**
          * @return ALWAYS_INLINE 
          */
-        ALWAYS_INLINE void unlock()
-        {
-            m_lock.unlock();
+        ALWAYS_INLINE void unlock() 
+        { 
+            m_lock.unlock(); 
         }
 
         /**
          * @param mode 
          * @return ALWAYS_INLINE 
          */
-        ALWAYS_INLINE void lock(Lock::Mode mode = Lock::Mode::Exclusive)
-        {
-            m_lock.lock(mode);
+        ALWAYS_INLINE void lock(Lock::Mode mode = Lock::Mode::Exclusive) 
+        { 
+            m_lock.lock(mode); 
         }
 
     private:
+
         Lock& m_lock;
     }; // class Locker
+
+    #ifdef LOCK_DEBUG
+    #    define LOCKER(...) Locker locker(__FILE__, __LINE__, __VA_ARGS__)
+    #else
+    #    define LOCKER(...) Locker locker(__VA_ARGS__)
+    #endif
+
+    template<typename T>
+    class Lockable 
+    {
+    public:
+        Lockable() { }
+
+        /**
+         * @param resource 
+         */
+        Lockable(T&& resource)
+            : m_resource(move(resource))
+        {
+        }
+
+        /**
+         * @return Lock& 
+         */
+        Lock& lock() 
+        { 
+            return m_lock; 
+        }
+
+        /**
+         * @return T& 
+         */
+        T& resource() 
+        { 
+            return m_resource; 
+        }
+
+        /**
+         * @return T 
+         */
+        T lock_and_copy()
+        {
+            LOCKER(m_lock);
+            return m_resource;
+        }
+
+    private:
+
+        T m_resource;
+
+        Lock m_lock;
+    }; // class Lockable 
 
 } // namespace Kernel
