dcbc97784105e99cca3d23b6a43431aebbe641e0
krishpranav <krisna.pranav@gmail.com>
2023-08-13 16:29:30 +0530

    kernel: device[diskpartition]

--- kernel/device/diskpartition.h
+++ kernel/device/diskpartition.h
@@ -16,16 +16,30 @@
 #include <mods/refptr.h>
 #include <kernel/device/blockdevice.h>
 
-namespace Kernel
+namespace Kernel 
 {
 
-    class DiskPartition final : public BlockDevice
+    class DiskPartition final : public BlockDevice 
     {
     public:
+
+        /**
+         * @param block_offset 
+         * @param block_limit 
+         * @return NonnullRefPtr<DiskPartition> 
+         */
+        static NonnullRefPtr<DiskPartition> create(BlockDevice&, unsigned block_offset, unsigned block_limit);
+
+        /// @brief Destroy the DiskPartition object
+        virtual ~DiskPartition();
+
+        /// @brief: start_request
+        virtual void start_request(AsyncBlockDeviceRequest&) override;
+
         /**
          * @return KResultOr<size_t> 
          */
-        virtual KResultOr<size_t> read(FileDescription&, size_t, UserOrKernelBuffer&) const override;
+        virtual KResultOr<size_t> read(FileDescription&, size_t, UserOrKernelBuffer&, size_t) override;
 
         /**
          * @return true 
@@ -36,7 +50,7 @@ namespace Kernel
         /**
          * @return KResultOr<size_t> 
          */
-        virtual KResultOr<size_t> write(FileDescription&, size_t, const UserOrKernelBuffer&) override;
+        virtual KResultOr<size_t> write(FileDescription&, size_t, const UserOrKernelBuffer&, size_t) override;
 
         /**
          * @return true 
@@ -45,10 +59,23 @@ namespace Kernel
         virtual bool can_write(const FileDescription&, size_t) const override;
 
     private:
+
+        /**
+         * @return const char* 
+         */
+        virtual const char* class_name() const override;
+
+        /**
+         * @param block_offset 
+         * @param block_limit 
+         */
+        DiskPartition(BlockDevice&, unsigned block_offset, unsigned block_limit);
+
         NonnullRefPtr<BlockDevice> m_device;
 
         unsigned m_block_offset;
         unsigned m_block_limit;
+
     }; // class DiskPartition
 
 } // namespace Kernel
