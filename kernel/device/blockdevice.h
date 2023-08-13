0479a845ba08dc0f8684a5a954e2c1326036428d
krishpranav <krisna.pranav@gmail.com>
2023-08-13 16:17:17 +0530

    kernel: device[blockdevice]

--- kernel/device/blockdevice.h
+++ kernel/device/blockdevice.h
@@ -13,65 +13,81 @@
 
 #include <kernel/device/device.h>
 
-namespace Kernel
+namespace Kernel 
 {
 
     class BlockDevice;
 
-    class AsyncBlockDeviceRequest : public AsyncDeviceRequest
-    {   
+    class AsyncBlockDeviceRequest : public AsyncDeviceRequest 
+    {
     public:
-        enum RequestType
+        enum RequestType 
         {
             Read,
-            Write,
-        }; 
+            Write
+        }; // enum
 
-        /** 
+        /**
+         * @param block_device 
+         * @param request_type 
+         * @param block_index 
+         * @param block_count 
+         * @param buffer 
+         * @param buffer_size 
+         */
+        AsyncBlockDeviceRequest(Device& block_device, RequestType request_type,
+            u32 block_index, u32 block_count, const UserOrKernelBuffer& buffer, size_t buffer_size);
+
+        /**
          * @return RequestType 
          */
-        RequestType request_type() const
-        {
-            return m_request_type;
+        RequestType request_type() const 
+        { 
+            return m_request_type; 
         }
 
         /**
          * @return u32 
          */
-        u32 block_index() const
-        {
-            return m_block_index;
+        u32 block_index() const 
+        { 
+            return m_block_index; 
         }
 
         /**
          * @return u32 
          */
-        u32 block_count() const
-        {
-            return m_block_count;
+        u32 block_count() const 
+        { 
+            return m_block_count; 
         }
 
-        /// @brief: buffer
-        UserOrKernelBuffer& buffer 
-        {
-            return m_buffer;
+        /**
+         * @return UserOrKernelBuffer& 
+         */
+        UserOrKernelBuffer& buffer() 
+        { 
+            return m_buffer; 
         }
 
         /**
          * @return const UserOrKernelBuffer& 
          */
-        const UserOrKernelBuffer& buffer() const
-        {
-            return m_buffer;
+        const UserOrKernelBuffer& buffer() const 
+        { 
+            return m_buffer; 
         }
 
         /**
          * @return size_t 
          */
-        size_t buffer_size() const
-        {
-            return m_buffer_size;
+        size_t buffer_size() const 
+        { 
+            return m_buffer_size; 
         }
+
+        /// @brief: start
+        virtual void start() override;
 
         /**
          * @return const char* 
@@ -80,9 +96,9 @@ namespace Kernel
         {
             switch (m_request_type) {
             case Read:
-                return "blockDeviceRequest (read)";
+                return "BlockDeviceRequest (read)";
             case Write:
-                return "BlockDeviceRequest (write)";
+                return "BlockDeviceRequest (read)";
             default:
                 ASSERT_NOT_REACHED();
             }
@@ -100,5 +116,71 @@ namespace Kernel
 
         const size_t m_buffer_size;
     }; // class AsyncBlockDeviceRequest
+
+    class BlockDevice : public Device 
+    {
+    public:
+        /// @brief Destroy the BlockDevice object
+        virtual ~BlockDevice() override;
+
+        /**
+         * @return size_t 
+         */
+        size_t block_size() const 
+        { 
+            return m_block_size; 
+        }
+
+        /**
+         * @return true 
+         * @return false 
+         */
+        virtual bool is_seekable() const override 
+        { 
+            return true; 
+        }
+
+        /**
+         * @param index 
+         * @return true 
+         * @return false 
+         */
+        bool read_block(unsigned index, UserOrKernelBuffer&);
+
+        /**
+         * @param index 
+         * @return true 
+         * @return false 
+         */
+        bool write_block(unsigned index, const UserOrKernelBuffer&);
+
+        /// @breif: start_request
+        virtual void start_request(AsyncBlockDeviceRequest&) = 0;
+
+    protected:
+
+        /**
+         * @param major 
+         * @param minor 
+         * @param block_size 
+         */
+        BlockDevice(unsigned major, unsigned minor, size_t block_size = PAGE_SIZE)
+            : Device(major, minor)
+            , m_block_size(block_size)
+        { }
+
+    private:
+        /**
+         * @return true 
+         * @return false 
+         */
+        virtual bool is_block_device() const final 
+        { 
+            return true; 
+        }
+
+        size_t m_block_size { 0 };
+
+    }; // class BlockDevice
 
 } // namespace Kernel
