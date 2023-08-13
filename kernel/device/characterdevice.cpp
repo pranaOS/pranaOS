bd671af1d974c0c2d26297592dde4edf8fec0f26
krishpranav <krisna.pranav@gmail.com>
2023-08-13 13:26:02 +0530

    kernel: device[characterdevice]

---
+++ kernel/device/characterdevice.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file characterdevice.cpp
+ * @author Krisna Pranav
+ * @brief character device
+ * @version 1.0
+ * @date 2023-08-13
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include <kernel/device/characterdevice.h>
+
+namespace Kernel
+{
+    
+    /// @brief Destroy the CharacterDevice::CharacterDevice object
+    CharacterDevice::~CharacterDevice()
+    {}
+
+} // namespace Kernel
