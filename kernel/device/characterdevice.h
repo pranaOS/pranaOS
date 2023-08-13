d6011c45eb485438d181dd0790e140e8f9e9a505
krishpranav <krisna.pranav@gmail.com>
2023-08-13 13:20:35 +0530

    kernel: device[characterdevice]

---
+++ kernel/device/characterdevice.h
@@ -0,0 +1,37 @@
+/**
+ * @file characterdevice.h
+ * @author Krisna Pranav
+ * @brief character device
+ * @version 1.0
+ * @date 2023-08-13
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+#include <kernel/device/device.h>
+
+namespace Kernel
+{
+
+    class CharacterDevice : public Device
+    {
+    public: 
+        /// @brief Destroy the CharacterDevice object
+        virtual ~CharacterDevice() override;
+
+    private:
+        /**
+         * @return true 
+         * @return false 
+         */
+        virtual bool is_character_device() const final 
+        {
+            return true;
+        }
+
+    }; // class CharacterDevice
+
+} // namespace Kernel
