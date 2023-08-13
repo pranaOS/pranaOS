3a6b05bd779530c3d04c542c8747f9997a32f6d4
krishpranav <krisna.pranav@gmail.com>
2023-08-13 13:01:39 +0530

    mods: doublelinkedlist

--- mods/doublelinkedlist.h
+++ mods/doublelinkedlist.h
@@ -17,18 +17,34 @@
 
 namespace Mods 
 {
+
+    /**
+     * @tparam ListType 
+     * @tparam ElementType 
+     */
     template<typename ListType, typename ElementType>
-    class DoubleLinkedListIterator
+    class DoubleLinkedListIterator 
     {
     public:
-        bool operator!=(const DoubleLinkedListIterator& other) const
-        {
-            return m_node != other.m_node;
-        } 
 
-        bool operator==(const DoubleLinkedListIterator& other) const
-        {
-            return m_node == other.m_node;
+        /**
+         * @param other 
+         * @return true 
+         * @return false 
+         */
+        bool operator!=(const DoubleLinkedListIterator& other) const 
+        { 
+            return m_node != other.m_node; 
+        }
+
+        /**
+         * @param other 
+         * @return true 
+         * @return false 
+         */
+        bool operator==(const DoubleLinkedListIterator& other) const 
+        { 
+            return m_node == other.m_node; 
         }
 
         /**
@@ -39,60 +55,77 @@ namespace Mods
             m_node = m_node->next;
             return *this;
         }
-
+        
         /**
          * @return ElementType& 
          */
-        ElementType& operator*()
-        {
-            return m_node->value;
+        ElementType& operator*() 
+        { 
+            return m_node->value; 
         }
 
         /**
          * @return ElementType* 
          */
-        ElementType* operator->()
+        ElementType* operator->() 
         {
-            return &m_node->value;
+            return &m_node->value; 
         }
 
-        bool is_end() const
-        {
-            return !m_node;
+        /**
+         * @return true 
+         * @return false 
+         */
+        bool is_end() const 
+        { 
+            return !m_node; 
         }
 
         /**
          * @return DoubleLinkedListIterator 
          */
-        static DoubleLinkedListIterator universal_end()
-        {
-            return DoubleLinkedListIterator(nullptr);
+        static DoubleLinkedListIterator universal_end() 
+        { 
+            return DoubleLinkedListIterator(nullptr); 
         }
 
     private:
         friend ListType;
 
+        /**
+         * @param node 
+         */
         explicit DoubleLinkedListIterator(typename ListType::Node* node)
             : m_node(node)
-        {}
+        { }
 
         typename ListType::Node* m_node;
 
     }; // class DoubleLinkedListIterator
 
     template<typename T>
-    class DoubleLinkedList
+    class DoubleLinkedList 
     {
     private:
-        struct Node
+        struct Node 
         {
+            /**
+             * @param v 
+             */
             explicit Node(const T& v)
                 : value(v)
-            {}
+            {
+            }
 
+            /**
+             * @brief Construct a new Node object
+             * 
+             * @param v 
+             */
             explicit Node(T&& v)
                 : value(move(v))
-            {}
+            {
+            }
 
             T value;
             Node* next { nullptr };
@@ -100,13 +133,16 @@ namespace Mods
         }; // struct Node
 
     public:
-        DoubleLinkedList() {}
-
+        DoubleLinkedList() { }
         ~DoubleLinkedList() { clear(); }
 
-        bool is_empty() const
-        {
-            return !head();
+        /**
+         * @return true 
+         * @return false 
+         */
+        bool is_empty() const 
+        { 
+            return !head(); 
         }
 
         void clear()
@@ -140,11 +176,156 @@ namespace Mods
         }
 
         /**
+         * @return T& 
+         */
+        T& last()
+        {
+            ASSERT(head());
+            return tail()->value;
+        }
+
+        /**
+         * @return const T& 
+         */
+        const T& last() const
+        {
+            ASSERT(head());
+            return tail()->value;
+        }
+
+        /**
          * @param value 
          */
         void append(T&& value)
         {
             append_node(new Node(move(value)));
+        }
+
+        /**
+         * @param value 
+         */
+        void append(const T& value)
+        {
+            append_node(new Node(value));
+        }
+
+        /**
+         * @param value 
+         */
+        void prepend(T&& value)
+        {
+            prepend_node(new Node(move(value)));
+        }
+
+        /**
+         * @param value 
+         */
+        void prepend(const T& value)
+        {
+            prepend_node(new Node(value));
+        }
+
+        /**
+         * @param value 
+         * @return true 
+         * @return false 
+         */
+        bool contains_slow(const T& value) const
+        {
+            return find_node(value) != nullptr;
+        }
+
+        using Iterator = DoubleLinkedListIterator<DoubleLinkedList, T>;
+        friend Iterator;
+
+        /**
+         * @return Iterator 
+         */
+        Iterator begin() 
+        { 
+            return Iterator(m_head); 
+        }
+
+        /**
+         * @return Iterator 
+         */
+        Iterator end() 
+        { 
+            return Iterator::universal_end(); 
+        }
+
+        using ConstIterator = DoubleLinkedListIterator<const DoubleLinkedList, const T>;
+        friend ConstIterator;
+
+        /**
+         * @return ConstIterator 
+         */
+        ConstIterator begin() const 
+        { 
+            return ConstIterator(m_head); 
+        }
+
+        /**
+         * @return ConstIterator 
+         */
+        ConstIterator end() const 
+        { 
+            return ConstIterator::universal_end(); 
+        }
+
+        /**
+         * @param value 
+         * @return ConstIterator 
+         */
+        ConstIterator find(const T& value) const
+        {
+            Node* node = find_node(value);
+
+            if (node)
+                return ConstIterator(node);
+
+            return end();
+        }
+
+        /**
+         * @param value 
+         * @return Iterator 
+         */
+        Iterator find(const T& value)
+        {
+            Node* node = find_node(value);
+
+            if (node)
+                return Iterator(node);
+
+            return end();
+        }
+
+        /**
+         * @param it 
+         */
+        void remove(Iterator it)
+        {
+            ASSERT(it.m_node);
+
+            auto* node = it.m_node;
+
+            if (node->prev) {
+                ASSERT(node != m_head);
+                node->prev->next = node->next;
+            } else {
+                ASSERT(node == m_head);
+                m_head = node->next;
+            }
+            if (node->next) {
+                ASSERT(node != m_tail);
+                node->next->prev = node->prev;
+            } else {
+                ASSERT(node == m_tail);
+                m_tail = node->prev;
+            }
+
+            delete node;
         }
 
     private:
@@ -163,6 +344,7 @@ namespace Mods
 
             ASSERT(m_tail);
             ASSERT(!node->next);
+
             m_tail->next = node;
             node->prev = m_tail;
 
@@ -186,6 +368,7 @@ namespace Mods
 
             m_head->prev = node;
             node->next = m_head;
+            
             m_head = node;
         }
 
@@ -195,50 +378,51 @@ namespace Mods
          */
         Node* find_node(const T& value) const
         {
-            for(auto* node = m_head; node; node = node->next) {
-                if (Traits<T>::equals(node->value, value)) 
+            for (auto* node = m_head; node; node = node->next) {
+                if (Traits<T>::equals(node->value, value))
                     return node;
             }
-
+            
             return nullptr;
         }
 
         /**
          * @return Node* 
          */
-        Node* head()
-        {
-            return m_head;
+        Node* head() 
+        { 
+            return m_head; 
         }
 
         /**
          * @return const Node* 
          */
-        const Node* head() const
-        {
-            return m_head;
+        const Node* head() const 
+        { 
+            return m_head; 
         }
 
         /**
          * @return Node* 
          */
         Node* tail() 
-        {
-            return m_tail;
+        { 
+            return m_tail; 
         }
 
         /**
          * @return const Node* 
          */
-        const Node* tail() const
-        {
-            return m_tail;
+        const Node* tail() const 
+        { 
+            return m_tail; 
         }
-    
+
+        /// @brief: m_head + m_tail
         Node* m_head { nullptr };
         Node* m_tail { nullptr };
-        
-        
     }; // class DoubleLinkedList
 
-} // namespace Mods 
+}
+
+using Mods::DoubleLinkedList;
