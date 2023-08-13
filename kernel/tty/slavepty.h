50990d88737eb8871262fbe390880522de58c88e
krishpranav <krisna.pranav@gmail.com>
2023-08-13 13:46:03 +0530

    kernel: tty[slavepty]

--- kernel/tty/slavepty.h
+++ kernel/tty/slavepty.h
@@ -14,11 +14,43 @@
 #include <kernel/filesystem/inodeidentifier.h>
 #include <kernel/tty/tty.h>
 
-namespace Kernel
+namespace Kernel 
 {
-    class SlavePTY final : public TTY
+
+    class MasterPTY;
+
+    class SlavePTY final : public TTY 
     {
+    public:
+
+        /// @brief Destroy the SlavePTY object
+        virtual ~SlavePTY() override;
+
+        void on_master_write(const UserOrKernelBuffer&, ssize_t);
+
+        /**
+         * @return unsigned 
+         */
+        unsigned index() const 
+        { 
+            return m_index; 
+        }
+
+        /**
+         * @return time_t 
+         */
+        time_t time_of_last_write() const 
+        { 
+            return m_time_of_last_write; 
+        }
+
+        /**
+         * @return FileBlockCondition& 
+         */
+        virtual FileBlockCondition& block_condition() override;
+
     private:
+        
         /**
          * @return String 
          */
@@ -29,19 +61,51 @@ namespace Kernel
          */
         virtual ssize_t on_tty_write(const UserOrKernelBuffer&, ssize_t) override;
 
+        /// @brief: echo
         virtual void echo(u8) override;
 
+        /**
+         * @return true 
+         * @return false 
+         */
         virtual bool can_read(const FileDescription&, size_t) const override;
 
-        virtual KResultOr<size_t> read(FileDescription)
+        /**
+         * @return KResultOr<size_t> 
+         */
+        virtual KResultOr<size_t> read(FileDescription&, size_t, UserOrKernelBuffer&, size_t) override;
+
+        /**
+         * @return true 
+         * @return false 
+         */
+        virtual bool can_write(const FileDescription&, size_t) const override;
+
+        /**
+         * @return const char* 
+         */
+        virtual const char* class_name() const override 
+        { 
+            return "SlavePTY"; 
+        }
+
+        /**
+         * @return KResult 
+         */
+        virtual KResult close() override;
+
+        friend class MasterPTY;
+
+        SlavePTY(MasterPTY&, unsigned index);
 
         RefPtr<MasterPTY> m_master;
 
         time_t m_time_of_last_write { 0 };
 
         unsigned m_index { 0 };
-
+        
         char m_tty_name[32];
-    }; // class SlavePTY
+
+    }; // class SlavePTY 
 
 } // namespace Kernel
