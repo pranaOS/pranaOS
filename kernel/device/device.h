8ab1520a92aa34e12da8ba1ca6258bb352e258dc
krishpranav <krisna.pranav@gmail.com>
2023-08-13 13:02:46 +0530

    kernel: device[device]

---
+++ kernel/device/device.h
@@ -0,0 +1,25 @@
+/**
+ * @file device.h
+ * @author Krisna Pranav
+ * @brief device
+ * @version 1.0
+ * @date 2023-08-13
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+#include <mods/function.h>
+#include <mods/doublelinkedlist.h>
+#include <mods/hashmap.h>
+#include <kernel/lock.h>
+#include <kernel/unixtypes.h>
+#include <kernel/device/asyncdevicerequest.h>
+#include <kernel/filesystem/file.h>
+
+namespace Kernel
+{
+
+} // namespace Kernel
