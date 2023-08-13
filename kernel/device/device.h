b9424f6188bac937e5979a15f8576438a4dd057c
krishpranav <krisna.pranav@gmail.com>
2023-08-13 13:16:37 +0530

    kernel: device[device]

--- kernel/device/device.h
+++ kernel/device/device.h
@@ -19,72 +19,68 @@
 #include <kernel/device/asyncdevicerequest.h>
 #include <kernel/filesystem/file.h>
 
-namespace Kernel
+namespace Kernel 
 {
     class Device : public File 
     {
-
     public:
+        /// @brief Destroy the Device object
         virtual ~Device() override;
 
         /**
          * @return unsigned 
          */
-        unsigned major() const
-        {
-            return m_major;
+        unsigned major() const 
+        { 
+            return m_major; 
         }
 
         /**
          * @return unsigned 
          */
-        unsigned minor() const
-        {
-            return m_minor;
+        unsigned minor() const 
+        { 
+            return m_minor; 
         }
 
         /**
          * @return String 
          */
         virtual String absolute_path(const FileDescription&) const override;
-
-        /**
-         * @return String 
-         */
         virtual String absolute_path() const;
 
         /**
          * @return uid_t 
          */
-        uid_t uid() const
-        {
-            return m_uid;
+        uid_t uid() const 
+        { 
+            return m_uid; 
         }
 
         /**
          * @return uid_t 
          */
-        uid_t gid() const
-        {
-            return m_gid;
+        uid_t gid() const 
+        { 
+            return m_gid; 
         }
 
         /**
          * @return true 
          * @return false 
          */
-        virtual bool is_device() const override
-        {
-            return true;
+        virtual bool is_device() const override 
+        { 
+            return true; 
         }
 
         /**
          * @return true 
          * @return false 
          */
-        virtual bool is_disk_Device() const
-        {
-            return false;
+        virtual bool is_disk_device() const 
+        { 
+            return false; 
         }
 
         /// @brief: for_each
@@ -97,7 +93,33 @@ namespace Kernel
          */
         static Device* get_device(unsigned major, unsigned minor);
 
+        /// @breif: process next queued request(async)
         void process_next_queued_request(Badge<AsyncDeviceRequest>, const AsyncDeviceRequest&);
+
+        /**
+         * @tparam AsyncRequestType 
+         * @tparam Args 
+         * @param args 
+         * @return NonnullRefPtr<AsyncRequestType> 
+         */
+        template<typename AsyncRequestType, typename... Args>
+        NonnullRefPtr<AsyncRequestType> make_request(Args&&... args)
+        {
+            auto request = adopt(*new AsyncRequestType(*this, forward<Args>(args)...));
+
+            bool was_empty;
+            
+            {
+                ScopedSpinLock lock(m_requests_lock);
+                was_empty = m_requests.is_empty();
+                m_requests.append(request);
+            }
+
+            if (was_empty)
+                request->do_start({});
+                
+            return request;
+        }
 
     protected:
         /**
@@ -109,22 +131,26 @@ namespace Kernel
         /**
          * @param uid 
          */
-        void set_uid(uid_t uid)
-        {
-            m_uid = uid;
+        void set_uid(uid_t uid) 
+        { 
+            m_uid = uid; 
         }
 
         /**
          * @param gid 
          */
-        void set_gid(gid_t gid)
-        {
-            m_gid = gid;
+        void set_gid(gid_t gid) 
+        { 
+            m_gid = gid; 
         }
 
+        /**
+         * @return HashMap<u32, Device*>& 
+         */
         static HashMap<u32, Device*>& all_devices();
-    
+
     private:
+
         unsigned m_major { 0 };
         unsigned m_minor { 0 };
 
@@ -133,7 +159,7 @@ namespace Kernel
 
         SpinLock<u8> m_requests_lock;
 
-        DoubleLinkedList<RefPtr<AsyncDeviceRequest>> m_request;
+        DoublyLinkedList<RefPtr<AsyncDeviceRequest>> m_requests;
 
     }; // class Device
 
