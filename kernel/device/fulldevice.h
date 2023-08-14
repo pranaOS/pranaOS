e1198ddc27d704f9e338585cdbfe980885b275a8
krishpranav <krisna.pranav@gmail.com>
2023-08-14 18:30:55 +0530

    kernel: device[fulldevice]

--- kernel/device/fulldevice.h
+++ kernel/device/fulldevice.h
@@ -13,35 +13,47 @@
 
 #include <kernel/device/characterdevice.h>
 
-namespace Kernel
+namespace Kernel 
 {
-    class FullDevice final : public CharacterDevice
+
+    class FullDevice final : public CharacterDevice 
     {
+        MOD_MAKE_ETERNAL
+
     public:
+
+        /// @brief Construct a new FullDevice object
+        FullDevice();
+
         /// @brief Destroy the FullDevice object
         virtual ~FullDevice() override;
 
-    private:    
+    private:
+        
         /**
          * @return KResultOr<size_t> 
          */
-        virtual KResultOr<size_t> read(FileDescription&, size_t, UserOrKernelBuffer&) const override;
-        virtual KResultOr<size_t> write(FileDescription&, size_t, const UserOrKernelBuffer&) const override;
+        virtual KResultOr<size_t> read(FileDescription&, size_t, UserOrKernelBuffer&, size_t) override;
+        virtual KResultOr<size_t> write(FileDescription&, size_t, const UserOrKernelBuffer&, size_t) override;
 
         /**
          * @return true 
          * @return false 
          */
         virtual bool can_read(const FileDescription&, size_t) const override;
-        virtual bool can_write(const FileDescription&, size_t) const override;
+        virtual bool can_write(const FileDescription&, size_t) const override 
+        { 
+            return true; 
+        }
 
         /**
          * @return const char* 
          */
-        virtual const char* class_name() const override
-        {
-            return "FullDevice";
+        virtual const char* class_name() const override 
+        { 
+            return "FullDevice"; 
         }
+
     }; // class FullDevice
 
 } // namespace Kernel
