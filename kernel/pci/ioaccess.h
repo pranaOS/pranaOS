52dc175bd3d04ea7af6dc7b59533bf8a6871463b
krishpranav <krisna.pranav@gmail.com>
2023-08-14 22:04:25 +0530

    kernel: pci[ioaccess]

--- kernel/pci/ioaccess.h
+++ kernel/pci/ioaccess.h
@@ -13,39 +13,89 @@
 
 #include <kernel/pci/access.h>
 
-namespace Kernel
+namespace Kernel 
 {
-
-    namespace PCI
+    namespace PCI 
     {
-        class IOAccess final : public PCI::Access
+        class IOAccess final : public PCI::Access 
         {
+        public:
+
+            /// @breif: initialize
+            static void initialize();
+
         protected:
+            /// @brief Construct a new IOAccess object
             IOAccess();
 
         private:
-
-            /// enumerate_hardware
+            /// @brief: enumerate hardware
             virtual void enumerate_hardware(Function<void(Address, ID)>) override;
 
             /**
              * @return const char* 
              */
-            virtual const char* access_type() const override
-            {
-                return "IO-Access";
-            }
+            virtual const char* access_type() const override 
+            { 
+                return "IO-Access"; 
+            };
 
             /**
              * @return uint32_t 
              */
-            virtual uint32_t segment_count() const override
-            {
-                return 1;
+            virtual uint32_t segment_count() const override 
+            { 
+                return 1; 
+            };
+
+            /**
+             * @param address 
+             */
+            virtual void write8_field(Address address, u32, u8) override final;
+
+            /**
+             * @param address 
+             */
+            virtual void write16_field(Address address, u32, u16) override final;
+
+            /**
+             * @param address 
+             */
+            virtual void write32_field(Address address, u32, u32) override final;
+
+            /**
+             * @param address 
+             * @return u8 
+             */
+            virtual u8 read8_field(Address address, u32) override;
+
+            /**
+             * @param address 
+             * @return u16 
+             */
+            virtual u16 read16_field(Address address, u32) override;
+
+            /**
+             * @param address 
+             * @return u32 
+             */
+            virtual u32 read32_field(Address address, u32) override;
+
+            /**
+             * @return uint8_t 
+             */
+            virtual uint8_t segment_start_bus(u32) const override 
+            { 
+                return 0x0; 
             }
-        }; // class IOAccess 
 
-    } // namespace PCI
-
-    
+            /**
+             * @return uint8_t 
+             */
+            virtual uint8_t segment_end_bus(u32) const override 
+            { 
+                return 0xFF; 
+            }
+        }; // class IOAccess
+    } // namespace PCI 
 } // namespace Kernel
