aac21fc76da7c8ecae070fd84f9a3b4091c1fdc0
krishpranav <krisna.pranav@gmail.com>
2023-08-14 20:17:01 +0530

    kernel: acpi[initialize]

---
+++ kernel/acpi/initialize.h
@@ -0,0 +1,23 @@
+/**
+ * @file initialize.h
+ * @author Krisna Pranav
+ * @brief initialize
+ * @version 1.
+ * @date 2023-08-14
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+namespace Kernel 
+{
+    
+    namespace ACPI 
+    {
+        bool is_enabled();
+        void initialize();
+    } // namespace ACPI
+
+} // namespace Kernel
