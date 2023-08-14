522bc50828fbb55b64d56614f609055bb980e8bf
krishpranav <krisna.pranav@gmail.com>
2023-08-14 18:56:09 +0530

    kernel: device[zerodevice]

--- kernel/device/zerodevice.cpp
+++ kernel/device/zerodevice.cpp
@@ -9,24 +9,53 @@
  * 
  */
 
-#include "zerodevice.h"
-#include "characterdevice.h"
 #include <kernel/device/zerodevice.h>
 #include <mods/memory.h>
 #include <mods/stdlibextra.h>
 
-namespace Kernel
+namespace Kernel 
 {
+    
+    /// @brief Construct a new ZeroDevice::ZeroDevice object
     ZeroDevice::ZeroDevice()
         : CharacterDevice(1, 5)
-    {}
-
+    { }
+    
+    /// @brief Destroy the ZeroDevice::ZeroDevice object
     ZeroDevice::~ZeroDevice()
-    {}
+    { }
 
+    /**
+     * @return true 
+     * @return false 
+     */
     bool ZeroDevice::can_read(const FileDescription&, size_t) const
     {
         return true;
     }
-    
-} // namespace kernel
+
+    /**
+     * @param buffer 
+     * @param size 
+     * @return KResultOr<size_t> 
+     */
+    KResultOr<size_t> ZeroDevice::read(FileDescription&, size_t, UserOrKernelBuffer& buffer, size_t size)
+    {
+        ssize_t count = min(static_cast<size_t>(PAGE_SIZE), size);
+
+        if (!buffer.memset(0, count))
+            return KResult(-EFAULT);
+
+        return count;
+    }
+
+    /**
+     * @param size 
+     * @return KResultOr<size_t> 
+     */
+    KResultOr<size_t> ZeroDevice::write(FileDescription&, size_t, const UserOrKernelBuffer&, size_t size)
+    {
+        return min(static_cast<size_t>(PAGE_SIZE), size);
+    }
+
+} // namespace Kernel
