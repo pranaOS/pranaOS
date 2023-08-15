d7f01f3489b1ed8e4852156939aafe77348175c2
krishpranav <krisna.pranav@gmail.com>
2023-08-15 18:31:30 +0530

    kernel: net[network_task]

---
+++ kernel/net/network_task.h
@@ -0,0 +1,23 @@
+/**
+ * @file network_task.h
+ * @author Krisna Pranav
+ * @brief network task
+ * @version 1.0
+ * @date 2023-08-15
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+namespace Kernel 
+{
+    
+    class NetworkTask 
+    {
+    public:
+        static void spawn();
+    }; // class NetworkTask
+
+} // namespace Kernel
