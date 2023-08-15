030b430241226745f03488213ad221fb05eed08d
krishpranav <krisna.pranav@gmail.com>
2023-08-15 18:37:59 +0530

    kernel: net[socket]

--- kernel/net/socket.h
+++ kernel/net/socket.h
@@ -31,6 +31,56 @@ namespace Kernel
 
     class FileDescription;
 
+    class Socket : public File
+    {
+    public:
+        enum class SetupState
+        {
+            Unstarted,
+            InProgress,
+            Completed,
+        }; 
+        
+        enum class Role : u8
+        {
+            None,
+            Listener,
+            Accepted,
+            Connected,
+            Connecting
+        };
+
+        /**
+         * @param setup_state 
+         * @return const char* 
+         */
+        static const char* to_string(SetupState setup_state)
+        {
+            switch (setup_state) {
+            case SetupState::Unstarted:
+                return "Unsarted";
+            default:
+                return "None";
+            }
+        }  
     
+    private:
+        /**
+         * @return true 
+         * @return false 
+         */
+        virtual bool is_socket() const final
+        {
+            return true;
+        }
+
+        Lock m_lock { "Socket" };
+
+        int m_domain { 0 };
+        int m_type { 0 };
+        int m_protocol { 0 };
+
+        size_t m_backlog { 0 };
+    }; // class Socket  
 
 } // namespace Kernel
