f12fafa9489e1bbee2d4c4dde9817a6817ec5c63
krishpranav <krisna.pranav@gmail.com>
2023-08-15 08:13:01 +0530

    kernel: pci[initializer]

---
+++ kernel/pci/initializer.h
@@ -0,0 +1,24 @@
+/**
+ * @file initializer.h
+ * @author Krisna Pranav
+ * @brief intializer
+ * @version 1.0
+ * @date 2023-08-15
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+namespace Kernel 
+{
+    
+    namespace PCI 
+    {
+
+        void initialize();
+
+    } // namespace PCI
+
+} // namespace Kernel
