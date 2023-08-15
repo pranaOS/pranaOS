d35d8b629a51b34a50cbf6752ffae121742be775
krishpranav <krisna.pranav@gmail.com>
2023-08-15 08:23:44 +0530

    kernel: net[icmp]

--- kernel/net/icmp.h
+++ kernel/net/icmp.h
@@ -61,8 +61,52 @@ public:
         m_code = b;
     }
 
+    /**
+     * @return u16 
+     */
+    u16 checksum() const
+    {
+        return m_checksum;
+    }
+    
+    /**
+     * @param w 
+     */
+    void set_checksum(u16 w)
+    {
+        m_checksum = w;
+    }
+
+    /**
+     * @return const void* 
+     */
+    const void* payload() const
+    {
+        return this + 1;
+    }
+    
+    void payload()
+    {
+        return this + 1;
+    }
+
 private:
     u8 m_type { 0 };
     u8 m_code { 0 };
     NetworkOrdered<u16> m_checksum { 0 };
 }; // class
+
+static_assert(sizeof(ICMPHeader) == 1);
+
+struct ICMPEchoPacket
+{
+    void payload()
+    {
+        return this + 1;
+    }
+
+    const void* payload() const
+    {
+        return this + 1;
+    }
+};
