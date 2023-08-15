0fa08fc221418b559eccc8e83726e9615f5ccddc
krishpranav <krisna.pranav@gmail.com>
2023-08-15 08:26:24 +0530

    kernel: net[icmp]

--- kernel/net/icmp.h
+++ kernel/net/icmp.h
@@ -18,95 +18,111 @@ struct ICMPType
 {
     enum 
     {
-        EchoReploy = 0,
+        EchoReply = 0,
         EchoRequest = 8,
-    };
-}; // struct 
+    }; // enum 
+}; // struct
 
-class ICMPHeader
+class [[gnu::packed]] ICMPHeader
 {
 public:
-    ICMPHeader() {}
-    ~ICMPHeader() {}
+    /// @brief Construct a new ICMPHeader object
+    ICMPHeader() { }
+
+    /// @brief Destroy the ICMPHeader object
+    ~ICMPHeader() { }
 
     /**
      * @return u8 
      */
-    u8 type() const
-    {
-        return m_type;
+    u8 type() const 
+    { 
+        return m_type; 
     }
 
     /**
      * @param b 
      */
-    void set_type(u8 b)
-    {
-        m_type = b;
-    }
-
-    /**
-     * @return u8 
-     */
-    u8 code() const
-    {
-        return m_code;
-    }
-
-    /**
-     * @param b 
-     */
-    void set_code(u8 b)
-    {
-        m_code = b;
-    }
-
-    /**
-     * @return u16 
-     */
-    u16 checksum() const
-    {
-        return m_checksum;
+    void set_type(u8 b) 
+    { 
+        m_type = b; 
     }
     
     /**
+     * @return u8 
+     */
+    u8 code() const 
+    { 
+        return m_code; 
+    }
+
+    /**
+     * @param b 
+     */
+    void set_code(u8 b) 
+    { 
+        m_code = b;
+    }
+    
+    /**
+     * @return u16 
+     */
+    u16 checksum() const 
+    { 
+        return m_checksum; 
+    }
+
+    /**
      * @param w 
      */
-    void set_checksum(u16 w)
-    {
-        m_checksum = w;
+    void set_checksum(u16 w) 
+    { 
+        m_checksum = w; 
     }
 
     /**
      * @return const void* 
      */
-    const void* payload() const
-    {
-        return this + 1;
+    const void* payload() const 
+    { 
+        return this + 1; 
     }
-    
-    void payload()
-    {
-        return this + 1;
+
+    /**
+     * @return void* 
+     */
+    void* payload() 
+    { 
+        return this + 1; 
     }
 
 private:
     u8 m_type { 0 };
     u8 m_code { 0 };
     NetworkOrdered<u16> m_checksum { 0 };
-}; // class
+}; // class ICMPHeader
 
-static_assert(sizeof(ICMPHeader) == 1);
+static_assert(sizeof(ICMPHeader) == 4);
 
-struct ICMPEchoPacket
+struct [[gnu::packed]] ICMPEchoPacket
 {
-    void payload()
-    {
-        return this + 1;
+    ICMPHeader header;
+    NetworkOrdered<u16> identifier;
+    NetworkOrdered<u16> sequence_number;
+
+    /**
+     * @return void* 
+     */
+    void* payload() 
+    { 
+        return this + 1; 
     }
 
-    const void* payload() const
-    {
-        return this + 1;
+    /**
+     * @return const void* 
+     */
+    const void* payload() const 
+    { 
+        return this + 1; 
     }
 };
