b86e78ba231f9253eab1f6ccd196cc56dbabecd1
krishpranav <krisna.pranav@gmail.com>
2023-08-16 20:21:47 +0530

    kernel: net[tcp]

---
+++ kernel/net/tcp.h
@@ -0,0 +1,19 @@
+/**
+ * @file tcp.h
+ * @author Krisna Pranav
+ * @brief tcp
+ * @version 1.0
+ * @date 2023-08-15
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+#include <kernel/net/ipv4.h>
+
+namespace Kernel
+{
+
+} // namespace Kernel
