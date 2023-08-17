55b7a8ce25e6a67b8776c24c087f6e4a26741780
krishpranav <krisna.pranav@gmail.com>
2023-08-17 18:49:34 +0530

    kernel: net[ethernetframeheader]

--- kernel/net/ethernetframeheader.h
+++ kernel/net/ethernetframeheader.h
@@ -19,23 +19,71 @@
 class [[gnu::packed]] EthernetFrameHeader
 {
 public:
-    EthernetFrameHeader() {}
-    ~EthernetFrameHeader() {}
+    EthernetFrameHeader() { }
+    ~EthernetFrameHeader() { }
 
     /**
      * @return MACAddress 
      */
-    MACAddress destination() const
-    {
-        return m_destination;
-    }   
+    MACAddress destination() const 
+    { 
+        return m_destination; 
+    }
 
     /**
      * @param address 
      */
-    void set_destination(const MACAddress& address)
-    {
-        m_destination = address;
+    void set_destination(const MACAddress& address) 
+    { 
+        m_destination = address; 
+    }
+
+    /**
+     * @return MACAddress 
+     */
+    MACAddress source() const 
+    { 
+        return m_source; 
+    }
+
+    /**
+     * @param address 
+     */
+    void set_source(const MACAddress& address) 
+    { 
+        m_source = address; 
+    }
+
+    /**
+     * @return u16 
+     */
+    u16 ether_type() const 
+    { 
+        return m_ether_type; 
+    }
+
+    /**
+     * @param ether_type 
+     */
+    void set_ether_type(u16 ether_type) 
+    { 
+        m_ether_type = ether_type; 
+    }
+    
+    /**
+     * @return const void* 
+     */
+    const void* payload() const 
+    { 
+        return &m_payload[0]; 
+    }
+
+    /**
+     * @return void* 
+     */
+    void* payload() 
+    { 
+        return &m_payload[0]; 
     }
 
 private:
@@ -43,4 +91,6 @@ private:
     MACAddress m_source;
     NetworkOrdered<u16> m_ether_type;
     u32 m_payload[0];
-}; // class EthernetFrameHeader
+};
+
+static_assert(sizeof(EthernetFrameHeader) == 14);
