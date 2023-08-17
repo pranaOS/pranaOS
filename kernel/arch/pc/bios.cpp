455d192c8718a596dab9e02550829a1f3154f95a
krishpranav <krisna.pranav@gmail.com>
2023-08-17 21:23:40 +0530

    kernel: arch[pc(bios)]

---
+++ kernel/arch/pc/bios.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file bios.cpp
+ * @author Krisna Pranav aka (krishpranav)
+ * @brief bios
+ * @version 1.0
+ * @date 2023-08-17
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include <mods/string_view.h>
+#include <kernel/vm/memorymanager.h>
+#include <kernel/vm/typedmapping.h>
+#include <kernel/arch/pc/bios.h>
+
+namespace Kernel
+{
+
+} // namespace Kernel
