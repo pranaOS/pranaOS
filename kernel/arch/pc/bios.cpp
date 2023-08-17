b86e517cd5b3b12a97113b0203f09e33d3848824
krishpranav <krisna.pranav@gmail.com>
2023-08-17 21:26:15 +0530

    kernel: arch[pc(bios)]

--- kernel/arch/pc/bios.cpp
+++ kernel/arch/pc/bios.cpp
@@ -14,18 +14,46 @@
 #include <kernel/vm/typedmapping.h>
 #include <kernel/arch/pc/bios.h>
 
-namespace Kernel
+namespace Kernel 
 {
+
     /**
+     * @brief map_bios
+     * 
      * @return MappedROM 
      */
     MappedROM map_bios()
     {
         MappedROM mapping;
-        mapping.size = 128 * kib;
+
+        mapping.size = 128 * KiB;
         mapping.paddr = PhysicalAddress(0xe0000);
-        mapping.region = MM.allocate_kernel_region(mapping.paddr, PAGE_ROUND_UP);
+        mapping.region = MM.allocate_kernel_region(mapping.paddr, PAGE_ROUND_UP(mapping.size), {}, Region::Access::Read);
+        
         return mapping;
-    } // map_bios
+    }
+
+    /**
+     * @brief map_ebda
+     * 
+     * @return MappedROM 
+     */
+    MappedROM map_ebda()
+    {
+        auto ebda_segment_ptr = map_typed<u16>(PhysicalAddress(0x40e));
+        auto ebda_length_ptr = map_typed<u16>(PhysicalAddress(0x413));
+
+        PhysicalAddress ebda_paddr(*ebda_segment_ptr << 4);
+        size_t ebda_size = *ebda_length_ptr;
+
+        MappedROM mapping;
+
+        mapping.region = MM.allocate_kernel_region(ebda_paddr.page_base(), PAGE_ROUND_UP(ebda_size), {}, Region::Access::Read);
+        mapping.offset = ebda_paddr.offset_in_page();
+        mapping.size = ebda_size;
+        mapping.paddr = ebda_paddr;
+
+        return mapping;
+    }
 
 } // namespace Kernel
