73c3ddf5f66f7b5963c4fb210c23b4f26d2464fc
krishpranav <krisna.pranav@gmail.com>
2023-08-17 19:12:37 +0530

    kernel: acpi[dynamicparser]

---
+++ kernel/acpi/dynamicparser.cpp
@@ -0,0 +1,47 @@
+/**
+ * @file dynamicparser.cpp
+ * @author Krisna Pranav
+ * @brief dynamic parser
+ * @version 1.0
+ * @date 2023-08-17
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include "dynamicparser.h"
+#include <kernel/acpi/parser.h>
+#include <kernel/acpi/dynamicparser.h>
+
+namespace Kernel
+{
+    namespace ACPI
+    {
+
+        DynamicParser::DynamicParser(PhysicalAddress rsdp)
+            : IRQHandler(9)
+            , Parser(rsdp)
+        {}
+
+        void DynamicParser::handle_irq(const RegisterState&)
+        {
+            ASSERT_NOT_REACHED();
+        }
+
+        void DynamicParser::enable_aml_interpretation()
+        {
+            ASSERT_NOT_REACHED();
+        }
+
+        void DynamicParser::enable_aml_interpretation(File&)
+        {
+            ASSERT_NOT_REACHED();
+        }
+
+        void DynamicParser::enable_aml_interpretation(u8*, u32)
+        {
+            ASSERT_NOT_REACHED();
+        }
+
+    } // namespace ACPI
+} // namespace Kernel
