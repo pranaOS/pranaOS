dafac67f1a92f1dedcf63e7a45f80fa6b8bfd486
krishpranav <krisna.pranav@gmail.com>
2023-08-20 09:33:38 +0530

    kernel: interrupt[mishandler] g

--- kernel/interrupt/msihandler.h
+++ kernel/interrupt/msihandler.h
@@ -16,34 +16,66 @@
 #include <kernel/interrupt/genericinterrupthandler.h>
 #include <kernel/pci/definitions.h>
 
-namespace Kernel
+namespace Kernel 
 {
-    class MSIHandler final : public GenericInterruptHandler
+
+    class MSIHandler final : public GenericInterruptHandler 
     {
     public:
         /// @brief Destroy the MSIHandler object
         virtual ~MSIHandler();
 
         /// @brief: handle_interrupt
-        virtual void handle_interrupt(RegisterState&) override {}
+        virtual void handle_interrupt(RegisterState&) override { }
 
-        /// @breif: eoi
-        void bool eoi() override;
+        /// @breif: enable(and)disable irq
+        void enable_irq();
+        void disable_irq();
+
+        /// @brief: eoi
+        /// @b: bool
+        virtual bool eoi() override;
 
         /**
          * @return size_t 
          */
-        virtual size_t sharing_device_count() const override
-        {
-            return 0;
+        virtual size_t sharing_devices_count() const override 
+        { 
+            return 0; 
+        }
+        
+        /**
+         * @return true 
+         * @return false 
+         */
+        virtual bool is_shared_handler() const override 
+        { 
+            return false; 
+        }
+
+        /**
+         * @return true 
+         * @return false 
+         */
+        virtual bool is_sharing_with_others() const override 
+        { 
+            return m_shared_with_others; 
         }
 
     protected:
+
+        /**
+         * @param irq 
+         */
         void change_irq_number(u8 irq);
+
+        //// @brief Construct a new MSIHandler object
         explicit MSIHandler(PCI::Address);
 
     private:
         bool m_shared_with_others;
         bool m_enabled;
+
     }; // class MSIHandler
+
 } // namespace Kernel
