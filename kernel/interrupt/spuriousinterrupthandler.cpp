287e83b78978e874d7aed7274885435c0c8b43a1
krishpranav <krisna.pranav@gmail.com>
2023-08-20 12:51:48 +0530

    kernel: interrupt[spuriousinterrupthandler]

--- kernel/interrupt/spuriousinterrupthandler.cpp
+++ kernel/interrupt/spuriousinterrupthandler.cpp
@@ -9,11 +9,10 @@
  * 
  */
 
-#include "spuriousinterrupthandler.h"
 #include <kernel/interrupt/interruptmanagement.h>
 #include <kernel/interrupt/spuriousinterrupthandler.h>
 
-namespace Kernel
+namespace Kernel 
 {
 
     /**
@@ -24,6 +23,16 @@ namespace Kernel
         new SpuriousInterruptHandler(interrupt_number);
     }
 
+    /// @brief: register handler
+    void SpuriousInterruptHandler::register_handler(GenericInterruptHandler&)
+    {
+    }
+
+    /// @brief: unregister handle
+    void SpuriousInterruptHandler::unregister_handler(GenericInterruptHandler&)
+    {
+    }
+
     /**
      * @return true 
      * @return false 
@@ -31,37 +40,50 @@ namespace Kernel
     bool SpuriousInterruptHandler::eoi()
     {
         m_responsible_irq_controller->eoi(*this);
-
         return false;
     }
-
+    
     /**
+     * @brief Construct a new SpuriousInterruptHandler::SpuriousInterruptHandler object
+     * 
      * @param irq 
      */
     SpuriousInterruptHandler::SpuriousInterruptHandler(u8 irq)
         : GenericInterruptHandler(irq)
         , m_responsible_irq_controller(InterruptManagement::the().get_responsible_irq_controller(irq))
-    {}
+    {
+    }
+
+    /**
+     * @brief Destroy the SpuriousInterruptHandler::SpuriousInterruptHandler object
+     * 
+     */
+    SpuriousInterruptHandler::~SpuriousInterruptHandler()
+    { }
+
+    /// @breif: handle interrupt 
+    void SpuriousInterruptHandler::handle_interrupt(const RegisterState&)
+    {
+        klog() << "Spurious Interrupt, vector " << interrupt_number();
+    }
 
     /// @brief: enable interrupt vector
     void SpuriousInterruptHandler::enable_interrupt_vector()
     {
         if (m_enabled)
             return;
-        
-        m_enabled = true;
 
+        m_enabled = true;
         m_responsible_irq_controller->enable(*this);
     }
 
-    /// @breif: disable interrupt vector
+    /// @brief: disable interrupt vector
     void SpuriousInterruptHandler::disable_interrupt_vector()
     {
         if (!m_enabled)
             return;
-        
+            
         m_enabled = false;
-
         m_responsible_irq_controller->disable(*this);
     }
 
@@ -72,8 +94,8 @@ namespace Kernel
     {
         if (m_responsible_irq_controller->type() == IRQControllerType::i82093AA)
             return "";
-        
+
         return m_responsible_irq_controller->model();
     }
 
-} // namepsace Kernel
+} // namespace Kernel
