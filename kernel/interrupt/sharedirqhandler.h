965f42995e96279f2e60851bc19c99e49e69945b
krishpranav <krisna.pranav@gmail.com>
2023-08-20 09:56:40 +0530

    kernel: interrupt[sharedirqhandler]

--- kernel/interrupt/sharedirqhandler.h
+++ kernel/interrupt/sharedirqhandler.h
@@ -15,12 +15,12 @@
 #include <kernel/arch/i386/cpu.h>
 #include <kernel/interrupt/genericinterrupthandler.h>
 
-namespace Kernel
+namespace Kernel 
 {
     class IRQHandler;
 
-    class SharedIRQHandler final : public GenericInterruptHandler
-    {   
+    class SharedIRQHandler final : public GenericInterruptHandler 
+    {
     public:
 
         /**
@@ -36,40 +36,82 @@ namespace Kernel
          */
         virtual void handle_interrupt(const RegisterState& regs) override;
 
+        /// @brief register(&)unregister handler
+        void register_handler(GenericInterruptHandler&);
+        void unregister_handler(GenericInterruptHandler&);
+
+        /**
+         * @return true 
+         * @return false 
+         */
         virtual bool eoi() override;
 
         /**
          * @return size_t 
          */
-        virtual size_t sharing_device_count() const override 
-        {
-            return m_handlers;
+        virtual size_t sharing_devices_count() const override 
+        { 
+            return m_handlers.size(); 
         }
 
         /**
          * @return true 
          * @return false 
          */
-        virtual bool is_shared_handler() const override
-        {
-            return true;
+        virtual bool is_shared_handler() const override 
+        { 
+            return true; 
+        }
+
+        /**
+         * @return true 
+         * @return false 
+         */
+        virtual bool is_sharing_with_others() const override 
+        { 
+            return false; 
         }
 
         /**
          * @return HandlerType 
          */
-        virtual HandlerType type() const override
-        {
-            return HandlerType::SharedIRQHandler;
+        virtual HandlerType type() const override 
+        { 
+            return HandlerType::SharedIRQHandler; 
+        }
+
+        /**
+         * @return const char* 
+         */
+        virtual const char* purpose() const override 
+        { 
+            return "Shared IRQ Handler"; 
+        }
+
+        /**
+         * @return const char* 
+         */
+        virtual const char* controller() const override 
+        { 
+            return m_responsible_irq_controller->model(); 
         }
 
     private:
+
         void enable_interrupt_vector();
         void disable_interrupt_vector();
+
+        /**
+         * @param interrupt_number 
+         */
         explicit SharedIRQHandler(u8 interrupt_number);
+
         bool m_enabled;
+
         HashTable<GenericInterruptHandler*> m_handlers;
+
         RefPtr<IRQController> m_responsible_irq_controller;
+        
     }; // class SharedIRQHandler
 
 } // namespace Kernel
