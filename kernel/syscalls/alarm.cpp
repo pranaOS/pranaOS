2a7da7fa358682115cd123649a06fefd05ee6344
krishpranav <krisna.pranav@gmail.com>
2023-08-22 18:46:43 +0530

    kernel: syscalls[alarm]

--- kernel/syscalls/alarm.cpp
+++ kernel/syscalls/alarm.cpp
@@ -12,30 +12,39 @@
 #include <kernel/process.h>
 #include <kernel/time/timemanagement.h>
 
-namespace Kernel
+namespace Kernel 
 {
 
+    /**
+     * @brief sys alaram at process
+     * 
+     * @param seconds
+     */
     unsigned Process::sys$alarm(unsigned seconds)
     {
         REQUIRE_PROMISE(stdio);
-
-        unsigned previous_alaram_remaining = 0;
-
+        unsigned previous_alarm_remaining = 0;
         if (auto alarm_timer = move(m_alarm_timer)) {
             if (TimerQueue::the().cancel_timer(*alarm_timer)) {
                 timespec remaining;
-                previous_alaram_remaining = remaining.tv_sec;
+                timespec_add(alarm_timer->remaining(), { 0, 1000000000 - 1 }, remaining);
+                previous_alarm_remaining = remaining.tv_sec;
             }
 
-            if (previous_alaram_remaining == 0)
-                previous_alaram_remaining = 1;
+            if (previous_alarm_remaining == 0)
+                previous_alarm_remaining = 1;
         }
 
         if (seconds > 0) {
-            auto deadline = TimeManagement::the().current_time(CLOCK_REALTIME);
-        }
+            auto deadline = TimeManagement::the().current_time(CLOCK_REALTIME).value();
 
-        return previous_alaram_remaining;
+            timespec_add(deadline, { seconds, 0 }, deadline);
+
+            m_alarm_timer = TimerQueue::the().add_timer_without_id(CLOCK_REALTIME, deadline, [this]() {
+                (void)send_signal(SIGALRM, nullptr);
+            });
+        }
+        return previous_alarm_remaining;
     }
 
 } // namespace Kernel
