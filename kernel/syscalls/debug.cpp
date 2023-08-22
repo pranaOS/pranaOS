355013e89a958d97f4d891609518ad14f3a76259
krishpranav <krisna.pranav@gmail.com>
2023-08-22 20:12:43 +0530

    kernel: syscalls[debug]

--- kernel/syscalls/debug.cpp
+++ kernel/syscalls/debug.cpp
@@ -8,3 +8,24 @@
  * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
  * 
  */
+
+#include <kernel/io.h>
+#include <kernel/ksyms.h>
+#include <kernel/process.h>
+#include <kernel/userorkernelbuffer.h>
+
+namespace Kernel
+{
+    int Process::sys$dump_backtrace()
+    {
+        dump_backtrace();
+        return 0;
+    }
+    
+    int Process::sys$dbgputch(u8 ch)
+    {
+        IO::out8(0xe9, ch);
+
+        return 0;
+    }
+} // namespace Kernel 
