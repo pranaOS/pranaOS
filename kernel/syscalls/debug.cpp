6271d333ad1afa2c850b82d8c4e12911cb99b29a
krishpranav <krisna.pranav@gmail.com>
2023-08-22 20:13:36 +0530

    kernel: syscalls[debug]

--- kernel/syscalls/debug.cpp
+++ kernel/syscalls/debug.cpp
@@ -14,18 +14,55 @@
 #include <kernel/process.h>
 #include <kernel/userorkernelbuffer.h>
 
-namespace Kernel
+namespace Kernel 
 {
+
+    /**
+     * @brief dump bactrace sys
+     * 
+     */
     int Process::sys$dump_backtrace()
     {
         dump_backtrace();
         return 0;
     }
-    
+
+    /**
+     * @brief dbgputch sys
+     * 
+     */
     int Process::sys$dbgputch(u8 ch)
     {
         IO::out8(0xe9, ch);
+        return 0;
+    }
+
+    /**
+     * @brief dbgputstr sys 
+     * 
+     */
+    int Process::sys$dbgputstr(Userspace<const u8*> characters, int length)
+    {
+        if (length <= 0)
+            return 0;
+
+        SmapDisabler disabler;
+
+        auto buffer = UserOrKernelBuffer::for_user_buffer(characters, length);
+
+        if (!buffer.has_value())
+            return -EFAULT;
+            
+        ssize_t nread = buffer.value().read_buffered<1024>(length, [&](const u8* buffer, size_t buffer_size) {
+            for (size_t i = 0; i < buffer_size; ++i)
+                IO::out8(0xe9, buffer[i]);
+            return (ssize_t)buffer_size;
+        });
+
+        if (nread < 0)
+            return (int)nread;
 
         return 0;
     }
-} // namespace Kernel 
+
+} // namespace Kernel
