2e7c05ec50b7ca325e1f94d035c293a51690fc53
krishpranav <krisna.pranav@gmail.com>
2023-08-22 19:18:18 +0530

    kernel: syscalls[chdir]

--- kernel/syscalls/chdir.cpp
+++ kernel/syscalls/chdir.cpp
@@ -14,10 +14,10 @@
 #include <kernel/filesystem/virtualfilesystem.h>
 #include <kernel/process.h>
 
-namespace Kernel
+namespace Kernel 
 {
     /**
-     * @brief chdir sys
+     * @brief sys chdir 
      * 
      */
     int Process::sys$chdir(Userspace<const char*> user_path, size_t path_length)
@@ -26,19 +26,45 @@ namespace Kernel
 
         auto path = get_syscall_path_argument(user_path, path_length);
 
-        if (path.is_error()) 
+        if (path.is_error())
             return path.error();
-        
+
         auto directory_or_error = VFS::the().open_directory(path.value(), current_directory());
 
         if (directory_or_error.is_error())
             return directory_or_error.error();
-        
+
+        m_cwd = *directory_or_error.value();
+
         return 0;
     }
 
     /**
-     * @brief getcwd sys
+     * @brief sys fchdir
+     * 
+     */
+    int Process::sys$fchdir(int fd)
+    {
+        REQUIRE_PROMISE(stdio);
+
+        auto description = file_description(fd);
+
+        if (!description)
+            return -EBADF;
+
+        if (!description->is_directory())
+            return -ENOTDIR;
+
+        if (!description->metadata().may_execute(*this))
+            return -EACCES;
+
+        m_cwd = description->custody();
+
+        return 0;
+    }
+
+    /**
+     * @brief: getcwd
      * 
      */
     int Process::sys$getcwd(Userspace<char*> buffer, ssize_t size)
@@ -47,15 +73,16 @@ namespace Kernel
 
         if (size < 0)
             return -EINVAL;
-        
+
         auto path = current_directory().absolute_path();
 
         if ((size_t)size < path.length() + 1)
             return -ERANGE;
-        
-        if(!copy_to_user(buffer, path.characters(), path.length() + 1))
+
+        if (!copy_to_user(buffer, path.characters(), path.length() + 1))
             return -EFAULT;
 
         return 0;
     }
+
 } // namespace Kernel
