c45a31f03e28e38942cb2a9a62129857f76c5ccf
krishpranav <krisna.pranav@gmail.com>
2023-08-22 19:32:38 +0530

    kernel: syscalls[chown]

--- kernel/syscalls/chown.cpp
+++ kernel/syscalls/chown.cpp
@@ -12,8 +12,8 @@
 #include <kernel/process.h>
 #include <kernel/filesystem/filedescription.h>
 
-namespace Kernel
-{  
+namespace Kernel 
+{
     /**
      * @brief fchown sys
      * 
@@ -24,10 +24,31 @@ namespace Kernel
 
         auto description = file_description(fd);
 
-        if (!description)   
+        if (!description)
             return -EBADF;
-        
+
         return description->chown(uid, gid);
+    }
+
+    /**
+     * @brief chown sys
+     * 
+     */
+    int Process::sys$chown(Userspace<const Syscall::SC_chown_params*> user_params)
+    {
+        REQUIRE_PROMISE(chown);
+
+        Syscall::SC_chown_params params;
+
+        if (!copy_from_user(&params, user_params))
+            return -EFAULT;
+
+        auto path = get_syscall_path_argument(params.path);
+
+        if (path.is_error())
+            return path.error();
+
+        return VFS::the().chown(path.value(), params.uid, params.gid, current_directory());
     }
 
 } // namespace Kernel
