827b41fe04df8fb423c5347dcecb0e4d6cc25c4a
krishpranav <krisna.pranav@gmail.com>
2023-08-22 19:56:01 +0530

    kernel: syscalls[debug]

---
+++ kernel/syscalls/debug.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file debug.cpp
+ * @author Krisna Pranav aka (krishpranav)
+ * @brief debug
+ * @version 6.0
+ * @date 2023-08-22
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
