cfd4a3f4793b8767ad947570c7d3aa0b2a129bc2
krishpranav <krisna.pranav@gmail.com>
2023-08-22 19:15:55 +0530

    kernel: syscalls[chdir]

---
+++ kernel/syscalls/chdir.cpp
@@ -0,0 +1,45 @@
+/**
+ * @file chdir.cpp
+ * @author Krisna Pranav
+ * @brief chdir
+ * @version 6.0
+ * @date 2023-08-22
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include <kernel/filesystem/custody.h>
+#include <kernel/filesystem/filedescription.h>
+#include <kernel/filesystem/virtualfilesystem.h>
+#include <kernel/process.h>
+
+namespace Kernel
+{
+    /**
+     * @brief chdir sys
+     * 
+     */
+    int Process::sys$chdir(Userspace<const char*> user_path, size_t path_length)
+    {
+        REQUIRE_PROMISE(rpath);
+
+        auto path = get_syscall_path_argument(user_path, path_length);
+
+        if (path.is_error()) 
+            return path.error();
+        
+        auto directory_or_error = VFS::the().open_directory(path.value(), current_directory());
+
+        if (directory_or_error.is_error())
+            return directory_or_error.error();
+        
+        return 0;
+    }
+
+    int Process::sys$getcwd(Userspace<char*> buffer, ssize_t size)
+    {
+        REQUIRE_PROMISE(rpath);
+        
+    }
+} // namespace Kernel
