17b35d5570d2951b772e1262bbace32c16de0649
krishpranav <krisna.pranav@gmail.com>
2023-08-22 19:39:16 +0530

    kernel: syscalls[chroot]

--- kernel/syscalls/chroot.cpp
+++ kernel/syscalls/chroot.cpp
@@ -14,22 +14,39 @@
 #include <kernel/filesystem/custody.h>
 #include <kernel/filesystem/virtualfilesystem.h>
 
-namespace Kernel
+namespace Kernel 
 {
 
+    /**
+     * @brief chroot sys
+     * 
+     */
     int Process::sys$chroot(Userspace<const char*> user_path, size_t path_length, int mount_flags)
     {
         if (!is_superuser())
             return -EPERM;
-        
+
         REQUIRE_PROMISE(chroot);
 
         auto path = get_syscall_path_argument(user_path, path_length);
 
         if (path.is_error())
             return path.error();
+
+        auto directory_or_error = VFS::the().open_directory(path.value(), current_directory());
+        
+        if (directory_or_error.is_error())
+            return directory_or_error.error();
+
+        auto directory = directory_or_error.value();
+
+        m_root_directory_relative_to_global_root = directory;
+
+        int chroot_mount_flags = mount_flags == -1 ? directory->mount_flags() : mount_flags;
+
+        set_root_directory(Custody::create(nullptr, "", directory->inode(), chroot_mount_flags));
         
         return 0;
     }
-    
+
 } // namespace Kernel
