621b80e2a59a82e6faa653a6f283716d249c1e0c
krishpranav <krisna.pranav@gmail.com>
2023-08-26 18:38:49 +0530

    kernel: syscalls[exit]

---
+++ kernel/syscalls/exit.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file exit.cpp
+ * @author Krisna Pranav
+ * @brief exit
+ * @version 6.0
+ * @date 2023-08-26
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include <kernel/ksyms.h>
+#include <kernel/process.h>
+
+namespace Kernel
+{
+   void Process::sys$exit(int status)
+   {
+        cli();
+
+        m_termination_status = status;
+        m_termination_signal = 0;
+
+        die();
+   } 
+}
