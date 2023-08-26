1862caa5f40d6fbaa17623282d6a3b1cf5dcf029
krishpranav <krisna.pranav@gmail.com>
2023-08-26 22:37:26 +0530

    kernel: profiling

--- kernel/profiling.h
+++ kernel/profiling.h
@@ -15,27 +15,42 @@
 #include <mods/string.h>
 #include <mods/types.h>
 
-namespace Kernel
+namespace Kernel 
 {
+
     class Process;
 
-    namespace Profiling
+    namespace Profiling 
     {
+
         constexpr size_t max_stack_frame_count = 50;
 
-        struct Sample
+        struct Sample 
         {
             ProcessID pid;
             ThreadID tid;
             u64 timestamp;
-            u32 frames[max_stacks_frame_count];
-        }; 
+            u32 frames[max_stack_frame_count];
+        }; // struct 
 
         extern ProcessID pid();
         extern String& executable_path();
 
+        /// @breif: next slot
         Sample& next_sample_slot();
+        
+        /// @brief: start & stop
+        void start(Process&);
+        void stop();
 
+        /**
+         * @brief did exec?
+         * 
+         * @param new_executable_path 
+         */
+        void did_exec(const String& new_executable_path);
+
+        /// @broef: each sample for
         void for_each_sample(Function<void(Sample&)>);
 
     } // namespace Profiling
