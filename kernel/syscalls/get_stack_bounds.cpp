edda47746301a13daed1cfed9bcfc1e120b735fb
krishpranav <krisna.pranav@gmail.com>
2023-08-26 22:32:59 +0530

    kernel: syscalls[get_stack_bounds]

--- kernel/syscalls/get_stack_bounds.cpp
+++ kernel/syscalls/get_stack_bounds.cpp
@@ -12,18 +12,14 @@
 #include <kernel/process.h>
 #include <kernel/vm/memorymanager.h>
 
-namespace Kernel
+namespace Kernel 
 {
 
-    /**
-     * @brief get stack bounds sys
-     * 
-     */
     int Process::sys$get_stack_bounds(FlatPtr* user_stack_base, size_t* user_stack_size)
     {
         FlatPtr stack_pointer = Thread::current()->get_register_dump_from_stack().userspace_esp;
-
-        auto* stack_region = MM.find_region_from_vaddr(*this);
+        
+        auto* stack_region = MM.find_region_from_vaddr(*this, VirtualAddress(stack_pointer));
 
         if (!stack_region) {
             ASSERT_NOT_REACHED();
@@ -31,13 +27,15 @@ namespace Kernel
         }
 
         FlatPtr stack_base = stack_region->range().base().get();
-
         size_t stack_size = stack_region->size();
 
         if (!copy_to_user(user_stack_base, &stack_base))
             return -EFAULT;
-        
+
+        if (!copy_to_user(user_stack_size, &stack_size))
+            return -EFAULT;
+
         return 0;
-    }
+    } // int
 
 } // namespace Kernel
