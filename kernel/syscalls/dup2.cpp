e691a388899e7089ff37b1f85201d714b4c8c876
krishpranav <krisna.pranav@gmail.com>
2023-08-26 18:36:44 +0530

    kernel: syscalls[dup2]

--- kernel/syscalls/dup2.cpp
+++ kernel/syscalls/dup2.cpp
@@ -12,22 +12,31 @@
 #include <kernel/process.h>
 #include <kernel/filesystem/filedescription.h>
 
-namespace Kernel
+namespace Kernel 
 {
+
+    /**
+     * @brief dup2 sys
+     * 
+     */
     int Process::sys$dup2(int old_fd, int new_fd)
     {
         REQUIRE_PROMISE(stdio);
-
+        
         auto description = file_description(old_fd);
 
         if (!description)
             return -EBADF;
+
         if (old_fd == new_fd)
             return 0;
-        
+
+        if (new_fd < 0 || new_fd >= m_max_open_file_descriptors)
+            return -EINVAL;
+
         m_fds[new_fd].set(*description);
 
         return new_fd;
     }
-    
-} // namespace Kernel 
+
+} // namespace Kernel
