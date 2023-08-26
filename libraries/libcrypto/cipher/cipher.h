b6e488a89c5db94ddc1ea6b8bbe396bb5e9e82a0
krishpranav <krisna.pranav@gmail.com>
2023-08-26 22:47:18 +0530

    libraries: libcrypto[cipher]

--- libraries/libcrypto/cipher/cipher.h
+++ libraries/libcrypto/cipher/cipher.h
@@ -15,54 +15,217 @@
 #include <mods/optional.h>
 #include <mods/types.h>
 
-namespace Crypto
+namespace Crypto 
 {
-    namespace Cipher
+    namespace Cipher 
     {
-        enum class Intent
+
+        enum class Intent 
         {
             Encryption,
-            Descryption,
-        };
+            Decryption,
+        }; // enum
 
-        enum class PaddingMode
+        enum class PaddingMode 
         {
-            CMS,
+            CMS,    
             RFC5246,
             Null,
             Bit,
-            Random
-        };
+            Random,
+            Space,
+            ZeroLength,
+        }; // enum
 
+        /**
+         * @tparam B 
+         * @tparam T 
+         */
         template<typename B, typename T>
         class Cipher;
 
-        struct CipherBlock
+        struct CipherBlock 
         {
         public:
+            /**
+             * @param mode 
+             */
             explicit CipherBlock(PaddingMode mode)
                 : m_padding_mode(mode)
-            {}
-
-            static size_t block_size() 
             {
-                ASSERT_NOT_REACHED();
             }
 
-            virtual ByteBuffer get() const = 0;
+            /**
+             * @return size_t 
+             */
+            static size_t block_size() 
+            { 
+                ASSERT_NOT_REACHED(); 
+            }
 
+            /**
+             * @return ByteBuffer 
+             */
+            virtual ByteBuffer get() const = 0;
             virtual const ByteBuffer& data() const = 0;
 
             virtual void overwrite(ReadonlyBytes) = 0;
-            virtual void overwrite(const ByteBuffer& buffer)
+
+            /**
+             * @param buffer 
+             */
+            virtual void overwrite(const ByteBuffer& buffer) 
+            { 
+                overwrite(buffer.bytes()); 
+            }
+
+            /**
+             * @param data 
+             * @param size 
+             */
+            virtual void overwrite(const u8* data, size_t size) 
+            { 
+                overwrite({ data, size }); 
+            }
+
+            /**
+             * @param ivec 
+             */
+            virtual void apply_initialization_vector(const u8* ivec) = 0;
+
+            /**
+             * @return PaddingMode 
+             */
+            PaddingMode padding_mode() const 
+            { 
+                return m_padding_mode; 
+            }
+
+            /**
+             * @param mode 
+             */
+            void set_padding_mode(PaddingMode mode) 
+            { 
+                m_padding_mode = mode; 
+            }
+
+            /**
+             * @tparam T 
+             * @param offset 
+             * @param value 
+             */
+            template<typename T>
+            void put(size_t offset, T value)
             {
+                ASSERT(offset + sizeof(T) <= data().size());
+
+                auto* ptr = data().data() + offset;
+                auto index { 0 };
+
+                ASSERT(sizeof(T) <= 4);
+
+                if constexpr (sizeof(T) > 3)
+                    ptr[index++] = (u8)(value >> 24);
+
+                if constexpr (sizeof(T) > 2)
+                    ptr[index++] = (u8)(value >> 16);
+
+                if constexpr (sizeof(T) > 1)
+                    ptr[index++] = (u8)(value >> 8);
+
+                ptr[index] = (u8)value;
             }
 
         private:
             virtual ByteBuffer& data() = 0;
-
-            PaddingMode m_padding_mode ;
+            PaddingMode m_padding_mode;
         };
-    } // namespace Cipher 
 
+        struct CipherKey 
+        {
+            /**
+             * @return ByteBuffer 
+             */
+            virtual ByteBuffer data() const = 0;
+
+            /**
+             * @return true 
+             * @return false 
+             */
+            static bool is_valid_key_size(size_t) 
+            { 
+                return false; 
+            };
+
+            virtual ~CipherKey() { }
+
+        protected:
+            /**
+             * @param user_key 
+             * @param bits 
+             */
+            virtual void expand_encrypt_key(const ByteBuffer& user_key, size_t bits) = 0;
+            virtual void expand_decrypt_key(const ByteBuffer& user_key, size_t bits) = 0;
+
+            size_t bits { 0 };
+        }; // struct CipherKey
+
+        /**
+         * @tparam KeyT 
+         * @tparam BlockT 
+         */
+
+        template<typename KeyT = CipherKey, typename BlockT = CipherBlock>
+        class Cipher 
+        {
+        public:
+            using KeyType = KeyT;
+            using BlockType = BlockT;
+
+            /**
+             * @param mode 
+             */
+            explicit Cipher<KeyT, BlockT>(PaddingMode mode)
+                : m_padding_mode(mode)
+            {
+            }
+
+            /**
+             * @return const KeyType& 
+             */
+            virtual const KeyType& key() const = 0;
+            virtual KeyType& key() = 0;
+
+            /**
+             * @return size_t 
+             */
+            static size_t block_size() 
+            { 
+                return BlockType::block_size(); 
+            }
+
+            /**
+             * @return PaddingMode 
+             */
+            PaddingMode padding_mode() const 
+            { 
+                return m_padding_mode; 
+            }
+
+            /**
+             * @param in 
+             * @param out 
+             */
+            virtual void encrypt_block(const BlockType& in, BlockType& out) = 0;
+            virtual void decrypt_block(const BlockType& in, BlockType& out) = 0;
+
+            /**
+             * @return String 
+             */
+            virtual String class_name() const = 0;
+
+        private:
+            PaddingMode m_padding_mode;
+        }; // class Cipher
+    } // namespace Cipher
 } // namespace Crypto
