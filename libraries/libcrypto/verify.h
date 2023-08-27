04da1fc8f1c935672db1eac40565e77dec4c3b04
krishpranav <krisna.pranav@gmail.com>
2023-08-27 20:19:16 +0530

    libraries: libcrypto[verify]

---
+++ libraries/libcrypto/verify.h
@@ -0,0 +1,23 @@
+/**
+ * @file verify.h
+ * @author Krisna Pranav
+ * @brief verify
+ * @version 6.0
+ * @date 2023-08-27
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+namespace Crypto 
+{
+
+    enum class VerificationConsistency 
+    {
+        Consistent,
+        Inconsistent
+    }; // enum
+
+} // namespace Crypto 
