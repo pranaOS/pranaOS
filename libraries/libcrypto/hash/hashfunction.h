f5367e06e746d1fd348a51b3b6d66002b85f343b
krishpranav <krisna.pranav@gmail.com>
2023-08-29 18:44:56 +0530

    libraries: libcrypto[hash(hashfunction)]

--- libraries/libcrypto/hash/hashfunction.h
+++ libraries/libcrypto/hash/hashfunction.h
@@ -15,48 +15,63 @@
 #include <mods/string_view.h>
 #include <mods/types.h>
 
-namespace Crypto
+namespace Crypto 
 {
-    namespace Hash
+    namespace Hash 
     {
+
         template<size_t BlockS, typename DigestT>
-        class HashFunction
+        class HashFunction 
         {
         public:
             static constexpr auto BlockSize = BlockS / 8;
             static constexpr auto DigestSize = DigestT::Size;
 
             using DigestType = DigestT;
-            
+
             /**
              * @return size_t 
              */
-            static size_t block_size()
-            {
-                return BlockSize;
-            }
+            static size_t block_size() 
+            { 
+                return BlockSize; 
+            };
 
             static size_t digest_size() 
-            {
-                return DigestSize;
-            }
+            { 
+                return DigestSize; 
+            };
 
-            /// @breif: update
+            /// @brief: update
             virtual void update(const u8*, size_t) = 0;
 
             /**
              * @param buffer 
              */
-            virtual void update(const ByteBuffer& buffer)
-            {
-                update(buffer.data());
-            }
+            virtual void update(const ByteBuffer& buffer) 
+            { 
+                update(buffer.data(), buffer.size()); 
+            };
+
+            /**
+             * @param string 
+             */
+            virtual void update(const StringView& string) 
+            { 
+                update((const u8*)string.characters_without_null_termination(), string.length()); 
+            };
+
+            /// @brief: peek + digest
+            virtual DigestType peek() = 0;
+            virtual DigestType digest() = 0;
+
+            /// @brief: reset
+            virtual void reset() = 0;
 
             /**
              * @return String 
              */
             virtual String class_name() const = 0;
-
         }; // class HashFunction
     } // namespace Hash
 } // namespace Crypto
