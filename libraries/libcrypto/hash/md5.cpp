1761cd02b8f3750b652d9a597500f41cbba46e93
krishpranav <krisna.pranav@gmail.com>
2023-08-31 22:24:35 +0530

    libraries: libcrypto[hash(md5)]

--- libraries/libcrypto/hash/md5.cpp
+++ libraries/libcrypto/hash/md5.cpp
@@ -65,3 +65,81 @@ static constexpr u32 ROTATE_LEFT(u32 x, size_t n)
 {
     return (x << n) | (x >> (32 - n));
 }
+
+static constexpr void round_1(u32& a, u32 b, u32 c, u32 d, u32 x, u32 s, u32 ac)
+{
+    a += F(b, c, d) + x + ac;
+    a = ROTATE_LEFT(a, s);
+    a += b;
+}
+
+/**
+ * 
+ * @param a 
+ * @param b 
+ * @param c 
+ * @param d 
+ * @param x 
+ * @param s 
+ * @param ac 
+ */
+static constexpr void round_2(u32& a, u32 b, u32 c, u32 d, u32 x, u32 s, u32 ac)
+{
+    a += G(b, c, d) + x + ac;
+    a = ROTATE_LEFT(a, s);
+    a += b;
+}
+
+/**
+ * @param a 
+ * @param b 
+ * @param c 
+ * @param d 
+ * @param x 
+ * @param s 
+ * @param ac 
+ */
+static constexpr void round_3(u32& a, u32 b, u32 c, u32 d, u32 x, u32 s, u32 ac)
+{
+    a += H(b, c, d) + x + ac;
+    a = ROTATE_LEFT(a, s);
+    a += b;
+}
+
+/**
+ * @param a 
+ * @param b 
+ * @param c 
+ * @param d 
+ * @param x 
+ * @param s 
+ * @param ac 
+ */
+static constexpr void round_4(u32& a, u32 b, u32 c, u32 d, u32 x, u32 s, u32 ac)
+{
+    a += I(b, c, d) + x + ac;
+    a = ROTATE_LEFT(a, s);
+    a += b;
+}
+
+namespace Crypto
+{
+    namespace Hash
+    {
+        MD5::DigestType MD5::digest()
+        {
+            auto digest = peek();
+            reset();
+            return digest;
+        }
+
+        MD5::DigestType MD5::peek()
+        {
+            DigestType digest;
+
+            u8 bits[8];
+
+            return digest;
+        }
+    }
+}
