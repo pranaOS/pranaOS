2ff04169dda4826d454215bfbbc245e887b7a8b0
krishpranav <krisna.pranav@gmail.com>
2023-09-01 18:53:25 +0530

    kernel: module

---
+++ kernel/module.h
@@ -0,0 +1,29 @@
+/**
+ * @file module.h
+ * @author Krisna Pranav
+ * @brief module
+ * @version 6.0
+ * @date 2023-09-01
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+#include <kernel/kbuffer.h>
+#include <mods/string.h>
+#include <mods/vector.h>
+
+namespace Kernel
+{
+    typedef void* (*ModuleInitPtr)();
+    typedef void* (*ModuleFiniPtr)();
+
+    struct Module
+    {
+        String name;
+        Vector<KBuffer> sections;
+    }; // struct Module
+
+} // namespace Kernel
