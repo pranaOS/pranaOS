534ff7d90192028ac228f2d164b783359b56b02a
krishpranav <krisna.pranav@gmail.com>
2023-09-01 19:15:24 +0530

    kernel: console

--- kernel/console.h
+++ kernel/console.h
@@ -15,12 +15,18 @@
 #include <mods/vector.h>
 #include <kernel/device/characterdevice.h>
 
-class Console final : public Kernel::CharacterDevice
+class Console final : public Kernel::CharacterDevice 
 {
     MOD_MAKE_ETERNAL
-
 public:
+
+    /** 
+     * @return Console& 
+     */
     static Console& the();
+
+    static void initialize();
+    static bool is_initialized();
 
     Console();
     virtual ~Console() override;
@@ -30,12 +36,40 @@ public:
      * @return false 
      */
     virtual bool can_read(const Kernel::FileDescription&, size_t) const override;
+
+    virtual bool can_write(const Kernel::FileDescription&, size_t) const override 
+    { 
+        return true; 
+    }
+
+    /**
+     * @return Kernel::KResultOr<size_t> 
+     */
+    virtual Kernel::KResultOr<size_t> read(Kernel::FileDescription&, size_t, Kernel::UserOrKernelBuffer&, size_t) override;
+
+    /**
+     * @return Kernel::KResultOr<size_t> 
+     */
+    virtual Kernel::KResultOr<size_t> write(Kernel::FileDescription&, size_t, const Kernel::UserOrKernelBuffer&, size_t) override;
     
-    virtual bool can_write(const Kernel::FileDescription&, size_t) const override
-    {
-        return true;
+    /**
+     * @return const char* 
+     */
+    virtual const char* class_name() const override 
+    { 
+        return "Console"; 
+    }
+
+    void put_char(char);
+
+    /**
+     * @return const CircularQueue<char, 16384>& 
+     */
+    const CircularQueue<char, 16384>& logbuffer() const 
+    { 
+        return m_logbuffer; 
     }
 
 private:
     CircularQueue<char, 16384> m_logbuffer;
-}; // class Console.
+}; // class Console
