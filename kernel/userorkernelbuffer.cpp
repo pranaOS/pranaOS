f025972e6550e5e6b50a8ff3bfe6b1f4fce0e50f
krishpranav <krisna.pranav@gmail.com>
2023-09-06 12:29:38 +0530

    kernel: userorkernelbuffer

--- kernel/userorkernelbuffer.cpp
+++ kernel/userorkernelbuffer.cpp
@@ -12,7 +12,7 @@
 #include <kernel/userorkernelbuffer.h>
 #include <kernel/vm/memorymanager.h>
 
-namespace Kernel
+namespace Kernel 
 {
     /**
      * @return true 
@@ -31,18 +31,77 @@ namespace Kernel
     {
         if (!m_buffer)
             return {};
-        
+
         if (is_user_address(VirtualAddress(m_buffer))) {
             char* buffer;
             auto data_copy = StringImpl::create_uninitialized(size, buffer);
 
             if (!copy_from_user(buffer, m_buffer, size))
                 return {};
-            
+
             return data_copy;
         }
 
-        return String(ReadOnlyBytes { m_buffer, size });
+        return String(ReadonlyBytes { m_buffer, size });
     }
 
-}
+    /**
+     * @param src 
+     * @param offset 
+     * @param len 
+     * @return true 
+     * @return false 
+     */
+    bool UserOrKernelBuffer::write(const void* src, size_t offset, size_t len)
+    {
+        if (!m_buffer)
+            return false;
+
+        if (is_user_address(VirtualAddress(m_buffer)))
+            return copy_to_user(m_buffer + offset, src, len);
+
+        memcpy(m_buffer + offset, src, len);
+
+        return true;
+    }
+
+    /**
+     * @param dest 
+     * @param offset 
+     * @param len 
+     * @return true 
+     * @return false 
+     */
+    bool UserOrKernelBuffer::read(void* dest, size_t offset, size_t len) const
+    {
+        if (!m_buffer)
+            return false;
+
+        if (is_user_address(VirtualAddress(m_buffer)))
+            return copy_from_user(dest, m_buffer + offset, len);
+
+        memcpy(dest, m_buffer + offset, len);
+
+        return true;
+    }
+
+    /**
+     * @param value 
+     * @param offset 
+     * @param len 
+     * @return true 
+     * @return false 
+     */
+    bool UserOrKernelBuffer::memset(int value, size_t offset, size_t len)
+    {
+        if (!m_buffer)
+            return false;
+
+        if (is_user_address(VirtualAddress(m_buffer)))
+            return memset_user(m_buffer + offset, value, len);
+
+        ::memset(m_buffer + offset, value, len);
+
+        return true;
+    }
+} // namespace Kernel
