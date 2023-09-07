074e9c807166434d4bead9fa89a7c4195e99fd59
krishpranav <krisna.pranav@gmail.com>
2023-09-07 22:19:22 +0530

    kernel: doublebuffer

--- kernel/doublebuffer.cpp
+++ kernel/doublebuffer.cpp
@@ -12,19 +12,18 @@
 #include <mods/string_view.h>
 #include <kernel/doublebuffer.h>
 
-namespace Kernel
+namespace Kernel 
 {
 
     /**
-     * @brief ::compute_lockfree_metadata ()
-     * 
+     * @brief ::compute_lockfree_metadata()
      */
     inline void DoubleBuffer::compute_lockfree_metadata()
     {
         InterruptDisabler disabler;
-
-        m_empty = m_read_buffer_index >= m_read_buffer->size;
-        m_sapce_for_writing = m_capacity - m_write_buffer->size;
+        
+        m_empty = m_read_buffer_index >= m_read_buffer->size && m_write_buffer->size == 0;
+        m_space_for_writing = m_capacity - m_write_buffer->size;
     }
 
     /**
@@ -33,14 +32,13 @@ namespace Kernel
     DoubleBuffer::DoubleBuffer(size_t capacity)
         : m_write_buffer(&m_buffer1)
         , m_read_buffer(&m_buffer2)
-        , m_storage(KBuffer::create_with_size(capacity))
+        , m_storage(KBuffer::create_with_size(capacity * 2, Region::Access::Read | Region::Access::Write, "DoubleBuffer"))
         , m_capacity(capacity)
     {
         m_buffer1.data = m_storage.data();
         m_buffer1.size = 0;
         m_buffer2.data = m_storage.data() + capacity;
-        m_buffer.size = 0;
-
+        m_buffer2.size = 0;
         m_space_for_writing = capacity;
     }
 
@@ -51,7 +49,6 @@ namespace Kernel
     void DoubleBuffer::flip()
     {
         ASSERT(m_read_buffer_index == m_read_buffer->size);
-
         swap(m_read_buffer, m_write_buffer);
 
         m_write_buffer->size = 0;
@@ -69,9 +66,8 @@ namespace Kernel
     {
         if (!size)
             return 0;
-        
-        ASSERT(size > 0);
 
+        ASSERT(size > 0);
         LOCKER(m_lock);
 
         size_t bytes_to_write = min(size, m_space_for_writing);
@@ -81,14 +77,47 @@ namespace Kernel
 
         compute_lockfree_metadata();
 
-
         if (!data.read(write_ptr, bytes_to_write))
             return -EFAULT;
-        
+
         if (m_unblock_callback && !m_empty)
             m_unblock_callback();
+
+        return (ssize_t)bytes_to_write;
+    }
+
+    /**
+     * @param data 
+     * @param size 
+     * @return ssize_t 
+     */
+    ssize_t DoubleBuffer::read(UserOrKernelBuffer& data, size_t size)
+    {
+        if (!size)
+            return 0;
+
+        ASSERT(size > 0);
+        LOCKER(m_lock);
         
-        return (size_t) bytes_to_write;
+        if (m_read_buffer_index >= m_read_buffer->size && m_write_buffer->size != 0)
+            flip();
+
+        if (m_read_buffer_index >= m_read_buffer->size)
+            return 0;
+
+        size_t nread = min(m_read_buffer->size - m_read_buffer_index, size);
+
+        if (!data.write(m_read_buffer->data + m_read_buffer_index, nread))
+            return -EFAULT;
+            
+        m_read_buffer_index += nread;
+
+        compute_lockfree_metadata();
+
+        if (m_unblock_callback && m_space_for_writing > 0)
+            m_unblock_callback();
+            
+        return (ssize_t)nread;
     }
 
 } // namespace Kernel
