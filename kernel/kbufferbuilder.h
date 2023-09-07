b90be711a5c19b4c7e7d6bc8f74a8d600dbc83de
krishpranav <krisna.pranav@gmail.com>
2023-09-07 22:23:34 +0530

    kernel: kbufferbuilder

--- kernel/kbufferbuilder.h
+++ kernel/kbufferbuilder.h
@@ -15,22 +15,57 @@
 #include <kernel/kbuffer.h>
 #include <stdarg.h>
 
-namespace Kernel
+namespace Kernel 
 {
 
-    class KBufferBuilder
+    class KBufferBuilder 
     {
+    public:
+        using OutputType = KBuffer;
+
+        /// @brief Construct a new KBufferBuilder object
+        explicit KBufferBuilder();
+
+        /// @brief Destroy the KBufferBuilder object
+        ~KBufferBuilder() { }
+
+        void append(const StringView&);
+        void append(char);
+        
+        void append(const char*, int);
+        void appendf(const char*, ...);
+
+        void appendvf(const char*, va_list);
+
+        void append_escaped_for_json(const StringView&);
+
+        /**
+         * @tparam Parameters 
+         * @param fmtstr 
+         * @param parameters 
+         */
+        template<typename... Parameters>
+        void appendff(StringView fmtstr, const Parameters&... parameters)
+        {
+            append(String::formatted(fmtstr, parameters...));
+        }
+
+        KBuffer build();
+
     private:
         bool can_append(size_t) const;
 
+        /**
+         * @return u8* 
+         */
         u8* insertion_ptr() 
-        {
-            return m_buffer.data();
+        { 
+            return m_buffer.data() + m_size; 
         }
 
         KBuffer m_buffer;
+
         size_t m_size { 0 };
-        
-    }; // class KBufferBuidler
+    }; // class KBufferBuilder
 
 } // namespace Kernel
