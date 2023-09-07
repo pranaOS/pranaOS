7b4b7f4fd32b8dd6d5c696c2c7060488c8f305bf
krishpranav <krisna.pranav@gmail.com>
2023-09-07 22:18:17 +0530

    kernel: doubelbuffer

---
+++ kernel/doublebuffer.cpp
@@ -0,0 +1,94 @@
+/**
+ * @file doublebuffer.cpp
+ * @author Krisna Pranav
+ * @brief double buffer
+ * @version 6.0
+ * @date 2023-09-07
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include <mods/string_view.h>
+#include <kernel/doublebuffer.h>
+
+namespace Kernel
+{
+
+    /**
+     * @brief ::compute_lockfree_metadata ()
+     * 
+     */
+    inline void DoubleBuffer::compute_lockfree_metadata()
+    {
+        InterruptDisabler disabler;
+
+        m_empty = m_read_buffer_index >= m_read_buffer->size;
+        m_sapce_for_writing = m_capacity - m_write_buffer->size;
+    }
+
+    /**
+     * @param capacity 
+     */
+    DoubleBuffer::DoubleBuffer(size_t capacity)
+        : m_write_buffer(&m_buffer1)
+        , m_read_buffer(&m_buffer2)
+        , m_storage(KBuffer::create_with_size(capacity))
+        , m_capacity(capacity)
+    {
+        m_buffer1.data = m_storage.data();
+        m_buffer1.size = 0;
+        m_buffer2.data = m_storage.data() + capacity;
+        m_buffer.size = 0;
+
+        m_space_for_writing = capacity;
+    }
+
+    /**
+     * @brief ::flip()
+     * 
+     */
+    void DoubleBuffer::flip()
+    {
+        ASSERT(m_read_buffer_index == m_read_buffer->size);
+
+        swap(m_read_buffer, m_write_buffer);
+
+        m_write_buffer->size = 0;
+        m_read_buffer_index = 0;
+
+        compute_lockfree_metadata();
+    }
+
+    /**
+     * @param data 
+     * @param size 
+     * @return ssize_t 
+     */
+    ssize_t DoubleBuffer::write(const UserOrKernelBuffer& data, size_t size)
+    {
+        if (!size)
+            return 0;
+        
+        ASSERT(size > 0);
+
+        LOCKER(m_lock);
+
+        size_t bytes_to_write = min(size, m_space_for_writing);
+        u8* write_ptr = m_write_buffer->data + m_write_buffer->size;
+
+        m_write_buffer->size += bytes_to_write;
+
+        compute_lockfree_metadata();
+
+
+        if (!data.read(write_ptr, bytes_to_write))
+            return -EFAULT;
+        
+        if (m_unblock_callback && !m_empty)
+            m_unblock_callback();
+        
+        return (size_t) bytes_to_write;
+    }
+
+} // namespace Kernel
