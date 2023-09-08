f8757ce026f0a09ffdd1c99b718910d2ed57a64f
krishpranav <krisna.pranav@gmail.com>
2023-09-08 21:24:21 +0530

    mods: jsonobjectserialize

--- mods/jsonobjectserialize.h
+++ mods/jsonobjectserialize.h
@@ -14,11 +14,13 @@
 #include <mods/jsonarrayserializer.h>
 #include <mods/jsonval.h>
 
-namespace Mods
+namespace Mods 
 {
-    
+    /**
+     * @tparam Builder 
+     */
     template<typename Builder>
-    class JsonObjectSerializer
+    class JsonObjectSerializer 
     {
     public:
         /**
@@ -30,6 +32,7 @@ namespace Mods
             m_builder.append('{');
         }
 
+        /// @brief Construct a new Json Object Serializer object
         JsonObjectSerializer(const JsonObjectSerializer&) = delete;
         JsonObjectSerializer(JsonObjectSerializer&&) = delete;
 
@@ -40,16 +43,161 @@ namespace Mods
                 finish();
         }
 
+        /**
+         * @param key 
+         * @param value 
+         */
+        void add(const StringView& key, const JsonValue& value)
+        {
+            begin_item(key);
+            value.serialize(m_builder);
+        }
+
+        /**
+         * @param key 
+         * @param value 
+         */
+        void add(const StringView& key, const StringView& value)
+        {
+            begin_item(key);
+            m_builder.append('"');
+            m_builder.append_escaped_for_json(value);
+            m_builder.append('"');
+        }
+
+        /**
+         * @param key 
+         * @param value 
+         */
+        void add(const StringView& key, const String& value)
+        {
+            begin_item(key);
+            m_builder.append('"');
+            m_builder.append_escaped_for_json(value);
+            m_builder.append('"');
+        }
+
+        /**
+         * @param key 
+         * @param value 
+         */
+        void add(const StringView& key, const char* value)
+        {
+            begin_item(key);
+            m_builder.append('"');
+            m_builder.append_escaped_for_json(value);
+            m_builder.append('"');
+        }
+
+        /**
+         * @param key 
+         * @param value 
+         */
+        void add(const StringView& key, bool value)
+        {
+            begin_item(key);
+            m_builder.append(value ? "true" : "false");
+        }
+
+        /**
+         * @param key 
+         * @param value 
+         */
+        void add(const StringView& key, int value)
+        {
+            begin_item(key);
+            m_builder.appendf("%d", value);
+        }
+
+        /**
+         * @param key 
+         * @param value 
+         */
+        void add(const StringView& key, unsigned value)
+        {
+            begin_item(key);
+            m_builder.appendf("%u", value);
+        }
+
+        /**
+         * @param key 
+         * @param value 
+         */
+        void add(const StringView& key, long value)
+        {
+            begin_item(key);
+            m_builder.appendf("%ld", value);
+        }
+
+        /**
+         * @param key 
+         * @param value 
+         */
+        void add(const StringView& key, long unsigned value)
+        {
+            begin_item(key);
+            m_builder.appendf("%lu", value);
+        }
+
+        /**
+         * @param key 
+         * @param value 
+         */
+        void add(const StringView& key, long long value)
+        {
+            begin_item(key);
+            m_builder.appendf("%lld", value);
+        }
+
+        /**
+         * @param key 
+         * @param value 
+         */
+        void add(const StringView& key, long long unsigned value)
+        {
+            begin_item(key);
+            m_builder.appendf("%llu", value);
+        }
+
+        /**
+         * @param key 
+         * @param value 
+         */
+        void add(const StringView& key, double value)
+        {
+            begin_item(key);
+            m_builder.appendf("%f", value);
+        }
+
+        /**
+         * @param key 
+         * @return JsonArraySerializer<Builder> 
+         */
+        JsonArraySerializer<Builder> add_array(const StringView& key)
+        {
+            begin_item(key);
+            return JsonArraySerializer(m_builder);
+        }
+
+        /**
+         * @param key 
+         * @return JsonObjectSerializer<Builder> 
+         */
+        JsonObjectSerializer<Builder> add_object(const StringView& key)
+        {
+            begin_item(key);
+            return JsonObjectSerializer(m_builder);
+        }
+
         void finish()
         {
             ASSERT(!m_finished);
-
             m_finished = true;
-
             m_builder.append('}');
         }
 
     private:
+
         /**
          * @param key 
          */
@@ -57,7 +205,7 @@ namespace Mods
         {
             if (!m_empty)
                 m_builder.append(',');
-            
+
             m_empty = false;
 
             m_builder.append('"');
@@ -66,9 +214,18 @@ namespace Mods
         }
 
         Builder& m_builder;
-
+        
         bool m_empty { true };
         bool m_finished { false };
-    }; // class JsonObjectSerializer
+    };
+
+    template<typename Builder>
+    JsonObjectSerializer<Builder> JsonArraySerializer<Builder>::add_object()
+    {
+        begin_item();
+        return JsonObjectSerializer(m_builder);
+    }
 
 } // namespace Mods
+
+using Mods::JsonObjectSerializer;
