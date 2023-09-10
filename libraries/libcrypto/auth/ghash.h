f387ce0aac14bc0c4b017ff44aed6cb2f7177a9b
krishpranav <krisna.pranav@gmail.com>
2023-09-10 15:13:50 +0530

    libraries: libcrypto[auth(ghash)]

--- libraries/libcrypto/auth/ghash.h
+++ libraries/libcrypto/auth/ghash.h
@@ -15,48 +15,84 @@
 #include <mods/types.h>
 #include <libcrypto/hash/hashfunction.h>
 
-namespace Crypto
+namespace Crypto 
 {
-    namespace Authentication
+    namespace Authentication 
     {
-        struct  GHashDigest
+
+        void galois_multiply(u32 (&z)[4], const u32 (&x)[4], const u32 (&y)[4]);
+
+        struct GHashDigest 
         {
             constexpr static size_t Size = 16;
             u8 data[Size];
-
-            const u8* immutable_data() const
-            {
-                return data;
+            
+            /**
+             * @return const u8* 
+             */
+            const u8* immutable_data() const 
+            { 
+                return data; 
             }
 
+            /**
+             * @return size_t 
+             */
             size_t data_length() 
-            {
-                return Size;
+            { 
+                return Size; 
             }
-        }; // struct 
+        }; // struct GHashDigest
 
-        class GHash final
+        class GHash final 
         {
         public:
             using TagType = GHashDigest;
 
+            /**
+             * @tparam N 
+             */
             template<size_t N>
             explicit GHash(const char (&key)[N])
-                : GHash({ key. N})
-            {}
+                : GHash({ key, N })
+            { }
+            
+            /**
+             * @param key 
+             */
+            explicit GHash(const ReadonlyBytes& key)
+            {
+                for (size_t i = 0; i < 16; i += 4)
+                    m_key[i / 4] = Mods::convert_between_host_and_big_endian(*(const u32*)(key.offset(i)));
+            }
 
             /**
              * @return constexpr size_t 
              */
-            constexpr static size_t digest_size()
-            {
-                return TagType::Size;
+            constexpr static size_t digest_size() 
+            { 
+                return TagType::Size; 
             }
+
+            /**
+             * @return String 
+             */
+            String class_name() const 
+            { 
+                return "GHash"; 
+            }
+
+            /**
+             * @param aad 
+             * @param cipher 
+             * @return TagType 
+             */
+            TagType process(ReadonlyBytes aad, ReadonlyBytes cipher);
 
         private:
             inline void transform(ReadonlyBytes, ReadonlyBytes);
 
             u32 m_key[4];
-        }; // class 
-    }
-}
+        }; // class GHash
+    } // namespace Authentication
+} // namespace Crypto
