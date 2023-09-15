c19c14fa646c627ed6c1a2a757ed4a61b4de8cb6
krishpranav <krisna.pranav@gmail.com>
2023-09-15 22:17:46 +0530

    libraries: libunwind

---
+++ libraries/libunwind/unwidbase.h
@@ -0,0 +1,23 @@
+/**
+ * @file unwidbase.h
+ * @author Krisna Pranav
+ * @brief unwind base
+ * @version 6.0
+ * @date 2023-09-15
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+#include <stdint.h>
+
+extern "C"
+{
+
+    enum _Unwind_Reason_Code {
+        _URC_NO_REASON = 0,
+    }
+
+};
