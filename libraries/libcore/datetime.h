1a1c6a95b4f3ea1afd02ae28c7d1039b80837fcb
krishpranav <krisna.pranav@gmail.com>
2023-09-22 21:40:01 +0530

    libraries: libcore[datetime]

--- libraries/libcore/datetime.h
+++ libraries/libcore/datetime.h
@@ -14,46 +14,116 @@
 #include <mods/string.h>
 #include <time.h>
 
-namespace Core
+namespace Core 
 {
-
-    class DateTime
+    class DateTime 
     {
     public:
+
+        /**
+         * @return time_t 
+         */
         time_t timestamp() const 
-        {
-            return m_timestamp;
+        { 
+            return m_timestamp; 
         }
 
-        unsigned year() const
-        {
-            return m_year;
+        /**
+         * @return unsigned 
+         */
+        unsigned year() const 
+        { 
+            return m_year; 
+        }
+        
+        unsigned month() const 
+        { 
+            return m_month; 
         }
 
-        unsigned month() const
-        {
-            return m_month;
+        unsigned day() const 
+        { 
+            return m_day; 
         }
 
-        unsigned day() const
-        {
-            return m_day;
+        unsigned hour() const 
+        { 
+            return m_hour; 
         }
 
+        unsigned minute() const 
+        { 
+            return m_minute; 
+        }
+
+        unsigned second() const 
+        { 
+            return m_second; 
+        }
+
+        unsigned weekday() const;
+        unsigned days_in_month() const;
+        unsigned day_of_year() const;
+
+        bool is_leap_year() const;
+
+        /**
+         * @param year 
+         * @param month 
+         * @param day 
+         * @param hour 
+         * @param minute 
+         * @param second 
+         */
+        void set_time(unsigned year, unsigned month = 1, unsigned day = 0, unsigned hour = 0, unsigned minute = 0, unsigned second = 0);
+
+        /**
+         * @param format 
+         * @return String 
+         */
         String to_string(const String& format = "%Y-%m-%d %H:%M:%S") const;
 
+        /**
+         * @param year 
+         * @param month 
+         * @param day 
+         * @param hour 
+         * @param minute 
+         * @param second 
+         * @return DateTime 
+         */
+        static DateTime create(unsigned year, unsigned month = 1, unsigned day = 0, unsigned hour = 0, unsigned minute = 0, unsigned second = 0);
+
+        /**
+         * @return DateTime 
+         */
+        static DateTime now();
+
+        /**
+         * @return DateTime 
+         */
+        static DateTime from_timestamp(time_t);
+        
+        /**
+         * @return true 
+         * @return false 
+         */
+        bool is_before(const String&) const;
 
     private:
         time_t m_timestamp { 0 };
-
+        
         unsigned m_year { 0 };
         unsigned m_month { 0 };
         unsigned m_day { 0 };
         unsigned m_hour { 0 };
         unsigned m_minute { 0 };
         unsigned m_second { 0 };
-    }; // class DateTime
+    };
 
+    /**
+     * @return const LogStream& 
+     */
     const LogStream& operator<<(const LogStream&, const DateTime&);
 
-} // namespace Core
+}
