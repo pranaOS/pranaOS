e9fc1e69e160884bea613ba71c8dad3daaf1ef0e
krishpranav <krisna.pranav@gmail.com>
2023-09-22 20:51:01 +0530

    libraries: libcore[command]

---
+++ libraries/libcore/command.h
@@ -0,0 +1,35 @@
+/**
+ * @file command.h
+ * @author Krisna Pranav
+ * @brief command
+ * @version 6.0
+ * @date 2023-09-22
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+#include <mods/lexical_path.h>
+#include <mods/optional.h>
+#include <mods/string.h>
+#include <spawn.h>
+
+namespace Core
+{
+    /**
+     * @param program 
+     * @param arguments 
+     * @param chdir 
+     * @return String 
+     */
+    String command(const String& program, const Vector<String>& arguments, Optional<LexicalPath> chdir);
+
+    /**
+     * @param command_string 
+     * @param chdir 
+     * @return String 
+     */
+    String command(const String& command_string, Optional<LexicalPath> chdir);
+} // namespace Core
