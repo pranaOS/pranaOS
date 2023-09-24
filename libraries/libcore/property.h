5428f6f78167bf96c5bf1bcae156f1bccbf79c45
krishpranav <krisna.pranav@gmail.com>
2023-09-24 12:48:06 +0530

    libraries: libcore[property]

---
+++ libraries/libcore/property.h
@@ -0,0 +1,54 @@
+/**
+ * @file property.h
+ * @author Krisna Pranav
+ * @brief property
+ * @version 6.0
+ * @date 2023-09-24
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+#include <mods/function.h>
+#include <mods/jsonval.h>
+
+namespace Core
+{
+    class Property
+    {
+        MOD_MAKE_NONCOPYABLE(Property);
+
+    public:
+        /**
+         * @param name 
+         * @param getter 
+         * @param setter 
+         */
+        Property(String name, Function<JsonValue()> getter, Function<bool(const JsonValue&)> setter = nullptr);
+
+        /// @brief Destroy the Property object
+        ~Property()
+
+        /**
+         * @param value 
+         * @return true 
+         * @return false 
+         */
+        bool set(const JsonValue& value)
+        {
+            if (!m_setter)
+                return false;
+            
+            return m_setter(value);
+        }
+
+    private:
+        String m_name;
+
+        Function<JsonValue()> m_getter;
+
+        Function<bool(const JsonValue&)> m_setter;
+    }; // class Property
+}
