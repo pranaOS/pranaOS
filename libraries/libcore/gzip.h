5a809361768edc5812984191ee68bc160c5e365a
krishpranav <krisna.pranav@gmail.com>
2023-09-24 16:46:57 +0530

    libraries: libcore[gzip]

---
+++ libraries/libcore/gzip.h
@@ -0,0 +1,29 @@
+/**
+ * @file gzip.h
+ * @author Krisna Pranav
+ * @brief gzip
+ * @version 6.0
+ * @date 2023-09-24
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+#include <mods/byte_buffer.h>
+#include <mods/optional.h>
+#include <mods/string.h>
+
+namespace Core
+{
+    class Gzip 
+    {
+    public:
+        /**
+         * @param data 
+         * @return Optional<ByteBuffer> 
+         */
+        static Optional<ByteBuffer> decompress(const ByteBuffer& data);
+    }; // class Gzip
+} // namespace Core
