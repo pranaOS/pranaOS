b38c46f6beb85f1a0ce896f921d0cda132c4527f
krishpranav <krisna.pranav@gmail.com>
2023-09-25 18:42:50 +0530

    libraries: libcore[forward]

--- libraries/libcore/forward.h
+++ libraries/libcore/forward.h
@@ -11,15 +11,17 @@
 
 #pragma once 
 
-namespace Core
+namespace Core 
 {
     class ArgsParser;
     class ChildEvent;
     class ConfigFile;
     class CustomEvent;
     class DateTime;
+    class DirIterator;
     class ElapsedTimer;
     class Event;
+    class EventLoop;
     class File;
     class IODevice;
     class LocalServer;
@@ -28,4 +30,16 @@ namespace Core
     class NetworkJob;
     class NetworkResponse;
     class Notifier;
-}
+    class Object;
+    class ProcessStatisticsReader;
+    class Socket;
+    class SocketAddress;
+    class TCPServer;
+    class TCPSocket;
+    class Timer;
+    class TimerEvent;
+    class UDPServer;
+    class UDPSocket;
+
+    enum class TimerShouldFireWhenNotVisible;
+} // namespace Core
