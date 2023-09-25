6c34cc7c5bc7ed09d223c670286a1736bca84e1b
krishpranav <krisna.pranav@gmail.com>
2023-09-25 18:42:18 +0530

    libraries: libcore[forward]

---
+++ libraries/libcore/forward.h
@@ -0,0 +1,31 @@
+/**
+ * @file forward.h
+ * @author Krisna Pranav
+ * @brief forward
+ * @version 6.0
+ * @date 2023-09-25
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+namespace Core
+{
+    class ArgsParser;
+    class ChildEvent;
+    class ConfigFile;
+    class CustomEvent;
+    class DateTime;
+    class ElapsedTimer;
+    class Event;
+    class File;
+    class IODevice;
+    class LocalServer;
+    class LocalSocket;
+    class MimeData;
+    class NetworkJob;
+    class NetworkResponse;
+    class Notifier;
+}
