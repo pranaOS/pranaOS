815fc58d8cddd2a3606d83a5afebd8df3ed9c982
krishpranav <krisna.pranav@gmail.com>
2023-09-30 19:33:01 +0530

    libcoredump: coredumpread

--- libraries/libcoredump/coredumpread.h
+++ libraries/libcoredump/coredumpread.h
@@ -17,19 +17,22 @@
 #include <libelf/coredump.h>
 #include <libelf/image.h>
 
-class CoreDumpRead 
+
+class CoreDumpReader 
 {
+    MOD_MAKE_NONCOPYABLE(CoreDumpReader);
+
 public:
     /**
-     * @return OwnPtr<CoreDumpRead> 
+     * @return OwnPtr<CoreDumpReader> 
      */
-    static OwnPtr<CoreDumpRead> create(const String&);
+    static OwnPtr<CoreDumpReader> create(const String&);
 
-    ///  @brief Destroy the Core Dump Read object
-    ~CoreDumpRead();
+    /// @brief Destroy the Core Dump Reader object
+    ~CoreDumpReader();
 
-    /// @brief Construct a new Core Dump Read object
-    CoreDumpRead(OwnPtr<MappedFile>&&);
+    /// @brief Construct a new Core Dump Reader object
+    CoreDumpReader(OwnPtr<MappedFile>&&);
 
     /**
      * @tparam Func 
@@ -39,11 +42,18 @@ public:
     void for_each_memory_region_info(Func func) const;
 
     /**
+     * @tparam Func 
+     * @param func 
+     */
+    template<typename Func>
+    void for_each_thread_info(Func func) const;
+
+    /**
      * @return const ELF::Image& 
      */
-    const ELF::Image& image() const
-    {
-        return m_coredump_image;
+    const ELF::Image& image() const 
+    { 
+        return m_coredump_image; 
     }
 
     /**
@@ -51,10 +61,15 @@ public:
      * @return Optional<uint32_t> 
      */
     Optional<uint32_t> peek_memory(FlatPtr address) const;
+    
+    /**
+     * @param address 
+     * @return const ELF::Core::MemoryRegionInfo* 
+     */
+    const ELF::Core::MemoryRegionInfo* region_containing(FlatPtr address) const;
 
 private:
-    
-    class NotesEntryIterator
+    class NotesEntryIterator 
     {
     public:
         /**
@@ -73,17 +88,53 @@ private:
         const ELF::Core::NotesEntry* current() const;
 
         void next();
-        
+
         bool at_end() const;
 
     private:
         const ELF::Core::NotesEntry* m_current { nullptr };
         const u8* start { nullptr };
-    }; // class NotesEntryIterator
-
+    }; // class NotesEntryHeader
 
     OwnPtr<MappedFile> m_coredump_file;
     ELF::Image m_coredump_image;
     ssize_t m_notes_segment_index { -1 };
+}; // class CoreDumpReader
 
-}; // class CoreDumpRead
+/**
+ * @tparam Func 
+ * @param func 
+ */
+template<typename Func>
+void CoreDumpReader::for_each_memory_region_info(Func func) const
+{
+    for (NotesEntryIterator it((const u8*)m_coredump_image.program_header(m_notes_segment_index).raw_data()); !it.at_end(); it.next()) {
+        if (it.type() != ELF::Core::NotesEntryHeader::Type::MemoryRegionInfo)
+            continue;
+
+        auto* region = (const ELF::Core::MemoryRegionInfo*)(it.current());
+
+        IterationDecision decision = func(region);
+        if (decision == IterationDecision::Break)
+            return;
+    }
+}
+
+/**
+ * @tparam Func 
+ * @param func 
+ */
+template<typename Func>
+void CoreDumpReader::for_each_thread_info(Func func) const
+{
+    for (NotesEntryIterator it((const u8*)m_coredump_image.program_header(m_notes_segment_index).raw_data()); !it.at_end(); it.next()) {
+        if (it.type() != ELF::Core::NotesEntryHeader::Type::ThreadInfo)
+            continue;
+
+        auto* region = (const ELF::Core::ThreadInfo*)(it.current());
+
+        IterationDecision decision = func(region);
+        if (decision == IterationDecision::Break)
+            return;
+    }
+} 
