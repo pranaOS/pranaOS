6ed0e3e75dde8c50d813311485fb55f1937e70ff
krishpranav <krisna.pranav@gmail.com>
2023-10-02 11:49:06 +0530

    libraries: libcore[tcpsocket]

--- libraries/libcore/tcpsocket.cpp
+++ libraries/libcore/tcpsocket.cpp
@@ -17,17 +17,49 @@
 #   include <sys/ioctl.h>
 #endif
 
-namespace Core
+namespace Core 
 {
-
+    /**
+     * @brief Construct a new TCPSocket::TCPSocket object
+     * 
+     * @param fd 
+     * @param parent 
+     */
     TCPSocket::TCPSocket(int fd, Object* parent)
-            : Socket(Socket::Type::TCP, parent)
+        : Socket(Socket::Type::TCP, parent)
     {
         m_connected = true;
         set_fd(fd);
+        set_mode(IODevice::ReadWrite);
+        set_error(0);
     }
 
-    TCPSocket::~TCPSocket()
-    {}
+    /**
+     * @brief Construct a new TCPSocket::TCPSocket object
+     * 
+     * @param parent 
+     */
+    TCPSocket::TCPSocket(Object* parent)
+        : Socket(Socket::Type::TCP, parent)
+    {
+    #ifdef SOCK_NONBLOCK
+        int fd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
+    #else
+        int fd = socket(AF_INET, SOCK_STREAM, 0);
+        int option = 1;
+        ioctl(fd, FIONBIO, &option);
+    #endif
+        if (fd < 0) {
+            set_error(errno);
+        } else {
+            set_fd(fd);
+            set_mode(IODevice::ReadWrite);
+            set_error(0);
+        }
+    }
 
-} // namespace Core
+    /// @brief Destroy the TCPSocket::TCPSocket object
+    TCPSocket::~TCPSocket()
+    { }
+
+} // namesapce Core
