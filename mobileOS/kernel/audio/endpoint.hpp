e5f67dcb4f06cb6f93038ef17e40bc5641d82750
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-07-30 18:32:37 +0530

    mobileOS: kerenl[bluetooth(endpoint)]

--- mobileOS/kernel/audio/endpoint.hpp
+++ mobileOS/kernel/audio/endpoint.hpp
@@ -8,3 +8,36 @@
  * @copyright Copyright (c) 2021-2024, pranaOS Developers, Krisna Pranav
  *
  */
+
+#pragma once
+
+#include "abstractstream.hpp"
+#include "audioformat.hpp"
+#include <chrono>
+#include <cstdint>
+#include <optional>
+#include <vector>
+
+namespace audio
+{
+class Endpoint
+{
+  public:
+    struct Traits
+    {
+        bool usesDMA = false;
+        std::optional<std::size_t> blockSizeConstraint = std::nullopt;
+        std::optional<std::chrono::milliseconds> timeConstraint = std::nullopt;
+    }; // struct Traits
+
+    Endpoint() = default;
+
+    /**
+     * @param stream
+     */
+    void connectStream(AbstractStream& stream);
+
+    void disconnectStream();
+    bool isConnected() const noexcept;
+}; // class Endpoint
+} // namespace audio
