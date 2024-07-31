f91d2ea684bb33991e88154a1d942ccc5a82546b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-07-31 19:22:56 +0530

    mobileOS: kerenl[vfs(vfs_io_syscalls)]

--- mobileOS/kernel/vfs/vfs_io_syscalls.hpp
+++ mobileOS/kernel/vfs/vfs_io_syscalls.hpp
@@ -16,5 +16,9 @@ struct statvfs;
 
 namespace vfsn::internal::syscalls
 {
-    
+    int open(int &_errno_, const char *file, int flags, int mode);
+    long close(int &_errno_, int fd);
+    long write(int &_errno_, int fd, const void *buf, size_t cnt);
+    long read(int &_errno_, int fd, void *buf, size_t cnt);
+    off_t lseek(int &_errno_, int fd, off_t pos, int dir);
 } // namespace vfs::internals::syscalls
