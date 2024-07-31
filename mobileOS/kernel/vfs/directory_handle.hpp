5455abf8b8efd390d68c8f568ee66899af00ad31
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-07-31 19:31:42 +0530

    mobileOS: kerenl[vfs(directory_handle)]

--- mobileOS/kernel/vfs/directory_handle.hpp
+++ mobileOS/kernel/vfs/directory_handle.hpp
@@ -4,3 +4,21 @@
 //
 //  Created by Krisna Pranav on 31/07/24.
 //
+
+#pragma once
+
+#include <memory>
+
+namespace purefs::fs::internal
+{
+
+    class mount_point;
+    class directory_handle
+    {
+    public:
+        directory_handle(std::shared_ptr<mount_point> mp, int error) : m_error(error), m_mount_point(mp)
+        {}
+        
+        directory_handle(const directory_handle &) = delete;
+    };
+} // namespace purefs::fs::internal
