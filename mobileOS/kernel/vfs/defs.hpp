a626241773c3de138d84b197e2ff52ae8aa706ed
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-08-01 20:20:24 +0530

    mobileOS: kerenl[vfs(defs)]

---
+++ mobileOS/kernel/vfs/defs.hpp
@@ -0,0 +1,66 @@
+/**
+ * @file defs.hpp
+ * @author Krisna Pranav
+ * @brief defs
+ * @version 1.0
+ * @date 2024-08-01
+ *
+ * @copyright Copyright (c) 2024 Krisna Pranav
+ *
+ */
+
+#pragma once
+
+#include <cstdint>
+#include <memory>
+
+namespace purefs::blkdev
+{
+    using sector_t = uint64_t;
+    using scount_t = int64_t;
+    using hwpart_t = uint8_t;
+    using part_t = int16_t;
+
+    constexpr hwpart_t default_hw_partition = 0;
+
+    namespace internal
+    {
+        class disk_handle;
+    } // namespace internal
+
+    using disk_fd = std::shared_ptr<internal::disk_handle>;
+
+    enum class media_status
+    {
+        healthy,
+        uninit,
+        nomedia,
+        wprotect,
+        error,
+    }; // enum class media_status
+
+    enum class info_type
+    {
+        sector_count,
+        sector_size,
+        erase_block,
+        start_sector
+    }; // enum class info_type
+
+    enum class pm_state
+    {
+        active,
+        low_power,
+        suspend,
+        force_suspend,
+        power_off
+    }; // enum class pm_state
+
+    struct flags
+    {
+        enum _flags
+        {
+            no_parts_scan = 0x1
+        }; // enum _flags
+    }; // struct flags
+} // namespace purefs::blkdev
