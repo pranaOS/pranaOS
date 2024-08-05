1137ec76aaf77fe2fe989579ad6c8f46eb1e96a0
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-08-05 18:42:19 +0530

    mobileOS: kerenl[vfs(disk)]

--- mobileOS/kernel/vfs/disk.hpp
+++ mobileOS/kernel/vfs/disk.hpp
@@ -21,37 +21,104 @@ namespace purefs::blkdev
     {
       public:
         disk() = default;
-        disk(const disk &) = delete;
-        auto operator=(const disk &) = delete;
+
+        /**
+         * @brief Construct a new disk object
+         *
+         */
+        disk(const disk&) = delete;
+
+        /**
+         * @return auto
+         */
+        auto operator=(const disk&) = delete;
+
+        /**
+         * @brief Destroy the disk object
+         *
+         */
         virtual ~disk() = default;
-        
+
+        /**
+         * @param flags
+         * @return int
+         */
         virtual auto probe(unsigned flags) -> int = 0;
-        
+
+        /**
+         * @return int
+         */
         virtual auto cleanup() -> int;
-        
-        virtual auto write(const void *buf, sector_t lba, std::size_t count, hwpart_t hwpart) -> int = 0;
-        
-        virtual auto read(void *buf, sector_t lba, std::size_t count, hwpart_t hwpart) -> int = 0;
-        
+
+        /**
+         * @param buf
+         * @param lba
+         * @param count
+         * @param hwpart
+         * @return int
+         */
+        virtual auto write(const void* buf, sector_t lba, std::size_t count, hwpart_t hwpart) -> int = 0;
+
+        /**
+         * @param buf
+         * @param lba
+         * @param count
+         * @param hwpart
+         * @return int
+         */
+        virtual auto read(void* buf, sector_t lba, std::size_t count, hwpart_t hwpart) -> int = 0;
+
+        /**
+         * @param lba
+         * @param count
+         * @param hwpart
+         * @return int
+         */
         virtual auto erase(sector_t lba, std::size_t count, hwpart_t hwpart) -> int;
-        
+
+        /**
+         * @return int
+         */
         virtual auto sync() -> int;
-        
+
+        /**
+         * @param target_state
+         * @return int
+         */
         virtual auto pm_control(pm_state target_state) -> int;
-        
-        virtual auto pm_read(pm_state &current_state) -> int;
-        
+
+        /**
+         * @param current_state
+         * @return int
+         */
+        virtual auto pm_read(pm_state& current_state) -> int;
+
+        /**
+         * @return media_status
+         */
         [[nodiscard]] virtual auto status() const -> media_status = 0;
-        
+
+        /**
+         * @param what
+         * @param hwpart
+         * @return scount_t
+         */
         [[nodiscard]] virtual auto get_info(info_type what, hwpart_t hwpart) const -> scount_t = 0;
-        
-        [[nodiscard]] auto partitions() -> std::vector<partition> &
+
+        /**
+         * @return std::vector<partition>&
+         */
+        [[nodiscard]] auto partitions() -> std::vector<partition>&
         {
             return m_partitions;
         }
-        
-    private:
+
+        auto clear_partitions() -> void
+        {
+            m_partitions.clear();
+        }
+
+      private:
         std::vector<partition> m_partitions;
     }; // class disk
-
 } // namespace purefs::blkdev
