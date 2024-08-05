40a7a0321d7bde8365d99efc481b06fc9583ea40
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-08-05 18:35:49 +0530

    mobileOS: kerenl[vfs(disk) + xcode]

---
+++ mobileOS/kernel/vfs/disk.hpp
@@ -0,0 +1,25 @@
+/**
+ * @file disk.hpp
+ * @author Krisna Pranav
+ * @brief disk
+ * @version 6.0
+ * @date 2024-08-05
+ *
+ * @copyright Copyright (c) 2021 - 2024 pranaOS Developers, Krisna Pranav
+ *
+ */
+
+#pragma once
+
+#include "defs.hpp"
+#include "partition.hpp"
+#include <vector>
+
+namespace purefs::blkdev
+{
+    class disk
+    {
+      public:
+    }; // class disk
+
+} // namespace purefs::blkdev
