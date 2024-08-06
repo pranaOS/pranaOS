c6521f75499800b941d940342af6e1c5fc3841df
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-08-06 18:39:02 +0530

    mobileOS: kerenl[vfs(disk_manager)]

---
+++ mobileOS/kernel/vfs/disk_manager.hpp
@@ -0,0 +1,24 @@
+//
+//  disk_manager.hpp
+//  pranaOS
+//
+//  Created by Krisna Pranav on 06/08/24.
+//
+
+#pragma once
+
+#include <stdio.h>
+#include <memory>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <tuple>
+#include <optional>
+#include "defs.hpp"
+#include "partition.hpp"
+
+
+namespace cpp_freertos
+{
+    class MutexRecursive;
+} // namespace cpp_freertos
