f6f17ce241f81595d2812f363d76c4a8b2811a88
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-08-08 11:20:30 +0530

    mobileOS: kerenl[vfs(inotify_flags)]

--- mobileOS/kernel/vfs/inotify_flags.hpp
+++ mobileOS/kernel/vfs/inotify_flags.hpp
@@ -11,18 +11,60 @@
 
 #pragma once
 
-namespace purefs::fs 
+namespace purefs::fs
 {
-        enum class inotify_flags: unsigned
-        {
-            attrib = 0x01,
-            close_write = 0x02,
-            close_nowrite = 0x04,
-            del = 0x08,
-            move_src = 0x10,
-            move_dst = 0x20,
-            open = 0x40,
-            dmodify = 0x80,
-        }; // enum class inotify_flags
+    enum class inotify_flags : unsigned
+    {
+        attrib = 0x01,
+        close_write = 0x02,
+        close_nowrite = 0x04,
+        del = 0x08,
+        move_src = 0x10,
+        move_dst = 0x20,
+        open = 0x40,
+        dmodify = 0x80,
+    }; // enum class inotify_flags
 
-}
+    /**
+     * @param fl1
+     * @param fl2
+     * @return auto
+     */
+    inline auto operator|(inotify_flags fl1, inotify_flags fl2)
+    {
+        return static_cast<inotify_flags>(static_cast<unsigned>(fl1) | static_cast<unsigned>(fl2));
+    }
+
+    /**
+     * @param fl1
+     * @param fl2
+     * @return auto
+     */
+    inline auto operator&(inotify_flags fl1, inotify_flags fl2)
+    {
+        return static_cast<inotify_flags>(static_cast<unsigned>(fl1) & static_cast<unsigned>(fl2));
+    }
+
+    /**
+     * @param fl1
+     * @param fl2
+     * @return true
+     * @return false
+     */
+    inline auto operator&&(inotify_flags fl1, inotify_flags fl2) -> bool
+    {
+        return static_cast<bool>(static_cast<unsigned>(fl1) & static_cast<unsigned>(fl2));
+    }
+
+    /**
+     * @param fl1
+     * @param fl2
+     * @return true
+     * @return false
+     */
+    inline auto operator||(inotify_flags fl1, inotify_flags fl2) -> bool
+    {
+        return static_cast<bool>(static_cast<unsigned>(fl1) | static_cast<unsigned>(fl2));
+    }
+
+} // namespace purefs::fs
