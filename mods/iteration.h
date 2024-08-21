7a66ece1db644fe9ba943dcbc5056786ec66ca6f
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-08-21 18:52:30 +0530

    mods: iteration[Continue, break func for kernel]

---
+++ mods/iteration.h
@@ -0,0 +1,23 @@
+/**
+ * @file iteration.h
+ * @author Krisna Pranav
+ * @brief iteration
+ * @version 6.0
+ * @date 2024-08-21
+ * 
+ * @copyright Copyright (c) 2021 - 2023 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+namespace Mods
+{
+    enum class Iteration
+    {
+        Continue,
+        Break,
+    }; // enum class Iteration
+} // namespace Mods
+
+using Mods::Iteration;
