67c34d36f2cd738fa3726e160067076d19a47abf
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-08-22 19:11:03 +0530

    mods: iterator[simpleiterator]

--- mods/iterator.h
+++ mods/iterator.h
@@ -13,222 +13,235 @@
 
 #include "forward.h"
 
-namespace Mods {
-
+namespace Mods
+{
+    /**
+     * @tparam Container 
+     * @tparam ValueType 
+     */
     template<typename Container, typename ValueType>
-    class SimpleIterator {
+    class SimpleIterator 
+    {
     public:
         friend Container;
 
         /**
-         * @brief is_end
-         * 
          * @return true 
          * @return false 
          */
-        constexpr bool is_end() const { 
+        constexpr bool is_end() const 
+        { 
             return m_index == SimpleIterator::end(m_container).m_index; 
         }
 
         /**
-         * @brief index
-         * 
          * @return constexpr size_t 
          */
-        constexpr size_t index() const { 
-            return m_index;
+        constexpr size_t index() const 
+        { 
+            return m_index; 
         }
 
         /**
-         * @brief operator==
-         * 
          * @param other 
          * @return true 
          * @return false 
          */
-        constexpr bool operator==(SimpleIterator other) const { 
+        constexpr bool operator==(SimpleIterator other) const 
+        { 
             return m_index == other.m_index; 
         }
 
         /**
-         * @brief operator!=
-         * 
          * @param other 
          * @return true 
          * @return false 
          */
-        constexpr bool operator!=(SimpleIterator other) const { 
-            return m_index != other.m_index;
+        constexpr bool operator!=(SimpleIterator other) const 
+        { 
+            return m_index != other.m_index; 
         }
 
         /**
-         * @brief operator<
-         * 
          * @param other 
          * @return true 
          * @return false 
          */
-        constexpr bool operator<(SimpleIterator other) const { 
+        constexpr bool operator<(SimpleIterator other) const 
+        { 
             return m_index < other.m_index; 
         }
 
         /**
-         * @brief operator>
-         * 
          * @param other 
          * @return true 
          * @return false 
          */
-        constexpr bool operator>(SimpleIterator other) const { 
+        constexpr bool operator>(SimpleIterator other) const 
+        { 
             return m_index > other.m_index; 
         }
 
         /**
-         * @brief <=
-         * 
          * @param other 
          * @return true 
          * @return false 
          */
-        constexpr bool operator<=(SimpleIterator other) const { 
+        constexpr bool operator<=(SimpleIterator other) const 
+        { 
             return m_index <= other.m_index; 
         }
 
         /**
-         * @brief >=
-         * 
          * @param other 
          * @return true 
          * @return false 
          */
-        constexpr bool operator>=(SimpleIterator other) const { 
+        constexpr bool operator>=(SimpleIterator other) const 
+        { 
             return m_index >= other.m_index; 
         }
 
         /**
-         * @brief +
-         * 
          * @param delta 
          * @return constexpr SimpleIterator 
          */
-        constexpr SimpleIterator operator+(ptrdiff_t delta) const { 
-            return SimpleIterator { m_container, m_index + delta };
+        constexpr SimpleIterator operator+(ptrdiff_t delta) const 
+        { 
+            return SimpleIterator { m_container, m_index + delta }; 
         }
 
         /**
-         * @brief -
-         * 
          * @param delta 
          * @return constexpr SimpleIterator 
          */
-        constexpr SimpleIterator operator-(ptrdiff_t delta) const { 
+        constexpr SimpleIterator operator-(ptrdiff_t delta) const 
+        { 
             return SimpleIterator { m_container, m_index - delta }; 
         }
 
         /**
-         * @brief -
-         * 
          * @param other 
          * @return constexpr ptrdiff_t 
          */
-        constexpr ptrdiff_t operator-(SimpleIterator other) const { 
+        constexpr ptrdiff_t operator-(SimpleIterator other) const 
+        { 
             return static_cast<ptrdiff_t>(m_index) - other.m_index; 
         }
-        
+
         /**
-         * @brief ++
-         * 
+
          * @return constexpr SimpleIterator 
          */
-        constexpr SimpleIterator operator++() {
+        constexpr SimpleIterator operator++()
+        {
             ++m_index;
             return *this;
         }
 
         /**
-         * @brief ++
-         * 
          * @return constexpr SimpleIterator 
          */
-        constexpr SimpleIterator operator++(int) {
+        constexpr SimpleIterator operator++(int)
+        {
             ++m_index;
             return SimpleIterator { m_container, m_index - 1 };
-        }
+        }   
 
         /**
-         * @brief --
-         * 
          * @return constexpr SimpleIterator 
          */
-        constexpr SimpleIterator operator--() {
+        constexpr SimpleIterator operator--()
+        {
             --m_index;
             return *this;
         }
 
         /**
-         * @brief --
-         * 
          * @return constexpr SimpleIterator 
          */
-        constexpr SimpleIterator operator--(int) {
+        constexpr SimpleIterator operator--(int)
+        {
             --m_index;
             return SimpleIterator { m_container, m_index + 1 };
         }
 
         /**
-         * @brief *
-         * 
          * @return ALWAYS_INLINE constexpr const& 
          */
-        ALWAYS_INLINE constexpr const ValueType& operator*() const { return m_container[m_index]; }
-        ALWAYS_INLINE constexpr ValueType& operator*() { return m_container[m_index]; }
+        ALWAYS_INLINE constexpr ValueType const& operator*() const 
+        { 
+            return m_container[m_index]; 
+        }
 
         /**
-         * @brief ->
-         * 
-         * @return constexpr const ValueType* 
+         * @return ALWAYS_INLINE constexpr& 
          */
-        constexpr const ValueType* operator->() const { return &m_container[m_index]; }
-        constexpr ValueType* operator->() { return &m_container[m_index]; }
+        ALWAYS_INLINE constexpr ValueType& operator*() 
+        { 
+            return m_container[m_index]; 
+        }
+
+        /**
+         * @return ALWAYS_INLINE constexpr const* 
+         */
+        ALWAYS_INLINE constexpr ValueType const* operator->() const 
+        { 
+            return &m_container[m_index]; 
+        }
+
+        /**
+         * @return ALWAYS_INLINE constexpr* 
+         */
+        ALWAYS_INLINE constexpr ValueType* operator->() 
+        { 
+            return &m_container[m_index];
+        }
+
+        /**
+         * @param other 
+         * @return SimpleIterator& 
+         */
+        SimpleIterator& operator=(const SimpleIterator& other)
+        {
+            m_index = other.m_index;
+            return *this;
+        }
+
+        /**
+         * @param obj 
+         */
+        SimpleIterator(const SimpleIterator& obj) = default;
 
     private:
-        /**
-         * @brief begin
-         * 
-         * @param container 
-         * @return constexpr SimpleIterator 
-         */
-        static constexpr SimpleIterator begin(Container& container) { 
-            return { container, 0 }; 
-        }
-        
-        /**
-         * @brief end
-         * 
-         * @param container 
-         * @return constexpr SimpleIterator 
-         */
-        static constexpr SimpleIterator end(Container& container) {
-            using RawContainerType = typename RemoveCV<Container>::Type;
 
-            if constexpr (IsSame<StringView, RawContainerType>::value || IsSame<String, RawContainerType>::value)
+        /**
+         * @param container 
+         * @return constexpr SimpleIterator 
+         */
+        static constexpr SimpleIterator begin(Container& container) { return { container, 0 }; }
+
+        /**
+         * @param container 
+         * @return constexpr SimpleIterator 
+         */
+        static constexpr SimpleIterator end(Container& container)
+        {
+            using RawContainerType = RemoveCV<Container>;
+
+            if constexpr (IsSame<StringView, RawContainerType> || IsSame<String, RawContainerType>)
                 return { container, container.length() };
             else
                 return { container, container.size() };
         }
 
-        /**
-         * @brief simpleiterator
-         * 
-         */
         constexpr SimpleIterator(Container& container, size_t index)
             : m_container(container)
             , m_index(index)
-        {
-        }
+        { }
 
         Container& m_container;
         size_t m_index;
-    };
+    }; // class SimpleIterator
 
-}
+} // namespace Mods
