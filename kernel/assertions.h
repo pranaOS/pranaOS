f76362ec2f85dc21cd11cb37e0b34b0519c8c9c1
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-08-25 12:33:28 +0530

    kernel: assertions

--- kernel/assertions.h
+++ kernel/assertions.h
@@ -11,36 +11,33 @@
 
 #pragma once
 
+#include <mods/platform.h>
+
 #define __STRINGIFY_HELPER(x) #x
 #define __STRINGIFY(x) __STRINGIFY_HELPER(x)
 
-#ifdef DEBUG
-
-/**
- * @brief assertion_failed
- * 
- * @param msg 
- * @param file 
- * @param line 
- * @param func 
- */
 [[noreturn]] void __assertion_failed(const char* msg, const char* file, unsigned line, const char* func);
-#    define ASSERT(expr) (static_cast<bool>(expr) ? (void)0 : __assertion_failed(#    expr, __FILE__, __LINE__, __PRETTY_FUNCTION__))
-#    define ASSERT_NOT_REACHED() ASSERT(false)
-#else
-#    define ASSERT(expr)
-#    define ASSERT_NOT_REACHED() CRASH()
-#endif
-#define CRASH()              \
-    do {                     \
-        asm volatile("ud2"); \
-    } while (0)
-#define RELEASE_ASSERT(x) \
-    do {                  \
-        if (!(x))         \
-            CRASH();      \
+#define VERIFY(expr)                                                            \
+    do {                                                                        \
+        if (!static_cast<bool>(expr)) [[unlikely]]                              \
+            __assertion_failed(#expr, __FILE__, __LINE__, __PRETTY_FUNCTION__); \
     } while (0)
 
-#define ASSERT_INTERRUPTS_DISABLED() ASSERT(!(cpu_flags() & 0x200))
-#define ASSERT_INTERRUPTS_ENABLED() ASSERT(cpu_flags() & 0x200)
-#define TODO ASSERT_NOT_REACHED
+#define VERIFY_NOT_REACHED() VERIFY(false)
+
+extern "C" 
+{
+    [[noreturn]] void _abort();
+    [[noreturn]] void abort();
+}
+
+static constexpr bool TODO = false;
+#define TODO() VERIFY(TODO)
+
+#if ARCH(I386) || ARCH(X86_64)
+#    define VERIFY_INTERRUPTS_DISABLED() VERIFY(!(cpu_flags() & 0x200))
+#    define VERIFY_INTERRUPTS_ENABLED() VERIFY(cpu_flags() & 0x200)
+#else
+#    define VERIFY_INTERRUPTS_DISABLED() TODO()
+#    define VERIFY_INTERRUPTS_ENABLED() TODO()
+#endif
