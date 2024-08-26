33ae5bc9c728ef61bdeccb7eef14429eee4db1bd
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-08-26 12:41:51 +0530

    libraries: libc[bits(wchar)]

---
+++ libraries/libc/bits/wchar.h
@@ -0,0 +1,15 @@
+/**
+ * @file wchar.h
+ * @author Krisna Pranav
+ * @brief wchar
+ * @version 6.0
+ * @date 2024-08-26
+ * 
+ * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#define MB_CUR_MAX 4
+#define MB_LEN_MAX 16
