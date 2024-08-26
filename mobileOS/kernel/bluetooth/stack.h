4da5468c94c1416833114f4af406437cccf9b957
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-08-26 13:03:42 +0530

    mobileOS: kernel[bluetooth(stack)]

---
+++ mobileOS/kernel/bluetooth/stack.h
@@ -0,0 +1,20 @@
+/**
+ * @file stack.hpp
+ * @author Krisna Pranav
+ * @brief Stack
+ * @version 6.0
+ * @date 2023-11-02
+ *
+ * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ *
+ */
+
+#pragma once
+
+struct Stack
+{
+	int id;
+
+	Stack() : id (0)
+	{};
+}; // struct Stack
