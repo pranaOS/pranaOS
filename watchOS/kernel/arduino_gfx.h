1b5c8f8f8e08a2e8a5fcf2395b421a4ac81ba89c
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-08-26 16:47:19 +0530

    watchOS: kernel[arduino_gfx]

---
+++ watchOS/kernel/arduino_gfx.h
@@ -0,0 +1,18 @@
+/**
+ * @file arduino_gfx.h
+ * @author Krisna Pranav
+ * @brief Arduino_GFX
+ * @version 6.0
+ * @date 2023-06-27
+ *
+ * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ *
+ */
+
+#pragma once
+
+#include "arduino_g.h"
+
+class Graphics2DPrint;
+
+#define GFX_NOT_DEFINED -1
