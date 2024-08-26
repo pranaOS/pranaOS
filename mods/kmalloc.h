1890c696d1232444cb6612eced15bc65003f55ac
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-08-26 12:53:50 +0530

    mods: kmalloc

--- mods/kmalloc.h
+++ mods/kmalloc.h
@@ -9,64 +9,72 @@
  * 
  */
 
+
 #pragma once
 
-#ifndef __pranaos__
-#    include <new>
-#endif
-
-#ifdef KERNEL
-#    define MOD_MAKE_ETERNAL                                               \
-    public:                                                               \
-        void* operator new(size_t size) { return kmalloc_eternal(size); } \
-                                                                          \
-    private:
-#else
-#    define MOD_MAKE_ETERNAL
-#endif
+#include <mods/check.h>
 
 #if defined(KERNEL)
 #    include <kernel/heap/kmalloc.h>
 #else
+#    include <new>
 #    include <stdlib.h>
 
-#    define kcalloc calloc
 #    define kmalloc malloc
-#    define kfree free
-#    define krealloc realloc
-
-#    ifdef __pranaos__
-
-#        include <new>
+#    define kmalloc_good_size malloc_good_size
 
 /**
- * @param size 
+ * @param ptr 
+ */
+inline void kfree_sized(void* ptr, size_t)
+{
+    free(ptr);
+}
+#endif
+
+#ifndef __pranaos__
+#    include <mods/types.h>
+
+#    ifndef MODS_OS_MACOS
+extern "C" 
+{
+    /**
+     * @param size 
+     * @return size_t 
+     */
+    inline size_t malloc_good_size(size_t size) 
+    { 
+        return size; 
+    }
+}
+#    else
+#        include <malloc/malloc.h>
+#    endif
+#endif
+
+using std::nothrow;
+
+/**
+ * @param a 
+ * @param b 
  * @return void* 
  */
-inline void* operator new(size_t size) {
-    return kmalloc(size);
+inline void* kmalloc_array(Checked<size_t> a, Checked<size_t> b)
+{
+    auto size = a * b;
+    VERIFY(!size.has_overflow());
+    return kmalloc(size.value());
 }
 
-inline void operator delete(void* ptr) {
-    return kfree(ptr);
+/**
+ * @param a 
+ * @param b 
+ * @param c 
+ * @return void* 
+ */
+inline void* kmalloc_array(Checked<size_t> a, Checked<size_t> b, Checked<size_t> c)
+{
+    auto size = a * b * c;
+    VERIFY(!size.has_overflow());
+    return kmalloc(size.value());
 }
-
-inline void operator delete(void* ptr, size_t) {
-    return kfree(ptr);
-}
-
-inline void* operator new[](size_t size) {
-    return kmalloc(size);
-}
-
-inline void operator delete[](void* ptr) {
-    return kfree(ptr);
-}
-
-inline void operator delete[](void* ptr, size_t) {
-    return kfree(ptr);
-}
-
-#    endif
-
-#endif
