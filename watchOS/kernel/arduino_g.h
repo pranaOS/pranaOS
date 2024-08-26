6bf96f1def68b98dfd20432d93ee4f3800b9b7b9
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-08-26 16:44:00 +0530

    watchOS: kernel[arduino_g]

---
+++ watchOS/kernel/arduino_g.h
@@ -0,0 +1,22 @@
+/**
+ * @file arduino_g.h
+ * @author Krisna Pranav
+ * @brief Arduino_G
+ * @version 6.0
+ * @date 2023-06-27
+ *
+ * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ *
+ */
+
+#pragma once
+
+#include "types.h"
+
+class ArduinoG
+{
+	protected:
+		int16_t,
+		WIDTH,
+		HEIGHT;
+}; // class ArduinoG
