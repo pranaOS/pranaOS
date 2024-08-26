822e6f793c430c20a3696c0c68386eb08c31a308
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-08-26 16:45:28 +0530

    watchOS: kernel[arduino_g]

--- watchOS/kernel/arduino_g.h
+++ watchOS/kernel/arduino_g.h
@@ -15,8 +15,31 @@
 
 class ArduinoG
 {
+	public:
+		/**
+		 * @param w 
+		 * @param h 
+		 */
+		ArduinoG(int16_t w, int16_t h) : WIDTH(w), HEIGHT(h) {};
+
+		/**
+		 * @param speed 
+		 */
+		virtual void begin(int32_t speed = 0) = 0;
+
+		/**
+		 * @param x 
+		 * @param y 
+		 * @param bitmap 
+		 * @param w 
+		 * @param h 
+		 * @param color 
+		 * @param bg 
+		 */
+	    virtual void drawBitmap(int16_t x, int16_t y, uint8_t* bitmap, int16_t w, int16_t h, uint16_t color, uint16_t bg) = 0;
+
 	protected:
-		int16_t,
+		int16_t
 		WIDTH,
 		HEIGHT;
 }; // class ArduinoG
