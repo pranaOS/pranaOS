24c5c418bef6ff7b001bc9a9bf2edd49153d7629
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-08-26 16:38:50 +0530

    watchOS: kernel[defines]

---
+++ watchOS/kernel/defines.h
@@ -0,0 +1,25 @@
+/**
+ * @file defines.h
+ * @author Krisna Pranav
+ * @brief Defines
+ * @version 6.0
+ * @date 2023-06-27
+ *
+ * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ *
+ */
+
+
+#pragma once
+
+#define PI 3.1415926535897932384626433832795
+#define HALF_PI 1.5707963267948966192313216916398
+#define TWO_PI 6.283185307179586476925286766559
+#define DEG_TO_RAD 0.017453292519943295769236907684886
+#define RAD_TO_DEG 57.295779513082320876798154814105
+#define EULER 2.718281828459045235360287471352
+
+#define DEC 10
+#define HEX 16
+#define OCT 8
+#define BIN 2
