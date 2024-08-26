8b8ee8d26f57f345317eeec2630565b888dbec0a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-08-26 16:46:04 +0530

    watchOS: kernel[arduino_g]

--- watchOS/kernel/arduino_g.h
+++ watchOS/kernel/arduino_g.h
@@ -38,6 +38,43 @@ class ArduinoG
 		 */
 	    virtual void drawBitmap(int16_t x, int16_t y, uint8_t* bitmap, int16_t w, int16_t h, uint16_t color, uint16_t bg) = 0;
 
+		/**
+		 * @param x 
+		 * @param y 
+		 * @param bitmap 
+		 * @param color_index 
+		 * @param w 
+		 * @param h 
+		 */
+	    virtual void drawIndexedBitmap(int16_t x, int16_t y, uint8_t* bitmap, uint16_t* color_index, int16_t w, int16_t h) = 0;
+
+		/**
+		 * @param x 
+		 * @param y 
+		 * @param bitmap 
+		 * @param w 
+		 * @param h 
+		 */
+	    virtual void draw3bitRGBBitmap(int16_t x, int16_t y, uint8_t* bitmap, int16_t w, int16_t h) = 0;
+
+		/**
+		 * @param x 
+		 * @param y 
+		 * @param bitmap 
+		 * @param w 
+		 * @param h 
+		 */
+	    virtual void draw16bitRGBBitmap(int16_t x, int16_t y, uint16_t* bitmap, int16_t w, int16_t h) = 0;
+
+		/**
+		 * @param x 
+		 * @param y 
+		 * @param bitmap 
+		 * @param w 
+		 * @param h 
+		 */
+	    virtual void draw24bitRGBBitmap(int16_t x, int16_t y, uint8_t* bitmap, int16_t w, int16_t h) = 0;
+
 	protected:
 		int16_t
 		WIDTH,
