27ce4a3ec34c2a1b6a0a789ae74d16a520b6f792
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-08-26 16:59:24 +0530

    watchOS: kernel[try]

---
+++ watchOS/kernel/try.java
@@ -0,0 +1,23 @@
+/**
+ * @file try.java
+ * @author Krisna Pranav
+ * @brief TRY
+ * @version 6.0
+ * @date 2023-06-27
+ *
+ * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ *
+ */
+
+package kernel;
+
+import java.util.*;
+import java.sql.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Try extends JFrame {
+    static JFrame f;
+    static JLabel l;
+}
