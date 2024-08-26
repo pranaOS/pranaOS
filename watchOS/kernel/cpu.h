60cf5b494d1f6185bed1ba10c878729fe2b919a4
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-08-26 16:37:39 +0530

    watchOS: kernel[cpu]

---
+++ watchOS/kernel/cpu.h
@@ -0,0 +1,16 @@
+/**
+ * @file nvs_flash.h
+ * @author Krisna Pranav
+ * @brief Data Types
+ * @version 6.0
+ * @date 2023-06-27
+ *
+ * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ *
+ */
+
+#pragma once
+
+void setCpuFrequencyMhz(unsigned int);
+
+unsigned int getCpuFrequencyMhz();
