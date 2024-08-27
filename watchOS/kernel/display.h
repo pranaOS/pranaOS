3c7e65d4342a0468201ee58099147aa930680c8a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-08-27 18:52:29 +0530

    watchOS: kernel[display]

--- watchOS/kernel/display.h
+++ watchOS/kernel/display.h
@@ -27,4 +27,58 @@ public:
 
 	void displayOn();
 	void displayOff();
+
+	/**
+	 * @param speed 
+	 */
+	void begin(int32_t speed = 0) override;
+
+	/**
+	 * @param x 
+	 * @param y 
+	 * @param bitmap 
+	 * @param w 
+	 * @param h 
+	 * @param color 
+	 * @param bg 
+	 */
+    void drawBitmap(int16_t x, int16_t y, uint8_t* bitmap, int16_t w, int16_t h, uint16_t color, uint16_t bg) override;
+
+	/**
+	 * @param x 
+	 * @param y 
+	 * @param bitmap 
+	 * @param color_index 
+	 * @param w 
+	 * @param h 
+	 */
+    void drawIndexedBitmap(int16_t x, int16_t y, uint8_t* bitmap, uint16_t* color_index, int16_t w, int16_t h) override;
+
+	/**
+	 * @param x 
+	 * @param y 
+	 * @param bitmap 
+	 * @param w 
+	 * @param h 
+	 */
+    void draw3bitRGBBitmap(int16_t x, int16_t y, uint8_t* bitmap, int16_t w, int16_t h) override;
+
+	/**
+	 * @param x 
+	 * @param y 
+	 * @param bitmap 
+	 * @param w 
+	 * @param h 
+	 */
+    void draw16bitRGBBitmap(int16_t x, int16_t y, uint16_t* bitmap, int16_t w, int16_t h) override;
+
+	/**
+	 * @param x 
+	 * @param y 
+	 * @param bitmap 
+	 * @param w 
+	 * @param h 
+	 */
+    void draw24bitRGBBitmap(int16_t x, int16_t y, uint8_t* bitmap, int16_t w, int16_t h) override;
+
 }; // class FakeDisplay
