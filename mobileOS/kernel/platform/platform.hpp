289cac2200e46bbb6f0298adfa43e7972ce3a794
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-08-28 19:23:35 +0530

    mobileOS: kernel[platform]

---
+++ mobileOS/kernel/platform/platform.hpp
@@ -0,0 +1,23 @@
+/**
+ * @file platform.hpp
+ * @author Krisna Pranav
+ * @brief platform
+ * @version 6.0
+ * @date 2024-08-27
+ *
+ * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ *
+ */
+
+#pragma once
+
+namespace platform
+{
+	class Platform
+	{
+	public:
+		virtual ~Platform() = default;
+		virtual void init() = 0;
+		virtual void deinit() = 0;
+	}; // class Platform
+} // namespace platform
