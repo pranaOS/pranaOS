27c3f1bdf70478b08931e0b33a0ceaacdf8ef1a1
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-08-29 19:27:50 +0530

    libraries: libipc[forward]

---
+++ libraries/libipc/forward.h
@@ -0,0 +1,22 @@
+/**
+ * @file forward.h
+ * @author Krisna Pranav
+ * @brief forward
+ * @version 6.0
+ * @date 2024-08-29
+ * 
+ * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+namespace IPC
+{
+    class Decoder;
+    class Dictionary;
+    class Encoder;
+    class Message;
+    class File;
+    class Stub;
+} // namespace IPC
