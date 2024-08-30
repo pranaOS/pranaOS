895d2d0999bc1c4ca2ca6e87ebc901b4ea39d055
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-08-30 14:54:38 +0530

    libraries: libipc[stub]

---
+++ libraries/libipc/stub.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file stub.cpp
+ * @author Krisna Pranav
+ * @brief STUB
+ * @version 6.0
+ * @date 2024-08-30
+ * 
+ * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include <libipc/stub.h>
+
+namespace IPC 
+{  
+    /**
+     * @brief Construct a new Stub::Stub object
+     * 
+     */
+    Stub::Stub()
+    { 
+    }
+
+    /**
+     * @brief Destroy the Stub::Stub object
+     * 
+     */
+    Stub::~Stub()
+    { 
+    }
+} // namespace IPC
