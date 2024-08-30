113ee0201bca366d7684f99f4db406a81f619460
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-08-30 14:50:29 +0530

    libraries: libipc[stub]

---
+++ libraries/libipc/stub.h
@@ -0,0 +1,26 @@
+/**
+ * @file stub.h
+ * @author Krisna Pranav
+ * @brief STUB
+ * @version 6.0
+ * @date 2024-08-30
+ * 
+ * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <mods/ownptr.h>
+#include <mods/string.h>
+
+namespace Mods
+{
+    class BufferStream;
+} // namespace Mods
+
+namespace IPC
+{
+    class Message;
+    struct MessageBuffer;
+} // namespace IPC
