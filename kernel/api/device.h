2307a6e21ed4a824781ade7853206e352c8d3c08
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-09-02 18:40:58 +0530

    kernel: api[device]

---
+++ kernel/api/device.h
@@ -0,0 +1,11 @@
+/**
+ * @file device.h
+ * @author Krisna Pranav
+ * @brief DEVICE
+ * @version 6.0
+ * @date 2023-07-07
+ *
+ * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ *
+ */
+
