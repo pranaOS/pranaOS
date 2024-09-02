b30f5a13acdf5d1ead5414c2654c62cc304bd1b9
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-09-02 18:48:48 +0530

    kernel: api[device]

--- kernel/api/device.h
+++ kernel/api/device.h
@@ -9,3 +9,40 @@
  *
  */
 
+#pragma once
+
+#include <mods/platform.h>
+#include <sys/cdefs.h>
+#include <sys/types.h>
+
+__BEGIN_DECLS
+
+/**
+ * @param major 
+ * @param minor 
+ * @return ALWAYS_INLINE 
+ */
+ALWAYS_INLINE dev_t pranaos_dev_makedev(unsigned major, unsigned minor)
+{
+    return (minor & 0xffu) | (major << 8u) | ((minor & ~0xffu) << 12u);
+}
+
+/**
+ * @param dev 
+ * @return ALWAYS_INLINE 
+ */
+ALWAYS_INLINE unsigned int pranaos_dev_major(dev_t dev)
+{
+    return (dev & 0xfff00u) >> 8u;
+}
+
+/**
+ * @param dev 
+ * @return ALWAYS_INLINE 
+ */
+ALWAYS_INLINE unsigned int pranaos_dev_minor(dev_t dev)
+{
+    return (dev & 0xffu) | ((dev >> 12u) & 0xfff00u);
+}
+
+__END_DECLS
