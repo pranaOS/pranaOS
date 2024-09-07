55867a9d096414181e006b7ab7663c93f25e06b3
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-09-07 12:07:50 +0530

    kernel: api[posix(stdio)]

---
+++ kernel/api/posix/stdio.h
@@ -0,0 +1,26 @@
+/**
+ * @file stdio.h
+ * @author Krisna Pranav
+ * @brief stdio
+ * @version 6.0
+ * @date 2024-09-07
+ * 
+ * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <kernel/api/posix/sys/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define SEEK_SET 0
+#define SEEK_CUR 1
+#define SEEK_END 2
+
+#ifdef __cplusplus
+}
+#endif
