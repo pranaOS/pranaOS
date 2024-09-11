dffb82acf1f84e95a955fc34717e1be1f3436057
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-09-11 18:53:54 +0530

    kernel: api[memorylayout]

---
+++ kernel/api/memorylayout.h
@@ -0,0 +1,17 @@
+/**
+ * @file memorylayout.h
+ * @author Krisna Pranav
+ * @brief Memory Layout
+ * @version 6.0
+ * @date 2024-09-11
+ * 
+ * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <mods/types.h>
+#include <kernel/sections.h>
+
+constexpr FlatPtr userspace_range_base = USER_RANGE_BASE;
