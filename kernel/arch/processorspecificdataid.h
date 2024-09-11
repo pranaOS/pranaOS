3b4cb9ac6677bfaec98292252e8fbab06b3fd23c
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-09-11 18:58:42 +0530

    kernel: arch[processorspecificdataid]

---
+++ kernel/arch/processorspecificdataid.h
@@ -0,0 +1,22 @@
+/**
+ * @file processorspecificdataid.h
+ * @author Krisna Pranav
+ * @brief Processor Specific Data ID
+ * @version 6.0
+ * @date 2024-09-11
+ * 
+ * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+namespace Kernel 
+{
+
+    enum class ProcessorSpecificDataID 
+    {
+        MemoryManager,
+        __Count,
+    };
+} // namespace Kernel
