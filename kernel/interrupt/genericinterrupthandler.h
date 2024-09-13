3096c9ca07b8abcfbd3c29eff4b50fc6489fe657
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-09-13 20:09:47 +0530

    kernel: interrupt[genericinterrupthandler]

--- kernel/interrupt/genericinterrupthandler.h
+++ kernel/interrupt/genericinterrupthandler.h
@@ -9,23 +9,21 @@
  * 
  */
 
-#pragma once 
+#pragma once
 
-#include <mods/hashtable.h>
-#include <mods/string.h>
+#include <mods/intrusivelist.h>
 #include <mods/types.h>
-#include <kernel/arch/i386/cpu.h>
+#include <kernel/arch/registerstate.h>
 
 namespace Kernel 
 {
-
     enum class HandlerType : u8 
     {
         IRQHandler = 1,
         SharedIRQHandler = 2,
         UnhandledInterruptHandler = 3,
         SpuriousInterruptHandler = 4
-    }; // enum
+    }; // enum class HandlerType : u8
 
     class GenericInterruptHandler 
     {
@@ -36,13 +34,31 @@ namespace Kernel
          */
         static GenericInterruptHandler& from(u8 interrupt_number);
 
-        /// @brief Destroy the Generic Interrupt Handler object
-        virtual ~GenericInterruptHandler();
-
+        /**
+         * @brief Destroy the Generic Interrupt Handler object
+         * 
+         */
+        virtual ~GenericInterruptHandler()
+        {
+            VERIFY(!m_registered);
+        }
+        
         /**
          * @param regs 
+         * @return true 
+         * @return false 
          */
-        virtual void handle_interrupt(const RegisterState& regs) = 0;
+        virtual bool handle_interrupt(const RegisterState& regs) = 0;
+
+        void will_be_destroyed();
+        
+        bool is_registered() const 
+        { 
+            return m_registered; 
+        }
+
+        void register_interrupt_handler();
+        void unregister_interrupt_handler();
 
         /**
          * @return u8 
@@ -53,22 +69,19 @@ namespace Kernel
         }
 
         /**
+         * @brief Get the invoking count object
+         * 
          * @return size_t 
          */
         size_t get_invoking_count() const 
         { 
-            return m_invoking_count.load(Mods::MemoryOrder::memory_order_relaxed); 
+            return m_invoking_count; 
         }
 
         /**
          * @return size_t 
          */
         virtual size_t sharing_devices_count() const = 0;
-
-        /**
-         * @return true 
-         * @return false 
-         */
         virtual bool is_shared_handler() const = 0;
         virtual bool is_sharing_with_others() const = 0;
 
@@ -76,12 +89,8 @@ namespace Kernel
          * @return HandlerType 
          */
         virtual HandlerType type() const = 0;
-
-        /**
-         * @return const char* 
-         */
-        virtual const char* purpose() const = 0;
-        virtual const char* controller() const = 0;
+        virtual StringView purpose() const = 0;
+        virtual StringView controller() const = 0;
 
         virtual bool eoi() = 0;
 
@@ -90,34 +99,34 @@ namespace Kernel
          */
         ALWAYS_INLINE void increment_invoking_counter()
         {
-            m_invoking_count.fetch_add(1, Mods::MemoryOrder::memory_order_relaxed);
+            m_invoking_count++;
         }
 
     protected:
-
         /**
          * @param number 
          */
         void change_interrupt_number(u8 number);
 
         /**
+         * @brief Construct a new Generic Interrupt Handler object
+         * 
          * @param interrupt_number 
          * @param disable_remap 
          */
         GenericInterruptHandler(u8 interrupt_number, bool disable_remap = false);
 
-        /// @brief: disable_remap
-        void disable_remap() 
-        { 
-            m_disable_remap = true; 
-        }
+        void disable_remap() { m_disable_remap = true; }
 
     private:
-
-        Atomic<u32> m_invoking_count { 0 };
+        Atomic<u32, Mods::MemoryOrder::memory_order_relaxed> m_invoking_count { 0 };
         u8 m_interrupt_number { 0 };
         bool m_disable_remap { false };
+        bool m_registered { false };
 
-    }; // class GenericInterruptHandler
+        IntrusiveListNode<GenericInterruptHandler> m_list_node;
 
+    public:
+        using List = IntrusiveList<&GenericInterruptHandler::m_list_node>;
+    }; // class GenericInterruptHandler 
 } // namespace Kernel
