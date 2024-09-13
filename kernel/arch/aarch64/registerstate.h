1bff8893c638f3b0280268a83f43b63183c719d2
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-09-13 20:09:31 +0530

    kernel: aarch64[registerstate]

---
+++ kernel/arch/aarch64/registerstate.h
@@ -0,0 +1,20 @@
+/**
+ * @file registerstate.h
+ * @author Krisna Pranav
+ * @brief Register State
+ * @version 6.0
+ * @date 2024-09-13
+ * 
+ * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+struct RegisterState
+{
+};
+
+struct DebugRegisterState 
+{
+};
