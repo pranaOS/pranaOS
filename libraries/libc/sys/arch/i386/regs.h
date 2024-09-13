2c312d8fbd6d3de3b891ca19ff7812bc94810016
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-09-13 20:00:17 +0530

    libraries: libc[sys(arch regs)]

--- libraries/libc/sys/arch/i386/regs.h
+++ libraries/libc/sys/arch/i386/regs.h
@@ -11,27 +11,89 @@
 
 #pragma once
 
-#include <mods/types.h>
+#include <mods/platform.h>
 
-/**
- * @brief PTraceRegisters
- * 
- */
-struct [[gnu::packed]] PtraceRegisters {
-    u32 eax;
-    u32 ecx;
-    u32 edx;
-    u32 ebx;
-    u32 esp;
-    u32 ebp;
-    u32 esi;
-    u32 edi;
-    u32 eip;
-    u32 eflags;
-    u32 cs;
-    u32 ss;
-    u32 ds;
-    u32 es;
-    u32 fs;
-    u32 gs;
-};
+#if defined(__cplusplus) && defined(__cpp_concepts)
+#    include <mods/types.h>
+#else
+#    include <sys/types.h>
+#endif
+
+struct [[gnu::packed]] PtraceRegisters 
+{
+#if ARCH(I386)
+    uint32_t eax;
+    uint32_t ecx;
+    uint32_t edx;
+    uint32_t ebx;
+    uint32_t esp;
+    uint32_t ebp;
+    uint32_t esi;
+    uint32_t edi;
+    uint32_t eip;
+    uint32_t eflags;
+#else
+    uint64_t rax;
+    uint64_t rcx;
+    uint64_t rdx;
+    uint64_t rbx;
+    uint64_t rsp;
+    uint64_t rbp;
+    uint64_t rsi;
+    uint64_t rdi;
+    uint64_t rip;
+    uint64_t r8;
+    uint64_t r9;
+    uint64_t r10;
+    uint64_t r11;
+    uint64_t r12;
+    uint64_t r13;
+    uint64_t r14;
+    uint64_t r15;
+    uint64_t rflags;
+#endif
+    uint32_t cs;
+    uint32_t ss;
+    uint32_t ds;
+    uint32_t es;
+    uint32_t fs;
+    uint32_t gs;
+
+#if defined(__cplusplus) && defined(__cpp_concepts)
+    FlatPtr ip() const
+    {
+#    if ARCH(I386)
+        return eip;
+#    else
+        return rip;
+#    endif
+    }
+
+    void set_ip(FlatPtr ip)
+    {
+#    if ARCH(I386)
+        eip = ip;
+#    else
+        rip = ip;
+#    endif
+    }
+
+    FlatPtr bp() const
+    {
+#    if ARCH(I386)
+        return ebp;
+#    else
+        return rbp;
+#    endif
+    }
+
+    void set_bp(FlatPtr bp)
+    {
+#    if ARCH(I386)
+        ebp = bp;
+#    else
+        rbp = bp;
+#    endif
+    }
+#endif
+}; // struct [[gnu::packed]] PtraceRegisters 
