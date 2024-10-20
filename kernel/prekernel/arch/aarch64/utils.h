af4453272087ecd1638694fdf954cf29382d21a9
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-10-20 20:52:03 +0530

    kernel: prekernel[aarch64(utils)]

---
+++ kernel/prekernel/arch/aarch64/utils.h
@@ -0,0 +1,25 @@
+/**
+ * @file utils.h
+ * @author Krisna Pranav
+ * @brief utils
+ * @version 6.0
+ * @date 2024-10-20
+ * 
+ * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+namespace Prekernel 
+{
+    /**
+     * @param text 
+     */
+    void dbgln(const char* text);
+
+    /**
+     * @param text 
+     */
+    void warnln(const char* text);
+} // namespace Prekernel
