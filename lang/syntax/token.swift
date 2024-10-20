b82626a52d6b2c0723eb8ec8a9b11c48caba4414
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-10-20 19:51:55 +0530

    lang: syntax[token]

---
+++ lang/syntax/token.swift
@@ -0,0 +1,27 @@
+/**
+ * @file token.swift
+ * @author Krisna Pranav
+ * @brief Token
+ * @version 6.0
+ * @date 2024-10-20
+ *
+ * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ *
+ */
+
+struct Token {
+    enum Kind {
+        case associated(String)
+        case keyword(String)
+        case punctuation(String)
+    }
+
+    let caseName: String
+    let name: String
+    let kind: Kind
+
+    init(name: String, _ kind: Kind) {
+        self.kind = kind
+        self.name = name
+    }
+}
