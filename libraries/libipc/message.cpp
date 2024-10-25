d165c7d4ea6d95e3190e9b69f602d5b2d314da5a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-10-25 15:35:37 +0530

    libraries: libipc[message]

---
+++ libraries/libipc/message.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file message.cpp
+ * @author Krisna Pranav
+ * @brief Message
+ * @version 6.0
+ * @date 2024-10-25
+ * 
+ * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include <libipc/message.h>
+
+namespace IPC 
+{
+    /**
+     * @brief Construct a new Message::Message object
+     * 
+     */
+    Message::Message()
+    {
+    }
+
+    /**
+     * @brief Destroy the Message::Message object
+     * 
+     */
+    Message::~Message()
+    {
+    }
+} // namespace IPC
