b795975ad07e7c39ff0197ead63d2267d5de533c
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-10-25 15:28:32 +0530

    libraries: libcore[anonymousbuffer]

--- libraries/libcore/anonymousbuffer.h
+++ libraries/libcore/anonymousbuffer.h
@@ -18,7 +18,7 @@
 #include <mods/refptr.h>
 #include <mods/types.h>
 
-namespace Core
+namespace Core 
 {
     class AnonymousBufferImpl final : public RefCounted<AnonymousBufferImpl> 
     {
@@ -37,14 +37,14 @@ namespace Core
 
         int fd() const 
         { 
-            return m_fd; 
+            return m_fd;
         }
 
         size_t size() const 
         { 
             return m_size; 
         }
-
+        
         void* data() 
         { 
             return m_data; 
@@ -67,4 +67,93 @@ namespace Core
         size_t m_size { 0 };
         void* m_data { nullptr };
     }; // class AnonymousBufferImpl final : public RefCounted<AnonymousBufferImpl> 
+
+    class AnonymousBuffer 
+    {
+    public:
+        /**
+         * @brief Create a with size object
+         * 
+         * @return ErrorOr<AnonymousBuffer> 
+         */
+        static ErrorOr<AnonymousBuffer> create_with_size(size_t);
+
+        /**
+         * @brief Create a from anon fd object
+         * 
+         * @param fd 
+         * @return ErrorOr<AnonymousBuffer> 
+         */
+        static ErrorOr<AnonymousBuffer> create_from_anon_fd(int fd, size_t);
+
+        /**
+         * @brief Construct a new Anonymous Buffer object
+         * 
+         */
+        AnonymousBuffer() { }
+
+        bool is_valid() const 
+        { 
+            return m_impl; 
+        }
+
+        int fd() const 
+        { 
+            return m_impl ? m_impl->fd() : -1; 
+        }
+
+        size_t size() const 
+        { 
+            return m_impl ? m_impl->size() : 0; 
+        }
+
+        /**
+         * @tparam T 
+         * @return T* 
+         */
+        template<typename T>
+        T* data()
+        {
+            static_assert(IsVoid<T> || IsTrivial<T>);
+            if (!m_impl)
+                return nullptr;
+            return (T*)m_impl->data();
+        }
+
+        /**
+         * @tparam T 
+         * @return const T* 
+         */
+        template<typename T>
+        const T* data() const
+        {
+            static_assert(IsVoid<T> || IsTrivial<T>);
+            if (!m_impl)
+                return nullptr;
+            return (const T*)m_impl->data();
+        }
+
+    private:
+        /**
+         * @brief Construct a new Anonymous Buffer object
+         * 
+         */
+        explicit AnonymousBuffer(NonnullRefPtr<AnonymousBufferImpl>);
+
+        RefPtr<AnonymousBufferImpl> m_impl;
+    }; // class AnonymousBuffer 
 } // namespace Core
+
+namespace IPC 
+{
+    /**
+     * @return true 
+     * @return false 
+     */
+    bool encode(Encoder&, const Core::AnonymousBuffer&);
+
+    /**
+     * @return ErrorOr<void> 
+     */
+    ErrorOr<void> decode(Decoder&, Core::AnonymousBuffer&);
+} // namespace IPC
