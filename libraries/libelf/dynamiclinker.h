58b1ae9e813685d249d125905e8a755a1c95a48b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-10-26 19:09:47 +0530

    libraries: libelf[dynamiclinker]

---
+++ libraries/libelf/dynamiclinker.h
@@ -0,0 +1,52 @@
+/**
+ * @file dynamiclinker.h
+ * @author your name (you@domain.com)
+ * @brief Dynamic Linker
+ * @version 6.0
+ * @date 2024-10-26
+ * 
+ * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <mods/result.h>
+#include <mods/vector.h>
+#include <libelf/dynamicobject.h>
+
+namespace ELF 
+{
+    class DynamicLinker 
+    {
+    public:
+        /**
+         * @param symbol 
+         * @return Optional<DynamicObject::SymbolLookupResult> 
+         */
+        static Optional<DynamicObject::SymbolLookupResult> lookup_global_symbol(StringView symbol);
+
+        /**
+         * @param main_program_name 
+         * @param fd 
+         * @param is_secure 
+         * @param argc 
+         * @param argv 
+         * @param envp 
+         */
+        [[noreturn]] static void linker_main(String&& main_program_name, int fd, bool is_secure, int argc, char** argv, char** envp);
+
+    private:
+        /**
+         * @brief Construct a new Dynamic Linker object
+         * 
+         */
+        DynamicLinker() = delete;
+
+        /**
+         * @brief Destroy the Dynamic Linker object
+         * 
+         */
+        ~DynamicLinker() = delete;
+    }; // namespace DynamicLinker
+} // namespace ELF
