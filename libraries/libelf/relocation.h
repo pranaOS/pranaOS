f62a44335da5e7bcdc59ec6fe2f280650079d588
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-10-27 12:02:43 +0530

    libraries: libelf[relocation]

---
+++ libraries/libelf/relocation.h
@@ -0,0 +1,25 @@
+/**
+ * @file relocation.h
+ * @author Krisna Pranav
+ * @brief Relocation
+ * @version 6.0
+ * @date 2024-10-27
+ * 
+ * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <mods/concept.h>
+#include <kernel/virtual_address.h>
+
+namespace ELF 
+{
+    /**
+     * @param base_address 
+     * @return true 
+     * @return false 
+     */
+    bool perform_relative_relocations(FlatPtr base_address);
+} // namespace ELF
