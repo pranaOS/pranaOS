7cbced46f5d46a662b010c2d61d354d7fea7851f
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-10-27 13:31:28 +0530

    libraries: libtimezone[forward]

---
+++ libraries/libtimezone/forward.h
@@ -0,0 +1,20 @@
+/**
+ * @file forward.h
+ * @author Krisna Pranav
+ * @brief forward
+ * @version 6.0
+ * @date 2024-10-27
+ * 
+ * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <mods/types.h>
+
+namespace TimeZone 
+{
+    enum class DaylightSavingsRule : u8;
+    enum class TimeZone : u16;
+} // namespace TimeZone
