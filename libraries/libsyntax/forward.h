132bed7277a33d8986bb31d4b4c5d9a4843a952e
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-10-28 12:45:53 +0530

    libraries: libsyntax[forward]

---
+++ libraries/libsyntax/forward.h
@@ -0,0 +1,18 @@
+/**
+ * @file forward.h
+ * @author Krisna Pranav
+ * @brief forward
+ * @version 6.0
+ * @date 2024-10-28
+ *
+ * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ *
+ */
+
+#pragma once
+
+namespace Syntax
+{
+    class Highlighter;
+    class HighlighterClient;
+} // namespace Syntax
