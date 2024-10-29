30cf04aeebb2046cc1a2955e7db992de498a7955
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-10-29 18:42:12 +0530

    libraries: libgfx[orientation]

---
+++ libraries/libgfx/orientation.h
@@ -0,0 +1,23 @@
+/**
+ * @file orientation.h
+ * @author Krisna Pranav
+ * @brief 
+ * @version 0.1
+ * @date 2024-10-29
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#pragma once
+
+namespace Gfx 
+{
+    enum class Orientation 
+    {
+        Horizontal,
+        Vertical
+    }; // enum class Orientation
+} // namespace Gfx
+
+using Gfx::Orientation;
