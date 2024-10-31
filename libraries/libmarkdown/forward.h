80d58689f3cc686e66c62885d06925765779a4cf
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-10-31 11:36:04 +0530

    libraries: libmarkdown[forward]

---
+++ libraries/libmarkdown/forward.h
@@ -0,0 +1,28 @@
+/**
+ * @file forward.h
+ * @author Krisna Pranav
+ * @brief Forward
+ * @version 6.0
+ * @date 2024-10-31
+ * 
+ * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+namespace Markdown 
+{
+    class Block;
+    class Document;
+    class Text;
+    class BlockQuote;
+    class CodeBlock;
+    class ContainerBlock;
+    class Heading;
+    class HoriziontalRule;
+    class List;
+    class Paragraph;
+    class Table;
+    class Visitor;
+} // namespace Markdown
