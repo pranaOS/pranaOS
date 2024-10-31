a2b0612dc482ceefbc3473643014aca2be661895
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-10-31 11:42:36 +0530

    libraries: recursiondecision

---
+++ mods/recursiondecision.h
@@ -0,0 +1,24 @@
+/**
+ * @file recursiondecision.h
+ * @author Krisna Pranav
+ * @brief Recursion Decision
+ * @version 6.0
+ * @date 2024-10-31
+ * 
+ * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+namespace Mods 
+{
+    enum class RecursionDecision 
+    {
+        Recurse,
+        Continue,
+        Break,
+    }; // enum class RecursionDecision 
+} // namespace Mods
+
+using Mods::RecursionDecision;
