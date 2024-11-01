29022e5db942ede54c62b8d9c1a53e21ee517ab9
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-11-01 12:00:09 +0530

    libraries: libmarkdown[paragraph]

---
+++ libraries/libmarkdown/paragraph.h
@@ -0,0 +1,60 @@
+/**
+ * @file paragraph.h
+ * @author Krisna Pranav
+ * @brief Paragrah
+ * @version 6.0
+ * @date 2024-11-01
+ * 
+ * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <mods/nonnullownptrvector.h>
+#include <mods/ownptr.h>
+#include <libmarkdown/block.h>
+#include <libmarkdown/text.h>
+
+namespace Markdown 
+{
+    class Paragraph final : public Block 
+    {
+    public:
+        /**
+         * @brief Construct a new Paragraph object
+         * 
+         * @param text 
+         */
+        Paragraph(Text text)
+            : m_text(move(text))
+        {
+        }
+
+        /**
+         * @brief Destroy the Paragraph object
+         * 
+         */
+        virtual ~Paragraph() override { }
+
+        /**
+         * @param tight 
+         * @return String 
+         */
+        virtual String render_to_html(bool tight = false) const override;
+
+        /**
+         * @param view_width 
+         * @return String 
+         */
+        virtual String render_for_terminal(size_t view_width = 0) const override;
+
+        /**
+         * @return RecursionDecision 
+         */
+        virtual RecursionDecision walk(Visitor&) const override;
+
+    private:
+        Text m_text;
+    }; // class Paragraph final : public Block 
+} // namespace Markdown
