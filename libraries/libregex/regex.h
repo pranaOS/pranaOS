ebe48f6629722581952d7b5a68dac933c10f63ae
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-11-06 14:35:16 +0530

    libraries: libregex[regex]

---
+++ libraries/libregex/regex.h
@@ -0,0 +1,16 @@
+/**
+ * @file regex.h
+ * @author Krisna Pranav
+ * @brief Regex
+ * @version 6.0
+ * @date 2024-11-06
+ * 
+ * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <libregex/forward.h>
+#include <libregex/regexdebug.h>
+#include <libregex/regexmatcher.h>
