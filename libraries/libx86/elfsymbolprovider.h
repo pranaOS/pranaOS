6a0b34dc11def79c208a1459b81221c402775bae
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-11-07 12:17:06 +0530

    libraries: libx86[elfsymbolprovider]

---
+++ libraries/libx86/elfsymbolprovider.h
@@ -0,0 +1,48 @@
+/**
+ * @file elfsymbolprovider.h
+ * @author Krisna Pranav
+ * @brief ELF Symbol Provider
+ * @version 6.0
+ * @date 2024-11-07
+ * 
+ * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <libelf/image.h>
+#include <libx86/instruction.h>
+
+namespace X86 
+{
+    class ELFSymbolProvider final : public SymbolProvider 
+    {
+    public:
+        /**
+         * @brief Construct a new ELFSymbolProvider object
+         * 
+         * @param elf 
+         * @param base_address 
+         */
+        ELFSymbolProvider(const ELF::Image& elf, FlatPtr base_address = 0)
+            : m_elf(elf)
+            , m_base_address(base_address)
+        {
+        }
+
+        /**
+         * @param address 
+         * @param offset 
+         * @return String 
+         */
+        virtual String symbolicate(FlatPtr address, u32* offset = nullptr) const override
+        {
+            return m_elf.symbolicate(address - m_base_address, offset);
+        }
+
+    private:
+        const ELF::Image& m_elf;
+        FlatPtr m_base_address;
+    }; // class ELFSymbolProvider final : public SymbolProvider
+} // namespace X86
