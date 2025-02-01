efe9edf07ea88427d95057ab3e639bed82b91eaf
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-01 15:23:28 +0530

    mods: arbitrarysizedenum

--- mods/arbitrarysizedenum.h
+++ mods/arbitrarysizedenum.h
@@ -13,62 +13,193 @@
 
 #include <mods/distinctnums.h>
 
-namespace Mods
+namespace Mods 
 {
     /**
      * @tparam T 
      */
     template<typename T>
-    struct ArbitrarySizedEnum : public T
+    struct ArbitrarySizedEnum : public T 
     {
         using T::T;
 
+        /**
+         * @param v 
+         * @return consteval 
+         */
         consteval ArbitrarySizedEnum(T v)
             : T(v)
-        {}
+        {
+        }
 
         constexpr ArbitrarySizedEnum(T v, Badge<ArbitrarySizedEnum<T>>)
             : T(v)
-        {}
+        {
+        }
 
+        /**
+         * @tparam X 
+         * @param other 
+         * @return consteval 
+         */
         template<Integral X>
         [[nodiscard]] consteval ArbitrarySizedEnum<T> operator<<(X other) const
         {
             return T(this->value() << other);
+        }   
+
+        /**
+         * @tparam X 
+         * @param other 
+         * @return constexpr ArbitrarySizedEnum<T>& 
+         */
+        template<Integral X>
+        constexpr ArbitrarySizedEnum<T>& operator<<=(X other)
+        {
+            this->value() <<= other;
+            return *this;
         }
 
+        /**
+         * @tparam X 
+         * @param other 
+         * @return consteval 
+         */
+        template<Integral X>
+        [[nodiscard]] consteval ArbitrarySizedEnum<T> operator>>(X other) const
+        {
+            return T(this->value() >> other);
+        }
+
+        /**
+         * @tparam X 
+         * @param other 
+         * @return constexpr ArbitrarySizedEnum<T>& 
+         */
+        template<Integral X>
+        constexpr ArbitrarySizedEnum<T>& operator>>=(X other)
+        {
+            this->value() >>= other;
+            return *this;
+        }
+
+        /**
+         * @tparam X 
+         * @param other 
+         * @return true 
+         * @return false 
+         */
         template<Integral X>
         [[nodiscard]] constexpr bool operator==(X other) const
         {
             return this->value() == T(other);
         }
 
-        [[nodiscard]] constexpr bool operator==(ArbitrarySizedEnum<T> const& other)
+        /**
+         * @param other 
+         * @return true 
+         * @return false 
+         */
+        [[nodiscard]] constexpr bool operator==(ArbitrarySizedEnum<T> const& other) const
         {
             return this->value() == other.value();
         }
 
+        /**
+         * @param other 
+         * @return constexpr ArbitrarySizedEnum<T> 
+         */
+        [[nodiscard]] constexpr ArbitrarySizedEnum<T> operator|(ArbitrarySizedEnum<T> const& other) const
+        {
+            return { T(this->value() | other.value()), {} };
+        }
+
+        /**
+         * @param other 
+         * @return constexpr ArbitrarySizedEnum<T> 
+         */
+        [[nodiscard]] constexpr ArbitrarySizedEnum<T> operator&(ArbitrarySizedEnum<T> const& other) const
+        {
+            return { T(this->value() & other.value()), {} };
+        }
+
+        /**
+         * @param other 
+         * @return constexpr ArbitrarySizedEnum<T> 
+         */
+        [[nodiscard]] constexpr ArbitrarySizedEnum<T> operator^(ArbitrarySizedEnum<T> const& other) const
+        {
+            return { T(this->value() ^ other.value()), {} };
+        }
+
+        /**
+         * @return constexpr ArbitrarySizedEnum<T> 
+         */
+        [[nodiscard]] constexpr ArbitrarySizedEnum<T> operator~() const
+        {
+            return { T(~this->value()), {} };
+        }
+
+        /**
+         * @param other 
+         * @return constexpr ArbitrarySizedEnum<T>& 
+         */
         constexpr ArbitrarySizedEnum<T>& operator|=(ArbitrarySizedEnum<T> const& other)
         {
             this->value() |= other.value();
             return *this;
         }
-        
+
+        /**
+         * @param other 
+         * @return constexpr ArbitrarySizedEnum<T>& 
+         */
         constexpr ArbitrarySizedEnum<T>& operator&=(ArbitrarySizedEnum<T> const& other)
         {
             this->value() &= other.value();
             return *this;
         }
 
+        /**
+         * @param other 
+         * @return constexpr ArbitrarySizedEnum<T>& 
+         */
+        constexpr ArbitrarySizedEnum<T>& operator^=(ArbitrarySizedEnum<T> const& other)
+        {
+            this->value() ^= other.value();
+            return *this;
+        }
+
+        /**
+         * @param mask 
+         * @return true 
+         * @return false 
+         */
+        [[nodiscard]] constexpr bool has_flag(ArbitrarySizedEnum<T> const& mask) const
+        {
+            return (*this & mask) == mask;
+        }
+
+        /**
+         * @param mask 
+         * @return true 
+         * @return false 
+         */
+        [[nodiscard]] constexpr bool has_any_flag(ArbitrarySizedEnum<T> const& mask) const
+        {
+            return (*this & mask) != 0u;
+        }
     }; // struct ArbitrarySizedEnum : public T
 
-#define MOD_MAKE_ARBITRARY_SIZED_ENUM(EnumName, T, ...)                               \
-    namespace EnumName {                                                             \
-    using EnumName = ArbitrarySizedEnum<DistinctNumeric<T, struct __##EnumName##Tag, \
-        false, true, false, false, false, false>>;                                   \
-    using Type = EnumName;                                                           \
-    using UnderlyingType = T;                                                        \
-    inline constexpr static EnumName __VA_ARGS__;                                    \
+#define MODS_MAKE_ARBITRARY_SIZED_ENUM(EnumName, T, ...)                               \
+    namespace EnumName {                                                               \
+    using EnumName = ArbitrarySizedEnum<DistinctNumeric<T, struct __##EnumName##Tag,   \
+        false, true, false, false, false, false>>;                                     \
+    using Type = EnumName;                                                             \
+    using UnderlyingType = T;                                                          \
+    inline constexpr static EnumName __VA_ARGS__;                                      \
     }
 
-} // namespace Mods
+}
+
+using Mods::ArbitrarySizedEnum;
