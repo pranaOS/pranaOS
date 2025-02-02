62df2d4135a7f4410a4a7d0f3bac9e233478e99a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-02 12:43:20 +0530

    mods: binarysearch

--- mods/binarysearch.h
+++ mods/binarysearch.h
@@ -16,7 +16,7 @@
 
 namespace Mods
 {
-    struct DefaultComparator
+    struct DefaultComparator 
     {
         /**
          * @tparam T 
@@ -28,13 +28,63 @@ namespace Mods
         template<typename T, typename S>
         constexpr int operator()(T& lhs, S& rhs)
         {
-            if (lhs > rhs) 
+            if (lhs > rhs)
                 return 1;
-            
+
             if (lhs < rhs)
                 return -1;
-            
+
             return 0;
         }
-    }; // struct DefaultComparator
-} // namespace Mods
+    }; // struct DefaultComparator 
+
+    /**
+     * @tparam Container 
+     * @tparam Needle 
+     * @tparam Comparator 
+     * @param haystack 
+     * @param needle 
+     * @param nearby_index 
+     * @param comparator 
+     * @return decltype(&haystack[0]) 
+     */
+    template<typename Container, typename Needle, typename Comparator = DefaultComparator>
+    constexpr auto binary_search(Container&& haystack, Needle&& needle, size_t* nearby_index = nullptr, Comparator comparator = Comparator {}) -> decltype(&haystack[0])
+    {
+        if (haystack.size() == 0) {
+            if (nearby_index)
+                *nearby_index = 0;
+
+            return nullptr;
+        }
+
+        size_t low = 0;
+        size_t high = haystack.size() - 1;
+        
+        while (low <= high) {
+            size_t middle = low + (high - low) / 2;
+
+            int comparison = comparator(needle, haystack[middle]);
+
+            if (comparison < 0)
+                if (middle != 0)
+                    high = middle - 1;
+                else
+                    break;
+            else if (comparison > 0)
+                low = middle + 1;
+            else {
+                if (nearby_index)
+                    *nearby_index = middle;
+                return &haystack[middle];
+            }
+        }
+
+        if (nearby_index)
+            *nearby_index = min(low, high);
+
+        return nullptr;
+    } // constexpr auto binary_search(Container&& haystack, Needle&& needle, size_t* nearby_index = nullptr, Comparator comparator = Comparator {}) -> decltype(&haystack[0])
+}
+
+using Mods::binary_search;
