117699883f2e8a85b6e1574dd120ff51e587dbf7
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-02 11:37:30 +0530

    mods: assertions

--- mods/assertions.h
+++ mods/assertions.h
@@ -4,19 +4,19 @@
  * @brief Assertions
  * @version 6.0
  * @date 2023-06-28
- * 
+ *
  * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
- * 
+ *
  */
 
-#pragma once
+ #pragma once
 
-#if defined(KERNEL)
-#    include <kernel/assertions.h>
-#else
-#    include <assert.h>
-#    define VERIFY assert
-#    define VERIFY_NOT_REACHED() assert(false) 
-static constexpr bool TODO = false;
-#    define TODO() VERIFY(TODO)                
-#endif
+ #if defined(KERNEL)
+ #    include <kernel/assertions.h>
+ #else
+ #    include <assert.h>
+ #    define VERIFY assert
+ #    define VERIFY_NOT_REACHED() assert(false)
+ static constexpr bool TODO = false;
+ #    define TODO() VERIFY(TODO)
+ #endif
