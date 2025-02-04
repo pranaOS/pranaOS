d51f2a5b61b9b5debb5e0179a748e65d53f80961
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-04 18:48:10 +0530

    mods: dateconstants

---
+++ mods/dateconstants.h
@@ -0,0 +1,15 @@
+/**
+ * @file dateconstants.h
+ * @author Krisna Pranav
+ * @brief Date Constants
+ * @version 6.0
+ * @date 2025-02-04
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <mods/array.h>
+#include <mods/string_view.h>
