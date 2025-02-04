43b133bc48d798c389e34512886232a24ddb0cd2
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-04 16:19:40 +0530

    mods: buffered

---
+++ mods/buffered.h
@@ -0,0 +1,24 @@
+/**
+ * @file buffered.h
+ * @author Krisna Pranav
+ * @brief buffered
+ * @version 6.0
+ * @date 2025-02-04
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <mods/span.h>
+#include <mods/stdlibextra.h>
+#include <mods/stream.h>
+#include <mods/types.h>
+#include <mods/noncopyable.h>
+#include <mods/kmalloc.h>
+
+namespace Mods
+{
+
+} // namespace Mods
