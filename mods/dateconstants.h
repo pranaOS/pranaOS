86b2d84e43f703926c71eb5186047e6d390d2db5
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-04 18:49:09 +0530

    mods: dateconstants

--- mods/dateconstants.h
+++ mods/dateconstants.h
@@ -13,3 +13,39 @@
 
 #include <mods/array.h>
 #include <mods/string_view.h>
+
+namespace Mods
+{
+    static constexpr Array<StringView, 7> long_day_names = {
+        "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+    };
+
+    static constexpr Array<StringView, 7> short_day_names = {
+        "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"
+    };
+
+    static constexpr Array<StringView, 7> mini_day_names = {
+        "Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"
+    };
+
+    static constexpr Array<StringView, 7> micro_day_names = {
+        "S", "M", "T", "W", "T", "F", "S"
+    };
+
+    static constexpr Array<StringView, 12> long_month_names = {
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    };
+
+    static constexpr Array<StringView, 12> short_month_names = {
+        "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    };
+}
+
+using Mods::long_day_names;
+using Mods::long_month_names;
+using Mods::micro_day_names;
+using Mods::mini_day_names;
+using Mods::short_day_names;
+using Mods::short_month_names;
