01d9ee6d6e0103f0d64f0e299e1d328962c1941e
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-04 18:30:04 +0530

    mods: complex

---
+++ mods/complex.h
@@ -0,0 +1,10 @@
+/**
+ * @file complex.h
+ * @author Krisna Pranav
+ * @brief complex
+ * @version 6.0
+ * @date 2025-02-04
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
