afa2a2497710edb916c89c8f51775ac84f4e5629
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-04 16:15:30 +0530

    mods: bitstream

--- mods/bitstream.h
+++ mods/bitstream.h
@@ -17,7 +17,7 @@
 namespace Mods
 {
     class InputBitStream final : public InputStream 
-    {   
+    {
     public:
         /**
          * @brief Construct a new Input Bit Stream object
@@ -26,7 +26,8 @@ namespace Mods
          */
         explicit InputBitStream(InputStream& stream)
             : m_stream(stream)
-        {}
+        {
+        }
 
         /**
          * @param bytes 
@@ -38,26 +39,151 @@ namespace Mods
                 return 0;
 
             size_t nread = 0;
-
-            if(bytes.size() >= 1) {
+            
+            if (bytes.size() >= 1) {
                 if (m_next_byte.has_value()) {
                     bytes[0] = m_next_byte.value();
                     m_next_byte.clear();
+
                     ++nread;
                 }
             }
 
-            return neard + m_stream.read(bytes.slice(nread));
+            return nread + m_stream.read(bytes.slice(nread));
         }
 
+        /**
+         * @param bytes 
+         * @return true 
+         * @return false 
+         */
+        bool read_or_error(Bytes bytes) override
+        {
+            if (read(bytes) != bytes.size()) {
+                set_fatal_error();
+                return false;
+            }
+
+            return true;
+        }
+
+        /**
+         * @return true 
+         * @return false 
+         */
+        bool unreliable_eof() const override 
+        { 
+            return !m_next_byte.has_value() && m_stream.unreliable_eof(); 
+        }
+
+        /**
+         * @param count 
+         * @return true 
+         * @return false 
+         */
+        bool discard_or_error(size_t count) override
+        {
+            if (count >= 1) {
+                if (m_next_byte.has_value()) {
+                    m_next_byte.clear();
+                    --count;
+                }
+            }
+
+            return m_stream.discard_or_error(count);
+        }
+
+        /**
+         * @param count 
+         * @return u64 
+         */
+        u64 read_bits(size_t count)
+        {
+            u64 result = 0;
+            size_t nread = 0;
+
+            while (nread < count) {
+                if (m_stream.has_any_error()) {
+                    set_fatal_error();
+                    return 0;
+                }
+
+                if (m_next_byte.has_value()) {
+                    auto const bit = (m_next_byte.value() >> m_bit_offset) & 1;
+                    result |= bit << nread;
+                    ++nread;
+
+                    if (m_bit_offset++ == 7)
+                        m_next_byte.clear();
+                } else {
+                    m_stream >> m_next_byte;
+                    m_bit_offset = 0;
+                }
+            }
+
+            return result;
+        }
+
+        /**
+         * @param count 
+         * @return u64 
+         */
+        u64 read_bits_big_endian(size_t count)
+        {
+            u64 result = 0;
+            size_t nread = 0;
+
+            while (nread < count) {
+                if (m_stream.has_any_error()) {
+                    set_fatal_error();
+                    return 0;
+                }
+
+                if (m_next_byte.has_value()) {
+                    if (((count - nread) >= 8) && m_bit_offset == 0) {
+                        result <<= 8;
+                        result |= m_next_byte.value();
+                        nread += 8;
+                        m_next_byte.clear();
+                    } else {
+                        auto const bit = (m_next_byte.value() >> (7 - m_bit_offset)) & 1;
+                        result <<= 1;
+                        result |= bit;
+                        ++nread;
+                        if (m_bit_offset++ == 7)
+                            m_next_byte.clear();
+                    }
+                } else {
+                    m_stream >> m_next_byte;
+                    m_bit_offset = 0;
+                }
+            }
+
+            return result;
+        }
+
+        /**
+         * @return true 
+         * @return false 
+         */
         bool read_bit() 
-        {
-            return static_cast<bool>(read_bits(1));
+        { 
+            return static_cast<bool>(read_bits(1)); 
         }
 
+        /**
+         * @return true 
+         * @return false 
+         */
         bool read_bit_big_endian() 
+        { 
+            return static_cast<bool>(read_bits_big_endian(1)); 
+        }
+
+        void align_to_byte_boundary()
         {
-            return static_cast<bool>(read_bit_big_endian(1));
+            if (m_next_byte.has_value())
+                m_next_byte.clear();
         }
 
         bool handle_any_error() override
@@ -72,26 +198,41 @@ namespace Mods
         InputStream& m_stream;
     }; // class InputBitStream final : public InputStream
 
-    class OutputBitStream final : public OutputStream
+    class OutputBitStream final : public OutputStream 
     {
     public:
+        /**
+         * @brief Construct a new Output Bit Stream object
+         * 
+         * @param stream 
+         */
+        explicit OutputBitStream(OutputStream& stream)
+            : m_stream(stream)
+        {
+        }
+
         /**
          * @param bytes 
          * @return size_t 
          */
         size_t write(ReadonlyBytes bytes) override
         {
-            if (has_any_error()) 
+            if (has_any_error())
                 return 0;
-            
+
             align_to_byte_boundary();
 
             if (has_fatal_error()) 
                 return 0;
-            
+                
             return m_stream.write(bytes);
         }
 
+        /**
+         * @param bytes 
+         * @return true 
+         * @return false 
+         */
         bool write_or_error(ReadonlyBytes bytes) override
         {
             if (write(bytes) < bytes.size()) {
@@ -102,24 +243,80 @@ namespace Mods
             return true;
         }
 
-        void align_to_byte_boundary()
+        /**
+         * @param bits 
+         * @param count 
+         */
+        void write_bits(u32 bits, size_t count)
         {
-            if (m_next_byte.m_has_value()) {
-                if (!m_stream.write_or_error(ReadonlyBytes)) {
+            VERIFY(count <= 32);
+
+            if (count == 32 && !m_next_byte.has_value()) { 
+                m_stream << bits;
+                return;
+            }
+
+            size_t n_written = 0;
+
+            while (n_written < count) {
+                if (m_stream.has_any_error()) {
                     set_fatal_error();
+                    return;
                 }
 
+                if (m_next_byte.has_value()) {
+                    m_next_byte.value() |= ((bits >> n_written) & 1) << m_bit_offset;
+                    ++n_written;
+
+                    if (m_bit_offset++ == 7) {
+                        m_stream << m_next_byte.value();
+                        m_next_byte.clear();
+                    }
+                } else if (count - n_written >= 16) { // fast path for aligned 16 bit writes
+                    m_stream << (u16)((bits >> n_written) & 0xFFFF);
+                    n_written += 16;
+                } else if (count - n_written >= 8) { // fast path for aligned 8 bit writes
+                    m_stream << (u8)((bits >> n_written) & 0xFF);
+                    n_written += 8;
+                } else {
+                    m_bit_offset = 0;
+                    m_next_byte = 0;
+                }
+            }
+        }
+
+        /**
+         * @param bit 
+         */
+        void write_bit(bool bit)
+        {
+            write_bits(bit, 1);
+        }
+
+        void align_to_byte_boundary()
+        {
+            if (m_next_byte.has_value()) {
+                if (!m_stream.write_or_error(ReadonlyBytes { &m_next_byte.value(), 1 })) {
+                    set_fatal_error();
+                }
                 m_next_byte.clear();
             }
         }
 
+        /**
+         * @return size_t 
+         */
         size_t bit_offset() const
         {
             return m_bit_offset;
         }
+
     private:
         Optional<u8> m_next_byte;
         size_t m_bit_offset { 0 };
         OutputStream& m_stream;
     }; // class OutputBitStream final : public OutputStream
 } // namespace Mods
+
+using Mods::InputBitStream;
+using Mods::OutputBitStream;
