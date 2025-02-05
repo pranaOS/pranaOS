8bdf1222c6d2d1f9466b1f5a8ed4ba0fc8ef1396
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-05 18:19:36 +0530

    mods: filestream

---
+++ mods/filestream.h
@@ -0,0 +1,10 @@
+/**
+ * @file filestream.h
+ * @author Krisna Pranav
+ * @brief File Stream
+ * @version 6.0
+ * @date 2025-02-05
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
