b9a6b479ed7224b9f0c74a1603702c92c1e3edbd
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-05 18:38:16 +0530

    mods: fixedarray

--- mods/fixedarray.h
+++ mods/fixedarray.h
@@ -17,13 +17,13 @@
 #include <mods/kmalloc.h>
 #include <initializer_list>
 
-namespace Mods
+namespace Mods 
 {
     /**
      * @tparam T 
      */
     template<typename T>
-    class FixedArray
+    class FixedArray 
     {
     public:
         /**
@@ -41,7 +41,7 @@ namespace Mods
             auto array = TRY(try_create(initializer.size()));
             auto it = initializer.begin();
 
-            for(size_t i = 0; i < array.size(); ++i) {
+            for (size_t i = 0; i < array.size(); ++i) {
                 array[i] = move(*it);
                 ++it;
             }
@@ -57,16 +57,172 @@ namespace Mods
         {
             if (size == 0)
                 return FixedArray<T>();
-            
+
             T* elements = static_cast<T*>(kmalloc_array(size, sizeof(T)));
 
             if (!elements)
                 return Error::from_errno(ENOMEM);
-            
+
             for (size_t i = 0; i < size; ++i)
                 new (&elements[i]) T();
-            
+
             return FixedArray<T>(size, elements);
+        }
+
+        /**
+         * @param size 
+         * @return FixedArray<T> 
+         */
+        static FixedArray<T> must_create_but_fixme_should_propagate_errors(size_t size)
+        {
+            return MUST(try_create(size));
+        }
+
+        /**
+         * @tparam N 
+         * @return ErrorOr<FixedArray<T>> 
+         */
+        template<size_t N>
+        static ErrorOr<FixedArray<T>> try_create(T (&&array)[N])
+        {
+            if (N == 0)
+                return FixedArray<T>();
+
+            T* elements = static_cast<T*>(kmalloc_array(N, sizeof(T)));
+
+            if (!elements)
+                return Error::from_errno(ENOMEM);
+
+            for (size_t i = 0; i < N; ++i)
+                new (&elements[i]) T(move(array[i]));
+                
+            return FixedArray<T>(N, elements);
+        }
+
+        /**
+         * @tparam U 
+         * @param span 
+         * @return ErrorOr<FixedArray<T>> 
+         */
+        template<typename U>
+        static ErrorOr<FixedArray<T>> try_create(Span<U> span)
+        {
+            if (span.size() == 0)
+                return FixedArray<T>();
+
+            T* elements = static_cast<T*>(kmalloc_array(span.size(), sizeof(T)));
+
+            if (!elements)
+                return Error::from_errno(ENOMEM);
+
+            for (size_t i = 0; i < span.size(); ++i)
+                new (&elements[i]) T(span[i]);
+
+            return FixedArray<T>(span.size(), elements);
+        }
+
+        /**
+         * @return ErrorOr<FixedArray<T>> 
+         */
+        ErrorOr<FixedArray<T>> try_clone() const
+        {
+            if (m_size == 0)
+                return FixedArray<T>();
+
+            T* elements = static_cast<T*>(kmalloc_array(m_size, sizeof(T)));
+
+            if (!elements)
+                return Error::from_errno(ENOMEM);
+
+            for (size_t i = 0; i < m_size; ++i)
+                new (&elements[i]) T(m_elements[i]);
+
+            return FixedArray<T>(m_size, elements);
+        }
+
+        /**
+         * @return FixedArray<T> 
+         */
+        FixedArray<T> must_clone_but_fixme_should_propagate_errors() const
+        {
+            return MUST(try_clone());
+        }
+
+        /**
+         * @brief Construct a new Fixed Array object
+         * 
+         */
+        FixedArray(FixedArray<T> const&) = delete;
+
+        /**
+         * @return FixedArray<T>& 
+         */
+        FixedArray<T>& operator=(FixedArray<T> const&) = delete;
+
+        /**
+         * @brief Construct a new Fixed Array object
+         * 
+         * @param other 
+         */
+        FixedArray(FixedArray<T>&& other)
+            : m_size(other.m_size)
+            , m_elements(other.m_elements)
+        {
+            other.m_size = 0;
+            other.m_elements = nullptr;
+        }
+        
+        FixedArray<T>& operator=(FixedArray<T>&&) = delete;
+
+        /**
+         * @brief Destroy the Fixed Array object
+         * 
+         */
+        ~FixedArray()
+        {
+            if (!m_elements)
+                return;
+
+            for (size_t i = 0; i < m_size; ++i)
+                m_elements[i].~T();
+
+            kfree_sized(m_elements, sizeof(T) * m_size);
+            
+            m_size = 0;
+            m_elements = nullptr;
+        }
+
+        /**
+         * @return size_t 
+         */
+        size_t size() const 
+        { 
+            return m_size; 
+        }
+
+        /**
+         * @return true 
+         * @return false 
+         */
+        bool is_empty() const 
+        { 
+            return m_size == 0; 
+        }
+
+        /**
+         * @return T* 
+         */
+        T* data() 
+        { 
+            return m_elements; 
+        }
+
+        /**
+         * @return T const* 
+         */
+        T const* data() const 
+        { 
+            return m_elements; 
         }
 
         /**
@@ -77,7 +233,7 @@ namespace Mods
         {
             VERIFY(index < m_size);
             return m_elements[index];
-        }
+        }   
 
         /**
          * @param index 
@@ -96,7 +252,6 @@ namespace Mods
         T& operator[](size_t index)
         {
             return at(index);
-            
         }
 
         /**
@@ -120,7 +275,7 @@ namespace Mods
                     return true;
             }
 
-            return false
+            return false;
         }
 
         /**
@@ -131,6 +286,58 @@ namespace Mods
             ::swap(m_size, other.m_size);
             ::swap(m_elements, other.m_elements);
         }
+
+        using Iterator = SimpleIterator<FixedArray, T>;
+        using ConstIterator = SimpleIterator<FixedArray const, T const>;
+
+        /**
+         * @return Iterator 
+         */
+        Iterator begin() 
+        { 
+            return Iterator::begin(*this); 
+        }
+
+        /**
+         * @return ConstIterator 
+         */
+        ConstIterator begin() const 
+        { 
+            return ConstIterator::begin(*this); 
+        }
+
+        /**
+         * @return Iterator 
+         */
+        Iterator end() 
+        { 
+            return Iterator::end(*this); 
+        }
+
+        /**
+         * @return ConstIterator 
+         */
+        ConstIterator end() const 
+        { 
+            return ConstIterator::end(*this); 
+        }
+
+        /**
+         * @return Span<T> 
+         */
+        Span<T> span() 
+        { 
+            return { data(), size() }; 
+        }
+
+        /**
+         * @return Span<T const> 
+         */
+        Span<T const> span() const 
+        { 
+            return { data(), size() }; 
+        }
+
     private:
         /**
          * @brief Construct a new Fixed Array object
