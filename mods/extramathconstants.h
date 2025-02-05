ec2cf99b020b5eab3d701fabfad60ee61e6d020a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-05 18:19:05 +0530

    mods: extramathconstants

---
+++ mods/extramathconstants.h
@@ -0,0 +1,16 @@
+/**
+ * @file extramathconstants.h
+ * @author Krisna Pranav
+ * @brief Extra Math Constants
+ * @version 6.0
+ * @date 2025-02-05
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#define M_TAU 6.28318530717958647692
+#define M_DEG2RAD 0.01745329251994329576
+#define M_RAD2DEG 57.2957795130823208767
