0ecf53da1a451968d4a2623672b28f9a76a8a0dc
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-07 20:28:28 +0530

    mods: format

--- mods/format.cpp
+++ mods/format.cpp
@@ -61,4 +61,44 @@ namespace Mods
          */
         bool consume_replacement_field(size_t& index);
     }; // class FormatParser : public GenericLexer
+
+    namespace
+    {
+        static constexpr size_t use_next_index = NumericLimits<size_t>;
+        
+        /**
+         * @param value 
+         * @param buffer 
+         * @param base 
+         * @param upper_case 
+         * @return constexpr size_t 
+         */
+        static constexpr size_t convert_unsigned_to_string(u64 value, Array<u8, 128>& buffer, u8 base, bool upper_case)
+        {
+            VERIFY(base >= 2 && base <= 16);
+
+            constexpr char const* lowercase_lookup = "0123456789abcdef";
+            constexpr char const* uppercase_lookup = "0123456789ABCDEF";
+
+            if (value == 0) {
+                buffer[0] = '0';
+                return 1;
+            }
+
+            size_t used = 0;
+            while (value > 0) {
+                if (upper_case)
+                    buffer[used++] = uppercase_lookup[value % base];
+                else
+                    buffer[used++] = lowercase_lookup[value % base];
+
+                value /= base;
+            }
+
+            for (size_t i = 0; i < used / 2; ++i)
+                swap(buffer[i], buffer[used - i - 1]);
+
+            return used;
+        }
+    } // namespace
 } // namespace Mods
