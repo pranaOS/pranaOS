b0f74c079c05464331d9c98047c0e0e009bf132e
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-09 16:43:59 +0530

    mods: integralmath

---
+++ mods/integralmath.h
@@ -0,0 +1,39 @@
+/**
+ * @file integralmath.h
+ * @author Krisna Pranav
+ * @brief Integral Math
+ * @version 6.0
+ * @date 2023-07-20
+ *
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ *
+ */
+
+#pragma once 
+
+#include <mods/forward.h>
+#include <mods/hashtable.h>
+#include <mods/random.h>
+
+namespace Mods
+{
+    class IDAllocator
+    {
+    public:
+        /**
+         * @brief Construct a new IDAllocator object
+         * 
+         */
+        IDAllocator() = default;
+
+        /**
+         * @brief Destroy the IDAllocator object
+         * 
+         */
+        ~IDAllocator() = default;
+    private:
+        HashTable<int> m_allocated_ids;
+    }; // class IDAllocator
+} // namespace Mods
+
+using Mods::IDAllocator;
