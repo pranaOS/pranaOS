a3cd7685994ef72b950282fc06fcbfe53b91acc8
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-09 14:12:32 +0530

    mods: genericlexer

--- mods/genericlexer.h
+++ mods/genericlexer.h
@@ -4,81 +4,186 @@
  * @brief genericlexer
  * @version 6.0
  * @date 2023-07-01
- * 
+ *
  * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
- * 
+ *
  */
 
 #pragma once
 
-#include "string_view.h"
+#include <mods/result.h>
+#include <mods/stringview.h>
 
-namespace Mods {
-
-    class GenericLexer {
+namespace Mods
+{
+    class GenericLexer
+    {
     public:
-        explicit GenericLexer(const StringView& input);
-        virtual ~GenericLexer();
-
         /**
-         * @return size_t 
+         * @return input
+         * 
          */
-        size_t tell() const { 
-            return m_index; 
+        constexpr explicit GenericLexer(StringView input)
+            : m_input(input)
+        {
         }
 
         /**
-         * @return size_t 
+         * @return constexpr size_t 
          */
-        size_t tell_remaining() const { 
-            return m_input.length() - m_index; 
+        constexpr size_t tell() const
+        {
+            return m_index;
+        }
+
+        /**
+         * @return constexpr size_t 
+         */
+        constexpr size_t tell_remaining() const
+        {
+            return m_input.length() - m_index;
         }
 
         /**
          * @return StringView 
          */
-        StringView remaining() const { 
-            return m_input.substring_view(m_index); 
+        StringView remaining() const
+        {
+            return m_input.substring_view(m_index);
         }
 
         /**
          * @return true 
          * @return false 
          */
-        bool is_eof() const;
+        constexpr bool is_eof() const
+        {
+            return m_index >= m_input.length();
+        }
 
         /**
          * @param offset 
-         * @return char 
+         * @return constexpr char 
          */
-        char peek(size_t offset = 0) const;
+        constexpr char peek(size_t offset = 0) const
+        {
+            return (m_index + offset < m_input.length()) ? m_input[m_index + offset] : '\0';
+        }
 
         /**
-         * @brief next_is
-         * 
+         * @param expected 
          * @return true 
          * @return false 
          */
-        bool next_is(char) const;
-        bool next_is(StringView) const;
-        bool next_is(const char*) const;
-
-        void retreat();
-
-        /**
-         * @return char 
-         */
-        char consume();
-        bool consume_specific(char);
+        constexpr bool next_is(char expected) const
+        {
+            return peek() == expected;
+        }
 
         /**
+         * @param expected 
          * @return true 
          * @return false 
          */
-        bool consume_specific(StringView);
-        bool consume_specific(const char*);
-        char consume_escaped_character(char escape_char = '\\', const StringView& escape_map = "n\nr\rt\tb\bf\f");
+        constexpr bool next_is(StringView expected) const
+        {
+            for(size_t i = 0; i < expected.length(); ++i)
+                if(peek(i) != expected[i])
+                    return false;
+            return true;
+        }
+        
+        /**
+         * @param expected 
+         * @return true 
+         * @return false 
+         */
+        constexpr bool next_is(char const* expected) const
+        {
+            for(size_t i = 0; expected[i] != '\0'; ++i)
+                if(peek(i) != expected[i])
+                    return false;
 
+            return true;
+        }
+
+        constexpr void retreat()
+        {
+            VERIFY(m_index > 0);
+            --m_index;
+        }
+
+        /**
+         * @param count 
+         */
+        constexpr void retreat(size_t count)
+        {
+            VERIFY(m_index >= count);
+            m_index -= count;
+        }
+
+        constexpr char consume()
+        {
+            VERIFY(!is_eof());
+            return m_input[m_index++];
+        }
+
+        /**
+         * @tparam T 
+         * @param next 
+         * @return true 
+         * @return false 
+         */
+        template <typename T>
+        constexpr bool consume_specific(const T& next)
+        {
+            if(!next_is(next))
+                return false;
+
+            if constexpr(requires { next.length(); })
+            {
+                ignore(next.length());
+            }
+            else
+            {
+                ignore(sizeof(next));
+            }
+            return true;
+        }
+
+    #ifndef KERNEL
+        bool consume_specific(String const& next)
+        {
+            return consume_specific(StringView{next});
+        }
+    #endif
+
+        /**
+         * @param next 
+         * @return true 
+         * @return false 
+         */
+        constexpr bool consume_specific(char const* next)
+        {
+            return consume_specific(StringView{next});
+        }
+
+        constexpr char consume_escaped_character(char escape_char = '\\', StringView escape_map = "n\nr\rt\tb\bf\f")
+        {
+            if(!consume_specific(escape_char))
+                return consume();
+
+            auto c = consume();
+
+            for(size_t i = 0; i < escape_map.length(); i += 2)
+            {
+                if(c == escape_map[i])
+                    return escape_map[i + 1];
+            }
+
+            return c;
+        }
+        
         /**
          * @param count 
          * @return StringView 
@@ -99,106 +204,176 @@ namespace Mods {
          * @return StringView 
          */
         StringView consume_until(char);
-        StringView consume_until(const char*);
+
+        /**
+         * @return StringView 
+         */
+        StringView consume_until(char const*);
+
+        /**
+         * @return StringView 
+         */
+        StringView consume_until(StringView);
 
         /**
          * @param escape_char 
          * @return StringView 
          */
         StringView consume_quoted_string(char escape_char = 0);
+    #ifndef KERNEL
         String consume_and_unescape_string(char escape_char = '\\');
+    #endif
 
+        enum class UnicodeEscapeError
+        {
+            MalformedUnicodeEscape,
+            UnicodeEscapeOverflow,
+        }; // enum class UnicodeEscapeError
+
+    #ifndef KERNEL
+        Result<u32, UnicodeEscapeError> consume_escaped_code_point(bool combine_surrogate_pairs = true);
+    #endif
+        
         /**
+         * @brief 
+         * 
          * @param count 
          */
-        void ignore(size_t count = 1);
-        void ignore_until(char);
-        void ignore_until(const char*);
+        constexpr void ignore(size_t count = 1)
+        {
+            count = min(count, m_input.length() - m_index);
+            m_index += count;
+        }
 
         /**
-         * @tparam C 
-         * @param condition 
+         * @param stop 
+         */
+        constexpr void ignore_until(char stop)
+        {
+            while(!is_eof() && peek() != stop)
+            {
+                ++m_index;
+            }
+            ignore();
+        }
+
+        /**
+         * @param stop 
+         */
+        constexpr void ignore_until(char const* stop)
+        {
+            while(!is_eof() && !next_is(stop))
+            {
+                ++m_index;
+            }
+            ignore(__builtin_strlen(stop));
+        }
+
+        /**
+         * @tparam TPredicate 
+         * @param pred 
          * @return true 
          * @return false 
          */
-        template<typename C>
-        bool next_is(C condition) const {
-            return condition(peek());
+        template <typename TPredicate>
+        constexpr bool next_is(TPredicate pred) const
+        {
+            return pred(peek());
         }
 
         /**
-         * @tparam C 
-         * @param condition 
+         * @tparam TPredicate 
+         * @param pred 
          * @return StringView 
          */
-        template<typename C>
-        StringView consume_while(C condition) {
+        template <typename TPredicate>
+        StringView consume_while(TPredicate pred)
+        {
             size_t start = m_index;
-            while (!is_eof() && condition(peek()))
-                m_index++;
+            while(!is_eof() && pred(peek()))
+                ++m_index;
             size_t length = m_index - start;
 
-            if (length == 0)
+            if(length == 0)
                 return {};
             return m_input.substring_view(start, length);
         }
 
         /**
-         * @tparam C 
-         * @param condition 
+         * @tparam TPredicate 
+         * @param pred 
          * @return StringView 
          */
-        template<typename C>
-        StringView consume_until(C condition) {
+        template <typename TPredicate>
+        StringView consume_until(TPredicate pred)
+        {
             size_t start = m_index;
-            while (!is_eof() && !condition(peek()))
-                m_index++;
+            while(!is_eof() && !pred(peek()))
+                ++m_index;
             size_t length = m_index - start;
 
-            if (length == 0)
+            if(length == 0)
                 return {};
             return m_input.substring_view(start, length);
         }
-        
+
         /**
-         * @tparam C 
-         * @param condition 
+         * @tparam TPredicate 
+         * @param pred 
          */
-        template<typename C>
-        void ignore_while(C condition) {
-            while (!is_eof() && condition(peek()))
-                m_index++;
+        template <typename TPredicate>
+        constexpr void ignore_while(TPredicate pred)
+        {
+            while(!is_eof() && pred(peek()))
+                ++m_index;
         }
 
         /**
-         * @tparam C 
-         * @param condition 
+         * @tparam TPredicate 
+         * @param pred 
          */
-        template<typename C>
-        void ignore_until(C condition) {
-            while (!is_eof() && !condition(peek()))
-                m_index++;
+        template <typename TPredicate>
+        constexpr void ignore_until(TPredicate pred)
+        {
+            while(!is_eof() && !pred(peek()))
+                ++m_index;
         }
 
     protected:
         StringView m_input;
-        size_t m_index { 0 };
+        size_t m_index{0};
+
+    private:
+    #ifndef KERNEL
+        Result<u32, UnicodeEscapeError> decode_code_point();
+        Result<u32, UnicodeEscapeError> decode_single_or_paired_surrogate(bool combine_surrogate_pairs);
+    #endif
     };
 
     /**
-     * @brief is_any_of
-     * 
      * @param values 
      * @return constexpr auto 
      */
-    constexpr auto is_any_of(const StringView& values) {
-        return [values](auto c) { return values.contains(c); };
+    constexpr auto is_any_of(StringView values)
+    {
+        return [values](auto c)
+        { return values.contains(c); };
+    }
+
+    /**
+     * @param values 
+     * @return constexpr auto 
+     */
+    constexpr auto is_not_any_of(StringView values)
+    {
+        return [values](auto c)
+        { return !values.contains(c); };
     }
 
     constexpr auto is_path_separator = is_any_of("/\\");
     constexpr auto is_quote = is_any_of("'\"");
 
-}
+} // namespace Mods
 
 using Mods::GenericLexer;
 using Mods::is_any_of;
