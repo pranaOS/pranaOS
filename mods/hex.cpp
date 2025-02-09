f8f40fab317db2ee6aab6e22bfaff6965545ce73
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-09 15:08:03 +0530

    mods: hex

--- mods/hex.cpp
+++ mods/hex.cpp
@@ -4,76 +4,70 @@
  * @brief hex
  * @version 6.0
  * @date 2023-07-11
- * 
+ *
  * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
- * 
+ *
  */
 
-#include "array.h"
-#include "byte_buffer.h"
-#include "hex.h"
-#include "string.h"
-#include "string_builder.h"
-#include "string_view.h"
-#include "types.h"
-#include "vector.h"
-#include "platform.h"
+#include <mods/array.h>
+#include <mods/hex.h>
+#include <mods/stringbuilder.h>
+#include <mods/types.h>
+#include <mods/vector.h>
 
-namespace AK {
-
-    /**
-     * @param digit 
-     * @return u8 
-     */
-    static u8 decode_hex_digit(char digit)
-    {
-        if (digit >= '0' && digit <= '9')
-            return digit - '0';
-        if (digit >= 'a' && digit <= 'f')
-            return 10 + (digit - 'a');
-        if (digit >= 'A' && digit <= 'F')
-            return 10 + (digit - 'A');
-        return 255;
-    }
-
+namespace Mods
+{
     /**
      * @param input 
-     * @return Optional<ByteBuffer> 
+     * @return ErrorOr<ByteBuffer> 
      */
-    Optional<ByteBuffer> decode_hex(const StringView& input)
+    ErrorOr<ByteBuffer> decode_hex(StringView input)
     {
-        if ((input.length() % 2) != 0)
-            return {};
+        if((input.length() % 2) != 0)
+            return Error::from_string_literal("Hex string was not an even length");
 
-        auto output = ByteBuffer::create_zeroed(input.length() / 2);
+        auto output = TRY(ByteBuffer::create_zeroed(input.length() / 2));
 
-        for (long unsigned int i = 0; i < input.length() / 2; i++) {
-            auto c1 = decode_hex_digit(input[i * 2]);
-            if (c1 >= 16)
-                return {};
+        for(size_t i = 0; i < input.length() / 2; ++i)
+        {
+            auto const c1 = decode_hex_digit(input[i * 2]);
+            if(c1 >= 16)
+                return Error::from_string_literal("Hex string contains invalid digit");
 
-            auto c2 = decode_hex_digit(input[i * 2 + 1]);
-            if (c2 >= 16)
-                return {};
+            auto const c2 = decode_hex_digit(input[i * 2 + 1]);
+            if(c2 >= 16)
+                return Error::from_string_literal("Hex string contains invalid digit");
 
             output[i] = (c1 << 4) + c2;
         }
 
-        return output;
+        return {move(output)};
     }
 
+    #ifdef KERNEL
     /**
      * @param input 
-     * @return String 
+     * @return ErrorOr<NonnullOwnPtr<Kernel::KString>> 
      */
-    String encode_hex(ReadonlyBytes input)
+    ErrorOr<NonnullOwnPtr<Kernel::KString>> encode_hex(const ReadonlyBytes input)
     {
         StringBuilder output(input.size() * 2);
 
-        for (auto ch : input)
-            output.appendf("%02x", ch);
+        for(auto ch : input)
+            TRY(output.try_appendff("{:02x}", ch));
+
+        return Kernel::KString::try_create(output.string_view());
+    }
+    #else
+    String encode_hex(const ReadonlyBytes input)
+    {
+        StringBuilder output(input.size() * 2);
+
+        for(auto ch : input)
+            output.appendff("{:02x}", ch);
 
         return output.build();
     }
+    #endif
 
-}
+} // namespace Mods
