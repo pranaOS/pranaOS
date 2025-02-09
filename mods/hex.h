3981d041dc2ac74e8c2150ea3784dcc3873ae620
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-09 15:06:47 +0530

    mods: hex

--- mods/hex.h
+++ mods/hex.h
@@ -4,36 +4,52 @@
  * @brief hex
  * @version 6.0
  * @date 2023-07-11
- * 
+ *
  * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
- * 
+ *
  */
 
 #pragma once
 
-#include "forward.h"
-#include <mods/byte_buffer.h>
-#include <mods/optional.h>
-#include <mods/span.h>
+#include <mods/bytebuffer.h>
+#include <mods/error.h>
+#include <mods/stringview.h>
+
+#ifdef KERNEL
+#include <kernel/kstring.h>
+#else
 #include <mods/string.h>
-#include <mods/string_view.h>
+#endif
 
-namespace Mods 
+namespace Mods
 {
+    /**
+     * @param digit 
+     * @return constexpr u8 
+     */
+    constexpr u8 decode_hex_digit(char digit)
+    {
+        if(digit >= '0' && digit <= '9')
+            return digit - '0';
+        if(digit >= 'a' && digit <= 'f')
+            return 10 + (digit - 'a');
+        if(digit >= 'A' && digit <= 'F')
+            return 10 + (digit - 'A');
+        return 255;
+    }
 
     /**
-     * @return Optional<ByteBuffer> 
+     * @return ErrorOr<ByteBuffer> 
      */
-    Optional<ByteBuffer> decode_hex(const StringView&);
+    ErrorOr<ByteBuffer> decode_hex(StringView);
 
-    /**
-     * @return String 
-     */
+    #ifdef KERNEL
+    ErrorOr<NonnullOwnPtr<Kernel::KString>> encode_hex(ReadonlyBytes);
+    #else
     String encode_hex(ReadonlyBytes);
-
+    #endif
 } // namespace Mods
 
-
-// using mods
 using Mods::decode_hex;
+using Mods::decode_hex_digit;
 using Mods::encode_hex;
