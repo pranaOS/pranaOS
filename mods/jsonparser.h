1f5e72a4622db9f0ef0a0c1fcf9c29fe28cc0580
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-10 19:34:05 +0530

    mods: jsonparser

--- mods/jsonparser.h
+++ mods/jsonparser.h
@@ -4,19 +4,19 @@
  * @brief Json Parser
  * @version 6.0
  * @date 2025-02-10
- * 
+ *
  * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
- * 
+ *
  */
 
-#pragma once 
+#pragma once
 
 #include <mods/genericlexer.h>
 #include <mods/jsonvalue.h>
 
 namespace Mods
 {
-    class JsonParser : private GenericLexer 
+    class JsonParser : private GenericLexer
     {
     public:
         /**
@@ -26,14 +26,36 @@ namespace Mods
          */
         explicit JsonParser(StringView input)
             : GenericLexer(input)
-        {}
+        {
+        }
+
+        /**
+         * @return ErrorOr<JsonValue> 
+         */
+        ErrorOr<JsonValue> parse();
+
     private:
+        /**
+         * @return ErrorOr<JsonValue> 
+         */
         ErrorOr<JsonValue> parse_helper();
+
+        /**
+         * @return ErrorOr<String> 
+         */
         ErrorOr<String> consume_and_unescape_string();
+
+        /**
+         * @return ErrorOr<JsonValue> 
+         */
         ErrorOr<JsonValue> parse_array();
+        ErrorOr<JsonValue> parse_object();
         ErrorOr<JsonValue> parse_number();
         ErrorOr<JsonValue> parse_string();
         ErrorOr<JsonValue> parse_false();
         ErrorOr<JsonValue> parse_true();
-    }; // class JsonParser : private GenericLexer 
+        ErrorOr<JsonValue> parse_null();
+    }; // class JsonParser : private GenericLexer
 } // namespace Mods
+
+using Mods::JsonParser;
