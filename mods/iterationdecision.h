d4c2cc88ce65e32dbf4f791ad7d3a48d41041308
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-10 19:03:43 +0530

    mods: iterationdecision

---
+++ mods/iterationdecision.h
@@ -0,0 +1,10 @@
+/**
+ * @file iterationdecision.h
+ * @author Krisna Pranav
+ * @brief Iteration Decision
+ * @version 6.0
+ * @date 2025-02-10
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
