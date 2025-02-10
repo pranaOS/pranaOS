842a6b211d6da679448978c085573c7b45d3eb01
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-10 19:22:48 +0530

    mods: jsonobject

--- mods/jsonobject.h
+++ mods/jsonobject.h
@@ -4,57 +4,64 @@
  * @brief json object
  * @version 6.0
  * @date 2023-09-09
- * 
+ *
  * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
- * 
+ *
  */
 
-#pragma once 
+#pragma once
 
 #include <mods/hashmap.h>
 #include <mods/jsonarray.h>
 #include <mods/jsonobjectserialize.h>
-#include <mods/jsonval.h>
+#include <mods/jsonvalue.h>
 #include <mods/string.h>
-#include <mods/string_impl.h>
 
-namespace Mods 
+namespace Mods
 {
-
-    class JsonObject 
+    class JsonObject
     {
     public:
-        /// @brief Construct a new Json Object object
-        JsonObject() { }
-        ~JsonObject() { }
+        /**
+         * @brief Construct a new Json Object object
+         * 
+         */
+        JsonObject() = default;
 
         /**
+         * @brief Destroy the Json Object object
+         * 
+         */
+        ~JsonObject() = default;
+
+        /**
+         * @brief Construct a new Json Object object
+         * 
          * @param other 
          */
-        JsonObject(const JsonObject& other)
-            : m_order(other.m_order)
-            , m_members(other.m_members)
-        { }     
+        JsonObject(JsonObject const& other)
+            : m_members(other.m_members)
+        {
+        }
 
         /**
+         * @brief Construct a new Json Object object
+         * 
          * @param other 
          */
         JsonObject(JsonObject&& other)
-            : m_order(move(other.m_order))
-            , m_members(move(other.m_members))
-        { }
+            : m_members(move(other.m_members))
+        {
+        }
 
         /**
          * @param other 
          * @return JsonObject& 
          */
-        JsonObject& operator=(const JsonObject& other)
+        JsonObject& operator=(JsonObject const& other)
         {
-            if (this != &other) {
+            if(this != &other)
                 m_members = other.m_members;
-                m_order = other.m_order;
-            }
-
             return *this;
         }
 
@@ -64,62 +71,56 @@ namespace Mods
          */
         JsonObject& operator=(JsonObject&& other)
         {
-            if (this != &other) {
+            if(this != &other)
                 m_members = move(other.m_members);
-                m_order = move(other.m_order);
-            }
-
             return *this;
         }
 
         /**
-         * @return int 
+         * @return size_t 
          */
-        int size() const 
-        { 
-            return m_members.size(); 
+        [[nodiscard]] size_t size() const
+        {
+            return m_members.size();
         }
 
         /**
          * @return true 
          * @return false 
          */
-        bool is_empty() const 
-        { 
-            return m_members.is_empty(); 
-        }
-
-        /**
-         * @param key 
-         * @return JsonValue 
-         */
-        JsonValue get(const String& key) const
+        [[nodiscard]] bool is_empty() const
         {
-            auto* value = get_ptr(key);
-            return value ? *value : JsonValue(JsonValue::Type::Null);
+            return m_members.is_empty();
         }
 
         /**
          * @param key 
-         * @param alternative 
-         * @return JsonValue 
+         * @return JsonValue const& 
          */
-        JsonValue get_or(const String& key, JsonValue alternative) const
+        [[nodiscard]] JsonValue const& get(StringView key) const
         {
-            auto* value = get_ptr(key);
-            return value ? *value : alternative;
+            auto const* value = get_ptr(key);
+            static JsonValue* s_null_value{nullptr};
+            if(!value)
+            {
+                if(!s_null_value)
+                    s_null_value = new JsonValue;
+                return *s_null_value;
+            }
+            return *value;
         }
 
         /**
+         * @brief Get the ptr object
+         * 
          * @param key 
-         * @return const JsonValue* 
+         * @return JsonValue const* 
          */
-        const JsonValue* get_ptr(const String& key) const
+        [[nodiscard]] JsonValue const* get_ptr(StringView key) const
         {
             auto it = m_members.find(key);
-            if (it == m_members.end())
+            if(it == m_members.end())
                 return nullptr;
-
             return &(*it).value;
         }
 
@@ -128,33 +129,9 @@ namespace Mods
          * @return true 
          * @return false 
          */
-        bool has(const String& key) const
+        [[nodiscard]] [[nodiscard]] bool has(StringView key) const
         {
             return m_members.contains(key);
-        }
-
-        /**
-         * @param key 
-         * @param value 
-         */
-        void set(const String& key, JsonValue value)
-        {
-            if (m_members.set(key, move(value)) == HashSetResult::ReplacedExistingEntry)
-                m_order.remove(m_order.find_first_index(key).value());
-            m_order.append(key);
-        }
-
-        /**
-         * @tparam Callback 
-         * @param callback 
-         */
-        template<typename Callback>
-        void for_each_member(Callback callback) const
-        {
-            for (size_t i = 0; i < m_order.size(); ++i) {
-                auto property = m_order[i];
-                callback(property, m_members.get(property).value());
-            }
         }
 
         /**
@@ -162,59 +139,191 @@ namespace Mods
          * @return true 
          * @return false 
          */
-        bool remove(const String& key)
+        [[nodiscard]] bool has_null(StringView key) const
         {
-            if (m_members.remove(key)) {
-                m_order.remove(m_order.find_first_index(key).value());
-                return true;
-            }
-            return false;
+            auto const* value = get_ptr(key);
+            return value && value->is_null();
+        }
+
+        /**
+         * @param key 
+         * @return true 
+         * @return false 
+         */
+        [[nodiscard]] bool has_bool(StringView key) const
+        {
+            auto const* value = get_ptr(key);
+            return value && value->is_bool();
+        }
+
+        /**
+         * @param key 
+         * @return true 
+         * @return false 
+         */
+        [[nodiscard]] bool has_string(StringView key) const
+        {
+            auto const* value = get_ptr(key);
+            return value && value->is_string();
+        }
+
+        /**
+         * @param key 
+         * @return true 
+         * @return false 
+         */
+        [[nodiscard]] bool has_i32(StringView key) const
+        {
+            auto const* value = get_ptr(key);
+            return value && value->is_i32();
+        }
+
+        /**
+         * @param key 
+         * @return true 
+         * @return false 
+         */
+        [[nodiscard]] bool has_u32(StringView key) const
+        {
+            auto const* value = get_ptr(key);
+            return value && value->is_u32();
+        }
+
+        /**
+         * @param key 
+         * @return true 
+         * @return false 
+         */
+        [[nodiscard]] bool has_i64(StringView key) const
+        {
+            auto const* value = get_ptr(key);
+            return value && value->is_i64();
+        }
+
+        /**
+         * @param key 
+         * @return true 
+         * @return false 
+         */
+        [[nodiscard]] bool has_u64(StringView key) const
+        {
+            auto const* value = get_ptr(key);
+            return value && value->is_u64();
+        }
+
+        /**
+         * @param key 
+         * @return true 
+         * @return false 
+         */
+        [[nodiscard]] bool has_number(StringView key) const
+        {
+            auto const* value = get_ptr(key);
+            return value && value->is_number();
+        }
+
+        /**
+         * @param key 
+         * @return true 
+         * @return false 
+         */
+        [[nodiscard]] bool has_array(StringView key) const
+        {
+            auto const* value = get_ptr(key);
+            return value && value->is_array();
+        }
+
+        /**
+         * @param key 
+         * @return true 
+         * @return false 
+         */
+        [[nodiscard]] bool has_object(StringView key) const
+        {
+            auto const* value = get_ptr(key);
+            return value && value->is_object();
+        }
+    #ifndef KERNEL
+        [[nodiscard]] [[nodiscard]] bool has_double(StringView key) const
+        {
+            auto const* value = get_ptr(key);
+            return value && value->is_double();
+        }
+    #endif
+
+        /**
+         * @param key 
+         * @param value 
+         */
+        void set(String const& key, JsonValue value)
+        {
+            m_members.set(key, move(value));
+        }
+
+        /**
+         * @tparam Callback 
+         * @param callback 
+         */
+        template <typename Callback>
+        void for_each_member(Callback callback) const
+        {
+            for(auto const& member : m_members)
+                callback(member.key, member.value);
+        }
+
+        /**
+         * @param key 
+         * @return true 
+         * @return false 
+         */
+        bool remove(StringView key)
+        {
+            return m_members.remove(key);
         }
 
         /**
          * @tparam Builder 
          * @return Builder::OutputType 
          */
-        template<typename Builder>
+        template <typename Builder>
         typename Builder::OutputType serialized() const;
 
         /**
          * @tparam Builder 
          */
-        template<typename Builder>
+        template <typename Builder>
         void serialize(Builder&) const;
 
         /**
          * @return String 
          */
-        String to_string() const 
-        { 
+        [[nodiscard]] String to_string() const
+        {
             return serialized<StringBuilder>();
         }
 
     private:
-        Vector<String> m_order;
-        HashMap<String, JsonValue> m_members;
+        OrderedHashMap<String, JsonValue> m_members;
     }; // class JsonObject
 
     /**
      * @tparam Builder 
      * @param builder 
      */
-    template<typename Builder>
+    template <typename Builder>
     inline void JsonObject::serialize(Builder& builder) const
     {
-        JsonObjectSerializer serializer { builder };
-        for_each_member([&](auto& key, auto& value) {
-            serializer.add(key, value);
-        });
+        auto serializer = MUST(JsonObjectSerializer<>::try_create(builder));
+        for_each_member([&](auto& key, auto& value)
+                        { MUST(serializer.add(key, value)); });
+        MUST(serializer.finish());
     }
 
     /**
      * @tparam Builder 
      * @return Builder::OutputType 
      */
-    template<typename Builder>
+    template <typename Builder>
     inline typename Builder::OutputType JsonObject::serialized() const
     {
         Builder builder;
@@ -226,15 +335,18 @@ namespace Mods
      * @tparam Builder 
      * @param builder 
      */
-    template<typename Builder>
+    template <typename Builder>
     inline void JsonValue::serialize(Builder& builder) const
     {
-        switch (m_type) {
-        case Type::String: {
+        switch(m_type)
+        {
+        case Type::String:
+        {
             builder.append("\"");
-            builder.append_escaped_for_json({ m_value.as_string->characters(), m_value.as_string->length() });
+            builder.append_escaped_for_json({m_value.as_string->characters(), m_value.as_string->length()});
             builder.append("\"");
-        } break;
+        }
+        break;
         case Type::Array:
             m_value.as_array->serialize(builder);
             break;
@@ -246,7 +358,7 @@ namespace Mods
             break;
     #if !defined(KERNEL)
         case Type::Double:
-            builder.appendf("%g", m_value.as_double);
+            builder.appendff("{}", m_value.as_double);
             break;
     #endif
         case Type::Int32:
@@ -265,7 +377,7 @@ namespace Mods
             builder.append("null");
             break;
         default:
-            ASSERT_NOT_REACHED();
+            VERIFY_NOT_REACHED();
         }
     }
 
@@ -273,7 +385,7 @@ namespace Mods
      * @tparam Builder 
      * @return Builder::OutputType 
      */
-    template<typename Builder>
+    template <typename Builder>
     inline typename Builder::OutputType JsonValue::serialized() const
     {
         Builder builder;
