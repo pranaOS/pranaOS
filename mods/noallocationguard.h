7f986990b004515a89ff2152759311f4603c3dbd
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-11 20:15:35 +0530

    mods: noallocationguard

--- mods/noallocationguard.h
+++ mods/noallocationguard.h
@@ -4,24 +4,26 @@
  * @brief no allocation guard
  * @version 6.0
  * @date 2025-02-11
- * 
+ *
  * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
- * 
+ *
  */
 
-#pragma once 
+#pragma once
 
 #include <mods/forward.h>
 #include <mods/noncopyable.h>
 
 #if defined(KERNEL)
-#   include <kernel/arch/processor.h>
+#include <kernel/arch/processor.h>
+#include <kernel/heap/kmalloc.h>
 #else
-#   include <libc/mallocdefs.h>
+#include <libc/mallocdefs.h>
 #endif
 
 namespace Mods
 {
+
     class NoAllocationGuard
     {
         MOD_MAKE_NONCOPYABLE(NoAllocationGuard);
@@ -33,7 +35,7 @@ namespace Mods
          * 
          */
         NoAllocationGuard()
-            : m_allocation_enabled_previously
+            : m_allocation_enabled_previously(get_thread_allocation_state())
         {
             set_thread_allocation_state(false);
         }
@@ -48,9 +50,21 @@ namespace Mods
         }
 
     private:
+        /**
+         * @brief Get the thread allocation state object
+         * 
+         * @return true 
+         * @return false 
+         */
         static bool get_thread_allocation_state()
         {
+    #if defined(KERNEL)
+            return Processor::current_thread()->get_allocation_enabled();
+    #elif defined(__pranaos__)
+            return s_allocation_enabled;
+    #else
             return true;
+    #endif
         }
 
         /**
@@ -60,10 +74,17 @@ namespace Mods
          */
         static void set_thread_allocation_state(bool value)
         {
+    #if defined(KERNEL)
+            Processor::current_thread()->set_allocation_enabled(value);
+    #elif defined(__pranaos__)
+            s_allocation_enabled = value;
+    #else
             (void)value;
+    #endif
         }
 
-        bool m_allocation_enabled_previously { true };
-
+        bool m_allocation_enabled_previously{true};
     }; // class NoAllocationGuard
 } // namespace Mods
+
+using Mods::NoAllocationGuard;
