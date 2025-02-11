9eb07d7bd1bbd8818c005cec8742e116214dcd55
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-11 19:15:06 +0530

    mods: neverdestroyed

--- mods/neverdestroyed.h
+++ mods/neverdestroyed.h
@@ -4,17 +4,97 @@
  * @brief neverdestroyed
  * @version 6.0
  * @date 2023-07-11
- * 
+ *
  * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
- * 
+ *
  */
 
 #pragma once
 
-#include "noncopyable.h"
-#include "types.h"
+#include <mods/noncopyable.h>
+#include <mods/types.h>
 
 namespace Mods
 {
-    
-}
+    /**
+     * @tparam T 
+     */
+    template <typename T>
+    class NeverDestroyed
+    {
+        MOD_MAKE_NONCOPYABLE(NeverDestroyed);
+        MOD_MAKE_NONMOVABLE(NeverDestroyed);
+
+    public:
+        /**
+         * @brief Construct a new Never Destroyed object
+         * 
+         * @tparam Args 
+         * @param args 
+         */
+        template <typename... Args>
+        NeverDestroyed(Args&&... args)
+        {
+            new(storage) T(forward<Args>(args)...);
+        }   
+
+        /**
+         * @brief Destroy the Never Destroyed object
+         * 
+         */
+        ~NeverDestroyed() = default;
+
+        /**
+         * @return T* 
+         */
+        T* operator->()
+        {
+            return &get();
+        }
+
+        /**
+         * @return const T* 
+         */
+        const T* operator->() const
+        {
+            return &get();
+        }
+
+        /**
+         * @return T& 
+         */
+        T& operator*()
+        {
+            return get();
+        }
+
+        /**
+         * @return const T& 
+         */
+        const T& operator*() const
+        {
+            return get();
+        }
+
+        /**
+         * @return T& 
+         */
+        T& get()
+        {
+            return reinterpret_cast<T&>(storage);
+        }
+
+        /**
+         * @return const T& 
+         */
+        const T& get() const
+        {
+            return reinterpret_cast<T&>(storage);
+        }
+
+    private:
+        alignas(T) u8 storage[sizeof(T)];
+    }; // class NeverDestroyed
+} // namespace Mods
+
+using Mods::NeverDestroyed;
