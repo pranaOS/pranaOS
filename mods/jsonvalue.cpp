e3df759d40f82994ecd1ccc5751117c16b633947
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-11 13:18:53 +0530

    mods: jsonvalue

---
+++ mods/jsonvalue.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file jsonvalue.cpp
+ * @author Krisna Pranav
+ * @brief Json Value
+ * @version 6.0
+ * @date 2025-02-11
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include <mods/jsonarray.h>
+#include <mods/jsonobject.h>
+#include <mods/jsonvalue.h>
+#include <mods/stringview.h>
+
+#ifndef KERNEL
+#   include <mods/jsonparser.h>
+#endif
+
+namespace Mods
+{
+    
+}
