7799e2119c360f876a4821226399decbf103591c
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-11 13:04:53 +0530

    mods: jsonpath

--- mods/jsonpath.cpp
+++ mods/jsonpath.cpp
@@ -4,9 +4,9 @@
  * @brief Json Path
  * @version 6.0
  * @date 2025-02-11
- * 
+ *
  * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
- * 
+ *
  */
 
 #include <mods/jsonobject.h>
@@ -15,7 +15,9 @@
 
 namespace Mods
 {
-    JsonPathElement JsonPathElement::any_array_element { Kind::AnyIndex };
+    JsonPathElement JsonPathElement::any_array_element{Kind::AnyIndex};
+    JsonPathElement JsonPathElement::any_object_element{Kind::AnyKey};
+
     /**
      * @param top_root 
      * @return JsonValue 
@@ -24,17 +26,39 @@ namespace Mods
     {
         auto root = top_root;
 
-        for (auto const& element: *this) {
-            switch (element.kind()) {
+        for(auto const& element : *this)
+        {
+            switch(element.kind())
+            {
             case JsonPathElement::Kind::Key:
-                root = JsonValue { root.as_object().get(element.key()) };
+                root = JsonValue{root.as_object().get(element.key())};
                 break;
-            case JsonPathElement::Kind::Index;
-                root = JsonValue { root.as_object().get(element.key()) };
+            case JsonPathElement::Kind::Index:
+                root = JsonValue{root.as_array().at(element.index())};
                 break;
             default:
                 VERIFY_NOT_REACHED();
             }
         }
+
+        return root;
+    }
+
+    /**
+     * @return String 
+     */
+    String JsonPath::to_string() const
+    {
+        StringBuilder builder;
+        builder.append("{ .");
+
+        for(auto const& el : *this)
+        {
+            builder.append(" > ");
+            builder.append(el.to_string());
+        }
+
+        builder.append(" }");
+        return builder.to_string();
     }
 } // namespace Mods
