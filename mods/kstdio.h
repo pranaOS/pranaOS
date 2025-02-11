8a112460c59e13aa2cc9c44be279415e3742f753
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-11 13:35:09 +0530

    mods: kstdio

--- mods/kstdio.h
+++ mods/kstdio.h
@@ -4,48 +4,35 @@
  * @brief kstdio
  * @version 6.0
  * @date 2023-07-01
- * 
+ *
  * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
- * 
+ *
  */
 
 #pragma once
 
 #ifdef __pranaos__
-#    ifdef KERNEL
-#        include <kernel/kstdio.h>
-#    else
-#        include "types.h"
-#        include <stdarg.h>
-extern "C" {
-    int vdbgprintf(const char* fmt, va_list);
-    int dbgprintf(const char* fmt, ...);
-    int dbgputstr(const char*, ssize_t);
-    int sprintf(char* buf, const char* fmt, ...);
-    int snprintf(char* buffer, size_t, const char* fmt, ...);
-}
-#    endif
+#ifdef KERNEL
+#include <kernel/kstdio.h>
 #else
-#    include <stdio.h>
-#    define kprintf printf
-#    define dbgprintf(...) fprintf(stderr, __VA_ARGS__)
-/**
- * @param characters 
- * @param length 
- * @return int 
- */
-inline int dbgputstr(const char* characters, ssize_t length) {
-    fwrite(characters, 1, length, stderr);
-    return 0;
+#include <mods/types.h>
+#include <stdarg.h>
+extern "C"
+{
+    void dbgputstr(char const*, size_t);
+    int sprintf(char* buf, char const* fmt, ...) __attribute__((format(printf, 2, 3)));
+    int snprintf(char* buffer, size_t, char const* fmt, ...) __attribute__((format(printf, 3, 4)));
 }
 #endif
-
-/**
- * 
- * @tparam N 
- * @return int 
- */
-template<size_t N>
-inline int dbgputstr(const char (&array)[N]) {
+#else
+#include <stdio.h>
+inline void dbgputstr(char const* characters, size_t length)
+{
+    fwrite(characters, 1, length, stderr);
+}
+#endif
+template <size_t N>
+inline void dbgputstr(char const (&array)[N])
+{
     return ::dbgputstr(array, N);
 }
