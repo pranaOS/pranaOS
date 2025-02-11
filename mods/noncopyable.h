bcef25754f7adfe372b0e468c9132febd9d4cb97
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-11 20:17:01 +0530

    mods: noncopyable

--- mods/noncopyable.h
+++ mods/noncopyable.h
@@ -4,19 +4,19 @@
  * @brief NonCopyable
  * @version 6.0
  * @date 2023-06-29
- * 
+ *
  * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
- * 
+ *
  */
 
 #pragma once
 
-#define MOD_MAKE_NONCOPYABLE(c) \
-private:                       \
-    c(const c&) = delete;      \
-    c& operator=(const c&) = delete
+#define AK_MAKE_NONCOPYABLE(c) \
+  private:                     \
+    c(c const&) = delete;      \
+    c& operator=(c const&) = delete
 
-#define MOD_MAKE_NONMOVABLE(c) \
-private:                      \
+#define AK_MAKE_NONMOVABLE(c) \
+  private:                    \
     c(c&&) = delete;          \
     c& operator=(c&&) = delete
