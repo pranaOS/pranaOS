8aefb0baf62ab7cfcd49782860d8576c70579686
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-12 18:54:07 +0530

    mods: nonnullrefptrvector

--- mods/nonnullrefptrvector.h
+++ mods/nonnullrefptrvector.h
@@ -4,22 +4,27 @@
  * @brief nonnullrefptrvector
  * @version 6.0
  * @date 2023-07-30
- * 
+ *
  * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
- * 
+ *
  */
 
-#pragma once 
+#pragma once
 
-#include "nonnullptrvector.h"
-#include "nonnullrefptrvector.h"
-#include "nonnullrefptr.h"
+#include <mods/nonnullptrvector.h>
+#include <mods/nonnullrefptr.h>
 
 namespace Mods
 {
-    template<typename T, int inline_capacity>
-    class NonnullRefPtrVector : public NonnullPtrVector<NonnullRefPtr<T>, inline_capacity> 
+    /**
+     * @tparam T
+     * @tparam inline_capacity
+     */
+    template <typename T, size_t inline_capacity>
+    class NonnullRefPtrVector : public NonnullPtrVector<NonnullRefPtr<T>, inline_capacity>
     {
-    }; // class
-
+        using NonnullPtrVector<NonnullRefPtr<T>, inline_capacity>::NonnullPtrVector;
+    };
 } // namespace Mods
+
+using Mods::NonnullRefPtrVector;
