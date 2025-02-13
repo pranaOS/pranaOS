1427812975170ffed9e4b3575b09628d7add47a0
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-13 22:09:26 +0530

    mods: recursiondecision

--- mods/recursiondecision.h
+++ mods/recursiondecision.h
@@ -4,21 +4,21 @@
  * @brief Recursion Decision
  * @version 6.0
  * @date 2024-10-31
- * 
+ *
  * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
- * 
+ *
  */
 
 #pragma once
 
-namespace Mods 
+namespace Mods
 {
-    enum class RecursionDecision 
+    enum class RecursionDecision
     {
         Recurse,
         Continue,
         Break,
-    }; // enum class RecursionDecision 
+    }; // enum class RecursionDecision
 } // namespace Mods
 
 using Mods::RecursionDecision;
