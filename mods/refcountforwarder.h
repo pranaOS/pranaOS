edbe5fcddd809933ef5530c3e301d182ad138565
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-13 22:21:01 +0530

    mods: refcountforwarder

--- mods/refcountforwarder.h
+++ mods/refcountforwarder.h
@@ -13,9 +13,32 @@
 
 namespace Mods
 {
+    /**
+     * @tparam T 
+     */
     template<typename T>
     class RefCountedForwarder
     {
+    public:
+        void ref() 
+        {
+            m_ref_count_target.ref();
+        }
 
+    protected:
+        /**
+         * @brief Construct a new RefCountedForwarder object
+         * 
+         * @param target 
+         */
+        RefCountedForwarder(T& target)
+            : m_ref_count_target(target)
+        {
+        }
+
+    private:
+        T& m_ref_count_target;
     }; // class RefCountedForwarder
 } // namespace Mods
+
+using Mods::RefCountedForwarder;
