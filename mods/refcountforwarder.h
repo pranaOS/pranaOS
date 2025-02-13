286599bf39fb17cef0a21a3310791f53fd056085
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-13 22:19:44 +0530

    mods: refcountforwarder

---
+++ mods/refcountforwarder.h
@@ -0,0 +1,21 @@
+/**
+ * @file refcountforwarder.h
+ * @author Krisna Pranav
+ * @brief Ref Count Forwarder
+ * @version 6.0
+ * @date 2025-02-13
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+namespace Mods
+{
+    template<typename T>
+    class RefCountedForwarder
+    {
+
+    }; // class RefCountedForwarder
+} // namespace Mods
