f2f1428c6a67c836188070fabf5ca01b1ead9dda
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-13 22:07:24 +0530

    mods: random

--- mods/random.cpp
+++ mods/random.cpp
@@ -4,9 +4,9 @@
  * @brief Random
  * @version 6.0
  * @date 2025-02-13
- * 
+ *
  * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
- * 
+ *
  */
 
 #include <mods/random.h>
@@ -21,6 +21,13 @@ namespace Mods
      */
     u32 get_random_uniform(u32 max_bounds)
     {
+        const u32 max_usable = UINT32_MAX - (static_cast<u64>(UINT32_MAX) + 1) % max_bounds;
+        auto random_value = get_random<u32>();
 
+        for(int i = 0; i < 20 && random_value > max_usable; ++i)
+        {
+            random_value = get_random<u32>();
+        }
+        return random_value % max_bounds;
     }
 } // namespace Mods
