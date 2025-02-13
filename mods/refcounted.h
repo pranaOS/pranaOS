fc2d96f57d7848e2d779410cb29acb1182cd6e2b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-13 22:18:17 +0530

    mods: refcounted

--- mods/refcounted.h
+++ mods/refcounted.h
@@ -4,26 +4,26 @@
  * @brief RefCounted
  * @version 6.0
  * @date 2023-06-29
- * 
+ *
  * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
- * 
+ *
  */
 
 #pragma once
 
 #ifdef KERNEL
-#    include <kernel/library/threadsaferefcounted.h>
+#include <kernel/library/threadsaferefcounted.h>
 #else
 
-#    include <mods/assertions.h>
-#    include <mods/check.h>
-#    include <mods/noncopyable.h>
-#    include <mods/platform.h>
-#    include <mods/stdlibextra.h>
+#include <mods/assertions.h>
+#include <mods/checked.h>
+#include <mods/noncopyable.h>
+#include <mods/platform.h>
+#include <mods/stdlibextra.h>
 
-namespace Mods 
+namespace Mods
 {
-    class RefCountedBase 
+    class RefCountedBase
     {
         MOD_MAKE_NONCOPYABLE(RefCountedBase);
         MOD_MAKE_NONMOVABLE(RefCountedBase);
@@ -48,8 +48,9 @@ namespace Mods
          */
         [[nodiscard]] bool try_ref() const
         {
-            if (m_ref_count == 0)
+            if(m_ref_count == 0)
                 return false;
+
             ref();
             return true;
         }
@@ -57,25 +58,25 @@ namespace Mods
         /**
          * @return RefCountType 
          */
-        [[nodiscard]] RefCountType ref_count() const 
-        { 
-            return m_ref_count; 
+        [[nodiscard]] RefCountType ref_count() const
+        {
+            return m_ref_count;
         }
 
     protected:
         /**
-         * @brief Construct a new RefCountedBase object
+         * @brief Construct a new Ref Counted Base object
          * 
          */
         RefCountedBase() = default;
 
         /**
-         * @brief Destroy the RefCountedBase object
+         * @brief Destroy the Ref Counted Base object
          * 
          */
-        ~RefCountedBase() 
-        { 
-            VERIFY(!m_ref_count); 
+        ~RefCountedBase()
+        {
+            VERIFY(!m_ref_count);
         }
 
         /**
@@ -87,14 +88,14 @@ namespace Mods
             return --m_ref_count;
         }
 
-        RefCountType mutable m_ref_count { 1 };
-    };
+        RefCountType mutable m_ref_count{1};
+    }; // class RefCountedBase
 
     /**
      * @tparam T 
      */
-    template<typename T>
-    class RefCounted : public RefCountedBase 
+    template <typename T>
+    class RefCounted : public RefCountedBase
     {
     public:
         bool unref() const
@@ -102,17 +103,16 @@ namespace Mods
             auto* that = const_cast<T*>(static_cast<T const*>(this));
 
             auto new_ref_count = deref_base();
-            
-            if (new_ref_count == 0) {
-                if constexpr (requires { that->will_be_destroyed(); })
+            if(new_ref_count == 0)
+            {
+                if constexpr(requires { that->will_be_destroyed(); })
                     that->will_be_destroyed();
                 delete static_cast<const T*>(this);
                 return true;
             }
-            
             return false;
         }
-    }; // class RefCounted: public RefCountedBase
+    }; // class RefCounted : public RefCountedBase
 
 } // namespace Mods
 
