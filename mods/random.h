896712b6b751ff58ca513f32865c9a987764ed62
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-13 22:04:17 +0530

    mods: random

--- mods/random.h
+++ mods/random.h
@@ -4,51 +4,51 @@
  * @brief Random
  * @version 6.0
  * @date 2023-07-29
- * 
+ *
  * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
- * 
+ *
  */
 
 #pragma once
 
-#include <mods/types.h>
 #include <mods/platform.h>
+#include <mods/types.h>
 
-#if defined(__prana__)
-#    include <stdlib.h>
+#if defined(__pranaos__)
+#include <stdlib.h>
 #endif
 
 #if defined(__unix__)
-#    include <unistd.h>
+#include <unistd.h>
 #endif
 
-#if defined(__APPLE__)
-#    include <sys/random.h>
+#if defined(MODS_OS_MACOS)
+#include <sys/random.h>
 #endif
 
 namespace Mods
-{   
+{
     /**
      * @param buffer 
      * @param length 
      */
-    inline void fill_with_random(void* buffer, size_t length)
+    inline void fill_with_random([[maybe_unused]] void* buffer, [[maybe_unused]] size_t length)
     {
-        #if defined(__prana__)
-            arc4random_buf(buffer, length);
-        #elif defined(OSS_FUZZ)
-            (void)buffer;
-            (void)length;
-        #elif defined(__unix__) or defined(__APPLE__)
-            int rc = getentropy(buffer, length);
-            (void)rc;
-        #endif
+    #if defined(__pranaos__)
+        arc4random_buf(buffer, length);
+    #elif defined(OSS_FUZZ)
+    #elif defined(__unix__) or defined(AK_OS_MACOS)
+        [[maybe_unused]] int rc = getentropy(buffer, length);
+    #endif
     }
 
     /**
-     * @tparam T
-    */
-    template<typename T>
+     * @brief Get the random object
+     * 
+     * @tparam T 
+     * @return T 
+     */
+    template <typename T>
     inline T get_random()
     {
         T t;
@@ -56,7 +56,16 @@ namespace Mods
         return t;
     }
 
+    /**
+     * @brief Get the random uniform object
+     * 
+     * @param max_bounds 
+     * @return u32 
+     */
+    u32 get_random_uniform(u32 max_bounds);
+
 } // namespace Mods
 
 using Mods::fill_with_random;
 using Mods::get_random;
+using Mods::get_random_uniform;
