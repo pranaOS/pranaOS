f91d1968aceaf3273961a25eb812a00b54aa36dd
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-13 22:05:27 +0530

    mods: random

---
+++ mods/random.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file random.cpp
+ * @author Krisna Pranav
+ * @brief Random
+ * @version 6.0
+ * @date 2025-02-13
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include <mods/random.h>
+
+namespace Mods
+{
+
+} // namespace Mods
