0e16330a43bd4a05e7ec0d7287ca3053d4f80304
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-14 22:35:42 +0530

    mods: scopedvaluerollback

--- mods/scopedvaluerollback.h
+++ mods/scopedvaluerollback.h
@@ -1,36 +1,41 @@
 /**
  * @file scoped_value_rollback.h
  * @author Krisna Pranav
- * @brief ScopedValueRollBack 
+ * @brief ScopedValueRollBack
  * @version 6.0
  * @date 2023-07-01
- * 
+ *
  * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
- * 
+ *
  */
 
 #pragma once
 
-namespace Mods {
-    template<typename T>
-    class ScopedValueRollback {
+namespace Mods
+{
+    /**
+     * @tparam T 
+     */
+    template <typename T>
+    class ScopedValueRollback
+    {
     public:
         /**
-         * @brief Construct a new Scoped Value Rollback object
+         * @brief Construct a new ScopedValueRollback object
          * 
          * @param variable 
          */
         ScopedValueRollback(T& variable)
-            : m_variable(variable)
-            , m_saved_value(variable)
+            : m_variable(variable), m_saved_value(variable)
         {
         }
 
         /**
-         * @brief Destroy the Scoped Value Rollback object
+         * @brief Destroy the ScopedValueRollback object
          * 
          */
-        ~ScopedValueRollback() {
+        ~ScopedValueRollback()
+        {
             m_variable = m_saved_value;
         }
 
@@ -39,15 +44,15 @@ namespace Mods {
          * 
          * @param value 
          */
-        void set_override_rollback_value(const T& value) {
+        void set_override_rollback_value(const T& value)
+        {
             m_saved_value = value;
         }
 
     private:
         T& m_variable;
         T m_saved_value;
-    };
-
-}
+    }; // class ScopedValueRollback
+} // namespace Mods
 
 using Mods::ScopedValueRollback;
