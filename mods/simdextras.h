1c7a97d4737a8c4d972be188a7314c2b1f90d661
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-14 22:55:59 +0530

    mods: simdextras

--- mods/simdextras.h
+++ mods/simdextras.h
@@ -38,8 +38,42 @@ namespace Mods::SIMD
 
     ALWAYS_INLINE static constexpr u32x4 expand4(u32 u)
     {
-        return u32x4 { u, u, u, u }
+        return u32x4 { u, u, u, u };
     }
+
+    /**
+     * @tparam TSrc 
+     * @param v 
+     * @return ALWAYS_INLINE 
+     */
+    template<typename TSrc>
+    ALWAYS_INLINE static u32x4 to_u32x4(TSrc v)
+    {
+        return __builtin_convertvector(v, u32x4);
+    }
+
+    /**
+     * @tparam TSrc 
+     * @param v 
+     * @return ALWAYS_INLINE 
+     */
+    template<typename TSrc>
+    ALWAYS_INLINE static i32x4 to_i32x4(TSrc v)
+    {
+        return __builtin_convertvector(v, i32x4);
+    }
+
+    /**
+     * @tparam TSrc 
+     * @param v 
+     * @return ALWAYS_INLINE 
+     */
+    template<typename TSrc>
+    ALWAYS_INLINE static f32x4 to_f32x4(TSrc v)
+    {
+        return __builtin_convertvector(v, f32x4);
+    }
+
 } // namespace Mods::SIMD 
 
 #pragma GCC diagnostic pop
