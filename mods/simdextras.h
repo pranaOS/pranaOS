e1c7e475e69b93e5f94966f80a25a88a02cc792b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-14 22:53:52 +0530

    mods: simdextras

--- mods/simdextras.h
+++ mods/simdextras.h
@@ -18,5 +18,28 @@
 
 namespace Mods::SIMD 
 {
-    
+    /**
+     * @param f 
+     * @return ALWAYS_INLINE constexpr 
+     */
+    ALWAYS_INLINE static constexpr f32x4 expand4(float f)
+    {
+        return f32x4 { f, f, f, f };
+    }
+
+    /**
+     * @param i 
+     * @return ALWAYS_INLINE constexpr 
+     */
+    ALWAYS_INLINE static constexpr i32x4 expand(i32 i)
+    {
+        return i32x4 { i, i, i, i };
+    }
+
+    ALWAYS_INLINE static constexpr u32x4 expand4(u32 u)
+    {
+        return u32x4 { u, u, u, u }
+    }
 } // namespace Mods::SIMD 
+
+#pragma GCC diagnostic pop
