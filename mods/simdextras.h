4b71d70cfd2248422bd9a96c0950f91807d75c91
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-14 22:44:16 +0530

    mods: simdextras

---
+++ mods/simdextras.h
@@ -0,0 +1,22 @@
+/**
+ * @file simdextras.h
+ * @author Krisna Pranav
+ * @brief simd extras
+ * @version 6.0
+ * @date 2025-02-14
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <mods/simd.h>
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored
+
+namespace Mods::SIMD 
+{
+    
+} // namespace Mods::SIMD 
