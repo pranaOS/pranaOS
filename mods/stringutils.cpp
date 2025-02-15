2d0a2d5e50c73c6dd2a7b20e7b386ad973e897ab
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-15 20:43:37 +0530

    mods: stringutils

--- mods/stringutils.cpp
+++ mods/stringutils.cpp
@@ -4,10 +4,23 @@
  * @brief String Utils
  * @version 6.0
  * @date 2025-02-15
- * 
+ *
  * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
  *
  */
+
+#include <mods/charactertypes.h>
+#include <mods/memmem.h>
+#include <mods/memory.h>
+#include <mods/optional.h>
+#include <mods/stringbuilder.h>
+#include <mods/stringutils.h>
+#include <mods/stringview.h>
+#include <mods/vector.h>
+
+#ifndef KERNEL
+#include <mods/string.h>
+#endif
 
 namespace Mods
 {
