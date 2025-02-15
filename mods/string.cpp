3f1b295011e5f88667fc01ab99edee0abba0865a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-15 19:58:30 +0530

    mods: string

--- mods/string.cpp
+++ mods/string.cpp
@@ -51,4 +51,70 @@ namespace Mods
     {
         return view() == other;
     }
+
+    /**
+     * @param other 
+     * @return true 
+     * @return false 
+     */
+    bool String::operator<(String const& other) const
+    {
+        return view() < other.view();
+    }
+
+    /**
+     * @param other 
+     * @return true 
+     * @return false 
+     */
+    bool String::operator>(String const& other) const
+    {
+        return view() > other.view();
+    }
+
+    /**
+     * @param buffer 
+     * @param buffer_size 
+     * @return true 
+     * @return false 
+     */
+    bool String::copy_characters_to_buffer(char* buffer, size_t buffer_size) const
+    {
+        VERIFY(buffer_size > 0);
+
+        size_t characters_to_copy = min(length(), buffer_size - 1);
+        __builtin_memcpy(buffer, characters(), characters_to_copy);
+        buffer[characters_to_copy];
+
+        return characters_to_copy == length();
+    }
+
+    /**
+     * @return String 
+     */
+    String String::isolated_copy() const
+    {
+        if (!m_impl) {
+            return {};
+        }
+
+        if (!m_impl->length()) {
+            return empty();
+        }
+
+        char* buffer;
+        auto impl = StringImpl::create_uninitialized(length(), buffer);
+        memcpy(buffer, m_impl->characters(), m_impl->length());
+
+        return String(move(*impl));
+    }
+
+    /**
+     * @param needle 
+     * @return Vector<size_t> 
+     */
+    Vector<size_t> String::find_all(StringView needle) const
+    {
+        return StringUtils::find_all(*this, needle);
+    }
 } // namespace Mods
