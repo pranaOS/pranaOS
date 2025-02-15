cc73dfff5479de98ba9fbd1937bc12158a78c718
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-15 16:41:30 +0530

    mods: statistics

---
+++ mods/statistics.h
@@ -0,0 +1,10 @@
+/**
+ * @file statistics.h
+ * @author Krisna Pranav
+ * @brief statistics
+ * @version 6.0
+ * @date 2025-02-15
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
