da9fc1b3ca1189b8e2f51312df061c06add964d6
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-15 15:51:00 +0530

    mods: sourcelocation

---
+++ mods/sourcelocation.h
@@ -0,0 +1,16 @@
+/**
+ * @file sourcelocation.h
+ * @author Krisna Pranav
+ * @brief Source Location
+ * @version 6.0
+ * @date 2025-02-15
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+#include <mods/format.h>
+#include <mods/stringview.h>
+#include <mods/types.h>
