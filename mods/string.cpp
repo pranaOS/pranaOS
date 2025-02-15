1730fd52e98143712f2a898c81ba1b97b9386c33
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-15 19:54:38 +0530

    mods: string

--- mods/string.cpp
+++ mods/string.cpp
@@ -18,3 +18,37 @@
 #include <mods/string.h>
 #include <mods/stringview.h>
 #include <mods/vector.h>
+
+namespace Mods
+{
+    /**
+     * @param fly_string 
+     * @return true 
+     * @return false 
+     */
+    bool String::operator==(FlyString const& fly_string) const
+    {
+        return m_impl == fly_string.impl() || view() == fly_string.view();
+    }
+
+    /**
+     * @param other 
+     * @return true 
+     * @return false 
+     */
+    bool String::operator==(String const& other) const
+    {
+        return m_impl == other.impl() || view() == other.view();
+    }
+
+    /**
+
+     * @param other 
+     * @return true 
+     * @return false 
+     */
+    bool String::operator==(StringView other) const
+    {
+        return view() == other;
+    }
+} // namespace Mods
