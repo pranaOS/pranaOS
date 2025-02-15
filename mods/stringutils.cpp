80e695574cd8fc7b365e7526a0f85809891849e1
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-15 20:42:27 +0530

    mods: stringutils

---
+++ mods/stringutils.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file stringutils.cpp
+ * @author Krisna Pranav
+ * @brief String Utils
+ * @version 6.0
+ * @date 2025-02-15
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ *
+ */
+
+namespace Mods
+{
+    namespace StringUtils
+    {
+    }
+} // namespace Mods
