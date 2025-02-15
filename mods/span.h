eaa838939ecf2287eabde891f44c871aa88895a4
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-15 16:04:55 +0530

    mods: span

--- mods/span.h
+++ mods/span.h
@@ -4,25 +4,28 @@
  * @brief span
  * @version 6.0
  * @date 2023-06-29
- * 
+ *
  * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
- * 
+ *
  */
 
 #pragma once
 
-#include "assertions.h"
-#include "iterator.h"
-#include "typedtransfer.h"
-#include "types.h"
+#include <mods/array.h>
+#include <mods/assertions.h>
+#include <mods/iterator.h>
+#include <mods/typedtransfer.h>
+#include <mods/types.h>
 
-namespace Mods 
+namespace Mods
 {
-    namespace Detail 
+    namespace Detail
     {
-
-        template<typename T>
-        class Span 
+        /**
+         * @tparam T 
+         */
+        template <typename T>
+        class Span
         {
         public:
             /**
@@ -36,28 +39,51 @@ namespace Mods
              * @return ALWAYS_INLINE constexpr 
              */
             ALWAYS_INLINE constexpr Span(T* values, size_t size)
-                : m_values(values)
-                , m_size(size)
-            { }
+                : m_values(values), m_size(size)
+            {
+            }
 
             /**
              * @tparam size 
              * @return ALWAYS_INLINE constexpr 
              */
-            template<size_t size>
+            template <size_t size>
             ALWAYS_INLINE constexpr Span(T (&values)[size])
-                : m_values(values)
-                , m_size(size)
-            { }
+                : m_values(values), m_size(size)
+            {
+            }
+
+            /**
+             * @tparam size 
+             * @param array 
+             * @return ALWAYS_INLINE constexpr 
+             */
+            template <size_t size>
+            ALWAYS_INLINE constexpr Span(Array<T, size>& array)
+                : m_values(array.data()), m_size(size)
+            {
+            }
+
+            /**
+             * @tparam size 
+             */
+            template <size_t size>
+                requires(IsConst<T>)
+            ALWAYS_INLINE constexpr Span(Array<T, size> const& array)
+                : m_values(array.data()), m_size(size)
+            {
+            }
 
         protected:
-            T* m_values { nullptr };
-            size_t m_size { 0 };
+            T* m_values{nullptr};
+            size_t m_size{0};
         }; // class Span
 
-
-        template<>
-        class Span<u8> 
+        /**
+         * @tparam  
+         */
+        template <>
+        class Span<u8>
         {
         public:
             /**
@@ -71,9 +97,9 @@ namespace Mods
              * @return ALWAYS_INLINE constexpr 
              */
             ALWAYS_INLINE constexpr Span(u8* values, size_t size)
-                : m_values(values)
-                , m_size(size)
-            { }
+                : m_values(values), m_size(size)
+            {
+            }
 
             /**
              * @param values 
@@ -81,19 +107,25 @@ namespace Mods
              * @return ALWAYS_INLINE 
              */
             ALWAYS_INLINE Span(void* values, size_t size)
-                : m_values(reinterpret_cast<u8*>(values))
-                , m_size(size)
-            { }
+                : m_values(reinterpret_cast<u8*>(values)), m_size(size)
+            {
+            }
 
         protected:
-            u8* m_values { nullptr };
-            size_t m_size { 0 };
-        }; // class Span 
+            u8* m_values{nullptr};
+            size_t m_size{0};
+        }; // class Span<u8>
 
-        template<>
-        class Span<u8 const> 
+        /**
+         * @tparam  
+         */
+        template <>
+        class Span<u8 const>
         {
         public:
+            /**
+             * @return ALWAYS_INLINE constexpr 
+             */
             ALWAYS_INLINE constexpr Span() = default;
 
             /**
@@ -102,9 +134,9 @@ namespace Mods
              * @return ALWAYS_INLINE constexpr 
              */
             ALWAYS_INLINE constexpr Span(u8 const* values, size_t size)
-                : m_values(values)
-                , m_size(size)
-            { }
+                : m_values(values), m_size(size)
+            {
+            }
 
             /**
              * @param values 
@@ -112,9 +144,9 @@ namespace Mods
              * @return ALWAYS_INLINE 
              */
             ALWAYS_INLINE Span(void const* values, size_t size)
-                : m_values(reinterpret_cast<u8 const*>(values))
-                , m_size(size)
-            { }
+                : m_values(reinterpret_cast<u8 const*>(values)), m_size(size)
+            {
+            }
 
             /**
              * @param values 
@@ -122,21 +154,21 @@ namespace Mods
              * @return ALWAYS_INLINE 
              */
             ALWAYS_INLINE Span(char const* values, size_t size)
-                : m_values(reinterpret_cast<u8 const*>(values))
-                , m_size(size)
-            { }
+                : m_values(reinterpret_cast<u8 const*>(values)), m_size(size)
+            {
+            }
 
         protected:
-            u8 const* m_values { nullptr };
-            size_t m_size { 0 };
-        }; // class Span<u8>
+            u8 const* m_values{nullptr};
+            size_t m_size{0};
+        }; // class Span<u8 const>
     } // namespace Detail
 
     /**
      * @tparam T 
      */
-    template<typename T>
-    class Span : public Detail::Span<T> 
+    template <typename T>
+    class Span : public Detail::Span<T>
     {
     public:
         using Detail::Span<T>::Span;
@@ -146,35 +178,35 @@ namespace Mods
         /**
          * @return ALWAYS_INLINE constexpr const* 
          */
-        [[nodiscard]] ALWAYS_INLINE constexpr T const* data() const 
-        { 
-            return this->m_values; 
+        [[nodiscard]] ALWAYS_INLINE constexpr T const* data() const
+        {
+            return this->m_values;
         }
 
         /**
          * @return ALWAYS_INLINE constexpr* 
          */
-        [[nodiscard]] ALWAYS_INLINE constexpr T* data() 
-        { 
-            return this->m_values; 
+        [[nodiscard]] ALWAYS_INLINE constexpr T* data()
+        {
+            return this->m_values;
         }
 
         /**
          * @param offset 
          * @return ALWAYS_INLINE constexpr const* 
          */
-        [[nodiscard]] ALWAYS_INLINE constexpr T const* offset_pointer(size_t offset) const 
-        { 
-            return this->m_values + offset; 
+        [[nodiscard]] ALWAYS_INLINE constexpr T const* offset_pointer(size_t offset) const
+        {
+            return this->m_values + offset;
         }
 
         /**
          * @param offset 
          * @return ALWAYS_INLINE constexpr* 
          */
-        [[nodiscard]] ALWAYS_INLINE constexpr T* offset_pointer(size_t offset) 
-        { 
-            return this->m_values + offset; 
+        [[nodiscard]] ALWAYS_INLINE constexpr T* offset_pointer(size_t offset)
+        {
+            return this->m_values + offset;
         }
 
         using ConstIterator = SimpleIterator<Span const, T const>;
@@ -183,53 +215,56 @@ namespace Mods
         /**
          * @return constexpr ConstIterator 
          */
-        constexpr ConstIterator begin() const 
-        { 
-            return ConstIterator::begin(*this); 
+        constexpr ConstIterator begin() const
+        {
+            return ConstIterator::begin(*this);
         }
 
         /**
          * @return constexpr Iterator 
          */
-        constexpr Iterator begin() 
-        { 
-            return Iterator::begin(*this); 
+        constexpr Iterator begin()
+        {
+            return Iterator::begin(*this);
         }
 
         /**
          * @return constexpr ConstIterator 
          */
-        constexpr ConstIterator end() const 
-        { 
-            return ConstIterator::end(*this); 
+        constexpr ConstIterator end() const
+        {
+            return ConstIterator::end(*this);
         }
 
         /**
          * @return constexpr Iterator 
          */
-        constexpr Iterator end() 
-        { 
-            return Iterator::end(*this); 
+        constexpr Iterator end()
+        {
+            return Iterator::end(*this);
         }
 
         /**
          * @return ALWAYS_INLINE constexpr 
          */
-        [[nodiscard]] ALWAYS_INLINE constexpr size_t size() const { return this->m_size; }
+        [[nodiscard]] ALWAYS_INLINE constexpr size_t size() const
+        {
+            return this->m_size;
+        }
 
         /**
          * @return ALWAYS_INLINE constexpr 
          */
-        [[nodiscard]] ALWAYS_INLINE constexpr bool is_null() const 
-        { 
-            return this->m_values == nullptr; 
+        [[nodiscard]] ALWAYS_INLINE constexpr bool is_null() const
+        {
+            return this->m_values == nullptr;
         }
 
         /**
-         * @return * ALWAYS_INLINE constexpr 
+         * @return ALWAYS_INLINE constexpr 
          */
-        [[nodiscard]] ALWAYS_INLINE constexpr bool is_empty() const 
-        { 
+        [[nodiscard]] ALWAYS_INLINE constexpr bool is_empty() const
+        {
             return this->m_size == 0;
         }
 
@@ -241,7 +276,7 @@ namespace Mods
         [[nodiscard]] ALWAYS_INLINE constexpr Span slice(size_t start, size_t length) const
         {
             VERIFY(start + length <= size());
-            return { this->m_values + start, length };
+            return {this->m_values + start, length};
         }
 
         /**
@@ -251,7 +286,7 @@ namespace Mods
         [[nodiscard]] ALWAYS_INLINE constexpr Span slice(size_t start) const
         {
             VERIFY(start <= size());
-            return { this->m_values + start, size() - start };
+            return {this->m_values + start, size() - start};
         }
 
         /**
@@ -261,7 +296,7 @@ namespace Mods
         [[nodiscard]] ALWAYS_INLINE constexpr Span slice_from_end(size_t count) const
         {
             VERIFY(count <= size());
-            return { this->m_values + size() - count, count };
+            return {this->m_values + size() - count, count};
         }
 
         /**
@@ -270,15 +305,12 @@ namespace Mods
          */
         [[nodiscard]] ALWAYS_INLINE constexpr Span trim(size_t length) const
         {
-            return 
-            { 
-                this->m_values, min(size(), length) 
-            };
+            return {this->m_values, min(size(), length)};
         }
 
         /**
          * @param start 
-         * @return * ALWAYS_INLINE constexpr* 
+         * @return ALWAYS_INLINE constexpr* 
          */
         [[nodiscard]] ALWAYS_INLINE constexpr T* offset(size_t start) const
         {
@@ -324,12 +356,12 @@ namespace Mods
          */
         ALWAYS_INLINE constexpr size_t fill(T const& value)
         {
-            for (size_t idx = 0; idx < size(); ++idx)
+            for(size_t idx = 0; idx < size(); ++idx)
                 data()[idx] = value;
 
             return size();
         }
-        
+
         /**
          * @param value 
          * @return true 
@@ -337,8 +369,9 @@ namespace Mods
          */
         [[nodiscard]] bool constexpr contains_slow(T const& value) const
         {
-            for (size_t i = 0; i < size(); ++i) {
-                if (at(i) == value)
+            for(size_t i = 0; i < size(); ++i)
+            {
+                if(at(i) == value)
                     return true;
             }
             return false;
@@ -351,7 +384,7 @@ namespace Mods
          */
         [[nodiscard]] bool constexpr starts_with(Span<T const> other) const
         {
-            if (size() < other.size())
+            if(size() < other.size())
                 return false;
 
             return TypedTransfer<T>::compare(data(), other.data(), other.size());
@@ -367,7 +400,6 @@ namespace Mods
             return this->m_values[index];
         }
 
-
         /**
          * @param index 
          * @return ALWAYS_INLINE constexpr& 
@@ -379,13 +411,29 @@ namespace Mods
         }
 
         /**
+         * @return ALWAYS_INLINE constexpr const& 
+         */
+        [[nodiscard]] ALWAYS_INLINE constexpr T const& last() const
+        {
+            return this->at(this->size() - 1);
+        }
+
+        /**
+         * @return ALWAYS_INLINE constexpr& 
+         */
+        [[nodiscard]] ALWAYS_INLINE constexpr T& last()
+        {
+            return this->at(this->size() - 1);
+        }
+
+        /**
          * @param index 
          * @return ALWAYS_INLINE constexpr const& 
          */
         [[nodiscard]] ALWAYS_INLINE constexpr T const& operator[](size_t index) const
         {
             return at(index);
-        }
+        }   
 
         /**
          * @param index 
@@ -403,7 +451,7 @@ namespace Mods
          */
         constexpr bool operator==(Span const& other) const
         {
-            if (size() != other.size())
+            if(size() != other.size())
                 return false;
 
             return TypedTransfer<T>::compare(data(), other.data(), size());
@@ -414,14 +462,35 @@ namespace Mods
          */
         ALWAYS_INLINE constexpr operator Span<T const>() const
         {
-            return 
-            { 
-                data(), size() 
-            };
-      }
-    }; // class Span
+            return {data(), size()};
+        }
+    }; // class Span : public Detail::Span<T>
+
+    /**
+     * @tparam T 
+     */
+    template <typename T>   
+    struct Traits<Span<T>> : public GenericTraits<Span<T>>
+    {
+        /**
+         * @param span 
+         * @return unsigned 
+         */
+        static unsigned hash(Span<T> const& span)
+        {
+            unsigned hash = 0;
+            for(auto const& value : span)
+            {
+                auto value_hash = Traits<T>::hash(value);
+                hash = pair_int_hash(hash, value_hash);
+            }
+            return hash;
+        }
+    }; // struct Traits<Span<T>> : public GenericTraits<Span<T>>
+
     using ReadonlyBytes = Span<u8 const>;
     using Bytes = Span<u8>;
+
 } // namespace Mods
 
 using Mods::Bytes;
