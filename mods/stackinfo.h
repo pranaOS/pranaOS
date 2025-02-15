81a57df1f0e5d08209d5c9185a0a6f088ba8936d
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-15 16:35:55 +0530

    mods: stackinfo

--- mods/stackinfo.h
+++ mods/stackinfo.h
@@ -4,51 +4,45 @@
  * @brief StackInfo
  * @version 6.0
  * @date 2023-07-25
- * 
+ *
  * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
- * 
+ *
  */
 
-#pragma once 
+#pragma once
 
-#include "types.h"
+#include <mods/types.h>
 
-
-namespace Mods 
+namespace Mods
 {
-
-    class StackInfo 
+    class StackInfo
     {
     public:
-        StackInfo();
+        /**
+         * @brief Construct a new Stack Info object
+         * 
+         */
+        StackInfo();    
 
         /**
          * @return FlatPtr 
          */
-        FlatPtr base() const 
-        { 
-            return m_base; 
+        FlatPtr base() const
+        {
+            return m_base;
         }
-
-        /**
-         * @return FlatPtr 
-         */
-        FlatPtr top() const 
-        { 
-            return m_top; 
+        FlatPtr top() const
+        {
+            return m_top;
         }
 
         /**
          * @return size_t 
          */
-        size_t size() const 
-        { 
-            return m_size; 
+        size_t size() const
+        {
+            return m_size;
         }
-
-        /**
-         * @return size_t 
-         */
         size_t size_free() const
         {
             FlatPtr dummy;
@@ -60,7 +54,6 @@ namespace Mods
         FlatPtr m_top;
         size_t m_size;
     }; // class StackInfo
-
 } // namespace Mods
 
 using Mods::StackInfo;
