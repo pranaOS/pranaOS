4b5a30dcb0e72c1d042aaf27220c3e687b1ce19e
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-15 15:52:23 +0530

    mods: sourcelocation

--- mods/sourcelocation.h
+++ mods/sourcelocation.h
@@ -14,3 +14,22 @@
 #include <mods/format.h>
 #include <mods/stringview.h>
 #include <mods/types.h>
+
+namespace Mods
+{
+    class SourceLocation
+    {
+    private:
+        constexpr SourceLocation(char const* const file, u32 line, char* function)
+            : m_function(function)
+            , m_file(file)
+            , m_line(line)
+        {}
+
+        char const* const m_function { nullptr };
+        char const* const m_file { nullptr };
+        const u32 m_line { 0 };
+    };
+}
+
+using Mods::SourceLocation;
