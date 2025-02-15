81e3e18998776af3f6235837fd0587d1bb1813a4
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-15 16:07:08 +0530

    mods: stack

--- mods/stack.h
+++ mods/stack.h
@@ -4,23 +4,23 @@
  * @brief Stack
  * @version 6.0
  * @date 2024-11-06
- * 
+ *
  * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
- * 
+ *
  */
 
 #pragma once
 
 #include <mods/vector.h>
 
-namespace Mods 
+namespace Mods
 {
     /**
      * @tparam T 
      * @tparam stack_size 
      */
-    template<typename T, size_t stack_size>
-    class Stack 
+    template <typename T, size_t stack_size>
+    class Stack
     {
     public:
         /**
@@ -42,7 +42,7 @@ namespace Mods
          */
         bool push(const T& item)
         {
-            if (m_stack.size() >= stack_size)
+            if(m_stack.size() >= stack_size)
                 return false;
 
             m_stack.unchecked_append(item);
@@ -56,18 +56,25 @@ namespace Mods
          */
         bool push(T&& item)
         {
-            if (m_stack.size() >= stack_size)
+            if(m_stack.size() >= stack_size)
                 return false;
 
             m_stack.unchecked_append(move(item));
             return true;
         }
 
+        /**
+         * @return true 
+         * @return false 
+         */
         bool is_empty() const
         {
             return m_stack.is_empty();
         }
 
+        /**
+         * @return size_t 
+         */
         size_t size() const
         {
             return m_stack.size();
@@ -75,18 +82,24 @@ namespace Mods
 
         bool pop()
         {
-            if (is_empty())
+            if(is_empty())
                 return false;
 
             m_stack.resize_and_keep_capacity(m_stack.size() - 1);
             return true;
         }
 
+        /**
+         * @return T& 
+         */
         T& top()
         {
             return m_stack.last();
         }
 
+        /**
+         * @return const T& 
+         */
         const T& top() const
         {
             return m_stack.last();
