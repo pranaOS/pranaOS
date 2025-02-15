6405a360ee3322864e7bb6ff583f33a7cba9b390
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-15 20:19:52 +0530

    mods: stringbuilder

--- mods/stringbuilder.cpp
+++ mods/stringbuilder.cpp
@@ -18,5 +18,45 @@
 
 namespace Mods
 {
+    /**
+     * @param size 
+     * @return ErrorOr<void> 
+     */
+    inline ErrorOr<void> StringBuilder::will_append(size_t size)
+    {
+        Checked<size_t> needed_capacity = m_buffer.size();
+        needed_capacity += size;
+        VERIFY(!needed_capacity.has_overflow());
 
+        if (needed_capacity <= m_buffer.capacity())
+            return {};
+        
+        Checked<size_t> expanded_capacity = needed_capacity;
+        expanded_capacity *= 2;
+        VERIFY(!expanded_capacity.has_overflow());
+        TRY(m_buffer.try_ensure_capacity(expanded_capacity.value()));
+        return {};
+    }
+
+    /**
+     * @param initial_capacity 
+     */
+    StringBuilder::StringBuilder(size_t initial_capacity)
+    {
+        m_buffer.ensure_capacity(initial_capacity);
+    }
+
+    /**
+     * @param string 
+     * @return ErrorOr<void> 
+     */
+    ErrorOr<void> StringBuilder::try_append(StringView string)
+    {
+        if (string.is_empty())
+            return {};
+        
+        TRY(will_append(string.length()));
+        TRY(m_buffer.try_append(string.characters_without_null_termination()));
+        return {};
+    }
 } // namespace Mods
