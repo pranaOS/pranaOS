82a5bc65a31ad041624ba50e9a85b4be86d374e4
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-16 12:57:59 +0530

    mods: tuple

---
+++ mods/tuple.h
@@ -0,0 +1,11 @@
+/**
+ * @file tuple.h
+ * @author Krisna Pranav
+ * @brief tuple
+ * @version 6.0
+ * @date 2025-02-16
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
