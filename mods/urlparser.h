63630258e78169df38980ed52ba47519dd96f0d6
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-16 16:50:35 +0530

    mods: urlparser

--- mods/urlparser.h
+++ mods/urlparser.h
@@ -4,31 +4,89 @@
  * @brief url_parser
  * @version 6.0
  * @date 2023-06-29
- * 
+ *
  * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
- * 
+ *
  */
 
 #pragma once
 
-#include "forward.h"
+#include <mods/optional.h>
+#include <mods/stringview.h>
+#include <mods/url.h>
 
-namespace Mods {
+namespace Mods
+{
+
+#define ENUMERATE_STATES                 \
+    STATE(SchemeStart)                   \
+    STATE(Scheme)                        \
+    STATE(NoScheme)                      \
+    STATE(SpecialRelativeOrAuthority)    \
+    STATE(PathOrAuthority)               \
+    STATE(Relative)                      \
+    STATE(RelativeSlash)                 \
+    STATE(SpecialAuthoritySlashes)       \
+    STATE(SpecialAuthorityIgnoreSlashes) \
+    STATE(Authority)                     \
+    STATE(Host)                          \
+    STATE(Hostname)                      \
+    STATE(Port)                          \
+    STATE(File)                          \
+    STATE(FileSlash)                     \
+    STATE(FileHost)                      \
+    STATE(PathStart)                     \
+    STATE(Path)                          \
+    STATE(CannotBeABaseUrlPath)          \
+    STATE(Query)                         \
+    STATE(Fragment)
+
+class URLParser
+{
+  public:
+    enum class State
+    {
+#define STATE(state) state,
+        ENUMERATE_STATES
+#undef STATE
+    }; // enum class State
+
     /**
-     * @brief urlencode
-     * 
-     * @return String 
+     * @param state 
+     * @return char const* 
      */
-    String urlencode(const StringView&);
+    static char const* state_name(State const& state)
+    {
+        switch(state)
+        {
+#define STATE(state)   \
+    case State::state: \
+        return #state;
+            ENUMERATE_STATES
+#undef STATE
+        }
+        VERIFY_NOT_REACHED();
+    }
 
     /**
-     * @brief urldecode 
-     * 
-     * @return String 
+     * @param input 
+     * @param base_url 
+     * @param url 
+     * @param state_override 
+     * @return URL 
      */
-    String urldecode(const StringView&);
+    static URL parse(StringView input, URL const* base_url = nullptr, Optional<URL> url = {}, Optional<State> state_override = {});
 
-}
+  private:
+    /**
+     * @param raw_input 
+     * @return Optional<URL> 
+     */
+    static Optional<URL> parse_data_url(StringView raw_input);
+}; // class URLParser
 
-using Mods::urldecode;
-using Mods::urlencode;
+#undef ENUMERATE_STATES
+
+} // namespace Mods
+
+using Mods::URLParser;
