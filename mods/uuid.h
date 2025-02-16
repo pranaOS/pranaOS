445de2dd4a2d0c98053ee91d02a8f5ba90728263
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-16 17:32:33 +0530

    mods: uuid

--- mods/uuid.h
+++ mods/uuid.h
@@ -13,6 +13,24 @@ namespace Mods
 {
     class UUID
     {
+    public:
+        enum class Endianness
+        {
+            Mixed,
+            Little
+        }; // enum class Endianness
 
+        /**
+         * @brief Construct a new UUID object
+         * 
+         */
+        UUID() = default;
+    
+    private:
+        void convert_string_view_to_little_endian_uuid(StringView);
+
+        void convert_string_view_to_mixed_endian_uuid(StringView);
+
+        Array<u8, 16> m_uuid_buffer {};
     }; // class UUID
 }
