f81a71daba855b3c210ce8b79488fb3426a6c951
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-16 12:17:11 +0530

    mods: temporarychange

--- mods/temporarychange.h
+++ mods/temporarychange.h
@@ -4,16 +4,21 @@
  * @brief TemporaryChange
  * @version 6.0
  * @date 2023-06-29
- * 
+ *
  * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
- * 
+ *
  */
 
 #pragma once
 
-namespace Mods {
-    template<typename T>
-    class TemporaryChange {
+namespace Mods
+{
+    /**
+     * @tparam T 
+     */
+    template <typename T>
+    class TemporaryChange
+    {
     public:
         /**
          * @brief Construct a new Temporary Change object
@@ -22,22 +27,24 @@ namespace Mods {
          * @param value 
          */
         TemporaryChange(T& variable, T value)
-            : m_variable(variable)
-            , m_old_value(variable)
+            : m_variable(variable), m_old_value(variable)
         {
             m_variable = value;
         }
-        
+
         /**
          * @brief Destroy the Temporary Change object
          * 
          */
-        ~TemporaryChange() { m_variable = m_old_value; }
+        ~TemporaryChange()
+        {
+            m_variable = m_old_value;
+        }
 
     private:
         T& m_variable;
         T m_old_value;
-    };
-}
+    }; // class TemporaryChange
+} // namespace Mods
 
 using Mods::TemporaryChange;
