c440ebc2265f010ca9d9379fe91face70ce3125b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-16 17:31:27 +0530

    mods: uuid

---
+++ mods/uuid.h
@@ -0,0 +1,18 @@
+/**
+ * @file uuid.h
+ * @author Krisna Pranav
+ * @brief UUID
+ * @version 6.0
+ * @date 2025-02-16
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+namespace Mods 
+{
+    class UUID
+    {
+
+    }; // class UUID
+}
