7f51dde82ca530a30f044432effa69a15a49f879
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-16 14:57:42 +0530

    mods: unicodeutils

---
+++ mods/unicodeutils.h
@@ -0,0 +1,19 @@
+/**
+ * @file unicodeutils.h
+ * @author Krisna Pranav
+ * @brief Unicode Utils
+ * @version 6.0
+ * @date 2025-02-16
+ * 
+ * @copyright Copyright (c) 2021-2025, pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <mods/forward.h>
+
+namespace Mods::UnicodeUtils
+{
+
+} // namespace Mods::UnicodeUtils
