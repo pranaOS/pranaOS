f2815d66d787913e4ed9d79d967e3a4a377212e9
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-16 13:31:13 +0530

    mods: ufixedbigint

---
+++ mods/ufixedbigint.h
@@ -0,0 +1,10 @@
+/**
+ * @file ufixedbigint.h
+ * @author Krisna Pranav
+ * @brief fixed big int
+ * @version 6.0
+ * @date 2025-02-16
+ * 
+ * @copyright Copyright (c) 2021-2025, pranaOS Developers, Krisna Pranav
+ * 
+ */
