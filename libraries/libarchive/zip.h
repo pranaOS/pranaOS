b2429c8daf2c461aa53b4bd84ec350eaba7e16e7
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-16 18:16:01 +0530

    libraries: libarchive[zip]

--- libraries/libarchive/zip.h
+++ libraries/libarchive/zip.h
@@ -8,3 +8,18 @@
  * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
  * 
  */
+
+#pragma once
+
+#include <mods/array.h>
+#include <mods/function.h>
+#include <mods/iterationdecision.h>
+#include <mods/stream.h>
+#include <mods/string.h>
+#include <mods/vector.h>
+#include <string.h>
+
+namespace Archive
+{
+    
+} // namespace Archive
