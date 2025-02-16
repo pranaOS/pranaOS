9a22d2ba887560d24473edfc9c5710a01d9f117a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-16 18:15:20 +0530

    libraries: libarchive[zip]

---
+++ libraries/libarchive/zip.h
@@ -0,0 +1,10 @@
+/**
+ * @file zip.h
+ * @author Krisna Pranav
+ * @brief zip
+ * @version 6.0
+ * @date 2025-02-16
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
