b448b47e62b419a3b25c13f22b36c6053d06f0ba
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-16 12:56:59 +0530

    mods: try

--- mods/try.h
+++ mods/try.h
@@ -4,19 +4,19 @@
  * @brief try
  * @version 6.0
  * @date 2024-08-30
- * 
+ *
  * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
- * 
+ *
  */
 
 #pragma once
 
-#define TRY(expression)                                \
-    ({                                                 \
-        auto _temporary_result = (expression);         \
-        if (_temporary_result.is_error()) [[unlikely]] \
-            return _temporary_result.release_error();  \
-        _temporary_result.release_value();             \
+#define TRY(expression)                               \
+    ({                                                \
+        auto _temporary_result = (expression);        \
+        if(_temporary_result.is_error()) [[unlikely]] \
+            return _temporary_result.release_error(); \
+        _temporary_result.release_value();            \
     })
 
 #define MUST(expression)                       \
