3cf5101e5b6c4a4bb5f749a9870304fd6e2861e4
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-16 12:35:04 +0530

    mods: trie

---
+++ mods/trie.h
@@ -0,0 +1,11 @@
+/**
+ * @file trie.h
+ * @author Krisna Pranav
+ * @brief Trie
+ * @version 6.0
+ * @date 2025-02-16
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
