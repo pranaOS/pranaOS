fa6e879c8dd03664ecef23060e6a287a6052586a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-17 22:11:59 +0530

    libraries: libraries[tararchive]

---
+++ libraries/libarchive/tarstream.h
@@ -0,0 +1,36 @@
+/**
+ * @file tarstream.h
+ * @author Krisna Pranav
+ * @brief Tar Stream
+ * @version 6.0
+ * @date 2025-02-17
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <mods/span.h>
+#include <mods/stream.h>
+#include <libarchive/tar.h>
+
+namespace Archive
+{
+    class TarInputStream;
+
+    class TarFileStream : public InputStream
+    {
+        
+    }; // class TarFileStream : public InputStream
+
+    class TarInputStream
+    {
+
+    }; // class TarInputStream
+
+    class TarOutputStream 
+    {
+
+    }; // class TarOutputStream 
+} // namespace Archive
