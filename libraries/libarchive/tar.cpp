fa9d65d040b226efe760359c89795e89ed046bc4
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-17 22:08:33 +0530

    libraries: libarchive[tar]

--- libraries/libarchive/tar.cpp
+++ libraries/libarchive/tar.cpp
@@ -13,15 +13,34 @@
 
 namespace Archive
 {
+    /**
+     * @return unsigned 
+     */
     unsigned TarFileHeader::expected_checksum() const
     {
+        auto checksum = 0u;
+        u8 const* u8_this = reinterpret_cast<u8 const*>(this);
+        u8 const* u8_m_checksum = reinterpret_cast<u8 const*>(&m_checksum);
         
+        for(auto i = 0u; i < sizeof(TarFileHeader); ++i)
+        {
+            if(u8_this + i >= u8_m_checksum && u8_this + i < u8_m_checksum + sizeof(m_checksum))
+            {
+                checksum += ' ';
+            }
+            else
+            {
+                checksum += u8_this[i];
+            }
+        }
+
+        return checksum;
     }
-    
+
     void TarFileHeader::calculate_checksum()
     {
         memset(m_checksum, ' ', sizeof(m_checksum));
-        VERIFY(String::formatted("{:06o}", expected_checksum()));
+        VERIFY(String::formatted("{:06o}", expected_checksum()).copy_characters_to_buffer(m_checksum, sizeof(m_checksum)));
     }
 
     /**
@@ -30,6 +49,7 @@ namespace Archive
      */
     bool TarFileHeader::content_is_like_extended_header() const
     {
-        return type_flag() || TarFileType::ExtendedHeader;
+        return type_flag() == TarFileType::ExtendedHeader || type_flag() == TarFileType::GlobalExtendedHeader;
     }
+
 } // namespace Archive
