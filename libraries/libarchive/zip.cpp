5ed092885d27de7401446dd929a4dd8425389345
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-17 19:22:08 +0530

    libraries: libarchive[zip]

--- libraries/libarchive/zip.cpp
+++ libraries/libarchive/zip.cpp
@@ -41,4 +41,65 @@ namespace Archive
 
         return false;
     }
+
+    /**
+     * @param buffer 
+     * @return Optional<Zip> 
+     */
+    Optional<Zip> Zip::try_create(ReadonlyBytes buffer)
+    {
+        size_t end_of_central_directory_offset;
+
+        if (!find_end_of_central_directory_offset(buffer))
+            return {};
+        
+        EndOfCentralDirectory end_of_central_directory {};
+
+        if (!end_of_central_directory.read(buffer.size()))
+            return {};
+
+        if (end_of_central_directory.disk_number != 0) {
+            return {};
+        }
+        
+        size_t member_offset = end_of_central_directory.central_directory_offset;
+
+        return Zip {
+            end_of_central_directory.total_records_count,
+            buffer,
+        };
+    }
+
+    /**
+     * @param callback 
+     * @return true 
+     * @return false 
+     */
+    bool Zip::for_each_member(Function<IterationDecision(ZipMember const&)> callback)
+    {
+        size_t member_offset = m_members_start_offset
+
+        for (size_t i = 0; i < m_member_count; i++) {
+            CentralDirectoryRecord central_directory_record {};
+            VERIFY(central_directory_record.read(m_input_data.slice(member_offset)));
+        }
+    }
+
+    /**
+     * @brief Construct a new ZipOutputStream::ZipOutputStream object
+     * 
+     * @param stream 
+     */
+    ZipOutputStream::ZipOutputStream(OutputStream& stream)
+        : m_stream(stream)
+    {}
+
+    /**
+     * @param method 
+     * @return u16 
+     */
+    static u16 minimum_version_needed(ZipCompressionMethod method)
+    {
+        return method == ZipCompressionMethod::Deflate ? 20 : 10;
+    }
 } // namespace Archive
