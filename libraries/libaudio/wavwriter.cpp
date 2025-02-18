0a49fcf60c8c555a7496aac667e8d9db9928f7c0
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-18 19:25:53 +0530

    libraries: libaudio[wavwriter]

---
+++ libraries/libaudio/wavwriter.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file wavwriter.cpp
+ * @author Krisna Pranav
+ * @brief wav writer
+ * @version 6.0
+ * @date 2025-02-18
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include <libaudio/wavwriter.h>
+
+namespace Audio
+{
+
+} // namespace Audio
