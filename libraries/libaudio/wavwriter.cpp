da59baf4440504dba26e3ec6da318bf713b061ea
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-18 19:30:14 +0530

    libraries: libaudio[wavwriter]

--- libraries/libaudio/wavwriter.cpp
+++ libraries/libaudio/wavwriter.cpp
@@ -4,9 +4,9 @@
  * @brief wav writer
  * @version 6.0
  * @date 2025-02-18
- * 
+ *
  * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
- * 
+ *
  */
 
 #include <libaudio/wavwriter.h>
@@ -19,12 +19,24 @@ namespace Audio
      * @param path 
      * @param sample_rate 
      * @param num_channels 
+     * @param bits_per_sample 
      */
-    WavWriter::WavWriter(StringView path, int sample_rate, u16 num_channels)
-        : m_sample_rate(sample_rate)
-        , m_num_channels(num_channels)
+    WavWriter::WavWriter(StringView path, int sample_rate, u16 num_channels, u16 bits_per_sample)
+        : m_sample_rate(sample_rate), m_num_channels(num_channels), m_bits_per_sample(bits_per_sample)
     {
         set_file(path);
+    }
+
+    /**
+     * @brief Construct a new WavWriter::WavWriter object
+     * 
+     * @param sample_rate 
+     * @param num_channels 
+     * @param bits_per_sample 
+     */
+    WavWriter::WavWriter(int sample_rate, u16 num_channels, u16 bits_per_sample)
+        : m_sample_rate(sample_rate), m_num_channels(num_channels), m_bits_per_sample(bits_per_sample)
+    {
     }
 
     /**
@@ -33,8 +45,24 @@ namespace Audio
      */
     WavWriter::~WavWriter()
     {
-        if (!m_finalized)
+        if(!m_finalized)
             finalize();
+    }
+
+    /**
+     * @param path 
+     */
+    void WavWriter::set_file(StringView path)
+    {
+        m_file = Core::File::construct(path);
+        if(!m_file->open(Core::OpenMode::ReadWrite))
+        {
+            m_error_string = String::formatted("Can't open file: {}", m_file->error_string());
+            return;
+        }
+
+        m_file->seek(44);
+        m_finalized = false;
     }
 
     /**
@@ -52,7 +80,8 @@ namespace Audio
         VERIFY(!m_finalized);
         m_finalized = true;
 
-        if (m_file) {
+        if(m_file)
+        {
             m_file->seek(0);
             write_header();
             m_file->close();
@@ -63,6 +92,40 @@ namespace Audio
 
     void WavWriter::write_header()
     {
-        
+        static u32 riff = 0x46464952;
+        m_file->write(reinterpret_cast<u8*>(&riff), sizeof(riff));
+
+        u32 sz = m_data_sz + (44 - 4 - 4);
+        m_file->write(reinterpret_cast<u8*>(&sz), sizeof(sz));
+
+        static u32 wave = 0x45564157;
+        m_file->write(reinterpret_cast<u8*>(&wave), sizeof(wave));
+
+        static u32 fmt_id = 0x20746D66;
+        m_file->write(reinterpret_cast<u8*>(&fmt_id), sizeof(fmt_id));
+
+        static u32 fmt_size = 16;
+        m_file->write(reinterpret_cast<u8*>(&fmt_size), sizeof(fmt_size));
+
+        static u16 audio_format = 1;
+        m_file->write(reinterpret_cast<u8*>(&audio_format), sizeof(audio_format));
+
+        m_file->write(reinterpret_cast<u8*>(&m_num_channels), sizeof(m_num_channels));
+
+        m_file->write(reinterpret_cast<u8*>(&m_sample_rate), sizeof(m_sample_rate));
+
+        u32 byte_rate = m_sample_rate * m_num_channels * (m_bits_per_sample / 8);
+        m_file->write(reinterpret_cast<u8*>(&byte_rate), sizeof(byte_rate));
+
+        u16 block_align = m_num_channels * (m_bits_per_sample / 8);
+        m_file->write(reinterpret_cast<u8*>(&block_align), sizeof(block_align));
+
+        m_file->write(reinterpret_cast<u8*>(&m_bits_per_sample), sizeof(m_bits_per_sample));
+
+        static u32 chunk_id = 0x61746164;
+        m_file->write(reinterpret_cast<u8*>(&chunk_id), sizeof(chunk_id));
+
+        m_file->write(reinterpret_cast<u8*>(&m_data_sz), sizeof(m_data_sz));
     }
+
 } // namespace Audio
