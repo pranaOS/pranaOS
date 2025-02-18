f26a98818a46d2e99d9bfc8f0fc5d67fa4aa330b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-18 19:21:38 +0530

    libraries: libaudio[wavwriter]

--- libraries/libaudio/wavwriter.h
+++ libraries/libaudio/wavwriter.h
@@ -21,7 +21,69 @@ namespace Audio
     {
         MOD_MAKE_NONCOPYABLE(WavWriter);
         MOD_MAKE_NONMOVABLE(WavWriter);
+    public:
+        /**
+         * @brief Construct a new Wav Writer object
+         * 
+         * @param path 
+         */
+        WavWriter(StringView path, int_sample_rate = 44100);
 
+        /**
+         * @brief Construct a new Wav Writer object
+         * 
+         * @param sample_rate 
+         * @param num_channels 
+         */
+        WavWriter(int sample_rate = 44100, u16 num_channels = 2);
+
+        /**
+         * @brief Destroy the Wav Writer object
+         * 
+         */
+        ~WavWriter();
+
+        /**
+         * @return true 
+         * @return false 
+         */
+        bool has_error() const
+        {
+            return !m_error_string.is_null();
+        }
+
+        /**
+         * @return char const* 
+         */
+        char const* error_string() const
+        {
+            return m_error_string;
+        }
+
+        /**
+         * @return RefPtr<Core::File> 
+         */
+        RefPtr<Core::File> file() const
+        {
+            return m_file;
+        }
+
+        /**
+         * @brief Set the file object
+         * 
+         * @param path 
+         */
+        void set_file(StringView path);
+
+        /**
+         * @brief Set the num channels object
+         * 
+         * @param num_channels 
+         */
+        void set_num_channels(int num_channels)
+        {
+            m_num_channels = num_channels;
+        }
     private:
         void write_header();
         RefPtr<Core::File> m_file;
