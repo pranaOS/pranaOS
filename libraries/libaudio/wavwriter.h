ea1e7c7bcbf1f63dbb8dfcc42fafa74940246661
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-18 19:23:27 +0530

    libraries: libaudio[wavwriter]

--- libraries/libaudio/wavwriter.h
+++ libraries/libaudio/wavwriter.h
@@ -4,43 +4,44 @@
  * @brief WAV Writer
  * @version 6.0
  * @date 2025-02-18
- * 
+ *
  * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
- * 
+ *
  */
 
 #pragma once
 
+#include <libcore/file.h>
 #include <mods/noncopyable.h>
 #include <mods/stringview.h>
-#include <libcore/file.h>
 
 namespace Audio
 {
+
     class WavWriter
     {
         MOD_MAKE_NONCOPYABLE(WavWriter);
         MOD_MAKE_NONMOVABLE(WavWriter);
+
     public:
         /**
-         * @brief Construct a new Wav Writer object
+         * @brief Construct a new WavWriter object
          * 
          * @param path 
+         * @param sample_rate 
+         * @param num_channels 
+         * @param bits_per_sample 
          */
-        WavWriter(StringView path, int_sample_rate = 44100);
+        WavWriter(StringView path, int sample_rate = 44100, u16 num_channels = 2, u16 bits_per_sample = 16);
 
         /**
-         * @brief Construct a new Wav Writer object
+         * @brief Construct a new WavWriter object
          * 
          * @param sample_rate 
          * @param num_channels 
+         * @param bits_per_sample 
          */
-        WavWriter(int sample_rate = 44100, u16 num_channels = 2);
-
-        /**
-         * @brief Destroy the Wav Writer object
-         * 
-         */
+        WavWriter(int sample_rate = 44100, u16 num_channels = 2, u16 bits_per_sample = 16);
         ~WavWriter();
 
         /**
@@ -57,7 +58,39 @@ namespace Audio
          */
         char const* error_string() const
         {
-            return m_error_string;
+            return m_error_string.characters();
+        }
+
+        /**
+         * @param samples 
+         * @param size 
+         */
+        void write_samples(u8 const* samples, size_t size);
+        
+        void finalize(); 
+
+        /**
+         * @return u32 
+         */
+        u32 sample_rate() const
+        {
+            return m_sample_rate;
+        }
+        
+        /**
+         * @return u16 
+         */
+        u16 num_channels() const
+        {
+            return m_num_channels;
+        }
+
+        /**
+         * @return u16 
+         */
+        u16 bits_per_sample() const
+        {
+            return m_bits_per_sample;
         }
 
         /**
@@ -84,15 +117,42 @@ namespace Audio
         {
             m_num_channels = num_channels;
         }
+
+        /**
+         * @brief Set the sample rate object
+         * 
+         * @param sample_rate 
+         */
+        void set_sample_rate(int sample_rate)
+        {
+            m_sample_rate = sample_rate;
+        }
+        
+        /**
+         * @brief Set the bits per sample object
+         * 
+         * @param bits_per_sample 
+         */
+        void set_bits_per_sample(int bits_per_sample)
+        {
+            m_bits_per_sample = bits_per_sample;
+        }
+
+        void clear_error()
+        {
+            m_error_string = String();
+        }
+
     private:
         void write_header();
         RefPtr<Core::File> m_file;
         String m_error_string;
-        bool m_finalized { false }
+        bool m_finalized{false};
 
         u32 m_sample_rate;
         u16 m_num_channels;
         u16 m_bits_per_sample;
-        u32 m_data_sz { 0 };
+        u32 m_data_sz{0};
     }; // class WavWriter
+
 } // namespace Audio
