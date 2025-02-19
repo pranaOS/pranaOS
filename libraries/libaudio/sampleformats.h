e1e23b4001aa2b3bce917c0cf773af2d2888c66a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-19 20:56:53 +0530

    libraries: libaudio[sampleformats]

--- libraries/libaudio/sampleformats.h
+++ libraries/libaudio/sampleformats.h
@@ -4,9 +4,9 @@
  * @brief Sample Formats
  * @version 6.0
  * @date 2025-02-19
- * 
+ *
  * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
- * 
+ *
  */
 
 #pragma once
@@ -16,9 +16,15 @@
 
 namespace Audio
 {
+
     enum class PcmSampleFormat : u8
     {
-
+        Uint8,
+        Int16,
+        Int24,
+        Int32,
+        Float32,
+        Float64,
     }; // enum class PcmSampleFormat : u8
 
     /**
@@ -26,10 +32,11 @@ namespace Audio
      * @return u16 
      */
     u16 pcm_bits_per_sample(PcmSampleFormat format);
-    
+
     /**
      * @param format 
      * @return String 
      */
     String sample_format_name(PcmSampleFormat format);
+    
 } // namespace Audio
