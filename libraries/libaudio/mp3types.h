0862cdb9c67af7d6494ed862d3f83874a989f3b2
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-19 21:07:16 +0530

    libraries: libraries[mp3types]

--- libraries/libaudio/mp3types.h
+++ libraries/libaudio/mp3types.h
@@ -17,5 +17,32 @@
 
 namespace Audio::MP3
 {
-     
+    struct Granule
+    {
+
+    }; // struct Granule
+
+    struct Channel
+    {
+        Array<Granule, 2> granules;
+        Array<int, 39> scale_factors;
+        Array<int, 4> scale_factor_selection_info;
+    }; // struct Channel
+
+    struct MP3Frame
+    {
+        Header header;
+        FixedArray<Channel> channels;
+        off_t main_data_begin;
+        u32 private_bits;
+        
+        /**
+         * @brief Construct a new MP3Frame object
+         * 
+         * @param header 
+         */
+        MP3Frame(Header header)
+            : header(header)
+            , channels(FixedArray<Channel>::must_create_but_fixme_should_propagate_errors());
+    }; // struct MP3Frame
 } // namespace Audio::MP3
