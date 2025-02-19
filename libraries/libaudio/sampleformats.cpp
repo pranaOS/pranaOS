b12adb43b83d56eabf98259bc1bc4612427cf94b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-19 20:57:28 +0530

    libraries: libaudio[sampleformats]

---
+++ libraries/libaudio/sampleformats.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file sampleformats.cpp
+ * @author Krisna Pranav
+ * @brief Sample Formats
+ * @version 6.0
+ * @date 2025-02-19
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include "sampleformats.h"
+
+namespace Audio
+{
+
+} // namespace Audio
