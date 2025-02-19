ddd05262b32b22f8b83b06654684a6a0e3acaaf9
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-19 21:05:44 +0530

    libraries: libraries[mp3types]

---
+++ libraries/libaudio/mp3types.h
@@ -0,0 +1,21 @@
+/**
+ * @file mp3types.h
+ * @author Krisna Pranav
+ * @brief mp3 types
+ * @version 6.0
+ * @date 2025-02-19
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <mods/array.h>
+#include <mods/enumbits.h>
+#include <mods/fixedarray.h>
+
+namespace Audio::MP3
+{
+     
+} // namespace Audio::MP3
