a9d15fdbe99fe62322325b16029ad0ab821715d0
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-20 23:20:21 +0530

    libraries: libaudio[usersamplequeue]

---
+++ libraries/libaudio/usersamplequeue.cpp
@@ -0,0 +1,37 @@
+/**
+ * @file usersamplequeue.cpp
+ * @author Krisna Pranav
+ * @brief User Sample Queue 
+ * @version 6.0
+ * @date 2025-02-20
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include "usersamplequeue.h"
+
+namespace Audio
+{
+    void UserSampleQueue::clear()
+    {
+        discard_samples(size());
+    }
+
+    void UserSampleQueue::fix_spans()
+    {
+        Threading::MutexLocker lock(m_sample_mutex);
+        m_enqueued_samples = m_backing_samples.spans();
+        m_samples_to_discard = 0;
+    }
+
+    /**
+     * @param index 
+     * @return Sample 
+     */
+    Sample UserSampleQueue::operator[](size_t index)
+    {
+        Threading::MutexLocker lock(m_sample_mutex)
+        return m_enqueued_samples[index];
+    }
+} // namespace Audio
