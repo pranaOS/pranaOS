bbe8e80e502c921d4d786ca42aa169e6024e0157
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-20 22:23:04 +0530

    libraries: libaudio[flacloader]

--- libraries/libaudio/flacloader.h
+++ libraries/libaudio/flacloader.h
@@ -23,5 +23,34 @@
 
 namespace Audio
 {
+    using Core::Stream::BigEndianInputBitStream;
     
+    constexpr size_t FLAC_BUFFER_SIZE = 8 * KiB;
+
+    class FlacLoaderPlugin : public LoaderPlugin
+    {
+    private:
+        MaybeLoaderError parse_header();
+
+        /**
+         * @param bitstream 
+         * @return ErrorOr<FlacRawMetadataBlock, LoaderError> 
+         */
+        ErrorOr<FlacRawMetadataBlock, LoaderError> next_meta_block(BigEndianInputBitStream& bitstream);
+
+        /**
+         * @return MaybeLoaderError 
+         */
+        MaybeLoaderError next_frame(Span<Sample>);
+
+        /**
+         * @return ErrorOr<FlacSubframeHeader, LoaderError> 
+         */
+        ErrorOr<FlacSubframeHeader, LoaderError> next_subframe_header();
+
+        /**
+         * @return ALWAYS_INLINE 
+         */
+        ALWAYS_INLINE ErrorOr<Vector<i32>, LoaderError> decode_rice_partition();
+    }; // class FlacLoaderPlugin : public LoaderPlugin
 } // namespace Audio
