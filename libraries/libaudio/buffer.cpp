4932908356b71c95b25bcec0a7a0ab2ddb10590c
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-20 22:46:13 +0530

    libraries: libaudio[buffer]

---
+++ libraries/libaudio/buffer.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file buffer.cpp
+ * @author Krisna Pranav
+ * @brief buffer
+ * @version 6.0
+ * @date 2025-02-20
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include "buffer.h"
+#include <mods/atomic.h>
+#include <mods/debug.h>
+#include <mods/stdlibextra.h>
+#include <mods/string.h>
+
+namespace Audio
+{
+} // namespace Audio
