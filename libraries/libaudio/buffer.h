318e0c16d18c5d1b6c85a05f7a08ca2c5f26daae
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-20 22:34:14 +0530

    libraries: libaudio[buffer]

--- libraries/libaudio/buffer.h
+++ libraries/libaudio/buffer.h
@@ -8,3 +8,29 @@
  * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
  * 
  */
+
+
+#pragma once
+
+#include "mods/typedtransfer.h"
+#include <mods/bytebuffer.h>
+#include <mods/error.h>
+#include <mods/fixedarray.h>
+#include <mods/memorystream.h>
+#include <mods/nonnullrefptr.h>
+#include <mods/refptr.h>
+#include <mods/string.h>
+#include <mods/types.h>
+#include <mods/vector.h>
+#include <mods/kmalloc.h>
+#include <libaudio/resampler.h>
+#include <libaudio/sample.h>
+#include <libaudio/sampleformats.h>
+#include <libcore/anonymousbuffer.h>
+#include <libcore/sharedcircularqueue.h>
+#include <string.h>
+
+namespace Audio
+{
+
+} // namespace Audio
