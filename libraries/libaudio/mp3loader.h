84f97c03046e02ba1fa38668d66f246bd4fdea96
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-20 20:07:23 +0530

    libraries: libaudio[mp3loader]

---
+++ libraries/libaudio/mp3loader.h
@@ -0,0 +1,11 @@
+/**
+ * @file mp3loader.h
+ * @author Krisna Pranav
+ * @brief mp3 loader
+ * @version 6.0
+ * @date 2025-02-20
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
