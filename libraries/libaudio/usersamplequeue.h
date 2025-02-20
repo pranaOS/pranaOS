0f0389d629bc31da5263372e73c71e525008692c
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-20 22:56:53 +0530

    libraries: libaudio[usersamplequeue]

---
+++ libraries/libaudio/usersamplequeue.h
@@ -0,0 +1,27 @@
+/**
+ * @file usersamplequeue.h
+ * @author Krisna Pranav
+ * @brief User Sample Queue
+ * @version 6.0
+ * @date 2025-02-20
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <mods/disjointchunks.h>
+#include <mods/fixedarray.h>
+
+namespace Audio
+{
+
+    class UserSampleQueue
+    {
+    public:
+
+    private:
+    }; // class UserSampleQueue
+
+} // namespace Audio
