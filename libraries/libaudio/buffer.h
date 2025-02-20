a9050fc69fe4a45efce8fb7781754be6918123d4
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-20 22:33:13 +0530

    libraries: libaudio[buffer]

---
+++ libraries/libaudio/buffer.h
@@ -0,0 +1,10 @@
+/**
+ * @file buffer.h
+ * @author Krisna Pranav
+ * @brief buffer
+ * @version 6.0
+ * @date 2025-02-20
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
