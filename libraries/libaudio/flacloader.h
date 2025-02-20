d3a42e86ab5dafdbbd15c87368ac14b68a64c94e
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-20 22:20:02 +0530

    libraries: libaudio[flacloader]

---
+++ libraries/libaudio/flacloader.h
@@ -0,0 +1,27 @@
+/**
+ * @file flacloader.h
+ * @author Krisna Pranav
+ * @brief flac loader
+ * @version 6.0
+ * @date 2025-02-20
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once 
+
+#include "buffer.h"
+#include "flactypes.h"
+#include "loader.h"
+#include <mods/error.h>
+#include <mods/span.h>
+#include <mods/types.h>
+#include <libcore/inputbitstream.h>
+#include <libcore/memorystream.h>
+#include <libcore/stream.h>
+
+namespace Audio
+{
+    
+} // namespace Audio
