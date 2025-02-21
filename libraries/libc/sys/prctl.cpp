f02eafe89f8dbf9dc094dec24120d30222fed047
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 18:48:13 +0530

    libraries: libc[sys(prctl)]

---
+++ libraries/libc/sys/prctl.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file prctl.cpp
+ * @author Krisna Pranav
+ * @brief prctl
+ * @version 6.0
+ * @date 2025-02-21
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+extern "C" {
+    
+}
