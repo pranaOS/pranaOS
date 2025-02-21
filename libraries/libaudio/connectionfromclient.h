41b7dac046b12282a6c8debc1fe5c36c852fb5ef
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 16:10:23 +0530

    libraries: libaudio[connectionfromclient]

--- libraries/libaudio/connectionfromclient.h
+++ libraries/libaudio/connectionfromclient.h
@@ -8,3 +8,22 @@
  * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
  * 
  */
+
+#pragma once
+
+namespace Audio
+{
+
+    class ConnectionFromClient final : public IPC::ConnectionToServer<AudioClientEndpoint, AudioServerEndpoint>, public AudioClientEndpoint 
+    {
+    private:
+        OwnPtr<AudioQueue> m_buffer;
+        NonnullOwnPtr<UserSampleQueue> m_user_queue;
+        NonnullRefPtr<Threading::Thread> m_background_audio_enqueuer;
+        Core::EventLoop* m_enqueuer_loop;
+        Threading::Mutex m_enqueuer_loop_destruction;
+        Atomic<bool> m_audio_enqueuer_active { false };
+        timespec m_good_sleep_time {};
+    }; // class ConnectionFromClient final : public IPC::ConnectionToServer<AudioClientEndpoint, AudioServerEndpoint>, public AudioClientEndpoint 
+
+} // namespace Audio
