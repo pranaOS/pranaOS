97fd8baa890753edaba415c2c8f4adee13ffe700
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 19:57:45 +0530

    libraries: libc[sys(ttydefaults)]

---
+++ libraries/libc/sys/ttydefaults.h
@@ -0,0 +1,10 @@
+/**
+ * @file ttydefaults.h
+ * @author Krisna Pranav
+ * @brief tty defaults
+ * @version 6.0
+ * @date 2025-02-21
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
