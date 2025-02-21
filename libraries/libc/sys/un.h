bca23a2a17fb51ccf386f48ff8e533f2b4961542
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 20:00:37 +0530

    libraries: libc[sys(un)]

--- libraries/libc/sys/un.h
+++ libraries/libc/sys/un.h
@@ -11,15 +11,4 @@
 
 #pragma once
 
-#include <sys/cdefs.h>
-
-__BEGIN_DECLS
-
-#define UNIX_PATH_MAX 108
-
-struct sockaddr_un {
-    uint16_t sun_family;
-    char sun_path[UNIX_PATH_MAX];
-};
-
-__END_DECLS
+#include <kernel/api/posix/sys/un.h>
