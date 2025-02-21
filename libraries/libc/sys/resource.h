ee318854d128ae2f9dd354a834d91d356e4227b6
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 19:27:10 +0530

    libraries: libc[sys(resource)]

--- libraries/libc/sys/resource.h
+++ libraries/libc/sys/resource.h
@@ -9,16 +9,15 @@
  * 
  */
 
-#pragma once 
+#pragma once
 
+#include <stdint.h>
 #include <sys/cdefs.h>
 #include <sys/time.h>
 
-
 __BEGIN_DECLS
 
-struct rusage 
-{
+struct rusage {
     struct timeval ru_utime;
     struct timeval ru_stime;
     long ru_maxrss;
@@ -46,5 +45,48 @@ struct rusage
  * @return int 
  */
 int getrusage(int who, struct rusage* usage);
+
+#define RLIMIT_CORE 1
+#define RLIMIT_CPU 2
+#define RLIMIT_DATA 3
+#define RLIMIT_FSIZE 4
+#define RLIMIT_NOFILE 5
+#define RLIMIT_STACK 6
+#define RLIMIT_AS 7
+
+#define RLIM_NLIMITS 8
+
+#define RLIM_INFINITY SIZE_MAX
+
+typedef size_t rlim_t;
+
+struct rlimit {
+    rlim_t rlim_cur;
+    rlim_t rlim_max;
+}; // struct rlimit
+
+/**
+ * @return int 
+ */
+int getrlimit(int, struct rlimit*);
+
+/**
+ * @return int 
+ */
+int setrlimit(int, struct rlimit const*);
+
+#define PRIO_PROCESS 0
+#define PRIO_PGRP 1
+#define PRIO_USER 2
+
+/**
+ * @return int 
+ */
+int getpriority(int, id_t);
+
+/**
+ * @return int 
+ */
+int setpriority(int, id_t, int);
 
 __END_DECLS
