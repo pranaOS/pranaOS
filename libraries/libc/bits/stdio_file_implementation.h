c3fd811df5bd9492394a13d3614c0b3e2f3881ba
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 16:52:52 +0530

    libraries: libc[bits(stdio_file_implementation)]

--- libraries/libc/bits/stdio_file_implementation.h
+++ libraries/libc/bits/stdio_file_implementation.h
@@ -8,3 +8,35 @@
  * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
  * 
  */
+
+#include <libc/bits/FILE.h>
+
+#pragma once
+
+struct File
+{
+
+}; // struct File
+
+class ScopedFileLock 
+{
+public:
+    /**
+     * @brief Construct a new ScopedFileLock object
+     * 
+     * @param file 
+     */
+    ScopedFileLock(FILE* file)
+        : m_file(file)
+    {
+        m_file->lock();
+    }
+
+    ~ScopedFileLock()
+    {
+        m_file->unlocked();
+    }
+
+private:
+    FILE* file;
+}; // class ScopedFileLock 
