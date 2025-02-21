a3e68cafe977a0cce58173732293b604a403fe5b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 16:27:31 +0530

    libraries: libaudio[loader]

--- libraries/libaudio/loader.h
+++ libraries/libaudio/loader.h
@@ -12,11 +12,29 @@
 
 #pragma once
 
-namespace Audio
+#include <mods/nonnullownptr.h>
+#include <mods/nonnullrefptr.h>
+#include <mods/refcounted.h>
+#include <mods/refptr.h>
+#include <mods/result.h>
+#include <mods/span.h>
+#include <mods/stringview.h>
+#include <mods/try.h>
+#include <libaudio/buffer.h>
+#include <libaudio/loadererror.h>
+#include <libcore/file.h>
+
+namespace Audio 
 {
+
+    static constexpr StringView no_plugin_error = "No loader plugin available";
+
+    using LoaderSamples = Result<FixedArray<Sample>, LoaderError>;
+    using MaybeLoaderError = Result<void, LoaderError>;
+
     class LoaderPlugin 
     {
-    public: 
+    public:
         /**
          * @brief Destroy the LoaderPlugin object
          * 
@@ -57,8 +75,127 @@ namespace Audio
         virtual RefPtr<Core::File> file() = 0;
     }; // class LoaderPlugin
 
-    class Loader : public RefCounted<Loader>
+    class Loader : public RefCounted<Loader> 
     {
+    public:
+        /**
+         * @param path 
+         * @return Result<NonnullRefPtr<Loader>, LoaderError> 
+         */
+        static Result<NonnullRefPtr<Loader>, LoaderError> create(StringView path) 
+        { 
+            return adopt_ref(*new Loader(TRY(try_create(path)))); 
+        }
 
+        /**
+         * @param buffer 
+         * @return Result<NonnullRefPtr<Loader>, LoaderError> 
+         */
+        static Result<NonnullRefPtr<Loader>, LoaderError> create(Bytes& buffer) 
+        { 
+            return adopt_ref(*new Loader(TRY(try_create(buffer)))); 
+        }
+
+        /**
+         * @brief Get the more samples object
+         * 
+         * @param max_samples_to_read_from_input 
+         * @return LoaderSamples 
+         */
+        LoaderSamples get_more_samples(size_t max_samples_to_read_from_input = 128 * KiB) const 
+        { 
+            return m_plugin->get_more_samples(max_samples_to_read_from_input); 
+        }
+
+        /**
+         * @return MaybeLoaderError 
+         */
+        MaybeLoaderError reset() const 
+        { 
+            return m_plugin->reset(); 
+        }
+
+        /**
+         * @param position 
+         * @return MaybeLoaderError 
+         */
+        MaybeLoaderError seek(int const position) const 
+        { 
+            return m_plugin->seek(position); 
+        }
+
+        /**
+         * @return int 
+         */
+        int loaded_samples() const 
+        { 
+            return m_plugin->loaded_samples(); 
+        }
+
+        /**
+         * @return int 
+         */
+        int total_samples() const 
+        { 
+            return m_plugin->total_samples(); 
+        }
+
+        /**
+         * @return u32 
+         */
+        u32 sample_rate() const 
+        { 
+            return m_plugin->sample_rate(); 
+        }
+
+        /**
+         * @return u16 
+         */
+        u16 num_channels() const 
+        { 
+            return m_plugin->num_channels(); 
+        }
+
+        /**
+         * @return String 
+         */
+        String format_name() const 
+        { 
+            return m_plugin->format_name(); 
+        }
+
+        /**
+         * @return u16 
+         */
+        u16 bits_per_sample() const 
+        { 
+            return pcm_bits_per_sample(m_plugin->pcm_format()); 
+        }
+
+        /**
+         * @return RefPtr<Core::File> 
+         */
+        RefPtr<Core::File> file() const 
+        { 
+            return m_plugin->file(); 
+        }
+
+    private:
+        /**
+         * @param path 
+         * @return Result<NonnullOwnPtr<LoaderPlugin>, LoaderError> 
+         */
+        static Result<NonnullOwnPtr<LoaderPlugin>, LoaderError> try_create(StringView path);
+
+        /**
+         * @param buffer 
+         * @return Result<NonnullOwnPtr<LoaderPlugin>, LoaderError> 
+         */
+        static Result<NonnullOwnPtr<LoaderPlugin>, LoaderError> try_create(Bytes& buffer);
+
+        explicit Loader(NonnullOwnPtr<LoaderPlugin>);
+
+        mutable NonnullOwnPtr<LoaderPlugin> m_plugin;
     }; // class Loader : public RefCounted<Loader>
+
 } // namespace Audio
