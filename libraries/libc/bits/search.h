18d202110d945ad1c6676705571cad778313bb98
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 16:48:19 +0530

    libraries: libc[bits(search)]

---
+++ libraries/libc/bits/search.h
@@ -0,0 +1,17 @@
+/**
+ * @file search.h
+ * @author Krisna Pranav
+ * @brief Search
+ * @version 6.0
+ * @date 2025-02-21
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+struct search_tree_node
+{
+
+}; // struct search_tree_node
