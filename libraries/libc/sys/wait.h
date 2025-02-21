afe47e8bcdc2740263e979aa2619fb3e877c91e5
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 20:01:48 +0530

    libraries: libc[sys(wait)]

--- libraries/libc/sys/wait.h
+++ libraries/libc/sys/wait.h
@@ -9,37 +9,12 @@
  * 
  */
 
-#pragma once 
+#pragma once
 
-#include <sys/cdefs.h>
-#include <sys/types.h>
-#include <signal.h>
+#include <kernel/api/posix/signal.h>
+#include <kernel/api/posix/sys/wait.h>
 
 __BEGIN_DECLS
-
-// STATUS
-#define WEXITSTATUS(status) (((status)&0xff00) >> 8)
-#define WSTOPSIG(status) WEXITSTATUS(status)
-#define WTERMSIG(status) ((status)&0x7f)
-#define WIFEXITED(status) (WTERMSIG(status) == 0)
-#define WIFSTOPPED(status) (((status)&0xff) == 0x7f)
-#define WIFSIGNALED(status) (((char)(((status)&0x7f) + 1) >> 1) > 0)
-
-// TRACED
-#define WNOHANG 1
-#define WUNTRACED 2
-#define WSTOPPED WUNTRACED
-#define WEXITED 4
-#define WCONTINUED 8
-#define WNOWAIT 0x1000000
-
-/// @brief: idtype_t
-typedef enum 
-{
-    P_ALL = 1,
-    P_PID,
-    P_PGID
-} idtype_t;
 
 /**
  * @param wstatus 
