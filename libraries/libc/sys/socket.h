39b514b5679e53644c9ffd69a0abd3a79d83dbf1
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 19:30:32 +0530

    libraries: libc[sys(socket)]

--- libraries/libc/sys/socket.h
+++ libraries/libc/sys/socket.h
@@ -9,131 +9,12 @@
  * 
  */
 
-#pragma once 
+#pragma once
 
-#include <bits/stdint.h>
-#include <sys/cdefs.h>
-#include <sys/types.h>
+#include <kernel/api/posix/sys/socket.h>
 #include <sys/un.h>
 
 __BEGIN_DECLS
-
-#define AF_MASK 0xff
-#define AF_UNSPEC 0
-#define AF_LOCAL 1
-#define AF_UNIX AF_LOCAL
-#define AF_INET 2
-#define AF_MAX 3
-#define PF_LOCAL AF_LOCAL
-#define PF_UNIX PF_LOCAL
-#define PF_INET AF_INET
-#define PF_UNSPEC AF_UNSPEC
-#define PF_MAX AF_MAX
-
-#define SOCK_TYPE_MASK 0xff
-#define SOCK_STREAM 1
-#define SOCK_DGRAM 2
-#define SOCK_RAW 3
-#define SOCK_NONBLOCK 04000
-#define SOCK_CLOEXEC 02000000
-
-#define SHUT_RD 1
-#define SHUT_WR 2
-#define SHUT_RDWR 3
-
-#define IPPROTO_IP 0
-#define IPPROTO_ICMP 1
-#define IPPROTO_TCP 6
-#define IPPROTO_UDP 17
-
-#define MSG_TRUNC 0x1
-#define MSG_CTRUNC 0x2
-#define MSG_DONTWAIT 0x40
-
-// sa_family_t[16]
-typedef uint16_t sa_family_t;
-
-struct cmsghdr 
-{
-    socklen_t cmsg_len;
-    int cmsg_level;
-    int cmsg_type;
-}; // struct cmsghdr
-
-struct msghdr 
-{
-    void* msg_name;
-    socklen_t msg_namelen;
-    struct iovec* msg_iov;
-    int msg_iovlen;
-    void* msg_control;
-    socklen_t msg_controllen;
-    int msg_flags;
-}; // struct msghdr
-
-struct sockaddr 
-{
-    sa_family_t sa_family;
-    char sa_data[14];
-}; // struct sockaddr
-
-struct ucred 
-{
-    pid_t pid;
-    uid_t uid;
-    gid_t gid;
-}; // struct ucred
-
-/// @breif: SOCKET + SOMAXCONNECTION
-#define SOL_SOCKET 1
-#define SOMAXCONN 128
-
-enum 
-{
-    SO_RCVTIMEO,
-    SO_SNDTIMEO,
-    SO_TYPE,
-    SO_ERROR,
-    SO_PEERCRED,
-    SO_REUSEADDR,
-    SO_BINDTODEVICE,
-    SO_KEEPALIVE,
-    SO_TIMESTAMP,
-    SO_BROADCAST,
-};
-
-// ENUM Bindings
-#define SO_RCVTIMEO SO_RCVTIMEO
-#define SO_SNDTIMEO SO_SNDTIMEO
-#define SO_TYPE SO_TYPE
-#define SO_ERROR SO_ERROR
-#define SO_PEERCRED SO_PEERCRED
-#define SO_REUSEADDR SO_REUSEADDR
-#define SO_BINDTODEVICE SO_BINDTODEVICE
-#define SO_KEEPALIVE SO_KEEPALIVE
-#define SO_TIMESTAMP SO_TIMESTAMP
-#define SO_BROADCAST SO_BROADCAST
-
-/// @breif: SCM
-enum 
-{
-    SCM_TIMESTAMP,
-    SCM_RIGHTS,
-};
-
-/// @brief: ENUM BINDINGS
-#define SCM_TIMESTAMP SCM_TIMESTAMP
-#define SCM_RIGHTS SCM_RIGHTS
-
-struct sockaddr_storage 
-{
-    sa_family_t ss_family;
-    union 
-    {
-        char data[sizeof(struct sockaddr_un)];
-        void* alignment;
-    };
-}; // struct sockaddr_storage
 
 /**
  * @param domain 
@@ -150,7 +31,7 @@ int socket(int domain, int type, int protocol);
  */
 int bind(int sockfd, const struct sockaddr* addr, socklen_t);
 
-/**
+/** 
  * @param sockfd 
  * @param backlog 
  * @return int 
@@ -162,6 +43,12 @@ int listen(int sockfd, int backlog);
  * @return int 
  */
 int accept(int sockfd, struct sockaddr*, socklen_t*);
+
+/**
+ * @param sockfd 
+ * @return int 
+ */
+int accept4(int sockfd, struct sockaddr*, socklen_t*, int);
 
 /**
  * @param sockfd 
@@ -181,9 +68,10 @@ int shutdown(int sockfd, int how);
  * @param flags 
  * @return ssize_t 
  */
-ssize_t send(int sockfd, const void*, size_t, int flags);
+ssize_t send(int sockfd, void const*, size_t, int flags);
 
 /**
+
  * @param sockfd 
  * @param flags 
  * @return ssize_t 
@@ -195,7 +83,7 @@ ssize_t sendmsg(int sockfd, const struct msghdr*, int flags);
  * @param flags 
  * @return ssize_t 
  */
-ssize_t sendto(int sockfd, const void*, size_t, int flags, const struct sockaddr*, socklen_t);
+ssize_t sendto(int sockfd, void const*, size_t, int flags, const struct sockaddr*, socklen_t);
 
 /**
  * @param sockfd 
@@ -232,7 +120,7 @@ int getsockopt(int sockfd, int level, int option, void*, socklen_t*);
  * @param option 
  * @return int 
  */
-int setsockopt(int sockfd, int level, int option, const void*, socklen_t);
+int setsockopt(int sockfd, int level, int option, void const*, socklen_t);
 
 /**
  * @param sockfd 
@@ -247,6 +135,15 @@ int getsockname(int sockfd, struct sockaddr*, socklen_t*);
 int getpeername(int sockfd, struct sockaddr*, socklen_t*);
 
 /**
+ * @param domain 
+ * @param type 
+ * @param protocol 
+ * @param sv 
+ * @return int 
+ */
+int socketpair(int domain, int type, int protocol, int sv[2]);
+
+/**
  * @param sockfd 
  * @param fd 
  * @return int 
@@ -255,11 +152,12 @@ int sendfd(int sockfd, int fd);
 
 /**
  * @param sockfd 
+ * @param options 
  * @return int 
  */
-int recvfd(int sockfd);
+int recvfd(int sockfd, int options);
 
-/// @brief CMSG
+
 #define CMSG_ALIGN(x) (((x) + sizeof(void*) - 1) & ~(sizeof(void*) - 1))
 #define CMSG_SPACE(x) (CMSG_ALIGN(sizeof(struct cmsghdr)) + CMSG_ALIGN(x))
 #define CMSG_LEN(x) (CMSG_ALIGN(sizeof(struct cmsghdr)) + (x))
@@ -273,7 +171,7 @@ static inline struct cmsghdr* CMSG_FIRSTHDR(struct msghdr* msg)
     if (msg->msg_controllen < sizeof(struct cmsghdr))
         return 0;
     return (struct cmsghdr*)msg->msg_control;
-} // struct CMSG_FIRSTHDR
+}
 
 /**
  * @param msg 
@@ -287,7 +185,7 @@ static inline struct cmsghdr* CMSG_NXTHDR(struct msghdr* msg, struct cmsghdr* cm
     if (msg->msg_controllen < offset + sizeof(struct cmsghdr))
         return NULL;
     return next;
-} // static inline struct CMSG_NXTHDR
+}
 
 /**
  * @param cmsg 
