cd2a47559fbfe763991ad00f96f5836c5e6fb722
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 16:15:22 +0530

    libraries: libaudio[connectionfromclient]

---
+++ libraries/libaudio/connectionfromclient.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file connectionfromclient.cpp
+ * @author Krisna Pranav
+ * @brief Connection From Client
+ * @version 6.0
+ * @date 2025-02-21
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
