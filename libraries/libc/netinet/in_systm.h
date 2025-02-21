59bff8bba7406f672cf5edf6dc0b8b64d7d5be70
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 17:04:56 +0530

    libraries: libc[netinet(in_systm)]

---
+++ libraries/libc/netinet/in_systm.h
@@ -0,0 +1,11 @@
+/**
+ * @file in_systm.h
+ * @author Krisna Pranav
+ * @brief in systm
+ * @version 6.0
+ * @date 2025-02-21
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
