520f43c2a635686b2498922e1b803c5fe9a84025
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 19:28:12 +0530

    libraries: libc[sys(select)]

--- libraries/libc/sys/select.cpp
+++ libraries/libc/sys/select.cpp
@@ -11,12 +11,13 @@
 
 #include <errno.h>
 #include <stdio.h>
+#include <sys/poll.h>
 #include <sys/select.h>
 #include <sys/time.h>
-#include <kernel/api/syscall.h>
+#include <syscall.h>
+#include <mods/vector.h>
 
-extern "C" 
-{
+extern "C" {
 
     /**
      * @param nfds 
@@ -30,12 +31,14 @@ extern "C"
     {
         timespec* timeout_ts = nullptr;
         timespec timeout;
+
         if (timeout_tv) {
             timeout_ts = &timeout;
             TIMEVAL_TO_TIMESPEC(timeout_tv, timeout_ts);
         }
+        
         return pselect(nfds, readfds, writefds, exceptfds, timeout_ts, nullptr);
-    } // int select
+    }
 
     /**
      * @param nfds 
@@ -46,11 +49,55 @@ extern "C"
      * @param sigmask 
      * @return int 
      */
-    int pselect(int nfds, fd_set* readfds, fd_set* writefds, fd_set* exceptfds, const timespec* timeout, const sigset_t* sigmask)
+    int pselect(int nfds, fd_set* readfds, fd_set* writefds, fd_set* exceptfds, timespec const* timeout, sigset_t const* sigmask)
     {
-        Syscall::SC_select_params params { nfds, readfds, writefds, exceptfds, timeout, sigmask };
-        int rc = syscall(SC_select, &params);
-        __RETURN_WITH_ERRNO(rc, rc, -1);
-    } // int pselect
-    
-} // extern
+        Vector<pollfd, FD_SETSIZE> fds;
+
+        if (nfds < 0 || static_cast<size_t>(nfds) >= fds.capacity())
+            return EINVAL;
+
+        for (int fd = 0; fd < nfds; fd++) {
+            short events = 0;
+            if (readfds && FD_ISSET(fd, readfds))
+                events |= POLLIN;
+            if (writefds && FD_ISSET(fd, writefds))
+                events |= POLLOUT;
+            if (exceptfds && FD_ISSET(fd, exceptfds))
+                events |= POLLPRI;
+            if (!events)
+                continue;
+
+            fds.unchecked_append({ fd, events, 0 });
+        }
+
+        if (ppoll(fds.data(), fds.size(), timeout, sigmask) < 0)
+            return -1;
+
+        if (readfds)
+            FD_ZERO(readfds);
+        if (writefds)
+            FD_ZERO(writefds);
+        if (exceptfds)
+            FD_ZERO(exceptfds);
+
+        int marked_fd_count = 0;
+        for (auto& fd_entry : fds) {
+            if (fd_entry.revents == 0)
+                continue;
+            if (readfds && (fd_entry.revents & POLLIN)) {
+                FD_SET(fd_entry.fd, readfds);
+                marked_fd_count++;
+            }
+            if (writefds && (fd_entry.revents & POLLOUT)) {
+                FD_SET(fd_entry.fd, writefds);
+                marked_fd_count++;
+            }
+            if (exceptfds && (fd_entry.revents & (POLLPRI | POLLERR | POLLHUP))) {
+                FD_SET(fd_entry.fd, exceptfds);
+                marked_fd_count++;
+            }
+        }
+
+        return marked_fd_count;
+    }
+}
