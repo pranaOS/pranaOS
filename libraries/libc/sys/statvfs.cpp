b96f587d2d935a8736721ac4d83b6921f28972de
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 19:51:57 +0530

    libraries: libc[sys(statvfs)]

---
+++ libraries/libc/sys/statvfs.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file statvfs.cpp
+ * @author Krisna Pranav
+ * @brief statvfs
+ * @version 6.0
+ * @date 2025-02-21
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
