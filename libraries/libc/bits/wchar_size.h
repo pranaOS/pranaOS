c5e0d07fcde442f49f1c76579f991a8439dc6c45
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 16:59:38 +0530

    libraries: libc[bits(wchar_size)]

---
+++ libraries/libc/bits/wchar_size.h
@@ -0,0 +1,10 @@
+/**
+ * @file wchar_size.h
+ * @author Krisna Pranav
+ * @brief wchar size
+ * @version 6.0
+ * @date 2025-02-21
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
