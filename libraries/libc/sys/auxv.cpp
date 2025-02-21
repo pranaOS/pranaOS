2082cb8c793e41db0c646ea58ae70f5f85209aef
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 18:29:11 +0530

    libraries: libc[sys(auxv)]

--- libraries/libc/sys/auxv.cpp
+++ libraries/libc/sys/auxv.cpp
@@ -13,7 +13,21 @@
 #include <sys/auxv.h>
 #include <sys/internals.h>
 
-extern "C"
-{
+extern "C" {
+    /**
+     * @param type 
+     * @return long 
+     */
+    long getauxval(long type)
+    {
+        errno = 0;
 
+        auxv_t* auxvp = (auxv_t*)__auxiliary_vector;
+        for (; auxvp->a_type != AT_NULL; ++auxvp) {
+            if (auxvp->a_type == type)
+                return auxvp->a_un.a_val;
+        }
+        errno = ENOENT;
+        return 0;
+    }
 } // extern "C"
