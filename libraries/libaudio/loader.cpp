0ff4c842013785689e25837eb6cfabd790d8c1d7
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 15:38:46 +0530

    libraries: libaudio[loader]

--- libraries/libaudio/loader.cpp
+++ libraries/libaudio/loader.cpp
@@ -9,7 +9,38 @@
  * 
  */
 
+#include <libaudio/flacloader.h>
+#include <libaudio/loader.h>
+#include <libaudio/mp3loader.h>
+#include <libaudio/wavloader.h>
+
 namespace Audio
 {
+    /**
+     * @brief Construct a new Loader::Loader object
+     * 
+     * @param plugin 
+     */
+    Loader::Loader(NonnullOwnPtr<LoaderPlugin> plugin)
+        : m_plugin(move(plugin))
+    {}
 
+    /**
+     * @param buffer 
+     * @return Result<NonnullOwnPtr<LoaderPlugin>, LoaderError> 
+     */
+    Result<NonnullOwnPtr<LoaderPlugin>, LoaderError> Loader::try_create(Bytes& buffer)
+    {
+        NonnullOwnPtr<LoaderPlugin> plugin = adopt_own(*new WavLoaderPlugin(buffer));
+
+        if (auto initstate = plugin->initialize(); !initstate.is_error())
+            return plugin;
+
+        plugin = adopt_own(*new FlacLoaderPlugin(buffer));
+
+        if (auto initstate = plugin->initialize(); !initstate.is_error())
+            return plugin;
+            
+        return LoaderError { "No loader plugin available" };
+    }
 } // namespace Audio
