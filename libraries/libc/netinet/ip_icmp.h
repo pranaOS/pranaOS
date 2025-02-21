e855b4189245381177ded9f2fe4a2b12e76bc695
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 17:06:39 +0530

    libraries: libc[netinet(ip_icmp)]

--- libraries/libc/netinet/ip_icmp.h
+++ libraries/libc/netinet/ip_icmp.h
@@ -16,18 +16,32 @@
 
 __BEGIN_DECLS
 
-// icmphdr
 struct icmphdr {
-    uint8_t type; // type[uint8]
-    uint8_t code; // code[uint8]
-    uint16_t checksum; // checksum[uint16]
+    uint8_t type;
+    uint8_t code;
+    uint16_t checksum;
     union {
         struct {
-            uint16_t id; // id
-            uint16_t sequence; // sequence
+            uint16_t id;
+            uint16_t sequence;
         } echo;
         uint32_t gateway;
     } un;
-};
+}; // st ruct icmphdr
+
+#define ICMP_ECHOREPLY 0       
+#define ICMP_DEST_UNREACH 3    
+#define ICMP_SOURCE_QUENCH 4   
+#define ICMP_REDIRECT 5        
+#define ICMP_ECHO 8            
+#define ICMP_TIME_EXCEEDED 11  
+#define ICMP_PARAMETERPROB 12  
+#define ICMP_TIMESTAMP 13      
+#define ICMP_TIMESTAMPREPLY 14 
+#define ICMP_INFO_REQUEST 15   
+#define ICMP_INFO_REPLY 16     
+#define ICMP_ADDRESS 17        
+#define ICMP_ADDRESSREPLY 18   
+#define NR_ICMP_TYPES 18
 
 __END_DECLS
