60b6b2c5700dd7f32b1548d48e0d9ead67889edd
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 17:04:07 +0530

    libraries: libc[netinet(if_ether)]

---
+++ libraries/libc/netinet/if_ether.h
@@ -0,0 +1,10 @@
+/**
+ * @file if_ether.h
+ * @author Krisna Pranav
+ * @brief if ether
+ * @version 6.0
+ * @date 2025-02-21
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
