bac01d833e48b01381af982835cddada48cf17a2
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 18:29:58 +0530

    libraries: libc[sys(cdefs)]

--- libraries/libc/sys/cdefs.h
+++ libraries/libc/sys/cdefs.h
@@ -13,10 +13,6 @@
 
 #define _POSIX_VERSION 200809L
 
-#ifndef ALWAYS_INLINE
-#    define ALWAYS_INLINE inline __attribute__((always_inline))
-#endif
-
 #ifdef __cplusplus
 #    ifndef __BEGIN_DECLS
 #        define __BEGIN_DECLS extern "C" {
@@ -31,5 +27,3 @@
 
 #undef __P
 #define __P(a) a
-
-#define offsetof(type, member) __builtin_offsetof(type, member)
