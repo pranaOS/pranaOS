13154821648545d4a1ca86b1dc0f8985815a9c74
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 17:01:46 +0530

    libraries: libc[net(if_arp)]

---
+++ libraries/libc/net/if_arp.h
@@ -0,0 +1,12 @@
+/**
+ * @file if_arp.h
+ * @author Krisna Pranav
+ * @brief if arp
+ * @version 6.0
+ * @date 2025-02-21
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+ 
