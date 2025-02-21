b99e44f1fc0e112ddb2291e5c6f38f199e9a4ba5
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 17:07:31 +0530

    libraries: libc[netinet(tcp)]

--- libraries/libc/netinet/tcp.h
+++ libraries/libc/netinet/tcp.h
@@ -5,10 +5,11 @@
  * @version 6.0
  * @date 2023-07-05
  * 
- * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
  * 
  */
 
 #pragma once
 
 #define TCP_NODELAY 10
+#define TCP_MAXSEG 11
