6abf5eb4efa900d3b630c3c517284c29d32cec66
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 17:05:08 +0530

    libraries: libc[netinet(in_systm)]

--- libraries/libc/netinet/in_systm.h
+++ libraries/libc/netinet/in_systm.h
@@ -9,3 +9,16 @@
  * 
  */
 
+#pragma once
+
+#include <bits/stdint.h>
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
+typedef uint16_t n_short;
+typedef uint32_t n_long;
+
+typedef uint32_t n_time;
+
+__END_DECLS
