f7ff6d9475ed9c4b99bfe3b95fb8906eeee3a1e2
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 17:05:59 +0530

    libraries: libc[netinet(in)]

--- libraries/libc/netinet/in.h
+++ libraries/libc/netinet/in.h
@@ -5,59 +5,76 @@
  * @version 6.0
  * @date 2023-07-05
  * 
- * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
  * 
  */
 
 #pragma once
 
-#include <bits/stdint.h>
-#include <sys/cdefs.h>
-#include <sys/socket.h>
+#include <kernel/api/posix/netinet/in.h>
+#include <endian.h>
 
 __BEGIN_DECLS
-
-typedef uint32_t in_addr_t;
 
 /**
  * @return in_addr_t 
  */
-in_addr_t inet_addr(const char*);
+in_addr_t inet_addr(char const*);
 
-#define INADDR_ANY ((in_addr_t)0)
-#define INADDR_NONE ((in_addr_t)-1)
-#define INADDR_LOOPBACK 0x7f000001
+/**
+ * @param value 
+ * @return uint16_t 
+ */
+static inline uint16_t htons(uint16_t value)
+{
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+    return __builtin_bswap16(value);
+#else
+    return value;
+#endif
+}
 
-#define IN_LOOPBACKNET 127
+/**
+ * @param value 
+ * @return uint16_t 
+ */
+static inline uint16_t ntohs(uint16_t value)
+{
+    return htons(value);
+}
 
-#define IP_TTL 2
+/**
+ * @param value 
+ * @return uint32_t 
+ */
+static inline uint32_t htonl(uint32_t value)
+{
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+    return __builtin_bswap32(value);
+#else
+    return value;
+#endif
+}
 
-#define IPPORT_RESERVED 1024
-#define IPPORT_USERRESERVED 5000
+/**
+ * @param value 
+ * @return uint32_t 
+ */
+static inline uint32_t ntohl(uint32_t value)
+{
+    return htonl(value);
+}
 
-typedef uint16_t in_port_t;
+#define IN_MULTICAST(x) (((x)&0xf0000000) == 0xe0000000)
 
-struct in_addr {
-    uint32_t s_addr;
-};
+#define IN6_IS_ADDR_LOOPBACK(addr) \
+    ((addr)->s6_addr[0] == 0 && (addr)->s6_addr[1] == 0 && (addr)->s6_addr[2] == 0 && (addr)->s6_addr[3] == 0 && (addr)->s6_addr[4] == 0 && (addr)->s6_addr[5] == 0 && (addr)->s6_addr[6] == 0 && (addr)->s6_addr[7] == 0 && (addr)->s6_addr[8] == 0 && (addr)->s6_addr[9] == 0 && (addr)->s6_addr[10] == 0 && (addr)->s6_addr[11] == 0 && (addr)->s6_addr[12] == 0 && (addr)->s6_addr[13] == 0 && (addr)->s6_addr[14] == 0 && (addr)->s6_addr[15] == 1)
 
-struct sockaddr_in {
-    sa_family_t sin_family;
-    in_port_t sin_port;
-    struct in_addr sin_addr;
-    char sin_zero[8];
-};
 
-struct in6_addr {
-    uint8_t s6_addr[16];
-};
+#define IN6_IS_ADDR_V4MAPPED(addr) \
+    ((((addr)->s6_addr[0]) == 0) && (((addr)->s6_addr[1]) == 0) && (((addr)->s6_addr[2]) == 0) && (((addr)->s6_addr[3]) == 0) && (((addr)->s6_addr[4]) == 0) && (((addr)->s6_addr[5]) == 0) && (((addr)->s6_addr[6]) == 0) && (((addr)->s6_addr[7]) == 0) && (((addr)->s6_addr[8]) == 0) && (((addr)->s6_addr[9]) == 0) && (((addr)->s6_addr[10]) == 0xFF) && (((addr)->s6_addr[11]) == 0xFF))
 
-struct sockaddr_in6 {
-    sa_family_t sin6_family;   
-    in_port_t sin6_port;       
-    uint32_t sin6_flowinfo;    
-    struct in6_addr sin6_addr; 
-    uint32_t sin6_scope_id;    
-};
+#define IN6_IS_ADDR_LINKLOCAL(addr) \
+    (((addr)->s6_addr[0] == 0xfe) && (((addr)->s6_addr[1] & 0xc0) == 0x80))
 
 __END_DECLS
