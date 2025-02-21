cc5b0a151b8eeda6cd76c353780fbe9e1898091c
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 18:46:30 +0530

    libraries: libc[sys(prctl_numbers)]

---
+++ libraries/libc/sys/prctl_numbers.h
@@ -0,0 +1,12 @@
+/**
+ * @file prctl_numbers.h
+ * @author Krisna Pranav
+ * @brief prctl numbers
+ * @version 0.1
+ * @date 2025-02-21
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+ 
