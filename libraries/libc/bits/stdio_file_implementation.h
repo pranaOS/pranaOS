a86971be3d8c784a064e7c6c00fb34320a5ffbda
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 16:51:32 +0530

    libraries: libc[bits(stdio_file_implementation)]

---
+++ libraries/libc/bits/stdio_file_implementation.h
@@ -0,0 +1,10 @@
+/**
+ * @file stdio_file_implementation.h
+ * @author Krisna Pranav
+ * @brief stdio file implementation
+ * @version 6.0
+ * @date 2025-02-21
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
