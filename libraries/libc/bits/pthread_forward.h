ee109c7aea173288632ee113399fe21a687f6e66
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 16:46:17 +0530

    libraries: libc[bits(pthread_forward)]

--- libraries/libc/bits/pthread_forward.h
+++ libraries/libc/bits/pthread_forward.h
@@ -13,30 +13,21 @@
 
 #include <libpthread/pthread.h>
 
-struct PthreadFunctions
-{
+struct PthreadFunctions {
     int (*pthread_mutex_trylock)(pthread_mutex_t* mutex);
-
     int (*pthread_mutex_destroy)(pthread_mutex_t*);
 
     int (*pthread_mutexattr_init)(pthread_mutexattr_t*);
-
     int (*pthread_mutexattr_settype)(pthread_mutexattr_t*, int);
-
     int (*pthread_mutexattr_destroy)(pthread_mutexattr_t*);
 
     int (*pthread_once)(pthread_once_t*, void (*)(void));
 
     int (*pthread_cond_broadcast)(pthread_cond_t*);
-
-    int (*pthread_cond_init)(pthread_cond_t*, const pthread_condattr_t*);
-
+    int (*pthread_cond_init)(pthread_cond_t*, pthread_condattr_t const*);
     int (*pthread_cond_signal)(pthread_cond_t*);
-
     int (*pthread_cond_wait)(pthread_cond_t*, pthread_mutex_t*);
-
     int (*pthread_cond_destroy)(pthread_cond_t*);
-
     int (*pthread_cond_timedwait)(pthread_cond_t*, pthread_mutex_t*, const struct timespec*);
 }; // struct PthreadFunctions
 
