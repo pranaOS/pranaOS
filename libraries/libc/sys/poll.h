98a3542012ffc3215f8983833f4173e3f3865f95
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 18:45:19 +0530

    libraries: libc[sys(poll)]

---
+++ libraries/libc/sys/poll.h
@@ -0,0 +1,14 @@
+/**
+ * @file poll.h
+ * @author Krisna Pranav
+ * @brief poll
+ * @version 6.0
+ * @date 2025-02-21
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <poll.h>
