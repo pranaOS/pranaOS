a4a96627dd7b5bd70e504701cda91a849afdbeb2
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 16:38:46 +0530

    libraries: libc[arpa(inet)]

--- libraries/libc/arpa/inet.h
+++ libraries/libc/arpa/inet.h
@@ -11,21 +11,23 @@
 
 #pragma once
 
-#include <endian.h>
+#include <inttypes.h>
+#include <netinet/in.h>
 #include <sys/cdefs.h>
 #include <sys/socket.h>
 
 __BEGIN_DECLS
 
 #define INET_ADDRSTRLEN 16
+#define INET6_ADDRSTRLEN 46
 
 /**
  * @param af 
  * @param src 
  * @param dst 
- * @return const char* 
+ * @return char const* 
  */
-const char* inet_ntop(int af, const void* src, char* dst, socklen_t);
+char const* inet_ntop(int af, void const* src, char* dst, socklen_t);
 
 /**
  * @param af 
@@ -33,14 +35,15 @@ const char* inet_ntop(int af, const void* src, char* dst, socklen_t);
  * @param dst 
  * @return int 
  */
-int inet_pton(int af, const char* src, void* dst);
+int inet_pton(int af, char const* src, void* dst);
 
 /**
  * @param cp 
  * @param inp 
  * @return int 
  */
-static inline int inet_aton(const char* cp, struct in_addr* inp) {
+static inline int inet_aton(char const* cp, struct in_addr* inp)
+{
     return inet_pton(AF_INET, cp, inp);
 }
 
@@ -48,45 +51,5 @@ static inline int inet_aton(const char* cp, struct in_addr* inp) {
  * @return char* 
  */
 char* inet_ntoa(struct in_addr);
-
-/**
- * @param value 
- * @return uint16_t 
- */
-inline uint16_t htons(uint16_t value) {
-#if __BYTE_ORDER == __LITTLE_ENDIAN
-    return __builtin_bswap16(value);
-#else
-    return value;
-#endif
-}
-
-/**
- * @param value 
- * @return uint16_t 
- */
-inline uint16_t ntohs(uint16_t value) {
-    return htons(value);
-}
-
-/**
- * @param value 
- * @return uint32_t 
- */
-inline uint32_t htonl(uint32_t value) {
-#if __BYTE_ORDER == __LITTLE_ENDIAN
-    return __builtin_bswap32(value);
-#else
-    return value;
-#endif
-}
-
-/**
- * @param value 
- * @return uint32_t 
- */
-inline uint32_t ntohl(uint32_t value) {
-    return htonl(value);
-}
 
 __END_DECLS
