f4d59a0831b6d089431658add1e57960d6fd12f5
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 18:30:42 +0530

    libraries: libc[sys(file)]

--- libraries/libc/sys/file.h
+++ libraries/libc/sys/file.h
@@ -10,3 +10,21 @@
  */
 
 #pragma once
+
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
+#define LOCK_SH 0
+#define LOCK_EX 1
+#define LOCK_UN 2
+#define LOCK_NB (1 << 2)
+
+/**
+ * @param fd 
+ * @param operation 
+ * @return int 
+ */
+int flock(int fd, int operation);
+
+__END_DECLS
