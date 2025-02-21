180ceb68de52d5bcfb669e8d57ce6c15e2cb1e57
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 19:52:26 +0530

    libraries: libc[sys(statvfs)]

--- libraries/libc/sys/statvfs.cpp
+++ libraries/libc/sys/statvfs.cpp
@@ -9,3 +9,34 @@
  * 
  */
 
+#include <errno.h>
+#include <string.h>
+#include <sys/statvfs.h>
+#include <syscall.h>
+
+extern "C" {
+
+/**
+ * @param path 
+ * @param buf 
+ * @return int 
+ */
+int statvfs(char const* path, struct statvfs* buf)
+{
+    Syscall::SC_statvfs_params params { { path, strlen(path) }, buf };
+    int rc = syscall(SC_statvfs, &params);
+    __RETURN_WITH_ERRNO(rc, rc, -1);
+}
+
+/**
+ * @param fd 
+ * @param buf 
+ * @return int 
+ */
+int fstatvfs(int fd, struct statvfs* buf)
+{
+    int rc = syscall(SC_fstatvfs, fd, buf);
+    __RETURN_WITH_ERRNO(rc, rc, -1);
+}
+
+}
