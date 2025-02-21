925708eb655781a6fb94ddac7376eea7a099b59a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 20:05:54 +0530

    libraries: libc[sys(xattr)]

--- libraries/libc/sys/xattr.cpp
+++ libraries/libc/sys/xattr.cpp
@@ -21,3 +21,75 @@ ssize_t getxattr(char const*, char const*, void*, size_t)
     dbgln("FIXME: Implement getxattr()");
     return 0;
 }
+
+/**
+ * @return ssize_t 
+ */
+ssize_t lgetxattr(char const*, char const*, void*, size_t)
+{
+    dbgln("FIXME: Implement lgetxattr()");
+    return 0;
+}
+
+/**
+ * @return ssize_t 
+ */
+ssize_t fgetxattr(int, char const*, void*, size_t)
+{
+    dbgln("FIXME: Implement fgetxattr()");
+    return 0;
+}
+
+/**
+ * @return int 
+ */
+int setxattr(char const*, char const*, void const*, size_t, int)
+{
+    dbgln("FIXME: Implement setxattr()");
+    return 0;
+}
+
+/**
+ * @return int 
+ */
+int lsetxattr(char const*, char const*, void const*, size_t, int)
+{
+    dbgln("FIXME: Implement lsetxattr()");
+    return 0;
+}
+
+/**
+ * @return int 
+ */
+int fsetxattr(int, char const*, void const*, size_t, int)
+{
+    dbgln("FIXME: Implement fsetxattr()");
+    return 0;
+}
+
+/**
+ * @return ssize_t 
+ */
+ssize_t listxattr(char const*, char*, size_t)
+{
+    dbgln("FIXME: Implement listxattr()");
+    return 0;
+}
+
+/**
+ * @return ssize_t 
+ */
+ssize_t llistxattr(char const*, char*, size_t)
+{
+    dbgln("FIXME: Implement llistxattr()");
+    return 0;
+}
+
+/**
+ * @return ssize_t 
+ */
+ssize_t flistxattr(int, char*, size_t)
+{
+    dbgln("FIXME: Implement flistxattr()");
+    return 0;
+}
