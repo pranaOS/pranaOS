fefba260f236f37c2b4761aaab7c6c6b5d8afea3
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 12:23:21 +0530

    libraries: libaudio[mp3huffmantables]

--- libraries/libaudio/mp3huffmantables.h
+++ libraries/libaudio/mp3huffmantables.h
@@ -34,10 +34,43 @@ namespace Audio::MP3::Tables::Huffman
         unsigned code;
         size_t code_length;
         Symbol symbol;
-        
+
         bool is_leaf() const
         {
             return left == -1 && right == -1;
         }
     }; // struct HuffmanNode
+
+    /**
+     * @tparam Symbol 
+     * @tparam Size 
+     */
+    template<typename Symbol, size_t Size>
+    using HuffmanEntries = Array<Symbol, Size>;
+
+    /**
+     * @tparam Symbol 
+     * @tparam Size 
+     */
+    template<typename Symbol, size_t Size>
+    using HuffmanNodes = Array<HuffmanNode<Symbol>, Size>;
+
+    /**
+     * @tparam Symbol 
+     * @tparam Size 
+     * @param table 
+     * @return consteval 
+     */
+    template<typename Symbol, size_t Size>
+    consteval size_t length_of_longest_huffman_code(HuffmanEntries<Symbol, Size> const& table)
+    {
+        size_t max = 0;
+
+        for(size_t i = 0; i < table.size(); i++) {
+            if (table[i].code_length > max)
+                max = table[i].code_length;
+        }
+
+        return max;
+    }
 } // namespace Audio::MP3::Tables::Huffman
