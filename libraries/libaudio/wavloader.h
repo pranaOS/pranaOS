b6a8610460b9321910690393def2601546a5f342
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 11:16:32 +0530

    libraries: libaudio[wavloader]

--- libraries/libaudio/wavloader.h
+++ libraries/libaudio/wavloader.h
@@ -24,59 +24,56 @@
 #include <libcore/file.h>
 #include <libcore/filestream.h>
 
-namespace Audio
+namespace Audio 
 {
-    #define WAVE_FORMAT_PCM
-    #define WAVE_FORMAT_IEEE_FLOAT
-    #define WAVE_FORMAT_ALAW
-    #define WAVE_FORMAT_MULAW
-    #define WAVE_FORMAT_EXTENSIBLE
 
-    class WavLoaderPlugin : public LoaderPlugin
-    {
+    #define WAVE_FORMAT_PCM 0x0001        // PCM
+    #define WAVE_FORMAT_IEEE_FLOAT 0x0003 // IEEE float
+    #define WAVE_FORMAT_ALAW 0x0006       // 8-bit ITU-T G.711 A-law
+    #define WAVE_FORMAT_MULAW 0x0007      // 8-bit ITU-T G.711 µ-law
+    #define WAVE_FORMAT_EXTENSIBLE 0xFFFE // Determined by SubFormat
+
+    // Parses a WAV file and produces an Audio::LegacyBuffer.
+    class WavLoaderPlugin : public LoaderPlugin {
     public:
-        /**
-         * @brief Construct a new WavLoaderPlugin object
-         * 
-         * @param path 
-         */
         explicit WavLoaderPlugin(StringView path);
-
-        /**
-         * @brief Construct a new WavLoaderPlugin object
-         * 
-         * @param buffer 
-         */
         explicit WavLoaderPlugin(Bytes const& buffer);
 
-        /**
-         * @return MaybeLoaderError 
-         */
         virtual MaybeLoaderError initialize() override;
 
-        /**
-         * @brief Get the more samples object
-         * 
-         * @param max_bytes_to_read_from_input 
-         * @return LoaderSamples 
-         */
+        // The Buffer returned contains input data resampled at the
+        // destination audio device sample rate.
         virtual LoaderSamples get_more_samples(size_t max_bytes_to_read_from_input = 128 * KiB) override;
+
+        virtual MaybeLoaderError reset() override { return seek(0); }
+
+        // sample_index 0 is the start of the raw audio sample data
+        // within the file/stream.
+        virtual MaybeLoaderError seek(int const sample_index) override;
+
+        virtual int loaded_samples() override { return m_loaded_samples; }
+        virtual int total_samples() override { return m_total_samples; }
+        virtual u32 sample_rate() override { return m_sample_rate; }
+        virtual u16 num_channels() override { return m_num_channels; }
+        virtual String format_name() override { return "RIFF WAVE (.wav)"; }
+        virtual PcmSampleFormat pcm_format() override { return m_sample_format; }
+        virtual RefPtr<Core::File> file() override { return m_file; }
 
     private:
         MaybeLoaderError parse_header();
 
         RefPtr<Core::File> m_file;
-        OwnPtr<Mods::InputStream> m_stream;
-        Mods::InputMemoryStream* m_memory_stream;
+        OwnPtr<AK::InputStream> m_stream;
+        AK::InputMemoryStream* m_memory_stream;
         Optional<LoaderError> m_error {};
 
         u32 m_sample_rate { 0 };
         u16 m_num_channels { 0 };
         PcmSampleFormat m_sample_format;
-
         size_t m_byte_offset_of_data_samples { 0 };
 
         int m_loaded_samples { 0 };
         int m_total_samples { 0 };
-    }; // class WavLoaderPlugin : public LoaderPlugin
-} // namespace Audio
+    };
+
+}
