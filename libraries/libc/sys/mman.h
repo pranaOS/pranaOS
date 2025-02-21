b69705c9f8b8ddc271094f266f06b5719b555d7d
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 18:39:39 +0530

    libraries: libc[sys(mman)]

--- libraries/libc/sys/mman.h
+++ libraries/libc/sys/mman.h
@@ -9,6 +9,101 @@
  * 
  */
 
-#pragma once 
+#pragma once
 
-#include <mman.h>
+#include <kernel/api/posix/sys/mman.h>
+
+__BEGIN_DECLS
+
+/**
+ * @param addr 
+ * @param prot 
+ * @param flags 
+ * @param fd 
+ * @return void* 
+ */
+void* mmap(void* addr, size_t, int prot, int flags, int fd, off_t);
+
+/**
+ * @param addr 
+ * @param prot 
+ * @param flags 
+ * @param fd 
+ * @param name 
+ * @return void* 
+ */
+void* mmap_with_name(void* addr, size_t, int prot, int flags, int fd, off_t, char const* name);
+
+/**
+ * @param addr 
+ * @param prot 
+ * @param flags 
+ * @param fd 
+ * @param alignment 
+ * @param name 
+ * @return void* 
+ */
+void* pranaos_mmap(void* addr, size_t, int prot, int flags, int fd, off_t, size_t alignment, char const* name);
+
+/**
+ * @param old_address 
+ * @param old_size 
+ * @param new_size 
+ * @param flags 
+ * @return void* 
+ */
+void* mremap(void* old_address, size_t old_size, size_t new_size, int flags);
+
+/**
+ * @return int 
+ */
+int munmap(void*, size_t);
+
+/**
+ * @param prot 
+ * @return int 
+ */
+int mprotect(void*, size_t, int prot);
+
+/**
+ * @brief Set the mmap name object
+ * 
+ * @return int 
+ */
+int set_mmap_name(void*, size_t, char const*);
+
+/**
+ * @param advice 
+ * @return int 
+ */
+int madvise(void*, size_t, int advice);
+
+/**
+ * @param advice 
+ * @return int 
+ */
+int posix_madvise(void*, size_t, int advice);
+
+/**
+ * @param initial_data 
+ * @return void* 
+ */
+void* allocate_tls(char const* initial_data, size_t);
+
+/**
+ * @return int 
+ */
+int mlock(void const*, size_t);
+
+/**
+ * @return int 
+ */
+int munlock(void const*, size_t);
+
+/**
+ * @param flags 
+ * @return int 
+ */
+int msync(void*, size_t, int flags);
+
+__END_DECLS
