9343f465d5ef3b3fa88fa279610faae907ff05c4
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 16:49:31 +0530

    libraries: libc[bits(sighow)]

---
+++ libraries/libc/bits/sighow.h
@@ -0,0 +1,12 @@
+/**
+ * @file sighow.h
+ * @author Krisna Pranav
+ * @brief sighow
+ * @version 6.0
+ * @date 2025-02-21
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
