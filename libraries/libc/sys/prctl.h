6543f9450081baf93db50ba7c2b49882ebc2fd51
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 18:47:43 +0530

    libraries: libc[sys(prctl)]

--- libraries/libc/sys/prctl.h
+++ libraries/libc/sys/prctl.h
@@ -8,3 +8,20 @@
  * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
  * 
  */
+
+#pragma once
+
+#include <sys/cdefs.h>
+#include <sys/prctl_numbers.h>
+#include <sys/types.h>
+
+__BEGIN_DECLS
+
+/**
+ * @param option 
+ * @param ... 
+ * @return int 
+ */
+int prctl(int option, ...);
+
+__END_DECLS
