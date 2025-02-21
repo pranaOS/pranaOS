7d6cde4143d597db13bd1fd09e82139f31d9589a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 15:40:41 +0530

    libraries: libaudio[flacloader]

---
+++ libraries/libaudio/flacloader.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file flacloader.cpp
+ * @author Krisna Pranav
+ * @brief flac loader
+ * @version 6.0
+ * @date 2025-02-21
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
