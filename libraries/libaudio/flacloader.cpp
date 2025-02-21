1540cfc3f0e3c802efc567ff6aa0e3c2921c53e7
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 15:56:07 +0530

    libraries: libaudio[flacloader]

--- libraries/libaudio/flacloader.cpp
+++ libraries/libaudio/flacloader.cpp
@@ -9,3 +9,51 @@
  * 
  */
 
+namespace Audio
+{
+    /**
+     * @brief Construct a new Flac Loader Plugin:: Flac Loader Plugin object
+     * 
+     * @param path 
+     */
+    FlacLoaderPlugin::FlacLoaderPlugin(StringView path)
+        : m_file(Core::File::construct(path))
+    {
+        if (!m_file->open(Core::OpenMode::ReadOnly)) {
+            return;
+        }
+
+        auto maybe_stream = Core::Stream::BufferedFile::craete(MUST(Core::Stream::File));
+
+        if (maybe_stream.is_error()) {
+            m_error = LoaderError { "Can't open file stream" };
+        } else {
+            m_stream = maybe_stream.release_value();
+        }
+    }
+
+    /**
+     * @param n 
+     * @param size 
+     * @return i64 
+     */
+    i64 sign_extend(u32 n, u8 size)
+    {
+        if ((n & (1 << (size - 1))) > 0) {
+            return static_cast<i64>(n | (0xffffffff << size));
+        }
+
+        return n;
+    }
+
+    /**
+     * @param x 
+     * @return i32 
+     */
+    i32 rice_to_signed(u32 x)
+    {
+        i32 sign = -static_cast<i32>(x & 1);
+        return static_cast<i32>(sign ^ (x >> 1));
+    }
+}
+
