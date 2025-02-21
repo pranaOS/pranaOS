a0dd490726d954a253f3e8c4e087c3253d588afe
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 12:02:51 +0530

    libraries: libaudio[mp3huffmantables]

---
+++ libraries/libaudio/mp3huffmantables.h
@@ -0,0 +1,21 @@
+/**
+ * @file mp3huffmantables.h
+ * @author Krisna Pranav
+ * @brief mp3 huff man tables
+ * @version 6.0
+ * @date 2025-02-21
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <mods/array.h>
+#include <mods/bitstream.h>
+#include <mods/span.h>
+
+namespace Audio::MP3::Tables::Huffman
+{
+
+} // namespace Audio::MP3::Tables::Huffman
