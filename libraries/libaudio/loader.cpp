709efe8db2513ba8687385eae49c623b36b54130
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 15:37:31 +0530

    libraries: libaudio[loader]

---
+++ libraries/libaudio/loader.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file loader.cpp
+ * @author Krisna Pranav
+ * @brief Loader
+ * @version 6.0
+ * @date 2025-02-21
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+namespace Audio
+{
+
+} // namespace Audio
