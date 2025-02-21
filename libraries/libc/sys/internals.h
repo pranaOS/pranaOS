943bdd03097d70cffd61cf3aabae11c1da492d80
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-21 18:34:47 +0530

    libraries: libc[sys(internals)]

--- libraries/libc/sys/internals.h
+++ libraries/libc/sys/internals.h
@@ -9,7 +9,7 @@
  * 
  */
 
-#pragma once 
+#pragma once
 
 #include <sys/cdefs.h>
 
@@ -17,16 +17,30 @@ __BEGIN_DECLS
 
 typedef void (*AtExitFunction)(void*);
 
-extern void __libc_init();
-extern void __malloc_init();
-extern void __stdio_init();
-extern void _init();
+extern void __libc_init(void);
+extern void __malloc_init(void);
+extern void __stdio_init(void);
+extern void __begin_atexit_locking(void);
+extern void _init(void);
 extern bool __environ_is_malloced;
 extern bool __stdio_is_initialized;
+extern bool __heap_is_stable;
+extern void* __auxiliary_vector;
 
+/**
+ * @param exit_function 
+ * @param parameter 
+ * @param dso_handle 
+ * @return int 
+ */
 int __cxa_atexit(AtExitFunction exit_function, void* parameter, void* dso_handle);
+
+/**
+ * @param dso_handle 
+ */
 void __cxa_finalize(void* dso_handle);
-[[noreturn]] void __cxa_pure_virtual() __attribute__((weak));
-[[noreturn]] void __stack_chk_fail();
+__attribute__((noreturn)) void __cxa_pure_virtual(void) __attribute__((weak));
+__attribute__((noreturn)) void __stack_chk_fail(void);
+__attribute__((noreturn)) void __stack_chk_fail_local(void);
 
 __END_DECLS
