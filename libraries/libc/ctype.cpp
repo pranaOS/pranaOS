4b68d92123d51b0c482d6c24ee6ef6a6821e94f1
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-22 18:42:07 +0530

    libraries: libc[ctype]

--- libraries/libc/ctype.cpp
+++ libraries/libc/ctype.cpp
@@ -11,10 +11,9 @@
 
 #include <ctype.h>
 
-extern "C" 
-{
+extern "C" {
 
-    const char _ctype_[256] = {
+    char const _ctype_[256] = {
         _C, _C, _C, _C, _C, _C, _C, _C,
         _C, _C | _S, _C | _S, _C | _S, _C | _S, _C | _S, _C, _C,
         _C, _C, _C, _C, _C, _C, _C, _C,
@@ -33,26 +32,99 @@ extern "C"
         _L, _L, _L, _P, _P, _P, _P, _C
     };
 
-    /**
-     * @param c 
-     * @return int 
-     */
-    int tolower(int c)
+    #undef isalnum
+    int isalnum(int c)
     {
-        if (c >= 'A' && c <= 'Z')
-            return c | 0x20;
-        return c;
+        return __inline_isalnum(c);
     }
 
-    /**
-     * @param c 
-     * @return int 
-     */
+    #undef isalpha
+    int isalpha(int c)
+    {
+        return __inline_isalpha(c);
+    }
+
+    #undef iscntrl
+    int iscntrl(int c)
+    {
+        return __inline_iscntrl(c);
+    }
+
+    #undef isdigit
+    int isdigit(int c)
+    {
+        return __inline_isdigit(c);
+    }
+
+    #undef isxdigit
+    int isxdigit(int c)
+    {
+        return __inline_isxdigit(c);
+    }
+
+    #undef isspace
+    int isspace(int c)
+    {
+        return __inline_isspace(c);
+    }
+
+    #undef ispunct
+    int ispunct(int c)
+    {
+        return __inline_ispunct(c);
+    }
+
+    #undef isprint
+    int isprint(int c)
+    {
+        return __inline_isprint(c);
+    }
+
+    #undef isgraph
+    int isgraph(int c)
+    {
+        return __inline_isgraph(c);
+    }
+
+    #undef isupper
+    int isupper(int c)
+    {
+        return __inline_isupper(c);
+    }
+
+    #undef islower
+    int islower(int c)
+    {
+        return __inline_islower(c);
+    }
+
+    #undef isascii
+    int isascii(int c)
+    {
+        return __inline_isascii(c);
+    }
+
+    #undef isblank
+    int isblank(int c)
+    {
+        return __inline_isblank(c);
+    }
+
+    #undef toascii
+    int toascii(int c)
+    {
+        return __inline_toascii(c);
+    }
+
+    #undef tolower
+    int tolower(int c)
+    {
+        return __inline_tolower(c);
+    }
+
+    #undef toupper
     int toupper(int c)
     {
-        if (c >= 'a' && c <= 'z')
-            return c & ~0x20;
-        return c;
+        return __inline_toupper(c);
     }
-    
 }
