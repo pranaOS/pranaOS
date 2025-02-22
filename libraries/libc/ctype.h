ed2d7df5784c933236c464874354575a8917a3d8
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-22 18:41:25 +0530

    libraries: libc[ctype]

--- libraries/libc/ctype.h
+++ libraries/libc/ctype.h
@@ -9,13 +9,12 @@
  * 
  */
 
-#pragma once 
+#pragma once
 
 #include <sys/cdefs.h>
 
 __BEGIN_DECLS
 
-/// @brief: values
 #define _U 01
 #define _L 02
 #define _N 04
@@ -25,122 +24,197 @@ __BEGIN_DECLS
 #define _X 0100
 #define _B 0200
 
-/// @brief ctype[256]
-extern const char _ctype_[256];
-
-/**
- * @return int 
- */
-int tolower(int);
-
-/**
- * @return int 
- */
-int toupper(int);
+extern char const _ctype_[256];
 
 /**
  * @param c 
  * @return int 
  */
-static inline int isalnum(int c)
+static inline int __inline_isalnum(int c)
 {
-    return (_ctype_[(unsigned char)(c)] & (_U | _L | _N));
+    return _ctype_[(unsigned char)(c)] & (_U | _L | _N);
 }
 
 /**
  * @param c 
  * @return int 
  */
-static inline int isalpha(int c)
+static inline int __inline_isalpha(int c)
 {
-    return (_ctype_[(unsigned char)(c)] & (_U | _L));
+    return _ctype_[(unsigned char)(c)] & (_U | _L);
 }
 
 /**
  * @param c 
  * @return int 
  */
-static inline int iscntrl(int c)
+static inline int __inline_isascii(int c)
 {
-    return (_ctype_[(unsigned char)(c)] & (_C));
+    return (unsigned)c <= 127;
 }
 
 /**
  * @param c 
  * @return int 
  */
-static inline int isdigit(int c)
+static inline int __inline_iscntrl(int c)
 {
-    return (_ctype_[(unsigned char)(c)] & (_N));
+    return _ctype_[(unsigned char)(c)] & (_C);
 }
 
 /**
  * @param c 
  * @return int 
  */
-static inline int isxdigit(int c)
+static inline int __inline_isdigit(int c)
 {
-    return (_ctype_[(unsigned char)(c)] & (_N | _X));
+    return _ctype_[(unsigned char)(c)] & (_N);
 }
 
 /**
  * @param c 
  * @return int 
  */
-static inline int isspace(int c)
+static inline int __inline_isxdigit(int c)
 {
-    return (_ctype_[(unsigned char)(c)] & (_S));
+    return _ctype_[(unsigned char)(c)] & (_N | _X);
 }
 
 /**
  * @param c 
  * @return int 
  */
-static inline int ispunct(int c)
+static inline int __inline_isspace(int c)
 {
-    return (_ctype_[(unsigned char)(c)] & (_P));
+    return _ctype_[(unsigned char)(c)] & (_S);
 }
 
 /**
  * @param c 
  * @return int 
  */
-static inline int isprint(int c)
+static inline int __inline_ispunct(int c)
 {
-    return (_ctype_[(unsigned char)(c)] & (_P | _U | _L | _N | _B));
+    return _ctype_[(unsigned char)(c)] & (_P);
 }
 
 /**
  * @param c 
  * @return int 
  */
-static inline int isgraph(int c)
+static inline int __inline_isprint(int c)
 {
-    return (_ctype_[(unsigned char)(c)] & (_P | _U | _L | _N));
+    return _ctype_[(unsigned char)(c)] & (_P | _U | _L | _N | _B);
 }
 
 /**
  * @param c 
  * @return int 
  */
-static inline int islower(int c)
+static inline int __inline_isgraph(int c)
 {
-    return ((_ctype_[(unsigned char)(c)] & (_U | _L)) == _L);
+    return _ctype_[(unsigned char)(c)] & (_P | _U | _L | _N);
 }
 
 /**
  * @param c 
  * @return int 
  */
-static inline int isupper(int c)
+static inline int __inline_islower(int c)
 {
-    return ((_ctype_[(unsigned char)(c)] & (_U | _L)) == _U);
+    return _ctype_[(unsigned char)(c)] & (_L);
 }
 
-/// @brief: isascii
-#define isascii(c) ((unsigned)c <= 127)
+/**
+ * @param c 
+ * @return int 
+ */
+static inline int __inline_isupper(int c)
+{
+    return _ctype_[(unsigned char)(c)] & (_U);
+}
 
-/// @brief: toascii
-#define toascii(c) ((c)&127)
+/**
+ * @param c 
+ * @return int 
+ */
+static inline int __inline_isblank(int c)
+{
+    return _ctype_[(unsigned char)(c)] & (_B) || (c == '\t');
+}
+
+/**
+ * @param c 
+ * @return int 
+ */
+static inline int __inline_toascii(int c)
+{
+    return c & 127;
+}
+
+/**
+ * @param c 
+ * @return int 
+ */
+static inline int __inline_tolower(int c)
+{
+    if (c >= 'A' && c <= 'Z')
+        return c | 0x20;
+
+    return c;
+}
+
+/**
+ * @param c 
+ * @return int 
+ */
+static inline int __inline_toupper(int c)
+{
+    if (c >= 'a' && c <= 'z')
+        return c & ~0x20;
+        
+    return c;
+}
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int isalnum(int c);
+int isalpha(int c);
+int isascii(int c);
+int iscntrl(int c);
+int isdigit(int c);
+int isxdigit(int c);
+int isspace(int c);
+int ispunct(int c);
+int isprint(int c);
+int isgraph(int c);
+int islower(int c);
+int isupper(int c);
+int isblank(int c);
+int toascii(int c);
+int tolower(int c);
+int toupper(int c);
+
+#ifdef __cplusplus
+}
+#endif
+#define isalnum __inline_isalnum
+#define isalpha __inline_isalpha
+#define isascii __inline_isascii
+#define iscntrl __inline_iscntrl
+#define isdigit __inline_isdigit
+#define isxdigit __inline_isxdigit
+#define isspace __inline_isspace
+#define ispunct __inline_ispunct
+#define isprint __inline_isprint
+#define isgraph __inline_isgraph
+#define islower __inline_islower
+#define isupper __inline_isupper
+#define isblank __inline_isblank
+#define toascii __inline_toascii
+#define tolower __inline_tolower
+#define toupper __inline_toupper
 
 __END_DECLS
