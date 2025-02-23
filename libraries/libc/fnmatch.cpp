cdf4eaea0f4f3f12d73ff55da4ae69d1dfe5933f
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-23 13:10:42 +0530

    libraries: libc[fnmatch]

---
+++ libraries/libc/fnmatch.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file fnmatch.cpp
+ * @author Krisna Pranav
+ * @brief fnmatch
+ * @version 6.0
+ * @date 2025-02-23
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include <mods/format.h>
+#include <fnmatch.h>
+
+/**
+ * @return int 
+ */
+int fnmatch(char const*, char const*, int)
+{
+    dbgln("FIXME: Implement fnmatch()");
+    return 0;
+}
