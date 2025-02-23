1d17d333a893672a41154f8f6c5e44cc8d9a6603
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-23 13:19:37 +0530

    libraries: libc[grp]

--- libraries/libc/grp.h
+++ libraries/libc/grp.h
@@ -11,39 +11,47 @@
 
 #pragma once
 
+#include <bits/FILE.h>
 #include <sys/cdefs.h>
 #include <sys/types.h>
 
 __BEGIN_DECLS
 
-/// @brief group
 struct group {
     char* gr_name;
     char* gr_passwd;
     gid_t gr_gid;
     char** gr_mem;
-};
+}; // struct group
 
 /**
  * @return struct group* 
  */
-struct group* getgrent();
+struct group* getgrent(void);
+
+void setgrent(void);
+void endgrent(void);
 
 /**
  * @param name 
  * @return struct group* 
  */
-struct group* getgrnam(const char* name);
+struct group* getgrnam(char const* name);
 
 /**
  * @return struct group* 
-*/
+ */
 struct group* getgrgid(gid_t);
+
+/**
+ * @return int 
+ */
+int putgrent(const struct group*, FILE*);
 
 /**
  * @param user 
  * @return int 
  */
-int initgroups(const char* user, gid_t);
+int initgroups(char const* user, gid_t);
 
 __END_DECLS
