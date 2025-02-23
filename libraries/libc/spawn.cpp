934a77b34505b73fc7e787c460a115fb2ce39ccd
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-23 16:32:59 +0530

    libraries: libc[spawn]

---
+++ libraries/libc/spawn.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file spawn.cpp
+ * @author Krisna Pranav
+ * @brief spawn
+ * @version 6.0
+ * @date 2025-02-23
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
