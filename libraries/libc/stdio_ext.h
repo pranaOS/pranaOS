726364539d02a6a544608e2b1e580c0d0cc4aa5e
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-23 16:47:25 +0530

    libraries: libc[stdio_ext]

--- libraries/libc/stdio_ext.h
+++ libraries/libc/stdio_ext.h
@@ -8,3 +8,21 @@
  * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
  * 
  */
+
+#pragma once
+
+#include <stdio.h>
+
+__BEGIN_DECLS
+
+size_t __fpending(FILE*);
+int __freading(FILE*);
+int __fwriting(FILE*);
+void __fpurge(FILE*);
+
+size_t __freadahead(FILE*);
+char const* __freadptr(FILE*, size_t*);
+void __freadptrinc(FILE*, size_t);
+void __fseterr(FILE*);
+
+__END_DECLS
