da69726d4b9f06f8f52e6ea239717069a3724b8e
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-23 13:29:34 +0530

    libraries: libc[langinfo]

---
+++ libraries/libc/langinfo.h
@@ -0,0 +1,11 @@
+/**
+ * @file langinfo.h
+ * @author Krisna Pranav
+ * @brief lang info
+ * @version 6.0
+ * @date 2025-02-23
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
