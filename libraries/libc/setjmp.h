c42cd8d6b5b42b25ebfb44455141d76ccc7e1d13
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-23 14:42:47 +0530

    libraries: libc[setjmp]

--- libraries/libc/setjmp.h
+++ libraries/libc/setjmp.h
@@ -9,46 +9,64 @@
  * 
  */
 
-#pragma once 
+#pragma once
 
+#include <bits/stdint.h>
 #include <signal.h>
 #include <stdbool.h>
 #include <sys/cdefs.h>
 #include <sys/types.h>
-#include <bits/stdint.h>
 
 __BEGIN_DECLS
 
-struct __jmp_buf 
-{
-    uint32_t regs[6];
-    bool did_save_signal_mask;
+struct __jmp_buf {
+#ifdef __i386__
+    uint32_t ebx;
+    uint32_t esi;
+    uint32_t edi;
+    uint32_t ebp;
+    uint32_t esp;
+    uint32_t eip;
+#elif __x86_64__
+    uint64_t rbx;
+    uint64_t r12;
+    uint64_t r13;
+    uint64_t r14;
+    uint64_t r15;
+    uint64_t rbp;
+    uint64_t rsp;
+    uint64_t rip;
+#elif __aarch64__
+    uint64_t regs[22];
+#else
+#    error
+#endif
+    int did_save_signal_mask;
     sigset_t saved_signal_mask;
-}; // struct __jmp_buf
+};
 
-/// @brief: jmp_buf, sigjmp_buf
 typedef struct __jmp_buf jmp_buf[1];
 typedef struct __jmp_buf sigjmp_buf[1];
 
-/**
- * @return int 
- */
+#ifdef __cplusplus
+#    ifdef __i386__
+static_assert(sizeof(struct __jmp_buf) == 32, "struct __jmp_buf unsynchronized with i386/setjmp.S");
+#    elif __x86_64__
+static_assert(sizeof(struct __jmp_buf) == 72, "struct __jmp_buf unsynchronized with x86_64/setjmp.S");
+#    elif __aarch64__
+static_assert(sizeof(struct __jmp_buf) == 184, "struct __jmp_buf unsynchronized with aarch64/setjmp.S");
+#    else
+#        error
+#    endif
+#endif
+
 int setjmp(jmp_buf);
+__attribute__((noreturn)) void longjmp(jmp_buf, int val);
 
-/**
- * @param val 
- */
-void longjmp(jmp_buf, int val);
-
-/**
- * @param savesigs 
- * @return int 
- */
 int sigsetjmp(sigjmp_buf, int savesigs);
+__attribute__((noreturn)) void siglongjmp(sigjmp_buf, int val);
 
-/**
- * @param val 
- */
-void siglongjmp(sigjmp_buf, int val);
+int _setjmp(jmp_buf);
+__attribute__((noreturn)) void _longjmp(jmp_buf, int val);
 
 __END_DECLS
