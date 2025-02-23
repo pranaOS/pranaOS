14cf8f9136fe9bb25bac17a3e0aa4014747adae7
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-23 13:26:02 +0530

    libraries: libc[inttypes]

--- libraries/libc/inttypes.cpp
+++ libraries/libc/inttypes.cpp
@@ -12,7 +12,62 @@
 #include <mods/numericlimits.h>
 #include <errno.h>
 #include <inttypes.h>
+#include <stdlib.h>
 
 extern "C" {
 
+/**
+ * @param numerator 
+ * @param denominator 
+ * @return imaxdiv_t 
+ */
+imaxdiv_t imaxdiv(intmax_t numerator, intmax_t denominator)
+{
+    imaxdiv_t result;
+    result.quot = numerator / denominator;
+    result.rem = numerator % denominator;
+
+    if (numerator >= 0 && result.rem < 0) {
+        result.quot++;
+        result.rem -= denominator;
+    }
+
+    return result;
+}
+
+/**
+ * @param str 
+ * @param endptr 
+ * @param base 
+ * @return intmax_t 
+ */
+intmax_t strtoimax(char const* str, char** endptr, int base)
+{
+    long long_value = strtoll(str, endptr, base);
+
+    intmax_t max_int_value = NumericLimits<intmax_t>::max();
+    intmax_t min_int_value = NumericLimits<intmax_t>::min();
+    if (long_value > max_int_value) {
+        errno = -ERANGE;
+        return max_int_value;
+    } else if (long_value < min_int_value) {
+        errno = -ERANGE;
+        return min_int_value;
+    }
+
+    return long_value;
+}
+
+uintmax_t strtoumax(char const* str, char** endptr, int base)
+{
+    unsigned long ulong_value = strtoull(str, endptr, base);
+
+    uintmax_t max_uint_value = NumericLimits<uintmax_t>::max();
+    if (ulong_value > max_uint_value) {
+        errno = -ERANGE;
+        return max_uint_value;
+    }
+
+    return ulong_value;
+}
 }
