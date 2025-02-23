ebe972ce975df2a4dd1c4521475a3eb7b91bae70
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-23 14:41:10 +0530

    libraries: libc[search]

--- libraries/libc/search.h
+++ libraries/libc/search.h
@@ -8,3 +8,35 @@
  * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
  * 
  */
+
+#pragma once
+
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
+typedef enum {
+    preorder,
+    postorder,
+    endorder,
+    leaf,
+} VISIT;
+
+/**
+ * @return void* 
+ */
+void* tsearch(void const*, void**, int (*)(void const*, void const*));
+
+/**
+ * @return void* 
+ */
+void* tfind(void const*, void* const*, int (*)(void const*, void const*));
+
+/**
+ * @return void* 
+ */
+void* tdelete(void const*, void**, int (*)(void const*, void const*));
+
+void twalk(void const*, void (*)(void const*, VISIT, int));
+
+__END_DECLS
