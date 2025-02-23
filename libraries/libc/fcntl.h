4089d609da9ba0341bc2357f07dce5352b720cca
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-23 12:06:36 +0530

    libraries: libc[fcntl]

--- libraries/libc/fcntl.h
+++ libraries/libc/fcntl.h
@@ -11,66 +11,22 @@
 
 #pragma once
 
-#include <sys/cdefs.h>
-#include <sys/types.h>
+#include <kernel/api/posix/fcntl.h>
 
 __BEGIN_DECLS
 
-#define F_DUPFD 0
-#define F_GETFD 1
-#define F_SETFD 2
-#define F_GETFL 3
-#define F_SETFL 4
-#define F_ISTTY 5
-#define FD_CLOEXEC 1
-
-#define O_RDONLY (1 << 0)
-#define O_WRONLY (1 << 1)
-#define O_RDWR (O_RDONLY | O_WRONLY)
-#define O_ACCMODE (O_RDONLY | O_WRONLY)
-#define O_EXEC (1 << 2)
-#define O_CREAT (1 << 3)
-#define O_EXCL (1 << 4)
-#define O_NOCTTY (1 << 5)
-#define O_TRUNC (1 << 6)
-#define O_APPEND (1 << 7)
-#define O_NONBLOCK (1 << 8)
-#define O_DIRECTORY (1 << 9)
-#define O_NOFOLLOW (1 << 10)
-#define O_CLOEXEC (1 << 11)
-#define O_DIRECT (1 << 12)
-
-#define S_IFMT 0170000
-#define S_IFDIR 0040000
-#define S_IFCHR 0020000
-#define S_IFBLK 0060000
-#define S_IFREG 0100000
-#define S_IFIFO 0010000
-#define S_IFLNK 0120000
-#define S_IFSOCK 0140000
-#define S_ISUID 04000
-#define S_ISGID 02000
-#define S_ISVTX 01000
-#define S_IRUSR 0400
-#define S_IWUSR 0200
-#define S_IXUSR 0100
-#define S_IRGRP 0040
-#define S_IWGRP 0020
-#define S_IXGRP 0010
-#define S_IROTH 0004
-#define S_IWOTH 0002
-#define S_IXOTH 0001
-
-#define S_IRWXU (S_IRUSR | S_IWUSR | S_IXUSR)
-
-#define S_IRWXG (S_IRWXU >> 3)
-#define S_IRWXO (S_IRWXG >> 3)
+#define POSIX_FADV_DONTNEED 1
+#define POSIX_FADV_NOREUSE 2
+#define POSIX_FADV_NORMAL 3
+#define POSIX_FADV_RANDOM 4
+#define POSIX_FADV_SEQUENTIAL 5
+#define POSIX_FADV_WILLNEED 6
 
 /**
  * @param path 
  * @return int 
  */
-int creat(const char* path, mode_t);
+int creat(char const* path, mode_t);
 
 /**
  * @param path 
@@ -78,25 +34,7 @@ int creat(const char* path, mode_t);
  * @param ... 
  * @return int 
  */
-int open(const char* path, int options, ...);
-
-/**
- * @param path 
- * @param path_length 
- * @return int 
- */
-int creat_with_path_length(const char* path, size_t path_length, mode_t);
-
-/**
- * @param path 
- * @param path_length 
- * @param options 
- * @return int 
- */
-int open_with_path_length(const char* path, size_t path_length, int options, mode_t);
-
-// AT_FDCWD -
-#define AT_FDCWD -100
+int open(char const* path, int options, ...);
 
 /**
  * @param dirfd 
@@ -105,16 +43,7 @@ int open_with_path_length(const char* path, size_t path_length, int options, mod
  * @param ... 
  * @return int 
  */
-int openat(int dirfd, const char* path, int options, ...);
-
-/**
- * @param dirfd 
- * @param path 
- * @param path_length 
- * @param options 
- * @return int 
- */
-int openat_with_path_length(int dirfd, const char* path, size_t path_length, int options, mode_t);
+int openat(int dirfd, char const* path, int options, ...);
 
 /**
  * @param fd 
@@ -125,25 +54,36 @@ int openat_with_path_length(int dirfd, const char* path, size_t path_length, int
 int fcntl(int fd, int cmd, ...);
 
 /**
- * @param path 
- * @param path_length 
+ * @brief Create a inode watcher object
+ * 
+ * @param flags 
  * @return int 
  */
-int watch_file(const char* path, size_t path_length);
+int create_inode_watcher(unsigned flags);
 
-#define F_RDLCK 0
-#define F_WRLCK 1
-#define F_UNLCK 2
-#define F_SETLK 6
-#define F_SETLKW 7
+/**
+ * @param fd 
+ * @param path 
+ * @param path_length 
+ * @param event_mask 
+ * @return int 
+ */
+int inode_watcher_add_watch(int fd, char const* path, size_t path_length, unsigned event_mask);
 
-/// @brief: flock 
-struct flock {
-    short l_type;
-    short l_whence;
-    off_t l_start;
-    off_t l_len;
-    pid_t l_pid;
-};
+/**
+ * @param fd 
+ * @param wd 
+ * @return int 
+ */
+int inode_watcher_remove_watch(int fd, int wd);
 
-__END_DECLS 
+/**
+ * @param fd 
+ * @param offset 
+ * @param len 
+ * @param advice 
+ * @return int 
+ */
+int posix_fadvise(int fd, off_t offset, off_t len, int advice);
+
+__END_DECLS
