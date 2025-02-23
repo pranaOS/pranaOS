11dead4ef19fe2f6761a3bdebad0e3a691c09dcc
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-23 13:25:33 +0530

    libraries: libc[inttypes]

---
+++ libraries/libc/inttypes.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file inittypes.cpp
+ * @author Krisna Pranav
+ * @brief init types
+ * @version 6.0
+ * @date 2025-02-23
+ * 
+ * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include <mods/numericlimits.h>
+#include <errno.h>
+#include <inttypes.h>
+
+extern "C" {
+
+}
