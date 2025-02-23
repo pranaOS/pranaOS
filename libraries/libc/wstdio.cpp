360efbebc37aebd11aead21c740513bde057e894
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-23 18:36:47 +0530

    libraries: libc[wstdio]

---
+++ libraries/libc/wstdio.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file wstdio.cpp
+ * @author Krisna Pranav
+ * @brief wstdio
+ * @version 6.0
+ * @date 2025-02-23
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
