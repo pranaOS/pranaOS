031ce5e9b767edd46f20f789a1c539e5d5f19ea9
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-23 14:44:25 +0530

    libraries: libc[shadow]

---
+++ libraries/libc/shadow.h
@@ -0,0 +1,10 @@
+/**
+ * @file shadow.h
+ * @author Krisna Pranav
+ * @brief shadow
+ * @version 6.0
+ * @date 2025-02-23
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
