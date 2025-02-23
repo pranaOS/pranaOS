1bfe6f79f79c2562971c147abfc937f1e8b58c5f
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-23 16:39:09 +0530

    libraries: libc[stat]

---
+++ libraries/libc/stat.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file stat.cpp
+ * @author Krisna Pranav
+ * @brief stat
+ * @version 6.0
+ * @date 2025-02-23
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
