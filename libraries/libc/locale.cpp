b084163d6dcbf6ccf792b53832ecfd13dc162bde
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-23 13:35:16 +0530

    libraries: libc[locale]

--- libraries/libc/locale.cpp
+++ libraries/libc/locale.cpp
@@ -9,63 +9,62 @@
  * 
  */
 
-#include <mods/logstream.h>
 #include <assert.h>
 #include <locale.h>
 #include <stdio.h>
+#include <string.h>
 
 extern "C" {
 
-    static char default_decimal_point[] = ".";
-    static char default_thousands_sep[] = ",";
-    static char default_grouping[] = "\x03\x03";
+static char default_decimal_point[] = ".";
+static char default_thousands_sep[] = ",";
+static char default_grouping[] = "\x03\x03";
 
-    /// @brief  empty values
-    static char default_empty_string[] = "";
-    static char default_empty_value = 127;
+static char default_empty_string[] = "";
+static char default_empty_value = 127;
 
-    /// @brief default_locale
-    static struct lconv default_locale = {
-        default_decimal_point,
-        default_thousands_sep,
-        default_grouping,
-        default_empty_string,
-        default_empty_string,
-        default_empty_string,
-        default_empty_string,
-        default_empty_string,
-        default_empty_string,
-        default_empty_string,
-        default_empty_value,
-        default_empty_value,
-        default_empty_value,
-        default_empty_value,
-        default_empty_value,
-        default_empty_value,
-        default_empty_value,
-        default_empty_value,
-        default_empty_value,
-        default_empty_value,
-        default_empty_value,
-        default_empty_value,
-        default_empty_value,
-        default_empty_value
-    };
+static struct lconv default_locale = {
+    default_decimal_point,
+    default_thousands_sep,
+    default_grouping,
+    default_empty_string,
+    default_empty_string,
+    default_empty_string,
+    default_empty_string,
+    default_empty_string,
+    default_empty_string,
+    default_empty_string,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value,
+    default_empty_value
+};
 
-    /**
-     * @param category 
-     * @param locale 
-     * @return char* 
-     */
-    char* setlocale(int category, const char* locale) {
-        dbgln("FIXME(LibC): setlocale({}, '{}')", category, locale);
-        return nullptr;
-    }
+/**
+ * @return char* 
+ */
+char* setlocale(int, char const*)
+{
+    static char locale[2];
+    memcpy(locale, "C", 2);
+    return locale;
+}
 
-    /**
-     * @return struct lconv* 
-     */
-    struct lconv* localeconv() {
-        return &default_locale;
-    }
+/**
+ * @return struct lconv* 
+ */
+struct lconv* localeconv()
+{
+    return &default_locale;
+}
 }
