06ba7d236a019bad41dd85e8a25682f0ca8bdf31
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-23 18:25:33 +0530

    libraries: libc[utime]

--- libraries/libc/utime.cpp
+++ libraries/libc/utime.cpp
@@ -11,27 +11,23 @@
 
 #include <errno.h>
 #include <string.h>
+#include <syscall.h>
 #include <utime.h>
-#include <kernel/api/syscall.h>
 
-extern "C" 
+extern "C" {
+
+/**
+ * @param pathname 
+ * @param buf 
+ * @return int 
+ */
+int utime(char const* pathname, const struct utimbuf* buf)
 {
-
-    /**
-     * @param pathname 
-     * @param buf 
-     * @return int 
-     */
-    int utime(const char* pathname, const struct utimbuf* buf)
-    {
-        if (!pathname) {
-            errno = EFAULT;
-            return -1;
-        }
-
-        int rc = syscall(SC_utime, pathname, strlen(pathname), buf);
-        
-        __RETURN_WITH_ERRNO(rc, rc, -1);
+    if (!pathname) {
+        errno = EFAULT;
+        return -1;
     }
-    
-} // extern
+    int rc = syscall(SC_utime, pathname, strlen(pathname), buf);
+    __RETURN_WITH_ERRNO(rc, rc, -1);
+}
+}
