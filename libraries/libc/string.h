1cbf8923c468acde272e3599a25b69fc80535a7a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-23 17:30:39 +0530

    libraries: libc[string]

---
+++ libraries/libc/string.h
@@ -0,0 +1,11 @@
+/**
+ * @file string.h
+ * @author Krisna Pranav
+ * @brief string
+ * @version 6.0
+ * @date 2025-02-23
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
