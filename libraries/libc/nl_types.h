236eb06a8cd742a564be7ea58d5edd3e1c050473
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-23 14:05:34 +0530

    libraries: libc[nl_types]

---
+++ libraries/libc/nl_types.h
@@ -0,0 +1,20 @@
+/**
+ * @file nl_types.h
+ * @author Krisna Pranav
+ * @brief nl types
+ * @version 6.0
+ * @date 2025-02-23
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
+typedef int nl_item;
+
+__END_DECLS
