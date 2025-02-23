60f4fcff0640641f8e01634cda69b74ca570a9da
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-23 14:26:25 +0530

    libraries: libc[regex]

--- libraries/libc/regex.h
+++ libraries/libc/regex.h
@@ -18,64 +18,59 @@ __BEGIN_DECLS
 
 typedef ssize_t regoff_t;
 
-typedef struct 
-{
+typedef struct {
     void* __data;
     size_t re_nsub;
 } regex_t;
 
-enum __Regex_Error 
-{
+enum __Regex_Error {
     __Regex_NoError,
-    __Regex_InvalidPattern,              
-    __Regex_InvalidCollationElement,     
+    __Regex_InvalidPattern,             
+    __Regex_InvalidCollationElement,    
     __Regex_InvalidCharacterClass,      
-    __Regex_InvalidTrailingEscape,       
+    __Regex_InvalidTrailingEscape,      
     __Regex_InvalidNumber,              
     __Regex_MismatchingBracket,         
-    __Regex_MismatchingParen,            
+    __Regex_MismatchingParen,           
     __Regex_MismatchingBrace,           
     __Regex_InvalidBraceContent,        
     __Regex_InvalidBracketContent,      
     __Regex_InvalidRange,               
-    __Regex_InvalidRepetitionMarker,     
+    __Regex_InvalidRepetitionMarker,    
     __Regex_ReachedMaxRecursion,        
-    __Regex_EmptySubExpression,          
-    __Regex_InvalidCaptureGroup,         
-    __Regex_InvalidNameForCaptureGroup,  
-    __Regex_InvalidNameForProperty,      
-    __Regex_DuplicateNamedCapture,       
+    __Regex_EmptySubExpression,         
+    __Regex_InvalidCaptureGroup,        
+    __Regex_InvalidNameForCaptureGroup, 
+    __Regex_InvalidNameForProperty,     
+    __Regex_DuplicateNamedCapture,      
 }; // enum __Regex_Error
 
-enum ReError 
-{
+enum ReError {
     REG_NOERR = __Regex_NoError,
     REG_BADPAT = __Regex_InvalidPattern,            
     REG_ECOLLATE = __Regex_InvalidCollationElement, 
     REG_ECTYPE = __Regex_InvalidCharacterClass,     
     REG_EESCAPE = __Regex_InvalidTrailingEscape,    
-    REG_ESUBREG = __Regex_InvalidNumber,           
-    REG_EBRACK = __Regex_MismatchingBracket,       
-    REG_EPAREN = __Regex_MismatchingParen,         
-    REG_EBRACE = __Regex_MismatchingBrace,         
-    REG_BADBR = __Regex_InvalidBraceContent,       
-    REG_ERANGE = __Regex_InvalidRange,             
-    REG_BADRPT = __Regex_InvalidRepetitionMarker, 
-    REG_EMPTY_EXPR = __Regex_EmptySubExpression,  
-    REG_ENOSYS,                                   
-    REG_ESPACE,                                   
-    REG_NOMATCH,                                  
-}; // enum ReError 
+    REG_ESUBREG = __Regex_InvalidNumber,            
+    REG_EBRACK = __Regex_MismatchingBracket,        
+    REG_EPAREN = __Regex_MismatchingParen,          
+    REG_EBRACE = __Regex_MismatchingBrace,          
+    REG_BADBR = __Regex_InvalidBraceContent,        
+    REG_ERANGE = __Regex_InvalidRange,              
+    REG_BADRPT = __Regex_InvalidRepetitionMarker,  
+    REG_EMPTY_EXPR = __Regex_EmptySubExpression,    
+    REG_ENOSYS,                                     
+    REG_ESPACE,                                     
+    REG_NOMATCH,                                    
+}; // enum ReError
 
-typedef struct 
-{
+typedef struct {
     regoff_t rm_so;  
     regoff_t rm_eo;  
     regoff_t rm_cnt; 
 } regmatch_t;
 
-enum __RegexAllFlags 
-{
+enum __RegexAllFlags {
     __Regex_Global = 1,                                      
     __Regex_Insensitive = __Regex_Global << 1,               
     __Regex_Ungreedy = __Regex_Global << 2,                  
@@ -95,13 +90,13 @@ enum __RegexAllFlags
     __Regex_Internal_BrowserExtended = __Regex_Global << 16, 
     __Regex_Internal_ConsiderNewline = __Regex_Global << 17, 
     __Regex_Last = __Regex_SingleMatch
-}; // enum __RegexAllFlags 
+}; // enum __RegexAllFlags
 
-#define REG_EXTENDED __Regex_Extended               
-#define REG_ICASE __Regex_Insensitive               
-#define REG_NOSUB __Regex_SkipSubExprResults        
-#define REG_GLOBAL __Regex_Global                   
-#define REG_NEWLINE (__Regex_Multiline | REG_GLOBAL)
+#define REG_EXTENDED __Regex_Extended                
+#define REG_ICASE __Regex_Insensitive                
+#define REG_NOSUB __Regex_SkipSubExprResults         
+#define REG_GLOBAL __Regex_Global                    
+#define REG_NEWLINE (__Regex_Multiline | REG_GLOBAL) 
 
 #define REG_NOTBOL __Regex_MatchNotBeginOfLine 
 #define REG_NOTEOL __Regex_MatchNotEndOfLine   
@@ -111,17 +106,17 @@ enum __RegexAllFlags
 /**
  * @return int 
  */
-int regcomp(regex_t*, const char*, int);
+int regcomp(regex_t*, char const*, int);
 
 /**
  * @return int 
  */
-int regexec(const regex_t*, const char*, size_t, regmatch_t[], int);
+int regexec(regex_t const*, char const*, size_t, regmatch_t[], int);
 
 /**
  * @return size_t 
  */
-size_t regerror(int, const regex_t*, char*, size_t);
+size_t regerror(int, regex_t const*, char*, size_t);
 
 void regfree(regex_t*);
 
