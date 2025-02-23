e1ad179670dbf711ef520d8c4fa7995024da43af
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-23 13:12:00 +0530

    libraries: libc[getopt]

--- libraries/libc/getopt.h
+++ libraries/libc/getopt.h
@@ -15,22 +15,26 @@
 
 __BEGIN_DECLS
 
-/// @brief args
 #define no_argument 0
 #define required_argument 1
 #define optional_argument 2
 
-
-/// @brief  option
 struct option {
-    const char* name;
+    char const* name;
     int has_arg;
     int* flag;
     int val;
-};
+}; // struct option
 
+extern int opterr;
+extern int optopt;
+extern int optind;
+extern int optreset;
+extern char* optarg;
 
 /**
+ * @brief Get the opt long object
+ * 
  * @param argc 
  * @param argv 
  * @param short_options 
@@ -38,6 +42,6 @@ struct option {
  * @param out_long_option_index 
  * @return int 
  */
-int getopt_long(int argc, char** argv, const char* short_options, const struct option* long_options, int* out_long_option_index);
+int getopt_long(int argc, char* const* argv, char const* short_options, const struct option* long_options, int* out_long_option_index);
 
 __END_DECLS
