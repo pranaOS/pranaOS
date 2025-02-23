50ab853c726f1910c9da0ba9340e024ef02390d0
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-23 13:34:49 +0530

    libraries: libc[locale]

--- libraries/libc/locale.h
+++ libraries/libc/locale.h
@@ -15,17 +15,23 @@
 
 __BEGIN_DECLS
 
-/// @brief LC
 enum {
     LC_ALL,
+#define LC_ALL LC_ALL
     LC_NUMERIC,
+#define LC_NUMERIC LC_NUMERIC
     LC_CTYPE,
+#define LC_CTYPE LC_CTYPE
     LC_COLLATE,
+#define LC_COLLATE LC_COLLATE
     LC_TIME,
+#define LC_TIME LC_TIME
     LC_MONETARY,
-};
+#define LC_MONETARY LC_MONETARY
+    LC_MESSAGES,
+#define LC_MESSAGES LC_MESSAGES
+}; // enum
 
-/// @brief lconv
 struct lconv {
     char* decimal_point;
     char* thousands_sep;
@@ -51,18 +57,15 @@ struct lconv {
     char int_n_sep_by_space;
     char int_p_sign_posn;
     char int_n_sign_posn;
-};
+}; // struct lconv
 
-/**
- * @return struct lconv* 
- */
-struct lconv* localeconv();
+struct lconv* localeconv(void);
 
 /**
  * @param category 
  * @param locale 
  * @return char* 
  */
-char* setlocale(int category, const char* locale);
+char* setlocale(int category, char const* locale);
 
 __END_DECLS
