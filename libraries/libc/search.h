2d4cee606ec3b42592236fd587889330b804ce20
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-23 14:39:57 +0530

    libraries: libc[search]

---
+++ libraries/libc/search.h
@@ -0,0 +1,10 @@
+/**
+ * @file search.h
+ * @author Krisna Pranav
+ * @brief search
+ * @version 6.0
+ * @date 2025-02-23
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
