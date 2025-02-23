698dc7992ba652e9c62ebc6c776fdb47b5c541c0
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-23 13:32:08 +0530

    libraries: libc[libcinit]

--- libraries/libc/libcinit.cpp
+++ libraries/libc/libcinit.cpp
@@ -9,29 +9,45 @@
  * 
  */
 
-#include <assert.h>
-#include <unistd.h>
-#include <sys/internals.h>
 #include <mods/types.h>
+#include <assert.h>
+#include <errno.h>
+#include <sys/internals.h>
+#include <unistd.h>
 
-extern "C" 
+extern "C" {
+
+#ifdef NO_TLS
+int errno_storage;
+#else
+__thread int errno_storage;
+#endif
+char** environ;
+bool __environ_is_malloced;
+bool __stdio_is_initialized;
+bool s_global_initializers_ran;
+void* __auxiliary_vector;
+
+static void __auxiliary_vector_init();
+
+int* __errno_location()
 {
+    return &errno_storage;
+}
 
-    #ifdef NO_TLS
-        int errno;
-    #else
-        __thread int errno;
-    #endif
+void __libc_init()
+{
+    __auxiliary_vector_init();
+    __malloc_init();
+    __stdio_init();
+}
 
-    char** environ;
+static void __auxiliary_vector_init()
+{
+    char** env;
+    for (env = environ; *env; ++env) {
+    }
 
-    bool __environ_is_malloced;
-    bool __stdio_is_initialized;
-
-    void __libc_init()
-    {
-        __malloc_init();
-        __stdio_init();
-    } 
-
-} // extern
+    __auxiliary_vector = (void*)++env;
+}
+}
