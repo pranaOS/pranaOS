38b36db606795e1870f86fc4fea9cd7424ee6562
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-23 18:40:25 +0530

    libraries: libcards[card]

---
+++ libraries/libcards/card.h
@@ -0,0 +1,11 @@
+/**
+ * @file card.h
+ * @author Krisna Pranav
+ * @brief card
+ * @version 6.0
+ * @date 2025-02-23
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
