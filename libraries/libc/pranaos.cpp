ead7a16a67f502b8c3356e51c730e7d0e0cf98bc
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-23 14:11:40 +0530

    libraries: libc[pranaos]

---
+++ libraries/libc/pranaos.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file pranaos.cpp
+ * @author Krisna Pranav
+ * @brief pranaos
+ * @version 6.0
+ * @date 2025-02-23
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
