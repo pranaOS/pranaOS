f0f60432624742bfcc5b1c2a308f08f986dd1ff7
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-23 16:47:05 +0530

    libraries: libc[stdio_ext]

---
+++ libraries/libc/stdio_ext.h
@@ -0,0 +1,10 @@
+/**
+ * @file stdio_ext.h
+ * @author Krisna Pranav
+ * @brief stdio extension
+ * @version 6.0
+ * @date 2025-02-23
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
