d2b375dd09d2bb1dfb8afe0d1210d32757ecbae7
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-23 17:40:38 +0530

    libraries: libc[strings]

--- libraries/libc/strings.cpp
+++ libraries/libc/strings.cpp
@@ -10,60 +10,72 @@
  */
 
 #include <assert.h>
-#include <strings.h>
 #include <ctype.h>
+#include <string.h>
+#include <strings.h>
 
-extern "C" 
+extern "C" {
+
+/**
+ * @param dest 
+ * @param n 
+ */
+void bzero(void* dest, size_t n)
 {
+    memset(dest, 0, n);
+}
 
-    /**
-     * @param ch 
-     * @return char 
-     */
-    static char foldcase(char ch)
-    {
+/**
+ * @param src 
+ * @param dest 
+ * @param n 
+ */
+void bcopy(void const* src, void* dest, size_t n)
+{
+    memmove(dest, src, n);
+}
 
-        if (isalpha(ch))
-            return tolower(ch);
+/**
+ * @param ch 
+ * @return char 
+ */
+static char foldcase(char ch)
+{
+    if (isalpha(ch))
+        return tolower(ch);
+    return ch;
+}
 
-        return ch;
-    }
-
-    /**
-     * @param s1 
-     * @param s2 
-     * @return int 
-     */
-    int strcasecmp(const char* s1, const char* s2)
-    {
-        for (; foldcase(*s1) == foldcase(*s2); ++s1, ++s2) {
-            if (*s1 == 0)
-                return 0;
-        }
-
-        return foldcase(*(const unsigned char*)s1) < foldcase(*(const unsigned char*)s2) ? -1 : 1;
-
-    }
-
-    /**
-     * @param s1 
-     * @param s2 
-     * @param n 
-     * @return int 
-     */
-    int strncasecmp(const char* s1, const char* s2, size_t n)
-    {
-        if (!n)
+/**
+ * @param s1 
+ * @param s2 
+ * @return int 
+ */
+int strcasecmp(char const* s1, char const* s2)
+{
+    for (; foldcase(*s1) == foldcase(*s2); ++s1, ++s2) {
+        if (*s1 == 0)
             return 0;
-
-        do {
-            if (foldcase(*s1) != foldcase(*s2++))
-                return foldcase(*(const unsigned char*)s1) - foldcase(*(const unsigned char*)--s2);
-            if (*s1++ == 0)
-                break;
-        } while (--n);
-
-        return 0;
     }
-    
-} // extern
+    return foldcase(*(unsigned char const*)s1) < foldcase(*(unsigned char const*)s2) ? -1 : 1;
+}
+
+/**
+ * @param s1 
+ * @param s2 
+ * @param n 
+ * @return int 
+ */
+int strncasecmp(char const* s1, char const* s2, size_t n)
+{
+    if (!n)
+        return 0;
+    do {
+        if (foldcase(*s1) != foldcase(*s2++))
+            return foldcase(*(unsigned char const*)s1) - foldcase(*(unsigned char const*)--s2);
+        if (*s1++ == 0)
+            break;
+    } while (--n);
+    return 0;
+}
+}
