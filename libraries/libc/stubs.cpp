dbff4e8297b058177548ffa20d97ce997bf94565
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-23 17:42:06 +0530

    libraries: libc[stubs]

--- libraries/libc/stubs.cpp
+++ libraries/libc/stubs.cpp
@@ -9,3 +9,14 @@
  * 
  */
 
+extern "C" {
+
+#define DO_STUB(name) \
+    void name();      \
+    void name() { }
+
+DO_STUB(__register_frame_info);
+DO_STUB(__deregister_frame_info);
+DO_STUB(_ITM_registerTMCloneTable);
+DO_STUB(_ITM_deregisterTMCloneTable);
+}
