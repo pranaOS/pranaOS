de988a422c56c8a6b23fce50e75dda1312fc530d
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-23 17:41:53 +0530

    libraries: libc[stubs]

---
+++ libraries/libc/stubs.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file stubs.cpp
+ * @author Krisna Pranav
+ * @brief stubs
+ * @version 6.0
+ * @date 2025-02-23
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
