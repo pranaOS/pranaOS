7b92fddc79f1693eba511099a22d9214b37687c3
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-23 18:50:51 +0530

    libraries: libcards[card]

---
+++ libraries/libcards/card.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file card.cpp
+ * @author Krisna Pranav
+ * @brief card
+ * @version 6.0
+ * @date 2025-02-23
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+namespace Cards 
+{
+
+} // namespace Cards 
