60519bbb3fd8e04e702bb5757ba2cc56b19f6805
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-24 16:49:05 +0530

    libraries: libchess[uciendpoint]

--- libraries/libchess/uciendpoint.h
+++ libraries/libchess/uciendpoint.h
@@ -10,3 +10,35 @@
  */
 
 #pragma once
+
+#include <libchess/ucicommand.h>
+#include <libcore/iodevice.h>
+#include <libcore/notifier.h>
+#include <libcore/object.h>
+
+namespace Chess::UCI
+{
+    class Endpoint : public Core::Object
+    {
+        C_OBJECT(Endpoint);
+
+    protected:
+        Endpoint() = default;
+
+        /**
+         * @brief Construct a new Endpoint object
+         * 
+         * @param in 
+         * @param out 
+         */
+        Endpoint(NonnullRefPtr<Core::IODevice> in, NonnullRefPtr<Core::IODevice> out);
+
+    private:
+        void set_in_notifier();
+        NonnullOwnPtr<Command> read_command();
+
+        RefPtr<Core::IODevice> m_in;
+        RefPtr<Core::IODevice> m_out;
+        RefPtr<Core::IODevice> m_in_notifier;
+    } // class Endpoint : public Core::Object
+} // namespace Chess::UCI
