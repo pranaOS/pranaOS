b76de8169469926db81d815aaccb1562d2c8ce4c
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-24 16:39:08 +0530

    libraries: libchess[chess]

--- libraries/libchess/chess.h
+++ libraries/libchess/chess.h
@@ -49,5 +49,67 @@ namespace Chess
         Black,
         None,
     }; // enum class Color : u8 
-    
+
+    Color opposing_color(Color color);
+
+    struct Piece
+    {
+        constexpr Piece()
+            : color(Color::None)
+            , type(Type::None)
+        {
+        }
+
+        /**
+         * @param c
+         * @param t 
+         * 
+         */
+        constexpr Piece(Color c, Type t)
+            : color(c)
+            , type(t)
+        {
+        }
+
+        Color color : 4;
+        Type type : 4;
+
+        /**
+         * @param other 
+         * @return true 
+         * @return false 
+         */
+        bool operator==(Piece const& other) const
+        {
+            return color == other.color;
+        }
+    }; // struct Piece
+
+    constexpr Piece EmptyPiece = { Color::None, Type::None };
+
+    struct Square 
+    {
+        i8 rank;
+        i8 file;
+
+        /**
+         * @brief Construct a new Square object
+         * 
+         * @param name 
+         */
+        Square(StringView name);
+
+        /**
+         * @brief Construct a new Square object
+         * 
+         * @param file 
+         */
+        Square(int const& rank int const& file)
+            : rank(rank)
+            , file(file)
+        {}
+    }; // struct Square 
+
+    class Board {}
+
 } // namespace Chess
