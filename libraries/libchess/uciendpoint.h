6a92f380629b3923b3bb904feb3128c2a5ac7a94
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-24 16:47:28 +0530

    libraries: libchess[uciendpoint]

---
+++ libraries/libchess/uciendpoint.h
@@ -0,0 +1,12 @@
+/**
+ * @file uciendpoint.h
+ * @author Krisna Pranav
+ * @brief uci end point
+ * @version 6.0
+ * @date 2025-02-24
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
