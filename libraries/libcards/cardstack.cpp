839c5b88b4bf312098dda747149325d5fff8691b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-24 16:17:08 +0530

    libraries: libcards[cardstack]

--- libraries/libcards/cardstack.cpp
+++ libraries/libcards/cardstack.cpp
@@ -9,3 +9,33 @@
  * 
  */
 
+#include "cardstack.h"
+
+namespace Cards
+{
+    /**
+     * @brief Construct a new CardStack::CardStack object
+     * 
+     */
+    CardStack::CardStack()
+        : m_position ({ 0, 0 })
+        , m_type(Type::Invalid)
+        , m_base(m_position, { Card::width, Card::height })
+    {}
+
+    /**
+     * @brief Construct a new CardStack::CardStack object
+     * 
+     * @param position 
+     * @param type 
+     */
+    CardStack::CardStack(Gfx::IntPoint const& position, Type type)
+    : m_position(position)
+    , m_type(type)
+    , m_rules(rules_for_type(type))
+    , m_base(m_position, { Card::width, Card::height })
+    {
+        VERIFY(type != Type::Invalid);
+        calculate_bounding_box();
+    }
+} // namespace Cards
