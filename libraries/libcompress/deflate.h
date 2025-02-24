a444bedf769b11f6efc5214397edf8a6bd5be126
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-24 19:26:39 +0530

    libraries: libcompress[deflate]

---
+++ libraries/libcompress/deflate.h
@@ -0,0 +1,12 @@
+/**
+ * @file deflate.h
+ * @author Krisna Pranav
+ * @brief deflate
+ * @version 6.0
+ * @date 2025-02-24
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+ 
