15cf9dc0cee578e457ba7a9234dde7af7d86e21c
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-24 16:58:44 +0530

    libraries: libchess[ucicommand]

---
+++ libraries/libchess/ucicommand.h
@@ -0,0 +1,42 @@
+/**
+ * @file ucicommand.h
+ * @author Krisna Pranav
+ * @brief uci command
+ * @version 6.0
+ * @date 2025-02-24
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <mods/optional.h>
+#include <mods/string.h>
+#include <libchess/chess.h>
+#include <libcore/event.h>
+
+namespace Chess::UCI 
+{
+    class Command : public Core::Event
+    {
+    public:
+        enum class Type
+        {
+
+        }; // enum class Type
+            
+        explicit Command(Type type)
+            : Core::Event(to_underlying(type))
+        {}
+
+        virtual String to_string() const = 0;
+
+        virtual ~Command() = default;
+    }; // class Command : public Core::Event
+
+    class UCICommand : public Command
+    {
+
+    } // class UCICommand : public Command
+} // namespace Chess::UCI    
