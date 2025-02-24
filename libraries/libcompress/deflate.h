e7e3fd8dd84141148adc612a1c69f50600896bd9
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-24 19:31:15 +0530

    libraries: libcompress[deflate(CanonicalCode)]

--- libraries/libcompress/deflate.h
+++ libraries/libcompress/deflate.h
@@ -20,5 +20,32 @@
 
 namespace Compress
 {
+    class CanonicalCode 
+    {
+    public:
+        /**
+         * @brief Construct a new Canonical Code object
+         * 
+         */
+        CanonicalCode() = default;
 
+        /**
+         * @return u32 
+         */
+        u32 read_symbol(InputBitStream&) const;
+
+        void write_symbol(OutputBitStream&, u32) const;
+
+        static CanonicalCode const& fixed_literal_codes();
+        static CanonicalCode const& fixed_distance_codes();
+
+        static Optional<CanonicalCode> from_bytes(ReadonlyBytes);
+
+    private:
+        Vector<u16> m_symbol_codes;
+        Vector<u16> m_symbol_values;
+
+        Array<u16, 288> m_bit_codes {}; 
+        Array<u16, 288> m_bit_code_lengths {};
+    }; // class CanonicalCode 
 } // namespace Compress
