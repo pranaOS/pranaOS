e17d05ef5cf0d294658eecb516e22db584faa9b6
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-24 16:31:47 +0530

    libraries: libchess[chess]

---
+++ libraries/libchess/chess.h
@@ -0,0 +1,20 @@
+/**
+ * @file chess.h
+ * @author Krisna Pranav
+ * @brief chess
+ * @version 6.0
+ * @date 2025-02-24
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <mods/hashmap.h>
+#include <mods/iterationdecision.h>
+#include <mods/optional.h>
+#include <mods/stringview.h>
+#include <mods/traits.h>
+#include <mods/vector.h>
+
