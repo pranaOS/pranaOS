6c3ce82b69432eae2fb7f50a5d67d1dda4e7be59
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-25 19:19:36 +0530

    libraries: libcompress[zlib]

---
+++ libraries/libcompress/zlib.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file zlib.cpp
+ * @author Krisna Pranav
+ * @brief zlib
+ * @version 6.0
+ * @date 2025-02-25
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include <mods/span.h>
+#include <mods/types.h>
+#include <libcompress/deflate.h>
+#include <libcompress/zlib.h>
+
+namespace Compress 
+{
+
+} // namespace Compress 
