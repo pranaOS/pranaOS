469de2c283328cc7e103bcf21830e85d002ee475
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-25 19:10:06 +0530

    libraries: libcompress[gzip]

---
+++ libraries/libcompress/gzip.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file gzip.cpp
+ * @author Krisna Pranav
+ * @brief gzip
+ * @version 6.0
+ * @date 2025-02-25
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
