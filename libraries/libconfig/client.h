a629dad8cb806d3e9502f28057fd70cf2c630a5e
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-25 19:55:08 +0530

    libraries: libconfig[client]

--- libraries/libconfig/client.h
+++ libraries/libconfig/client.h
@@ -12,20 +12,153 @@
 #pragma once
 
 #include <configserver/configclientendpoint.h>
+#include <configserver/configserverendpoint.h>
 #include <libcore/file.h>
 #include <libcore/promise.h>
 #include <libcore/standardpaths.h>
-#include <libcip/connectiontoserver.h>
+#include <libipc/connectiontoserver.h>
 
 namespace Config 
 {
 
-    class Client 
+    class Client final
+        : public IPC::ConnectionToServer<ConfigClientEndpoint, ConfigServerEndpoint>
+        , public ConfigClientEndpoint 
     {
+        IPC_CLIENT_CONNECTION(Client, "/tmp/portal/config")
+
+    public:
+        void pledge_domains(Vector<String> const&);
+        void monitor_domain(String const&);
+
+        /**
+         * @param domain 
+         * @return Vector<String> 
+         */
+        Vector<String> list_groups(StringView domain);
+
+        /**
+         * @param domain 
+         * @param group 
+         * @return Vector<String> 
+         */
+        Vector<String> list_keys(StringView domain, StringView group);
+
+        /**
+         * @param domain 
+         * @param group 
+         * @param key 
+         * @param fallback 
+         * @return String 
+         */
+        String read_string(StringView domain, StringView group, StringView key, StringView fallback);
+
+        /**
+         * @param domain 
+         * @param group 
+         * @param key 
+         * @param fallback 
+         * @return i32 
+         */
+        i32 read_i32(StringView domain, StringView group, StringView key, i32 fallback);
+
+        /**
+         * @param domain 
+         * @param group 
+         * @param key 
+         * @param fallback 
+         * @return true 
+         * @return false 
+         */
+        bool read_bool(StringView domain, StringView group, StringView key, bool fallback);
+
+        /**
+         * @param domain 
+         * @param group 
+         * @param key 
+         * @param value 
+         */
+        void write_string(StringView domain, StringView group, StringView key, StringView value);
+
+        /**
+         * @param domain 
+         * @param group 
+         * @param key 
+         * @param value 
+         */
+        void write_i32(StringView domain, StringView group, StringView key, i32 value);
+
+        /**
+         * @param domain 
+         * @param group 
+         * @param key 
+         * @param value 
+         */
+        void write_bool(StringView domain, StringView group, StringView key, bool value);
+
+        /**
+         * @param domain 
+         * @param group 
+         * @param key 
+         */
+        void remove_key(StringView domain, StringView group, StringView key);
+
+        /**
+         * @return Client& 
+         */
+        static Client& the();
+
     private:
+        /**
+         * @brief Construct a new Client object
+         * 
+         * @param socket 
+         */
         explicit Client(NonnullOwnPtr<Core::Stream::LocalSocket> socket)
+            : IPC::ConnectionToServer<ConfigClientEndpoint, ConfigServerEndpoint>(*this, move(socket))
+        {
+        }
+
+        /**
+         * @param domain 
+         * @param group 
+         * @param key 
+         * @param value 
+         */
+        void notify_changed_string_value(String const& domain, String const& group, String const& key, String const& value) override;
+
+        /**
+         * @param domain 
+         * @param group 
+         * @param key 
+         * @param value 
+         */
+        void notify_changed_i32_value(String const& domain, String const& group, String const& key, i32 value) override;
+
+        /**
+         * @param domain 
+         * @param group 
+         * @param key 
+         * @param value 
+         */
+        void notify_changed_bool_value(String const& domain, String const& group, String const& key, bool value) override;
+
+        /**
+         * @param domain 
+         * @param group 
+         * @param key 
+         */
+        void notify_removed_key(String const& domain, String const& group, String const& key) override;
     }; // class Client
 
+    /**
+     * @param domain 
+     * @return Vector<String> 
+     */
+    inline Vector<String> list_groups(StringView domain)
+    {
+        return Client::the().list_groups(domain);
+    }
 
     /**
      * @param domain 
@@ -141,5 +274,4 @@ namespace Config
         Client::the().monitor_domain(domain);
     }
 
-
-} // namespace Config 
+} // namespace Config
