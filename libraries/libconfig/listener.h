ce7cc57750ae4d7fdd352c74923f2f9b9f89ae47
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-25 19:35:17 +0530

    libraries: libconfig[listener]

---
+++ libraries/libconfig/listener.h
@@ -0,0 +1,24 @@
+/**
+ * @file listener.h
+ * @author Krisna Pranav
+ * @brief listener
+ * @version 6.0
+ * @date 2025-02-25
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <mods/forward.h>
+
+namespace Config 
+{
+    
+    class Listener 
+    {
+
+    }; // class Listener 
+
+} // namespace Config 
