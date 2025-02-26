067189b3f4d1c6a153ccf74071c9b32c0f9775cf
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-26 20:39:14 +0530

    libraries: libcore[argsparser]

---
+++ libraries/libcore/argsparser.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file argsparser.cpp
+ * @author Krisna Pranav
+ * @brief args parser
+ * @version 6.0
+ * @date 2025-02-26
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
