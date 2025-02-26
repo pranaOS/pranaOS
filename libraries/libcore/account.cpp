c41e70e7bcfa6369f8dc5bd9711ad9def8b43ba4
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-26 18:47:47 +0530

    libraries: libcore[account]

--- libraries/libcore/account.cpp
+++ libraries/libcore/account.cpp
@@ -73,4 +73,64 @@ namespace Core
         endgrent();
         return extra_gids;
     }
+
+    void Account::delete_password()
+    {
+        m_password_hash = "";
+    }
+
+    /**
+     * @brief Construct a new Account::Account object
+     * 
+     * @param pwd 
+     * @param spwd 
+     * @param extra_gids 
+     */
+    Account::Account(passwd const& pwd, spwd const& spwd, Vector<gid_t> extra_gids)
+        : m_username(pwd.pw_name)
+        , m_password_hash(spwd.sp_pwdp)
+        , m_uid(pwd.pw_uid)
+        , m_gid(pwd.pw_gid)
+        , m_gecos(pwd.pw_gecos)
+        , m_home_directory(pwd.pw_dir)
+        , m_shell(pwd.pw_shell)
+        , m_extra_gids(move(extra_gids))
+    {
+    }
+
+    /**
+     * @return ErrorOr<String> 
+     */
+    ErrorOr<String> Account::generate_passwd_file() const
+    {
+        StringBuilder builder;
+
+        setpwent();
+
+        struct passwd* p;
+        errno = 0;
+        while ((p = getpwent())) {
+            if (p->pw_name == m_username) {
+                builder.appendff("{}:!:{}:{}:{}:{}:{}\n",
+                    m_username,
+                    m_uid, m_gid,
+                    m_gecos,
+                    m_home_directory,
+                    m_shell);
+
+            } else {
+                builder.appendff("{}:!:{}:{}:{}:{}:{}\n",
+                    p->pw_name, p->pw_uid,
+                    p->pw_gid, p->pw_gecos, p->pw_dir,
+                    p->pw_shell);
+            }
+        }
+        endpwent();
+
+        if (errno)
+            return Error::from_errno(errno);
+
+        return builder.to_string();
+    }
+
 } // namespace Core 
