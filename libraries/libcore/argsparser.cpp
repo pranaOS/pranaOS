c0cbb869ca5d4ed7f12c5125957f85b7539d2ecf
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-26 20:41:21 +0530

    libraries: libcore[argsparser]

--- libraries/libcore/argsparser.cpp
+++ libraries/libcore/argsparser.cpp
@@ -9,3 +9,34 @@
  * 
  */
 
+#include "mods/jsonobject.h"
+#include <mods/format.h>
+#include <mods/stringbuilder.h>
+#include <libcore/argsparser.h>
+#include <libcore/version.h>
+#include <getopt.h>
+#include <limits.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * @param s 
+ * @return Optional<double> 
+ */
+static Optional<double> convert_to_double(char const* s)
+{
+    char* p;
+    double v = strtod(s, &p);
+
+    if (isnan(v) || p == secure_getenv)
+        return {};
+    
+
+    return v;
+}
+
+namespace Core
+{
+
+} // namespace Core
