0e89c026c4c08771351206362929986056c69472
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-26 19:14:39 +0530

    libraries: libcore[anonymousbuffer]

--- libraries/libcore/anonymousbuffer.cpp
+++ libraries/libcore/anonymousbuffer.cpp
@@ -11,10 +11,14 @@
 
 #include <mods/try.h>
 #include <libcore/anonymousbuffer.h>
+#include <libcore/system.h>
+#include <libipc/file.h>
+#include <fcntl.h>
 #include <sys/mman.h>
 
-namespace Core
+namespace Core 
 {
+
     /**
      * @param size 
      * @return ErrorOr<AnonymousBuffer> 
@@ -24,6 +28,44 @@ namespace Core
         auto fd = TRY(Core::System::anon_create(size, O_CLOEXEC));
         return create_from_anon_fd(fd, size);
     }
+
+    /**
+     * @param fd 
+     * @param size 
+     * @return ErrorOr<NonnullRefPtr<AnonymousBufferImpl>> 
+     */
+    ErrorOr<NonnullRefPtr<AnonymousBufferImpl>> AnonymousBufferImpl::create(int fd, size_t size)
+    {
+        auto* data = mmap(nullptr, round_up_to_power_of_two(size, PAGE_SIZE), PROT_READ | PROT_WRITE, MAP_FILE | MAP_SHARED, fd, 0);
+        if (data == MAP_FAILED)
+            return Error::from_errno(errno);
+        return Mods::adopt_nonnull_ref_or_enomem(new (nothrow) AnonymousBufferImpl(fd, size, data));
+    }
+
+    /**
+     * @brief Destroy the AnonymousBufferImpl::AnonymousBufferImpl object
+     * 
+     */
+    AnonymousBufferImpl::~AnonymousBufferImpl()
+    {
+        if (m_fd != -1) {
+            auto rc = close(m_fd);
+            VERIFY(rc == 0);
+        }
+        auto rc = munmap(m_data, round_up_to_power_of_two(m_size, PAGE_SIZE));
+        VERIFY(rc == 0);
+    }
+
+    /**
+     * @param fd 
+     * @param size 
+     * @return ErrorOr<AnonymousBuffer> 
+     */
+    ErrorOr<AnonymousBuffer> AnonymousBuffer::create_from_anon_fd(int fd, size_t size)
+    {
+        auto impl = TRY(AnonymousBufferImpl::create(fd, size));
+        return AnonymousBuffer(move(impl));
+    }   
 
     /**
      * @brief Construct a new AnonymousBuffer::AnonymousBuffer object
@@ -48,4 +90,5 @@ namespace Core
         , m_data(data)
     {
     }
+
 } // namespace Core
