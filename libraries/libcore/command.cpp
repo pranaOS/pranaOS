a8ff01376038da6a1a61b7537d72c29611cb9016
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-27 19:39:58 +0530

    libraries: libcore[command]

--- libraries/libcore/command.cpp
+++ libraries/libcore/command.cpp
@@ -9,3 +9,31 @@
  * 
  */
 
+#include "command.h"
+#include <mods/format.h>
+#include <mods/scopeguard.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+namespace Core
+{
+    /**
+     * @param command_string 
+     * @param chdir 
+     * @return ErrorOr<CommandResult> 
+     */
+    ErrorOr<CommandResult> command(String const& command_string, Optional<LexicalPath> chdir)
+    {
+        auto parts = command_string.split(' ');
+
+        if (parts.is_empty())
+            return Error::from_string_literal("empty command"sv);
+        
+        auto program = parts[0];
+        parts.remove(0);
+
+        return command(program, parts, chdir);
+    }
+} // namespace Core
