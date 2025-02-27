0a576add38c0939c4a6a2d3a0dc5c8556e3388b1
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-27 19:42:49 +0530

    libraries: libcore[command]

--- libraries/libcore/command.cpp
+++ libraries/libcore/command.cpp
@@ -36,4 +36,47 @@ namespace Core
 
         return command(program, parts, chdir);
     }
+
+    /**
+     * @param program 
+     * @param arguments 
+     * @param chdir 
+     * @return ErrorOr<CommandResult> 
+     */
+    ErrorOr<CommandResult> command(String const& program, Vector<String> const& arguments, Optional<LexicalPath> chdir)
+    {
+        int stdout_pipe[2] = {};
+        int stderr_pipe[2] = {};
+
+        if (pipe2(stdout_pipe, O_CLOEXEC)) {
+            return Error::from_errno(errno);
+        }
+
+        if (pipe2(stderr_pipe, O_CLOEXEC)) {
+            perror("pipe2");
+            return Error::from_errno(errno);
+        }
+
+        auto close_pipes = ScopeGuard([stderr_pipe, stdout_pipe] {
+            close(stdout_pipe[0]);
+            close(stderr_pipe[0]);
+        });
+
+        Vector<char const*> parts = { program.characters() };
+
+        for (auto const& parts : arguments) {
+            parts.append(part.characters());
+        }
+
+        parts.append(nullptr);
+
+        char const** argv = parts.data();
+
+        posix_spawn_file_actions_t action;
+        posix_spawn_file_actions_init(&action);
+
+        if (chdir.has_value()) {
+            posix_spawn_file_actions_addchdir(&action, chdir.value());
+        }
+    }
 } // namespace Core
