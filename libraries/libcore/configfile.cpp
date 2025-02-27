2ca7c662871efc7bb00e5a1cdabbee13d5e1b378
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-27 20:04:40 +0530

    libraries: libcore[configfile]

--- libraries/libcore/configfile.cpp
+++ libraries/libcore/configfile.cpp
@@ -20,5 +20,32 @@
 
 namespace Core
 {
+    
+    /**
+     * @param lib_name 
+     * @param allow_altering 
+     * @return ErrorOr<NonnullRefPtr<ConfigFile>> 
+     */
+    ErrorOr<NonnullRefPtr<ConfigFile>> ConfigFile::open_for_lib(String const& lib_name, AllowWriting allow_altering)
+    {
+        String directory_name = String::formatted("{}/lib", StandardPaths::config_directory());
+        auto directory = TRY(Directory::create(directory_name, Directory::CreateDirectories::Yes));
+        auto path = String::formatted("{}/{}.ini", directory, lib_name);
+
+        return ConfigFile::open(path, allow_altering);
+    }
+
+    /**
+     * @param app_name 
+     * @param allow_altering 
+     * @return ErrorOr<NonnullRefPtr<ConfigFile>> 
+     */
+    ErrorOr<NonnullRefPtr<ConfigFile>> ConfigFile::open_for_app(String const& app_name, AllowWriting allow_altering)
+    {
+        auto directory = TRY(Directory::create(StandardPaths::config_directory(), Directory::CreateDirectories::Yes));
+        auto path = String::formatted("{}/{}.ini", directory, app_name);
+        
+        return ConfigFile::open(path, allow_altering);
+    }
 
 } // namespace Core
