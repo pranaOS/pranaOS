373c0e6036c61805e6bdfa1228c4eb832ce3f37b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-27 20:01:57 +0530

    libraries: libcore[configfile]

---
+++ libraries/libcore/configfile.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file configfile.cpp
+ * @author Krisna Pranav
+ * @brief config file
+ * @version 6.0
+ * @date 2025-02-27
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
