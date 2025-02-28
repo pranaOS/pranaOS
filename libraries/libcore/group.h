d94a996b4e96f2d5fe2bd7979fec858b60802c66
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-28 23:05:42 +0530

    libraries: libcore[group]

---
+++ libraries/libcore/group.h
@@ -0,0 +1,11 @@
+/**
+ * @file group.h
+ * @author Krisna Pranav
+ * @brief group
+ * @version 6.0
+ * @date 2025-02-28
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
