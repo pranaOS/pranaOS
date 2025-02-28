0c8aab25bd8f0680c318e45953f4df7b9448ac34
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-28 20:22:17 +0530

    libraries: libcore[filepermissionsmask]

---
+++ libraries/libcore/filepermissionsmask.h
@@ -0,0 +1,26 @@
+/**
+ * @file filepermissionsmask.h
+ * @author Krisna Pranav
+ * @brief File Permissions Mask
+ * @version 6.0
+ * @date 2025-02-28
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <mods/error.h>
+#include <mods/string.h>
+#include <sys/stat.h>
+
+namespace Core
+{
+
+    class FilePermissionMask
+    {
+
+    }; // class FilePermissionMask
+    
+} // namespace Core
