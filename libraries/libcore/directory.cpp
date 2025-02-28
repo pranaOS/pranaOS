3cb573b4eec91fef99e57ea0c72b169c52930d31
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-28 19:00:22 +0530

    libraries: libcore[directory]

--- libraries/libcore/directory.cpp
+++ libraries/libcore/directory.cpp
@@ -57,4 +57,31 @@ namespace Core
 
         return m_path.value();
     }
+
+    /**
+     * @param filename 
+     * @param mode 
+     * @return ErrorOr<NonnullOwnPtr<Stream::File>> 
+     */
+    ErrorOr<NonnullOwnPtr<Stream::File>> Directory::open(StringView filename, Stream::OpenMode mode) const
+    {
+        auto fd = TRY(System::openat(m_directory_fd, filename, Stream::File::open_mode_to_options(mode)));
+        return Stream::File::adopt_fd(fd, mode);
+    }
+
+    /**
+     * @return ErrorOr<struct stat> 
+     */
+    ErrorOr<struct stat> Directory::stat() const
+    {
+        return System::fstat(m_directory_fd);
+    }   
+
+    /**
+     * @return ErrorOr<DirIterator> 
+     */
+    ErrorOr<DirIterator> Directory::create_iterator() const
+    {
+        return DirIterator { TRY(path()).string() };
+    }
 } // namespace Core
