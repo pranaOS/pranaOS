ef81bcedf5dcc66e353ccd155e9815c992104d86
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-28 22:43:08 +0530

    libraries: libcore[filewatcher]

---
+++ libraries/libcore/filewatcher.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file filewatcher.cpp
+ * @author Krisna Pranav
+ * @brief File Watcher
+ * @version 6.0
+ * @date 2025-02-28
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+ 
