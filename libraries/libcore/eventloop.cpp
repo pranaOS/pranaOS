f0d434d99a5a085b163f6797b5948d59b9054b44
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-28 19:50:53 +0530

    libraries: libcore[eventloop]

---
+++ libraries/libcore/eventloop.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file eventloop.cpp
+ * @author Krisna Pranav
+ * @brief event loop
+ * @version 6.0
+ * @date 2025-02-28
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
