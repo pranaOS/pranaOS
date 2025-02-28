0dbf166821a73cc17a0792e26773eac33e531695
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-28 20:05:53 +0530

    libraries: libcore[file]

--- libraries/libcore/file.h
+++ libraries/libcore/file.h
@@ -18,9 +18,53 @@
 
 namespace Core
 {
+
     class File final : public IODevice
     {
+        C_OBJECT(File);
 
+
+        enum class RecursionMode
+        {
+            Allowed,
+            Disallowed
+        }; // enum class RecursionMode
+
+        enum class LinkMode 
+        {
+            Allowed,
+            Disallowed
+        }; // enum class LinkMode 
+
+        enum class AddDuplicateFileMarker
+        {
+            Yes,
+            No,
+        }; // enum class AddDuplicateFileMarker
+
+        enum class PreserveMode 
+        {
+            Nothing,
+            PermissionsOwnershipTimestamp,
+        }; // enum class PreserveMode 
+        
+
+    private:
+        /**
+         * @brief Construct a new File object
+         * 
+         * @param parent 
+         */
+        File(Object* parent = nullptr)
+            : IODevice(parent)
+        {}
+
+        explicit File(String filename, Object* parent = nullptr);
+
+        bool open_impl(OpenMode, mode_t);
+
+        String m_filename;
+        ShouldCloseFileDescriptor m_should_close_file_descriptor { ShouldCloseFileDescriptor::Yes };
     }; // class File final : public IODevice
 
 } // namespace Core
