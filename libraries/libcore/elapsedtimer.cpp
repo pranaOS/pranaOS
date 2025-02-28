f858f58e4fee726d7ce5afec95cb9a7baafd125c
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-28 19:41:11 +0530

    libraries: libcore[elapsedtimer]

--- libraries/libcore/elapsedtimer.cpp
+++ libraries/libcore/elapsedtimer.cpp
@@ -15,9 +15,8 @@
 #include <sys/time.h>
 #include <time.h>
 
-namespace Core
+namespace Core 
 {
-
     /**
      * @return ElapsedTimer 
      */
@@ -32,11 +31,46 @@ namespace Core
     {
         m_valid = true;
         timespec now_spec;
+
         clock_gettime(m_precise ? CLOCK_MONOTONIC : CLOCK_MONOTONIC_COARSE, &now_spec);
 
         m_origin_time.tv_sec = now_spec.tv_sec;
         m_origin_time.tv_usec = now_spec.tv_nsec / 1000;
     }
 
-    
+    void ElapsedTimer::reset()
+    {
+        m_valid = false;
+        m_origin_time = { 0, 0 };
+    }
+
+    /**
+     * @return int 
+     */
+    int ElapsedTimer::elapsed() const
+    {
+        VERIFY(is_valid());
+        struct timeval now;
+        timespec now_spec;
+
+        clock_gettime(m_precise ? CLOCK_MONOTONIC : CLOCK_MONOTONIC_COARSE, &now_spec);
+
+        now.tv_sec = now_spec.tv_sec;
+        now.tv_usec = now_spec.tv_nsec / 1000;
+
+        struct timeval diff;
+
+        timeval_sub(now, m_origin_time, diff);
+
+        return diff.tv_sec * 1000 + diff.tv_usec / 1000;
+    }
+
+    /**
+     * @return Time 
+     */
+    Time ElapsedTimer::elapsed_time() const
+    {
+        return Time::from_milliseconds(elapsed());
+    }
+
 } // namespace Core
