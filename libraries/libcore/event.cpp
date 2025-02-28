6945d6d36971dd7b252091e260b0dcf5b1ce8a1a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-28 19:46:07 +0530

    libraries: libcore[event]

---
+++ libraries/libcore/event.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file event.cpp
+ * @author Krisna Pranav
+ * @brief event
+ * @version 6.0
+ * @date 2025-02-28
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include <mods/weakptr.h>
+#include <libcore/event.h>
+#include <libcore/object.h>
+
+namespace Core
+{
+} // namespace Core
