fd3442e6f61180eab134c9940f6c8f5eadcbdbc0
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-28 19:47:04 +0530

    libraries: libcore[event]

--- libraries/libcore/event.cpp
+++ libraries/libcore/event.cpp
@@ -13,6 +13,65 @@
 #include <libcore/event.h>
 #include <libcore/object.h>
 
-namespace Core
+namespace Core 
 {
+
+    /**
+     * @brief Construct a new ChildEvent::ChildEvent object
+     * 
+     * @param type 
+     * @param child 
+     * @param insertion_before_child 
+     */
+    ChildEvent::ChildEvent(Type type, Object& child, Object* insertion_before_child)
+        : Core::Event(type)
+        , m_child(child.make_weak_ptr())
+        , m_insertion_before_child(Mods::make_weak_ptr_if_nonnull(insertion_before_child))
+    {
+    }
+
+    /**
+     * @return Object* 
+     */
+    Object* ChildEvent::child()
+    {
+        if (auto ref = m_child.strong_ref())
+            return ref.ptr();
+
+        return nullptr;
+    }
+
+    /**
+     * @return Object const* 
+     */
+    Object const* ChildEvent::child() const
+    {
+        if (auto ref = m_child.strong_ref())
+            return ref.ptr();
+
+        return nullptr;
+    }
+
+    /**
+     * @return Object* 
+     */
+    Object* ChildEvent::insertion_before_child()
+    {
+        if (auto ref = m_insertion_before_child.strong_ref())
+            return ref.ptr();
+
+        return nullptr;
+    }
+
+    /**
+     * @return Object const* 
+     */
+    Object const* ChildEvent::insertion_before_child() const
+    {
+        if (auto ref = m_insertion_before_child.strong_ref())
+            return ref.ptr();
+
+        return nullptr;
+    }
+
 } // namespace Core
