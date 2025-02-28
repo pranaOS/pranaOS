9241d32f765945b0550901e5aa26bf3fbd610c91
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-28 20:33:33 +0530

    libraries: libcore[filestream]

--- libraries/libcore/filestream.h
+++ libraries/libcore/filestream.h
@@ -46,7 +46,48 @@ namespace Core
             return InputFileStream { move(file) };
         }
 
-        
+        /**
+         * @param filename 
+         * @param mode 
+         * @param permissions 
+         * @return ErrorOr<Buffered<InputFileStream>> 
+         */
+        static ErrorOr<Buffered<InputFileStream>> open_buffered(StringView filename, OpenMode mode = OpenMode::ReadOnly, mode_t permissions = 0644)
+        {
+            VERIFY(has_flag(mode, OpenMode::ReadOnly));
+            auto file = TRY(File::open(filename, mode, permissions));
+
+            return Buffered<InputFileStream> { move(file) };
+        }
+
+        /**
+         * @param bytes 
+         * @return size_t 
+         */
+        size_t read(Bytes bytes) override
+        {
+            if (has_any_error())
+                return 0;
+
+            auto const buffer = m_file->read(bytes.size());
+            return buffer.bytes().copy_to(bytes);
+        }
+
+        /**
+         * @param bytes 
+         * @return true 
+         * @return false 
+         */
+        bool read_or_error(Bytes bytes) override
+        {
+            if (read(bytes) < bytes.size()) {
+                set_fatal_error();
+                return false;
+            }
+
+            return true;
+        }
+
     private:
         NonnullRefPtr<File> m_file;
     }; // class InputFileStream final : public InputStream
