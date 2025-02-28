7e6e19e8fc0130812fd31ad61b402be1bf17e05c
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-28 20:03:41 +0530

    libraries: libcore[file]

---
+++ libraries/libcore/file.h
@@ -0,0 +1,26 @@
+/**
+ * @file file.h
+ * @author Krisna Pranav
+ * @brief file
+ * @version 6.0
+ * @date 2025-02-28
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <mods/error.h>
+#include <mods/string.h>
+#include <libcore/iodevice.h>
+#include <sys/stat.h>
+
+namespace Core
+{
+    class File final : public IODevice
+    {
+
+    }; // class File final : public IODevice
+
+} // namespace Core
