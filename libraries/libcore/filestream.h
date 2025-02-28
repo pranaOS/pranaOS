b305765506a1888f29f38389ee2a1a49af61b875
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-28 20:32:11 +0530

    libraries: libcore[filestream]

--- libraries/libcore/filestream.h
+++ libraries/libcore/filestream.h
@@ -8,3 +8,46 @@
  * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
  * 
  */
+
+#pragma once
+
+#include <mods/buffered.h>
+#include <mods/bytebuffer.h>
+#include <mods/stream.h>
+#include <mods/try.h>
+#include <libcore/file.h>
+
+namespace Core
+{
+
+    class InputFileStream final : public InputStream
+    {
+    public:
+        /**
+         * @brief Construct a new Input File Stream object
+         * 
+         * @param file 
+         */
+        explicit InputFileStream(NonnullRefPtr<File> file)
+            : m_file(file)
+        {}
+
+        /**
+         * @param filename 
+         * @param mode 
+         * @param permissions 
+         * @return ErrorOr<InputFileStream> 
+         */
+        static ErrorOr<InputFileStream> open(StringView filename, OpenMode mode = OpenMode::ReadOnly, mode_t permissions = 0644)
+        {
+            VERIFY(has_flag(mode, OpenMode::ReadOnly));
+            auto file = TRY(File::open(filename, mode, permissions));
+
+            return InputFileStream { move(file) };
+        }
+
+        
+    private:
+        NonnullRefPtr<File> m_file;
+    }; // class InputFileStream final : public InputStream
+} // namespace Core
