0fc2eeecea7a993272651beb9815a8d31fcfeb3c
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-28 19:30:04 +0530

    libraries: libcore[diriterator]

---
+++ libraries/libcore/diriterator.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file diriterator.cpp
+ * @author Krisna Pranav
+ * @brief dir iterator
+ * @version 6.0
+ * @date 2025-02-28
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include <mods/vector.h>
+#include <libcore/diriterator.h>
+#include <errno.h>
+#include <unistd.h>
+
+namespace Core
+{
+    
+} // namespace Core
