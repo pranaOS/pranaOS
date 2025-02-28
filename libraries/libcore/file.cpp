257029fece268d0d0a271059e92b3e42588c1415
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-28 20:11:40 +0530

    libraries: libcore[file]

---
+++ libraries/libcore/file.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file file.cpp
+ * @author Krisna Pranav
+ * @brief file
+ * @version 6.0
+ * @date 2025-02-28
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
