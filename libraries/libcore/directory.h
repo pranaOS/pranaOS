3c317ab313d14132d6e8a0ad6c8d422ebe68e876
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-28 18:56:00 +0530

    libraries: libcore[directory]

--- libraries/libcore/directory.h
+++ libraries/libcore/directory.h
@@ -20,24 +20,84 @@
 #include <dirent.h>
 #include <sys/stat.h>
 
-namespace Core
+namespace Core 
 {
+
     class DirIterator;
 
-    class Directory
+    class Directory 
     {
         MOD_MAKE_NONCOPYABLE(Directory);
 
     public:
-        Directory();
+        /**
+         * @brief Construct a new Directory object
+         * 
+         */
+        Directory(Directory&&);
 
+        /**
+         * @brief Destroy the Directory object
+         * 
+         */
         ~Directory();
 
         enum class CreateDirectories : bool 
         {
-            NO,
-            YES
+            No,
+            Yes,
         }; // enum class CreateDirectories : bool 
+
+        /**
+         * @param path 
+         * @param creation_mode 
+         * @return ErrorOr<Directory> 
+         */
+        static ErrorOr<Directory> create(LexicalPath path, CreateDirectories, mode_t creation_mode = 0755);
+
+        /**
+         * @param path 
+         * @param creation_mode 
+         * @return ErrorOr<Directory> 
+         */
+        static ErrorOr<Directory> create(String path, CreateDirectories, mode_t creation_mode = 0755);
+
+        /**
+         * @param fd 
+         * @param path 
+         * @return ErrorOr<Directory> 
+         */
+        static ErrorOr<Directory> adopt_fd(int fd, Optional<LexicalPath> path = {});
+
+        /**
+         * @param filename 
+         * @param mode 
+         * @return ErrorOr<NonnullOwnPtr<Stream::File>> 
+         */
+        ErrorOr<NonnullOwnPtr<Stream::File>> open(StringView filename, Stream::OpenMode mode) const;
+
+        /**
+         * @return ErrorOr<struct stat> 
+         */
+        ErrorOr<struct stat> stat() const;
+
+        /**
+         * @brief Create a iterator object
+         * 
+         * @return ErrorOr<DirIterator> 
+         */
+        ErrorOr<DirIterator> create_iterator() const;
+
+        /**
+         * @return ErrorOr<LexicalPath> 
+         */
+        ErrorOr<LexicalPath> path() const;
+
+        /**
+         * @param fd 
+         * @return ErrorOr<bool> 
+         */
+        static ErrorOr<bool> is_valid_directory(int fd);
 
     private:
         /**
@@ -50,18 +110,17 @@ namespace Core
 
         /**
          * @param path 
+         * @param creation_mode 
          * @return ErrorOr<void> 
          */
-        static ErrorOr<void> ensure_directory(LexicalPath const& path);
+        static ErrorOr<void> ensure_directory(LexicalPath const& path, mode_t creation_mode = 0755);
 
         Optional<LexicalPath> m_path;
 
         int m_directory_fd;
-
     }; // class Directory
 
 } // namespace Core
-
 
 namespace Mods
 {
@@ -69,17 +128,19 @@ namespace Mods
      * @tparam  
      */
     template<>
-    struct Formatter<Core::Directory> : Formatter<StringView>
-    {
-        ErrorOr<void> format()
+    struct Formatter<Core::Directory> : Formatter<StringView> {
+        /**
+         * @param builder 
+         * @param directory 
+         * @return ErrorOr<void> 
+         */
+        ErrorOr<void> format(FormatBuilder& builder, Core::Directory const& directory)
         {
             auto path = directory.path();
-
-            if (path.is_error()) {
-                return Formatter<StringView>::format(builder);
-            }
-
-            return Formatter<StringView>::format(builder);
+            if (path.is_error())
+                return Formatter<StringView>::format(builder, "<unknown>");
+            return Formatter<StringView>::format(builder, path.release_value().string());
         }
-    }; // struct Formatter<Core::Directory> : Formatter<StringView>
+    };
+
 } // namespace Mods
