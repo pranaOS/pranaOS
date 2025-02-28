bc63c5c622975903e837aedf81ffe96e632e57a3
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-28 20:07:45 +0530

    libraries: libcore[file]

--- libraries/libcore/file.h
+++ libraries/libcore/file.h
@@ -47,7 +47,46 @@ namespace Core
             Nothing,
             PermissionsOwnershipTimestamp,
         }; // enum class PreserveMode 
-        
+
+        struct CopyError : public Error
+        {
+            /**
+             * @brief Construct a new CopyError object
+             * 
+             * @param error_code 
+             * @param t 
+             */
+            CopyError(int error_code, bool t)
+                : Error(error_code)
+                , tried_recursing(t)
+            {
+            }
+
+            bool tried_recursing;
+        }; // struct CopyError : public Error
+
+        /**
+         * @param dst_path 
+         * @param src_stat 
+         * @param source 
+         * @return ErrorOr<void, CopyError> 
+         */
+        static ErrorOr<void, CopyError> copy_file(String const& dst_path, struct stat const& src_stat, File& source, PreserveMode = PreserveMode::Nothing);
+
+        /**
+         * @param dst_path 
+         * @param src_path 
+         * @param src_stat 
+         * @return ErrorOr<void, CopyError> 
+         */
+        static ErrorOr<void, CopyError> copy_directory(String const& dst_path, String const& src_path, struct stat const& src_stat, LinkMode = LinkMode::Disallowed, PreserveMode = PreserveMode::Nothing);
+
+        /**
+         * @param dst_path 
+         * @param src_path 
+         * @return ErrorOr<void, CopyError> 
+         */
+        static ErrorOr<void, CopyError> copy_file_or_directory(String const& dst_path, String const& src_path, RecursionMode = RecursionMode::Allowed, LinkMode = LinkMode::Disallowed, AddDuplicateFileMarker = AddDuplicateFileMarker::Yes, PreserveMode = PreserveMode::Nothing);
 
     private:
         /**
