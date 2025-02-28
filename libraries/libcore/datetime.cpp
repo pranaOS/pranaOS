9d2862d1f12157bbcfb66f0926e1d71e0b547c5b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-28 18:34:12 +0530

    libraries: libcore[datetime]

---
+++ libraries/libcore/datetime.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file datetime.cpp
+ * @author Krisna Pranav
+ * @brief datetime
+ * @version 6.0
+ * @date 2025-02-28
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include <mods/charactertypes.h>
+#include <mods/dateconstants.h>
+#include <mods/stringbuilder.h>
+#include <mods/time.h>
+#include <errno.h>
+#include <time.h>
+
+namespace Core
+{
+    
+} // namespace Core
