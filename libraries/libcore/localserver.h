9787b415f52440c42209f3254b2f548ff260bea9
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-01 16:28:22 +0530

    libraries: libcore[localserver]

---
+++ libraries/libcore/localserver.h
@@ -0,0 +1,22 @@
+/**
+ * @file localserver.h
+ * @author Krisna Pranav
+ * @brief local server
+ * @version 6.0
+ * @date 2025-03-01
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+namespace Core
+{
+    
+    class LocalServer : public Object
+    {
+
+    }; // class LocalServer : public Object
+    
+} // namespace Core
