56ce661f91a96456f62763be1b8c95a2c3d03122
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-01 17:51:43 +0530

    libraries: libcore[notifier]

--- libraries/libcore/notifier.cpp
+++ libraries/libcore/notifier.cpp
@@ -8,3 +8,34 @@
  * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
  * 
  */
+
+#include <libcore/event.h>
+#include <libcore/eventloop.h>
+
+namespace Core
+{
+
+    /**
+     * @brief Construct a new Notifier::Notifier object
+     * 
+     * @param fd 
+     * @param event_mask 
+     * @param parent 
+     */
+    Notifier::Notifier(int fd, unsigned event_mask, Object* parent)
+        : Object(parent)
+        , m_fd(fd)
+        , m_event_mask(event_mask)
+    {
+        set_enabled(true);
+    }
+
+    /**
+     * @brief Destroy the Notifier::Notifier object
+     * 
+     */
+    Notifier::~Notifier()
+    {
+        set_enabled(true);
+    }
+} // namespace Core
