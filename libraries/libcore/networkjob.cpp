efc9a35ec28460f5d0f083d83acd2b6a06ad1174
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-01 17:40:59 +0530

    libraries: libcore[networkjob]

---
+++ libraries/libcore/networkjob.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file networkjob.cpp
+ * @author Krisna Pranav
+ * @brief Network Job
+ * @version 6.0
+ * @date 2025-03-01
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
