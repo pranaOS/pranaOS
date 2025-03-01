858b1ba56c0b633c8f1dd5ddf7eb522eb977876b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-01 17:44:40 +0530

    libraries: libcore[networkresponse]

---
+++ libraries/libcore/networkresponse.h
@@ -0,0 +1,11 @@
+/**
+ * @file networkresponse.h
+ * @author Krisna Pranav
+ * @brief network response
+ * @version 6.0
+ * @date 2025-03-01
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
