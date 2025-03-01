851ec0f18a303d0c27c4950d042e808f8ccb3907
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-01 17:42:27 +0530

    libraries: libcore[networkjob]

--- libraries/libcore/networkjob.cpp
+++ libraries/libcore/networkjob.cpp
@@ -8,3 +8,47 @@
  * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
  * 
  */
+
+#include <mods/debug.h>
+#include <libcore/networkjob.h>
+#include <libcore/networkresponse.h>
+
+namespace Core
+{
+
+    /**
+     * @brief Construct a new NetworkJob::NetworkJob object
+     * 
+     * @param output_stream 
+     */
+    NetworkJob::NetworkJob(Core::Stream::Stream& output_stream)
+        : m_output_stream(output_stream)
+    {
+    }
+
+    void NetworkJob::start(Core::Stream::Socket&)
+    {
+    }
+
+    void NetworkJob::shutdown(ShutdownMode)
+    {
+    }
+
+    /**
+     * @param response 
+     */
+    void NetworkJob::did_finish(NonnullRefPtr<NetworkResponse>&& response)
+    {
+        if (is_cancelled())
+            return;
+
+        NonnullRefPtr<NetworkJob> protector(*this);
+
+        m_response = move(response);
+        dbgln_if(NETWORKJOB_DEBUG, "{} job did_finish", *this);
+        VERIFY(on_finish);
+        on_finish(true);
+        shutdown(ShutdownMode::DetachFromSocket);
+    }
+    
+} // namespace Core
