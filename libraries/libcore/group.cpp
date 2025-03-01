c255e3ff9fef6f279fcc62f17e6d6d2b8a45cc8f
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-01 12:46:36 +0530

    libraries: libcore[group]

---
+++ libraries/libcore/group.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file group.cpp
+ * @author Krisna Pranav
+ * @brief group
+ * @version 6.0 
+ * @date 2025-03-01
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+namespace Core
+{
+    #ifndef MOD_OS_BSD_GENERIC
+    /**
+     * @param group 
+     * @return ErrorOr<void> 
+     */
+    ErrorOr<void> Group::add_group(Group& group)
+    {        
+    }
+    #endif
+} // namespace Core
