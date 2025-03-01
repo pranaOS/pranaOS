285489e2447d4ba936f322e05e2d904843e25ca4
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-01 16:57:54 +0530

    libraries: libcore[lockfile]

---
+++ libraries/libcore/lockfile.h
@@ -0,0 +1,17 @@
+/**
+ * @file lockfile.h
+ * @author Krisna Pranav
+ * @brief Lock File
+ * @version 6.0
+ * @date 2025-03-01
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+namespace Core
+{
+
+} // namespace Core
