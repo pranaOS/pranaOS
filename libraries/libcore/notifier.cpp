7230f52c57d34b6be9100b4d59031f4c30883c19
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-01 17:50:13 +0530

    libraries: libcore[notifier]

---
+++ libraries/libcore/notifier.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file notifier.cpp
+ * @author Krisna Pranav
+ * @brief notifier
+ * @version 6.0
+ * @date 2025-03-01
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
