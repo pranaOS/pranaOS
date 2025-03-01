44c91c7286e19f5dc52d490c3910b1435f1b6b23
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-01 16:47:37 +0530

    libraries: libcore[localserver]

--- libraries/libcore/localserver.cpp
+++ libraries/libcore/localserver.cpp
@@ -34,4 +34,50 @@ namespace Core
     LocalServer::LocalServer(Object* parent)
         : Object(parent)
     {}  
+
+
+    /**
+     * @brief Destroy the LocalServer::LocalServer object
+     * 
+     */
+    LocalServer::~LocalServer()
+    {
+        if (m_fd >= 0)
+            ::close(m_fd);
+    }
+
+    /**
+     * @param socket_path 
+     * @return ErrorOr<void> 
+     */
+    ErrorOr<void> LocalServer::take_over_from_system_server(String const& socket_path)
+    {
+        if (m_listening)
+            return Error::from_string_literal("Core::LocalServer: Can't perform socket takeover when already listening"sv);
+
+        auto socket = TRY(take_over_socket_from_system_server(socket_path));
+        m_fd = TRY(socket->release_fd());
+
+        m_listening = true;
+        setup_notifier();
+        return {};
+    }
+
+    void LocalServer::setup_notifier()
+    {
+        m_notifier = Notifier::construct(m_fd, Notifier::Event::Read, this);
+        
+        m_notifier->on_ready_to_read = [this] {
+            if (on_accept) {
+                auto maybe_client_socket = accept();
+                if (maybe_client_socket.is_error()) {
+                    dbgln("LocalServer::on_ready_to_read: Error accepting a connection: {} (FIXME: should propagate!)", maybe_client_socket.error());
+                    return;
+                }
+
+                on_accept(maybe_client_socket.release_value());
+            }
+        };
+    }
+
 } // namespace Core
