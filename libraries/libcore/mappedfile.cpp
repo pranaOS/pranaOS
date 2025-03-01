0cb02e1bcb4a8465f004b46cbe302480379c2de0
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-01 17:05:37 +0530

    libraries: libcore[mappedfile]

--- libraries/libcore/mappedfile.cpp
+++ libraries/libcore/mappedfile.cpp
@@ -9,7 +9,67 @@
  * 
  */
 
-namespace Core
+#include <mods/scopeguard.h>
+#include <mods/string.h>
+#include <libcore/mappedfile.h>
+#include <libcore/system.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+namespace Core 
 {
-    
+
+    /**
+     * @param path 
+     * @return ErrorOr<NonnullRefPtr<MappedFile>> 
+     */
+    ErrorOr<NonnullRefPtr<MappedFile>> MappedFile::map(String const& path)
+    {
+        auto fd = TRY(Core::System::open(path, O_RDONLY | O_CLOEXEC, 0));
+        return map_from_fd_and_close(fd, path);
+    }
+
+    /**
+     * @param fd 
+     * @param path 
+     * @return ErrorOr<NonnullRefPtr<MappedFile>> 
+     */
+    ErrorOr<NonnullRefPtr<MappedFile>> MappedFile::map_from_fd_and_close(int fd, [[maybe_unused]] String const& path)
+    {
+        TRY(Core::System::fcntl(fd, F_SETFD, FD_CLOEXEC));
+
+        ScopeGuard fd_close_guard = [fd] {
+            close(fd);
+        };
+
+        auto stat = TRY(Core::System::fstat(fd));
+        auto size = stat.st_size;
+
+        auto* ptr = TRY(Core::System::mmap(nullptr, size, PROT_READ, MAP_SHARED, fd, 0, 0, path));
+
+        return adopt_ref(*new MappedFile(ptr, size));
+    }
+
+    /**
+     * @brief Construct a new MappedFile::MappedFile object
+     * 
+     * @param ptr 
+     * @param size 
+     */
+    MappedFile::MappedFile(void* ptr, size_t size)
+        : m_data(ptr)
+        , m_size(size)
+    {
+    }
+
+    /**
+     * @brief Destroy the MappedFile::MappedFile object
+     * 
+     */
+    MappedFile::~MappedFile()
+    {
+        MUST(Core::System::munmap(m_data, m_size));
+    }
+
 } // namespace Core
