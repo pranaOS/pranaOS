01fc0b1136ae0d9b06b764b7c135beac6a38f8bf
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-01 17:30:06 +0530

    libraries: libcore[mimedata]

--- libraries/libcore/mimedata.cpp
+++ libraries/libcore/mimedata.cpp
@@ -15,5 +15,37 @@
 
 namespace Core
 {
+    
+    /**
+     * @return Vector<String> 
+     */
+    Vector<String> MimeData::formats() const
+    {
+        Vector<String> mime_types;
+        mime_types.ensure_capacity(m_data.size());
 
+        for (auto& it : m_data)
+            mime_types.unchecked_append(it.key);
+
+        return mime_types;
+    }
+
+    /**
+     * @return Vector<URL> 
+     */
+    Vector<URL> MimeData::urls() const
+    {
+        auto it = m_data.find("text/uri-list");
+
+        if (it == m_data.end())
+            return {};
+        
+        Vector<URL> urls;
+
+        for (auto& line : StringView(it->value).split_view('\n')) {
+            urls.append(URL(line));
+        }
+
+        return urls;
+    }
 } // namespace Core
