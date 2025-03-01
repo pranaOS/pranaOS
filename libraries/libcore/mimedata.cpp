934f62db7c3dbb9d0d7fc95d5c46474ee317d6bb
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-01 17:24:20 +0530

    libraries: libcore[mimedata]

---
+++ libraries/libcore/mimedata.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file mimedata.cpp
+ * @author Krisna Pranav
+ * @brief mime data
+ * @version 6.0
+ * @date 2025-03-01
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
