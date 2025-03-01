b761df6df8f731a40b32a0264401cb7f071a6b76
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-01 17:02:57 +0530

    libraries: libcore[mappedfile]

---
+++ libraries/libcore/mappedfile.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file mappedfile.cpp
+ * @author Krisna Pranav
+ * @brief mapped file
+ * @version 6.0
+ * @date 2025-03-01
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
