42df31cd570fc76cbcfe071c2c46278894ebdfe3
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-01 16:37:34 +0530

    libraries: libcore[localserver]

--- libraries/libcore/localserver.h
+++ libraries/libcore/localserver.h
@@ -11,17 +11,51 @@
 
 #pragma once
 
-namespace Core
+#include <LibCore/Notifier.h>
+#include <LibCore/Object.h>
+#include <LibCore/Stream.h>
+
+namespace Core 
 {
-    
-    class LocalServer : public Object
+
+    class LocalServer : public Object 
     {
+        C_OBJECT(LocalServer)
     public:
         /**
          * @brief Destroy the LocalServer object
          * 
          */
         virtual ~LocalServer() override;
+
+        /**
+         * @param path 
+         * @return ErrorOr<void> 
+         */
+        ErrorOr<void> take_over_from_system_server(String const& path = String());
+
+        /**
+         * @return true 
+         * @return false 
+         */
+        bool is_listening() const 
+        { 
+            return m_listening; 
+        }
+
+        /**
+         * @param address 
+         * @return true 
+         * @return false 
+         */
+        bool listen(String const& address);
+
+        /**
+         * @return ErrorOr<NonnullOwnPtr<Stream::LocalSocket>> 
+         */
+        ErrorOr<NonnullOwnPtr<Stream::LocalSocket>> accept();
+
+        Function<void(NonnullOwnPtr<Stream::LocalSocket>)> on_accept;
 
     private:
         /**
