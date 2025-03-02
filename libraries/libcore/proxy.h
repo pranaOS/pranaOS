4c01c201c93b98ea4cfd176cfbce2f31f4f7e946
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-02 16:13:07 +0530

    libraries: libcore[proxy]

---
+++ libraries/libcore/proxy.h
@@ -0,0 +1,14 @@
+/**
+ * @file proxy.h
+ * @author Krisna Pranav
+ * @brief proxy
+ * @version 6.0
+ * @date 2025-03-02
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <mods/error.h>
