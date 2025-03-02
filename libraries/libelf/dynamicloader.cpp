97518a6df8934fca1d032acb842a192e8cd7cb1c
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-02 15:31:17 +0530

    libraries: libelf[dynamicloader]

---
+++ libraries/libelf/dynamicloader.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file dynamicloader.cpp
+ * @author Krisna Pranav
+ * @brief Dynamic Loader
+ * @version 6.0
+ * @date 2024-10-27
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
