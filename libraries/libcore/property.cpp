8d5c0852fb7fe13e9064db82521d32634ca066b1
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-02 15:49:49 +0530

    libraries: libcore[property]

---
+++ libraries/libcore/property.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file property.cpp
+ * @author Krisna Pranav
+ * @brief property
+ * @version 6.0
+ * @date 2025-03-02
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
