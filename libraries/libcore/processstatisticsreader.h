9680e8a5be22ead43bbf927c3cad3d363699c139
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-02 11:06:19 +0530

    libraries: libcore[processstatisticsreader]

--- libraries/libcore/processstatisticsreader.h
+++ libraries/libcore/processstatisticsreader.h
@@ -9,21 +9,20 @@
  * 
  */
 
-#pragma once 
+#pragma once
 
-#include <mods/hashmap.h>
 #include <mods/string.h>
+#include <libcore/file.h>
 #include <unistd.h>
 
 namespace Core 
 {
 
-    struct ThreadStatistics 
-    {
+    struct ThreadStatistics {
         pid_t tid;
-
         unsigned times_scheduled;
-        unsigned ticks;
+        u64 time_user;
+        u64 time_kernel;
         unsigned syscall_count;
         unsigned inode_faults;
         unsigned zero_faults;
@@ -34,17 +33,13 @@ namespace Core
         unsigned ipv4_socket_write_bytes;
         unsigned file_read_bytes;
         unsigned file_write_bytes;
-
         String state;
-        String name;
-
         u32 cpu;
         u32 priority;
-        u32 effective_priority;        
+        String name;
     }; // struct ThreadStatistics
 
-    struct ProcessStatistics 
-    {
+    struct ProcessStatistics {
         pid_t pid;
         pid_t pgid;
         pid_t pgp;
@@ -52,14 +47,13 @@ namespace Core
         uid_t uid;
         gid_t gid;
         pid_t ppid;
-
         unsigned nfds;
-
+        bool kernel;
         String name;
+        String executable;
         String tty;
         String pledge;
         String veil;
-
         size_t amount_virtual;
         size_t amount_resident;
         size_t amount_shared;
@@ -68,20 +62,33 @@ namespace Core
         size_t amount_purgeable_volatile;
         size_t amount_purgeable_nonvolatile;
 
-        int icon_id;
-
         Vector<Core::ThreadStatistics> threads;
 
         String username;
     }; // struct ProcessStatistics
 
+    struct AllProcessesStatistics {
+        Vector<ProcessStatistics> processes;
+        u64 total_time_scheduled;
+        u64 total_time_scheduled_kernel;
+    }; // struct AllProcessesStatistics
+
     class ProcessStatisticsReader 
     {
     public:
         /**
-         * @return HashMap<pid_t, Core::ProcessStatistics> 
+         * @brief Get the all object
+         * 
+         * @return Optional<AllProcessesStatistics> 
          */
-        static HashMap<pid_t, Core::ProcessStatistics> get_all();
+        static Optional<AllProcessesStatistics> get_all(RefPtr<Core::File>&);
+
+        /**
+         * @brief Get the all object
+         * 
+         * @return Optional<AllProcessesStatistics> 
+         */
+        static Optional<AllProcessesStatistics> get_all();
 
     private:
         /**
@@ -89,6 +96,6 @@ namespace Core
          */
         static String username_from_uid(uid_t);
         static HashMap<uid_t, String> s_usernames;
-    }; // class ProcessStatisticsReader
+    }; // class ProcessStatisticsReader 
 
 } // namespace Core
