56bc529bbac9794e3d9304df0c513cfa23a94369
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-02 12:17:48 +0530

    libraries: libelf[coredump]

--- libraries/libelf/coredump.h
+++ libraries/libelf/coredump.h
@@ -5,7 +5,7 @@
  * @version 0.1
  * @date 2023-08-06
  * 
- * @copyright Copyright (c) 2021-2024 pranaOS Developers, Krisna Pranav
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
  * 
  */
 
