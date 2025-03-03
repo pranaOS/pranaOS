dd4c13474319db7fc3e2d85ee7ae97516d58fd16
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-03 20:34:07 +0530

    libraries: libcore[standard]

---
+++ libraries/libcore/standardpaths.h
@@ -0,0 +1,11 @@
+/**
+ * @file standardpaths.h
+ * @author Krisna Pranav
+ * @brief standard paths
+ * @version 6.0
+ * @date 2025-03-03
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
