7f066481e1bd227a645d63ce169dd18d46a52cce
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-03 18:42:36 +0530

    libraries: libcore[sharedcircularqueue]

---
+++ libraries/libcore/sharedcircularqueue.h
@@ -0,0 +1,11 @@
+/**
+ * @file sharedcircularqueue.h
+ * @author Krisna Pranav
+ * @brief shared circular queue
+ * @version 6.0
+ * @date 2025-03-03
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
