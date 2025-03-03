a368f81d385f2f42d003a1fe17c187b796197b67
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-03 18:59:16 +0530

    libraries: libcore[socksproxyclient]

---
+++ libraries/libcore/socksproxyclient.h
@@ -0,0 +1,12 @@
+/**
+ * @file socksproxyclient.h
+ * @author Krisna Pranav
+ * @brief socks proxy client
+ * @version 6.0
+ * @date 2025-03-03
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
