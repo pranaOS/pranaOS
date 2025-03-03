77d9cc83b798601f2a612cce4c846a3f6e39be8f
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-03 19:01:36 +0530

    libraries: libcore[socksproxyclient]

--- libraries/libcore/socksproxyclient.h
+++ libraries/libcore/socksproxyclient.h
@@ -10,3 +10,28 @@
  */
 
 #pragma once
+
+#include <mods/ownptr.h>
+#include <libcore/proxy.h>
+#include <libcore/stream.h>
+
+namespace Core
+{
+
+    class SOCKSProxyClient final : public Stream::Socket 
+    {
+    public:
+
+    private:
+        SOCKSProxyClient(Socket& sock, OwnPtr<Socket> own_socket)
+            : m_socket(socket)
+            , m_own_underlying_socket(move(own_socket))
+        {
+            m_socket.on_ready_to_read = [this] { on_ready_to_read(); }
+        }
+
+        Socket& m_socket;
+        OwnPtr<Socket> m_own_underlying_socket;
+    }; // class SOCKSProxyClient final : public Stream::Socket 
+
+} // namespace Core
