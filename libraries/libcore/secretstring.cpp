5ede6d7f9a43ba2e343c90e68e69330f69a77183
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-03 18:35:26 +0530

    libraries: libcore[secretstring]

---
+++ libraries/libcore/secretstring.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file secretstring.cpp
+ * @author Krisna Pranav
+ * @brief secret string
+ * @version 6.0
+ * @date 2025-03-03
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
