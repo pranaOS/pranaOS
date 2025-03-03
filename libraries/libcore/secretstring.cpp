bd242c2d4f282cc682115b0cd28cd09f730fb044
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-03 18:38:12 +0530

    libraries: libcore[secretstring]

--- libraries/libcore/secretstring.cpp
+++ libraries/libcore/secretstring.cpp
@@ -12,7 +12,7 @@
 #include <mods/memory.h>
 #include <libcore/secretstring.h>
 
-namespace Core
+namespace Core 
 {
 
     /**
@@ -22,7 +22,7 @@ namespace Core
      */
     SecretString SecretString::take_ownership(char*& cstring, size_t length)
     {
-        auto buffer = ByteBuffer::copy(cstring, length);
+        auto buffer = ByteBuffer::copy(cstring, length).release_value_but_fixme_should_propagate_errors();
 
         secure_zero(cstring, length);
         free(cstring);
@@ -33,13 +33,37 @@ namespace Core
     }
 
     /**
+     * @param buffer 
+     * @return SecretString 
+     */
+    SecretString SecretString::take_ownership(ByteBuffer&& buffer)
+    {
+        return SecretString(move(buffer));
+    }
+
+    /**
+     * @brief Construct a new SecretString::SecretString object
+     * 
+     * @param buffer 
+     */
+    SecretString::SecretString(ByteBuffer&& buffer)
+        : m_secure_buffer(move(buffer))
+    {
+        if (m_secure_buffer.is_empty() || (m_secure_buffer[m_secure_buffer.size() - 1] != 0)) {
+            u8 nul = '\0';
+            m_secure_buffer.append(&nul, 1);
+        }
+    }
+
+    /**
      * @brief Destroy the SecretString::SecretString object
      * 
      */
     SecretString::~SecretString()
     {
         if (!m_secure_buffer.is_empty()) {
-            secure_zero(m_secure_buffer.data(), m_secure_buffer.bytes());
+            secure_zero(m_secure_buffer.data(), m_secure_buffer.capacity());
         }
     }
+
 } // namespace Core
