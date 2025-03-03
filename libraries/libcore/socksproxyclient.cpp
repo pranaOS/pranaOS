bb11a1a32f1597ba4223a9f1b1b27bd5be3f9594
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-03 19:09:23 +0530

    libraries: libcore[socksproxyclient]

---
+++ libraries/libcore/socksproxyclient.cpp
@@ -0,0 +1,48 @@
+/**
+ * @file socksproxyclient.cpp
+ * @author Krisna Pranav
+ * @brief socks proxy client
+ * @version 6.0
+ * @date 2025-03-03
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include <mods/memorystream.h>
+#include <libcore/socksproxyclient.h>
+
+enum class Method : u8 
+{
+
+}; // enum class Method : u8 
+
+enum class AddressType : u8 
+{
+
+}; // enum class AddressType : u8 
+
+enum class Reply 
+{
+
+}; // enum class Reply 
+
+struct [[gnu::packed]] Socks5VersionIdentifierAndMethodSelectionMessage
+{
+
+}; // struct [[gnu::packed]] Socks5VersionIdentifierAndMethodSelectionMessage
+
+struct [[gnu::packed]] Socks5InitialResponse 
+{
+
+}; // struct [[gnu::packed]] Socks5InitialResponse 
+
+struct [[gnu::packed]] Socks5ConnectRequestHeader 
+{
+
+}; // struct [[gnu::packed]] Socks5ConnectRequestHeader 
+
+struct [[gnu::packed]] Socks5ConnectRequestTrailer
+{
+    
+}; // struct [[gnu::packed]] Socks5ConnectRequestTrailer
