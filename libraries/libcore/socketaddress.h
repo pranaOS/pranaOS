873d086d2cef84197e5074c1e70b35ee1365d7f5
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-03 18:56:29 +0530

    libraries: libcore[socketaddress]

--- libraries/libcore/socketaddress.h
+++ libraries/libcore/socketaddress.h
@@ -9,7 +9,8 @@
  * 
  */
 
-#pragma once 
+
+#pragma once
 
 #include <mods/ipv4address.h>
 #include <arpa/inet.h>
@@ -20,29 +21,52 @@
 
 namespace Core 
 {
-    class SocketAddress {
+
+    class SocketAddress 
+    {
     public:
-        enum class Type {
+        enum class Type 
+        {
             Invalid,
             IPv4,
             Local
-        };
+        }; // enum class Type
 
-        SocketAddress() { }
-        SocketAddress(const IPv4Address& address)
+        /**
+         * @brief Construct a new SocketAddress object
+         * 
+         */
+        SocketAddress() = default;
+
+        /**
+         * @brief Construct a new SocketAddress object
+         * 
+         * @param address 
+         */
+        SocketAddress(IPv4Address const& address)
             : m_type(Type::IPv4)
             , m_ipv4_address(address)
         {
         }
 
-        SocketAddress(const IPv4Address& address, u16 port)
+        /**
+         * @brief Construct a new SocketAddress object
+         * 
+         * @param address 
+         * @param port 
+         */
+        SocketAddress(IPv4Address const& address, u16 port)
             : m_type(Type::IPv4)
             , m_ipv4_address(address)
             , m_port(port)
         {
         }
 
-        static SocketAddress local(const String& address)
+        /**
+         * @param address 
+         * @return SocketAddress 
+         */
+        static SocketAddress local(String const& address)
         {
             SocketAddress addr;
             addr.m_type = Type::Local;
@@ -90,22 +114,28 @@ namespace Core
         {
             switch (m_type) {
             case Type::IPv4:
-                return String::format("%s:%d", m_ipv4_address.to_string().characters(), m_port);
+                return String::formatted("{}:{}", m_ipv4_address, m_port);
             case Type::Local:
                 return m_local_address;
             default:
                 return "[SocketAddress]";
             }
-        }
+        }   
 
+        /**
+         * @return Optional<sockaddr_un> 
+         */
         Optional<sockaddr_un> to_sockaddr_un() const
         {
-            ASSERT(type() == Type::Local);
+            VERIFY(type() == Type::Local);
             sockaddr_un address;
             address.sun_family = AF_LOCAL;
+
             bool fits = m_local_address.copy_characters_to_buffer(address.sun_path, sizeof(address.sun_path));
+
             if (!fits)
                 return {};
+
             return address;
         }
 
@@ -114,11 +144,13 @@ namespace Core
          */
         sockaddr_in to_sockaddr_in() const
         {
-            ASSERT(type() == Type::IPv4);
+            VERIFY(type() == Type::IPv4);
             sockaddr_in address {};
+
             address.sin_family = AF_INET;
             address.sin_addr.s_addr = m_ipv4_address.to_in_addr_t();
             address.sin_port = htons(m_port);
+
             return address;
         }
 
@@ -127,10 +159,22 @@ namespace Core
         IPv4Address m_ipv4_address;
         u16 m_port { 0 };
         String m_local_address;
-    }; // class SocketAddress 
+    }; // class SocketADdress
 
-    /**
-     * @return const LogStream& 
-     */
-    const LogStream& operator<<(const LogStream&, const SocketAddress&);
 } // namespace Core
+
+/**
+ * @tparam  
+ */
+template<>
+struct Mods::Formatter<Core::SocketAddress> : Formatter<String> {
+    /**
+     * @param builder 
+     * @param value 
+     * @return ErrorOr<void> 
+     */
+    ErrorOr<void> format(FormatBuilder& builder, Core::SocketAddress const& value)
+    {
+        return Formatter<String>::format(builder, value.to_string());
+    }
+};
