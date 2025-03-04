49ee6ffff454a8f72e41639aa5e2ef72a57ccda9
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-04 22:49:20 +0530

    libraries: libcore[stream]

--- libraries/libcore/stream.cpp
+++ libraries/libcore/stream.cpp
@@ -29,5 +29,68 @@
 namespace Core::Stream
 {
 
+    /**
+     * @param buffer 
+     * @return true 
+     * @return false 
+     */
+    bool Stream::read_or_error(Bytes buffer)
+    {
+        VERIFY(buffer.size());
 
+        size_t nread = 0;
+        do {
+            if (is_eof())
+                return false;
+
+            auto result = read(buffer.slice(nread));
+
+            if (result.is_error()) {
+                if (result.error().is_errno() && result.error().code() == EINTR) {
+                    continue;
+                }
+
+                return false;
+            }
+
+            nread += result.value().size();
+        } while (nread < buffer.size());
+
+        return true;
+    }
+
+    /**
+     * @param buffer 
+     * @return true 
+     * @return false 
+     */
+    bool Stream::write_or_error(ReadonlyBytes buffer)
+    {
+        VERIFY(buffer.size());
+
+        size_t nwritten = 0;
+
+        do {
+            auto result = write(buffer.slice(nwritten));
+            if (result.is_error()) {
+                if (result.error().is_errno() && result.error().code() == EINTR) {
+                    continue;
+                }
+
+                return false;
+            }
+
+            nwritten += result.value();
+        } while (nwritten < buffer.size());
+
+        return true;
+    }
+
+    /**
+     * @return ErrorOr<off_t> 
+     */
+    ErrorOr<off_t> SeekableStream::tell() const
+    {
+        return const_cast<SeekableStream*>(this)->seek(0);
+    }
 } // namespace Core::Stream
