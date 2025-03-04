213828c83200554b693f3a9a1e07e6f5d987860e
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-04 18:27:34 +0530

    libraries: libcore[stream]

--- libraries/libcore/stream.h
+++ libraries/libcore/stream.h
@@ -10,3 +10,85 @@
  */
 
 #pragma once
+
+#include <mods/enumbits.h>
+#include <mods/function.h>
+#include <mods/ipv4address.h>
+#include <mods/memmem.h>
+#include <mods/noncopyable.h>
+#include <mods/result.h>
+#include <mods/span.h>
+#include <mods/string.h>
+#include <mods/time.h>
+#include <mods/variant.h>
+#include <libcore/notifier.h>
+#include <libcore/socketaddress.h>
+#include <errno.h>
+#include <netdb.h>
+
+namespace Core::Stream
+{
+
+    class Stream
+    {
+    public:
+        virtual bool is_readable() const 
+        {
+            return false;
+        }
+
+        virtual ErrorOr<Bytes> read(Bytes) = 0;
+
+        virtual bool read_or_error(Bytes);
+
+        virtual ErrorOr<size_t> write(ReadonlyBytes) = 0;
+
+        virtual bool is_eof() const = 0;
+
+        virtual bool is_open() const = 0;
+
+        virtual void close() = 0;
+
+        virtual ~Stream() {}
+    protected:
+        enum class SocketDomain 
+        {
+            Local,
+            Inet,
+        };
+
+        enum class SocketType
+        {
+            Stream,
+            Datagram,
+        }; // enum class SocketType
+
+        Socket() {}
+
+        /**
+         * @brief Create a fd object
+         * 
+         * @return ErrorOr<int> 
+         */
+        static ErrorOr<int> create_fd(SocketDomain, SocketType);
+
+        /**
+         * @return ErrorOr<IPv4Address> 
+         */
+        static ErrorOr<IPv4Address> resolve_host(String const&, SocketType);
+
+        /**
+         * @param fd 
+         * @param path 
+         * @return ErrorOr<void> 
+         */
+        static ErrorOr<void> connect_local(int fd, String const& path);
+
+        /**
+         * @param fd 
+         * @return ErrorOr<void> 
+         */
+        static ErrorOr<void> current_inet(int fd, SocketAddress const&);
+    }; // class Stream
+
+} // namespace Core::Stream
