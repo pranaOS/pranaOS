341e906e103d45cd46ca87259099380860c745e4
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-05 12:52:30 +0530

    libraries: libcpp[token]

---
+++ libraries/libcpp/token.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file token.cpp
+ * @author Krisna Pranav
+ * @brief token
+ * @version 6.0
+ * @date 2025-03-05
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
