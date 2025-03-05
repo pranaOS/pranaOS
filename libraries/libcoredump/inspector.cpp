2a3055a6cd06d21393acc0d8c1fdb9dc7a47eb5d
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-05 12:16:14 +0530

    libraries: libcoredump[inspector]

---
+++ libraries/libcoredump/inspector.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file inspector.cpp
+ * @author Krisna Pranav
+ * @brief inspector
+ * @version 6.0
+ * @date 2025-03-05
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include "inspector.h"
