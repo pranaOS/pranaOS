bf3896a99ba9948ad6f94df0eed3cffd5ac475b9
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-05 08:28:46 +0530

    libraries: libcore[system]

---
+++ libraries/libcore/system.h
@@ -0,0 +1,11 @@
+/**
+ * @file system.h
+ * @author Krisna Pranav
+ * @brief system
+ * @version 6.0
+ * @date 2025-03-05
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
