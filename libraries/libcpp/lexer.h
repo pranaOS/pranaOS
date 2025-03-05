7f47d0941bed744b3baa89da664482100b7d008f
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-05 15:15:09 +0530

    libraries: libcpp[lexer]

---
+++ libraries/libcpp/lexer.h
@@ -0,0 +1,10 @@
+/**
+ * @file lexer.h
+ * @author Krisna Pranav
+ * @brief lexer
+ * @version 6.0
+ * @date 2025-03-05
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
