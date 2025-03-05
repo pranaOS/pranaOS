9e344f6a41d19898ab807d7d3ab9958dd27c8b99
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-05 14:55:27 +0530

    libraries: libcpp[semanticsyntaxhighlighter]

--- libraries/libcpp/semanticsyntaxhighlighter.h
+++ libraries/libcpp/semanticsyntaxhighlighter.h
@@ -16,7 +16,29 @@ namespace Cpp
 
     class SemanticSyntaxHighlighter final : public Syntax::Highlighter 
     {
-        
+    protected:
+        /**
+         * @return Vector<MatchingTokenPair> 
+         */
+        virtual Vector<MatchingTokenPair> matching_token_pairs_impl() const override { return m_simple_syntax_highlighter.matching_token_pairs_impl(); };
+
+        /**
+         * @param token1 
+         * @param token2 
+         * @return true 
+         * @return false 
+         */
+        virtual bool token_types_equal(u64 token1, u64 token2) const override { return m_simple_syntax_highlighter.token_types_equal(token1, token2); };
+
+    private:
+        void update_spans(Vector<GUI::AutocompleteProvider::TokenInfo> const&, Gfx::Palette const&);
+
+        Cpp::SyntaxHighlighter m_simple_syntax_highlighter;
+        Vector<GUI::AutocompleteProvider::TokenInfo> m_tokens_info;
+        String m_saved_tokens_text;
+        Vector<Token> m_saved_tokens;
+        Threading::Mutex m_lock;
+
     }; // class SemanticSyntaxHighlighter final : public Syntax::Highlighter 
 
 } // namespace Cpp
