782c6d66a11127df599d3b6773cc2fd8ad615421
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-05 08:30:41 +0530

    libraries: libcore[system]

--- libraries/libcore/system.h
+++ libraries/libcore/system.h
@@ -36,5 +36,94 @@
 namespace Core::System
 {
 
+    #ifdef __pranaos__
+    /**
+     * @return ErrorOr<void> 
+     */
+    ErrorOr<void> beep();
+
+    /**
+     * @param promises 
+     * @param execpromises 
+     * @return ErrorOr<void> 
+     */
+    ErrorOr<void> pledge(StringView promises, StringView execpromises = {});
+
+    /**
+     * @param path 
+     * @param permissions 
+     * @return ErrorOr<void> 
+     */
+    ErrorOr<void> unveil(StringView path, StringView permissions);
+
+    /**
+     * @param sockfd 
+     * @param fd 
+     * @return ErrorOr<void> 
+     */
+    ErrorOr<void> sendfd(int sockfd, int fd);
+
+    /**
+     * @param sockfd 
+     * @param options 
+     * @return ErrorOr<int> 
+     */
+    ErrorOr<int> recvfd(int sockfd, int options);
+
+    /**
+     * @param tid 
+     * @param tracee_addr 
+     * @param destination_buf 
+     * @return ErrorOr<void> 
+     */
+    ErrorOr<void> ptrace_peekbuf(pid_t tid, void const* tracee_addr, Bytes destination_buf);
+
+    /**
+     * @return ErrorOr<void> 
+     */
+    ErrorOr<void> setgroups(Span<gid_t const>);
+
+    /**
+     * @param source_fd 
+     * @param target 
+     * @param fs_type 
+     * @param flags 
+     * @return ErrorOr<void> 
+     */
+    ErrorOr<void> mount(int source_fd, StringView target, StringView fs_type, int flags);
+
+    /**
+     * @param mount_point 
+     * @return ErrorOr<void> 
+     */
+    ErrorOr<void> umount(StringView mount_point);
+
+    /**
+     * @param request 
+     * @param tid 
+     * @param address 
+     * @param data 
+     * @return ErrorOr<long> 
+     */
+    ErrorOr<long> ptrace(int request, pid_t tid, void* address, void* data);
+
+    /**
+     * @param pid 
+     * @return ErrorOr<void> 
+     */
+    ErrorOr<void> disown(pid_t pid);
+
+    /**
+     * @param event_mask 
+     * @return ErrorOr<void> 
+     */
+    ErrorOr<void> profiling_enable(pid_t, u64 event_mask);
+
+    /**
+     * @return ErrorOr<void> 
+     */
+    ErrorOr<void> profiling_disable(pid_t);
+    ErrorOr<void> profiling_free_buffer(pid_t);
+    #endif
 
 } // namespace Core::System
