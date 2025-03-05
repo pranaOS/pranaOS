886769b3fe360caa202b3b42db34e47a09616d05
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-05 11:54:48 +0530

    libraries: libcore[umaskscope]

--- libraries/libcore/umaskscope.h
+++ libraries/libcore/umaskscope.h
@@ -9,3 +9,38 @@
  * 
  */
 
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#pragma once
+
+namespace Core 
+{
+
+    class UmaskScope 
+    {
+    public:
+        /**
+         * @brief Construct a new UmaskScope object
+         * 
+         * @param mask 
+         */
+        explicit UmaskScope(mode_t mask)
+        {
+            m_old_mask = umask(mask);
+        }
+
+        /**
+         * @brief Destroy the UmaskScope object
+         * 
+         */
+        ~UmaskScope()
+        {
+            umask(m_old_mask);
+        }
+
+    private:
+        mode_t m_old_mask {};
+    }; // class UmaskScope
+
+} // namespace Core
