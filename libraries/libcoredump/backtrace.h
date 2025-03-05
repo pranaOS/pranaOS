cc36f24bfd3aac5b81ffdc1fa7d385654ec8d302
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-05 12:23:14 +0530

    libraries: libcoredump[backtrace]

--- libraries/libcoredump/backtrace.h
+++ libraries/libcoredump/backtrace.h
@@ -9,3 +9,25 @@
  * 
  */
 
+#include <mods/types.h>
+#include <libcoredump/reader.h>
+#include <libdebug/debuginfo.h>
+#include <libelf/core.h>
+
+namespace Coredump
+{
+
+    class Backtrace
+    {
+    private:
+        void add_entry(Reader const&, FlatPtr ip);
+
+        ELFObjectInfo const* object_info_for_region(Reader const&);
+
+        bool m_skip_loader_so { false };
+        ELF::Core::ThreadInfo m_thread_info;
+        Vector<Entry> m_entries;
+        HashMap<String, NonnullOwnPtr<ELFObjectInfo>> m_debug_info_cache;
+    }; // class Backtrace
+
+} // namespace Coredump
