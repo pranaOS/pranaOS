f4a1100454c6da40d586dae1156f141258386c37
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-05 11:33:44 +0530

    libraries: libcore[tempfile]

---
+++ libraries/libcore/tempfile.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file tempfile.cpp
+ * @author Krisna Pranav
+ * @brief tempfile
+ * @version 6.0
+ * @date 2025-03-05
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
