e252fe64a8ada27210937c86ba04a6d3b4a9ef50
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-05 17:27:36 +0530

    libraries: libcpp[semanticsyntaxhighlighter]

---
+++ libraries/libcpp/semanticsyntaxhighlighter.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file semanticsyntaxhighlighter.cpp
+ * @author Krisna Pranav
+ * @brief semantic syntax highlighter
+ * @version 6.0
+ * @date 2025-03-05
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include "semanticsyntaxhighlighter.h"
+#include "lexer.h"
+#include <libdiff/generator.h>
+#include <libgui/autocompleteprovider.h>
+#include <libgfx/palette.h>
+
+namespace Cpp
+{
+
+    
+} // namespace Cpp
