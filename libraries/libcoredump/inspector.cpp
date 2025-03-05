3142f40408838a6b95a6b9528e640525dc843dec
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-05 12:17:42 +0530

    libraries: libcoredump[inspector]

--- libraries/libcoredump/inspector.cpp
+++ libraries/libcoredump/inspector.cpp
@@ -10,3 +10,46 @@
  */
 
 #include "inspector.h"
+
+namespace Coredump
+{
+
+    /**
+     * @param coredump_path 
+     * @return OwnPtr<Inspector> 
+     */
+    OwnPtr<Inspector> Inspector::create(String const& coredump_path)
+    {
+        auto reader = Reader::create(coredump_path);
+
+        if (!reader) {
+            return {};
+        }
+
+    }
+
+    /**
+     * @brief Construct a new Inspector:: Inspector object
+     * 
+     * @param reader 
+     */
+    Inspector::Inspector(NonnullOwnPtr<Reader>&& reader)
+        : m_reader(move(reader))
+    {
+        parse_loaded_libraries(move(on_progress));
+    }
+
+    /**
+     * @return size_t 
+     */
+    size_t Inspector::number_of_libraries_in_coredump() const
+    {
+        size_t count = 0;
+
+        m_reader->for_each_library([&count] {
+            ++count;
+        });
+
+        return count;
+    }
+} // namespace Coredump
