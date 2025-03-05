1514bec6e6825e61126bc130c9796de28e81e5cd
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-05 11:32:13 +0530

    libraries: libcore[tempfile]

--- libraries/libcore/tempfile.h
+++ libraries/libcore/tempfile.h
@@ -14,37 +14,56 @@
 #include <mods/noncopyable.h>
 #include <mods/string.h>
 
-namespace Core
+namespace Core 
 {
 
-    class TempFile
+    class TempFile 
     {
         MOD_MAKE_NONCOPYABLE(TempFile);
         MOD_MAKE_NONMOVABLE(TempFile);
 
     public:
-        enum class Type
-        {
-            FILE,
-            DIRECTORY
+        enum class Type {
+            File,
+            Directory
         }; // enum class Type
+
+        /**
+         * @return NonnullOwnPtr<TempFile> 
+         */
+        static NonnullOwnPtr<TempFile> create(Type = Type::File);
+
+        /**
+         * @brief Destroy the TempFile object
+         * 
+         */
+        ~TempFile();
 
         /**
          * @return String 
          */
         String path() const 
-        {
-            return m_path;
+        { 
+            return m_path; 
         }
 
     private:
-        TempFile(Type);
 
+        /**
+         * @brief Construct a new TempFile object
+         * 
+         */
+        TempFile(Type);
+        
+        /**
+         * @brief Create a temp object
+         * 
+         * @return String 
+         */
         static String create_temp(Type);
 
         Type m_type { Type::File };
-
         String m_path;
     }; // class TempFile
-    
+
 } // namespace Core
