f128be915be342792e8d895787f1c712e4d8d2ad
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-05 11:30:38 +0530

    libraries: libcore[tempfile]

---
+++ libraries/libcore/tempfile.h
@@ -0,0 +1,35 @@
+/**
+ * @file tempfile.h
+ * @author Krisna Pranav
+ * @brief temp file
+ * @version 6.0
+ * @date 2025-03-05
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#include <mods/noncopyable.h>
+#include <mods/string.h>
+
+namespace Core
+{
+
+    class TempFile
+    {
+        MOD_MAKE_NONCOPYABLE(TempFile);
+        MOD_MAKE_NONMOVABLE(TempFile);
+
+    private:
+        TempFile(Type);
+
+        static String create_temp(Type);
+
+        Type m_type { Type::File };
+
+        String m_path;
+    }; // class TempFile
+    
+} // namespace Core
