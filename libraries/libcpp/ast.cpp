898aeff1d1ed4c3f80e6c79d71133a2ede238a28
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-05 16:26:52 +0530

    libraries: libcpp[ast]

---
+++ libraries/libcpp/ast.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file ast.cpp
+ * @author Krisna Pranav
+ * @brief ast
+ * @version 6.0
+ * @date 2025-03-05
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
