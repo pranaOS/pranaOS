11c90da0ea2e39fd6ca13e5db3cc3f8edafd698d
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-05 11:17:29 +0530

    libraries: libcore[tcpserver]

--- libraries/libcore/tcpserver.h
+++ libraries/libcore/tcpserver.h
@@ -16,12 +16,61 @@
 #include <libcore/object.h>
 #include <libcore/stream.h>
 
-namespace Core
+namespace Core 
 {
 
-    class TCPServer : public Object
+    class TCPServer : public Object 
     {
-        C_OBJECT_ABSTRACT(TcpServer)
+        C_OBJECT_ABSTRACT(TCPServer)
+    public:
+        /**
+         * @param parent 
+         * @return ErrorOr<NonnullRefPtr<TCPServer>> 
+         */
+        static ErrorOr<NonnullRefPtr<TCPServer>> try_create(Object* parent = nullptr);
+
+        /**
+         * @brief Destroy the TCPServer object
+         * 
+         */
+        virtual ~TCPServer() override;
+
+        /**
+         * @return true 
+         * @return false 
+         */
+        bool is_listening() const 
+        { 
+            return m_listening; 
+        }
+
+        /**
+         * @param address 
+         * @param port 
+         * @return ErrorOr<void> 
+         */
+        ErrorOr<void> listen(IPv4Address const& address, u16 port);
+
+        /**
+         * @brief Set the blocking object
+         * 
+         * @param blocking 
+         * @return ErrorOr<void> 
+         */
+        ErrorOr<void> set_blocking(bool blocking);
+
+        /**
+         * @return ErrorOr<NonnullOwnPtr<Stream::TCPSocket>> 
+         */
+        ErrorOr<NonnullOwnPtr<Stream::TCPSocket>> accept();
+
+        /**
+         * @return Optional<IPv4Address> 
+         */
+        Optional<IPv4Address> local_address() const;
+        Optional<u16> local_port() const;
+
+        Function<void()> on_ready_to_accept;
 
     private:
         /**
@@ -34,7 +83,6 @@ namespace Core
 
         int m_fd { -1 };
         bool m_listening { false };
-
         RefPtr<Notifier> m_notifier;
     }; // class TCPServer : public Object
 
