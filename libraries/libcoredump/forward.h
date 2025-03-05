e4560da528999ec899716dd3444faf86fbc48dc9
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-05 12:19:44 +0530

    libraries: libcoredump[forward]

---
+++ libraries/libcoredump/forward.h
@@ -0,0 +1,11 @@
+/**
+ * @file forward.h
+ * @author Krisna Pranav
+ * @brief forward
+ * @version 6.0
+ * @date 2025-03-05
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
