e1d250d31cb517c62375b7a803321458d774e7d0
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-05 15:18:22 +0530

    libraries: libcpp[lexer]

--- libraries/libcpp/lexer.h
+++ libraries/libcpp/lexer.h
@@ -15,28 +15,56 @@
 #include <mods/stringview.h>
 #include <mods/vector.h>
 
-namespace Cpp
+namespace Cpp 
 {
 
-    class Lexer
+    class Lexer 
     {
+    public:
+        /**
+         * @brief Construct a new Lexer object
+         * 
+         * @param start_line 
+         */
+        explicit Lexer(StringView, size_t start_line = 0);
+
+        /**
+         * @return Vector<Token> 
+         */
+        Vector<Token> lex();
+        template<typename Callback>
+        void lex_iterable(Callback);
+
+        /**
+         * @brief Set the ignore whitespace object
+         * 
+         * @param value 
+         */
+        void set_ignore_whitespace(bool value) 
+        { 
+            m_options.ignore_whitespace = value; 
+        }
+
     private:
+        /**
+         * @param offset 
+         * @return char 
+         */
         char peek(size_t offset = 0) const;
+
         char consume();
         
         void lex_impl(Function<void(Token)>);
 
         StringView m_input;
         size_t m_index { 0 };
-
         Position m_previous_position { 0, 0 };
         Position m_position { 0, 0 };
 
         struct Options {
             bool ignore_whitespace { false };
         } m_options;
-
-    }; // class Lexer
+    }; // class Lexer 
 
     /**
      * @tparam Callback 
