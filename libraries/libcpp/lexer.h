0d6eb1b6f65f2c571506cacd9882da9e4265e3f1
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-05 15:16:41 +0530

    libraries: libcpp[lexer]

--- libraries/libcpp/lexer.h
+++ libraries/libcpp/lexer.h
@@ -8,3 +8,34 @@
  * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
  * 
  */
+
+#pragma once
+
+#include "libcpp/token.h"
+#include <mods/stringview.h>
+#include <mods/vector.h>
+
+namespace Cpp
+{
+
+    class Lexer
+    {
+    private:
+        char peek(size_t offset = 0) const;
+        char consume();
+        
+        void lex_impl(Function<void(Token)>);
+
+        StringView m_input;
+        size_t m_index { 0 };
+
+        Position m_previous_position { 0, 0 };
+        Position m_position { 0, 0 };
+
+        struct Options {
+            bool ignore_whitespace { false };
+        } m_options;
+
+    }; // class Lexer
+
+} // namespace Cpp
