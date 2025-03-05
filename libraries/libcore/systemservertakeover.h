8aee1f7ee49d53c66da6cc74e2b5d3ec73534c73
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-05 11:11:16 +0530

    libraries: libcore[systemservertakeover]

---
+++ libraries/libcore/systemservertakeover.h
@@ -0,0 +1,11 @@
+/**
+ * @file systemservertakeover.h
+ * @author Krisna Pranav
+ * @brief system server takeover
+ * @version 6.0
+ * @date 2025-03-05
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
