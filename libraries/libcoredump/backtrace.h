6b71c7f1f06412d2f659180c53cefad6b21013f6
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-05 12:21:38 +0530

    libraries: libcoredump[backtrace]

---
+++ libraries/libcoredump/backtrace.h
@@ -0,0 +1,11 @@
+/**
+ * @file backtrace.h
+ * @author Krisna Pranav
+ * @brief backtrace
+ * @version 6.0
+ * @date 2025-03-05
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
