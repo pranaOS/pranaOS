69ee14760289f1d9d0eaf79e26f4d23211f6cfd8
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-05 11:20:10 +0530

    libraries: libcore[tcpserver]

---
+++ libraries/libcore/tcpserver.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file tcpserver.cpp
+ * @author Krisna Pranav
+ * @brief tcp server
+ * @version 6.0
+ * @date 2025-03-05
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
