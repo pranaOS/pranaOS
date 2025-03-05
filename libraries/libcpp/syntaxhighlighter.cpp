cac02960a82260efb3f080216bb75c09c6e43781
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-05 17:33:11 +0530

    libraries: libcpp[syntaxhighlighter]

--- libraries/libcpp/syntaxhighlighter.cpp
+++ libraries/libcpp/syntaxhighlighter.cpp
@@ -16,8 +16,43 @@
 #include <libgfx/font/font.h>
 #include <libgfx/palette.h>
 
-namespace Cpp
+namespace Cpp 
 {
+
+    /**
+     * @param palette 
+     * @param type 
+     * @return Syntax::TextStyle 
+     */
+    static Syntax::TextStyle style_for_token_type(Gfx::Palette const& palette, Cpp::Token::Type type)
+    {
+        switch (type) {
+        case Cpp::Token::Type::Keyword:
+            return { palette.syntax_keyword(), true };
+        case Cpp::Token::Type::KnownType:
+            return { palette.syntax_type(), true };
+        case Cpp::Token::Type::Identifier:
+            return { palette.syntax_identifier(), false };
+        case Cpp::Token::Type::DoubleQuotedString:
+        case Cpp::Token::Type::SingleQuotedString:
+        case Cpp::Token::Type::RawString:
+            return { palette.syntax_string(), false };
+        case Cpp::Token::Type::Integer:
+        case Cpp::Token::Type::Float:
+            return { palette.syntax_number(), false };
+        case Cpp::Token::Type::IncludePath:
+            return { palette.syntax_preprocessor_value(), false };
+        case Cpp::Token::Type::EscapeSequence:
+            return { palette.syntax_keyword(), true };
+        case Cpp::Token::Type::PreprocessorStatement:
+        case Cpp::Token::Type::IncludeStatement:
+            return { palette.syntax_preprocessor_statement(), false };
+        case Cpp::Token::Type::Comment:
+            return { palette.syntax_comment(), false };
+        default:
+            return { palette.base_text(), false };
+        }
+    }
 
     /**
      * @param token 
@@ -29,7 +64,7 @@ namespace Cpp
         auto cpp_token = static_cast<Cpp::Token::Type>(token);
         return cpp_token == Cpp::Token::Type::Identifier;
     }
-
+    
     /**
      * @param token 
      * @return true 
@@ -68,6 +103,33 @@ namespace Cpp
         highlight_matching_token_pair();
 
         m_client->do_update();
+    }
+
+    /**
+     * @return Vector<SyntaxHighlighter::MatchingTokenPair> 
+     */
+    Vector<SyntaxHighlighter::MatchingTokenPair> SyntaxHighlighter::matching_token_pairs_impl() const
+    {
+        static Vector<SyntaxHighlighter::MatchingTokenPair> pairs;
+
+        if (pairs.is_empty()) {
+            pairs.append({ static_cast<u64>(Cpp::Token::Type::LeftCurly), static_cast<u64>(Cpp::Token::Type::RightCurly) });
+            pairs.append({ static_cast<u64>(Cpp::Token::Type::LeftParen), static_cast<u64>(Cpp::Token::Type::RightParen) });
+            pairs.append({ static_cast<u64>(Cpp::Token::Type::LeftBracket), static_cast<u64>(Cpp::Token::Type::RightBracket) });
+        }
+
+        return pairs;
+    }
+
+    /**
+     * @param token1 
+     * @param token2 
+     * @return true 
+     * @return false 
+     */
+    bool SyntaxHighlighter::token_types_equal(u64 token1, u64 token2) const
+    {
+        return static_cast<Cpp::Token::Type>(token1) == static_cast<Cpp::Token::Type>(token2);
     }
 
 } // namespace Cpp
