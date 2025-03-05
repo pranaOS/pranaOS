60e28662e93defa6304eac4b1b662926a27853b0
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-05 12:52:15 +0530

    libraries: libcpp[preprocessor]

--- libraries/libcpp/preprocessor.h
+++ libraries/libcpp/preprocessor.h
@@ -20,19 +20,115 @@
 #include <mods/vector.h>
 #include <libcpp/token.h>
 
-namespace Cpp
+namespace Cpp 
 {
 
     class Preprocessor 
     {
+    public:
+        /**
+         * @brief Construct a new Preprocessor object
+         * 
+         * @param filename 
+         * @param program 
+         */
+        explicit Preprocessor(String const& filename, StringView program);
+
+        /**
+         * @return Vector<Token> 
+         */
+        Vector<Token> process_and_lex();
+
+        /**
+         * @return Vector<StringView> 
+         */
+        Vector<StringView> included_paths() const 
+        { 
+            return m_included_paths; 
+        }
+
+        struct Definition {
+            String key;
+            Vector<String> parameters;
+            String value;
+            FlyString filename;
+            size_t line { 0 };
+            size_t column { 0 };
+        }; // struct Definition
+
+        using Definitions = HashMap<String, Definition>;
+
+        struct Substitution {
+            Vector<Token> original_tokens;
+            Definition defined_value;
+            String processed_value;
+        }; // struct Substitution
+
+        /**
+         * @return Definitions const& 
+         */
+        Definitions const& definitions() const 
+        { 
+            return m_definitions; 
+        }
+
+        /**
+         * @return Vector<Substitution> const& 
+         */
+        Vector<Substitution> const& substitutions() const 
+        { 
+            return m_substitutions; 
+        }
+        
+        /**
+         * @brief Set the ignore unsupported keywords object
+         * 
+         * @param ignore 
+         */
+        void set_ignore_unsupported_keywords(bool ignore) 
+        { 
+            m_options.ignore_unsupported_keywords = ignore; 
+        }
+
+        /**
+         * @brief Set the ignore invalid statements object
+         * 
+         * @param ignore 
+         */
+        void set_ignore_invalid_statements(bool ignore) 
+        { 
+            m_options.ignore_invalid_statements = ignore; 
+        }
+
+        /**
+         * @brief Set the keep include statements object
+         * 
+         * @param keep 
+         */
+        void set_keep_include_statements(bool keep) 
+        { 
+            m_options.keep_include_statements = keep; 
+        }
+
+        Function<Definitions(StringView)> definitions_in_header_callback { nullptr };
+
+        /**
+         * @return Vector<Token> const& 
+         */
+        Vector<Token> const& unprocessed_tokens() const 
+        { 
+            return m_unprocessed_tokens; 
+        }
+
     private:
         void handle_preprocessor_statement(StringView);
         void handle_include_statement(StringView);
 
         /**
          * @param keyword 
+         * @param line_lexer 
          */
-        void handle_preprocessor_keyword(StringView keyword);
+        void handle_preprocessor_keyword(StringView keyword, GenericLexer& line_lexer);
         
         /**
          * @param value 
@@ -40,9 +136,24 @@ namespace Cpp
          */
         String remove_escaped_newlines(StringView value);
 
+        /**
+         * @param tokens 
+         * @param token_index 
+         * @return size_t 
+         */
+        size_t do_substitution(Vector<Token> const& tokens, size_t token_index, Definition const&);
+
+        /**
+         * @brief Create a definition object
+         * 
+         * @param line 
+         * @return Optional<Definition> 
+         */
+        Optional<Definition> create_definition(StringView line);
+
         struct MacroCall {
             Token name;
-
+            
             struct Argument {
                 Vector<Token> tokens;
             }; // struct Argument
@@ -51,8 +162,32 @@ namespace Cpp
             size_t end_token_index { 0 };
         }; // struct MacroCall
 
-        enum class State
-        {
+        /**
+         * @param tokens 
+         * @param token_index 
+         * @return Optional<MacroCall> 
+         */
+        Optional<MacroCall> parse_macro_call(Vector<Token> const& tokens, size_t token_index);
+
+        /**
+         * @return String 
+         */
+        String evaluate_macro_call(MacroCall const&, Definition const&);
+
+        String m_filename;
+        String m_program;
+
+        Vector<Token> m_unprocessed_tokens;
+        Vector<Token> m_processed_tokens;
+        Definitions m_definitions;
+        Vector<Substitution> m_substitutions;
+
+        size_t m_current_line { 0 };
+        size_t m_current_depth { 0 };
+        Vector<size_t> m_depths_of_taken_branches;
+        Vector<size_t> m_depths_of_not_taken_branches;
+
+        enum class State {
             Normal,
             SkipIfBranch,
             SkipElseBranch
@@ -65,9 +200,8 @@ namespace Cpp
         struct Options {
             bool ignore_unsupported_keywords { false };
             bool ignore_invalid_statements { false };
-            bool keep_invalid_statements { false };
-        }; // struct Options 
-        
+            bool keep_include_statements { false };
+        } m_options;
     }; // class Preprocessor 
 
 } // namespace Cpp
