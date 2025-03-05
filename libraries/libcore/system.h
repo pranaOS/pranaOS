77a818395e510cb3a3112afbb910c22519e12d3f
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-05 08:29:47 +0530

    libraries: libcore[system]

--- libraries/libcore/system.h
+++ libraries/libcore/system.h
@@ -9,3 +9,32 @@
  * 
  */
 
+#pragma once
+
+#include <mods/error.h>
+#include <mods/stringview.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <grp.h>
+#include <pwd.h>
+#include <signal.h>
+#include <spawn.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/utsname.h>
+#include <sys/wait.h>
+#include <termios.h>
+#include <time.h>
+#include <utime.h>
+
+#ifndef MODS_OS_BSD_GENERIC
+#    include <shadow.h>
+#endif
+
+namespace Core::System
+{
+
+
+} // namespace Core::System
