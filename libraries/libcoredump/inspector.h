3954a9d5a1b76c1a2aa924ce2eb0a8ebe8173574
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-05 12:14:51 +0530

    libraries: libcoredump[inspector]

--- libraries/libcoredump/inspector.h
+++ libraries/libcoredump/inspector.h
@@ -9,21 +9,79 @@
  * 
  */
 
-
 #pragma once
 
 #include "reader.h"
 #include <mods/noncopyable.h>
-#include <libdebug/processinspector>
+#include <libdebug/processinspector.h>
 
-namespace Coredump
+namespace Coredump 
 {
 
-    class Inspector : public Debug::ProcessInspector
+    class Inspector : public Debug::ProcessInspector 
     {
+        MOD_MAKE_NONCOPYABLE(Inspector);
+        MOD_MAKE_NONMOVABLE(Inspector);
+
+    public:
+        /**
+         * @param coredump_path 
+         * @param on_progress 
+         * @return OwnPtr<Inspector> 
+         */
+        static OwnPtr<Inspector> create(String const& coredump_path, Function<void(float)> on_progress = {});
+
+        /**
+         * @brief Destroy the Inspector object
+         * 
+         */
+        virtual ~Inspector() override = default;
+
+        /**
+         * @param address 
+         * @param data 
+         * @return true 
+         * @return false 
+         */
+        virtual bool poke(FlatPtr address, FlatPtr data) override;
+
+        /**
+         * @param address 
+         * @return Optional<FlatPtr> 
+         */
+        virtual Optional<FlatPtr> peek(FlatPtr address) const override;
+
+        /**
+         * @brief Get the registers object
+         * 
+         * @return PtraceRegisters 
+         */
+        virtual PtraceRegisters get_registers() const override;
+
+        /**
+         * @brief Set the registers object
+         * 
+         */
+        virtual void set_registers(PtraceRegisters const&) override;
+
+        virtual void for_each_loaded_library(Function<IterationDecision(Debug::LoadedLibrary const&)>) const override;
+
     private:
+        /**
+         * @brief Construct a new Inspector object
+         * 
+         * @param on_progress 
+         */
+        Inspector(NonnullOwnPtr<Reader>&&, Function<void(float)> on_progress);
+
+        /**
+         * @param on_progress 
+         */
         void parse_loaded_libraries(Function<void(float)> on_progress);
 
+        /**
+         * @return size_t 
+         */
         size_t number_of_libraries_in_coredump() const;
 
         NonnullOwnPtr<Reader> m_reader;
