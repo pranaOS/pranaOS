9b854fa35da1bfcb6206445d506e6202b155cdf9
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-05 11:55:21 +0530

    libraries: libcore[version]

---
+++ libraries/libcore/version.h
@@ -0,0 +1,11 @@
+/**
+ * @file version.h
+ * @author Krisna Pranav
+ * @brief version
+ * @version 6.0
+ * @date 2025-03-05
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
