5d0822bfd82f74b92552ace4aa376deb2f6dd8bf
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-05 12:54:44 +0530

    libraries: libcpp[token]

--- libraries/libcpp/token.cpp
+++ libraries/libcpp/token.cpp
@@ -9,3 +9,35 @@
  * 
  */
 
+#include "token.h"
+#include <mods/string.h>
+
+namespace Cpp
+{
+
+    /**
+     * @param other 
+     * @return true 
+     * @return false 
+     */
+    bool Position::operator<(Position const& other) const
+    {
+        return line < other.line || (line == other.line && column < other.line);
+    }
+
+    /**
+     * @return String 
+     */
+    String Token::to_string() const
+    {
+        return String::formatted("{} {}:-{}:{}");
+    }
+
+    /**
+     * @return String 
+     */
+    String Token::type_as_string() const 
+    {
+        return type_to_string(m_type);
+    }
+} // namespace Cpp
