a035bc352d992a3d69c7edea052a692346a32044
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-05 19:29:20 +0530

    libraries: libcrypto[authentication(poly1305)]

--- libraries/libcrypto/authentication/poly1305.h
+++ libraries/libcrypto/authentication/poly1305.h
@@ -13,14 +13,41 @@
 
 #include <mods/bytebuffer.h>
 
-namespace Crypto
+namespace Crypto::Authentication 
 {
-    struct State {};
 
-    class Poly1305 {
+    struct State {
+        u32 r[4] {};
+        u32 s[4] {};
+        u64 a[8] {};
+        u8 blocks[17] {};
+        u8 block_count {};
+    }; // struct State
+
+    class Poly1305 
+    {
+    public:
+        /**
+         * @brief Construct a new Poly1305 object
+         * 
+         * @param key 
+         */
+        explicit Poly1305(ReadonlyBytes key);
+        
+        /**
+         * @param message 
+         */
+        void update(ReadonlyBytes message);
+
+        /**
+         * @return ErrorOr<ByteBuffer> 
+         */
+        ErrorOr<ByteBuffer> digest();
+
     private:
         void process_block();
 
         State m_state;
-    };
-}
+    }; // class Poly1305 
+
+} // namespace Crypto::Authentication
