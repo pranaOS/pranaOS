4c847e0145574b7ccb980bf93d27a662f1cac4d6
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-05 11:37:05 +0530

    libraries: libcore[tempfile]

--- libraries/libcore/tempfile.cpp
+++ libraries/libcore/tempfile.cpp
@@ -15,51 +15,68 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <sys/stat.h>
- 
-namespace Core
+
+namespace Core 
 {
 
+    /**
+     * @param type 
+     * @return NonnullOwnPtr<TempFile> 
+     */
+    NonnullOwnPtr<TempFile> TempFile::create(Type type)
+    {
+        return adopt_own(*new TempFile(type));
+    }
+
+    /**
+     * @param type 
+     * @return String 
+     */
     String TempFile::create_temp(Type type)
     {
-        char name_template[] = "tmp/tmp.XXXXXX";
+        char name_template[] = "/tmp/tmp.XXXXXX";
 
-        switch(type) {
-        case Type::File {
+        switch (type) {
+        case Type::File: {
             auto fd = mkstemp(name_template);
             VERIFY(fd >= 0);
             close(fd);
             break;
         }
-
-        case Type::Directory {
+        case Type::Directory: {
             auto fd = mkdtemp(name_template);
             VERIFY(fd != nullptr);
             break;
         }
         }
+        return String { name_template };
     }
-
+    
     /**
-     * @brief Destroy the Temp File:: Temp File object
+     * @brief Construct a new TempFile::TempFile object
      * 
      * @param type 
      */
-    TempFile::~TempFile(Type type)
-        : m_type(path)
+    TempFile::TempFile(Type type)
+        : m_type(type)
         , m_path(create_temp(type))
-    {}
+    {
+    }
 
     /**
-     * @brief Destroy the Temp File:: Temp File object
+     * @brief Destroy the TempFile::TempFile object
      * 
      */
     TempFile::~TempFile()
     {
         File::RecursionMode recursion_allowed { File::RecursionMode::Disallowed };
-
         if (m_type == Type::Directory)
             recursion_allowed = File::RecursionMode::Allowed;
-        
-        auto rc = File::remove(m_path.characters());
+
+        auto rc = File::remove(m_path.characters(), recursion_allowed, false);
+        if (rc.is_error()) {
+            warnln("File::remove failed: {}", rc.error().string_literal());
+        }
     }
+
 } // namespace Core
