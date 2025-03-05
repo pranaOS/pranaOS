6edcae00c31309798173b689d1a280f2b34c6839
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-05 12:12:47 +0530

    libraries: libcoredump[inspector]

--- libraries/libcoredump/inspector.h
+++ libraries/libcoredump/inspector.h
@@ -9,3 +9,26 @@
  * 
  */
 
+
+#pragma once
+
+#include "reader.h"
+#include <mods/noncopyable.h>
+#include <libdebug/processinspector>
+
+namespace Coredump
+{
+
+    class Inspector : public Debug::ProcessInspector
+    {
+    private:
+        void parse_loaded_libraries(Function<void(float)> on_progress);
+
+        size_t number_of_libraries_in_coredump() const;
+
+        NonnullOwnPtr<Reader> m_reader;
+
+        NonnullOwnPtrVector<Debug::LoadedLibrary> m_loaded_libraries;
+    }; // class Inspector : public Debug::ProcessInspector
+
+} // namespace Coredump
