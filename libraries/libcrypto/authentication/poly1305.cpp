fe4f35d6f43ea4790fca5e7be3b0530350d6f5e4
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-06 18:24:37 +0530

    libraries: libcrypto[authentication(poly1305)]

---
+++ libraries/libcrypto/authentication/poly1305.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file poly1305.cpp
+ * @author Krisna Pranav
+ * @brief poly1305
+ * @version 6.0
+ * @date 2025-03-05
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
