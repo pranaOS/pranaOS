36014ea554994be3eaf6fb25fc3c0cdd6f90cba3
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-06 20:33:08 +0530

    libraries: libcrypto[checksum(checksumfunction)]

---
+++ libraries/libcrypto/checksum/checksumfunction.h
@@ -0,0 +1,11 @@
+/**
+ * @file checksumfunction.h
+ * @author Krisna Pranav
+ * @brief check sum function
+ * @version 6.0
+ * @date 2025-03-06
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
