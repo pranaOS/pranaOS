633d810433c5a01831d87a6d701f568cc325c151
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-07 19:52:13 +0530

    libraries: libcrypto[cipher(aestables)]

---
+++ libraries/libcrypto/cipher/aestables.h
@@ -0,0 +1,11 @@
+/**
+ * @file aestables.h
+ * @author Krisna Pranav
+ * @brief aes tables
+ * @version 6.0
+ * @date 2025-03-07
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
