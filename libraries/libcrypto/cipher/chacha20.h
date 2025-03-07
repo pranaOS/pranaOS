8189cf8ab07e4c39177acd51948464c521023ec9
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-07 20:15:35 +0530

    libraries: libcrypto[cipher(chacha20)]

---
+++ libraries/libcrypto/cipher/chacha20.h
@@ -0,0 +1,11 @@
+/**
+ * @file chacha20.h
+ * @author Krisna Pranav
+ * @brief chacha 20
+ * @version 6.0
+ * @date 2025-03-07
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
