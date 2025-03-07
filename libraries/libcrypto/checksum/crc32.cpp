e93d5d9e38a1a3232741623dff72e915f0251c27
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-07 18:39:16 +0530

    libraries: libcrypto[checksum(crc32)]

---
+++ libraries/libcrypto/checksum/crc32.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file crc32.cpp
+ * @author Krisna Pranav
+ * @brief crc32
+ * @version 6.0
+ * @date 2025-03-07
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
