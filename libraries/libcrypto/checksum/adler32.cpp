a0377c0e611888f32ef8e20d85bc0fd815a70360
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-07 18:37:55 +0530

    libraries: libcrypto[checksum(adler32)]

---
+++ libraries/libcrypto/checksum/adler32.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file adler32.cpp
+ * @author Krisna Pranav
+ * @brief adler32
+ * @version 6.0
+ * @date 2025-03-07
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include <mods/span.h>
+#include <mods/types.h>
+#include <libcrypto/checksum/adler32.h>
+
+namespace Crypto::Checksum
+{
+
+} // namespace Crypto::Checksum
