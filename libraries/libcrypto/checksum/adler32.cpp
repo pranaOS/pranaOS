ea5cef61c94dcf8b7c9a83eda43d6639a6614b4c
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-07 18:38:39 +0530

    libraries: libcrypto[checksum(adler32)]

--- libraries/libcrypto/checksum/adler32.cpp
+++ libraries/libcrypto/checksum/adler32.cpp
@@ -13,7 +13,26 @@
 #include <mods/types.h>
 #include <libcrypto/checksum/adler32.h>
 
-namespace Crypto::Checksum
+namespace Crypto::Checksum 
 {
 
-} // namespace Crypto::Checksum
+    /**
+     * @param data 
+     */
+    void Adler32::update(ReadonlyBytes data)
+    {
+        for (size_t i = 0; i < data.size(); i++) {
+            m_state_a = (m_state_a + data.at(i)) % 65521;
+            m_state_b = (m_state_b + m_state_a) % 65521;
+        }
+    };
+
+    /**
+     * @return u32 
+     */
+    u32 Adler32::digest()
+    {
+        return (m_state_b << 16) | m_state_a;
+    }
+
+} // namespace Crypto::Checksum 
