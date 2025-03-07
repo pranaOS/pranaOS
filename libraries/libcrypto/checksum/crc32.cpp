108040d6732ec8e273412b4d4423623082ddbdc0
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-07 18:41:10 +0530

    libraries: libcrypto[checksum(crc32)]

--- libraries/libcrypto/checksum/crc32.cpp
+++ libraries/libcrypto/checksum/crc32.cpp
@@ -9,3 +9,37 @@
  * 
  */
 
+#include <mods/array.h>
+#include <mods/span.h>
+#include <mods/types.h>
+#include <libcrypto/checksum/crc32.h>
+
+namespace Crypto::Checksum
+{
+
+    static constexpr auto generate_table()
+    {
+        Array<u32, 256> data {};
+
+        for (auto i = 0u; i < data.size(); i++) {
+            u32 value = i;
+
+            for (auto j = 0; j < 8; j++) {
+                if (value & 1) {
+
+                } else {
+
+                }
+            }
+
+            data[i] = value;
+        }
+
+        return data;
+    }
+
+    u32 CRC32::digest()
+    {
+        return ~m_state;
+    }
+} // namespace Crypto::Checksum
