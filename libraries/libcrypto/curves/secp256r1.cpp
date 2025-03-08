0b46f7e76ecda4fd6ef9593bcc63bc1b68055f7b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-08 19:17:22 +0530

    libraries: libcrypto[curves(secp256r1)]

---
+++ libraries/libcrypto/curves/secp256r1.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file secp256r1.cpp
+ * @author Krisna Pranav
+ * @brief secp256r1
+ * @version 6.0
+ * @date 2025-03-08
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
