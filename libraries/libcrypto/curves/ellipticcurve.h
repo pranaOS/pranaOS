247c5164adedc329323f795acea333cfd4bab335
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-08 19:11:12 +0530

    libraries: libcrypto[curves(ellipticcurve)]

--- libraries/libcrypto/curves/ellipticcurve.h
+++ libraries/libcrypto/curves/ellipticcurve.h
@@ -13,12 +13,46 @@
 
 #include <mods/bytebuffer.h>
 
-namespace Crypto::Curves
+namespace Crypto::Curves 
 {
 
-    class EllipticCurve
+    class EllipticCurve 
     {
+    public:
+        /**
+         * @return size_t 
+         */
+        virtual size_t key_size() = 0;
 
+        /**
+         * @return ErrorOr<ByteBuffer> 
+         */
+        virtual ErrorOr<ByteBuffer> generate_private_key() = 0;
+
+        /**
+         * @param a 
+         * @return ErrorOr<ByteBuffer> 
+         */
+        virtual ErrorOr<ByteBuffer> generate_public_key(ReadonlyBytes a) = 0;
+
+        /**
+         * @param scalar_bytes 
+         * @param point_bytes 
+         * @return ErrorOr<ByteBuffer> 
+         */
+        virtual ErrorOr<ByteBuffer> compute_coordinate(ReadonlyBytes scalar_bytes, ReadonlyBytes point_bytes) = 0;
+
+        /**
+         * @param shared_point 
+         * @return ErrorOr<ByteBuffer> 
+         */
+        virtual ErrorOr<ByteBuffer> derive_premaster_key(ReadonlyBytes shared_point) = 0;
+
+        /**
+         * @brief Destroy the Elliptic Curve object
+         * 
+         */
+        virtual ~EllipticCurve() = default;
     }; // class EllipticCurve
-    
+
 } // namespace Crypto::Curves
