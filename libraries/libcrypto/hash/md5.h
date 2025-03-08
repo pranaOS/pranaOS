18812d1ff8b79209c2da9aa1c35bf1000ba44e59
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-08 20:49:16 +0530

    libraries: libcrypto[hash(md5)]

--- libraries/libcrypto/hash/md5.h
+++ libraries/libcrypto/hash/md5.h
@@ -11,37 +11,18 @@
 
 #pragma once
 
-#include <libcrypto/hash/hashfunction.h>
-#include <mods/string.h>
 #include <mods/types.h>
+#include <libcrypto/hash/hashfunction.h>
 
+#ifndef KERNEL
+#    include <mods/string.h>
+#endif
 
 namespace Crypto 
 {
+
     namespace Hash 
     {
-
-        struct MD5Digest 
-        {
-            constexpr static size_t Size = 16;
-            u8 data[Size];
-
-            /**
-             * @return const u8* 
-             */
-            const u8* immutable_data() const 
-            { 
-                return data; 
-            }
-
-            /**
-             * @return size_t 
-             */
-            size_t data_length() 
-            { 
-                return Size; 
-            }
-        }; // struct
 
         namespace MD5Constants 
         {
@@ -66,9 +47,7 @@ namespace Crypto
             constexpr u32 S42 = 10;
             constexpr u32 S43 = 15;
             constexpr u32 S44 = 21;
-
-            constexpr u8 PADDING[] = 
-            {
+            constexpr u8 PADDING[] = {
                 0x80, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -76,54 +55,30 @@ namespace Crypto
                 0
             };
 
-        } // namespace MD5Contstants
+        } // namespace MD5Constants
 
-        class MD5 final : public HashFunction<512, MD5Digest> 
+        class MD5 final : public HashFunction<512, 128> 
         {
         public:
-            /// @brief Construct a new MD5 object
-            MD5()
-            {
-                m_buffer = ByteBuffer::wrap(m_data_buffer, sizeof(m_data_buffer));
-            }
+            using HashFunction::update;
 
-            /// @brief: update
-            virtual void update(const u8*, size_t) override;
-
-            /**
-             * @param buffer 
-             */
-            virtual void update(const ByteBuffer& buffer) override 
-            { 
-                update(buffer.data(), buffer.size()); 
-            };
-
-            /**
-             * @param string 
-             */
-            virtual void update(const StringView& string) override 
-            { 
-                update((const u8*)string.characters_without_null_termination(), string.length()); 
-            };
-
-            /// @brief: digest + peek
+            virtual void update(u8 const*, size_t) override;
             virtual DigestType digest() override;
             virtual DigestType peek() override;
-            
-            /**
-             * @return String 
-             */
-            virtual String class_name() const override 
-            { 
-                return "MD5"; 
+
+        #ifndef KERNEL
+            virtual String class_name() const override
+            {
+                return "MD5";
             }
+        #endif
 
             /**
              * @param data 
              * @param length 
              * @return DigestType 
              */
-            inline static DigestType hash(const u8* data, size_t length)
+            inline static DigestType hash(u8 const* data, size_t length)
             {
                 MD5 md5;
                 md5.update(data, length);
@@ -134,7 +89,7 @@ namespace Crypto
              * @param buffer 
              * @return DigestType 
              */
-            inline static DigestType hash(const ByteBuffer& buffer) 
+            inline static DigestType hash(ByteBuffer const& buffer) 
             { 
                 return hash(buffer.data(), buffer.size()); 
             }
@@ -143,12 +98,11 @@ namespace Crypto
              * @param buffer 
              * @return DigestType 
              */
-            inline static DigestType hash(const StringView& buffer) 
+            inline static DigestType hash(StringView buffer) 
             { 
-                return hash((const u8*)buffer.characters_without_null_termination(), buffer.length()); 
+                return hash((u8 const*)buffer.characters_without_null_termination(), buffer.length()); 
             }
 
-            /// @brief: reset
             inline virtual void reset() override
             {
                 m_A = MD5Constants::init_A;
@@ -163,26 +117,22 @@ namespace Crypto
             }
 
         private:
-            /// @brief: transform
-            inline void transform(const u8*);
+            inline void transform(u8 const*);
 
             /**
              * @param from 
              * @param to 
              * @param length 
              */
-            static void encode(const u32* from, u8* to, size_t length);
-            static void decode(const u8* from, u32* to, size_t length);
+            static void encode(u32 const* from, u8* to, size_t length);
+            static void decode(u8 const* from, u32* to, size_t length);
 
-            /// @breif: constatns
             u32 m_A { MD5Constants::init_A }, m_B { MD5Constants::init_B }, m_C { MD5Constants::init_C }, m_D { MD5Constants::init_D };
-
-            /// @breif: count
             u32 m_count[2] { 0, 0 };
 
-            /// @brief: buffer + data buffer
-            ByteBuffer m_buffer;
-            u8 m_data_buffer[64];
-        }; // class MD5
+            u8 m_data_buffer[64] {};
+        }; // class MD5 final : public HashFunction<512, 128>
+
     } // namespace Hash
-} // namespace Crypto
+
+} // namespace Crypto 
