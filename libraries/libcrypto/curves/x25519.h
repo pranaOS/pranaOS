dd6631eb8e383a49ebf1b4960c6acf1eb5cef10b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-08 20:05:34 +0530

    libraries: libcrypto[curves(x25519)]

---
+++ libraries/libcrypto/curves/x25519.h
@@ -0,0 +1,11 @@
+/**
+ * @file x25519.h
+ * @author Krisna Pranav
+ * @brief x25519
+ * @version 0.1
+ * @date 2025-03-08
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
