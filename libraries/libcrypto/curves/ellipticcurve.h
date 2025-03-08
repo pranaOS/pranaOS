e268fa84985cad3b7bd5cf5dba575aaae8f0972b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-08 19:09:41 +0530

    libraries: libcrypto[curves(ellipticcurve)]

---
+++ libraries/libcrypto/curves/ellipticcurve.h
@@ -0,0 +1,11 @@
+/**
+ * @file ellipticcurve.h
+ * @author Krisna Pranav
+ * @brief elliptic curve
+ * @version 6.0
+ * @date 2025-03-08
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
