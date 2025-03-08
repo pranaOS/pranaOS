4ec085486911eaf34924ae1ae31cab3bcac47f6f
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-08 19:50:44 +0530

    libraries: libcrypto[curves(x448)]

--- libraries/libcrypto/curves/x448.h
+++ libraries/libcrypto/curves/x448.h
@@ -9,3 +9,48 @@
  * 
  */
 
+#pragma once
+
+#include <mods/bytebuffer.h>
+#include <libcrypto/curves/ellipticcurve.h>
+
+namespace Crypto::Curves 
+{
+
+    class X448 : public EllipticCurve 
+    {
+    public:
+        /**
+         * @return size_t 
+         */
+        size_t key_size() override 
+        { 
+            return 56; 
+        }
+
+        /**
+         * @return ErrorOr<ByteBuffer> 
+         */
+        ErrorOr<ByteBuffer> generate_private_key() override;
+
+        /**
+         * @param a 
+         * @return ErrorOr<ByteBuffer> 
+         */
+        ErrorOr<ByteBuffer> generate_public_key(ReadonlyBytes a) override;
+
+        /**
+         * @param a 
+         * @param b 
+         * @return ErrorOr<ByteBuffer> 
+         */
+        ErrorOr<ByteBuffer> compute_coordinate(ReadonlyBytes a, ReadonlyBytes b) override;
+
+        /**
+         * @param shared_point 
+         * @return ErrorOr<ByteBuffer> 
+         */
+        ErrorOr<ByteBuffer> derive_premaster_key(ReadonlyBytes shared_point) override;
+    }; // class X448 : public EllipticCurve 
+
+} // namespace Crypto::Curves 
