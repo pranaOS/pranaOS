90a4ff63afca2794eb3688403a79ef282a7f7505
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-08 19:49:38 +0530

    libraries: libcrypto[curves(x448)]

---
+++ libraries/libcrypto/curves/x448.h
@@ -0,0 +1,11 @@
+/**
+ * @file x448.h
+ * @author Krisna Pranav
+ * @brief x448
+ * @version 6.0
+ * @date 2025-03-08
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
