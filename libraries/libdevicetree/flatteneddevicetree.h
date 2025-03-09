49a5e9e9f8db9cc345dea14c1f4bc3d0f9ece40b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 18:40:41 +0530

    libraries: libdevicetree[flatteneddevicetree]

--- libraries/libdevicetree/flatteneddevicetree.h
+++ libraries/libdevicetree/flatteneddevicetree.h
@@ -9,3 +9,38 @@
  * 
  */
 
+
+#pragma once
+
+#include <mods/endian.h>
+#include <mods/types.h>
+
+namespace DeviceTree {
+
+    struct FlattenedDeviceTreeHeader {
+        BigEndian<u32> magic;             
+        BigEndian<u32> totalsize;         
+        BigEndian<u32> off_dt_struct;     
+                                        
+        BigEndian<u32> off_dt_strings;    
+                                        
+        BigEndian<u32> off_mem_rsvmap;    
+        BigEndian<u32> version;           
+        BigEndian<u32> last_comp_version; 
+        BigEndian<u32> boot_cpuid_phys;   
+        BigEndian<u32> size_dt_strings;   
+        BigEndian<u32> size_dt_struct;    
+    };
+    static_assert(sizeof(FlattenedDeviceTreeHeader) == 40, "FDT Header size must match specification");
+
+    struct FlattenedDeviceTreeReserveEntry {
+        BigEndian<u64> address;
+        BigEndian<u64> size;
+
+        bool operator==(FlattenedDeviceTreeReserveEntry const& other) const { return other.address == address && other.size == size; }
+        bool operator!=(FlattenedDeviceTreeReserveEntry const& other) const { return !(operator==(other)); }
+    }; // struct FlattenedDeviceTreeReserveEntry
+
+    static_assert(sizeof(FlattenedDeviceTreeReserveEntry) == 16, "FDT Memory Reservation entry size must match specification");
+
+} // namespace DeviceTree
