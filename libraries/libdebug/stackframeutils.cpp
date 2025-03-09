64d15f5963aec078fff30b609e6d00dae9cd35a2
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 17:34:24 +0530

    libraries: libdebug[stackframeutils]

---
+++ libraries/libdebug/stackframeutils.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file stackframeutils.cpp
+ * @author Krisna Pranav
+ * @brief Stack Frame Utils
+ * @version 6.0
+ * @date 2025-03-09
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+namespace Debug::StackFrameUtils
+{
+
+} // namespace Debug::StackFrameUtils
