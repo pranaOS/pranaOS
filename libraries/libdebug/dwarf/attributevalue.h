bf3b0be196dd01aea572a7346b8c6a691e64c0a7
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 13:14:24 +0530

    libraries: libdebug[dwarf(attributevalue)]

--- libraries/libdebug/dwarf/attributevalue.h
+++ libraries/libdebug/dwarf/attributevalue.h
@@ -9,3 +9,34 @@
  * 
  */
 
+#pragma once
+
+#include <mods/span.h>
+#include <mods/types.h>
+#include <libdebug/dwarf/dwarftypes.h>
+
+namespace Debug::Dwarf
+{
+
+    class CompilationUnit;
+
+    class AttributeValue
+    {
+        friend class DwarfInfo;
+
+    private:
+        Type m_type;
+
+        union {
+            FlatPtr as_addr;
+            u64 as_unsigned;
+            i64 as_signed;
+            char const* as_string;
+            bool as_bool;
+        } m_data {};
+
+        AttributeDataForm m_form {};
+
+        CompilationUnit const* m_compilation_unit { nullptr };
+    }; // class AttributeValue
+} // namespace Debug::Dwarf
