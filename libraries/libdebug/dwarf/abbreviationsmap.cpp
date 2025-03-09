e63c8cb9c0696bece74b0c6258dc71ca99b7f6c1
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 14:18:33 +0530

    libraries: libdebug[dwarf(abbreviationsmap)]

--- libraries/libdebug/dwarf/abbreviationsmap.cpp
+++ libraries/libdebug/dwarf/abbreviationsmap.cpp
@@ -9,3 +9,22 @@
  * 
  */
 
+#include "abbreviationsmap.h"
+#include "dwarfinfo.h"
+
+namespace Debug::Dwarf
+{
+
+    /**
+     * @brief Construct a new AbbreviationsMap::AbbreviationsMap object
+     * 
+     * @param dwarf_info 
+     * @param offset 
+     */
+    AbbreviationsMap::AbbreviationsMap(DwarfInfo const& dwarf_info, u32 offset)
+        : m_dwarf_info(dwarf_info)
+        , m_offset(offset);
+    {
+        populate_map();
+    }
+} // namespace Debug::Dwarf
