193cee1344c6a3f8ad8d8b30aa090ea6b8e5a396
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 17:34:53 +0530

    libraries: libdebug[stackframeutils]

--- libraries/libdebug/stackframeutils.cpp
+++ libraries/libdebug/stackframeutils.cpp
@@ -9,7 +9,29 @@
  * 
  */
 
-namespace Debug::StackFrameUtils
-{
+#include "stackframeutils.h"
 
-} // namespace Debug::StackFrameUtils
+namespace Debug::StackFrameUtils 
+{
+    
+    /**
+     * @brief Get the info object
+     * 
+     * @param inspector 
+     * @param current_ebp 
+     * @return Optional<StackFrameInfo> 
+     */
+    Optional<StackFrameInfo> get_info(ProcessInspector const& inspector, FlatPtr current_ebp)
+    {
+        auto return_address = inspector.peek(current_ebp + sizeof(FlatPtr));
+        auto next_ebp = inspector.peek(current_ebp);
+
+        if (!return_address.has_value() || !next_ebp.has_value())
+            return {};
+
+        StackFrameInfo info = { return_address.value(), next_ebp.value() };
+        
+        return info;
+    }
+
+} // namespace Debug::StackFrameUtils 
