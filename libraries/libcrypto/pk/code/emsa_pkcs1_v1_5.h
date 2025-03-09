489b1819027bf47d40ca7a2d9c977d354b749b5a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 12:35:37 +0530

    libraries: libcrypto[pk(code - emsa_pkcs1_v1_5)]

---
+++ libraries/libcrypto/pk/code/emsa_pkcs1_v1_5.h
@@ -0,0 +1,10 @@
+/**
+ * @file emsa_pkcs1_v1_5.h
+ * @author Krisna Pranav
+ * @version 6.0
+ * @date 2025-03-09
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
