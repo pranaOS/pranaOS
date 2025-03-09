c40227e3ccb526db57750f9b5da17f54e28e32e0
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 13:35:32 +0530

    libraries: libdebug[dwarf(expression)]

---
+++ libraries/libdebug/dwarf/expression.h
@@ -0,0 +1,11 @@
+/**
+ * @file expression.h
+ * @author Krisna Pranav
+ * @brief expression
+ * @version 6.0
+ * @date 2025-03-09
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
