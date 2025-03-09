0bc2651ec4654e55b619aeabdeeb4139c42084bf
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 14:17:29 +0530

    libraries: libdebug[dwarf(abbreviationsmap)]

---
+++ libraries/libdebug/dwarf/abbreviationsmap.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file abbreviationsmap.cpp
+ * @author Krisna Pranav
+ * @brief Abbreviations Map
+ * @version 6.0
+ * @date 2025-03-09
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
