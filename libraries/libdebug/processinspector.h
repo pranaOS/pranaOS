c02c554ab68aed804d9b535de3c6dd5c39bc1a59
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 17:38:04 +0530

    libraries: libdebug[processinspector]

--- libraries/libdebug/processinspector.h
+++ libraries/libdebug/processinspector.h
@@ -11,12 +11,20 @@
 
 #pragma once
 
-namespace Debug
+#include "loadedlibrary.h"
+#include <mods/types.h>
+#include <libc/sys/arch/i386/regs.h>
+
+namespace Debug 
 {
-    
-    class ProcessInspector
+
+    class ProcessInspector 
     {
     public:
+        /**
+         * @brief Destroy the Process Inspector object
+         * 
+         */
         virtual ~ProcessInspector() = default;
 
         /**
@@ -26,8 +34,59 @@ namespace Debug
          * @return false 
          */
         virtual bool poke(FlatPtr address, FlatPtr data) = 0;
-    protected:
-        ProcessInspector() = default;
-    }; // class ProcessInspector
 
-} // namespace Debug
+        /**
+         * @param address 
+         * @return Optional<FlatPtr> 
+         */
+        virtual Optional<FlatPtr> peek(FlatPtr address) const = 0;
+
+        virtual PtraceRegisters get_registers() const = 0;
+
+        virtual void set_registers(PtraceRegisters const&) = 0;
+
+        virtual void for_each_loaded_library(Function<IterationDecision(LoadedLibrary const&)>) const = 0;
+
+        /**
+         * @param address 
+         * @return LoadedLibrary const* 
+         */
+        LoadedLibrary const* library_at(FlatPtr address) const;
+
+        struct SymbolicationResult {
+            String library_name;
+            String symbol;
+        }; // struct SymbolicationResult
+ 
+        /**
+         * @param address 
+         * @return Optional<SymbolicationResult> 
+         */
+        Optional<SymbolicationResult> symbolicate(FlatPtr address) const;
+
+        /**
+         * @brief Get the address from source position object
+         * 
+         * @param file 
+         * @param line 
+         * @return Optional<DebugInfo::SourcePositionAndAddress> 
+         */
+        Optional<DebugInfo::SourcePositionAndAddress> get_address_from_source_position(String const& file, size_t line) const;
+
+        /**
+         * @brief Get the source position object
+         * 
+         * @param address 
+         * @return Optional<DebugInfo::SourcePosition> 
+         */
+        Optional<DebugInfo::SourcePosition> get_source_position(FlatPtr address) const;
+
+    protected:
+        /**
+         * @brief Construct a new ProcessInspector object
+         * 
+         */
+        ProcessInspector() = default;
+    }; // class ProcessInspector 
+
+}; // namespace Debug
