f73f52cc3d6c9dbac7608786da984ab2f8e7c17e
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 13:16:02 +0530

    libraries: libdebug[dwarf(attributevalue)]

--- libraries/libdebug/dwarf/attributevalue.h
+++ libraries/libdebug/dwarf/attributevalue.h
@@ -15,14 +15,83 @@
 #include <mods/types.h>
 #include <libdebug/dwarf/dwarftypes.h>
 
-namespace Debug::Dwarf
+namespace Debug::Dwarf 
 {
 
     class CompilationUnit;
 
-    class AttributeValue
+    class AttributeValue 
     {
         friend class DwarfInfo;
+
+    public:
+        enum class Type : u8 {
+            UnsignedNumber,
+            SignedNumber,
+            String,
+            DieReference, 
+            Boolean,
+            DwarfExpression,
+            SecOffset,
+            RawBytes,
+            Address
+        }; // enum class Type : u8
+
+        /**
+         * @return Type 
+         */
+        Type type() const 
+        { 
+            return m_type; 
+        }
+
+        /**
+         * @return AttributeDataForm 
+         */
+        AttributeDataForm form() const 
+        { 
+            return m_form; 
+        }
+
+        /**
+         * @return FlatPtr 
+         */
+        FlatPtr as_addr() const;
+
+        /**
+         * @return u64 
+         */
+        u64 as_unsigned() const 
+        { 
+            return m_data.as_unsigned; 
+        }
+
+        /**
+         * @return i64 
+         */
+        i64 as_signed() const 
+        { 
+            return m_data.as_signed; 
+        }
+
+        char const* as_string() const;
+
+        /**
+         * @return true 
+         * @return false 
+         */
+        bool as_bool() const 
+        { 
+            return m_data.as_bool; 
+        }
+
+        /**
+         * @return ReadonlyBytes 
+         */
+        ReadonlyBytes as_raw_bytes() const 
+        { 
+            return m_data.as_raw_bytes; 
+        }
 
     private:
         Type m_type;
@@ -31,12 +100,14 @@ namespace Debug::Dwarf
             FlatPtr as_addr;
             u64 as_unsigned;
             i64 as_signed;
-            char const* as_string;
+            char const* as_string; 
             bool as_bool;
+            ReadonlyBytes as_raw_bytes;
         } m_data {};
 
         AttributeDataForm m_form {};
 
         CompilationUnit const* m_compilation_unit { nullptr };
     }; // class AttributeValue
+
 } // namespace Debug::Dwarf
