105e23a295f7360fc6f5c201053baf5f4a17d676
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 12:26:01 +0530

    libraries: libcrypto[numbertheory(modularfunctions)]

---
+++ libraries/libcrypto/numbertheory/modularfunctions.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file modularfunctions.cpp
+ * @author Krisna Pranav
+ * @brief Modular Functions
+ * @version 6.0
+ * @date 2025-03-09
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+namespace Crypto
+{
+
+    namespace NumberTheory
+    {
+
+    } // namespace NumberTheory
+    
+} // namespace Crypto
