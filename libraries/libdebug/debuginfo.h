b5f7f08aa126a20909c487722b5f1af5d9cc07a8
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 18:08:26 +0530

    libraries: libdebug[debuginfon]

---
+++ libraries/libdebug/debuginfo.h
@@ -0,0 +1,11 @@
+/**
+ * @file debuginfo.h
+ * @author Krisna Pranav
+ * @brief debug info
+ * @version 6.0
+ * @date 2025-03-09
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
