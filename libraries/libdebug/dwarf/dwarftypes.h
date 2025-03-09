097fcec4ff06dc1ef4183e2a57c3615175082f3f
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 13:08:49 +0530

    libraries: libdebug[dwarf(dwarftypes)]

---
+++ libraries/libdebug/dwarf/dwarftypes.h
@@ -0,0 +1,17 @@
+/**
+ * @file dwarftypes.h
+ * @author Krisna Pranav
+ * @brief dwarf types
+ * @version 6.0
+ * @date 2025-03-09
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+namespace Debug::Dwarf
+{
+
+} // namespace Debug::Dwarf
