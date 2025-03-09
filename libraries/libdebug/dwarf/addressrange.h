b897cb0aabe2bebc283ab933c80c9db2b1207bd1
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 13:49:01 +0530

    libraries: libdebug[dwarf(addressrange)]

---
+++ libraries/libdebug/dwarf/addressrange.h
@@ -0,0 +1,11 @@
+/**
+ * @file addressrange.h
+ * @author Krisna Pranav
+ * @brief address range
+ * @version 6.0
+ * @date 2025-03-09
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
