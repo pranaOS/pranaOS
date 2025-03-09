846c34a77906a10ff6196c7fb969733edfafc050
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 14:35:22 +0530

    libraries: libdebug[dwarf(attributevalue)]

---
+++ libraries/libdebug/dwarf/attributevalue.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file attributevalue.cpp
+ * @author Krisna Pranav
+ * @brief attribute value
+ * @version 6.0
+ * @date 2025-03-09
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
