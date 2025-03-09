b6bdae5036e695b7690db782b2470b9b052fbafb
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 17:49:40 +0530

    libraries: libdebug[debugsession]

---
+++ libraries/libdebug/debugsession.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file debugsession.cpp
+ * @author Krisna Pranav
+ * @brief debug session
+ * @version 6.0
+ * @date 2025-03-09
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
