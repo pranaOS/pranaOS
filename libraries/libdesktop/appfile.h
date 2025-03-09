ade96ebb956a1c21b99740fe53ab7bb4fbac9ebf
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 18:23:01 +0530

    libraries: libdesktop[appfile]

--- libraries/libdesktop/appfile.h
+++ libraries/libdesktop/appfile.h
@@ -9,14 +9,85 @@
  * 
  */
 
+
 #pragma once
 
-namespace Desktop
+#include <libcore/configfile.h>
+#include <libgui/fileiconprovider.h>
+#include <libgui/icon.h>
+
+namespace Desktop 
 {
 
-    class AppFile
+    class AppFile : public RefCounted<AppFile> 
     {
+    public:
+        static constexpr char const* APP_FILES_DIRECTORY = "/res/apps";
 
-    }; // class AppFile
+        /**
+         * @brief Get the for app object
+         * 
+         * @param app_name 
+         * @return NonnullRefPtr<AppFile> 
+         */
+        static NonnullRefPtr<AppFile> get_for_app(StringView app_name);
+
+        /**
+         * @param path 
+         * @return NonnullRefPtr<AppFile> 
+         */
+        static NonnullRefPtr<AppFile> open(StringView path);
+
+        /**
+         * @param directory 
+         */
+        static void for_each(Function<void(NonnullRefPtr<AppFile>)>, StringView directory = APP_FILES_DIRECTORY);
+
+        /**
+         * @brief Destroy the AppFile object
+         * 
+         */
+        ~AppFile() = default;
+
+        bool is_valid() const 
+        { 
+            return m_valid; 
+        }
+
+        /**
+         * @return String 
+         */
+        String filename() const 
+        { 
+            return m_config->filename(); 
+        }
+
+        /**
+         * @return String 
+         */
+        String name() const;
+        String executable() const;
+        String category() const;
+        String description() const;
+        String icon_path() const;
+        GUI::Icon icon() const;
+        bool run_in_terminal() const;
+        Vector<String> launcher_file_types() const;
+        Vector<String> launcher_protocols() const;
+        bool spawn() const;
+
+    private:
+        /**
+         * @brief Construct a new AppFile object
+         * 
+         * @param path 
+         */
+        explicit AppFile(StringView path);
+
+        bool validate() const;
+
+        RefPtr<Core::ConfigFile> m_config;
+        bool m_valid { false };
+    }; // class AppFile : public RefCounted<AppFile>
 
 } // namespace Desktop
