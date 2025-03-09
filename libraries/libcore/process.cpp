0a8c45dd4079eb291263b8a2a6906d8bbdc838ab
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 18:32:11 +0530

    libraries: libcore[process]

---
+++ libraries/libcore/process.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file process.cpp
+ * @author Krisna Pranav
+ * @brief process
+ * @version 6.0
+ * @date 2025-03-09
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
