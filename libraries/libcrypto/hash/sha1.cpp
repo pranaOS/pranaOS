d08ec9a0b976f7af61afd4200618b74b55c7d421
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 09:07:36 +0530

    libraries: libcrypto[hash(sha1)]

---
+++ libraries/libcrypto/hash/sha1.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file sha1.cpp
+ * @author Krisna Pranav
+ * @brief sha1
+ * @version 6.0
+ * @date 2025-03-09
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
