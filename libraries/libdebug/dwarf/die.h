107697f821dc47dc8f207bbe0ba26dbfe1f42d7f
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 13:19:50 +0530

    libraries: libdebug[dwarf(die)]

---
+++ libraries/libdebug/dwarf/die.h
@@ -0,0 +1,11 @@
+/**
+ * @file die.h
+ * @author Krisna Pranav
+ * @brief die
+ * @version 6.0
+ * @date 2025-03-09
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
