527df6d83ba743ed5b8dbd198fc3f4d470745e0c
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 18:39:56 +0530

    libraries: libdevicetree[flatteneddevicetree]

---
+++ libraries/libdevicetree/flatteneddevicetree.h
@@ -0,0 +1,11 @@
+/**
+ * @file flatteneddevicetree.h
+ * @author Krisna Pranav
+ * @brief Flattened Device Tree
+ * @version 6.0
+ * @date 2025-03-09
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
