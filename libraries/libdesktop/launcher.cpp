8a3f2a5b028578aa617c68782755aa35152fd56f
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 18:35:10 +0530

    libraries: libdesktop[launcher]

---
+++ libraries/libdesktop/launcher.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file launcher.cpp
+ * @author Krisna Pranav
+ * @brief launcher
+ * @version 6.0
+ * @date 2025-03-09
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
