177725af81d9fa5a4cfa270169cb7e54a30dc702
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 18:47:34 +0530

    libraries: libdiff[hunk]

--- libraries/libdiff/hunks.cpp
+++ libraries/libdiff/hunks.cpp
@@ -10,16 +10,16 @@
  */
 
 #include "hunks.h"
-
-#define DEBUG_HUNKS
+#include <mods/debug.h>
 
 namespace Diff 
 {
+
     /**
      * @param diff 
      * @return Vector<Hunk> 
      */
-    Vector<Hunk> parse_hunks(const String& diff)
+    Vector<Hunk> parse_hunks(String const& diff)
     {
         Vector<String> diff_lines = diff.split('\n');
         if (diff_lines.is_empty())
@@ -30,7 +30,6 @@ namespace Diff
         size_t line_index = 0;
         HunkLocation current_location {};
 
-        // Skip to first hunk
         while (diff_lines[line_index][0] != '@') {
             ++line_index;
         }
@@ -68,21 +67,19 @@ namespace Diff
             hunks.append(hunk);
         }
 
-    #ifdef DEBUG_HUNKS
-        for (const auto& hunk : hunks) {
-            dbg() << "Hunk location:";
-            dbg() << "orig: " << hunk.original_start_line;
-            dbg() << "target: " << hunk.target_start_line;
-            dbg() << "removed:";
-            for (const auto& line : hunk.removed_lines) {
-                dbg() << "- " << line;
-            }
-            dbg() << "added:";
-            for (const auto& line : hunk.added_lines) {
-                dbg() << "+ " << line;
+        if constexpr (HUNKS_DEBUG) {
+            for (auto const& hunk : hunks) {
+                dbgln("Hunk location:");
+                dbgln("  orig: {}", hunk.original_start_line);
+                dbgln("  target: {}", hunk.target_start_line);
+                dbgln("  removed:");
+                for (auto const& line : hunk.removed_lines)
+                    dbgln("- {}", line);
+                dbgln("  added:");
+                for (auto const& line : hunk.added_lines)
+                    dbgln("+ {}", line);
             }
         }
-    #endif
 
         return hunks;
     }
@@ -91,37 +88,35 @@ namespace Diff
      * @param location_line 
      * @return HunkLocation 
      */
-    HunkLocation parse_hunk_location(const String& location_line)
+    HunkLocation parse_hunk_location(String const& location_line)
     {
         size_t char_index = 0;
-
-        struct StartAndLength 
-        {
+        struct StartAndLength {
             size_t start { 0 };
             size_t length { 0 };
         };
+        auto parse_start_and_length_pair = [](String const& raw) {
+            auto index_of_separator = raw.find(',').value();
+            auto start = raw.substring(0, index_of_separator).to_uint().value();
+            auto length = raw.substring(index_of_separator + 1, raw.length() - index_of_separator - 1).to_uint().value();
 
-        auto parse_start_and_length_pair = [](const String& raw) 
-        {
-            auto index_of_separator = raw.index_of(",").value();
-            auto start = raw.substring(0, index_of_separator);
-            auto length = raw.substring(index_of_separator + 1, raw.length() - index_of_separator - 1);
-            auto res = StartAndLength { start.to_uint().value() - 1, length.to_uint().value() - 1 };
-            return res;
+            if (start != 0)
+                start--;
+
+            if (length != 0)
+                length--;
+
+            return StartAndLength { start, length };
         };
-
         while (char_index < location_line.length() && location_line[char_index++] != '-') {
         }
-
-        ASSERT(char_index < location_line.length());
+        VERIFY(char_index < location_line.length());
 
         size_t original_location_start_index = char_index;
 
         while (char_index < location_line.length() && location_line[char_index++] != ' ') {
         }
-
-        ASSERT(char_index < location_line.length() && location_line[char_index] == '+');
-
+        VERIFY(char_index < location_line.length() && location_line[char_index] == '+');
         size_t original_location_end_index = char_index - 2;
 
         size_t target_location_start_index = char_index + 1;
@@ -129,8 +124,7 @@ namespace Diff
         char_index += 1;
         while (char_index < location_line.length() && location_line[char_index++] != ' ') {
         }
-
-        ASSERT(char_index < location_line.length());
+        VERIFY(char_index < location_line.length());
 
         size_t target_location_end_index = char_index - 2;
 
@@ -145,17 +139,14 @@ namespace Diff
      */
     void HunkLocation::apply_offset(size_t offset, HunkLocation::LocationType type)
     {
-        if (type == LocationType::Original || type == LocationType::Both) 
-        {
+        if (type == LocationType::Original || type == LocationType::Both) {
             original_start_line += offset;
             original_length -= offset;
         }
-
-        if (type == LocationType::Target || type == LocationType::Both) 
-        {
+        if (type == LocationType::Target || type == LocationType::Both) {
             target_start_line += offset;
             target_length -= offset;
         }
     }
 
-};
+}; // namespace Diff
