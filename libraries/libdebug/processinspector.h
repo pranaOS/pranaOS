23d717bf84248d7a84cf95db177d1334faf1e472
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 17:36:14 +0530

    libraries: libdebug[processinspector]

---
+++ libraries/libdebug/processinspector.h
@@ -0,0 +1,17 @@
+/**
+ * @file processinspector.h
+ * @author Krisna Pranav
+ * @brief process inspector
+ * @version 6.0
+ * @date 2025-03-09
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+namespace Debug
+{
+
+} // namespace Debug
