dcbe74909987e80b7141c15ac8e20beee13f6b2a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 18:19:18 +0530

    libraries: libdesktop[launcher]

---
+++ libraries/libdesktop/launcher.h
@@ -0,0 +1,22 @@
+/**
+ * @file launcher.h
+ * @author Krisna Pranav
+ * @brief launcher
+ * @version 6.0
+ * @date 2025-03-09
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+namespace Desktop
+{
+
+    class Launcher
+    {
+        
+    }; // class Launcher
+    
+} // namespace Desktop
