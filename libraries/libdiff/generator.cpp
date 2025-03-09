a946ea4a1da410f9f90702ab5f20f43736ee29b6
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 18:49:25 +0530

    libraries: libdiff[generator]

---
+++ libraries/libdiff/generator.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file generator.cpp
+ * @author Krisna Pranav
+ * @brief generator
+ * @version 6.0 
+ * @date 2025-03-09
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
