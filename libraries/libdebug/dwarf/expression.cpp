0bbb6cba6029a3699f1f54c6b8cb3e967b05d652
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 17:26:33 +0530

    libraries: libdebug[dwarf(expression)]

--- libraries/libdebug/dwarf/expression.cpp
+++ libraries/libdebug/dwarf/expression.cpp
@@ -9,14 +9,50 @@
  * 
  */
 
-#include "expression.h"
 
-namespace Debug::Dwarf::Expression
+#include "expression.h"
+#include <mods/format.h>
+#include <mods/memorystream.h>
+#include <sys/arch/i386/regs.h>
+
+namespace Debug::Dwarf::Expression 
 {
 
-    Value evaluate()
+    /**
+     * @param bytes 
+     * @param regs 
+     * @return Value 
+     */
+    Value evaluate(ReadonlyBytes bytes, [[maybe_unused]] PtraceRegisters const& regs)
     {
+        InputMemoryStream stream(bytes);
+
+        while (!stream.eof()) {
+            u8 opcode = 0;
+            stream >> opcode;
+
+            switch (static_cast<Operations>(opcode)) {
+    #if ARCH(I386)
+            case Operations::RegEbp: {
+                ssize_t offset = 0;
+                stream.read_LEB128_signed(offset);
+                return Value { Type::UnsignedInteger, { regs.ebp + offset } };
+            }
+
+            case Operations::FbReg: {
+                ssize_t offset = 0;
+                stream.read_LEB128_signed(offset);
+                return Value { Type::UnsignedInteger, { regs.ebp + 2 * sizeof(size_t) + offset } };
+            }
+    #endif
+
+            default:
+                dbgln("DWARF expr addr: {:p}", bytes.data());
+                dbgln("unsupported opcode: {}", opcode);
+                VERIFY_NOT_REACHED();
+            }
+        }
+        VERIFY_NOT_REACHED();
     }
 
-    
 } // namespace Debug::Dwarf::Expression
