5bb382f0230997331a2595e3dc3d522a744e8a0d
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 12:39:31 +0530

    libraries: libcrypto[pk(rsa)]

---
+++ libraries/libcrypto/pk/rsa.h
@@ -0,0 +1,31 @@
+/**
+ * @file rsa.h
+ * @author Krisna Pranav
+ * @brief rsa
+ * @version 6.0
+ * @date 2025-03-09
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+namespace Crypto
+{
+
+    namespace PK
+    {
+
+        /**
+         * @tparam Integer 
+         */
+        template<typename Integer = UnsignedBigInteger>
+        class RSAPublicKey
+        {
+
+        }; // class RSAPublicKey
+
+    } // namespace PK
+    
+} // namespace Crypto
