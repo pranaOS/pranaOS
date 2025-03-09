9ce1146d5bb35aa0aaa5427b9abb81a300a0d3eb
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 13:21:15 +0530

    libraries: libdebug[dwarf(die)]

--- libraries/libdebug/dwarf/die.h
+++ libraries/libdebug/dwarf/die.h
@@ -9,3 +9,27 @@
  * 
  */
 
+#pragma once
+
+namespace Debug::Dwarf
+{
+
+    class CompilationUnit;
+
+    class DIE
+    {
+    
+    private:
+        void rehydrate_from(u32 offset, Optional<u32> parent_offset);
+
+        CompilationUnit const& m_compilation_unit;
+        u32 m_offset { 0 };
+        u32 m_data_offset { 0 };
+        size_t m_abbreviation_code { 0 };
+        EntryTag m_tag { EntryTag::None };
+        bool m_has_children { false };
+        u32 m_size { 0 };
+        Optional<u32> m_parent_offset;
+    }; // class DIE
+
+} // namespace Debug::Dwarf
