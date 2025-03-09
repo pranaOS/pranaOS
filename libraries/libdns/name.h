a48216693d787f53f75c3f72212c4d98a8ecac4b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 18:53:59 +0530

    libraries: libdns[name]

---
+++ libraries/libdns/name.h
@@ -0,0 +1,12 @@
+/**
+ * @file name.h
+ * @author Krisna Pranav
+ * @brief name
+ * @version 6.0
+ * @date 2025-03-09
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
