56b976100389de6f6f17c4ca4c67b718c6d391da
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 18:45:39 +0530

    libraries: libdiff[format]

--- libraries/libdiff/format.cpp
+++ libraries/libdiff/format.cpp
@@ -11,23 +11,25 @@
 
 #include "format.h"
 #include <mods/string.h>
-#include <mods/string_builder.h>
+#include <mods/stringbuilder.h>
 #include <mods/vector.h>
 
 namespace Diff 
-{   
+{
+
     /**
      * @param text 
      * @return String 
      */
-    String generate_only_additions(const String& text)
+    String generate_only_additions(String const& text)
     {
         auto lines = text.split('\n', true); 
         StringBuilder builder;
-        builder.appendf("@@ -1,%u +1,%u @@\n", lines.size());
-        for (const auto& line : lines) {
-            builder.appendf("+%s\n", line.characters());
+        builder.appendff("@@ -0,0 +1,{} @@\n", lines.size());
+        for (auto const& line : lines) {
+            builder.appendff("+{}\n", line);
         }
         return builder.to_string();
     }
-}; // namespace Mods
+
+}; // namespace Diff 
