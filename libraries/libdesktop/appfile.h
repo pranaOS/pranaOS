3007eb7be7a524c39300aaf4718f2c1a3b4c4d0f
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-09 18:21:06 +0530

    libraries: libdesktop[appfile]

---
+++ libraries/libdesktop/appfile.h
@@ -0,0 +1,11 @@
+/**
+ * @file appfile.h
+ * @author Krisna Pranav
+ * @brief App File
+ * @version 6.0
+ * @date 2025-03-09
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
