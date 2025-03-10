fab165e8fea60114baba6a3315243b5c58a64ce5
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-10 20:23:00 +0530

    libraries: libdns[answer]

---
+++ libraries/libdns/answer.h
@@ -0,0 +1,12 @@
+/**
+ * @file answer.h
+ * @author Krisna Pranav
+ * @brief answer
+ * @version 6.0
+ * @date 2025-03-10
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
