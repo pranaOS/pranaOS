ecd52faf6f23eea2d93a5d80bdce9e73b93d1e81
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-11 21:19:49 +0530

    mods: assertions

---
+++ mods/assertions.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file assertions.cpp
+ * @author Krisna Pranav
+ * @brief assertions
+ * @version 6.0
+ * @date 2025-03-11
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include <mods/assertions.h>
+
+namespace 
+{
+
+    void dump_backtrace()
+    {
+
+    }
+
+} // anonymous namespace
