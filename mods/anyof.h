83b447038ddc3a29e6bec99fb8bb8b97f169aeae
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-11 19:21:26 +0530

    mods: anyof

--- mods/anyof.h
+++ mods/anyof.h
@@ -28,15 +28,12 @@ namespace Mods
      * @return false 
      */
     template<typename TEndIterator, IteratorPairWith<TEndIterator> TIterator>
-    [[nodiscard]] constexpr bool all_of(
+    [[nodiscard]] constexpr bool any_of(
         TIterator const& begin,
         TEndIterator const& end,
         auto const& predicate)
     {
-        constexpr auto negated_predicate = [](auto const& pred) {
-            return [&](auto const& elem) { return !pred(elem); };
-        };
-        return !(find_if(begin, end, negated_predicate(predicate)) != end);
+        return find_if(begin, end, predicate) != end;
     }
 
     /**
@@ -47,13 +44,13 @@ namespace Mods
      * @return false 
      */
     template<IterableContainer Container>
-    [[nodiscard]] constexpr bool all_of(Container&& container, auto const& predicate)
+    [[nodiscard]] constexpr bool any_of(Container&& container, auto const& predicate)
     {
-        return all_of(container.begin(), container.end(), predicate);
+        return any_of(container.begin(), container.end(), predicate);
     }
 
 } // namespace Mods
- 
+
 #if USING_MODS_GLOBALLY
-using Mods::all_of;
+using Mods::any_of;
 #endif
