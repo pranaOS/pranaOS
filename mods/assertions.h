c25661cef4fa006a1716c9ecc4d0bb2600ce8650
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-11 20:28:47 +0530

    mods :assertions

--- mods/assertions.h
+++ mods/assertions.h
@@ -9,14 +9,21 @@
  *
  */
 
- #pragma once
+#pragma once
 
- #if defined(KERNEL)
- #    include <kernel/assertions.h>
- #else
- #    include <assert.h>
- #    define VERIFY assert
- #    define VERIFY_NOT_REACHED() assert(false)
- static constexpr bool TODO = false;
- #    define TODO() VERIFY(TODO)
- #endif
+#if defined(KERNEL)
+#    include <kernel/library/assertions.h>
+#else
+extern "C" __attribute__((noreturn)) void mods_verification_failed(char const*);
+#    define __stringify_helper(x) #x
+#    define __stringify(x) __stringify_helper(x)
+#    define VERIFY(expr)                                                                  \
+        (__builtin_expect(!(expr), 0)                                                     \
+                ? mods_verification_failed(#expr " at " __FILE__ ":" __stringify(__LINE__)) \
+                : (void)0)
+#    define VERIFY_NOT_REACHED() VERIFY(false) 
+static constexpr bool TODO = false;
+#    define TODO() VERIFY(TODO)                
+#    define TODO_AARCH64() VERIFY(TODO)        
+#    define TODO_RISCV64() VERIFY(TODO)        
+#endif
