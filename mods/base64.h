ff44c8bb4cb7252386a46b53b3bf65bc3b9931ef
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-13 19:20:17 +0530

    mods: base64 r the commit message for your changes. Lines starting

--- mods/base64.h
+++ mods/base64.h
@@ -11,34 +11,85 @@
 
 #pragma once
 
+#include <mods/array.h>
 #include <mods/bytebuffer.h>
 #include <mods/error.h>
 #include <mods/string.h>
 #include <mods/stringview.h>
 
-namespace Mods
+namespace Mods 
 {
+
+    constexpr Array base64_alphabet = {
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+        'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+        'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+        'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+        'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+        'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+        'w', 'x', 'y', 'z', '0', '1', '2', '3',
+        '4', '5', '6', '7', '8', '9', '+', '/'
+    };
+
+    constexpr Array base64url_alphabet = {
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+        'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+        'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+        'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+        'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+        'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+        'w', 'x', 'y', 'z', '0', '1', '2', '3',
+        '4', '5', '6', '7', '8', '9', '-', '_'
+    };
+
     /**
-     * @return size_t 
+     * @return consteval 
      */
-    size_t calculate_base64_decoded_length(StringView);
+    consteval auto base64_lookup_table()
+    {
+        Array<i16, 256> table;
+        table.fill(-1);
+        for (size_t i = 0; i < base64_alphabet.size(); ++i) {
+            table[base64_alphabet[i]] = static_cast<i16>(i);
+        }
+        return table;
+    }
+
+    /**
+     * @return consteval 
+     */
+    consteval auto base64url_lookup_table()
+    {
+        Array<i16, 256> table;
+        table.fill(-1);
+        for (size_t i = 0; i < base64url_alphabet.size(); ++i) {
+            table[base64url_alphabet[i]] = static_cast<i16>(i);
+        }
+        return table;
+    }
 
     /**
      * @return size_t 
      */
-    size_t calculate_base64_encoded_length(ReadonlyBytes);
+    [[nodiscard]] size_t calculate_base64_decoded_length(StringView);
+
+    [[nodiscard]] size_t calculate_base64_encoded_length(ReadonlyBytes);
 
     /**
      * @return ErrorOr<ByteBuffer> 
      */
-    ErrorOr<ByteBuffer> decode_base64(StringView);
+    [[nodiscard]] ErrorOr<ByteBuffer> decode_base64(StringView);
+    [[nodiscard]] ErrorOr<ByteBuffer> decode_base64url(StringView);
 
-    /**
-     * @return String 
-     */
-    String encode_base64(ReadonlyBytes);
+    [[nodiscard]] ErrorOr<String> encode_base64(ReadonlyBytes);
+    [[nodiscard]] ErrorOr<String> encode_base64url(ReadonlyBytes);
 
 } // namespace Mods
 
+
+#if USING_MODS_GLOBALLY
 using Mods::decode_base64;
+using Mods::decode_base64url;
 using Mods::encode_base64;
+using Mods::encode_base64url;
+#endif
