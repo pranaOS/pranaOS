b7be5e9960da031f0d297a6ffa8ef05a4a9f5e64
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-13 20:19:29 +0530

    mods: bufferedhelper

---
+++ mods/bufferedhelper.h
@@ -0,0 +1,12 @@
+/**
+ * @file bufferedhelper.h
+ * @author Krisna Pranav
+ * @brief buffered helper
+ * @version 6.0
+ * @date 2025-03-13
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
