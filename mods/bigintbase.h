0f42d30d23b5673d97935b17ae5305b56ecf24f2
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-13 19:25:01 +0530

    mods: bigintbase

---
+++ mods/bigintbase.h
@@ -0,0 +1,22 @@
+/**
+ * @file bigintbase.h
+ * @author Krisna Pranav
+ * @brief big int base
+ * @version 6.0
+ * @date 2025-03-13
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+namespace Mods
+{
+
+    namespace Detail
+    {
+
+    } // namespace Detail
+    
+} // namespace Mods
