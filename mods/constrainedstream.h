470b56cc6682e598918dfb89a4b1d78e257b5053
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-15 15:23:37 +0530

    mods: constrainedstream

--- mods/constrainedstream.h
+++ mods/constrainedstream.h
@@ -9,3 +9,52 @@
  * 
  */
 
+#pragma once
+
+#include <mods/maybeowned.h>
+#include <mods/stream.h>
+
+namespace Mods  
+{
+
+    class ConstrainedStream : public Stream 
+    {
+    public:
+        /**
+         * @brief Construct a new Constrained Stream object
+         * 
+         * @param limit 
+         */
+        ConstrainedStream(MaybeOwned<Stream>, u64 limit);
+
+        /**
+         * @return ErrorOr<Bytes> 
+         */
+        virtual ErrorOr<Bytes> read_some(Bytes) override;
+
+        /**
+         * @param discarded_bytes 
+         * @return ErrorOr<void> 
+         */
+        virtual ErrorOr<void> discard(size_t discarded_bytes) override;
+
+        /**
+         * @return ErrorOr<size_t> 
+         */
+        virtual ErrorOr<size_t> write_some(ReadonlyBytes) override;
+
+        /**
+         * @return true 
+         * @return false 
+         */
+        virtual bool is_eof() const override;
+        virtual bool is_open() const override;
+        
+        virtual void close() override;
+
+    private:
+        MaybeOwned<Stream> m_stream;
+        u64 m_limit;
+    }; // class ConstrainedStream : public Stream
+ 
+} // namespace Mods 
