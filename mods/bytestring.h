dd1d44b9b27f3c48aed91cc10e00ead95d022a37
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-15 12:05:04 +0530

    mods: bytestring

---
+++ mods/bytestring.h
@@ -0,0 +1,12 @@
+/**
+ * @file bytestring.h
+ * @author Krisna Pranav
+ * @brief byte string
+ * @version 6.0
+ * @date 2025-03-15
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
