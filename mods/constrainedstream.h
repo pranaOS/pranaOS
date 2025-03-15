b372976276bb1f0d6d106391816b34e5c8ad55ff
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-15 15:22:27 +0530

    mods: constrainedstream

---
+++ mods/constrainedstream.h
@@ -0,0 +1,11 @@
+/**
+ * @file constrainedstream.h
+ * @author Krisna Pranav
+ * @brief constrained stream
+ * @version 6.0
+ * @date 2025-03-15
+ * 
+ * @copyright Copyright (c) 2021-2025 Patrick, pranaOS Developers, Krisna Pranav
+ * 
+ */
+
