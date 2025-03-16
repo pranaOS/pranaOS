6ce6d1ef9fdc3452d9545bf0fff2d8d4d4997948
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-16 15:39:52 +0530

    mods: defaultdelete

---
+++ mods/defaultdelete.h
@@ -0,0 +1,11 @@
+/**
+ * @file defaultdelete.h
+ * @author Krisna Pranav
+ * @brief default delete
+ * @version 6.0
+ * @date 2025-03-16
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
