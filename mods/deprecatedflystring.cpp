ce31baba31f0d50c24e3533b8d98da86caae639b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-16 15:47:03 +0530

    mods: deprecatedflystring

---
+++ mods/deprecatedflystring.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file deprecatedflystring.cpp
+ * @author Krisna Pranav
+ * @brief deprecated fly string
+ * @version 6.0
+ * @date 2025-03-16
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
