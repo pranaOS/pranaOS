cde0ccc137351397a44234eecc003f3ca7308008
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-16 15:29:21 +0530

    mods: countingstream

--- mods/countingstream.h
+++ mods/countingstream.h
@@ -8,3 +8,57 @@
  * @copyright Copyright (c) 2021-2025 Patrick, pranaOS Developers, Krisna Pranav
  * 
  */
+
+#pragma once
+
+#include <mods/maybeowned.h>
+#include <mods/stream.h>
+
+namespace Mods 
+{
+
+    class CountingStream : public Stream 
+    {
+    public:
+        /**
+         * @brief Construct a new Counting Stream object
+         * 
+         */
+        CountingStream(MaybeOwned<Stream>);
+
+        /**
+         * @return u64 
+         */
+        u64 read_bytes() const;
+
+        /**
+         * @return ErrorOr<Bytes> 
+         */
+        virtual ErrorOr<Bytes> read_some(Bytes) override;
+
+        /**
+         * @param discarded_bytes 
+         * @return ErrorOr<void> 
+         */
+        virtual ErrorOr<void> discard(size_t discarded_bytes) override;
+
+        /**
+         * @return ErrorOr<size_t> 
+         */
+        virtual ErrorOr<size_t> write_some(ReadonlyBytes) override;
+
+        /**
+         * @return true 
+         * @return false 
+         */
+        virtual bool is_eof() const override;
+        virtual bool is_open() const override;
+
+        virtual void close() override;
+
+    private:
+        MaybeOwned<Stream> m_stream;
+        u64 m_read_bytes { 0 };
+    }; // class CountingStream : public Stream 
+
+} // namespace Mods
