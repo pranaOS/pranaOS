b046713cbdc16b296fa046f81a33c2b56095ae89
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-16 15:28:12 +0530

    mods: countingstream

---
+++ mods/countingstream.h
@@ -0,0 +1,10 @@
+/**
+ * @file countingstream.h
+ * @author Krisna Pranav
+ * @brief couting stream
+ * @version 6.0
+ * @date 2025-03-16
+ * 
+ * @copyright Copyright (c) 2021-2025 Patrick, pranaOS Developers, Krisna Pranav
+ * 
+ */
