d88f7025146a8d1aff945b39eeab605c7ede4dad
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-16 15:34:21 +0530

    mods: cowvector

---
+++ mods/cowvector.h
@@ -0,0 +1,11 @@
+/**
+ * @file cowvector.h
+ * @author Krisna Pranav
+ * @brief cowvector
+ * @version 6.0
+ * @date 2025-03-16
+ * 
+ * @copyright Copyright (c) 2021-2025 Patrick, pranaOS Developers, Krisna Pranav
+ * 
+ */
+
