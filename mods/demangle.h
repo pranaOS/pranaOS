3c2c3c3a09d9cbe30da541b339231b1921a11000
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-16 15:41:33 +0530

    mods: demangle

--- mods/demangle.h
+++ mods/demangle.h
@@ -9,39 +9,35 @@
  * 
  */
 
-/**
- * @link https://linux.die.net/man/1/c++filt
- * @link https://gcc.gnu.org/onlinedocs/libstdc++/manual/ext_demangling.html
- * @link https://blog.oakbits.com/how-to-mangle-and-demangle-a-c-method-name.html
- */
-
 #pragma once
 
 #ifndef KERNEL
 
-#    include <mods/string.h>
+#    include <mods/bytestring.h>
 #    include <mods/stringview.h>
 #    include <cxxabi.h>
 
-namespace Mods
+namespace Mods 
 {
+
     /**
      * @param name 
-     * @return String 
+     * @return ByteString 
      */
-    inline String demangle(StringView name)
+    inline ByteString demangle(StringView name)
     {
         int status = 0;
-        auto* demangled_name = abi::__cxa_demangle(name.to_string().characters(), nullptr, nullptr, &status);
-        auto string = String(status == 0 ? demangled_name : name);
-
+        auto* demangled_name = abi::__cxa_demangle(name.to_byte_string().characters(), nullptr, nullptr, &status);
+        auto string = ByteString(status == 0 ? StringView { demangled_name, strlen(demangled_name) } : name);
         if (status == 0)
             free(demangled_name);
-            
         return string;
     }
-}
 
+} // namespace Mods
+
+#    if USING_MODS_GLOBALLY
 using Mods::demangle;
+#    endif
 
 #endif
