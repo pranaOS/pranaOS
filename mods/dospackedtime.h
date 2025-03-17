3666eebe891d02a1fc9d9b68d981aec61c92e2f2
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-17 15:05:44 +0530

    mods: dospackedtime

---
+++ mods/dospackedtime.h
@@ -0,0 +1,17 @@
+/**
+ * @file dospackedtime.h
+ * @author Krisna Pranav
+ * @brief DOSPackedTime
+ * @version 6.0
+ * @date 2025-03-17
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+namespace Mods
+{
+
+} // namespace Mods
