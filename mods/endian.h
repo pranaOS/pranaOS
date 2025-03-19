d7cdae62aece487aa4ab4c7919d6ed5c313b5303
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-19 17:00:03 +0530

    mods: endian

--- mods/endian.h
+++ mods/endian.h
@@ -15,222 +15,176 @@
 #include <mods/forward.h>
 #include <mods/platform.h>
 
-#if defined(MODS_OS_MACOS)
-#    include <libkern/OSByteOrder.h>
-#    include <machine/endian.h>
-
-#    define htobe16(x) OSSwapHostToBigInt16(x)
-#    define htole16(x) OSSwapHostToLittleInt16(x)
-#    define be16toh(x) OSSwapBigToHostInt16(x)
-#    define le16toh(x) OSSwapLittleToHostInt16(x)
-
-#    define htobe32(x) OSSwapHostToBigInt32(x)
-#    define htole32(x) OSSwapHostToLittleInt32(x)
-#    define be32toh(x) OSSwapBigToHostInt32(x)
-#    define le32toh(x) OSSwapLittleToHostInt32(x)
-
-#    define htobe64(x) OSSwapHostToBigInt64(x)
-#    define htole64(x) OSSwapHostToLittleInt64(x)
-#    define be64toh(x) OSSwapBigToHostInt64(x)
-#    define le64toh(x) OSSwapLittleToHostInt64(x)
-
-#    define __BIG_ENDIAN BIG_ENDIAN
-#    define __LITTLE_ENDIAN LITTLE_ENDIAN
-#    define __BYTE_ORDER BYTE_ORDER
-#endif
-
 namespace Mods 
 {
-    /**
-     * @tparam T 
-     * @param value 
-     * @return ALWAYS_INLINE constexpr 
-     */
-    template<typename T>
-    ALWAYS_INLINE constexpr T convert_between_host_and_little_endian(T value)
-    {
-    #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+inline constexpr static bool HostIsLittleEndian = true;
+#else
+inline constexpr static bool HostIsLittleEndian = false;
+#endif
+
+/** 
+ * @tparam T 
+ * @param value 
+ * @return ALWAYS_INLINE constexpr 
+ */
+template<typename T>
+ALWAYS_INLINE constexpr T convert_between_host_and_little_endian(T value)
+{
+    if constexpr (HostIsLittleEndian || sizeof(T) == 1)
         return value;
-    #elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
-        if constexpr (sizeof(T) == 8)
-            return __builtin_bswap64(value);
-        if constexpr (sizeof(T) == 4)
-            return __builtin_bswap32(value);
-        if constexpr (sizeof(T) == 2)
-            return __builtin_bswap16(value);
-        if constexpr (sizeof(T) == 1)
-            return value;
-    #endif
-    }
+    else if constexpr (sizeof(T) == 8)
+        return static_cast<T>(__builtin_bswap64(static_cast<u64>(value)));
+    else if constexpr (sizeof(T) == 4)
+        return static_cast<T>(__builtin_bswap32(static_cast<u32>(value)));
+    else if constexpr (sizeof(T) == 2)
+        return static_cast<T>(__builtin_bswap16(static_cast<u16>(value)));
+    else
+        static_assert(DependentFalse<T>, "Cannot byte-swap values larger than 64-bits");
+}
 
-    /**
-     * @tparam T 
-     * @param value 
-     * @return ALWAYS_INLINE constexpr 
-     */
-    template<typename T>
-    ALWAYS_INLINE constexpr T convert_between_host_and_big_endian(T value)
-    {
-    #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-        if constexpr (sizeof(T) == 8)
-            return __builtin_bswap64(value);
-        if constexpr (sizeof(T) == 4)
-            return __builtin_bswap32(value);
-        if constexpr (sizeof(T) == 2)
-            return __builtin_bswap16(value);
-        if constexpr (sizeof(T) == 1)
-            return value;
-    #elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+/**
+ * @tparam T 
+ * @param value 
+ * @return ALWAYS_INLINE constexpr 
+ */
+template<typename T>
+ALWAYS_INLINE constexpr T convert_between_host_and_big_endian(T value)
+{
+    if constexpr (sizeof(T) == 1 || !HostIsLittleEndian)
         return value;
-    #endif
-    }
-    
-    /**
-     * @tparam T 
-     * @param value 
-     * @return ALWAYS_INLINE 
-     */
-    template<typename T>
-    ALWAYS_INLINE T convert_between_host_and_network_endian(T value)
-    {
-        return convert_between_host_and_big_endian(value);
-    }
+    else if constexpr (sizeof(T) == 8)
+        return static_cast<T>(__builtin_bswap64(static_cast<u64>(value)));
+    else if constexpr (sizeof(T) == 4)
+        return static_cast<T>(__builtin_bswap32(static_cast<u32>(value)));
+    else if constexpr (sizeof(T) == 2)
+        return static_cast<T>(__builtin_bswap16(static_cast<u16>(value)));
+    else
+        static_assert(DependentFalse<T>, "Cannot byte-swap values larger than 64-bits");
+}
+
+/**
+ * @tparam T 
+ * @param value 
+ * @return ALWAYS_INLINE 
+ */
+template<typename T>
+ALWAYS_INLINE T convert_between_host_and_network_endian(T value)
+{
+    return convert_between_host_and_big_endian(value);
+}
+
+/**
+ * @tparam T 
+ */
+template<typename T>
+class [[gnu::packed]] LittleEndian 
+{
+public:
+    constexpr LittleEndian() = default;
 
     /**
-     * @tparam T 
-     */
-    template<typename T>
-    class LittleEndian;
-
-    /**
-     * @tparam T 
-     * @return InputStream& 
-     */
-    template<typename T>
-    InputStream& operator>>(InputStream&, LittleEndian<T>&);
-
-    /**
-     * @tparam T 
-     * @return OutputStream& 
-     */
-    template<typename T>
-    OutputStream& operator<<(OutputStream&, LittleEndian<T>);
-
-    /**
-     * @tparam T 
-     */
-    template<typename T>
-    class [[gnu::packed]] LittleEndian 
-    {
-    public:
-        /**
-         * @return InputStream& 
-         */
-        friend InputStream& operator>><T>(InputStream&, LittleEndian<T>&);
-
-        /**
-         * @return OutputStream& 
-         */
-        friend OutputStream& operator<< <T>(OutputStream&, LittleEndian<T>);
-
-        constexpr LittleEndian() = default;
-
-        constexpr LittleEndian(T value)
-            : m_value(convert_between_host_and_little_endian(value))
-        {
-        }
-
-        /**
-         * @return T 
-         */
-        constexpr operator T() const 
-        { 
-            return convert_between_host_and_little_endian(m_value); 
-        }
-
-    private:
-        T m_value { 0 };
-    }; // class [[gnu::packed]] LittleEndian
-
-    /**
-     * @tparam T 
-     */
-    template<typename T>
-    class BigEndian;
-
-    /**
-     * @tparam T 
-     * @return InputStream& 
-     */
-    template<typename T>
-    InputStream& operator>>(InputStream&, BigEndian<T>&);
-
-    /**
-     * @tparam T 
-     * @return OutputStream& 
-     */
-    template<typename T>
-    OutputStream& operator<<(OutputStream&, BigEndian<T>);
-    
-    /**
-     * @tparam T 
-     */
-    template<typename T>
-    class [[gnu::packed]] BigEndian 
-    {
-    public:
-
-        /**
-         * @return InputStream& 
-         */
-        friend InputStream& operator>><T>(InputStream&, BigEndian<T>&);
-
-        /**
-         * @return OutputStream& 
-         */
-        friend OutputStream& operator<< <T>(OutputStream&, BigEndian<T>);
-
-        constexpr BigEndian() = default;
-
-        constexpr BigEndian(T value)
-            : m_value(convert_between_host_and_big_endian(value))
-        {
-        }
-
-        constexpr operator T() const 
-        { 
-            return convert_between_host_and_big_endian(m_value); 
-        }
-
-    private:
-        T m_value { 0 };
-    }; // class [[gnu::packed]] BigEndian 
-
-    /**
-     * @tparam T 
-     */
-    template<typename T>
-    using NetworkOrdered = BigEndian<T>;
-
-    /**
-     * @brief Construct a new requires object
+     * @param value
      * 
-     * @tparam T 
      */
-    template<typename T>
-    requires(HasFormatter<T>) struct Formatter<LittleEndian<T>> : Formatter<T> {
-    };
+    constexpr LittleEndian(T value)
+        : m_value(convert_between_host_and_little_endian(value))
+    {
+    }
+
+    constexpr operator T() const 
+    { 
+        return convert_between_host_and_little_endian(m_value); 
+    }
+
+private:
+    T m_value { 0 };
+}; // class [[gnu::packed]] LittleEndian
+
+/**
+ * @tparam T 
+ */
+template<typename T>
+class [[gnu::packed]] BigEndian 
+{
+public:
+    constexpr BigEndian() = default;
 
     /**
-     * @brief Construct a new requires object
+     * @param value
      * 
-     * @tparam T 
      */
-    template<typename T>
-    requires(HasFormatter<T>) struct Formatter<BigEndian<T>> : Formatter<T> {
-    };
-} // namespace Mods
+    constexpr BigEndian(T value)
+        : m_value(convert_between_host_and_big_endian(value))
+    {
+    }
 
+    constexpr operator T() const 
+    { 
+        return convert_between_host_and_big_endian(m_value); 
+    }
+
+private:
+    T m_value { 0 };
+}; // class [[gnu::packed]] BigEndian
+
+/**
+ * @tparam T 
+ */
+template<typename T>
+using NetworkOrdered = BigEndian<T>;
+
+/**
+ * @brief Construct a new requires object
+ * 
+ * @tparam T 
+ */
+template<typename T>
+requires(HasFormatter<T>) struct Formatter<LittleEndian<T>> : Formatter<T> {
+};
+
+/**
+ * @brief Construct a new requires object
+ * 
+ * @tparam T 
+ */
+template<typename T>
+requires(HasFormatter<T>) struct Formatter<BigEndian<T>> : Formatter<T> {
+};
+
+/**
+ * @tparam T 
+ */
+template<typename T>
+struct Traits<LittleEndian<T>> : public DefaultTraits<LittleEndian<T>> {
+    static constexpr bool is_trivially_serializable() { return Traits<T>::is_trivially_serializable(); }
+};
+
+/**
+ * @tparam T 
+ */
+template<typename T>
+struct Traits<BigEndian<T>> : public DefaultTraits<BigEndian<T>> {
+    static constexpr bool is_trivially_serializable() { return Traits<T>::is_trivially_serializable(); }
+};
+
+/**
+ * @param v 
+ * @return constexpr u16 
+ */
+constexpr u16 bitswap(u16 v)
+{
+    v = ((v >> 1) & 0x5555) | ((v & 0x5555) << 1);    
+    v = ((v >> 2) & 0x3333) | ((v & 0x3333) << 2);    
+    v = ((v >> 4) & 0x0F0F) | ((v & 0x0F0F) << 4);    
+    return ((v >> 8) & 0x00FF) | ((v & 0x00FF) << 8); 
+}
+
+}
+
+#if USING_MODS_GLOBALLY
 using Mods::BigEndian;
+using Mods::bitswap;
 using Mods::LittleEndian;
 using Mods::NetworkOrdered;
+#endif
