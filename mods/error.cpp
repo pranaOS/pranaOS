b454fb897b534c338004beb45e20051159f6e685
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-22 12:32:11 +0530

    mods: error

--- mods/error.cpp
+++ mods/error.cpp
@@ -9,3 +9,28 @@
  * 
  */
 
+#include <mods/error.h>
+
+#ifdef KERNEL
+#    include <mods/format.h>
+#endif
+
+namespace Mods 
+{
+
+    /**
+     * @param string_literal 
+     * @param code 
+     * @return Error 
+     */
+    Error Error::from_string_view_or_print_error_and_return_errno(StringView string_literal, [[maybe_unused]] int code)
+    {
+    #ifdef KERNEL
+        dmesgln("{}", string_literal);
+        return Error::from_errno(code);
+    #else
+        return Error::from_string_view(string_literal);
+    #endif
+    }
+
+}
