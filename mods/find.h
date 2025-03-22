da2c91b7429cc6220ce4ef8895c04673ea048355
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-22 12:37:54 +0530

    mods: find

--- mods/find.h
+++ mods/find.h
@@ -11,12 +11,13 @@
 
 #pragma once
 
-#include <mods/concept.h>
+#include <mods/concepts.h>
 #include <mods/traits.h>
 #include <mods/types.h>
 
 namespace Mods 
 {
+
     /**
      * @tparam TEndIterator 
      * @tparam TIterator 
@@ -27,40 +28,47 @@ namespace Mods
      * @return constexpr TIterator 
      */
     template<typename TEndIterator, IteratorPairWith<TEndIterator> TIterator, typename TUnaryPredicate>
-    constexpr TIterator find_if(TIterator first, TEndIterator last, TUnaryPredicate&& pred)
+    [[nodiscard]] constexpr TIterator find_if(TIterator first, TEndIterator last, TUnaryPredicate&& pred)
     {
         for (; first != last; ++first) {
             if (pred(*first)) {
                 return first;
             }
         }
-
         return last;
     }
 
     /**
      * @tparam TEndIterator 
      * @tparam TIterator 
-     * @tparam T 
+     * @tparam V 
      * @param first 
      * @param last 
      * @param value 
      * @return constexpr TIterator 
      */
-    template<typename TEndIterator, IteratorPairWith<TEndIterator> TIterator, typename T>
-    constexpr TIterator find(TIterator first, TEndIterator last, T const& value)
+    template<typename TEndIterator, IteratorPairWith<TEndIterator> TIterator, typename V>
+    [[nodiscard]] constexpr TIterator find(TIterator first, TEndIterator last, V const& value)
     {
-        return find_if(first, last, [&](auto const& v) { return Traits<T>::equals(value, v); });
+        return find_if(first, last, [&]<typename T>(T const& entry) { return Traits<T>::equals(entry, value); });
     }
 
     /**
      * @tparam TEndIterator 
      * @tparam TIterator 
-     * @tparam T 
+     * @tparam V 
      */
-    template<typename TEndIterator, IteratorPairWith<TEndIterator> TIterator, typename T>
-    constexpr size_t find_index(TIterator first, TEndIterator last, T const& value) requires(requires(TIterator it) { it.index(); })
+    template<typename TEndIterator, IteratorPairWith<TEndIterator> TIterator, typename V>
+    [[nodiscard]] constexpr size_t find_index(TIterator first, TEndIterator last, V const& value)
+    requires(requires(TIterator it) { it.index(); })
     {
-        return find_if(first, last, [&](auto const& v) { return Traits<T>::equals(value, v); }).index();
+        return find_if(first, last, [&]<typename T>(T const& entry) { return Traits<T>::equals(entry, value); }).index();
     }
-} // namespace Mods
+
+}
+
+#if USING_MODS_GLOBALLY
+using Mods::find;
+using Mods::find_if;
+using Mods::find_index;
+#endif
