3172dc8e32e812a9a680fb66ae9a8953d99ddd97
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-22 13:00:21 +0530

    mods: fixedstringbuffer

---
+++ mods/fixedstringbuffer.h
@@ -0,0 +1,11 @@
+/**
+ * @file fixedstringbuffer.h
+ * @author Krisna Pranav
+ * @brief Fixed String Buffer
+ * @version 6.0
+ * @date 2025-03-22
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
