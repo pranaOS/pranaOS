2134ca02a7613bf670c36296ce6d4d931cde24c7
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-22 12:31:32 +0530

    mods: error

---
+++ mods/error.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file error.cpp
+ * @author Krisna Pranav
+ * @brief error
+ * @version 6.0
+ * @date 2025-03-22
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
