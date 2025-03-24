5e3b5688a3b4b922904a20bac6658e9da6aef027
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-24 22:17:36 +0530

    mods: floatingpointstringconversions

---
+++ mods/floatingpointstringconversions.h
@@ -0,0 +1,10 @@
+/**
+ * @file floatingpointstringconversions.h
+ * @author Krisna Pranav
+ * @brief Floating Point String Conversions
+ * @version 6.0
+ * @date 2025-03-24
+ *
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ *
+ */
