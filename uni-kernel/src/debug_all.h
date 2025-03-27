6ae12fa22ea7450b16b21812f75a73fdd0b385ae
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-27 22:10:44 +0530

    uni-kernel: src[debug_all]

---
+++ uni-kernel/src/debug_all.h
@@ -0,0 +1,46 @@
+/**
+ * @file debug_all.h
+ * @author Krisna Pranav
+ * @brief Debug All
+ * @version 6.0
+ * @date 2025-03-27
+ *
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ *
+ */
+
+#define BLOCKQ_DEBUG
+#define CLOCK_DEBUG
+#define CONTEXT_DEBUG
+#define ELF_DEBUG
+#define EPOLL_DEBUG
+#define EXEC_DEBUG
+#define GDB_DEBUG
+#define INT_DEBUG
+#define IOUR_DEBUG
+#define KLIB_DEBUG
+#define KVM_DEBUG
+#define MGMT_DEBUG
+#define MUTEX_DEBUG
+#define NETLINK_DEBUG
+#define PAGECACHE_DEBUG
+#define PAGE_DEBUG
+#define PF_DEBUG
+#define PIPE_DEBUG
+#define SCHED_DEBUG
+#define SIGNAL_DEBUG
+#define STORAGE_DEBUG
+#define TFS_DEBUG
+#define TLOG_DEBUG
+#define UNIX_TIMER_DEBUG
+#define VIRTIO_BALLOON_DEBUG
+#define VIRTIO_BLK_DEBUG
+#define VIRTIO_MMIO_DEBUG
+#define VIRTIO_NET_DEBUG
+#define VIRTIO_PCI_DEBUG
+#define VIRTIO_SCSI_DEBUG
+#define VIRTQUEUE_DEBUG
+#define XEN_DEBUG
+#define XEN_INT_DEBUG
+#define XENNET_DEBUG
+#define XENSTORE_DEBUG
