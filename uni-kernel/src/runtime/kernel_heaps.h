acde1dd5e91e7c29efcf6194e5df665f0d181b43
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-27 22:15:10 +0530

    uni-kernel: runtime[kernel_heaps]

--- uni-kernel/src/runtime/kernel_heaps.h
+++ uni-kernel/src/runtime/kernel_heaps.h
@@ -9,9 +9,27 @@
  *
  */
 
-typedef struct kernel_heaps {
+typedef struct kernel_heaps
+{
     heap physical;
-} *kernel_heaps;
+
+    id_heap virtual_huge;
+    id_heap virtual_page;
+
+    backed_heap page_backed;
+
+    backed_heap linear_backed;
+
+    heap pages;
+
+    heap general;
+
+    heap locked;
+
+    heap malloc;
+
+    heap dma;
+}* kernel_heaps;
 
 /**
  * @param heaps 
@@ -20,4 +38,58 @@ typedef struct kernel_heaps {
 static inline heap heap_physical(kernel_heaps heaps)
 {
     return heaps->physical;
+}
+
+/**
+ * @param heaps 
+ * @return id_heap 
+ */
+static inline id_heap heap_virtual_huge(kernel_heaps heaps)
+{
+    return heaps->virtual_huge;
+}
+
+/**
+ * @param heaps 
+ * @return id_heap 
+ */
+static inline id_heap heap_virtual_page(kernel_heaps heaps)
+{
+    return heaps->virtual_page;
+}
+
+/**
+ * @param heaps 
+ * @return backed_heap 
+ */
+static inline backed_heap heap_page_backed(kernel_heaps heaps)
+{
+    return heaps->page_backed;
+}
+
+/**
+ * @param heaps 
+ * @return backed_heap 
+ */
+static inline backed_heap heap_linear_backed(kernel_heaps heaps)
+{
+    return heaps->linear_backed;
+}
+
+/**
+ * @param heaps 
+ * @return heap 
+ */
+static inline heap heap_general(kernel_heaps heaps)
+{
+    return heaps->general;
+}
+
+/**
+ * @param heaps 
+ * @return heap 
+ */
+static inline heap heap_locked(kernel_heaps heaps)
+{
+    return heaps->locked;
 }
