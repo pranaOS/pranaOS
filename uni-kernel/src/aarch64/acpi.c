19ea285b65e3a0210444d304d70ce3682e489b9b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-28 18:40:36 +0530

    uni-kernel: aarch64[acpi]

--- uni-kernel/src/aarch64/acpi.c
+++ uni-kernel/src/aarch64/acpi.c
@@ -9,6 +9,11 @@
  *
  */
 
+#include <acpi.h>
+#include <boot/uefi.h>
+#include <drivers/acpi.h>
+#include <kernel.h>
+
 /**
  * @param irq
  * @param t
@@ -16,7 +21,12 @@
  */
 void acpi_register_irq_handler(int irq, thunk t, sstring name)
 {
-    irq_register_handle(irq, t, name, irange(0, 0));
+    irq_register_handler(irq, t, name, irange(0, 0));
+}
+
+ACPI_PHYSICAL_ADDRESS AcpiOsGetRootPointer(void)
+{
+    return boot_params.acpi_rsdp;
 }
 
 /**
@@ -35,7 +45,36 @@ ACPI_STATUS AcpiOsReadPort(ACPI_IO_ADDRESS address, UINT32* value, UINT32 width)
     case 16:
         *value = mmio_read_16(address);
         break;
-    default:
+    case 32:
+        *value = mmio_read_32(address);
         break;
+    default:
+        return AE_BAD_PARAMETER;
     }
+    return AE_OK;
+}
+
+/**
+ * @param address
+ * @param value
+ * @param width
+ * @return ACPI_STATUS
+ */
+ACPI_STATUS AcpiOsWritePort(ACPI_IO_ADDRESS address, UINT32 value, UINT32 width)
+{
+    switch(width)
+    {
+    case 8:
+        mmio_write_8(address, value);
+        break;
+    case 16:
+        mmio_write_16(address, value);
+        break;
+    case 32:
+        mmio_write_32(address, value);
+        break;
+    default:
+        return AE_BAD_PARAMETER;
+    }
+    return AE_OK;
 }
