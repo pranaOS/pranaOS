02be10e43bc6e2ea355b84c818f42b54b313986f
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-28 18:41:05 +0530

    uni-kernel: aarch64[clock]

---
+++ uni-kernel/src/aarch64/clock.c
@@ -0,0 +1,10 @@
+/**
+ * @file clock.c
+ * @author Krisna Pranav
+ * @brief clock
+ * @version 6.0
+ * @date 2025-03-28
+ *
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ *
+ */
