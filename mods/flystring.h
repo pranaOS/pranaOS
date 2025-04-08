19c3436b0cb2ef8ac413cec49b5918e1056a8296
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-08 18:40:44 +0530

    mods: flystring

--- mods/flystring.h
+++ mods/flystring.h
@@ -4,20 +4,28 @@
  * @brief fly string
  * @version 6.0
  * @date 2023-07-04
- * 
+ *
  * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
- * 
+ *
  */
 
 #pragma once
 
-#include "mods/stringutils.h"
+#include <mods/error.h>
+#include <mods/format.h>
+#include <mods/platform.h>
 #include <mods/string.h>
+#include <mods/traits.h>
+#include <mods/types.h>
 
-namespace Mods 
+namespace Mods
 {
-    class FlyString 
+
+    class FlyString
     {
+        MOD_MAKE_DEFAULT_MOVABLE(FlyString);
+        MOD_MAKE_DEFAULT_COPYABLE(FlyString);
+
     public:
         /**
          * @brief Construct a new Fly String object
@@ -25,25 +33,15 @@ namespace Mods
          */
         FlyString() = default;
 
-        /**
-         * @brief Construct a new Fly String object
-         * 
-         * @param other 
-         */
-        FlyString(FlyString const& other)
-            : m_impl(other.impl())
-        {
-        }
+        static ErrorOr<FlyString> from_utf8(StringView);
+        static FlyString from_utf8_without_validation(ReadonlyBytes);
 
         /**
-         * @brief Construct a new Fly String object
-         * 
-         * @param other 
+         * @tparam T 
          */
-        FlyString(FlyString&& other)
-            : m_impl(move(other.m_impl))
-        {
-        }
+        template <typename T>
+            requires(IsOneOf<RemoveCVReference<T>, ByteString, DeprecatedFlyString, FlyString, String>)
+        static ErrorOr<String> from_utf8(T&&) = delete;
 
         /**
          * @brief Construct a new Fly String object
@@ -52,241 +50,179 @@ namespace Mods
         FlyString(String const&);
 
         /**
-         * @brief Construct a new Fly String object
-         * 
-         */
-        FlyString(StringView);
-
-        /**
-         * @brief Construct a new Fly String object
-         * 
-         * @param string 
-         */
-        FlyString(char const* string)
-            : FlyString(static_cast<String>(string))
-        {
-        }
-
-        /**
-         * @param impl 
-         * @return FlyString 
-         */
-        static FlyString from_fly_impl(NonnullRefPtr<StringImpl> impl)
-        {
-            VERIFY(impl->is_fly());
-            FlyString string;
-            string.m_impl = move(impl);
-            return string;
-        }
-
-        /**
-         * @param other 
          * @return FlyString& 
          */
-        FlyString& operator=(FlyString const& other)
-        {
-            m_impl = other.m_impl;
-            return *this;
-        }
+        FlyString& operator=(String const&);
+
+        /**
+         * @return true 
+         * @return false 
+         */
+        [[nodiscard]] bool is_empty() const;
+        [[nodiscard]] unsigned hash() const;
+
+        /**
+         * @return u32 
+         */
+        [[nodiscard]] u32 ascii_case_insensitive_hash() const;
+
+        explicit operator String() const;
+
+        /**
+         * @return String 
+         */
+        String to_string() const;
+
+        /**
+         * @return Utf8View 
+         */
+        [[nodiscard]] Utf8View code_points() const;
+
+        /**
+         * @return ReadonlyBytes 
+         */
+        [[nodiscard]] ReadonlyBytes bytes() const;
+
+        /**
+         * @return StringView 
+         */
+        [[nodiscard]] StringView bytes_as_string_view() const;
 
         /**
          * @param other 
-         * @return FlyString& 
-         */
-        FlyString& operator=(FlyString&& other)
-        {
-            m_impl = move(other.m_impl);
-            return *this;
-        }
-
-        /**
-         * @return true 
-         * @return false 
-         */
-        bool is_empty() const 
-        { 
-            return !m_impl || !m_impl->length(); 
-        }
-
-        /**
-         * @return true 
-         * @return false 
-         */
-        bool is_null() const 
-        { 
-            return !m_impl; 
-        }
-
-        /**
-         * @param other 
-         * @return true 
-         * @return false 
-         */
-        bool operator==(FlyString const& other) const 
-        { 
-            return m_impl == other.m_impl; 
-        }
-
-        /**
-         * @param other 
-         * @return true 
-         * @return false 
-         */
-        bool operator!=(FlyString const& other) const 
-        { 
-            return m_impl != other.m_impl; 
-        }
-
-        /**
-         * @return true 
-         * @return false 
-         */
-        bool operator==(String const&) const;
-
-        /**
-         * @param string 
-         * @return true 
-         * @return false 
-         */
-        bool operator!=(String const& string) const 
-        { 
-            return !(*this == string); 
-        }
-
-        /**
-         * @return true 
-         * @return false 
-         */
-        bool operator==(StringView) const;
-
-        /**
-         * @param string 
-         * @return true 
-         * @return false 
-         */
-        bool operator!=(StringView string) const 
-        { 
-            return !(*this == string); 
-        }
-
-        /**
-         * @return true 
-         * @return false 
-         */
-        bool operator==(char const*) const;
-
-        /**
-         * @param string 
-         * @return true 
-         * @return false 
-         */
-        bool operator!=(char const* string) const 
-        { 
-            return !(*this == string); 
-        }
-
-        /**
-         * @return StringImpl const* 
-         */
-        StringImpl const* impl() const 
-        { 
-            return m_impl; 
-        }
-
-        /**
-         * @return char const* 
-         */
-        char const* characters() const 
-        { 
-            return m_impl ? m_impl->characters() : nullptr; 
-        }
-
-        /**
-         * @return size_t 
-         */
-        size_t length() const 
-        { 
-            return m_impl ? m_impl->length() : 0; 
-        }
-
-        /**
          * @return ALWAYS_INLINE 
          */
-        ALWAYS_INLINE u32 hash() const 
-        { 
-            return m_impl ? m_impl->existing_hash() : 0; 
+        [[nodiscard]] ALWAYS_INLINE bool operator==(FlyString const& other) const
+        {
+            return m_data.raw({}) == other.m_data.raw({});
         }
 
         /**
-         * @return ALWAYS_INLINE 
+         * @return true 
+         * @return false 
          */
-        ALWAYS_INLINE StringView view() const 
-        { 
-            return m_impl ? m_impl->view() : StringView {}; 
-        }
+        [[nodiscard]] bool operator==(String const&) const;
+        [[nodiscard]] bool operator==(StringView) const;
+        [[nodiscard]] bool operator==(char const*) const;
 
         /**
-         * @return FlyString 
+         * @param other 
          */
-        FlyString to_lowercase() const;
+        [[nodiscard]] int operator<=>(FlyString const& other) const;
+
+        static void did_destroy_fly_string_data(Badge<Detail::StringData>, Detail::StringData const&);
+
+        [[nodiscard]] Detail::StringBase data(Badge<String>) const;
+
+        [[nodiscard]] static size_t number_of_fly_strings();
+
+        [[nodiscard]] DeprecatedFlyString to_deprecated_fly_string() const;
+        static ErrorOr<FlyString> from_deprecated_fly_string(DeprecatedFlyString const&);
 
         /**
          * @tparam T 
-         * @return Optional<T> 
          */
-        template<typename T = int>
-        Optional<T> to_int(TrimWhitespace = TrimWhitespace::Yes) const;
-
-        /**
-         * @tparam T 
-         * @return Optional<T> 
-         */
-        template<typename T = unsigned>
-        Optional<T> to_uint(TrimWhitespace = TrimWhitespace::Yes) const;
+        template <typename T>
+            requires(IsSame<RemoveCVReference<T>, StringView>)
+        static ErrorOr<String> from_deprecated_fly_string(T&&) = delete;
 
         /**
          * @return true 
          * @return false 
          */
-        bool equals_ignoring_case(StringView) const;
+        [[nodiscard]] bool equals_ignoring_ascii_case(FlyString const&) const;
+        [[nodiscard]] bool equals_ignoring_ascii_case(StringView) const;
 
         /**
          * @return true 
          * @return false 
          */
-        bool starts_with(StringView, CaseSensitivity = CaseSensitivity::CaseSensitive) const;
+        [[nodiscard]] bool starts_with_bytes(StringView, CaseSensitivity = CaseSensitivity::CaseSensitive) const;
 
         /**
          * @return true 
          * @return false 
          */
-        bool ends_with(StringView, CaseSensitivity = CaseSensitivity::CaseSensitive) const;
-
-        static void did_destroy_impl(Badge<StringImpl>, StringImpl&);
+        [[nodiscard]] bool ends_with_bytes(StringView, CaseSensitivity = CaseSensitivity::CaseSensitive) const;
 
         /**
          * @tparam Ts 
          * @param strings 
          * @return ALWAYS_INLINE constexpr 
          */
-        template<typename... Ts>
+        template <typename... Ts>
         [[nodiscard]] ALWAYS_INLINE constexpr bool is_one_of(Ts... strings) const
         {
             return (... || this->operator==(forward<Ts>(strings)));
         }
 
     private:
-        RefPtr<StringImpl> m_impl;
+        /**
+         * @brief Construct a new Fly String object
+         * 
+         * @param data 
+         */
+        explicit FlyString(Detail::StringBase data)
+            : m_data(move(data))
+        {
+        }
+
+        Detail::StringBase m_data;
     }; // class FlyString
 
     /**
      * @tparam  
      */
-    template<>
-    struct Traits<FlyString> : public GenericTraits<FlyString> 
+    template <>
+    struct Traits<FlyString> : public DefaultTraits<FlyString>
     {
-        static unsigned hash(FlyString const& s) { return s.hash(); }
+        static unsigned hash(FlyString const&);
     };
+
+    /**
+     * @tparam  
+     */
+    template <>
+    struct Formatter<FlyString> : Formatter<StringView>
+    {
+        ErrorOr<void> format(FormatBuilder&, FlyString const&);
+    };
+
+    struct ASCIICaseInsensitiveFlyStringTraits : public Traits<String>
+    {
+        /**
+         * @param s 
+         * @return unsigned 
+         */
+        static unsigned hash(FlyString const& s)
+        {
+            return s.ascii_case_insensitive_hash();
+        }
+
+        /**
+         * @param a 
+         * @param b 
+         * @return true 
+         * @return false 
+         */
+        static bool equals(FlyString const& a, FlyString const& b)
+        {
+            return a.equals_ignoring_ascii_case(b);
+        }
+    }; // struct ASCIICaseInsensitiveFlyStringTraits : public Traits<String>
+
 } // namespace Mods
 
+/**
+ * @param cstring 
+ * @param length 
+ * @return ALWAYS_INLINE 
+ */
+[[nodiscard]] ALWAYS_INLINE Mods::FlyString operator""_fly_string(char const* cstring, size_t length)
+{
+    return Mods::FlyString::from_utf8(Mods::StringView(cstring, length)).release_value();
+}
+
+#if USING_MODS_GLOBALLY
 using Mods::FlyString;
+#endif
