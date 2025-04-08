ef477610e0dc6045908ed5f9b95ef76f0605f04e
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-08 18:51:31 +0530

    mods: flystring

--- mods/flystring.cpp
+++ mods/flystring.cpp
@@ -9,25 +9,27 @@
  * 
  */
 
+#include <mods/deprecatedflystring.h>
 #include <mods/flystring.h>
-#include <mods/hashtable.h>
-#include <mods/optional.h>
+#include <mods/hashmap.h>
 #include <mods/singleton.h>
 #include <mods/string.h>
-#include <mods/stringutils.h>
+#include <mods/stringdata.h>
 #include <mods/stringview.h>
+#include <mods/utf8view.h>
 
-namespace Mods
+namespace Mods 
 {
-    struct FlyStringImplTraits : public Traits<StringImpl*> 
+
+    struct FlyStringTableHashTraits : public Traits<Detail::StringData const*> 
     {
         /**
-         * @param s 
-         * @return unsigned 
+         * @param string 
+         * @return u32 
          */
-        static unsigned hash(StringImpl const* s) 
+        static u32 hash(Detail::StringData const* string) 
         { 
-            return s ? s->hash() : 0; 
+            return string->hash(); 
         }
 
         /**
@@ -36,153 +38,157 @@ namespace Mods
          * @return true 
          * @return false 
          */
-        static bool equals(StringImpl const* a, StringImpl const* b)
-        {
-            VERIFY(a);
-            VERIFY(b);
-            return *a == *b;
+        static bool equals(Detail::StringData const* a, Detail::StringData const* b) 
+        { 
+            return *a == *b; 
         }
-    }; // struct FlyStringImplTraits : public Traits<StringImpl*> 
+    }; // struct FlyStringTableHashTraits : public Traits<Detail::StringData const*> 
 
-    static Singleton<HashTable<StringImpl*, FlyStringImplTraits>> s_table;
-
-    /**
-     * @return HashTable<StringImpl*, FlyStringImplTraits>& 
-     */
-    static HashTable<StringImpl*, FlyStringImplTraits>& fly_impls()
+    static auto& all_fly_strings()
     {
-        return *s_table;
+        static Singleton<HashTable<Detail::StringData const*, FlyStringTableHashTraits>> table;
+        return *table;
     }
 
     /**
-     * @param impl 
+     * @param string 
+     * @return ErrorOr<FlyString> 
      */
-    void FlyString::did_destroy_impl(Badge<StringImpl>, StringImpl& impl)
+    ErrorOr<FlyString> FlyString::from_utf8(StringView string)
     {
-        fly_impls().remove(&impl);
+        if (string.is_empty())
+            return FlyString {};
+
+        if (string.length() <= Detail::MAX_SHORT_STRING_BYTE_COUNT)
+            return FlyString { TRY(String::from_utf8(string)) };
+
+        if (auto it = all_fly_strings().find(string.hash(), [&](auto& entry) { return entry->bytes_as_string_view() == string; }); it != all_fly_strings().end())
+            return FlyString { Detail::StringBase(**it) };
+
+        return FlyString { TRY(String::from_utf8(string)) };
     }
-    
+
     /**
-     * @brief Construct a new FlyString::FlyString object
+     * @param string 
+     * @return FlyString 
+     */
+    FlyString FlyString::from_utf8_without_validation(ReadonlyBytes string)
+    {
+        if (string.is_empty())
+            return FlyString {};
+
+        if (string.size() <= Detail::MAX_SHORT_STRING_BYTE_COUNT)
+            return FlyString { String::from_utf8_without_validation(string) };
+            
+        if (auto it = all_fly_strings().find(StringView(string).hash(), [&](auto& entry) { return entry->bytes_as_string_view() == string; }); it != all_fly_strings().end())
+            return FlyString { Detail::StringBase(**it) };
+
+        return FlyString { String::from_utf8_without_validation(string) };
+    }
+
+    /**
+     * @brief Construct a new Fly String:: Fly String object
      * 
      * @param string 
      */
     FlyString::FlyString(String const& string)
     {
-        if (string.is_null())
-            return;
-
-        if (string.impl()->is_fly()) {
-            m_impl = string.impl();
+        if (string.is_short_string()) {
+            m_data = string;
             return;
         }
 
-        auto it = fly_impls().find(const_cast<StringImpl*>(string.impl()));
+        if (string.m_data->is_fly_string()) {
+            m_data = string;
+            return;
+        }
 
-        if (it == fly_impls().end()) {
-            fly_impls().set(const_cast<StringImpl*>(string.impl()));
-            string.impl()->set_fly({}, true);
-            m_impl = string.impl();
+        auto it = all_fly_strings().find(string.m_data);
+
+        if (it == all_fly_strings().end()) {
+            m_data = string;
+            all_fly_strings().set(string.m_data);
+            string.m_data->set_fly_string(true);
         } else {
-            VERIFY((*it)->is_fly());
-            m_impl = *it;
+            m_data.m_data = *it;
+            m_data.m_data->ref();
         }
     }
 
     /**
-     * @brief Construct a new FlyString::FlyString object
-     * 
      * @param string 
+     * @return FlyString& 
      */
-    FlyString::FlyString(StringView string)
+    FlyString& FlyString::operator=(String const& string)
     {
-        if (string.is_null())
-            return;
-
-        auto it = fly_impls().find(string.hash(), [&](auto& candidate) {
-            return string == candidate;
-        });
-
-        if (it == fly_impls().end()) {
-            auto new_string = string.to_string();
-            fly_impls().set(new_string.impl());
-            new_string.impl()->set_fly({}, true);
-            m_impl = new_string.impl();
-        } else {
-            VERIFY((*it)->is_fly());
-            m_impl = *it;
-        }
-    }
-    
-    /**
-     * @tparam T 
-     * @param trim_whitespace 
-     * @return Optional<T> 
-     */
-    template<typename T>
-    Optional<T> FlyString::to_int(TrimWhitespace trim_whitespace) const
-    {
-        return StringUtils::convert_to_int<T>(view(), trim_whitespace);
+        *this = FlyString { string };
+        return *this;
     }
 
-    template Optional<i8> FlyString::to_int(TrimWhitespace) const;
-    template Optional<i16> FlyString::to_int(TrimWhitespace) const;
-    template Optional<i32> FlyString::to_int(TrimWhitespace) const;
-    template Optional<i64> FlyString::to_int(TrimWhitespace) const;
-
     /**
-     * @param trim_whitespace 
-     * @return * template<typename T> 
-     */
-    template<typename T>
-    Optional<T> FlyString::to_uint(TrimWhitespace trim_whitespace) const
-    {
-        return StringUtils::convert_to_uint<T>(view(), trim_whitespace);
-    }
-
-    template Optional<u8> FlyString::to_uint(TrimWhitespace) const;
-    template Optional<u16> FlyString::to_uint(TrimWhitespace) const;
-    template Optional<u32> FlyString::to_uint(TrimWhitespace) const;
-    template Optional<u64> FlyString::to_uint(TrimWhitespace) const;
-
-    /**
-     * @param other 
      * @return true 
      * @return false 
      */
-    bool FlyString::equals_ignoring_case(StringView other) const
+    bool FlyString::is_empty() const
     {
-        return StringUtils::equals_ignoring_case(view(), other);
+        return bytes_as_string_view().is_empty();
     }
 
     /**
-     * @param str 
-     * @param case_sensitivity 
-     * @return true 
-     * @return false 
+     * @return unsigned 
      */
-    bool FlyString::starts_with(StringView str, CaseSensitivity case_sensitivity) const
+    unsigned FlyString::hash() const
     {
-        return StringUtils::starts_with(view(), str, case_sensitivity);
+        return m_data.hash();
     }
 
     /**
-     * @param str 
-     * @param case_sensitivity 
-     * @return true 
-     * @return false 
+     * @return u32 
      */
-    bool FlyString::ends_with(StringView str, CaseSensitivity case_sensitivity) const
+    u32 FlyString::ascii_case_insensitive_hash() const
     {
-        return StringUtils::ends_with(view(), str, case_sensitivity);
+        return case_insensitive_string_hash(reinterpret_cast<char const*>(bytes().data()), bytes().size());
     }
 
     /**
-     * @return FlyString 
+     * @return String 
      */
-    FlyString FlyString::to_lowercase() const
+    FlyString::operator String() const
     {
-        return String(*m_impl).to_lowercase();
+        return to_string();
+    }
+
+    /**
+     * @return String 
+     */
+    String FlyString::to_string() const
+    {
+        Detail::StringBase copy = m_data;
+        return String(move(copy));
+    }
+
+    /**
+     * @return Utf8View 
+     */
+    Utf8View FlyString::code_points() const
+    {
+        return Utf8View { bytes_as_string_view() };
+    }
+
+    /**
+     * @return ReadonlyBytes 
+     */
+    ReadonlyBytes FlyString::bytes() const
+    {
+        return bytes_as_string_view().bytes();
+    }
+
+    /**
+     * @return StringView 
+     */
+    StringView FlyString::bytes_as_string_view() const
+    {
+        return m_data.bytes();
     }
 
     /**
@@ -192,7 +198,7 @@ namespace Mods
      */
     bool FlyString::operator==(String const& other) const
     {
-        return m_impl == other.impl() || view() == other.view();
+        return m_data == other;
     }
 
     /**
@@ -202,7 +208,7 @@ namespace Mods
      */
     bool FlyString::operator==(StringView string) const
     {
-        return view() == string;
+        return bytes_as_string_view() == string;
     }
 
     /**
@@ -212,6 +218,120 @@ namespace Mods
      */
     bool FlyString::operator==(char const* string) const
     {
-        return view() == string;
+        return bytes_as_string_view() == string;
     }
+
+    /**
+     * @param string_data 
+     */
+    void FlyString::did_destroy_fly_string_data(Badge<Detail::StringData>, Detail::StringData const& string_data)
+    {
+        all_fly_strings().remove(&string_data);
+    }
+
+    /**
+     * @return Detail::StringBase 
+     */
+    Detail::StringBase FlyString::data(Badge<String>) const
+    {
+        return m_data;
+    }
+
+    /**
+     * @return size_t 
+     */
+    size_t FlyString::number_of_fly_strings()
+    {
+        return all_fly_strings().size();
+    }
+
+    /**
+     * @return DeprecatedFlyString 
+     */
+    DeprecatedFlyString FlyString::to_deprecated_fly_string() const
+    {
+        return DeprecatedFlyString(bytes_as_string_view());
+    }
+
+    /**
+     * @param deprecated_fly_string 
+     * @return ErrorOr<FlyString> 
+     */
+    ErrorOr<FlyString> FlyString::from_deprecated_fly_string(DeprecatedFlyString const& deprecated_fly_string)
+    {
+        return FlyString::from_utf8(deprecated_fly_string.view());
+    }
+
+    /**
+     * @param fly_string 
+     * @return unsigned 
+     */
+    unsigned Traits<FlyString>::hash(FlyString const& fly_string)
+    {
+        return fly_string.hash();
+    }
+
+    /**
+     * @param other
+     */
+    int FlyString::operator<=>(FlyString const& other) const
+    {
+        return bytes_as_string_view().compare(other.bytes_as_string_view());
+    }
+
+    /**
+     * @param builder 
+     * @param fly_string 
+     * @return ErrorOr<void> 
+     */
+    ErrorOr<void> Formatter<FlyString>::format(FormatBuilder& builder, FlyString const& fly_string)
+    {
+        return Formatter<StringView>::format(builder, fly_string.bytes_as_string_view());
+    }
+
+    /**
+     * @param other 
+     * @return true 
+     * @return false 
+     */
+    bool FlyString::equals_ignoring_ascii_case(FlyString const& other) const
+    {
+        if (*this == other)
+            return true;
+
+        return StringUtils::equals_ignoring_ascii_case(bytes_as_string_view(), other.bytes_as_string_view());
+    }
+
+    /**
+     * @param other 
+     * @return true 
+     * @return false 
+     */
+    bool FlyString::equals_ignoring_ascii_case(StringView other) const
+    {
+        return StringUtils::equals_ignoring_ascii_case(bytes_as_string_view(), other);
+    }
+
+    /**
+     * @param bytes 
+     * @param case_sensitivity 
+     * @return true 
+     * @return false 
+     */
+    bool FlyString::starts_with_bytes(StringView bytes, CaseSensitivity case_sensitivity) const
+    {
+        return bytes_as_string_view().starts_with(bytes, case_sensitivity);
+    }
+
+    /**
+     * @param bytes 
+     * @param case_sensitivity 
+     * @return true 
+     * @return false 
+     */
+    bool FlyString::ends_with_bytes(StringView bytes, CaseSensitivity case_sensitivity) const
+    {
+        return bytes_as_string_view().ends_with(bytes, case_sensitivity);
+    }
+
 } // namespace Mods
