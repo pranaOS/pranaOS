e53a061eac722ad6bf36b4219a049e80219aabbe
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-16 11:58:46 +0530

    dvcs: commands[init]

--- dvcs/src/commands/init.rs
+++ dvcs/src/commands/init.rs
@@ -8,3 +8,22 @@
  * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
  *
  */
+
+use std::fs;
+use std::path::Path;
+
+pub fn run() -> anyhow::Result<()> {
+    let dvcs_dir = Path::new(".dvcs");
+
+    if dvcs_dir.exists() {
+        println!("Repository already initialized.");
+    } else {
+        fs::create_dir_all(dvcs_dir.join("objects"))?;
+        fs::create_dir_all(dvcs_dir.join("refs"))?;
+        fs::write(dvcs_dir.join("HEAD"), b"ref: refs/heads/master\n")?;
+
+        println!("Initialized empty DVCS repository.");
+    }
+
+    Ok(())
+}
