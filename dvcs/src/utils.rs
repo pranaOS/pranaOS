d7527ffa36d3824c6eae24dedbbb3e1c2e6fa16d
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-16 12:55:01 +0530

    dvcs: utils

--- dvcs/src/utils.rs
+++ dvcs/src/utils.rs
@@ -9,6 +9,35 @@
  *
  */
 
-pub fn print_banner() {
-    println!("DVCS - Distributed Version Control System");
+use blake3::Hasher;
+use flate2::{Compression, write::ZlibEncoder, read::ZlibDecoder};
+use std::fs;
+use std::io::{Read, Write};
+use std::path::PathBuf;
+
+pub fn hash_data(data: &[u8]) -> String {
+    let mut hasher = Hasher::new();
+    hasher.update(data);
+    hasher.finalize().to_hex().to_string()
+}
+
+pub fn compress(data: &[u8]) -> anyhow::Result<Vec<u8>> {
+    let mut encoder = ZlibEncoder::new(Vec::new(), Compression::default());
+    encoder.write_all(data)?;
+    Ok(encoder.finish()?)
+}
+
+pub fn decompress(data: &[u8]) -> anyhow::Result<Vec<u8>> {
+    let mut decoder = ZlibDecoder::new(data);
+    let mut decoded = Vec::new();
+    decoder.read_to_end(&mut decoded)?;
+    Ok(decoded)
+}
+
+pub fn object_path(hash: &str) -> PathBuf {
+    let (dir, file) = hash.split_at(2);
+    let mut path = PathBuf::from(".dvcs/objects");
+    path.push(dir);
+    path.push(file);
+    path
 }
