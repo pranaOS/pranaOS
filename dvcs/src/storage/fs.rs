6668fd0e57dc54ba08e12bd89e017efda8971ce5
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-16 17:24:25 +0530

    dvcs: storage[fs]

---
+++ dvcs/src/storage/fs.rs
@@ -0,0 +1,29 @@
+/**
+ * @file fs.rs
+ * @author Krisna Pranav
+ * @brief fs
+ * @version 6.0
+ * @date 2023-06-28
+ *
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ *
+ */
+
+use std::fs::{self, File};
+use std::path::Path;
+use std::io::{self, Write};
+use crate::objects::commit::Commit;
+
+pub fn save_commit(commit: Commit) -> io::Result<()> {
+    let path = Path::new(".dvcs/commits");
+    if !path.exists() {
+        fs::create_dir_all(path)?;
+    }
+
+    let commit_path = path.join("commit_1234");  // Placeholder for commit ID
+    let mut file = File::create(commit_path)?;
+    // file.write_all(&commit.serialize())?;
+    file.write(&commit.serialize()?.into_bytes())?;
+
+    Ok(())
+}
