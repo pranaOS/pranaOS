f5aa08869e8e5b690d2b173d68d41c5dea4d662b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-16 12:07:17 +0530

    dvcs: utils

---
+++ dvcs/src/utils.rs
@@ -0,0 +1,14 @@
+/**
+ * @file utils.rs
+ * @author Krisna Pranav
+ * @brief utils
+ * @version 6.0
+ * @date 2023-06-28
+ *
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ *
+ */
+
+pub fn print_banner() {
+    println!("DVCS - Distributed Version Control System");
+}
