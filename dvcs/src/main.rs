2565e73a1e716b8a70121007672be35f0ad09d42
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-16 12:55:23 +0530

    dvcs: main

--- dvcs/src/main.rs
+++ dvcs/src/main.rs
@@ -1,8 +1,46 @@
-mod commands;
-mod core;
-mod utils;
-mod object;
+/**
+ * @file main.rs
+ * @author Krisna Pranav
+ * @brief utils
+ * @version 6.0
+ * @date 2023-06-28
+ *
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ *
+ */
 
-fn main() {
-    println!("Hello, world!");
+mod commands;
+mod object;
+mod utils;
+mod core;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "dvcs")]
+#[command(about = "A simple Git-inspired DVCS written in Rust")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Init,
+    Commit { message: String },
+    Log,
+    Status,
+}
+
+fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Init => commands::init::run()?,
+        Commands::Commit { message } => commands::commit::run(&message)?,
+        Commands::Log => commands::log::run()?,
+        Commands::Status => commands::status::run()?,
+    }
+
+    Ok(())
 }
