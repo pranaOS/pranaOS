3f008ed573dcafcadf102b05397f459e97d09ef4
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-16 12:09:58 +0530

    dvcs: commands[log + mod]

--- dvcs/src/commands/log.rs
+++ dvcs/src/commands/log.rs
@@ -1,15 +1,10 @@
 /**
- * @file mod.rs
+ * @file log.rs
  * @author Krisna Pranav
- * @brief mod
+ * @brief log[similar to git log]
  * @version 6.0
  * @date 2023-06-28
  *
  * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
  *
  */
-
-pub mod init;
-mod add;
-mod commit;
-mod log;
