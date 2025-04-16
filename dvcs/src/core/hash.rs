5b97eca025d4d6279f41e8598c2207bc0d201c36
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-16 12:04:15 +0530

    dvcs: core[hash]

--- dvcs/src/core/hash.rs
+++ dvcs/src/core/hash.rs
@@ -8,3 +8,12 @@
  * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
  *
  */
+
+use sha2::{Sha256, Digest};
+
+pub fn hash_blob(data: &[u8]) -> String {
+    let mut hasher = Sha256::new();
+    hasher.update(data);
+    let result = hasher.finalize();
+    hex::encode(result)
+}
