780758d5660d6d7bd3a914d3eea0335db9f01d00
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-16 11:57:04 +0530

    dvcs: commands[init + mod]

---
+++ dvcs/src/commands/init.rs
@@ -0,0 +1,10 @@
+/**
+ * @file init.rs
+ * @author Krisna Pranav
+ * @brief init[similar to git init]
+ * @version 6.0
+ * @date 2023-06-28
+ *
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ *
+ */
--- dvcs/src/main.rs
+++ dvcs/src/main.rs
@@ -1,3 +1,5 @@
+mod commands;
+
 fn main() {
     println!("Hello, world!");
 }
