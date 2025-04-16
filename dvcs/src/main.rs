2ea4415f2e2fbc6045f9f198867b57f785c94b0e
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-16 12:43:04 +0530

    dvcs: object

--- dvcs/src/main.rs
+++ dvcs/src/main.rs
@@ -1,6 +1,7 @@
 mod commands;
 mod core;
 mod utils;
+mod object;
 
 fn main() {
     println!("Hello, world!");
---
+++ dvcs/src/object/blob.rs
@@ -0,0 +1,10 @@
+/**
+ * @file blob.rs
+ * @author Krisna Pranav
+ * @brief blob
+ * @version 6.0
+ * @date 2023-06-28
+ *
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ *
+ */
