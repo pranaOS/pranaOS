9e2de44dbea44fcc23b967f3056a03fb4f6bb7c7
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-18 14:14:21 +0530

    libraries: libbrush[lib(Matrix)]

--- userland/libraries/libbrush/src/lib.rs
+++ userland/libraries/libbrush/src/lib.rs
@@ -1,1 +1,25 @@
+/**
+ * @file lib.rs
+ * @author Krisna Pranav
+ * @version 6.0
+ * @date 2023-06-28
+ *
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ *
+ */
+
 mod errors;
+
+pub use glyph_brush;
+
+pub type Matrix = [[f32; 4]; 4];
+
+#[rustfmt::skip]
+pub fn ortho(width: f32, height: f32) -> Matrix {
+    [
+        [2.0 / width, 0.0,          0.0, 0.0],
+        [0.0,        -2.0 / height, 0.0, 0.0],
+        [0.0,         0.0,          1.0, 0.0],
+        [-1.0,        1.0,          0.0, 1.0]
+    ]
+}
