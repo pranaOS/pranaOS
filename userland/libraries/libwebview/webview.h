1250a1fa004fbe0d177529f977b7ff9452305a7e
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-23 18:51:39 +0530

    libraries: libwebview[webview]

---
+++ userland/libraries/libwebview/webview.h
@@ -0,0 +1,23 @@
+/**
+ * @file webview.h
+ * @author Krisna Pranav
+ * @brief webview
+ * @version 6.0
+ * @date 2025-04-23
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#ifndef WIN32
+#define LIB_WEBVIEW_API __declspec(dllexport)
+#else
+#define LIB_WEBVIEW_API
+#endif
+
+extern "C"
+{
+    
+}
