1296d991d4371ca7caaa29db65be59fa692e733d
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-28 13:46:30 +0530

    libraries: libwebview[injection]

---
+++ userland/libraries/libwebview/injection.hpp
@@ -0,0 +1,20 @@
+/**
+ * @file injection.hpp
+ * @author Krisna Pranav
+ * @brief injection
+ * @version 6.0
+ * @date 2025-04-28
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+namespace LibWebView
+{
+    namespace JS
+    {
+
+    } // namespace JS
+} // namespace LibWebView
