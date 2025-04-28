95d57164f68fefacfac46acf8459b1fe99cd3bae
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-28 13:48:00 +0530

    libraries: libwebview[injection]

--- userland/libraries/libwebview/injection.hpp
+++ userland/libraries/libwebview/injection.hpp
@@ -15,8 +15,96 @@ namespace LibWebView
 {
     namespace JS
     {
-        std::string const onLoadHTMLInjection = R"()";
+        std::string const onLoadHTMLInjection = R"(
+            class Queue {{
+                constructor() {{
+                    this.elements = {{}};
+                    this.head = 0;
+                    this.tail = 0;
+                }}
 
-        std::string const onResultResolveInjection = "webview.results";
+                enqueue(element) {{
+                    this.elements[this.tail] = element;
+                    this.tail++;
+                }}
+
+                dequeue() {{
+                    const item = this.elements[this.head];
+                    delete this.elements[this.head];
+                    this.head++;
+                    return item;
+                }}
+
+                peek() {{
+                    return this.elements[this.head];
+                }}
+
+                length() {{
+                    return this.tail - this.head;
+                }}
+
+                isEmpty() {{
+                    return this.length == 0;
+                }}
+            }}
+
+            class IndexAllocator {{
+                constructor(count) {{
+                    this.queue = new Queue();
+
+                    for(let i = 0; i < count; i++) {{
+                        this.queue.enqueue(i);
+                    }}
+                }}
+
+                allocate() {{
+                    return this.queue.dequeue();
+                }}
+
+                deallocate(element) {{
+                    this.queue.enqueue(element);
+                }}
+            }}
+
+            class WebView {{
+                static MAX_RESULTS = 1000;
+
+                constructor() {{
+                    this.results = {{}};
+                    this.allocator = new IndexAllocator(WebView.MAX_RESULTS);
+                }}
+
+                __free_result__(index) {{
+                    this.allocator.deallocate(index);
+                }}
+
+                invoke(name, ...args) {{
+                    const index = this.allocator.allocate();
+
+                    let promise = new Promise((resolve, reject) => {{
+                            this.results[index] = {{
+                            resolve: resolve,
+                            reject: reject
+                        }};
+                    }});
+
+                    {0}.postMessage(
+                        JSON.stringify({{
+                            index: index,
+                            func: name,
+                            args: Array.from(args)
+                        }})
+                    );
+                    return promise;
+                }}
+            }}
+
+            let webview = new WebView();
+        )";
+
+        std::string const onResultResolveInjection = "webview.results[{0}].resolve({1}); webview.__free_result__({0});";
+
+        std::string const onResultRejectInjection = "webview.results[{0}].reject({1}); webview.__free_result__({0});";
     } // namespace JS
+
 } // namespace LibWebView
