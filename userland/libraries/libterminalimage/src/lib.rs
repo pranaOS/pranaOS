d50bf92a63ae0c55e4b3a225b57783a10313ed39
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-05-02 12:37:32 +0530

    libraries: libterminalimage[lib]

--- userland/libraries/libterminalimage/src/lib.rs
+++ userland/libraries/libterminalimage/src/lib.rs
@@ -1,14 +1,39 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+/**
+ * @file lib.rs
+ * @author Krisna Pranav
+ * @version 6.0
+ * @date 2024-05-2
+ *
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ *
+ */
+
+use ansi_escapes::{CursorMove, CursorTo};
+use colored::Colorize;
+use image::{DynamicImage, GenericImageView, Pixel, Rgba};
+
+pub struct Image {
+    pub path: String,
+    image: Option<DynamicImage>,
+    cached_image: Option<(u16, u16, Vec<String>)>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Image {
+    pub fn new(path: &str) -> Image {
+        Image {
+            path: path.to_owned(),
+            image: None,
+            cached_image: None,
+        }
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn generated_cache_image(&self, width: u16, height: u16, image: &DynamicImage) -> (u16, u16, Vec<String>) {
+        let pixels: Vec<_> = image.pixels().collect();
+        let (image_width, image_height) = image.dimensions();
+        return Image::transform_pixels_to_lines(width, height, &pixels, image_width, image_height);
+    }
+
+    fn transform_pixels_to_lines(width: u16, height: u16, pixels: &[(u32, u32, Rgba<u8>)], image_width: u32, image_height: u32) {
+
     }
 }
