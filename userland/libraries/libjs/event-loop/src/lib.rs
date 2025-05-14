d4a046caf4b02bd713dae5014437f97bea6dc4f1
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-05-14 15:51:04 +0530

    libraries: libjs[event-loop(lib)]

--- userland/libraries/libjs/event-loop/src/lib.rs
+++ userland/libraries/libjs/event-loop/src/lib.rs
@@ -1,14 +1,33 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::time::Duration;
+use downcast_rs::{impl_downcast, Downcast};
+/**
+ * @file lib.rs
+ * @author Krisna Pranav
+ * @version 6.0
+ * @date 2025-05-14
+ *
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ *
+ */
+
+#[cfg(target_family = "unix")]
+use signal_hook_mio::v0_8::Signals;
+
+pub type Index = u32;
+
+trait Resource: Downcast + 'static {
+    fn close(&mut self) {}
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl_downcast!(Resource);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+struct TimerWrap {
+    cb: Box<dyn FnMut(LoopHandle) + 'static>,
+    expires_at: Duration,
+    repeat: bool
+}
+
+impl Resource for TimerWrap {}
+
+struct TaskWrap {
 }
