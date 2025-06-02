1bbc055a470a8c9d2190d6e729adb9afb91f6bfe
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-06-02 11:17:29 +0530

    uni-kernel: klib[kbli(enum values)]

---
+++ uni-kernel/klib/klib.h
@@ -0,0 +1,17 @@
+/**
+ * @file klib.h
+ * @author Krisna Pranav
+ * @version 6.0
+ * @date 2025-06-02
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+enum {
+    KLIB_INIT_OK = 0,
+    KLIB_LOAD_FAILED,
+    KLIB_MISSING_DEP,
+    KLIB_INIT_IN_PROGRESS,
+    KLIB_INIT_FAILED
+};
