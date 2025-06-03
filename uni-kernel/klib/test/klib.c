8b523805ce3148146446107cbd299ba9bff8ab3d
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-06-03 18:30:00 +0530

    uni-kernel: klib[test(klib + lock)]

---
+++ uni-kernel/klib/test/klib.c
@@ -0,0 +1,37 @@
+#include <kernel.h>
+
+#define Z_LEN 1024
+
+int y = 123;
+unsigned long z[Z_LEN];
+
+/**
+ * @param x
+ * @return int
+ */
+int foo(int x)
+{
+    z[0] = x;
+    return z[0] + y;
+}
+
+/**
+ * @param complete
+ * @return int
+ */
+int init(status_handler complete)
+{
+    for(int i = 0; i < Z_LEN; i++)
+        if(z[i] != 0)
+            return KLIB_INIT_FAILED;
+
+    int r = foo(1);
+
+    if(r != 124)
+        return KLIB_INIT_FAILED;
+
+    unsigned long a = -1ull;
+    runtime_memset((void*)&a, 0, sizeof(unsigned long));
+
+    return a == 0 ? KLIB_INIT_OK : KLIB_INIT_FAILED;
+}
---
+++ uni-kernel/klib/test/lock.c
@@ -0,0 +1,76 @@
+/**
+ * @file lock.c
+ * @author Krisna Pranav
+ * @version 6.0
+ * @date 2025-06-04
+ *
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ *
+ */
+
+#include <kernel.h>
+
+static boolean klib_test_rw_spinlock(void)
+{
+    struct rw_spinlock l;
+    spin_rw_lock_init(&l);
+
+    if(!spin_tryrlock(&l))
+    {
+        msg_err("%s: couldn't rlock unlocked spinlock", func_ss);
+        return false;
+    }
+
+    if(spin_trywlock(&l))
+    {
+        msg_err("%s: could wlock rlocked spinlock", func_ss);
+        return false;
+    }
+
+#if defined(SMP_ENABLE)
+    if(!spin_tryrlock(&l))
+    {
+        msg_err("%s: couldn't rlock rlocked spinlock", func_ss);
+        return false;
+    }
+
+    spin_runlock(&l);
+#endif
+    spin_runlock(&l);
+
+    if(!spin_trywlock(&l))
+    {
+        msg_err("%s: couldn't wlock unlocked spinlock", func_ss);
+        return false;
+    }
+
+    if(spin_tryrlock(&l))
+    {
+        msg_err("%s: could rlock wlocked spinlock", func_ss);
+        return false;
+    }
+
+    if(spin_trywlock(&l))
+    {
+        msg_err("%s: could wlock wlocked spinlock", func_ss);
+        return false;
+    }
+
+    spin_wunlock(&l);
+
+    return true;
+}
+
+/**
+ * @param complete
+ * @return int
+ */
+int init(status_handler complete)
+{
+    if(!klib_test_rw_spinlock())
+        return KLIB_INIT_FAILED;
+
+    rprintf("Lock test OK\n");
+
+    return KLIB_INIT_OK;
+}
