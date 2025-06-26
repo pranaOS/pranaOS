6ebdd2ac74c1635611918b108e09053c8b56b048
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-06-26 11:22:18 +0530

    uni-kernel: virtio[virtio_internal]

---
+++ uni-kernel/src/virtio/virtio_internal.h
@@ -0,0 +1,10 @@
+/**
+ * @file virtio_internal.h
+ * @author Krisna Pranav
+ * @brief virtio internal
+ * @version 6.0
+ * @date 2025-06-26
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
