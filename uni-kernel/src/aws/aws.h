be7d6514c87b58f4310eaed75cb9dbc09b14fc93
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-07-09 17:03:57 +0530

    aws: aws[init_aws_ena]

---
+++ uni-kernel/src/aws/aws.h
@@ -0,0 +1,15 @@
+/**
+ * @file aws.h
+ * @author Krisna Pranav
+ * @brief aws
+ * @version 6.0
+ * @date 2025-07-09
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+/**
+ * @param kh 
+ */
+void init_aws_ena(kernel_heaps kh);
