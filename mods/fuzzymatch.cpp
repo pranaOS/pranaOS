ed20eb1490146c4751bacd2bf71a5ad9750db4c8
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-07-21 18:03:49 +0530

    mods: fuzzymatch

---
+++ mods/fuzzymatch.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file fuzzymatch.cpp
+ * @author Krisna Pranav
+ * @brief fuzzy match
+ * @version 6.0
+ * @date 2025-07-21
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#include <string.h>
+
+namespace Mods
+{
+    static constexpr int const RECURSION_LIMIT = 10;
+    static constexpr int const MAX_MATCHES = 256;
+
+    /**
+     * @param string 
+     * @param index_pointer 
+     * @return int 
+     */
+    static int calculate_score(StringView string, u8* index_pointer)
+    {
+
+    }
+} // namespace Mods
