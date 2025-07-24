4ba88dfbcb49c8663a3717f8ffbacc9833a19216
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-07-24 18:29:35 +0530

    kernel: api[archctl_numbers]

---
+++ kernel/api/archctl_numbers.h
@@ -0,0 +1,14 @@
+/**
+ * @file archctl_numbers.h
+ * @author Krisna Pranav
+ * @brief archctl numbers
+ * @version 6.0
+ * @date 2025-07-24
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+#pragma once
+
+#define ARCHCTL_X86_64_SET_FS_BASE_FOR_CURRENT_THREAD 1
