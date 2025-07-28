54c73675732e2c91f2290590c55aa17a65745a4a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-07-28 12:00:00 +0530

    unix_process: unix process runtime

---
+++ uni-kernel/src/unix_process/unix_process_runtime.h
@@ -0,0 +1,12 @@
+/**
+ * @file unix_process_runtime.h
+ * @author Krisna Pranav
+ * @brief Unix Process Runtime
+ * @version 6.0
+ * @date 2025-07-29
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+*/
+
+typedef int descriptor;
