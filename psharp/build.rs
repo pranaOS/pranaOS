734b5a295c9d7b97d9ecfbc82a643bc2a299c982
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-07-30 19:09:43 +0530

    psharp: build

--- psharp/build.rs
+++ psharp/build.rs
@@ -1,3 +1,15 @@
+/**
+ * @file build.rs
+ * @author Krisna Pranav
+ * @brief buildrs
+ * @version 6.0
+ * @date 2025-06-15
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+
 fn main() {
     lalrpop::Configuration::new()
         .use_cargo_dir_conventions()
