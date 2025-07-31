721e310c8b72b7d7680e4b408719fbff0631b8e1
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-07-31 20:35:36 +0530

    psharp: lib

--- psharp/src/lib.rs
+++ psharp/src/lib.rs
@@ -1,3 +1,14 @@
+/**
+ * @file lib.rs
+ * @author Krisna Pranav
+ * @version 6.0
+ * @date 2023-06-28
+ *
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ *
+ */
+
+
 mod types;
 mod error;
 
