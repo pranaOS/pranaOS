de7fae8d36cb39e7c47f4338443ec687d534bdbd
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-08-02 12:00:00 +0530

    Backdated commit for August 2, 2025

---
+++ uni-kernel/src/net/stdlib.h
@@ -0,0 +1,11 @@
+/**
+ * @file stdlib.h
+ * @author Krisna Pranav
+ * @brief stdlib
+ * @version 6.0
+ * @date 2025-08-03
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
