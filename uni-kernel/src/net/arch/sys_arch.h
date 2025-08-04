0268cbc68ebe3a9f8256072cdf7c0d7a46797beb
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-08-04 18:20:57 +0530

    uni-kernel: arch[sys_arch]

---
+++ uni-kernel/src/net/arch/sys_arch.h
@@ -0,0 +1,12 @@
+/**
+ * @file sys_arch.h
+ * @author Krisna Pranav
+ * @brief sys arch
+ * @version 6.0
+ * @date 2025-08-04
+ * 
+ * @copyright Copyright (c) 2021-2025 pranaOS Developers, Krisna Pranav
+ * 
+ */
+
+typedef void* sys_prot_t;
