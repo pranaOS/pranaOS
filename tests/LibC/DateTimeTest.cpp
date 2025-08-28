24105f41ef22c43dff00105b1732f8a35f7576e7
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-08-28 12:00:00 +0530

    tests: Libc[DateTimeTest]

--- tests/LibC/DateTimeTest.cpp
+++ tests/LibC/DateTimeTest.cpp
@@ -3,8 +3,25 @@
 
 #include <datetime.h>
 #include <syscall.h>
+#include <iostream>
+#include <cassert>
 
+using namespace Core;
 using namespace LibC;
+
+void test_create_and_accessors()
+{
+    auto dt = DateTime::create(2025, 9, 1, 12, 30, 45);
+
+    assert(dt.year() == 2025);
+    assert(dt.month() == 9);
+    assert(dt.day() == 1);
+    assert(dt.hour() == 12);
+    assert(dt.minute() == 30);
+    assert(dt.second() == 45);
+
+    std::cout << "[PASS] test_create_and_accessors\n";
+}
 
 Time dateTime::current() {
     Time result;
