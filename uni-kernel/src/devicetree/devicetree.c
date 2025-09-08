5ac629a6b80962234d5ba59d433d8df718bdcfbf
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-09-08 12:00:00 +0530

    uni-kernel: devicetree

--- uni-kernel/src/devicetree/devicetree.c
+++ uni-kernel/src/devicetree/devicetree.c
@@ -18,3 +18,23 @@
 #define FDT_PROP 0x00000003
 #define FDT_NOP 0x00000004
 #define FDT_END 0x00000009
+
+typedef struct dt_header
+{
+    u32 magic;
+    u32 totalsize;
+    u32 off_dt_struct;
+    u32 off_dt_strings;
+    u32 off_mem_rsvmap;
+    u32 version;
+    u32 last_comp_version;
+    u32 boot_cpuid_phys;
+    u32 size_dt_strings;
+    u32 size_dt_struct;
+}* dt_header;
+
+typedef struct dt_reserve_entry
+{
+    u64 address;
+    u64 size;
+}* dt_reserve_entry;
